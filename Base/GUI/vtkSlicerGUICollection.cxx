d0c0bd297d068f74691fd4cea81a99b730a2f798
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-02-10 18:32:10 +0000

    ENH: draft Slicer GUI class definitions
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerGUICollection.cxx
@@ -0,0 +1,22 @@
+
+#include "vtkSlicerGUICollection.h"
+#include "vtkObjectFactory.h"
+
+
+//---------------------------------------------------------------------------
+vtkStandardNewMacro(vtkSlicerGUICollection);
+vtkCxxRevisionMacro(vtkSlicerGUICollection, "$Revision: 1.0 $");
+
+
+
+//---------------------------------------------------------------------------
+vtkSlicerGUICollection::vtkSlicerGUICollection ( ) {
+}
+
+
+
+//---------------------------------------------------------------------------
+vtkSlicerGUICollection::~vtkSlicerGUICollection ( ) {
+}
+
+
