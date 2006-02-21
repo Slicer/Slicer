78c4d92058c4225260de0339bc5dcbc9141dc13f
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-02-21 21:39:28 +0000

    ENH: modified for 2nd test (doesn't compile)
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=58
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@58 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerLogicUpdate.cxx
@@ -0,0 +1,20 @@
+
+#include "vtkObjectFactory.h"
+#include "vtkObject.h"
+#include "vtkSlicerLogicUpdate.h"
+
+
+//---------------------------------------------------------------------------
+vtkCxxRevisionMacro ( vtkSlicerLogicUpdate, "$Revision: 1.0 $" );
+
+
+//---------------------------------------------------------------------------
+vtkSlicerLogicUpdate::vtkSlicerLogicUpdate ( ) {
+}
+
+
+//---------------------------------------------------------------------------
+vtkSlicerLogicUpdate::~vtkSlicerLogicUpdate ( ) {
+}
+
+
