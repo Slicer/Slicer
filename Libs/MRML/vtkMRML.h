7a1c736e197f13876ee90cbd7fc816aa012fa547
Steve Pieper <pieper@bwh.harvard.edu>
2006-02-22 00:06:17 +0000

    COMP: changes to make slicer3 app compile and run on windows
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=69
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@69 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/MRML/vtkMRML.h
@@ -0,0 +1,28 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   MRML
+  Module:    $RCSfile: vtkMRML.h,v $
+  Date:      $Date: 2006/01/06 17:56:51 $
+  Version:   $Revision: 1.6 $
+
+=========================================================================auto=*/
+
+/*
+ * This is needed for loading mrml code as module.
+ */
+
+
+//
+// use an ifdef on MRML_VTK5 to flag code that won't
+// compile on vtk4.4 and before
+//
+#if ( (VTK_MAJOR_VERSION >= 5) || ( VTK_MAJOR_VERSION == 4 && VTK_MINOR_VERSION >= 5 ) )
+#define MRML_VTK5
+#endif
+
+#include "vtkMRMLWin32Header.h"
---
+++ Libs/MRML/vtkMRMLWin32Header.h
@@ -0,0 +1,34 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D MRML
+  Module:    $RCSfile: vtkMRMLWin32Header.h,v $
+  Date:      $Date: 2006/01/06 17:56:51 $
+  Version:   $Revision: 1.4 $
+
+=========================================================================auto=*/
+// .NAME vtkMRMLWin32Header - manage Windows system differences
+// .SECTION Description
+// The vtkMRMLWin32Header captures some system differences between Unix
+// and Windows operating systems. 
+
+#ifndef __vtkMRMLWin32Header_h
+#define __vtkMRMLWin32Header_h
+
+#include <vtkMRMLConfigure.h>
+
+#if defined(WIN32) && !defined(MRML_STATIC)
+#if defined(MRML_EXPORTS)
+#define VTK_MRML_EXPORT __declspec( dllexport ) 
+#else
+#define VTK_MRML_EXPORT __declspec( dllimport ) 
+#endif
+#else
+#define VTK_MRML_EXPORT
+#endif
+
+#endif
