ff9ac99a7f8af08701c51c88b1f6aba5dfab8ccc
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-02-25 01:59:37 +0000

    ENH: new base classes
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=92
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@92 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/vtkSlicerApplicationGUI.cxx
+++ Base/GUI/vtkSlicerApplicationGUI.cxx
@@ -13,11 +13,32 @@
 =========================================================================auto=*/
 
 #include "vtkKWApplication.h"
-#include "vtkSlicerGUICollection.h"
 #include "vtkSlicerApplicationGUI.h"
-#include "vtkSlicerComponentGUI.h"
 #include "vtkObjectFactory.h"
-#include "vtkKWWindowBase.h"
+#include "vtkCommand.h"
+#include "vtkKWApplication.h"
+#include "vtkKWScale.h"
+#include "vtkCornerAnnotation.h"
+#include "vtkImageData.h"
+#include "vtkImageViewer2.h"
+#include "vtkKWFrame.h"
+#include "vtkKWFrameWithLabel.h"
+#include "vtkKWMenuButton.h"
+#include "vtkKWNotebook.h"
+#include "vtkKWRenderWidget.h"
+#include "vtkKWScale.h"
+#include "vtkKWWindow.h"
+#include "vtkKWWindowLevelPresetSelector.h"
+#include "vtkObjectFactory.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkKWLoadSaveButton.h"
+#include "vtkKWLoadSaveDialog.h"
+#include "vtkToolkits.h"
+#include "vtkSlicerApplicationGUI.h"
+#include "vtkSlicerApplicationLogic.h"
+#include "vtkMRMLVolumeNode.h"
+
 
 //---------------------------------------------------------------------------
 vtkStandardNewMacro (vtkSlicerApplicationGUI);
@@ -25,19 +46,14 @@ vtkCxxRevisionMacro(vtkSlicerApplicationGUI, "$Revision: 1.0 $");
 
 
 //---------------------------------------------------------------------------
-vtkSlicerApplicationGUI::vtkSlicerApplicationGUI ( ) {
+vtkSlicerApplicationGUI::vtkSlicerApplicationGUI (  ) {
 
-    NumberOfGUIs = 0;
-    // Create the application
-    this->KWApplication = vtkKWApplication::New ( );
-    this->KWApplication->SetName ( "3D Slicer Version 3.0 Alpha" );
-    this->KWApplication->RestoreApplicationSettingsFromRegistry ( );
-    this->KWApplication->SetHelpDialogStartingPage ( "http://www.slicer.org" );
-    this->GUICollection = vtkSlicerGUICollection::New ( );
-
+    this->Scale = NULL;
+    this->FileBrowseButton = NULL;
+    this->ImageViewer = NULL;
+    this->WindowLevelPresetSelector = NULL;
+    this->RenderWidget = NULL;
     this->Logic = NULL;
-
-    // could initialize Tcl here, no?
 
 }
 
@@ -45,73 +61,169 @@ vtkSlicerApplicationGUI::vtkSlicerApplicationGUI ( ) {
 //---------------------------------------------------------------------------
 vtkSlicerApplicationGUI::~vtkSlicerApplicationGUI ( ) {
 
-    if ( this->GUICollection ) {
-       this->GUICollection->Delete ( );
+    if ( this->Scale ) {
+        this->Scale->Delete ( );
     }
-    if ( this->KWApplication ) {
-        this->KWApplication->Delete ( );
+    if (this->FileBrowseButton ) {
+        this->FileBrowseButton->Delete ( );
     }
-
+    if (this->ImageViewer) {
+        this->ImageViewer->Delete ( );
+    }
+    if (this->WindowLevelPresetSelector ) {
+        this->WindowLevelPresetSelector->Delete ( );
+    }
+    if (this->RenderWidget ) {
+        this->RenderWidget->Delete ( );
+    }
+    this->Logic = NULL;
 }
 
 
 
-//---------------------------------------------------------------------------
-void vtkSlicerApplicationGUI::AddWindow ( vtkKWWindowBase *win ) {
-    this->KWApplication->AddWindow ( win );
-}
+
 
 
 
 //---------------------------------------------------------------------------
-void vtkSlicerApplicationGUI::AddGUI ( vtkSlicerComponentGUI *gui ) {
-
-    // Create if it doesn't exist already
-    if ( this->GUICollection == NULL ) {
-        this->GUICollection = vtkSlicerGUICollection::New ( );
-    } 
-    // Add a gui
-    this->GUICollection->AddItem ( gui );
-    this->NumberOfGUIs = this->GUICollection->GetNumberOfItems ( );
-}
-
-
-
-//---------------------------------------------------------------------------
-void vtkSlicerApplicationGUI::ConfigureApplication ( ) {
-
-    this->KWApplication->PromptBeforeExitOn ( );
-    this->KWApplication->SupportSplashScreenOn ( );
-    this->KWApplication->SplashScreenVisibilityOn ( );
-    this->KWApplication->SaveUserInterfaceGeometryOn ( );
-}
-
-
-
-//---------------------------------------------------------------------------
-void vtkSlicerApplicationGUI::CloseWindows ( ) {
-    int n, i;
-    vtkKWWindowBase *win;
+void vtkSlicerApplicationGUI::AddGUIObservers ( ) {
     
-    n= this->KWApplication->GetNumberOfWindows ( );
-    for (i=0; i<n; i++) {
-        win = this->KWApplication->GetNthWindow ( n );
-        win->Close ( );
-    }
+
+    this->FileBrowseButton->AddObserver ( vtkCommand::ModifiedEvent,  (vtkCommand *)this->GUICommand );
+    this->Scale->AddObserver ( vtkCommand::ModifiedEvent, (vtkCommand *)this->GUICommand );
+
 }
+
+
 
 
 //---------------------------------------------------------------------------
-int vtkSlicerApplicationGUI::StartApplication ( ) {
+void vtkSlicerApplicationGUI::AddLogicObservers ( ) {
 
-    int ret = 0;
-
-    // Start the application & event loop here
-    this->KWApplication->Start ( );
-    ret = this->KWApplication->GetExitStatus ( );
-
-    // Clean up and exit
-    this->CloseWindows ( );
-    this->KWApplication->Delete ( );
-    return ret;
+    this->Logic->GetMRMLScene()->AddObserver ( vtkCommand::ModifiedEvent,  (vtkCommand *)this->LogicCommand );
 }
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerApplicationGUI::UpdateLogicWithGUIEvents ( vtkObject *caller,
+                                                   unsigned long event,
+                                                   void *callData ) 
+{
+
+    vtkKWScale *scalewidget = vtkKWScale::SafeDownCast(caller);
+    vtkKWLoadSaveButton *filebrowse = vtkKWLoadSaveButton::SafeDownCast(caller);
+
+    if (scalewidget == this->Scale && event == vtkCommand::ModifiedEvent )
+        {
+            // Set the current slice in the image viewer.
+            if ( this->ImageViewer->GetSlice ( ) != this->Scale->GetValue ( ) ) {
+                this->ImageViewer->SetSlice((int) this->Scale->GetValue ( ) );
+            }
+        }
+    else if (filebrowse == this->FileBrowseButton  && event == vtkCommand::ModifiedEvent )
+        {
+            // If a file has been selected for loading...
+            if ( this->FileBrowseButton->GetFileName ( ) ) {
+                this->Logic->Connect ( filebrowse->GetFileName ( ) );
+            }
+        }
+
+}
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerApplicationGUI::UpdateGUIWithLogicEvents ( vtkObject *caller,
+                                                   unsigned long event,
+                                                   void *callData ) 
+{
+
+    // process Logic changes
+    // In this example, only thing that will come from the logic is new image data.
+    vtkMRMLScene *mrml = vtkMRMLScene::SafeDownCast(caller);
+    
+    if (mrml == (this->Logic->GetMRMLScene ( ) ) && event == vtkCommand::ModifiedEvent )
+        {
+            // If the MRML scene has changed, get the 0th volume node.
+            // and set that as input into the ImageViewer.
+            vtkMRMLVolumeNode* volumenode = vtkMRMLVolumeNode::SafeDownCast (this->Logic->GetMRMLScene()->GetNthNodeByClass( 0, "vtkMRMLVolumeNode" ) );
+            if ( volumenode )
+              {
+              this->ImageViewer->SetInput ( volumenode->GetImageData( ) );
+              double *range = volumenode->GetImageData()->GetScalarRange ( );
+              this->ImageViewer->SetColorWindow ( range [1] - range [0] );
+              this->ImageViewer->SetColorLevel (0.5 * (range [1] - range [0] ));
+              }
+            else
+              {
+              this->ImageViewer->SetInput (NULL);
+              }
+            this->ImageViewer->Render();
+
+            // configure window, level, camera, etc.
+            this->RenderWidget->ResetCamera ( );
+            vtkCornerAnnotation *ca = this->RenderWidget->GetCornerAnnotation ( );
+            ca->SetImageActor (this->ImageViewer->GetImageActor ( ) );
+            ca->SetWindowLevel (this->ImageViewer->GetWindowLevel ( ) );
+
+            // set the range of the slider
+            this->Scale->SetRange ( this->ImageViewer->GetSliceMin ( ), this->ImageViewer->GetSliceMax ( ) );
+            this->Scale->SetValue (this->ImageViewer->GetSlice ( ) );
+        }
+
+}
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerApplicationGUI::BuildGUI ( ) {
+
+    // for starters, the goal is to make a simple file browse,
+    // image viewer and slice slider in the ViewFrame.
+    
+    // add a LOAD file browser (comes with a popup file browser/selector)
+    this->FileBrowseButton = vtkKWLoadSaveButton::New ( );
+    this->FileBrowseButton->SetParent (this->Parent );
+    this->FileBrowseButton->Create ( );
+    this->FileBrowseButton->SetText ("Choose a file to load");
+    //this->FileBrowseButton->GetLoadSaveDialog()->SaveDialogOff ( );
+
+    // Should be in TK format. Default is: "{{Text Document} {.txt}}"
+    this->FileBrowseButton->GetLoadSaveDialog()->SetFileTypes(
+        "{ {MRML Document} {.mrml .xml} }");
+
+    // and add a render widget
+    this->RenderWidget = vtkKWRenderWidget::New();
+    this->RenderWidget->SetParent ( this->Parent );
+    this->RenderWidget->Create();
+    this->RenderWidget->CornerAnnotationVisibilityOn();
+    
+    // Create an image viewer
+    // Use the render window and renderer of the renderwidget
+    this->ImageViewer = vtkImageViewer2::New();
+    this->ImageViewer->SetRenderWindow(this->RenderWidget->GetRenderWindow());
+    this->ImageViewer->SetRenderer(this->RenderWidget->GetRenderer());
+    this->ImageViewer->SetupInteractor( this->RenderWidget->GetRenderWindow()->GetInteractor());
+
+    // Create a scale to control the slice number displayed
+    this->Scale = vtkKWScale::New();
+    this->Scale->SetParent(this->Parent );
+    this->Scale->Create();
+
+    // Pack in the window's view frame
+    if ( this->GetApplication() != NULL ) {
+        this->GetApplication()->Script("pack %s -expand n -side top -anchor c -padx 2 -pady 2", 
+                                       this->FileBrowseButton->GetWidgetName());
+        this->GetApplication()->Script("pack %s -expand y -fill both -anchor c -expand y", 
+                                       this->RenderWidget->GetWidgetName());
+        this->GetApplication()->Script("pack %s -side top -expand n -fill x -padx 2 -pady 2", 
+                                       this->Scale->GetWidgetName());
+    }
+
+}
+
+
+
+
+
