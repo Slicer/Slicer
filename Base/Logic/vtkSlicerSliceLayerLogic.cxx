bfa15ad01223363ee58553215c2ad6a9dfa9ff5f
Steve Pieper <pieper@bwh.harvard.edu>
2006-03-24 22:56:16 +0000

    ENH: new logic to handle layers of slices through volumes
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=128
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@128 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/Logic/vtkSlicerSliceLayerLogic.cxx
@@ -0,0 +1,92 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerSliceLayerLogic.cxx,v $
+  Date:      $Date: 2006/01/06 17:56:48 $
+  Version:   $Revision: 1.58 $
+
+=========================================================================auto=*/
+
+#include "vtkObjectFactory.h"
+#include "vtkSlicerSliceLayerLogic.h"
+
+vtkCxxRevisionMacro(vtkSlicerSliceLayerLogic, "$Revision: 1.9.12.1 $");
+vtkStandardNewMacro(vtkSlicerSliceLayerLogic);
+
+//----------------------------------------------------------------------------
+vtkSlicerSliceLayerLogic::vtkSlicerSliceLayerLogic()
+{
+  this->VolumeNode = NULL;
+  this->SliceNode = NULL;
+
+  this->Reslice = vtkImageReslice::New();
+  this->MapToRGBA = vtkImageMapToRGBA::New();
+  this->MapToWindowLevelColors = vtkImageMapToWindowLevelColors::New();
+
+  this->Reslice->SetInput( this->MapToRGBA->GetOutput() );
+  this->MapToRGBA->SetInput( this->MapToWindowLevelColors->GetOutput() );
+}
+
+//----------------------------------------------------------------------------
+vtkSlicerSliceLayerLogic::~vtkSlicerSliceLayerLogic()
+{
+}
+
+
+//----------------------------------------------------------------------------
+void vtkSlicerSliceLayerLogic::SetSliceNode(vtkMRMLSliceNode *SliceNode)
+{
+  if (this->SliceNode)
+    {
+    this->SliceNode->Delete();
+    }
+  this->SliceNode = SliceNode;
+  this->SliceNode->Register(this);
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerSliceLayerLogic::SetVolumeNode(vtkMRMLVolumeNode *VolumeNode)
+{
+  if (this->VolumeNode)
+    {
+    this->VolumeNode->Delete();
+    }
+  this->VolumeNode = VolumeNode;
+  this->VolumeNode->Register(this);
+
+  if (this->VolumeNode)
+    {
+    this->Reslice->SetInput( this->VolumeNode->GetImageData() ); 
+    }
+    else
+    {
+    this->Reslice->SetInput( NULL ); 
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerSliceLayerLogic::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->vtkObject::PrintSelf(os, indent);
+
+  os << indent << "SlicerSliceLayerLogic:             " << this->GetClassName() << "\n";
+
+  os << indent << "VolumeNode: " <<
+    (this->VolumeNode ? this->VolumeNode->GetName() : "(none)") << "\n";
+  os << indent << "SliceNode: " <<
+    (this->SliceNode ? this->SliceNode->GetName() : "(none)") << "\n";
+  // TODO: fix printing of vtk objects
+  os << indent << "Reslice: " <<
+    (this->Reslice ? "this->Reslice" : "(none)") << "\n";
+  os << indent << "MapToRGBA: " <<
+    (this->MapToRGBA ? "this->MapToRGBA" : "(none)") << "\n";
+  os << indent << "MapToWindowLevelColors: " <<
+    (this->MapToWindowLevelColors ? "this->MapToWindowLevelColors" : "(none)") << "\n";
+
+}
+
---
+++ Base/Logic/vtkSlicerSliceLayerLogic.h
@@ -0,0 +1,98 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerSliceLayerLogic.h,v $
+  Date:      $Date: 2006/01/08 04:48:05 $
+  Version:   $Revision: 1.45 $
+
+=========================================================================auto=*/
+
+// .NAME vtkSlicerSliceLayerLogic - slicer logic class for slice manipulation
+// .SECTION Description
+// This class manages the logic associated with reslicing of volumes
+// (but not the GUI).  Features of the class include:
+//
+// - Reslicing 
+// -- uses the vtkImageData and IJKToRAS transform from a vtkMRMLVolumeNode 
+// -- disp
+// -- uses a current slice view specification (typically set by vtkSlicerSliceLogic)
+// - Outputs
+// -- RGBA vtkImageData for the given slice
+// -- image is mapped through current window/level and lookup table
+//
+// This class can also be used for resampling volumes for further computation.
+//
+
+
+#ifndef __vtkSlicerSliceLayerLogic_h
+#define __vtkSlicerSliceLayerLogic_h
+
+#include <stdlib.h>
+
+#include "vtkSlicerBaseLogic.h"
+#include "vtkSlicerLogic.h"
+
+#include "vtkMrml.h"
+#include "vtkMrmlVolumeNode.h"
+#include "vtkMrmlSliceNode.h"
+
+#include "vtkImageReslice.h"
+#include "vtkImageMapToRGBA.h"
+#include "vtkImageMapToWindowLevelColors.h"
+
+class VTK_SLICER_BASE_LOGIC_EXPORT vtkSlicerSliceLayerLogic : public vtkSlicerLogic 
+{
+  public:
+  
+  // The Usual vtk class functions
+  static vtkSlicerSliceLayerLogic *New();
+  vtkTypeRevisionMacro(vtkSlicerSliceLayerLogic,vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // The volume node to operate on
+  vtkGetObjectMacro (VolumeNode, vtkMRMLVolumeNode);
+  void SetVolumeNode (vtkMRMLVolumeNode *VolumeNode);
+
+  // Description:
+  // The slice node that defines the view 
+  vtkGetObjectMacro (SliceNode, vtkMRMLSliceNode);
+  void SetSliceNode (vtkMRMLSliceNode *SliceNode);
+
+  // Description:
+  // The image reslice being used
+  vtkGetObjectMacro (Reslice, vtkImageReslice);
+
+  // Description:
+  // The image map that applies the window/level
+  // this happens before the color map, so can't use the color map
+  // that's part of WindowLevelColors
+  vtkGetObjectMacro (MapToWindowLevelColors, vtkImageMapToWindowLevelColors);
+
+  // Description:
+  // The image map that applies the lookup table
+  vtkGetObjectMacro (MapToRGBA, vtkImageMapToRGBA);
+
+    
+protected:
+  vtkSlicerSliceLayerLogic();
+  ~vtkSlicerSliceLayerLogic();
+  vtkSlicerSliceLayerLogic(const vtkSlicerSliceLayerLogic&);
+  void operator=(const vtkSlicerSliceLayerLogic&);
+
+  // Description:
+  // 
+  vtkMRMLVolumeNode *VolumeNode;
+  vtkMRMLSliceNode *SliceNode;
+  vtkImageReslice *Reslice;
+  vtkImageMapToRGBA *MapToRGBA;
+  vtkImageMapToWindowLevelColors *MapToWindowLevelColors;
+};
+
+#endif
+
