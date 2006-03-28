6e6c3911b1fb89954093d9925eec3ee931a93a13
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-03-28 22:19:06 +0000

    ENH: collection of SliceWidgets
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=173
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@173 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerSliceWidgetCollection.cxx
@@ -0,0 +1,17 @@
+#include "vtkSlicerSliceWidgetCollection.h"
+#include "vtkObjectFactory.h"
+
+
+//---------------------------------------------------------------------------
+vtkStandardNewMacro (vtkSlicerSliceWidgetCollection);
+vtkCxxRevisionMacro (vtkSlicerSliceWidgetCollection, "$Revision: 1.0 $");
+
+
+//---------------------------------------------------------------------------
+vtkSlicerSliceWidgetCollection::vtkSlicerSliceWidgetCollection ( ) {
+}
+
+
+//---------------------------------------------------------------------------
+vtkSlicerSliceWidgetCollection::~vtkSlicerSliceWidgetCollection ( ) {
+}
