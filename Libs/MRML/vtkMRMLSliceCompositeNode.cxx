36233e6a60666f70128d7baa471145dcdcc03f0c
Steve Pieper <pieper@bwh.harvard.edu>
2006-03-28 22:20:22 +0000

    ENH: new node to store parameters for slice compositing
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=175
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@175 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/MRML/vtkMRMLSliceCompositeNode.cxx
@@ -0,0 +1,128 @@
+/*=auto=========================================================================
+
+Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+See Doc/copyright/copyright.txt
+or http://www.slicer.org/copyright/copyright.txt for details.
+
+Program:   3D Slicer
+Module:    $RCSfile: vtkMRMLSliceCompositeNode.cxx,v $
+Date:      $Date: 2006/03/17 15:10:10 $
+Version:   $Revision: 1.2 $
+
+=========================================================================auto=*/
+
+#include <string>
+#include <ostream>
+#include <sstream>
+
+#include "vtkObjectFactory.h"
+#include "vtkMRMLSliceCompositeNode.h"
+
+#include "vtkMatrix4x4.h"
+
+//------------------------------------------------------------------------------
+vtkMRMLSliceCompositeNode* vtkMRMLSliceCompositeNode::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLSliceCompositeNode");
+  if(ret)
+    {
+      return (vtkMRMLSliceCompositeNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLSliceCompositeNode;
+}
+
+//----------------------------------------------------------------------------
+
+vtkMRMLNode* vtkMRMLSliceCompositeNode::CreateNodeInstance()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLSliceCompositeNode");
+  if(ret)
+    {
+      return (vtkMRMLSliceCompositeNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLSliceCompositeNode;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLSliceCompositeNode::vtkMRMLSliceCompositeNode()
+{
+  this->BackgroundVolumeID = NULL;
+  this->ForegroundVolumeID = NULL;
+  this->Opacity = 0.5;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLSliceCompositeNode::~vtkMRMLSliceCompositeNode()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLSliceCompositeNode::WriteXML(ostream& of, int nIndent)
+{
+  Superclass::WriteXML(of, nIndent);
+
+  vtkIndent indent(nIndent);
+
+  of << indent << "BackgroundVolumeID='" << this->BackgroundVolumeID << "' ";
+  of << indent << "ForegroundVolumeID='" << this->ForegroundVolumeID << "' ";
+  of << indent << "Opacity='" << this->Opacity << "' ";
+
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLSliceCompositeNode::ReadXMLAttributes(const char** atts)
+{
+
+  vtkMRMLNode::ReadXMLAttributes(atts);
+
+  const char* attName;
+  const char* attValue;
+  while (*atts != NULL) 
+    {
+    attName = *(atts++);
+    attValue = *(atts++);
+    if (!strcmp(attName, "BackgroundVolumeID")) 
+      {
+      this->SetBackgroundVolumeID(attValue);
+      }
+    if (!strcmp(attName, "ForegroundVolumeID")) 
+      {
+      this->SetForegroundVolumeID(attValue);
+      }
+    if (!strcmp(attName, "Opacity")) 
+      {
+      this->SetOpacity( atof(attValue) );
+      }
+    }
+}
+
+//----------------------------------------------------------------------------
+// Copy the node's attributes to this object.
+// Does NOT copy: ID, FilePrefix, Name, SliceID
+void vtkMRMLSliceCompositeNode::Copy(vtkMRMLNode *anode)
+{
+  Superclass::Copy(anode);
+  vtkMRMLSliceCompositeNode *node = vtkMRMLSliceCompositeNode::SafeDownCast(anode);
+
+  this->SetBackgroundVolumeID(node->GetBackgroundVolumeID());
+  this->SetForegroundVolumeID(node->GetForegroundVolumeID());
+  this->SetOpacity(node->GetOpacity());
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLSliceCompositeNode::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkMRMLNode::PrintSelf(os,indent);
+
+  os << "BackgroundVolumeID: " << this->BackgroundVolumeID << "\n";
+  os << "ForegroundVolumeID: " << this->ForegroundVolumeID << "\n";
+  os << "Opacity: " << this->Opacity << "\n";
+}
+
+
+// End
---
+++ Libs/MRML/vtkMRMLSliceCompositeNode.h
@@ -0,0 +1,85 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLSliceCompositeNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:29 $
+  Version:   $Revision: 1.3 $
+
+=========================================================================auto=*/
+// .NAME vtkMRMLSliceCompositeNode - MRML node for storing a slice through RAS space
+// .SECTION Description
+// This node stores the information about how to composite two
+// vtkMRMLVolumes into a single display image
+//
+
+#ifndef __vtkMRMLSliceCompositeNode_h
+#define __vtkMRMLSliceCompositeNode_h
+
+#include "vtkMRML.h"
+#include "vtkMRMLNode.h"
+
+#include "vtkMatrix4x4.h"
+
+class VTK_MRML_EXPORT vtkMRMLSliceCompositeNode : public vtkMRMLNode
+{
+  public:
+  static vtkMRMLSliceCompositeNode *New();
+  vtkTypeMacro(vtkMRMLSliceCompositeNode,vtkMRMLNode);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+
+  // Description:
+  // Set node attributes
+  virtual void ReadXMLAttributes( const char** atts);
+
+  // Description:
+  // Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+  // Description:
+  // Copy the node's attributes to this object
+  virtual void Copy(vtkMRMLNode *node);
+
+  // Description:
+  // Get node XML tag name (like Volume, Model)
+  virtual const char* GetNodeTagName() {return "SliceComposite";};
+
+  // Description:
+  // the ID of a MRMLVolumeNode
+  vtkGetStringMacro (BackgroundVolumeID);
+  vtkSetStringMacro (BackgroundVolumeID);
+
+  // Description:
+  // the ID of a MRMLVolumeNode
+  // TODO: make this an arbitrary list of layers
+  vtkGetStringMacro (ForegroundVolumeID);
+  vtkSetStringMacro (ForegroundVolumeID);
+
+  // Description:
+  // opacity of the Foreground for rendering over background
+  // TODO: make this an arbitrary list of layers
+  // TODO: make different composite types (checkerboard, etc)
+  vtkGetMacro (Opacity, double);
+  vtkSetMacro (Opacity, double);
+
+
+protected:
+  vtkMRMLSliceCompositeNode();
+  ~vtkMRMLSliceCompositeNode();
+  vtkMRMLSliceCompositeNode(const vtkMRMLSliceCompositeNode&);
+  void operator=(const vtkMRMLSliceCompositeNode&);
+
+  char *BackgroundVolumeID;
+  char *ForegroundVolumeID;
+  double Opacity;
+
+};
+
+#endif
+
