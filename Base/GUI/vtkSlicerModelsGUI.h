bf6c612730146e88bf5482bdf436f5245984c197
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-03-28 22:10:44 +0000

    ENH: added start of a Models GUI class
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=164
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@164 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerModelsGUI.h
@@ -0,0 +1,58 @@
+// .NAME vtkSlicerModelsGUI 
+// .SECTION Description
+// Main Models GUI for slicer3.  Points to the ModelsLogic and
+// reflects changes in that logic back onto the UI.  Also routes changes
+// from the GUI into the Logic to effect the user's desires.
+
+#ifndef __vtkSlicerModelsGUI_h
+#define __vtkSlicerModelsGUI_h
+
+#include "vtkSlicerBaseGUIWin32Header.h"
+#include "vtkSlicerComponentGUI.h"
+//#include "vtkSlicerModelsLogic.h"
+#include "vtkKWLoadSaveButton.h"
+#include "vtkKWLoadSaveDialog.h"
+#include "vtkKWFrame.h"
+
+// Description:
+// This class implements Slicer's Models GUI
+//
+class VTK_SLICER_BASE_GUI_EXPORT vtkSlicerModelsGUI : public vtkSlicerComponentGUI
+{
+ public:
+    static vtkSlicerModelsGUI* New (  );
+    vtkTypeRevisionMacro ( vtkSlicerModelsGUI, vtkSlicerComponentGUI );
+
+    //vtkGetObjectMacro ( ModelsLogic, vtkSlicerModelsLogic);
+    //vtkSetObjectMacro ( ModelsLogic, vtkSlicerModelsLogic);
+    vtkGetObjectMacro ( LoadModelButton, vtkKWLoadSaveButton );
+
+    // Description:
+    // This method builds a Slice GUI
+    virtual void BuildGUI ( ) ;
+    virtual void BuildGUI ( vtkKWWidget *f );
+    virtual void AddGUIObservers ( );
+    virtual void AddLogicObservers ( );
+    virtual void AddMrmlObservers ( );
+    virtual void ProcessLogicEvents ( vtkObject *caller, unsigned long event,
+                                            void *callData );
+    virtual void ProcessGUIEvents ( vtkObject *caller, unsigned long event,
+                                            void *callData );
+    virtual void ProcessMrmlEvents ( vtkObject *caller, unsigned long event,
+                                            void *callData );
+
+ protected:
+    vtkSlicerModelsGUI ( );
+    ~vtkSlicerModelsGUI ( );
+
+    // Widgets for the Models module
+    //vtkSlicerModelsLogic *ModelsLogic;
+    vtkKWLoadSaveButton *LoadModelButton;
+
+ private:
+    vtkSlicerModelsGUI ( const vtkSlicerModelsGUI& ); // Not implemented.
+    void operator = ( const vtkSlicerModelsGUI& ); //Not implemented.
+};
+
+
+#endif
