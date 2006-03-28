d34aa7bd81ed727dc005cda33c42d605cc1f3c91
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-03-28 22:15:52 +0000

    ENH: draft GUI for Slice Windows
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=168
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@168 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerSliceGUI.h
@@ -0,0 +1,62 @@
+// .NAME vtkSlicerSliceGUI 
+// .SECTION Description
+// Main Slice GUI for slicer3.  Points to the SliceLogic and
+// reflects changes in that logic back onto the UI.  Also routes changes
+// from the GUI into the Logic to effect the user's desires.
+
+#ifndef __vtkSlicerSliceGUI_h
+#define __vtkSlicerSliceGUI_h
+
+#include "vtkSlicerBaseGUIWin32Header.h"
+#include "vtkSlicerComponentGUI.h"
+#include "vtkSlicerSliceLogic.h"
+#include "vtkSlicerSliceWidgetCollection.h"
+#include "vtkMRMLScene.h"
+
+class vtkObject;
+class vtkSlicerSliceWidget;
+class vtkKWFrame;
+
+// Description:
+// This class implements Slicer's Slice GUI.
+//
+class VTK_SLICER_BASE_GUI_EXPORT vtkSlicerSliceGUI : public vtkSlicerComponentGUI
+{
+ public:
+    static vtkSlicerSliceGUI* New (  );
+    vtkTypeRevisionMacro ( vtkSlicerSliceGUI, vtkSlicerComponentGUI );
+
+    vtkGetObjectMacro ( SliceLogic, vtkSlicerSliceLogic);
+    vtkSetObjectMacro ( SliceLogic, vtkSlicerSliceLogic);
+    vtkGetObjectMacro ( SliceWidgets, vtkSlicerSliceWidgetCollection);
+    vtkSetObjectMacro ( SliceWidgets, vtkSlicerSliceWidgetCollection);
+    
+    virtual void AddSliceWidget ( vtkSlicerSliceWidget *w );
+    vtkSlicerSliceWidget* GetSliceWidget ( int SliceWidgetNum );
+    vtkSlicerSliceWidget* GetSliceWidget ( char *SliceWidgetColor );
+    
+    virtual void BuildGUI ( );
+    virtual void AddGUIObservers ( );
+    virtual void AddLogicObservers ( );
+    virtual void AddMrmlObservers ( );
+
+    virtual void ProcessLogicEvents ( vtkObject *caller, unsigned long event,
+                                            void *callData );
+    virtual void ProcessGUIEvents ( vtkObject *caller, unsigned long event,
+                                            void *callData );
+    virtual void ProcessMrmlEvents ( vtkObject *caller, unsigned long event,
+                                            void *callData );
+ protected:
+    vtkSlicerSliceGUI ( );
+    ~vtkSlicerSliceGUI ( );
+
+    vtkSlicerSliceLogic *SliceLogic;
+    // Collection of slice widgets.
+    vtkSlicerSliceWidgetCollection *SliceWidgets;
+    
+ private:
+    vtkSlicerSliceGUI ( const vtkSlicerSliceGUI& ); // Not implemented.
+    void operator = ( const vtkSlicerSliceGUI& ); //Not implemented.
+}; 
+
+#endif
