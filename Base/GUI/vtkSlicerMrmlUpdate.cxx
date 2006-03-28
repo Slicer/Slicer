9d80caeec7e98b768b7c4abf4bf7ec64e93908ba
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-03-28 22:13:03 +0000

    ENH: new GUI mediator helper class
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=165
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@165 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerMrmlUpdate.cxx
@@ -0,0 +1,18 @@
+
+#include "vtkObjectFactory.h"
+#include "vtkObject.h"
+#include "vtkSlicerMrmlUpdate.h"
+
+
+//---------------------------------------------------------------------------
+vtkCxxRevisionMacro ( vtkSlicerMrmlUpdate, "$Revision: 1.0 $" );
+
+
+//---------------------------------------------------------------------------
+vtkSlicerMrmlUpdate::vtkSlicerMrmlUpdate ( ) {
+}
+
+
+//---------------------------------------------------------------------------
+vtkSlicerMrmlUpdate::~vtkSlicerMrmlUpdate ( ) {
+}
