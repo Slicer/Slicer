e47211fca4862c2c0aaf8ab4ddcd3a82e217ea5b
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-03-28 22:24:01 +0000

    ENH: draft volumes module GUI
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=179
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@179 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerVolumesGUI.cxx
@@ -0,0 +1,159 @@
+#include "vtkObject.h"
+#include "vtkObjectFactory.h"
+#include "vtkCommand.h"
+#include "vtkKWWidget.h"
+#include "vtkSlicerVolumesGUI.h"
+#include "vtkSlicerGUI.h"
+#include "vtkSlicerStyle.h"
+#include "vtkKWFrameWithLabel.h"
+
+
+//---------------------------------------------------------------------------
+vtkStandardNewMacro (vtkSlicerVolumesGUI );
+vtkCxxRevisionMacro ( vtkSlicerVolumesGUI, "$Revision: 1.0 $");
+
+
+//---------------------------------------------------------------------------
+vtkSlicerVolumesGUI::vtkSlicerVolumesGUI ( ) {
+
+    //this->VolumesLogic = NULL;
+    this->LoadVolumeButton = NULL;
+}
+
+
+//---------------------------------------------------------------------------
+vtkSlicerVolumesGUI::~vtkSlicerVolumesGUI ( ) {
+
+    if (this->LoadVolumeButton ) {
+        this->LoadVolumeButton->Delete ( );
+    }
+    //this->VolumesLogic = NULL;
+}
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerVolumesGUI::AddGUIObservers ( ) {
+
+    // observer load volume button
+    this->LoadVolumeButton->AddObserver ( vtkCommand::ModifiedEvent,  (vtkCommand *)this->GUICommand );    
+
+}
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerVolumesGUI::AddMrmlObservers ( ) {
+    
+}
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerVolumesGUI::AddLogicObservers ( ) {
+
+
+}
+
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerVolumesGUI::ProcessGUIEvents ( vtkObject *caller,
+                                                     unsigned long event,
+                                                     void *callData ) 
+{
+    vtkKWLoadSaveButton *filebrowse = vtkKWLoadSaveButton::SafeDownCast(caller);
+    if (filebrowse == this->LoadVolumeButton  && event == vtkCommand::ModifiedEvent )
+        {
+            // If a file has been selected for loading...
+            if ( this->LoadVolumeButton->GetFileName ( ) ) {
+                //this->VolumesLogic->Connect ( filebrowse->GetFileName ( ) );
+            }
+        }
+
+}
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerVolumesGUI::ProcessLogicEvents ( vtkObject *caller,
+                                                     unsigned long event,
+                                                     void *callData ) {
+
+}
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerVolumesGUI::ProcessMrmlEvents ( vtkObject *caller,
+                                                     unsigned long event,
+                                                     void *callData ) {
+
+}
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerVolumesGUI::BuildGUI ( ) {
+}
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerVolumesGUI::BuildGUI ( vtkKWWidget* f ) {
+
+    vtkSlicerGUI *app = (vtkSlicerGUI *)this->GetApplication();
+    vtkSlicerStyle *style = app->GetSlicerStyle();
+    // ---
+    // MODULE GUI FRAME 
+    // configure a page for a volume loading UI for now.
+    // later, switch on the modulesButton in the SlicerControlGUI
+    // ---
+    // HELP FRAME
+    vtkKWFrameWithLabel *volHelpFrame = vtkKWFrameWithLabel::New ( );
+    volHelpFrame->SetParent ( f );
+    volHelpFrame->Create ( );
+    volHelpFrame->CollapseFrame ( );
+    volHelpFrame->SetLabelText ("help");
+    volHelpFrame->SetDefaultLabelCaseToLowercaseFirst( );
+    volHelpFrame->SetDefaultLabelFontWeightToNormal( );
+    app->Script ( "pack %s -side top -anchor nw -fill x -padx 2 -pady 2 -in %s",
+                  volHelpFrame->GetWidgetName(), f->GetWidgetName());
+
+    // ---
+    // LOAD FRAME            
+    vtkKWFrameWithLabel *volLoadFrame = vtkKWFrameWithLabel::New ( );
+    volLoadFrame->SetParent ( f );
+    volLoadFrame->Create ( );
+    volLoadFrame->SetLabelText ("load");
+    volLoadFrame->SetDefaultLabelFontWeightToNormal( );
+    volLoadFrame->SetDefaultLabelCaseToLowercaseFirst( );
+    volLoadFrame->ExpandFrame ( );
+
+    // add a file browser 
+    this->LoadVolumeButton = vtkKWLoadSaveButton::New ( );
+    this->LoadVolumeButton->SetParent ( volLoadFrame->GetFrame() );
+    this->LoadVolumeButton->Create ( );
+    this->LoadVolumeButton->SetText ("Choose a file to load");
+    this->LoadVolumeButton->GetLoadSaveDialog()->SetFileTypes(
+                                                              "{ {MRML Document} {.mrml .xml} }");
+    app->Script ( "pack %s -side top -anchor nw -fill x -padx 2 -pady 2 -in %s",
+                  volLoadFrame->GetWidgetName(), f->GetWidgetName());
+    app->Script("pack %s -side top -anchor w -padx 2 -pady 4", 
+                this->LoadVolumeButton->GetWidgetName());
+
+    // ---
+    // DISPLAY FRAME            
+    vtkKWFrameWithLabel *volDisplayFrame = vtkKWFrameWithLabel::New ( );
+    volDisplayFrame->SetParent ( f );
+    volDisplayFrame->Create ( );
+    volDisplayFrame->SetLabelText ("display");
+    volDisplayFrame->SetDefaultLabelFontWeightToNormal( );
+    volDisplayFrame->SetDefaultLabelCaseToLowercaseFirst( );
+    volDisplayFrame->CollapseFrame ( );
+    app->Script ( "pack %s -side top -anchor nw -fill x -padx 2 -pady 2 -in %s",
+                  volDisplayFrame->GetWidgetName(), f->GetWidgetName());
+
+}
+
+
+
+
+
