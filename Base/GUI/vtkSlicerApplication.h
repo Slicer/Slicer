a818253e010a0c3f7f935a1bed71da35eadb5a40
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-03-29 19:50:18 +0000

    ENH: renamed Slicer Application class
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=189
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@189 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerApplication.h
@@ -0,0 +1,70 @@
+// .NAME vtkSlicerApplication
+// .SECTION Description
+// Contains slicer's style, application and collection of associated guis.
+//
+
+
+#ifndef __vtkSlicerApplication_h
+#define __vtkSlicerApplication_h
+
+#include "vtkSlicerBaseGUIWin32Header.h"
+#include "vtkKWApplication.h"
+#include "vtkSlicerStyle.h"
+#include "vtkSlicerGUICollection.h"
+
+class vtkSlicerComponentGUI;
+
+
+// Description:
+// Contains slicer's style, application and collection of associated guis.
+//
+class VTK_SLICER_BASE_GUI_EXPORT vtkSlicerApplication : public vtkKWApplication
+{
+ public:
+    static vtkSlicerApplication* New ( );
+    vtkTypeRevisionMacro ( vtkSlicerApplication, vtkKWApplication );
+
+    vtkGetObjectMacro ( SlicerStyle, vtkSlicerStyle );
+    vtkSetObjectMacro ( SlicerStyle, vtkSlicerStyle );
+    
+    vtkGetObjectMacro ( GUICollection, vtkSlicerGUICollection );
+    vtkSetObjectMacro ( GUICollection, vtkSlicerGUICollection );
+    
+    vtkGetMacro ( NumberOfGUIs, int );
+    vtkSetMacro ( NumberOfGUIs, int );
+
+    // Description:
+    // This method collects GUIs added to Slicer.
+    virtual void AddGUI ( vtkSlicerComponentGUI *gui );
+    // Description:
+    // These methods manage windows associated with the application
+    virtual void CloseAllWindows ( ) ;
+    
+    // Description:
+    // Sets application behavior.
+    virtual void ConfigureApplication ( );
+    virtual int StartApplication ( );
+    
+ protected:
+    vtkSlicerApplication ( );
+    ~vtkSlicerApplication ( );
+
+    vtkSlicerStyle *SlicerStyle;
+
+    // Description:
+    // Collections of GUIs
+    vtkSlicerGUICollection *GUICollection;
+    
+    // Description:
+    // numbers of widgets
+    int NumberOfGUIs;
+    
+    // Tcl_Interp *interp; 
+
+    
+ private:
+    vtkSlicerApplication ( const vtkSlicerApplication& ); // Not implemented.
+    void operator = ( const vtkSlicerApplication& ); //Not implemented.
+}; 
+
+#endif
