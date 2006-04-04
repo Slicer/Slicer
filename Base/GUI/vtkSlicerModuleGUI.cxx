6d0a4ce86b85171e6e34e7946798304c2cf6209e
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-04-04 19:41:48 +0000

    ENH: new module base class for guis which populate the main panel
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=241
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@241 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerModuleGUI.cxx
@@ -0,0 +1,105 @@
+#include "vtkObjectFactory.h"
+#include "vtkSlicerModuleGUI.h"
+#include "vtkSlicerApplicationGUI.h"
+#include "vtkSlicerApplicationLogic.h"
+#include "vtkSlicerGUIUpdate.h"
+#include "vtkSlicerLogicUpdate.h"
+#include "vtkSlicerMrmlUpdate.h"
+#include "vtkKWApplication.h"
+#include "vtkKWUserInterfacePanel.h"
+
+//---------------------------------------------------------------------------
+vtkStandardNewMacro (vtkSlicerModuleGUI);
+vtkCxxRevisionMacro(vtkSlicerModuleGUI, "$Revision: 1.0 $");
+
+
+
+//---------------------------------------------------------------------------
+vtkSlicerModuleGUI::vtkSlicerModuleGUI ( ) {
+
+    // Every gui has a helper class called LogicCommand
+    // whose execute method propagates logic changes
+    // into the GUI state.
+    this->LogicCommand = vtkSlicerLogicUpdate::New ( );
+    this->LogicCommand->SetGUI ( this );
+    this->GUICommand = vtkSlicerGUIUpdate::New ( );
+    this->GUICommand->SetGUI ( this );
+    this->MrmlCommand = vtkSlicerMrmlUpdate::New ( );
+    this->MrmlCommand->SetGUI ( this );
+    this->UIPanel = vtkKWUserInterfacePanel::New ( );
+    this->Logic = NULL;
+    this->Mrml = NULL;
+
+}
+
+
+
+//---------------------------------------------------------------------------
+vtkSlicerModuleGUI::~vtkSlicerModuleGUI ( ) {
+
+    if ( this->LogicCommand ) {
+        this->LogicCommand->Delete ( );
+        this->LogicCommand = NULL;
+    }
+    if (this->GUICommand ) {
+        this->GUICommand->Delete ( );
+        this->GUICommand = NULL;
+    }
+    if (this->MrmlCommand ) {
+        this->MrmlCommand->Delete ( );
+        this->MrmlCommand = NULL;
+    }
+    if ( this->UIPanel ) {
+        this->UIPanel->Delete ( );
+        this->UIPanel = NULL;
+    }
+    this->Logic = NULL;
+    this->Mrml = NULL;
+    this->Parent = NULL;
+}
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerModuleGUI::BuildGUI ( ) {
+
+
+}
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerModuleGUI::SetParent ( vtkKWFrame *frame ) {
+    
+}
+
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerModuleGUI::ProcessLogicEvents ( vtkObject *caller,
+                                                 unsigned long event,
+                                                 void *callData ) {
+
+}
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerModuleGUI::ProcessGUIEvents ( vtkObject *caller,
+                                                 unsigned long event,
+                                                 void *callData ) {
+
+}
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerModuleGUI::ProcessMrmlEvents ( vtkObject *caller,
+                                                 unsigned long event,
+                                                 void *callData ) {
+
+}
+
+
+
+
+
+
