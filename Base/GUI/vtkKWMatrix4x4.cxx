d3bbd17c2ee3803d13adce3c9f0af1d26bf20feb
Steve Pieper <pieper@bwh.harvard.edu>
2006-04-05 22:44:30 +0000

    ENH: new widget to watch vtk matrices
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=246
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@246 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkKWMatrix4x4.cxx
@@ -0,0 +1,142 @@
+/*=========================================================================
+
+  Module:    $RCSfile: vtkKWMatrix4x4.cxx,v $
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkKWMatrix4x4.h"
+
+#include "vtkKWMultiColumnList.h"
+#include "vtkObjectFactory.h"
+
+//----------------------------------------------------------------------------
+vtkStandardNewMacro( vtkKWMatrix4x4 );
+vtkCxxRevisionMacro(vtkKWMatrix4x4, "$Revision: 1.49 $");
+
+//----------------------------------------------------------------------------
+vtkKWMatrix4x4::vtkKWMatrix4x4()
+{
+  this->Command = NULL;
+  //this->StartCommand = NULL;
+  //this->EndCommand   = NULL;
+
+  this->Matrix4x4 = NULL;
+
+  this->MultiColumnList = vtkKWMultiColumnList::New();
+
+  this->UpdateWidget();
+}
+
+//----------------------------------------------------------------------------
+vtkKWMatrix4x4::~vtkKWMatrix4x4()
+{
+  if (this->Command)
+    {
+    delete [] this->Command;
+    }
+
+  this->SetMatrix4x4(NULL);
+  this->MultiColumnList->Delete();
+
+}
+
+//----------------------------------------------------------------------------
+void vtkKWMatrix4x4::Create()
+{
+  // Check if already created
+
+  if (this->IsCreated())
+    {
+    vtkErrorMacro(<< this->GetClassName() << " already created");
+    return;
+    }
+
+  // Call the superclass to create the whole widget
+
+  this->Superclass::Create();
+
+  this->MultiColumnList->SetParent(this);
+  this->MultiColumnList->Create();
+
+  int col;
+  for (col = 0; col < 4; col++)
+    {
+    this->MultiColumnList->AddColumn(" ");
+    this->MultiColumnList->SetColumnAlignmentToCenter(col);
+    this->MultiColumnList->ColumnEditableOn(col);
+    }
+
+  // avoid the empty row of labels (and avoid accidental sorting!)
+  this->MultiColumnList->SetColumnLabelsVisibility(0);
+
+  this->Script("pack %s -fill both -expand true",
+                 this->MultiColumnList->GetWidgetName());
+
+  // Override the column sorting behavior by always updating 
+}
+
+//----------------------------------------------------------------------------
+void vtkKWMatrix4x4::UpdateWidget()
+{
+  this->MultiColumnList->DeleteAllRows();
+
+  if (this->Matrix4x4 == NULL)
+    {
+    this->MultiColumnList->InsertCellText(0, 0, "NULL");
+    return;
+    }
+
+
+  int row, col;
+  for (row=0; row<4; row++)
+    {
+    for (col=0; col<4; col++)
+      {
+      this->MultiColumnList->InsertCellTextAsDouble(row, col, this->Matrix4x4->GetElement(row, col));
+      this->MultiColumnList->SetCellEditWindowToSpinBox(row, col);
+      }
+    }
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+void vtkKWMatrix4x4::UpdateVTK()
+{
+  if (this->Matrix4x4 == NULL)
+    {
+    return;
+    }
+
+
+  int row, col;
+  for (row=0; row<4; row++)
+    {
+    for (col=0; col<4; col++)
+      {
+      this->Matrix4x4->SetElement(row, col, this->MultiColumnList->GetCellTextAsDouble(row, col));
+      }
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkKWMatrix4x4::SetCommand(vtkObject *object, const char *method)
+{
+  this->SetObjectMethodCommand(&this->Command, object, method);
+}
+
+
+//----------------------------------------------------------------------------
+void vtkKWMatrix4x4::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+
+  os << indent << "Matrix4x4: " << this->GetMatrix4x4() << endl;
+}
+
---
+++ Base/GUI/vtkKWMatrix4x4.h
@@ -0,0 +1,88 @@
+/*=========================================================================
+
+  Module:    $RCSfile: vtkKWMatrix4x4.h,v $
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkKWMatrix4x4 -  multi column list box to display/edit matrix4x4
+// .SECTION Description
+// vtkKWMatrix4x4 is a widget containing widgets that help view and
+// edit a matrix
+//
+
+#ifndef __vtkKWMatrix4x4_h
+#define __vtkKWMatrix4x4_h
+
+#include "vtkKWCompositeWidget.h"
+
+#include "vtkMatrix4x4.h" // Needed for some constants
+#include "vtkKWMultiColumnList.h" // Needed for some constants
+
+#include "vtkSlicerBaseGUI.h"
+
+class VTK_SLICER_BASE_GUI_EXPORT vtkKWMatrix4x4 : public vtkKWCompositeWidget
+{
+public:
+  static vtkKWMatrix4x4* New();
+  vtkTypeRevisionMacro(vtkKWMatrix4x4,vtkKWCompositeWidget);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Create the widget.
+  virtual void Create();
+
+  // Description:
+  // the vtk object to manipulate
+  vtkGetObjectMacro (Matrix4x4, vtkMatrix4x4);
+  vtkSetObjectMacro (Matrix4x4, vtkMatrix4x4);
+
+  // Description:
+  // the listbox widget
+  vtkGetObjectMacro (MultiColumnList, vtkKWMultiColumnList);
+  vtkSetObjectMacro (MultiColumnList, vtkKWMultiColumnList);
+
+  // Description:
+  // Update the widget with the current values of the Matrix
+  void UpdateWidget();
+  // Description:
+  // Update the matrix with the current values of the widget
+  void UpdateVTK();
+  
+  // Description:
+  // Command to call when the User manipulates the widget
+  virtual void SetCommand(vtkObject *object, const char *method);
+
+  // TODO: have special commands for start/end events
+  //virtual void SetStartCommand(vtkObject *object, const char *method);
+  //virtual void SetEndCommand(vtkObject *object, const char *method);
+
+  // Description:
+  // TODO: access internal widgets
+  //vtkKWRange* GetXRange() { return this->Range[0]; };
+
+protected:
+  vtkKWMatrix4x4();
+  ~vtkKWMatrix4x4();
+
+  char *Command;
+  //char *StartCommand;
+  //char *EndCommand;
+
+  vtkMatrix4x4 *Matrix4x4;
+
+  vtkKWMultiColumnList *MultiColumnList;
+
+private:
+  vtkKWMatrix4x4(const vtkKWMatrix4x4&); // Not implemented
+  void operator=(const vtkKWMatrix4x4&); // Not implemented
+};
+
+#endif
+
