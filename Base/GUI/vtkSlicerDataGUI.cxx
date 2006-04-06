867520078a148392f95a160aac44a78154daebc1
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-04-06 21:52:47 +0000

    ENH: Added new draft class for Data module
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=252
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@252 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerDataGUI.cxx
@@ -0,0 +1,110 @@
+#include "vtkObject.h"
+#include "vtkObjectFactory.h"
+#include "vtkCommand.h"
+#include "vtkKWWidget.h"
+#include "vtkSlicerDataGUI.h"
+#include "vtkSlicerApplication.h"
+#include "vtkSlicerStyle.h"
+#include "vtkKWFrameWithLabel.h"
+
+//---------------------------------------------------------------------------
+vtkStandardNewMacro (vtkSlicerDataGUI );
+vtkCxxRevisionMacro ( vtkSlicerDataGUI, "$Revision: 1.0 $");
+
+
+//---------------------------------------------------------------------------
+vtkSlicerDataGUI::vtkSlicerDataGUI ( ) {
+
+    //this->DataLogic = NULL;
+
+}
+
+
+//---------------------------------------------------------------------------
+vtkSlicerDataGUI::~vtkSlicerDataGUI ( ) {
+
+    //this->DataLogic = NULL;
+}
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerDataGUI::RemoveGUIObservers ( ) {
+
+}
+
+//---------------------------------------------------------------------------
+void vtkSlicerDataGUI::RemoveLogicObservers ( ) {
+}
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerDataGUI::AddGUIObservers ( ) {
+
+}
+
+//---------------------------------------------------------------------------
+void vtkSlicerDataGUI::AddLogicObservers ( ) {
+
+}
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerDataGUI::ProcessGUIEvents ( vtkObject *caller,
+                                                    unsigned long event,
+                                                    void *callData ) {
+}
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerDataGUI::ProcessLogicEvents ( vtkObject *caller,
+                                                    unsigned long event,
+                                                    void *callData ) {
+
+}
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerDataGUI::BuildGUI ( ) {
+
+    vtkSlicerApplication *app = (vtkSlicerApplication *)this->GetApplication();
+    vtkSlicerStyle *style = app->GetSlicerStyle();
+
+    // ---
+    // MODULE GUI FRAME 
+    // configure a page for a model loading UI for now.
+    // later, switch on the modulesButton in the SlicerControlGUI
+    // ---
+    // create a page
+    this->UIPanel->AddPage ( "Data", "Data", NULL );
+    
+    // HELP FRAME
+    vtkKWFrameWithLabel *modHelpFrame = vtkKWFrameWithLabel::New ( );
+    modHelpFrame->SetParent ( this->UIPanel->GetPageWidget ( "Data" ) );
+    modHelpFrame->Create ( );
+    modHelpFrame->CollapseFrame ( );
+    modHelpFrame->SetLabelText ("Help");
+    app->Script ( "pack %s -side top -anchor nw -fill x -padx 2 -pady 2 -in %s",
+                  modHelpFrame->GetWidgetName(), this->UIPanel->GetPageWidget("Data")->GetWidgetName());
+
+    // ---
+    // DISPLAY FRAME            
+    vtkKWFrameWithLabel *modDisplayFrame = vtkKWFrameWithLabel::New ( );
+    modDisplayFrame->SetParent ( this->UIPanel->GetPageWidget ( "Data" ) );
+    modDisplayFrame->Create ( );
+    //modDisplayFrame->SetBackgroundColor ( style->GetGUIBgColor() );
+    modDisplayFrame->SetLabelText ("Display");
+    modDisplayFrame->SetDefaultLabelFontWeightToNormal( );
+    modDisplayFrame->CollapseFrame ( );
+    app->Script ( "pack %s -side top -anchor nw -fill x -padx 2 -pady 2 -in %s",
+                  modDisplayFrame->GetWidgetName(), this->UIPanel->GetPageWidget("Data")->GetWidgetName());
+
+    modHelpFrame->Delete ( );
+    modDisplayFrame->Delete ( );
+}
+
+
+
+
+
---
+++ Base/GUI/vtkSlicerDataGUI.h
@@ -0,0 +1,47 @@
+#ifndef __vtkSlicerDataGUI_h
+#define __vtkSlicerDataGUI_h
+
+#include "vtkSlicerBaseGUIWin32Header.h"
+#include "vtkSlicerModuleGUI.h"
+//#include "vtkSlicerDataLogic.h"
+
+// Description:
+// This class implements Slicer's Data GUI
+//
+class VTK_SLICER_BASE_GUI_EXPORT vtkSlicerDataGUI : public vtkSlicerModuleGUI
+{
+ public:
+    static vtkSlicerDataGUI* New (  );
+    vtkTypeRevisionMacro ( vtkSlicerDataGUI, vtkSlicerModuleGUI );
+
+    //vtkGetObjectMacro ( DataLogic, vtkSlicerDataLogic);
+    //vtkSetObjectMacro ( DataLogic, vtkSlicerDataLogic);
+
+    // Description:
+    // This method builds the Data module's GUI
+    virtual void BuildGUI ( ) ;
+    virtual void AddGUIObservers ( );
+    virtual void RemoveGUIObservers ( );
+    virtual void AddLogicObservers ( );
+    virtual void RemoveLogicObservers ( );
+    virtual void ProcessLogicEvents ( vtkObject *caller, unsigned long event,
+                                            void *callData );
+    virtual void ProcessGUIEvents ( vtkObject *caller, unsigned long event,
+                                            void *callData );
+
+ protected:
+    vtkSlicerDataGUI ( );
+    ~vtkSlicerDataGUI ( );
+
+    // Logic pointer for the Data module
+
+    //vtkSlicerDataLogic *DataLogic;
+    // Widgets for the Data module
+
+ private:
+    vtkSlicerDataGUI ( const vtkSlicerDataGUI& ); // Not implemented.
+    void operator = ( const vtkSlicerDataGUI& ); //Not implemented.
+};
+
+
+#endif
