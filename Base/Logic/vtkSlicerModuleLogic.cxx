55f8e949f3aba18a635a77356cd1e4bb0fcf418d
Alex Yarmarkovich <alexy@bwh.harvard.edu>
2006-04-10 18:50:40 +0000

    ENH: added vtkSlicerModuleLogic
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=259
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@259 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/Logic/vtkSlicerModuleLogic.cxx
@@ -0,0 +1,38 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerModuleLogic.cxx,v $
+  Date:      $Date: 2006/01/06 17:56:48 $
+  Version:   $Revision: 1.58 $
+
+=========================================================================auto=*/
+
+#include "vtkObjectFactory.h"
+#include "vtkSlicerModuleLogic.h"
+
+vtkCxxRevisionMacro(vtkSlicerModuleLogic, "$Revision: 1.9.12.1 $");
+vtkStandardNewMacro(vtkSlicerModuleLogic);
+
+//----------------------------------------------------------------------------
+vtkSlicerModuleLogic::vtkSlicerModuleLogic()
+{
+}
+
+//----------------------------------------------------------------------------
+vtkSlicerModuleLogic::~vtkSlicerModuleLogic()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleLogic::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->vtkObject::PrintSelf(os, indent);
+
+  os << indent << "SlicerLogic:             " << this->GetClassName() << "\n";
+}
+
---
+++ Base/Logic/vtkSlicerModuleLogic.h
@@ -0,0 +1,49 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerModuleLogic.h,v $
+  Date:      $Date: 2006/01/08 04:48:05 $
+  Version:   $Revision: 1.45 $
+
+=========================================================================auto=*/
+// .NAME vtkSlicerModuleLogic - superclass for slicer module logic classes
+// .SECTION Description
+// Superclass for all slicer m,odule logic classes
+// There must be a corresponding vtkSlicerGUI subclass corresponding 
+// to each logic class that handles all GUI interaction (no GUI code
+// goes in the logic class).
+
+#ifndef __vtkSlicerModuleLogic_h
+#define __vtkSlicerModuleLogic_h
+
+#include "vtkSlicerLogic.h"
+#include "vtkObject.h"
+#include "vtkObjectFactory.h"
+
+class VTK_SLICER_BASE_LOGIC_EXPORT vtkSlicerModuleLogic : public vtkSlicerLogic 
+{
+  public:
+  
+  // The Usual vtk class functions
+  static vtkSlicerModuleLogic *New();
+  vtkTypeRevisionMacro(vtkSlicerModuleLogic,vtkSlicerLogic);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Additional functionality:
+  //  -- an undo stack (also need helper classes)
+  //  -- a tracing/macro infrastructure
+    
+protected:
+  vtkSlicerModuleLogic();
+  ~vtkSlicerModuleLogic();
+  vtkSlicerModuleLogic(const vtkSlicerModuleLogic&);
+  void operator=(const vtkSlicerModuleLogic&);
+};
+
+#endif
+
