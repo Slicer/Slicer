fafaacee2004733c49019c51e67bb38815959b0f
Alex Yarmarkovich <alexy@bwh.harvard.edu>
2006-04-18 20:40:49 +0000

    ENH: added vtkSlicerVolumesLogic
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=290
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@290 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/Logic/vtkSlicerVolumesLogic.cxx
@@ -0,0 +1,113 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerVolumesLogic.cxx,v $
+  Date:      $Date: 2006/01/06 17:56:48 $
+  Version:   $Revision: 1.58 $
+
+=========================================================================auto=*/
+
+#include "vtkObjectFactory.h"
+#include "vtkCallbackCommand.h"
+
+#include "vtkSlicerVolumesLogic.h"
+
+#include "vtkMRMLScalarVolumeNode.h"
+#include "vtkMRMLVectorVolumeNode.h"
+#include "vtkMRMLVolumeArchetypeStorageNode.h"
+#include "vtkMRMLVolumeDisplayNode.h"
+
+vtkCxxRevisionMacro(vtkSlicerVolumesLogic, "$Revision: 1.9.12.1 $");
+vtkStandardNewMacro(vtkSlicerVolumesLogic);
+
+//----------------------------------------------------------------------------
+vtkSlicerVolumesLogic::vtkSlicerVolumesLogic()
+{
+  this->ActiveVolumeNode = NULL;
+}
+
+//----------------------------------------------------------------------------
+vtkSlicerVolumesLogic::~vtkSlicerVolumesLogic()
+{
+  if (this->ActiveVolumeNode != NULL)
+    {
+    this->ActiveVolumeNode->Delete();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerVolumesLogic::ProcessMRMLEvents()
+{
+  // TODO: implement if needed
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerVolumesLogic::SetActiveVolumeNode(vtkMRMLVolumeNode *activeNode)
+{
+  this->SetMRML( vtkObjectPointer(&this->ActiveVolumeNode), activeNode );
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLVolumeNode* vtkSlicerVolumesLogic::AddArchetypeVolume (char* filename)
+{
+  vtkMRMLVolumeNode *volumeNode = NULL;
+  
+  vtkMRMLScalarVolumeNode *scalarNode = vtkMRMLScalarVolumeNode::New();
+  vtkMRMLVectorVolumeNode *vectorNode = vtkMRMLVectorVolumeNode::New();
+  
+  vtkMRMLVolumeDisplayNode *displayNode = vtkMRMLVolumeDisplayNode::New();
+  vtkMRMLVolumeArchetypeStorageNode *storageNode = vtkMRMLVolumeArchetypeStorageNode::New();
+
+  storageNode->SetAbsoluteFileName(true);
+  storageNode->SetFileArchetype(filename);
+  if (storageNode->ReadData(scalarNode) == 0)
+    {
+    // cannot read scalar data, try vector
+    if (storageNode->ReadData(vectorNode) != 0)
+      {
+      volumeNode = vectorNode;
+      }
+    }
+  else
+    {
+    volumeNode = scalarNode;
+    }
+  
+  if (volumeNode != NULL)
+    {
+    this->GetMRMLScene()->SaveStateForUndo();
+
+    this->GetMRMLScene()->AddNode(volumeNode);  
+    this->GetMRMLScene()->AddNode(storageNode);  
+    this->GetMRMLScene()->AddNode(displayNode);  
+    
+    this->SetActiveVolumeNode(volumeNode);
+    
+    this->Modified();  
+    }
+
+  scalarNode->Delete();
+  vectorNode->Delete();
+  storageNode->Delete();
+  displayNode->Delete();
+
+  return volumeNode;
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerVolumesLogic::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->vtkObject::PrintSelf(os, indent);
+
+  os << indent << "vtkSlicerVolumesLogic:             " << this->GetClassName() << "\n";
+
+  os << indent << "ActiveVolumeNode: " <<
+    (this->ActiveVolumeNode ? this->ActiveVolumeNode->GetName() : "(none)") << "\n";
+}
+
---
+++ Base/Logic/vtkSlicerVolumesLogic.h
@@ -0,0 +1,68 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerVolumesLogic.h,v $
+  Date:      $Date: 2006/01/08 04:48:05 $
+  Version:   $Revision: 1.45 $
+
+=========================================================================auto=*/
+
+// .NAME vtkSlicerVolumesLogic - slicer logic class for volumes manipulation
+// .SECTION Description
+// This class manages the logic associated with reading, saving,
+// and changing propertied of the volumes
+
+
+#ifndef __vtkSlicerVolumesLogic_h
+#define __vtkSlicerVolumesLogic_h
+
+#include <stdlib.h>
+
+#include "vtkSlicerBaseLogic.h"
+#include "vtkSlicerLogic.h"
+
+#include "vtkMRML.h"
+#include "vtkMRMLVolumeNode.h"
+
+
+class VTK_SLICER_BASE_LOGIC_EXPORT vtkSlicerVolumesLogic : public vtkSlicerLogic 
+{
+  public:
+  
+  // The Usual vtk class functions
+  static vtkSlicerVolumesLogic *New();
+  vtkTypeRevisionMacro(vtkSlicerVolumesLogic,vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // The currently active mrml volume node 
+  vtkGetObjectMacro (ActiveVolumeNode, vtkMRMLVolumeNode);
+  void SetActiveVolumeNode (vtkMRMLVolumeNode *ActiveVolumeNode);
+
+  // Description:
+  // Create new mrml node and associated storage node.
+  // Read image data from a specified file
+  vtkMRMLVolumeNode* AddArchetypeVolume (char* filename);
+
+  // Description:
+  // Update logic state when MRML scene chenges
+  void ProcessMRMLEvents(); 
+    
+protected:
+  vtkSlicerVolumesLogic();
+  ~vtkSlicerVolumesLogic();
+  vtkSlicerVolumesLogic(const vtkSlicerVolumesLogic&);
+  void operator=(const vtkSlicerVolumesLogic&);
+
+  // Description:
+  //
+  vtkMRMLVolumeNode *ActiveVolumeNode;
+};
+
+#endif
+
