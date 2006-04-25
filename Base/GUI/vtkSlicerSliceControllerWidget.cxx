98721569cf1347d0384d7625dde11a74887e938d
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-04-25 17:49:30 +0000

    ENH: renamed vtkSlicerSliceController to vtkSlicerSliceControllerWidget
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=333
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@333 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerSliceControllerWidget.cxx
@@ -0,0 +1,109 @@
+#include "vtkObjectFactory.h"
+#include "vtkCommand.h"
+#include "vtkImageData.h"
+
+#include "vtkSlicerSliceControllerWidget.h"
+#include "vtkSlicerApplication.h"
+
+#include "vtkKWWidget.h"
+#include "vtkKWScaleWithEntry.h"
+#include "vtkKWEntryWithLabel.h"
+#include "vtkKWScale.h"
+#include "vtkKWRenderWidget.h"
+#include "vtkKWMenu.h"
+#include "vtkKWFrame.h"
+#include "vtkKWMenuButton.h"
+
+
+
+//---------------------------------------------------------------------------
+vtkStandardNewMacro ( vtkSlicerSliceControllerWidget );
+vtkCxxRevisionMacro ( vtkSlicerSliceControllerWidget, "$Revision: 1.0 $");
+
+
+//---------------------------------------------------------------------------
+vtkSlicerSliceControllerWidget::vtkSlicerSliceControllerWidget ( ) {
+
+    //---  
+    // widgets comprising the SliceControllerWidget for now.
+    this->OffsetScale = NULL;
+    this->FieldOfViewEntry = NULL;
+    this->OrientationMenu = NULL;
+    this->ControlFrame = NULL;
+
+}
+
+
+//---------------------------------------------------------------------------
+vtkSlicerSliceControllerWidget::~vtkSlicerSliceControllerWidget ( ){
+
+    if ( this->OffsetScale ) {
+        this->OffsetScale->Delete ( );
+        this->OffsetScale = NULL;
+    }
+    if ( this->FieldOfViewEntry ) {
+        this->FieldOfViewEntry->Delete ( );
+        this->FieldOfViewEntry = NULL;
+    }
+    if ( this->OrientationMenu ) {
+        this->OrientationMenu->Delete ( );
+        this->OrientationMenu = NULL;
+    }
+    if ( this->ControlFrame ) {
+        this->ControlFrame->Delete ( );
+        this->ControlFrame = NULL;
+    }
+    
+}
+
+
+
+//---------------------------------------------------------------------------
+void vtkSlicerSliceControllerWidget::Create ( ) {
+
+    // the widget is a frame with some widgets inside
+    if (this->IsCreated ( ) ) {
+        vtkErrorMacro ( << this->GetClassName() << "already created.");
+        return;
+    }
+    this->Superclass::Create ( );
+    
+    this->ControlFrame = vtkKWFrame::New ( );
+    this->ControlFrame->SetParent ( this->GetParent( ) );
+    this->ControlFrame->Create( );
+
+    this->FieldOfViewEntry = vtkKWEntryWithLabel::New();
+    this->FieldOfViewEntry->SetParent (this->ControlFrame);
+    this->FieldOfViewEntry->SetLabelText ( "FOV:" );
+    this->FieldOfViewEntry->Create ( );
+    this->FieldOfViewEntry->SetWidth ( 8 );
+
+    this->OrientationMenu = vtkKWMenuButtonWithLabel::New ();
+    this->OrientationMenu->SetParent ( this->ControlFrame );
+    this->OrientationMenu->Create ( );
+    this->OrientationMenu->SetLabelText ( "Orientation: ");
+    vtkKWMenuButton *mb = this->OrientationMenu->GetWidget ( );
+    mb->SetWidth ( 8 );
+    mb->GetMenu()->AddRadioButton ( "Axial" );
+    mb->GetMenu()->AddRadioButton ( "Saggital" );
+    mb->GetMenu()->AddRadioButton ( "Coronal" );
+    mb->SetValue ("Axial");    
+        
+    //---
+    // Create a scale to control the slice number displayed
+    this->OffsetScale = vtkKWScaleWithEntry::New();
+    this->OffsetScale->SetParent ( this->ControlFrame );
+    this->OffsetScale->Create();
+    this->OffsetScale->RangeVisibilityOff ( );
+            
+}
+
+
+
+//----------------------------------------------------------------------------
+void vtkSlicerSliceControllerWidget::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+  // widgets?
+}
+
---
+++ Base/GUI/vtkSlicerSliceControllerWidget.h
@@ -0,0 +1,54 @@
+#ifndef __vtkSlicerSliceControllerWidget_h
+#define __vtkSlicerSliceControllerWidget_h
+
+#include "vtkSlicerBaseGUIWin32Header.h"
+#include "vtkKWCompositeWidget.h"
+#include "vtkKWScaleWithEntry.h"
+#include "vtkKWEntryWithLabel.h"
+#include "vtkKWMenuButtonWithLabel.h"
+#include "vtkKWFrame.h"
+
+class VTK_SLICER_BASE_GUI_EXPORT vtkSlicerSliceControllerWidget : public vtkKWCompositeWidget
+{
+    
+ public:
+    static vtkSlicerSliceControllerWidget* New ( );
+    vtkTypeRevisionMacro ( vtkSlicerSliceControllerWidget, vtkKWCompositeWidget );
+    void PrintSelf (ostream& os, vtkIndent indent);
+    
+    // Description:
+    // Get/Set the Widgets in this composite widget.
+    vtkGetObjectMacro ( OffsetScale, vtkKWScaleWithEntry );
+    vtkGetObjectMacro ( FieldOfViewEntry, vtkKWEntryWithLabel );
+    vtkGetObjectMacro ( OrientationMenu, vtkKWMenuButtonWithLabel );
+    vtkGetObjectMacro ( ControlFrame, vtkKWFrame );
+    vtkSetObjectMacro ( ControlFrame, vtkKWFrame );
+
+    // Description:
+    // TODO: Use this flag to determine how to display
+    // the SliceControllerWidget.
+    vtkGetMacro ( ControllerStyle, int );
+    vtkSetMacro ( ControllerStyle, int );
+
+    // Description:
+    // Create the Widget inside a parent frame.
+    virtual void Create ( );
+
+ protected:
+    vtkSlicerSliceControllerWidget ( );
+    ~vtkSlicerSliceControllerWidget ( );
+    // Slice controller
+    int ControllerStyle;
+    vtkKWFrame *ControlFrame;
+    vtkKWScaleWithEntry *OffsetScale;
+    vtkKWEntryWithLabel *FieldOfViewEntry;
+    vtkKWMenuButtonWithLabel *OrientationMenu;
+
+ private:
+    vtkSlicerSliceControllerWidget (const vtkSlicerSliceControllerWidget &); //Not implemented
+    void operator=(const vtkSlicerSliceControllerWidget &);         //Not implemented
+
+};
+
+#endif
+
