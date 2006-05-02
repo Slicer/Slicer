bb68d6c2289cec51fc0efc2417592a9cd694e7d0
Steve Pieper <pieper@bwh.harvard.edu>
2006-05-02 16:00:35 +0000

    ENH: added a second widget for testing key bindings
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=357
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@357 3bd1e089-480b-0410-8dfb-8563597acbee

--- Testing/MultiColumnListTest.tcl
+++ Testing/MultiColumnListTest.tcl
@@ -25,3 +25,15 @@ update
 $::mcl DeleteAllRows
 $::mcl AddColumn "Test Col"
 $::mcl InsertCellText 0 0 "Test"
+$::mcl InsertCellText 1 0 "Test2"
+
+set ::mcl2 [vtkKWMultiColumnList New]
+$::mcl2 SetParent [$::lframe GetFrame]
+$::mcl2 Create
+pack [$::mcl2 GetWidgetName] -side top -anchor nw -expand false -fill none -padx 2 -pady 2
+
+update
+$::mcl2 DeleteAllRows
+$::mcl2 AddColumn "Test Col"
+$::mcl2 InsertCellText 0 0 "Test"
+$::mcl2 InsertCellText 1 0 "Test2"
