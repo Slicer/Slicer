f6a133216c8984c8caeef606b3228dae03939c61
Steve Pieper <pieper@bwh.harvard.edu>
2006-05-03 22:26:29 +0000

    ENH: new volume handling
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=362
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@362 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/vtkSlicerVolumesGUI.cxx
+++ Base/GUI/vtkSlicerVolumesGUI.cxx
@@ -66,7 +66,7 @@ void vtkSlicerVolumesGUI::AddGUIObservers ( )
 
     // Fill in
     // observer load volume button
-this->LoadVolumeButton->AddObserver ( vtkCommand::ModifiedEvent, (vtkCommand *)this->GUICallbackCommand );
+    this->LoadVolumeButton->AddObserver ( vtkCommand::ModifiedEvent, (vtkCommand *)this->GUICallbackCommand );
 }
 
 
@@ -75,20 +75,26 @@ void vtkSlicerVolumesGUI::ProcessGUIEvents ( vtkObject *caller,
                                              unsigned long event, void *callData )
 {
 
-    // Fill in
+    // Find the widget, and process the events from it...
     vtkKWLoadSaveButton *filebrowse = vtkKWLoadSaveButton::SafeDownCast(caller);
     if (filebrowse == this->LoadVolumeButton  && event == vtkCommand::ModifiedEvent )
         {
             // If a file has been selected for loading...
-            if ( filebrowse->GetFileName ( ) ) 
+            char *fileName = filebrowse->GetFileName();
+            if ( fileName ) 
                 {
                     vtkSlicerVolumesLogic* volumeLogic = this->Logic;
       
-                    if (volumeLogic->AddArchetypeVolume( filebrowse->GetFileName ( ) ) == NULL) 
+                    vtkMRMLVolumeNode *volumeNode = volumeLogic->AddArchetypeVolume( fileName );
+                    if ( volumeNode == NULL ) 
                         {
-        
+                        // TODO: generate an error...
                         }
-      
+                        else
+                        {
+                        this->ApplicationLogic->GetSelectionNode()->SetActiveVolumeID( volumeNode->GetID() );
+                        this->ApplicationLogic->PropagateVolumeSelection();
+                        }
                 }
         }
 }
