72a0bc992d4cf24cb767b47e96d4969bd0ba250d
Steve Pieper <pieper@bwh.harvard.edu>
2006-05-08 20:47:40 +0000

    ENH: test to demonstrate accessing the main UI from a tcl script
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=381
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@381 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/Testing/TestMainNotebook.tcl
@@ -0,0 +1,12 @@
+
+# there should only ever be one application gui in the slicer3 context
+
+
+proc moduleFrame {module} {
+    set appGUI [vtkSlicerApplicationGUI ListInstances]
+    set moduleFrame [[[[$appGUI GetMainSlicerWin] GetMainNotebook] GetFrame $module] GetWidgetName]
+    return $moduleFrame
+}
+
+set f [moduleFrame Volumes]
+button $f.b -text "my button"
