f3ee86095c2c1b8bfc90feeb05d5894e5dc9f4ff
Jim Miller <millerjv@ge.com>
2006-06-09 20:34:25 +0000

    ENH: stubs for parser
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=501
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@501 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/ModuleDescriptionParser/ModuleDescriptionParser.cxx
@@ -0,0 +1,11 @@
+#include "ModuleDescriptionParser.h"
+
+#include "ModuleDescription.h"
+
+#include <string>
+
+void
+ModuleDescriptionParser::Parse( const std::string& xml, ModuleDescription& description)
+{
+
+}
---
+++ Libs/ModuleDescriptionParser/ModuleDescriptionParser.h
@@ -0,0 +1,27 @@
+#ifndef __ModuleDescriptionParser_h
+#define __ModuleDescriptionParser_h
+
+#include <string>
+
+class ModuleDescription;
+
+#if defined(WIN32) && !defined(ModuleDescriptionParser_STATIC)
+#if defined(ModuleDescriptionParser_EXPORTS)
+#define ModuleDescriptionParser_EXPORT __declspec( dllexport ) 
+#else
+#define ModuleDescriptionParser_EXPORT __declspec( dllimport ) 
+#endif
+#else
+#define ModuleDescriptionParser_EXPORT
+#endif
+
+class ModuleDescriptionParser_EXPORT ModuleDescriptionParser
+{
+public:
+  ModuleDescriptionParser() {};
+  ~ModuleDescriptionParser() {};
+
+  void Parse( const std::string& xml, ModuleDescription& description);
+};
+
+#endif
