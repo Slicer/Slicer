03e03f1a7c0b857c398acde410423b64aa4d9e2d
Bill Lorensen <bill.lorensen@gmail.com>
2006-06-12 01:02:11 +0000

    ENH: Added tests.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=511
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@511 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/ModuleDescriptionParser/Testing/Parser1Test.cxx
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+#include "ModuleDescriptionParser.h"
+#include "ModuleDescription.h"
+
+int main (int argc, char *argv[])
+{
+  if (argc < 2)
+    {
+    std::cout << "Usage: ParserTest1 xml_file" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::ifstream fin(argv[1],std::ios::in);
+  if (fin.fail())
+    {
+    std::cerr << argv[0] << ": Cannot open " << argv[1] << " for input" << std::endl;
+    perror(argv[0]);
+    return EXIT_FAILURE;
+    }
+
+  // Get the length of the file
+  fin.seekg (0, std::ios::end);
+  size_t len = fin.tellg();
+  fin.seekg (0, std::ios::beg);
+  char * XML = new char[len+1];
+  fin.read (XML, len);
+
+  ModuleDescription module;
+  ModuleDescriptionParser parser;
+  parser.Parse(XML, module);
+
+  std::cout << module;
+
+  return EXIT_SUCCESS;
+}
