ea403b67d9947b3d7fd46ff0c79412f238c8f80d
Bill Lorensen <bill.lorensen@gmail.com>
2006-06-21 18:31:28 +0000

    ENH: a tour of the execution model.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=643
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@643 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/CLI/ExecutionModelTour.cxx
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+#include "ExecutionModelTourCLP.h"
+
+int main (int argc, char *argv[])
+{
+  PARSE_ARGS;
+
+  return EXIT_SUCCESS;
+}
