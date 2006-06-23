17bab34e966e2018584e4c3efaa2da9f7464e83f
Bill Lorensen <bill.lorensen@gmail.com>
2006-06-23 17:02:10 +0000

    ENH: more new filters.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=686
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@686 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/CLI/OtsuThresholdImageFilter.cxx
@@ -0,0 +1,239 @@
+/*=========================================================================
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: OtsuThresholdImageFilter.cxx,v $
+  Language:  C++
+  Date:      $Date: 2006/05/21 13:00:23 $
+  Version:   $Revision: 1.10 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
+// Software Guide : BeginLatex
+//
+// This example illustrates how to use the \doxygen{OtsuThresholdImageFilter}.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+#include "itkOtsuThresholdImageFilter.h"
+// Software Guide : EndCodeSnippet
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+#include "itkXMLFilterWatcher.h"
+
+#include "OtsuThresholdImageFilterCLP.h"
+
+int main( int argc, char * argv[] )
+{
+  PARSE_ARGS;
+
+  //  Software Guide : BeginLatex
+  //
+  //  The next step is to decide which pixel types to use for the input and output
+  //  images.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef  short  InputPixelType;
+  typedef  short  OutputPixelType;
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The input and output image types are now defined using their respective
+  //  pixel types and dimensions.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::Image< InputPixelType,  3 >   InputImageType;
+  typedef itk::Image< OutputPixelType, 3 >   OutputImageType;
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The filter type can be instantiated using the input and output image
+  //  types defined above.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::OtsuThresholdImageFilter<
+               InputImageType, OutputImageType >  FilterType;
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  An \doxygen{ImageFileReader} class is also instantiated in order to read
+  //  image data from a file. (See Section \ref{sec:IO} on page 
+  //  \pageref{sec:IO} for more information about reading
+  //  and writing data.) 
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::ImageFileReader< InputImageType >  ReaderType;
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //  
+  // An \doxygen{ImageFileWriter} is instantiated in order to write the output
+  // image to a file.
+  //
+  //  Software Guide : EndLatex 
+
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Both the filter and the reader are created by invoking their \code{New()}
+  //  methods and assigning the result to \doxygen{SmartPointer}s.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  ReaderType::Pointer reader = ReaderType::New();
+  FilterType::Pointer filter = FilterType::New();
+  itk::XMLFilterWatcher watcher(filter, "Otsu Threshold Image Filter");
+
+  // Software Guide : EndCodeSnippet
+
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( filter->GetOutput() );
+  reader->SetFileName( inputVolume.c_str() );
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  The image obtained with the reader is passed as input to the
+  //  OtsuThresholdImageFilter.
+  //
+  //  \index{itk::Otsu\-Threshold\-Image\-Filter!SetInput()}
+  //  \index{itk::FileImageReader!GetOutput()}
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetInput( reader->GetOutput() );
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  The method \code{SetOutsideValue()} defines the intensity value to be
+  //  assigned to those pixels whose intensities are outside the range defined
+  //  by the lower and upper thresholds. The method \code{SetInsideValue()}
+  //  defines the intensity value to be assigned to pixels with intensities
+  //  falling inside the threshold range.
+  //  
+  //  \index{itk::Otsu\-Threshold\-Image\-Filter!SetOutsideValue()}
+  //  \index{itk::Otsu\-Threshold\-Image\-Filter!SetInsideValue()}
+  //  \index{SetOutsideValue()!itk::Otsu\-Threshold\-Image\-Filter}
+  //  \index{SetInsideValue()!itk::Otsu\-Threshold\-Image\-Filter}
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetOutsideValue( outsideValue );
+  filter->SetInsideValue(  insideValue  );
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  The method \code{SetNumberOfHistogramBins()} defines the number of bins
+  //  to be used for computing the histogram. This histogram will be used
+  //  internally in order to compute the Otsu threshold.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetNumberOfHistogramBins( numberOfBins );
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  The execution of the filter is triggered by invoking the \code{Update()}
+  //  method.   If the filter's output has been passed as input to subsequent
+  //  filters, the \code{Update()} call on any posterior filters in the
+  //  pipeline will indirectly trigger the update of this filter.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  filter->Update();
+  // Software Guide : EndCodeSnippet
+
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  We print out here the Threshold value that was computed internally by the
+  //  filter. For this we invoke the \code{GetThreshold} method.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  int threshold = filter->GetThreshold();
+  std::cout << "Threshold = " << threshold << std::endl;
+  // Software Guide : EndCodeSnippet
+
+
+
+  //  Software Guide : BeginLatex
+  //  
+  // \begin{figure}
+  // \center
+  // \includegraphics[width=0.44\textwidth]{BrainProtonDensitySlice.eps}
+  // \includegraphics[width=0.44\textwidth]{OtsuThresholdImageFilterOutput.eps}
+  // \itkcaption[OtsuThresholdImageFilter output]{Effect of the OtsuThresholdImageFilter on a slice from a MRI
+  // proton density image  of the brain.}
+  // \label{fig:OtsuThresholdImageFilterInputOutput}
+  // \end{figure}
+  //
+  //  Figure \ref{fig:OtsuThresholdImageFilterInputOutput} illustrates the
+  //  effect of this filter on a MRI proton density image of the brain. This
+  //  figure shows the limitations of this filter for performing segmentation
+  //  by itself. These limitations are particularly noticeable in noisy images
+  //  and in images lacking spatial uniformity as is the case with MRI due to
+  //  field bias.
+  //
+  //  \relatedClasses
+  //  \begin{itemize}
+  //  \item \doxygen{ThresholdImageFilter}
+  //  \end{itemize}
+  //
+  //  Software Guide : EndLatex 
+
+  writer->SetFileName( outputVolume.c_str() );
+  writer->Update();
+
+  return EXIT_SUCCESS;
+}
+
---
+++ Applications/CLI/VotingBinaryHoleFillingImageFilter.cxx
@@ -0,0 +1,241 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: VotingBinaryHoleFillingImageFilter.cxx,v $
+  Language:  C++
+  Date:      $Date: 2005/11/20 13:27:52 $
+  Version:   $Revision: 1.10 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
+// Software Guide : BeginCommandLineArgs
+// INPUTS: {BinaryThresholdImageFilterOutput.png}
+// OUTPUTS: {VotingBinaryHoleFillingImageFilterOutput1.png}
+// 1 1 
+// Software Guide : EndCommandLineArgs
+
+// Software Guide : BeginCommandLineArgs
+// INPUTS: {BinaryThresholdImageFilterOutput.png}
+// OUTPUTS: {VotingBinaryHoleFillingImageFilterOutput2.png}
+// 2 2 
+// Software Guide : EndCommandLineArgs
+
+// Software Guide : BeginCommandLineArgs
+// INPUTS: {BinaryThresholdImageFilterOutput.png}
+// OUTPUTS: {VotingBinaryHoleFillingImageFilterOutput3.png}
+// 3 3 
+// Software Guide : EndCommandLineArgs
+
+
+//  Software Guide : BeginLatex
+//
+//  The \doxygen{VotingBinaryHoleFillingImageFilter} applies a voting operation
+//  in order to fill-in cavities. This can be used for smoothing contours and
+//  for filling holes in binary images. 
+//
+//  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter}
+//
+//  Software Guide : EndLatex 
+
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+
+//  Software Guide : BeginLatex
+//
+//  The header file corresponding to this filter should be included first.
+//
+//  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!header}
+//
+//  Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
+#include "itkVotingBinaryHoleFillingImageFilter.h"
+#include "itkXMLFilterWatcher.h"
+
+// Software Guide : EndCodeSnippet
+#include "VotingBinaryHoleFillingImageFilterCLP.h"
+
+int main( int argc, char * argv[] )
+{
+  PARSE_ARGS;
+
+  //  Software Guide : BeginLatex
+  //
+  //  Then the pixel and image types of the input and output must be defined.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef   short  InputPixelType;
+  typedef   short  OutputPixelType;
+
+  typedef itk::Image< InputPixelType,  3 >   InputImageType;
+  typedef itk::Image< OutputPixelType, 3 >   OutputImageType;
+  // Software Guide : EndCodeSnippet
+
+
+  typedef itk::ImageFileReader< InputImageType  >  ReaderType;
+  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+
+  reader->SetFileName( inputVolume.c_str() );
+  writer->SetFileName( outputVolume.c_str() );
+
+  //  Software Guide : BeginLatex
+  //
+  //  Using the image types, it is now possible to define the filter type
+  //  and create the filter object.
+  //
+  //  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!instantiation}
+  //  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!New()}
+  //  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!Pointer}
+  // 
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::VotingBinaryHoleFillingImageFilter<
+               InputImageType, OutputImageType >  FilterType;
+
+  FilterType::Pointer filter = FilterType::New();
+  itk::XMLFilterWatcher watcher(filter, "Median Image Filter");
+
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The size of the neighborhood is defined along every dimension by
+  //  passing a \code{SizeType} object with the corresponding values. The
+  //  value on each dimension is used as the semi-size of a rectangular
+  //  box. For example, in $2D$ a size of \(1,2\) will result in a $3 \times
+  //  5$ neighborhood.
+  //
+  //  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!Radius}
+  //  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!Neighborhood}
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  InputImageType::SizeType indexRadius;
+  
+  indexRadius[0] = radius[0]; // radius along x
+  indexRadius[1] = radius[1]; // radius along y
+  indexRadius[2] = radius[2]; // radius along z
+
+  filter->SetRadius( indexRadius );
+  // Software Guide : EndCodeSnippet
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Since the filter is expecting a binary image as input, we must specify
+  //  the levels that are going to be considered background and foreground. This
+  //  is done with the \code{SetForegroundValue()} and
+  //  \code{SetBackgroundValue()} methods.
+  //
+  //  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!SetForegroundValue()}
+  //  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!SetBackgroundValue()}
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetBackgroundValue( background );
+  filter->SetForegroundValue( foreground );
+  // Software Guide : EndCodeSnippet
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  We must also specify the majority threshold that is going to be used as
+  //  the decision criterion for converting a background pixel into a
+  //  foreground pixel. The rule of conversion is that a background pixel will
+  //  be converted into a foreground pixel if the number of foreground
+  //  neighbors surpass the number of background neighbors by the majority
+  //  value. For example, in a 2D image, with neighborhood or radius 1, the
+  //  neighborhood will have size $3 \times 3$. If we set the majority value to
+  //  2, then we are requiring that the number of foreground neighbors should
+  //  be at least (3x3 -1 )/2 + majority. This is done with the
+  //  \code{SetMajorityThreshold()} method.
+  //
+  //  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!SetMajorityThreshold()}
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetMajorityThreshold( majorityThreshold );
+  // Software Guide : EndCodeSnippet
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The input to the filter can be taken from any other filter, for example
+  //  a reader. The output can be passed down the pipeline to other filters,
+  //  for example, a writer. An update call on any downstream filter will
+  //  trigger the execution of the median filter.
+  //
+  //  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!SetInput()}
+  //  \index{itk::Voting\-Binary\-Hole\-Filling\-Image\-Filter!GetOutput()}
+  //
+  //  Software Guide : EndLatex 
+
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetInput( reader->GetOutput() );
+  writer->SetInput( filter->GetOutput() );
+  writer->Update();
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  // 
+  // \begin{figure}
+  // \center
+  // \includegraphics[width=0.44\textwidth]{BinaryThresholdImageFilterOutput.eps}
+  // \includegraphics[width=0.44\textwidth]{VotingBinaryHoleFillingImageFilterOutput1.eps}
+  // \includegraphics[width=0.44\textwidth]{VotingBinaryHoleFillingImageFilterOutput2.eps}
+  // \includegraphics[width=0.44\textwidth]{VotingBinaryHoleFillingImageFilterOutput3.eps}
+  // \itkcaption[Effect of the VotingBinaryHoleFilling filter.]{Effect of the
+  // VotingBinaryHoleFillingImageFilter on a slice from a MRI proton density brain image
+  // that has been thresholded in order to produce a binary image. The output
+  // images have used radius 1,2 and 3 respectively.}
+  // \label{fig:VotingBinaryHoleFillingImageFilterOutput}
+  // \end{figure}
+  //
+  //  Figure \ref{fig:VotingBinaryHoleFillingImageFilterOutput} illustrates the effect of
+  //  the VotingBinaryHoleFillingImageFilter filter on a thresholded slice of MRI brain
+  //  image using neighborhood radii of \(1,1\), \(2,2\) and \(3,3\) that
+  //  correspond respectively to neighborhoods of size $ 3 \times 3 $,  $ 5
+  //  \times 5 $, $ 7 \times 7 $.  The filtered image demonstrates the
+  //  capability of this filter for reducing noise both in the background and
+  //  foreground of the image, as well as smoothing the contours of the regions.
+  //  
+  //  Software Guide : EndLatex 
+
+
+  return EXIT_SUCCESS;
+}
+
