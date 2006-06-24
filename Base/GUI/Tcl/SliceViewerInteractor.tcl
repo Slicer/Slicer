0cfe81f5f8f3903660ec40ebf3da47d6465415c9
Steve Pieper <pieper@bwh.harvard.edu>
2006-06-24 23:01:59 +0000

    ENH: support for panning and zooming in the slice windows!
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=722
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@722 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Tcl/SliceViewerInteractor.tcl
+++ Base/GUI/Tcl/SliceViewerInteractor.tcl
@@ -28,12 +28,37 @@ proc SliceViewerRemoveObservers {sliceViewer} {
   }
 }
 
+set ::SliceViewerMode ""
+set ::SliceViewerStorageXYToRAS [vtkMatrix4x4 New]
+set ::SliceViewerStorageSliceToRAS [vtkMatrix4x4 New]
+set ::SliceViewerScratchMatrix [vtkMatrix4x4 New]
+
 proc SliceViewerHandleEvent {sliceGUI event} {
 
   set interactor [[$sliceGUI GetSliceViewer] GetRenderWindowInteractor]
+  set sliceNode [[$sliceGUI GetLogic]  GetSliceNode]
+  set sliceCompositeNode [[$sliceGUI GetLogic]  GetSliceCompositeNode]
+  set background [[$sliceGUI GetLogic]  GetBackgroundLayer]
+  set backgroundNode [$background GetVolumeNode]
+  set backgroundImage [$backgroundNode GetImageData]
+  
+  foreach {x y} [$interactor GetEventPosition] {}
 
-  if { [lsearch "MouseMoveEvent ModifiedEvent" $event] == -1 } {
-    puts -nonewline "got a $event for $sliceGUI, interactor $interactor at [$interactor GetEventPosition]"
+  set xyToRAS [$sliceNode GetXYToRAS]
+  set ras [$xyToRAS MultiplyPoint $x $y 0 1]
+
+  set xyToIJK [$background GetXYToIJKTransform]
+  foreach {i j k} [$xyToIJK TransformVector $x $y 0] {}
+  foreach v {i j k} { ;# cast to integer
+    set $v [expr int([set $v])]
+  }
+
+  #set pixel [$backgroundImage GetScalarComponentAsDouble $i $j $k 0]
+
+  set ignoreEvents "MouseMoveEvent ModifiedEvent TimerEvent RenderEvent"
+
+  if { [lsearch $ignoreEvents $event] == -1 } {
+    puts -nonewline "got a $event for $sliceGUI, interactor $interactor at xy $x $y, ras $ras"
     if { [$interactor GetControlKey] } {
       puts -nonewline " with control"
     }
@@ -43,11 +68,52 @@ proc SliceViewerHandleEvent {sliceGUI event} {
     puts ""
   }
 
+  if { [string match "*PressEvent" $event] } {
+    set ::SliceViewerStartRAS $ras 
+    set ::SliceViewerStartXY [$interactor GetEventPosition]
+  }
+
+  if { [string match "*ReleaseEvent" $event] ||
+        [string match "LeaveEvent" $event] } {
+    set ::SliceViewerMode ""
+  }
+
+
   switch $event {
 
     MouseMoveEvent {
+      # puts "background pixel at $i $j $k is $pixel"
+      switch $::SliceViewerMode {
+        Translate {
+          set currentRAS [$::SliceViewerStorageXYToRAS MultiplyPoint $x $y 0 1]
+          foreach d {dr da ds} start $::SliceViewerStartRAS current $currentRAS {
+            set $d [expr $current - $start]
+          }
+          $::SliceViewerScratchMatrix DeepCopy $::SliceViewerStorageSliceToRAS
+          foreach d {dr da ds} i {0 1 2} {
+            set v [$::SliceViewerScratchMatrix GetElement $i 3]
+            $::SliceViewerScratchMatrix SetElement $i 3 [expr $v - [set $d]]
+          }
+          [$sliceNode GetSliceToRAS] DeepCopy $::SliceViewerScratchMatrix
+          $sliceNode UpdateMatrices
+        }
+        Zoom {
+          set deltay [expr $y - [lindex $::SliceViewerStartXY 1]]
+          set h [lindex [[[[$sliceGUI GetSliceViewer]  GetRenderWidget]  GetRenderWindow]  GetSize] 1]
+          set percent [expr ($h + $deltay) / (1.0 * $h)]
+          set newFOV ""
+          foreach f $::SliceViewerStorageFieldOfView {
+            lappend newFOV [expr $f * $percent]
+          }
+          eval $sliceNode SetFieldOfView $newFOV
+          $sliceNode UpdateMatrices
+        }
+      }
     }
     RightButtonPressEvent {
+      set ::SliceViewerMode Zoom
+      set ::SliceViewerStorageFieldOfView [$sliceNode GetFieldOfView]
+      $::slicer3::MRMLScene SaveStateForUndo $sliceNode
     }
     RightButtonReleaseEvent {
     }
@@ -56,6 +122,10 @@ proc SliceViewerHandleEvent {sliceGUI event} {
     LeftButtonReleaseEvent {
     }
     MiddleButtonPressEvent {
+      set ::SliceViewerMode Translate
+      $::SliceViewerStorageXYToRAS DeepCopy [$sliceNode GetXYToRAS]
+      $::SliceViewerStorageSliceToRAS DeepCopy [$sliceNode GetSliceToRAS]
+      $::slicer3::MRMLScene SaveStateForUndo $sliceNode
     }
     MiddleButtonReleaseEvent {
     }
@@ -67,9 +137,8 @@ proc SliceViewerHandleEvent {sliceGUI event} {
     }
     ConfigureEvent {
       set size [[[[$sliceGUI GetSliceViewer]  GetRenderWidget]  GetRenderWindow]  GetSize]
-      foreach {x y} $size {}
-      if { $x < $y } { set min $x } else { set min $y }
-      set sliceNode [[$sliceGUI GetLogic]  GetSliceNode]
+      foreach {w h} $size {}
+      if { $w < $h } { set min $w } else { set min $h }
       set oldDim [$sliceNode GetDimensions]
       $sliceNode SetDimensions $min $min [lindex $oldDim 2]
       puts "[$sliceNode GetDimensions]"
