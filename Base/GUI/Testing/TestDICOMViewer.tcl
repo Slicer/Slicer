44a67b0a0da0a766119dc341296149c44c4a1428
Steve Pieper <pieper@bwh.harvard.edu>
2006-06-29 16:26:14 +0000

    ENH: set up demo with now frame
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=798
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@798 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Testing/TestDICOMViewer.tcl
+++ Base/GUI/Testing/TestDICOMViewer.tcl
@@ -4,13 +4,16 @@ catch "f Delete"
 catch "app Delete"
 catch "appLogic Delete"
 catch "scene Delete"
+catch "sliceLogic Delete"
 catch "sliceGUI Delete"
 
+source $::SLICER_BUILD/SliceViewerInteractor.tcl
 
 vtkSlicerApplication app
 vtkSlicerApplicationLogic appLogic
 vtkMRMLScene scene
 appLogic SetAndObserveMRMLScene scene
+appLogic ProcessMRMLEvents
 
 vtkKWTopLevel t
 t SetApplication app
@@ -21,84 +24,56 @@ f SetParent t
 f Create
 pack [f GetWidgetName] -fill both -expand true
 
+vtkSlicerSliceLogic sliceLogic
+sliceLogic SetMRMLScene scene
+sliceLogic ProcessMRMLEvents
+sliceLogic ProcessLogicEvents
+sliceLogic SetAndObserveMRMLScene scene
+
 vtkSlicerSliceGUI sliceGUI
 sliceGUI SetApplication app
 sliceGUI SetApplicationLogic appLogic
 sliceGUI SetAndObserveMRMLScene scene
+sliceGUI SetAndObserveModuleLogic sliceLogic
 sliceGUI BuildGUI f
+sliceGUI AddGUIObservers
 
+t HideDecorationOn
 t Display
 
 
-
-
-set dicomArchetype [tk_getOpenFile]
 
 catch "volumeNode Delete"
 catch "displayNode Delete"
 catch "storageNode Delete"
 
-vtkMRMLScalarNode volumeNode
+vtkMRMLScalarVolumeNode volumeNode
 vtkMRMLVolumeDisplayNode displayNode
 vtkMRMLVolumeArchetypeStorageNode storageNode
 
+volumeNode SetScene scene
+displayNode SetScene scene
+storageNode SetScene scene
+
+scene AddNode volumeNode
+scene AddNode displayNode
+scene AddNode storageNode
+
+volumeNode SetName dicom
+volumeNode SetStorageNodeID [storageNode GetID]
+volumeNode SetDisplayNodeID [displayNode GetID]
+
+
 set dicomArchetype [tk_getOpenFile]
-storageNode SetAbsoluteFileName $dicomArchetype
+storageNode SetFileArchetype $dicomArchetype
+storageNode ReadData volumeNode
 
 
-tk_messageBox -message "finished"
+[appLogic GetSelectionNode] SetActiveVolumeID [volumeNode GetID]
+appLogic PropagateVolumeSelection
 
-  storageNode->SetFileArchetype(filename);
-  if (storageNode->ReadData(scalarNode) == 0)
-    {
-    // cannot read scalar data, try vector
-    if (storageNode->ReadData(vectorNode) != 0)
-      {
-      volumeNode = vectorNode;
-      }
-    }
-  else
-    {
-    volumeNode = scalarNode;
-    }
-  
-  if (volumeNode != NULL)
-    {
-    const vtksys_stl::string fname(filename);
-    vtksys_stl::string name = vtksys::SystemTools::GetFilenameName(fname);
-    volumeNode->SetName(name.c_str());
+[[sliceGUI GetSliceController] GetSliceNode] SetOrientationToCoronal
+pack forget [[sliceGUI GetSliceController] GetWidgetName]
 
-    this->GetMRMLScene()->SaveStateForUndo();
-
-    volumeNode->SetScene(this->GetMRMLScene());
-    storageNode->SetScene(this->GetMRMLScene());
-    displayNode->SetScene(this->GetMRMLScene());
-
-    double range[2];
-    volumeNode->GetImageData()->GetScalarRange(range);
-    displayNode->SetLowerThreshold(range[0]);
-    displayNode->SetUpperThreshold(range[1]);
-    displayNode->SetWindow(range[1] - range[0]);
-    displayNode->SetLevel(0.5 * (range[1] + range[0]) );
-
-    this->GetMRMLScene()->AddNode(volumeNode);  
-    this->GetMRMLScene()->AddNode(storageNode);  
-    this->GetMRMLScene()->AddNode(displayNode);  
-
-    volumeNode->SetStorageNodeID(storageNode->GetID());
-
-    volumeNode->SetDisplayNodeID(displayNode->GetID());
-    
-    this->SetActiveVolumeNode(volumeNode);
-    
-    this->Modified();  
-    }
-
-  scalarNode->Delete();
-  vectorNode->Delete();
-  storageNode->Delete();
-  displayNode->Delete();
-
-  return volumeNode;
-}
-
+set quit 0
+vwait quit
