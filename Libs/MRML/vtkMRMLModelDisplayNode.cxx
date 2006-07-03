84ad2769588846d0955e879ef75198f0488f8006
Alex Yarmarkovich <alexy@bwh.harvard.edu>
2006-07-03 20:23:54 +0000

    ENH: added support for model display
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=834
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@834 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/MRML/vtkMRMLModelDisplayNode.cxx
@@ -0,0 +1,263 @@
+/*=auto=========================================================================
+
+Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+See Doc/copyright/copyright.txt
+or http://www.slicer.org/copyright/copyright.txt for details.
+
+Program:   3D Slicer
+Module:    $RCSfile: vtkMRMLModelDisplayNode.cxx,v $
+Date:      $Date: 2006/03/03 22:26:39 $
+Version:   $Revision: 1.3 $
+
+=========================================================================auto=*/
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "vtkObjectFactory.h"
+
+#include "vtkMRMLModelDisplayNode.h"
+#include "vtkMRMLScene.h"
+
+//------------------------------------------------------------------------------
+vtkMRMLModelDisplayNode* vtkMRMLModelDisplayNode::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLModelDisplayNode");
+  if(ret)
+    {
+    return (vtkMRMLModelDisplayNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLModelDisplayNode;
+}
+
+//-----------------------------------------------------------------------------
+
+vtkMRMLNode* vtkMRMLModelDisplayNode::CreateNodeInstance()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLModelDisplayNode");
+  if(ret)
+    {
+    return (vtkMRMLModelDisplayNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLModelDisplayNode;
+}
+
+
+//----------------------------------------------------------------------------
+vtkMRMLModelDisplayNode::vtkMRMLModelDisplayNode()
+{
+
+  PolyData = NULL;
+
+  // Strings
+  this->Color = NULL;
+
+  // Numbers
+  this->Opacity = 1.0;
+  this->Visibility = 1;
+  this->Clipping = 0;
+  this->BackfaceCulling = 1;
+  this->ScalarVisibility = 0;
+  this->VectorVisibility = 0;
+  this->TensorVisibility = 0;
+  
+  // Arrays
+  this->ScalarRange[0] = 0;
+  this->ScalarRange[1] = 100;
+
+  // Scalars
+  this->LUTName = -1;
+
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLModelDisplayNode::~vtkMRMLModelDisplayNode()
+{
+  if (this->Color) 
+    {
+    delete [] this->Color;
+    this->Color = NULL;
+    }
+  if (this->PolyData) 
+    {
+    this->PolyData->Delete();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLModelDisplayNode::WriteXML(ostream& of, int nIndent)
+{
+  // Write all attributes not equal to their defaults
+  
+  Superclass::WriteXML(of, nIndent);
+
+  vtkIndent indent(nIndent);
+
+  if (this->Color && strcmp(this->Color, "")) 
+    {
+    of << indent << " color=\"" << this->Color << "\"";
+    }
+
+  //if (this->LUTName && strcmp(this->LUTName,""))
+  if (this->LUTName != -1)
+    {
+    of << indent << " lutName=\"" << this->LUTName << "\"";
+    }
+  
+  // Numbers
+  if (this->Opacity != 1.0)
+    {
+    of << indent << " opacity=\"" << this->Opacity << "\"";
+    }
+  if (this->Visibility != 1)
+    {
+    of << indent << " visibility=\"" << (this->Visibility ? "true" : "false") << "\"";
+    }
+  if (this->Clipping != 0)
+    {
+    of << indent << " clipping=\"" << (this->Clipping ? "true" : "false") << "\"";
+    }
+  if (this->BackfaceCulling != 1)
+    {
+    of << indent << " backfaceCulling=\"" << (this->BackfaceCulling ? "true" : "false") << "\"";
+    }
+  if (this->ScalarVisibility != 0)
+    {
+    of << indent << " scalarVisibility=\"" << (this->ScalarVisibility ? "true" : "false") << "\"";
+    }
+
+  // Arrays
+  if (this->ScalarRange[0] != 0 || this->ScalarRange[1] != 100)
+    {
+    of << indent << " scalarRange=\"" << this->ScalarRange[0] << " "
+       << this->ScalarRange[1] << "\"";
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLModelDisplayNode::ReadXMLAttributes(const char** atts)
+{
+
+  vtkMRMLNode::ReadXMLAttributes(atts);
+
+  const char* attName;
+  const char* attValue;
+  while (*atts != NULL) 
+    {
+    attName = *(atts++);
+    attValue = *(atts++);
+    if (!strcmp(attName, "color")) 
+      {
+      this->SetColor(attValue);
+      }
+    else if (!strcmp(attName, "scalarRange")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> ScalarRange[0];
+      ss >> ScalarRange[1];
+      }
+    else if (!strcmp(attName, "LUTName")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> LUTName;
+      }
+    else if (!strcmp(attName, "opacity")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> Opacity;
+      }
+    else if (!strcmp(attName, "visibility")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> Visibility;
+      }
+    else if (!strcmp(attName, "backfaceCulling")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> BackfaceCulling;
+      }
+    else if (!strcmp(attName, "scalarVisibility")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> ScalarVisibility;
+      }
+    else if (!strcmp(attName, "vectorVisibility")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> VectorVisibility;
+      }
+    else if (!strcmp(attName, "tensorVisibility")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> TensorVisibility;
+      }
+    }  
+}
+
+
+//----------------------------------------------------------------------------
+// Copy the node's attributes to this object.
+// Does NOT copy: ID, FilePrefix, Name, ID
+void vtkMRMLModelDisplayNode::Copy(vtkMRMLNode *anode)
+{
+  vtkMRMLNode::Copy(anode);
+  vtkMRMLModelDisplayNode *node = (vtkMRMLModelDisplayNode *) anode;
+
+  // Strings
+
+  this->SetColor(node->Color);
+
+  // Vectors
+  this->SetScalarRange(node->ScalarRange);
+  
+  // Numbers
+  this->SetOpacity(node->Opacity);
+  this->SetVisibility(node->Visibility);
+  this->SetScalarVisibility(node->ScalarVisibility);
+  this->SetBackfaceCulling(node->BackfaceCulling);
+  this->SetClipping(node->Clipping);
+  this->SetPolyData(node->PolyData);
+
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLModelDisplayNode::PrintSelf(ostream& os, vtkIndent indent)
+{
+  int idx;
+  
+  vtkMRMLNode::PrintSelf(os,indent);
+
+  os << indent << "Color: " <<
+    (this->Color ? this->Color : "(none)") << "\n";
+
+  os << indent << "Opacity:           " << this->Opacity << "\n";
+  os << indent << "Visibility:        " << this->Visibility << "\n";
+  os << indent << "ScalarVisibility:  " << this->ScalarVisibility << "\n";
+  os << indent << "BackfaceCulling:   " << this->BackfaceCulling << "\n";
+  os << indent << "Clipping:          " << this->Clipping << "\n";
+
+  os << "ScalarRange:\n";
+  for (idx = 0; idx < 2; ++idx)
+    {
+    os << indent << ", " << this->ScalarRange[idx];
+    }
+  os << "\nPoly Data:\n";
+  if (this->PolyData) 
+    {
+    this->PolyData->PrintSelf(os, indent.GetNextIndent());
+    }
+
+}
---
+++ Libs/MRML/vtkMRMLModelDisplayNode.h
@@ -0,0 +1,156 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLModelDisplayNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:28 $
+  Version:   $Revision: 1.6 $
+
+=========================================================================auto=*/
+// .NAME vtkMRMLModelDisplayNode - MRML node to represent a 3D surface model.
+// .SECTION Description
+// Model nodes describe polygonal data.  They indicate where the model is 
+// stored on disk, and how to render it (color, opacity, etc).  Models 
+// are assumed to have been constructed with the orientation and voxel 
+// dimensions of the original segmented volume.
+
+#ifndef __vtkMRMLModelDisplayNode_h
+#define __vtkMRMLModelDisplayNode_h
+
+#include <string>
+
+#include "vtkMRML.h"
+#include "vtkMRMLNode.h"
+#include "vtkMRMLStorageNode.h"
+
+#include "vtkMatrix4x4.h"
+#include "vtkTransform.h"
+#include "vtkPolyData.h" 
+
+
+class VTK_MRML_EXPORT vtkMRMLModelDisplayNode : public vtkMRMLNode
+{
+public:
+  static vtkMRMLModelDisplayNode *New();
+  vtkTypeMacro(vtkMRMLModelDisplayNode,vtkMRMLNode);
+  void PrintSelf(ostream& os, vtkIndent indent);
+  
+  //--------------------------------------------------------------------------
+  // MRMLNode methods
+  //--------------------------------------------------------------------------
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+
+  // Description:
+  // Set node attributes
+  virtual void ReadXMLAttributes( const char** atts);
+
+  // Description:
+  // Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+
+  // Description:
+  // Copy the node's attributes to this object
+  virtual void Copy(vtkMRMLNode *node);
+  
+  // Description:
+  // Get node XML tag name (like Volume, Model)
+  virtual const char* GetNodeTagName() {return "ModelDisplay";};
+
+
+  // Description:
+  // Name of the model's color, which is defined by a Color node in a MRML file
+  vtkSetStringMacro(Color);
+  vtkGetStringMacro(Color);
+
+  // Description:
+  // Opacity of the surface expressed as a number from 0 to 1
+  vtkSetMacro(Opacity, float);
+  vtkGetMacro(Opacity, float);
+
+  // Description:
+  // Indicates if the surface is visible
+  vtkBooleanMacro(Visibility, int);
+  vtkGetMacro(Visibility, int);
+  vtkSetMacro(Visibility, int);
+
+  // Description:
+  // Specifies whether to clip the surface with the slice planes
+  vtkBooleanMacro(Clipping, int);
+  vtkGetMacro(Clipping, int);
+  vtkSetMacro(Clipping, int);
+
+  // Description:
+  // Indicates whether to cull (not render) the backface of the surface
+  vtkBooleanMacro(BackfaceCulling, int);
+  vtkGetMacro(BackfaceCulling, int);
+  vtkSetMacro(BackfaceCulling, int);
+
+  // Description:
+  // Indicates whether to render the scalar value associated with each polygon vertex
+  vtkBooleanMacro(ScalarVisibility, int);
+  vtkGetMacro(ScalarVisibility, int);
+  vtkSetMacro(ScalarVisibility, int);
+
+  // Description:
+  // Indicates whether to render the vector value associated with each polygon vertex
+  vtkBooleanMacro(VectorVisibility, int);
+  vtkGetMacro(VectorVisibility, int);
+  vtkSetMacro(VectorVisibility, int);
+
+  // Description:
+  // Indicates whether to render the tensor value associated with each polygon vertex
+  vtkBooleanMacro(TensorVisibility, int);
+  vtkGetMacro(TensorVisibility, int);
+  vtkSetMacro(TensorVisibility, int);
+
+  // Description:
+  // Range of scalar values to render rather than the single color designated by colorName
+  vtkSetVector2Macro(ScalarRange, double);
+  vtkGetVector2Macro(ScalarRange, double);
+
+
+  // Description:
+  // Numerical ID of the color lookup table to use for rendering the overlay
+  // for this model
+  vtkGetMacro(LUTName,int);
+  vtkSetMacro(LUTName,int);
+    
+  vtkGetObjectMacro(PolyData, vtkPolyData);
+  vtkSetObjectMacro(PolyData, vtkPolyData);
+
+protected:
+  vtkMRMLModelDisplayNode();
+  ~vtkMRMLModelDisplayNode();
+  vtkMRMLModelDisplayNode(const vtkMRMLModelDisplayNode&);
+  void operator=(const vtkMRMLModelDisplayNode&);
+
+  // Data
+  vtkPolyData *PolyData;
+
+  // Strings
+  char *Color;
+  int LUTName;
+    
+  // Numbers
+  float Opacity;
+
+  // Booleans
+  int Visibility;
+  int Clipping;
+  int BackfaceCulling;
+  int ScalarVisibility;
+  int VectorVisibility;
+  int TensorVisibility;
+
+  // Arrays
+  double ScalarRange[2];
+
+};
+
+#endif
