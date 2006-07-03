95b4e8bb33b53a934f96c54cd0e1a85d4eacd8b9
Alex Yarmarkovich <alexy@bwh.harvard.edu>
2006-07-03 15:29:14 +0000

    ENH: added support for models
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=830
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@830 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/Logic/vtkSlicerModelsLogic.cxx
@@ -0,0 +1,105 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerModelsLogic.cxx,v $
+  Date:      $Date: 2006/01/06 17:56:48 $
+  Version:   $Revision: 1.58 $
+
+=========================================================================auto=*/
+
+#include "vtkObjectFactory.h"
+#include "vtkCallbackCommand.h"
+#include <vtksys/SystemTools.hxx> 
+
+#include "vtkSlicerModelsLogic.h"
+
+#include "vtkMRMLModelNode.h"
+#include "vtkMRMLModelStorageNode.h"
+#include "vtkMRMLModelDisplayNode.h"
+
+vtkCxxRevisionMacro(vtkSlicerModelsLogic, "$Revision: 1.9.12.1 $");
+vtkStandardNewMacro(vtkSlicerModelsLogic);
+
+//----------------------------------------------------------------------------
+vtkSlicerModelsLogic::vtkSlicerModelsLogic()
+{
+  this->ActiveModelNode = NULL;
+}
+
+//----------------------------------------------------------------------------
+vtkSlicerModelsLogic::~vtkSlicerModelsLogic()
+{
+  if (this->ActiveModelNode != NULL)
+    {
+        this->ActiveModelNode->Delete();
+        this->ActiveModelNode = NULL;
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModelsLogic::ProcessMRMLEvents()
+{
+  // TODO: implement if needed
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModelsLogic::SetActiveModelNode(vtkMRMLModelNode *activeNode)
+{
+  this->SetMRML( vtkObjectPointer(&this->ActiveModelNode), activeNode );
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLModelNode* vtkSlicerModelsLogic::AddModel (char* filename)
+{
+  vtkMRMLModelNode *modelNode = vtkMRMLModelNode::New();
+  vtkMRMLModelDisplayNode *displayNode = vtkMRMLModelDisplayNode::New();
+  vtkMRMLModelStorageNode *storageNode = vtkMRMLModelStorageNode::New();
+
+  storageNode->SetFileName(filename);
+  if (storageNode->ReadData(modelNode) != 0)
+    {
+    const vtksys_stl::string fname(filename);
+    vtksys_stl::string name = vtksys::SystemTools::GetFilenameName(fname);
+    modelNode->SetName(name.c_str());
+
+    this->GetMRMLScene()->SaveStateForUndo();
+
+    modelNode->SetScene(this->GetMRMLScene());
+    storageNode->SetScene(this->GetMRMLScene());
+    displayNode->SetScene(this->GetMRMLScene());
+
+    this->GetMRMLScene()->AddNode(modelNode);  
+    this->GetMRMLScene()->AddNode(storageNode);  
+    this->GetMRMLScene()->AddNode(displayNode);  
+
+    modelNode->SetStorageNodeID(storageNode->GetID());
+
+    modelNode->SetDisplayNodeID(displayNode->GetID());
+        
+    this->Modified();  
+    }
+
+  modelNode->Delete();
+  storageNode->Delete();
+  displayNode->Delete();
+
+  return modelNode;  
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModelsLogic::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->vtkObject::PrintSelf(os, indent);
+
+  os << indent << "vtkSlicerModelsLogic:             " << this->GetClassName() << "\n";
+
+  os << indent << "ActiveModelNode: " <<
+    (this->ActiveModelNode ? this->ActiveModelNode->GetName() : "(none)") << "\n";
+}
+
---
+++ Base/Logic/vtkSlicerModelsLogic.h
@@ -0,0 +1,68 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerModelsLogic.h,v $
+  Date:      $Date: 2006/01/08 04:48:05 $
+  Version:   $Revision: 1.45 $
+
+=========================================================================auto=*/
+
+// .NAME vtkSlicerModelsLogic - slicer logic class for volumes manipulation
+// .SECTION Description
+// This class manages the logic associated with reading, saving,
+// and changing propertied of the volumes
+
+
+#ifndef __vtkSlicerModelsLogic_h
+#define __vtkSlicerModelsLogic_h
+
+#include <stdlib.h>
+
+#include "vtkSlicerBaseLogic.h"
+#include "vtkSlicerLogic.h"
+
+#include "vtkMRML.h"
+#include "vtkMRMLModelNode.h"
+
+
+class VTK_SLICER_BASE_LOGIC_EXPORT vtkSlicerModelsLogic : public vtkSlicerLogic 
+{
+  public:
+  
+  // The Usual vtk class functions
+  static vtkSlicerModelsLogic *New();
+  vtkTypeRevisionMacro(vtkSlicerModelsLogic,vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // The currently active mrml volume node 
+  vtkGetObjectMacro (ActiveModelNode, vtkMRMLModelNode);
+  void SetActiveModelNode (vtkMRMLModelNode *ActiveModelNode);
+
+  // Description:
+  // Create new mrml node and associated storage node.
+  // Read image data from a specified file
+  vtkMRMLModelNode* AddModel (char* filename);
+
+  // Description:
+  // Update logic state when MRML scene chenges
+  void ProcessMRMLEvents(); 
+    
+protected:
+  vtkSlicerModelsLogic();
+  ~vtkSlicerModelsLogic();
+  vtkSlicerModelsLogic(const vtkSlicerModelsLogic&);
+  void operator=(const vtkSlicerModelsLogic&);
+
+  // Description:
+  //
+  vtkMRMLModelNode *ActiveModelNode;
+};
+
+#endif
+
