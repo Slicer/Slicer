df99025e2941ab9b987b263e014d8ae916710083
Steve Pieper <pieper@bwh.harvard.edu>
2006-07-07 11:41:39 +0000

    BUG: debug leak testing
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=848
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@848 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Testing/TestDICOMViewer.tcl
+++ Base/GUI/Testing/TestDICOMViewer.tcl
@@ -1,79 +1,118 @@
 
 catch "t Delete"
 catch "f Delete"
-catch "app Delete"
+catch "slicerApp Delete"
 catch "appLogic Delete"
 catch "scene Delete"
-catch "sliceLogic Delete"
 catch "sliceGUI Delete"
 
 source $::SLICER_BUILD/SliceViewerInteractor.tcl
 
-vtkSlicerApplication app
+vtkSlicerApplication slicerApp
+slicerApp StartApplication
+
 vtkSlicerApplicationLogic appLogic
 vtkMRMLScene scene
 appLogic SetAndObserveMRMLScene scene
 appLogic ProcessMRMLEvents
 
-vtkKWTopLevel t
-t SetApplication app
-t Create
+if { 1 } {
 
-vtkKWFrame f
-f SetParent t
-f Create
-pack [f GetWidgetName] -fill both -expand true
+  vtkKWTopLevel t
+  t SetApplication slicerApp
+  t Create
 
-vtkSlicerSliceLogic sliceLogic
-sliceLogic SetMRMLScene scene
-sliceLogic ProcessMRMLEvents
-sliceLogic ProcessLogicEvents
-sliceLogic SetAndObserveMRMLScene scene
+  vtkKWFrame f
+  f SetParent t
+  f Create
+  pack [f GetWidgetName] -fill both -expand true
 
-vtkSlicerSliceGUI sliceGUI
-sliceGUI SetApplication app
-sliceGUI SetApplicationLogic appLogic
-sliceGUI SetAndObserveMRMLScene scene
-sliceGUI SetAndObserveModuleLogic sliceLogic
-sliceGUI BuildGUI f
-sliceGUI AddGUIObservers
-
-t HideDecorationOn
-t Display
+  t SetSize 512 512
+  t HideDecorationOn
+  t Display
 
 
+  if { 1 } {
 
-catch "volumeNode Delete"
-catch "displayNode Delete"
-catch "storageNode Delete"
+    catch "volumeNode Delete"
+    catch "displayNode Delete"
+    catch "storageNode Delete"
 
-vtkMRMLScalarVolumeNode volumeNode
-vtkMRMLVolumeDisplayNode displayNode
-vtkMRMLVolumeArchetypeStorageNode storageNode
+    vtkMRMLScalarVolumeNode volumeNode
+    vtkMRMLVolumeDisplayNode displayNode
+    vtkMRMLVolumeArchetypeStorageNode storageNode
 
-volumeNode SetScene scene
-displayNode SetScene scene
-storageNode SetScene scene
+    volumeNode SetScene scene
+    displayNode SetScene scene
+    storageNode SetScene scene
 
-scene AddNode volumeNode
-scene AddNode displayNode
-scene AddNode storageNode
+    scene AddNode volumeNode
+    scene AddNode displayNode
+    scene AddNode storageNode
 
-volumeNode SetName dicom
-volumeNode SetStorageNodeID [storageNode GetID]
-volumeNode SetDisplayNodeID [displayNode GetID]
-
-
-set dicomArchetype [tk_getOpenFile]
-storageNode SetFileArchetype $dicomArchetype
-storageNode ReadData volumeNode
+    volumeNode SetName dicom
+    volumeNode SetStorageNodeID [storageNode GetID]
+    volumeNode SetDisplayNodeID [displayNode GetID]
 
 
-[appLogic GetSelectionNode] SetActiveVolumeID [volumeNode GetID]
-appLogic PropagateVolumeSelection
+    #set dicomArchetype [tk_getOpenFile]
+    set dicomArchetype c:/tmp/S2.001
+    storageNode SetFileArchetype $dicomArchetype
+    storageNode ReadData volumeNode
 
-[[sliceGUI GetSliceController] GetSliceNode] SetOrientationToCoronal
-pack forget [[sliceGUI GetSliceController] GetWidgetName]
+    [appLogic GetSelectionNode] SetActiveVolumeID [volumeNode GetID]
+    appLogic PropagateVolumeSelection
 
-set quit 0
-vwait quit
+    if { 1 } {
+
+      catch "sliceLogic Delete"
+      vtkSlicerSliceLogic sliceLogic
+      sliceLogic SetMRMLScene scene
+      sliceLogic ProcessMRMLEvents
+      sliceLogic ProcessLogicEvents
+      sliceLogic SetAndObserveMRMLScene scene
+
+        if { 0 } {
+          vtkSlicerSliceGUI sliceGUI
+          sliceGUI SetApplication slicerApp
+          sliceGUI SetApplicationLogic appLogic
+          sliceGUI SetAndObserveMRMLScene scene
+          sliceGUI SetAndObserveModuleLogic sliceLogic
+
+          sliceGUI BuildGUI f
+          sliceGUI AddGUIObservers
+
+          [[sliceGUI GetSliceController] GetSliceNode] SetOrientationToCoronal
+          pack forget [[sliceGUI GetSliceController] GetWidgetName]
+
+        
+          sliceGUI Delete
+        }
+
+      sliceLogic Delete
+    }
+
+    volumeNode Delete
+    displayNode Delete
+    storageNode Delete
+
+  }
+
+
+  t Delete
+  f Delete
+
+}
+
+
+  update
+  tk_messageBox -message "look!"
+
+appLogic Delete
+scene Delete
+
+slicerApp Exit
+slicerApp Delete
+
+#tk_messageBox -message "finished"
+exit
