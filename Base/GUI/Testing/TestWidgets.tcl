8773c7d1a8f4741166a1343d7a4cb81d5b67eadf
Steve Pieper <pieper@bwh.harvard.edu>
2006-07-15 20:34:52 +0000

    ENH: experiments with various wwidgets
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=890
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@890 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Testing/TestWidgets.tcl
+++ Base/GUI/Testing/TestWidgets.tcl
@@ -1,44 +1,145 @@
 
-catch "t Delete"
-catch "w Delete"
-catch "b Delete"
-catch "slicerApp Delete"
+## admin
 
+if { [file exists c:/Tcl/bin/tkcon.tcl] } {
+  source c:/Tcl/bin/tkcon.tcl
+  tkcon::Init
+  tkcon::Attach Main
+}
 
-vtkSlicerApplication slicerApp
+set ::vtkObjects ""
 
-vtkKWTopLevel t
-t SetApplication slicerApp
-t Create
+proc vtkNew {class} {
+  set o [$class New]
+  set ::vtkObjects "$o $::vtkObjects"
+  return $o
+}
 
-vtkKWPushButton b
-b SetParent t
-b SetText "Quit"
+proc vtkDelete {} {
+  foreach o $::vtkObjects {
+    $o Delete
+  }
+  set ::vtkObjects ""
+}
+
+## kwwidgets 
+
+set slicerApp [vtkNew vtkSlicerApplication]
+
+set topLevel [vtkNew vtkKWTopLevel]
+$topLevel SetApplication $slicerApp
+$topLevel Create
+
+set pushButton [vtkNew vtkKWPushButton]
+$pushButton SetParent $topLevel
+$pushButton SetText "Quit"
 set ::quit 0
 proc WidgetQuit {} {set ::quit 1}
-b SetCommand slicerApp "Evaluate WidgetQuit"
-b Create
-pack [b GetWidgetName]
+$pushButton SetCommand $slicerApp "Evaluate WidgetQuit"
+$pushButton Create
+pack [$pushButton GetWidgetName]
 
-vtkKWRenderWidget w
-w SetParent t
-w Create
-pack [w GetWidgetName] -fill both -expand true
-t Display
-
-
-set rwi [[w GetRenderWindow] GetInteractor]
+set renderWidget [vtkNew vtkKWRenderWidget]
+$renderWidget SetParent $topLevel
+$renderWidget Create
+pack [$renderWidget GetWidgetName] -fill both -expand true
+$topLevel Display
 
 
+set rwi [[$renderWidget GetRenderWindow] GetInteractor]
+
+#### wwidget code
+
+  # Logo
+  set logoRep [vtkNew vtkLogoRepresentation]
+  set ellip [vtkNew vtkImageEllipsoidSource]
+  $logoRep SetImage [$ellip GetOutput]
+
+  set logoWidget [vtkNew vtkLogoWidget]
+  $logoWidget SetInteractor $rwi
+  $logoWidget SetRepresentation $logoRep
+
+  $logoWidget On
+
+  set widgetType "Affine"
+
+  switch $widgetType {
+
+    "Affine" {
+      set affineRep [vtkNew vtkAffineRepresentation2D]
+
+      set affineWidget [vtkNew vtkAffineWidget]
+      $affineWidget SetInteractor $rwi
+      $affineWidget SetRepresentation $affineRep
+
+      $affineWidget On
+    }
+    "Contour" {
+      set contourWidget [vtkNew vtkContourWidget]
+      $contourWidget SetInteractor $rwi
+
+      $contourWidget On
+    }
+    "Distance" {
+      set pointRep [vtkNew vtkPointHandleRepresentation2D]
+      [$pointRep GetProperty] SetColor 1 0 0 
+      
+      set distRep [vtkNew vtkDistanceRepresentation2D]
+      $distRep SetHandleRepresentation $pointRep
+
+      set distWidget [vtkNew vtkDistanceWidget]
+      $distWidget SetRepresentation $distRep
+      $distWidget SetInteractor $rwi
+
+      $distWidget On
+    }
+    "Seed" {
+      # Seed
+      set pointRep [vtkNew vtkPointHandleRepresentation2D]
+      [$pointRep GetProperty] SetColor 1 0 0 
+      
+      set seedRep [vtkNew vtkSeedRepresentation]
+      $seedRep SetHandleRepresentation $pointRep
+
+      set seedWidget [vtkNew vtkSeedWidget]
+      $seedWidget SetRepresentation $seedRep
+      $seedWidget SetInteractor $rwi
+
+      $seedWidget On
+    }
+    "BiDimensional" {
+      set biRep [vtkNew vtkBiDimensionalRepresentation2D]
+      
+      set biWidget [vtkNew vtkBiDimensionalWidget]
+      $biWidget SetRepresentation $biRep
+      $biWidget SetInteractor $rwi
+
+      $biWidget On
+    }
+    "Caption" {
+      set capRep [vtkNew vtkCaptionRepresentation]
+
+      [$capRep GetCaptionActor2D] SetCaption "Where in Slicer\nwill we use this?"
+      [[[$capRep GetCaptionActor2D] GetTextActor] GetTextProperty] SetJustificationToCentered
+      [[[$capRep GetCaptionActor2D] GetTextActor] GetTextProperty] SetVerticalJustificationToCentered
+
+      set capWidget [vtkNew vtkCaptionWidget]
+      $capWidget SetRepresentation $capRep
+      $capWidget SetInteractor $rwi
+
+      $capWidget On
+    }
+  }
 
 vwait ::quit
 
-slicerApp Exit
+## cleanup 
 
-w Delete
-t Delete
-b Delete
 
-slicerApp Delete
+$slicerApp Exit
+
+vtkDelete
 
 exit
+
+
