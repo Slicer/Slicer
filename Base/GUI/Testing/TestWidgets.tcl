28ea9bc67db3e35e03a6ac4ecf4f920a7f9ec810
Steve Pieper <pieper@bwh.harvard.edu>
2006-07-25 22:45:47 +0000

    ENH: more widget tests
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=946
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@946 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Testing/TestWidgets.tcl
+++ Base/GUI/Testing/TestWidgets.tcl
@@ -1,29 +1,133 @@
 
-foreach arg $args {
-  tk_messageBox -message "arg is $arg"
+if { [info exists args] } {
+  foreach arg $args {
+    tk_messageBox -message "arg is $arg"
+  }
 }
 
 ## admin
 
-if { [file exists c:/Tcl/bin/tkcon.tcl] } {
-  source c:/Tcl/bin/tkcon.tcl
-  tkcon::Init
-  tkcon::Attach Main
+if { [info command vtkKWTkcon?*] == "" && ![winfo exists .vtkKWTopLevel0] } {
+  if { [file exists c:/Tcl/bin/tkcon.tcl] } {
+    source c:/Tcl/bin/tkcon.tcl
+    tkcon::Init
+    tkcon::Attach Main
+  }
 }
 
-set ::vtkObjects ""
-
-proc vtkNew {class} {
+proc vtkNew {class {group "default"}} {
+  if { ![info exists ::vtkObjects($group)] } {set ::vtkObjects($group) ""}
   set o [$class New]
-  set ::vtkObjects "$o $::vtkObjects"
+  set ::vtkObjects($group) "$o $::vtkObjects($group)"
   return $o
 }
 
-proc vtkDelete {} {
-  foreach o $::vtkObjects {
+proc vtkDelete {{group "default"}} {
+  if { ![info exists ::vtkObjects($group)] } {return}
+  foreach o $::vtkObjects($group) {
+    puts "$o ([$o GetClassName]) refcount [$o GetReferenceCount]"
     $o Delete
   }
-  set ::vtkObjects ""
+  set ::vtkObjects($group) ""
+}
+
+proc vtkRefPrint {{group "default"}} {
+  if { ![info exists ::vtkObjects($group)] } {return}
+  foreach o $::vtkObjects($group) {
+    puts "$o ([$o GetClassName]) refcount [$o GetReferenceCount]"
+  }
+}
+
+proc createWidget {rwi widgetType} {
+
+  switch $widgetType {
+
+    "User" {
+      set ::userRep [vtkNew vtkUserRepresentation widgets]
+      set ::userWidget [vtkNew vtkUserWidget widgets]
+      $::userWidget SetInteractor $rwi
+      $::userWidget SetRepresentation $::userRep
+      $::userWidget On
+    }
+
+    "Affine" {
+      set affineRep [vtkNew vtkAffineRepresentation2D widgets]
+      set affineWidget [vtkNew vtkAffineWidget widgets]
+      $affineWidget SetInteractor $rwi
+      $affineWidget SetRepresentation $affineRep
+      $affineWidget On
+
+      set affineRep2 [vtkNew vtkAffineRepresentation2D widgets]
+      set affineWidget2 [vtkNew vtkAffineWidget widgets]
+      $affineWidget2 SetInteractor $rwi
+      $affineWidget2 SetRepresentation $affineRep2
+      $affineWidget2 On
+    }
+    "Box" {
+      set ::boxWidget [vtkNew vtkBoxWidget widgets]
+      $::boxWidget SetInteractor $rwi
+
+      $::boxWidget SetRotationEnabled 0
+      $::boxWidget SetScalingEnabled 0
+
+      $::boxWidget On
+    }
+    "Contour" {
+      set contourWidget [vtkNew vtkContourWidget widgets]
+      $contourWidget SetInteractor $rwi
+
+      $contourWidget On
+    }
+    "Distance" {
+      set pointRep [vtkNew vtkPointHandleRepresentation2D widgets]
+      [$pointRep GetProperty] SetColor 1 0 0 
+      
+      set distRep [vtkNew vtkDistanceRepresentation2D widgets]
+      $distRep SetHandleRepresentation $pointRep
+
+      set distWidget [vtkNew vtkDistanceWidget widgets]
+      $distWidget SetRepresentation $distRep
+      $distWidget SetInteractor $rwi
+
+      $distWidget On
+    }
+    "Seed" {
+      # Seed
+      set pointRep [vtkNew vtkPointHandleRepresentation2D widgets]
+      #[$pointRep GetProperty] SetColor 1 0 0 
+      
+      set seedRep [vtkNew vtkSeedRepresentation widgets]
+      $seedRep SetHandleRepresentation $pointRep
+
+      set seedWidget [vtkNew vtkSeedWidget widgets]
+      $seedWidget SetRepresentation $seedRep
+      $seedWidget SetInteractor $rwi
+
+      $seedWidget On
+    }
+    "BiDimensional" {
+      set biRep [vtkNew vtkBiDimensionalRepresentation2D widgets]
+      
+      set biWidget [vtkNew vtkBiDimensionalWidget widgets]
+      $biWidget SetRepresentation $biRep
+      $biWidget SetInteractor $rwi
+
+      $biWidget On
+    }
+    "Caption" {
+      set capRep [vtkNew vtkCaptionRepresentation widgets]
+
+      [$capRep GetCaptionActor2D] SetCaption "Where in Slicer\nwill we use this?"
+      [[[$capRep GetCaptionActor2D] GetTextActor] GetTextProperty] SetJustificationToCentered
+      [[[$capRep GetCaptionActor2D] GetTextActor] GetTextProperty] SetVerticalJustificationToCentered
+
+      set capWidget [vtkNew vtkCaptionWidget widgets]
+      $capWidget SetRepresentation $capRep
+      $capWidget SetInteractor $rwi
+
+      $capWidget On
+    }
+  }
 }
 
 ## kwwidgets 
@@ -48,100 +152,46 @@ $renderWidget SetParent $topLevel
 $renderWidget Create
 pack [$renderWidget GetWidgetName] -fill both -expand true
 $topLevel Display
+$renderWidget AddObserver DeleteEvent "puts BYErenderWidget"
 
 
 set rwi [[$renderWidget GetRenderWindow] GetInteractor]
 
 #### wwidget code
 
-  # Logo
-  set logoRep [vtkNew vtkLogoRepresentation]
-  set ellip [vtkNew vtkImageEllipsoidSource]
-  $logoRep SetImage [$ellip GetOutput]
+# Logo
+set logoRep [vtkNew vtkLogoRepresentation]
+set ellip [vtkNew vtkImageEllipsoidSource]
+$logoRep SetImage [$ellip GetOutput]
 
-  set logoWidget [vtkNew vtkLogoWidget]
-  $logoWidget SetInteractor $rwi
-  $logoWidget SetRepresentation $logoRep
+set logoWidget [vtkNew vtkLogoWidget]
+$logoWidget SetInteractor $rwi
+$logoWidget SetRepresentation $logoRep
 
-  $logoWidget On
+$logoWidget On
 
-  set widgetType "Affine"
 
-  switch $widgetType {
+set widgets { "User" "Affine" "Box" "Contour" "Distance" "Seed" "BiDimensional" "Caption" }
 
-    "Affine" {
-      set affineRep [vtkNew vtkAffineRepresentation2D]
+foreach w $widgets {
+  set pushButton [vtkNew vtkKWPushButton]
+  $pushButton SetParent $topLevel
+  $pushButton SetText "$w"
+  $pushButton SetCommand $slicerApp "Evaluate \"createWidget $rwi $w\""
+  $pushButton Create
+  pack [$pushButton GetWidgetName]
+}
 
-      set affineWidget [vtkNew vtkAffineWidget]
-      $affineWidget SetInteractor $rwi
-      $affineWidget SetRepresentation $affineRep
-
-      $affineWidget On
-    }
-    "Contour" {
-      set contourWidget [vtkNew vtkContourWidget]
-      $contourWidget SetInteractor $rwi
-
-      $contourWidget On
-    }
-    "Distance" {
-      set pointRep [vtkNew vtkPointHandleRepresentation2D]
-      [$pointRep GetProperty] SetColor 1 0 0 
-      
-      set distRep [vtkNew vtkDistanceRepresentation2D]
-      $distRep SetHandleRepresentation $pointRep
-
-      set distWidget [vtkNew vtkDistanceWidget]
-      $distWidget SetRepresentation $distRep
-      $distWidget SetInteractor $rwi
-
-      $distWidget On
-    }
-    "Seed" {
-      # Seed
-      set pointRep [vtkNew vtkPointHandleRepresentation2D]
-      [$pointRep GetProperty] SetColor 1 0 0 
-      
-      set seedRep [vtkNew vtkSeedRepresentation]
-      $seedRep SetHandleRepresentation $pointRep
-
-      set seedWidget [vtkNew vtkSeedWidget]
-      $seedWidget SetRepresentation $seedRep
-      $seedWidget SetInteractor $rwi
-
-      $seedWidget On
-    }
-    "BiDimensional" {
-      set biRep [vtkNew vtkBiDimensionalRepresentation2D]
-      
-      set biWidget [vtkNew vtkBiDimensionalWidget]
-      $biWidget SetRepresentation $biRep
-      $biWidget SetInteractor $rwi
-
-      $biWidget On
-    }
-    "Caption" {
-      set capRep [vtkNew vtkCaptionRepresentation]
-
-      [$capRep GetCaptionActor2D] SetCaption "Where in Slicer\nwill we use this?"
-      [[[$capRep GetCaptionActor2D] GetTextActor] GetTextProperty] SetJustificationToCentered
-      [[[$capRep GetCaptionActor2D] GetTextActor] GetTextProperty] SetVerticalJustificationToCentered
-
-      set capWidget [vtkNew vtkCaptionWidget]
-      $capWidget SetRepresentation $capRep
-      $capWidget SetInteractor $rwi
-
-      $capWidget On
-    }
-  }
 
 vwait ::quit
 
-## cleanup 
+vtkRefPrint 
 
+## cleanup 
 
 $slicerApp Exit
 
+vtkDelete widgets
 vtkDelete
 
 exit
