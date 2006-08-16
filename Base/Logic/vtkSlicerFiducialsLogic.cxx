596751cf91cbeaaa990ce8b61003efd2c0da16ee
Nicole Aucoin <nicole@bwh.harvard.edu>
2006-08-16 22:43:50 +0000

    ENH: added a logic class for the fiducials module
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1075
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1075 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/Logic/vtkSlicerFiducialsLogic.cxx
@@ -0,0 +1,164 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerFiducialsLogic.cxx,v $
+  Date:      $Date: 2006/01/06 17:56:48 $
+  Version:   $Revision: 1.58 $
+
+=========================================================================auto=*/
+
+#include "vtkObjectFactory.h"
+#include "vtkCallbackCommand.h"
+#include <vtksys/SystemTools.hxx> 
+
+#include "vtkSlicerFiducialsLogic.h"
+
+#include "vtkMRMLFiducialNode.h"
+#include "vtkMRMLFiducialListNode.h"
+#include "vtkMRMLFiducialListDisplayNode.h"
+
+vtkCxxRevisionMacro(vtkSlicerFiducialsLogic, "$Revision: 1.9.12.1 $");
+vtkStandardNewMacro(vtkSlicerFiducialsLogic);
+
+//----------------------------------------------------------------------------
+vtkSlicerFiducialsLogic::vtkSlicerFiducialsLogic()
+{
+  this->ActiveFiducialListNode = NULL;
+}
+
+//----------------------------------------------------------------------------
+vtkSlicerFiducialsLogic::~vtkSlicerFiducialsLogic()
+{
+  if (this->ActiveFiducialListNode != NULL)
+    {
+        this->ActiveFiducialListNode->Delete();
+        this->ActiveFiducialListNode = NULL;
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerFiducialsLogic::ProcessMRMLEvents()
+{
+  // TODO: implement if needed
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerFiducialsLogic::SetActiveFiducialListNode(vtkMRMLFiducialListNode *activeNode)
+{
+  this->SetMRML( vtkObjectPointer(&this->ActiveFiducialListNode), activeNode );
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLFiducialListNode* vtkSlicerFiducialsLogic::AddFiducials ()
+{
+    std::cout << "vtkSlicerFiducialsLogic::AddFiducials : making a new fiducial list node\n";
+  vtkMRMLFiducialListNode *modelNode = vtkMRMLFiducialListNode::New();
+
+  std::cout << "\tmaking a new display node\n";
+// this creates an empty display node for now
+  vtkMRMLFiducialListDisplayNode *displayNode = vtkMRMLFiducialListDisplayNode::New();
+
+    /*
+  storageNode->SetFileName(filename);
+  if (storageNode->ReadData(modelNode) != 0)
+    {
+    const vtksys_stl::string fname(filename);
+    vtksys_stl::string name = vtksys::SystemTools::GetFilenameName(fname);
+    modelNode->SetName(name.c_str());
+
+    this->GetMRMLScene()->SaveStateForUndo();
+
+    modelNode->SetScene(this->GetMRMLScene());
+    storageNode->SetScene(this->GetMRMLScene());
+    displayNode->SetScene(this->GetMRMLScene());
+
+    this->GetMRMLScene()->AddNode(storageNode);  
+    this->GetMRMLScene()->AddNode(displayNode);
+    modelNode->SetStorageNodeID(storageNode->GetID());
+    modelNode->SetAndObserveDisplayNodeID(displayNode->GetID());  
+
+    this->GetMRMLScene()->AddNode(modelNode);  
+
+    this->Modified();  
+    }
+  */
+
+   this->GetMRMLScene()->SaveStateForUndo();
+
+    modelNode->SetScene(this->GetMRMLScene());
+    this->GetMRMLScene()->AddNode(displayNode);
+    modelNode->SetAndObserveDisplayNodeID(displayNode->GetID());
+    displayNode->SetScene(this->GetMRMLScene());
+
+    this->GetMRMLScene()->AddNode(modelNode);
+
+    std::cout << "After adding the node to the scene, it's id is " << (modelNode->GetID() ? modelNode->GetID() : "NULL") << endl;
+
+    // set it to be active
+    this->SetActiveFiducialListNode(modelNode);
+        
+  modelNode->Delete();
+  displayNode->Delete();
+
+  return modelNode;  
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLFiducialNode *vtkSlicerFiducialsLogic::AddFiducial()
+{
+   
+    
+    // get the Fiducials list
+    //vtkMRMLFiducialListNode *listNode = this->GetActiveFiducialListNode();
+    // make a new fiducial node
+    vtkMRMLFiducialNode * modelNode = vtkMRMLFiducialNode::New();
+    
+    // add it to the list
+    if (this->GetActiveFiducialListNode() != NULL)
+    {
+        this->GetActiveFiducialListNode()->AddFiducialNode(modelNode);
+    }
+    else
+    {
+        std::cerr << "Error: the active ficucials list node is NULL\n";
+        return NULL;
+    }
+    this->GetMRMLScene()->SaveStateForUndo();
+
+    modelNode->SetScene(this->GetMRMLScene());
+    modelNode->Delete();
+    return modelNode;
+    
+
+/*
+    this->GetMRMLScene()->SaveStateForUndo();
+
+    modelNode->SetScene(this->GetMRMLScene());
+    this->GetMRMLScene()->AddNode(displayNode);
+    modelNode->SetAndObserveDisplayNodeID(displayNode->GetID());  
+
+    this->GetMRMLScene()->AddNode(modelNode);  
+  
+    modelNode->Delete();
+    displayNode->Delete();
+
+    return modelNode;
+*/
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerFiducialsLogic::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->vtkObject::PrintSelf(os, indent);
+
+  os << indent << "vtkSlicerFiducialsLogic:             " << this->GetClassName() << "\n";
+
+  os << indent << "ActiveFiducialListNode: " <<
+    (this->ActiveFiducialListNode ? this->ActiveFiducialListNode->GetName() : "(none)") << "\n";
+}
---
+++ Base/Logic/vtkSlicerFiducialsLogic.h
@@ -0,0 +1,70 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerFiducialsLogic.h,v $
+  Date:      $Date: 2006/01/08 04:48:05 $
+  Version:   $Revision: 1.45 $
+
+=========================================================================auto=*/
+
+// .NAME vtkSlicerFiducialsLogic - slicer logic class for volumes manipulation
+// .SECTION Description
+// This class manages the logic associated with reading, saving,
+// and changing propertied of the volumes
+
+
+#ifndef __vtkSlicerFiducialsLogic_h
+#define __vtkSlicerFiducialsLogic_h
+
+#include <stdlib.h>
+
+#include "vtkSlicerBaseLogic.h"
+#include "vtkSlicerLogic.h"
+
+#include "vtkMRML.h"
+#include "vtkMRMLFiducialNode.h"
+#include "vtkMRMLFiducialListNode.h"
+
+class VTK_SLICER_BASE_LOGIC_EXPORT vtkSlicerFiducialsLogic : public vtkSlicerLogic 
+{
+  public:
+  
+  // The Usual vtk class functions
+  static vtkSlicerFiducialsLogic *New();
+  vtkTypeRevisionMacro(vtkSlicerFiducialsLogic,vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // The currently active mrml volume node 
+  vtkGetObjectMacro (ActiveFiducialListNode, vtkMRMLFiducialListNode);
+  void SetActiveFiducialListNode (vtkMRMLFiducialListNode *ActiveFiducialListNode);
+
+  // Description:
+  // Create new mrml node and associated display node for a full list
+  vtkMRMLFiducialListNode* AddFiducials();
+  // Create a new mrml node and associated display node for a new point on
+  // a list
+  vtkMRMLFiducialNode *AddFiducial();
+
+  // Description:
+  // Update logic state when MRML scene chenges
+  void ProcessMRMLEvents(); 
+    
+protected:
+  vtkSlicerFiducialsLogic();
+  ~vtkSlicerFiducialsLogic();
+  vtkSlicerFiducialsLogic(const vtkSlicerFiducialsLogic&);
+  void operator=(const vtkSlicerFiducialsLogic&);
+
+  // Description:
+  //
+  vtkMRMLFiducialListNode *ActiveFiducialListNode;
+};
+
+#endif
+
