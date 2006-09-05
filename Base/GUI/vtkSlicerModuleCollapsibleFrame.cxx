410911acf5cf6981ae5f84eb3fb1e8ce4d03f8e2
Wendy Plesniak <wjp@bwh.harvard.edu>
2006-09-05 18:24:05 +0000

    ENH: added new class containing vtkKWFrameWithLabel to Theme
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1171
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1171 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkSlicerModuleCollapsibleFrame.cxx
@@ -0,0 +1,586 @@
+
+#include "vtkSlicerModuleCollapsibleFrame.h"
+
+#include "vtkKWApplication.h"
+#include "vtkKWDragAndDropTargetSet.h"
+#include "vtkKWFrame.h"
+#include "vtkKWInternationalization.h"
+#include "vtkKWIcon.h"
+#include "vtkKWLabel.h"
+#include "vtkKWLabelWithLabel.h"
+#include "vtkKWTkUtilities.h"
+
+#include "vtkObjectFactory.h"
+#include <vtksys/SystemTools.hxx>
+
+//----------------------------------------------------------------------------
+vtkStandardNewMacro( vtkSlicerModuleCollapsibleFrame );
+vtkCxxRevisionMacro(vtkSlicerModuleCollapsibleFrame, "$Revision: 1.10 $");
+
+int vtkSlicerModuleCollapsibleFrame::DefaultLabelCase = vtkSlicerModuleCollapsibleFrame::LabelCaseUppercaseFirst;
+int vtkSlicerModuleCollapsibleFrame::DefaultLabelFontWeight = vtkSlicerModuleCollapsibleFrame::LabelFontWeightNormal;
+int vtkSlicerModuleCollapsibleFrame::DefaultAllowFrameToCollapse = 1;
+
+
+// Description:
+// This class is a reimplementation of vtkKWFrameWithLabel, with a few
+// ajustments, to give the collapsing frames in Slicer's Module GUI panel
+// a distinct themed look, feel and behavior.
+
+//----------------------------------------------------------------------------
+vtkSlicerModuleCollapsibleFrame::vtkSlicerModuleCollapsibleFrame()
+{
+  this->ExternalMarginFrame  = vtkKWFrame::New();
+  this->CollapsibleFrame     = vtkKWFrame::New();
+  this->InternalMarginFrame  = vtkKWFrame::New();
+  this->Frame                = vtkKWFrame::New();
+  this->LabelFrame           = vtkKWFrame::New();
+  this->Label                = vtkKWLabelWithLabel::New();
+  this->Icon                 = vtkKWLabel::New();
+  this->IconData             = vtkKWIcon::New();
+
+  this->AllowFrameToCollapse = 1;
+  this->LimitedEditionModeIconVisibility = 0;
+}
+
+//----------------------------------------------------------------------------
+vtkSlicerModuleCollapsibleFrame::~vtkSlicerModuleCollapsibleFrame()
+{
+  if (this->Icon)
+    {
+    this->Icon->Delete();
+    this->Icon = NULL;
+    }
+  if (this->IconData)
+    {
+    this->IconData->Delete();
+    this->IconData = NULL;
+    }
+  if (this->Label)
+    {
+    this->Label->Delete();
+    this->Label = NULL;
+    }
+  if (this->Frame)
+    {
+    this->Frame->Delete();
+    this->Frame = NULL;
+    }
+  if (this->LabelFrame)
+    {
+    this->LabelFrame->Delete();
+    this->LabelFrame = NULL;
+    }
+  if (this->ExternalMarginFrame)
+    {
+    this->ExternalMarginFrame->Delete();
+    this->ExternalMarginFrame = NULL;
+    }
+  if (this->InternalMarginFrame)
+    {
+    this->InternalMarginFrame->Delete();
+    this->InternalMarginFrame = NULL;
+    }
+  if (this->CollapsibleFrame)
+    {
+    this->CollapsibleFrame->Delete();
+    this->CollapsibleFrame = NULL;
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::CreateWidget()
+{
+  // Check if already created
+
+  if (this->IsCreated())
+    {
+    vtkErrorMacro(<< this->GetClassName() << " already created");
+    return;
+    }
+
+  // Call the superclass to create the whole widget
+
+  this->Superclass::CreateWidget();
+
+  this->ExternalMarginFrame->SetParent(this);
+  this->ExternalMarginFrame->Create();
+
+  this->CollapsibleFrame->SetParent(this);
+  this->CollapsibleFrame->Create();
+  this->CollapsibleFrame->SetReliefToGroove();
+  this->CollapsibleFrame->SetBorderWidth(2);
+
+  this->InternalMarginFrame->SetParent(this->CollapsibleFrame);
+  this->InternalMarginFrame->Create();
+
+  this->Frame->SetParent(this->CollapsibleFrame);
+  this->Frame->Create();
+
+  this->LabelFrame->SetParent(this);
+  this->LabelFrame->Create();
+
+  this->Label->SetParent(this->LabelFrame);
+  this->Label->Create();
+  this->Label->SetBorderWidth(0);
+  this->Label->ExpandWidgetOff();
+
+  vtkKWLabel *label = this->GetLabel();
+  label->SetBorderWidth(1);
+  label->SetPadX(0);
+  label->SetPadY(0);
+
+  // At this point, although this->Label (a labeled label) has been created,
+  // UpdateEnableState() has been called already and ShowLabelOff() has been
+  // called on the label. Therefore, the label of this->Label was not created
+  // since it is lazy created/allocated on the fly only when needed.
+  // Force label icon to be created now, so that we can set its image option.
+
+  this->Label->LabelVisibilityOn();
+  this->GetLabel()->SetBinding("<Button-1>", this, "LabelSingleClickCallback");
+
+  label = this->GetLabelIcon();
+  label->SetImageToPredefinedIcon(vtkKWIcon::IconLock);
+  label->SetBorderWidth(0);
+  label->SetPadX(0);
+  label->SetPadY(0);
+
+  const char *lem_name = this->GetApplication()->GetLimitedEditionModeName() 
+    ? this->GetApplication()->GetLimitedEditionModeName() : "Limited Edition";
+  
+  char buffer[500];
+  sprintf(buffer, k_("This feature is not available in '%s'mode."), lem_name);
+  this->GetLabelIcon()->SetBalloonHelpString(buffer);
+
+  if (vtkSlicerModuleCollapsibleFrame::DefaultLabelFontWeight ==
+      vtkSlicerModuleCollapsibleFrame::LabelFontWeightBold)
+    {
+    vtkKWTkUtilities::ChangeFontWeightToBold(this->GetLabel());
+    }
+
+  this->IconData->SetImage(vtkKWIcon::IconShrink);
+
+  this->Icon->SetParent(this);
+  this->Icon->Create();
+  this->Icon->SetImageToIcon(this->IconData);
+  this->Icon->SetBalloonHelpString(
+    ks_("Frame With Label|Shrink or expand the frame"));
+  
+  this->Script(
+    "pack %s -fill x -expand y -side top", this->ExternalMarginFrame->GetWidgetName());
+  this->Script(
+    "pack %s -fill both -expand y -side top", this->CollapsibleFrame->GetWidgetName());
+  this->Script(
+    "pack %s -fill x -expand y -side top", this->InternalMarginFrame->GetWidgetName());
+
+  this->Script(
+    "pack %s -padx 2 -pady 2 -fill both -expand yes -side top",
+               this->Frame->GetWidgetName());
+
+  this->Script(
+    "pack %s -anchor nw -side left -fill both -expand y -padx 2 -pady 0",
+    this->Label->GetWidgetName());
+
+  this->Script("place %s -relx 0 -relwidth 1.0 -x 5 -y 0 -anchor nw",
+               this->LabelFrame->GetWidgetName());
+  this->Label->Raise();
+
+  this->Icon->SetBinding("<ButtonRelease-1>",this,"CollapseButtonCallback");
+
+  // If the label frame get resize, reset the margins
+
+  vtksys_stl::string callback("catch {");
+  callback += this->GetTclName();
+  callback += " AdjustMarginCallback}";
+  this->LabelFrame->SetBinding("<Configure>", NULL, callback.c_str());
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::SetWidth(int width)
+{
+  this->Superclass::SetWidth(width);
+  if (this->CollapsibleFrame)
+    {
+    this->CollapsibleFrame->SetWidth(width);
+    width -= this->CollapsibleFrame->GetBorderWidth() * 2;
+    if (this->InternalMarginFrame)
+      {
+      this->InternalMarginFrame->SetWidth(width);
+      }
+    if (this->Frame)
+      {
+      int padx = 2; // see Create()
+      this->Frame->SetWidth(width - padx * 2);
+      }
+    }
+}
+
+//----------------------------------------------------------------------------
+int vtkSlicerModuleCollapsibleFrame::GetWidth()
+{
+  int width = this->Superclass::GetWidth();
+  if (this->CollapsibleFrame)
+    {
+    int internal_width = this->CollapsibleFrame->GetWidth();
+    if (internal_width > width)
+      {
+      width = internal_width;
+      }
+    if (this->Frame)
+      {
+      int padx = 2; // see Create()
+      internal_width = this->Frame->GetWidth() + 
+        padx * 2 + this->CollapsibleFrame->GetBorderWidth() * 2;
+      if (internal_width > width)
+        {
+        width = internal_width;
+        }
+      }
+    }
+  return width;
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::SetHeight(int height)
+{
+  this->Superclass::SetHeight(height);
+  if (this->CollapsibleFrame)
+    {
+    if (this->ExternalMarginFrame)
+      {
+      height -= this->ExternalMarginFrame->GetHeight();
+      }
+    this->CollapsibleFrame->SetHeight(height);
+    height -= this->CollapsibleFrame->GetBorderWidth() * 2;
+    if (this->InternalMarginFrame)
+      {
+      height -= this->InternalMarginFrame->GetHeight();
+      }
+    if (this->Frame)
+      {
+      int pady = 2; // see Create()
+      this->Frame->SetHeight(height - pady * 2);
+      }
+    }
+}
+
+//----------------------------------------------------------------------------
+int vtkSlicerModuleCollapsibleFrame::GetHeight()
+{
+  int height = this->Superclass::GetHeight();
+  if (this->CollapsibleFrame)
+    {
+    int internal_height = this->CollapsibleFrame->GetHeight();
+    if (this->ExternalMarginFrame)
+      {
+      internal_height += this->ExternalMarginFrame->GetHeight();
+      }
+    if (internal_height > height)
+      {
+      height = internal_height;
+      }
+    if (this->Frame)
+      {
+      int pady = 2; // see Create()
+      internal_height = this->Frame->GetHeight() + 
+        pady * 2 + this->CollapsibleFrame->GetBorderWidth() * 2;
+      if (this->ExternalMarginFrame)
+        {
+        internal_height += this->ExternalMarginFrame->GetHeight();
+        }
+      if (this->InternalMarginFrame)
+        {
+        internal_height += this->InternalMarginFrame->GetHeight();
+        }
+      if (internal_height > height)
+        {
+        height = internal_height;
+        }
+      }
+    }
+  return height;
+}
+
+//----------------------------------------------------------------------------
+vtkKWLabel* vtkSlicerModuleCollapsibleFrame::GetLabel()
+{
+  if (this->Label)
+    {
+    return this->Label->GetWidget();
+    }
+  return NULL;
+}
+
+//----------------------------------------------------------------------------
+vtkKWLabel* vtkSlicerModuleCollapsibleFrame::GetLabelIcon()
+{
+  if (this->Label)
+    {
+    return this->Label->GetLabel();
+    }
+  return NULL;
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::SetLabelText(const char *text)
+{
+  if (!text)
+    {
+    return;
+    }
+
+  if (vtkSlicerModuleCollapsibleFrame::DefaultLabelCase == 
+      vtkSlicerModuleCollapsibleFrame::LabelCaseUserSpecified)
+    {
+    this->GetLabel()->SetText(text);
+    }
+  else
+    {
+    vtksys_stl::string res;
+    switch (vtkSlicerModuleCollapsibleFrame::DefaultLabelCase)
+      {
+      case vtkSlicerModuleCollapsibleFrame::LabelCaseUppercaseFirst:
+        res = vtksys::SystemTools::CapitalizedWords(text);
+        break;
+      case vtkSlicerModuleCollapsibleFrame::LabelCaseLowercaseFirst:
+        res = vtksys::SystemTools::UnCapitalizedWords(text);
+        break;
+      }
+    this->GetLabel()->SetText(res.c_str());
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::AdjustMarginCallback()
+{
+  if (this->IsCreated())
+    {
+    // Get the height of the label frame, and share it between
+    // the two borders (frame).
+
+    int height = 0, width = 0;
+    vtkKWTkUtilities::GetWidgetRequestedSize(
+      this->LabelFrame, &width, &height);
+
+    // If the frame has not been packed yet, reqheight will return 1,
+    // so try the hard way by checking what's inside the pack, provided
+    // that it's simple (i.e. packed in a single row or column)
+
+    if (height <= 1) 
+      {
+      vtkKWTkUtilities::GetSlavesBoundingBoxInPack(
+        this->LabelFrame, &width, &height);
+      }
+
+    // Don't forget the show/hide collapse icon, it might be bigger than
+    // the LabelFrame contents (really ?)
+
+    if (vtkSlicerModuleCollapsibleFrame::DefaultAllowFrameToCollapse && 
+        this->AllowFrameToCollapse &&
+        height < this->IconData->GetHeight())
+      {
+      height = this->IconData->GetHeight();
+      }
+
+    int border_h = height / 2;
+    int border2_h = height / 2;
+#ifdef _WIN32
+    border_h++;
+#else
+    border2_h++;
+#endif
+
+    this->ExternalMarginFrame->SetHeight(border_h);
+    this->InternalMarginFrame->SetHeight(border2_h);
+
+    if (vtkSlicerModuleCollapsibleFrame::DefaultAllowFrameToCollapse && 
+        this->AllowFrameToCollapse)
+      {
+      this->Script("place %s -relx 1 -x %d -rely 0 -y %d -anchor center",
+                   this->Icon->GetWidgetName(),
+                   -this->IconData->GetWidth() -1,
+                   border_h + 1);    
+      this->Icon->Raise();
+      }
+
+    // Now check if we need to expand the widget to show the label
+    // This is required if the frame is empty for example, since
+    // the Label is 'placed', it does not claim its own space
+
+    int min_width = width + this->IconData->GetWidth() + 40;
+    if (this->GetWidth() < min_width)
+      {
+      vtkKWTkUtilities::GetWidgetRequestedSize(this, &width, NULL);
+      if (width < min_width)
+        {
+        this->SetWidth(min_width);
+        }
+      }
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::ExpandFrame()
+{
+  if (this->Frame && this->Frame->IsCreated())
+    {
+    this->Script("pack %s -fill both -expand yes -padx 2 -pady 2",
+                 this->Frame->GetWidgetName());
+    }
+  if (this->IconData && this->Icon)
+    {
+    this->IconData->SetImage(vtkKWIcon::IconShrink);
+    this->Icon->SetImageToIcon(this->IconData);
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::CollapseFrame()
+{
+  if (this->Frame && this->Frame->IsCreated())
+    {
+    this->Script("pack forget %s", this->Frame->GetWidgetName());
+    this->SetWidth(this->GetWidth());
+    this->SetHeight(this->GetHeight());
+    }
+  if (this->IconData && this->Icon)
+    {
+    this->IconData->SetImage(vtkKWIcon::IconExpand);
+    this->Icon->SetImageToIcon(this->IconData);
+    }
+}
+
+//----------------------------------------------------------------------------
+int vtkSlicerModuleCollapsibleFrame::IsFrameCollapsed()
+{
+  return (this->Frame && this->Frame->IsCreated() && !this->Frame->IsPacked());
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::LabelSingleClickCallback()
+{
+  this->CollapseButtonCallback();
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::CollapseButtonCallback()
+{
+  if (vtkSlicerModuleCollapsibleFrame::DefaultAllowFrameToCollapse && 
+      this->AllowFrameToCollapse)
+    {
+    if (this->IsFrameCollapsed())
+      {
+      this->ExpandFrame();
+      }
+    else
+      {
+      this->CollapseFrame();
+      }
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::SetDefaultAllowFrameToCollapse(int arg) 
+{ 
+  vtkSlicerModuleCollapsibleFrame::DefaultAllowFrameToCollapse = arg; 
+}
+
+//----------------------------------------------------------------------------
+int vtkSlicerModuleCollapsibleFrame::GetDefaultAllowFrameToCollapse() 
+{ 
+  return vtkSlicerModuleCollapsibleFrame::DefaultAllowFrameToCollapse; 
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::SetDefaultLabelFontWeight(int arg) 
+{ 
+  vtkSlicerModuleCollapsibleFrame::DefaultLabelFontWeight = arg; 
+}
+
+//----------------------------------------------------------------------------
+int vtkSlicerModuleCollapsibleFrame::GetDefaultLabelFontWeight() 
+{ 
+  return vtkSlicerModuleCollapsibleFrame::DefaultLabelFontWeight; 
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::SetDefaultLabelCase(int v) 
+{ 
+  vtkSlicerModuleCollapsibleFrame::DefaultLabelCase = v;
+}
+
+//----------------------------------------------------------------------------
+int vtkSlicerModuleCollapsibleFrame::GetDefaultLabelCase() 
+{ 
+  return vtkSlicerModuleCollapsibleFrame::DefaultLabelCase;
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::SetLimitedEditionModeIconVisibility(int arg)
+{
+  if (this->LimitedEditionModeIconVisibility == arg)
+    {
+    return;
+    }
+
+  this->LimitedEditionModeIconVisibility = arg;
+  this->Modified();
+
+  this->UpdateEnableState();
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::UpdateEnableState()
+{
+  this->Superclass::UpdateEnableState();
+
+  // Disable only the label part of the labeled label
+  // (we want the icon not to look disabled)
+
+  this->PropagateEnableState(this->GetLabel());
+
+  int limited = (this->GetApplication() && 
+                 this->GetApplication()->GetLimitedEditionMode());
+  
+  if (limited && this->LimitedEditionModeIconVisibility && !this->GetEnabled())
+    {
+    this->Label->LabelVisibilityOn();
+    }
+  else
+    {
+    this->Label->LabelVisibilityOff();
+    }
+  this->PropagateEnableState(this->Frame);
+  this->PropagateEnableState(this->LabelFrame);
+  this->PropagateEnableState(this->ExternalMarginFrame);
+  this->PropagateEnableState(this->InternalMarginFrame);
+  this->PropagateEnableState(this->CollapsibleFrame);
+  this->PropagateEnableState(this->Icon);
+}
+
+//----------------------------------------------------------------------------
+vtkKWDragAndDropTargetSet* vtkSlicerModuleCollapsibleFrame::GetDragAndDropTargetSet()
+{
+  int exist = this->HasDragAndDropTargetSet();
+  vtkKWDragAndDropTargetSet *targets = this->Superclass::GetDragAndDropTargetSet();
+  if (!exist)
+    {
+    targets->SetSourceAnchor(this->GetLabel());
+    }
+  return targets;
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerModuleCollapsibleFrame::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+  os << indent << "AllowFrameToCollapse: " 
+     << (this->AllowFrameToCollapse ? "On" : "Off") << endl;
+  os << indent << "Frame: " << this->Frame << endl;
+  os << indent << "LabelFrame: " << this->LabelFrame << endl;
+  os << indent << "Groove: " << this->CollapsibleFrame << endl;
+  os << indent << "Label: " << this->Label << endl;
+  os << indent << "LimitedEditionModeIconVisibility: " 
+     << (this->LimitedEditionModeIconVisibility ? "On" : "Off") << endl;
+}
+
---
+++ Base/GUI/vtkSlicerModuleCollapsibleFrame.h
@@ -0,0 +1,194 @@
+// .NAME vtkSlicerModuleCollapsibleFrame
+// .SECTION Description
+// Collapsible frame for module GUI logical sections.
+
+#ifndef __vtkSlicerModuleCollapsibleFrame_h
+#define __vtkSlicerModuleCollapsibleFrame_h
+
+// Description:
+// This class is a reimplementation of vtkKWFrameWithLabel, with a few
+// ajustments, to give the collapsing frames in Slicer's Module GUI panel
+// a distinct themed look, feel and behavior.
+
+#include "vtkSlicerBaseGUIWin32Header.h"
+#include "vtkKWCompositeWidget.h"
+
+class vtkKWFrame;
+class vtkKWIcon;
+class vtkKWLabel;
+class vtkKWLabelWithLabel;
+
+class VTK_SLICER_BASE_GUI_EXPORT vtkSlicerModuleCollapsibleFrame : public vtkKWCompositeWidget
+{
+ public:
+  static vtkSlicerModuleCollapsibleFrame* New();
+  vtkTypeRevisionMacro(vtkSlicerModuleCollapsibleFrame, vtkKWCompositeWidget);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Set the label for the frame.
+  void SetLabelText(const char *);
+  
+  // Description:
+  // Get the internal frame.
+  vtkGetObjectMacro(Frame, vtkKWFrame);
+
+  // Description:
+  // Get the internal frame containing the label.
+  vtkGetObjectMacro(LabelFrame, vtkKWFrame);
+
+  // Description:
+  // Get the label (frame title).
+  virtual vtkKWLabel *GetLabel();
+
+  // Description:
+  // Collapse/expand the frame.
+  virtual void CollapseFrame();
+  virtual void ExpandFrame();
+  virtual int IsFrameCollapsed();
+
+  // Description:
+  // Set/Get if the frame can be collapsed, i.e. display a button that will
+  // let the user collapse the frame. On by default.
+  vtkSetMacro(AllowFrameToCollapse, int);
+  vtkBooleanMacro(AllowFrameToCollapse, int);
+  vtkGetMacro(AllowFrameToCollapse, int);
+
+  // Description:
+  // Globally Set/Get if the frame can be collapsed.
+  // NOTE: this is a *static* method that defines the behaviour for *all*
+  // instances of this class to be created afterwards. This is typically
+  // called in your application class or your main executable, before 
+  // creating any UI; this will ensure all instances have a consistent look.
+  // By default it is globally enabled.
+  static int GetDefaultAllowFrameToCollapse();
+  static void SetDefaultAllowFrameToCollapse(int);
+  static void DefaultAllowFrameToCollapseOn()
+    { vtkSlicerModuleCollapsibleFrame::SetDefaultAllowFrameToCollapse(1); };
+  static void DefaultAllowFrameToCollapseOff()
+    { vtkSlicerModuleCollapsibleFrame::SetDefaultAllowFrameToCollapse(0); };
+
+  // Description:
+  // Globally override the case of the label to ensure GUI consistency.
+  // This will change the label when SetLabelText() is called.
+  // NOTE: this is a *static* method that defines the behaviour for *all*
+  // instances of this class to be created afterwards. This is typically
+  // called in your application class or your main executable, before 
+  // creating any UI; this will ensure all instances have a consistent look.
+  // Defaults to LabelCaseUppercaseFirst.
+  //BTX
+  enum
+  {
+    LabelCaseUserSpecified = 0,
+    LabelCaseUppercaseFirst,
+    LabelCaseLowercaseFirst
+  };
+  //ETX
+  static void SetDefaultLabelCase(int v);
+  static int GetDefaultLabelCase();
+  static void SetDefaultLabelCaseToUserSpecified() 
+    { vtkSlicerModuleCollapsibleFrame::SetDefaultLabelCase(
+      vtkSlicerModuleCollapsibleFrame::LabelCaseUserSpecified);};
+  static void SetDefaultLabelCaseToUppercaseFirst() 
+    {vtkSlicerModuleCollapsibleFrame::SetDefaultLabelCase(
+      vtkSlicerModuleCollapsibleFrame::LabelCaseUppercaseFirst);};
+  static void SetDefaultLabelCaseToLowercaseFirst() 
+    {vtkSlicerModuleCollapsibleFrame::SetDefaultLabelCase(
+      vtkSlicerModuleCollapsibleFrame::LabelCaseLowercaseFirst);};
+
+  // Description:
+  // Globally enable or disable the font weight of the label.
+  // NOTE: this is a *static* method that defines the behaviour for *all*
+  // instances of this class to be created afterwards. This is typically
+  // called in your application class or your main executable, before 
+  // creating any UI; this will ensure all instances have a consistent look.
+  // By default it is set to bold.
+  //BTX
+  enum
+  {
+    LabelFontWeightNormal = 0,
+    LabelFontWeightBold
+  };
+  //ETX
+  static void SetDefaultLabelFontWeight(int v);
+  static int GetDefaultLabelFontWeight();
+  static void SetDefaultLabelFontWeightToNormal() 
+    { vtkSlicerModuleCollapsibleFrame::SetDefaultLabelFontWeight(
+      vtkSlicerModuleCollapsibleFrame::LabelFontWeightNormal);};
+  static void SetDefaultLabelFontWeightToBold() 
+    {vtkSlicerModuleCollapsibleFrame::SetDefaultLabelFontWeight(
+      vtkSlicerModuleCollapsibleFrame::LabelFontWeightBold);};
+
+  // Description:
+  // Set/Get the visibility of a special icon (lock) when the application
+  // is in Limited Edition Mode and the label frame is disabled.
+  virtual void SetLimitedEditionModeIconVisibility(int);
+  vtkBooleanMacro(LimitedEditionModeIconVisibility, int);
+  vtkGetMacro(LimitedEditionModeIconVisibility, int);
+
+  // Description:
+  // Update the "enable" state of the object and its internal parts.
+  // Depending on different Ivars (this->Enabled, the application's 
+  // Limited Edition Mode, etc.), the "enable" state of the object is updated
+  // and propagated to its internal parts/subwidgets. This will, for example,
+  // enable/disable parts of the widget UI, enable/disable the visibility
+  // of 3D widgets, etc.
+  virtual void UpdateEnableState();
+
+  // Description:
+  // Get the drag and drop framework.
+  // Override the superclass to allow the frame to be dragged using
+  // the label.
+  virtual vtkKWDragAndDropTargetSet* GetDragAndDropTargetSet();
+
+  // Description:
+  // Override parent's SetWidth/SetHeight to propagate to internal frames.
+  // A frame's size can be set explicitly, until something else is packed
+  // inside.
+  virtual void SetWidth(int);
+  virtual int GetWidth();
+  virtual void SetHeight(int);
+  virtual int GetHeight();
+
+  // Description:
+  // Get the internal frame responsible for drawing the collapsible border.
+  vtkGetObjectMacro(CollapsibleFrame, vtkKWFrame);
+
+  // Description:
+  // Callbacks. Internal, do not use.
+  virtual void AdjustMarginCallback();
+  virtual void CollapseButtonCallback();
+  virtual void LabelSingleClickCallback();
+
+ protected:
+  vtkSlicerModuleCollapsibleFrame();
+  ~vtkSlicerModuleCollapsibleFrame();
+
+  // Description:
+  // Create the widget.
+  virtual void CreateWidget();
+
+  vtkKWFrame          *Frame;
+  vtkKWFrame          *LabelFrame;
+  vtkKWLabelWithLabel *Label;
+  vtkKWFrame          *ExternalMarginFrame;
+  vtkKWFrame          *InternalMarginFrame;
+  vtkKWFrame          *CollapsibleFrame;
+  vtkKWLabel          *Icon;
+  vtkKWIcon           *IconData;
+
+  int AllowFrameToCollapse;
+  int LimitedEditionModeIconVisibility;
+
+  static int DefaultAllowFrameToCollapse;
+  static int DefaultLabelFontWeight;
+  static int DefaultLabelCase;
+
+  virtual vtkKWLabel *GetLabelIcon();
+
+ private:
+  vtkSlicerModuleCollapsibleFrame(const vtkSlicerModuleCollapsibleFrame&); // Not implemented
+  void operator=(const vtkSlicerModuleCollapsibleFrame&); // Not implemented
+};
+
+#endif
