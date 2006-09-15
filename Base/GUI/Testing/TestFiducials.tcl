c880eea9dfb2b5117ffaf1ed0448b6eddbb99fa4
Nicole Aucoin <nicole@bwh.harvard.edu>
2006-09-15 21:14:45 +0000

    BUG: fixed error string calls, multiply point offset by 10 to allow for better viewing
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1261
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1261 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Testing/TestFiducials.tcl
+++ Base/GUI/Testing/TestFiducials.tcl
@@ -6,21 +6,23 @@ proc TestFiducial {} {
   set numberOfFiducials [$fiducialList GetNumberOfFiducials]
 
   for {set f 0} {$f < $numberOfFiducials} {incr f} {
+    set tenf [expr $f * 10]
     $fiducialList SetNthFiducialLabelText $f RenameTest$f
-    $fiducialList SetNthFiducialXYZ $f $f $f $f
+    $fiducialList SetNthFiducialXYZ $f $tenf $tenf $tenf
     $fiducialList SetNthFiducialOrientation $f $f $f $f 1
   }
 
 
   for {set f 0} {$f < $numberOfFiducials} {incr f} {
+    set tenf [expr $f * 10]
     if { [$fiducialList GetNthFiducialLabelText $f] != "RenameTest$f" } {
-      error "fiducial name didn't change ($fiducial)"
+      error "fiducial name didn't change ($f)"
     }
-    if { [string trim [$fiducialList GetNthFiducialXYZString $f]] != "$f $f $f" } {
-      error "fiducial XYZ didn't change ($fiducial) (XYZ is [$fiducial GetXYZ] not $f $f $f)"
+    if { [string trim [$fiducialList GetNthFiducialXYZString $f]] != "$tenf $tenf $tenf" } {
+      error "fiducial XYZ didn't change ($f) (XYZ is [$fiducialList GetNthFiducialXYZString $f] not $tenf $tenf $tenf)"
     }
     if { [string trim [$fiducialList GetNthFiducialOrientationString $f]] != "$f $f $f 1" } {
-      error "fiducial OrientationWXYZ didn't change ($fiducial)"
+        error "fiducial OrientationWXYZ didn't change ($f) (WXYZ is [$fiducialList GetNthFiducialOrientationString $f] not $f $f $f 1)"
     }
   }
 
