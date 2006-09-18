32a347caca6ff7ceffc276a9daae495f1d0a0909
Steve Pieper <pieper@bwh.harvard.edu>
2006-09-18 20:12:36 +0000

    ENH: infrastructure to support mouse-over label queries of freesurfer data
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1269
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1269 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/FreeSurfer/Testing/QueryAtlas3.tcl
+++ Libs/FreeSurfer/Testing/QueryAtlas3.tcl
@@ -21,6 +21,8 @@ proc QueryAtlasInit { {filename ""} } {
 
   QueryAtlasAddModel
   QueryAtlasAddAnnotations 
+  QueryAtlasInitializePicker 
+  QueryAtlasRenderView
 }
 
 proc QueryAtlasAddModel {} {
@@ -116,4 +118,195 @@ proc QueryAtlasAddAnnotations {} {
       $fssar Delete
       [$viewer GetMainViewer] Reset
   }
+}
+
+
+proc QueryAtlasNumberToRGBA {number} {
+  set r [expr $number / (256 * 256)]
+  set number [expr $number % (256 * 256)]
+  set g [expr $number / 256]
+  set b [expr $number % 256]
+
+  return "$r $g $b 255"
+}
+
+proc QueryAtlasRGBAToNumber {rgba} {
+  foreach {r g b a} $rgba {}
+  return [expr $r * (256*256) + $g * 256 + $b] 
+}
+
+
+
+proc QueryAtlasInitializePicker {} {
+
+  #
+  # get the polydata for the model
+  # - model node comes from the scene (retrieved by the ID)
+  # - actor comes from the main Viewer
+  # - mapper comes from the actor
+  #
+  set modelNode [$::slicer3::MRMLScene GetNodeByID $::QA(modelNodeID)]
+  set polyData [$modelNode GetPolyData]
+  set actor [[$::slicer3::ApplicationGUI GetViewerWidget] GetActorByID $::QA(modelNodeID)]
+  set mapper [$actor GetMapper]
+
+  #
+  # instrument the polydata with cell number colors
+  #
+
+  $polyData Update
+
+  set cellData [$polyData GetCellData]
+  set cellNumberColors [$cellData GetArray "CellNumberColors"] 
+  if { $cellNumberColors == "" } {
+    set cellNumberColors [vtkUnsignedCharArray New]
+    $cellNumberColors SetName "CellNumberColors"
+    $cellData AddArray $cellNumberColors
+    $cellData SetScalars $cellNumberColors
+  }
+  $cellData SetScalars $cellNumberColors
+
+  set cellNumberColors [$cellData GetArray "CellNumberColors"] 
+  $cellNumberColors Initialize
+  $cellNumberColors SetNumberOfComponents 4
+
+  set numberOfCells [$polyData GetNumberOfCells]
+  for {set i 0} {$i < $numberOfCells} {incr i} {
+    eval $cellNumberColors InsertNextTuple4 [QueryAtlasNumberToRGBA $i]
+  }
+
+  set ::QA(cellData) $cellData
+  set ::QA(numberOfCells) $numberOfCells
+  set ::QA(mapper) $mapper
+  set ::QA(actor) $actor
+
+  #
+  # add the mouse move callback
+  # - create the classes that will be used every render and in the callback
+  # - add the callback with the current render info
+  #
+  if { ![info exists ::QA(viewer)] } {
+    set ::QA(viewer) [vtkImageViewer New]
+    set ::QA(windowToImage) [vtkWindowToImageFilter New]
+  }
+  set renderWidget [[$::slicer3::ApplicationGUI GetViewerWidget] GetMainViewer]
+  set renderer [$renderWidget GetRenderer]
+  set interactor [$renderWidget GetRenderWindowInteractor] 
+  $interactor AddObserver MouseMoveEvent "QueryAtlasPickCallback $renderer $interactor $::QA(windowToImage)"
+}
+
+proc QueryAtlasRenderView {} {
+
+  #
+  # get the renderer related instances
+  #
+  set renderWidget [[$::slicer3::ApplicationGUI GetViewerWidget] GetMainViewer]
+  set renderWindow [$renderWidget GetRenderWindow]
+  set renderer [$renderWidget GetRenderer]
+
+  #
+  # set the render parameters to draw with the cell labels
+  #
+  set renderState [QueryAtlasOverrideRenderState $::QA(cellData) $::QA(numberOfCells) $::QA(mapper) $::QA(actor)]
+
+  # if needed - remove other props and only add ours 
+  # (need to keep track of others somehow so we can restore them)
+  #$renderWidget RemoveAllViewProps
+  #$renderer AddActor $actor
+
+  #
+  # draw the image and get the pixels
+  #
+  $renderWindow SetSwapBuffers 0
+  puts "render renderWidget"; update
+  $renderWidget Render
+  puts "...render done"; update
+
+  $::QA(viewer) SetColorWindow 255
+  $::QA(viewer) SetColorLevel 127.5
+  $::QA(windowToImage) SetInputBufferTypeToRGBA
+  $::QA(windowToImage) ShouldRerenderOn
+  $::QA(windowToImage) ReadFrontBufferOff
+  $::QA(windowToImage) SetInput [$renderWidget GetRenderWindow]
+  $::QA(windowToImage) Modified
+  $::QA(viewer) SetInput [$::QA(windowToImage) GetOutput]
+  puts "render viewer"; update
+  $::QA(viewer) Render
+  puts "...render done"; update
+
+  $renderWindow SetSwapBuffers 1
+  QueryAtlasRestoreRenderState $::QA(cellData) $::QA(mapper) $::QA(actor) $renderState
+
+  puts "render renderWidget"; update
+  $renderWidget Render
+  puts "...render done"; update
+
+}
+
+proc QueryAtlasOverrideRenderState {cellData numberOfCells mapper actor} {
+
+  #
+  # save the render state before overriding it with the 
+  # parameters needed for cell rendering
+  #
+  if { [$cellData GetScalars] == "" } {
+    set state(activeScalars) ""
+  } else {
+    set state(activeScalars) [[$cellData GetScalars] GetName]
+  }
+  set state(scalarVisibility) [$mapper GetScalarVisibility]
+  set state(immediateModeRendering) [$mapper GetImmediateModeRendering]
+  set state(scalarMode) [$mapper GetScalarMode]
+  set state(scalarMaterialMode) [$mapper GetScalarMaterialMode]
+  set state(scalarRange) [$mapper GetScalarRange]
+  set state(ambient) [[$actor GetProperty] GetAmbient]
+  set state(diffuse) [[$actor GetProperty] GetDiffuse]
+
+  parray state
+
+  $cellData SetActiveScalars "CellNumberColors"
+  $mapper SetImmediateModeRendering 1
+  $mapper SetScalarVisibility 1
+  $mapper SetScalarModeToUseCellData
+  $mapper SetScalarMaterialModeToAmbient
+  $mapper SetScalarRange 0 $numberOfCells
+  [$actor GetProperty] SetAmbient 1
+  [$actor GetProperty] SetDiffuse 0
+
+  return [array get state]
+}
+
+proc QueryAtlasRestoreRenderState {cellData mapper actor renderState} {
+
+  array set state $renderState
+
+  if { $state(activeScalars) != "" } {
+    $cellData SetActiveScalars $state(activeScalars)
+  }
+  $mapper SetScalarVisibility $state(scalarVisibility)
+  $mapper SetImmediateModeRendering $state(immediateModeRendering) 
+  $mapper SetScalarMode $state(scalarMode)
+  $mapper SetScalarMaterialMode $state(scalarMaterialMode)
+  eval $mapper SetScalarRange $state(scalarRange)
+  [$actor GetProperty] SetAmbient $state(ambient)
+  [$actor GetProperty] SetDiffuse $state(diffuse)
+}
+
+#
+# query the cell number at the mouse location
+#
+proc QueryAtlasPickCallback {renderer interactor windowToImage} {
+
+  if { ![info exists ::QA(viewer)] } {
+    return
+  }
+
+  eval $interactor UpdateSize [$renderer GetSize]
+  foreach {x y} [$interactor GetEventPosition] {}
+  $windowToImage Update
+  set color ""
+  foreach c {0 1 2 3} {
+    lappend color [[$windowToImage GetOutput] GetScalarComponentAsFloat $x $y 0 $c]
+  }
+  puts "[format {%4d %4d} $x $y]:  [QueryAtlasRGBAToNumber $color] ($color)"
 }
