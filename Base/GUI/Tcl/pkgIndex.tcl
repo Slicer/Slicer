f6e68d023247227e21d058a77e3cf1cf9ad4e0db
Steve Pieper <pieper@bwh.harvard.edu>
2006-09-21 20:27:38 +0000

    BUG: generate a real error if itcl isn't present
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1306
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1306 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Tcl/pkgIndex.tcl
+++ Base/GUI/Tcl/pkgIndex.tcl
@@ -1,4 +1,6 @@
-if {[catch {package require Itcl}]} { return }
+
+package require Itcl
+
 package ifneeded SlicerBaseGUITcl 3.0 [list 
   source [file join $dir SliceViewerInteractor.tcl]
   source [file join $dir SWidget.tcl]
