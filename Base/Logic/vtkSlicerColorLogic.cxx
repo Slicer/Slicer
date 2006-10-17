1a62660719550f2bdc194150596d8fc32880a7b3
Nicole Aucoin <nicole@bwh.harvard.edu>
2006-10-17 17:47:09 +0000

    ENH: adding color module logic. TODO: get the events set up so that the default colour nodes are added to the scene on start up
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1464
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1464 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/Logic/vtkSlicerColorLogic.cxx
@@ -0,0 +1,91 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerColorLogic.cxx,v $
+  Date:      $Date: 2006/01/06 17:56:48 $
+  Version:   $Revision: 1.58 $
+
+=========================================================================auto=*/
+
+#include "vtkObjectFactory.h"
+#include "vtkCallbackCommand.h"
+#include <vtksys/SystemTools.hxx> 
+
+#include "vtkSlicerColorLogic.h"
+
+#include "vtkMRMLFiducial.h"
+#include "vtkMRMLFiducialListNode.h"
+
+vtkCxxRevisionMacro(vtkSlicerColorLogic, "$Revision: 1.9.12.1 $");
+vtkStandardNewMacro(vtkSlicerColorLogic);
+
+//----------------------------------------------------------------------------
+vtkSlicerColorLogic::vtkSlicerColorLogic()
+{
+    this->AddDefaultColorNodes();
+}
+
+//----------------------------------------------------------------------------
+vtkSlicerColorLogic::~vtkSlicerColorLogic()
+{
+    
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerColorLogic::ProcessMRMLEvents(vtkObject * caller,
+                                            unsigned long event,
+                                            void * callData)
+{
+  // when there's a new scene, add the default nodes
+  if (event == vtkMRMLScene::NewSceneEvent)
+    {
+    std::cout << "vtkSlicerColorLogic::ProcessMRMLEvents: got a NewScene event " << event << endl;
+    this->AddDefaultColorNodes();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerColorLogic::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->vtkObject::PrintSelf(os, indent);
+
+  os << indent << "vtkSlicerColorLogic:             " << this->GetClassName() << "\n";
+
+}
+
+//----------------------------------------------------------------------------
+void vtkSlicerColorLogic::AddDefaultColorNodes()
+{
+    // create the default color nodes, they don't get saved with the scenes as
+    // they'll be created on start up, and when a new
+    // scene is opened
+  if (this->GetMRMLScene() == NULL)
+    {
+    std::cerr << "vtkSlicerColorLogic::AddDefaultColorNodes: no scene to which to add nodes\n";
+    return;
+    }
+  for (int i = vtkMRMLColorNode::Grey; i < vtkMRMLColorNode::File; i++)
+    {
+    vtkMRMLColorNode *node = vtkMRMLColorNode::New();
+    node->SetType(i);
+    node->SaveWithSceneOff();
+    std::string name = std::string(node->GetClassName()) + std::string(node->GetTypeAsString());
+    std::cout << "vtkSlicerColorLogic::AddDefaultColorNodes: requesting id " << name.c_str() << endl;
+    //this->GetMRMLScene()->RequestNodeID(node, name.c_str());
+    if (this->GetMRMLScene()->GetNodeByID(name.c_str()) == NULL)
+      {
+      this->GetMRMLScene()->AddNode(node);
+      std::cout << "vtkSlicerColorLogic::AddDefaultColorNodes: added node " << node->GetID() << ", will eventually try to name it " << name.c_str() << endl;
+      }
+    else
+      {
+      std::cout << "vtkSlicerColorLogic::AddDefaultColorNodes: didn't add node " << node->GetID() << " as it was already in the scene.\n";
+      }
+    node->Delete();
+    }    
+}
---
+++ Base/Logic/vtkSlicerColorLogic.h
@@ -0,0 +1,61 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerColorLogic.h,v $
+  Date:      $Date: 2006/01/08 04:48:05 $
+  Version:   $Revision: 1.45 $
+
+=========================================================================auto=*/
+
+// .NAME vtkSlicerColorLogic - slicer logic class for color manipulation
+// .SECTION Description
+// This class manages the logic associated with reading, saving,
+// and changing propertied of the colors
+
+
+#ifndef __vtkSlicerColorLogic_h
+#define __vtkSlicerColorLogic_h
+
+#include <stdlib.h>
+
+#include "vtkSlicerBaseLogic.h"
+#include "vtkSlicerLogic.h"
+
+#include "vtkMRML.h"
+#include "vtkMRMLColorNode.h"
+
+class VTK_SLICER_BASE_LOGIC_EXPORT vtkSlicerColorLogic : public vtkSlicerLogic 
+{
+  public:
+  
+  // The Usual vtk class functions
+  static vtkSlicerColorLogic *New();
+  vtkTypeRevisionMacro(vtkSlicerColorLogic,vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Update logic state when MRML scene chenges
+  void ProcessMRMLEvents(vtkObject * caller,
+                         unsigned long event,
+                         void * callData);
+
+  // Description:
+  // Add a series of color nodes, setting the types to the defaults, so that
+  // they're accessible to the rest of Slicer
+  void AddDefaultColorNodes();
+    
+protected:
+  vtkSlicerColorLogic();
+  ~vtkSlicerColorLogic();
+  vtkSlicerColorLogic(const vtkSlicerColorLogic&);
+  void operator=(const vtkSlicerColorLogic&);
+
+};
+
+#endif
+
