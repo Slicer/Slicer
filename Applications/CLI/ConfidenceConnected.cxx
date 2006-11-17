e944778c05b2034f23f03cb95b3e692cf3aca97c
Bill Lorensen <bill.lorensen@gmail.com>
2006-11-17 21:55:10 +0000

    BUG: Needed an OrientedImage,
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1623
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1623 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/CLI/ConfidenceConnected.cxx
+++ Applications/CLI/ConfidenceConnected.cxx
@@ -13,7 +13,7 @@
 
 
 #include "itkConfidenceConnectedImageFilter.h"
-#include "itkImage.h"
+#include "itkOrientedImage.h"
 #include "itkCastImageFilter.h"
 #include "itkCurvatureFlowImageFilter.h"
 #include "itkImageFileReader.h"
@@ -28,10 +28,10 @@ int main( int argc, char *argv[] )
 
   typedef   float           InternalPixelType;
   const     unsigned int    Dimension = 3;
-  typedef itk::Image< InternalPixelType, Dimension >  InternalImageType;
+  typedef itk::OrientedImage< InternalPixelType, Dimension >  InternalImageType;
 
   typedef unsigned char OutputPixelType;
-  typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+  typedef itk::OrientedImage< OutputPixelType, Dimension > OutputImageType;
 
   typedef itk::CastImageFilter< InternalImageType, OutputImageType >
     CastingFilterType;
