f4864b6a5556761f7513ad532b7b3f381a7ef83a
Steve Pieper <pieper@bwh.harvard.edu>
2006-11-21 14:45:23 +0000

    ENH: import of model nodes now working
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1643
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1643 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/Testing/slicer2-import.tcl
+++ Libs/MRML/Testing/slicer2-import.tcl
@@ -1,4 +1,8 @@
 
+#
+# test of importing a slicer2 mrml scene using the 
+# vtk xml data parser mechanism
+#
 
 
 proc ImportSlicer2Scene {sceneFile} {
@@ -7,39 +11,99 @@ proc ImportSlicer2Scene {sceneFile} {
   $parser SetFileName $sceneFile
   $parser Parse
 
-  puts [$parser Print]
-  puts $parser
-
   set root [$parser GetRootElement]
 
-  PrintElement 0 $root
+  set ::S2(dir) [file dirname [file normalize $sceneFile]]
+  set ::S2(stack) ""
+  ImportElement $root
 }
 
-proc PrintIndent {indent} {
-  for {set i 0} {$i < $indent} {incr i} {
-    puts -nonewline " "
-  }
-}
-
-proc PrintElement {indent element} {
+proc ImportElement {element} {
   
-  PrintIndent $indent
-  incr indent
-  puts "$element [$element GetName]"
-
-  set nAtts [$element GetNumberOfAttributes]
-  for {set i 0} {$i < $nAtts} {incr i} {
-    PrintIndent $indent
-    set attName [$element GetAttributeName $i]
-    puts "$attName [$element GetAttributeValue $i]"
-  } 
-
+  ImportNode $element
 
   set nNested [$element GetNumberOfNestedElements]
   for {set i 0} {$i < $nNested} {incr i} {
     set nestElement [$element GetNestedElement $i]
-    PrintElement $indent $nestElement
+    ImportElement $nestElement
   }
 }
 
+proc ImportNode {element} {
+
+  set nAtts [$element GetNumberOfAttributes]
+  for {set i 0} {$i < $nAtts} {incr i} {
+    set attName [$element GetAttributeName $i]
+    set node($attName) [$element GetAttributeValue $i]
+  } 
+
+  set nodeType [$element GetName]
+  set handler ImportNode$nodeType
+  if { [info command $handler] == "" } {
+    error "no handler for $nodeType"
+  }
+
+
+  # call the handler for this element
+  $handler node
+}
+
+proc ImportNodeMRML {node} {
+  # no op
+  puts MRML
+}
+
+proc ImportNodeTransform {node} {
+}
+
+proc ImportNodeMatrix {node} {
+  upvar $node n
+
+  #parray n
+}
+
+proc ImportNodeVolume {node} {
+  upvar $node n
+
+  #parray n
+}
+
+proc ImportNodeModel {node} {
+  upvar $node n
+
+  set modelNode [vtkMRMLModelNode New]
+  set modelStorageNode [vtkMRMLModelStorageNode New]
+  set modelDisplayNode [vtkMRMLModelDisplayNode New]
+
+  $modelNode SetName $n(name)
+  if { [file pathtype $n(fileName)] == "relative" } {
+    $modelStorageNode SetFileName $::S2(dir)/$n(fileName)
+  } else {
+    $modelStorageNode SetFileName $n(fileName)
+  }
+
+  if { [$modelStorageNode ReadData $modelNode] != 0 } {
+
+    $::slicer3::MRMLScene AddNode $modelStorageNode
+    $::slicer3::MRMLScene AddNode $modelDisplayNode
+
+    $modelNode SetStorageNodeID [$modelStorageNode GetID]
+    $modelNode SetAndObserveDisplayNodeID [$modelDisplayNode GetID]
+
+    $::slicer3::MRMLScene AddNode $modelNode
+
+  } else {
+    puts stderr "warning: could not read data for model node from $n(fileName)"
+  }
+
+  $modelNode Delete
+  $modelStorageNode Delete
+  $modelDisplayNode Delete
+}
+
+proc ImportNodeOptions {node} {
+  puts stderr "warning: option nodes cannot be imported"
+}
+
 ImportSlicer2Scene c:/data/tutorial/tutorial.xml
+
