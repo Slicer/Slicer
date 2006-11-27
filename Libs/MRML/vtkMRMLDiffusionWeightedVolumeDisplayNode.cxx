f27de506b510f0a806f194d81574710a1ddddec8
Raul San Jose <rjosest@bwh.harvard.edu>
2006-11-27 19:15:15 +0000

    ENH: New MRML class to display diffusion weighted images
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1698
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1698 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/MRML/vtkMRMLDiffusionWeightedVolumeDisplayNode.cxx
@@ -0,0 +1,122 @@
+/*=auto=========================================================================
+
+Portions (c) Copyright 2005 Brigham and Women\"s Hospital (BWH) All Rights Reserved.
+
+See Doc/copyright/copyright.txt
+or http://www.slicer.org/copyright/copyright.txt for details.
+
+Program:   3D Slicer
+Module:    $RCSfile: vtkMRMLDiffusionWeightedVolumeDisplayNode.cxx,v $
+Date:      $Date: 2006/03/17 15:10:10 $
+Version:   $Revision: 1.2 $
+
+=========================================================================auto=*/
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "vtkObjectFactory.h"
+#include "vtkCallbackCommand.h"
+
+#include "vtkMRMLDiffusionWeightedVolumeDisplayNode.h"
+#include "vtkMRMLScene.h"
+
+
+//------------------------------------------------------------------------------
+vtkMRMLDiffusionWeightedVolumeDisplayNode* vtkMRMLDiffusionWeightedVolumeDisplayNode::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLDiffusionWeightedVolumeDisplayNode");
+  if(ret)
+    {
+    return (vtkMRMLDiffusionWeightedVolumeDisplayNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLDiffusionWeightedVolumeDisplayNode;
+}
+
+//----------------------------------------------------------------------------
+
+vtkMRMLNode* vtkMRMLDiffusionWeightedVolumeDisplayNode::CreateNodeInstance()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLDiffusionWeightedVolumeDisplayNode");
+  if(ret)
+    {
+    return (vtkMRMLDiffusionWeightedVolumeDisplayNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLDiffusionWeightedVolumeDisplayNode;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLDiffusionWeightedVolumeDisplayNode::vtkMRMLDiffusionWeightedVolumeDisplayNode()
+{
+  // Strings
+  this->DiffusionComponent = 0;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLDiffusionWeightedVolumeDisplayNode::~vtkMRMLDiffusionWeightedVolumeDisplayNode()
+{
+
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLDiffusionWeightedVolumeDisplayNode::WriteXML(ostream& of, int nIndent)
+{
+  Superclass::WriteXML(of, nIndent);
+
+  vtkIndent indent(nIndent);
+
+  std::stringstream ss;
+  ss << this->DiffusionComponent;
+  of << indent << "diffusionComponent=\"" << ss.str() << "\" ";
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLDiffusionWeightedVolumeDisplayNode::ReadXMLAttributes(const char** atts)
+{
+
+  Superclass::ReadXMLAttributes(atts);
+
+  const char* attName;
+  const char* attValue;
+  while (*atts != NULL) 
+    {
+    attName = *(atts++);
+    attValue = *(atts++);
+    if (!strcmp(attName, "diffusionComponent")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> this->DiffusionComponent;
+      }
+    }  
+}
+
+//----------------------------------------------------------------------------
+// Copy the node\"s attributes to this object.
+// Does NOT copy: ID, FilePrefix, Name, VolumeID
+void vtkMRMLDiffusionWeightedVolumeDisplayNode::Copy(vtkMRMLNode *anode)
+{
+  Superclass::Copy(anode);
+  vtkMRMLDiffusionWeightedVolumeDisplayNode *node = (vtkMRMLDiffusionWeightedVolumeDisplayNode *) anode;
+
+  this->SetDiffusionComponent(node->DiffusionComponent);
+
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLDiffusionWeightedVolumeDisplayNode::PrintSelf(ostream& os, vtkIndent indent)
+{
+  
+  Superclass::PrintSelf(os,indent);
+
+  os << indent << "Diffusion Component:   " << this->DiffusionComponent << "\n";
+
+}
+
+
+
---
+++ Libs/MRML/vtkMRMLDiffusionWeightedVolumeDisplayNode.h
@@ -0,0 +1,82 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLDiffusionWeightedVolumeDisplayNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:29 $
+  Version:   $Revision: 1.3 $
+
+=========================================================================auto=*/
+// .NAME vtkMRMLDiffusionWeightedVolumeDisplayNode - MRML node for representing a volume (image stack).
+// .SECTION Description
+// Volume nodes describe data sets that can be thought of as stacks of 2D 
+// images that form a 3D volume.  Volume nodes describe where the images 
+// are stored on disk, how to render the data (window and level), and how 
+// to read the files.  This information is extracted from the image 
+// headers (if they exist) at the time the MRML file is generated.  
+// Consequently, MRML files isolate MRML browsers from understanding how 
+// to read the myriad of file formats for medical data. 
+
+#ifndef __vtkMRMLDiffusionWeightedVolumeDisplayNode_h
+#define __vtkMRMLDiffusionWeightedVolumeDisplayNode_h
+
+#include "vtkMRML.h"
+#include "vtkMRMLVolumeDisplayNode.h"
+#include "vtkMRMLStorageNode.h"
+#include "vtkMRMLColorNode.h"
+
+#include "vtkMatrix4x4.h"
+#include "vtkImageData.h"
+
+class vtkImageData;
+
+class VTK_MRML_EXPORT vtkMRMLDiffusionWeightedVolumeDisplayNode : public vtkMRMLVolumeDisplayNode
+{
+  public:
+  static vtkMRMLDiffusionWeightedVolumeDisplayNode *New();
+  vtkTypeMacro(vtkMRMLDiffusionWeightedVolumeDisplayNode,vtkMRMLNode);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+
+  // Description:
+  // Set node attributes
+  virtual void ReadXMLAttributes( const char** atts);
+
+  // Description:
+  // Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+  // Description:
+  // Copy the node's attributes to this object
+  virtual void Copy(vtkMRMLNode *node);
+
+  // Description:
+  // Get node XML tag name (like Volume, Model)
+  virtual const char* GetNodeTagName() {return "DiffusionWeightedVolumeDisplay";};
+
+  //--------------------------------------------------------------------------
+  // Display Information
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Set/Get interpolate reformated slices
+  vtkGetMacro(DiffusionComponent, int);
+  vtkSetMacro(DiffusionComponent, int);
+ 
+protected:
+  vtkMRMLDiffusionWeightedVolumeDisplayNode();
+  ~vtkMRMLDiffusionWeightedVolumeDisplayNode();
+  vtkMRMLDiffusionWeightedVolumeDisplayNode(const vtkMRMLDiffusionWeightedVolumeDisplayNode&);
+  void operator=(const vtkMRMLDiffusionWeightedVolumeDisplayNode&);
+
+  int DiffusionComponent;
+
+};
+
+#endif
+
