ba751fa9d6345f3f35a953bb8ce5829ac89e50f0
Raul San Jose <rjosest@bwh.harvard.edu>
2006-12-11 23:43:08 +0000

    ENH: Adding new class to read/write NRRD volumes based on the vtkNRRDReader in vtkTeem. We need this to support I/O of diffusion data while the Archetype reader/writer infrastructure evolves to handle more particular nrrd datasets like diffusion weighted images
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1790
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1790 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/MRML/vtkMRMLNRRDStorageNode.cxx
@@ -0,0 +1,430 @@
+/*=auto=========================================================================
+
+Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+See Doc/copyright/copyright.txt
+or http://www.slicer.org/copyright/copyright.txt for details.
+
+Program:   3D Slicer
+Module:    $RCSfile: vtkMRMLNRRDStorageNode.cxx,v $
+Date:      $Date: 2006/03/17 15:10:10 $
+Version:   $Revision: 1.6 $
+
+=========================================================================auto=*/
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "vtkObjectFactory.h"
+#include "vtkImageChangeInformation.h"
+#include "vtkMRMLNRRDStorageNode.h"
+#include "vtkMRMLVolumeNode.h"
+#include "vtkMRMLScalarVolumeNode.h"
+#include "vtkMRMLVectorVolumeNode.h"
+#include "vtkMRMLDiffusionWeightedVolumeNode.h"
+#include "vtkMRMLDiffusionTensorVolumeNode.h"
+
+#include "vtkMatrix4x4.h"
+#include "vtkImageData.h"
+#include "vtkNRRDReader.h"
+#include "vtkDoubleArray.h"
+
+//------------------------------------------------------------------------------
+vtkMRMLNRRDStorageNode* vtkMRMLNRRDStorageNode::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLNRRDStorageNode");
+  if(ret)
+    {
+    return (vtkMRMLNRRDStorageNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLNRRDStorageNode;
+}
+
+//----------------------------------------------------------------------------
+
+vtkMRMLNode* vtkMRMLNRRDStorageNode::CreateNodeInstance()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLNRRDStorageNode");
+  if(ret)
+    {
+    return (vtkMRMLNRRDStorageNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLNRRDStorageNode;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLNRRDStorageNode::vtkMRMLNRRDStorageNode()
+{
+  this->CenterImage = 1;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLNRRDStorageNode::~vtkMRMLNRRDStorageNode()
+{
+}
+
+void vtkMRMLNRRDStorageNode::WriteXML(ostream& of, int nIndent)
+{
+  Superclass::WriteXML(of, nIndent);
+  vtkIndent indent(nIndent);
+
+  std::stringstream ss;
+  ss << this->CenterImage;
+  of << indent << "centerImage=\"" << ss.str() << "\" ";
+
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLNRRDStorageNode::ReadXMLAttributes(const char** atts)
+{
+
+  Superclass::ReadXMLAttributes(atts);
+
+  const char* attName;
+  const char* attValue;
+  while (*atts != NULL) 
+    {
+    attName = *(atts++);
+    attValue = *(atts++);
+    if (!strcmp(attName, "centerImage")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> this->CenterImage;
+      }
+    }
+}
+
+//----------------------------------------------------------------------------
+// Copy the node's attributes to this object.
+// Does NOT copy: ID, FilePrefix, Name, StorageID
+void vtkMRMLNRRDStorageNode::Copy(vtkMRMLNode *anode)
+{
+  Superclass::Copy(anode);
+  vtkMRMLNRRDStorageNode *node = (vtkMRMLNRRDStorageNode *) anode;
+
+  this->SetCenterImage(node->CenterImage);
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLNRRDStorageNode::PrintSelf(ostream& os, vtkIndent indent)
+{  
+  vtkMRMLStorageNode::PrintSelf(os,indent);
+  os << indent << "CenterImage:   " << this->CenterImage << "\n";
+
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLNRRDStorageNode::ProcessParentNode(vtkMRMLNode *parentNode)
+{
+  this->ReadData(parentNode);
+}
+
+//----------------------------------------------------------------------------
+
+int vtkMRMLNRRDStorageNode::ReadData(vtkMRMLNode *refNode)
+{
+
+  // test whether refNode is a valid node to hold a volume
+  if ( !(refNode->IsA("vtkMRMLScalarVolumeNode")) || refNode->IsA("vtkMRMLVectorVolumeNode" ) || 
+       !(refNode->IsA("vtkMRMLDiffusionWeightedVolumeNode")) ||
+       !(refNode->IsA("vtkMRMLDiffusionTensorVolumeNode"))
+     )
+    {
+    vtkErrorMacro("Reference node is not a vtkMRMLVolumeNode");
+    return 0;         
+    }
+  if (this->GetFileName() == NULL) 
+    {
+      return 0;
+    }
+  vtkMRMLVolumeNode *volNode;
+
+  vtkNRRDReader* reader;
+
+  if ( refNode->IsA("vtkMRMLScalarVolumeNode") ) 
+    {
+    volNode = dynamic_cast <vtkMRMLScalarVolumeNode *> (refNode);
+    }
+  else if ( refNode->IsA("vtkMRMLVectorVolumeNode") ) 
+    {
+    volNode = dynamic_cast <vtkMRMLVectorVolumeNode *> (refNode);
+    }
+  else if ( refNode->IsA("vtkMRMLDiffusionWeightedVolumeNode") )
+    {
+    volNode = dynamic_cast <vtkMRMLDiffusionWeightedVolumeNode *> (refNode);
+    }
+  else if ( refNode->IsA("vtkMRMLDiffusionTensorVolumeNode") )
+    {
+    volNode = dynamic_cast <vtkMRMLDiffusionTensorVolumeNode *> (refNode);
+    }
+
+  reader = vtkNRRDReader::New();
+
+  if (volNode->GetImageData()) 
+    {
+    volNode->SetAndObserveImageData (NULL);
+    }
+
+  std::string fullName;
+  if (this->SceneRootDir != NULL && this->Scene->IsFilePathRelative(this->GetFileName())) 
+    {
+    fullName = std::string(this->SceneRootDir) + std::string(this->GetFileName());
+    }
+  else 
+    {
+    fullName = std::string(this->GetFileName());
+    }
+
+  if (fullName == std::string("")) 
+    {
+    vtkErrorMacro("vtkMRMLVolumeNode: File name not specified");
+    reader->Delete();
+    return 0;
+    }
+
+  reader->SetFileName(fullName.c_str());
+
+  // Check if this is a NRRD file that we can read
+  if (!reader->CanReadFile(fullName.c_str()))
+    {
+    vtkErrorMacro("vtkMRMLNRRDStorageNode: This is not a nrrd file");
+    reader->Delete();
+    return 0;
+    }
+
+  // Read the header to see if the NRRD file corresponds to the
+  // MRML Node
+  reader->UpdateInformation();
+  // Check type
+  if ( refNode->IsA("vtkMRMLScalarVolumeNode") )
+    {
+    if (!(reader->GetPointDataType() == SCALARS && 
+        (reader->GetNumberOfComponents() == 1 || reader->GetNumberOfComponents()==3) ))
+      {
+      vtkErrorMacro("MRMLVolumeNode does not match file kind");
+      reader->Delete();
+      return 0;
+      }
+    }
+  else if ( refNode->IsA("vtkMRMLVectorVolumeNode") )
+    {
+    if (! (reader->GetPointDataType() == VECTORS || reader->GetPointDataType() == NORMALS))
+      {
+      vtkErrorMacro("MRMLVolumeNode does not match file kind");
+      reader->Delete();
+      return 0;
+      }
+    }
+  else if ( refNode->IsA("vtkMRMLDiffusionTensorVolumeNode") )
+    {
+    if ( ! (reader->GetPointDataType() == TENSORS))
+      {
+      vtkErrorMacro("MRMLVolumeNode does not match file kind");
+      reader->Delete();
+      return 0;
+      }
+    }
+  else if ( refNode->IsA("vtkMRMLDiffusionWeightedVolumeNode"))
+    {
+    if ( ! (reader->GetPointDataType() == SCALARS &&
+            !strcmp(reader->GetHeaderValue("modality"),"DWMRI") ) )
+      {
+      vtkErrorMacro("MRMLVolumeNode does not match file kind");
+      reader->Delete();
+      return 0;
+      }
+    }
+
+  // Set Reader member variables
+
+  if (this->CenterImage) 
+    {
+    reader->SetUseNativeOriginOff();
+    }
+  else
+    {
+    reader->SetUseNativeOriginOn();
+    }
+  reader->Update();
+
+  // set volume attributes
+  vtkMatrix4x4* mat = reader->GetRasToIjkMatrix();
+  volNode->SetRASToIJKMatrix(mat);
+
+  if ( !refNode->IsA("vtkMRMLScalarVolumeNode") )
+    {
+    vtkMatrix4x4 *mat2;
+    mat2 = reader->GetMeasurementFrameMatrix();
+    dynamic_cast <vtkMRMLTensorVolumeNode *> (volNode)->SetMeasurementFrameMatrix(mat2);
+    mat2->Delete();
+    }
+
+  // parse additional key-value pairs
+  if ( refNode->IsA("vtkMRMLDiffusionWeightedVolumeNode") )
+    {
+    vtkDoubleArray *grad = vtkDoubleArray::New();
+    vtkDoubleArray *bvalue = vtkDoubleArray::New();
+    if (this->ParseDiffusionInformation(reader,grad,bvalue))
+      {
+      vtkErrorMacro("vtkMRMLDiffusionWeightedVolumeNode: Cannot parse Diffusion Information");
+      grad->Delete();
+      bvalue->Delete();
+      reader->Delete();
+      return 0;
+      }
+    dynamic_cast <vtkMRMLDiffusionWeightedVolumeNode *> (volNode)->SetGradients(grad);
+    dynamic_cast <vtkMRMLDiffusionWeightedVolumeNode *> (volNode)->SetBValues(bvalue);
+    grad->Delete();
+    bvalue->Delete();
+    }
+
+  volNode->SetStorageNodeID(this->GetID());
+  //TODO update scene to send Modified event
+ 
+  vtkImageChangeInformation *ici = vtkImageChangeInformation::New();
+  ici->SetInput (reader->GetOutput());
+  ici->SetOutputSpacing( 1, 1, 1 );
+  ici->SetOutputOrigin( 0, 0, 0 );
+  ici->Update();
+
+  volNode->SetAndObserveImageData (ici->GetOutput());
+
+  reader->Delete();
+  ici->Delete();
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkMRMLNRRDStorageNode::WriteData(vtkMRMLNode *refNode)
+{
+
+  vtkErrorMacro("We cannot write NRRD data");
+  return 0;
+
+  // test whether refNode is a valid node to hold a volume
+  if (!refNode->IsA("vtkMRMLScalarVolumeNode") ) 
+    {
+    vtkErrorMacro("Reference node is not a vtkMRMLVolumeNode");
+    return 0;
+    }
+  
+  vtkMRMLVolumeNode *volNode;
+  
+  if ( refNode->IsA("vtkMRMLScalarVolumeNode") ) 
+    {
+    volNode = vtkMRMLScalarVolumeNode::SafeDownCast(refNode);
+    }
+  
+  if (volNode->GetImageData() == NULL) 
+    {
+    vtkErrorMacro("cannot write ImageData, it's NULL");
+    }
+  
+  std::string fullName;
+  if (this->SceneRootDir != NULL && this->Scene->IsFilePathRelative(this->GetFileName())) 
+    {
+    fullName = std::string(this->SceneRootDir) + std::string(this->GetFileName());
+    }
+  else 
+    {
+    fullName = std::string(this->GetFileName());
+    }
+  
+  if (fullName == std::string("")) 
+    {
+    vtkErrorMacro("vtkMRMLVolumeNode: File name not specified");
+    return 0;
+    }
+  // Use here the NRRD Writer
+  //vtkITKImageWriter *writer = vtkITKImageWriter::New();
+  //writer->SetFileName(fullName.c_str());
+  //writer->SetInput( volNode->GetImageData() );
+
+  // set volume attributes
+  vtkMatrix4x4* mat = vtkMatrix4x4::New();
+  volNode->GetRASToIJKMatrix(mat);
+  //writer->SetRasToIJKMatrix(mat);
+
+  //writer->Write();
+  //writer->Delete();
+
+  return 1;
+
+}
+
+//----------------------------------------------------------------------------
+int vtkMRMLNRRDStorageNode::ParseDiffusionInformation(vtkNRRDReader *reader,vtkDoubleArray *grad,vtkDoubleArray *bvalues)
+{
+  std::string keys(reader->GetHeaderKeys());
+  std::string key,value,num;
+  std::string tag,tagnex;
+  vtkDoubleArray *factor = vtkDoubleArray::New();
+  grad->SetNumberOfComponents(3);
+  double g[3];
+  int rep;
+
+  // search for modality tag
+  tag = "modality";
+  value = std::string(reader->GetHeaderValue((char *) tag.c_str()));
+  if (value.size() == 0)
+  {
+  return 0;
+  }
+  if (strcmp(value.c_str(),"DWMRI"))
+  {
+  return 0;
+  }
+  // search for tag DWMRI_gradient_
+  tag = "DWMRI_gradient_";
+  tagnex = "DWMRI_NEX_";
+  int pos = 0;
+  int gbeginpos =0;
+  int gendpos = 0;
+  pos = keys.find(tag,pos);
+  while ( pos < keys.size() )
+    {
+    num = keys.substr(pos+tag.size(),pos+tag.size()+4);
+    // Insert gradient
+    key = tag+num;
+    value = reader->GetHeaderValue((char *) key.c_str());
+    for (int i=0 ;i<3; i++)
+      {
+      gendpos=value.find(" ",gbeginpos);
+      g[i] = atof(value.substr(gbeginpos,gendpos).c_str());
+      }
+    grad->InsertNextTuple3(g[0],g[1],g[2]);
+    factor->InsertNextValue(sqrt(g[0]*g[0]+g[1]*g[1]+g[2]*g[2]));
+    // find repetitions of this gradient
+    key = tagnex+num;
+    value = reader->GetHeaderValue((char *) key.c_str());
+    if (value.size()>0) {
+      rep = atoi(value.c_str());
+      for (int i=0;i<rep-1;i++) {
+        grad->InsertNextTuple3(g[0],g[1],g[2]);
+      }
+    }
+    pos = keys.find(tag,pos);
+  }
+
+  grad->Modified();
+  factor->Modified();
+  double range[2];
+  // search for tag DWMRI_b-value
+  key = "DWMRI_b-value";
+  double bval = atof(reader->GetHeaderValue((char *) key.c_str()));
+  factor->GetRange(range);
+  bvalues->SetNumberOfTuples(grad->GetNumberOfTuples());
+  for (int i=0; i<grad->GetNumberOfTuples();i++)
+    {
+    bvalues->SetValue(i,bval*factor->GetValue(i)/range[1]);
+    }
+  factor->Delete();
+}
+
---
+++ Libs/MRML/vtkMRMLNRRDStorageNode.h
@@ -0,0 +1,89 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLNRRDStorageNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:29 $
+  Version:   $Revision: 1.8 $
+
+=========================================================================auto=*/
+// .NAME vtkMRMLNRRDStorageNode - MRML node for representing a volume storage
+// .SECTION Description
+// vtkMRMLNRRDStorageNode nodes describe the archetybe based volume storage
+// node that allows to read/write volume data from/to file using generic ITK mechanism.
+
+#ifndef __vtkMRMLNRRDStorageNode_h
+#define __vtkMRMLNRRDStorageNode_h
+
+#include "vtkMRML.h"
+#include "vtkMRMLScene.h"
+#include "vtkMRMLStorageNode.h"
+class vtkNRRDReader;
+class vtkDoubleArray;
+
+class vtkImageData;
+
+class VTK_MRML_EXPORT vtkMRMLNRRDStorageNode : public vtkMRMLStorageNode
+{
+  public:
+  static vtkMRMLNRRDStorageNode *New();
+  vtkTypeMacro(vtkMRMLNRRDStorageNode,vtkMRMLStorageNode);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+
+  // Description:
+  // Read node attributes from XML file
+  virtual void ReadXMLAttributes( const char** atts);
+
+   // Description:
+  // Read data and set it in the referenced node
+  // NOTE: Subclasses should implement this method
+  virtual int ReadData(vtkMRMLNode *refNode);
+
+  // Description:
+  // Write data from a  referenced node
+  // NOTE: Subclasses should implement this method
+  virtual int WriteData(vtkMRMLNode *refNode);
+
+  // Description:
+  // Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+ // Description:
+  // Set dependencies between this node and the parent node
+  // when parsing XML file
+  virtual void ProcessParentNode(vtkMRMLNode *parentNode);
+
+  // Description:
+  // Copy the node's attributes to this object
+  virtual void Copy(vtkMRMLNode *node);
+
+  // Description:
+  // Get node XML tag name (like Storage, Model)
+  virtual const char* GetNodeTagName()  {return "VolumeArchetypeStorage";};
+
+  // Description:
+  // Center image on read
+  vtkGetMacro(CenterImage, int);
+  vtkSetMacro(CenterImage, int);
+
+protected:
+
+
+  vtkMRMLNRRDStorageNode();
+  ~vtkMRMLNRRDStorageNode();
+  vtkMRMLNRRDStorageNode(const vtkMRMLNRRDStorageNode&);
+  void operator=(const vtkMRMLNRRDStorageNode&);
+
+  int ParseDiffusionInformation(vtkNRRDReader *reader,vtkDoubleArray *grad,vtkDoubleArray *bvalues);
+  int CenterImage;
+
+};
+
+#endif
+
