6d8aa29aa22dac6ca3e288baee3a35b58a68d785
Bill Lorensen <bill.lorensen@gmail.com>
2006-12-19 15:37:19 +0000

    ENH: Added new command line executables for adding and curvature anisotropic filtering.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1874
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1874 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/CLI/Add.cxx
@@ -0,0 +1,133 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $HeadURL: http://www.na-mic.org/svn/Slicer3/trunk/Applications/CLI/GradientAnisotropicDiffusion.cxx $
+  Language:  C++
+  Date:      $Date: 2006-11-14 15:03:40 -0500 (Tue, 14 Nov 2006) $
+  Version:   $Revision: 1582 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+#include "itkAddImageFilter.h"
+
+#include "itkPluginUtilities.h"
+#include "AddCLP.h"
+
+template<class T> int DoIt( int argc, char * argv[], T )
+{
+
+  PARSE_ARGS;
+
+  typedef    T       InputPixelType;
+  typedef    T       OutputPixelType;
+
+  typedef itk::Image< InputPixelType,  3 >   InputImageType;
+  typedef itk::Image< OutputPixelType, 3 >   OutputImageType;
+
+  typedef itk::ImageFileReader< InputImageType >  ReaderType;
+  typedef itk::ImageFileWriter< OutputImageType > WriterType;
+
+  typedef itk::AddImageFilter<
+    InputImageType, InputImageType, OutputImageType >  FilterType;
+
+  typename ReaderType::Pointer reader1 = ReaderType::New();
+  itk::PluginFilterWatcher watchReader1(reader1, "Read Volume 2",
+                                        CLPProcessInformation);
+  
+  typename ReaderType::Pointer reader2 = ReaderType::New();
+  itk::PluginFilterWatcher watchReader2(reader2,
+                                        "Read Volume 2",
+                                        CLPProcessInformation);
+  reader1->SetFileName( inputVolume1.c_str() );
+  reader2->SetFileName( inputVolume2.c_str() );
+
+  typename FilterType::Pointer filter = FilterType::New();
+  itk::PluginFilterWatcher watchFilter(filter,
+                                       "Add images",
+                                       CLPProcessInformation);
+
+  filter->SetInput( 0, reader1->GetOutput() );
+  filter->SetInput( 1, reader2->GetOutput() );
+
+  typename WriterType::Pointer writer = WriterType::New();
+  itk::PluginFilterWatcher watchWriter(writer,
+                                       "Write Volume",
+                                       CLPProcessInformation);
+  writer->SetFileName( outputVolume.c_str() );
+  writer->SetInput( filter->GetOutput() );
+  writer->Update();
+
+  return EXIT_SUCCESS;
+}
+
+int main( int argc, char * argv[] )
+{
+  
+  PARSE_ARGS;
+
+  itk::ImageIOBase::IOPixelType pixelType;
+  itk::ImageIOBase::IOComponentType componentType;
+
+  try
+    {
+    itk::GetImageType (inputVolume1, pixelType, componentType);
+
+    // This filter handles all types on input, but only produces
+    // signed types
+    
+    switch (componentType)
+      {
+      case itk::ImageIOBase::UCHAR:
+      case itk::ImageIOBase::CHAR:
+        return DoIt( argc, argv, static_cast<char>(0));
+        break;
+      case itk::ImageIOBase::USHORT:
+      case itk::ImageIOBase::SHORT:
+        return DoIt( argc, argv, static_cast<short>(0));
+        break;
+      case itk::ImageIOBase::UINT:
+      case itk::ImageIOBase::INT:
+        return DoIt( argc, argv, static_cast<int>(0));
+        break;
+      case itk::ImageIOBase::ULONG:
+      case itk::ImageIOBase::LONG:
+        return DoIt( argc, argv, static_cast<long>(0));
+        break;
+      case itk::ImageIOBase::FLOAT:
+        return DoIt( argc, argv, static_cast<float>(0));
+        break;
+      case itk::ImageIOBase::DOUBLE:
+        return DoIt( argc, argv, static_cast<double>(0));
+        break;
+      case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
+      default:
+        std::cout << "unknown component type" << std::endl;
+        break;
+      }
+    }
+  catch( itk::ExceptionObject &excep)
+    {
+    std::cerr << argv[0] << ": exception caught !" << std::endl;
+    std::cerr << excep << std::endl;
+    return EXIT_FAILURE;
+    }
+  return EXIT_SUCCESS;
+}
