f5aed791f56717ce4ae271233ce0d0ebbe2841a7
Bill Lorensen <bill.lorensen@gmail.com>
2006-12-20 13:59:57 +0000

    ENH: Added test for ModuleFactory.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1889
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1889 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/ModuleDescriptionParser/Testing/ModuleFactoryTest.cxx
@@ -0,0 +1,88 @@
+/*=========================================================================
+
+  Copyright Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   Module Description Parser
+  Module:    $HeadURL$
+  Date:      $Date$
+  Version:   $Revision$
+
+==========================================================================*/
+#include <string>
+#include "ModuleFactory.h"
+#include "itksys/Directory.hxx"
+
+void WarningMessage(const char *msg);
+void ErrorMessage(const char *msg);
+void InformationMessage(const char *msg);
+void DebugMessage(const char *msg);
+void SplashMessage(const char *msg);
+
+//
+// Test the module factory
+//
+
+int main (int argc, char *argv[])
+{
+
+  if (argc < 2)
+    {
+    std::cout << "Usage: " << argv[0] << " search_path" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  itksys::Directory myDir;
+  myDir.Load(argv[1]);
+  for ( unsigned long i = 0; i < myDir.GetNumberOfFiles(); i++)
+    {
+    ModuleFactory moduleFactory;
+    std::string searchPath(argv[1]);
+    searchPath += myDir.GetFile(i);
+    moduleFactory.SetSearchPath( searchPath );
+    moduleFactory.SetWarningMessageCallback( WarningMessage );
+    moduleFactory.SetErrorMessageCallback( ErrorMessage );
+    moduleFactory.SetInformationMessageCallback( InformationMessage );
+    moduleFactory.SetModuleDiscoveryMessageCallback( SplashMessage );
+    moduleFactory.Scan();
+
+    // Get the module names
+    std::vector<std::string> moduleNames = moduleFactory.GetModuleNames();
+    
+    std::vector<std::string>::const_iterator mit;
+    mit = moduleNames.begin();
+    while (mit != moduleNames.end())
+      {
+      std::cout << "Found module: " << *mit << std::endl;
+      ++mit;
+      }
+    }
+  return EXIT_SUCCESS;
+}
+
+void WarningMessage(const char *msg)
+{
+  std::cout << "Warning: " << msg << std::endl;
+}
+
+void ErrorMessage(const char *msg)
+{
+  std::cout << "Error: " << msg << std::endl;
+}
+
+void InformationMessage(const char *msg)
+{
+  std::cout << "Information: " << msg << std::endl;
+}
+
+void DebugMessage(const char *msg)
+{
+  std::cout << "Debug: " << msg << std::endl;
+}
+
+void SplashMessage(const char *msg)
+{
+  std::cout << "Splash: " << msg << std::endl;
+}
