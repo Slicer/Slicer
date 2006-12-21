35606ac70343f2d7f3467824bfd970dc6dce0ee4
Raul San Jose <rjosest@bwh.harvard.edu>
2006-12-21 16:37:41 +0000

    ENH: New class to display vector volumes
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=1905
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@1905 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/MRML/vtkMRMLVectorVolumeDisplayNode.cxx
@@ -0,0 +1,145 @@
+/*=auto=========================================================================
+
+Portions (c) Copyright 2005 Brigham and Women\"s Hospital (BWH) All Rights Reserved.
+
+See Doc/copyright/copyright.txt
+or http://www.slicer.org/copyright/copyright.txt for details.
+
+Program:   3D Slicer
+Module:    $RCSfile: vtkMRMLVectorVolumeDisplayNode.cxx,v $
+Date:      $Date: 2006/03/17 15:10:10 $
+Version:   $Revision: 1.2 $
+
+=========================================================================auto=*/
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "vtkObjectFactory.h"
+#include "vtkCallbackCommand.h"
+
+#include "vtkMRMLVectorVolumeDisplayNode.h"
+#include "vtkMRMLScene.h"
+
+
+//------------------------------------------------------------------------------
+vtkMRMLVectorVolumeDisplayNode* vtkMRMLVectorVolumeDisplayNode::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLVectorVolumeDisplayNode");
+  if(ret)
+    {
+    return (vtkMRMLVectorVolumeDisplayNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLVectorVolumeDisplayNode;
+}
+
+//----------------------------------------------------------------------------
+
+vtkMRMLNode* vtkMRMLVectorVolumeDisplayNode::CreateNodeInstance()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLVectorVolumeDisplayNode");
+  if(ret)
+    {
+    return (vtkMRMLVectorVolumeDisplayNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLVectorVolumeDisplayNode;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLVectorVolumeDisplayNode::vtkMRMLVectorVolumeDisplayNode()
+{
+ this->VisualizationMode = this->visModeScalar;
+ this->ScalarMode = this->scalarModeMagnitude;
+ this->GlyphMode = this->glyphModeLines;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLVectorVolumeDisplayNode::~vtkMRMLVectorVolumeDisplayNode()
+{
+
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLVectorVolumeDisplayNode::WriteXML(ostream& of, int nIndent)
+{
+  Superclass::WriteXML(of, nIndent);
+
+  vtkIndent indent(nIndent);
+
+  std::stringstream ss;
+  ss << this->VisualizationMode;
+  of << indent << "visualizationMode=\"" << ss.str() << "\" ";
+
+  ss.clear();
+  ss << this->ScalarMode;
+  of << indent << "scalarMode=\"" << ss.str() << "\" ";
+
+  ss.clear();
+  ss << this->GlyphMode;
+  of << indent << "glyphMode=\"" << ss.str() << "\" ";
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLVectorVolumeDisplayNode::ReadXMLAttributes(const char** atts)
+{
+
+  Superclass::ReadXMLAttributes(atts);
+
+  const char* attName;
+  const char* attValue;
+  while (*atts != NULL) 
+    {
+    attName = *(atts++);
+    attValue = *(atts++);
+    if (!strcmp(attName, "visualizationMode")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> this->VisualizationMode;
+      }
+    else if (!strcmp(attName, "scalarMode"))
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> this->ScalarMode;
+      }
+    else if (!strcmp(attName, "glyphMode")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> this->GlyphMode;
+      }
+    }        
+}
+
+//----------------------------------------------------------------------------
+// Copy the node\"s attributes to this object.
+// Does NOT copy: ID, FilePrefix, Name, VolumeID
+void vtkMRMLVectorVolumeDisplayNode::Copy(vtkMRMLNode *anode)
+{
+  Superclass::Copy(anode);
+  vtkMRMLVectorVolumeDisplayNode *node = (vtkMRMLVectorVolumeDisplayNode *) anode;
+
+  this->SetVisualizationMode(node->VisualizationMode);
+  this->SetScalarMode(node->ScalarMode);
+  this->SetGlyphMode(node->GlyphMode);
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLVectorVolumeDisplayNode::PrintSelf(ostream& os, vtkIndent indent)
+{
+  
+  Superclass::PrintSelf(os,indent);
+
+  os << indent << "Visualization Mode:   " << this->VisualizationMode << "\n";
+  os << indent << "Scalar Mode:   " << this->ScalarMode << "\n";
+  os << indent << "Glyph Mode:    " << this->GlyphMode << "\n";
+}
+
+
+
---
+++ Libs/MRML/vtkMRMLVectorVolumeDisplayNode.h
@@ -0,0 +1,133 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLVectorVolumeDisplayNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:29 $
+  Version:   $Revision: 1.3 $
+
+=========================================================================auto=*/
+// .NAME vtkMRMLVectorVolumeDisplayNode - MRML node for representing a volume (image stack).
+// .SECTION Description
+// Volume nodes describe data sets that can be thought of as stacks of 2D 
+// images that form a 3D volume.  Volume nodes describe where the images 
+// are stored on disk, how to render the data (window and level), and how 
+// to read the files.  This information is extracted from the image 
+// headers (if they exist) at the time the MRML file is generated.  
+// Consequently, MRML files isolate MRML browsers from understanding how 
+// to read the myriad of file formats for medical data. 
+
+#ifndef __vtkMRMLVectorVolumeDisplayNode_h
+#define __vtkMRMLVectorVolumeDisplayNode_h
+
+#include "vtkMRML.h"
+#include "vtkMRMLVolumeDisplayNode.h"
+#include "vtkMRMLStorageNode.h"
+#include "vtkMRMLColorNode.h"
+
+#include "vtkMatrix4x4.h"
+#include "vtkImageData.h"
+
+class vtkImageData;
+
+class VTK_MRML_EXPORT vtkMRMLVectorVolumeDisplayNode : public vtkMRMLVolumeDisplayNode
+{
+  public:
+  static vtkMRMLVectorVolumeDisplayNode *New();
+  vtkTypeMacro(vtkMRMLVectorVolumeDisplayNode,vtkMRMLNode);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+
+  // Description:
+  // Set node attributes
+  virtual void ReadXMLAttributes( const char** atts);
+
+  // Description:
+  // Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+  // Description:
+  // Copy the node's attributes to this object
+  virtual void Copy(vtkMRMLNode *node);
+
+  // Description:
+  // Get node XML tag name (like Volume, Model)
+  virtual const char* GetNodeTagName() {return "DiffusionWeightedVolumeDisplay";};
+
+  //--------------------------------------------------------------------------
+  // Display Information
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Set/Get visualization Mode
+  //BTX
+  enum 
+    {
+    visModeScalar = 0,
+    visModeGlyph = 1,
+    visModeBoth = 2,
+    };
+  //ETX
+
+  vtkGetMacro(VisualizationMode, int);
+  vtkSetMacro(VisualizationMode, int);
+
+  void SetVisualizationModeToScalarVolume() {
+    this->SetVisualizationMode(this->visModeScalar);
+  };  
+  void SetVisualizationModeToGlyphs() {
+    this->SetVisualizationMode(this->visModeGlyph);
+  };  
+  void SetVisualizationModeToBoth() {
+    this->SetVisualizationMode(this->visModeBoth);
+  };  
+
+  //BTX
+  enum
+    {
+    scalarModeMagnitude = 0,
+    };
+  //ETX
+
+  vtkGetMacro(ScalarMode, int);
+  vtkSetMacro(ScalarMode, int);
+ 
+  void SetScalarModeToMagnitude() {
+    this->SetScalarMode(this->scalarModeMagnitude);
+  };
+
+  //BTX
+  enum
+    {
+    glyphModeLines = 1,
+    glyphModeTubes = 2,
+    };
+  //ETX
+  vtkGetMacro(GlyphMode, int);
+  vtkSetMacro(GlyphMode, int);
+  
+  void SetGlyphModeToLines() {
+    this->SetGlyphMode(this->glyphModeLines);
+  };
+  void SetGlyphModeToTubes() {
+    this->SetGlyphMode(this->glyphModeTubes);
+  };
+  
+protected:
+  vtkMRMLVectorVolumeDisplayNode();
+  ~vtkMRMLVectorVolumeDisplayNode();
+  vtkMRMLVectorVolumeDisplayNode(const vtkMRMLVectorVolumeDisplayNode&);
+  void operator=(const vtkMRMLVectorVolumeDisplayNode&);
+
+  int VisualizationMode;
+  int ScalarMode;
+  int GlyphMode;
+};
+
+#endif
+
