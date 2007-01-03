82b26bbda0e14eb7e91ca14769509e3bad107dfa
Raul San Jose <rjosest@bwh.harvard.edu>
2007-01-03 20:38:26 +0000

    ENH: Adding logic to read DWI, Tensor and Vector images in NRRD format using vtkNRRDReader
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=2005
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@2005 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Logic/vtkSlicerVolumesLogic.cxx
+++ Base/Logic/vtkSlicerVolumesLogic.cxx
@@ -21,7 +21,19 @@
 #include "vtkMRMLScalarVolumeNode.h"
 #include "vtkMRMLVectorVolumeNode.h"
 #include "vtkMRMLVolumeArchetypeStorageNode.h"
+
+//#define USE_TEEM 1
+
+#ifdef USE_TEEM
+  #include "vtkMRMLNRRDStorageNode.h"
+#endif
+
+#include "vtkMRMLDiffusionTensorVolumeNode.h"
+#include "vtkMRMLDiffusionWeightedVolumeNode.h"
 #include "vtkMRMLVolumeDisplayNode.h"
+#include "vtkMRMLVectorVolumeDisplayNode.h"
+#include "vtkMRMLDiffusionTensorVolumeDisplayNode.h"
+#include "vtkMRMLDiffusionWeightedVolumeDisplayNode.h"
 
 vtkCxxRevisionMacro(vtkSlicerVolumesLogic, "$Revision: 1.9.12.1 $");
 vtkStandardNewMacro(vtkSlicerVolumesLogic);
@@ -56,10 +68,135 @@ void vtkSlicerVolumesLogic::SetActiveVolumeNode(vtkMRMLVolumeNode *activeNode)
   vtkSetMRMLNodeMacro(this->ActiveVolumeNode, activeNode );
   this->Modified();
 }
+#ifdef USE_TEEM
+//----------------------------------------------------------------------------
+vtkMRMLVolumeNode* vtkSlicerVolumesLogic::AddArchetypeVolume (char* filename, int centerImage, int labelMap, const char* volname)
+{
+  cout<<"AddAchretypeVolume Intro"<<endl;
+  vtkMRMLVolumeNode *volumeNode = NULL;
+  vtkMRMLVolumeDisplayNode *displayNode = NULL;
+
+  vtkMRMLScalarVolumeNode *scalarNode = vtkMRMLScalarVolumeNode::New();
+  vtkMRMLVectorVolumeNode *vectorNode = vtkMRMLVectorVolumeNode::New();
+  vtkMRMLDiffusionTensorVolumeNode *tensorNode = vtkMRMLDiffusionTensorVolumeNode::New();
+  vtkMRMLDiffusionWeightedVolumeNode *dwiNode = vtkMRMLDiffusionWeightedVolumeNode::New();
+
+  // Instanciation of the two I/O mechanism
+  vtkMRMLNRRDStorageNode *storageNode1 = vtkMRMLNRRDStorageNode::New();
+  vtkMRMLVolumeArchetypeStorageNode *storageNode2 = vtkMRMLVolumeArchetypeStorageNode::New();
+  vtkMRMLStorageNode *storageNode = NULL;
+  
+  storageNode1->SetFileName(filename);
+  storageNode1->SetCenterImage(centerImage);
+  storageNode2->SetFileName(filename);
+  storageNode2->SetCenterImage(centerImage);
+  
+  // Try to read first with NRRD reader (look if file is a dwi or a tensor)
+  cout<<"TEST DWI: "<< storageNode1->ReadData(dwiNode)<<endl;
+
+  if (storageNode1->ReadData(dwiNode))
+    {
+    cout<<"DWI HAS BEEN READ"<<endl;
+    displayNode = vtkMRMLDiffusionWeightedVolumeDisplayNode::New();
+    // Give a chance to set/update displayNode
+    volumeNode =  dwiNode;
+    storageNode = storageNode1;
+    cout<<"Done setting volumeNode to class: "<<volumeNode->GetClassName()<<endl;
+    }
+  else if (storageNode1->ReadData(tensorNode))
+    {
+    cout<<"Tensor HAS BEEN READ"<<endl;
+    displayNode = vtkMRMLDiffusionTensorVolumeDisplayNode::New();
+    volumeNode = tensorNode;
+    storageNode = storageNode1;
+    }
+  else if (storageNode1->ReadData(vectorNode))
+    {
+    cout<<"Vector HAS BEEN READ WITH NRRD READER"<<endl;
+    displayNode = vtkMRMLVectorVolumeDisplayNode::New();
+    volumeNode = vectorNode;
+    storageNode = storageNode1;
+    }
+  else if (storageNode2->ReadData(scalarNode))
+    {
+    cout<<"Scalar HAS BEEN READ WITH ARCHTYPE READER"<<endl;
+    displayNode = vtkMRMLVolumeDisplayNode::New();
+    scalarNode->SetLabelMap(labelMap);
+    volumeNode = scalarNode;
+    storageNode = storageNode2;
+    }
+  else if (storageNode2->ReadData(vectorNode))
+    {
+    cout<<"Vector HAS BEEN READ WITH ARCHTYPE READER"<<endl;
+    displayNode = vtkMRMLVectorVolumeDisplayNode::New();
+    volumeNode = vectorNode;
+    storageNode = storageNode2;
+    }
+  
+  if (volumeNode != NULL)
+    {
+    if (volname == NULL)
+      {
+      const vtksys_stl::string fname(filename);
+      vtksys_stl::string name = vtksys::SystemTools::GetFilenameName(fname);
+      volumeNode->SetName(name.c_str());
+      }
+    else
+      {
+      volumeNode->SetName(volname);
+      }
+
+    this->GetMRMLScene()->SaveStateForUndo();
+    cout<<"Setting scene info"<<endl;
+    volumeNode->SetScene(this->GetMRMLScene());
+    storageNode->SetScene(this->GetMRMLScene());
+    displayNode->SetScene(this->GetMRMLScene());
+  
+    //should we give the user the chance to modify this?.
+    double range[2];
+    cout<<"Set basic display info"<<endl;
+    volumeNode->GetImageData()->GetScalarRange(range);
+    displayNode->SetLowerThreshold(range[0]);
+    displayNode->SetUpperThreshold(range[1]);
+    displayNode->SetWindow(range[1] - range[0]);
+    displayNode->SetLevel(0.5 * (range[1] + range[0]) );
+
+    cout<<"Adding node.."<<endl;
+    this->GetMRMLScene()->AddNode(storageNode);  
+    this->GetMRMLScene()->AddNode(displayNode);  
+    displayNode->SetDefaultColorMap();
+    volumeNode->SetStorageNodeID(storageNode->GetID());
+    volumeNode->SetAndObserveDisplayNodeID(displayNode->GetID());
+
+    cout<<"Name vol node "<<volumeNode->GetClassName()<<endl;
+    cout<<"Display node "<<displayNode->GetClassName()<<endl;
+    this->GetMRMLScene()->AddNode(volumeNode);
+    cout<<"Node added to scene"<<endl;
+
+    this->SetActiveVolumeNode(volumeNode);
+
+    this->Modified();
+    }
+
+  scalarNode->Delete();
+  vectorNode->Delete();
+  dwiNode->Delete();
+  tensorNode->Delete();
+  storageNode1->Delete();
+  storageNode2->Delete();
+  if (displayNode)
+    {
+    displayNode->Delete();
+    }
+  return volumeNode;
+}
+
+#else
 
 //----------------------------------------------------------------------------
 vtkMRMLVolumeNode* vtkSlicerVolumesLogic::AddArchetypeVolume (char* filename, int centerImage, int labelMap, const char* volname)
 {
+  cout<<"NO NRRD AddArchetypeVolume"<<endl;
   vtkMRMLVolumeNode *volumeNode = NULL;
   
   vtkMRMLScalarVolumeNode *scalarNode = vtkMRMLScalarVolumeNode::New();
@@ -70,18 +207,15 @@ vtkMRMLVolumeNode* vtkSlicerVolumesLogic::AddArchetypeVolume (char* filename, in
 
   storageNode->SetFileName(filename);
   storageNode->SetCenterImage(centerImage);
-  if (storageNode->ReadData(scalarNode) == 0)
-    {
-    // cannot read scalar data, try vector
-    if (storageNode->ReadData(vectorNode) != 0)
-      {
-      volumeNode = vectorNode;
-      }
-    }
-  else
+  if (storageNode->ReadData(scalarNode))
     {
     scalarNode->SetLabelMap(labelMap);
     volumeNode = scalarNode;
+    }
+  else if (storageNode->ReadData(vectorNode))
+    {
+    // cannot read scalar data, try vector
+    volumeNode = vectorNode;
     }
   
   if (volumeNode != NULL)
@@ -131,6 +265,8 @@ vtkMRMLVolumeNode* vtkSlicerVolumesLogic::AddArchetypeVolume (char* filename, in
   return volumeNode;
 }
 
+
+#endif
 
 //----------------------------------------------------------------------------
 int vtkSlicerVolumesLogic::SaveArchetypeVolume (char* filename, vtkMRMLVolumeNode *volumeNode)
