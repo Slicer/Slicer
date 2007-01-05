bd09d40a86ea7b1e9216fab6289299daa6276e50
Bill Lorensen <bill.lorensen@gmail.com>
2007-01-05 21:40:27 +0000

    ENH: Logos for everyone.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=2056
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@2056 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/CLI/Resources/BillsLogo.h
@@ -0,0 +1,56 @@
+/* 
+ * Resource generated for file:
+ *    BillsLogo.png (zlib, base64) (image file)
+ */
+static const unsigned int  image_BillsLogo_width          = 40;
+static const unsigned int  image_BillsLogo_height         = 40;
+static const unsigned int  image_BillsLogo_pixel_size     = 3;
+static const unsigned long image_BillsLogo_length         = 3080;
+static const unsigned long image_BillsLogo_decoded_length = 4800;
+
+static const unsigned char image_BillsLogo[] = 
+  "eNrtmPlv2+YZx/+RYSgwYMC6Nk2XOE58KbZly5dsS9Zh3RIpUbxJ8RBFkRQP3dZt67Blx3"
+  "KaOIfj1E6adl2bblj3j40Z9kuBrVDaoj8VeEBABKnP+zzv+zzf5yHFF6lf3Ui+QP3G/Y8x"
+  "vEFzGslqBKuT3LjLS3H/NYrP02mDeve68V7copjVcCzudsxN3r73l4nl+7awO6zQmYd7nZ"
+  "fd41f9s+v+6LJz9Hn35PP+yfXh6HL/8LLdf9LYP6vUhkaxl9P2BFGnKDiOjc81l7qf09vZ"
+  "TJ5Nd7X8abHQ142hUXhWb73aH3x7cvb96NG/Ro/fDk/+MTz9u2mnD78eDK6b+08q9ZGuH8"
+  "pKP6scSLn9rJyG34PLvuNqJ8XKy/3eVbf7stO9qDcfFsvPm62Xrc6b7uGbTu/LTve1ee0N"
+  "vjzofXEwuKy3nleb56XaWb7QFeU6x7dFscKlxSRMMPqYXDn7LlBHRvGiuf+83n5a33tYKD"
+  "4qV180W1ed7le9w792j77uDb9om9yjL3v91/u9l432Vat73e5f1FtDxSiQlIbjGkGbXIyU"
+  "x+QacqGf0/qKdqSpAzU3VFRz/Y20eGyUHpfrV53B26PR92ePvxuefjscfX148u3xg6/6R6"
+  "9Nr2vNU72cxwnI4922LlMRkIsBGJYZk6uLasfcHUkp00yVYTMgGNlygW4fHoowQNIguaN8"
+  "/ao/fNM//u7BZ98cPXh7Mvq81TlU1QLFoL7g2v25uckpm2VxbcGa8PiTEE3xpbH8FXOdrF"
+  "yiSR4AYw6Hc92+tby6almYnrj18Z8+nL49tbq4QcbQtlK4PDi6anffDA6bLI3s+t2rG/NT"
+  "s5srdjwC4OEotLvr3dgEAIL6Iej/cYtZtcqnM3EQ8/mxMCDAZJ5JFzkhh2KQ221fsC5Ozq"
+  "zNL5MA2pKV0V7zrFgSAGB7Ycm+sBhxuohoLJNE2XgC9fvdttVEghkzzlXZKDGMBMPxHQ8R"
+  "Abh4XIRhAUrEnTvLU/csd+7apqamb960TE7jEbCtFutCen16etMya/n0xkd/+OCD3/3+4w"
+  "8/ufnRp3MTExtWK4ILY3JbWrmvyGb+CmDMtbJkvTvx0R//PHvz5ic3JiZu3HQtzgft26HN"
+  "jbu370Z3w8eVWo1liiTsnp/dti4t3blxa2IxHkEdVhvgdqPhMEkrY3IPCnvn1fplt/O8XT"
+  "uQZA2FEV9EQQgFo3MoKsfjBkGlgZDl9qR3Y6fA8jkYLpKoBkN0OIx6fZAvzERheNcvo0kJ"
+  "w6ixuR29dFFvv9xvPatVhpp6rBoNUWrLuX1FafFCjaZLOO5Zslrv3HGtrCsYUSHp0PqKgc"
+  "JsKEYFI6kokIrG0hAkIckcQaJJekxuQ9Ie6PpZqXxeLj8pV57Xmue1+rNG+0Wj/biQP1YV"
+  "BYz6VpdXpqYcCzYyHFNhdOu+Jbmzk0eRHIobVEonUzkMzSGIhpFwLDkmt5IWm1zmRCuaWf"
+  "mq1XxaqDwqVB4XymbtPZblA55Nh/z+1ZUNy6xtdh5weqRE0r0471leZsOhXCKRJ4giQRZx"
+  "spHi6yyPBGNj5m+e4fcodiCqL2qtv/UGX7R6rxrtJ/nSkZQbZIQGk5JBAHE5N2anLHemMF"
+  "9IgWAJSMC73jKFVyiTiFdwssNkWmymRnNIIDomVybZIkF1M9LjYvV1q/fVweC6cXC5987x"
+  "h0bhotYaadnohm3+9i3rvTksAHQy+sgoZWPAiZr/rFA6lMSekG2QXD3F6wiaeOfvWHFOk5"
+  "xKEGWG68vao1Ltaal2UW1cNTpv2l1TDt4eHvdS2Or0jHXy7trcIhaKn5Vb152eBkF6MtkX"
+  "pRNVO87pB2nJDJoAgEAAIMfUQSIto5hhntsU21P0k1zhWaX5qj34bnj2z5PRs1Kxy6V25p"
+  "f9KzbLp7dscwtpmK6m+K25+X1eaJihZvhDRe8Kch4jyUAoGobG1X06m4VRBcVVnCyx75To"
+  "Ubl+3Rl8c/jgRbV6XsgPBA5cX7fPWhyLa86ldd+a3ayHSY+/I4hDRSqQRJnl62lRQTDIuw"
+  "uA5Lh9DqNKCCYlEZObw/BKijO5V/v9i9reebk40pUyjlGBYHR7J+EOoP4oGoiKMEGGojKK"
+  "l1JMM5Mp05RB0mkwEdp0oGPrL8UbAowKECzCiIJhxRR7Wq6e79XOq5WzfKkppDUMF+JQKh"
+  "LPJPEsQogQvidqMkboJJ3DCZ1mKhyn4hgZjLjXNt+vn0xiMopyAJBOQGbQhnnts1LxtKB3"
+  "c1otIxoMY9BMKcU1RXmgaIdZeSDl+GiUjYFkJEIDcQnFFQSOOlx+T/C9uFiSYqOxVCxGRG"
+  "JFlu+pcjcntbJiXcwaJJH0uiC3C9zaBracmNtHe4OsLxjfdrqXbMG11ZjbzcNEKhwJbTnA"
+  "eOr9uLQMuDxYIECFI+YuNzJCX1fbUjqHJchQAA2Y2xpgwaSGUzqKi/FkBoTZKIjs7mpIUs"
+  "WSGZSCvL6g00P9r977R+eFwu6OL7C5bcqKCKMFhjspFwaabKLbPKNjiIpiEoxoOCFBcI6g"
+  "FYJQEESCIBVFdBKlATBgOgvgP2FOSWCCdWYuuOWgYzEZI9s5sS1lGqLQloRG2jy0Qo3naz"
+  "zbEMybUonl9tK8RmBZNGkqUdzt9jnd5qzxE7hmkdmyuxZnZs2A48GIRpFllqny3F7G/DvM"
+  "oFAhAZppvmfWCkE0u1YOBNk4QJul2+/3b2xCMP+T5zIYF2cm7m7ML4EuLxmJmiJeMluLFJ"
+  "mFkxwQM0VWwVIDWe9KkllbzMyKezygx+1YWgoF4j9zHnRvexbuTS3PTDtXVhJeH5tI8ImE"
+  "AEEsCKahZE3URqXqabE0MAolho17PauWuS27++fPoQmEW7danbaV5dm5hal7jpWVoMMJh6"
+  "JsAuIhOM8KzazaVY1mVjZXYj5mnZrFaPUXmX+9O7v2xUX36vra/XnLncnpiYn70zPbtjX/"
+  "piPs8uKRGBIK++xbS3Oz1unZaBT5Bedu0wWzLgZ8Mdemc2PJZl+2OVfXPRt2sy13ra05Vt"
+  "d27Ju7u1Gz7yN/KPG/4LxvnnOC0RA8i+EZDBNwSjJ//vZ948e5hBm0X9/ShX8D8tWR2w==";
+
