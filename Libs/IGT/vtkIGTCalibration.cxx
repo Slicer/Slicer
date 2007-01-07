895f3df1dfcbd2f01f5ad8c201c65bbcd8927828
Haiying Liu <hliu@bwh.harvard.edu>
2007-01-07 00:39:32 +0000

    ENH: addig directory for IGT library
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=2079
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@2079 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/IGT/vtkIGTCalibration.cxx
@@ -0,0 +1,19 @@
+#include "vtkIGTCalibration.h"
+
+#include "vtkObjectFactory.h"
+
+vtkIGTCalibration* vtkIGTCalibration::New()
+{
+  vtkObject* ret=vtkObjectFactory::CreateInstance("vtkIGTCalibration");
+  if(ret)
+    {
+      return (vtkIGTCalibration*) ret;
+    }
+  return new vtkIGTCalibration;
+
+}
+
+vtkIGTCalibration::vtkIGTCalibration()
+{
+}
+
---
+++ Libs/IGT/vtkIGTTracker.cxx
@@ -0,0 +1,19 @@
+#include "vtkIGTTracker.h"
+
+#include "vtkObjectFactory.h"
+
+vtkIGTTracker* vtkIGTTracker::New()
+{
+  vtkObject* ret=vtkObjectFactory::CreateInstance("vtkIGTTracker");
+  if(ret)
+    {
+      return (vtkIGTTracker*) ret;
+    }
+  return new vtkIGTTracker;
+
+}
+
+vtkIGTTracker::vtkIGTTracker()
+{
+}
+
