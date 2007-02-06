25419f9ea4292e8f39d11c63d98c7eae26ef1dbc
Steve Pieper <pieper@bwh.harvard.edu>
2007-02-06 19:04:10 +0000

    ENH: timing test to compare slice rendering
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=2462
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@2462 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Logic/Testing/TestImageSlice.tcl
+++ Base/Logic/Testing/TestImageSlice.tcl
@@ -1,21 +1,47 @@
+# run the timing test with:
 
-set ellip [vtkImageEllipsoidSource New]
-set trans [vtkTransform New]
-set viewer [vtkImageViewer New]
+# ./bin/Slicer3-real -e "source ../Slicer3/Base/Logic/Testing/TestImageSlice.tcl ,. slicePerfTest ../Slicer3/Libs/MRML/Testing/TestData/fixed.nrrd"
 
-if { 0 } {
-  set slice [vtkImageReslice New]
-  #$slice SetSliceTransform $trans
-} else {
-  set slice [vtkImageSlice New]
-  $slice SetOutputDimensions 512 512 1
-  $slice SetSliceTransform $trans
+proc slicePerfTest { fileName } {
+
+
+  set volumesLogic [$::slicer3::VolumesGUI GetLogic]
+  set volumeNode [$volumesLogic AddArchetypeVolume $fileName 1 0 brain]
+
+  set selectionNode [$::slicer3::ApplicationLogic GetSelectionNode]
+  $selectionNode SetReferenceActiveVolumeID [$volumeNode GetID]
+  $::slicer3::ApplicationLogic PropagateVolumeSelection
+
+  update idletasks
+
+  timeTest 10
+  update idletasks
+  timeTest 10
+
+  exit
+
 }
 
-$slice SetInput [$ellip GetOutput]
-$viewer SetInput [$slice GetOutput]
 
-$viewer Render
+proc sliceTest {} {
+  set ellip [vtkImageEllipsoidSource New]
+  set trans [vtkTransform New]
+  set viewer [vtkImageViewer New]
+
+  if { 0 } {
+    set slice [vtkImageReslice New]
+    #$slice SetSliceTransform $trans
+  } else {
+    set slice [vtkImageSlice New]
+    $slice SetOutputDimensions 512 512 1
+    $slice SetSliceTransform $trans
+  }
+
+  $slice SetInput [$ellip GetOutput]
+  $viewer SetInput [$slice GetOutput]
+
+  $viewer Render
+}
 
 
 proc toggleSlice {} {
