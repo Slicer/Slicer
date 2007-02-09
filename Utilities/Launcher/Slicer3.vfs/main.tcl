5dae06fbad13f428b7dfd512feb6188c7220d347
Steve Pieper <pieper@bwh.harvard.edu>
2007-02-09 16:44:44 +0000

    ENH: new launcher mechanism based on tclkit (same as slicer2) to provide fast feedback to users during launch process
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=2523
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@2523 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Utilities/Launcher/Slicer3.vfs/main.tcl
@@ -0,0 +1,30 @@
+
+#
+# slicer3.vfs/main.tcl
+#
+# This is the first entry point for slicer3.  This is run from a "starpack"
+# which is STand Alone Runtime version of tcl.  (see www.equi4.com)
+#
+# The purpose is the this script to determine where the executable is located 
+# and set the environment variable SLICER_HOME accordingly; all other slicer
+# files should be found relative to that dir.
+#
+# We then source the launch.tcl script from the lib directory of top level of SLICER_HOME 
+# which does the rest from there.
+#
+
+package require Tk
+wm withdraw .
+
+#
+# follow links to find "real" install dir
+#
+set cmdname [file dir [info script]]
+while { [file type $cmdname] == "link" } {
+    set cmdname [file readlink $cmdname]
+}
+
+set env(SLICER_HOME) [file dir $cmdname]
+
+source $env(SLICER_HOME)/lib/launch.tcl
+
---
+++ Utilities/Launcher/make-launchers.tcl
@@ -0,0 +1,43 @@
+#!/bin/sh
+# \
+exec tclkitsh "$0" ${1+"$@"}
+
+#
+# make-launchers.tcl
+#
+
+set __comment {
+
+    This script creates the stand-alone executables
+    (starpacks) to startup slicer from an arbitrary directory.
+
+    See www.equi4.com for info about starkits and starpacks.
+
+    Note:
+    * you'll need to have tclkitsh in your path somewhere
+    * the tclkitsh you use to generate the launcher cannot be the same one as the 
+      -runtime argument (cannot be circular).
+    * this can be run on any platform to generate launchers for all the platforms
+    * the 32 bit linux launcher can be used for both 32 and 64 bit installs
+      (probably the same for win32 and win64)
+    * solaris has been dropped from this list for now, but presumably solaris,
+      irix, aix, and other 'old' systems can be added if needed.
+}
+
+cd [file dirname [info script]]
+
+puts "making linux..."
+exec tclkitsh tclkits/sdx.kit wrap slicer3 -runtime tclkits/tclkit-linux-x86
+file rename -force slicer3 slicer3launchers/slicer3-linux-x86
+
+puts "making win32..."
+exec tclkitsh tclkits/sdx.kit wrap slicer3 -runtime tclkits/tclkit-win32.exe
+file rename -force slicer3 slicer3launchers/slicer3-win32.exe
+
+puts "making darwin-ppc..."
+exec tclkitsh tclkits/sdx.kit wrap slicer3 -runtime tclkits/tclkit-darwin-ppc
+file rename -force slicer3 slicer3launchers/slicer3-darwin-ppc
+
+puts "making darwin-x86..."
+exec tclkitsh tclkits/sdx.kit wrap slicer3 -runtime tclkits/tclkit-darwin-x86
+file rename -force slicer3 slicer3launchers/slicer3-darwin-x86
