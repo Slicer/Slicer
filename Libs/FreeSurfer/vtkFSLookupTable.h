4c4c647660d2e238c5bd9e65e140b99087981aec
Nicole Aucoin <nicole@bwh.harvard.edu>
2007-02-28 22:14:32 +0000

    STYLE: updated documentation
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=2704
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@2704 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/FreeSurfer/vtkFSLookupTable.h
+++ Libs/FreeSurfer/vtkFSLookupTable.h
@@ -11,10 +11,10 @@
   Version:   $Revision: 1.4 $
 
 =========================================================================auto=*/
-/// .NAME vtkFSLookupTable - A look up table for FreeSurfer colour scales.
-/// .SECTION Description
-///
-///
+// .NAME vtkFSLookupTable - A look up table for FreeSurfer colour scales.
+// .SECTION Description
+// Reimplements vtkLookupTable to provide custom mapping of scalars to colours.
+//
 
 #ifndef __vtkFSLookupTable_h
 #define __vtkFSLookupTable_h
@@ -38,7 +38,12 @@ public:
     vtkSetMacro(HiThresh,float);
 
     vtkGetMacro(LutType,int);
+    // Description:
+    // Map from integer type to a string description
     const char *GetLutTypeString();
+    // Description:
+    // reset all the values in case one was changed, so the next map of
+    // scalars to colours will use the new values
     void SetLutTypeToLabels();
     void SetLutTypeToHeat();
     void SetLutTypeToBlueRed();
@@ -66,17 +71,30 @@ public:
     vtkGetMacro(FMid,float);
     vtkSetMacro(FMid,float);
 
+    // Description:
     // from vtkScalarsToColors
     double *GetRange();
+    // Description:
+    // don't do anything as it's overriding the LUTs low threshold with the
+    // scalar values' lowest value
     void SetRange(double, double);
-    unsigned char *MapValue(double);
+    // Description:
+    // Given a scalar value val, return an rgba color value
+    // returns array of length 3, 0-255
+    unsigned char *MapValue(double val);
     //BTX - begin tcl exclude
+    // Description:
+    // passes val to MapValue
     void GetColor(double, double[3]);
     //ETX - end tcl exclude
-    void MapScalarsThroughTable2(void*, unsigned char*, int, int, int, int);
+    // Description:
+    // take input scalars and push them through the calculation to get colours
+    // to put int the output array
+    void MapScalarsThroughTable2(void* input, unsigned char* outupt, int inputDataType, int numberOfValues, int inputIncrement, int outputIncrement);
 
     //BTX
-    /// Type constant, can have different types of colour scales
+    // Description:
+    // Type constant, can have different types of colour scales
     enum
     {
       FSLUTLABELS = 0,
@@ -91,42 +109,51 @@ protected:
     vtkFSLookupTable();
     ~vtkFSLookupTable();
 
-    /// Low cut off, values passed in that are below this level are mapped to
-    /// low cut off colour
-    /// FS cmin and fthresh
+    // Description:
+    // Low cut off, values passed in that are below this level are mapped to
+    // low cut off colour
+    // FS cmin and fthresh
     float LowThresh;
-    /// High cut off, values passed in that are above this value are mapped to
-    /// high cut off colour
-    /// FS cmax
+    // Description:
+    // High cut off, values passed in that are above this value are mapped to
+    // high cut off colour
+    // FS cmax
     float HiThresh;
 
-
-    /// which type of colour scale this is supposed to model, from defined
-    /// constants above
+    // Description:
+    // which type of colour scale this is supposed to model, from defined
+    // constants above
     int LutType;
 
-    /// is the colour scale reversed?
-    /// from FS invphaseflag
+    // Description:
+    // is the colour scale reversed?
+    // from FS invphaseflag
     int Reverse;
 
-    /// truncate the values below zero?
-    /// from FS truncphaseflag
+    // Description:
+    // truncate the values below zero?
+    // from FS truncphaseflag
     int Truncate;
-    
-    /// offset value added to the input value - a brightening factor
+
+    // Description:
+    // offset value added to the input value - a brightening factor
     float Offset;
 
-    /// slope of the function that maps from input values to colours
+    // Description:
+    // slope of the function that maps from input values to colours
     float Slope;
 
-    /// blue factor?
+    // Description:
+    // blue factor?
     float Blufact;
 
-    /// Midpoint of the function that maps from input values to colours
-    /// FS fmid and cmid
+    // Description:
+    // Midpoint of the function that maps from input values to colours
+    // FS fmid and cmid
     float FMid;
 
-    /// output of colour computation
+    // Description:
+    // output of colour computation
     unsigned char RGBA[4];
 };
 
