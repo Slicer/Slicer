cdf3be43c4afb5409e46ae0cc57e0e66ec65f01f
Steve Pieper <pieper@bwh.harvard.edu>
2007-03-03 21:19:50 +0000

    ENH: added quick model generation code for adding a model of the current label map to the scene.  No gui hook for this yet.  Can be tried by typing EditorTestQuickModel in the tcl console
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=2731
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@2731 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Editor/EditorLogic.tcl
+++ Modules/Editor/EditorLogic.tcl
@@ -1,3 +1,68 @@
 #
 # Editor logic procs
 #
+
+
+#
+# make a model of the current label map for the given slice logic
+#
+proc EditorAddQuickModel { sliceLogic } {
+
+  #
+  # get the image data for the label layer
+  #
+  set layerLogic [$sliceLogic GetLabelLayer]
+  set volumeNode [$layerLogic GetVolumeNode]
+  if { $volumeNode == "" } {
+    return
+  }
+  set imageData [$volumeNode GetImageData]
+
+  puts [$imageData Print]
+
+  #
+  # make a poly data in RAS space
+  #
+  set dsm [vtkDiscreteMarchingCubes New]
+  $dsm SetInput $imageData
+  set tpdf [vtkTransformPolyDataFilter New]
+  set ijkToRAS [vtkTransform New]
+  $volumeNode GetRASToIJKMatrix [$ijkToRAS GetMatrix] 
+  $ijkToRAS Inverse
+  $tpdf SetInput [$dsm GetOutput]
+  $tpdf SetTransform $ijkToRAS
+  $tpdf Update
+
+  #
+  # create a mrml model for the new data
+  #
+  set modelNode [vtkMRMLModelNode New]
+  set modelDisplayNode [vtkMRMLModelDisplayNode New]
+  $modelNode SetName "QuickModel"
+  $modelNode SetScene $::slicer3::MRMLScene
+  $modelDisplayNode SetScene $::slicer3::MRMLScene
+  $::slicer3::MRMLScene AddNode $modelDisplayNode
+  $modelNode SetAndObserveDisplayNodeID [$modelDisplayNode GetID]
+  $::slicer3::MRMLScene AddNode $modelNode
+  $modelNode SetAndObservePolyData [$tpdf GetOutput]
+
+  puts [[$tpdf GetOutput] Print]
+
+  #
+  # clean up
+  #
+  $dsm Delete
+  $ijkToRAS Delete
+  $tpdf Delete
+  $modelNode Delete
+  $modelDisplayNode Delete
+
+}
+
+#
+# make it easier to test the model by looking for the first slice logic
+#
+proc EditorTestQuickModel {} {
+  set sliceLogic [lindex [vtkSlicerSliceLogic ListInstances] 0]
+  EditorAddQuickModel $sliceLogic
+}
