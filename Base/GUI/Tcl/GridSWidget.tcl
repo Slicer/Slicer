cf3334de3ff5a28e31924df284a5860c1522db5b
Steve Pieper <pieper@bwh.harvard.edu>
2007-03-09 19:50:36 +0000

    BUG: avoid one error condition that results in a warning about missing image data
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=2890
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@2890 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Tcl/GridSWidget.tcl
+++ Base/GUI/Tcl/GridSWidget.tcl
@@ -186,6 +186,8 @@ itcl::body GridSWidget::updateGrid { } {
   #
   # check the size cutoff
   # - map a single pixel from IJK to XY and check the size
+  # - if the grid won't be visible, don't show it.
+  # - also don't so if image data doesn't exist
   #
   set ijkToXY [vtkMatrix4x4 New]
   $ijkToXY DeepCopy [[$_layers($layer,logic) GetXYToIJKTransform] GetMatrix]
@@ -194,7 +196,7 @@ itcl::body GridSWidget::updateGrid { } {
   $ijkToXY SetElement 2 3  0
   $ijkToXY Invert
   foreach {x y z w} [$ijkToXY MultiplyPoint 1 1 1 0] {}
-  if { $x < $cutoff && $y < $cutoff } {
+  if { $x < $cutoff && $y < $cutoff || $_layers($layer,image) == "" } {
     $o(gridActor) SetVisibility 0
     $ijkToXY Delete
     return
