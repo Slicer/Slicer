75ea4e830e97698f5414d2f142c54c0ab93c7f9c
Steve Pieper <pieper@bwh.harvard.edu>
2007-03-09 15:20:20 +0000

    BUG: add space directions to test volumes
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=2875
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@2875 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Editor/Testing/TestVolume.tcl
+++ Modules/Editor/Testing/TestVolume.tcl
@@ -1,14 +1,16 @@
-
+# source ../slicer3/Modules/Editor/Testing/TestVolume.tcl; EditorMakeTestVolume c:/tmp/test.nrrd
 proc EditorMakeTestVolume {filename} {
 
   set fp [open $filename "w"]
 
-  puts $fp "NRRD0001"
+  puts $fp "NRRD0004"
   puts $fp "content: testvolume"
   puts $fp "type: double"
   puts $fp "dimension: 3"
-  puts $fp "sizes: 16 16 16"
+  puts $fp "sizes: 32 32 32"
   puts $fp "centers: cell cell cell"
+  puts $fp "space: RAS"
+  puts $fp "space directions: (1,0,0) (0,1,0) (0,0,1)"
   puts $fp "encoding: ascii"
   puts $fp ""
 
