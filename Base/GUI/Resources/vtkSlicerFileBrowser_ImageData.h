650b9f331206b02b4aa6768bd3e077a88258e8f4
Wendy Plesniak <wjp@bwh.harvard.edu>
2007-04-01 14:08:36 +0000

    ENH: Resource for slicer-style file browser, not currently used
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=3102
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@3102 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/Resources/vtkSlicerFileBrowser_ImageData.h
@@ -0,0 +1,89 @@
+/* 
+ * Resource generated for file:
+ *    FileBrowserCreateNew.png (zlib, base64) (image file)
+ */
+static const unsigned int  image_FileBrowserCreateNew_width          = 17;
+static const unsigned int  image_FileBrowserCreateNew_height         = 17;
+static const unsigned int  image_FileBrowserCreateNew_pixel_size     = 3;
+static const unsigned long image_FileBrowserCreateNew_length         = 164;
+static const unsigned long image_FileBrowserCreateNew_decoded_length = 867;
+
+static const unsigned char image_FileBrowserCreateNew[] = 
+  "eNpjYCAH/AeDZ1OjIIzn1sJoIsgAWQtU8fMUBIkNoGmBqGyVZ4Dowq8F6AagMrh6CAlEmG"
+  "5DswWuEqILIsjExABBaFrg4kAEVI/MfX+1AYIgupC1wKWA5sPZyAiPFqAtpGohwxZcaAhp"
+  "IQZhSWNEALLyCgMASPDAfg==";
+
+/* 
+ * Resource generated for file:
+ *    FileBrowserFavorites.png (zlib, base64) (image file)
+ */
+static const unsigned int  image_FileBrowserFavorites_width          = 17;
+static const unsigned int  image_FileBrowserFavorites_height         = 17;
+static const unsigned int  image_FileBrowserFavorites_pixel_size     = 3;
+static const unsigned long image_FileBrowserFavorites_length         = 276;
+static const unsigned long image_FileBrowserFavorites_decoded_length = 867;
+
+static const unsigned char image_FileBrowserFavorites[] = 
+  "eNpjYCAH/AeDw3dfMuTvZ8jewhA7A0Tm7weK/McAcC0g9anr5AqWX3718+3Xn0ASyAaKYO"
+  "qCa2HI2wNUc/3VbwanXAZNFyAJZIN05e3BqcW9AmgySH3NJRCj5hKQDWK4V+DUoukAdA/Q"
+  "fJCylFUgUtMFLOLAxMQAQWhaIIIM6euQEUTw/dUGCILoQtYCFGRgYoSrV1FTgSvGowWhi4"
+  "kRTT0eLRBdmOrxa8GFhpAWYhCyFkzw79+/79+/f/z48cePH0A2WoIhFQAAiQuomQ==";
+
+/* 
+ * Resource generated for file:
+ *    FileBrowserGoBack.png (zlib, base64) (image file)
+ */
+static const unsigned int  image_FileBrowserGoBack_width          = 17;
+static const unsigned int  image_FileBrowserGoBack_height         = 17;
+static const unsigned int  image_FileBrowserGoBack_pixel_size     = 3;
+static const unsigned long image_FileBrowserGoBack_length         = 120;
+static const unsigned long image_FileBrowserGoBack_decoded_length = 867;
+
+static const unsigned char image_FileBrowserGoBack[] = 
+  "eNpjYCAH/CcF0EJL5aZKkrSUrStTiVUlXgtQvV25PfFaIOp5FwoQqQWunkgtQDVw9cTbgq"
+  "yLeL+oxKmS5Bdku0jSAgSWxVakagECjURNOqQxXFpIBQAcsFRV";
+
+/* 
+ * Resource generated for file:
+ *    FileBrowserGoForward.png (zlib, base64) (image file)
+ */
+static const unsigned int  image_FileBrowserGoForward_width          = 17;
+static const unsigned int  image_FileBrowserGoForward_height         = 17;
+static const unsigned int  image_FileBrowserGoForward_pixel_size     = 3;
+static const unsigned long image_FileBrowserGoForward_length         = 128;
+static const unsigned long image_FileBrowserGoForward_decoded_length = 867;
+
+static const unsigned char image_FileBrowserGoForward[] = 
+  "eNpjYCANCAoKAsn/pACIRsq1VG6qJFWLSqxq2boyUrXYldvj0oVLC+9CAVy68GjBpQu/Fo"
+  "guIJckW9DUE/SLSpwqSSGGaT5+LZbFViTFi0aiJh3SGDFaSAUA0gdUiA==";
+
+/* 
+ * Resource generated for file:
+ *    FileBrowserGoUp.png (zlib, base64) (image file)
+ */
+static const unsigned int  image_FileBrowserGoUp_width          = 17;
+static const unsigned int  image_FileBrowserGoUp_height         = 17;
+static const unsigned int  image_FileBrowserGoUp_pixel_size     = 3;
+static const unsigned long image_FileBrowserGoUp_length         = 236;
+static const unsigned long image_FileBrowserGoUp_decoded_length = 867;
+
+static const unsigned char image_FileBrowserGoUp[] = 
+  "eNpjYCAH/EcFTEzoIsgAUwtQPe9CATy60LRA1EOQmpoqQS3I6iEIq13EOAzIhiA0LXBxqB"
+  "ZmBPf91QYIguhC1gIUfH6uWkSIC6IFrhK/ljeX6yTEeIBaONiZX16oJUYLEEUG6AO1SEjy"
+  "HF6XQaSWjfPi5H2Fgbp6aryI1PLuSv3qmdHeBdpAXce2ZhGjBaLr4cmKVTOiJzb6EqkFF8"
+  "LUQgzCGvsEAVl5hQEAxbGkDg==";
+
+/* 
+ * Resource generated for file:
+ *    FileBrowserHistory.png (zlib, base64) (image file)
+ */
+static const unsigned int  image_FileBrowserHistory_width          = 12;
+static const unsigned int  image_FileBrowserHistory_height         = 17;
+static const unsigned int  image_FileBrowserHistory_pixel_size     = 3;
+static const unsigned long image_FileBrowserHistory_length         = 116;
+static const unsigned long image_FileBrowserHistory_decoded_length = 612;
+
+static const unsigned char image_FileBrowserHistory[] = 
+  "eNpjYCAK/McL4GrksQEFBQWCaoAAWU0aDkCqmpnYwKxZs0hVcwYHIFXNcxyAVDVwIFuoxL"
+  "tQAIKAbKxqwMrkgQpUK9XR4gtTGWacEox3ggAADcZECQ==";
+
