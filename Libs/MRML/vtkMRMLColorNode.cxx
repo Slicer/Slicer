d396df5c3facfd3f1b4ac3ceca627978bd6b78fc
Nicole Aucoin <nicole@bwh.harvard.edu>
2007-04-02 18:14:47 +0000

    BUG: return a declared constant character string rather than name.c_str()
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=3114
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@3114 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/vtkMRMLColorNode.cxx
+++ Libs/MRML/vtkMRMLColorNode.cxx
@@ -284,6 +284,7 @@ const char *vtkMRMLColorNode::GetColorName(int ind)
 const char *vtkMRMLColorNode::GetColorNameWithoutSpaces(int ind, const char *subst)
 {
   std::string name = std::string(this->GetColorName(ind));
+  const char *returnName;
   if (strstr(name.c_str(), " ") != NULL)
     {
     std::string::size_type spaceIndex = name.find( " ", 0 );
@@ -292,13 +293,14 @@ const char *vtkMRMLColorNode::GetColorNameWithoutSpaces(int ind, const char *sub
       name.replace(spaceIndex, 1, subst, 0, strlen(subst));
       spaceIndex = name.find( " ", spaceIndex );
       }
-    return name.c_str();
+    returnName =  name.c_str();
     }
   else
     {
     // no spaces, return it as is
-    return name.c_str();
+    returnName = name.c_str();
     }
+  return returnName;
 }
 
 //---------------------------------------------------------------------------
