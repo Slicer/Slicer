39d82c5d9161db4ec74035f2fbbfb119a943da7d
Steve Pieper <pieper@bwh.harvard.edu>
2007-04-06 21:07:29 +0000

    ENH: improved card handling
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=3171
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@3171 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/QueryAtlas/Tcl/Card.tcl
+++ Modules/QueryAtlas/Tcl/Card.tcl
@@ -24,10 +24,29 @@ namespace eval Card {
   }
 }
 
+proc randRange {min max} {
+  return [expr $min + rand() * ($max-$min)]
+}
+
 proc TestCards {} {
   set renderWidget [[$::slicer3::ApplicationGUI GetViewerWidget] GetMainViewer]
-  set card [Card #auto $renderWidget]
-  $card configure -text "hoot! - I'm a card"
+  set icondir $::env(SLICER_HOME)/../Slicer3/Utilities/Launcher/3DSlicerLogoICOimages
+  set icon $icondir/3DSlicerLogo-DesktopIcon-48x48x256.png
+  for { set i 0 } { $i < 20 } { incr i } {
+    set card [Card #auto $renderWidget]
+    set ras [list [randRange -100 100] [randRange -100 100] [randRange -100 100]]
+    set scale [randRange 1 30]
+    $card configure -text "card $i" -ras $ras -scale $scale -icon $icon -follow 0
+  }
+}
+
+
+namespace eval Card {
+  proc ConfigureAll {args} {
+    foreach c [itcl::find objects -class Card] {
+      eval $c configure $args
+    }
+  }
 }
 
 #########################################################
@@ -65,10 +84,12 @@ if { [itcl::find class Card] == "" } {
     public variable state ""  ;# the interaction state of the Card
     public variable description ""  ;# a status string describing the current state
     public variable renderWidget ""  ;# the vtkKWRenderWidget in which the Card lives
-    public variable iconURI ""  ;# the uri from which to grab the image (should be in png format)
+    public variable icon ""  ;# the uri from which to grab the image (should be in png format)
     public variable text ""  ;# the text to display - can contain line breaks
     public variable follow 1  ;# make this a follower or not
     public variable font "default"  ;# what font to use (default means arial).
+    public variable ras "0 0 0"  ;# what font to use (default means arial).
+    public variable scale "1"  ;# what font to use (default means arial).
 
     variable _vtkObjects ""
     variable _pickState "outside"
@@ -77,8 +98,9 @@ if { [itcl::find class Card] == "" } {
     variable o ;# array of the objects for this widget, for convenient access
     variable _observerTags ;# save so destructor can remove observer
 
-    # parts of the sliceGUI saved for easy access
+    # parts of the renderWidget saved for easy access
     variable _renderer ""
+    variable _camera ""
     variable _interactor ""
     variable _annotation ""
 
@@ -88,6 +110,7 @@ if { [itcl::find class Card] == "" } {
     method removeActors {} {}
     method addActors {} {}
     method updateActors {} {}
+    method positionActors {} {}
     method pick {} {}
     method highlight {} {}
     method place {x y z} {}
@@ -111,19 +134,33 @@ if { [itcl::find class Card] == "" } {
 }
 
 #
-# called on 'widget configure -renderWidget rw'
-# - caches handles to useful objects
+# called on 'widget configure -blah '
 #
 itcl::configbody Card::renderWidget {
   $this removeActors
   #set _renderer [$renderWidget GetRenderer]
   set _renderer [[[$renderWidget GetRenderWindow] GetRenderers] GetItemAsObject 1]
+  set _camera [$_renderer GetActiveCamera]
   set _interactor [$renderWidget GetRenderWindowInteractor]
   set _annotation [$renderWidget GetCornerAnnotation]
   $this addActors
 }
 
-foreach param {iconURI text follow font} {
+itcl::configbody Card::icon {
+  
+  if { ![info exists o(iconImage)] } {
+    set o(iconImage) [vtkNew vtkImageData]
+  }
+  set reader [vtkPNGReader New]
+  $reader SetOutput $o(iconImage)
+  $reader SetFileName $icon
+  $reader Update
+  $reader Delete
+  $this updateActors
+}
+
+# generic configbody for several different parameters
+foreach param {text follow font ras scale} {
   itcl::configbody Card::$param { $this updateActors }
 }
 
@@ -139,8 +176,12 @@ itcl::body Card::constructor {renderWidget} {
   set tag [$renderWidget AddObserver DeleteEvent "::Card::ProtectedDelete $this"]
   lappend _observerTags [list $renderWidget $tag]
 
+    
+  # set tag [$_camera AddObserver ModifiedEvent "::Card::ProtectedCallback $this processEvent $_camera ModifiedEvent"]
+  # lappend _observerTags [list $_camera $tag]
+
   set events {  
-    "KeyPressEvent" "LeftButtonPressEvent"
+    "KeyPressEvent" "LeftButtonPressEvent" "EndInteractionEvent"
     }
   foreach event $events {
     set tag [$_interactor AddObserver $event "::Card::ProtectedCallback $this processEvent $_interactor $event"]    
@@ -174,7 +215,7 @@ itcl::body Card::processEvent { caller event } {
     return
   }
 
-  if { $caller == $_interactor } {
+  if { $caller ==$_interactor } {
     switch $event {
       "KeyPressEvent" {
         switch [$_interactor GetKeySym] {
@@ -184,7 +225,12 @@ itcl::body Card::processEvent { caller event } {
           }
         }
       }
+      "EndInteractionEvent" {
+        $this positionActors
+      }
     }
+  } elseif { $caller == $_camera } {
+    $this positionActors
   }
 }
 
@@ -192,26 +238,42 @@ itcl::body Card::removeActors {} {
   if { [info exists o(actor)] } {
     $_renderer RemoveActor $o(actor)
   }
+  if { [info exists o(iconActor)] } {
+    $_renderer RemoveActor $o(iconActor)
+  }
 }
 
 itcl::body Card::addActors {} {
   
+  #
+  # set up the texture tex
+  #
   set o(tText) [vtkNew vtkTextureText]
-
-  $this updateActors
-
-  if { $follow } {
-    set o(actor) [vtkNew vtkFollower]
-    $o(actor) SetCamera [$_renderer GetActiveCamera]
-  } else {
-    set o(actor) [vtkNew vtkActor]
-  }
+  set o(actor) [vtkNew vtkFollower]
   $o(actor) SetMapper [[$o(tText) GetFollower] GetMapper]
   $o(actor) SetTexture [$o(tText) GetTexture]
   $_renderer AddActor $o(actor)
+  
+  #
+  # set up the icon
+  #
+  set o(iconSource) [vtkNew vtkPlaneSource]
+  set o(iconTexture) [vtkNew vtkTexture]
+  set o(iconMapper) [vtkNew vtkPolyDataMapper]
+  set o(iconActor) [vtkNew vtkFollower]
+  $o(iconMapper) SetInput [$o(iconSource) GetOutput]
+  $o(iconActor) SetMapper $o(iconMapper)
+  $o(iconActor) SetTexture $o(iconTexture)
+  $_renderer AddActor $o(iconActor)
+
+  $this updateActors
 }
 
 itcl::body Card::updateActors {} {
+
+  $this positionActors
+
+  # update text actor
 
   set fontParams [$o(tText) GetFontParameters] 
   if { $font != "default" } {
@@ -224,5 +286,51 @@ itcl::body Card::updateActors {} {
   $fontParams SetStyle 2
   $o(tText) SetText $text
   $o(tText) CreateTextureText
+
+  # update icon actor
+
+  if { [info exists o(iconImage)] } {
+    $o(iconTexture) SetInput $o(iconImage)
+  }
+
 }
 
+itcl::body Card::positionActors {} {
+
+  set viewT [vtkTransform New]
+  $viewT DeepCopy [$_camera GetViewTransformObject]
+  foreach i {0 1 2} {
+    [$viewT GetMatrix] SetElement $i 3  0
+  }
+  $viewT Inverse
+
+  # update text actor
+
+  foreach {r a s} $ras {}
+
+  set delta [$viewT TransformVector $scale 0 0]
+  foreach {dr da ds} $delta {}
+  $o(actor) SetPosition [expr $r + $dr] [expr $a + $da] [expr $s + $ds] 
+  $o(actor) SetScale $scale
+  $o(actor) SetUserTransform $viewT
+
+  if { $follow } {
+    $o(actor) SetCamera $_camera
+  } else {
+    $o(actor) SetCamera ""
+  }
+  
+  # update icon actor
+
+  $o(iconActor) SetPosition $r $a $s
+  $o(iconActor) SetScale $scale
+  $o(iconActor) SetUserTransform $viewT
+
+  if { $follow } {
+    $o(iconActor) SetCamera $_camera
+  } else {
+    $o(iconActor) SetCamera ""
+  }
+
+  $viewT Delete
+}
