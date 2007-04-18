6b098586a86f67705358778ca1efd91d1316f6c5
Nicole Aucoin <nicole@bwh.harvard.edu>
2007-04-18 17:37:46 +0000

    STYLE: explain the overlay arguments a bit more clearly
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=3231
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@3231 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/vtkMRMLModelNode.h
+++ Libs/MRML/vtkMRMLModelNode.h
@@ -152,7 +152,9 @@ public:
   // Take scalar fields and composite them into a new one.
   // New array will have values from the background array where the overlay is
   // +/- if showOverlayPositive/Negative are 0.
-  // overlayMin and Max are used to adjust the color transfer function points
+  // overlayMin and Max are used to adjust the color transfer function points,
+  // both should be positive, as they are mirrored around 0. -Min to Min gives the gap
+  // where the curvature will show through.
   // New array name is backgroundName+overlayName
   // Returns 1 on success, 0 on failure.
   // Based on code from K. Teich, MGH
