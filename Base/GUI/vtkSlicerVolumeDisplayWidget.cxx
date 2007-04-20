15b6efd3c69453fbd141f64ab8fb56d56254773c
Nicole Aucoin <nicole@bwh.harvard.edu>
2007-04-20 21:12:57 +0000

    BUG: invoke modified events rather than calling ProcessMRMLEvents directly
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=3263
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@3263 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/vtkSlicerVolumeDisplayWidget.cxx
+++ Base/GUI/vtkSlicerVolumeDisplayWidget.cxx
@@ -48,19 +48,26 @@ void vtkSlicerVolumeDisplayWidget::PrintSelf ( ostream& os, vtkIndent indent )
     // print widgets?
 }
 
+//---------------------------------------------------------------------------
 void vtkSlicerVolumeDisplayWidget::SetVolumeNode ( vtkMRMLVolumeNode *volumeNode )
-{ 
+{
+  vtkDebugMacro(<< this->GetClassName() << " (" << this << "): setting VolumeNode to " << volumeNode);
   // Select this volume node
-  this->VolumeNode = volumeNode; 
-
+  if (this->VolumeNode != volumeNode)
+    {
+    this->VolumeNode = volumeNode;
+    vtkDebugMacro("SetVolumeNode: calling modified on this, should trigger a process mrml event via the observers");
+    this->Modified();
+    }
   // TODO: display node modified events are not being observed
 
+  
   // 
   // Set the member variables and do a first process
   //
   if ( volumeNode != NULL)
-    {  
-    this->ProcessMRMLEvents(volumeNode, vtkCommand::ModifiedEvent, NULL);
+    {
+//    this->ProcessMRMLEvents(volumeNode, vtkCommand::ModifiedEvent, NULL);
     }
 }
 
@@ -89,21 +96,28 @@ vtkMRMLVolumeDisplayNode * vtkSlicerVolumeDisplayWidget::GetVolumeDisplayNode ()
 void vtkSlicerVolumeDisplayWidget::ProcessWidgetEvents ( vtkObject *caller,
                                                          unsigned long event, void *callData )
 {
-
+  vtkDebugMacro("ProcessWidgetEvents: nothing here, event = " << event);
 }
-
-
 
 //---------------------------------------------------------------------------
 void vtkSlicerVolumeDisplayWidget::ProcessMRMLEvents ( vtkObject *caller,
                                               unsigned long event, void *callData )
 {
 
+  vtkDebugMacro("ProcessMRMLEvents: event = " << event);
+  // when the volume node changes, just get a modified on this
+  if (event == vtkCommand::ModifiedEvent)
+    {
+    vtkDebugMacro("ProcessMRMLEvents: got modified event, hopefully from my volume node, calling myupdate widget from mrml");
+    // check that the display node is correct
+    this->UpdateWidgetFromMRML();
+    }
 }
+
 //---------------------------------------------------------------------------
 void vtkSlicerVolumeDisplayWidget::UpdateWidgetFromMRML ()
 {
-
+  vtkDebugMacro("UpdateWidgetFromMRML: subclass has not implemented this method!");
 }
 
 //---------------------------------------------------------------------------
@@ -119,13 +133,14 @@ void vtkSlicerVolumeDisplayWidget::TearDownWidget ()
 //---------------------------------------------------------------------------
 void vtkSlicerVolumeDisplayWidget::AddWidgetObservers ( ) 
 {
-
+  vtkDebugMacro("vtkSlicerVolumeDisplayWidget: adding observer");
+  this->AddObserver(vtkCommand::ModifiedEvent, (vtkCommand *)this->MRMLCallbackCommand );
 }
 
 //---------------------------------------------------------------------------
 void vtkSlicerVolumeDisplayWidget::RemoveWidgetObservers ( ) 
 {
-
+  this->RemoveObservers(vtkCommand::ModifiedEvent, (vtkCommand *)this->MRMLCallbackCommand );
 }
 
 
