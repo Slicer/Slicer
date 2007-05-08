4a76835338d9e258eae038911070c3451fb5bff2
Brad Davis <brad.davis@kitware.com>
2007-05-08 22:03:11 +0000

    ENH: add shell for command line EM segmenter
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=3358
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@3358 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/EMSegment/CommandLineApplication/vtkEMSegmentCommandLine.cxx
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void PrintUsage()
+{
+  std::cerr << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"  << std::endl
+            << "WARNING: This interface is under construction!!"  << std::endl
+            << "It will change."                                  << std::endl
+            << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"  << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+  PrintUsage();
+}
