7745557de79adad398c159e9a3e4feb39fdd4cf1
Katharina Quintus <kquintus@bwh.harvard.edu>
2007-05-23 20:27:36 +0000

    ENH: now works tensors as well
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=3429
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@3429 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/SlicerDaemon/matlab/pwriteNrrd.m
+++ Modules/SlicerDaemon/matlab/pwriteNrrd.m
@@ -3,6 +3,8 @@ function pwriteNrrd( p, header )
 
 % write a nrrd image to a pipe opened by popenw
 
+header
+
 popenw(p,double(['NRRD0001', 10]),'char');
 
 popenw(p,double(['content: from pwriteNrrd', 10]),'char');
@@ -10,45 +12,73 @@ popenw(p,double(['content: from pwriteNrrd', 10]),'char');
 popenw(p,double('type: '),'char');
 popenw(p,double([header.type, 10]),'char');
 
-header
-
 str = sprintf ('dimension: %d', header.dimension);
 popenw(p,double([str, 10]),'char');
 
 popenw(p,double('space: '),'char');
 popenw(p,double([header.space, 10]),'char');
 
-popenw(p,double('sizes:'),'char');
-for looper = 1:header.dimension
-  str = sprintf (' %d', header.sizes(looper));
-  popenw(p,double(str),'char');
-end
-popenw(p,10,'char');
 
 popenw(p,double('space origin: ('),'char');
-for looper = 1:header.dimension
-  str = sprintf ('%f', header.spaceorigin(looper)); popenw(p,double(str),'char');
-  if (looper < header.dimension)
-    popenw(p,double(','),'char');
-  end
+for looper = 1:3
+    str = sprintf ('%f', header.spaceorigin(looper)); popenw(p,double(str),'char');
+    if (looper < 3)
+        popenw(p,double(','),'char');
+    end
 end
 popenw(p,double(')'),'char');
 popenw(p,10,'char');
 
-
-popenw(p,double('space directions: '),'char');
-for outerlooper = 1:header.dimension
-  popenw(p,double('('),'char');
-  for looper = 1:3
-    str = sprintf ( '%f', header.spacedirections( (outerlooper-1) * header.dimension + looper ) );
+popenw(p,double('sizes:'),'char');
+for looper = 1:max(size(header.sizes))
+    str = sprintf (' %d', header.sizes(looper));
     popenw(p,double(str),'char');
-    if (looper < header.dimension)
-      popenw(p,double(','),'char');
-    end
-  end
-  popenw(p,double(') '),'char');
 end
 popenw(p,10,'char');
+
+popenw(p,double('space directions: '),'char');
+if ( (length(header.spacedirections)==10) && isnan(header.spacedirections(1)) )
+    % assume tensor data
+    header.spacedirections = header.spacedirections(2:end);
+    popenw(p,double('none '),'char');
+end
+for outerlooper = 1:3
+    popenw(p,double('('),'char');
+    for looper = 1:3
+        str = sprintf ( '%f', header.spacedirections( (outerlooper-1) * 3 + looper ) );
+        popenw(p,double(str),'char');
+        if (looper < 3)
+            popenw(p,double(','),'char');
+        end
+    end
+    popenw(p,double(') '),'char');
+end
+popenw(p,10,'char');
+
+popenw(p,double('kinds:'),'char');
+for looper = 1:max(size(header.kinds))
+    str = sprintf (' %s', char(header.kinds(looper)));
+    popenw(p,double(str),'char');
+end
+popenw(p,10,'char');
+
+
+if (strcmp(header.kinds(1), '3D-masked-symmetric-matrix'))
+    % assume tensor data
+    popenw(p,double('measurement frame: '),'char');
+    for outerlooper = 1:3
+        popenw(p,double('('),'char');
+        for looper = 1:3
+            str = sprintf ( '%f', header.measurementframe( (outerlooper-1) * 3 + looper ) );
+            popenw(p,double(str),'char');
+            if (looper < 3)
+                popenw(p,double(','),'char');
+            end
+        end
+        popenw(p,double(') '),'char');
+    end
+    popenw(p,10,'char');
+end
 
 popenw(p,double(['encoding: raw', 10]),'char');
 
