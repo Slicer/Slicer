5c98169ba8eff5bc9c200e515b5610338532c3c5
Katharina Quintus <kquintus@bwh.harvard.edu>
2007-06-08 20:45:32 +0000

    ENH: The helper does now no more transformations with the data. Transform Functions still available in ReceiveImageDataTensors_DoSlicerTransform and SendImageDataTensors_UndoSlicerTransform.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=3533
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@3533 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/SlicerDaemon/vtkTclHelper.cxx
+++ Modules/SlicerDaemon/vtkTclHelper.cxx
@@ -129,9 +129,160 @@ vtkTclHelper::SendImageDataScalars(char *sockname)
 
 void
 vtkTclHelper::SendImageDataTensors(char *sockname)
+{  
+  int mode, elementSize, numTuples, written;
+  int bytes, tensorArraySize;
+  Tcl_Channel channel = Tcl_GetChannel(this->Interp, sockname, &mode);
+  int dims[3];
+  vtkFloatingPointType tensor[3][3];
+  vtkDataArray *tensorArray;
+  // vtkMatrix4x4 *RASToVTK;
+  /*vtkMatrix4x4 *measurementFrame_T;
+  vtkMatrix4x4 *measurementFrame_inv;
+  vtkMatrix4x4 *measurementFrame_T_inv;
+  vtkMatrix4x4 *RASToVTK_inv;
+  vtkMatrix4x4 *RASToVTK_T_inv;
+  vtkMatrix4x4 *tmp;*/
+  vtkMatrix4x4 *t;
+
+  //  tmp = vtkMatrix4x4::New();
+  t = vtkMatrix4x4::New();
+  
+  // RASToVTK = vtkMatrix4x4::New();
+  // GetRASToVTKMatrix(RASToVTK);                 
+                         
+  // RASToVTK_inv = vtkMatrix4x4::New();
+  //RASToVTK_inv->Identity();
+  //vtkMatrix4x4::Invert(RASToVTK,RASToVTK_inv);
+  //RASToVTK_T_inv = vtkMatrix4x4::New();
+  //RASToVTK_T_inv->Identity();
+  //vtkMatrix4x4::Transpose(RASToVTK,RASToVTK_T_inv);
+  //RASToVTK_T_inv->Invert();
+  
+  //  measurementFrame_inv = vtkMatrix4x4::New();
+  //m//easurementFrame_inv->Identity();
+  //vtkMatrix4x4::Invert(this->MeasurementFrame,measurementFrame_inv);
+  //measurementFrame_T_inv = vtkMatrix4x4::New();
+  //measurementFrame_T_inv->Identity();
+  //vtkMatrix4x4::Transpose(this->MeasurementFrame,measurementFrame_T_inv);
+  //m/easurementFrame_T_inv->Invert();
+  
+  if ( ! (mode & TCL_WRITABLE) )
+    {   vtkErrorMacro ("Socket " << sockname << " is not writable\n");
+      return;
+    }
+  
+  if ( this->ImageData == NULL )
+    {   vtkErrorMacro ("Image Data is NULL");
+      return;
+    }
+    
+  this->ImageData->GetDimensions(dims);
+  tensorArray = this->ImageData->GetPointData()->GetTensors();
+  elementSize = this->ImageData->GetScalarSize();
+  numTuples = tensorArray->GetNumberOfTuples();
+ 
+  if (!tensorArray)
+    {
+      vtkGenericWarningMacro("No tensor data to send!");
+      return;
+    }
+ 
+  /* sending data as a "3D-masked-symmetric-matrix", which 
+     has 7 values: mask Mxx Mxy Mxz Myy Myz Mzz */
+  tensorArraySize =  dims[0] * dims[1] * dims[2] * 7 ;
+  bytes = elementSize * tensorArraySize;
+    
+  if (this->ImageData->GetScalarType() == 10) 
+    { // type float 
+      float *serializedTensorsP, *serializedTensorsP_copy;
+      serializedTensorsP = new float[tensorArraySize];
+      serializedTensorsP_copy = serializedTensorsP;
+     
+      for (int i = 0; i < numTuples; i++)
+        {
+          //tmp->Identity();
+          // t->Identity();
+          tensorArray->GetTuple(i, (vtkFloatingPointType *) tensor);
+          
+          /* t->SetElement(0,0,(double)(tensor[0][0]));
+          t->SetElement(0,1,(double)(tensor[0][1]));
+          t->SetElement(0,2,(double)(tensor[0][2]));
+          t->SetElement(1,0,(double)(tensor[1][0]));
+          t->SetElement(1,1,(double)(tensor[1][1]));
+          t->SetElement(1,2,(double)(tensor[1][2]));
+          t->SetElement(2,0,(double)(tensor[2][0]));
+          t->SetElement(2,1,(double)(tensor[2][1]));
+          t->SetElement(2,2,(double)(tensor[2][2]));*/
+        
+          /* vtkMatrix4x4::Multiply4x4(measurementFrame_inv, RASToVTK_inv, tmp);
+          vtkMatrix4x4::Multiply4x4(tmp, t, tmp);
+          vtkMatrix4x4::Multiply4x4(tmp, RASToVTK_T_inv, tmp);
+          vtkMatrix4x4::Multiply4x4(tmp, measurementFrame_T_inv, t);
+          */
+        
+          // sending out tensor in this form:  mask Mxx Mxy Mxz Myy Myz Mzz
+          // mask for now is always 1
+          serializedTensorsP[7*i+0] = (float) 1;
+          serializedTensorsP[7*i+1] = (float)(tensor[0][0]);//(t->GetElement(0,0));
+          serializedTensorsP[7*i+2] = (float)(tensor[0][1]);//(t->GetElement(0,1));
+          serializedTensorsP[7*i+3] = (float)(tensor[0][2]); //(t->GetElement(0,2));
+          serializedTensorsP[7*i+4] = (float)(tensor[1][1]);// (t->GetElement(1,1));
+          serializedTensorsP[7*i+5] = (float)(tensor[1][2]);// (t->GetElement(1,2));
+          serializedTensorsP[7*i+6] = (float)(tensor[2][2]); //(t->GetElement(2,2));
+        }
+
+      written = Tcl_WriteRaw(channel, (char *)(serializedTensorsP_copy), bytes);
+    
+    } else if (this->ImageData->GetScalarType() == 11) 
+    { // type double
+      double *serializedTensorsP, *serializedTensorsP_copy;
+      serializedTensorsP = new double[tensorArraySize];
+      serializedTensorsP_copy = serializedTensorsP;
+        
+      for (int i = 0; i < numTuples; i++)
+        {
+          tensorArray->GetTuple(i,(vtkFloatingPointType *)tensor);
+          // sending out tensor in this form:  mask Mxx Mxy Mxz Myy Myz Mzz
+          // mask for now is always 1
+          
+          serializedTensorsP[7*i+0] = (double) 1;
+          serializedTensorsP[7*i+1] = (double) (tensor[0][0]); 
+          serializedTensorsP[7*i+2] = (double) (tensor[0][1]);
+          serializedTensorsP[7*i+3] = (double) (tensor[0][2]);
+          serializedTensorsP[7*i+4] = (double) (tensor[1][1]);
+          serializedTensorsP[7*i+5] = (double) (tensor[1][2]);
+          serializedTensorsP[7*i+6] = (double) (tensor[2][2]);
+        }  
+      
+      written = Tcl_WriteRaw(channel, (char *)(serializedTensorsP_copy), bytes);
+      
+    } else 
+    { //other tensor data types we don't support here
+      vtkGenericWarningMacro("Tensor type (vtk type !" << 
+                             this->ImageData->GetScalarType()<< 
+                             " not supported." << "\n");
+      return;
+    } 
+  
+  Tcl_Flush(channel);
+   
+  if ( written != bytes )
+    {   vtkErrorMacro ("Only wrote " << written << " but expected to write " << bytes << "\n");
+      return;
+    }
+}
+
+/*Meeting with Steve and Raul (05-26-07): decision that in Matlab you'll get
+  whatever Slicer has im memory. Don't worry for the measurement frame and space
+  directions reduction that has been done to the data. This is the old method, 
+  that tried to undo measurement frame and space directions reduction.*/
+
+void
+vtkTclHelper::SendImageDataTensors_UndoSlicerTransform(char *sockname)
 {
-  vtkGenericWarningMacro("This is SendImageDataTensors.\n");
-  vtkGenericWarningMacro("The Node got passed in! "<<this->VolumeNode->GetClassName()<<"\n");
+  
+  //vtkGenericWarningMacro("The Node got passed in! "<<this->VolumeNode->GetClassName()<<"\n");
    
   int mode, elementSize, numTuples, written;
   int bytes, tensorArraySize;
@@ -140,6 +291,7 @@ vtkTclHelper::SendImageDataTensors(char *sockname)
   vtkFloatingPointType tensor[3][3];
   vtkDataArray *tensorArray;
   vtkMatrix4x4 *RASToVTK;
+  vtkMatrix4x4 *measurementFrame_T;
   vtkMatrix4x4 *measurementFrame_inv;
   vtkMatrix4x4 *measurementFrame_T_inv;
   vtkMatrix4x4 *RASToVTK_inv;
@@ -221,7 +373,7 @@ vtkTclHelper::SendImageDataTensors(char *sockname)
           vtkMatrix4x4::Multiply4x4(tmp, t, tmp);
           vtkMatrix4x4::Multiply4x4(tmp, RASToVTK_T_inv, tmp);
           vtkMatrix4x4::Multiply4x4(tmp, measurementFrame_T_inv, t);
-      
+          
         
           // sending out tensor in this form:  mask Mxx Mxy Mxz Myy Myz Mzz
           // mask for now is always 1
@@ -275,7 +427,6 @@ vtkTclHelper::SendImageDataTensors(char *sockname)
     }
 }
 
-
 void 
 vtkTclHelper::ReceiveImageDataScalars(char *sockname)
 {
@@ -309,6 +460,116 @@ vtkTclHelper::ReceiveImageDataScalars(char *sockname)
 
 void 
 vtkTclHelper::ReceiveImageDataTensors(char *sockname)
+{
+  int mode, elementSize, numberOfTuples, tensorArraySize, bytes;
+  int dims[3];
+  float *serializedTensorsP;
+  float mxx, mxy, mxz, myy, myz, mzz; 
+  vtkFloatArray* tensorArray;
+  /* vtkMatrix4x4 *RASToVTK;
+  vtkMatrix4x4 *measurementFrame_T;
+  vtkMatrix4x4 *RASToVTK_T;
+  vtkMatrix4x4 *tmp;
+  vtkMatrix4x4 *T_nrrd;
+  */   
+  Tcl_Channel channel = Tcl_GetChannel(this->Interp, sockname, &mode);
+  
+  if ( ! (mode & TCL_READABLE) )
+    {   vtkErrorMacro ("Socket " << sockname << " is not readable" << "\n");
+      return;
+    }
+
+  if ( this->ImageData == NULL )
+    {   vtkErrorMacro ("Image Data is NULL");
+      return;
+    }
+  // RASToVTK = vtkMatrix4x4::New();
+  // GetRASToVTKMatrix(RASToVTK);           
+  //RASToVTK_T = vtkMatrix4x4::New();
+  //RASToVTK_T->Identity();
+  //vtkMatrix4x4::Transpose(RASToVTK,RASToVTK_T);
+  //measurementFrame_T = vtkMatrix4x4::New();
+  //measurementFrame_T ->Identity();
+  //vtkMatrix4x4::Transpose(this->MeasurementFrame, measurementFrame_T);
+ 
+  this->ImageData->GetDimensions(dims);
+  elementSize = this->ImageData->GetScalarSize();
+  numberOfTuples = dims[0] * dims[1] * dims[2]; 
+  tensorArraySize = numberOfTuples * 7;
+  bytes = elementSize * tensorArraySize;
+  serializedTensorsP = new float[tensorArraySize];
+  
+  if (this->ImageData->GetScalarType() != 10) {
+    //only accept type float!
+    vtkErrorMacro ("Attempt to send tensor data that is not type float." <<
+                   " Sorry, not supported yet!\n");
+    return;
+  } else { // type float
+    tensorArray = vtkFloatArray::New();
+    // assume number of components will always be 9 for tensors in slicer
+    tensorArray->SetNumberOfComponents(9);
+    tensorArray->SetNumberOfTuples(numberOfTuples);
+    
+    int read = Tcl_Read(channel, (char *)serializedTensorsP, bytes);
+    
+    if ( read != bytes )
+      {   vtkErrorMacro ("Only read " << read << " but expected to read " << bytes << "\n");
+        return;
+      }
+    
+    // T_nrrd = vtkMatrix4x4::New();
+    //tmp = vtkMatrix4x4::New();
+    for (int i=0; i < tensorArray->GetNumberOfTuples(); i++)
+      {
+        /* throw away every 7th value because that is the mask that we don't pay
+           attention to for now */
+        serializedTensorsP++;
+        mxx = *serializedTensorsP; serializedTensorsP++;
+        mxy = *serializedTensorsP; serializedTensorsP++;
+        mxz = *serializedTensorsP; serializedTensorsP++;
+        myy = *serializedTensorsP; serializedTensorsP++;
+        myz = *serializedTensorsP; serializedTensorsP++;
+        mzz = *serializedTensorsP; serializedTensorsP++;
+        
+       //  T_nrrd->Identity(); 
+//         T_nrrd->SetElement(0,0,mxx);
+//         T_nrrd->SetElement(0,1,mxy);
+//         T_nrrd->SetElement(1,0,mxy);
+//         T_nrrd->SetElement(0,2,mxz);
+//         T_nrrd->SetElement(2,0,mxz); 
+//         T_nrrd->SetElement(1,1,myy);
+//         T_nrrd->SetElement(1,2,myz);
+//         T_nrrd->SetElement(2,1,myz); 
+//         T_nrrd->SetElement(2,2,mzz);
+        
+//         tmp->Identity();
+//         vtkMatrix4x4::Multiply4x4(RASToVTK, this->MeasurementFrame, tmp);
+//         vtkMatrix4x4::Multiply4x4(tmp, T_nrrd, tmp);
+//         vtkMatrix4x4::Multiply4x4(tmp, measurementFrame_T, tmp);
+//         vtkMatrix4x4::Multiply4x4(tmp,RASToVTK_T, tmp);
+       
+        tensorArray->SetComponent(i,0,mxx);
+        tensorArray->SetComponent(i,1,mxy);
+        tensorArray->SetComponent(i,2,mxz);
+        tensorArray->SetComponent(i,3,mxy);
+        tensorArray->SetComponent(i,4,myy);
+        tensorArray->SetComponent(i,5,myz);
+        tensorArray->SetComponent(i,6,mxz);
+        tensorArray->SetComponent(i,7,myz);
+        tensorArray->SetComponent(i,8,mzz);
+      }
+      
+      this->ImageData->GetPointData()->SetTensors(tensorArray);
+  }  
+}
+
+/*Meeting with Steve and Raul (05-26-07): decision that the measurement frame
+  reduce and space directions reduce transformations that slicer does with
+  tensordata has to be handled by the volume node. The plan is to have a method
+  in the volumeNodeClass that will be in charge of doing what this method tries 
+  to do.*/
+void 
+vtkTclHelper::ReceiveImageDataTensors_DoSlicerTransform(char *sockname)
 {
   int mode, elementSize, numberOfTuples, tensorArraySize, bytes;
   int dims[3];
