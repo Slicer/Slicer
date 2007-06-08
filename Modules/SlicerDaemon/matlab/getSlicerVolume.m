ec7ee73e731862ac1fe748b674ec34a915955439
Katharina Quintus <kquintus@bwh.harvard.edu>
2007-06-08 20:30:00 +0000

    ENH: Adding flag to transform diffusion tensor from gradient space into ijk space or back.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=3528
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@3528 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/SlicerDaemon/matlab/getSlicerVolume.m
+++ Modules/SlicerDaemon/matlab/getSlicerVolume.m
@@ -1,4 +1,4 @@
-function volume = getSlicerVolume( id_or_name )
+function volume = getSlicerVolume( id_or_name, transformDT_flag)
 
 % This is an example script that shows how to establish a reading pipe to a
 % running slicer daemon (start Slicer3 with option --daemon).
@@ -10,6 +10,11 @@ function volume = getSlicerVolume( id_or_name )
 % - make sure to add the path to the matlab scripts in 
 %   $SLICER_HOME/Modules/SlicerDaemon/Tcl
 %
+% Input: Name or ID of volume loaded in Slicer. For Tensor Data, you can
+%        optionally input the string 'transformDT'. If 'transformDT' is 
+%        given, the diffusion tensors 
+%        from slicer get transformed back into their orginal gradient space 
+%        according to measurement frame and space directions.   
 % returns a struct with image data from slicer volume with id "id" or
 % name "name" and image information according to the nrrd format.
 
@@ -41,7 +46,16 @@ if p_r < 0
     error(['Error running popenr(',cmd_r,')']);
 end
 
-volume = preadNrrd(p_r);
+if nargin==2
+    if (strcmp(transformDT_flag,'transformDT'))
+        volume = preadNrrd(p_r,1);
+    else
+        volume = preadNrrd(p_r,0); 
+    end
+else 
+   volume = preadNrrd(p_r,0);    
+end
+    
 
 % close pipe
 popenr(p_r,-1)
