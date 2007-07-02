ef016ce52cca014202bce9c7b9a9d81c04b7d083
Steve Pieper <pieper@bwh.harvard.edu>
2007-07-02 21:42:26 +0000

    ENH: new test of scrolling behavior
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=3789
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@3789 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/GUI/Testing/ScrollTesting.tcl
@@ -0,0 +1,30 @@
+
+# a test to load a scene with a volume and scroll through the slices
+
+
+update
+
+$::slicer3::MRMLScene SetURL $::SLICER_BUILD/../Slicer3/Libs/MRML/Testing/vol_and_cube_camera.mrml
+$::slicer3::MRMLScene Connect
+
+set sliceLogic [$::slicer3::ApplicationGUI GetMainSliceLogic0]
+
+$::slicer3::ApplicationGUI RepackMainViewer 4 "Red"
+
+update
+
+[$::slicer3::ApplicationGUI GetSlicesControlGUI] FitSlicesToBackground
+
+update
+
+set outerloops 5
+set innerloops 5
+
+for {set j 0} {$j < $outerloops} {incr j} {
+  for {set i 0} {$i < $innerloops} {incr i} {
+    $sliceLogic SetSliceOffset $i
+    update
+  }
+}
+
+exit 0
