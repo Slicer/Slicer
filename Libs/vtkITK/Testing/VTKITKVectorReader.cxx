61698a22e3275a3866ad6d54b79e4c49a734bc60
Nicole Aucoin <nicole@bwh.harvard.edu>
2007-08-08 17:40:14 +0000

    ENH: adding a test of the vector reader to try and narrow down the garbage collecting problem when freeing the vector reader
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=4026
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@4026 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/vtkITK/Testing/VTKITKVectorReader.cxx
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+#include <itksys/SystemTools.hxx>
+#include <vtkITKArchetypeImageSeriesVectorReader.h>
+#include <vtkImageData.h>
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+    {
+    std::cout << "ERROR: need to specify a file to try reading on the command line." << std::endl;
+    return 1;
+    }
+  std::cout << "Trying to read file '" << argv[1] << "'" << std::endl;
+
+  vtkITKArchetypeImageSeriesVectorReader *vectorReader = vtkITKArchetypeImageSeriesVectorReader::New();
+  vectorReader->SetArchetype(argv[1]);
+  vectorReader->SetOutputScalarTypeToNative();
+  vectorReader->SetDesiredCoordinateOrientationToNative();
+
+  try
+    {
+    vectorReader->Update();
+    }
+  catch (itk::ExceptionObject err)
+    {
+    std::cout << "Unable to read file '" << argv[1] << "', err = \n" << err << std::endl;
+    vectorReader->Delete();
+    return 1;
+    }
+  
+  // now assign it to another image
+  vtkImageData *imageData;
+
+  imageData = vectorReader->GetOutput();
+  if (imageData)
+    {
+    std::cout << "Read file, image data has " << imageData->GetNumberOfPoints() << " points" << std::endl;
+    }
+  else
+    {
+    std::cout << "ERROR: image data is null from vector reader" << std::endl;
+    vectorReader->Delete();
+    return 1;
+    }
+
+  std::cout << "Deleting vector reader" << std::endl;
+  
+  vectorReader->Delete();
+  
+  return 0;
+}
