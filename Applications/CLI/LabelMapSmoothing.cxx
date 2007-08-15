fcf634c466b728a81e826526036796cc0e8ef763
Dirk Padfield <padfield@research.ge.com>
2007-08-15 15:07:47 +0000

    ENH: Crop label of interest to increase speed of algorithm and reduce memory requirement
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=4062
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@4062 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/CLI/LabelMapSmoothing.cxx
+++ Applications/CLI/LabelMapSmoothing.cxx
@@ -25,6 +25,9 @@
 #include "itkDiscreteGaussianImageFilter.h"
 #include "itkBinaryThresholdImageFilter.h"
 #include "itkMinimumMaximumImageFilter.h"
+#include "itkLabelStatisticsImageFilter.h"
+#include "itkRegionOfInterestImageFilter.h"
+#include "itkPasteImageFilter.h"
 
 #include "itkImage.h"
 #include "itkImageFileReader.h"
@@ -38,31 +41,38 @@ int main( int argc, char * argv[] )
 {
   PARSE_ARGS;
 
-// Image Types
-  typedef  unsigned int  InputPixelType;
-  typedef  float  InternalPixelType;
-  typedef  unsigned int  OutputPixelType;
+  unsigned int numberOfLayers = 2;
+  unsigned int boundingBoxPadding = vnl_math_max((unsigned int)(std::ceil(4.0*gaussianSigma)),numberOfLayers);
 
-  typedef itk::Image< InputPixelType,  3 >   InputImageType;
-  typedef itk::Image< InternalPixelType, 3 >   InternalImageType;
-  typedef itk::Image< OutputPixelType, 3 >   OutputImageType;
+// Image Types
+  const unsigned short ImageDimension = 3;
+  typedef itk::Image< float, ImageDimension >  FloatImageType;
+  typedef itk::Image< unsigned char, ImageDimension >    UCharImageType;
 
 // Filter Types
-  typedef itk::BinaryThresholdImageFilter< InputImageType, InputImageType > InputThresholdType;
-  typedef itk::AntiAliasBinaryImageFilter<InputImageType, InternalImageType> AntiAliasType;
-  typedef itk::DiscreteGaussianImageFilter<InternalImageType, InternalImageType >  GaussianType;
-  typedef itk::BinaryThresholdImageFilter< InternalImageType, OutputImageType > OutputThresholdType;
+  typedef itk::BinaryThresholdImageFilter< UCharImageType, UCharImageType > InputThresholdType;
+  typedef itk::MinimumMaximumImageFilter< UCharImageType > MinMaxType;
+  typedef itk::LabelStatisticsImageFilter< UCharImageType, UCharImageType > LabelStatisticsType;
+  typedef itk::RegionOfInterestImageFilter< UCharImageType, UCharImageType > ExtracterType;
+  typedef itk::AntiAliasBinaryImageFilter< UCharImageType, FloatImageType > AntiAliasType;
+  typedef itk::DiscreteGaussianImageFilter< FloatImageType, FloatImageType >  GaussianType;
+  typedef itk::BinaryThresholdImageFilter< FloatImageType, UCharImageType > OutputThresholdType;
+  typedef itk::PasteImageFilter< UCharImageType, UCharImageType, UCharImageType> PasteType;
 
 // I/O Types
-  typedef itk::ImageFileReader< InputImageType >  ReaderType;
-  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+  typedef itk::ImageFileReader< UCharImageType >  ReaderType;
+  typedef itk::ImageFileWriter< UCharImageType >  WriterType;
 
 // Instances
   ReaderType::Pointer reader = ReaderType::New();
   InputThresholdType::Pointer inputThresholder = InputThresholdType::New();
+  MinMaxType::Pointer minMaxCalculator = MinMaxType::New();
+  LabelStatisticsType::Pointer labelStatisticsFilter = LabelStatisticsType::New();
+  ExtracterType::Pointer extracter = ExtracterType::New();
   AntiAliasType::Pointer antiAliasFilter = AntiAliasType::New();
   GaussianType::Pointer gaussianFilter = GaussianType::New();
   OutputThresholdType::Pointer outputThresholder = OutputThresholdType::New();
+  PasteType::Pointer paster = PasteType::New();
   WriterType::Pointer writer = WriterType::New();
 
 // Watchers
@@ -70,39 +80,118 @@ int main( int argc, char * argv[] )
   itk::PluginFilterWatcher GaussianWatcher(gaussianFilter, "Gaussian Image Filter", CLPProcessInformation, 1.0/3.0, 2.0/3.0);
 
   reader->SetFileName (inputVolume.c_str());
+  std::cout << "Updating reader..." << std::endl;
+  reader->Update();
 
-  // Choose a label to smooth.  All others will be ignored.  If no
-  // label is selected by the user, the maximum label in the image is chosen by default.
+
+  // Choose a label to smooth.  All others will be ignored.  
+  // If the chosen label is greater than the largest label in the
+  // image or lower than the smallest, the label will be set to the
+  // closest one.
+  // If no label is selected by the user, the maximum label in the image is chosen by default.
+  minMaxCalculator->SetInput( reader->GetOutput() );
+  minMaxCalculator->ReleaseDataFlagOn();
+  minMaxCalculator->Update();
+  
   if( labelToSmooth == -1 )
     {
-    typedef itk::MinimumMaximumImageFilter< InputImageType > MinMaxType;
-    MinMaxType::Pointer minMaxCalculator = MinMaxType::New();
-    minMaxCalculator->SetInput( reader->GetOutput() );
-    minMaxCalculator->Update();
     labelToSmooth = minMaxCalculator->GetMaximum();
     }
+  else if( labelToSmooth > minMaxCalculator->GetMaximum() )
+    {
+    labelToSmooth = minMaxCalculator->GetMaximum();
+    }
+  else if( labelToSmooth < minMaxCalculator->GetMinimum() )
+    {
+    labelToSmooth = minMaxCalculator->GetMinimum();
+    }
+
 
   inputThresholder->SetInput( reader->GetOutput() );
   inputThresholder->SetInsideValue( 1 );
   inputThresholder->SetOutsideValue( 0 );
   inputThresholder->SetLowerThreshold( labelToSmooth );
   inputThresholder->SetUpperThreshold( labelToSmooth );
+  inputThresholder->ReleaseDataFlagOn();
+  std::cout << "Updating inputThresholder..." << std::endl;
+  inputThresholder->Update();
 
-  antiAliasFilter->SetInput( inputThresholder->GetOutput() );  
+  // Find the bounding box of the desired label.
+  labelStatisticsFilter->SetInput( inputThresholder->GetOutput() );
+  labelStatisticsFilter->SetLabelInput( inputThresholder->GetOutput() );
+  //labelStatisticsFilter->ReleaseDataFlagOn();
+  labelStatisticsFilter->Update();
+  LabelStatisticsType::BoundingBoxType boundingBox = labelStatisticsFilter->GetBoundingBox( 1 );
+  // Extend the bounding box in each direction to ensure that the
+  // cropping of the image does not affect the final result.
+  // Make sure the new bounding box does not extend outside of the
+  // original image size.
+  UCharImageType::SizeType imageSize = reader->GetOutput()->GetLargestPossibleRegion().GetSize();
+  UCharImageType::RegionType::SizeType regionSize;
+  UCharImageType::IndexType regionIndex;
+  for( unsigned int i = 0; i < ImageDimension; i++ )
+    {
+    boundingBox[2*i] = vnl_math_max(0,(int)(boundingBox[2*i]-boundingBoxPadding));
+    boundingBox[2*i+1] = vnl_math_min(imageSize[i]-1,boundingBox[2*i+1]+boundingBoxPadding);
+    regionIndex[i] = boundingBox[2*i];
+    regionSize[i] = boundingBox[2*i+1]-boundingBox[2*i]+1;
+    }
+
+  // Create a region corresponding to the bounding box.
+  UCharImageType::RegionType boundingRegion;
+  boundingRegion.SetSize( regionSize );
+  boundingRegion.SetIndex( regionIndex );
+
+  // Extract the region from the image.
+  extracter->SetInput( inputThresholder->GetOutput() );
+  extracter->SetRegionOfInterest ( boundingRegion );
+  extracter->ReleaseDataFlagOn();
+  extracter->Update();
+
+  FloatImageType::Pointer antiAliasImage;
+  {
+  antiAliasFilter->SetInput( extracter->GetOutput() );  
   antiAliasFilter->SetMaximumRMSError( maxRMSError );
   antiAliasFilter->SetNumberOfIterations( numberOfIterations );
-  antiAliasFilter->SetNumberOfLayers( 2 );
-
-  gaussianFilter->SetInput( antiAliasFilter->GetOutput() );
+  antiAliasFilter->SetNumberOfLayers( numberOfLayers );
+  antiAliasFilter->ReleaseDataFlagOn();
+  std::cout << "Updating antiAliasFilter..." << std::endl;
+  antiAliasFilter->Update();
+  
+  antiAliasImage = antiAliasFilter->GetOutput();
+  antiAliasImage->DisconnectPipeline();
+  }
+  
+  gaussianFilter->SetInput( antiAliasImage );
   gaussianFilter->SetVariance( gaussianSigma * gaussianSigma );
+  gaussianFilter->ReleaseDataFlagOn();
+  std::cout << "Updating gaussianFilter..." << std::endl;
+  gaussianFilter->Update();
 
   outputThresholder->SetInput( gaussianFilter->GetOutput() );
   outputThresholder->SetLowerThreshold( 0 );
   outputThresholder->SetInsideValue( 1 );
   outputThresholder->SetOutsideValue( 0 );
+  outputThresholder->ReleaseDataFlagOn();
+  std::cout << "Updating outputThresholder..." << std::endl;
+  outputThresholder->Update();
 
-  writer->SetInput( outputThresholder->GetOutput() );
+  // Paste the smoothed result back into the original thresholded
+  // image.  This is done rather than creating a new volume to save
+  // memory.  Since the bounding box ensures that the entire region of
+  // the inputThresholder image is included, the pasting will cover
+  // all foreground regions.
+  paster->SetDestinationImage( inputThresholder->GetOutput() );
+  paster->SetSourceImage( outputThresholder->GetOutput() );
+  paster->SetSourceRegion( outputThresholder->GetOutput()->GetRequestedRegion() );
+  paster->SetDestinationIndex( regionIndex );
+  paster->ReleaseDataFlagOn();
+  std::cout << "Updating paster..." << std::endl;
+  paster->Update();
+
+  writer->SetInput( paster->GetOutput() );
   writer->SetFileName( outputVolume.c_str() );
+  std::cout << "Updating writer..." << std::endl;
   writer->Update();
 
   return EXIT_SUCCESS;
