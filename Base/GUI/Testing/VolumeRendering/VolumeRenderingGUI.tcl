6aa52f8ed832cbf79e1053ee0aab4682e7a41384
Andreas Freudling <freudling@bwh.harvard.edu>
2007-08-30 16:53:05 +0000

    ENH: Switch between volumes on runtime, Change opacity for labelMaps, differentation between labelmap, grayscale
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=4206
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@4206 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Testing/VolumeRendering/VolumeRenderingGUI.tcl
+++ Base/GUI/Testing/VolumeRendering/VolumeRenderingGUI.tcl
@@ -123,6 +123,28 @@ proc VolumeRenderingBuildGUI {this} {
     #No Observer:performance!
     #$::VR($this,vpw) AddObserver AnyEvent "$::VR($this,renderWidget) Render"
     pack [$::VR($this,vpw) GetWidgetName] -side top -anchor nw -expand y -padx 2 -pady 2
+    
+    #Scale for opacity
+    set ::VR($this,opacityScale) [vtkKWScaleWithLabel New]
+    puts "opactiyScale"
+    $::VR($this,opacityScale) SetParent [$::VR($this,cFrameVolumes) GetFrame]
+    $::VR($this,opacityScale) Create
+    $::VR($this,opacityScale) SetLabelText "Opacity for labelmaps"
+    [$::VR($this,opacityScale) GetWidget] SetRange 0 1
+    [$::VR($this,opacityScale) GetWidget] SetResolution 0.1
+    [$::VR($this,opacityScale) GetWidget] SetValue 0.2
+    $::VR($this,opacityScale) SetBalloonHelpString "set opacity for labelmaps"
+    $::VR($this,opacityScale) EnabledOff
+    pack [$::VR($this,opacityScale) GetWidgetName] -side top -anchor nw -expand y -padx 2 -pady 2
+    
+    set ::VR($this,buttonOpacity) [vtkKWPushButton New]
+    puts "buttonOpacity: $::VR($this,buttonAllModelsInvisible)"
+    $::VR($this,buttonOpacity) SetParent [$::VR($this,cFrameVolumes) GetFrame]
+    $::VR($this,buttonOpacity) Create
+    $::VR($this,buttonOpacity) SetText "OpactiyChange"
+    $::VR($this,buttonOpacity) SetBalloonHelpString "Change Opacity"
+    $::VR($this,buttonOpacity) EnabledOff
+    pack [$::VR($this,buttonOpacity) GetWidgetName] -side top -anchor nw -padx 0 -pady 2
     puts "End VolumeRendering BuildGui"
 
 }
@@ -132,6 +154,7 @@ puts "Enter"
 proc VolumeRenderingAddGUIObservers {this} {
     $this AddObserverByNumber $::VR($this,buttonLoadNode) 10000
     $this AddObserverByNumber $::VR($this,buttonAllModelsInvisible) 10000
+    $this AddObserverByNumber $::VR($this,buttonOpacity) 10000
     puts "Observer Added VolumeRendering"
 }
 
@@ -166,7 +189,10 @@ proc VolumeRenderingProcessGUIEvents {this caller event} {
                 allModelsInvisible $this
             } ;#Switch inner
         } ;#Switch
-    } ;#elif
+    } elseif {$caller == $::VR($this,buttonOpacity)} {
+        puts "update opacity"
+        UpdateOpacity $this
+    }
     puts "Events processed"
 } ;#procedure
 proc allModelsInvisible {this} {
@@ -181,7 +207,18 @@ proc allModelsInvisible {this} {
     }
 }
 proc buttonLoadNode {this} {
-puts "Begin buttonLoadNode"
+    puts "Begin buttonLoadNode"
+    if {[info exists ::VR($this,volume)]} {
+        #Remove old volumes
+        puts "remove old volumes"
+        $::VR($this,renderWidget) RemoveViewProp $::VR($this,volume)
+                    $::VR($this,opacityScale) EnabledOff
+            $::VR($this,buttonOpacity) EnabledOff
+    } else {
+        puts "no old volumes"
+    }
+    
+
     set ::VR($this,aMRMLNODE) [$::VR($this,nodeSelector) GetSelected]
     #Something selected?
     if { $::VR($this,aMRMLNODE) == "" } {
@@ -191,75 +228,106 @@ puts "Begin buttonLoadNode"
         set ::VR($this,aImageData) [$::VR($this,aMRMLNODE) GetImageData]
         set reader  $::VR($this,aImageData)
         puts "278"
-
-
-            #Histogram bauen
-              #$reader Update
-              set pfed_hist [vtkKWHistogram New]
-              puts "pfed_hist: $pfed_hist"
-              $pfed_hist BuildHistogram \
-                [[$reader GetPointData] GetScalars] 0
-            puts "285"
-            #$pfed_hist Update
-            
-            set opacityTransferFunction [vtkPiecewiseFunction New]
-            puts "opacityTransferFunction: $opacityTransferFunction"
-            
-            set colorTransferFunction [vtkColorTransferFunction New]
-            puts "colorTransferFunction: $colorTransferFunction"
-            
-            #mapping functions
-                
+        
+        
+        #Build Histogram and mapping functions
+        set ::VR($this,pfed_hist) [vtkKWHistogram New]
+        puts "pfed_hist: $$::VR($this,pfed_hist)"
+        $::VR($this,pfed_hist) BuildHistogram [[$reader GetPointData] GetScalars] 0
+        
+        set ::VR($this,opacityTransferFunction) [vtkPiecewiseFunction New]
+        puts "opacityTransferFunction: $::VR($this,opacityTransferFunction)"
+        
+        set colorTransferFunction [vtkColorTransferFunction New]
+        puts "colorTransferFunction: $colorTransferFunction"
+        
+        if {[$::VR($this,aMRMLNODE) GetLabelMap] == 1} {
+            puts "volume is labelmap"
+            #Look at labelmap for color
             #Get Lookup Table for RGB COLORS
             #Get Display Node
-            
+            $::VR($this,opacityScale) EnabledOn
+            $::VR($this,buttonOpacity) EnabledOn
             set colorNode [[$::VR($this,aMRMLNODE) GetVolumeDisplayNode] GetColorNode]
             puts "Color: $colorNode"
             #$colorNode SetTypeToLabels
             set lookupRGB [$colorNode GetLookupTable]
             
-            set lowerRange [lindex [$pfed_hist GetRange] 0]
-            set upperRange [lindex [$pfed_hist GetRange] 1]
+            set lowerRange [lindex [$::VR($this,pfed_hist) GetRange] 0]
+            set upperRange [lindex [$::VR($this,pfed_hist) GetRange] 1]
             set index 1
             set opacity .2
             set countTreshold 10
             if 1 {
                 while {$index<$upperRange} {
-                    if {[$pfed_hist GetOccurenceAtValue $index]>$countTreshold} {
-                        puts "Color at $index"
+                    if {[$::VR($this,pfed_hist) GetOccurenceAtValue $index]>$countTreshold} {
+                        #puts "Color at $index"
                         ##Opacity
                         #Set Lower Points
-                        $opacityTransferFunction AddPoint [expr $index - .5] 0
-                        $opacityTransferFunction AddPoint [expr $index - .4999999] $opacity
+                        $::VR($this,opacityTransferFunction) AddPoint [expr $index - .5] 0
+                        $::VR($this,opacityTransferFunction) AddPoint [expr $index - .4999999] $opacity
                         #Set upper Point
-                        $opacityTransferFunction AddPoint [expr $index + .5] 0
-                        $opacityTransferFunction AddPoint [expr $index + .4999999] $opacity
+                        $::VR($this,opacityTransferFunction) AddPoint [expr $index + .5] 0
+                        $::VR($this,opacityTransferFunction) AddPoint [expr $index + .4999999] $opacity
                         
                         #Colors
                         set color [$lookupRGB GetColor $index]
-                        puts "$color"
+                        #puts "$color"
                         $colorTransferFunction AddRGBPoint [expr $index + .01] [lindex $color 0] [lindex $color 1] [lindex $color 2]
                         $colorTransferFunction AddRGBPoint [expr $index + .99] [lindex $color 0] [lindex $color 1] [lindex $color 2]
                         
-                        puts "Color at $index finished"
-                    }
+                        #puts "Color at $index finished"
+                    } ;#if
                     incr index
-                }
+                } ;#while
+            } ;#if
+        } else {
+            puts "normal grayscaledata"
+            $::VR($this,opacityScale) EnabledOff
+            # 2tresholds
+            set treshold1  [expr [$::VR($this,pfed_hist) GetTotalOccurence]/20.]
+            set treshold2 [expr [$::VR($this,pfed_hist) GetTotalOccurence]/10.]
+
+            #counters for loop
+            set indexTreshold1 10
+            set totalTreshold1 0
+            set indexTreshold2 10
+            set totalTreshold2 0
+
+            while {$totalTreshold1<$treshold1} {
+            incr totalTreshold1 [$::VR($this,pfed_hist) GetOccurenceAtValue $indexTreshold1]
+            incr indexTreshold1 
             }
+            while {$totalTreshold2<$treshold2} {
+            incr totalTreshold2 [$::VR($this,pfed_hist) GetOccurenceAtValue $indexTreshold2]
+            incr indexTreshold2 
+            }
+            
+            $::VR($this,opacityTransferFunction) AddPoint [lindex [$::VR($this,pfed_hist) GetRange] 0] .0
+            $::VR($this,opacityTransferFunction) AddPoint  $indexTreshold1  0.0    
+            $::VR($this,opacityTransferFunction) AddPoint  $indexTreshold2  0.2
+            $::VR($this,opacityTransferFunction) AddPoint [lindex [$::VR($this,pfed_hist) GetRange] 1] .2
+            $colorTransferFunction AddRGBPoint      [lindex [$::VR($this,pfed_hist) GetRange] 0] 0.3 0.3 1.0
+            $colorTransferFunction AddRGBPoint      [expr [lindex [$::VR($this,pfed_hist) GetRange] 1] * 0.25] 0.3 0.3 1.0
+            $colorTransferFunction AddRGBPoint      [expr [lindex [$::VR($this,pfed_hist) GetRange] 1] * 0.5] 0.3 1.0 0.3
+            $colorTransferFunction AddRGBPoint    [expr [lindex [$::VR($this,pfed_hist) GetRange] 1] * 0.75] 1.0 0.3 0.3
+            $colorTransferFunction AddRGBPoint     [lindex [$::VR($this,pfed_hist) GetRange] 1] 1    .3    .3
+        } ;#else
+            
 
         
-          set gradientOpacityTransferFunction [vtkPiecewiseFunction New]
-        puts "gradientOpacityTransferFunction: $gradientOpacityTransferFunction"
-        $gradientOpacityTransferFunction AddPoint 1    1
+          #set gradientOpacityTransferFunction [vtkPiecewiseFunction New]
+        #puts "gradientOpacityTransferFunction: $gradientOpacityTransferFunction"
+        #$gradientOpacityTransferFunction AddPoint 1    1
             #$gradientOpacityTransferFunction AddPoint  41   0.0
             #$gradientOpacityTransferFunction AddPoint  45  1
-            $gradientOpacityTransferFunction AddPoint 145    1
+         #   $gradientOpacityTransferFunction AddPoint 145    1
 
         set volumeProperty [vtkVolumeProperty New]
         puts "volumeProperty: $volumeProperty"
-            $volumeProperty SetScalarOpacity $opacityTransferFunction
+            $volumeProperty SetScalarOpacity $::VR($this,opacityTransferFunction)
             $volumeProperty SetColor $colorTransferFunction
-            $volumeProperty SetGradientOpacity $gradientOpacityTransferFunction
+            #$volumeProperty SetGradientOpacity $gradientOpacityTransferFunction
             $volumeProperty SetInterpolationTypeToNearest
             $volumeProperty ShadeOff
 
@@ -278,26 +346,26 @@ puts "Begin buttonLoadNode"
             $volumeMapper SetSampleDistance 0.1
         }
             puts "348"
-        set volume [vtkVolume New]
-        $volume SetMapper $volumeMapper
-        $volume SetProperty $volumeProperty
+        set ::VR($this,volume) [vtkVolume New]
+        $::VR($this,volume) SetMapper $volumeMapper
+        $::VR($this,volume) SetProperty $volumeProperty
         set matrix [vtkMatrix4x4 New]
         puts "after setMatrix"
         puts "after cast"
         $::VR($this,aMRMLNODE) GetIJKToRASMatrix $matrix
         puts "after get"
-        $volume PokeMatrix $matrix
+        $::VR($this,volume) PokeMatrix $matrix
         puts "PokeMatrix"
         #Add data to vtkKWVolumePropertyWidget
         $::VR($this,vpw) SetVolumeProperty $volumeProperty
         $::VR($this,vpw) ComponentWeightsVisibilityOff
-        $::VR($this,vpw) Update
+        #$::VR($this,vpw) Update
 
         #Add property to RenderWidget
-        $::VR($this,renderWidget) AddViewProp $volume
+        $::VR($this,renderWidget) AddViewProp $::VR($this,volume)
         #Add Histograms to Widget
-        [$::VR($this,vpw) GetScalarOpacityFunctionEditor]  SetHistogram $pfed_hist
-        [$::VR($this,vpw) GetScalarColorFunctionEditor] SetHistogram $pfed_hist
+        [$::VR($this,vpw) GetScalarOpacityFunctionEditor]  SetHistogram $::VR($this,pfed_hist)
+        [$::VR($this,vpw) GetScalarColorFunctionEditor] SetHistogram $::VR($this,pfed_hist)
         #No Gradient
         #Update vtkKWVolumePropertyWidget
         $::VR($this,renderWidget) Create
@@ -308,7 +376,37 @@ puts "Begin buttonLoadNode"
 #
 # MRML Event processing
 #
+proc UpdateOpacity {this} {
+puts "Begin proc UpdateOpacity"
 
+
+ set lowerRange [lindex [$::VR($this,pfed_hist) GetRange] 0]
+            set upperRange [lindex [$::VR($this,pfed_hist) GetRange] 1]
+            set index 1
+            set opacity [[$::VR($this,opacityScale) GetWidget] GetValue]
+            puts "opacity $opacity"
+            set countTreshold 10
+            if 1 {
+                while {$index<$upperRange} {
+                    if {[$::VR($this,pfed_hist) GetOccurenceAtValue $index]>$countTreshold} {
+                        #puts "Color at $index"
+                        ##Opacity
+                        #Set Lower Points
+                        $::VR($this,opacityTransferFunction) AddPoint [expr $index - .5] 0
+                        $::VR($this,opacityTransferFunction) AddPoint [expr $index - .4999999] $opacity
+                        #Set upper Point
+                        $::VR($this,opacityTransferFunction) AddPoint [expr $index + .5] 0
+                        $::VR($this,opacityTransferFunction) AddPoint [expr $index + .4999999] $opacity
+                        
+                    } ;#if
+                    incr index
+                } ;#while
+                #$::VR($this,opacityTransferFunction) Update
+                [$::VR($this,vpw) GetScalarOpacityFunctionEditor] Update
+            } ;#if
+puts "Ende proc UpdateOpacity"
+
+}
 proc VolumeRenderingUpdateMRML {this} {
 }
 
