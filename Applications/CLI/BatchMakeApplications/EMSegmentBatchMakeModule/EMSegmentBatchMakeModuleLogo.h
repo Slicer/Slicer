822b9fd4caaed4878a219af2e3d0e622cdd11758
Stephen Aylward <Stephen.Aylward@kitware.com>
2007-09-23 19:12:25 +0000

    ENH: Forgot to add a few files to last submission
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=4411
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@4411 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/CLI/BatchMakeApplications/EMSegmentBatchMakeModule/EMSegmentBatchMakeModuleLogo.h
@@ -0,0 +1,16 @@
+/* 
+ * Resource generated for file:
+ *    EMSegmentBatchMakeLogo.png (zlib, base64) (image file)
+ */
+static const unsigned int  image_EMSegmentBatchMakeLogo_width          = 40;
+static const unsigned int  image_EMSegmentBatchMakeLogo_height         = 40;
+static const unsigned int  image_EMSegmentBatchMakeLogo_pixel_size     = 3;
+static const unsigned long image_EMSegmentBatchMakeLogo_length         = 228;
+static const unsigned long image_EMSegmentBatchMakeLogo_decoded_length = 4800;
+
+static const unsigned char image_EMSegmentBatchMakeLogo[] = 
+  "eNrt18ENgCAQRFEa8WYnVmhJ9uDdKjwrCYkhQdYBlp3E8DP3d57rGo3ec85R0HOf7GmKG9"
+  "AwS5rixqgZnaJEtzedQ7vSMkp0e9AISnR1aRxVpEtRRfeYZz8QXdbNr5F+0GeforEbo410"
+  "ir7qqdjiCmjs5tBqWnYDLaMV9Cfqh6BEF6d1UdBF0FIXoTu5Mg2iui6O1rk5muIWodVuSl"
+  "NcZ9648L/vBqXki6g=";
+
