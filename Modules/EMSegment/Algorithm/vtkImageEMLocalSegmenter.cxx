41e59efcd252db450c3526fff17d593d2ca5a537
Brad Davis <brad.davis@kitware.com>
2007-09-28 23:16:33 +0000

    ENH: added SLICER_HOME as input to doxygen configuration file
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=4472
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@4472 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/EMSegment/Algorithm/vtkImageEMLocalSegmenter.cxx
+++ Modules/EMSegment/Algorithm/vtkImageEMLocalSegmenter.cxx
@@ -661,6 +661,9 @@ static void vtkImageEMLocalSegmenterExecute(vtkImageEMLocalSegmenter *self,float
 // the datas data types.
 void vtkImageEMLocalSegmenter::ExecuteData(vtkDataObject *)
 {
+  std::cerr << "###vtkImageEMLocalSegmenter::ExecuteData" << std::endl;
+  this->PrintSelf(vtkstd::cerr, 0);
+
   cout << "Local Version" << endl;
   void *outPtr;
   int idx1, i;
