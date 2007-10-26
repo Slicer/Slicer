9d56304cf3f0a3cda33bb1d8b6ed0b310608378e
Nobuhiko Hata <hata@bwh.harvard.edu>
2007-10-26 01:48:44 +0000

    BUG: SVN demonstration
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=4713
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@4713 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/NeuroNav/vtkNeuroNavGUI.cxx
+++ Modules/NeuroNav/vtkNeuroNavGUI.cxx
@@ -570,10 +570,6 @@ void vtkNeuroNavGUI::AddGUIObservers ( )
     this->RegisterPushButton->AddObserver ( vtkKWPushButton::InvokedEvent, (vtkCommand *)this->GUICallbackCommand );
     this->ResetPushButton->AddObserver ( vtkKWPushButton::InvokedEvent, (vtkCommand *)this->GUICallbackCommand );
 
-    //
-    //
-    //
-
     this->LocatorCheckButton->AddObserver ( vtkKWCheckButton::SelectedStateChangedEvent, (vtkCommand *)this->GUICallbackCommand );
     this->LocatorModeCheckButton->AddObserver ( vtkKWCheckButton::SelectedStateChangedEvent, (vtkCommand *)this->GUICallbackCommand );
     this->UserModeCheckButton->AddObserver ( vtkKWCheckButton::SelectedStateChangedEvent, (vtkCommand *)this->GUICallbackCommand );
