89b4b945545cd7911751dd28261bd886d50bc1bf
Steve Pieper <pieper@bwh.harvard.edu>
2007-11-08 17:46:28 +0000

    BUG: support images where the pixel values all fall into a single histogram bucket
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=4785
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@4785 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Logic/vtkSlicerVolumesLogic.cxx
+++ Base/Logic/vtkSlicerVolumesLogic.cxx
@@ -530,21 +530,26 @@ void vtkSlicerVolumesLogic::CalculateScalarAutoLevels(vtkImageData *imageData, v
     bimodal->SetInput(accumulate->GetOutput());
     bimodal->Update();
 
-    // Workaround DT case that does not work with DTMath
+    // Workaround for image data where all accumulate samples fall
+    // within the same histogram bin
     if (bimodal->GetWindow() == 0.0 && bimodal->GetLevel() == 0.0) 
       {
-      double min = bimodal->GetMin();
-      double max = bimodal->GetMax();
+      double range[2];
+      imageDataScalar->GetScalarRange(range);
+      double min = range[0];
+      double max = range[1];
       displayNode->SetWindow (max-min);
       displayNode->SetLevel (0.5*(max+min));
+      displayNode->SetLowerThreshold (displayNode->GetLevel());
+      displayNode->SetUpperThreshold (range[1]);
       }
     else
       {
       displayNode->SetWindow (bimodal->GetWindow());
       displayNode->SetLevel (bimodal->GetLevel());
+      displayNode->SetLowerThreshold (bimodal->GetThreshold());
+      displayNode->SetUpperThreshold (bimodal->GetMax());
       }
-    displayNode->SetLowerThreshold (bimodal->GetThreshold());
-    displayNode->SetUpperThreshold (bimodal->GetMax());
 
     accumulate->Delete();
     bimodal->Delete();
