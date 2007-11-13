273e21fe6136c721df71992dbf0ffaef501f421f
Andreas Freudling <freudling@bwh.harvard.edu>
2007-11-13 21:57:21 +0000

    ENH: Delete volume rendering tcl prototype
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=4816
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@4816 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Testing/VolumeRendering/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-if {[catch {package require Itcl}]} { return }
-package ifneeded VolumeRendering 0.1 [list 
-  source [file join $dir VolumeRendering.tcl]
-  source [file join $dir VolumeRenderingNode.tcl]
-  source [file join $dir VolumeRenderingLogic.tcl]
-  source [file join $dir VolumeRenderingGUI.tcl]
-
-]
