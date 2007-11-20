8dd981a6e310c8ab22c0eb8f974f6b59b68cfde2
Steve Pieper <pieper@bwh.harvard.edu>
2007-11-20 21:10:57 +0000

    ENH: enhanced test for diffusion modes
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=4899
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@4899 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Tractography/Testing/TestGlyphs.tcl
+++ Modules/Tractography/Testing/TestGlyphs.tcl
@@ -1,25 +1,59 @@
 
-# source c:/pieper/bwh/slicer3/latest/slicer3/Modules/Tractography/Testing/testglyphs.tcl
-
-$::slicer3::MRMLScene Clear 0
-
-set gui [$::slicer3::Application GetModuleGUIByName DisplayLoadSave] 
-set logic [$gui GetLogic]
-
-set testData $::env(SLICER_HOME)/../Slicer3/Modules/Tractography/Testing/TestData/twoFibers.vtk
-
-set fbNode [$logic AddFiberBundle $testData]
-
-update
-
-set lineNode [$fbNode GetLineDisplayNode]
-set tubeNode [$fbNode GetTubeDisplayNode]
-set glyphNode [$fbNode GetGlyphDisplayNode]
+if {0} {
+  source c:/pieper/bwh/slicer3/latest/slicer3/Modules/Tractography/Testing/testglyphs.tcl
+}
 
 
+proc TestSceneClear {} {
+
+  for { set i 0 } { $i < 1 } { incr i } {
+    $::slicer3::MRMLScene Clear 0
+  }
+}
+
+# $::slicer3::Application TraceScript TestSceneClear
 
 
+proc bounceParam {code min max step} {
+  for {set param $min} {$param <= $max} {set param [expr $param + $step]} {
+    eval $code $param
+    update
+  }
+  for {set param $max} {$param >= $min} {set param [expr $param - $step]} {
+    eval $code $param
+    update
+  }
+}
 
 
+proc TestGlyphs {} {
 
+  $::slicer3::MRMLScene Clear 0
+
+  set ::gui [$::slicer3::Application GetModuleGUIByName DisplayLoadSave] 
+  set ::logic [$::gui GetLogic]
+
+  set testData $::env(SLICER_HOME)/../Slicer3/Modules/Tractography/Testing/TestData/twoFibers.vtk
+
+  set ::fbNode [$::logic AddFiberBundle $testData]
+
+  update
+
+  set ::lineNode [$::fbNode GetLineDisplayNode]
+  set ::tubeNode [$::fbNode GetTubeDisplayNode]
+  set ::glyphNode [$::fbNode GetGlyphDisplayNode]
+
+  $::tubeNode SetVisibility 1
+  $::glyphNode SetVisibility 1
+  set ::dtNode [$::glyphNode GetDTDisplayPropertiesNode]
+
+  bounceParam "$::tubeNode SetTubeRadius" 0.5 5.0 0.5
+  foreach mode "Lines Tubes Ellipsoids" {
+    $::dtNode SetGlyphGeometryTo$mode
+    bounceParam "$::dtNode SetGlyphScaleFactor" 50 500 20
+  }
+
+}
+
+$::slicer3::Application TraceScript TestGlyphs
 
