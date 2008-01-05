313156334beea3162a1747750738fff72affabfa
Steve Pieper <pieper@bwh.harvard.edu>
2008-01-05 22:20:16 +0000

    ENH: avoid event loops by flushing event queue after adding fiducial
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=5422
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@5422 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Tcl/FiducialsSWidget.tcl
+++ Base/GUI/Tcl/FiducialsSWidget.tcl
@@ -348,6 +348,9 @@ proc FiducialsSWidget::AddFiducial { r a s } {
   # the logic handles saving the state for undo
   set fidIndex [$fidLogic AddFiducialSelected $r $a $s 1]
   $::slicer3::MRMLScene Modified
+
+  # make sure everything gets updated before adding another fiducial
+  update
 }
 
 #
