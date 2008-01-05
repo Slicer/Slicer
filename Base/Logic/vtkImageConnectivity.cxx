69178b82de6f3711a2ee498c4038184301f9a86e
Steve Pieper <pieper@bwh.harvard.edu>
2008-01-05 22:21:24 +0000

    ENH: detect bad scalar types and bail out (rather than deleting volume)
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=5423
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@5423 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Logic/vtkImageConnectivity.cxx
+++ Base/Logic/vtkImageConnectivity.cxx
@@ -860,9 +860,8 @@ void vtkImageConnectivity::ExecuteData(vtkDataObject *)
   s = inData->GetScalarType();
   if (s != VTK_SHORT) 
     {
-    vtkErrorMacro("Input scalars are type "<<s 
+    vtkErrorMacro("Warning: Input scalars are type "<<s 
       << " instead of "<<VTK_SHORT);
-    return;
     }
 
   vtkImageConnectivityExecute(this, inData, (short *)inPtr, 
