f6b6977bdb8a922d7ef70f81b8c1c662712812c7
Steve Pieper <pieper@bwh.harvard.edu>
2008-01-15 17:11:37 +0000

    ENH: new test for event broker (currently broken, but being fixed)
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=5506
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@5506 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/GUI/Testing/EventBrokerTest.cxx
@@ -0,0 +1,58 @@
+/*=auto=========================================================================
+
+Portions (c) Copyright 2006 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+See Doc/copyright/copyright.txt
+or http://www.slicer.org/copyright/copyright.txt for details.
+
+Program:   3D Slicer
+Module:    $RCSfile$
+Date:      $Date: 2007-12-20 16:55:07 -0500 (Thu, 20 Dec 2007) $
+Version:   $Revision: 5304 $
+
+=========================================================================auto=*/
+
+#include "EventBrokerTestCLP.h"
+
+#include "vtkEventBroker.h"
+#include "vtkCommand.h"
+#include "vtkCallbackCommand.h"
+#include "vtkImageEllipsoidSource.h"
+#include "vtkImageViewer.h"
+
+//----------------------------------------------------------------------------
+void Callback(vtkObject *caller, unsigned long eid, void *clientData, void *callData)
+{
+  std::cerr << "Got an event " << eid << " from " << caller->GetClassName() << "\n";
+
+  vtkImageViewer *viewer = reinterpret_cast<vtkImageViewer *>(clientData);
+  viewer->Render();
+}
+
+int main(int argc, char * argv[])
+{
+  PARSE_ARGS;
+
+  vtkEventBroker *broker = vtkEventBroker::New();
+  vtkCallbackCommand *callback = vtkCallbackCommand::New();
+  vtkImageEllipsoidSource *ellip = vtkImageEllipsoidSource::New();
+  vtkImageViewer *viewer = vtkImageViewer::New();
+
+  viewer->SetColorWindow(200);
+  viewer->SetColorLevel(100);
+  viewer->SetInput( ellip->GetOutput() );
+
+  callback->SetCallback( Callback ); 
+  callback->SetClientData( reinterpret_cast<void *> (viewer) );
+
+  broker->AddObservation(ellip, vtkCommand::ModifiedEvent, viewer, callback);
+
+  ellip->Modified();
+
+  viewer->Delete();
+  ellip->Delete();
+  callback->Delete();
+  broker->Delete();
+
+  return (EXIT_SUCCESS);
+}
