02bef9f5a8c7b0eed0a177612477ca5549ab4bf0
Steve Pieper <pieper@bwh.harvard.edu>
2008-02-21 22:25:59 +0000

    ENH: use the model maker command line module to build models - very cool
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=5918
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@5918 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Editor/MakeModelEffect.tcl
+++ Modules/Editor/MakeModelEffect.tcl
@@ -36,6 +36,7 @@ if { [itcl::find class MakeModelEffect] == "" } {
     method apply {} {}
     method buildOptions {} {}
     method tearDownOptions {} {}
+    method goToModelMaker {} {}
 
   }
 }
@@ -63,55 +64,75 @@ itcl::body MakeModelEffect::processEvent { {caller ""} {event ""} } {
 
 itcl::body MakeModelEffect::apply {} {
 
-  # TODO: call the model maker
-  set sliceLogic [lindex [vtkSlicerSliceLogic ListInstances] 0]
-
   #
   # get the image data for the label layer
   #
+  set sliceLogic [lindex [vtkSlicerSliceLogic ListInstances] 0]
   set layerLogic [$sliceLogic GetLabelLayer]
   set volumeNode [$layerLogic GetVolumeNode]
   if { $volumeNode == "" } {
-    puts "cannot make quick model - no volume node for $layerLogic in $sliceLogic"
+    errorDialog "cannot make model - no volume node for $layerLogic in $sliceLogic"
     return
   }
-  set imageData [$volumeNode GetImageData]
 
   #
-  # make a poly data in RAS space
+  # set up the model maker
   #
-  set dsm [vtkDiscreteMarchingCubes New]
-  $dsm SetInput $imageData
-  set tpdf [vtkTransformPolyDataFilter New]
-  set ijkToRAS [vtkTransform New]
-  $volumeNode GetRASToIJKMatrix [$ijkToRAS GetMatrix] 
-  $ijkToRAS Inverse
-  $tpdf SetInput [$dsm GetOutput]
-  $tpdf SetTransform $ijkToRAS
-  $tpdf Update
+  set module [$::slicer3::MRMLScene CreateNodeByClass "vtkMRMLCommandLineModuleNode"]
+  $module SetModuleDescription "Model Maker"
+
+  $module SetParameterAsString "Name" "Quick Model"
+  $module SetParameterAsString "FilterType" "Sinc"
+  $module SetParameterAsBool "GenerateAll" "0"
+  $module SetParameterAsString "Labels" [EditorGetPaintLabel]
+  $module SetParameterAsBool "JointSmooth" 1
+  $module SetParameterAsBool "SplitNormals" 1
+  $module SetParameterAsBool "PointNormals" 1
+  $module SetParameterAsBool "SkipUnNamed" 1
+  $module SetParameterAsInt "Start" -1
+  $module SetParameterAsInt "End" -1
+  if { [[$o(smooth) GetWidget] GetSelectedState] } {
+    $module SetParameterAsDouble "Decimate" 0.25
+    $module SetParameterAsDouble "Smooth" 10
+  } else {
+    $module SetParameterAsDouble "Decimate" 0
+    $module SetParameterAsDouble "Smooth" 0
+  }
+
+  $module SetParameterAsString "InputVolume" [$volumeNode GetID]
 
   #
-  # create a mrml model for the new data
+  # output 
   #
-  set modelNode [vtkMRMLModelNode New]
-  set modelDisplayNode [vtkMRMLModelDisplayNode New]
-  $modelNode SetName "QuickModel"
-  $modelNode SetScene $::slicer3::MRMLScene
-  $modelDisplayNode SetScene $::slicer3::MRMLScene
-  eval $modelDisplayNode SetColor [lrange [EditorGetPaintColor $::Editor(singleton)] 0 2]
-  $::slicer3::MRMLScene AddNode $modelDisplayNode
-  $modelNode SetAndObserveDisplayNodeID [$modelDisplayNode GetID]
-  $::slicer3::MRMLScene AddNode $modelNode
-  $modelNode SetAndObservePolyData [$tpdf GetOutput]
+  set outHierarchy [$::slicer3::MRMLScene CreateNodeByClass "vtkMRMLModelHierarchyNode"]
+  $outHierarchy SetScene $::slicer3::MRMLScene
+  $outHierarchy SetName "Editor Models"
+  $::slicer3::MRMLScene AddNode $outHierarchy
+  $module SetParameterAsString "ModelSceneFile" [$outHierarchy GetID]
+
 
   #
-  # clean up
+  # create a logic to run the module
   #
-  $dsm Delete
-  $ijkToRAS Delete
-  $tpdf Delete
-  $modelNode Delete
-  $modelDisplayNode Delete
+  set logic [vtkCommandLineModuleLogic New]
+  $logic SetAndObserveMRMLScene $::slicer3::MRMLScene
+  $logic SetApplicationLogic [$::slicer3::ApplicationGUI GetApplicationLogic]
+  $logic SetTemporaryDirectory [$::slicer3::Application GetTemporaryDirectory]
+
+  # 
+  # run the task (in the background)
+  # - model will show up when the processing is finished
+  #
+  $logic Apply $module
+
+  $this statusText "Model Making Started..."
+
+  #
+  # clean up our references
+  #
+  $module Delete
+  $outHierarchy Delete
+  $logic Delete
 
 }
 
@@ -121,13 +142,32 @@ itcl::body MakeModelEffect::buildOptions {} {
   chain
 
   #
+  # go directly to model maker button
+  #
+  set o(goToModelMaker) [vtkNew vtkKWPushButton]
+  $o(goToModelMaker) SetParent [$this getOptionsFrame]
+  $o(goToModelMaker) Create
+  $o(goToModelMaker) SetText "Go To Model Maker"
+  $o(goToModelMaker) SetBalloonHelpString "The Model Maker interface contains a whole range of options for building sets of models and controlling the parameters."
+  pack [$o(goToModelMaker) GetWidgetName] \
+    -side top -anchor e -fill x -padx 2 -pady 2 
+
+  set o(smooth) [vtkKWCheckButtonWithLabel New]
+  $o(smooth) SetParent [$this getOptionsFrame]
+  $o(smooth) Create
+  $o(smooth) SetLabelText "Smooth Model: "
+  $o(smooth) SetBalloonHelpString "When smoothed, the model will look better, but some details of the label map will not be visible on the model.  When not smoothed you will see individual voxel boundaries in the model.  Smoothing here corresponds to Decimation of 0.25 and Smooting iterations of 10."
+  pack [$o(smooth) GetWidgetName] \
+    -side top -anchor e -fill x -padx 2 -pady 2 
+
+  #
   # an apply button
   #
   set o(apply) [vtkNew vtkKWPushButton]
   $o(apply) SetParent [$this getOptionsFrame]
   $o(apply) Create
   $o(apply) SetText "Apply"
-  $o(apply) SetBalloonHelpString "Build a model for the current label value of the label map being edited in the Red slice window."
+  $o(apply) SetBalloonHelpString "Build a model for the current label value of the label map being edited in the Red slice window.  Model will be created in the background."
   pack [$o(apply) GetWidgetName] \
     -side right -anchor e -padx 2 -pady 2 
 
@@ -158,13 +198,15 @@ itcl::body MakeModelEffect::buildOptions {} {
   #
   # event observers - TODO: if there were a way to make these more specific, I would...
   #
+  set tag [$o(goToModelMaker) AddObserver AnyEvent "$this goToModelMaker"]
+  lappend _observerRecords "$o(goToModelMaker) $tag"
   set tag [$o(apply) AddObserver AnyEvent "$this apply"]
   lappend _observerRecords "$o(apply) $tag"
   set tag [$o(cancel) AddObserver AnyEvent "after idle ::EffectSWidget::RemoveAll"]
   lappend _observerRecords "$o(cancel) $tag"
 
-  if { [$this getInputBackground] == "" || [$this getOutputLabel] == "" } {
-    $this errorDialog "Background and Label map needed for MakeModeling"
+  if { [$this getOutputLabel] == "" } {
+    $this errorDialog "Label map needed for MakeModeling"
     after idle ::EffectSWidget::RemoveAll
   }
 
@@ -176,10 +218,16 @@ itcl::body MakeModelEffect::tearDownOptions { } {
   # call superclass version of tearDownOptions
   chain
 
-  foreach w "help cancel apply" {
+  foreach w "smooth goToModelMaker help cancel apply" {
     if { [info exists o($w)] } {
       $o($w) SetParent ""
       pack forget [$o($w) GetWidgetName] 
     }
   }
+}
+
+itcl::body MakeModelEffect::goToModelMaker { } {
+  set toolbar [$::slicer3::ApplicationGUI GetApplicationToolbar]
+  [$toolbar GetModuleChooseGUI] SelectModule "Model Maker"
+  after idle ::EffectSWidget::RemoveAll
 }
