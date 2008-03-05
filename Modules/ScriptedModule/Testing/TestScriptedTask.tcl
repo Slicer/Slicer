d19068130500df1a59b6efc193c86d27953cd6a7
Steve Pieper <pieper@bwh.harvard.edu>
2008-03-05 22:36:24 +0000

    ENH: debugging threaded access
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6085
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6085 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/ScriptedModule/Testing/TestScriptedTask.tcl
+++ Modules/ScriptedModule/Testing/TestScriptedTask.tcl
@@ -6,22 +6,27 @@
 #
 proc filterVolumeInThread { volumeNode } {
 
+  if 0 {
   set filter [vtkITKGradientAnisotropicDiffusionImageFilter New]
-  $filter SetInput [$volumeNode GetImageData]
   $filter SetTimeStep 0.0625
   $filter SetConductanceParameter 1
   $filter SetNumberOfIterations 14
   $filter Update
+  }
+
+  set filter [vtkImageAnisotropicDiffusion3D New]
+  $filter SetNumberOfIterations 50
+  $filter SetDiffusionFactor 30
+  $filter SetInput [$volumeNode GetImageData]
+  [$filter GetOutput] Update
 
   $volumeNode SetDisableModifiedEvent 1
   $volumeNode SetAndObserveImageData [$filter GetOutput]
   $volumeNode SetDisableModifiedEvent 0
-  $filter SetOutput ""
+#  $filter SetOutput ""
   $filter Delete
 
   $::slicer3::ApplicationLogic RequestModified $volumeNode
-
-  exec rxvt &
 }
 
 
@@ -32,6 +37,8 @@ set selNode [$::slicer3::ApplicationLogic GetSelectionNode]
 $selNode SetReferenceActiveVolumeID [$volumeNode GetID]
 $::slicer3::ApplicationLogic PropagateVolumeSelection
 
+update
+
 
 set task [vtkScriptedTask New]
 set tag [$task AddObserver AnyEvent ""]
@@ -39,10 +46,15 @@ $task SetInterpFromCommand $tag
 $task RemoveObserver $tag
 $task SetScript "filterVolumeInThread $volumeNode"
 
-[$::slicer3::ApplicationGUI GetApplicationLogic] ScheduleTask $task
+set threading 1
+if { $threading } {
+  [$::slicer3::ApplicationGUI GetApplicationLogic] ScheduleTask $task
+  puts "$task Started"
+  #$task Delete
+} else {
+  filterVolumeInThread $volumeNode
+}
 
-#$task Delete
-puts "$task Started"
 
 if 0 {
   source c:/pieper/bwh/slicer3/vtkcvshead/Slicer3/Modules/ScriptedModule/Testing/TestScriptedTask.tcl
