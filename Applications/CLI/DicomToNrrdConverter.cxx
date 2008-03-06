b9cb75b5ced582f49332823d4c9a6ac45cd78a14
Xiaodong Tao <taox@research.ge.com>
2008-03-06 18:05:50 +0000

    ENH: works for Siemens data for both mosaic and split formats. TODO: when input is siemens mosaic image, need to figure out the slice order in each mosaic.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6101
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6101 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/CLI/DicomToNrrdConverter.cxx
+++ Applications/CLI/DicomToNrrdConverter.cxx
@@ -100,24 +100,51 @@ typedef itk::ImageSeriesReader< VolumeType > ReaderType;
 typedef itk::GDCMImageIO ImageIOType;
 typedef itk::GDCMSeriesFileNames InputNamesGeneratorType;
 
+void InsertUnique( std::vector<float> & vec, float value )
+{
+  int n = vec.size();
+  if (n == 0)
+    {
+    vec.push_back( value );
+    return;
+    }
+
+  for (int k = 0; k < n ; k++)
+    {
+    if (vec[k] == value)
+      {
+      return;
+      }
+    }
+
+  // if we get here, it means value is not in vec.
+  vec.push_back( value );
+  return;
+  
+}
+
 int main(int argc, char* argv[])
 {
 
   PARSE_ARGS;
 
+  bool SliceOrderIS = true;
+  std::string vendor;
+  bool SliceMosaic = false;
+
   // check if the file name is valid
   std::string nhdrname = outputFileName;
   std::string dataname;
   {
-    int i;
-    i = nhdrname.find(".nhdr");
-    if (i == std::string::npos)
+  int i;
+  i = nhdrname.find(".nhdr");
+  if (i == std::string::npos)
     {
-      std::cerr << "Output file must be a nrrd header file.\n";
-      std::cerr << "Version:   $Revision: 1.2 $" << std::endl;
-      return EXIT_FAILURE;
+    std::cerr << "Output file must be a nrrd header file.\n";
+    std::cerr << "Version:   $Revision: 1.2 $" << std::endl;
+    return EXIT_FAILURE;
     }
-    dataname = nhdrname.substr(0, i) + ".raw";
+  dataname = nhdrname.substr(0, i) + ".raw";
   }
 
   //////////////////////////////////////////////////  
@@ -138,50 +165,65 @@ int main(int argc, char* argv[])
   sReader->SetImageIO( gdcmIO );
   sReader->SetFileName( filenames[0] );
   try
-  {
+    {
     sReader->Update();
-  }
+    }
   catch (itk::ExceptionObject &excp)
-  {
+    {
     std::cerr << "Exception thrown while reading the first file in the series" << std::endl;
     std::cerr << excp << std::endl;
     return EXIT_FAILURE;
-  }
+    }
 
   // check the tag 0008|0070 for vendor information
   itk::MetaDataDictionary sliceDict = sReader->GetMetaDataDictionary();
-  std::string vendor;
 
   itk::ExposeMetaData<std::string> ( sliceDict, "0008|0070", vendor );
   std::cout << vendor << std::endl;
 
+  std::string ImageType;
+  itk::ExposeMetaData<std::string> ( sliceDict, "0008|0008", ImageType );
+  std::cout << ImageType << std::endl;
+
   //////////////////////////////////////////////////////////////////
   //  0b) Add private dictionary
   if ( vendor.find("GE") != std::string::npos )
-  {
+    {
     // for GE data
     gdcm::Global::GetDicts()->GetDefaultPubDict()->AddEntry(GEDictBValue);
     gdcm::Global::GetDicts()->GetDefaultPubDict()->AddEntry(GEDictXGradient);
     gdcm::Global::GetDicts()->GetDefaultPubDict()->AddEntry(GEDictYGradient);
     gdcm::Global::GetDicts()->GetDefaultPubDict()->AddEntry(GEDictZGradient);
-  }
+    }
   else if( vendor.find("SIEMENS") != std::string::npos )
-  {
+    {
     // for Siemens data
     gdcm::Global::GetDicts()->GetDefaultPubDict()->AddEntry(SiemensMosiacParameters);
     gdcm::Global::GetDicts()->GetDefaultPubDict()->AddEntry(SiemensDictNMosiac);
     gdcm::Global::GetDicts()->GetDefaultPubDict()->AddEntry(SiemensDictBValue);
     gdcm::Global::GetDicts()->GetDefaultPubDict()->AddEntry(SiemensDictDiffusionDirection);
     gdcm::Global::GetDicts()->GetDefaultPubDict()->AddEntry(SiemensDictDiffusionMatrix);
-  }
+
+    if ( ImageType.find("MOSAIC") != std::string::npos )
+      {
+      std::cout << "Siemens Mosaic format\n";
+      SliceMosaic = true;
+      }
+    else
+      {
+      std::cout << "Siemens split format\n";
+      SliceMosaic = false;
+      }
+
+    }
   else if( vendor.find("PHILIPS") != std::string::npos )
-  {
-  // for philips data
-  }
+    {
+    // for philips data
+    }
   else
-  {
+    {
     std::cerr << "Unrecognized vendor.\n" << std::endl;
-  }
+    }
 
   //////////////////////////////////////////////////  
   // 1) Read the input series as an array of slices
@@ -189,15 +231,15 @@ int main(int argc, char* argv[])
   reader->SetImageIO( gdcmIO );
   reader->SetFileNames( filenames );
   try
-  {
+    {
     reader->Update();
-  }
+    }
   catch (itk::ExceptionObject &excp)
-  {
+    {
     std::cerr << "Exception thrown while reading the series" << std::endl;
     std::cerr << excp << std::endl;
     return EXIT_FAILURE;
-  }
+    }
 
   /////////////////////////////////////////////////////
   // 2) Analyze the DICOM header to determine the 
@@ -205,14 +247,6 @@ int main(int argc, char* argv[])
   //    vectors, and form volume based on this info
 
   ReaderType::DictionaryArrayRawPointer inputDict = reader->GetMetaDataDictionaryArray();
-
-  /// for debug. to check what tags have been exposed
-  itk::MetaDataDictionary & debugDict = gdcmIO->GetMetaDataDictionary();
-  std::vector<std::string> allKeys = debugDict.GetKeys();
-  for (int k = 0; k < allKeys.size(); k++)
-    {
-//     std::cout << allKeys[k] << std::endl;
-    }
 
   // load in all public tags
   int nSlice = inputDict->size();   
@@ -247,80 +281,85 @@ int main(int argc, char* argv[])
   itk::ExposeMetaData<std::string> ( *(*inputDict)[0], "0018|0088", tag );
   float sliceSpacing = atof( tag.c_str() );
 
-  tag.clear();
-  itk::ExposeMetaData<std::string> ( *(*inputDict)[0], "0020|1041", tag );  
-  float maxSliceLocation = atof( tag.c_str() );
-  float minSliceLocation = maxSliceLocation;
-
-  // figure out the largest and smallest slice location. This does not
-  // work for Siemens data since it is stored in mosaic format
+  // figure out how many slices are there in a volume, each unique
+  // SliceLocation represent one slice 
+  std::vector<float> sliceLocations(0);
   for (int k = 0; k < nSlice; k++)
-  {
+    {
     tag.clear();
     itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0020|1041",  tag);
     float sliceLocation = atof( tag.c_str() );
-
-    if (sliceLocation > maxSliceLocation)
-    {
-      maxSliceLocation = sliceLocation;
-    }
-
-    if (sliceLocation < minSliceLocation)
-    {
-      minSliceLocation = sliceLocation;
-    }
-  }    
-
+    InsertUnique( sliceLocations, sliceLocation );
+    }    
+  
+  
+  
   // check ImageOrientationPatient and figure out slice direction in
   // L-P-I (right-handed) system.
   // In Dicom, the coordinate frame is L-P by default. Look at
   // http://medical.nema.org/dicom/2007/07_03pu.pdf ,  page 301
   tag.clear();
   itk::ExposeMetaData<std::string> ( *(*inputDict)[0], "0020|0037", tag );
-  float xRow, yRow, zRow, xCol, yCol, zCol, xSlice, ySlice, zSlice;
+  float xRow, yRow, zRow, xCol, yCol, zCol, xSlice, ySlice, zSlice, orthoSliceSpacing;
   sscanf( tag.c_str(), "%f\\%f\\%f\\%f\\%f\\%f", &xRow, &yRow, &zRow, &xCol, &yCol, &zCol );
-  // Cross product, this gives I-axis direction
-  xSlice = (yRow*zCol-zRow*yCol)*sliceSpacing;
-  ySlice = (zRow*xCol-xRow*zCol)*sliceSpacing;
-  zSlice = (xRow*yCol-yRow*xCol)*sliceSpacing;
-
+  std::cout << "ImageOrientationPatient (0020:0037): ";
+  std::cout << xRow << ", " << yRow << ", " << zRow << "; ";
+  std::cout << xCol << ", " << yCol << ", " << zCol << "\n";
+  
   // In Dicom, the measurement frame is L-P by default. Look at
   // http://medical.nema.org/dicom/2007/07_03pu.pdf ,  page 301, in
   // order to make this compatible with Slicer's RAS frame, we
   // multiply the direction cosines by the negatives of the resolution
   // (resolution is required by nrrd format). Direction cosine is not
   // affacted since the resulting frame is still a right-handed frame.
-  xRow *= -xRes;
-  yRow *= -xRes;
-  zRow *= -xRes;
+  xRow = -xRow;
+  yRow = -yRow;
 
-  xCol *= -yRes;
-  yCol *= -yRes;
-  zCol *= -yRes;
+  xCol = -xCol;
+  yCol = -yCol;
 
+  // Cross product, this gives I-axis direction
+  xSlice = (yRow*zCol-zRow*yCol)*sliceSpacing;
+  ySlice = (zRow*xCol-xRow*zCol)*sliceSpacing;
+  zSlice = (xRow*yCol-yRow*xCol)*sliceSpacing;
+
+  xRow *= xRes;
+  yRow *= xRes;
+  zRow *= xRes;
+
+  xCol *= yRes;
+  yCol *= yRes;
+  zCol *= yRes;
+
+  std::cout << "Row: " << xRow << ", " << yRow << ", " << zRow << std::endl;
+  std::cout << "Col: " << xCol << ", " << yCol << ", " << zCol << std::endl;
+  std::cout << "Sli: " << xSlice << ", " << ySlice << ", " << zSlice << std::endl;
+    
   // figure out slice order
-  bool SliceOrderIS = true;
-  if ( vendor.find("GE") != std::string::npos )
-  {
+  if ( vendor.find("GE") != std::string::npos ||
+       (vendor.find("SIEMENS") != std::string::npos && !SliceMosaic) )
+    {
     float x0, y0, z0;
     float x1, y1, z1;
     tag.clear();
     itk::ExposeMetaData<std::string> ( *(*inputDict)[0], "0020|0032", tag );
     sscanf( tag.c_str(), "%f\\%f\\%f", &x0, &y0, &z0 );
+    std::cout << "Slice 0: " << tag << std::endl;
     tag.clear();
 
-    // assume volume interleving, i.e. the second dicom file stores
+    // assume volume interleaving, i.e. the second dicom file stores
     // the second slice in the same volume as the first dicom file
     itk::ExposeMetaData<std::string> ( *(*inputDict)[1], "0020|0032", tag );
     sscanf( tag.c_str(), "%f\\%f\\%f", &x1, &y1, &z1 );
+    std::cout << "Slice 1: " << tag << std::endl;
     x1 -= x0; y1 -= y0; z1 -= z0;
     x0 = x1*xSlice + y1*ySlice + z1*zSlice;
     if (x0 < 0)
-    {
-    SliceOrderIS = false;
+      {
+      SliceOrderIS = false;
+      }
     }
-  }
-  else if ( vendor.find("SIEMENS") != std::string::npos )
+  else if ( vendor.find("SIEMENS") != std::string::npos && SliceMosaic )
     {
     // for siemens mosaic image, we have not figured out the slice
     // order yet, from the example provided by Vince, the slice order
@@ -331,20 +370,31 @@ int main(int argc, char* argv[])
     {
     }
   
+  if ( SliceOrderIS )
+    {
+    std::cout << "Slice order is IS\n";
+    }
+  else
+    {
+    std::cout << "Slice order is SI\n";
+    }
+
+
   if (!SliceOrderIS)
-  {
+    {
     xSlice = -xSlice;
     ySlice = -ySlice;
     zSlice = -zSlice;
-  }
+    }
 
+  orthoSliceSpacing = fabs(zSlice);
+  
   int nSliceInVolume;
   int nVolume;
 
-  float bValue;
+  std::vector<float> bValues(0);
   int nBaseline = 0;
-  int nMeasurement; 
-  std::vector< int > idVolume;
+
   std::vector< vnl_vector_fixed<double, 3> > DiffusionVectors;
   ////////////////////////////////////////////////////////////
   // vendor dependent tags.
@@ -352,44 +402,28 @@ int main(int argc, char* argv[])
 
   if ( vendor.find("GE") != std::string::npos )
     {
-    nSliceInVolume = static_cast<int> ((maxSliceLocation - minSliceLocation) / fabs(zSlice) + 1.5 ); 
-    // .5 is for rounding up, 1 is for adding one slice at one end.
+    nSliceInVolume = sliceLocations.size();
     nVolume = nSlice/nSliceInVolume;
 
-    for (int k = 0; k < nSlice; k += nSliceInVolume)
-      {
-      tag.clear();
-      itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0043|1039",  tag);
-      float b = atof( tag.c_str() );
-      if (b == 0)
-        {
-        nBaseline ++;
-        }
-      else
-        {
-        bValue = b;
-        }
-      }    
-
-    nMeasurement = nVolume - nBaseline;
-
-    // extract gradient vectors and form DWImages
-    vnl_vector_fixed<double, 3> vect3d;
-    idVolume.resize( nMeasurement );
-    int count = 0;
+    // assume volume interleaving
+    std::cout << "Number of Slices: " << nSlice << std::endl;
+    std::cout << "Number of Volume: " << nVolume << std::endl;
+    std::cout << "Number of Slices in each volume: " << nSliceInVolume << std::endl;
 
     for (int k = 0; k < nSlice; k += nSliceInVolume)
       {
       tag.clear();
       itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0043|1039",  tag);
       float b = atof( tag.c_str() );
+      bValues.push_back(b);
+
+      vnl_vector_fixed<double, 3> vect3d;
       if (b == 0)
         {
+        vect3d.fill( 0 );
+        DiffusionVectors.push_back(vect3d);      
         continue;
         }
-
-      idVolume[count] = k/nSliceInVolume;
-      count ++;
 
       tag.clear();
       itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0019|10bb",  tag);
@@ -398,79 +432,102 @@ int main(int argc, char* argv[])
       tag.clear();
       itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0019|10bc",  tag);
       vect3d[1] = atof( tag.c_str() );
-
+  
       tag.clear();
       itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0019|10bd",  tag);
       vect3d[2] = atof( tag.c_str() );
-
+  
       DiffusionVectors.push_back(vect3d);      
       }
     }
-  else if ( vendor.find("SIEMENS") != std::string::npos )
+  else if ( vendor.find("SIEMENS") != std::string::npos && !SliceMosaic )
     {
-    // each slice is a volume in mosiac form
-    nVolume = nSlice;
-    nSliceInVolume = 1;
-    for ( int k = 0; k < nSlice; k += nSliceInVolume )
+    std::cout << orthoSliceSpacing << std::endl;
+    
+    nSliceInVolume = sliceLocations.size();
+    nVolume = nSlice/nSliceInVolume;
+
+    std::cout << "Number of Slices: " << nSlice << std::endl;
+    std::cout << "Number of Volume: " << nVolume << std::endl;
+    std::cout << "Number of Slices in each volume: " << nSliceInVolume << std::endl;
+    
+    for (int k = 0; k < nSlice; k += nSliceInVolume)
       {
       tag.clear();
       itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0019|100c",  tag);
       float b = atof( tag.c_str() );
+      bValues.push_back(b);
+
+      vnl_vector_fixed<double, 3> vect3d;
       if (b == 0)
         {
-        nBaseline ++;
-        }
-      else
-        {
-        bValue = b;
-        }
-      }    
-
-    nMeasurement = nVolume - nBaseline;
-
-    // extract gradient vectors and form DWImages
-    vnl_vector_fixed<double, 3> vect3d;
-    idVolume.resize( nMeasurement );
-    int count = 0;
-
-    for ( int k = 0; k < nSlice; k += nSliceInVolume )
-      {
-      tag.clear();
-      itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0019|100c",  tag);
-      float b = atof( tag.c_str() );
-      if (b == 0)
-        {
+        vect3d.fill( 0 );
+        DiffusionVectors.push_back(vect3d);      
         continue;
         }
-
-      idVolume[count] = k/nSliceInVolume;
-      count ++;
 
       tag.clear();
       itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0019|100e",  tag);
       memcpy( &vect3d[0], tag.c_str()+0, 8 );
       memcpy( &vect3d[1], tag.c_str()+8, 8 );
       memcpy( &vect3d[2], tag.c_str()+16, 8 );
+    
+      DiffusionVectors.push_back(vect3d);
 
-      DiffusionVectors.push_back(vect3d);      
+      std::cout << "Dicom Image Number: " << k << " B Value; " << b << " Gradient direction: " << vect3d << std::endl;
+      
       }
+    }
+  else if ( vendor.find("SIEMENS") != std::string::npos && SliceMosaic )
+    {
+    // each slice is a volume in mosiac form
+    std::cout << "Data in Siemens Mosaic Format\n";
+    nVolume = nSlice;
+    nSliceInVolume = 1;
 
+    std::cout << "Number of Slices: " << nSlice << std::endl;
+    std::cout << "Number of Volume: " << nVolume << std::endl;
+    std::cout << "Number of Slices in each volume: " << nSliceInVolume << std::endl;
+    
+    for ( int k = 0; k < nSlice; k += nSliceInVolume )
+      {
+      tag.clear();
+      itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0019|100c",  tag);
+      
+      float b = atof( tag.c_str() );
+      bValues.push_back(b);
+
+      vnl_vector_fixed<double, 3> vect3d;
+      if (b == 0)
+        {
+        vect3d.fill( 0 );
+        DiffusionVectors.push_back(vect3d);      
+        continue;
+        }
+
+      tag.clear();
+      itk::ExposeMetaData<std::string> ( *(*inputDict)[k], "0019|100e",  tag);
+      
+      memcpy( &vect3d[0], tag.c_str()+0, 8 );
+      memcpy( &vect3d[1], tag.c_str()+8, 8 );
+      memcpy( &vect3d[2], tag.c_str()+16, 8 );
+    
+      DiffusionVectors.push_back(vect3d);      
+      std::cout << "Dicom Image Number: " << k << " B Value; " << b << " Gradient direction: " << vect3d << std::endl;
+      }
+      
+    }
+  else
+    {
     }
   // transform gradient directions into RAS frame 
 
-  for (int k = 0; k < nMeasurement; k++)
+  for (int k = 0; k < nVolume; k++)
     {
-    vnl_vector_fixed<double, 3> vecTemp = DiffusionVectors[k];
-    //                                                   Dicom   Slicer 
-    DiffusionVectors[k][0] = -DiffusionVectors[k][0];    // L -> R
-    DiffusionVectors[k][1] = -DiffusionVectors[k][1];    // P -> A
-    if (vendor.find("GE") != std::string::npos )
+    if ( !SliceOrderIS )
       {
       DiffusionVectors[k][2] = -DiffusionVectors[k][2];  // I -> S
       }
-    
-    DiffusionVectors[k].normalize();
-    std::cout << idVolume[k] << "\t"<< DiffusionVectors[k] << std::endl;
     }
 
   ///////////////////////////////////////////////
@@ -482,7 +539,8 @@ int main(int argc, char* argv[])
   rawWriter->SetImageIO( rawIO );
   rawIO->SetByteOrderToLittleEndian();
 
-  if ( vendor.find("GE") != std::string::npos )
+  if ( vendor.find("GE") != std::string::npos ||
+       (vendor.find("SIEMENS") != std::string::npos && !SliceMosaic) )
     {
     rawWriter->SetInput( reader->GetOutput() );
     try
@@ -496,7 +554,7 @@ int main(int argc, char* argv[])
       return EXIT_FAILURE;
       }
     }
-  else if ( vendor.find("SIEMENS") != std::string::npos )
+  else if ( vendor.find("SIEMENS") != std::string::npos && SliceMosaic)
     {
     // de-mosaic
     tag.clear();
@@ -608,9 +666,9 @@ int main(int argc, char* argv[])
 
   // need to check
   header << "space directions: " 
-    << "(" << xRow << ","<< yRow << ","<< zRow << ") "
-    << "(" << xCol << ","<< yCol << ","<< zCol << ") "
-    << "(" << xSlice << ","<< ySlice << ","<< zSlice << ") none" << std::endl;
+         << "(" << xRow << ","<< yRow << ","<< zRow << ") "
+         << "(" << xCol << ","<< yCol << ","<< zCol << ") "
+         << "(" << xSlice << ","<< ySlice << ","<< zSlice << ") none" << std::endl;
 
   header << "centerings: cell cell cell ???" << std::endl;
   header << "kinds: space space space list" << std::endl;
@@ -618,23 +676,34 @@ int main(int argc, char* argv[])
   header << "encoding: raw" << std::endl;
   header << "space units: \"mm\" \"mm\" \"mm\"" << std::endl;
   header << "space origin: "
-    <<"(" << xOrigin << ","<< yOrigin << ","<< zOrigin << ") " << std::endl;
+         <<"(" << xOrigin << ","<< yOrigin << ","<< zOrigin << ") " << std::endl;
   header << "data file: " << dataname << std::endl;
 
   // Need to check
   header << "measurement frame: (1,0,0) (0,1,0) (0,0,1)" << std::endl;
 
   header << "modality:=DWMRI" << std::endl;
+  float bValue = 0;
+  for (int k = 0; k < nVolume; k++)
+    {
+    if (bValues[k] > 0)
+      {
+      bValue = bValues[k];
+      break;
+      }
+    }
+  
   header << "DWMRI_b-value:=" << bValue << std::endl;
-  header << "DWMRI_gradient_0000:=0  0  0" << std::endl;
-  header << "DWMRI_NEX_0000:=" << nBaseline << std::endl;
-  // need to check
-  for (int k = nBaseline; k < nVolume; k++)
-  {
+
+  //  header << "DWMRI_gradient_0000:=0  0  0" << std::endl;
+  //  header << "DWMRI_NEX_0000:=" << nBaseline << std::endl;
+  //  need to check
+  for (int k = 0; k < nVolume; k++)
+    {
     header << "DWMRI_gradient_" << std::setw(4) << std::setfill('0') << k << ":=" 
-      << DiffusionVectors[k-nBaseline][0] << "   " << DiffusionVectors[k-nBaseline][1] << "   " << DiffusionVectors[k-nBaseline][2]
-      << std::endl;
-  }
+           << DiffusionVectors[k-nBaseline][0] << "   " << DiffusionVectors[k-nBaseline][1] << "   " << DiffusionVectors[k-nBaseline][2]
+           << std::endl;
+    }
   header.close();
 
   return EXIT_SUCCESS;  
