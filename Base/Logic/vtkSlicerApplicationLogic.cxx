bf3a2e303f6dcf25076bd90db5bf5f970fbdd685
Nicole Aucoin <nicole@bwh.harvard.edu>
2008-04-03 20:42:22 +0000

    BUG: test for null strings
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6391
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6391 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Logic/vtkSlicerApplicationLogic.cxx
+++ Base/Logic/vtkSlicerApplicationLogic.cxx
@@ -1049,7 +1049,7 @@ void vtkSlicerApplicationLogic::ProcessReadNodeData(ReadDataRequest& req)
       vtkMRMLStorageNode *testStorageNode = storableNode->GetNthStorageNode(n);
       if (testStorageNode)
         {
-        if (useURI)
+            if (useURI && testStorageNode->GetURI() != NULL)
           {
           if (req.GetFilename().compare(testStorageNode->GetURI()) == 0)
             {
@@ -1060,7 +1060,8 @@ void vtkSlicerApplicationLogic::ProcessReadNodeData(ReadDataRequest& req)
             break;
             }
           }
-        else if (req.GetFilename().compare(testStorageNode->GetFileName()) == 0)
+            else if (testStorageNode->GetFileName() != NULL &&
+                     req.GetFilename().compare(testStorageNode->GetFileName()) == 0)
           {
           // found the right storage node for a local file
           vtkDebugMacro("ProcessReadNodeData: found a storage node with the right filename: " << testStorageNode->GetFileName());
