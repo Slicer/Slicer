46e27848c473b95645225f113bc9c039544ca9a8
Steve Pieper <pieper@bwh.harvard.edu>
2008-04-22 19:55:05 +0000

    BUG: back out removal of allocation of output data (keep fix for warning about unused variable)
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6518
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6518 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Logic/vtkImageFillROI.cxx
+++ Base/Logic/vtkImageFillROI.cxx
@@ -765,7 +765,7 @@ void vtkImageFillROI::ExecuteData(vtkDataObject *out)
     return;
     }
     
-  // vtkImageData *outData = this->AllocateOutputData(out); UNUSED
+  this->AllocateOutputData(out);
 
   if ( this->GetInput()->GetDataObjectType() != VTK_IMAGE_DATA )
     {
