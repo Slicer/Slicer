2a34defaa2e7b5ce4ca877ad4d8b45801ec84a20
Kerstin Kessel <kerstin@bwh.harvard.edu>
2008-04-27 18:22:19 +0000

    ENH: added testing environment in volumes modul and first DiffusionEditor ctest
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6556
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6556 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/Volumes/Testing/DiffusionEditorTest.tcl
@@ -0,0 +1,24 @@
+vtkSlicerApplication app
+vtkMRMLScene scene
+
+vtkKWTopLevel t
+t SetApplication app
+t Create
+
+vtkSlicerGradientEditorWidget editor
+editor SetParent t
+editor SetAndObserveMRMLScene scene
+editor Create
+pack [editor GetWidgetName] -side top -anchor nw -fill x -padx 2 -pady 2
+t Display
+
+update
+after 2000
+
+
+t Delete
+editor Delete
+scene Delete
+app Delete
+
+exit 0
