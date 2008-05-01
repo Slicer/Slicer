96619392073c406575d8b8e92bd76816a7d851f2
Sebastien BARRE <sebastien.barre@kitware.com>
2008-05-01 21:17:04 +0000

    ENH: recreating the dashboard as Slicer3 to fix update pb
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6588
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6588 3bd1e089-480b-0410-8dfb-8563597acbee

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,5 +3,5 @@ set(CTEST_NIGHTLY_START_TIME "2:00:00 EDT")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Slicer")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Slicer3")
 set(CTEST_DROP_SITE_CDASH TRUE)
