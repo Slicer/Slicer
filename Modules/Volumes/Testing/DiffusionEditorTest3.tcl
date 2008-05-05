8ea6a928b1bcf56c4c69c588c2bd0925a5739162
Kerstin Kessel <kerstin@bwh.harvard.edu>
2008-05-05 23:08:34 +0000

    ENH: added last test
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6620
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6620 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/Volumes/Testing/DiffusionEditorTest3.tcl
@@ -0,0 +1,40 @@
+
+set sceneFileName $::env(SLICER_HOME)/../Slicer3/Libs/MRML/Testing/diffusionEditor.mrml
+
+$::slicer3::MRMLScene SetURL $sceneFileName
+$::slicer3::MRMLScene Connect
+update
+
+set volumesGUI [$::slicer3::Application GetModuleGUIByName Volumes] 
+$volumesGUI Enter
+
+set editor [$volumesGUI GetDiffusionEditorWidget] 
+
+$editor UpdateWidget [$::slicer3::MRMLScene GetNthNodeByClass 0 vtkMRMLDiffusionWeightedVolumeNode]
+
+  
+set testWidget [$::editor  GetTestingWidget] 
+
+
+$testWidget RunTensor
+
+$testWidget SetGlyphVisibility 0 1
+
+$testWidget SetGlyphVisibility 1 1
+
+$testWidget SetGlyphVisibility 2 1
+
+
+$testWidget SetGlyphVisibility 0 0
+
+$testWidget SetGlyphVisibility 1 0
+
+$testWidget SetGlyphVisibility 2 0
+
+[$testWidget GetFiducialSelector] SetSelected [$::slicer3::MRMLScene GetNthNodeByClass 0 vtkMRMLFiducialListNode]
+
+$testWidget SetTractVisibility 1
+
+
+exit 0
+
