0768b71d341b6983f39ca78a00579300c91f8a0a
Nicole Aucoin <nicole@bwh.harvard.edu>
2008-05-16 22:43:34 +0000

    BUG: modifications to work with automated testing system as well as direct source into running slicer
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6746
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6746 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/Testing/testFiducialAdd.tcl
+++ Libs/MRML/Testing/testFiducialAdd.tcl
@@ -1,10 +1,29 @@
 # test adding 200 fiducials to a new list
+
+# set exit on completion flag to 1 if using it in an automated test with -e source testFiducialAdd.tcl, as it will have to exit once it's done so as not to hang up testing. 
+
+# if source it into slicer, this probably won't be set yet
+if {[info exists ::exitOnCompletion] == 0} {
+    set ::exitOnCompletion 0
+}
+
 # renameFlag int set to 1 if want to test time renaming
 # visibilityFlag int set to 0 if wish to see how fast it works w/o the 3d display widget in the way
 # numToAdd int number of fiducials to add
-proc TestFiducialAdd { {renameFlag 1} {visibilityFlag 1} {numToAdd 100} } {
-    set fidLogic [ $::slicer3::FiducialsGUI GetLogic ]
-    set fidList [$fidLogic AddFiducialList]
+proc TestFiducialAdd { {renameFlag 1} {visibilityFlag 1} {numToAdd 20} } {
+    # check to see if we were sourced into slicer
+    if {[info exists ::slicer3::MRMLScene] == 1} {
+        set fidLogic [ $::slicer3::FiducialsGUI GetLogic ]
+        set fidList [$fidLogic AddFiducialList]
+    } else {
+        # can get here via automated add_test -f script.tcl
+        # but: this doesn't engage the fiducial list widget and is misleadingly fast
+        catch "sc Delete"
+        vtkMRMLScene sc
+        set fidList [vtkMRMLFiducialListNode fidNode]
+        sc AddNode $fidList
+    }
+
     if {$visibilityFlag} {
         # Note: May 16/08 w/o setting the visibility to 1 explictly, the 3d widgets don't show up, and this test runs much faster
         $fidList SetVisibility 1
@@ -46,6 +65,9 @@ proc TestFiducialAdd { {renameFlag 1} {visibilityFlag 1} {numToAdd 100} } {
     }
     $fidList DisableModifiedEventOff
     $fidList Modified
-} 
+}
 time "TestFiducialAdd"
 
+if {$::exitOnCompletion} {
+    exit
+}
