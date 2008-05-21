2d37a988dc10c29b0fcf2f3297dc3f2b4b6697ea
Sebastien BARRE <sebastien.barre@kitware.com>
2008-05-21 17:53:26 +0000

    ENH: stat fixing problem with space in paths
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6794
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6794 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/GenerateCLP/GenerateGenerateCLPConfig.cmake
+++ Libs/GenerateCLP/GenerateGenerateCLPConfig.cmake
@@ -4,14 +4,14 @@
 # Following the standard pattern, UseGenerateCLP.cmake is a configured file
 #
 if(GenerateCLP_EXE_PATH)
-  set(GENERATECLP_EXE ${GenerateCLP_EXE_PATH})
+  set(GENERATECLP_EXE "${GenerateCLP_EXE_PATH}")
 else(GenerateCLP_EXE_PATH)
   if(Slicer3_BINARY_DIR)
     set(USE_CONFIG_FILE_INSTALL_DIR ${Slicer3_BINARY_DIR}/bin)
   else(Slicer3_BINARY_DIR)
     set(USE_CONFIG_FILE_INSTALL_DIR ${GenerateCLP_BINARY_DIR})
   endif(Slicer3_BINARY_DIR)
-  set(GENERATECLP_EXE ${USE_CONFIG_FILE_INSTALL_DIR}/${CMAKE_CFG_INTDIR}/GenerateCLP)
+  set(GENERATECLP_EXE "${USE_CONFIG_FILE_INSTALL_DIR}/${CMAKE_CFG_INTDIR}/GenerateCLP")
 endif(GenerateCLP_EXE_PATH)
 
 # Settings specific to build trees
@@ -24,7 +24,7 @@ configure_file(${GenerateCLP_SOURCE_DIR}/UseGenerateCLP.cmake.in
 
 set(GenerateCLP_INCLUDE_DIRS_CONFIG ${GenerateCLP_BINARY_DIR} ${GenerateCLP_SOURCE_DIR})
 set(GenerateCLP_LIBRARY_DIRS_CONFIG ${GenerateCLP_BINARY_DIR})
-set(GENERATECLP_EXE_CONFIG ${GENERATECLP_EXE})
+set(GENERATECLP_EXE_CONFIG "${GENERATECLP_EXE}")
 set(ITK_DIR_CONFIG ${ITK_DIR})
 configure_file(${GenerateCLP_SOURCE_DIR}/GenerateCLPConfig.cmake.in
   ${GenerateCLP_BINARY_DIR}/GenerateCLPConfig.cmake @ONLY IMMEDIATE)
--- Libs/GenerateLM/GenerateGenerateLMConfig.cmake
+++ Libs/GenerateLM/GenerateGenerateLMConfig.cmake
@@ -4,14 +4,14 @@
 # Following the standard pattern, UseGenerateLM.cmake is a configured file
 #
 if(GenerateLM_EXE_PATH)
-  set(GENERATELM_EXE ${GenerateLM_EXE_PATH})
+  set(GENERATELM_EXE "${GenerateLM_EXE_PATH}")
 else(GenerateLM_EXE_PATH)
   if(Slicer3_BINARY_DIR)
     set(USE_CONFIG_FILE_INSTALL_DIR ${Slicer3_BINARY_DIR}/bin)
   else(Slicer3_BINARY_DIR)
     set(USE_CONFIG_FILE_INSTALL_DIR ${GenerateLM_BINARY_DIR})
   endif(Slicer3_BINARY_DIR)
-  set(GENERATELM_EXE ${USE_CONFIG_FILE_INSTALL_DIR}/${CMAKE_CFG_INTDIR}/GenerateLM)
+  set(GENERATELM_EXE "${USE_CONFIG_FILE_INSTALL_DIR}/${CMAKE_CFG_INTDIR}/GenerateLM")
 endif(GenerateLM_EXE_PATH)
 
 # Settings specific to build trees
@@ -24,7 +24,7 @@ configure_file(${GenerateLM_SOURCE_DIR}/UseGenerateLM.cmake.in
 
 set(GenerateLM_INCLUDE_DIRS_CONFIG ${GenerateLM_BINARY_DIR} ${GenerateLM_SOURCE_DIR})
 set(GenerateLM_LIBRARY_DIRS_CONFIG ${GenerateLM_BINARY_DIR})
-set(GENERATELM_EXE_CONFIG ${GENERATELM_EXE})
+set(GENERATELM_EXE_CONFIG "${GENERATELM_EXE}")
 set(ITK_DIR_CONFIG ${ITK_DIR})
 configure_file(${GenerateLM_SOURCE_DIR}/GenerateLMConfig.cmake.in
   ${GenerateLM_BINARY_DIR}/GenerateLMConfig.cmake @ONLY IMMEDIATE)
