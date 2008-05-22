c9641ae6c2355ca491469e1576aa43305a86b5fe
Sebastien BARRE <sebastien.barre@kitware.com>
2008-05-22 20:17:55 +0000

    ENH: more macros
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6825
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6825 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/Slicer3PluginsMacros.cmake
+++ CMake/Slicer3PluginsMacros.cmake
@@ -1,4 +1,17 @@
 #-----------------------------------------------------------------------------
+macro(slicer3_set_default_plugins_install_prefix)
+  if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+    if(Slicer3_INSTALL_PREFIX)
+      set(__install_prefix "${Slicer3_INSTALL_PREFIX}")
+    else(Slicer3_INSTALL_PREFIX)
+      set(__install_prefix "${Slicer3_HOME}")
+    endif(Slicer3_INSTALL_PREFIX)
+    set(CMAKE_INSTALL_PREFIX "${__install_prefix}"
+      CACHE PATH "Install path prefix." FORCE)
+  endif(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+endmacro(slicer3_set_default_plugins_install_prefix)
+
+#-----------------------------------------------------------------------------
 macro(slicer3_set_default_plugins_output_path)
 #  set(LIBRARY_OUTPUT_PATH "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_PLUGINS_BIN_DIR}"
 #    CACHE PATH "Single output directory for building all libraries." FORCE)
