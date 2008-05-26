488adc6b61a4dbac4de4119a46d0093382aca731
Sebastien BARRE <sebastien.barre@kitware.com>
2008-05-26 15:09:42 +0000

    ENH: minor, just align the style with LoadableModule
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6846
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6846 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/ModuleDescriptionParser/GenerateModuleDescriptionParserConfig.cmake
+++ Libs/ModuleDescriptionParser/GenerateModuleDescriptionParserConfig.cmake
@@ -4,22 +4,35 @@
 # Settings specific to build trees
 #
 #
-SET(ModuleDescriptionParser_INCLUDE_DIRS_CONFIG ${ModuleDescriptionParser_BINARY_DIR} ${ModuleDescriptionParser_SOURCE_DIR})
-SET(ModuleDescriptionParser_LIBRARY_DIRS_CONFIG ${ModuleDescriptionParser_BINARY_DIR})
-SET(ModuleDescriptionParser_USE_FILE_CONFIG ${ModuleDescriptionParser_BINARY_DIR}/UseModuleDescriptionParser.cmake)
-SET(ITK_DIR_CONFIG ${ITK_DIR})
+set(ModuleDescriptionParser_INCLUDE_DIRS_CONFIG 
+  ${ModuleDescriptionParser_BINARY_DIR} 
+  ${ModuleDescriptionParser_SOURCE_DIR}
+  )
+set(ModuleDescriptionParser_LIBRARY_DIRS_CONFIG 
+  ${ModuleDescriptionParser_BINARY_DIR}
+  )
+set(ModuleDescriptionParser_USE_FILE_CONFIG 
+  ${ModuleDescriptionParser_BINARY_DIR}/UseModuleDescriptionParser.cmake
+  )
+set(ITK_DIR_CONFIG ${ITK_DIR})
 
 
-# Configure ModuleDescriptionParserConfig.cmake for the install tree.
-CONFIGURE_FILE(${ModuleDescriptionParser_SOURCE_DIR}/ModuleDescriptionParserConfig.cmake.in
-               ${ModuleDescriptionParser_BINARY_DIR}/ModuleDescriptionParserConfig.cmake @ONLY IMMEDIATE)
-
-
+# Configure ModuleDescriptionParserConfig.cmake for the build tree.
+#
+configure_file(
+  ${ModuleDescriptionParser_SOURCE_DIR}/ModuleDescriptionParserConfig.cmake.in
+  ${ModuleDescriptionParser_BINARY_DIR}/ModuleDescriptionParserConfig.cmake 
+  @ONLY IMMEDIATE
+  )
 
 # Settings specific for installation trees
 #
 #
 
 # Configure ModuleDescriptionParserConfig.cmake for the install tree.
-CONFIGURE_FILE(${ModuleDescriptionParser_SOURCE_DIR}/ModuleDescriptionParserInstallConfig.cmake.in
-               ${ModuleDescriptionParser_BINARY_DIR}/install/ModuleDescriptionParserConfig.cmake @ONLY IMMEDIATE)
+#
+configure_file(
+  ${ModuleDescriptionParser_SOURCE_DIR}/ModuleDescriptionParserInstallConfig.cmake.in
+  ${ModuleDescriptionParser_BINARY_DIR}/install/ModuleDescriptionParserConfig.cmake 
+  @ONLY IMMEDIATE
+  )
