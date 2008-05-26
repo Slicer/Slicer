61eae7880dafaa5abc815460b8c5357d741d4c71
Sebastien BARRE <sebastien.barre@kitware.com>
2008-05-26 15:08:45 +0000

    BUG: fix LoadableModule installation scripts, they were busted (the installation directory was hard-coded)
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6845
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6845 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/LoadableModule/GenerateLoadableModuleConfig.cmake
+++ Libs/LoadableModule/GenerateLoadableModuleConfig.cmake
@@ -4,27 +4,35 @@
 # Settings specific to build trees
 #
 #
-SET(LoadableModule_INCLUDE_DIRS_CONFIG ${LoadableModule_BINARY_DIR} ${LoadableModule_SOURCE_DIR})
-SET(LoadableModule_LIBRARY_DIRS_CONFIG ${LoadableModule_BINARY_DIR})
-SET(LoadableModule_USE_FILE_CONFIG ${LoadableModule_BINARY_DIR}/UseLoadableModule.cmake)
-SET(ITK_DIR_CONFIG ${ITK_DIR})
+set(LoadableModule_INCLUDE_DIRS_CONFIG 
+  ${LoadableModule_BINARY_DIR} 
+  ${LoadableModule_SOURCE_DIR}
+  )
+set(LoadableModule_LIBRARY_DIRS_CONFIG 
+  ${LoadableModule_BINARY_DIR}
+  )
+set(LoadableModule_USE_FILE_CONFIG 
+  ${LoadableModule_BINARY_DIR}/UseLoadableModule.cmake
+  )
+set(ITK_DIR_CONFIG ${ITK_DIR})
 
 
-# Configure LoadableModuleConfig.cmake for the install tree.
-CONFIGURE_FILE(${LoadableModule_SOURCE_DIR}/LoadableModuleConfig.cmake.in
-               ${LoadableModule_BINARY_DIR}/LoadableModuleConfig.cmake @ONLY IMMEDIATE)
-
-
+# Configure LoadableModuleConfig.cmake for the build tree.
+#
+configure_file(
+  ${LoadableModule_SOURCE_DIR}/LoadableModuleConfig.cmake.in
+  ${LoadableModule_BINARY_DIR}/LoadableModuleConfig.cmake 
+  @ONLY IMMEDIATE
+  )
 
 # Settings specific for installation trees
 #
 #
-SET(LoadableModule_INCLUDE_DIRS_CONFIG ${CMAKE_INSTALL_PREFIX}/include/LoadableModule)
-SET(LoadableModule_LIBRARY_DIRS_CONFIG ${CMAKE_INSTALL_PREFIX}/lib/LoadableModule)
-SET(LoadableModule_USE_FILE_CONFIG ${CMAKE_INSTALL_PREFIX}/lib/LoadableModule/UseLoadableModule.cmake)
-SET(ITK_DIR_CONFIG ${CMAKE_INSTALL_PREFIX}/lib/InsightToolkit)
-
 
 # Configure LoadableModuleConfig.cmake for the install tree.
-CONFIGURE_FILE(${LoadableModule_SOURCE_DIR}/LoadableModuleConfig.cmake.in
-               ${LoadableModule_BINARY_DIR}/install/LoadableModuleConfig.cmake @ONLY IMMEDIATE)
+#
+configure_file(
+  ${LoadableModule_SOURCE_DIR}/LoadableModuleInstallConfig.cmake.in
+  ${LoadableModule_BINARY_DIR}/install/LoadableModuleConfig.cmake 
+  @ONLY IMMEDIATE
+  )
