d5f75d37caebafd3d30e1b59b01bdc2b5e06aeaa
Sebastien BARRE <sebastien.barre@kitware.com>
2008-05-26 15:04:40 +0000

    ENH: added slicer3_install_modules and slicer3_install_plugins macro to hide the big INSTALL() command that was used to install plugins/modules; this is more elegant, and enable us to have better control on installation by modifying just one file.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6842
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6842 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/Slicer3ModulesMacros.cmake
+++ CMake/Slicer3ModulesMacros.cmake
@@ -1,4 +1,6 @@
 #-----------------------------------------------------------------------------
+# Set the default CMAKE_INSTALL_PREFIX variable
+# 
 macro(slicer3_set_default_modules_install_prefix)
   if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
     if(Slicer3_INSTALL_PREFIX)
@@ -12,15 +14,26 @@ macro(slicer3_set_default_modules_install_prefix)
 endmacro(slicer3_set_default_modules_install_prefix)
 
 #-----------------------------------------------------------------------------
+# Set the default output paths for Loadable Modules
+# 
 macro(slicer3_set_default_modules_output_path)
-#  set(LIBRARY_OUTPUT_PATH "${CMAKE_BINARY_DIR}/bin"
-#    CACHE PATH "Single output directory for building all libraries." FORCE)
-#  set(EXECUTABLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}/bin"
-#    CACHE PATH "Single output directory for building all executables." FORCE)
-  set(LIBRARY_OUTPUT_PATH "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_MODULES_LIB_DIR}")
-  set(EXECUTABLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_MODULES_BIN_DIR}")
+  set(LIBRARY_OUTPUT_PATH 
+    "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_MODULES_LIB_DIR}")
+  set(EXECUTABLE_OUTPUT_PATH 
+    "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_MODULES_BIN_DIR}")
   mark_as_advanced(
     LIBRARY_OUTPUT_PATH
     EXECUTABLE_OUTPUT_PATH
     )
 endmacro(slicer3_set_default_modules_output_path)
+
+#-----------------------------------------------------------------------------
+# Install one or more modules to the default plugin location
+# 
+macro(slicer3_install_modules)
+  install(TARGETS ${ARGN}
+    RUNTIME DESTINATION ${Slicer3_INSTALL_MODULES_BIN_DIR} COMPONENT RuntimeLibraries 
+    LIBRARY DESTINATION ${Slicer3_INSTALL_MODULES_LIB_DIR} COMPONENT RuntimeLibraries
+    ARCHIVE DESTINATION ${Slicer3_INSTALL_MODULES_LIB_DIR} COMPONENT Development
+    )
+endmacro(slicer3_install_modules)
--- CMake/Slicer3PluginsMacros.cmake
+++ CMake/Slicer3PluginsMacros.cmake
@@ -1,4 +1,6 @@
 #-----------------------------------------------------------------------------
+# Set the default CMAKE_INSTALL_PREFIX variable
+# 
 macro(slicer3_set_default_plugins_install_prefix)
   if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
     if(Slicer3_INSTALL_PREFIX)
@@ -12,15 +14,25 @@ macro(slicer3_set_default_plugins_install_prefix)
 endmacro(slicer3_set_default_plugins_install_prefix)
 
 #-----------------------------------------------------------------------------
+# Set the default output paths for plugins/CLP
+# 
 macro(slicer3_set_default_plugins_output_path)
-#  set(LIBRARY_OUTPUT_PATH "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_PLUGINS_BIN_DIR}"
-#    CACHE PATH "Single output directory for building all libraries." FORCE)
-#  set(EXECUTABLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_PLUGINS_BIN_DIR}"
-#    CACHE PATH "Single output directory for building all executables." FORCE)
-  set(LIBRARY_OUTPUT_PATH "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_PLUGINS_LIB_DIR}")
-  set(EXECUTABLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_PLUGINS_BIN_DIR}")
+  set(LIBRARY_OUTPUT_PATH 
+    "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_PLUGINS_LIB_DIR}")
+  set(EXECUTABLE_OUTPUT_PATH 
+    "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_PLUGINS_BIN_DIR}")
   mark_as_advanced(
     LIBRARY_OUTPUT_PATH
     EXECUTABLE_OUTPUT_PATH
     )
 endmacro(slicer3_set_default_plugins_output_path)
+
+#-----------------------------------------------------------------------------
+# Install one or more plugins to the default plugin location
+# 
+macro(slicer3_install_plugins)
+  install(TARGETS ${ARGN}
+    RUNTIME DESTINATION ${Slicer3_INSTALL_PLUGINS_BIN_DIR} COMPONENT RuntimeLibraries
+    LIBRARY DESTINATION ${Slicer3_INSTALL_PLUGINS_LIB_DIR} COMPONENT RuntimeLibraries
+    )
+endmacro(slicer3_install_plugins)
