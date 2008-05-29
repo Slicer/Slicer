efda56bc13c22205f666bdcef01975fea0660a2d
Sebastien BARRE <sebastien.barre@kitware.com>
2008-05-29 19:22:36 +0000

    ENH: hide testing tricks inside a macro
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6920
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6920 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/Slicer3PluginsMacros.cmake
+++ CMake/Slicer3PluginsMacros.cmake
@@ -22,3 +22,19 @@ macro(slicer3_install_plugins)
     LIBRARY DESTINATION ${Slicer3_INSTALL_PLUGINS_LIB_DIR} COMPONENT RuntimeLibraries
     )
 endmacro(slicer3_install_plugins)
+
+#-----------------------------------------------------------------------------
+# Test a plugin
+# 
+include("${KWWidgets_CMAKE_DIR}/KWWidgetsTestingMacros.cmake")
+macro(slicer3_add_plugins_test test_name clp_to_test)
+  set(build_type ".")
+  if(WIN32 AND CMAKE_CONFIGURATION_TYPES)
+    # Sadly, there is no way to know if the user picked Debug or Release
+    # here, so we are going to have to stick to the value of CMAKE_BUILD_TYPE
+    # if it has been set explicitly (by a dashboard for example), or the
+    # first value in CMAKE_CONFIGURATION_TYPES (i.e. Debug)/
+    KWWidgets_GET_CMAKE_BUILD_TYPE(build_type)
+  endif(WIN32 AND CMAKE_CONFIGURATION_TYPES)
+  add_test(${test_name} ${Slicer3_DIR}/Slicer3 --launch ${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_PLUGINS_BIN_DIR}/${build_type}/${clp_to_test} ${ARGN}) 
+endmacro(slicer3_add_plugins_test)
