e15867a93e06bc30ff19cbaf2fe2392465b6ffc2
Steve Pieper <pieper@bwh.harvard.edu>
2008-05-30 12:26:59 +0000

    COMP: fix warnings about SQRT
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6933
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6933 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/vtkTeem/vtkDiffusionTensorMathematicsSimple.cxx
+++ Libs/vtkTeem/vtkDiffusionTensorMathematicsSimple.cxx
@@ -19,9 +19,6 @@
 #endif
 
 // But, if you are on VS6.0 you don't get the define...
-#ifndef M_SQRT2
-#define M_SQRT2    1.41421356237309504880168872421      /* sqrt(2) */
-#endif
 #include "vtkInformation.h"
 #include "vtkInformationVector.h"
 #include "vtkImageData.h"
@@ -33,6 +30,9 @@
 #include "vtkPointData.h"
 #include "time.h"
 #include "vtkLookupTable.h"
+#ifndef M_SQRT2
+#define M_SQRT2    1.41421356237309504880168872421      /* sqrt(2) */
+#endif
 
 extern "C" {
 #include "teem/ten.h"
