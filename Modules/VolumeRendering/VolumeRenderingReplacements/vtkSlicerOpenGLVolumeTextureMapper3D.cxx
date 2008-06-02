37a29328704d99a8cd627be35c1a73814a9f8241
Sebastien BARRE <sebastien.barre@kitware.com>
2008-06-02 10:04:02 +0000

    ENH: fix inclusion of headers. Need an update VTK 5.2
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=6955
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@6955 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/VolumeRendering/VolumeRenderingReplacements/vtkSlicerOpenGLVolumeTextureMapper3D.cxx
+++ Modules/VolumeRendering/VolumeRenderingReplacements/vtkSlicerOpenGLVolumeTextureMapper3D.cxx
@@ -33,19 +33,12 @@
 #include "vtkOpenGLExtensionManager.h"
 #include "vtkgl.h"
 #include "vtkCommand.h"
-#include "vtkVolumeTextureMapper3D_OneComponentNoShadeFP.cxx"
-#include "vtkVolumeTextureMapper3D_OneComponentShadeFP.cxx"
-#include "vtkVolumeTextureMapper3D_TwoDependentNoShadeFP.cxx"
-#include "vtkVolumeTextureMapper3D_TwoDependentShadeFP.cxx"
-#include "vtkVolumeTextureMapper3D_FourDependentNoShadeFP.cxx"
-#include "vtkVolumeTextureMapper3D_FourDependentShadeFP.cxx"
-
-extern const char* vtkVolumeTextureMapper3D_OneComponentShadeFP;
-extern const char* vtkVolumeTextureMapper3D_OneComponentNoShadeFP;
-extern const char* vtkVolumeTextureMapper3D_TwoDependentNoShadeFP;
-extern const char* vtkVolumeTextureMapper3D_TwoDependentShadeFP;
-extern const char* vtkVolumeTextureMapper3D_FourDependentNoShadeFP;
-extern const char* vtkVolumeTextureMapper3D_FourDependentShadeFP;
+#include "vtkVolumeTextureMapper3D_OneComponentNoShadeFP.h"
+#include "vtkVolumeTextureMapper3D_OneComponentShadeFP.h"
+#include "vtkVolumeTextureMapper3D_TwoDependentNoShadeFP.h"
+#include "vtkVolumeTextureMapper3D_TwoDependentShadeFP.h"
+#include "vtkVolumeTextureMapper3D_FourDependentNoShadeFP.h"
+#include "vtkVolumeTextureMapper3D_FourDependentShadeFP.h"
 
 #define PrintError(S)                                                           \
   {                                                                             \
