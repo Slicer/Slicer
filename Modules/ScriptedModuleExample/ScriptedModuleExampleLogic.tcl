267abb0695c72d74a678641c242c0ab715c64e41
Steve Pieper <pieper@bwh.harvard.edu>
2008-08-27 17:01:51 +0000

    ENH: clone proc to quickly create a scripted module instance by copying the example
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=7497
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@7497 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/ScriptedModuleExample/ScriptedModuleExampleLogic.tcl
+++ Modules/ScriptedModuleExample/ScriptedModuleExampleLogic.tcl
@@ -2,6 +2,43 @@
 # ScriptedModuleExample logic procs
 #
 
+#
+# make a clone of the scripted module example with a new name
+#
+proc ScriptedModuleExampleClone { moduleName {slicerSourceDir ""} {targetDir ""} } {
+
+  if { $slicerSourceDir == "" } {
+    set slicerSourceDir $::env(Slicer3_HOME)/../Slicer3
+  }
+  if { $targetDir == "" } {
+    set targetDir $slicerSourceDir/Modules/$moduleName
+  }
+  if { [file exists $targetDir] } {
+    error "Target dir exists - please delete $targetDir"
+  }
+  puts "Making $targetDir"
+  file mkdir $targetDir
+
+  set files [glob $slicerSourceDir/Modules/ScriptedModuleExample/*]
+  foreach f $files {
+    set ff [file tail $f]
+    if { [string match ".*" $ff] || [string match "*~" $ff] } {
+      continue
+    }
+    set fp [open $f "r"]
+    set contents [read $fp]
+    close $fp
+    regsub -all "ScriptedModuleExample" $contents $moduleName newContents
+
+    regsub -all "ScriptedModuleExample" $ff $moduleName newFileName
+    set fp [open $targetDir/$newFileName "w"]
+    puts -nonewline $fp $newContents
+    close $fp
+
+    puts "  created file $newFileName"
+  }
+  puts "New module created - add entry to $slicerSourceDir/Modules/CMakeLists.txt"
+}
 
 #
 # make a model of the current label map for the given slice logic
