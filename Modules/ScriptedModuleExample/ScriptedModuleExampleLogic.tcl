69ff39d467836669372e80a98cf1b65be7e684e7
Steve Pieper <pieper@bwh.harvard.edu>
2008-08-27 15:25:49 +0000

    ENH: new scripted module example to use as starting point for new modules
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=7492
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@7492 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/ScriptedModuleExample/ScriptedModuleExampleLogic.tcl
@@ -0,0 +1,102 @@
+#
+# ScriptedModuleExample logic procs
+#
+
+
+#
+# make a model of the current label map for the given slice logic
+#
+proc ScriptedModuleExampleAddQuickModel { sliceLogic } {
+
+  #
+  # get the image data for the label layer
+  #
+  set layerLogic [$sliceLogic GetLabelLayer]
+  set volumeNode [$layerLogic GetVolumeNode]
+  if { $volumeNode == "" } {
+    puts "cannot make quick model - no volume node for $layerLogic in $sliceLogic"
+    return
+  }
+  set imageData [$volumeNode GetImageData]
+
+  #
+  # make a poly data in RAS space
+  #
+  set dsm [vtkDiscreteMarchingCubes New]
+  $dsm SetInput $imageData
+  set tpdf [vtkTransformPolyDataFilter New]
+  set ijkToRAS [vtkTransform New]
+  $volumeNode GetRASToIJKMatrix [$ijkToRAS GetMatrix] 
+  $ijkToRAS Inverse
+  $tpdf SetInput [$dsm GetOutput]
+  $tpdf SetTransform $ijkToRAS
+  $tpdf Update
+
+  #
+  # create a mrml model for the new data
+  #
+  set modelNode [vtkMRMLModelNode New]
+  set modelDisplayNode [vtkMRMLModelDisplayNode New]
+  $modelNode SetName "QuickModel"
+  $modelNode SetScene $::slicer3::MRMLScene
+  $modelDisplayNode SetScene $::slicer3::MRMLScene
+  eval $modelDisplayNode SetColor [lrange [ScriptedModuleExampleGetPaintColor $::ScriptedModuleExample(singleton)] 0 2]
+  $::slicer3::MRMLScene AddNode $modelDisplayNode
+  $modelNode SetAndObserveDisplayNodeID [$modelDisplayNode GetID]
+  $::slicer3::MRMLScene AddNode $modelNode
+  $modelNode SetAndObservePolyData [$tpdf GetOutput]
+
+  #
+  # clean up
+  #
+  $dsm Delete
+  $ijkToRAS Delete
+  $tpdf Delete
+  $modelNode Delete
+  $modelDisplayNode Delete
+
+}
+
+#
+# TODO: flesh this out...
+# - copy works, but image data is not correct somehow.
+# - also need a GUI to access this function
+#
+proc ScriptedModuleExampleLabelCheckpoint {} {
+
+  #
+  # get the image data for the label layer
+  #
+  set sliceLogic [$::slicer3::ApplicationLogic GetSliceLogic "Red"]
+  set layerLogic [$sliceLogic GetLabelLayer]
+  set volumeNode [$layerLogic GetVolumeNode]
+  if { $volumeNode == "" } {
+    puts "cannot make label checkpoint - no volume node"
+    return
+  }
+
+  # find a unique name for this copy
+  set sourceName [$volumeNode GetName]
+  set id 0
+  while {1} {
+    set targetName $sourceName-$id
+    set nodes [$::slicer3::MRMLScene GetNodesByName $targetName]
+    if { [$nodes GetNumberOfItems] == 0 } {
+      break
+    }
+    incr id
+  }
+
+  set volumesLogic [$::slicer3::VolumesGUI GetLogic]
+  set labelNode [$volumesLogic CloneVolume $::slicer3::MRMLScene $volumeNode $targetName]
+}
+
+
+
+#
+# make it easier to test the model by looking for the first slice logic
+#
+proc ScriptedModuleExampleTestQuickModel {} {
+  set sliceLogic [lindex [vtkSlicerSliceLogic ListInstances] 0]
+  ScriptedModuleExampleAddQuickModel $sliceLogic
+}
