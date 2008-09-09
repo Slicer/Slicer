5642c94b6b908d2c5a22b6da0288201f4431cccb
Alex Yarmarkovich <alexy@bwh.harvard.edu>
2008-09-09 21:22:46 +0000

    ENH: added vtkKWCheckBoxSelectionDialog
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=7561
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@7561 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/vtkKWCheckBoxSelectionDialog.cxx
@@ -0,0 +1,263 @@
+#include "vtkObject.h"
+#include "vtkObjectFactory.h"
+#include "vtkStringArray.h"
+#include <itksys/SystemTools.hxx> 
+#include <vtksys/stl/string>
+#include <vtksys/SystemTools.hxx>
+
+#include "vtkKWCheckBoxSelectionDialog.h"
+
+#include "vtkKWTopLevel.h"
+#include "vtkKWDialog.h"
+#include "vtkKWMultiColumnListWithScrollbars.h"
+#include "vtkKWMultiColumnList.h"
+#include "vtkKWFrameWithLabel.h"
+#include "vtkKWPushButton.h"
+#include "vtkKWEntryWithLabel.h"
+#include "vtkKWEntry.h"
+#include "vtkKWCheckButton.h"
+
+
+//---------------------------------------------------------------------------
+vtkStandardNewMacro (vtkKWCheckBoxSelectionDialog );
+vtkCxxRevisionMacro ( vtkKWCheckBoxSelectionDialog, "$Revision: 1.0 $");
+
+
+//---------------------------------------------------------------------------
+vtkKWCheckBoxSelectionDialog::vtkKWCheckBoxSelectionDialog ( )
+{
+  this->Dialog = NULL;
+  this->OkButton = NULL;
+  this->CancelButton = NULL;
+  this->MultiColumnList = NULL;
+
+  this->SetTitle("");
+
+  this->SelectedLabels = vtkStringArray::New();
+}
+
+
+//---------------------------------------------------------------------------
+vtkKWCheckBoxSelectionDialog::~vtkKWCheckBoxSelectionDialog ( )
+{
+  this->RemoveWidgetObservers();
+
+  if (this->MultiColumnList)
+    {
+    this->MultiColumnList->SetParent(NULL);
+    this->MultiColumnList->Delete();
+    }
+  if (this->OkButton)
+    {
+    this->OkButton->SetParent(NULL);
+    this->OkButton->Delete();
+    }
+  if (this->CancelButton)
+    {
+    this->CancelButton->SetParent(NULL);
+    this->CancelButton->Delete();
+    }
+  if (this->Dialog)
+    {
+    this->Dialog->SetParent(NULL);
+    this->Dialog->Delete();
+    }
+  this->SetTitle(NULL);
+
+  this->SelectedLabels->Delete();
+}
+
+//---------------------------------------------------------------------------
+void vtkKWCheckBoxSelectionDialog::PrintSelf ( ostream& os, vtkIndent indent )
+{
+  this->vtkObject::PrintSelf ( os, indent );
+  
+  os << indent << "vtkKWCheckBoxSelectionDialog: " << this->GetClassName ( ) << "\n";
+  // print widgets?
+}
+
+
+//---------------------------------------------------------------------------
+void vtkKWCheckBoxSelectionDialog::AddEntry(char *label, char *description, int selected)
+{
+  this->Create();
+
+  this->MultiColumnList->GetWidget()->AddRow();
+  int row = this->MultiColumnList->GetWidget()->GetNumberOfRows() - 1;
+  this->MultiColumnList->GetWidget()->SetCellText(row,0,label);
+  this->MultiColumnList->GetWidget()->SetCellText(row,1,description);
+  this->MultiColumnList->GetWidget()->SetCellTextAsInt(row,2,selected);
+  this->MultiColumnList->GetWidget()->SetCellWindowCommandToCheckButton(row, 2);
+}
+
+//---------------------------------------------------------------------------
+void vtkKWCheckBoxSelectionDialog::RemoveAllEntries()
+{
+  if (this->MultiColumnList->GetWidget()->GetNumberOfRows())
+    {
+    this->MultiColumnList->GetWidget()->DeleteAllRows ();
+    }
+  this->SelectedLabels->Reset();
+}
+
+//---------------------------------------------------------------------------
+vtkStringArray* vtkKWCheckBoxSelectionDialog::GetSelectedLabels()
+{
+  this->SelectedLabels->Reset();
+
+  int nrows = this->MultiColumnList->GetWidget()->GetNumberOfRows();
+  for (int row=0; row<nrows; row++)
+    {
+    if (this->MultiColumnList->GetWidget()->GetCellTextAsInt(row, 2))
+      {
+      std::string label (this->MultiColumnList->GetWidget()->GetCellText(row, 0));
+      this->SelectedLabels->InsertNextValue(label.c_str());
+      }
+    }
+  return SelectedLabels;
+}
+
+
+//---------------------------------------------------------------------------
+void vtkKWCheckBoxSelectionDialog::ProcessWidgetEvents ( vtkObject *caller,
+                                                    unsigned long event, 
+                                                    void *callData )
+{
+  if (this->OkButton ==  vtkKWPushButton::SafeDownCast(caller) && event ==  vtkKWPushButton::InvokedEvent)
+    {
+    this->MultiColumnList->GetWidget()->FinishEditing();
+    this->Dialog->OK();
+//  this->InvokeEvent(vtkKWCheckBoxSelectionDialog::DataSavedEvent);
+    }
+  else if (this->CancelButton ==  vtkKWPushButton::SafeDownCast(caller) && event ==  vtkKWPushButton::InvokedEvent)
+    { 
+    this->Dialog->Cancel();
+    }
+} 
+
+
+
+
+
+//---------------------------------------------------------------------------
+void vtkKWCheckBoxSelectionDialog::RemoveWidgetObservers ( ) 
+{
+  if (this->OkButton)
+    {
+    this->OkButton->RemoveObservers ( vtkKWPushButton::InvokedEvent,  
+        (vtkCommand *)this->GUICallbackCommand );
+    }
+  if (this->CancelButton)
+    {
+    this->CancelButton->RemoveObservers ( vtkKWPushButton::InvokedEvent,  
+        (vtkCommand *)this->GUICallbackCommand );
+    }
+}
+
+//---------------------------------------------------------------------------
+void vtkKWCheckBoxSelectionDialog::CreateWidget ( )
+{
+  // Check if already created
+
+  if (this->IsCreated())
+    {
+    return;
+    }
+
+  // Call the superclass to create the whole widget
+
+  this->Superclass::CreateWidget();
+
+  this->Dialog = vtkKWDialog::New();
+  this->Dialog->SetMasterWindow ( this->GetParent());
+  this->Dialog->SetParent ( this->GetParent());
+  this->Dialog->SetTitle(this->Title);
+  this->Dialog->SetSize(200, 400);
+  this->Dialog->Create ( );
+
+  // Data Frame
+  vtkKWFrameWithLabel *dataFrame = vtkKWFrameWithLabel::New ( );
+  dataFrame->SetParent ( this->Dialog );
+  dataFrame->Create ( );
+  dataFrame->SetLabelText (this->Title);
+  this->Script ( "pack %s -side top -anchor nw -fill x -padx 2 -pady 2",
+                 dataFrame->GetWidgetName() );
+  
+  // add the multicolumn list to show the points
+  this->MultiColumnList = vtkKWMultiColumnListWithScrollbars::New ( );
+  this->MultiColumnList->SetParent ( dataFrame->GetFrame() );
+  this->MultiColumnList->Create ( );
+  this->MultiColumnList->SetHeight(4);
+  this->MultiColumnList->GetWidget()->SetSelectionTypeToCell();
+    
+  // set up the columns of data for each point
+  // name, x, y, z, orientation w, x, y, z, selected
+  this->MultiColumnList->GetWidget()->AddColumn("Module Name");
+  this->MultiColumnList->GetWidget()->ColumnEditableOff(0);
+  this->MultiColumnList->GetWidget()->SetColumnWidth(0, 12);
+
+  this->MultiColumnList->GetWidget()->AddColumn("Module Description");
+  this->MultiColumnList->GetWidget()->ColumnEditableOff(1);
+  this->MultiColumnList->GetWidget()->SetColumnWidth(1, 32);
+
+  this->MultiColumnList->GetWidget()->AddColumn("Disable");
+  this->MultiColumnList->GetWidget()->SetColumnWidth(2, 6);
+  this->MultiColumnList->GetWidget()->SetColumnEditWindowToCheckButton(2);
+  this->MultiColumnList->GetWidget()->SetColumnFormatCommandToEmptyOutput(2);
+  this->MultiColumnList->GetWidget()->ColumnEditableOn(2);
+
+  // make the save column editable by checkbox
+  // now set the attributes that are equal across the columns
+  int col;
+  for (col = 0; col <= 2; col++)
+    {        
+    this->MultiColumnList->GetWidget()->SetColumnAlignmentToLeft(col);
+    }
+  this->Script ( "pack %s -fill both -expand true",
+                     this->MultiColumnList->GetWidgetName());
+ 
+
+  // this->MultiColumnList->GetWidget()->SetCellUpdatedCommand(this, "UpdateElement");
+    
+  // save Frame
+  vtkKWFrame *saveFrame = vtkKWFrame::New ( );
+  saveFrame->SetParent ( this->Dialog );
+  saveFrame->Create ( );
+  this->Script ( "pack %s -side top -anchor nw -fill x -padx 2 -pady 2",
+                 saveFrame->GetWidgetName() );
+
+  // add OK button
+  this->OkButton = vtkKWPushButton::New ( );
+  this->OkButton->SetParent ( saveFrame );
+  this->OkButton->Create ( );
+  this->OkButton->SetText ("Save");
+  this->Script("pack %s -side left -anchor w -padx 2 -pady 4", 
+              this->OkButton->GetWidgetName());
+
+  // add Cancel button
+  this->CancelButton = vtkKWPushButton::New ( );
+  this->CancelButton->SetParent ( saveFrame );
+  this->CancelButton->Create ( );
+  this->CancelButton->SetText ("Cancel");
+  this->Script("pack %s -side left -anchor w -padx 36 -pady 4", 
+              this->CancelButton->GetWidgetName());
+
+  // add observers
+  this->OkButton->AddObserver ( vtkKWPushButton::InvokedEvent,  (vtkCommand *)this->GUICallbackCommand );
+  this->CancelButton->AddObserver ( vtkKWPushButton::InvokedEvent,  (vtkCommand *)this->GUICallbackCommand );
+
+  
+  this->MultiColumnList->SetEnabled(1);
+  this->OkButton->SetEnabled(1);
+  dataFrame->Delete();
+  saveFrame->Delete();
+  
+}
+
+void vtkKWCheckBoxSelectionDialog::Invoke ( )
+{
+  if (this->Dialog)
+    {
+    this->Dialog->Invoke ( );
+    }
+}
---
+++ Base/GUI/vtkKWCheckBoxSelectionDialog.h
@@ -0,0 +1,117 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkSlicerNodeSelectorWidget.h,v $
+  Date:      $Date: 2006/01/08 04:48:05 $
+  Version:   $Revision: 1.45 $
+
+=========================================================================auto=*/
+
+// .NAME vtkSlicerNodeSelectorWidget - menu to select volumes from current mrml scene
+// .SECTION Description
+// Inherits most behavior from kw widget, but is specialized to observe
+// the current mrml scene and update the entries of the pop up menu to correspond
+// to the currently available volumes.  This widget also has a notion of the current selection
+// that can be observed or set externally
+//
+
+
+#ifndef __vtkKWCheckBoxSelectionDialog_h
+#define __vtkKWCheckBoxSelectionDialog_h
+
+#include "vtkStringArray.h"
+
+#include "vtkSlicerWidget.h"
+
+class vtkKWDialog;
+class vtkKWEntryWithLabel;
+class vtkKWCheckButton;
+class vtkKWMultiColumnListWithScrollbars;
+class vtkKWPushButton;
+
+class VTK_SLICER_BASE_GUI_EXPORT vtkKWCheckBoxSelectionDialog : public vtkSlicerWidget
+{
+  
+public:
+  static vtkKWCheckBoxSelectionDialog* New();
+  vtkTypeRevisionMacro(vtkKWCheckBoxSelectionDialog,vtkSlicerWidget);
+  void PrintSelf(ostream& os, vtkIndent indent);
+  
+  // Description:
+  // alternative method to propagate events generated in GUI to logic / mrml
+  virtual void ProcessWidgetEvents ( vtkObject *caller, unsigned long event, void *callData );
+  
+  // Description:
+  // removes observers on widgets in the class
+  virtual void RemoveWidgetObservers ( );
+
+  // Description:
+  // Add entry name
+  void AddEntry(char *Label, char *Description, int selected);
+
+  // Description:
+  // Clear all entries
+  void RemoveAllEntries();
+
+  // Description:
+  // Returns selected entries
+  vtkStringArray* GetSelectedLabels();
+  
+  // Description:
+  // invoke the widget
+  void Invoke();
+  
+//BTX
+  // Description:
+  // Returns selected entries
+  std::vector<std::string> GetSelected();
+//ETX
+
+  // Description:
+  // Title
+  vtkGetStringMacro(Title);
+  vtkSetStringMacro(Title);
+
+  // Update widget
+  //void UpdateWidget();
+  
+protected:
+  vtkKWCheckBoxSelectionDialog();
+  virtual ~vtkKWCheckBoxSelectionDialog();
+
+  // Description:
+  // Create the widget.
+  virtual void CreateWidget();
+
+
+private:
+    
+  //BTX
+  //std::vector<std::string> Labels;
+  //std::vector<std::string> Descriptions;
+  //std::vector<std::int> Selected;
+  //ETX
+  
+  vtkStringArray *SelectedLabels;
+
+  char *Title;
+
+  vtkKWDialog *Dialog;
+
+  vtkKWPushButton *OkButton;
+
+  vtkKWPushButton *CancelButton;
+  
+  vtkKWMultiColumnListWithScrollbars *MultiColumnList;
+
+  vtkKWCheckBoxSelectionDialog(const vtkKWCheckBoxSelectionDialog&); // Not implemented
+  void operator=(const vtkKWCheckBoxSelectionDialog&); // Not Implemented
+};
+
+#endif
+
