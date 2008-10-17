91eaebeee16e75e7d11048991f062976855792e8
jvs@bwh.harvard.edu <jvs@bwh.harvard.edu>
2008-10-17 20:54:39 +0000

    ENH: Adding a new file
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=7708
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@7708 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/Python/tensor_DWI_PerVolume.py
@@ -0,0 +1,67 @@
+XML = """<?xml version="1.0" encoding="utf-8"?>
+<executable>
+  <category>Python Modules</category>
+  <title>Python Extract DWI Volume</title>
+  <description>Python module</description>
+  <version>0.1.0.$Revision: 1892 $(alpha)</version>
+  <documentation-url></documentation-url>
+  <contributor>Julien von Siebenthal</contributor>
+  <parameters>
+    <label>IO</label>
+    <description>Input/output parameters</description>
+    <image type="diffusion-weighted">
+      <name>inputVolume</name>
+      <label>Input DWI Volume</label>
+      <channel>input</channel>
+      <index>0</index>
+      <description>Input DWI volume</description>
+    </image>
+  </parameters>
+</executable>
+"""
+
+def Execute (inputVolume):
+  Slicer = __import__ ( "Slicer" )
+  slicer = Slicer.slicer
+  scene = slicer.MRMLScene
+
+  inputVolume = scene.GetNodeByID(inputVolume)
+  data = inputVolume.GetImageData().ToArray()
+
+  shapeD = data.shape
+
+# for each gradient generate associated volume
+#
+  for i in range(shapeD[3]):
+    dvol1 = data[..., i]
+    r1 = slicer.vtkMRMLScalarVolumeNode()
+    r11 = slicer.vtkMRMLScalarVolumeDisplayNode()
+    scene.AddNode(r11)
+
+    r1.AddAndObserveDisplayNodeID(r11.GetName())
+
+    imgD = slicer.vtkImageData()
+    imgD.SetDimensions(256, 256, 36)
+    imgD.SetScalarTypeToShort()
+
+    org = inputVolume.GetOrigin()
+    spa = inputVolume.GetSpacing()
+
+    mat = slicer.vtkMatrix4x4()
+    inputVolume.GetIJKToRASMatrix(mat)
+
+    r1.SetAndObserveImageData(imgD)
+    r1.SetIJKToRASMatrix(mat)
+    r1.SetOrigin(org[0], org[1], org[2])
+    r1.SetSpacing(spa[0], spa[1], spa[2])
+
+    scene.AddNode(r1)
+
+    tmp = r1.GetImageData().ToArray()
+    tmp[:] = dvol1[:]
+
+    r1.GetDisplayNode().SetDefaultColorMap()
+    r1.Modified()
+
+  return
+
