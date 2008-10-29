86e1d7e8014d16b0097c0e37afb844bb15fb1e05
Steve Pieper <pieper@bwh.harvard.edu>
2008-10-29 20:41:19 +0000

    ENH: implementation of the module leak test script
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=7767
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@7767 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Testing/LeakTest.tcl
+++ Modules/Testing/LeakTest.tcl
@@ -1,2 +1,44 @@
 
---ignore-module "EMSegment" --ignore-module "Gradient Anisotropic Diffusion Filter" --ignore-module "Label Statistics" --ignore-module "Neuro Nav" --ignore-module "OpenIGTLink" --ignore-module "QdecModule" --ignore-module "QueryAtlas" --ignore-module "Slicer Tractography Display" --ignore-module "Slicer Tractography Fiducial Seeding" --ignore-module "Tumor Growth" --ignore-module "Volume Rendering" --ignore-module "Volumes"
+#!/bin/sh
+# the next line restarts using tclsh \
+    exec tclsh "$0" "$@"
+
+################################################################################
+#
+# test script to identify which (if any) of slicer's loadable modules is causing a leak
+#
+# - update the global modules list with all suspect modules
+# - run this script from the Slicer3-build directory
+#
+################################################################################
+
+set ::modules { 
+ "EMSegment"  "Gradient Anisotropic Diffusion Filter"  "Label Statistics"  "Neuro Nav"  
+ "OpenIGTLink"  "QdecModule"  "QueryAtlas"  "Slicer Tractography Display"  
+ "Slicer Tractography Fiducial Seeding"  "Tumor Growth"  "Volume Rendering"  "Volumes"
+ }
+
+
+
+proc runSlicer { {doNotIgnore ""} } {
+
+  set cmd "./Slicer3"
+  
+  foreach m $::modules {
+    if { [lsearch $m $doNotIgnore] == -1 } {
+      set cmd [concat $cmd "--ignore-module \"$m\""]
+    }
+  }
+  set cmd [concat $cmd "--exec exit"]
+  puts "running: $cmd"
+
+  set ret [catch "eval exec $cmd" res]
+
+  puts $res
+}
+
+
+foreach m $::modules {
+  puts "ignoring $m"
+  runSlicer $m
+}
