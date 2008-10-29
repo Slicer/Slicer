c0347757780b3fabd10c57947b22f34a65d0a446
Steve Pieper <pieper@bwh.harvard.edu>
2008-10-29 20:24:17 +0000

    ENH: start of new script to sequentially identify which loadable module is causing a leak
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=7766
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@7766 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/Testing/LeakTest.tcl
@@ -0,0 +1,2 @@
+
+--ignore-module "EMSegment" --ignore-module "Gradient Anisotropic Diffusion Filter" --ignore-module "Label Statistics" --ignore-module "Neuro Nav" --ignore-module "OpenIGTLink" --ignore-module "QdecModule" --ignore-module "QueryAtlas" --ignore-module "Slicer Tractography Display" --ignore-module "Slicer Tractography Fiducial Seeding" --ignore-module "Tumor Growth" --ignore-module "Volume Rendering" --ignore-module "Volumes"
