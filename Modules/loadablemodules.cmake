516dd07c24bb2e226e9c629d0b0603514b27045c
Terry Lorber <tgl@rideside.net>
2008-11-12 03:04:25 +0000

    BUG include necessary libraries
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=7845
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@7845 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/loadablemodules.cmake
+++ Modules/loadablemodules.cmake
@@ -1,5 +1,10 @@
 #
 # DO NOT EDIT!
 #
-# This file will be appended when using the Modules Wizard within Slicer3
+# This file will be appended to when using the Modules Wizard within Slicer3
 #
+
+set(CMAKE_MODULE_PATH ${Slicer3_SOURCE_DIR}/Utilities/BuildSystem/CMake)
+include(SlicerParseModule)
+include(SlicerDownloadModule)
+
