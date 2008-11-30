dd6aab31507e4469306d285af6cb4a01e3ee7404
Terry Lorber <tgl@rideside.net>
2008-11-30 00:22:08 +0000

    ENH switch location to na-mic.org
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=8003
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@8003 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/loadablemodules.cmake
+++ Modules/loadablemodules.cmake
@@ -8,3 +8,4 @@ set(CMAKE_MODULE_PATH ${Slicer3_SOURCE_DIR}/Utilities/BuildSystem/CMake)
 include(SlicerParseModule)
 include(SlicerDownloadModule)
 
+slicer_parse_module_url("http://svn.na-mic.org/NAMICSandBox/trunk/LoadableModuleExample/trunk/ExampleLoadableModule.xml")
