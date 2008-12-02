bf63839aa6a009b51f1d46d2c4962613b07e1f02
Junichi Tokuda <tokuda@bwh.harvard.edu>
2008-12-02 01:14:39 +0000

    BUG: Fixed orientation of the locator (the default orientation of the locator became (0, 0, 1))
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=8022
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@8022 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/OpenIGTLinkIF/vtkOpenIGTLinkIFLogic.cxx
+++ Modules/OpenIGTLinkIF/vtkOpenIGTLinkIFLogic.cxx
@@ -14,6 +14,8 @@
 
 #include "vtkObjectFactory.h"
 #include "vtkCallbackCommand.h"
+#include "vtkTransformPolyDataFilter.h"
+#include "vtkTransform.h"
 
 #include "vtkOpenIGTLinkIFLogic.h"
 
@@ -1282,8 +1284,18 @@ vtkMRMLModelNode* vtkOpenIGTLinkIFLogic::AddLocatorModel(const char* nodeName, d
   vtkCylinderSource *cylinder = vtkCylinderSource::New();
   cylinder->SetRadius(1.5);
   cylinder->SetHeight(100);
-  cylinder->SetCenter(0, 50, 0);
+  cylinder->SetCenter(0, 0, 0);
   cylinder->Update();
+
+  // Rotate cylinder
+  vtkTransformPolyDataFilter *tfilter = vtkTransformPolyDataFilter::New();
+  vtkTransform* trans =   vtkTransform::New();
+  trans->RotateX(90.0);
+  trans->Translate(0.0, -50.0, 0.0);
+  trans->Update();
+  tfilter->SetInput(cylinder->GetOutput());
+  tfilter->SetTransform(trans);
+  tfilter->Update();
   
   // Sphere represents the locator tip 
   vtkSphereSource *sphere = vtkSphereSource::New();
@@ -1293,7 +1305,8 @@ vtkMRMLModelNode* vtkOpenIGTLinkIFLogic::AddLocatorModel(const char* nodeName, d
   
   vtkAppendPolyData *apd = vtkAppendPolyData::New();
   apd->AddInput(sphere->GetOutput());
-  apd->AddInput(cylinder->GetOutput());
+  //apd->AddInput(cylinder->GetOutput());
+  apd->AddInput(tfilter->GetOutput());
   apd->Update();
   
   locatorModel->SetAndObservePolyData(apd->GetOutput());
@@ -1305,6 +1318,8 @@ vtkMRMLModelNode* vtkOpenIGTLinkIFLogic::AddLocatorModel(const char* nodeName, d
   locatorDisp->SetPolyData(locatorModel->GetPolyData());
   locatorDisp->SetColor(color);
   
+  trans->Delete();
+  tfilter->Delete();
   cylinder->Delete();
   sphere->Delete();
   apd->Delete();
