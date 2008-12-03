98bb205bbf58b8629b7afc99d027cf5be009c5d2
Steve Pieper <pieper@bwh.harvard.edu>
2008-12-03 16:19:05 +0000

    BUG: ignore the registry setting for the layout on windows to work around a crash
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=8036
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@8036 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/vtkSlicerApplication.cxx
+++ Base/GUI/vtkSlicerApplication.cxx
@@ -738,8 +738,11 @@ void vtkSlicerApplication::RestoreApplicationSettingsFromRegistry()
     }
   if ( this->HasRegistryValue (2, "RunTime", vtkSlicerApplication::ApplicationLayoutTypeRegKey))
     {
+    // TODO: don't restore registry layout on windows because some configurations cause a startup crash
+#if !defined(WIN32)
     this->ApplicationLayoutType = this->GetIntRegistryValue(
         2, "RunTime", vtkSlicerApplication::ApplicationLayoutTypeRegKey);
+#endif
     }
 
    if (this->HasRegistryValue(
