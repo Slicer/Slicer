00594760fe1eb36cf67a8e14a805b9aa8d9de665
Steve Pieper <pieper@bwh.harvard.edu>
2008-12-14 17:07:30 +0000

    ENH: improved leak testing script
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=8145
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@8145 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Testing/LeakTest.tcl
+++ Modules/Testing/LeakTest.tcl
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 # the next line restarts using tclsh \
     exec tclsh "$0" "$@"
@@ -12,33 +11,110 @@
 #
 ################################################################################
 
+
+#
+# two global lists of slicer modules and scripted modules 
+#
 set ::modules { 
- "EMSegment"  "Gradient Anisotropic Diffusion Filter"  "Label Statistics"  "Neuro Nav"  
- "OpenIGTLink"  "QdecModule"  "QueryAtlas"  "Slicer Tractography Display"  
- "Slicer Tractography Fiducial Seeding"  "Tumor Growth"  "Volume Rendering"  "Volumes"
+ "CLI" "Change Tracker" "EMSegment"  "Gradient Anisotropic Diffusion Filter"  "Label Statistics"  "Neuro Nav"  
+ "OpenIGTLinkIF"  "QdecModule"  "QueryAtlas"  "Slicer Tractography Display"  
+ "Slicer Tractography Fiducial Seeding"  "Volume Rendering"  "VolumeRenderingCuda" "Volumes"
+ "OpenIGTLink IF" "Skeleton" "IA_FEMesh"
  }
 
+set ::scriptedModules ""
+foreach dir [glob -nocomplain lib/Slicer3/Modules/*] {
+  if { [file exists $dir/pkgIndex.tcl] } {
+    lappend ::scriptedModules $dir
+  }
+}
+
+foreach f [glob -nocomplain lib/Slicer3/Plugins/*.py] {
+#  lappend ::scriptedModules $f
+}
 
 
+
+#
+# routine to run slicer with only the modules in the doNotIgnore list
+# included
+#
 proc runSlicer { {doNotIgnore ""} } {
 
-  set cmd "./Slicer3"
-  
-  foreach m $::modules {
-    if { [lsearch $m $doNotIgnore] == -1 } {
-      set cmd [concat $cmd "--ignore-module \"$m\""]
+  puts "\n\n"
+
+  # rename package files for any ignored scripted modules
+  puts -nonewline "ignoring: "
+  foreach sm $::scriptedModules {
+    if { [file isdir $sm] } {
+      if { [lsearch $sm $doNotIgnore] == -1 } {
+        puts -nonewline " [file tail $sm]"
+        file rename $sm/pkgIndex.tcl $sm/pkgIndexIgnore.tcl
+      }
+    } else {
+        puts -nonewline "[file tail $sm]"
+        file rename $sm [file root $sm].ignore
     }
   }
+  puts "\n"
+  
+  set cmd "./Slicer3"
+
+  # add command line flag for any ignored loadable modules
+  foreach m $::modules {
+    if { [lsearch $m $doNotIgnore] == -1 } {
+      set cmd [concat $cmd "--ignore-module '$m'"]
+    }
+  }
+
+  # exit slicer after the gui is built
   set cmd [concat $cmd "--exec exit"]
+
   puts "running: $cmd"
 
   set ret [catch "eval exec $cmd" res]
 
-  puts $res
+  puts ""
+  if { $ret } {
+    puts "****** Error when not ignoring $doNotIgnore"
+    puts $res
+  } else {
+    puts "-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ NO ERRORS"
+  }
+
+  # restore ignored scripted modules
+  puts -nonewline "restoring: "
+  foreach sm $::scriptedModules {
+    if { [file isdir $sm] } {
+      if { [lsearch $sm $doNotIgnore] == -1 } {
+        puts -nonewline " [file tail $sm]"
+        file rename $sm/pkgIndexIgnore.tcl $sm/pkgIndex.tcl
+      }
+    } else {
+        puts -nonewline " [file tail $sm]"
+        file rename [file root $sm].ignore [file root $sm].py
+    }
+  }
+  
+  return $ret
 }
 
+# run with all modules turned off
+puts "\n\nignoring all"
+runSlicer $::modules
 
+puts "ignoring none"
+runSlicer ""
+
+# sequentially enable only one module at a time
 foreach m $::modules {
-  puts "ignoring $m"
-  runSlicer $m
+  puts "\n\nnot ignoring $m"
+  set ::RESULTS($m) [runSlicer $m]
 }
+
+foreach m $::scriptedModules {
+  puts "\n\nnot ignoring $m"
+  set ::RESULTS($m) [runSlicer $m]
+}
+
+puts [parray ::RESULTS]
