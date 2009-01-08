5e58c38b0ee49d57319454b707fa25ba736ab705
jvs@bwh.harvard.edu <jvs@bwh.harvard.edu>
2009-01-08 18:38:24 +0000

    ENH: only first baseline loaded
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=8324
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@8324 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/Python/shift_DWI_Values.py
@@ -0,0 +1,41 @@
+XML = """<?xml version="1.0" encoding="utf-8"?>
+<executable>
+  <category>Python Modules</category>
+  <title>Python Shift DWI Values</title>
+  <description>Python module</description>
+  <version>0.1.0.$Revision: 1892 $(alpha)</version>
+  <documentation-url></documentation-url>
+  <contributor>Julien von Siebenthal</contributor>
+  <parameters>
+    <label>IO</label>
+    <description>Input/output parameters</description>
+    <image type="diffusion-weighted">
+      <name>inputVolume</name>
+      <label>Input DWI Volume</label>
+      <channel>input</channel>
+      <index>0</index>
+      <description>Input DWI volume</description>
+    </image>
+  </parameters>
+</executable>
+"""
+
+def Execute (inputVolume):
+  Slicer = __import__ ( "Slicer" )
+  slicer = Slicer.slicer
+  scene = slicer.MRMLScene
+
+  inputVolume = scene.GetNodeByID(inputVolume)
+  data = inputVolume.GetImageData().ToArray()
+  dims = inputVolume.GetImageData().GetDimensions()
+
+  shapeD = data.shape
+
+  tmp = inputVolume.GetImageData().ToArray()
+  minI = tmp.min()
+  tmp[:] = tmp[:] + abs(minI)
+
+  inputVolume.Modified()
+
+  return
+
