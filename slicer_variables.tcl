76fda06d9a89bfb9debeaec3035ed1126cb2afe8
Steve Pieper <pieper@bwh.harvard.edu>
2009-02-12 22:42:10 +0000

    COMP: switch to the KWWidgets release branch
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=8631
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@8631 3bd1e089-480b-0410-8dfb-8563597acbee

--- slicer_variables.tcl
+++ slicer_variables.tcl
@@ -75,7 +75,7 @@ puts stderr "Slicer3_HOME is $::Slicer3_HOME"
 set ::Slicer3_TAG "http://svn.slicer.org/Slicer3/trunk"
 set ::CMAKE_TAG "CMake-2-6"
 set ::Teem_TAG http://teem.svn.sourceforge.net/svnroot/teem/teem/tags/1.10.0
-set ::KWWidgets_TAG "HEAD"
+set ::KWWidgets_TAG "Slicer-3-4"
 set ::VTK_TAG "VTK-5-2"
 set ::ITK_TAG ITK-3-10
 set ::PYTHON_TAG "http://svn.python.org/projects/python/branches/release25-maint"
