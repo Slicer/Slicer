b1e45fc06a9eff25a3e90992777ee7a148af4099
jvs@bwh.harvard.edu <jvs@bwh.harvard.edu>
2009-02-16 20:58:25 +0000

    ENH: code clean
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=8649
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@8649 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Python/StochasticTractographyServer/smooth.py
+++ Modules/Python/StochasticTractographyServer/smooth.py
@@ -1,6 +1,6 @@
 import logging
 import time
-import numpy as np
+from numpy import floor, exp, arange, reshape, convolve, sum  
 #import scipy.signal as sciS
 
 logger                   = logging.getLogger(__name__)
@@ -23,20 +23,20 @@ def smooth(x,FWHM,dim):
   for k in range(x.ndim):
     # Create 1D filter kernel
     sigma = FWHM[k]/abs(dim[k])/2.35          # sigma in voxel units
-    l = np.floor(3*sigma+1)
-    f = np.exp(-np.arange(-l,l+1)**2/(2*sigma**2))
-    f = f/np.sum(f)
+    l = floor(3*sigma+1)
+    f = exp(-arange(-l,l+1)**2/(2*sigma**2))
+    f = f/sum(f)
     
     #f = f[:, np.newaxis]
 
     s = x.shape
-    x = np.reshape(x,(s[0], s[1]*s[2]))
+    x = reshape(x,(s[0], s[1]*s[2]))
 
     # Perform 1D convolution
     #x = sciS.convolve2d(f,x,'same')
-    x = np.convolve(f, x.flatten(), 'same')
+    x = convolve(f, x.flatten(), 'same')
 
     # Reshape and shift dims
-    x = np.reshape(x,s)
+    x = reshape(x,s)
     
   return x
