26401c7714ae0d316a44120a846de6681ff475f3
Terry Lorber <tgl@rideside.net>
2009-03-01 02:22:35 +0000

    ENH no longer used
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=8785
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@8785 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/loadablemodules.cmake
+++
@@ -1,9 +0,0 @@
-#
-# DO NOT EDIT!
-#
-# This file will be appended to when using the Modules Wizard within Slicer3
-#
-
-set(CMAKE_MODULE_PATH ${Slicer3_SOURCE_DIR}/Utilities/BuildSystem/CMake)
-include(SlicerParseModule)
-include(SlicerDownloadModule)
