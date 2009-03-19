df532ce91c057e89e17d93fc7b12e5cb812e0ab5
Steve Pieper <pieper@bwh.harvard.edu>
2009-03-19 17:53:46 +0000

    BUG: work in progress fix to island operations upgrade
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=8905
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@8905 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Editor/RemoveIslandsEffect.tcl
+++ Modules/Editor/RemoveIslandsEffect.tcl
@@ -85,19 +85,33 @@ itcl::body RemoveIslandsEffect::apply {} {
 
   foreach {x y} [$_interactor GetEventPosition] {}
 
-  if { [$this getInputLabel] == "" || [$this getInputLabel] == "" } {
+  if { [$this getInputLabel] == "" || [$this getOutputLabel] == "" } {
     $this flashCursor 3
     return
   }
 
+  puts 1
   set conn [vtkImageConnectivity New]
+  puts 2
   $conn SetFunctionToRemoveIslands
+  $conn SetBackground 10
+  puts 3
+  #$conn SetMinSize [expr int([$o(minIslandSize) GetValue])]
+  puts 4
   $conn SetInput [$this getInputLabel]
+  puts 5
   $conn SetOutput [$this getOutputLabel]
+  puts 6
   eval $conn SetSeed [$this getLayerIJK label $x $y]
+  puts 7
   $this setProgressFilter $conn "Remove Islands"
+  puts 8
   [$this getOutputLabel] Update
+  puts [[$this getOutputLabel] Print]
+  puts [$conn Print]
+  puts 9
   $conn Delete
+  puts 1
 
   $this postApply
 }
@@ -118,6 +132,23 @@ itcl::body RemoveIslandsEffect::buildOptions {} {
   $o(help) SetBalloonHelpString "Bring up help window."
   pack [$o(help) GetWidgetName] \
     -side right -anchor sw -padx 2 -pady 2 
+
+  #
+  # a min island size control
+  #
+  set o(minIslandSize) [vtkNew vtkKWThumbWheel]
+  $o(minIslandSize) SetParent [$this getOptionsFrame]
+  $o(minIslandSize) PopupModeOn
+  $o(minIslandSize) Create
+  $o(minIslandSize) DisplayEntryAndLabelOnTopOn
+  $o(minIslandSize) DisplayEntryOn
+  $o(minIslandSize) DisplayLabelOn
+  $o(minIslandSize) SetValue 10000
+  [$o(minIslandSize) GetLabel] SetText "Min Size: "
+  $o(minIslandSize) SetBalloonHelpString "Set the minimum number of pixels in an island to be removed"
+  pack [$o(minIslandSize) GetWidgetName] \
+    -side top -anchor e -fill x -padx 2 -pady 2 
+
 
   #
   # a cancel button
@@ -144,7 +175,7 @@ itcl::body RemoveIslandsEffect::buildOptions {} {
 
 itcl::body RemoveIslandsEffect::tearDownOptions { } {
   chain
-  foreach w "help cancel" {
+  foreach w "help minIslandSize cancel" {
     if { [info exists o($w)] } {
       $o($w) SetParent ""
       pack forget [$o($w) GetWidgetName] 
