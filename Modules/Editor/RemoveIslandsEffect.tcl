37cc7d5fae9e07da36501477c41a7eb6f2283445
Steve Pieper <pieper@bwh.harvard.edu>
2009-04-10 20:04:29 +0000

    BUG: fix remove islands effect
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=9148
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@9148 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Editor/RemoveIslandsEffect.tcl
+++ Modules/Editor/RemoveIslandsEffect.tcl
@@ -33,10 +33,12 @@ if { [itcl::find class RemoveIslandsEffect] == "" } {
 
     # methods
     method processEvent {{caller ""} {event ""}} {}
-    method preview {} {}
     method apply {} {}
     method buildOptions {} {}
     method tearDownOptions {} {}
+    method goToModelMaker {} {}
+    method findNonZeroBorderPixel {imageData} {}
+
   }
 }
 
@@ -81,73 +83,134 @@ itcl::body RemoveIslandsEffect::processEvent { {caller ""} {event ""} } {
   [$sliceGUI GetSliceViewer] RequestRender
 }
 
+# search the border of the image data looking for the first 
+# - usually whole border will be nonzero, but in some cases
+#   it may not be.  So check corners first, and then
+#   if can't find it, give up and use 1 (to avoid exhaustive search)
+itcl::body RemoveIslandsEffect::findNonZeroBorderPixel { imageData } {
+
+  foreach {w h d} [$imageData GetDimensions] {}
+  foreach v {w h d} { set $v [expr [set $v] - 1] }
+
+  set corners [list "0 0 0" "$w 0 0" "0 $h 0" "$w $h 0" \
+                    "0 0 $d" "$w 0 $d" "0 $h $d" "$w $h $d"]
+  foreach corner $corners {
+    foreach {i j k} $corner {}
+    set p [$imageData GetScalarComponentAsDouble $i $j $k 0]
+    if { $p != 0 } {
+      return $p
+    }
+  }
+  return 1
+}
+
 itcl::body RemoveIslandsEffect::apply {} {
 
-  foreach {x y} [$_interactor GetEventPosition] {}
-
-  if { [$this getInputLabel] == "" || [$this getOutputLabel] == "" } {
-    $this flashCursor 3
+  if { [$this getInputLabel] == "" } {
+    $this errorDialog "Label map needed for Island operations"
     return
   }
 
-  puts 1
-  set conn [vtkImageConnectivity New]
-  puts 2
-  $conn SetFunctionToRemoveIslands
-  $conn SetBackground 10
-  puts 3
-  #$conn SetMinSize [expr int([$o(minIslandSize) GetValue])]
-  puts 4
-  $conn SetInput [$this getInputLabel]
-  puts 5
-  $conn SetOutput [$this getOutputLabel]
-  puts 6
-  eval $conn SetSeed [$this getLayerIJK label $x $y]
-  puts 7
-  $this setProgressFilter $conn "Remove Islands"
-  puts 8
-  [$this getOutputLabel] Update
-  puts [[$this getOutputLabel] Print]
-  puts [$conn Print]
-  puts 9
-  $conn Delete
-  puts 1
+  set label [EditorGetPaintLabel]
 
+  # first, create an inverse binary version of the image
+  # so that islands inside segemented object will be detected, along
+  # with a big island of the background
+  set preThresh [vtkImageThreshold New]
+  $preThresh SetInValue 0
+  $preThresh SetOutValue 1
+  $preThresh ReplaceOutOn
+  $preThresh ThresholdBetween $label $label
+  $preThresh SetInput [$this getInputLabel]
+
+  # now identify the islands in the inverted volume
+  # and find the pixel that corresponds to the background
+  set islands [vtkITKIslandMath New]
+  $islands SetInput [$preThresh GetOutput]
+  $islands SetFullyConnected [$o(fullyConnected) GetSelectedState]
+  $islands Update
+  set bgLabel [$this findNonZeroBorderPixel [$islands GetOutput]]
+
+  # now rethreshold so that everything which is not background becomes the label
+  set postThresh [vtkImageThreshold New]
+  $postThresh SetInValue 0
+  $postThresh SetOutValue $label
+  $postThresh ReplaceOutOn
+  $postThresh ThresholdBetween $bgLabel $bgLabel
+  $postThresh SetInput [$islands GetOutput]
+  $postThresh SetOutput [$this getOutputLabel]
+  $postThresh Update
   $this postApply
+
+  $postThresh Delete
+
+
+
+  if { 0 } {
+
+    # Experiment
+
+    # create two pipelines 
+    # - one binary of only foreground
+    # - one binary of only non-foreground
+    # - remove islands from both
+    foreach branch {fore back} in {1 0} out {0 1} {
+      set ${branch}thresh [vtkImageThreshold New]
+      ${branch}thresh SetInValue $in
+      ${branch}thresh SetOutValue $out
+      ${branch}thresh SetReplaceOutOn
+      ${branch}thresh ThresholdBetween $label $label
+      ${branch}thresh SetOutputScalarType [[$this getOutputLabel] GetScalarType]
+
+      set ${branch}islands [vtkITKIslandMath New]
+      ${branch}islands SetInput [${branch}thresh GetOutput]
+      ${branch}islands SetFullyConnected [$o(fullyConnected) GetSelectedState]
+    }
+
+    $backthresh SetInput [$this getInputLabel]
+    $forethresh SetInput [$backislands GetOutput]
+
+    $this setProgressFilter $foreislands "Calculating Islands..."
+    $foreislands Update
+
+    set islandCount [$islands GetNumberOfIslands]
+    set islandOrigCount [$islands GetOriginalNumberOfIslands]
+    set ignoredIslands [expr $islandOrigCount - $islandCount]
+    $this statusText "[$islands GetNumberOfIslands] islands created ($ignoredIslands ignored)"
+    $islands Delete
+    $this postApply
+
+  }
 }
 
-  
 itcl::body RemoveIslandsEffect::buildOptions {} {
 
+  # call superclass version of buildOptions
   chain
 
   #
-  # a help button
+  # 4 or 8 neighbors
   #
-  set o(help) [vtkNew vtkSlicerPopUpHelpWidget]
-  $o(help) SetParent [$this getOptionsFrame]
-  $o(help) Create
-  $o(help) SetHelpTitle "Remove Islands"
-  $o(help) SetHelpText "Click in the \"sea\" that contains islands (areas that are completely surrounded by the sea).  Islands will be converted to the sea color"
-  $o(help) SetBalloonHelpString "Bring up help window."
-  pack [$o(help) GetWidgetName] \
-    -side right -anchor sw -padx 2 -pady 2 
+
+  set o(fullyConnected) [vtkKWCheckButton New]
+  $o(fullyConnected) SetParent [$this getOptionsFrame]
+  $o(fullyConnected) Create
+  $o(fullyConnected) SetText "Fully Connected"
+  $o(fullyConnected) SetBalloonHelpString "When on, do not treat diagonally adjacent voxels as neighbors."
+  $o(fullyConnected) SetSelectedState 0
+  pack [$o(fullyConnected) GetWidgetName] \
+    -side top -anchor e -fill x -padx 2 -pady 2 -expand true
 
   #
-  # a min island size control
+  # an apply button
   #
-  set o(minIslandSize) [vtkNew vtkKWThumbWheel]
-  $o(minIslandSize) SetParent [$this getOptionsFrame]
-  $o(minIslandSize) PopupModeOn
-  $o(minIslandSize) Create
-  $o(minIslandSize) DisplayEntryAndLabelOnTopOn
-  $o(minIslandSize) DisplayEntryOn
-  $o(minIslandSize) DisplayLabelOn
-  $o(minIslandSize) SetValue 10000
-  [$o(minIslandSize) GetLabel] SetText "Min Size: "
-  $o(minIslandSize) SetBalloonHelpString "Set the minimum number of pixels in an island to be removed"
-  pack [$o(minIslandSize) GetWidgetName] \
-    -side top -anchor e -fill x -padx 2 -pady 2 
+  set o(apply) [vtkNew vtkKWPushButton]
+  $o(apply) SetParent [$this getOptionsFrame]
+  $o(apply) Create
+  $o(apply) SetText "Apply"
+  $o(apply) SetBalloonHelpString "Run the selected operation."
+  pack [$o(apply) GetWidgetName] \
+    -side right -anchor e -padx 2 -pady 2 
 
 
   #
@@ -157,28 +220,48 @@ itcl::body RemoveIslandsEffect::buildOptions {} {
   $o(cancel) SetParent [$this getOptionsFrame]
   $o(cancel) Create
   $o(cancel) SetText "Cancel"
-  $o(cancel) SetBalloonHelpString "Cancel threshold without applying to label map."
+  $o(cancel) SetBalloonHelpString "Cancel this effect."
   pack [$o(cancel) GetWidgetName] \
     -side right -anchor e -padx 2 -pady 2 
 
   #
+  # a help button
+  #
+  set o(help) [vtkNew vtkSlicerPopUpHelpWidget]
+  $o(help) SetParent [$this getOptionsFrame]
+  $o(help) Create
+  $o(help) SetHelpTitle "RemoveIslands"
+  $o(help) SetHelpText "Use this tool to create a unique label value for each connected region in the current label map.  Connected regions are defined as groups of pixels which touch each other but are surrounded by zero valued voxels.  If FullyConnected is selected, then only voxels that share a face are counted as connected; if unselected, then voxels that touch at an edge or a corner are considered connected.\n\n Note: be aware that all non-zero label values labels values are considered equal by this filter and that the result will renumber the resulting islands in order of size."
+  $o(help) SetBalloonHelpString "Bring up help window."
+  pack [$o(help) GetWidgetName] \
+    -side right -anchor sw -padx 2 -pady 2 
+
+  #
   # event observers - TODO: if there were a way to make these more specific, I would...
   #
+  set tag [$o(apply) AddObserver AnyEvent "$this apply"]
+  lappend _observerRecords "$o(apply) $tag"
   set tag [$o(cancel) AddObserver AnyEvent "after idle ::EffectSWidget::RemoveAll"]
   lappend _observerRecords "$o(cancel) $tag"
 
-  if { [$this getInputLabel] == "" } {
-    $this errorDialog "Need to have a label layer to use island effect"
+  if { [$this getOutputLabel] == "" } {
+    $this errorDialog "Label map needed for RemoveIslandsing"
     after idle ::EffectSWidget::RemoveAll
   }
+
+  $this updateGUIFromMRML
 }
 
 itcl::body RemoveIslandsEffect::tearDownOptions { } {
+
+  # call superclass version of tearDownOptions
   chain
-  foreach w "help minIslandSize cancel" {
+
+  foreach w "minSize fullyConnected help cancel apply" {
     if { [info exists o($w)] } {
       $o($w) SetParent ""
       pack forget [$o($w) GetWidgetName] 
     }
   }
 }
+
