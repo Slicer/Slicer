4bacf27c1cf9dc4a78b5ca0897534f7471069f63
Fedorov <fedorov@bwh.harvard.edu>
2009-05-18 19:16:23 +0000

    BUG: incorrect name in win32 header
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=9527
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@9527 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/FastMarchingSegmentation/vtkPichonFastMarchingWin32Header.h
+++ Modules/FastMarchingSegmentation/vtkPichonFastMarchingWin32Header.h
@@ -5,7 +5,7 @@
 #include <vtkPichonFastMarchingConfigure.h>
 
 #if defined(WIN32) && !defined(VTKSLICER_STATIC)
-#if defined(FastMarching_EXPORTS)
+#if defined(PichonFastMarching_EXPORTS)
 #define VTK_PICHONFASTMARCHING_EXPORT __declspec( dllexport ) 
 #else
 #define VTK_PICHONFASTMARCHING_EXPORT __declspec( dllimport ) 
