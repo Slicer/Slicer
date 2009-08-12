6cf861479df4baf7c93190bc8a08b4cdd0fd824f
Nicole Aucoin <nicole@bwh.harvard.edu>
2009-08-12 18:30:21 +0000

    ENH: jump up to VTK 5.4
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=10227
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@10227 3bd1e089-480b-0410-8dfb-8563597acbee

--- slicer_variables.tcl
+++ slicer_variables.tcl
@@ -76,7 +76,7 @@ set ::Slicer3_TAG "http://svn.slicer.org/Slicer3/trunk"
 set ::CMAKE_TAG "CMake-2-6"
 set ::Teem_TAG http://teem.svn.sourceforge.net/svnroot/teem/teem/tags/1.10.0
 set ::KWWidgets_TAG "Slicer-3-4"
-set ::VTK_TAG "VTK-5-2"
+set ::VTK_TAG "VTK-5-4"
 set ::ITK_TAG ITK-3-14
 set ::PYTHON_TAG "http://svn.python.org/projects/python/branches/release25-maint"
 set ::BLAS_TAG http://svn.slicer.org/Slicer3-lib-mirrors/trunk/netlib/BLAS
