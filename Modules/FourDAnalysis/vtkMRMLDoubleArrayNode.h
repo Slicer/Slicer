de00f5424c0f4151812ac5250a3a172f90a42386
Steve Pieper <pieper@bwh.harvard.edu>
2009-08-27 18:16:53 +0000

    BUG: avoid name clash with Windows GetYValue macro
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=10373
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@10373 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/FourDAnalysis/vtkMRMLDoubleArrayNode.h
+++ Modules/FourDAnalysis/vtkMRMLDoubleArrayNode.h
@@ -101,12 +101,12 @@ class VTK_FourDAnalysis_EXPORT vtkMRMLDoubleArrayNode : public vtkMRMLNode
 
   // Description:
   // Get X and Y values at the data point specified by 'index'
-  int GetXYValue(int index, double* x, double* y);
+  int GetXYAxisValue(int index, double* x, double* y);
 
   // Description:
   // Get X and Y values with error (or standard deviation) of Y,
   // at the data point specified by 'index'
-  int GetXYValue(int index, double* x, double* y, double*  yerr);
+  int GetXYAxisValue(int index, double* x, double* y, double*  yerr);
 
   // Description:
   // Set X and Y values at he data point specified by 'index'
