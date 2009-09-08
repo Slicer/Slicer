58ab570330ac3c77d7c1a9308d7f996bd7e2a97d
Steve Pieper <pieper@bwh.harvard.edu>
2009-09-08 23:34:16 +0000

    COMP: handle differences in build path settings
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=10429
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@10429 3bd1e089-480b-0410-8dfb-8563597acbee

--- Scripts/genlib.tcl
+++ Scripts/genlib.tcl
@@ -793,12 +793,13 @@ if {  [BuildThis $::NUMPY_TEST_FILE "python"] && !$::USE_SYSTEM_PYTHON && [strin
           regsub -all ":" $::COMPILER_PATH "" vcbindir
           set devenvdir /cygdrive/$devenvdir
           set vcbindir /cygdrive/$vcbindir
+          set ::env(PATH) $devenvdir:$vcbindir:$::env(PATH)
         } else {
           # Jim's way - cygwin does mount c:/ as /c and doesn't use cygdrive
           set devenvdir [file dirname $::MAKE]
           set vcbindir $::COMPILER_PATH
+          set ::env(PATH) $devenvdir\;$vcbindir\;$::env(PATH)
         }
-        set ::env(PATH) $devenvdir\;$vcbindir\;$::env(PATH)
         set ::env(PATH) $::Slicer3_LIB/python-build/PCbuild\;$::env(PATH)
         set ::env(INCLUDE) [file dirname $::COMPILER_PATH]/include
         set ::env(INCLUDE) $::MSSDK_PATH/Include\;$::env(INCLUDE)
