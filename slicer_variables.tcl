e0a49a942f6109f96f70fb9c8e3af727f18f3d44
Wendy Plesniak <wjp@bwh.harvard.edu>
2009-11-04 12:44:16 +0000

    ENH: i think we have to change slicer_variables.tcl to use the branch tag? trying...
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=10859
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@10859 3bd1e089-480b-0410-8dfb-8563597acbee

--- slicer_variables.tcl
+++ slicer_variables.tcl
@@ -72,7 +72,7 @@ puts "Slicer3_HOME is $::Slicer3_HOME"
 # changes in the "Files to test if library has already been built"
 # section below, or genlib will happily build the library again.
 
-set ::Slicer3_TAG "http://svn.slicer.org/Slicer3/trunk"
+set ::Slicer3_TAG "http://svn.slicer.org/Slicer3/branches/Slicer3.5-RSNAWorkshop"
 set ::CMAKE_TAG "CMake-2-6"
 set ::Teem_TAG http://teem.svn.sourceforge.net/svnroot/teem/teem/tags/1.10.0
 set ::KWWidgets_TAG "Slicer-3-4"
