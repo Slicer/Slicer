4fa97a5ce8603847a82141298a4b0872b33bbcdc
Steve Pieper <pieper@bwh.harvard.edu>
2009-11-06 18:36:53 +0000

    ENH: fix plot widget example
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=10892
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@10892 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/FourDAnalysis/Testing/TestPlotGraph.tcl
+++ Modules/FourDAnalysis/Testing/TestPlotGraph.tcl
@@ -1,29 +1,39 @@
 
-# example code to create an XY Plot
-# TODO: this does not create a valid plot - not clear what the data fields need to be
+# example code to create an XY Plot based on:
+# http://www.slicer.org/slicerWiki/index.php/Slicer3:2DPlotting
+
+
+#
+# create a double array that has three components: 
+#
+set data1 [vtkDoubleArray New]
+$data1 SetNumberOfComponents 3
+$data1 InsertNextTuple3 0 10 1
+$data1 InsertNextTuple3 1 20 1
+$data1 InsertNextTuple3 2 50 3
+
+set doubleNode [vtkMRMLDoubleArrayNode New]
+$::slicer3::MRMLScene AddNode $doubleNode
+$doubleNode SetArray $data1
+
+set plotNode [vtkMRMLArrayPlotNode New]
+$::slicer3::MRMLScene AddNode $plotNode
+$plotNode SetAndObserveArray $doubleNode
+
+set managerNode [vtkMRMLXYPlotManagerNode New]
+$::slicer3::MRMLScene AddNode $managerNode
+$managerNode AddPlotNode $plotNode
+
 
 set toplevel [vtkKWTopLevel New]
 $toplevel SetApplication $::slicer3::Application
 $toplevel Create
 
-set plot [vtkKWPlotGraph New]
+set plot [vtkSlicerXYPlotWidget New]
 $plot SetParent $toplevel
+$plot SetAndObservePlotManagerNode $managerNode
 $plot Create
 pack [$plot GetWidgetName] -fill both -expand true
-
-set data1 [vtkDoubleArray New]
-$data1 InsertNextValue 10
-$data1 InsertNextValue 20
-$data1 InsertNextValue 50
-$data1 InsertNextValue 40
-$plot AddPlot $data1 "Sample Data"
-
-set data2 [vtkDoubleArray New]
-$data2 InsertNextValue 4
-$data2 InsertNextValue 6
-$data2 InsertNextValue 7
-$data2 InsertNextValue 9
-$plot AddPlot $data2 "Random Data"
 
 $plot UpdateGraph
 
