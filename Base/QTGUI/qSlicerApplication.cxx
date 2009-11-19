0f39309caf85304224a1cd7ace8ba4cc373722d7
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2009-11-19 13:43:22 +0000

    ENH: SlicerQT - Add intialize, appLogic, defaultWindowflags and setWindowFlags methods to qSlicerApplication. Move initPalette, initFont and loadStyleSheet to internal.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=10992
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@10992 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTGUI/qSlicerApplication.cxx
+++ Base/QTGUI/qSlicerApplication.cxx
@@ -1,9 +1,14 @@
-#include "qSlicerApplication.h" 
+#include "qSlicerApplication.h"
 
+// SlicerQT includes
 #include "qSlicerModuleManager.h"
 #include "qSlicerModuleFactory.h"
 #include "qSlicerModulePanel.h"
 
+// SlicerLogic includes
+#include "vtkSlicerApplicationLogic.h"
+
+// QT includes
 #include <QRect>
 #include <QPalette>
 #include <QColor>
@@ -14,59 +19,124 @@
 #include <QMap>
 #include <QDebug>
 
+// MRML includes
 #include "vtkMRMLScene.h"
+#include "vtkMRMLCrosshairNode.h"
+
+// VTK includes
+#include "vtkSmartPointer.h"
+
+// VTKSYS includes
+#include <vtksys/SystemTools.hxx>
+#include <vtksys/stl/string>
 
 #include "vtkSlicerConfigure.h"
 
 //-----------------------------------------------------------------------------
-class qSlicerApplication::qInternal
+struct qSlicerApplication::qInternal
 {
-public:
-  qInternal()
+  qInternal(qSlicerApplication* backPointer):
+    BackPointer(backPointer)
     {
-    this->MRMLScene = 0;
-    this->ModulePanel = 0;
+    this->initFont();
+    this->initPalette();
+    this->loadStyleSheet();
     }
-  vtkMRMLScene *              MRMLScene; 
-  QMap<QWidget*,bool>         TopLevelWidgetsSavedVisibilityState; 
-  QString                     SlicerHome;
-  qSlicerAbstractModulePanel* ModulePanel;
+
+  // Description:
+  // Initialize application Palette/Font
+  void initPalette();
+  void initFont();
+
+  // Description:
+  // Load application styleSheet
+  void loadStyleSheet();
+
+  vtkSmartPointer<vtkMRMLScene>               MRMLScene;
+  vtkSmartPointer<vtkSlicerApplicationLogic>  AppLogic;
+
+  qSlicerApplication*           BackPointer;
+  QMap<QWidget*,bool>           TopLevelWidgetsSavedVisibilityState;
+  QString                       SlicerHome;
+  Qt::WindowFlags               DefaultWindowFlags;
 };
 
 //-----------------------------------------------------------------------------
 qSlicerApplication::qSlicerApplication(int &argc, char **argv)
   : Superclass(argc, argv)
 {
-  this->Internal = new qInternal;
-  this->initFont(); 
-  this->initPalette(); 
-  this->loadStyleSheet(); 
-
-  this->Internal->ModulePanel = 
-    new qSlicerModulePanel(0, 
-                           Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint);
-  //this->Internal->ModulePanel->setScrollAreaAsParentContainer(true);
-
-  QObject::connect(this->Internal->ModulePanel, 
-                   SIGNAL(moduleAdded(qSlicerAbstractModule*)),
-                   this, 
-                   SLOT(showModule(qSlicerAbstractModule*)));
-  //this->Internal->ModulePanel->show();
+  this->Internal = new qInternal(this);
 }
 
 //-----------------------------------------------------------------------------
 qSlicerApplication::~qSlicerApplication()
 {
-  delete this->Internal; 
+  // Make sure all modules are unparented
+  qSlicerModuleManager::instance()->modulePanel()->removeAllModule();
+
+  // Uninstantiate modules
+  qSlicerModuleManager::instance()->factory()->uninstantiateAll();
+
+  delete this->Internal;
 }
 
 //-----------------------------------------------------------------------------
 qSlicerApplication* qSlicerApplication::application()
 {
-  qSlicerApplication* app =
-    qobject_cast<qSlicerApplication*>(QApplication::instance());
+  qSlicerApplication* app = qobject_cast<qSlicerApplication*>(QApplication::instance());
   return app;
 }
+
+//-----------------------------------------------------------------------------
+void qSlicerApplication::initialize()
+{
+  // Create MRML scene
+  vtkSmartPointer<vtkMRMLScene> scene = vtkSmartPointer<vtkMRMLScene>::New();
+  vtksys_stl::string root = vtksys::SystemTools::GetCurrentWorkingDirectory();
+  scene->SetRootDirectory(root.c_str());
+  vtkMRMLScene::SetActiveScene( scene );
+
+  // Create the application Logic object,
+  vtkSmartPointer<vtkSlicerApplicationLogic> appLogic =
+    vtkSmartPointer<vtkSlicerApplicationLogic>::New();
+  appLogic->SetMRMLScene(scene);
+
+  // pass through event handling once without observing the scene
+  // -- allows any dependent nodes to be created
+  appLogic->ProcessMRMLEvents(scene, vtkCommand::ModifiedEvent, NULL);
+  appLogic->SetAndObserveMRMLScene(scene);
+  appLogic->CreateProcessingThread();
+
+  // --- First scene needs a crosshair to be added manually
+  vtkSmartPointer<vtkMRMLCrosshairNode> crosshair = vtkSmartPointer<vtkMRMLCrosshairNode>::New();
+  crosshair->SetCrosshairName("default");
+  scene->AddNode( crosshair );
+
+  this->setMRMLScene(scene);
+  this->setAppLogic(appLogic);
+}
+
+//-----------------------------------------------------------------------------
+qSlicerSetInternalCxxMacro(qSlicerApplication, Qt::WindowFlags, setDefaultWindowFlags, DefaultWindowFlags);
+qSlicerGetInternalCxxMacro(qSlicerApplication, Qt::WindowFlags, defaultWindowFlags, DefaultWindowFlags);
+
+//-----------------------------------------------------------------------------
+void qSlicerApplication::setMRMLScene(vtkMRMLScene* mrmlScene)
+{
+  if (this->Internal->MRMLScene == mrmlScene)
+    {
+    return;
+    }
+  this->Internal->MRMLScene = mrmlScene;
+  emit this->currentMRMLSceneChanged(mrmlScene);
+}
+
+//-----------------------------------------------------------------------------
+qSlicerGetInternalCxxMacro(qSlicerApplication, vtkMRMLScene*, mrmlScene, MRMLScene);
+
+//-----------------------------------------------------------------------------
+qSlicerSetInternalCxxMacro(qSlicerApplication, vtkSlicerApplicationLogic*, setAppLogic, AppLogic);
+qSlicerGetInternalCxxMacro(qSlicerApplication, vtkSlicerApplicationLogic*, appLogic, AppLogic);
 
 //-----------------------------------------------------------------------------
 QString qSlicerApplication::slicerHome()
@@ -78,7 +148,7 @@ QString qSlicerApplication::slicerHome()
 //-----------------------------------------------------------------------------
 void qSlicerApplication::setSlicerHome(const QString& slicerHome)
 {
-  this->Internal->SlicerHome = slicerHome; 
+  this->Internal->SlicerHome = slicerHome;
 }
 
 //-----------------------------------------------------------------------------
@@ -88,25 +158,25 @@ void qSlicerApplication::initializeLoadableModulesPaths()
   // in the install location, and exec/libs are *automatically* installed
   // in intDir.
   QString defaultQTModulePaths = this->slicerHome() + Slicer3_INSTALL_QTLOADABLEMODULES_LIB_DIR;
-  
+
 //   if (hasIntDir)
 //     {
-//     defaultQTModulePaths = defaultQTModulePaths + PathSep + 
+//     defaultQTModulePaths = defaultQTModulePaths + PathSep +
 //       slicerHome + "/" + Slicer3_INSTALL_QTLOADABLEMODULES_LIB_DIR + "/" + intDir;
 //     }
-    
+
   // add the default modules directory (based on the slicer
   // installation or build tree) to the user paths
   QString qtModulePaths = /*userModulePaths + PathSep + */defaultQTModulePaths;
   this->addLibraryPath(defaultQTModulePaths);
-  
+
 //   foreach (QString path, this->libraryPaths())
 //     {
 //     qDebug() << "libraryPath:" << path;
 //     }
 
-  QStringList paths; 
-  paths << qtModulePaths; 
+  QStringList paths;
+  paths << qtModulePaths;
   qSlicerModuleManager::instance()->factory()->setLoadableModuleSearchPaths(paths);
   //qDebug() << "initializeLoadableModulesPaths - qtModulePaths:" << qtModulePaths;
 }
@@ -115,32 +185,85 @@ void qSlicerApplication::initializeLoadableModulesPaths()
 void qSlicerApplication::initializeCmdLineModulesPaths()
 {
   QString defaultCmdLineModulePaths;
-  
+
   // On Win32, *both* paths have to be there, since scripts are installed
   // in the install location, and exec/libs are *automatically* installed
   // in intDir.
   defaultCmdLineModulePaths = this->slicerHome() + "/" + Slicer3_INSTALL_PLUGINS_BIN_DIR;
 //   if (hasIntDir)
 //     {
-//     defaultQTModulePaths = defaultCmdLineModulePaths + PathSep + 
+//     defaultQTModulePaths = defaultCmdLineModulePaths + PathSep +
 //       slicerHome + "/" + Slicer3_INSTALL_PLUGINS_BIN_DIR + "/" + intDir;
 //     }
-    
+
   // add the default modules directory (based on the slicer
   // installation or build tree) to the user paths
   QString cmdLineModulePaths = /*userModulePaths + PathSep +*/ defaultCmdLineModulePaths;
   this->addLibraryPath(defaultCmdLineModulePaths);
-  
-  QStringList paths; 
-  paths << cmdLineModulePaths; 
+
+  QStringList paths;
+  paths << cmdLineModulePaths;
   qSlicerModuleManager::instance()->factory()->setCmdLineModuleSearchPaths(paths);
   //cout << "cmdLineModulePaths:" << cmdLineModulePaths << endl;
 }
 
 //-----------------------------------------------------------------------------
-void qSlicerApplication::initPalette()
+void qSlicerApplication::setTopLevelWidgetsVisible(bool visible)
 {
-  QPalette p = this->palette();
+  foreach(QWidget * widget, this->topLevelWidgets())
+    {
+    // Store current visibility state
+    if (!visible)
+      {
+      if (!this->Internal->TopLevelWidgetsSavedVisibilityState.contains(widget))
+        {
+        this->Internal->TopLevelWidgetsSavedVisibilityState[widget] = widget->isVisible();
+        }
+      widget->hide();
+      }
+    else
+      {
+      QMap<QWidget*,bool>::const_iterator it =
+        this->Internal->TopLevelWidgetsSavedVisibilityState.find(widget);
+
+      // If widget state was saved, restore it. Otherwise skip.
+      if (it != this->Internal->TopLevelWidgetsSavedVisibilityState.end())
+        {
+        widget->setVisible(it.value());
+        }
+      }
+    }
+
+  // Each time widget are set visible. Internal Map can be cleared.
+  if (visible)
+    {
+    this->Internal->TopLevelWidgetsSavedVisibilityState.clear();
+    }
+}
+
+//-----------------------------------------------------------------------------
+void qSlicerApplication::setTopLevelWidgetVisible(qSlicerWidget* widget, bool visible)
+{
+  if (!widget) { return; }
+  Q_ASSERT(!widget->parent());
+  // When internal Map is empty, it means top widget are visible
+  if (this->Internal->TopLevelWidgetsSavedVisibilityState.empty())
+    {
+    widget->setVisible(visible);
+    }
+  else
+    {
+    this->Internal->TopLevelWidgetsSavedVisibilityState[widget] = visible;
+    }
+}
+
+//-----------------------------------------------------------------------------
+// Internal methods
+
+//-----------------------------------------------------------------------------
+void qSlicerApplication::qInternal::initPalette()
+{
+  QPalette p = this->BackPointer->palette();
   /* Old palette that makes a high contrast in Windows.
   p.setColor(QPalette::Window, Qt::white);
   p.setColor(QPalette::Base, Qt::white);
@@ -157,124 +280,33 @@ void qSlicerApplication::initPalette()
   p.setColor(QPalette::Shadow, "#5a5a5a");
   p.setColor(QPalette::AlternateBase, QColor("#e4e4fe"));
 
-  this->setPalette(p);
+  this->BackPointer->setPalette(p);
 }
 
 //-----------------------------------------------------------------------------
-void qSlicerApplication::initFont()
+void qSlicerApplication::qInternal::initFont()
 {
   /*
   QFont f("Verdana", 9);
   QFontInfo ff(f);
   QFontDatabase database;
-  foreach (QString family, database.families()) 
+  foreach (QString family, database.families())
     {
     cout << family.toStdString() << endl;
     }
 
   cout << "Family: " << ff.family().toStdString() << endl;
   cout << "Size: " << ff.pointSize() << endl;
-  this->setFont(f);
+  this->BackPointer->setFont(f);
   */
 }
 
 //-----------------------------------------------------------------------------
-void qSlicerApplication::loadStyleSheet()
+void qSlicerApplication::qInternal::loadStyleSheet()
 {
-//   QString styleSheet = 
+//   QString styleSheet =
 //     "background-color: white;"
 //     "alternate-background-color: #e4e4fe;";
-//   
-//   this->setStyleSheet(styleSheet);
-}
-
-//-----------------------------------------------------------------------------
-void qSlicerApplication::setTopLevelWidgetsVisible(bool visible)
-{
-  foreach(QWidget * widget, this->topLevelWidgets())
-    {
-    // Store current visibility state
-    if (!visible)
-      {
-      if (!this->Internal->TopLevelWidgetsSavedVisibilityState.contains(widget))
-        {
-        this->Internal->TopLevelWidgetsSavedVisibilityState[widget] = widget->isVisible(); 
-        }
-      widget->hide();
-      }
-    else
-      {
-      QMap<QWidget*,bool>::const_iterator it = 
-        this->Internal->TopLevelWidgetsSavedVisibilityState.find(widget); 
-        
-      // If widget state was saved, restore it. Otherwise skip.
-      if (it != this->Internal->TopLevelWidgetsSavedVisibilityState.end())
-        {
-        widget->setVisible(it.value());
-        }
-      }
-    }
-  
-  // Each time widget are set visible. Internal Map can be cleared.
-  if (visible)
-    {
-    this->Internal->TopLevelWidgetsSavedVisibilityState.clear();
-    }
-}
-
-//-----------------------------------------------------------------------------
-void qSlicerApplication::setMRMLScene(vtkMRMLScene * scene)
-{
-  this->Internal->MRMLScene = scene; 
-}
-
-//-----------------------------------------------------------------------------
-vtkMRMLScene* qSlicerApplication::getMRMLScene()
-{
-  return this->Internal->MRMLScene; 
-}
-
-//-----------------------------------------------------------------------------
-qSlicerAbstractModulePanel* qSlicerApplication::modulePanel()
-{
-  return this->Internal->ModulePanel;
-}
-
-//-----------------------------------------------------------------------------
-void qSlicerApplication::showModule(qSlicerAbstractModule* module)
-{
-  this->setTopLevelWidgetVisibility(this->Internal->ModulePanel, module != 0);
-}
-
-//-----------------------------------------------------------------------------
-void qSlicerApplication::setTopLevelWidgetVisibility(qSlicerWidget* widget, bool visibility)
-{  
-  if (visibility)
-    {// is a valid Module
-    if (widget->isVisible() == false)
-      {
-      if (this->Internal->TopLevelWidgetsSavedVisibilityState.empty())
-        {
-        widget->setVisible(true);
-        }
-      else
-        {
-        this->Internal->TopLevelWidgetsSavedVisibilityState[widget] = true;
-        }
-      }
-    }
-  else
-    {// is not a module, we want to hide the ModulePanel
-    if (widget->isVisible() == false)
-      {
-      if (this->Internal->TopLevelWidgetsSavedVisibilityState.empty() == false)
-        {
-        this->Internal->TopLevelWidgetsSavedVisibilityState[widget] = false;
-        }
-      }
-    else
-      {
-      widget->setVisible(false);
-      }
-    }
+//
+//   this->BackPointer->setStyleSheet(styleSheet);
 }
--- Base/QTGUI/qSlicerApplication.h
+++ Base/QTGUI/qSlicerApplication.h
@@ -1,14 +1,15 @@
 #ifndef __qSlicerApplication_h
-#define __qSlicerApplication_h 
+#define __qSlicerApplication_h
 
 #include <QApplication>
 
 #include "qSlicerBaseQTGUIWin32Header.h"
 
-class vtkMRMLScene; 
 class qSlicerAbstractModule;
-class qSlicerAbstractModulePanel;
+// class qSlicerAbstractModulePanel;
 class qSlicerWidget;
+class vtkSlicerApplicationLogic;
+class vtkMRMLScene;
 
 class Q_SLICER_BASE_QTGUI_EXPORT qSlicerApplication : public QApplication
 {
@@ -19,46 +20,61 @@ public:
   typedef QApplication Superclass;
   qSlicerApplication(int &argc, char **argv);
   virtual ~qSlicerApplication();
-  
+
   // Description:
-  // Set the visibility of the top level widgets. 
+  // Return a reference to the application singleton
+  static qSlicerApplication* application();
+
+  // Description:
+  // Instantiate MRMLScene and Application logic.
+  //  - Configure scene
+  //  - AppLogic is set as a scene observer.
+  //  - Create processing thread
+  void initialize();
+
+  // Description:
+  // Set the visibility of the top level widgets.
   // If set to 'False', the state of the widget will be saved.
   // If set to 'True, if possible the saved state will be restored.
-  void setTopLevelWidgetsVisible(bool visible); 
-  
+  void setTopLevelWidgetsVisible(bool visible);
+
+  // Description:
+  // Set the visibility of a given top level widgets
+  void setTopLevelWidgetVisible(qSlicerWidget* widget, bool visible);
+
+  // Description:
+  // Set/Get default window flags that could be used when displaying top level widgets
+  void setDefaultWindowFlags(Qt::WindowFlags defaultWindowFlags);
+  Qt::WindowFlags defaultWindowFlags() const;
+
   // Description:
   // Set/Get MRML Scene
   void setMRMLScene(vtkMRMLScene * scene);
-  vtkMRMLScene* getMRMLScene();
+  vtkMRMLScene* mrmlScene() const;
 
-  static qSlicerApplication* application();
-  
+  // Description:
+  // Set/Get application logic
+  void setAppLogic(vtkSlicerApplicationLogic* appLogic);
+  vtkSlicerApplicationLogic* appLogic() const;
+
   // Description:
   // Return slicer home directory
   QString slicerHome();
   void setSlicerHome(const QString& slicerHome);
-  
+
+  // Description:
+  // Initialize paths
   void initializeLoadableModulesPaths();
   void initializeCmdLineModulesPaths();
 
-  qSlicerAbstractModulePanel* modulePanel();
-  void setTopLevelWidgetVisibility(qSlicerWidget* widget, bool visible);
+signals:
+  void currentMRMLSceneChanged(vtkMRMLScene* mrmlScene);
 
-public slots:
-  void showModule(qSlicerAbstractModule*);
-
-protected:
-  // Description:
-  // Initialize application Palette/Font
-  void initPalette(); 
-  void initFont(); 
-  
-  // Description:
-  // Load application styleSheet
-  void loadStyleSheet(); 
+// public slots:
+//   void showModule(qSlicerAbstractModule*);
 
 private:
-  class qInternal;
+  struct qInternal;
   qInternal* Internal;
 };
 
