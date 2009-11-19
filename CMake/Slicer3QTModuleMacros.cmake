af2b52bf28a5d4720efb0d7054045bcea2f827f6
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2009-11-19 16:18:07 +0000

    ENH: SlicerQT - Update macro 'Slicer3_build_qtmodule', the list of resources can now be paassed as a parameter.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11004
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11004 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/Slicer3QTModuleMacros.cmake
+++ CMake/Slicer3QTModuleMacros.cmake
@@ -4,27 +4,27 @@
 
 MACRO(Slicer3_build_qtmodule)
   SLICER3_PARSE_ARGUMENTS(QTMODULE
-    "NAME;TITLE;EXPORT_DIRECTIVE;SRCS;MOC_SRCS;UI_SRCS;INCLUDE_DIRECTORIES;TARGET_LIBRARIES"
-    "NO_RESOURCES"
+    "NAME;TITLE;EXPORT_DIRECTIVE;SRCS;MOC_SRCS;UI_SRCS;INCLUDE_DIRECTORIES;TARGET_LIBRARIES;RESOURCES"
+    ""
     ${ARGN}
     )
-  
+
   # Sanity checks
   IF(NOT DEFINED QTMODULE_NAME)
     MESSAGE(SEND_ERROR "NAME is mandatory")
   ENDIF(NOT DEFINED QTMODULE_NAME)
-  
+
   IF(NOT DEFINED QTMODULE_EXPORT_DIRECTIVE)
     MESSAGE(SEND_ERROR "EXPORT_DIRECTIVE is mandatory")
   ENDIF(NOT DEFINED QTMODULE_EXPORT_DIRECTIVE)
-  
+
   IF(NOT DEFINED QTMODULE_TITLE)
     SET(QTMODULE_TITLE ${QTMODULE_NAME})
   ENDIF(NOT DEFINED QTMODULE_TITLE)
-  
+
   # Define library name
   SET(lib_name qSlicer${QTMODULE_NAME}Module)
-  
+
   # --------------------------------------------------------------------------
   # Find Slicer3
 
@@ -44,45 +44,46 @@ MACRO(Slicer3_build_qtmodule)
     ${Slicer3_Base_INCLUDE_DIRS}
     ${QTMODULE_INCLUDE_DIRECTORIES}
     )
-  
+
   SET(MY_LIBRARY_EXPORT_DIRECTIVE ${QTMODULE_EXPORT_DIRECTIVE})
   SET(MY_WIN32_HEADER_PREFIX qSlicer${QTMODULE_NAME}Module)
   SET(MY_LIBNAME ${lib_name})
-  
+
   CONFIGURE_FILE(
-    ${QTModules_SOURCE_DIR}/qSlicerQTModulesConfigure.h.in 
+    ${QTModules_SOURCE_DIR}/qSlicerQTModulesConfigure.h.in
     ${CMAKE_CURRENT_BINARY_DIR}/${MY_WIN32_HEADER_PREFIX}Configure.h
     )
-  
+
   CONFIGURE_FILE(
-    ${QTModules_SOURCE_DIR}/qSlicerQTModulesWin32Header.h.in 
+    ${QTModules_SOURCE_DIR}/qSlicerQTModulesWin32Header.h.in
     ${CMAKE_CURRENT_BINARY_DIR}/${MY_WIN32_HEADER_PREFIX}Win32Header.h
     )
-  
+
   # Install headers
   FILE(GLOB headers "${CMAKE_CURRENT_SOURCE_DIR}/*.h")
-  INSTALL(FILES 
-    ${headers} 
+  INSTALL(FILES
+    ${headers}
     "${CMAKE_CURRENT_BINARY_DIR}/${MY_WIN32_HEADER_PREFIX}Configure.h"
     "${CMAKE_CURRENT_BINARY_DIR}/${MY_WIN32_HEADER_PREFIX}Win32Header.h"
     DESTINATION ${Slicer3_INSTALL_QTLOADABLEMODULES_INCLUDE_DIR}/${PROJECT_NAME} COMPONENT Development
     )
-  
-  #file(GLOB files "${CMAKE_CURRENT_SOURCE_DIR}/Resources/*.h") 
-  #install(FILES 
+
+  #file(GLOB files "${CMAKE_CURRENT_SOURCE_DIR}/Resources/*.h")
+  #install(FILES
   #  ${files}
   #  DESTINATION ${Slicer3_INSTALL_INCLUDE_DIR}/${PROJECT_NAME}/Resources COMPONENT Development
   #  )
-  
+
   #-----------------------------------------------------------------------------
   # Sources
   #
-  
+
   QT4_WRAP_CPP(QTMODULE_SRCS ${QTMODULE_MOC_SRCS})
   QT4_WRAP_UI(QTMODULE_UI_CXX ${QTMODULE_UI_SRCS})
-  IF (${QTMODULE_NO_RESOURCES} EQUAL FALSE)
-    QT4_ADD_RESOURCES(QTMODULE_QRC_SRCS Resources/qSlicer${QTMODULE_NAME}Module.qrc)
-  ENDIF(${QTMODULE_NO_RESOURCES} EQUAL FALSE)
+  IF(DEFINED QTMODULE_RESOURCES)
+    QT4_ADD_RESOURCES(QTMODULE_QRC_SRCS ${QTMODULE_RESOURCES})
+  ENDIF(DEFINED QTMODULE_RESOURCES)
+  #ENDIF(${QTMODULE_NO_RESOURCES} EQUAL FALSE)
 
   SET_SOURCE_FILES_PROPERTIES(
     ${QTMODULE_UI_CXX}
@@ -92,17 +93,17 @@ MACRO(Slicer3_build_qtmodule)
 
   # --------------------------------------------------------------------------
   # Wrapping
-  
+
   #include("${VTK_CMAKE_DIR}/vtkWrapTcl.cmake")
-  #vtk_wrap_tcl3(Volumes 
-  #  Volumes_TCL_SRCS 
+  #vtk_wrap_tcl3(Volumes
+  #  Volumes_TCL_SRCS
   #  "${Volumes_SRCS}" "")
-  
+
   #---------------------------------------------------------------------------
   # Add Loadable Module support
-  
+
   #generatelm(Volumes_SRCS SlicerVolumes.txt)
-  
+
   # --------------------------------------------------------------------------
   # Build and install the library
 
@@ -112,18 +113,18 @@ MACRO(Slicer3_build_qtmodule)
     ${QTMODULE_QRC_SRCS}
     #${qSlicerModule_TCL_SRCS}
     )
-  
+
   # Set qt loadable modules output path
   SET_TARGET_PROPERTIES(${lib_name}
-    PROPERTIES 
-    RUNTIME_OUTPUT_DIRECTORY 
+    PROPERTIES
+    RUNTIME_OUTPUT_DIRECTORY
     "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_QTLOADABLEMODULES_BIN_DIR}"
-    LIBRARY_OUTPUT_DIRECTORY 
+    LIBRARY_OUTPUT_DIRECTORY
     "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_QTLOADABLEMODULES_LIB_DIR}"
-    ARCHIVE_OUTPUT_DIRECTORY 
+    ARCHIVE_OUTPUT_DIRECTORY
     "${CMAKE_BINARY_DIR}/${Slicer3_INSTALL_QTLOADABLEMODULES_LIB_DIR}"
     )
-  
+
   TARGET_LINK_LIBRARIES(${lib_name}
     ${Slicer3_Libs_LIBRARIES}
     ${Slicer3_Base_LIBRARIES}
@@ -141,10 +142,10 @@ MACRO(Slicer3_build_qtmodule)
       ${Slicer3_LIBRARY_PROPERTIES}
     )
   ENDIF(Slicer3_LIBRARY_PROPERTIES)
-  
+
   # Install qt loadable modules
   INSTALL(TARGETS ${lib_name}
-    RUNTIME DESTINATION ${Slicer3_INSTALL_QTLOADABLEMODULES_BIN_DIR} COMPONENT RuntimeLibraries 
+    RUNTIME DESTINATION ${Slicer3_INSTALL_QTLOADABLEMODULES_BIN_DIR} COMPONENT RuntimeLibraries
     LIBRARY DESTINATION ${Slicer3_INSTALL_QTLOADABLEMODULES_LIB_DIR} COMPONENT RuntimeLibraries
     ARCHIVE DESTINATION ${Slicer3_INSTALL_QTLOADABLEMODULES_LIB_DIR} COMPONENT Development
     )
