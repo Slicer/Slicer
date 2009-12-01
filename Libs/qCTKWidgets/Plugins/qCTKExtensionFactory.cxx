35944194749cda3e5c3780e79b0892066e376ea8
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2009-12-01 21:29:03 +0000

    ENH: SlicerQT - Create a directory named Attic and add unused/obsolete piece of code that we could inspire from later on.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11098
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11098 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/qCTKWidgets/Plugins/qCTKExtensionFactory.cxx
+++ Libs/qCTKWidgets/Plugins/qCTKExtensionFactory.cxx
@@ -1,7 +1,7 @@
 #include "qCTKExtensionFactory.h"
 
-#include "qCTKCollapsibleWidgetContainerExtension.h"
-#include "qCTKCollapsibleWidget.h"
+// #include "qCTKCollapsibleWidgetContainerExtension.h"
+// #include "qCTKCollapsibleWidget.h"
 
 qCTKExtensionFactory::qCTKExtensionFactory(QExtensionManager *parent)
     : QExtensionFactory(parent)
@@ -13,11 +13,12 @@ QObject *qCTKExtensionFactory::createExtension(QObject *object,
                                                const QString &iid,
                                                QObject *parent) const
 {
-    qCTKCollapsibleWidget *widget = qobject_cast<qCTKCollapsibleWidget*>(object);
-
-    if (widget && (iid == Q_TYPEID(QDesignerContainerExtension))) {
-        return new qCTKCollapsibleWidgetContainerExtension(widget, parent);
-    } else {
-        return 0;
-    }
+  // qCTKCollapsibleWidget *widget = qobject_cast<qCTKCollapsibleWidget*>(object);
+  // 
+  // if (widget && (iid == Q_TYPEID(QDesignerContainerExtension))) {
+  //     return new qCTKCollapsibleWidgetContainerExtension(widget, parent);
+  // } else {
+  //     return 0;
+  // }
+  return 0; 
 }
