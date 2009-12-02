1f8e967cc4acb4a3e80494704fd1a1b27b26bcf3
Fedorov <fedorov@bwh.harvard.edu>
2009-12-02 17:57:40 +0000

    ENH: switched ImageLabelCombine to use ITK to fix bug 711. The module operates on input voxels.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11117
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11117 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/CLI/ImageLabelCombine.cxx
+++ Applications/CLI/ImageLabelCombine.cxx
@@ -26,16 +26,14 @@
 #include "itkPluginFilterWatcher.h"
 #include "itkPluginUtilities.h"
 
-#include "vtkSmartPointer.h"
-#include "vtkTimerLog.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageDuplicator.h"
+#include "itkOrientedImage.h"
 
-#include "vtkITKArchetypeImageSeriesReader.h"
-#include "vtkITKArchetypeImageSeriesScalarReader.h"
-#include "vtkITKImageWriter.h"
-#include "vtkImageData.h"
 #include "ImageLabelCombineCLP.h"
 
-#include "vtkImageLabelCombine.h"
 
 // Use an anonymous namespace to keep class types and function names
 // from colliding when module is used as shared object module.  Every
@@ -51,45 +49,62 @@ int main( int argc, char * argv[] ){
 
   PARSE_ARGS;
 
-  // Read in label volume inputs
-  vtkITKArchetypeImageSeriesReader* readerA = vtkITKArchetypeImageSeriesScalarReader::New();
-  readerA->SetArchetype(InputLabelMap_A.c_str());
-  readerA->SetOutputScalarTypeToNative();
-  readerA->SetDesiredCoordinateOrientationToNative();
-  readerA->SetUseNativeOriginOn();
-  readerA->Update();
-   
-  vtkITKArchetypeImageSeriesReader* readerB = vtkITKArchetypeImageSeriesScalarReader::New();
-  readerB->SetArchetype(InputLabelMap_B.c_str());
-  readerB->SetOutputScalarTypeToNative();
-  readerB->SetDesiredCoordinateOrientationToNative();
-  readerB->SetUseNativeOriginOn();
-  readerB->Update();
+  typedef short PixelType;
+  typedef itk::OrientedImage<PixelType,3> ImageType;
+  typedef itk::ImageFileReader<ImageType> ReaderType;
+  typedef itk::ImageFileWriter<ImageType> WriterType;
+  typedef itk::ImageRegionIterator<ImageType> IteratorType;
+  typedef itk::ImageDuplicator<ImageType> DuplicatorType;
 
-  
-  // combine labels
-  vtkImageLabelCombine *labelCombine = vtkImageLabelCombine::New();
-  labelCombine->SetOverwriteInput(!FirstOverwrites);
-  labelCombine->SetInput1( readerA->GetOutput() );
-  labelCombine->SetInput2( readerB->GetOutput() );
-  labelCombine->Update();
+  ReaderType::Pointer readerA = ReaderType::New();
+  ReaderType::Pointer readerB = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+  DuplicatorType::Pointer duplicator = DuplicatorType::New();
+  ImageType::Pointer input, output;
 
-  // Output
-  vtkITKImageWriter *writer = vtkITKImageWriter::New();
+  readerA->SetFileName(InputLabelMap_A.c_str());
+  readerB->SetFileName(InputLabelMap_B.c_str());
   writer->SetFileName(OutputLabelMap.c_str());
-  writer->SetInput( labelCombine->GetOutput() );
-  
-  if (readerA->GetRasToIjkMatrix())
-    {
-    writer->SetRasToIJKMatrix( readerA->GetRasToIjkMatrix() );
-    }
-  writer->Write();
 
-  //Delete everything
-  readerA->Delete();
-  readerB->Delete();
-  labelCombine->Delete();
-  writer->Delete();
+  try{
+    readerA->Update();
+    readerB->Update();
+  } catch (itk::ExceptionObject &e){
+    std::cerr << "Failed to read input images. Exception: " << e << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if(FirstOverwrites){
+    input = readerA->GetOutput();
+    duplicator->SetInputImage(readerB->GetOutput());
+    duplicator->Update();
+    output = duplicator->GetOutput();
+  } else {
+    input = readerB->GetOutput();
+    duplicator->SetInputImage(readerA->GetOutput());
+    duplicator->Update();
+    output = duplicator->GetOutput();
+  }
+
+  // This module operates on the image pixels. The images are expected to be
+  // coinciding in the voxel space.
+  IteratorType itIn(input, input->GetLargestPossibleRegion());
+  IteratorType itOut(output, output->GetLargestPossibleRegion());
+  itIn.GoToBegin();
+  itOut.GoToBegin();
+  for(;!itIn.IsAtEnd();++itIn,++itOut){
+    if(!itIn.Get())
+      continue;
+    itOut.Set(itIn.Get());
+  }
+
+  writer->SetInput(output);
+  try{
+    writer->Update();
+  } catch (itk::ExceptionObject &e){
+    std::cerr << "Failed to write output. Exception: " << e << std::endl;
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
