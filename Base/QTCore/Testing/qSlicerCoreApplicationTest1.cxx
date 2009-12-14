bf3e287b57184cdb6964e3dc199971060c5af486
Luis Ibanez <luis.ibanez@kitware.com>
2009-12-14 23:47:49 +0000

    ENH: Raising code coverage for qSlicerCoreApplication by exercising its API in the test.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11281
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11281 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTCore/Testing/qSlicerCoreApplicationTest1.cxx
+++ Base/QTCore/Testing/qSlicerCoreApplicationTest1.cxx
@@ -10,12 +10,96 @@
 =========================================================================auto=*/
 
 #include "qSlicerCoreApplication.h"
+#include "vtkMRMLScene.h"
+#include "vtkSlicerApplicationLogic.h"
+#include "qSlicerModuleManager.h" 
+#include "qSlicerCoreIOManager.h" 
 
 #include <stdlib.h>
 
 int qSlicerCoreApplicationTest1(int argc, char * argv [] )
 {
-  qSlicerCoreApplication( argc, argv );
+  int argc2 = argc - 1;
+  char ** argv2 = argv + 1;
+
+  qSlicerCoreApplication app( argc2, argv2 );
+
+  qSlicerCoreApplication * aptr = app.application();
+
+  if( aptr != (&app) )
+    {
+    std::cerr << "Problem with the application() singleton" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  app.initialize();
+
+  qSlicerModuleManager * moduleManager = new qSlicerModuleManager;
+
+  app.setModuleManager( moduleManager );
+
+  qSlicerModuleManager * moduleManager2 = app.moduleManager();
+
+  if( moduleManager2 != moduleManager )
+    {
+    std::cerr << "Problem with setModuleManager()/moduleManager()" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  qSlicerCoreIOManager * coreIOManager = new qSlicerCoreIOManager;
+
+  app.setCoreIOManager( coreIOManager );
+
+  qSlicerCoreIOManager * coreIOManager2 = app.coreIOManager();
+
+  if( coreIOManager2 != coreIOManager )
+    {
+    std::cerr << "Problem with setIOManager()/ioManager()" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  vtkMRMLScene * scene = vtkMRMLScene::New();
+
+  app.setMRMLScene( scene );
+
+  vtkMRMLScene * scene1 = app.mrmlScene();
+
+  if( scene1 != scene )
+    {
+    std::cerr << "Error in setMRMLScene()/mrmlScene() " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  vtkSlicerApplicationLogic * logic = vtkSlicerApplicationLogic::New();
+
+  app.setAppLogic( logic );
+
+  vtkSlicerApplicationLogic * logic1 = app.appLogic();
+
+  if( logic1 != logic )
+    {
+    std::cerr << "Error in setAppLogic()/appLogic() " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  QString homeDirectory = app.slicerHome();
+
+  std::cout << "Slicer Home Directory = " << qPrintable( homeDirectory ) << std::endl;
+
+  QString newHome = homeDirectory;
+
+  app.setSlicerHome( newHome );
+
+  QString newHome1 = app.slicerHome();
+
+  if( newHome1 != newHome )
+    {
+    std::cerr << "Error in setSlicerHome()/slicerHome() " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  app.setSlicerHome( homeDirectory );
 
   return EXIT_SUCCESS;
 }
