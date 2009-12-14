9961b5c471b1428b3ed0c896cb7819f677f4d5e7
Luis Ibanez <luis.ibanez@kitware.com>
2009-12-14 22:35:40 +0000

    STYLE: Adding copyright headers and license notice.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11277
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11277 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTCoreModules/qSlicerTransformsModuleWidget.h
+++ Base/QTCoreModules/qSlicerTransformsModuleWidget.h
@@ -1,3 +1,16 @@
+/*=auto=========================================================================
+
+ Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) 
+ All Rights Reserved.
+
+ See Doc/copyright/copyright.txt
+ or http://www.slicer.org/copyright/copyright.txt for details.
+
+ Program:   3D Slicer
+
+=========================================================================auto=*/
+
+
 #ifndef __qSlicerTransformsModuleWidget_h
 #define __qSlicerTransformsModuleWidget_h
 
