663ee8a0a050b8feeac49bf660599fdc78b54deb
Luis Ibanez <luis.ibanez@kitware.com>
2009-12-14 22:02:34 +0000

    STYLE: Added copyright header and license note.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11276
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11276 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTBase/qSlicerAbstractCoreModule.h
+++ Base/QTBase/qSlicerAbstractCoreModule.h
@@ -1,3 +1,16 @@
+/*=auto=========================================================================
+
+ Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) 
+ All Rights Reserved.
+
+ See Doc/copyright/copyright.txt
+ or http://www.slicer.org/copyright/copyright.txt for details.
+
+ Program:   3D Slicer
+
+=========================================================================auto=*/
+
+
 #ifndef __qSlicerAbstractCoreModule_h
 #define __qSlicerAbstractCoreModule_h
 
--- Base/QTBase/qSlicerIOManager.cxx
+++ Base/QTBase/qSlicerIOManager.cxx
@@ -1,3 +1,16 @@
+/*=auto=========================================================================
+
+ Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) 
+ All Rights Reserved.
+
+ See Doc/copyright/copyright.txt
+ or http://www.slicer.org/copyright/copyright.txt for details.
+
+ Program:   3D Slicer
+
+=========================================================================auto=*/
+
+
 #include "qSlicerIOManager.h" 
 
 
