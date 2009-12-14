5ca500c8f40a5bd41e87c537ce04d718fbb4744f
Luis Ibanez <luis.ibanez@kitware.com>
2009-12-14 19:15:19 +0000

    STYLE: Added missing copyright headers.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11275
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11275 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTCore/qSlicerCoreIOManager.h
+++ Base/QTCore/qSlicerCoreIOManager.h
@@ -1,3 +1,15 @@
+/*=auto=========================================================================
+
+ Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) 
+ All Rights Reserved.
+
+ See Doc/copyright/copyright.txt
+ or http://www.slicer.org/copyright/copyright.txt for details.
+
+ Program:   3D Slicer
+
+=========================================================================auto=*/
+
 #ifndef __qSlicerCoreIOManager_h
 #define __qSlicerCoreIOManager_h
 
