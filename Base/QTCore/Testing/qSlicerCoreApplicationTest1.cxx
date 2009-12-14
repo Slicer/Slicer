d3d7ea703f292c12f4941da4fa54fe489eb01e16
Luis Ibanez <luis.ibanez@kitware.com>
2009-12-14 19:13:42 +0000

    ENH: Putting in place Testing infrastructure for QTCore library.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11273
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11273 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/QTCore/Testing/qSlicerCoreApplicationTest1.cxx
@@ -0,0 +1,22 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+
+=========================================================================auto=*/
+
+#include "qSlicerCoreApplication.h"
+
+#include <stdlib.h>
+
+int qSlicerCoreApplicationTest1(int argc, char * argv [] )
+{
+  qSlicerCoreApplication( argc, argv );
+
+  return EXIT_SUCCESS;
+}
+
