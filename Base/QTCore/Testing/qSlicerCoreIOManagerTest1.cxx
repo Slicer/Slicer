3328fe9796f501188ce988900196be2f789424a4
Luis Ibanez <luis.ibanez@kitware.com>
2009-12-15 19:22:49 +0000

    ENH: Increasing code coverage of qSlicerCoreIOManager.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11302
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11302 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTCore/Testing/qSlicerCoreIOManagerTest1.cxx
+++ Base/QTCore/Testing/qSlicerCoreIOManagerTest1.cxx
@@ -10,13 +10,33 @@
 =========================================================================auto=*/
 
 #include "qSlicerCoreIOManager.h"
+#include "vtkMRMLScene.h"
 
 #include <stdlib.h>
+#include <iostream>
+#include <QString>
 
 int qSlicerCoreIOManagerTest1(int argc, char * argv [] )
 {
+  if( argc < 2 )
+    {
+    std::cerr << "Missing arguments" << std::endl;
+    return EXIT_FAILURE;
+    }
+
   qSlicerCoreIOManager manager;
+
+  manager.printAdditionalInfo();
+
+  vtkMRMLScene * scene1 = vtkMRMLScene::New();
+
+  QString filename = argv[1];
+
+  manager.loadScene( scene1, filename );
+
+  vtkMRMLScene * scene2 = vtkMRMLScene::New();
+
+  manager.loadScene( scene2, filename );
 
   return EXIT_SUCCESS;
 }
-
