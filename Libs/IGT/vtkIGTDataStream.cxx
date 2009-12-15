3e2e6304d271fc6484538c1535e4f8fd779c7968
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2009-12-15 09:02:15 +0000

    COMP: Updated Libs/IGT to include/build TCL/KWWidgets code only if required
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11294
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11294 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/IGT/vtkIGTDataStream.cxx
+++ Libs/IGT/vtkIGTDataStream.cxx
@@ -1,8 +1,10 @@
 
 #include "vtkIGTDataStream.h"
 #include "vtkObjectFactory.h"
+#if defined(VTK_WRAP_TCL) && defined(KWWidgets_FOUND)
 #include "vtkKWTkUtilities.h"
 #include "vtkKWApplication.h"
+#endif //defined(VTK_WRAP_TCL) && defined(KWWidgets_FOUND)
 #include "vtkCommand.h"
 #include "vtkCallbackCommand.h"
 
@@ -221,9 +223,10 @@ void vtkIGTDataStream::ProcessTimerEvents()
     {
         this->PullRealTime();
         this->InvokeEvent (vtkCommand::ModifiedEvent);
+        #if defined(VTK_WRAP_TCL) && defined(KWWidgets_FOUND)
         vtkKWTkUtilities::CreateTimerHandler(vtkKWApplication::GetMainInterp(), 
-                this->Speed, this, "ProcessTimerEvents");        
- 
+                this->Speed, this, "ProcessTimerEvents");
+        #endif //defined(VTK_WRAP_TCL) && defined(KWWidgets_FOUND)
     }
     else
     {
