c053dd732450daac33bf25fd70ce73ffed4cd330
Luis Ibanez <luis.ibanez@kitware.com>
2009-12-15 19:56:34 +0000

    ENH: Raising code coverage by exercising methods.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11305
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11305 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTCore/Testing/qSlicerModuleManagerTest1.cxx
+++ Base/QTCore/Testing/qSlicerModuleManagerTest1.cxx
@@ -1,6 +1,7 @@
 /*=auto=========================================================================
 
-  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH)
+  All Rights Reserved.
 
   See Doc/copyright/copyright.txt
   or http://www.slicer.org/copyright/copyright.txt for details.
@@ -10,13 +11,98 @@
 =========================================================================auto=*/
 
 #include "qSlicerModuleManager.h"
+#include "qSlicerModuleFactory.h"
 
 #include <stdlib.h>
+#include <iostream>
 
 int qSlicerModuleManagerTest1(int argc, char * argv [] )
 {
   qSlicerModuleManager moduleManager;
 
-  return EXIT_SUCCESS;
+  qSlicerModuleManager * modulePtr = moduleManager.instance();
+
+  if( modulePtr != & moduleManager )
+    {
+    std::cerr << "Error in instance() method" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  moduleManager.printAdditionalInfo();
+
+  qSlicerModuleFactory * factory = moduleManager.factory();
+
+  if( factory == NULL )
+    {
+    std::cerr << "Error in factory()" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  QString moduleName;
+
+  bool result0 = moduleManager.isLoaded( moduleName );
+
+  if( result0 != false )
+    {
+    std::cerr << "Error in isLoaded() " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  bool result1 = moduleManager.loadModule( moduleName );
+
+  if( result1 == false )
+    {
+    std::cerr << "Error in loadModule() " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  bool result2 = moduleManager.isLoaded( moduleName );
+
+  if( result2 != true )
+    {
+    std::cerr << "Error in isLoaded() or loadModule() " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  qSlicerAbstractModule * module = moduleManager.getModule( moduleName );
+  
+  if( module == NULL )
+    {
+    std::cerr << "Error in getModule() " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  QString moduleTitle = moduleManager.moduleTitle( moduleName );
+
+  QString moduleName1 = moduleManager.moduleName( moduleTitle );
+
+  if( moduleName != moduleName1 )
+    {
+    std::cerr << "Error in moduleName recovery" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "Module Name = " << qPrintable( moduleName ) << std::endl;
+
+  std::cout << "Module Title = " << qPrintable( moduleTitle ) << std::endl;
+
+
+  bool result3 = moduleManager.unLoadModule( moduleName );
+
+  if( result3 == false )
+    {
+    std::cerr << "Error in unLoadModule() " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  bool result4 = moduleManager.isLoaded( moduleName );
+
+  if( result4 != false )
+    {
+    std::cerr << "Error in isLoaded() or loadModule() " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+   return EXIT_SUCCESS;
 }
 
