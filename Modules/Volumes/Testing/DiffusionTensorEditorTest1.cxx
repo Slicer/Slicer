a816bf900868b171790922402b9fdd3d5263482e
Luis Ibanez <luis.ibanez@kitware.com>
2009-12-27 00:50:48 +0000

    ENH: Reorganizing the Testing directory and adding C++ test for DiffusionTensorEditor.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11403
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11403 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/Volumes/Testing/DiffusionTensorEditorTest1.cxx
@@ -0,0 +1,51 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) 
+  All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+
+=========================================================================auto=*/
+
+#include "vtkMRMLScene.h"
+
+#include <stdlib.h>
+#include <iostream>
+
+#include "TestingMacros.h"
+
+int DiffusionTensorEditorTest1(int argc, char * argv [] )
+{
+  if( argc < 2 )
+    {
+    std::cerr << "Error: missing arguments" << std::endl;
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << "  inputURL_scene.mrml " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  vtkSmartPointer< vtkMRMLScene > scene = vtkSmartPointer< vtkMRMLScene >::New();
+
+  EXERCISE_BASIC_OBJECT_METHODS( scene );
+
+  scene->SetURL( argv[1] );
+  scene->Connect();
+
+  vtkCollection * collection = scene->GetCurrentScene();
+
+  std::cout << "Collection GetNumberOfItems() = ";
+  std::cout << collection->GetNumberOfItems() << std::endl;
+
+  std::cout << "List of Node Names in this Scene" << std::endl;
+  vtkMRMLNode * nodePtr = scene->GetNextNode();
+  while( nodePtr != NULL )
+    {
+    std::cout << nodePtr->GetName() << std::endl;
+    nodePtr = scene->GetNextNode();
+    }
+
+  return EXIT_SUCCESS;
+}
