42d0f3929b9fe89cd64d3dfa0e533c21d17d7df1
Nicole Aucoin <nicole@bwh.harvard.edu>
2010-01-05 21:05:20 +0000

    BUG: undoing test, it fails if the modules are loaded
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11529
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11529 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/vtkSlicerToolbarGUI.cxx
+++ Base/GUI/vtkSlicerToolbarGUI.cxx
@@ -1214,15 +1214,8 @@ void vtkSlicerToolbarGUI::BuildGUI ( )
   this->VolumeIconButton->SetOverReliefToNone ( );
   this->VolumeIconButton->SetImageToIcon ( this->SlicerToolbarIcons->GetVolumeIcon ( ));
   this->VolumeIconButton->SetBalloonHelpString ( "Volumes");
-  vtkSlicerModuleGUI *m = app->GetModuleGUIByName("Volumes");
-  if ( m != NULL )
-    {
-     mtb->AddWidget ( this->VolumeIconButton );
-    }
-  else 
-    {
-      vtkDebugMacro("No Volumes module loaded, not adding a button for it on the toolbar.");
-    }
+  mtb->AddWidget ( this->VolumeIconButton );
+
   // models module icon
   this->ModelIconButton->SetParent (mtb->GetFrame ( ) );
   this->ModelIconButton->Create ( );
@@ -1272,15 +1265,8 @@ void vtkSlicerToolbarGUI::BuildGUI ( )
   this->EditorIconButton->SetOverReliefToNone ( );
   this->EditorIconButton->SetImageToIcon ( this->SlicerToolbarIcons->GetEditorIcon ( ) );
   this->EditorIconButton->SetBalloonHelpString ( "Editor");        
-  m = app->GetModuleGUIByName("Editor");
-  if ( m != NULL )
-    {
-    mtb->AddWidget ( this->EditorIconButton );
-    }
-  else 
-    {
-      vtkDebugMacro("No Editor module loaded, not adding a button for it on the toolbar.");
-    }
+  mtb->AddWidget ( this->EditorIconButton );
+
 #if ( (VTK_MAJOR_VERSION >= 6) || ( VTK_MAJOR_VERSION == 5 && VTK_MINOR_VERSION >= 4 ) )
   // measurements module icon
   this->MeasurementsIconButton->SetParent ( mtb->GetFrame ( ) );
@@ -1290,15 +1276,7 @@ void vtkSlicerToolbarGUI::BuildGUI ( )
   this->MeasurementsIconButton->SetOverReliefToNone ( );
   this->MeasurementsIconButton->SetImageToIcon ( this->SlicerToolbarIcons->GetMeasurementsIcon ( ) );
   this->MeasurementsIconButton->SetBalloonHelpString ( "Measurements");        
-  m = app->GetModuleGUIByName("Measurements");
-  if ( m != NULL )
-    {
-    mtb->AddWidget ( this->MeasurementsIconButton );
-    }
-  else 
-    {
-      vtkDebugMacro("No Measurements module loaded, not adding a button for it on the toolbar.");
-    }
+  mtb->AddWidget ( this->MeasurementsIconButton );
 #endif
   
   // color utility icon
