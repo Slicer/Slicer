73a2a735c8c6ad069057152a09b1c26e5c2a5300
Casey Goodlett <casey.goodlett@kitware.com>
2010-01-07 20:53:26 +0000

    COMP: Speed up compliation time of command line modules by linking Executable against shared library instead of recompiling.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11549
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11549 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/CLI/Templates/CommandLineSharedLibraryWrapper.cxx
@@ -0,0 +1,40 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $HeadURL$
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+// This file is intended to be compiled and linked against a shared
+// library CLP to prevent the need to compile twice.
+
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
+#ifdef WIN32
+#define MODULE_IMPORT __declspec(dllimport)
+#else
+#define MODULE_IMPORT
+#endif
+
+extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char* []);
+
+int main(int argc, char** argv)
+{
+  return ModuleEntryPoint(argc, argv);
+}
