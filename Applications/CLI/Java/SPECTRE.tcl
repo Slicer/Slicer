afeb4251f3faf04344da6d9afa56e8d150b756f4
Nicole Aucoin <nicole@bwh.harvard.edu>
2010-01-08 01:12:21 +0000

    ENH: first pass at the wrapper for SPECTRE. Todo: find MIPAV program and plugin dirs, they're hard coded right now. TODO: figure out why not getting the java exit 0 back on successful completion
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11557
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11557 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/CLI/Java/SPECTRE.tcl
@@ -0,0 +1,67 @@
+#!/bin/sh
+# the next line restarts using tclsh \
+#    exec tclsh "$0" "$@"
+
+set CLP "SPECTRE"
+
+# add the mipav classes
+#set pathToMipavPlugins "/cygdrive/c/Program\ Files/mipav"
+set pathToMipavPlugins "c:/Program Files/mipav"
+if {$argv != "--xml"} {
+puts "path to mip av plugins $pathToMipavPlugins"
+}
+
+proc glob-r {{dir .} args} {
+     set res {}
+     foreach i [lsort [glob -nocomplain -dir $dir *]] {
+       if {[file isdirectory $i]} {
+         eval [list lappend res] [eval [linsert $args 0 glob-r $i]]
+       } else {
+         if {[llength $args]} {
+           foreach arg $args {
+             if {[string match $arg $i]} {
+               lappend res $i
+               break
+}
+}
+} else {
+lappend res $i
+}
+       }
+     }
+     return $res
+ } ;# JH
+
+set jarList [glob-r $pathToMipavPlugins *.jar]
+# add the top level mipav dir
+lappend jarList {c:/Program Files/mipav}
+# now add the path to the spectre edu classes
+lappend jarList {c:/Documents and Settings/nicole/mipav/plugins}
+if {$argv != "--xml"} {
+  puts "Jar list = $jarList"
+}
+foreach j $jarList {
+  lappend cpath $j
+}
+set joinChar ";"
+set cpath [join $cpath $joinChar]
+
+if {$argv != "--xml"} {
+ puts "cpath = $cpath"
+ puts "argv = \"$argv\""
+}
+
+if {[info exists ::env(CLASSPATH)] != 0} {
+   if {$argv != "--xml"} {
+      puts "Current classpath = $::env(CLASSPATH)"
+   }
+}
+
+set ret [catch "exec java -Xmx700m -classpath \"$cpath\"  edu.jhu.ece.iacl.jist.cli.run edu.jhu.ece.iacl.plugins.segmentation.skull_strip.MedicAlgorithmSPECTRE2009 $argv" res]
+puts $res
+# exit $ret
+if {$argv != "--xml"} {
+puts "Return value = $ret"
+}
+exit 0
+#exit $ret
