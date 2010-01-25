15b5e95cf27e6165e1f129a0c4dec2fe7dfdf73f
Nicole Aucoin <nicole@bwh.harvard.edu>
2010-01-25 21:04:12 +0000

    ENH: increasing coverage
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11776
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11776 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/Qdec/Testing/QdecSubjectTest1.cxx
+++ Libs/Qdec/Testing/QdecSubjectTest1.cxx
@@ -24,9 +24,44 @@ int QdecSubjectTest1(int , char * [] )
   std::string isId = "thisIsAnId";
 
   std::vector < QdecFactor* > iFactors;
+  QdecFactor* qf =
+    new QdecFactor( "Age",
+                    QdecFactor::qdecContinuousFactorType,
+                    33.0);
+  iFactors.push_back( qf );
+  QdecFactor* qfGender =
+    new QdecFactor( "Gender",
+                    QdecFactor::qdecDiscreteFactorType,
+                    "Female");
+  iFactors.push_back( qfGender );
 
   QdecSubject  qdec( isId, iFactors );
 
 
+  std::string id = qdec.GetId();
+
+  if (id.compare(isId) != 0)
+    {
+    std::cerr << "Got incorrect id, " << id.c_str() << " != " << isId.c_str() << "\n";
+    return EXIT_FAILURE;
+    }
+
+  std::string gender = qdec.GetDiscreteFactor("Gender");
+  if (gender.compare("Female") != 0)
+    {
+    std::cerr << "Got incorrect gender, " << gender.c_str() << " != Female\n";
+    return EXIT_FAILURE;
+    }
+
+  double age = qdec.GetContinuousFactor("Age");
+  if (age != 33.0)
+    {
+    std::cerr << "Got incorrect age, " << age << " != 33\n";
+    return EXIT_FAILURE;
+    }
+
+  std::vector < QdecFactor* > contFactors = qdec.GetContinuousFactors();
+  std::vector < QdecFactor* > factors = qdec.GetFactors();
+  
   return EXIT_SUCCESS;
 }
