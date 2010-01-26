23e45768449eecf8db616c998b7ea574ac897b07
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-01-26 07:21:31 +0000

    BUG: SlicerQT - Add missing forward declaration to qMRMLItemHelper and delete obsolete test qMRMLTransformProxyModel_pTest.cxx
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11793
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11793 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/qMRMLWidgets/qMRMLItemHelper.h
+++ Libs/qMRMLWidgets/qMRMLItemHelper.h
@@ -14,6 +14,7 @@ class vtkObject;
 class vtkMRMLScene;
 class vtkMRMLNode;
 class qMRMLAbstractItemHelperPrivate;
+class qMRMLAbstractRootItemHelperPrivate;
 class qMRMLAbstractSceneItemHelperPrivate;
 class qMRMLAbstractNodeItemHelperPrivate;
 
