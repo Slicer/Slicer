08ebaabfe939b211f08a68e16eff21f0f8f1e264
Julien Finet <julien.finet@kitware.com>
2010-01-29 00:08:14 +0000

    ENH: Add a test for vtkSlicerTransformLogic.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11856
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11856 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/Logic/Testing/vtkSlicerTransformLogicTest1.cxx
@@ -0,0 +1,65 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+
+=========================================================================auto=*/
+
+// Logic includes
+#include "vtkSlicerTransformLogic.h"
+
+// MRML includes
+#include <vtkMRMLTransformNode.h>
+#include <vtkMRMLStorageNode.h>
+
+// VTK includes
+#include <vtkSmartPointer.h>
+
+// std includes
+#include <stdlib.h>
+
+#include "TestingMacros.h"
+
+int vtkSlicerTransformLogicTest1(int argc, char * argv [] )
+{
+  if( argc < 2 )
+    {
+    std::cerr << "Missing arguments" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  int argc2 = argc - 1;
+  char ** argv2 = argv + 1;
+  vtkMRMLScene* scene = vtkMRMLScene::New();
+  
+  vtkSlicerTransformLogic* transformModuleLogic = vtkSlicerTransformLogic::New();
+  transformModuleLogic->SetMRMLScene(scene);
+  if (transformModuleLogic->GetMRMLScene() != scene)
+    {
+    std::cerr << "A MRML Scene must be set to go further." << std::endl;
+    return EXIT_FAILURE;
+    }
+  
+  if (argc2 < 1) 
+    {
+    std::cerr << "Missing transform file name." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  vtkMRMLTransformNode* transform = transformModuleLogic->AddTransform(argv2[0], scene);
+  if (transform == 0)
+    {
+    std::cerr << "Could not read transform file: " << argv2[0] << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  transformModuleLogic->Delete();
+  scene->Delete();
+
+  return EXIT_SUCCESS;
+}
+
