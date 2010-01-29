6e01dc49e1290389dcc4012d72c45c2ca30a2272
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-01-29 16:05:56 +0000

    COMP: Review Slicer3FindQT.cmake to accept Qt version 4.6 and above
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11862
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11862 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/Slicer3FindQT.cmake
+++ CMake/Slicer3FindQT.cmake
@@ -8,19 +8,31 @@ OPTION(Slicer3_USE_QT  "Use Qt as an extra GUI library" OFF)
 MACRO(Slicer3_SETUP_QT)
 
 IF(Slicer3_USE_QT)
-  ## QT
+  SET(minimum_required_qt_version "4.6")
   IF(VTK_USE_QVTK)
-    FIND_PACKAGE(Qt4 4.6 EXACT)
+
+    # Check if QT_QMAKE_EXECUTABLE is set
+    IF(NOT QT_QMAKE_EXECUTABLE)
+      MESSAGE(FATAL_ERROR "error: Set QT_QMAKE_EXECUTABLE: version of Qt >= ${minimum_required_qt_version} *required* to build Slicer")
+    ENDIF()
+    
+    FIND_PACKAGE(Qt4)
+    
     IF(QT4_FOUND)
+      # Check version, note that ${QT_VERSION_PATCH} could also be used
+      IF(NOT "${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}" STREQUAL "${minimum_required_qt_version}")
+        MESSAGE(FATAL_ERROR "error: Slicer requires Qt >= ${minimum_required_qt_version} -- you cannot use Qt ${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}.${QT_VERSION_PATCH} ...")
+      ENDIF()
+      # Enable modules
       SET(QT_USE_QTNETWORK ON)
+      # Includes Qt headers
       INCLUDE(${QT_USE_FILE})
-      SET(Slicer3_USE_QT ON)
     ELSE(QT4_FOUND)
-      MESSAGE("Qt4 was not found on your system. You probably need to set the QT_QMAKE_EXECUTABLE variable")
+      MESSAGE(FATAL_ERROR "error: Qt >= ${minimum_required_qt_version} was not found on your system. You probably need to set the QT_QMAKE_EXECUTABLE variable.")
     ENDIF(QT4_FOUND)
+    
   ELSE(VTK_USE_QVTK)
-     MESSAGE("VTK was not configured to use QT, no QT-based code will be compiled in Slicer")
-     SET(Slicer3_USE_QT OFF)
+     MESSAGE("error: VTK was not configured to use QT, you probably need to recompile it with VTK_USE_GUISUPPORT ON, VTK_USE_QVTK ON, DESIRED_QT_VERSION 4 and QT_QMAKE_EXECUTABLE set appropriatly. Note that Qt >= ${minimum_required_qt_version} is *required*")
   ENDIF(VTK_USE_QVTK)
 ENDIF(Slicer3_USE_QT)
 
