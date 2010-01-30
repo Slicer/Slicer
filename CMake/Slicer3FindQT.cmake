a63f93423328bb4d768c1df42a552371b6920bc9
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-01-30 20:08:05 +0000

    COMP: Tune  Slicer3FindQT.cmake script. If VTK is configured properly, it's not required anymore to enter QT_QMAKE_EXECUTABLE.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11893
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11893 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/Slicer3FindQT.cmake
+++ CMake/Slicer3FindQT.cmake
@@ -13,23 +13,27 @@ IF(Slicer3_USE_QT)
 
     # Check if QT_QMAKE_EXECUTABLE is set
     IF(NOT VTK_QT_QMAKE_EXECUTABLE)
-      MESSAGE(FATAL_ERROR "error: Set QT_QMAKE_EXECUTABLE: version of Qt >= ${minimum_required_qt_version} *required* to build Slicer")
+      MESSAGE(FATAL_ERROR "error: There is a problem with your configuration, the variable VTK_QT_QMAKE_EXECUTABLE should be exposed by VTK.")
     ENDIF()
+
+    SET(QT_QMAKE_EXECUTABLE ${VTK_QT_QMAKE_EXECUTABLE})
     
     FIND_PACKAGE(Qt4)
+
+    MESSAGE(STATUS Slicer using Qt version: ${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}.${QT_VERSION_PATCH})
     
-    IF(QT4_FOUND)
+    IF(QT4_FOUND AND QT_QMAKE_EXECUTABLE)
       # Check version, note that ${QT_VERSION_PATCH} could also be used
       IF(NOT "${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}" STREQUAL "${minimum_required_qt_version}")
-        MESSAGE(FATAL_ERROR "error: Slicer requires Qt >= ${minimum_required_qt_version} -- you cannot use Qt ${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}.${QT_VERSION_PATCH} ...")
+        MESSAGE(FATAL_ERROR "error: Slicer requires Qt >= ${minimum_required_qt_version} -- you cannot use Qt ${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}.${QT_VERSION_PATCH}. You should probably reconfigure VTK.")
       ENDIF()
       # Enable modules
       SET(QT_USE_QTNETWORK ON)
       # Includes Qt headers
       INCLUDE(${QT_USE_FILE})
-    ELSE(QT4_FOUND)
+    ELSE(QT4_FOUND AND QT_QMAKE_EXECUTABLE)
       MESSAGE(FATAL_ERROR "error: Qt >= ${minimum_required_qt_version} was not found on your system. You probably need to set the QT_QMAKE_EXECUTABLE variable.")
-    ENDIF(QT4_FOUND)
+    ENDIF(QT4_FOUND AND QT_QMAKE_EXECUTABLE)
     
   ELSE(VTK_USE_QVTK)
      MESSAGE("error: VTK was not configured to use QT, you probably need to recompile it with VTK_USE_GUISUPPORT ON, VTK_USE_QVTK ON, DESIRED_QT_VERSION 4 and QT_QMAKE_EXECUTABLE set appropriatly. Note that Qt >= ${minimum_required_qt_version} is *required*")
