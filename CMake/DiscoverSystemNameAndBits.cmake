6eb62bc7b135db42f1111815e2ee5ae75abb81da
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-01-31 18:34:55 +0000

    COMP: Added cmake script allowing to obtain the current name of the system.
    After the script is included, the variable Slicer3_BUILD containing
    will be available and should contain on of these value:
     solaris8,
     linux-x86
     linux-x86_64
     darwin-ppc
     darwin-x86
     darwin-x86_64
     win32
     win64
    
    and  Slicer3_BUILD_BITS either "32" or "64"
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11899
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11899 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ CMake/DiscoverSystemNameAndBits.cmake
@@ -0,0 +1,42 @@
+#
+# DiscoverSystemNameAndBits - Set the variable Slicer3_BUILD and Slicer3_BUILD_BITS
+#
+
+# Slicer3_BUILD can take on of the following value:
+#   solaris8, linux-x86, linux-x86_64, darwin-ppc, darwin-x86, darwin-x86_64, win32, win64
+
+
+SET(Slicer3_BUILD "")
+
+SET(Slicer3_BUILD_BITS "32")
+IF(CMAKE_SIZEOF_VOID_P EQUAL 8)
+  SET(Slicer3_BUILD_BITS "64")
+ENDIF()
+
+IF (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+
+  SET(Slicer3_BUILD "win${Slicer_BUILD_BITS}")
+  
+ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+
+  SET(Slicer3_BUILD "linux-x86")
+  IF (Slicer3_BUILD_BITS STREQUAL "64")
+    SET(Slicer3_BUILD "linux-x86_64")
+  ENDIF()
+  
+ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+
+  IF(CMAKE_SYSTEM_PROCESSOR MATCHES "Intel")
+    SET(Slicer3_BUILD "darwin-x86")
+    IF (Slicer3_BUILD_BITS STREQUAL "64")
+      SET(Slicer3_BUILD "darwin-x86_64")
+    ENDIF()
+  ELSE(CMAKE_SYSTEM_PROCESSOR MATCHES "Intel")
+    SET(Slicer3_BUILD "darwin-ppc")
+  ENDIF()
+  
+ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Solaris")
+
+  SET(Slicer3_BUILD "solaris8") # What about solaris9 and solaris10 ?
+  
+ENDIF()
