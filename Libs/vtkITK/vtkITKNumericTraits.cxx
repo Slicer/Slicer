f804dbb715f20ecc722705c1c72a037f1dde5caa
Stephen Aylward <Stephen.Aylward@kitware.com>
2010-01-31 02:46:04 +0000

    BUG: Conditional for compiling LONG_LONG type not matched between .h and .cxx file.   Created conflict with cvs ITK.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11895
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11895 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/vtkITK/vtkITKNumericTraits.cxx
+++ Libs/vtkITK/vtkITKNumericTraits.cxx
@@ -11,7 +11,7 @@ const unsigned __int64 NumericTraits<unsigned __int64>::Zero = 0ui64;
 const unsigned __int64 NumericTraits<unsigned __int64>::One = 1ui64;
 #endif
 
-#if defined(VTK_TYPE_USE_LONG_LONG)
+#if defined(VTK_TYPE_USE_LONG_LONG) && !defined(ITK_TYPE_USE_LONG_LONG)
 const long long NumericTraits<long long>::Zero = 0;
 const long long NumericTraits<long long>::One = 1;
 
