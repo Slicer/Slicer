18846cb092e5a5400a796f0c62e5259043999a9e
Nicole Aucoin <nicole@bwh.harvard.edu>
2010-02-01 18:04:55 +0000

    ENH: use the new return string type
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=11911
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@11911 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/CLI/Java/Examples/HelloWorldApp.java
+++ Applications/CLI/Java/Examples/HelloWorldApp.java
@@ -1,11 +1,15 @@
 package Examples;
 /**
  * The HelloWorldApp class implements an application that
- * simply prints "Hello World!" to standard output.
+ * prints "Hello Name!" to standard output.
+ * Also returns a string with the greeting via a file.
  */
+import java.io.*;
+
 class HelloWorldApp {
   public static void main(String[] args) {
     String Name = "World";
+    String ReturnFileName = "";
             
     for (int i = 0; i < args.length; i++)
        {
@@ -36,6 +40,14 @@ class HelloWorldApp {
          System.out.println("\t\t<longflag>--name</longflag>");
          System.out.println("\t\t<description>Enter your name</description>");
          System.out.println("\t</string>");
+         System.out.println("\t<string>");
+         System.out.println("\t\t<name>helloReturn</name>");
+         System.out.println("\t\t<label>Your greeting:</label>");
+         System.out.println("\t\t<channel>output</channel>");
+         System.out.println("\t\t<default>Hello World</default>");
+         System.out.println("\t\t<description>The return greeting from the program, a string</description>");
+         System.out.println("\t</string>");
+
          System.out.println("</parameters>");
          System.out.println("</executable>");
          System.exit(0);
@@ -48,7 +60,31 @@ class HelloWorldApp {
       {
        Name = args[i].substring(7);
       }
+      else if (args[i].endsWith("--returnparameterfile"))
+        {
+        ReturnFileName = args[++i];
+        }
+      else if (args[i].startsWith("--returnparameterfile"))
+        {
+        ReturnFileName = args[i].substring(21);
+        }
     }
     System.out.println("Hello " + Name + "!"); // Display the string.
+    // return the string
+    // System.out.println("Writing to return file '" + ReturnFileName + "'");
+    try
+      {
+      // Create file 
+      FileWriter fstream = new FileWriter(ReturnFileName);
+      BufferedWriter out = new BufferedWriter(fstream);
+      out.write("helloReturn = Hello  " + Name + "!");
+      //Close the output stream
+      out.close();
+      }
+    catch (Exception e)
+      {
+      //Catch exception if any
+      System.err.println("Error: " + e.getMessage());
+      }
    }
 }
