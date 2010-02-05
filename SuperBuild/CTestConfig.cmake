039a66f4631d073ad106a6230f68b887b4b7f884
Dave Partyka <dave.partyka@kitware.com>
2010-02-05 14:05:44 +0000

    ENH: Creating a top level directory for CMake based Super Builds. To do a super build use the Slicer3\SuperBuild directory as your source directory.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12009
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12009 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ SuperBuild/CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "Slicer3")
+set(CTEST_NIGHTLY_START_TIME "23:00:00 EDT")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "www.cdash.org")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Slicer3")
+set(CTEST_DROP_SITE_CDASH TRUE)
