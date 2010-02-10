a8b4e14891ed46de431ec66a810576e68ce4279d
Sylvain Bouix <sylvain@bwh.harvard.edu>
2010-02-10 23:56:06 +0000

    COMP: remove MRIBiasFieldCorrection as it is now a command line module (see Applications/CLI). The duplication of this module caused a crash when closing Slicer because MRIBiasFieldCorrection was deleted twice. Julien suggested that a better fix would be in the Slicer deletion mechanism since deletion should not be based only on the name/title of the module.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12095
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12095 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/MRIBiasFieldCorrection/Testing/MRIBiasFieldCorrectionTest.cxx
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "itkTestMain.h" 
-void RegisterTests()
-{
-    REGISTER_TEST(MRIBiasFieldCorrectionTest);
-}
-#undef main
-#define main MRIBiasFieldCorrectionTest
-#include "MRIBiasFieldCorrectionCommandLine.cxx"
