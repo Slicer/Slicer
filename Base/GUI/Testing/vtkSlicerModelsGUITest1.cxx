49deea5ff35cebd9fb3ab94322075cd58a60d4d8
Julien Finet <julien.finet@kitware.com>
2010-02-15 18:05:53 +0000

    ENH: Add unit test for vtkSlicerModelsGUI
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12136
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12136 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/GUI/Testing/vtkSlicerModelsGUITest1.cxx
@@ -0,0 +1,50 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+
+=========================================================================auto=*/
+
+// GUI includes
+#include "vtkSlicerModelsGUI.h"
+
+// MRML includes
+#include <vtkMRMLScene.h>
+#include <vtkMRMLModelDisplayNode.h>
+#include <vtkMRMLModelHierarchyNode.h>
+
+// STL includes
+#include <stdlib.h>
+
+#include "TestingMacros.h"
+
+int vtkSlicerModelsGUITest1(int argc, char * argv [] )
+{
+  vtkSlicerModelsGUI* modelsGUI = vtkSlicerModelsGUI::New();
+  
+  vtkMRMLScene* mrmlScene = vtkMRMLScene::New();
+  modelsGUI->SetMRMLScene(mrmlScene);
+
+  vtkMRMLModelHierarchyNode *node = vtkMRMLModelHierarchyNode::New();
+  node->SetHideFromEditors(0);
+  node->SetSelectable(1);
+  mrmlScene->AddNode(node);
+  node->Delete();
+
+  vtkMRMLModelDisplayNode *dnode = vtkMRMLModelDisplayNode::New();
+  mrmlScene->AddNode(dnode);
+  dnode->Delete();
+
+  node->SetAndObserveDisplayNodeID(dnode->GetID());
+
+  modelsGUI->Delete();
+
+  mrmlScene->Delete();
+
+  return EXIT_SUCCESS;
+}
+
