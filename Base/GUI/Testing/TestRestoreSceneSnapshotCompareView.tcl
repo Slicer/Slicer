c279612b6c35be01a4fb0470020642e71b4fd09d
Steve Pieper <pieper@bwh.harvard.edu>
2010-02-17 21:28:24 +0000

    BUG: fix typo in scene name and avoid leaks on exit
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12162
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12162 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Testing/TestRestoreSceneSnapshotCompareView.tcl
+++ Base/GUI/Testing/TestRestoreSceneSnapshotCompareView.tcl
@@ -1,17 +1,13 @@
 
 proc TestRestoreSceneSnapshotCompareView {} {
     # load the scene
-    $::slicer3::MRMLScene SetURL $::Slicer3_HOME/share/MRML/Testing/restoreSceneSnapShotCompareView.mrml
+    $::slicer3::MRMLScene SetURL $::Slicer3_HOME/share/MRML/Testing/restoreSceneSnapshotCompareView.mrml
     $::slicer3::MRMLScene Connect
 
     # restore the snapshot
-    set nodes [$::slicer3::MRMLScene GetNodesByName view1]
-    $nodes InitTraversal
-    set nd [$nodes GetNextItemAsObject]
+    set node [$::slicer3::MRMLScene GetNthNodeByClass 0 "vtkMRMLSceneSnapshotNode"]
     $::slicer3::MRMLScene SaveStateForUndo
-    $nd RestoreScene
-
-    exit 0
+    $node RestoreScene
 }
 
 proc runtest {} {
