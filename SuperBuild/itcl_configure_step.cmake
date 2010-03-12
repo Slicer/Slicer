ea6f4a906cc53d33a4c9053b9678db040e7a226d
Dave Partyka <dave.partyka@kitware.com>
2010-03-12 14:54:53 +0000

    ENH: more fixes for building itcl on Mac.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12335
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12335 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/itcl_configure_step.cmake
+++ SuperBuild/itcl_configure_step.cmake
@@ -1,6 +1,10 @@
 
-set(ENV{PATH} "${CMAKE_BINARY_DIR}/python-build/lib")
+#set(ENV{PATH} "${CMAKE_BINARY_DIR}/python-build/lib")
+set(ENV{PATH} "@tcl_build@/bin")
 
-
+execute_process(
+  COMMAND sh configure --with-tcl=@tcl_build@/lib --with-tk=@tcl_build@/lib --prefix=@tcl_build@)
+)
+  
 #message("LD_LIBRARY_PATH:=ENV{LD_LIBRARY_PATH}")
 #set(ENV{PYTHONHOME} ${python_home})
