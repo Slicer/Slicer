80aa4aa0623ee78bbb7a281625db1822d68f69f0
Steve Pieper <pieper@bwh.harvard.edu>
2010-03-19 21:34:53 +0000

    BUG: fix example to correctly pass NULL instead of 0
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12406
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12406 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Testing/NodeSelector.py
+++ Base/GUI/Testing/NodeSelector.py
@@ -1,4 +1,13 @@
 
+comment = '''
+This example can be loaded into a running slicer from the python console using
+a command somthing like:
+
+  execfile("../Slicer3/Base/GUI/Testing/NodeSelector.py")
+
+The resulting node selector can be used to see and manipulate fiducial list nodes.
+'''
+
 
 import Slicer
 
@@ -12,7 +21,7 @@ t.Create()
 selector = Slicer.slicer.vtkSlicerNodeSelectorWidget()
 selector.SetParent( t )
 selector.Create()
-selector.SetNodeClass('vtkMRMLFiducialListNode', 0, 0, 0)
+selector.SetNodeClass('vtkMRMLFiducialListNode', "", "", "")
 selector.SetMRMLScene( Slicer.slicer.MRMLScene )
 selector.UpdateMenu()
 selector.SetLabelText('Fiducials')
