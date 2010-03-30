7c11c0ff8de7ddda6a2e54786f35ca23af75a823
Nicole Aucoin <nicole@bwh.harvard.edu>
2010-03-30 21:50:23 +0000

    ENH: don't turn off modified when reading, new fids are faster
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12552
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12552 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/vtkMRMLFiducialListStorageNode.cxx
+++ Libs/MRML/vtkMRMLFiducialListStorageNode.cxx
@@ -172,8 +172,8 @@ int vtkMRMLFiducialListStorageNode::ReadData(vtkMRMLNode *refNode)
     // clear out the list
     fiducialListNode->RemoveAllFiducials();
     // turn off modified events
-    int modFlag = fiducialListNode->GetDisableModifiedEvent();
-    fiducialListNode->DisableModifiedEventOn();
+//    int modFlag = fiducialListNode->GetDisableModifiedEvent();
+//    fiducialListNode->DisableModifiedEventOn();
     char line[1024];
     // default column ordering for fiducial info
     // first pass: line will have label,x,y,z,selected,visible
@@ -456,8 +456,8 @@ int vtkMRMLFiducialListStorageNode::ReadData(vtkMRMLNode *refNode)
           } // point line
         }
       }
-    fiducialListNode->SetDisableModifiedEvent(modFlag);
-    fiducialListNode->InvokeEvent(vtkMRMLScene::NodeAddedEvent, fiducialListNode);//vtkMRMLFiducialListNode::DisplayModifiedEvent);
+//    fiducialListNode->SetDisableModifiedEvent(modFlag);
+//    fiducialListNode->InvokeEvent(vtkMRMLScene::NodeAddedEvent, fiducialListNode);//vtkMRMLFiducialListNode::DisplayModifiedEvent);
     fstr.close();
     }
   else
