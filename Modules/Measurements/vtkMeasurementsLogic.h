bd556454aabe20425104d975c3c1b38e02f85bef
Nicole Aucoin <nicole@bwh.harvard.edu>
2010-04-02 20:29:02 +0000

    BUG: since 2d interactions are a bit iffy, use the last two fids on the active list to make a new ruler
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12625
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12625 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Measurements/vtkMeasurementsLogic.h
+++ Modules/Measurements/vtkMeasurementsLogic.h
@@ -15,6 +15,15 @@ class VTK_MEASUREMENTS_EXPORT vtkMeasurementsLogic : public vtkSlicerModuleLogic
   vtkTypeMacro(vtkMeasurementsLogic, vtkSlicerModuleLogic);
   void PrintSelf(ostream& os, vtkIndent indent);
 
+  ///
+  /// Make a new ruler node, popping up a rename dialogue first, placing it
+  /// between the last two fiducials added on the currently selected list,
+  /// then remove those two fiducials. If the rulerName is not NULL, use that
+  /// to name the new node and in the list annotation string, otherwise use
+  /// 'Ruler'. Fails if can't get at the mrml scene, the selection node, the
+  /// active fid list, or there aren't two points in the active fid list.
+  void NewRulerBetweenFiducials(const char *rulerName = NULL);
+  
  protected:
   vtkMeasurementsLogic();
   ~vtkMeasurementsLogic();
