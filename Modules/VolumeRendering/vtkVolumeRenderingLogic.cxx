533d5de2491c5305edf2f0a3d51d5ed646ff8eb3
Alex Yarmarkovich <alexy@bwh.harvard.edu>
2010-04-22 22:58:30 +0000

    BUG_FIXED: made volume rendering work with label maps
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12877
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12877 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/VolumeRendering/vtkVolumeRenderingLogic.cxx
+++ Modules/VolumeRendering/vtkVolumeRenderingLogic.cxx
@@ -19,6 +19,7 @@
 #include "vtkMRMLVolumePropertyNode.h"
 #include "vtkMRMLVolumePropertyStorageNode.h"
 #include "vtkMRMLScalarVolumeDisplayNode.h"
+#include "vtkMRMLLabelMapVolumeDisplayNode.h"
 
 #include "vtkSlicerVolumeTextureMapper3D.h"
 #include "vtkSlicerFixedPointVolumeRayCastMapper.h"
@@ -513,89 +514,98 @@ void vtkVolumeRenderingLogic::SetupVolumePropertyFromImageData(vtkMRMLVolumeRend
   {
     this->UpdateVolumePropertyByDisplayNode(vspNode);
   }
-  else
+  else if (vspNode->GetVolumeNode())
   {
-    //add points into transfer functions
-    vtkKWHistogram *histogram = this->Histograms->GetHistogramWithName("0");
-
-    double range[2];
-    histogram->GetRange(range);
-    
     vtkMRMLScalarVolumeDisplayNode *vpNode = vtkMRMLScalarVolumeDisplayNode::SafeDownCast(vspNode->GetVolumeNode()->GetDisplayNode());
+    vtkMRMLLabelMapVolumeDisplayNode *vlNode = vtkMRMLLabelMapVolumeDisplayNode::SafeDownCast(vspNode->GetVolumeNode()->GetDisplayNode());
     
-    double windowLevel[2];
-    windowLevel[0] = vpNode->GetWindow();
-    windowLevel[1] = vpNode->GetLevel();
-
-    vspNode->SetWindowLevel(windowLevel);
-    
-    vtkVolumeProperty *prop = vspNode->GetVolumePropertyNode()->GetVolumeProperty();
-    prop->SetInterpolationTypeToLinear();
-
-    vtkPiecewiseFunction *opacity = prop->GetScalarOpacity();
-    
-    opacity->RemoveAllPoints();
-    opacity->AddPoint(range[0], 0.0);
-    opacity->AddPoint(windowLevel[1] - windowLevel[0]*0.5, 0.0);
-    opacity->AddPoint(windowLevel[1] + windowLevel[0]*0.5, 1.0);
-    opacity->AddPoint(range[1], 1.0);
-      
-    vtkColorTransferFunction *colorTransfer = prop->GetRGBTransferFunction();
-
-    colorTransfer->RemoveAllPoints();
-
-    vtkLookupTable* pLut = vpNode->GetColorNode()->GetLookupTable();
-
-    if (pLut == NULL)
-    {
-      colorTransfer->AddRGBPoint(range[0], 0.0, 0.0, 0.0);
-      colorTransfer->AddRGBPoint(windowLevel[1] - windowLevel[0]*0.5, 0.0, 0.0, 0.0);
-      colorTransfer->AddRGBPoint(windowLevel[1] + windowLevel[0]*0.5, 1.0, 1.0, 1.0);
-      colorTransfer->AddRGBPoint(range[1], 1.0, 1.0, 1.0);
-    }
-    else
-    {
-      int size = pLut->GetNumberOfTableValues();
-
-      double color[4];
-      pLut->GetTableValue(0, color);
-    
-      if (size == 1)
+    if (vpNode)
       {
-        colorTransfer->AddRGBPoint(range[0], color[0], color[1], color[2]);
-        colorTransfer->AddRGBPoint(windowLevel[1] - windowLevel[0]*0.5, color[0], color[1], color[2]);
-        colorTransfer->AddRGBPoint(windowLevel[1] + windowLevel[0]*0.5, color[0], color[1], color[2]);
-        colorTransfer->AddRGBPoint(range[1], color[0], color[1], color[2]);
+      //add points into transfer functions
+      vtkKWHistogram *histogram = this->Histograms->GetHistogramWithName("0");
+
+      double range[2];
+      histogram->GetRange(range);
+          
+      double windowLevel[2];
+      windowLevel[0] = vpNode->GetWindow();
+      windowLevel[1] = vpNode->GetLevel();
+
+      vspNode->SetWindowLevel(windowLevel);
+      
+      vtkVolumeProperty *prop = vspNode->GetVolumePropertyNode()->GetVolumeProperty();
+      prop->SetInterpolationTypeToLinear();
+
+      vtkPiecewiseFunction *opacity = prop->GetScalarOpacity();
+      
+      opacity->RemoveAllPoints();
+      opacity->AddPoint(range[0], 0.0);
+      opacity->AddPoint(windowLevel[1] - windowLevel[0]*0.5, 0.0);
+      opacity->AddPoint(windowLevel[1] + windowLevel[0]*0.5, 1.0);
+      opacity->AddPoint(range[1], 1.0);
+        
+      vtkColorTransferFunction *colorTransfer = prop->GetRGBTransferFunction();
+
+      colorTransfer->RemoveAllPoints();
+
+      vtkLookupTable* pLut = vpNode->GetColorNode()->GetLookupTable();
+
+      if (pLut == NULL)
+      {
+        colorTransfer->AddRGBPoint(range[0], 0.0, 0.0, 0.0);
+        colorTransfer->AddRGBPoint(windowLevel[1] - windowLevel[0]*0.5, 0.0, 0.0, 0.0);
+        colorTransfer->AddRGBPoint(windowLevel[1] + windowLevel[0]*0.5, 1.0, 1.0, 1.0);
+        colorTransfer->AddRGBPoint(range[1], 1.0, 1.0, 1.0);
       }
       else
       {
-        colorTransfer->AddRGBPoint(range[0], color[0], color[1], color[2]);
+        int size = pLut->GetNumberOfTableValues();
+
+        double color[4];
+        pLut->GetTableValue(0, color);
       
-        double value = windowLevel[1] - windowLevel[0]*0.5;
-        double step;
-
-        step = windowLevel[0] / (size - 1);
-
-        int downSamplingFactor = 64;
-        
-        for (int i = 0; i < size; i += downSamplingFactor, value += downSamplingFactor*step)
+        if (size == 1)
         {
-          pLut->GetTableValue(i, color);
-          colorTransfer->AddRGBPoint(value, color[0], color[1], color[2]);
+          colorTransfer->AddRGBPoint(range[0], color[0], color[1], color[2]);
+          colorTransfer->AddRGBPoint(windowLevel[1] - windowLevel[0]*0.5, color[0], color[1], color[2]);
+          colorTransfer->AddRGBPoint(windowLevel[1] + windowLevel[0]*0.5, color[0], color[1], color[2]);
+          colorTransfer->AddRGBPoint(range[1], color[0], color[1], color[2]);
         }
+        else
+        {
+          colorTransfer->AddRGBPoint(range[0], color[0], color[1], color[2]);
+        
+          double value = windowLevel[1] - windowLevel[0]*0.5;
+          double step;
 
-        pLut->GetTableValue(size - 1, color);
-        colorTransfer->AddRGBPoint(windowLevel[1] + windowLevel[0]*0.5, color[0], color[1], color[2]);
-        colorTransfer->AddRGBPoint(range[1], color[0], color[1], color[2]);
+          step = windowLevel[0] / (size - 1);
+
+          int downSamplingFactor = 64;
+          
+          for (int i = 0; i < size; i += downSamplingFactor, value += downSamplingFactor*step)
+          {
+            pLut->GetTableValue(i, color);
+            colorTransfer->AddRGBPoint(value, color[0], color[1], color[2]);
+          }
+
+          pLut->GetTableValue(size - 1, color);
+          colorTransfer->AddRGBPoint(windowLevel[1] + windowLevel[0]*0.5, color[0], color[1], color[2]);
+          colorTransfer->AddRGBPoint(range[1], color[0], color[1], color[2]);
+        }
       }
+      
+      prop->ShadeOn();
+      prop->SetAmbient(0.30);
+      prop->SetDiffuse(0.60);
+      prop->SetSpecular(0.50);
+      prop->SetSpecularPower(40);
     }
-    
-    prop->ShadeOn();
-    prop->SetAmbient(0.30);
-    prop->SetDiffuse(0.60);
-    prop->SetSpecular(0.50);
-    prop->SetSpecularPower(40);
+  else if (vlNode)
+    {
+    //TODO label map
+    } 
   }
+
 }
 
 void vtkVolumeRenderingLogic::SetupFgVolumePropertyFromImageData(vtkMRMLVolumeRenderingParametersNode* vspNode)
