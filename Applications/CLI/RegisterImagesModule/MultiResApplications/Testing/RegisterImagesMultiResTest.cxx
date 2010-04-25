a718451930e139ce369915607ab64304a4f82a3e
Casey Goodlett <casey.goodlett@kitware.com>
2010-04-25 21:22:52 +0000

    ENH: Add test for register images multi-res.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12916
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12916 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/CLI/RegisterImagesModule/MultiResApplications/Testing/RegisterImagesMultiResTest.cxx
@@ -0,0 +1,23 @@
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
+#include <iostream>
+#include "itkTestMain.h" 
+
+#ifdef WIN32
+#define MODULE_IMPORT __declspec(dllimport)
+#else
+#define MODULE_IMPORT
+#endif
+
+extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char* []);
+
+void RegisterTests()
+{
+  StringToTestFunctionMap["ModuleEntryPoint"] = ModuleEntryPoint;
+}
