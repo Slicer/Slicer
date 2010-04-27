a403f87bb19bed451baf811621050a9801cddfb3
Alex Yarmarkovich <alexy@bwh.harvard.edu>
2010-04-27 19:47:04 +0000

    ENH: added reference VolView transfer functions
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12950
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12950 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/VolumeRendering/vtkVolumeRenderingGUI.cxx
+++ Modules/VolumeRendering/vtkVolumeRenderingGUI.cxx
@@ -214,7 +214,7 @@ void vtkVolumeRenderingGUI::BuildGUI(void)
 
   // Define your help text and build the help frame here.
   const char *help = "Volume Rendering allows the rendering of volumes in 3D space and not only as 2D surfaces defined in 3D space. \n<a>http://wiki.slicer.org/slicerWiki/index.php/Modules:VolumeRendering-Documentation-3.5</a>\n Tutorials are available at <a>http://www.na-mic.org/Wiki/index.php/Slicer3:Volume_Rendering_Tutorials</a>";
-  const char *about = "This work is supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community. See <a>http://www.slicer.org</a> for details. The VolumeRendering module was contributed by Yanling Liu, NCI-Frederick/SAIC-Frederick, Andreas Freudling, Student Intern at SPL, BWH (Ron Kikinis) and Alex Yarmarkovich, Isomics Inc. (Steve Pieper)";
+  const char *about = "This work is supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community. See <a>http://www.slicer.org</a> for details. The VolumeRendering module was contributed by Yanling Liu, NCI-Frederick/SAIC-Frederick, Andreas Freudling, Student Intern at SPL, BWH (Ron Kikinis) and Alex Yarmarkovich, Isomics Inc. (Steve Pieper). Some of the transfer functions were contributed by Kitware, Inc. (VolView)";
   vtkKWWidget *page = this->UIPanel->GetPageWidget ( "VolumeRendering" );
   this->BuildHelpAndAboutFrame ( page, help, about );
 
