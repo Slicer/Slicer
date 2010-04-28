06fb09a871f4d16d106ecea7a49040374c2f29af
Haiying Liu <hliu@bwh.harvard.edu>
2010-04-28 20:41:44 +0000

    BUG: asked to search for module "OpenIGTLink IF" not "OpenIGTLinkIF".
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12994
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12994 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/NeuroNav/Testing/TestDependency.tcl
+++ Modules/NeuroNav/Testing/TestDependency.tcl
@@ -1,7 +1,7 @@
 
 proc TestModuleDependency {} {
 
-  set ::gui [$::slicer3::Application GetModuleGUIByName OpenIGTLinkIF] 
+  set ::gui [$::slicer3::Application GetModuleGUIByName "OpenIGTLink IF"] 
   if {$::gui == ""} {
       puts "Cannot find OpenIGTLinkIF module.\n"
       return 1
@@ -14,7 +14,7 @@ set ret [ catch { $::slicer3::Application TraceScript TestModuleDependency } ]
 
 if {$ret == 1} {
     exit 1
-  } else {
+} else {
     exit 0
-  }
 }
+
