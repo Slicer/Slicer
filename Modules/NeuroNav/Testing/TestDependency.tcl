6b7867b37a74b795841bd2217604d81653d06e3d
Haiying Liu <hliu@bwh.harvard.edu>
2010-04-28 19:26:56 +0000

    ENH: added Testing.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=12981
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@12981 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/NeuroNav/Testing/TestDependency.tcl
@@ -0,0 +1,20 @@
+
+proc TestModuleDependency {} {
+
+  set ::gui [$::slicer3::Application GetModuleGUIByName OpenIGTLinkIF] 
+  if {$::gui == ""} {
+      puts "Cannot find OpenIGTLinkIF module.\n"
+      return 1
+  }
+
+  return 0;
+}
+
+set ret [ catch { $::slicer3::Application TraceScript TestModuleDependency } ]
+
+if {$ret == 1} {
+    exit 1
+  } else {
+    exit 0
+  }
+}
