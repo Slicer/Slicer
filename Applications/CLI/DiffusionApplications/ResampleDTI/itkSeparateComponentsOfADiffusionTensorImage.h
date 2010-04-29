a7f9967d417eaf20b6f25ac63ea0ddfee758ea98
Francois Budin <budin@bwh.harvard.edu>
2010-04-29 19:09:43 +0000

    BUG:Forgot to add 2 files
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13080
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13080 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/CLI/DiffusionApplications/ResampleDTI/itkSeparateComponentsOfADiffusionTensorImage.h
@@ -0,0 +1,76 @@
+/*=========================================================================
+
+  Program:   Diffusion Applications
+  Module:    $HeadURL: http://svn.slicer.org/Slicer3/trunk/Applications/CLI/DiffusionApplications/ResampleDTI/itkDiffusionTensor3DResample.h $
+  Language:  C++
+  Date:      $Date: 2010/04/05 10:04:59 $
+  Version:   $Revision: 1.1 $
+
+  Copyright (c) Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See License.txt or http://www.slicer.org/copyright/copyright.txt for details.
+
+==========================================================================*/
+#ifndef __itkSeparateComponentsOfADiffusionTensorImage_h
+#define __itkSeparateComponentsOfADiffusionTensorImage_h
+
+#include <itkImageToImageFilter.h>
+#include <itkOrientedImage.h>
+#include <itkDiffusionTensor3D.h>
+#include <itkImageRegionIterator.h>
+#include <itkImageRegionConstIterator.h>
+
+namespace itk
+{
+/** \class itkSeparateComponentsOfADiffusionTensorImage
+ * 
+ * 
+ */
+template< class TInput , class TOutput >
+class SeparateComponentsOfADiffusionTensorImage
+: public ImageToImageFilter
+< OrientedImage < DiffusionTensor3D < TInput > , 3 > ,
+  OrientedImage < TOutput , 3 > >
+{
+public :
+typedef TInput InputDataType ;
+typedef TOutput OutputDataType ;
+typedef ImageToImageFilter
+          < OrientedImage < DiffusionTensor3D < TInput > , 3 > ,
+            OrientedImage < TOutput , 3 > >
+Superclass ;
+typedef DiffusionTensor3D< InputDataType > InputTensorDataType ;
+typedef OrientedImage< InputTensorDataType , 3 > InputImageType ;
+typedef SeparateComponentsOfADiffusionTensorImage Self ;
+typedef SmartPointer< Self > Pointer ;
+typedef SmartPointer< const Self > ConstPointer ;
+typedef typename InputImageType::Pointer InputImagePointerType ;
+typedef OrientedImage< OutputDataType , 3 > OutputImageType ;
+typedef typename OutputImageType::Pointer OutputImagePointerType ;
+typedef itk::ImageRegionIterator< OutputImageType > OutputIteratorType ;
+typedef itk::ImageRegionConstIterator< InputImageType > InputIteratorType ;
+typedef typename OutputImageType::RegionType OutputImageRegionType ;
+//typedef typename OutputTensorDataType::RealValueType TensorRealType ;
+
+itkNewMacro( Self ) ;
+
+
+
+protected :
+SeparateComponentsOfADiffusionTensorImage() ;
+void ThreadedGenerateData( const OutputImageRegionType &outputRegionForThread ,
+                           int threadId ) ;
+void GenerateOutputInformation() ;
+void GenerateInputRequestedRegion() ;
+
+private :
+
+};
+
+}//end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkSeparateComponentsOfADiffusionTensorImage.txx"
+#endif
+
+#endif
