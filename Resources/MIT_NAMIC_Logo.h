033653152f2548760eec3f2e948034a928134553
Sylvain Bouix <sylvain@bwh.harvard.edu>
2010-05-02 00:25:44 +0000

    ENH: Add png file with MIT logo and NAMIC logo side-by-side
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13154
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13154 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Resources/MIT_NAMIC_Logo.h
@@ -0,0 +1,46 @@
+/* 
+ * Resource generated for file:
+ *    MIT_NAMIC_Logo.png (zlib, base64) (image file)
+ */
+static const unsigned int  image_MIT_NAMIC_Logo_width          = 97;
+static const unsigned int  image_MIT_NAMIC_Logo_height         = 40;
+static const unsigned int  image_MIT_NAMIC_Logo_pixel_size     = 3;
+static const unsigned long image_MIT_NAMIC_Logo_length         = 2332;
+static const unsigned long image_MIT_NAMIC_Logo_decoded_length = 11640;
+
+static const unsigned char image_MIT_NAMIC_Logo[] = 
+  "eNrtmQlMU3ccxx+KODavGTczXZxLlk1jli3RxeiWZSbTbCGOHQlHS29RIKIiOCfqcFOM4A"
+  "WiRsSIx9hkE9p3tKWFIodQipRDkKMtVwE5yrVyC233e1yW9nW6GTd9+Ms/hLy+1/R98vt/"
+  "f7/v72+x0DYMvQOS0vthaIV7jGrlfsXCQKmrv3TBdum7+xRwJUJSVdzQaTKbLNMvTGaz7F"
+  "4LK069eJcc4eKItwjxwRAOgfAIhD/6F/5nYggDneUr3nQyR1rabDabpw+fYn2XW3QewsEQ"
+  "bxTh4AhPjPBHFwtDPEUIC0f4kvErsLjACkXYqFdsQWNX33TgE5tZM9dPQsKZJDPOB//4aH"
+  "Zkinb90Wwyr/hTP4WbvdGlQbKkwiZ68zmeokGYIoRtR4CFrdinqO/shXsqW4yuwBCSx+ae"
+  "UYzOXOxMmo6ufEB8ZgsICj6QISz0tEy7Jb7weIo2XFzlJBDb5tjkgo3JRuOyam2Tc/Vqm/"
+  "V/fepoPUblGlwVmk5qMsVbE8v3pOnaepbvliMMyDGMGs7kYuPz/cTq+k6aIYIkQRhC6ldm"
+  "oOy4QpDiOf7S0aImfgQiWN4ij3MqOiEaHBn+6KcsxAenfl8mGpdZW9LQOWOLmFqC7BdXPE"
+  "tA3NYaaIMINoWLo9fnEjMFRElDl7CwiWwDeHYy5QiatzD4RiltEJ1T6MjehnIHcYg3gmQj"
+  "5pFjEg3ZQFrD4Ymd+OKZwJY32kzaNQnvH8wYeDBMD0T8y4UgONTJ4INtPKG0WMzMWPXDe6"
+  "CHBNFmCD89kp1a3vJagASqGOKDTqmGXMJ1q7iiqftpQ/hvYn14NkWtH1teoj2JsF/MK0Jv"
+  "IUycTDYGuvJAOpT167n1P4oq6jv6AhNKJKXNYVjFq9tl41aFN7YHcXSik3zeES3fI6eWFB"
+  "5ZwZPVjXWGHhdfycJA2YbInPO3qo2DQ1ZPP7Rm1QbjQWH5mp8z5wakkF/IQi9l1dED0aKd"
+  "UqhBlFr9ip+0xdj/R34D4iU8hFVYPzVsNuVWGyqau4v0U1ogEC73GBVpYbi4qIgmWfRmcI"
+  "oDQ4F9EJYBN/xwsxzxFM4PSAn5vSwxv6Gq5U+LxQT5sz+pLFxcceFWDUnMNJKtMVxX6r0u"
+  "3JkhIEcBrn7Symbj00a01S6eBt514VnUWsRAmRcL4IYvTynJ0QdgBMX2SD4l151Pr9l8Jo"
+  "8VW3AMr2LE3gFvixff33RSCSRHiyNZ45wERHpFKz0Q7Ui4S+4LqiY5QqIxmU1LwHpwJhgy"
+  "MbeovASV/rOI25BUBXUdkDbfnVMl5uuXBqcibKts9EE3HLs9ODxCA0Sk1LBxqrYQl95tVt"
+  "d1OvFHmx8rdLtv3K3v6Dkt14hLGs4qtF19A+uOZJOZZvMNDDRKrqUBovvd/Yt3ykj/NVWr"
+  "Z/tK6tqNx6Ua267JGw36rSwqRTuHKdwRf8eFIcyrbl8DFoaF21vapbvkurbe5x0RxLYrxV"
+  "M4QM6wiWUhaa3G/rWH7ewbG38vND1L0xadpvs1rzY2oxp22extDoZITGxzlIoGiHJ17S5Q"
+  "hsbekUM4ASJP0dfRKtBbZwE+ZZeNt5TCSIkG7H+WxlBj6NkYkUOqtIPZyFwBTgNEEP5Xi8"
+  "l5CAv/MCwzo6r1m7MqaKEDrpVQeBNyyIb9otTHKKrdonITlPW8y0W2QjSxWxftlF3JqaMH"
+  "ooauvreC5YiHyC06b+xKRmXLS75iRzPYtYczta3dINTye83zt1N1VkCSg13NqaWBXE/Gpa"
+  "xacFhg3gXxRWAl3g5JdejdYHkkf59YBk99Am2VIxfsjYbeLKcTop7BB6sOKEgfCm2Sp8i2"
+  "xlFNI6HtWRbsgCRpZvHoVN0T/tRnChHEIWG5w5SwK1XuMfnd/UNLdsseNpZT/QsI2uDwMM"
+  "0QKXUdMx9zAEuO/RVhonvOvmKKkjfWOXBxeVkzzRAZBx5AO/R3EmQryDj1uJLcZQT7ktrQ"
+  "M0AzRGDhPz+RS13B/9EapZesbnryn/rsIbL4xKkRBvYvsXCJ8QVS7yW8mFFHS0SC+GJq4/"
+  "8IacKd+cQcf+nLflKXrRInAbHpZG5X3xAtEXldKEC+TSIpgTXjPt7ZIgdfHpIqLW1u6OzV"
+  "thkL9Z2ljV39E8cf9EOkqu4Af7rtWsna8Ox5AVJSupki6ppl5UcSlHrL9IsRs0nf2Zekbn"
+  "SPUTlxMIdljo2/szcNGiTLtA4zWNF5fmLqQ21vNMTq4HU6R2p56+uBKbZzM3IDQn/Y8oLP"
+  "WGRp2hb4ScjTWCuhXhIkb+8degFnMm6qG0kN9xKR7SWHPBP56ozqBRabqGoxhhOVX5xSvr"
+  "1XgfCwSKn2kY/8BbpsEkc=";
+
