d89d8b7371fd63c229e33c5e0b472ada1d7ee7cd
Luis Ibanez <luis.ibanez@kitware.com>
2010-05-02 23:52:08 +0000

    ENH: Refining the generation of the list of modules.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13164
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13164 3bd1e089-480b-0410-8dfb-8563597acbee

--- Scripts/generateListOfModules.sh
+++ Scripts/generateListOfModules.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/csh
+#!/bin/csh
 ./Slicer3 --clear-module-cache --verbose-module-discovery 2>&1 | tee listOfModules.txt
-grep "A module named" listOfModules.txt | grep -v already > listOfUniqueModules.txt
+grep "A module named" listOfModules.txt | grep -v already | grep -v Python > listOfUniqueModules.txt
+cut -d\" -f 2 listOfUniqueModules.txt >  listOfUniqueModulesIsolated.txt
+sort listOfUniqueModulesIsolated.txt > listOfUniqueModulesIsolatedSorted.txt
 
