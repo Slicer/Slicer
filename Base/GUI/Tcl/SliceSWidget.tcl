a65f552d79ca8cfb227dd7c90b79a90c9bac9f98
Steve Pieper <pieper@bwh.harvard.edu>
2010-05-11 18:43:35 +0000

    BUG: detect case where empty string is returned since it is considered a double value by the format command
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13319
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13319 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Tcl/SliceSWidget.tcl
+++ Base/GUI/Tcl/SliceSWidget.tcl
@@ -1138,22 +1138,24 @@ itcl::body SliceSWidget::updateStatusAnnotation {r a s} {
       set labelText " Lb: $_layers(label,pixel) $colorName,"
   }
 
-  if { [string is double $_layers(foreground,pixel)] } {
-      set fgvoxelText [format " Fg: %.1f" $_layers(foreground,pixel)]
+  set pixelValue $_layers(foreground,pixel)
+  if { $pixelValue != "" && [string is double $pixelValue] } {
+      set fgvoxelText [format " Fg: %.1f" $pixelValue]
   } else {
-      if { $_layers(foreground,pixel) == "None"} {
+      if { $pixelValue == "None"} {
           set fgvoxelText ""
       } else {
-          set fgvoxelText " Fg: $_layers(foreground,pixel),"
+          set fgvoxelText " Fg: $pixelValue,"
       }
   }
-  if { [string is double $_layers(background,pixel)] } {
-      set bgvoxelText [format " Bg: %.1f" $_layers(background,pixel)]
+  set pixelValue $_layers(background,pixel)
+  if { $pixelValue != "" && [string is double $pixelValue] } {
+      set bgvoxelText [format " Bg: %.1f" $pixelValue]
   } else {
-      if { $_layers(background,pixel) == "None"} {
+      if { $pixelValue == "None"} {
           set bgvoxelText ""
       } else {
-          set bgvoxelText " Bg: $_layers(background,pixel),"
+          set bgvoxelText " Bg: $pixelValue,"
       }
   }
   
