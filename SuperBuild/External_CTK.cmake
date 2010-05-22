f6829456f377e4f1b9ad424022c6e61ae1b18b1d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-05-22 16:19:23 +0000

    COMP: Fix type error in Superbuild/CMakeLists.txt
    Use -DCTK_PLUGIN_org.commontk.eventbus:BOOL=OFF
    instead of -DCTK_LIB_org.commontk.eventbus:BOOL=OFF
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13448
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13448 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -21,7 +21,7 @@ if (Slicer3_USE_QT)
       -DCTK_LIB_Visualization/VTK/Widgets:BOOL=ON
       -DCTK_LIB_Scripting/Python/Widgets:BOOL=${Slicer3_USE_PYTHONQT}
       -DCTK_LIB_PluginFramework:BOOL=OFF
-      -DCTK_LIB_org.commontk.eventbus:BOOL=OFF
+      -DCTK_PLUGIN_org.commontk.eventbus:BOOL=OFF
     INSTALL_COMMAND ""
     
     )
