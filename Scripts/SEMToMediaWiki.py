3b2e3ea2f8935cba35765f86a3582f86521772aa
Nicole Aucoin <nicole@bwh.harvard.edu>
2010-06-10 20:53:21 +0000

    BUG: fixed a typo in the description field. ENH: updated to produce the output as defined by the Slicer3.6 template
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13751
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13751 3bd1e089-480b-0410-8dfb-8563597acbee

--- Scripts/SEMToMediaWiki.py
+++ Scripts/SEMToMediaWiki.py
@@ -79,7 +79,20 @@ doc = xml.dom.minidom.parse(sys.argv[1]);
 # Get the primary heirarchial tree node.
 executableNode=doc.getElementsByTagName("executable")[0]
 
+# header
+print "__NOTOC__"
+print "===%s===" % getThisNodesInfoAsText(executableNode,"title")
+print "%s\n" % getThisNodesInfoAsText(executableNode,"title")
+
+print "{|"
+print "|[[Image:screenshotBlankNotOptional.png|thumb|280px|User Interface]]"
+print "|[[Image:screenshotBlank.png|thumb|280px|Output]]"
+print "|[[Image:screenshotBlank.png|thumb|280px|Caption]]"
+print "|}"
+
+
 # Print information about the program in general
+print "== General Information ==\n"
 print "===Module Type & Category===\n"
 print "Type: CLI\n"
 print "Category: %s\n" % getThisNodesInfoAsText(executableNode,"category")
@@ -87,18 +100,43 @@ print "\n"
 
 print "===Authors, Collaborators & Contact===\n"
 print "Author: %s\n" % getThisNodesInfoAsText(executableNode,"contributor")
-print "Contributors: %s\n" % getThisNodesInfoAsText(executableNode,"acknowledgements")
+print "Contributors: \n"
+print "Contact: name, email"
 print "\n"
 
 print "===Module Description===\n"
 print "{| style=\"color:green\" border=\"1\"\n"
 getThisNodesInfoAsTextTableLine(executableNode,"title")
-getThisNodesInfoAsTextTableLine(executableNode,"descripition")
+getThisNodesInfoAsTextTableLine(executableNode,"description")
 getThisNodesInfoAsTextTableLine(executableNode,"version")
 getThisNodesInfoAsTextTableLine(executableNode,"documentation-url")
 getThisNodesInfoAsTextTableLine(executableNode,"doesnotexiststest")
 print "|}\n"
 
+print "== Usage ==\n"
+
+print "===Use Cases, Examples===\n"
+
+print "This module is especially appropriate for these use cases:\n"
+
+print "* Use Case 1:"
+print "* Use Case 2:\n"
+
+print "Examples of the module in use:\n"
+
+print "* Example 1:"
+print "* Example 2:\n"
+
+print "===Tutorials===\n"
+
+print "* Tutorial 1"
+print "** Data Set 1\n"
+
+print "===Quick Tour of Features and Use===\n"
+print "A list panels in the interface, their features, what they mean, and how to use them.\n"
+
+print "{|"
+print "|"
 # Now print all the command line arguments and the labels that showup in the GUI interface
 for parameterNode in executableNode.getElementsByTagName("parameters"):
   print "* <span style=\"color:blue\">'''''%s''''' </span> " % getThisNodesInfoAsText(parameterNode,"label")
@@ -114,3 +152,53 @@ for parameterNode in executableNode.getElementsByTagName("parameters"):
   print # newlines to make it more readable
   print
 
+print "|[[Image:screenshotBlankNotOptional.png|thumb|280px|User Interface]]"
+print "|}\n"
+
+# footer
+print "== Development ==\n"
+
+print "===Notes from the Developer(s)===\n"
+
+print "Algorithms used, library classes depended upon, use cases, etc.\n"
+
+print "===Dependencies===\n"
+
+print "Other modules or packages that are required for this module's use.\n"
+
+print "===Tests===\n"
+
+print "On the [http://www.cdash.org/CDash/index.php?project=Slicer3 Dashboard], these tests verify that the module is working on various platforms:\n"
+
+print "* MyModuleTest1 [http://viewvc.slicer.org/viewcvs.cgi/trunk MyModuleTest1.cxx]"
+print "* MyModuleTest2 [http://viewvc.slicer.org/viewcvs.cgi/trunk MyModuleTest2.cxx]\n"
+
+print "===Known bugs===\n"
+
+print "Links to known bugs in the Slicer3 bug tracker\n"
+
+print "* [http://www.na-mic.org/Bug/view.php?id=000 Bug 000: description] \n"
+
+
+print "===Usability issues===\n"
+
+print "Follow this [http://na-mic.org/Mantis/main_page.php link] to the Slicer3 bug tracker. Please select the '''usability issue category''' when browsing or contributing.\n"
+
+print "===Source code & documentation===\n"
+
+print "Links to the module's source code:\n"
+
+print "Source code:"
+print "*[http://viewvc.slicer.org/viewcvs.cgi/trunk file.cxx ]"
+print "*[http://viewvc.slicer.org/viewcvs.cgi/trunk file.h ]\n"
+ 
+print "Doxygen documentation:"
+print "*[http://www.na-mic.org/Slicer/Documentation/Slicer3-doc/html/classes.html class1]\n"
+
+print "== More Information == \n"
+
+print "===Acknowledgment===\n"
+print "%s\n" % getThisNodesInfoAsText(executableNode,"acknowledgements")
+
+print "===References===\n"
+print "Publications related to this module go here. Links to pdfs would be useful."
