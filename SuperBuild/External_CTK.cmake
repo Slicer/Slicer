ffb8d0789525b31498e927027efd3445fe009d50
Steve Pieper <pieper@bwh.harvard.edu>
2010-06-14 17:54:51 +0000

    COMP: pass down custom git executable location (for cygwin or if newer version is installed in local dir, for instance)
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13789
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13789 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -29,6 +29,7 @@ if (Slicer3_USE_QT)
         -DCTK_LIB_Scripting/Python/Widgets:BOOL=${Slicer3_USE_PYTHONQT}
         -DCTK_LIB_PluginFramework:BOOL=OFF
         -DCTK_PLUGIN_org.commontk.eventbus:BOOL=OFF
+        -Dgit_EXECUTABLE:FILEPATH=${git_EXECUTABLE}
       INSTALL_COMMAND ""
       DEPENDS ${CTK_DEPENDENCIES}
       )
