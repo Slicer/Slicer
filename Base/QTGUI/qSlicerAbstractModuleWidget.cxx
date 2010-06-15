55c753d2743477c030318331bf2fe05a9762db0b
Julien Finet <julien.finet@kitware.com>
2010-06-15 18:58:43 +0000

    COMP: Following the recent merge (QTBase->QtCore&QtGUI), fix comp issues
    Forgot to add the add the method
    qSlicerAbstractModuleWidget::showModuleAction()
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13802
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13802 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTGUI/qSlicerAbstractModuleWidget.cxx
+++ Base/QTGUI/qSlicerAbstractModuleWidget.cxx
@@ -26,3 +26,9 @@ void qSlicerAbstractModuleWidget::setName(const QString& name)
   // better than nothing but still not that...
   this->setWindowTitle(name);
 }
+
+//-----------------------------------------------------------------------------
+QAction* qSlicerAbstractModuleWidget::showModuleAction()
+{
+  return 0;
+}
--- Base/QTGUI/qSlicerAbstractModuleWidget.h
+++ Base/QTGUI/qSlicerAbstractModuleWidget.h
@@ -10,7 +10,6 @@
 
 =========================================================================auto=*/
 
-
 #ifndef __qSlicerAbstractModuleWidget_h
 #define __qSlicerAbstractModuleWidget_h
 
@@ -23,13 +22,26 @@
 
 #include "qSlicerBaseQTGUIExport.h"
 
-
+///
+/// Base class of all the Slicer module widgets. The widget is added in the module panels.
+/// Deriving from qSlicerWidget, it inherits the mrmlScene()/setMRMLScene() methods.
 class Q_SLICER_BASE_QTGUI_EXPORT qSlicerAbstractModuleWidget : public qSlicerWidget, public qSlicerAbstractModuleRepresentation
 {
   Q_OBJECT
 public:
+  /// Constructor
+  /// \sa QWidget
   qSlicerAbstractModuleWidget(QWidget *parent=0);
+
+  ///
+  /// Reimplemented from qSlicerAbstractModuleRepresentation
+  /// Set the module unique name to the window title of the widget
   virtual void setName(const QString& moduleName);
+
+  ///
+  /// Returns a QAction if the module would like to be added in a toolbar.
+  /// By default, returns no QAction.
+  virtual QAction * showModuleAction();
 };
 
 #endif
