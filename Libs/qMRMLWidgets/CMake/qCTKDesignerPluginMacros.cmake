039185915a59e8322b76d5830e34ba2b1aa7698e
Julien Finet <julien.finet@kitware.com>
2010-06-17 03:00:44 +0000

    COMP: Copy the Qt designer plugins in the correct location.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13831
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13831 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/qMRMLWidgets/CMake/qCTKDesignerPluginMacros.cmake
+++ Libs/qMRMLWidgets/CMake/qCTKDesignerPluginMacros.cmake
@@ -6,123 +6,105 @@
 # Build a qtdesigner plugin
 #
 MACRO(qctk_build_designer_plugin)
-  QCTK_PARSE_ARGUMENTS(QCTK_DESIGNERPLUGIN
-    "NAME;SRCS;MOC_SRCS;TARGET_LIBRARIES;RESOURCES"
+  QCTK_PARSE_ARGUMENTS(MY
+    "NAME;EXPORT_DIRECTIVE;SRCS;MOC_SRCS;UI_FORMS;INCLUDE_DIRECTORIES;TARGET_LIBRARIES;RESOURCES;LIBRARY_TYPE"
     ""
     ${ARGN}
     )
-  
+
   # Sanity checks
-  IF(NOT DEFINED QCTK_DESIGNERPLUGIN_NAME)
+  IF(NOT DEFINED MY_NAME)
     MESSAGE(SEND_ERROR "NAME is mandatory")
-  ENDIF(NOT DEFINED QCTK_DESIGNERPLUGIN_NAME)
-  
+  ENDIF()
+  IF(NOT DEFINED MY_LIBRARY_TYPE)
+    SET(MY_LIBRARY_TYPE "SHARED")
+  ENDIF()
+
   # Define library name
-  SET(lib_name ${QCTK_DESIGNERPLUGIN_NAME})
+  SET(lib_name ${MY_NAME})
 
   # --------------------------------------------------------------------------
   # Include dirs
-  #
-  SET(QT_INCLUDE_DIRS
-    ${QT_QTGUI_INCLUDE_DIR}
-    ${QT_QTCORE_INCLUDE_DIR}
+  SET(my_includes
+    #${CTK_BASE_INCLUDE_DIRS}
     ${QT_QTDESIGNER_INCLUDE_DIR}
-    )
-  
-  SET(include_dirs
-    ${QT_INCLUDE_DIRS}
+    ${CMAKE_CURRENT_SOURCE_DIR}
     ${CMAKE_CURRENT_BINARY_DIR}
+    ${MY_INCLUDE_DIRECTORIES}
     )
-  
-  INCLUDE_DIRECTORIES(${include_dirs})
-  
-    
-  #----------------------------------------------------------------------------
-  # Sources
-  #
-  
-  QT4_WRAP_CPP(QCTK_DESIGNERPLUGIN_SRCS ${QCTK_DESIGNERPLUGIN_MOC_SRCS})
-  IF(DEFINED QCTK_DESIGNERPLUGIN_RESOURCES)
-    QT4_ADD_RESOURCES(QCTK_DESIGNERPLUGIN_QRC_SRCS ${QCTK_DESIGNERPLUGIN_RESOURCES})
-  ENDIF(DEFINED QCTK_DESIGNERPLUGIN_RESOURCES)
-  
-  
-  # --------------------------------------------------------------------------
-  # Build the library
+  INCLUDE_DIRECTORIES(
+    ${my_includes}
+    )
 
-  ADD_LIBRARY(${lib_name} MODULE
-    ${QCTK_DESIGNERPLUGIN_SRCS}
-    ${QCTK_DESIGNERPLUGIN_QRC_SRCS})
-  
+  SET(MY_LIBNAME ${lib_name})
+
+  # Make sure variable are cleared
+  SET(MY_UI_CXX)
+  SET(MY_QRC_SRCS)
+
+  # Wrap
+  QT4_WRAP_CPP(MY_SRCS ${MY_MOC_SRCS})
+  QT4_WRAP_UI(MY_UI_CXX ${MY_UI_FORMS})
+  SET(MY_QRC_SRCS "")
+  IF(DEFINED MY_RESOURCES)
+    QT4_ADD_RESOURCES(MY_QRC_SRCS ${MY_RESOURCES})
+  ENDIF()
+
+  SOURCE_GROUP("Resources" FILES
+    ${MY_RESOURCES}
+    ${MY_UI_FORMS}
+    )
+
+  SOURCE_GROUP("Generated" FILES
+    ${MY_MOC_SRCS}
+    ${MY_QRC_SRCS}
+    ${MY_UI_CXX}
+    )
+
+  ADD_LIBRARY(${lib_name} ${MY_LIBRARY_TYPE}
+    ${MY_SRCS}
+    ${MY_UI_CXX}
+    ${MY_QRC_SRCS}
+    )
+
   # Apply properties to the library target.
   SET_TARGET_PROPERTIES(${lib_name}  PROPERTIES
-                      COMPILE_FLAGS "-DQT_PLUGIN")
-  
-  SET(QT_LIBRARIES 
-    ${QT_QTCORE_LIBRARY}
-    ${QT_QTGUI_LIBRARY}
+    COMPILE_FLAGS "-DQT_PLUGIN"
+    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/designer"
+    )
+
+  SET(my_libs
+    ${MY_TARGET_LIBRARIES}
     ${QT_QTDESIGNER_LIBRARY}
     )
-  
-  TARGET_LINK_LIBRARIES(${lib_name}
-    ${QT_LIBRARIES}
-    ${QCTK_DESIGNERPLUGIN_TARGET_LIBRARIES}
-    )
+  TARGET_LINK_LIBRARIES(${lib_name} ${my_libs})
 
-ENDMACRO(qctk_build_designer_plugin)
-
-#
-# Install qtdesigner plugin
-#
-MACRO(qctk_install_designer_plugin)
-  QCTK_PARSE_ARGUMENTS(QCTK_INSTALLDESIGNERPLUGIN
-    "NAME;INSTALL_BIN_DIR;INSTALL_LIB_DIR;INSTALL_HEADERS_DIR"
-    ""
-    ${ARGN}
-    )
-
-  #----------------------------------------------------------------------------
-  # Sanity checks
-  #
-  IF(NOT DEFINED QCTK_INSTALLDESIGNERPLUGIN_INSTALL_BIN_DIR)
-    MESSAGE(SEND_ERROR "INSTALL_BIN_DIR is mandatory")
-  ENDIF(NOT DEFINED QCTK_INSTALLDESIGNERPLUGIN_INSTALL_BIN_DIR)
-  
-  IF(NOT DEFINED QCTK_INSTALLDESIGNERPLUGIN_INSTALL_LIB_DIR)
-    MESSAGE(SEND_ERROR "INSTALL_LIB_DIR is mandatory")
-  ENDIF(NOT DEFINED QCTK_INSTALLDESIGNERPLUGIN_INSTALL_LIB_DIR)
-
-  IF(NOT DEFINED QCTK_INSTALLDESIGNERPLUGIN_INSTALL_HEADERS_DIR)
-    MESSAGE(SEND_ERROR "INSTALL_HEADERS_DIR is mandatory")
-  ENDIF(NOT DEFINED QCTK_INSTALLDESIGNERPLUGIN_INSTALL_HEADERS_DIR)
-
-  # --------------------------------------------------------------------------
   # Install the library
-
-  FILE(GLOB headers "${CMAKE_CURRENT_SOURCE_DIR}/*.h")
-  INSTALL(FILES 
-    ${headers}
-    DESTINATION ${QCTK_INSTALLDESIGNERPLUGIN_INSTALL_HEADERS_DIR}/${PROJECT_NAME} COMPONENT Development
-    )
-  
   INSTALL(TARGETS ${lib_name}
-    RUNTIME DESTINATION ${QCTK_INSTALLDESIGNERPLUGIN_INSTALL_BIN_DIR} COMPONENT RuntimeLibraries
-    LIBRARY DESTINATION ${QCTK_INSTALLDESIGNERPLUGIN_INSTALL_LIB_DIR} COMPONENT RuntimeLibraries
-    ARCHIVE DESTINATION ${QCTK_INSTALLDESIGNERPLUGIN_INSTALL_LIB_DIR} COMPONENT Development
-  )
-  
+    RUNTIME DESTINATION ${CTK_INSTALL_BIN_DIR}/designer COMPONENT Runtime
+    LIBRARY DESTINATION ${CTK_INSTALL_LIB_DIR}/designer COMPONENT Runtime
+    ARCHIVE DESTINATION ${CTK_INSTALL_LIB_DIR}/designer COMPONENT Development)
+
+  # Install headers - Are headers required ?
+  #FILE(GLOB headers "${CMAKE_CURRENT_SOURCE_DIR}/*.h")
+  #INSTALL(FILES
+  #  ${headers}
+  #  DESTINATION ${CTK_INSTALL_INCLUDE_DIR} COMPONENT Development
+  #  )
+
+
   # Since QtDesigner expects plugin to be directly located under the
-  # directory 'designer', let's copy them. 
+  # directory 'designer', let's copy them.
 
   IF(NOT CMAKE_CFG_INTDIR STREQUAL ".")
     GET_TARGET_PROPERTY(FILE_PATH ${lib_name} LOCATION)
     GET_TARGET_PROPERTY(DIR_PATH ${lib_name} LIBRARY_OUTPUT_DIRECTORY)
-  
+
     ADD_CUSTOM_COMMAND(
       TARGET ${lib_name}
       POST_BUILD
       COMMAND ${CMAKE_COMMAND} -E copy ${FILE_PATH} ${DIR_PATH}/../designer/${CMAKE_SHARED_LIBRARY_PREFIX}${lib_name}${CMAKE_BUILD_TYPE}${CMAKE_SHARED_LIBRARY_SUFFIX}
       )
   ENDIF()
-  
-ENDMACRO(qctk_install_designer_plugin)
+
+ENDMACRO()
