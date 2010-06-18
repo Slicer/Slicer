efd9440da1097a4766ce727b59d5f41c6f35ba68
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-06-18 14:33:39 +0000

    COMP: Superbuild - On windows, CLAPACK will now be built as 64bits library if the underlying architecture is 64bits
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13843
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13843 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CLAPACK.cmake
+++ SuperBuild/External_CLAPACK.cmake
@@ -9,6 +9,7 @@ set(CLAPACK_file http://svn.slicer.org/Slicer3-lib-mirrors/trunk/clapack-${CLAPA
 
 set(CLAPACK_source "${CMAKE_CURRENT_BINARY_DIR}/CLAPACK")
 set(CLAPACK_binary "${CMAKE_CURRENT_BINARY_DIR}/CLAPACK-build")
+
 # turn off the warnings for CLAPACK on windows
 string(REPLACE "/W3" "/W0" CMAKE_CXX_FLAGS_CLAPACK "${CMAKE_CXX_FLAGS}")
 string(REPLACE "/W4" "/W0" CMAKE_CXX_FLAGS_CLAPACK
@@ -16,12 +17,28 @@ string(REPLACE "/W4" "/W0" CMAKE_CXX_FLAGS_CLAPACK
 string(REPLACE "/W3" "/W0" CMAKE_C_FLAGS_CLAPACK "${CMAKE_C_FLAGS}")
 string(REPLACE "/W4" "/W0" CMAKE_C_FLAGS_CLAPACK
   "${CMAKE_C_FLAGS_CLAPACK}")
+
+set(clapack_gen ${gen})
+
+# HACK - On windows, if the underlying architecture is 64bits, 
+# let's make sure CLAPACK is built as 64bits library 
+# Ideally, would be great if the bitness of the entire application could match 
+# the bitness of the underlying architecture. 
+if(WIN32)
+  if(CMAKE_SIZEOF_VOID_P EQUALS 8)
+    string(REGEX MATCH ".+Win64" is_win64)
+    if (is_win64 STREQUAL "")
+      set(clapack_gen "${clapack_gen} Win64")
+    endif()
+  endif()
+endif()
+  
 ExternalProject_Add(CLAPACK
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${CLAPACK_source}
   BINARY_DIR ${CLAPACK_binary}
   URL ${CLAPACK_file}
-  CMAKE_GENERATOR ${gen}
+  CMAKE_GENERATOR ${clapack_gen}
   CMAKE_ARGS
     -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS_CLAPACK}
     -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS_CLAPACK}
