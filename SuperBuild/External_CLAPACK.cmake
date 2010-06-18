93540f040adbe210e377c9a041011fd9b98d5f0b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-06-18 14:51:24 +0000

    COMP: Revert revision 13843 - It resumes to be a wrong approach
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13844
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13844 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CLAPACK.cmake
+++ SuperBuild/External_CLAPACK.cmake
@@ -17,28 +17,13 @@ string(REPLACE "/W4" "/W0" CMAKE_CXX_FLAGS_CLAPACK
 string(REPLACE "/W3" "/W0" CMAKE_C_FLAGS_CLAPACK "${CMAKE_C_FLAGS}")
 string(REPLACE "/W4" "/W0" CMAKE_C_FLAGS_CLAPACK
   "${CMAKE_C_FLAGS_CLAPACK}")
-
-set(clapack_gen ${gen})
-
-# HACK - On windows, if the underlying architecture is 64bits, 
-# let's make sure CLAPACK is built as 64bits library 
-# Ideally, would be great if the bitness of the entire application could match 
-# the bitness of the underlying architecture. 
-if(WIN32)
-  if(CMAKE_SIZEOF_VOID_P EQUALS 8)
-    string(REGEX MATCH ".+Win64" is_win64)
-    if (is_win64 STREQUAL "")
-      set(clapack_gen "${clapack_gen} Win64")
-    endif()
-  endif()
-endif()
   
 ExternalProject_Add(CLAPACK
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${CLAPACK_source}
   BINARY_DIR ${CLAPACK_binary}
   URL ${CLAPACK_file}
-  CMAKE_GENERATOR ${clapack_gen}
+  CMAKE_GENERATOR ${gen}
   CMAKE_ARGS
     -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS_CLAPACK}
     -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS_CLAPACK}
