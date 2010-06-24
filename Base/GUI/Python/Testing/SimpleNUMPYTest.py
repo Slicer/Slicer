3af55c5f083e1ec0e71a696e6708a3f1fed7db93
Dave Partyka <dave.partyka@kitware.com>
2010-06-24 14:05:17 +0000

    ENH: make test numpy test more robust so that it actually fails if import numpy fails.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=13932
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@13932 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Python/Testing/SimpleNUMPYTest.py
+++ Base/GUI/Python/Testing/SimpleNUMPYTest.py
@@ -1,7 +1,14 @@
 
 # prevents dashboard from truncating output of this test.
-print "CTEST_FULL_OUTPUT"
-
-import numpy
+print "Enabling CTEST_FULL_OUTPUT\n"
+print "\nChecking NUMPY..."
+try:
+    import numpy
+    print "\t----> import numpy WORKS!"
+    
+except ImportError:
+    print "\t----> import numpy FAILED."
+    #os._exit(-1)
+    Slicer.tk.eval('exit 1')
 
 Slicer.tk.eval('exit 0')
