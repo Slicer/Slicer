a6e75e7802e02348ba6b1d0a662a440b17ca6268
Julien Finet <julien.finet@kitware.com>
2010-07-06 00:01:03 +0000

    ENH: the property "fileName" is now settable in qSlicerIOOptionsWidget
    Derive qSlicerIOOptionsWidget from qSlicerWidget, that way it knows about
    the application mrml scene.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=14039
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@14039 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTGUI/qSlicerIOOptionsWidget.cxx
+++ Base/QTGUI/qSlicerIOOptionsWidget.cxx
@@ -1,12 +1,64 @@
+// Qt includes
+#include <QDebug>
+
+// qSlicer includes
 #include "qSlicerIOOptionsWidget.h"
 
 //------------------------------------------------------------------------------
 qSlicerIOOptionsWidget::qSlicerIOOptionsWidget(QWidget* parentWidget)
-  :QWidget(parentWidget)
+  :qSlicerWidget(parentWidget)
 {
 }
 
 //------------------------------------------------------------------------------
 qSlicerIOOptionsWidget::~qSlicerIOOptionsWidget()
 {
+}
+
+//------------------------------------------------------------------------------
+bool qSlicerIOOptionsWidget::isValid()const
+{
+  return this->Properties.contains("fileName") || this->Properties.contains("fileNames");
+}
+
+//------------------------------------------------------------------------------
+void qSlicerIOOptionsWidget::setFileName(const QString& fileName)
+{
+  qDebug() << "setFileName:" << fileName;
+  bool oldValid = this->isValid();
+  // replace the old filename if any
+  if (!fileName.isEmpty())
+    {
+    this->Properties["fileName"] = fileName;
+    }
+  else
+    {
+    this->Properties.remove("fileName");
+    }
+  this->Properties.remove("fileNames");
+  bool newValid = this->isValid();
+  if (oldValid != newValid)
+    {
+    emit this->validChanged(newValid);
+    }
+}
+
+//------------------------------------------------------------------------------
+void qSlicerIOOptionsWidget::setFileNames(const QStringList& fileNames)
+{
+  bool oldValid = this->isValid();
+  if (fileNames.count())
+    {
+    this->Properties["fileNames"] = fileNames;
+    }
+  else
+    {
+    this->Properties.remove("fileNames");
+    }
+  this->Properties.remove("fileName");
+  bool newValid = this->isValid();
+  if (oldValid != newValid)
+    {
+    emit this->validChanged(newValid);
+    }
 }
--- Base/QTGUI/qSlicerIOOptionsWidget.h
+++ Base/QTGUI/qSlicerIOOptionsWidget.h
@@ -1,20 +1,29 @@
 #ifndef __qSlicerIOOptionsWidget_h
 #define __qSlicerIOOptionsWidget_h
 
-/// Qt includes
-#include <QWidget>
-
 /// QtCore includes
-#include "qSlicerIOOptions.h"
 #include "qSlicerBaseQTGUIExport.h"
+#include "qSlicerIOOptions.h"
+#include "qSlicerWidget.h"
 
 class Q_SLICER_BASE_QTGUI_EXPORT qSlicerIOOptionsWidget
-  : public QWidget, public qSlicerIOOptions
+  : public qSlicerWidget, public qSlicerIOOptions
 {
   Q_OBJECT
 public:
   explicit qSlicerIOOptionsWidget(QWidget* parent = 0);
   virtual ~qSlicerIOOptionsWidget();
+
+  /// Returns true if the options have been set and if they are
+  /// meaningful
+  virtual bool isValid()const;
+
+public slots:
+  virtual void setFileName(const QString& fileName);
+  virtual void setFileNames(const QStringList& fileNames);
+
+signals:
+  void validChanged(bool);
 };
 
 #endif
