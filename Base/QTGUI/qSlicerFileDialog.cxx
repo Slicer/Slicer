cabcd580c50b118c92685566566d9bbcc46a0fdf
Julien Finet <julien.finet@kitware.com>
2010-07-06 00:01:16 +0000

    ENH: Automatically add qSlicerIOOptionWidgets into the fileDialog
    qSlicerStandardFileDialog doesn't know what options to use for a given file
    type. However it can get an option widget from the Reader itself. It adds
    it at the bottom of the file dialog.
    The user cannot click on accept if the qSlicerIOOptionsWidget is not valid.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=14043
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@14043 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTGUI/qSlicerFileDialog.cxx
+++ Base/QTGUI/qSlicerFileDialog.cxx
@@ -5,11 +5,14 @@
 #include <QUrl>
 
 // CTK includes
+#include <ctkFileDialog.h>
 
 // SlicerQt includes
 #include "qSlicerApplication.h"
 #include "qSlicerFileDialog.h"
 #include "qSlicerIOManager.h"
+#include "qSlicerIOOptionsWidget.h"
+
 /*
 //-----------------------------------------------------------------------------
 class qSlicerFileDialogPrivate: public ctkPrivate<qSlicerFileDialog>
@@ -107,7 +110,7 @@ bool qSlicerStandardFileDialog::exec(const qSlicerIO::IOProperties& ioProperties
   CTK_D(qSlicerStandardFileDialog);
   Q_ASSERT(!ioProperties.contains("fileName"));
   qSlicerIOManager* ioManager = qSlicerApplication::application()->ioManager();
-  QFileDialog fileDialog(qobject_cast<QWidget*>(this->parent()));
+  ctkFileDialog fileDialog(qobject_cast<QWidget*>(this->parent()));
 #ifdef Slicer3_USE_KWWIDGETS
   fileDialog.setWindowFlags(fileDialog.windowFlags() | Qt::WindowStaysOnTopHint);
 #endif
@@ -118,11 +121,40 @@ bool qSlicerStandardFileDialog::exec(const qSlicerIO::IOProperties& ioProperties
     {
     fileDialog.setSidebarUrls(ioManager->favorites());
     }
+  // warning: we are responsible for the memory of options
+  qSlicerIOOptions* options = ioManager->fileOptions(this->fileType());
+  qSlicerIOOptionsWidget* optionsWidget =
+    dynamic_cast<qSlicerIOOptionsWidget*>(options);
+  // options is not necessary a qSLicerIOOptionsWidget (for the case of
+  // readers/modules with no UI. If there is a UI then add it inside the  file
+  // dialog.
+  if (optionsWidget)
+    {
+    // fileDialog will reparent optionsWidget and take care of deleting
+    // optionsWidget for us.
+    fileDialog.setBottomWidget(optionsWidget, tr("Options:"));
+    // TODO: support selection of more than 1 file
+    connect(&fileDialog, SIGNAL(currentChanged(const QString&)),
+            optionsWidget, SLOT(setFileName(const QString&)));
+    connect(optionsWidget, SIGNAL(validChanged(bool)),
+            &fileDialog, SLOT(setAcceptButtonEnable(bool)));
+    fileDialog.setAcceptButtonEnable(optionsWidget->isValid());
+    }
+  // we do not delete options now as it is still useful later (even if there is
+  // no UI.) they are the options of the reader, UI or not.
   bool res = fileDialog.exec();
   if (res)
     {
     qSlicerIO::IOProperties properties = ioProperties;
-    properties["fileName"] = fileDialog.selectedFiles()[0];
+    if (options)
+      {
+      properties.unite(options->properties());
+      }
+    else
+      {
+      // TODO: support selection of more than 1 file
+      properties["fileName"] = fileDialog.selectedFiles()[0];
+      }
     if (d->Action == qSlicerFileDialog::Read)
       {
       ioManager->loadNodes(this->fileType(), properties);
@@ -136,6 +168,14 @@ bool qSlicerStandardFileDialog::exec(const qSlicerIO::IOProperties& ioProperties
       Q_ASSERT(d->Action == qSlicerFileDialog::Read ||
                d->Action == qSlicerFileDialog::Write);
       }
+    }
+  // If options is not a qSlicerIOOptionsWidget, we are responsible for
+  // deleting options. If it is, then fileDialog would have reparent
+  // the options and take care of its destruction
+  if (!optionsWidget)
+    {
+    delete options;
+    options = 0;
     }
   return res;
 }
