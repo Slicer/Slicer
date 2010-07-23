6ae58711178602bc7896ff73abe80b22284f69c7
Nicole Aucoin <nicole@bwh.harvard.edu>
2010-07-23 20:28:11 +0000

    ENH: expanding the test before adding in displayable hierarchy node
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=14322
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@14322 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/Testing/vtkMRMLModelHierarchyNodeTest1.cxx
+++ Libs/MRML/Testing/vtkMRMLModelHierarchyNodeTest1.cxx
@@ -25,5 +25,35 @@ int vtkMRMLModelHierarchyNodeTest1(int , char * [] )
 
   EXERCISE_BASIC_MRML_METHODS(vtkMRMLModelHierarchyNode, node1);
 
+  TEST_SET_GET_STRING(node1, ModelNodeID);
+  //TEST_SET_GET_STRING(node1, DisplayNodeID);
+  vtkSmartPointer<vtkMRMLScene> scene = vtkSmartPointer<vtkMRMLScene>::New();
+  vtkSmartPointer<vtkMRMLModelDisplayNode> dnode = vtkSmartPointer<vtkMRMLModelDisplayNode>::New();
+  scene->AddNode(node1);
+  scene->AddNode(dnode);
+  node1->SetAndObserveDisplayNodeID(dnode->GetID());
+  TEST_SET_GET_BOOLEAN(node1, Expanded);
+  
+  vtkSmartPointer<vtkMRMLModelNode> mnode = vtkSmartPointer<vtkMRMLModelNode>::New();
+  scene->AddNode(mnode);
+  node1->SetModelNodeID(mnode->GetID());
+  vtkSmartPointer<vtkMRMLModelNode> mnode2 = vtkSmartPointer<vtkMRMLModelNode>::New();
+  mnode2 = node1->GetModelNode();
+  if (mnode2 != mnode)
+    {
+    std::cerr << "ERROR setting/getting model node" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  vtkSmartPointer<vtkMRMLModelHierarchyNode> hnode1 = node1->GetUnExpandedParentNode();
+  std::cout << "Unexpanded parent node = " << (hnode1 == NULL ? "NULL" : hnode1->GetID()) << std::endl;
+  vtkSmartPointer<vtkMRMLModelHierarchyNode> hnode2 = node1->GetTopParentNode();
+   std::cout << "Top parent node = " << (hnode2 == NULL ? "NULL" : hnode2->GetID()) << std::endl;
+  vtkSmartPointer<vtkCollection> col =  vtkSmartPointer<vtkCollection>::New();
+  node1->GetChildrenModelNodes(col);
+  std::cout << "Number of children model nodes = " << col->GetNumberOfItems() << std::endl;
+  vtkSmartPointer<vtkMRMLModelHierarchyNode> hnode3 = node1->GetModelHierarchyNode(scene, "myid");
+   std::cout << "Model hierarchy node = " << (hnode3 == NULL ? "NULL" : hnode3->GetID()) << std::endl;
+  
   return EXIT_SUCCESS;
 }
