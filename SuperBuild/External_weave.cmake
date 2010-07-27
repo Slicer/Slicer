3bd698604f93bec970dff5d853b24e3394e9bb36
Daniel Blezek <Blezek.Daniel@mayo.edu>
2010-07-27 02:48:06 +0000

    ENH: Added weave to SuperBuild.
    Weave is a Python library that allows C/C++ code to be "woven" into Python code.  This is usually done for performance reasons.  Weave has great integration with Numpy, and can be used to speed up image processing in Slicer3/Slicer4.
    
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=14346
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@14346 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ SuperBuild/External_weave.cmake
@@ -0,0 +1,33 @@
+# The Weave external project 
+
+set(weave_binary "${CMAKE_CURRENT_BINARY_DIR}/weave/")
+
+# to configure weave we run a cmake -P script
+# the script will create a site.cfg file
+# then run python setup.py config to verify setup
+# configure_file(
+#   ${CMAKE_CURRENT_SOURCE_DIR}/weave_configure_step.cmake.in
+#   ${CMAKE_CURRENT_BINARY_DIR}/weave_configure_step.cmake @ONLY)
+# to build weave we also run a cmake -P script.
+# the script will set LD_LIBRARY_PATH so that 
+# python can run after it is built on linux
+configure_file(
+  ${CMAKE_CURRENT_SOURCE_DIR}/weave_make_step.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/weave_make_step.cmake @ONLY)
+
+# create an external project to download weave,
+# and configure and build it
+message ( ${Slicer3_SOURCE_DIR}/Modules/Python/FilteredTractography/weave )
+ExternalProject_Add(weave
+  # URL ${Slicer3_SOURCE_DIR}/Modules/Python/FilteredTractography/weave
+  SVN_REPOSITORY http://svn.slicer.org/Slicer3-lib-mirrors/trunk/weave
+  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/weave
+  SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/weave
+  BUILD_COMMAND ${CMAKE_COMMAND}
+    -P ${CMAKE_CURRENT_BINARY_DIR}/weave_make_step.cmake
+  CONFIGURE_COMMAND ""
+  UPDATE_COMMAND ""
+  INSTALL_COMMAND ""
+  DEPENDS 
+    ${weave_DEPENDENCIES}
+  )
