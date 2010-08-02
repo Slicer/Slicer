60633c213e21defdf722671ed0fefb2cd1568152
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-08-02 19:03:16 +0000

    ENH: EMSegment - Updated External_CTK.cmake to enable CTK_PythonQt_Wrap_Qt{gui,uitools} option
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=14423
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@14423 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -30,6 +30,8 @@ if (Slicer3_USE_QT)
         -DCTK_LIB_Visualization/VTK/Widgets:BOOL=ON
         -DCTK_LIB_Scripting/Python/Core:BOOL=${Slicer3_USE_PYTHONQT}
         -DCTK_LIB_Scripting/Python/Widgets:BOOL=${Slicer3_USE_PYTHONQT}
+        -DCTK_PythonQt_Wrap_Qtgui:BOOL=ON
+        -DCTK_PythonQt_Wrap_Qtuitools:BOOL=ON
         -DCTK_LIB_PluginFramework:BOOL=OFF
         -DCTK_PLUGIN_org.commontk.eventbus:BOOL=OFF
         -Dgit_EXECUTABLE:FILEPATH=${git_EXECUTABLE}
