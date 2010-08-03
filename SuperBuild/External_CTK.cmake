348652de3679772dbc9200a21f218d410273d96a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-08-03 19:17:03 +0000

    COMP: Superbuild - Updated External_CTK to set CTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_{QTGUI, QTUITOOLS} options
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=14449
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@14449 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -29,9 +29,9 @@ if (Slicer3_USE_QT)
         -DCTK_LIB_Widgets:BOOL=ON
         -DCTK_LIB_Visualization/VTK/Widgets:BOOL=ON
         -DCTK_LIB_Scripting/Python/Core:BOOL=${Slicer3_USE_PYTHONQT}
+        -DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTGUI:BOOL=${Slicer3_USE_PYTHONQT}
+        -DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTUITOOLS:BOOL=${Slicer3_USE_PYTHONQT}
         -DCTK_LIB_Scripting/Python/Widgets:BOOL=${Slicer3_USE_PYTHONQT}
-        -DCTK_PythonQt_Wrap_Qtgui:BOOL=ON
-        -DCTK_PythonQt_Wrap_Qtuitools:BOOL=ON
         -DCTK_LIB_PluginFramework:BOOL=OFF
         -DCTK_PLUGIN_org.commontk.eventbus:BOOL=OFF
         -Dgit_EXECUTABLE:FILEPATH=${git_EXECUTABLE}
