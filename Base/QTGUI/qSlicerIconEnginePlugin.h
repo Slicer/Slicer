5e7230fdb57f9f92510fb92ab28535526961f3a1
Julien Finet <julien.finet@kitware.com>
2010-08-10 14:20:02 +0000

    COMP: Fix compilation on Windows (add dllimport/dllexport macros)
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=14516
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@14516 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTGUI/qSlicerIconEnginePlugin.h
+++ Base/QTGUI/qSlicerIconEnginePlugin.h
@@ -1,9 +1,20 @@
+#ifndef __qSlicerIconEnginePlugin_h
+#define __qSlicerIconEnginePlugin_h
+
+// Qt includes
 #include <ctkIconEnginePlugin.h>
 
+// SlicerQt includes
+#include "qSlicerBaseQTGUIExport.h"
+
 /// Customized ctkIconEnginePlugin to look for icons in specific directories.
-class qSlicerIconEnginePlugin: public ctkIconEnginePlugin
+/// By default it will look for icons in the Small/Medium/Large and XLarge
+/// directories
+class Q_SLICER_BASE_QTGUI_EXPORT qSlicerIconEnginePlugin: public ctkIconEnginePlugin
 {
   Q_OBJECT
 public:
   qSlicerIconEnginePlugin(QObject* parent = 0);
 };
+
+#endif
