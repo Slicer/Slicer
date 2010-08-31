f46dd123ebfd4637d2fd6ba20234b3b0738cc642
Julien Finet <julien.finet@kitware.com>
2010-08-31 19:15:02 +0000

    ENH: Add license to qMRMLWidget header files
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=14778
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@14778 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/qMRMLWidgets/Plugins/qMRMLWidgetsAbstractPlugin.cxx
+++ Libs/qMRMLWidgets/Plugins/qMRMLWidgetsAbstractPlugin.cxx
@@ -1,27 +1,47 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) 2010 Kitware Inc.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Julien Finet, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
 #include "qMRMLWidgetsAbstractPlugin.h"
 
 qMRMLWidgetsAbstractPlugin::qMRMLWidgetsAbstractPlugin()
 {
 }
 
-QString qMRMLWidgetsAbstractPlugin::group() const 
-{ 
-  return "Slicer [MRML Widgets]"; 
+QString qMRMLWidgetsAbstractPlugin::group() const
+{
+  return "Slicer [MRML Widgets]";
 }
 
 QIcon qMRMLWidgetsAbstractPlugin::icon() const
 {
-  return QIcon(); 
+  return QIcon();
 }
 
-QString qMRMLWidgetsAbstractPlugin::toolTip() const 
-{ 
-  return QString(); 
-}
-
-QString qMRMLWidgetsAbstractPlugin::whatsThis() const 
+QString qMRMLWidgetsAbstractPlugin::toolTip() const
 {
-  return QString(); 
+  return QString();
+}
+
+QString qMRMLWidgetsAbstractPlugin::whatsThis() const
+{
+  return QString();
 }
 
 
