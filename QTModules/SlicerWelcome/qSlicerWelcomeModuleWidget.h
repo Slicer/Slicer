cc798fa567c15b8e98d0a516fe16c2862e0d5dec
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-08-31 22:56:43 +0000

    STYLE: Add license header to QTModules files
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=14789
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@14789 3bd1e089-480b-0410-8dfb-8563597acbee

--- QTModules/SlicerWelcome/qSlicerWelcomeModuleWidget.h
+++ QTModules/SlicerWelcome/qSlicerWelcomeModuleWidget.h
@@ -1,3 +1,23 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) 2010 Kitware Inc.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
 #ifndef __qSlicerWelcomeModuleWidget_h
 #define __qSlicerWelcomeModuleWidget_h
 
@@ -6,7 +26,6 @@
 
 // SlicerQt includes
 #include "qSlicerAbstractModuleWidget.h"
-
 #include "qSlicerWelcomeModuleExport.h"
 
 class qSlicerWelcomeModuleWidgetPrivate;
