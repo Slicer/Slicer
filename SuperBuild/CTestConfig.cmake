47cee7c0b32ca8a2493d2aaf36fc6c1b465b3fa3
Dave Partyka <dave.partyka@kitware.com>
2010-09-30 22:08:05 +0000

    ENH: also update the SuperBuild CTestConfig.cmake to submit to Slicer4.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=15044
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@15044 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/CTestConfig.cmake
+++ SuperBuild/CTestConfig.cmake
@@ -1,7 +1,7 @@
-set(CTEST_PROJECT_NAME "Slicer3")
-set(CTEST_NIGHTLY_START_TIME "23:00:00 EDT")
+set(CTEST_PROJECT_NAME "Slicer4")
+set(CTEST_NIGHTLY_START_TIME "3:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Slicer3")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Slicer4")
 set(CTEST_DROP_SITE_CDASH TRUE)
