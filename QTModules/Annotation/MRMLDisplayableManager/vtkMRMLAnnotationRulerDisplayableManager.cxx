60cc0ca6e5dc7a88b9ca184dbff4a09047416c4c
Daniel Haehn <haehn@bwh.harvard.edu>
2010-10-06 17:47:09 +0000

    BUG: Annotation module: Hopefully widget creation now works on Unix aswell.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=15060
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@15060 3bd1e089-480b-0410-8dfb-8563597acbee

--- QTModules/Annotation/MRMLDisplayableManager/vtkMRMLAnnotationRulerDisplayableManager.cxx
+++ QTModules/Annotation/MRMLDisplayableManager/vtkMRMLAnnotationRulerDisplayableManager.cxx
@@ -186,11 +186,7 @@ void vtkMRMLAnnotationRulerDisplayableManager::OnWidgetCreated(vtkAbstractWidget
   o << "LeftButtonReleaseEvent " << position2[0] << " " << position2[1] << " 0 0 0 0 t\n";
   o << "ExitEvent 192 173 0 0 113 1 q\n";
 
-
-  std::string commandstr(o.str());
-  const char* commandstr2 = commandstr.c_str();
-
-  recorder->SetInputString(commandstr2);
+  recorder->SetInputString(o.str().c_str());
   recorder->Play();
   }
 
