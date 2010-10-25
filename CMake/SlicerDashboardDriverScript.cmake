69b2243f307cd4a68c8fb19b940bce2148e9a905
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-10-25 18:19:27 +0000

    ENH: Add dashboard template script and driver script
    Using the Template script is an easy way to setup a dashboard.
    
    By setting few variable and running the customized dashboard script,
    it's possible to setup either a nightly, continuous or experimental
    dashboard just by changing the value of the SCRIPT_MODE variable.
    
    The script also provides options like WITH_KWSTYLE, WITH_COVERAGE,
    WITH_DOCUMENTATION ... allowing to run the associated process.
    
    While the provided template script is very simple, the implementation
    details are left to the driver script. This later one is downloaded each
    time the customized template script is ran. Doing so allows to update and
    fine-tune the driver script without updating all associated dashboard scripts.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=15151
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@15151 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ CMake/SlicerDashboardDriverScript.cmake
@@ -0,0 +1,165 @@
+
+
+#
+# Included from a dashboard script, this cmake file will drive the configure and build
+# steps of Slicer
+#
+
+# The following variable are expected to be define in the top-level script:
+set(expected_variables
+  ADDITIONNAL_CMAKECACHE_OPTION
+  CTEST_NOTES_FILES
+  CTEST_SITE
+  CTEST_DASHBOARD_ROOT
+  CTEST_CMAKE_COMMAND
+  CTEST_CMAKE_GENERATOR
+  WITH_MEMCHECK
+  WITH_COVERAGE
+  WITH_DOCUMENTATION
+  CTEST_BUILD_CONFIGURATION
+  CTEST_TEST_TIMEOUT
+  CTEST_BUILD_FLAGS
+  TEST_TO_EXCLUDE_REGEX
+  CTEST_PROJECT_NAME
+  CTEST_SOURCE_DIRECTORY
+  CTEST_BINARY_DIRECTORY
+  CTEST_BUILD_NAME
+  SCRIPT_MODE
+  CTEST_COVERAGE_COMMAND
+  CTEST_MEMORYCHECK_COMMAND
+  CTEST_SVN_COMMAND
+  QT_QMAKE_EXECUTABLE
+  )
+if(WITH_DOCUMENTATION)
+  list(APPEND expected_variables DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY)
+endif()
+
+foreach(var ${expected_variables})
+  if(NOT DEFINED ${var})
+    message(FATAL_ERROR "Variable ${var} should be defined in top-level script !")
+  endif()
+endforeach()
+
+set(repository http://svn.slicer.org/Slicer4/trunk)
+
+# Should binary directory be cleaned?
+set(empty_binary_directory FALSE)
+
+# Attempt to build and test also if 'ctest_update' returned an error
+set(force_build FALSE)
+
+# Set model options
+set(model "")
+if (SCRIPT_MODE STREQUAL "experimental")
+  set(empty_binary_directory FALSE)
+  set(force_build TRUE)
+  set(model Experimental)
+elseif (SCRIPT_MODE STREQUAL "continuous")
+  set(empty_binary_directory TRUE)
+  set(force_build FALSE)
+  set(model Continuous)
+elseif (SCRIPT_MODE STREQUAL "nightly")
+  set(empty_binary_directory TRUE)
+  set(force_build TRUE)
+  set(model Nightly)
+else()
+  message(FATAL_ERROR "Unknown script mode: '${SCRIPT_MODE}'. Script mode should be either 'experimental', 'continuous' or 'nightly'")
+endif()
+
+# For more details, see http://www.kitware.com/blog/home/post/11
+set(CTEST_USE_LAUNCHERS 1)
+
+if(empty_binary_directory)
+  message("Directory ${CTEST_BINARY_DIRECTORY} cleaned !")
+  ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+endif()
+
+if(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}")
+  set(CTEST_CHECKOUT_COMMAND "${CTEST_SVN_COMMAND} checkout ${repository} ${CTEST_SOURCE_DIRECTORY}")
+endif()
+
+set(CTEST_UPDATE_COMMAND "${CTEST_SVN_COMMAND}")
+
+set(CTEST_SOURCE_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/SuperBuild")
+
+#
+# run_ctest macro
+#
+MACRO(run_ctest)
+  ctest_start(${model})
+  ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}/.." RETURN_VALUE res)
+  ctest_submit(PARTS Update)
+
+  # force a build if this is the first run and the build dir is empty
+  if(NOT EXISTS "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt")
+    message("First time build - Initialize CMakeCache.txt")
+    set(res 1)
+
+    # Write initial cache.
+    file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" "
+CTEST_USE_LAUNCHERS:BOOL=${CTEST_USE_LAUNCHERS}
+QT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
+WITH_COVERAGE:BOOL=${WITH_COVERAGE}
+DOCUMENTATION_TARGET_IN_ALL:BOOL=${WITH_DOCUMENTATION}
+DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY:PATH=${DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY}
+${ADDITIONNAL_CMAKECACHE_OPTION}
+")
+  endif()
+  
+  if (res GREATER 0 OR force_build)
+    message("----------- [ Configure ${CTEST_PROJECT_NAME} ] -----------")
+    
+    #set(label Slicer)
+    
+    set_property(GLOBAL PROPERTY SubProject ${label})
+    set_property(GLOBAL PROPERTY Label ${label})
+     
+    ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}")
+    ctest_read_custom_files("${CTEST_BINARY_DIRECTORY}")
+    ctest_submit(PARTS Configure)
+
+    # Build top level
+    message("----------- [ Build ${CTEST_PROJECT_NAME} ] -----------")
+    ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}" APPEND)
+    ctest_submit(PARTS Build)
+    
+    message("----------- [ Test ${CTEST_PROJECT_NAME} ] -----------")
+    ctest_test(
+      BUILD "${CTEST_BINARY_DIRECTORY}/Slicer-build" 
+      #INCLUDE_LABEL ${label}
+      PARALLEL_LEVEL 8
+      EXCLUDE ${TEST_TO_EXCLUDE_REGEX})
+    # runs only tests that have a LABELS property matching "${label}"
+    ctest_submit(PARTS Test)
+    
+    # Global coverage ... 
+    if (WITH_COVERAGE AND CTEST_COVERAGE_COMMAND)
+      message("----------- [ Global coverage ] -----------")
+      ctest_coverage(BUILD "${CTEST_BINARY_DIRECTORY}/Slicer-build")
+      ctest_submit(PARTS Coverage)
+    endif ()
+    
+    # Global dynamic analysis ...
+    if (WITH_MEMCHECK AND CTEST_MEMORYCHECK_COMMAND)
+        message("----------- [ Global memcheck ] -----------")
+        ctest_memcheck(BUILD "${CTEST_BINARY_DIRECTORY}/Slicer-build")
+        ctest_submit(PARTS MemCheck)
+      endif ()
+    
+    # Note should be at the end
+    ctest_submit(PARTS Notes)
+  
+  endif()
+endmacro()
+
+if(SCRIPT_MODE STREQUAL "continuous")
+  while(${CTEST_ELAPSED_TIME} LESS 68400)
+    set(START_TIME ${CTEST_ELAPSED_TIME})
+    run_ctest()
+    # Loop no faster than once every 5 minutes
+    message("Wait for 5 minutes ...")
+    ctest_sleep(${START_TIME} 300 ${CTEST_ELAPSED_TIME})
+  endwhile()
+else()
+  run_ctest()
+endif()
---
+++ CMake/SlicerDashboardScript.TEMPLATE.cmake
@@ -0,0 +1,126 @@
+#
+# OS: Ubuntu 9.04  2.6.28-19-generic
+# Hardware: x86_64 GNU/Linux
+# GPU: NA
+
+# Note: The specific version and processor type of this machine should be reported in the 
+# header above. Indeed, this file will be send to the dashboard as a NOTE file. 
+
+cmake_minimum_required(VERSION 2.8.2)
+
+#
+# For additional information, see http://www.slicer.org/slicerWiki/index.php/Slicer4:_Dashboard_Setup
+#
+
+#
+# Dashboard properties
+#
+set(MY_OPERATING_SYSTEM   "Linux") # Windows, Linux, Darwin... 
+set(MY_COMPILER           "g++4.4.3")
+set(MY_QT_VERSION         "4.7")
+set(QT_QMAKE_EXECUTABLE   "$ENV{HOME}/Dashboards/Support/qtsdk-2010.05/qt/bin/qmake")
+set(CTEST_SITE            "karakoram.kitware") # for example: mymachine.kitware, mymachine.bwh.harvard.edu, ...
+set(CTEST_DASHBOARD_ROOT  "$ENV{HOME}/Dashboards/")
+set(CTEST_CMAKE_COMMAND   "$ENV{HOME}/Dashboards/Support/cmake-2.8.3-rc3-Linux-i386/bin/cmake")
+# Open a shell and type in "cmake --help" to obtain the proper spelling of the generator
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+
+#
+# Dashboard options
+#
+set(WITH_KWSTYLE FALSE)
+set(WITH_MEMCHECK FALSE)
+set(WITH_COVERAGE FALSE)
+set(WITH_DOCUMENTATION FALSE)
+#set(DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY ) # for example: $ENV{HOME}/Projects/Doxygen
+set(CTEST_BUILD_CONFIGURATION "Release")
+set(CTEST_TEST_TIMEOUT 100)
+set(CTEST_BUILD_FLAGS "-j4") # Use multiple CPU cores to build
+
+# experimental: 
+#     - run_ctest() macro will be called *ONE* time
+#     - binary directory will *NOT* be cleaned
+# continuous: 
+#     - run_ctest() macro will be called EVERY 5 minutes ... 
+#     - binary directory will *NOT* be cleaned
+#     - configure/build will be executed *ONLY* if the repository has been updated
+# nightly: 
+#     - run_ctest() macro will be called *ONE* time
+#     - binary directory *WILL BE* cleaned
+set(SCRIPT_MODE "nightly") # "experimental", "continuous", "nightly"
+
+# You could invoke the script with the following syntax:
+#  ctest -S karakoram_Slicer4_nightly.cmake -V
+
+#
+# Additionnal CMakeCache options
+#
+set(ADDITIONNAL_CMAKECACHE_OPTION "
+  Slicer_USE_QT:BOOL=ON
+  Slicer_USE_PYTHONQT:BOOL=ON
+  Slicer_USE_PYTHONQT_WITH_TCL:BOOL=ON
+  Slicer_BUILD_CLI:BOOL=OFF
+")
+
+# Update the following variable to match the chosen build options. This variable is used to 
+# generate both the build directory and the build name.
+# See http://www.cdash.org/CDash/index.php?project=Slicer4 for examples
+set(BUILD_OPTIONS_STRING "QT${MY_QT_VERSION}-PythonQt-With-Tcl-NoCLI") 
+
+#
+# Project specific properties
+#
+set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer4")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer-build-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}")
+
+# List of test that should be explicitly disabled on this machine
+set(TEST_TO_EXCLUDE_REGEX "")
+
+# set any extra environment variables here
+if(UNIX)
+  set(ENV{DISPLAY} ":0")
+endif()
+
+find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
+find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+find_program(CTEST_SVN_COMMAND NAMES svn)
+
+##########################################
+# WARNING: DO NOT EDIT BEYOND THIS POINT #
+##########################################
+
+set(CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
+
+#
+# Project specific properties
+#
+set(CTEST_PROJECT_NAME "Slicer4")
+set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}")
+
+#
+# Display build info
+#
+message("site name: ${CTEST_SITE}")
+message("build name: ${CTEST_BUILD_NAME}")
+message("script mode: ${SCRIPT_MODE}")
+message("coverage: ${WITH_COVERAGE}, memcheck: ${WITH_MEMCHECK}")
+
+#
+# Convenient macro allowing to download a file
+#
+MACRO(downloadFile url dest)
+  FILE(DOWNLOAD ${url} ${dest} STATUS status)
+  LIST(GET status 0 error_code)
+  LIST(GET status 1 error_msg)
+  IF(error_code)
+    MESSAGE(FATAL_ERROR "error: Failed to download ${url} - ${error_msg}")
+  ENDIF()
+ENDMACRO()
+
+#
+# Download and include dashboard driver script 
+#
+set(url http://svn.slicer.org/Slicer4/trunk/CMake/SlicerDashboardDriverScript.cmake)
+set(dest ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}.driver)
+downloadFile(${url} ${dest})
+INCLUDE(${dest})
