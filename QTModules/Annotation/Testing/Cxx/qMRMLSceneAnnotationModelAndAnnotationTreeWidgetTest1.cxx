e47adf518becba11652061c9eec089d161536191
Daniel Haehn <haehn@bwh.harvard.edu>
2010-12-02 20:04:24 +0000

    ENH: Annotation module: New test for the qMRMLSceneAnnotationModel and the qMRMLSceneAnnotationTreeWidget.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=15358
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@15358 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ QTModules/Annotation/Testing/Cxx/qMRMLSceneAnnotationModelAndAnnotationTreeWidgetTest1.cxx
@@ -0,0 +1,82 @@
+
+// QT includes
+#include <QApplication>
+#include <QDebug>
+#include <QString>
+#include <QStringList>
+#include <QTimer>
+#include <QTreeView>
+
+// CTK includes
+#include <ctkModelTester.h>
+
+// qMRML includes
+#include "qMRMLSceneFactoryWidget.h"
+#include "qMRMLSceneDisplayableModel.h"
+#include "qMRMLUtils.h"
+
+#include "TestingMacros.h"
+#include <vtkEventBroker.h>
+
+#include "GUI/qMRMLAnnotationTreeWidget.h"
+#include "Logic/vtkSlicerAnnotationModuleLogic.h"
+#include "MRML/vtkMRMLAnnotationRulerNode.h"
+
+
+// STD includes
+#include <cstdlib>
+#include <iostream>
+
+// MRML includes
+#include <vtkMRMLDisplayableHierarchyNode.h>
+
+int qMRMLSceneAnnotationModelAndAnnotationTreeWidgetTest1(int argc, char * argv [])
+{
+  QApplication app(argc, argv);
+
+  qMRMLSceneFactoryWidget sceneFactory(0);
+
+  sceneFactory.generateScene();
+
+  qMRMLAnnotationTreeWidget* view = new qMRMLAnnotationTreeWidget(0);
+  //view->setSelectionBehavior(QAbstractItemView::SelectRows);
+
+  vtkSlicerAnnotationModuleLogic* logic = vtkSlicerAnnotationModuleLogic::New();
+
+  logic->SetMRMLScene(sceneFactory.mrmlScene());
+
+  view->setAndObserveLogic(logic);
+  view->setMRMLScene(sceneFactory.mrmlScene());
+  view->hideScene();
+
+  view->show();
+  view->resize(500, 800);
+
+
+  double worldCoordinates1[3] = {0,0,0};
+  double worldCoordinates2[3] = {50,50,50};
+
+  double distance = sqrt(vtkMath::Distance2BetweenPoints(worldCoordinates1,worldCoordinates2));
+
+  vtkMRMLAnnotationRulerNode *rulerNode = vtkMRMLAnnotationRulerNode::New();
+
+  rulerNode->SetPosition1(worldCoordinates1);
+  rulerNode->SetPosition2(worldCoordinates2);
+  rulerNode->SetDistanceMeasurement(distance);
+
+  rulerNode->SetName(sceneFactory.mrmlScene()->GetUniqueNameByString("AnnotationRuler"));
+
+  rulerNode->Initialize(sceneFactory.mrmlScene());
+
+  rulerNode->Delete();
+
+  std::cout << "Measurement in rulerNode: " << rulerNode->GetDistanceMeasurement() << std::endl;
+
+  if (argc < 2 || QString(argv[1]) != "-I" )
+    {
+    QTimer::singleShot(200, &app, SLOT(quit()));
+    }
+
+  return app.exec();
+}
+
