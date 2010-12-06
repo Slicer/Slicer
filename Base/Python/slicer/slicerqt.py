af183332e59492887bfa84b319e5f00e82e4e1f5
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-12-06 02:34:00 +0000

    ENH: slicerqt.py - Add module as attributes of "slicer.modules"
    It means the following will be valid variable:
      slicer.modules.emsegment
      slicer.modules.transforms
      ...
    
    Associated with the auto-completion feature of the python console, this
    change will make the loop-up of module instance easier.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=15409
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@15409 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Python/slicer/slicerqt.py
+++ Base/Python/slicer/slicerqt.py
@@ -56,16 +56,30 @@ def getNodes():
 #
 class _Internal():
   def __init__(self):
-    factoryManager = app().moduleManager().factoryManager()
+    import imp
+    moduleManager = app().moduleManager()
+    factoryManager = moduleManager.factoryManager()
     factoryManager.connect('allModulesRegistered()', self.setSlicerModuleNames)
+    moduleManager.connect('moduleLoaded(qSlicerAbstractCoreModule*)', self.setSlicerModules)
+    
+    # Add module slicer.moduleNames
+    _moduleNames = imp.new_module('moduleNames')
+    setattr(slicer, _moduleNames.__name__, _moduleNames)
+    
+    # Add module slicer.modules
+    _modules = imp.new_module('modules')
+    setattr(slicer, _modules.__name__, _modules)
+    
     
   def setSlicerModuleNames(self):
-    import imp
-    # Add module names as attributes of module slicer.moduleNames
-    _moduleNames = imp.new_module('moduleNames')
+    """Add module names as attributes of module slicer.moduleNames"""
     for name in moduleNames():
-      setattr(_moduleNames, name, name)
-    setattr(slicer, _moduleNames.__name__, _moduleNames)
+      setattr(slicer.moduleNames, name, name)
+    
+  def setSlicerModules(self, module):
+    """Add modules as attributes of module slicer.modules"""
+    setattr(slicer.modules, module.name, module)
+    
     
 _internalInstance = _Internal()
 
