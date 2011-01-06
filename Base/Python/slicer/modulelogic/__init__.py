85058cd2c9c968a7aaaba8d91b4c10830193ba11
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-01-06 21:55:46 +0000

    BUG: Add missing modulelogic/__init__.py python module
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=15740
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@15740 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/Python/slicer/modulelogic/__init__.py
@@ -0,0 +1,35 @@
+""" This module loads the Slicer Module Logic vtk classes into its namespace."""
+
+import glob, os
+
+# Retrieve list of modules
+for fname in glob.glob(__path__[0] + "/vtkSlicer*ModuleLogic.py"):
+
+  # Extract module logic name
+  module_logic_name = os.path.splitext(os.path.basename(fname))[0]
+  
+  # Import logic library
+  exec "import %s" % (module_logic_name)
+  
+  # ... and obtain a reference to the associated module
+  module_logic = eval(module_logic_name)
+  
+  # Loop over content of the python module associated with the 'Module logic library'
+  for item_name in dir(module_logic):
+  
+    # Obtain a reference associated with the current object
+    item = eval("%s.%s" % (module_logic_name, item_name))
+    
+    # Import the logic vtk class if any
+    if type(item).__name__ == 'vtkclass':
+      exec("from %s import %s" % (module_logic_name, item_name))
+    
+    # Removing things the user shouldn't have to see.  
+    del item, item_name
+  
+  # Removing things the user shouldn't have to see.
+  exec("del %s" % (module_logic_name))
+  del module_logic, module_logic_name
+
+# Removing things the user shouldn't have to see.
+del fname, glob, os
