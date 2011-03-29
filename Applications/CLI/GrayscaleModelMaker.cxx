a190e1f1e73ad7d8696deac946bf30b1b2c5d5c2
Julien Finet <julien.finet@kitware.com>
2011-03-29 20:52:20 +0000

    BUG: GrayscaleModelMaker was not working as a shared lib
    Similar to r15634, we can't check if a file exists using fopen if the
    module is run as a shared dll because the volume file name is a memory
    address and not a file path.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16129
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16129 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/CLI/GrayscaleModelMaker.cxx
+++ Applications/CLI/GrayscaleModelMaker.cxx
@@ -83,14 +83,20 @@ int main(int argc, char * argv[])
     vtkXMLPolyDataWriter * writer = NULL;
 
     // check for the input file
-    FILE * infile;
-    infile = fopen(InputVolume.c_str(),"r");
-    if (infile == NULL)
-    {
+    // - strings that start with slicer: are shared memory references, so they won't exist.
+    //   The memory address starts with 0x in linux but not on Windows
+    if ( InputVolume.find(std::string("slicer:")) != 0 )
+      {
+      // check for the input file
+      FILE * infile;
+      infile = fopen(InputVolume.c_str(),"r");
+      if (infile == NULL)
+        {
         std::cerr << "ERROR: cannot open input volume file " << InputVolume << endl;
         return EXIT_FAILURE;
-    }
-    fclose(infile);
+        }
+      fclose(infile);
+      }
 
     // Read the file
     reader = vtkITKArchetypeImageSeriesScalarReader::New();
