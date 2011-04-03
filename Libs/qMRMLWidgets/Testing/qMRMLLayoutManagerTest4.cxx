a6e34ea9ebe76710fd8cd58249638950438605bb
Julien Finet <julien.finet@kitware.com>
2011-04-03 16:14:51 +0000

    ENH: add qMRMLLayoutManagerTest4
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16193
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16193 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/qMRMLWidgets/Testing/qMRMLLayoutManagerTest4.cxx
@@ -0,0 +1,73 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) 2010 Kitware Inc.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Julien Finet, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+// Qt includes
+#include <QApplication>
+#include <QTimer>
+#include <QWidget>
+
+// SlicerQt includes
+#include "qMRMLLayoutManager.h"
+
+// MRML includes
+#include <vtkMRMLScene.h>
+#include <vtkMRMLLayoutLogic.h>
+#include <vtkMRMLLayoutNode.h>
+
+// VTK includes
+#include <vtkSmartPointer.h>
+
+// STD includes
+#include <cstdlib>
+
+// Convenient macro
+#define VTK_CREATE(type, name) \
+  vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
+
+int qMRMLLayoutManagerTest4(int argc, char * argv[] )
+{
+  QApplication app(argc, argv);
+
+  QWidget w;
+  w.show();
+
+  qMRMLLayoutManager layoutManager(&w, &w);
+
+  vtkMRMLScene* scene = vtkMRMLScene::New();
+
+  vtkMRMLLayoutNode* layoutNode = vtkMRMLLayoutNode::New();
+  layoutNode->SetViewArrangement(vtkMRMLLayoutNode::SlicerLayoutOneUpRedSliceView);
+  scene->AddNode(layoutNode);
+  layoutNode->Delete();
+
+  layoutManager.setMRMLScene(scene);
+
+  for (int i = vtkMRMLLayoutNode::SlicerLayoutInitialView;
+       i < vtkMRMLLayoutNode::SlicerLayoutFourOverFourView; ++i)
+    {
+    layoutManager.setLayout(vtkMRMLLayoutNode::SlicerLayoutInitialView);
+    scene->Clear(false);
+    }
+
+  scene->Delete();
+
+  return EXIT_SUCCESS;
+}
+
