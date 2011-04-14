4c189229fa1b8b21d77e29372b9689e7bc542a75
Steve Pieper <pieper@bwh.harvard.edu>
2011-04-14 21:23:48 +0000

    ENH: improved slice testing for timing
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16263
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16263 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Testing/Performance.py
+++ Base/Testing/Performance.py
@@ -4,12 +4,25 @@ import os
 execfile(os.environ['Slicer_HOME'] + "/../../Slicer4/Base/Testing/Performance.py")
 """
 
+global timeSteps
+def timeSteps(iters, f):
+  import time
+  elapsedTime = 0
+  for i in xrange(iters):
+    startTime = time.time() 
+    f()
+    slicer.app.processEvents()
+    endTime = time.time()
+    elapsedTime += (endTime - startTime)
+  fps = iters / elapsedTime
+  print ("fps = %g (%g ms per frame)" % (fps, 1000./fps))
+
 global reslicing
 def reslicing(iters=100):
   """ go into a loop that stresses the reslice performance
   """
   import time
-  sliceNode = getNodes()['redAxial']
+  sliceNode = getNode('vtkMRMLSliceNode1')
   dims = sliceNode.GetDimensions()
   elapsedTime = 0
   for i in xrange(iters):
@@ -22,17 +35,59 @@ def reslicing(iters=100):
     endTime2 = time.time()
     delta = ((endTime1-startTime) + (endTime2 - endTime1)) / 2.
     elapsedTime += delta
-  print ("%d x %d, fps = %g" % (dims[0], dims[1], iters / elapsedTime))
+  fps = iters / elapsedTime
+  print ("%d x %d, fps = %g (%g ms per frame)" % (dims[0], dims[1], fps, 1000./fps))
+  return (dims[0]*dims[1], 1000./fps)
 
 global sizeProbe
 def sizeProbe(iters=10, minSize=500, maxSize=2000, step=100):
+  fp = open('/tmp/size.csv', 'w')
+  fp.write('pixels, ms\n')
   for dim in xrange(minSize, maxSize, step):
     mainWindow().size = qt.QSize(dim,dim)
-    reslicing(iters)
+    sample = reslicing(iters)
+    fp.write('%d,%g\n' % sample)
+  fp.close()
+
     
 global timeProbe
 def timeProbe(iters=10, steps=30):
   for step in xrange(steps):
     reslicing(iters)
+
+global fakewin
+def fakewin():
+
+  global imageViewer2
+  try:
+    imageViewer2
+  except NameError:
+    imageViewer2 = vtk.vtkImageViewer2()
+    imageViewer2.SetInput( slicer.sliceWidgetRed_sliceLogic.GetImageData() )
+
+  import time
+  import random
+  sliceNode = getNode('vtkMRMLSliceNode1')
+  sliceNode.SetSliceOffset( (random.random()*100) )
+  imageViewer2.Render()
+
+
+global sliceLogicTest
+class sliceLogicTest:
+  def __init__(self):
+    self.step = 0
+    self.sliceLogic = slicer.vtkMRMLSliceLayerLogic()
+    self.sliceLogic.SetName('TestingSliceLogic')
+    self.sliceLogic.SetMRMLScene(slicer.mrmlScene)
+    self.sliceNode = slicer.vtkMRMLSliceNode()
+    slicer.mrmlScene.AddNode(self.sliceNode)
+    self.sliceLogic.Initialize(self.sliceNode)
+
+  def stepSliceLogic(self):
+    self.sliceNodeLogic.setSliceOffsetValue( -1*step*10)
+    step = 1^step
+
+  def testSliceLogic(self, iters):
+    timeSteps(iters, stepSliceLogic)
 
 
