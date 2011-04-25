2cab1c9e2d7b40e77ff9c2465b7e9132dc9f93f3
Julien Finet <julien.finet@kitware.com>
2011-04-25 22:06:08 +0000

    ENH: Add tests to qSlicerModulePanel
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16322
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16322 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/QTCoreModules/Testing/qSlicerModulePanelTest2.cxx
@@ -0,0 +1,82 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) 2010 Kitware Inc.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Julien Finet, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+// Qt includes
+#include <QApplication>
+#include <QDockWidget>
+#include <QTimer>
+#include <QHBoxLayout>
+// SlicerQt includes
+#include "qSlicerApplication.h"
+#include "qSlicerCoreModuleFactory.h"
+#include "qSlicerModuleManager.h"
+#include "qSlicerModuleFactoryManager.h"
+#include "qSlicerModulePanel.h"
+#include <qSlicerStyle.h>
+
+// STD includes
+#include <cstdlib>
+#include <QMainWindow>
+int qSlicerModulePanelTest2(int argc, char * argv[] )
+{
+  QApplication::setDesktopSettingsAware(false);
+  QApplication::setStyle(new qSlicerStyle);
+  qSlicerApplication app(argc, argv);
+  bool exitWhenDone;
+  app.initialize(exitWhenDone);
+
+  // Register core module factories for testing purpose
+  qSlicerCoreModuleFactory* coreModuleFactory = new qSlicerCoreModuleFactory();
+  app.moduleManager()->factoryManager()->registerFactory("qSlicerCoreModuleFactory", coreModuleFactory);
+  app.moduleManager()->factoryManager()->registerAllModules();
+  app.moduleManager()->factoryManager()->instantiateAllModules();
+  app.moduleManager()->loadAllModules();
+
+  QDockWidget dockWidget;
+  qSlicerModulePanel modulePanel(&dockWidget);
+  dockWidget.setWidget(&modulePanel);
+  //QHBoxLayout* hbox = new QHBoxLayout;
+  //hbox->addWidget(&modulePanel);
+  //dockWidget.setLayout(hbox);
+
+  modulePanel.setModuleManager(app.moduleManager());
+
+  modulePanel.setModule("colors");
+  if (modulePanel.currentModuleName() != "colors")
+    {
+    std::cerr<< "qSlicerModulePanel::setModule() failed "
+             << qPrintable(modulePanel.currentModuleName()) << std::endl;
+    return EXIT_FAILURE;
+    }
+  //modulePanel.setModule("transforms");
+  //modulePanel.show();
+  //dockWidget.show();
+  QMainWindow mainWindow;
+  mainWindow.addDockWidget(Qt::LeftDockWidgetArea, &dockWidget);
+  mainWindow.show();
+
+  if (argc < 2 || QString(argv[1]) != "-I")
+    {
+    QTimer::singleShot(100, qApp, SLOT(quit()));
+    }
+
+  return app.exec();
+}
+
---
+++ Base/QTGUI/Testing/qSlicerModulePanelTest1.cxx
@@ -0,0 +1,57 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) 2010 Kitware Inc.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Julien Finet, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+// Qt includes
+#include <QApplication>
+#include <QTimer>
+
+// SlicerQt includes
+#include "qSlicerApplication.h"
+#include "qSlicerModuleManager.h"
+#include "qSlicerModulePanel.h"
+
+// STD includes
+#include <cstdlib>
+
+int qSlicerModulePanelTest1(int argc, char * argv[] )
+{
+  qSlicerApplication app(argc, argv);
+  bool exitWhenDone;
+  app.initialize(exitWhenDone);
+
+  qSlicerModulePanel modulePanel;
+  modulePanel.setModuleManager(app.moduleManager());
+
+  if (modulePanel.moduleManager() != app.moduleManager())
+    {
+    std::cerr << "qSlicerModulePanel::setModuleManager() failed" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  modulePanel.show();
+
+  if (argc < 2 || QString(argv[1]) != "-I")
+    {
+    QTimer::singleShot(100, qApp, SLOT(quit()));
+    }
+
+  return app.exec();
+}
+
