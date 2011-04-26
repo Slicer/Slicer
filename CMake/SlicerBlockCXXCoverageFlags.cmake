9b70e2f5006cca0d00e38090a872a62168e9c411
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-04-26 08:29:42 +0000

    COMP: Factor out code related to C/CXX flags to that it could be re-used within extension
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16373
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16373 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ CMake/SlicerBlockCXXCoverageFlags.cmake
@@ -0,0 +1,37 @@
+################################################################################
+#
+#  Program: 3D Slicer
+#
+#  Copyright (c) 2010 Kitware Inc.
+#
+#  See Doc/copyright/copyright.txt
+#  or http://www.slicer.org/copyright/copyright.txt for details.
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+#  and was partially funded by NIH grant 3P41RR013218-12S1
+#
+################################################################################
+
+#
+# Coverage flags
+#
+
+#
+# Usage: If WITH_COVERAGE variable is True, the flags required to compute code coverage
+#        will be set into COVERAGE_CXX_FLAGS and COVERAGE_C_FLAGS variables.
+#
+
+
+IF(WITH_COVERAGE)
+  IF("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    SET(coverage_flags "-g -fprofile-arcs -ftest-coverage -O0 -DNDEBUG")
+    SET(COVERAGE_CXX_FLAGS ${coverage_flags})
+    SET(COVERAGE_C_FLAGS ${coverage_flags})
+  ENDIF()
+ENDIF()
---
+++ CMake/SlicerBlockCXXRequiredFlags.cmake
@@ -0,0 +1,68 @@
+################################################################################
+#
+#  Program: 3D Slicer
+#
+#  Copyright (c) 2010 Kitware Inc.
+#
+#  See Doc/copyright/copyright.txt
+#  or http://www.slicer.org/copyright/copyright.txt for details.
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+#  and was partially funded by NIH grant 3P41RR013218-12S1
+#
+################################################################################
+
+#
+# Required C/CXX flags
+#
+
+#
+# Usage: The variable Slicer_REQUIRED_C_FLAGS and Slicer_REQUIRED_CXX_FLAGS 
+#        will set appropriately.
+#
+
+IF(NOT DEFINED Slicer_REQUIRED_C_FLAGS} OR NOT DEFINED Slicer_REQUIRED_CXX_FLAGS)
+  
+  INCLUDE(SlicerFunctionCheckCompilerFlags)
+  INCLUDE(SlicerFunctionGetGccVersion)
+
+  SET(tmp_c_flags)
+  SET(tmp_cxx_flags)
+  
+  IF(CMAKE_COMPILER_IS_GNUCXX)
+    SET(cflags "-Wall -Wextra -Wpointer-arith -Winvalid-pch -Wcast-align -Wwrite-strings -D_FORTIFY_SOURCE=2")
+    SlicerFunctionCheckCompilerFlags("-fdiagnostics-show-option" cflags)
+    SlicerFunctionCheckCompilerFlags("-Wl,--no-undefined" cflags)
+    
+    slicerFunctionGetGccVersion(${CMAKE_CXX_COMPILER} GCC_VERSION)
+    # With older version of gcc supporting the flag -fstack-protector-all, an extra dependency to libssp.so
+    # is introduced. If gcc is smaller than 4.4.0 and the build type is Release let's not include the flag.
+    # Doing so should allow to build package made for distribution using older linux distro.
+    IF(GCC_VERSION VERSION_GREATER "4.4.0" OR (CMAKE_BUILD_TYPE STREQUAL "Debug" AND GCC_VERSION VERSION_LESS "4.4.0"))
+      SlicerFunctionCheckCompilerFlags("-fstack-protector-all" cflags)
+    ENDIF()
+    IF(MINGW)
+      # suppress warnings about auto imported symbols
+      SET(tmp_cxx_flags "-Wl,--enable-auto-import ${tmp_cxx_flags}")
+    ENDIF()
+
+    # Note: -Wold-style-cast is too verbose
+    #       Let's postpone the use of -Wsign-promo 
+    SET(tmp_c_flags "${cflags} ${tmp_c_flags}")
+    SET(tmp_cxx_flags "${cflags} -Wno-deprecated -Woverloaded-virtual -Wstrict-null-sentinel ${tmp_cxx_flags}")
+  ELSEIF(MSVC)
+    SET(cflags "/Zm1000 /W3")
+    SET(tmp_c_flags "${cflags} ${tmp_c_flags}")
+    SET(tmp_cxx_flags "${cflags} /EHsc /GR ${tmp_cxx_flags}")
+  ENDIF()
+  
+  SET(Slicer_REQUIRED_C_FLAGS ${tmp_c_flags})
+  SET(Slicer_REQUIRED_CXX_FLAGS ${tmp_cxx_flags})
+  
+ENDIF()
