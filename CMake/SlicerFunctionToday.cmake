fbfbfada4a63be841506378b8960253926dd6a0d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-04-26 08:27:50 +0000

    COMP: Change SlicerMacroToday into SlicerFunctionToday
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16328
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16328 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerFunctionToday.cmake
+++ CMake/SlicerFunctionToday.cmake
@@ -6,30 +6,33 @@
 #
 
 
-MACRO (TODAY RESULT)
+FUNCTION (TODAY RESULT_VAR)
+  
+  set(today_date)
   IF (WIN32)
     EXECUTE_PROCESS(COMMAND cmd /c "date /T"
                     ERROR_VARIABLE getdate_error
                     RESULT_VARIABLE getdate_result
-                    OUTPUT_VARIABLE ${RESULT}
+                    OUTPUT_VARIABLE today_date
                     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
     #MESSAGE(STATUS "getdate_error:${getdate_error}")
     #MESSAGE(STATUS "getdate_result:${getdate_result}")
-    #MESSAGE(STATUS "${RESULT}:${${RESULT}}")
+    #MESSAGE(STATUS "today_date:${today_date}")
 
     string(REGEX REPLACE ".*(..)/(..)/(....)"
                           "\\3-\\1-\\2"
-                          ${RESULT}
-                          ${${RESULT}})
+                          today_date
+                          ${today_date})
   ELSEIF(UNIX)
     EXECUTE_PROCESS(COMMAND date "+%Y-%m-%d"
-                    OUTPUT_VARIABLE ${RESULT}
+                    OUTPUT_VARIABLE today_date
                     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   ELSE (WIN32)
-    MESSAGE(SEND_ERROR "error: Failed to obtain today date - ${RESULT} set to 0000-00-00")
-    SET(${RESULT} 0000-00-00)
+    MESSAGE(SEND_ERROR "error: Failed to obtain today date - ${RESULT_VAR} set to 0000-00-00")
+    SET(today_date "0000-00-00")
   ENDIF (WIN32)
-ENDMACRO (TODAY)
+  SET(${RESULT_VAR} ${today_date} PARENT_SCOPE)
+ENDFUNCTION (TODAY)
 
