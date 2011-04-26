926feceaf5fcdb780244a7d0dfcb10838d8f1e36
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-04-26 08:27:57 +0000

    COMP: GenerateModuleDescriptionParserConfig properly configure LIBRARY_DIRS variable
    It was using ModuleDescriptionParser_BINARY_DIR, this didn't work in case
    a project including the library was setting ARCHIVE_OUTPUT_DIRECTORY
    or LIBRARY_OUTPUT_DIRECTORY properties.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16332
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16332 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/SlicerExecutionModel/ModuleDescriptionParser/GenerateModuleDescriptionParserConfig.cmake
+++ Libs/SlicerExecutionModel/ModuleDescriptionParser/GenerateModuleDescriptionParserConfig.cmake
@@ -8,9 +8,17 @@ set(ModuleDescriptionParser_INCLUDE_DIRS_CONFIG
   ${ModuleDescriptionParser_BINARY_DIR} 
   ${ModuleDescriptionParser_SOURCE_DIR}
   )
+
+get_target_property(ModuleDescriptionParser_ARCHIVE_OUTPUT_DIRECTORY 
+  ${lib_name} ARCHIVE_OUTPUT_DIRECTORY)
+get_target_property(ModuleDescriptionParser_LIBRARY_OUTPUT_DIRECTORY 
+  ${lib_name} LIBRARY_OUTPUT_DIRECTORY)
+
 set(ModuleDescriptionParser_LIBRARY_DIRS_CONFIG 
-  ${ModuleDescriptionParser_BINARY_DIR}
+  ${ModuleDescriptionParser_ARCHIVE_OUTPUT_DIRECTORY}
+  ${ModuleDescriptionParser_LIBRARY_OUTPUT_DIRECTORY}
   )
+
 set(ModuleDescriptionParser_USE_FILE_CONFIG 
   ${ModuleDescriptionParser_BINARY_DIR}/UseModuleDescriptionParser.cmake
   )
