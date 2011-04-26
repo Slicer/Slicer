1705d32807df3fa213a7d84ca589dd3aac1e2459
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-04-26 08:29:37 +0000

    ENH: Factor out CMake code used to check platform into SlicerBlockPlatformCheck.cmake
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16370
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16370 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ CMake/SlicerBlockPlatformCheck.cmake
@@ -0,0 +1,45 @@
+################################################################################
+#
+#  Program: 3D Slicer
+#
+#  Copyright (c) 2010 Kitware Inc.
+#
+#  See Doc/copyright/copyright.txt
+#  or http://www.slicer.org/copyright/copyright.txt for details.
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+#  and was partially funded by NIH grant 3P41RR013218-12S1
+#
+################################################################################
+
+#
+# Slicer Platform check
+#
+
+#
+# Usage:
+#   The check can be enabled/disabled by setting Slicer_PLATFORM_CHECK to 
+#   True/False before including the block.
+#
+
+IF(NOT DEFINED Slicer_PLATFORM_CHECK)
+  SET(Slicer_PLATFORM_CHECK true)
+ENDIF()
+
+if(Slicer_PLATFORM_CHECK)
+  # See CMake/Modules/Platform/Darwin.cmake)
+  #   6.x == Mac OSX 10.2 (Jaguar)
+  #   7.x == Mac OSX 10.3 (Panther)
+  #   8.x == Mac OSX 10.4 (Tiger)
+  #   9.x == Mac OSX 10.5 (Leopard)
+  #  10.x == Mac OSX 10.6 (Snow Leopard)
+  if (DARWIN_MAJOR_VERSION LESS "8")
+    message(FATAL_ERROR "Only Mac OSX >= 10.4 are supported !")
+  endif()
+endif()
