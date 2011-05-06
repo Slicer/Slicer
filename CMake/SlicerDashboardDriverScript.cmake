5b2be4bdd9bce2add5e5ece7dc51a4c7fd36c016
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-05-06 23:43:17 +0000

    COMP: Revert r16502 - See message body for details.
    In fact, submitting an experimental dashboard from a folder where a
    nightly already happened should be done only after the Testing folder
    in the build directory has been removed.
    
    Otherwise, the experimental submission re-use a Nightly submission tag.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16503
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16503 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerDashboardDriverScript.cmake
+++ CMake/SlicerDashboardDriverScript.cmake
@@ -104,10 +104,7 @@ set(CTEST_SOURCE_DIRECTORY "${CTEST_SOURCE_DIRECTORY}")
 #
 MACRO(run_ctest)
   ctest_start(${model})
-  set(FILES_UPDATED 0)
-  if(NOT SCRIPT_MODE STREQUAL "experimental")
-    ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" RETURN_VALUE FILES_UPDATED)
-  endif()
+  ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" RETURN_VALUE FILES_UPDATED)
 
   # force a build if this is the first run and the build dir is empty
   if(NOT EXISTS "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt")
