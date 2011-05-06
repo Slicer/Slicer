bd6bd5891390cbd430ecdf96edf693aeea55832b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-05-06 19:39:24 +0000

    COMP: Add Slicer_EXTERNAL_PROJECTS to SlicerConfig
    This variable will contain the list of external project that are expected
    to be able to build either slicer extension or command line module.
    
    Within:
     - SlicerConfig the associated find_package(<project_name>) are called
     - UseSlicer, the associated include(${<project_name>_USE_FILE}) are called
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16465
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16465 3bd1e089-480b-0410-8dfb-8563597acbee

--- GenerateSlicerConfig.cmake
+++ GenerateSlicerConfig.cmake
@@ -27,11 +27,6 @@ SET(Slicer_README_FILE_CONFIG ${Slicer_SOURCE_DIR}/README.txt)
 # Path to extension CPack script
 set(Slicer_EXTENSION_CPACK_CONFIG ${Slicer_SOURCE_DIR}/SlicerExtensionCPack.cmake)
 
-# Slicer/Libs
-SET(TCLAP_DIR_CONFIG ${Slicer_BINARY_DIR}/Libs/SlicerExecutionModel/tclap)
-SET(ModuleDescriptionParser_DIR_CONFIG ${Slicer_BINARY_DIR}/Libs/SlicerExecutionModel/ModuleDescriptionParser)
-SET(GenerateCLP_DIR_CONFIG ${Slicer_BINARY_DIR}/Libs/SlicerExecutionModel/GenerateCLP)
-
 SET(Slicer_Libs_INCLUDE_DIRS_CONFIG ${Slicer_Libs_INCLUDE_DIRS})
 SET(Slicer_Libs_LIBRARY_DIRS_CONFIG ${Slicer_BINARY_DIR}/bin ${Slicer_BINARY_DIR}/lib)
 
@@ -45,8 +40,15 @@ SET(Slicer_ModuleLogic_LIBRARY_DIRS_CONFIG ${CMAKE_BINARY_DIR}/${Slicer_INSTALL_
 # Qt
 SET(QT_QMAKE_EXECUTABLE_CONFIG ${QT_QMAKE_EXECUTABLE})
 
+# Slicer libraries
+SET(TCLAP_DIR_CONFIG ${Slicer_BINARY_DIR}/Libs/SlicerExecutionModel/tclap)
+SET(ModuleDescriptionParser_DIR_CONFIG ${Slicer_BINARY_DIR}/Libs/SlicerExecutionModel/ModuleDescriptionParser)
+SET(GenerateCLP_DIR_CONFIG ${Slicer_BINARY_DIR}/Libs/SlicerExecutionModel/GenerateCLP)
+
 # External project
-SET(BatchMake_DIR_CONFIG ${BatchMake_DIR})
+if(Slicer_USE_BatchMake)
+  SET(BatchMake_DIR_CONFIG ${BatchMake_DIR})
+endif()
 SET(CTK_DIR_CONFIG ${CTK_DIR})
 SET(ITK_DIR_CONFIG ${ITK_DIR})
 SET(OpenCV_DIR_CONFIG ${OpenCV_DIR})
@@ -54,6 +56,12 @@ SET(PYTHON_INCLUDE_PATH_CONFIG ${PYTHON_INCLUDE_PATH})
 SET(PYTHON_LIBRARIES_CONFIG ${PYTHON_LIBRARIES})
 SET(Teem_DIR_CONFIG ${Teem_DIR})
 SET(VTK_DIR_CONFIG ${VTK_DIR})
+
+# List all required external project
+SET(Slicer_EXTERNAL_PROJECTS_CONFIG CTK GenerateCLP ITK Teem VTK)
+IF(Slicer_USE_BatchMake)
+  LIST(APPEND Slicer_EXTERNAL_PROJECTS_CONFIG BatchMake)
+ENDIF()
 
 # Configure SlicerConfig.cmake for the install tree.
 CONFIGURE_FILE(
@@ -67,4 +75,4 @@ CONFIGURE_FILE(
 # Configure SlicerConfig.cmake for the install tree.      
 CONFIGURE_FILE(
   ${Slicer_SOURCE_DIR}/SlicerInstallConfig.cmake.in      
-  ${Slicer_BINARY_DIR}/Utilities/SlicerConfig.cmake @ONLY)      
+  ${Slicer_BINARY_DIR}/Utilities/SlicerConfig.cmake @ONLY)
