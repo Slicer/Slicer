12f60d9c2b2a9ebb4acb21fcfbf9e9424e112a48
Daniel Haehn <haehn@bwh.harvard.edu>
2011-05-10 16:08:31 +0000

    COMP: AtlasCreator:
     - added the vtkMRMLAtlasCreatorNode from Slicer3 to replace the one here
     - since the AtlasCreator module here was just a stub (copied from the GAD module), this should not hurt anybody
     - the new AtlasCreator node is required to compile EMSegment4
    
    
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16515
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16515 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/AtlasCreator/Logic/vtkMRMLAtlasCreatorNode.cxx
+++ Modules/AtlasCreator/Logic/vtkMRMLAtlasCreatorNode.cxx
@@ -6,7 +6,7 @@ See Doc/copyright/copyright.txt
 or http://www.slicer.org/copyright/copyright.txt for details.
 
 Program:   3D Slicer
-Module:    $RCSfile: vtkMRMLAtlasCreatorNode.cxx,v $
+Module:    $RCSfile: vtkMRMLGradientAnisotropicDiffusionFilterNode.cxx,v $
 Date:      $Date: 2006/03/17 15:10:10 $
 Version:   $Revision: 1.2 $
 
@@ -52,20 +52,128 @@ vtkMRMLNode* vtkMRMLAtlasCreatorNode::CreateNodeInstance()
 //----------------------------------------------------------------------------
 vtkMRMLAtlasCreatorNode::vtkMRMLAtlasCreatorNode()
 {
-   this->Conductance = 1.0;
-   this->NumberOfIterations = 1;
-   this->TimeStep = 0.1;
-   this->InputVolumeRef = NULL;
-   this->OutputVolumeRef = NULL;
-   this->HideFromEditors = true;
+
+  this->OriginalImagesFilePathList = 0;
+  this->SegmentationsFilePathList = 0;
+  this->OutputDirectory = 0;
+
+  this->Toolkit = 0;
+
+  this->TemplateType = 0;
+  this->DynamicTemplateIterations = 0;
+  this->FixedTemplateDefaultCaseFilePath = 0;
+  this->IgnoreTemplateSegmentation = 0;
+
+  this->LabelsList = 0;
+
+  this->RegistrationType = 0;
+
+  this->SaveTransforms = 0;
+  this->DeleteAlignedImages = 0;
+  this->DeleteAlignedSegmentations = 0;
+  this->NormalizeAtlases = 0;
+  this->NormalizeTo = 0;
+
+  this->OutputCast = 0;
+
+  this->PCAAnalysis = 0;
+  this->PCAMaxEigenVectors = 0;
+  this->PCACombine = 0;
+
+  this->UseCluster = 0;
+  this->SchedulerCommand = 0;
+
+  this->NumberOfThreads = 0;
+
+  this->SkipRegistration = 0;
+  this->ExistingTemplate = 0;
+  this->TransformsDirectory = 0;
+
+  this->DebugMode = 0;
+  this->DryrunMode = 0;
+
+  this->TestMode = 0;
+
 }
 
 //----------------------------------------------------------------------------
 vtkMRMLAtlasCreatorNode::~vtkMRMLAtlasCreatorNode()
 {
-   this->SetInputVolumeRef( NULL );
-   this->SetOutputVolumeRef( NULL );
+
+  if (this->OriginalImagesFilePathList)
+    {
+    delete [] this->OriginalImagesFilePathList;
+    this->OriginalImagesFilePathList = 0;
+    }
+
+  if (this->SegmentationsFilePathList)
+    {
+    delete [] this->SegmentationsFilePathList;
+    this->SegmentationsFilePathList = NULL;
+    }
+
+  if (this->OutputDirectory)
+    {
+    delete [] this->OutputDirectory;
+    this->OutputDirectory = 0;
+    }
+
+  if (this->Toolkit)
+    {
+    delete [] this->Toolkit;
+    this->Toolkit = 0;
+    }
+
+  if (this->TemplateType)
+    {
+    delete [] this->TemplateType;
+    this->TemplateType = 0;
+    }
+
+  if (this->FixedTemplateDefaultCaseFilePath)
+    {
+    delete [] this->FixedTemplateDefaultCaseFilePath;
+    this->FixedTemplateDefaultCaseFilePath = 0;
+    }
+
+  if (this->LabelsList)
+    {
+    delete [] this->LabelsList;
+    this->LabelsList = 0;
+    }
+
+  if (this->RegistrationType)
+    {
+    delete [] this->RegistrationType;
+    this->RegistrationType = 0;
+    }
+
+  if (this->OutputCast)
+    {
+    delete [] this->OutputCast;
+    this->OutputCast = 0;
+    }
+
+  if (this->SchedulerCommand)
+    {
+    delete [] this->SchedulerCommand;
+    this->SchedulerCommand = 0;
+    }
+
+  if (this->ExistingTemplate)
+    {
+    delete [] this->ExistingTemplate;
+    this->ExistingTemplate = 0;
+    }
+
+  if (this->TransformsDirectory)
+    {
+    delete [] this->TransformsDirectory;
+    this->TransformsDirectory = 0;
+    }
+
 }
+
 
 //----------------------------------------------------------------------------
 void vtkMRMLAtlasCreatorNode::WriteXML(ostream& of, int nIndent)
@@ -76,37 +184,99 @@ void vtkMRMLAtlasCreatorNode::WriteXML(ostream& of, int nIndent)
 
   vtkIndent indent(nIndent);
 
-  {
-    std::stringstream ss;
-    ss << this->Conductance;
-    of << indent << " Conductance=\"" << ss.str() << "\"";
-  }
-  {
-    std::stringstream ss;
-    ss << this->NumberOfIterations;
-    of << indent << " NumberOfIterations=\"" << ss.str() << "\"";
-  }
-  {
-    std::stringstream ss;
-    ss << this->TimeStep;
-    of << indent << " TimeStep=\"" << ss.str() << "\"";
-  }
-  {
-    std::stringstream ss;
-    if ( this->InputVolumeRef )
-      {
-      ss << this->InputVolumeRef;
-      of << indent << " InputVolumeRef=\"" << ss.str() << "\"";
-     }
-  }
-  {
-    std::stringstream ss;
-    if ( this->OutputVolumeRef )
-      {
-      ss << this->OutputVolumeRef;
-      of << indent << " OutputVolumeRef=\"" << ss.str() << "\"";
-      }
-  }
+  if (this->OriginalImagesFilePathList != 0)
+    {
+    of << " OriginalImagesFilePathList =\"" << this->OriginalImagesFilePathList << "\"";
+    }
+
+  if (this->SegmentationsFilePathList != 0)
+    {
+    of << " SegmentationsFilePathList =\"" << this->SegmentationsFilePathList << "\"";
+    }
+
+  if (this->OutputDirectory != 0)
+    {
+    of << " OutputDirectory =\"" << this->OutputDirectory << "\"";
+    }
+
+
+  if (this->Toolkit != 0)
+    {
+    of << " Toolkit =\"" << this->Toolkit << "\"";
+    }
+
+
+  if (this->TemplateType != 0)
+    {
+    of << " TemplateType =\"" << this->TemplateType << "\"";
+    }
+
+  of << " DynamicTemplateIterations =\"" << this->DynamicTemplateIterations << "\"";
+
+  if (this->FixedTemplateDefaultCaseFilePath != 0)
+    {
+    of << " FixedTemplateDefaultCaseFilePath =\"" << this->FixedTemplateDefaultCaseFilePath << "\"";
+    }
+
+  of << " IgnoreTemplateSegmentation =\"" << this->IgnoreTemplateSegmentation << "\"";
+
+
+  if (this->LabelsList != 0)
+    {
+    of << " LabelsList =\"" << this->LabelsList << "\"";
+    }
+
+
+  if (this->RegistrationType != 0)
+    {
+    of << " RegistrationType =\"" << this->RegistrationType << "\"";
+    }
+
+
+  of << " SaveTransforms =\"" << this->SaveTransforms << "\"";
+  of << " DeleteAlignedImages =\"" << this->DeleteAlignedImages << "\"";
+  of << " DeleteAlignedSegmentations =\"" << this->DeleteAlignedSegmentations << "\"";
+  of << " NormalizeAtlases =\"" << this->NormalizeAtlases << "\"";
+  of << " NormalizeTo =\"" << this->NormalizeTo << "\"";
+
+
+  if (this->OutputCast != 0)
+    {
+    of << " OutputCast =\"" << this->OutputCast << "\"";
+    }
+
+  of << " PCAAnalysis =\"" << this->PCAAnalysis << "\"";
+  of << " PCAMaxEigenVectors =\"" << this->PCAMaxEigenVectors << "\"";
+  of << " PCACombine =\"" << this->PCACombine << "\"";
+
+
+  of << " UseCluster =\"" << this->UseCluster << "\"";
+
+  if (this->SchedulerCommand != 0)
+    {
+    of << " SchedulerCommand =\"" << this->SchedulerCommand << "\"";
+    }
+
+
+  of << " NumberOfThreads =\"" << this->NumberOfThreads << "\"";
+
+
+  of << " SkipRegistration =\"" << this->SkipRegistration << "\"";
+
+  if (this->ExistingTemplate != 0)
+    {
+    of << " ExistingTemplate =\"" << this->ExistingTemplate << "\"";
+    }
+
+  if (this->TransformsDirectory != 0)
+    {
+    of << " TransformsDirectory =\"" << this->TransformsDirectory << "\"";
+    }
+
+
+  of << " DebugMode =\"" << this->DebugMode << "\"";
+  of << " DryrunMode =\"" << this->DryrunMode << "\"";
+
 }
 
 //----------------------------------------------------------------------------
@@ -114,41 +284,249 @@ void vtkMRMLAtlasCreatorNode::ReadXMLAttributes(const char** atts)
 {
   vtkMRMLNode::ReadXMLAttributes(atts);
 
-  // Read all MRML node attributes from two arrays of names and values
   const char* attName;
   const char* attValue;
   while (*atts != NULL) 
     {
     attName = *(atts++);
     attValue = *(atts++);
-    if (!strcmp(attName, "Conductance")) 
+
+    if ( !strcmp(attName, "OriginalImagesFilePathList") )
+      {
+      this->SetOriginalImagesFilePathList( attValue );
+      }
+
+    if ( !strcmp(attName, "SegmentationsFilePathList") )
+      {
+      this->SetSegmentationsFilePathList( attValue );
+      }
+
+    if ( !strcmp(attName, "OutputDirectory") )
+      {
+      this->SetOutputDirectory( attValue );
+      }
+
+
+    if ( !strcmp(attName, "Toolkit") )
+      {
+      this->SetToolkit( attValue );
+      }
+
+
+    if ( !strcmp(attName, "TemplateType") )
+      {
+      this->SetTemplateType( attValue );
+      }
+
+    if ( !strcmp(attName, "DynamicTemplateIterations") )
       {
       std::stringstream ss;
       ss << attValue;
-      ss >> this->Conductance;
+
+      int val;
+      ss >> val;
+
+      this->SetDynamicTemplateIterations(val);
       }
-    else if (!strcmp(attName, "NumberOfIterations")) 
+
+    if ( !strcmp(attName, "FixedTemplateDefaultCaseFilePath") )
+      {
+      this->SetFixedTemplateDefaultCaseFilePath( attValue );
+      }
+
+    if ( !strcmp(attName, "IgnoreTemplateSegmentation") )
       {
       std::stringstream ss;
       ss << attValue;
-      ss >> this->NumberOfIterations;
+
+      int val;
+      ss >> val;
+
+      this->SetIgnoreTemplateSegmentation(val);
       }
-    else if (!strcmp(attName, "TimeStep")) 
+
+
+    if ( !strcmp(attName, "LabelsList") )
+      {
+      this->SetLabelsList( attValue );
+      }
+
+
+    if ( !strcmp(attName, "RegistrationType") )
+      {
+      this->SetRegistrationType( attValue );
+      }
+
+
+    if ( !strcmp(attName, "SaveTransforms") )
       {
       std::stringstream ss;
       ss << attValue;
-      ss >> this->TimeStep;
+
+      int val;
+      ss >> val;
+
+      this->SetSaveTransforms(val);
       }
-    else if (!strcmp(attName, "InputVolumeRef"))
+
+    if ( !strcmp(attName, "DeleteAlignedImages") )
       {
-      this->SetInputVolumeRef(attValue);
-      this->Scene->AddReferencedNodeID(this->InputVolumeRef, this);
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetDeleteAlignedImages(val);
       }
-    else if (!strcmp(attName, "OutputVolumeRef"))
+
+    if ( !strcmp(attName, "DeleteAlignedSegmentations") )
       {
-      this->SetOutputVolumeRef(attValue);
-      this->Scene->AddReferencedNodeID(this->OutputVolumeRef, this);
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetDeleteAlignedSegmentations(val);
       }
+
+    if ( !strcmp(attName, "NormalizeAtlases") )
+      {
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetNormalizeAtlases(val);
+      }
+
+    if ( !strcmp(attName, "NormalizeTo") )
+      {
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetNormalizeTo(val);
+      }
+
+
+    if ( !strcmp(attName, "OutputCast") )
+      {
+      this->SetOutputCast( attValue );
+      }
+
+
+    if ( !strcmp(attName, "PCAAnalysis") )
+      {
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetPCAAnalysis(val);
+      }
+
+    if ( !strcmp(attName, "PCAMaxEigenVectors") )
+      {
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetPCAMaxEigenVectors(val);
+      }
+
+    if ( !strcmp(attName, "PCACombine") )
+      {
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetPCACombine(val);
+      }
+
+
+    if ( !strcmp(attName, "UseCluster") )
+      {
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetUseCluster(val);
+      }
+
+    if ( !strcmp(attName, "SchedulerCommand") )
+      {
+      this->SetSchedulerCommand( attValue );
+      }
+
+
+    if ( !strcmp(attName, "NumberOfThreads") )
+      {
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetNumberOfThreads(val);
+      }
+
+
+    if ( !strcmp(attName, "SkipRegistration") )
+      {
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetSkipRegistration(val);
+      }
+
+    if ( !strcmp(attName, "ExistingTemplate") )
+      {
+      this->SetExistingTemplate( attValue );
+      }
+
+    if ( !strcmp(attName, "TransformsDirectory") )
+      {
+      this->SetTransformsDirectory( attValue );
+      }
+
+
+    if ( !strcmp(attName, "DebugMode") )
+      {
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetDebugMode(val);
+      }
+
+    if ( !strcmp(attName, "DryrunMode") )
+      {
+      std::stringstream ss;
+      ss << attValue;
+
+      int val;
+      ss >> val;
+
+      this->SetDryrunMode(val);
+      }
+
     }
 }
 
@@ -160,37 +538,142 @@ void vtkMRMLAtlasCreatorNode::Copy(vtkMRMLNode *anode)
   Superclass::Copy(anode);
   vtkMRMLAtlasCreatorNode *node = (vtkMRMLAtlasCreatorNode *) anode;
 
-  this->SetConductance(node->Conductance);
-  this->SetNumberOfIterations(node->NumberOfIterations);
-  this->SetTimeStep(node->TimeStep);
-  this->SetInputVolumeRef(node->InputVolumeRef);
-  this->SetOutputVolumeRef(node->OutputVolumeRef);
+  this->SetOriginalImagesFilePathList( node->GetOriginalImagesFilePathList() );
+  this->SetSegmentationsFilePathList( node->GetSegmentationsFilePathList() );
+  this->SetOutputDirectory( node->GetOutputDirectory() );
+
+  this->SetToolkit( node->GetToolkit() );
+
+  this->SetTemplateType(node->GetTemplateType());
+  this->SetDynamicTemplateIterations( node->GetDynamicTemplateIterations() );
+  this->SetFixedTemplateDefaultCaseFilePath( node->GetFixedTemplateDefaultCaseFilePath() );
+  this->SetIgnoreTemplateSegmentation( node->GetIgnoreTemplateSegmentation() );
+
+  this->SetLabelsList( node->GetLabelsList() );
+
+  this->SetRegistrationType( node->GetRegistrationType());
+
+  this->SetSaveTransforms( node->GetSaveTransforms());
+  this->SetDeleteAlignedImages( node->GetDeleteAlignedImages());
+  this->SetDeleteAlignedSegmentations( node->GetDeleteAlignedSegmentations());
+  this->SetNormalizeAtlases( node->GetNormalizeAtlases());
+  this->SetNormalizeTo( node->GetNormalizeTo());
+
+  this->SetOutputCast( node->GetOutputCast());
+
+  this->SetPCAAnalysis( node->GetPCAAnalysis());
+  this->SetPCAMaxEigenVectors( node->GetPCAMaxEigenVectors());
+  this->SetPCACombine( node->GetPCACombine());
+
+  this->SetUseCluster( node->GetUseCluster());
+  this->SetSchedulerCommand( node->GetSchedulerCommand());
+
+  this->SetNumberOfThreads( node->GetNumberOfThreads());
+
+  this->SetSkipRegistration( node->GetSkipRegistration());
+  this->SetExistingTemplate( node->GetExistingTemplate());
+  this->SetTransformsDirectory( node->GetTransformsDirectory());
+
+  this->SetDebugMode( node->GetDebugMode());
+  this->SetDryrunMode( node->GetDryrunMode());
+
 }
 
 //----------------------------------------------------------------------------
 void vtkMRMLAtlasCreatorNode::PrintSelf(ostream& os, vtkIndent indent)
 {
-  
   vtkMRMLNode::PrintSelf(os,indent);
 
-  os << indent << "Conductance:   " << this->Conductance << "\n";
-  os << indent << "NumberOfIterations:   " << this->NumberOfIterations << "\n";
-  os << indent << "TimeStep:   " << this->TimeStep << "\n";
-  os << indent << "InputVolumeRef:   " << 
-   (this->InputVolumeRef ? this->InputVolumeRef : "(none)") << "\n";
-  os << indent << "OutputVolumeRef:   " << 
-   (this->OutputVolumeRef ? this->OutputVolumeRef : "(none)") << "\n";
+  os << indent << "OriginalImagesFilePathList: " << (this->GetOriginalImagesFilePathList() ? this->GetOriginalImagesFilePathList() : "(none)") << "\n";
+  os << indent << "SegmentationsFilePathList: " << (this->GetSegmentationsFilePathList() ? this->GetSegmentationsFilePathList() : "(none)") << "\n";
+  os << indent << "OutputDirectory: " << (this->GetOutputDirectory() ? this->GetOutputDirectory() : "(none)") << "\n";
+
+  os << indent << "Toolkit: " << (this->GetToolkit() ? this->GetToolkit() : "(none)") << "\n";
+
+  os << indent << "TemplateType: " << (this->GetTemplateType() ? this->GetTemplateType() : "(none)") << "\n";
+  os << indent << "DynamicTemplateIterations: " << this->GetDynamicTemplateIterations() << "\n";
+  os << indent << "FixedTemplateDefaultCaseFilePath: " << (this->GetFixedTemplateDefaultCaseFilePath() ? this->GetFixedTemplateDefaultCaseFilePath() : "(none)") << "\n";
+  os << indent << "IgnoreTemplateSegmentation: " << this->GetIgnoreTemplateSegmentation() << "\n";
+
+  os << indent << "LabelsList: " << (this->GetLabelsList() ? this->GetLabelsList() : "(none)") << "\n";
+
+  os << indent << "RegistrationType: " << (this->GetRegistrationType() ? this->GetRegistrationType() : "(none)") << "\n";
+
+  os << indent << "SaveTransforms: " << this->GetSaveTransforms() << "\n";
+  os << indent << "DeleteAlignedImages: " << this->GetDeleteAlignedImages() << "\n";
+  os << indent << "DeleteAlignedSegmentations: " << this->GetDeleteAlignedSegmentations() << "\n";
+  os << indent << "NormalizeAtlases: " << this->GetNormalizeAtlases() << "\n";
+  os << indent << "NormalizeTo: " << this->GetNormalizeTo() << "\n";
+
+  os << indent << "OutputCast: " << (this->GetOutputCast() ? this->GetOutputCast() : "(none)") << "\n";
+
+  os << indent << "PCAAnalysis: " << this->GetPCAAnalysis() << "\n";
+  os << indent << "PCAMaxEigenVectors: " << this->GetPCAMaxEigenVectors() << "\n";
+  os << indent << "PCACombine: " << this->GetPCACombine() << "\n";
+
+  os << indent << "UseCluster: " << this->GetUseCluster() << "\n";
+  os << indent << "SchedulerCommand: " << (this->GetSchedulerCommand() ? this->GetSchedulerCommand() : "(none)") << "\n";
+
+  os << indent << "NumberOfThreads: " << this->GetNumberOfThreads() << "\n";
+
+  os << indent << "SkipRegistration: " << this->GetSkipRegistration() << "\n";
+  os << indent << "ExistingTemplate: " << (this->GetExistingTemplate() ? this->GetExistingTemplate() : "(none)") << "\n";
+  os << indent << "TransformsDirectory: " << (this->GetTransformsDirectory() ? this->GetTransformsDirectory() : "(none)") << "\n";
+
+  os << indent << "DebugMode: " << this->GetDebugMode() << "\n";
+  os << indent << "DryrunMode: " << this->GetDryrunMode() << "\n";
+
 }
 
 //----------------------------------------------------------------------------
-void vtkMRMLAtlasCreatorNode::UpdateReferenceID(const char *oldID, const char *newID)
+void vtkMRMLAtlasCreatorNode::InitializeByDefault()
 {
-  if (!strcmp(oldID, this->InputVolumeRef))
-    {
-    this->SetInputVolumeRef(newID);
-    }
-  if (!strcmp(oldID, this->OutputVolumeRef))
-    {
-    this->SetOutputVolumeRef(newID);
-    }
+
+  this->SetOriginalImagesFilePathList("");
+  this->SetSegmentationsFilePathList("");
+  this->SetOutputDirectory("");
+
+  this->SetToolkit("BRAINSFit");
+
+  this->SetTemplateType("fixed");
+  this->DynamicTemplateIterations = 5;
+  this->SetFixedTemplateDefaultCaseFilePath("");
+  this->IgnoreTemplateSegmentation = 0;
+
+  this->SetLabelsList("");
+
+  this->SetRegistrationType("Affine");
+
+  this->SaveTransforms = 1;
+  this->DeleteAlignedImages = 0;
+  this->DeleteAlignedSegmentations = 0;
+  this->NormalizeAtlases = 0;
+  this->NormalizeTo = 1;
+
+  this->SetOutputCast("Short");
+
+  this->PCAAnalysis = 0;
+  this->PCAMaxEigenVectors = 10;
+  this->PCACombine = 0;
+
+  this->UseCluster = 0;
+  this->SetSchedulerCommand("");
+
+  this->NumberOfThreads = -1;
+
+  this->SkipRegistration = 0;
+  this->SetExistingTemplate("");
+  this->SetTransformsDirectory("");
+
+  this->DebugMode = 0;
+  this->DryrunMode = 0;
+
+  this->TestMode = 0;
 }
+
+//----------------------------------------------------------------------------
+void vtkMRMLAtlasCreatorNode::Launch()
+{
+  this->InvokeEvent(vtkMRMLAtlasCreatorNode::LaunchComputationEvent);
+}
+
--- Modules/AtlasCreator/Logic/vtkMRMLAtlasCreatorNode.h
+++ Modules/AtlasCreator/Logic/vtkMRMLAtlasCreatorNode.h
@@ -14,19 +14,14 @@
 #ifndef __vtkMRMLAtlasCreatorNode_h
 #define __vtkMRMLAtlasCreatorNode_h
 
-// MRMK includes
 #include "vtkMRML.h"
 #include "vtkMRMLNode.h"
-#include "vtkMRMLStorageNode.h"
-
-// VTK includes
-#include "vtkMatrix4x4.h"
-#include "vtkTransform.h"
-#include "vtkImageData.h"
 
 #include "vtkSlicerAtlasCreatorModuleLogicExport.h"
 
-class vtkImageData;
+// Description:
+// The atlas creator node is simply a MRMLNode container for
+// a configuration of the Atlas Creator module
 
 class VTK_SLICER_ATLASCREATOR_MODULE_LOGIC_EXPORT vtkMRMLAtlasCreatorNode : public vtkMRMLNode
 {
@@ -35,12 +30,10 @@ class VTK_SLICER_ATLASCREATOR_MODULE_LOGIC_EXPORT vtkMRMLAtlasCreatorNode : publ
   vtkTypeMacro(vtkMRMLAtlasCreatorNode,vtkMRMLNode);
   void PrintSelf(ostream& os, vtkIndent indent);
 
-  // Description:
-  // Create instance of a GAD node.
   virtual vtkMRMLNode* CreateNodeInstance();
 
   // Description:
-  // Set node attributes from name/value pairs
+  // Set node attributes
   virtual void ReadXMLAttributes( const char** atts);
 
   // Description:
@@ -52,51 +45,217 @@ class VTK_SLICER_ATLASCREATOR_MODULE_LOGIC_EXPORT vtkMRMLAtlasCreatorNode : publ
   virtual void Copy(vtkMRMLNode *node);
 
   // Description:
-  // Get unique node XML tag name (like Volume, Model)
-  virtual const char* GetNodeTagName() {return "GADParameters";};
+  // Get node XML tag name (like Volume, Model)
+  virtual const char* GetNodeTagName()
+    {return "AtlasCreator";};
 
-  // Description:
-  // Get/Set Number of iterations (module parameter)
-  vtkGetMacro(NumberOfIterations, int);
-  vtkSetMacro(NumberOfIterations, int);
+  //BTX
+  enum
+  {
+    LaunchComputationEvent = 31337
+  };
+  //ETX
 
-  // Description:
-  // Get/Set Conductance (module parameter)
-  vtkGetMacro(Conductance, double);
-  vtkSetMacro(Conductance, double);
+  // Description: Initialize the configuration with default values
+  //   The default values are:
+  //   - all input and output filePaths, filePathLists and labelLists to ""
+  //   - Toolkit: BRAINSFit
+  //   - Template Type: Fixed
+  //   - DynamicTemplateIterations: 5
+  //   - Affine Registration
+  //   - Save Transforms and keep aligned images
+  //   - Deactivate Normalization including all settings
+  //   - Deactivate PCA including all settings
+  //   - Deactivate Cluster including all settings
+  //   - Deactivate SkipRegistration including all settings
+  //   - Deactivate Debug and Dryrun Mode
+  void InitializeByDefault();
 
-  // Description:
-  // Get/Set time step (module parameter)
-  vtkGetMacro(TimeStep, double);
-  vtkSetMacro(TimeStep, double);
- 
-  // Description:
-  // Get/Set input volume MRML Id
-  vtkGetStringMacro(InputVolumeRef);
-  vtkSetStringMacro(InputVolumeRef);
-  
-  // Description:
-  // Get/Set output volume MRML Id
-  vtkGetStringMacro(OutputVolumeRef);
-  vtkSetStringMacro(OutputVolumeRef);
+  // Description: Launch the Atlas Creator computation with the assigned parameters
+  void Launch();
 
-  // Description:
-  // Update the stored reference to another node in the scene
-  virtual void UpdateReferenceID(const char *oldID, const char *newID);
+  // Description: The originalImages FilePaths divided by space
+  vtkGetStringMacro (OriginalImagesFilePathList);
+  vtkSetStringMacro (OriginalImagesFilePathList);
 
- 
+  // Description: The segmentation FilePaths divided by space
+  vtkGetStringMacro (SegmentationsFilePathList);
+  vtkSetStringMacro (SegmentationsFilePathList);
+
+  // Description: The output Directory
+  vtkGetStringMacro (OutputDirectory);
+  vtkSetStringMacro (OutputDirectory);
+
+
+  // Description: The Toolkit: "BRAINSFit" or "CMTK"
+  vtkGetStringMacro (Toolkit);
+  vtkSetStringMacro (Toolkit);
+
+
+  // Description: The Template Type: "fixed", "dynamic" or "group"
+  vtkGetStringMacro (TemplateType);
+  vtkSetStringMacro (TemplateType);
+
+  // Description: The Number of Iterations for DynamicTemplate
+  vtkGetMacro(DynamicTemplateIterations,int);
+  vtkSetMacro(DynamicTemplateIterations,int);
+
+  // Description: The Default Case FilePath for FixedTemplate
+  vtkGetStringMacro (FixedTemplateDefaultCaseFilePath);
+  vtkSetStringMacro (FixedTemplateDefaultCaseFilePath);
+
+  // Description: 1: Ignore the template segmentation in fixed mode 0: Don't ignore
+  vtkGetMacro(IgnoreTemplateSegmentation,int);
+  vtkSetMacro(IgnoreTemplateSegmentation,int);
+
+  // Description: The Labels divided by space
+  vtkGetStringMacro (LabelsList);
+  vtkSetStringMacro (LabelsList);
+
+
+  // Description: The Registration Type: "Affine" or "Non-Rigid"
+  vtkGetStringMacro (RegistrationType);
+  vtkSetStringMacro (RegistrationType);
+
+
+  // Description: 1: Save Transforms 0: Don't save
+  vtkGetMacro(SaveTransforms,int);
+  vtkSetMacro(SaveTransforms,int);
+
+  // Description: 1: Delete Aligned Images 0: Don't delete
+  vtkGetMacro(DeleteAlignedImages,int);
+  vtkSetMacro(DeleteAlignedImages,int);
+
+  // Description: 1: Delete Aligned Segmentations 0: Don't delete
+  vtkGetMacro(DeleteAlignedSegmentations,int);
+  vtkSetMacro(DeleteAlignedSegmentations,int);
+
+  // Description: 1: Normalize Atlases 0: Don't normalize
+  vtkGetMacro(NormalizeAtlases,int);
+  vtkSetMacro(NormalizeAtlases,int);
+
+  // Description: Set NormalizeTo Value
+  vtkGetMacro(NormalizeTo,int);
+  vtkSetMacro(NormalizeTo,int);
+
+
+  // Description: The Output Cast
+  //   "Char"
+  //   "Unsigned Char"
+  //   "Double"
+  //   "Float"
+  //   "Int"
+  //   "Unsigned Int"
+  //   "Long"
+  //   "Unsigned Long"
+  //   "Short"
+  //   "Unsigned Short"
+  vtkGetStringMacro (OutputCast);
+  vtkSetStringMacro (OutputCast);
+
+
+  // Description: 1: Use PCA Analysis 0: Don't use PCA Analysis
+  vtkGetMacro(PCAAnalysis,int);
+  vtkSetMacro(PCAAnalysis,int);
+
+  // Description: The Number of Max EigenVectors to use for PCA Analysis
+  vtkGetMacro(PCAMaxEigenVectors,int);
+  vtkSetMacro(PCAMaxEigenVectors,int);
+
+  // Description: 1: Combine all PCA output 0: Don't combine all PCA output
+  vtkGetMacro(PCACombine,int);
+  vtkSetMacro(PCACombine,int);
+
+
+  // Description: 1: Use Cluster 0: Don't use cluster
+  vtkGetMacro(UseCluster,int);
+  vtkSetMacro(UseCluster,int);
+
+  // Description: The Scheduler Command
+  vtkGetStringMacro (SchedulerCommand);
+  vtkSetStringMacro (SchedulerCommand);
+
+
+  // Description: The number of threads to use for Registration and Resampling, -1: Use Maximum Number of Threads
+  vtkGetMacro(NumberOfThreads,int);
+  vtkSetMacro(NumberOfThreads,int);
+
+
+  // Description: 1: Skip Registration 0: Don't skip registration
+  vtkGetMacro(SkipRegistration,int);
+  vtkSetMacro(SkipRegistration,int);
+
+  // Description: The Existing Template (Filepath)
+  vtkGetStringMacro (ExistingTemplate);
+  vtkSetStringMacro (ExistingTemplate);
+
+  // Description: The Transforms Directory
+  vtkGetStringMacro (TransformsDirectory);
+  vtkSetStringMacro (TransformsDirectory);
+
+
+  // Description: 1: Use Debug Mode 0: Don't use Debug Mode
+  vtkGetMacro(DebugMode,int);
+  vtkSetMacro(DebugMode,int);
+
+  // Description: 1: Use Dryrun Mode 0: Don't use Dryrun Mode
+  vtkGetMacro(DryrunMode,int);
+  vtkSetMacro(DryrunMode,int);
+
+  // Description: 1: Use Test Mode 0: Don't use Test Mode
+  vtkGetMacro(TestMode,int);
+  vtkSetMacro(TestMode,int);
+
 protected:
+
+  char *OriginalImagesFilePathList;
+  char *SegmentationsFilePathList;
+  char *OutputDirectory;
+
+  char *Toolkit;
+
+  char *TemplateType;
+  int DynamicTemplateIterations;
+  char *FixedTemplateDefaultCaseFilePath;
+  int IgnoreTemplateSegmentation;
+  
+  char *LabelsList;
+
+  char *RegistrationType;
+
+  int SaveTransforms;
+  int DeleteAlignedImages;
+  int DeleteAlignedSegmentations;
+  int NormalizeAtlases;
+  int NormalizeTo;
+
+  char *OutputCast;
+
+  int PCAAnalysis;
+  int PCAMaxEigenVectors;
+  int PCACombine;
+
+  int UseCluster;
+  char *SchedulerCommand;
+
+  int NumberOfThreads;
+
+  int SkipRegistration;
+  char *ExistingTemplate;
+  char *TransformsDirectory;
+
+  int DebugMode;
+  int DryrunMode;
+
+  // the testmode is only for internal use and will not be saved to MRML
+  int TestMode;
+
+
+private:
   vtkMRMLAtlasCreatorNode();
   ~vtkMRMLAtlasCreatorNode();
   vtkMRMLAtlasCreatorNode(const vtkMRMLAtlasCreatorNode&);
   void operator=(const vtkMRMLAtlasCreatorNode&);
-
-  double Conductance;
-  double TimeStep;
-  int NumberOfIterations;
-  
-  char* InputVolumeRef;
-  char* OutputVolumeRef;
 
 };
 
