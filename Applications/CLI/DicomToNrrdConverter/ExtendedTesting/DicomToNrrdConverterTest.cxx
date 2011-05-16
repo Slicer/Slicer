ce7460ea6606d62073d705ea577f9460d3b057dd
Mark Scully <mark-scully@uiowa.edu>
2011-05-16 19:09:38 +0000

    ENH: Added full DicomToNrrdConverter testing suite that pulls data from midas.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16565
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16565 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/CLI/DicomToNrrdConverter/ExtendedTesting/DicomToNrrdConverterTest.cxx
@@ -0,0 +1,29 @@
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
+#include <iostream>
+#include "itkTestMain.h"
+//#include "DicomToNrrdConverter.cxx"
+
+/*
+extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char* []);
+
+void RegisterTests()
+{
+  StringToTestFunctionMap["ModuleEntryPoint"] = ModuleEntryPoint;
+}
+*/
+
+void RegisterTests()
+{
+  REGISTER_TEST(DicomToNrrdConverterTest);
+}
+
+#undef main
+#define main DicomToNrrdConverterTest
+#include "../DicomToNrrdConverter.cxx"
