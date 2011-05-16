8e093dc5c16721140137279f96d4d06d0a595c35
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-05-16 21:45:06 +0000

    COMP: Remove reference to Slicer3_VTK5
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16575
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16575 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/vtkITK/vtkITK.h
+++ Libs/vtkITK/vtkITK.h
@@ -11,13 +11,12 @@
   Version:   $Revision$
 
 ==========================================================================*/
-//
-/// use an ifdef on Slicer3_VTK5 to flag code that won't
-/// compile on vtk4.4 and before
-//
-#if ( (VTK_MAJOR_VERSION >= 5) || ( VTK_MAJOR_VERSION == 4 && VTK_MINOR_VERSION >= 5 ) )
-#define Slicer3_VTK5
-#endif
+
+#ifndef __vtkITK_h
+#define __vtkITK_h
 
 #include "vtkITKWin32Header.h"
 #include "vtkITKNumericTraits.h"
+
+#endif
+
