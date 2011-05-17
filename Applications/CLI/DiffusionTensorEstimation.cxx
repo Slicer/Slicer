296da91584b680a79edd7609290988df49b8a794
Demian Wassermann <demian@bwh.harvard.edu>
2011-05-17 19:46:18 +0000

    ENH: Refactoring diffusion tensor estimation
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16596
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16596 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/CLI/DiffusionTensorEstimation.cxx
+++ Applications/CLI/DiffusionTensorEstimation.cxx
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <cmath>
 
 #include "itkPluginFilterWatcher.h"
 #include "itkPluginUtilities.h"
@@ -30,8 +31,12 @@
 
 #include "DiffusionTensorEstimationCLP.h"
 
+#define MAT_TOL 1e-6
+bool transformsNotEqual(const vtkMatrix4x4* mat1, const vtkMatrix4x4* mat2);
+
 int main( int argc, char * argv[] )
   {
+  bool applyMask=false;
 
   PARSE_ARGS;
     {
@@ -80,9 +85,9 @@ int main( int argc, char * argv[] )
       {
       rasToIjkRotation->SetElement(i,3,0);
       }
-    //Remove scaling in rasToIjk to make a real roation matrix
+    //Remove scaling in rasToIjk to make a real rotation matrix
     double col[3];
-    for (int jjj = 0; jjj < 3; jjj++) 
+    for (int jjj = 0; jjj < 3; jjj++)
       {
       for (int iii = 0; iii < 3; iii++)
         {
@@ -111,95 +116,61 @@ int main( int argc, char * argv[] )
       {
       estim->SetEstimationMethodToWLS();
       }
-    else if (estimationMethod == std::string("NL"))
-      {
-      estim->SetEstimationMethodToNLS();
-      }
     estim->Update();
     vtkImageData *tensorImage = estim->GetOutput();
     tensorImage->GetPointData()->SetScalars(NULL);
 
-    //compute tenor mask
-
-    vtkSmartPointer<vtkITKNewOtsuThresholdImageFilter> otsu =
-      vtkSmartPointer<vtkITKNewOtsuThresholdImageFilter>::New();
-    otsu->SetInput(estim->GetBaseline());
-    otsu->SetOmega (1 + otsuOmegaThreshold);
-    otsu->SetOutsideValue(1);
-    otsu->SetInsideValue(0);
-    otsu->Update();
-
+    //Read the tensor mask
     vtkSmartPointer<vtkImageData> mask = vtkSmartPointer<vtkImageData>::New();
-    mask->DeepCopy(otsu->GetOutput());
-
-    int *dims = mask->GetDimensions();
-    int px = dims[0]/2;
-    int py = dims[1]/2;
-    int pz = dims[2]/2;
-
-    vtkSmartPointer<vtkImageCast> cast =
-      vtkSmartPointer<vtkImageCast>::New();
-    cast->SetInput(mask);
-    cast->SetOutputScalarTypeToUnsignedChar();
-    cast->Update();
-
-    vtkSmartPointer<vtkImageSeedConnectivity> con =
-      vtkSmartPointer<vtkImageSeedConnectivity>::New();
-    con->SetInput(cast->GetOutput());
-    con->SetInputConnectValue(1);
-    con->SetOutputConnectedValue(1);
-    con->SetOutputUnconnectedValue(0);
-    con->AddSeed(px, py, pz);
-    con->Update();
-
-    vtkSmartPointer<vtkImageCast> cast1 =
-      vtkSmartPointer<vtkImageCast>::New();
-    cast1->SetInput(con->GetOutput());
-    cast1->SetOutputScalarTypeToShort();
-    cast1->Update();
-
-
-    vtkSmartPointer<vtkImageConnectivity> conn =
-      vtkSmartPointer<vtkImageConnectivity>::New();
-    if (removeIslands)  
+    if (strlen(inputMaskVolume.c_str()) == 0)
       {
-      conn->SetBackground(1);
-      conn->SetMinForeground( -32768);
-      conn->SetMaxForeground( 32767);
-      conn->SetFunctionToRemoveIslands();
-      conn->SetMinSize(10000);
-      conn->SliceBySliceOn();
-      conn->SetInput(cast1->GetOutput());
-      conn->Update();
-      } 
+      vtkSmartPointer<vtkNRRDReader> maskReader =
+        vtkSmartPointer<vtkNRRDReader>::New();
+      maskReader->SetFileName(inputMaskVolume.c_str());
+      maskReader->Update();
+      if ( maskReader->GetReadStatus() )
+        {
+        std::cerr << argv[0] << ": Error reading Mask file" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    // Maks tensor
-    //TODO: fix tenosr mask
-    /**/
+      //Check if the transforms are equal
+      if (transformsNotEqual(maskReader->GetRasToIjkMatrix(), reader->GetRasToIjkMatrix()))
+        {
+        std::cerr << argv[0] << ": Error reading Mask file, wrong coordinate space" << std::endl;
+        return EXIT_FAILURE;
+        }
+
+      vtkSmartPointer<vtkImageCast> cast =
+        vtkSmartPointer<vtkImageCast>::New();
+      cast->SetInput(maskReader->GetOutput());
+      cast->SetOutputScalarTypeToUnsignedChar();
+      cast->Update();
+
+      mask->DeepCopy(cast->GetOutput());
+      applyMask=true;
+      } 
+    else 
+      {
+      applyMask=false;
+      }
+
+    // Mask tensor
     vtkSmartPointer<vtkTensorMask> tensorMask =
       vtkSmartPointer<vtkTensorMask>::New();
     tensorMask->SetNumberOfThreads(1);
-    vtkSmartPointer<vtkImageCast> cast2 =
-      vtkSmartPointer<vtkImageCast>::New();
-    cast2->SetOutputScalarTypeToUnsignedChar();
     if (applyMask)
       {  
-
       tensorMask->SetMaskAlpha(0.0);
       tensorMask->SetInput(tensorImage);
-      if (removeIslands)  
-        {
-        cast2->SetInput(conn->GetOutput());
-        }
-      else
-        {
-        cast2->SetInput(cast1->GetOutput());
-        }
-      tensorMask->SetMaskInput(cast2->GetOutput());
+      tensorMask->SetMaskInput(mask);
       tensorMask->Update();
       tensorImage = tensorMask->GetOutput();
       }
     /**/
+    //Compute IjkToRas (used by Writer)
+    vtkSmartPointer<vtkMatrix4x4> ijkToRasMatrix = reader->GetRasToIjkMatrix();
+    ijkToRasMatrix->Invert();
 
     //Save tensor
     vtkSmartPointer<vtkNRRDWriter> writer =
@@ -208,9 +179,7 @@ int main( int argc, char * argv[] )
     writer->SetInput(tensorImage);
     writer->SetFileName( outputTensor.c_str() );
     writer->UseCompressionOn();
-    //Compute IjkToRas (used by Writer)
-    reader->GetRasToIjkMatrix()->Invert();
-    writer->SetIJKToRASMatrix( reader->GetRasToIjkMatrix() );
+    writer->SetIJKToRASMatrix( ijkToRasMatrix );
     //Compute measurement frame: Take into account that we have transformed
     //the gradients so tensor components are defined in ijk.
     rasToIjkRotation->Invert();
@@ -223,26 +192,19 @@ int main( int argc, char * argv[] )
     writer2->SetInput(estim->GetBaseline());
     writer2->SetFileName( outputBaseline.c_str() );
     writer2->UseCompressionOn();
-    writer2->SetIJKToRASMatrix( reader->GetRasToIjkMatrix() );
+    writer2->SetIJKToRASMatrix( ijkToRasMatrix );
     writer2->Write();
-
-    //Save mask
-    vtkSmartPointer<vtkNRRDWriter> writer3 =
-      vtkSmartPointer<vtkNRRDWriter>::New();
-    if (removeIslands)  
-      {
-      writer3->SetInput(conn->GetOutput());
-      }
-    else
-      {
-      writer3->SetInput(cast1->GetOutput());
-      }
-    writer3->SetFileName( thresholdMask.c_str() );
-    writer3->UseCompressionOn();
-    writer3->SetIJKToRASMatrix( reader->GetRasToIjkMatrix() );
-    writer3->Write();
-
-    }
 
     return EXIT_SUCCESS;
   }
+}
+
+bool transformsNotEqual(const vtkMatrix4x4* mat1, const vtkMatrix4x4* mat2)
+  {
+  for (int i = 0; i < 4; i++)
+    for (int j = 0; j < 4; j++)
+      if (fabs(mat1->GetElement(i, j) - mat2->GetElement(i, j)) > MAT_TOL)
+        return true;
+  return false;
+  }
+
---
+++ Applications/CLI/DiffusionWeightedMasking.cxx
@@ -0,0 +1,191 @@
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+#include "itkPluginFilterWatcher.h"
+#include "itkPluginUtilities.h"
+#include "vtkSmartPointer.h"
+#include "vtkMatrix4x4.h"
+#include "vtkNRRDReader.h"
+#include "vtkNRRDWriter.h"
+#include "vtkMRMLNRRDStorageNode.h"
+#include "vtkMath.h"
+#include "vtkDoubleArray.h"
+#include "vtkImageExtractComponents.h"
+#include "vtkImageWeightedSum.h"
+
+#include "vtkImageData.h"
+#include "vtkImageCast.h"
+#include "vtkImageSeedConnectivity.h"
+#include "vtkImageConnectivity.h"
+#include "vtkITKNewOtsuThresholdImageFilter.h"
+
+#include "DiffusionWeightedMaskingCLP.h"
+
+#define GRAD_0_TOL 1e-6
+
+int main( int argc, char * argv[] )
+  {
+
+  PARSE_ARGS;
+    {
+    vtkSmartPointer<vtkNRRDReader> reader =
+      vtkSmartPointer<vtkNRRDReader>::New();
+    reader->SetFileName(inputVolume.c_str());
+    reader->Update();
+    if ( reader->GetReadStatus() )
+      {
+      std::cerr << argv[0] << ": Error reading Diffusion file" << std::endl;
+      return EXIT_FAILURE;
+      }
+
+    vtkSmartPointer<vtkDoubleArray> bValues =
+      vtkSmartPointer<vtkDoubleArray>::New();
+    vtkSmartPointer<vtkDoubleArray> grads =
+      vtkSmartPointer<vtkDoubleArray>::New();
+    vtkSmartPointer<vtkMRMLNRRDStorageNode> helper =
+      vtkSmartPointer<vtkMRMLNRRDStorageNode>::New();
+
+    if ( !helper->ParseDiffusionInformation(reader,grads,bValues) )
+      {
+      std::cerr << argv[0] << ": Error parsing Diffusion information" << std::endl;
+      return EXIT_FAILURE;
+      }
+
+    //Compute the mean baseline image
+    vtkSmartPointer<vtkImageWeightedSum> imageWeightedSum = vtkImageWeightedSum::New();
+    imageWeightedSum->NormalizeByWeightOn();
+
+    int b0_count = 0;
+    for (int gradient_n=0; gradient_n<grads->GetNumberOfTuples(); gradient_n++)
+      {
+      double* gradient = grads->GetTuple3(gradient_n);
+      if (abs(gradient[0]) + abs(gradient[1]) + abs(gradient[2]) < GRAD_0_TOL)
+        {
+          vtkSmartPointer<vtkImageExtractComponents> extractComponents = vtkImageExtractComponents::New();
+          extractComponents->SetInput(reader->GetOutput());
+          extractComponents->SetComponents(gradient_n);
+          extractComponents->Update();
+
+          imageWeightedSum->AddInputConnection(extractComponents->GetOutputPort());
+          imageWeightedSum->SetWeight(b0_count++, 1.);
+        }
+      }
+    imageWeightedSum->Update();
+
+    if (b0_count == 0)
+      {
+      std::cerr << argv[0] << ": Error parsing Diffusion information, no B0 images" << std::endl;
+      return EXIT_FAILURE;       
+      } 
+
+    
+    //compute DWI mask
+    vtkSmartPointer<vtkITKNewOtsuThresholdImageFilter> otsu =
+      vtkSmartPointer<vtkITKNewOtsuThresholdImageFilter>::New();
+    otsu->SetInput(imageWeightedSum->GetOutput());
+    otsu->SetOmega (1 + otsuOmegaThreshold);
+    otsu->SetOutsideValue(1);
+    otsu->SetInsideValue(0);
+    otsu->Update();
+
+    vtkSmartPointer<vtkImageData> mask = vtkSmartPointer<vtkImageData>::New();
+    mask->DeepCopy(otsu->GetOutput());
+
+    int *dims = mask->GetDimensions();
+    int px = dims[0]/2;
+    int py = dims[1]/2;
+    int pz = dims[2]/2;
+
+    vtkSmartPointer<vtkImageCast> cast =
+      vtkSmartPointer<vtkImageCast>::New();
+    cast->SetInput(mask);
+    cast->SetOutputScalarTypeToUnsignedChar();
+    cast->Update();
+
+    vtkSmartPointer<vtkImageSeedConnectivity> con =
+      vtkSmartPointer<vtkImageSeedConnectivity>::New();
+    con->SetInput(cast->GetOutput());
+    con->SetInputConnectValue(1);
+    con->SetOutputConnectedValue(1);
+    con->SetOutputUnconnectedValue(0);
+    con->AddSeed(px, py, pz);
+    con->Update();
+
+    vtkSmartPointer<vtkImageCast> cast1 =
+      vtkSmartPointer<vtkImageCast>::New();
+    cast1->SetInput(con->GetOutput());
+    cast1->SetOutputScalarTypeToShort();
+    cast1->Update();
+
+
+    vtkSmartPointer<vtkImageConnectivity> conn =
+      vtkSmartPointer<vtkImageConnectivity>::New();
+
+    if (removeIslands)  
+      {
+      conn->SetBackground(1);
+      conn->SetMinForeground( -32768);
+      conn->SetMaxForeground( 32767);
+      conn->SetFunctionToRemoveIslands();
+      conn->SetMinSize(10000);
+      conn->SliceBySliceOn();
+      conn->SetInput(cast1->GetOutput());
+      conn->Update();
+      }
+
+
+    vtkSmartPointer<vtkImageCast> cast2 =
+      vtkSmartPointer<vtkImageCast>::New();
+    cast2->SetOutputScalarTypeToUnsignedChar();
+
+    if (removeIslands)  
+      {
+      cast2->SetInput(conn->GetOutput());
+      }
+    else
+      {
+      cast2->SetInput(cast1->GetOutput());
+      }
+
+    vtkSmartPointer<vtkMatrix4x4> ijkToRasMatrix = reader->GetRasToIjkMatrix();
+    ijkToRasMatrix->Invert();
+    
+    //Save baseline
+    vtkSmartPointer<vtkNRRDWriter> writer =
+      vtkSmartPointer<vtkNRRDWriter>::New();
+    writer->SetInput(imageWeightedSum->GetOutput());
+    writer->SetFileName( outputBaseline.c_str() );
+    writer->UseCompressionOn();
+    writer->SetIJKToRASMatrix( ijkToRasMatrix );
+    writer->Write();
+
+    //Save mask
+    vtkSmartPointer<vtkNRRDWriter> writer2 =
+      vtkSmartPointer<vtkNRRDWriter>::New();
+    if (removeIslands)  
+      {
+      writer2->SetInput(conn->GetOutput());
+      }
+    else
+      {
+      writer2->SetInput(cast1->GetOutput());
+      }
+    writer2->SetFileName( thresholdMask.c_str() );
+    writer2->UseCompressionOn();
+    writer2->SetIJKToRASMatrix( ijkToRasMatrix );
+    writer2->Write();
+
+    }
+
+    return EXIT_SUCCESS;
+  }
