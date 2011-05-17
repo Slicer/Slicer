cb5578ec659592cb0d8ed4268a457c4b30dc8fd2
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-05-17 16:59:21 +0000

    COMP: Rename SparseFieldLevelSetContour1Test into SparseFieldLevelSetContourTest
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16590
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16590 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/CLI/Testing/SparseFieldLevelSetContourTest.cxx
+++ Applications/CLI/Testing/SparseFieldLevelSetContourTest.cxx
@@ -6,18 +6,22 @@
 #define ITK_LEAN_AND_MEAN
 #endif
 
-#include <iostream>
-#include "itkTestMain.h"
-
 #ifdef WIN32
 #define MODULE_IMPORT __declspec(dllimport)
 #else
 #define MODULE_IMPORT
 #endif
 
-// Comment copied from ThesholdTest.cxx; This will be linked against the ModuleEntryPoint in SparseFieldLevelSetContourLib
-extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char* []);
+#include <iostream>
+#include "itkTestMain.h"
+
 void RegisterTests()
 {
-  StringToTestFunctionMap["ModuleEntryPoint"] = ModuleEntryPoint;
+REGISTER_TEST(SparseFieldLevelSetContour1Test);
 }
+
+#undef main
+#define main SparseFieldLevelSetContour1Test
+#include "SparseFieldLevelSetContour/SparseFieldLevelSetContour.cxx"
+
+
