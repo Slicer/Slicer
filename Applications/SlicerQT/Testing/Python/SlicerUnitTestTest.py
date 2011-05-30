ba89686d742435dee2b65804b78e8e119547898d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-05-30 20:48:00 +0000

    STYLE: SlicerUnitTestTest - Fix indent and add doc string
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16701
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16701 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/SlicerQT/Testing/Python/SlicerUnitTestTest.py
+++ Applications/SlicerQT/Testing/Python/SlicerUnitTestTest.py
@@ -2,25 +2,26 @@ import unittest
 import random
 
 class SlicerUnitTestTest(unittest.TestCase):
+  """ See http://docs.python.org/library/unittest.html#basic-example
+  """
+  def setUp(self):
+    self.seq = range(10)
 
-    def setUp(self):
-        self.seq = range(10)
+  def test_shuffle(self):
+    # Make sure the shuffled sequence does not lose any elements
+    random.shuffle(self.seq)
+    self.seq.sort()
+    self.assertEqual(self.seq, range(10))
 
-    def test_shuffle(self):
-        # make sure the shuffled sequence does not lose any elements
-        random.shuffle(self.seq)
-        self.seq.sort()
-        self.assertEqual(self.seq, range(10))
+    # Should raise an exception for an immutable sequence
+    self.assertRaises(TypeError, random.shuffle, (1,2,3))
 
-        # should raise an exception for an immutable sequence
-        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+  def test_choice(self):
+    element = random.choice(self.seq)
+    self.assertTrue(element in self.seq)
 
-    def test_choice(self):
-        element = random.choice(self.seq)
-        self.assertTrue(element in self.seq)
-
-    def test_sample(self):
-        self.assertRaises(ValueError, random.sample, self.seq, 20)
-        for element in random.sample(self.seq, 5):
-            self.assertTrue(element in self.seq)
+  def test_sample(self):
+    self.assertRaises(ValueError, random.sample, self.seq, 20)
+    for element in random.sample(self.seq, 5):
+      self.assertTrue(element in self.seq)
 
