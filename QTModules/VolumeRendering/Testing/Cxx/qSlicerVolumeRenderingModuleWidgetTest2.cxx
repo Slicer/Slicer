d840018916b7d6010a0fef9e2d1d7ded4022e74e
Julien Finet <julien.finet@kitware.com>
2011-06-03 02:12:14 +0000

    ENH: Add test to qSlicerVolumeRenderingModuleWidget
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16754
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16754 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ QTModules/VolumeRendering/Testing/Cxx/qSlicerVolumeRenderingModuleWidgetTest2.cxx
@@ -0,0 +1,85 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) 2010 Kitware Inc.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Julien Finet, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+// Qt includes
+#include <QApplication>
+#include <QTimer>
+
+// Slicer includes
+#include <qSlicerApplication.h>
+
+// VolumeRendering includes
+#include "qSlicerVolumeRenderingModule.h"
+#include "qSlicerVolumeRenderingModuleWidget.h"
+
+// Volumes includes
+#include <vtkSlicerVolumesLogic.h>
+
+// MRML includes
+#include <vtkMRMLScene.h>
+#include <vtkMRMLViewNode.h>
+
+// VTK includes
+#include <vtkSmartPointer.h>
+
+//-----------------------------------------------------------------------------
+int qSlicerVolumeRenderingModuleWidgetTest2( int argc, char * argv[] )
+{
+  qSlicerApplication app(argc, argv);
+
+  if (argc < 2)
+    {
+    std::cerr << "Usage: qSlicerVolumeRenderingModuleWidgetTest2 volumeName [-I]" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  qSlicerVolumeRenderingModule module;
+  module.setMRMLScene(app.mrmlScene());
+  module.initialize(0);
+
+  qSlicerVolumeRenderingModuleWidget* moduleWidget =
+    dynamic_cast<qSlicerVolumeRenderingModuleWidget*>(
+      module.widgetRepresentation());
+
+  vtkSmartPointer<vtkSlicerVolumesLogic> volumesLogic =
+    vtkSmartPointer<vtkSlicerVolumesLogic>::New();
+  volumesLogic->SetMRMLScene(app.mrmlScene());
+
+  vtkMRMLVolumeNode* volumeNode = volumesLogic->AddArchetypeVolume(argv[1], "volume");
+  if (!volumeNode)
+    {
+    std::cerr << "Bad volume file:" << argv[1] << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  vtkSmartPointer<vtkMRMLViewNode> view = vtkSmartPointer<vtkMRMLViewNode>::New();
+  app.mrmlScene()->AddNode(view);
+  vtkSmartPointer<vtkMRMLViewNode> view2 = vtkSmartPointer<vtkMRMLViewNode>::New();
+  app.mrmlScene()->AddNode(view2);
+
+  moduleWidget->show();
+
+  if (argc < 3 || QString(argv[2]) != "-I")
+    {
+    QTimer::singleShot(100, qApp, SLOT(quit()));
+    }
+
+  return app.exec();
+}
