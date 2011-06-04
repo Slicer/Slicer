f51d6d234c095bd6c276f880153d70eafc49fcbb
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-04 06:54:48 +0000

    ENH: Optimize python install rules
    Only python library and associated modules are installed.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16791
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16791 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallPython.cmake
+++ CMake/SlicerBlockInstallPython.cmake
@@ -5,26 +5,34 @@ if(Slicer_USE_PYTHON OR Slicer_USE_PYTHONQT)
 
   get_filename_component(SUPER_BUILD_DIR "${Slicer_BINARY_DIR}" PATH)
   set(PYTHON_DIR "${SUPER_BUILD_DIR}/python-build")
-
-  if(WIN32 AND NOT EXISTS "${PYTHON_DIR}" AND EXISTS "${PYTHON_INCLUDE_PATH}")
-    get_filename_component(PYTHON_DIR "${PYTHON_INCLUDE_PATH}" PATH)
+  set(python_lib_subdir /Lib/)
+  if(UNIX)
+    set(python_lib_subdir /lib/python2.6/)
+  endif()
+  if(NOT EXISTS "${PYTHON_DIR}${python_lib_subdir}")
+    message(FATAL_ERROR "error: Failed to install Python ! - Unexistant directory PYTHON_DIR:${PYTHON_DIR}${python_lib_subdir}")
   endif()
 
-  if(EXISTS "${PYTHON_DIR}")
-    install(DIRECTORY
-      "${PYTHON_DIR}/"
-      DESTINATION lib/Python
-      USE_SOURCE_PERMISSIONS
-      REGEX "include/" EXCLUDE
-      REGEX "share/man" EXCLUDE
+  install(
+    DIRECTORY "${PYTHON_DIR}${python_lib_subdir}"
+    DESTINATION ${Slicer_INSTALL_ROOT}lib/Python${python_lib_subdir}
+    USE_SOURCE_PERMISSIONS
+    REGEX "lib2to3/" EXCLUDE
+    REGEX "lib-old/" EXCLUDE
+    REGEX "plat-*" EXCLUDE
+    REGEX "test/" EXCLUDE
+    REGEX "wsgiref*" EXCLUDE
+    )
+  # Install python library
+  if(UNIX)
+    slicerInstallLibrary(
+      FILE ${PYTHON_LIBRARY}
+      DESTINATION ${Slicer_INSTALL_ROOT}lib/Python/lib
+      COMPONENT Runtime
       )
-  endif()
-
-  #install python dll.
-  if(WIN32)
+  elseif(WIN32)
     get_filename_component(PYTHON_LIB_BASE ${PYTHON_LIBRARY} NAME_WE)
     get_filename_component(PYTHON_LIB_PATH ${PYTHON_LIBRARY} PATH)
-
     install(FILES "${PYTHON_LIB_PATH}/${PYTHON_LIB_BASE}.dll"
       DESTINATION bin
       COMPONENT Runtime)
