fafffa2f3c95fc762de3585db9fa438a8c2ab6a8
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-07 22:45:59 +0000

    ENH: SlicerMacroDiscoverSystemNameAndBits - Standardized its output
    As a function of the machine architecture and system, on of the
    following value is returned:
    
      linux-i386
      linux-amd64
      win32-x86
      win64-x86
      macosx-i386
      macosx-amd64
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16841
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16841 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerMacroDiscoverSystemNameAndBits.cmake
+++ CMake/SlicerMacroDiscoverSystemNameAndBits.cmake
@@ -23,20 +23,20 @@
 # is used to extract information associated with the current platform.
 #
 # The macro defines the following variables:
-#  <var-prefix>_BUILD_BITS - bitness of the platform. 32 or 64
-#  <var-prefix>_BUILD - which is on the this value: solaris8, linux-x86, linux-x86_64, darwin-ppc, darwin-x86, darwin-x86_64, win32, win64
+#  <var-prefix>_BUILD_BITS - bitness of the platform: 32 or 64
+#  <var-prefix>_BUILD - which is on the this value: linux-i386, linux-amd64, macosx-ppc, macosx-i386, macosx-amd64, win32-x86, win64-x86
 
 MACRO(SlicerMacroDiscoverSystemNameAndBits)
   SET(options)
   SET(oneValueArgs VAR_PREFIX)
   SET(multiValueArgs)
   cmake_parse_arguments(MY "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-  
+
   # Sanity checks
   if("${MY_VAR_PREFIX}" STREQUAL "")
     message(FATAL_ERROR "error: VAR_PREFIX should be specified !")
   endif()
-  
+
   SET(${MY_VAR_PREFIX}_BUILD "")
 
   SET(${MY_VAR_PREFIX}_BUILD_BITS "32")
@@ -44,30 +44,35 @@ MACRO(SlicerMacroDiscoverSystemNameAndBits)
     SET(${MY_VAR_PREFIX}_BUILD_BITS "64")
   ENDIF()
 
-  IF (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+  IF(CMAKE_SYSTEM_NAME STREQUAL "Windows")
 
-    SET(${MY_VAR_PREFIX}_BUILD "win${${MY_VAR_PREFIX}_BUILD_BITS}")
-    
-  ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-
-    SET(${MY_VAR_PREFIX}_BUILD "linux-x86")
-    IF (${MY_VAR_PREFIX}_BUILD_BITS STREQUAL "64")
-      SET(${MY_VAR_PREFIX}_BUILD "linux-x86_64")
+    SET(${MY_VAR_PREFIX}_BUILD "win32-x86")
+    IF(${MY_VAR_PREFIX}_BUILD_BITS STREQUAL "64")
+      SET(${MY_VAR_PREFIX}_BUILD "win64-x86")
     ENDIF()
-    
+
+  ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  
+    SET(${MY_VAR_PREFIX}_BUILD "linux-i386")
+    IF(${MY_VAR_PREFIX}_BUILD_BITS STREQUAL "64")
+      SET(${MY_VAR_PREFIX}_BUILD "linux-amd64")
+    ENDIF()
+
   ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
 
-    # we do not differentiate 32 vs 64 for mac - all are 64 bit.
     IF(CMAKE_SYSTEM_PROCESSOR MATCHES "powerpc")
-      SET(${MY_VAR_PREFIX}_BUILD "darwin-ppc")
-    ELSE(CMAKE_SYSTEM_PROCESSOR MATCHES "powerpc")
-      SET(${MY_VAR_PREFIX}_BUILD "darwin-x86")
+      SET(${MY_VAR_PREFIX}_BUILD "macosx-ppc")
+    ELSE()
+      SET(${MY_VAR_PREFIX}_BUILD "macosx-i386")
+      IF (${MY_VAR_PREFIX}_BUILD_BITS STREQUAL "64")
+        SET(${MY_VAR_PREFIX}_BUILD "macosx-amd64")
+      ENDIF()
     ENDIF()
-    
-  ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Solaris")
 
-    SET(${MY_VAR_PREFIX}_BUILD "solaris8") # What about solaris9 and solaris10 ?
-    
+  #ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Solaris")
+
+  #  SET(${MY_VAR_PREFIX}_BUILD "solaris8") # What about solaris9 and solaris10 ?
+
   ENDIF()
 
 ENDMACRO()
