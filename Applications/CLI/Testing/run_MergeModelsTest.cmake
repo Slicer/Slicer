cde4e9234664551678ba3d0d9deea748cb7af991
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-08 14:37:28 +0000

    BUG: Fix MergeModelsTestCompare
    * Simplify run_MergeModelsTest
    * Use generator expression in add_test() call. the generator expression
    will be expanded to match the location of the associated target.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16874
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16874 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/CLI/Testing/run_MergeModelsTest.cmake
+++ Applications/CLI/Testing/run_MergeModelsTest.cmake
@@ -1,54 +1,36 @@
-# some argument checking:
-# test_cmd is the command to run without args
-if( NOT test_cmd )
- message( FATAL_ERROR "Variable test_cmd not defined" )
-endif( NOT test_cmd )
 
-# test_name is the name of the test found in the testing wrapper test_cmd
-if (NOT test_name)
-  message( FATAL_ERROR "Variable test_name not defined" )
-endif( NOT test_name)
+# test_cmd .........: command to run without args
+# test_name ........: name of the test found in the testing wrapper <test_cmd>
+# model{1,2} .......: 
+# output_model .....:
+# output_baseline ..: name of the "baseline" output file
+# output_test ......: name of the output file the <test_cmd> will produce
 
-# need all the args separately or else they're not passed properly to the command below
-if (NOT model1)
-message ( FATAL_ERROR "Variable model1 not defined" )
-endif (NOT model1)
-if (NOT model2)
-message ( FATAL_ERROR "Variable model2 not defined" )
-endif (NOT model2)
+# Sanity checks
+set(expected_defined_vars test_cmd test_name model1 model2 output_model output_baseline output_test)
+foreach(var ${expected_defined_vars})
+  if(NOT ${var})
+    message(FATAL_ERROR "Variable ${var} not defined !")
+  endif()
+endforeach()
 
-if (NOT output_model)
-   message ( FATAL_ERROR "Variable output_model not defined" )
-endif (NOT output_model)
-
-# output_baseline contains the name of the "baseline" output file
-if( NOT output_baseline )
- message( FATAL_ERROR "Variable output_baseline not defined" )
-endif( NOT output_baseline )
-
-# output_test contains the name of the output file the test_cmd will produce
-if( NOT output_test )
- message( FATAL_ERROR "Variable output_test not defined" )
-endif( NOT output_test )
-
-
-# now run the test
+# Run the test
 execute_process(
- COMMAND ${test_cmd} ${test_name} ${model1} ${model2} ${output_model}
- RESULT_VARIABLE exec_not_successful
-)
+  COMMAND ${test_cmd} ${test_name} ${model1} ${model2} ${output_model}
+  RESULT_VARIABLE exec_not_successful
+  )
 
 if (exec_not_successful)
  message( SEND_ERROR "${test_cmd} failed with args ${test_name} ${model1} ${model2} ${output_model}" )
-endif( exec_not_successful )
+endif()
 
 execute_process(
- COMMAND ${CMAKE_COMMAND} -E compare_files ${output_baseline} ${output_test}
- RESULT_VARIABLE test_not_successful
- OUTPUT_QUIET
- ERROR_QUIET
- )
+  COMMAND ${CMAKE_COMMAND} -E compare_files ${output_baseline} ${output_test}
+  RESULT_VARIABLE test_not_successful
+  OUTPUT_QUIET
+  ERROR_QUIET
+  )
 
-if( test_not_successful )
- message( SEND_ERROR "${output_test} does not match ${output_baseline}!" )
-endif( test_not_successful )
+if(test_not_successful)
+  message(SEND_ERROR "${output_test} does not match ${output_baseline}!")
+endif()
