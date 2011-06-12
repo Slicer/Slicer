6d6919de96d52a743d79bb3f659ab4c48eb81c09
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-12 07:14:11 +0000

    ENH: Update SlicerBlockStringFindReplace.cmake so that output file is written only if needed.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=16974
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@16974 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockStringFindReplace.cmake
+++ CMake/SlicerBlockStringFindReplace.cmake
@@ -1,5 +1,8 @@
 
 file(READ ${in} code)
-string(REPLACE "${find}" "${replace}" code "${code}")
-file(WRITE ${out} "${code}")
+string(REPLACE "${find}" "${replace}" code_updated "${code}")
+# Write output file only if required
+if(NOT "${code}" STREQUAL "${code_updated}")
+  file(WRITE ${out} "${code_updated}")
+endif()
 
