57ccced34d0e41ad4b9c2f382e9edea1cec606c3
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-16 06:26:58 +0000

    ENH: SlicerBlockInstallCMakeProjects - Improve test checking if an external project should be installed.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17026
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17026 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallCMakeProjects.cmake
+++ CMake/SlicerBlockInstallCMakeProjects.cmake
@@ -8,42 +8,42 @@ set(CPACK_INSTALL_CMAKE_PROJECTS
 # -------------------------------------------------------------------------
 # Install VTK
 # -------------------------------------------------------------------------
-if(EXISTS "${VTK_DIR}/CMakeCache.txt")
+if(NOT "${VTK_DIR}" STREQUAL "" AND EXISTS "${VTK_DIR}/CMakeCache.txt")
   set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${VTK_DIR};VTK;RuntimeLibraries;/")
 endif()
 
 # -------------------------------------------------------------------------
 # Install ITK
 # -------------------------------------------------------------------------
-if(EXISTS "${ITK_DIR}/CMakeCache.txt")
+if(NOT "${ITK_DIR}" STREQUAL "" AND EXISTS "${ITK_DIR}/CMakeCache.txt")
   set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${ITK_DIR};ITK;RuntimeLibraries;/")
 endif()
 
 # -------------------------------------------------------------------------
 # Install Teem
 # -------------------------------------------------------------------------
-if(EXISTS "${Teem_DIR}/CMakeCache.txt")
+if(NOT "${Teem_DIR}" STREQUAL "" AND EXISTS "${Teem_DIR}/CMakeCache.txt")
   set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Teem_DIR};teem;ALL;/")
 endif()
 
 # -------------------------------------------------------------------------
 # Install BatchMake
 # -------------------------------------------------------------------------
-if(EXISTS "${BatchMake_DIR}/CMakeCache.txt")
+if(NOT "${BatchMake_DIR}" STREQUAL "" AND EXISTS "${BatchMake_DIR}/CMakeCache.txt")
   set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${BatchMake_DIR};BatchMake;Runtime;/")
 endif()
 
 # -------------------------------------------------------------------------
 # Install OpenIGTLink
 # -------------------------------------------------------------------------
-if(EXISTS "${OpenIGTLink_DIR}/CMakeCache.txt")
+if(NOT "${OpenIGTLink_DIR}" STREQUAL "" AND EXISTS "${OpenIGTLink_DIR}/CMakeCache.txt")
   set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${OpenIGTLink_DIR};igtl;RuntimeLibraries;/")
 endif()
 
 # -------------------------------------------------------------------------
 # Install CTK
 # -------------------------------------------------------------------------
-if(EXISTS "${CTK_DIR}/CTK-build/CMakeCache.txt")
+if(NOT "${CTK_DIR}" STREQUAL "" AND EXISTS "${CTK_DIR}/CTK-build/CMakeCache.txt")
   set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${CTK_DIR}/CTK-build;CTK;Runtime;/")
 endif()
 
