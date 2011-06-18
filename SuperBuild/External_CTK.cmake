2198c018897866496c8dd3bb4e505350de984f47
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-18 03:34:18 +0000

    ENH: Update CTK tag
    The changes since b6304d3cd87acf3e0594 are listed below:
    
    2011-06-17  Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    
      * Libs/Widgets/ctkSliderWidget.cpp: Fix unused variable warning in
      ctkSliderWidget by commenting line
    
      * Utilities/DGraph/CMakeLists.txt: DGraph - Update CMakeLists so that DGraph
      depends only on QtCore
    
    2011-06-17  nherlambang <nherlambang@gmail.com>
    
      * Libs/DICOM/Widgets/ctkDICOMDatasetView.cpp: Keep intensity when
      ctkDICOMDatasetView display a sibling image
    
      * Libs/DICOM/Widgets/ctkDICOMDatasetView.cpp,
      Libs/DICOM/Widgets/ctkDICOMDatasetView.h: Implement left-drag intensity
      adjustment on ctkDICOMDatasetView
    
      * Libs/DICOM/Widgets/ctkDICOMThumbnailListWidget.cpp: Display middle-slice
      thumbnails on ctkDICOMThumbnailListWidget
    
      * Libs/DICOM/Core/ctkDICOMDatabase.cpp, Libs/DICOM/Core/ctkDICOMIndexer.cpp,
      Libs/DICOM/Core/ctkDICOMIndexer.h, Libs/DICOM/Widgets/ctkDICOMAppWidget.cpp:
      Use thumbnail generator in ctkDICOMIndexer
    
      * Libs/DICOM/Widgets/ctkDICOMThumbnailListWidget.cpp: Remove thumbnail
      generation in ctkDICOMThumbnailListWidget
    
      * Libs/DICOM/Core/CMakeLists.txt,
      .../Core/Testing/Cpp/ctkDICOMDatabaseTest1.cpp,
      .../Core/ctkDICOMAbstractThumbnailGenerator.cpp,
      .../Core/ctkDICOMAbstractThumbnailGenerator.h,
      Libs/DICOM/Core/ctkDICOMDatabase.cpp, Libs/DICOM/Core/ctkDICOMDatabase.h,
      Libs/DICOM/Core/ctkDICOMQuery.cpp, Libs/DICOM/Widgets/CMakeLists.txt,
      Libs/DICOM/Widgets/ctkDICOMAppWidget.cpp,
      Libs/DICOM/Widgets/ctkDICOMThumbnailGenerator.cpp,
      Libs/DICOM/Widgets/ctkDICOMThumbnailGenerator.h: Add thumbnail generator
      classes and re-implement thumbnail generation in ctkDICOMDatabase
    
      * .../Core/Testing/Cpp/ctkDICOMDatabaseTest1.cpp,
      Libs/DICOM/Core/ctkDICOMDatabase.cpp, Libs/DICOM/Core/ctkDICOMDatabase.h,
      Libs/DICOM/Core/ctkDICOMQuery.cpp, Libs/DICOM/Widgets/ctkDICOMAppWidget.cpp,
      Libs/DICOM/Widgets/ctkDICOMDatasetView.cpp,
      Libs/DICOM/Widgets/ctkDICOMThumbnailListWidget.cpp: Implement on-the-fly
      thumbnail generation to DICOM/Widgets and free ctkDICOMDatabase from using
      ctkDICOMImage
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17099
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17099 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -25,7 +25,7 @@ if(NOT DEFINED CTK_DIR)
   #message(STATUS "${__indent}Adding project ${proj}")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "b6304d3cd87acf3e05941fe1789809729608a1b5"
+    GIT_TAG "62628c0d561510cdf414"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
