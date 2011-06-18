97cb8ac005ff5b33a64f937c9f7118d5a9fca5d5
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-18 23:46:40 +0000

    ENH: Add SlicerMacroAddGenericCxxModuleTests  + add/update module tests
    * This macro allows to easily associate systematic test with a module
    
    * The file qSlicerModuleTest1.cxx.in is configured and compiled with each test suite
    
    * Existing module have been updated
    
    * Testing has been added to "untested" modules
    
    * Code within TractograpghyDisplay/CMakeLists.txt has been re-organized
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17121
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17121 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ CMake/SlicerMacroAddGenericCxxModuleTests.cmake
@@ -0,0 +1,34 @@
+
+#
+# SlicerMacroAddGenericCxxModuleTests
+#
+
+MACRO(SlicerMacroAddGenericCxxModuleTests MODULENAMES TEST_SRCS_OUTPUT_VAR TEST_NAMES_OUTPUT_VAR TEST_NAMES_CXX_OUTPUT_VAR)
+  # Sanity checks
+  set(expected_nonempty_vars MODULENAMES TEST_SRCS_OUTPUT_VAR TEST_NAMES_OUTPUT_VAR TEST_NAMES_CXX_OUTPUT_VAR)
+  foreach(var ${expected_nonempty_vars})
+    if("${var}" STREQUAL "")
+      message(FATAL_ERROR "error: Variable ${var} is empty !")
+    endif()
+  endforeach()
+  
+  FOREACH(MODULENAME ${MODULENAMES})
+    # Note: the variable MODULENAME is used to configure the different tests.
+    set(configured_test_src ${CMAKE_CURRENT_BINARY_DIR}/qSlicer${MODULENAME}ModuleTest1.cxx)
+    CONFIGURE_FILE(
+      ${Slicer_SOURCE_DIR}/Base/QTCore/Testing/qSlicerModuleTest1.cxx.in
+      ${configured_test_src}
+      @ONLY
+      )
+    
+    LIST(APPEND ${TEST_SRCS_OUTPUT_VAR} ${configured_test_src})
+
+    get_filename_component(test_name ${configured_test_src} NAME_WE)
+    LIST(APPEND ${TEST_NAMES_OUTPUT_VAR} ${test_name})
+
+    get_filename_component(test_filename ${configured_test_src} NAME)
+    LIST(APPEND ${TEST_NAMES_CXX_OUTPUT_VAR} ${test_filename})
+  ENDFOREACH()
+
+ENDMACRO()
+
