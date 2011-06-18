05ccc22f9d1541ac645131ae1df6853ca73f4681
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-18 09:44:58 +0000

    ENH: SlicerEnableExtensionTesting - Generate CTestConfig if it doesn't exist in source directory
    * Remove LoasableTemplateModule/CTestConfig.cmake
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17110
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17110 3bd1e089-480b-0410-8dfb-8563597acbee

--- Extensions/CMake/SlicerEnableExtensionTesting.cmake
+++ Extensions/CMake/SlicerEnableExtensionTesting.cmake
@@ -1,4 +1,18 @@
 
+# If no CTestConfig.cmake file is found in source directory,
+# one will be generated.
+IF(NOT EXISTS "${CMAKE_SOURCE_DIR}/CTestConfig.cmake")
+  MESSAGE(STATUS "CTestConfig.cmake has been written to: ${CMAKE_SOURCE_DIR}")
+  FILE(WRITE ${CMAKE_SOURCE_DIR}/CTestConfig.cmake
+"set(CTEST_PROJECT_NAME \"Slicer\")
+set(CTEST_NIGHTLY_START_TIME \"3:00:00 UTC\")
+
+set(CTEST_DROP_METHOD \"http\")
+set(CTEST_DROP_SITE \"www.cdash.org\")
+set(CTEST_DROP_LOCATION \"/slicer4/submit.php?project=Slicer4\")
+set(CTEST_DROP_SITE_CDASH TRUE)")
+ENDIF()
+
 SET_PROPERTY(GLOBAL PROPERTY CTEST_TARGETS_ADDED 1) # Do not add CTest default targets
 INCLUDE(CTest)
 
