e374623e8dc78ef0853854959508f09eebd3d348
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-19 01:57:08 +0000

    ENH: Update CTK tag
    The changes since 1b937b706e6a307d9 are list below:
    
    Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    Date:   Sat Jun 18 21:40:14 2011 -0400
    
        Add --config flag to CMAKE_COMMAND associated with CTK convenient target
    
        At the superbuild level, building the CTK target will allow
        to bypass the configure/build of the dependencies and will build only
        CTK inner project.
    
        This will be successful only if the project has been built at least once.
    
    Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    Date:   Sat Jun 18 21:37:25 2011 -0400
    
        Superbuild - Explicitly set BUILD_COMMAND associated with CTK-Build step.
    
        Setting CONFIGURE_COMMAND to an empty command is not enough to skip configuration step,
        the BUILD_COMMAND should also be specified explicitly otherwise it will in-conditionally
        default to 'make' on all platform.
    
        See _ep_get_configure_command_id and _ep_get_build_command in ExternalProject.cmake
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17135
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17135 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -25,7 +25,7 @@ if(NOT DEFINED CTK_DIR)
   #message(STATUS "${__indent}Adding project ${proj}")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "1b937b706e6a307d9"
+    GIT_TAG "ef4aa6a69680c482b817"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
