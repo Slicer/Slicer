18a8b7097af849003306fa3e88246535ad9da0ee
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-06-21 13:19:42 +0000

    ENH: Add ScriptedLoadableExtensionTemplate
    * This module is a basic hello world example illustrating how an
    scripted loadable extension could be implemented.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17162
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17162 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Extensions/Testing/ScriptedLoadableExtensionTemplate/ScriptedLoadableExtensionTemplate.py
@@ -0,0 +1,63 @@
+from __main__ import vtk, qt, ctk, slicer
+
+#
+# ScriptedLoadableExtensionTemplate
+#
+
+class ScriptedLoadableExtensionTemplate:
+  def __init__(self, parent):
+    parent.title = "Scripted Loadable Extension Template"
+    parent.category = "Examples"
+    parent.contributor = ""
+    parent.helpText = """
+    Example of scripted loadable extension.
+    """
+    parent.acknowledgementText = """
+    This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+and was partially funded by NIH grant 3P41RR013218-12S1.
+    """
+    self.parent = parent
+
+#
+# qScriptedLoadableExtensionTemplateWidget
+#
+
+class ScriptedLoadableExtensionTemplateWidget:
+  def __init__(self, parent = None):
+    if not parent:
+      self.parent = slicer.qMRMLWidget()
+      self.parent.setLayout(qt.QVBoxLayout())
+      self.parent.setMRMLScene(slicer.mrmlScene)
+    else:
+      self.parent = parent
+    self.layout = self.parent.layout()
+    if not parent:
+      self.setup()
+      self.parent.show()
+    
+  def setup(self):
+    # Instantiate and connect widgets ...
+    
+    # Collapsible button
+    dummyCollapsibleButton = ctk.ctkCollapsibleButton()
+    dummyCollapsibleButton.text = "A collapsible button"
+    self.layout.addWidget(dummyCollapsibleButton)
+    
+    # Layout within the dummy collapsible button
+    dummyFormLayout = qt.QFormLayout(dummyCollapsibleButton)
+    
+    # HelloWorld button
+    helloWorldButton = qt.QPushButton("Hello world")
+    helloWorldButton.toolTip = "Print 'Hello world' in standard ouput."
+    dummyFormLayout.addWidget(helloWorldButton)
+    helloWorldButton.connect('clicked(bool)', self.onHelloWorldButtonClicked)
+    
+    # Add vertical spacer
+    self.layout.addStretch(1)
+    
+    # Set local var as instance attribute
+    self.helloWorldButton = helloWorldButton
+    
+  def onHelloWorldButtonClicked(self):
+    print "Hello World !"
+
