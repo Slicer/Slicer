405e4234266b15d258acaa58f76eda008ebbbdf4
Steve Pieper <pieper@bwh.harvard.edu>
2011-06-23 17:23:59 +0000

    ENH: first pass at a template for creating editor extensions
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17201
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17201 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Extensions/Testing/EditorExtensionTemplate/EditorExtensionTemplate.py
@@ -0,0 +1,127 @@
+from __main__ import vtk, qt, ctk, slicer
+import EditorLib
+from EditorLib.EditOptions import HelpButton
+
+#
+# The Editor Extenion itself.
+# 
+# This needs to define the hooks to be come an editor effect.
+#
+
+class EditorExtensionTemplateOptions(EditorLib.LabelerOptions):
+  """ EditorExtensionTemplate-specfic gui
+  """
+
+  def __init__(self, parent=0):
+    super(EditorExtensionTemplateOptions,self).__init__(parent)
+    # self.attributes should be tuple of options:
+    # 'MouseTool' - grabs the cursor
+    # 'Nonmodal' - can be applied while another is active
+    # 'Disabled' - not available
+    self.attributes = ('MouseTool', 'Nonmodal')
+    self.displayName = 'Editor Extension Template'
+
+  def __del__(self):
+    super(EditorExtensionTemplateOptions,self).__del__()
+
+  def create(self):
+    super(EditorExtensionTemplateOptions,self).create()
+    self.apply = qt.QPushButton("Apply", self.frame)
+    self.apply.setToolTip("Apply the extension operation")
+    self.frame.layout().addWidget(self.apply)
+    self.widgets.append(self.apply)
+
+    HelpButton(self.frame, "This is a sample with no real functionality.")
+
+    self.apply.connect('clicked()', self.onApply)
+
+    # Add vertical spacer
+    self.frame.layout().addStretch(1)
+
+  def destroy(self):
+    super(EditorExtensionTemplateOptions,self).destroy()
+
+  # note: this method needs to be implemented exactly as-is
+  # in each leaf subclass so that "self" in the observer
+  # is of the correct type 
+  def updateParameterNode(self, caller, event):
+    nodeID = tcl('[EditorGetParameterNode] GetID')
+    node = slicer.mrmlScene.GetNodeByID(nodeID)
+    if node != self.parameterNode:
+      if self.parameterNode:
+        node.RemoveObserver(self.parameterNodeTag)
+      self.parameterNode = node
+      self.parameterNodeTag = node.AddObserver("ModifiedEvent", self.updateGUIFromMRML)
+
+  def setMRMLDefaults(self):
+    super(EditorExtensionTemplateOptions,self).setMRMLDefaults()
+
+  def updateGUIFromMRML(self,caller,event):
+    self.updatingGUI = True
+    super(EditorExtensionTemplateOptions,self).updateGUIFromMRML(caller,event)
+    self.updatingGUI = False
+
+  def onApply(self):
+    print('This is just an example - nothing here yet')
+
+  def updateMRMLFromGUI(self):
+    if self.updatingGUI:
+      return
+    disableState = self.parameterNode.GetDisableModifiedEvent()
+    self.parameterNode.SetDisableModifiedEvent(1)
+    super(EditorExtensionTemplateOptions,self).updateMRMLFromGUI()
+    self.parameterNode.SetDisableModifiedEvent(disableState)
+    if not disableState:
+      self.parameterNode.InvokePendingModifiedEvent()
+
+#
+# EditorExtensionTemplate
+#
+
+class EditorExtensionTemplate:
+  """
+  This class is the 'hook' for slicer to detect and recognize the extension
+  as a loadable scripted module
+  """
+  def __init__(self, parent):
+    parent.title = "Editor Extension Template"
+    parent.category = "Editor Extensions"
+    parent.contributor = "Steve Pieper"
+    parent.helpText = """
+    Example of an editor extension.  No module interface here, only in the Editor module
+    """
+    parent.acknowledgementText = """
+    This editor extension was developed by 
+    <Author>, <Institution>
+    based on work by:
+    Steve Pieper, Isomics, Inc.
+    based on work by:
+    Jean-Christophe Fillion-Robin, Kitware Inc.
+    and was partially funded by NIH grant 3P41RR013218.
+    """
+
+    # TODO:
+    # don't show this module - it only appears in the Editor module
+    parent.hidden = True
+
+    slicer.modules.editorExtensions['EditorExtensionTemplateOptions'] = EditorExtensionTemplateOptions
+
+#
+# EditorExtensionTemplate
+#
+
+class EditorExtensionTemplateWidget:
+  def __init__(self, parent = None):
+    self.parent = parent
+    
+  def setup(self):
+    # don't display anything for this widget - it will be hidden anyway
+    pass
+
+  def enter(self):
+    pass
+    
+  def exit(self):
+    pass
+
+
