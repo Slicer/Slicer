5b90a466a3f95cc3dc708f09e195840c210514f7
Daniel Haehn <haehn@bwh.harvard.edu>
2011-07-01 21:40:24 +0000

    ENH: Possibility check before accessing the factoryManager of the moduleManager when initializing python.
        # if the qSlicerApplication is only minimally initialized, the factoryManager does not exist.
        # this would be the case if f.e. a commandline module wants to use qSlicerApplication for tcl
        # access but without all the managers.
    
    
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17296
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17296 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Python/slicer/slicerqt.py
+++ Base/Python/slicer/slicerqt.py
@@ -1,7 +1,7 @@
 import vtk, qt, ctk
 import slicer
 
-from slicer.util import *  
+from slicer.util import *
 import slicer.cli
 
 #
@@ -12,67 +12,73 @@ import slicer.cli
 def loadSlicerRCFile():
   """If it exists, execute slicer resource script '~/.slicerrc.py'"""
   import os.path
-  rcfile = os.path.expanduser('~/.slicerrc.py')
-  if os.path.isfile(rcfile):
-    print 'Loading Slicer RC file [%s]' % (rcfile)
-    execfile(rcfile)
-    
+  rcfile = os.path.expanduser( '~/.slicerrc.py' )
+  if os.path.isfile( rcfile ):
+    print 'Loading Slicer RC file [%s]' % ( rcfile )
+    execfile( rcfile )
+
 #
 # Internal
 #
 
 class _Internal():
 
-  def __init__(self):
+  def __init__( self ):
     import imp
-    
+
     # Set attribute 'slicer.app'
-    setattr(slicer, 'app', _qSlicerCoreApplicationInstance)
-    
+    setattr( slicer, 'app', _qSlicerCoreApplicationInstance )
+
     # Listen factory and module manager to update slicer.{modules, moduleNames} when appropriate
     moduleManager = slicer.app.moduleManager()
-    factoryManager = moduleManager.factoryManager()
-    factoryManager.connect('allModulesRegistered()', self.setSlicerModuleNames)
-    moduleManager.connect('moduleLoaded(qSlicerAbstractCoreModule*)', self.setSlicerModules)
+
+    # if the qSlicerApplication is only minimally initialized, the factoryManager does not exist.
+    # this would be the case if f.e. a commandline module wants to use qSlicerApplication for tcl
+    # access but without all the managers.
+    # this is not the default behavior.
+    if hasattr( moduleManager, 'factoryManager' ):
+      factoryManager = moduleManager.factoryManager()
+      factoryManager.connect( 'allModulesRegistered()', self.setSlicerModuleNames )
+      moduleManager.connect( 'moduleLoaded(qSlicerAbstractCoreModule*)', self.setSlicerModules )
 
     # Add module 'slicer.moduleNames'
-    _moduleNames = imp.new_module('moduleNames')
-    setattr(slicer, _moduleNames.__name__, _moduleNames)
+    _moduleNames = imp.new_module( 'moduleNames' )
+    setattr( slicer, _moduleNames.__name__, _moduleNames )
 
     # Add module 'slicer.modules'
-    _modules = imp.new_module('modules')
-    setattr(slicer, _modules.__name__, _modules)
+    _modules = imp.new_module( 'modules' )
+    setattr( slicer, _modules.__name__, _modules )
 
     # Retrieve current instance of the scene and set 'slicer.mrmlScene'
-    setattr(slicer, 'mrmlScene', slicer.app.mrmlScene())
-    
+    setattr( slicer, 'mrmlScene', slicer.app.mrmlScene() )
+
     # HACK - Since qt.QTimer.singleShot is both a property and a static method, the property
     # is wrapped in python and prevent the call to the convenient static method having
     # the same name. To fix the problem, let's overwrite it's value.
     # Ideally this should be fixed in PythonQt itself.
-    def _singleShot(msec, receiverOrCallable, member = None):
+    def _singleShot( msec, receiverOrCallable, member=None ):
       """Calls either a python function or a slot after a given time interval."""
       # Add 'moduleManager' as parent to prevent the premature destruction of the timer.
       # Doing so, we ensure that the QTimer will be deleted before PythonQt is cleanup.
       # Indeed, the moduleManager is destroyed before the pythonManager.
-      timer = qt.QTimer(slicer.app.moduleManager())
-      timer.setSingleShot(True)
-      if callable(receiverOrCallable):
-        timer.connect("timeout()", receiverOrCallable)
+      timer = qt.QTimer( slicer.app.moduleManager() )
+      timer.setSingleShot( True )
+      if callable( receiverOrCallable ):
+        timer.connect( "timeout()", receiverOrCallable )
       else:
-        timer.connect("timeout()", receiverOrCallable, member)
-      timer.start(msec)
+        timer.connect( "timeout()", receiverOrCallable, member )
+      timer.start( msec )
 
-    qt.QTimer.singleShot = staticmethod(_singleShot)
-  
-  def setSlicerModuleNames(self):
+    qt.QTimer.singleShot = staticmethod( _singleShot )
+
+  def setSlicerModuleNames( self ):
     """Add module names as attributes of module slicer.moduleNames"""
     for name in moduleNames():
-      setattr(slicer.moduleNames, name, name)
+      setattr( slicer.moduleNames, name, name )
 
-  def setSlicerModules(self, module):
+  def setSlicerModules( self, module ):
     """Add modules as attributes of module slicer.modules"""
-    setattr(slicer.modules, module.name, module)
+    setattr( slicer.modules, module.name, module )
 
 
 _internalInstance = _Internal()
