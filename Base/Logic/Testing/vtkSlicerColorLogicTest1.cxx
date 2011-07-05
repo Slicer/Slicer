b4e2c05585314fed3e1b80d210dc55412de34fa0
Benjamin Long <benjamin.long@kitware.com>
2011-07-05 21:40:15 +0000

    ENH: Add test for vtkSlicerColorLogic.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17338
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17338 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/Logic/Testing/vtkSlicerColorLogicTest1.cxx
@@ -0,0 +1,62 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) 2010 Kitware Inc.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Julien Finet, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+// MRMLLogic includes
+#include "vtkSlicerColorLogic.h"
+
+// MRML includes
+
+// VTK includes
+#include <vtkSmartPointer.h>
+#include <vtkTimerLog.h>
+#include <vtksys/SystemTools.hxx>
+
+// STD includes
+#include <stdlib.h>
+#include <iostream>
+
+#include "TestingMacros.h"
+
+int vtkSlicerColorLogicTest1(int , char * [] )
+{
+  // To load the freesurfer file, SLICER_HOME is requested
+  //vtksys::SystemTools::PutEnv("SLICER_HOME=..." );
+  vtkSmartPointer<vtkMRMLScene> scene = vtkSmartPointer<vtkMRMLScene>::New();
+  vtkSlicerColorLogic* colorLogic = vtkSlicerColorLogic::New();
+  colorLogic->SetDebug(1);
+
+  vtkSmartPointer<vtkTimerLog> overallTimer = vtkSmartPointer<vtkTimerLog>::New();
+  overallTimer->StartTimer();
+
+  colorLogic->SetMRMLScene(scene);
+
+  overallTimer->StopTimer();
+  std::cout << "AddDefaultColorNodes: " << overallTimer->GetElapsedTime() << "s"
+            << " " << 1. / overallTimer->GetElapsedTime() << "fps" << std::endl;
+  overallTimer->StartTimer();
+
+  colorLogic->Delete();
+
+  std::cout << "RemoveDefaultColorNodes: " << overallTimer->GetElapsedTime() << "s"
+            << " " << 1. / overallTimer->GetElapsedTime() << "fps" << std::endl;
+
+  return EXIT_SUCCESS;
+}
+
