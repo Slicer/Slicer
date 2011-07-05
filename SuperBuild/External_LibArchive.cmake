404ec04a5e36b64ac3533b5ae667960d7e756090
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-07-05 22:54:24 +0000

    ENH: A different LibArchive external project can now be specified
    * This can be achieved configuring Slicer passing LibArchive_DIR
    
    * LibArchive_DIR should points to an installed tree of LibArchive
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17341
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17341 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_LibArchive.cmake
+++ SuperBuild/External_LibArchive.cmake
@@ -9,14 +9,6 @@ set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
 # Set dependency list
 set(LibArchive_DEPENDENCIES "")
 
-if(WIN32)
-  set(LibArchive_URL http://libarchive.googlecode.com/files/libarchive-2.8.4.zip)
-  set(LibArchive_MD5 bb416ba2e35693394a546e460797d87e)
-else()
-  set(LibArchive_URL http://libarchive.googlecode.com/files/libarchive-2.7.1.tar.gz)
-  set(LibArchive_MD5 f43382413b4457d0e192771b100a66e7)
-endif()
-
 # Include dependent projects if any
 SlicerMacroCheckExternalProjectDependency(LibArchive)
 set(proj LibArchive)
@@ -30,38 +22,39 @@ if(APPLE)
     -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
 endif()
 
-#message(STATUS "${__indent}Adding project ${proj}")
-
-ExternalProject_Add(${proj}
-  URL ${LibArchive_URL}
-  URL_MD5 ${LibArchive_MD5}
-  SOURCE_DIR LibArchive
-  BINARY_DIR LibArchive-build
-  INSTALL_DIR LibArchive-install
-  CMAKE_GENERATOR ${gen}
-  CMAKE_ARGS
-    ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-    -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
-    -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
-    -DBUILD_SHARED_LIBS:BOOL=ON
-    -DENABLE_TAR_SHARED:BOOL=ON
-    -DENABLE_ACL:BOOL=OFF
-    -DENABLE_TEST:BOOL=OFF
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-  DEPENDS
-    ${LibArchive_DEPENDENCIES}
-)
-
-set(LibArchive_DIR ${CMAKE_BINARY_DIR}/LibArchive-install)
-set(LibArchive_INCLUDE_DIR ${LibArchive_DIR}/include)
-set(LibArchive_LIBRARY)
-
-if(WIN32)
-  set(LibArchive_LIBRARY ${LibArchive_DIR}/lib/archive.lib)
-elseif(APPLE)
-  set(LibArchive_LIBRARY ${CMAKE_BINARY_DIR}/libarchive-build/libarchive/libarchive.dylib)
+if(NOT DEFINED qCDashAPI_DIR)
+  #message(STATUS "${__indent}Adding project ${proj}")
+  if(WIN32)
+    set(LibArchive_URL http://libarchive.googlecode.com/files/libarchive-2.8.4.zip)
+    set(LibArchive_MD5 bb416ba2e35693394a546e460797d87e)
+  else()
+    set(LibArchive_URL http://libarchive.googlecode.com/files/libarchive-2.7.1.tar.gz)
+    set(LibArchive_MD5 f43382413b4457d0e192771b100a66e7)
+  endif()
+  ExternalProject_Add(${proj}
+    URL ${LibArchive_URL}
+    URL_MD5 ${LibArchive_MD5}
+    SOURCE_DIR libarchive
+    BINARY_DIR libarchive-build
+    INSTALL_DIR libarchive-install
+    CMAKE_GENERATOR ${gen}
+    CMAKE_ARGS
+      ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
+      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+      -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+      -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
+      -DBUILD_SHARED_LIBS:BOOL=ON
+      -DENABLE_TAR_SHARED:BOOL=ON
+      -DENABLE_ACL:BOOL=OFF
+      -DENABLE_TEST:BOOL=OFF
+      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+    DEPENDS
+      ${LibArchive_DEPENDENCIES}
+    )
+  set(LibArchive_DIR ${CMAKE_BINARY_DIR}/libarchive-install)
 else()
-  set(LibArchive_LIBRARY ${LibArchive_DIR}/lib/libarchive.so)
+  # The project is provided using LibArchive_DIR, nevertheless since other project may depend on libarchive,
+  # let's add an 'empty' one
+  SlicerMacroEmptyExternalProject(${proj} "${LibArchive_DEPENDENCIES}")
 endif()
 
