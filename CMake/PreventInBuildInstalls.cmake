955cd1b8b655ed6cccf649a6dd5662f3311442eb
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-07-06 23:56:13 +0000

    ENH: Prevent in build installs
    * Adapated from ITKv4/CMake/PreventInBuildInstalls.cmake
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17362
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17362 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ CMake/PreventInBuildInstalls.cmake
@@ -0,0 +1,10 @@
+# Adapated from ITKv4/CMake/PreventInBuildInstalls.cmake
+string(TOLOWER "${CMAKE_INSTALL_PREFIX}" _PREFIX)
+string(TOLOWER "${Slicer_BINARY_DIR}" _BUILD)
+if("${_PREFIX}" STREQUAL "${_BUILD}")
+  message(FATAL_ERROR
+    "The current CMAKE_INSTALL_PREFIX points at the build tree:\n"
+    "  ${CMAKE_INSTALL_PREFIX}\n"
+    "This is not supported."
+    )
+endif()
