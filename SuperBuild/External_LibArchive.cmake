75ba00e81961f858f8ec1e5540ef0f2c490c96a7
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-07-07 20:16:34 +0000

    COMP: Use revision 3461 of LibArchive
    * It resumes that LibArchive compiles properly on Windows, Unix and MacOsx
    
    * Issue such as "warning treated as errors" should also be solved
    
    * Unneeded options are now explicitly disabled
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17382
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17382 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_LibArchive.cmake
+++ SuperBuild/External_LibArchive.cmake
@@ -25,11 +25,11 @@ endif()
 if(NOT DEFINED LibArchive_DIR)
   #message(STATUS "${__indent}Adding project ${proj}")
   # Note: On windows, version smaller than 2.8.4 can't be built on 64bits architecture.
-  set(LibArchive_URL http://svn.slicer.org/Slicer3-lib-mirrors/trunk/libarchive-2.8.4-backported-r3295.tgz)
-  set(LibArchive_MD5 4b01697777fe8e5f565353474951e276)
+  set(LibArchive_SVN_REPOSITORY http://libarchive.googlecode.com/svn/trunk/)
+  set(LibArchive_SVN_REVISION -r 3461)
   ExternalProject_Add(${proj}
-    URL ${LibArchive_URL}
-    URL_MD5 ${LibArchive_MD5}
+    SVN_REPOSITORY ${LibArchive_SVN_REPOSITORY}
+    SVN_REVISION ${LibArchive_SVN_REVISION}
     SOURCE_DIR LibArchive
     BINARY_DIR LibArchive-build
     INSTALL_DIR LibArchive-install
@@ -40,9 +40,14 @@ if(NOT DEFINED LibArchive_DIR)
       -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
       -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
       -DBUILD_SHARED_LIBS:BOOL=ON
-      -DENABLE_TAR_SHARED:BOOL=ON
+      -DBUILD_TESTING:BOOL=ON
       -DENABLE_ACL:BOOL=OFF
+      -DENABLE_CPIO:BOOL=OFF
+      -DENABLE_ICONV:BOOL=OFF
+      -DENABLE_OPENSSL:BOOL=OFF
+      -DENABLE_TAR:BOOL=OFF
       -DENABLE_TEST:BOOL=OFF
+      -DENABLE_XATTR:BOOL=OFF
       -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
     DEPENDS
       ${LibArchive_DEPENDENCIES}
