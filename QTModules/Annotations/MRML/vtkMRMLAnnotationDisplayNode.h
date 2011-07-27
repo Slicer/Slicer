03c84931aeeaa191857af025ec76897cd04a696c
Daniel Haehn <haehn@bwh.harvard.edu>
2011-07-27 20:57:11 +0000

    ENH: Annotations: Preparation for "Super"-Attributes on the MRMLAnnotationDisplayNode which get propagated to all other displayNodes of the associated displayableNode.
     * Idea behind is to be able to set the display properties (colors etc.) for an annotation with one click for points, lines and text
     * The settings than overwrite the attributes of the point-, line- and text displayNodes (each annotation has 3 three displayNodes now)
     * In the future each Annotation will have another displayNode additional to the three which enables the propagation of colors
     * Also submitted an associated test
    
    
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17587
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17587 3bd1e089-480b-0410-8dfb-8563597acbee

--- QTModules/Annotations/MRML/vtkMRMLAnnotationDisplayNode.h
+++ QTModules/Annotations/MRML/vtkMRMLAnnotationDisplayNode.h
@@ -11,7 +11,70 @@
 
 #include "vtkMRML.h"
 #include "vtkMRMLModelDisplayNode.h"
+#include "vtkMRMLDisplayableNode.h"
+#include "vtkMRMLDisplayNode.h"
 #include "vtkSlicerAnnotationsModuleMRMLExport.h"
+
+#define vtkSetAndPropagateVector3Macro(name,type) \
+virtual void SetAndPropagateSuper##name (type _arg1, type _arg2, type _arg3) \
+  { \
+  vtkDebugMacro(<< this->GetClassName() << " (" << this << "): setting Super" << #name " to (" << _arg1 << "," << _arg2 << "," << _arg3 << ")"); \
+  if ((this->Super##name[0] != _arg1)||(this->Super##name[1] != _arg2)||(this->Super##name[2] != _arg3)) \
+    { \
+    this->name[0] = _arg1; \
+    this->name[1] = _arg2; \
+    this->name[2] = _arg3; \
+    this->Super##name[0] = _arg1; \
+    this->Super##name[1] = _arg2; \
+    this->Super##name[2] = _arg3; \
+    vtkMRMLDisplayableNode *displayableNode = this->GetDisplayableNode(); \
+    if (!displayableNode) \
+      { \
+      return; \
+      } \
+    for (int i=0; i<displayableNode->GetNumberOfDisplayNodes(); i++) \
+      { \
+      vtkMRMLDisplayNode *displayNode = displayableNode->GetNthDisplayNode(i); \
+      if (!displayNode || displayNode==this) \
+        { \
+        continue; \
+        } \
+      displayNode->Set##name(this->Super##name); \
+      } \
+    this->Modified(); \
+    } \
+  }; \
+virtual void SetAndPropagateSuper##name (type _arg[3]) \
+  { \
+  this->SetAndPropagateSuper##name (_arg[0], _arg[1], _arg[2]);\
+  }
+
+#define vtkSetAndPropagateMacro(name,type) \
+virtual void SetAndPropagateSuper##name (type _arg) \
+  { \
+  vtkDebugMacro(<< this->GetClassName() << " (" << this << "): setting Super" << #name " to (" << _arg << ")"); \
+  if (this->Super##name != _arg) \
+    { \
+    this->name = _arg; \
+    this->Super##name = _arg; \
+    vtkMRMLDisplayableNode *displayableNode = this->GetDisplayableNode(); \
+    if (!displayableNode) \
+      { \
+      return; \
+      } \
+    for (int i=0; i<displayableNode->GetNumberOfDisplayNodes(); i++) \
+      { \
+      vtkMRMLDisplayNode *displayNode = displayableNode->GetNthDisplayNode(i); \
+      if (!displayNode || displayNode==this) \
+        { \
+        continue; \
+        } \
+      displayNode->Set##name(this->Super##name); \
+      } \
+    this->Modified(); \
+    } \
+  }; \
+
 
 /// \ingroup Slicer_QtModules_Annotation
 class  VTK_SLICER_ANNOTATIONS_MODULE_MRML_EXPORT vtkMRMLAnnotationDisplayNode : public vtkMRMLModelDisplayNode
@@ -62,6 +125,36 @@ class  VTK_SLICER_ANNOTATIONS_MODULE_MRML_EXPORT vtkMRMLAnnotationDisplayNode : 
   /// Returns the associated backup of this node
   vtkMRMLAnnotationDisplayNode * GetBackup();
 
+  //
+  // Set and propagate attributes to all other displayNodes
+  //
+
+  vtkSetAndPropagateVector3Macro(Color, double);
+  vtkGetVector3Macro(SuperColor, double);
+
+  vtkSetAndPropagateVector3Macro(SelectedColor, double);
+  vtkGetVector3Macro(SuperSelectedColor, double);
+
+  vtkSetAndPropagateMacro(Opacity, double);
+  vtkGetMacro(SuperOpacity, double);
+
+  vtkSetAndPropagateMacro(Ambient, double);
+  vtkGetMacro(SuperAmbient, double);
+
+  vtkSetAndPropagateMacro(Diffuse, double);
+  vtkGetMacro(SuperDiffuse, double);
+
+  vtkSetAndPropagateMacro(Specular, double);
+  vtkGetMacro(SuperSpecular, double);
+
+  vtkSetAndPropagateMacro(Power, double);
+  vtkGetMacro(SuperPower, double);
+
+  vtkSetAndPropagateMacro(SelectedAmbient, double);
+  vtkGetMacro(SuperSelectedAmbient, double);
+
+  vtkSetAndPropagateMacro(SelectedSpecular, double);
+  vtkGetMacro(SuperSelectedSpecular, double);
 
  protected:
   vtkMRMLAnnotationDisplayNode();
@@ -70,6 +163,18 @@ class  VTK_SLICER_ANNOTATIONS_MODULE_MRML_EXPORT vtkMRMLAnnotationDisplayNode : 
   void operator= ( const vtkMRMLAnnotationDisplayNode& );
 
   vtkMRMLAnnotationDisplayNode* m_Backup;
+
+  double SuperColor[3];
+  double SuperSelectedColor[3];
+
+  double SuperOpacity;
+  double SuperAmbient;
+  double SuperDiffuse;
+  double SuperSpecular;
+  double SuperPower;
+  double SuperSelectedAmbient;
+  double SuperSelectedSpecular;
+
 
 };
 
