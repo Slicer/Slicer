cf9a84713d76db01ec6c7a37bd0d39a32ed0bb13
Julien Finet <julien.finet@kitware.com>
2011-08-04 21:23:53 +0000

    COMP: Disable all DCMTK 3rdparty libraries to avoid linker errors
    Errors caused when source code using DCMTK libraries do not know DCMTK's 3party library setup.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17673
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17673 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -34,7 +34,7 @@ if(NOT DEFINED CTK_DIR)
   #message(STATUS "${__indent}Adding project ${proj}")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "0d244df26dd99f2d8b78f945bb7e816ef590f7b2"
+    GIT_TAG "f40cc24ac065252f9f65dad8e758977c06bb913b"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
