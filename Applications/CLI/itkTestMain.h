1a3d42bd3340ae0eb9d394075634ecf2c6dd46c7
Hans Johnson <hans-johnson@uiowa.edu>
2011-08-19 01:12:45 +0000

    COMP:  Removing compiler warning about nested '/*'
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=17754
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@17754 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/CLI/itkTestMain.h
+++ Applications/CLI/itkTestMain.h
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *=========================================================================*
+ *=========================================================================*/
 /*=========================================================================
  *
  *  Portions of this file are subject to the VTK Toolkit Version 3 copyright.
