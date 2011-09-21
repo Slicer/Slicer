006c079c626c09497e5cbf76c22689e969c3de09
Steve Pieper <pieper@bwh.harvard.edu>
2011-09-21 18:53:23 +0000

    BUG: fix permissions so that installed helper apps can be executed
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18133
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18133 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallDCMTKApps.cmake
+++ CMake/SlicerBlockInstallDCMTKApps.cmake
@@ -10,9 +10,9 @@ set(DCMTK_Apps storescu storescp dcmdump)
 foreach(dcmtk_App ${DCMTK_Apps})
   if(APPLE)
     install(FILES ${CTK_DIR}/CMakeExternals/Install/bin/${dcmtk_App}
-      DESTINATION ${Slicer_BUNDLE_LOCATION}/bin COMPONENT Runtime)
+      DESTINATION USE_SOURCE_PERMISSIONS ${Slicer_BUNDLE_LOCATION}/bin COMPONENT Runtime)
   else()
     install(FILES ${CTK_DIR}/CMakeExternals/Install/bin/${dcmtk_App}${EXE}
-        DESTINATION bin COMPONENT Runtime)
+        DESTINATION USE_SOURCE_PERMISSIONS bin COMPONENT Runtime)
   endif()
 endforeach()
