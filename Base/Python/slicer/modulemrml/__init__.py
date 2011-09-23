03fc3975b8658c157c381df3ac3c860ccfd00d50
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-09-23 18:23:18 +0000

    BUG: Update module{logic, mrml}/__init__.py to look in the appropriate folder
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18172
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18172 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Python/slicer/modulemrml/__init__.py
+++ Base/Python/slicer/modulemrml/__init__.py
@@ -1,10 +1,13 @@
 """ This module loads the Slicer Module MRML vtk classes into its namespace."""
 
-from __main__ import _qSlicerCoreApplicationInstance
+from __main__ import _qSlicerCoreApplicationInstance as app
 from slicer.util import importVTKClassesFromDirectory
 
-directory = _qSlicerCoreApplicationInstance.slicerHome + "/qt-loadable-modules/Python"
+# HACK Ideally constant from vtkSlicerConfigure and vtkSlicerVersionConfigure should
+#      be wrapped.
+slicer_qt_loadable_modules_lib_subdir =  "/lib/Slicer-%d.%d/qt-loadable-modules" % (app.majorVersion, app.minorVersion)
+directory = app.slicerHome + slicer_qt_loadable_modules_lib_subdir + "/Python"
 importVTKClassesFromDirectory(directory, __name__, filematch = "vtkSlicer*ModuleMRML.py")
 
 # Removing things the user shouldn't have to see.
-del _qSlicerCoreApplicationInstance, importVTKClassesFromDirectory, directory
+del app, importVTKClassesFromDirectory, directory, slicer_qt_loadable_modules_lib_subdir
