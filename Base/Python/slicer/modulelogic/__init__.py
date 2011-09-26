d66811e1d81f52fc9d24dccba700ed837fe73a87
Demian Wassermann <demian@bwh.harvard.edu>
2011-09-26 12:32:27 +0000

    COMP: Modified __init__.py to use python's tools for OS compatibility in path names
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18180
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18180 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Python/slicer/modulelogic/__init__.py
+++ Base/Python/slicer/modulelogic/__init__.py
@@ -5,12 +5,13 @@ from qSlicerBaseQTCLIPython import vtkSlicerCLIModuleLogic
 
 from __main__ import _qSlicerCoreApplicationInstance as app
 from slicer.util import importVTKClassesFromDirectory
+from os import path
 
 # HACK Ideally constant from vtkSlicerConfigure and vtkSlicerVersionConfigure should
 #      be wrapped.
-slicer_qt_loadable_modules_lib_subdir =  "/lib/Slicer-%d.%d/qt-loadable-modules" % (app.majorVersion, app.minorVersion)
-directory = app.slicerHome + slicer_qt_loadable_modules_lib_subdir + "/Python"
+slicer_qt_loadable_modules_lib_subdir =  path.join("lib", "Slicer-%d.%d", "qt-loadable-modules") % (app.majorVersion, app.minorVersion)
+directory = path.join(app.slicerHome, slicer_qt_loadable_modules_lib_subdir, "Python")
 importVTKClassesFromDirectory(directory, __name__, filematch = "vtkSlicer*ModuleLogic.py")
 
 # Removing things the user shouldn't have to see.
-del app, importVTKClassesFromDirectory, directory, slicer_qt_loadable_modules_lib_subdir
+del app, importVTKClassesFromDirectory, directory, slicer_qt_loadable_modules_lib_subdir, path
