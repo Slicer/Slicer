62ecad033dcca4305b7192ebd469f3dbe6a1d931
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-10-06 22:03:10 +0000

    ENH: Add SlicerFunctionMIDASCTestUploadURL
    Compute and upload to CTest the URL of the 'filepath' that has been
    previously uploaded to MIDAS.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18288
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18288 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ CMake/SlicerFunctionMIDASCTestUploadURL.cmake
@@ -0,0 +1,49 @@
+################################################################################
+#
+#  Program: 3D Slicer
+#
+#  Copyright (c) Kitware Inc.
+#
+#  See COPYRIGHT.txt
+#  or http://www.slicer.org/copyright/copyright.txt for details.
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+#  and was partially funded by NIH grant 3P41RR013218-12S1
+#
+################################################################################
+
+#
+# SlicerFunctionMIDASCTestUploadURL
+#
+
+#
+# filepath - Compute and upload to CTest the URL of the 'filepath' that has been previously 
+#            uploaded to MIDAS.
+#
+function(SlicerFunctionMIDASCTestUploadURL filepath)
+
+  if(NOT EXISTS "${filepath}")
+    message(FATAL_ERROR "error: filepath doesn't exist ! [${filepath}]")
+  endif()
+  
+  if("${MIDAS_PACKAGE_URL}" STREQUAL "")
+    message(FATAL_ERROR "error: MIDAS_PACKAGE_URL is either not defined or set to an empty string !")
+  endif()
+  
+  get_filename_component(filename ${filepath} NAME)
+
+  execute_process(COMMAND ${CMAKE_COMMAND} -E md5sum ${filepath} OUTPUT_VARIABLE output)
+  string(SUBSTRING "${output}" 0 32 computedChecksum)
+  
+  set(url ${MIDAS_PACKAGE_URL}/api/rest?method=midas.bitstream.download&name=${filename}&checksum=${computedChecksum})
+  
+  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/slicerFunctionCTestUploadURL.url "${url}")
+  ctest_upload(FILES ${CMAKE_CURRENT_BINARY_DIR}/slicerFunctionCTestUploadURL.url)
+endfunction()
+
