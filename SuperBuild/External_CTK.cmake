2bfe3b196afa058554ff1c92808a605f64e722af
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-10-18 19:09:17 +0000

    COMP: Update CTK tag to consider install rule fix
    See https://github.com/commontk/CTK/commit/d9e405671c07218999e5a986c2e29d0005459fd2
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18358
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18358 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -35,7 +35,7 @@ if(NOT DEFINED CTK_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "5c6d3019fa97c389b34cff2178ee91e8929acace"
+    GIT_TAG "41da2a1da866c149bfe1b0d6fa91d58d63adf450"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
