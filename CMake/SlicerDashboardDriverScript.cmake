c43930d4907677435b6c0e0d3fcc18762103a04a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-10-18 23:08:09 +0000

    COMP: DashboardDriverScript - Append "-Packages" to track name when WITH_PACKAGES is ON
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18362
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18362 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerDashboardDriverScript.cmake
+++ CMake/SlicerDashboardDriverScript.cmake
@@ -87,7 +87,11 @@ elseif(SCRIPT_MODE STREQUAL "nightly")
 else()
   message(FATAL_ERROR "Unknown script mode: '${SCRIPT_MODE}'. Script mode should be either 'experimental', 'continuous' or 'nightly'")
 endif()
-set(track ${model}${CTEST_TRACK_SUFFIX})
+set(track ${model})
+if(WITH_PACKAGES)
+  set(track "${track}-Packages")
+endif()
+set(track ${CTEST_TRACK_PREFIX}${track}${CTEST_TRACK_SUFFIX})
 
 # For more details, see http://www.kitware.com/blog/home/post/11
 set(CTEST_USE_LAUNCHERS 0)
