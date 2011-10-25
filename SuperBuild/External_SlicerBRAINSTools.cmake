9b29b65683ab76921daa82982e64881beedd4337
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-10-25 20:20:53 +0000

    COMP: BRAINS - Update Git tag to include 'compilation-windows-fix'
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18462
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18462 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_SlicerBRAINSTools.cmake
+++ SuperBuild/External_SlicerBRAINSTools.cmake
@@ -31,7 +31,7 @@ if(NOT DEFINED SlicerBRAINSTools_SOURCE_DIR)
   #message(STATUS "${__indent}Adding project ${proj}")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/jcfr/BRAINSStandAlone.git"
-    GIT_TAG "073cd81fa4c95fdbef6afab903dbbc859264695d"
+    GIT_TAG "c5cf4d37457aa36a60bb1efafdbffc7f345bf711"
     SOURCE_DIR ${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
