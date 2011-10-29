1e990a283d774f6a2d1f1a4fa26cd2820a4b2d94
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-10-29 01:27:51 +0000

    COMP: All dashboards will now be submitted to a single location.
    * See http://slicer.cdash.org/index.php?project=Slicer4
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18531
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18531 3bd1e089-480b-0410-8dfb-8563597acbee

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,12 +2,7 @@ set(CTEST_PROJECT_NAME "Slicer")
 set(CTEST_NIGHTLY_START_TIME "3:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Slicer4")
+set(CTEST_DROP_SITE "slicer.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=Slicer4")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
-# Since the instance of CDash available on www.cdash.org is 1.8.1, let's use
-# Slicer4 dedicated instance of CDash in case the command 'ctest_upload' is available.
-if((WITH_PACKAGES OR Slicer_BUILD_EXTENSIONS) AND COMMAND ctest_upload)
-  set(CTEST_DROP_LOCATION "/slicer4/submit.php?project=Slicer4")
-endif()
