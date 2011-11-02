43c91980374b62caa7a1b1fa6aa4f6c8de63052f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-11-02 23:06:44 +0000

    BUG: Use the full CDash drop_site/drop_location URL instead of the convenient redirect. See CDash bug #12555
    See http://public.kitware.com/Bug/view.php?id=12555
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18569
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18569 3bd1e089-480b-0410-8dfb-8563597acbee

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,7 +2,10 @@ set(CTEST_PROJECT_NAME "Slicer")
 set(CTEST_NIGHTLY_START_TIME "3:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "slicer.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=Slicer4")
+# See this issue for more details: http://public.kitware.com/Bug/view.php?id=12555
+#set(CTEST_DROP_SITE "slicer.cdash.org")
+#set(CTEST_DROP_LOCATION "/submit.php?project=Slicer4")
+set(CTEST_DROP_SITE "www.cdash.org")
+set(CTEST_DROP_LOCATION "/slicer4/submit.php?project=Slicer4")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
