b4c19a25ebb4b07a9002bfb9ae0d5c768790ebd8
Fedorov <fedorov@bwh.harvard.edu>
2011-11-15 15:42:05 +0000

    ENH: update git tag
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18688
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18688 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_ChangeTrackerPy.cmake
+++ SuperBuild/External_ChangeTrackerPy.cmake
@@ -22,7 +22,7 @@ if(NOT DEFINED ChangeTrackerPy_SOURCE_DIR)
   #message(STATUS "${__indent}Adding project ${proj}")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/fedorov/ChangeTrackerPy.git"
-    GIT_TAG "e624450d0c51017ce320f59a8795c40243b0a376"
+    GIT_TAG "75a1aed8590fd074e11f8ee7e13da5b797cba83a"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
