d54b5b59180ea518ed260ef5a0cfbf7e4039b59b
Nicole Aucoin <nicole@bwh.harvard.edu>
2011-11-25 23:20:08 +0000

    STYLE: updated wiki link and help text
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18770
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18770 3bd1e089-480b-0410-8dfb-8563597acbee

--- QTModules/Colors/qSlicerColorsModule.cxx
+++ QTModules/Colors/qSlicerColorsModule.cxx
@@ -113,22 +113,21 @@ QString qSlicerColorsModule::helpText()const
 {
   QString help =
     "The <b>Colors Module</b> manages color look up tables.<br>"
-    "For more information see <a href=\"%1/Modules:Colors-Documentation-3.6\">"
-    "%1/Modules:Colors-Documentation-3.6</a><br>"
+    "For more information see <a href=\"%1/Documentation/4.0/Modules/Colors\">"
+    "%1/Documentation/4.0/Modules/Colors</a><br>"
     "Tables are used by mappers to translate between an integer and a colour "
     "value for display of models and volumes.<br>Slicer supports three kinds "
     "of tables:<br>"
     "1. Continuous scales, like the greyscale table.<br>"
     "2. Parametric tables, defined by an equation, such as the FMRIPA table.<br>"
     "3. Discrete tables, such as those read in from a file.<br><br>"
-    "You can specify a directory from which to read color files using the "
-    "View -> Application Settings window, Module Settings frame, in the "
-    "User defined color file paths section. The color file format is a plain "
-    "text file with the .txt or .ctbl extension. Each line in the file has:<br>"
-    "label\tname\tR\tG\tB\tA\nlabel is an integer, name a string, and RGBA are "
-    "0-255.<br><br>Users are only allowed to edit User type tables. Use the "
-    "Edit frame to create a new color table (you can copy from an existing "
-    "one), and save it to a file using the File, Save interface.";
+    "You can create a duplicate of a color table to allow editing the "
+    "names and values and color by clicking on the folder+ icon.<br>"
+    "You can then save the new color table via the File -> Save interface.<br>"
+    "The color file format is a plain text file with the .txt or .ctbl extension. "
+    "Each line in the file has:<br>"
+    "label\tname\tR\tG\tB\tA<br>label is an integer, name a string, and RGBA are "
+    "0-255.";
   return help.arg(this->slicerWikiUrl());
 }
 
