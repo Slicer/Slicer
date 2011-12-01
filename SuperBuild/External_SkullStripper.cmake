88b76e142c930322ac3567e43bab3630cb83d81e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-01 15:53:29 +0000

    COMP: Add missing External_SkullStripper.cmake
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18789
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18789 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ SuperBuild/External_SkullStripper.cmake
@@ -0,0 +1,43 @@
+
+# Make sure this file is included only once
+get_filename_component(CMAKE_CURRENT_LIST_FILENAME ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
+if(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED)
+  return()
+endif()
+set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
+
+# Sanity checks
+if(DEFINED SkullStripper_SOURCE_DIR AND NOT EXISTS ${SkullStripper_SOURCE_DIR})
+  message(FATAL_ERROR "SkullStripper_SOURCE_DIR variable is defined but corresponds to non-existing directory")
+endif()
+
+# Set dependency list
+set(SkullStripper_DEPENDENCIES ${ITK_EXTERNAL_NAME} SlicerExecutionModel VTK)
+
+# Include dependent projects if any
+SlicerMacroCheckExternalProjectDependency(SkullStripper)
+set(proj SkullStripper)
+
+if(NOT DEFINED SkullStripper_SOURCE_DIR)
+  #message(STATUS "${__indent}Adding project ${proj}")
+  ExternalProject_Add(${proj}
+    #SVN_REPOSITORY "http://svn.na-mic.org/NAMICSandBox/trunk/SkullStripper"
+    #SVN_REVISION -r "7735"
+    GIT_REPOSITORY "${git_protocol}://github.com/Slicer/SkullStripper.git"
+    GIT_TAG "33dfe1e5c43ef944b4f28a9ed93a5ffb39096220"
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BINARY_DIR ${proj}-build
+    CMAKE_GENERATOR ${gen}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    DEPENDS
+      ${SkullStripper_DEPENDENCIES}
+    )
+  set(SkullStripper_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
+else()
+  # The project is provided using SkullStripper_SOURCE_DIR, nevertheless since other project may depend on SkullStripper,
+  # let's add an 'empty' one
+  SlicerMacroEmptyExternalProject(${proj} "${SkullStripper_DEPENDENCIES}")
+endif()
+
