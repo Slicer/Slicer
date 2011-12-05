88943c153e4d0adfe672249e36d92bd128c825cc
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-05 23:19:06 +0000

    COMP: Removed deprecated tcl build scripts
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18814
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18814 3bd1e089-480b-0410-8dfb-8563597acbee

--- Scripts/generateListOfModules.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/csh
-./Slicer3 --clear-module-cache --verbose-module-discovery 2>&1 | tee listOfModules.txt
-grep "A module named" listOfModules.txt | grep -v already | grep -v Python > listOfUniqueModules.txt
-cut -d\" -f 2 listOfUniqueModules.txt >  listOfUniqueModulesIsolated.txt
-sort listOfUniqueModulesIsolated.txt > listOfUniqueModulesIsolatedSorted.txt
-
