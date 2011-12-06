8e635903f161db606b4d12815f6b7e3d0b7bbe3c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-06 01:17:47 +0000

    COMP: Remove Slicer3 DistanceTransformModel module source code
    The corresponding source code is still available in Slicer3 repository.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18834
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18834 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/DistanceTransformModel/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-if {[catch {package require Itcl}]} { return }
-package ifneeded DistanceTransformModel 3.0 [list 
-  source [file join $dir DistanceTransformModel.tcl]
-  source [file join $dir DistanceTransformModelNode.tcl]
-  source [file join $dir DistanceTransformModelLogic.tcl]
-  source [file join $dir DistanceTransformModelGUI.tcl]
-]
