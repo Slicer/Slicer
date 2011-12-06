de54cb8c85dc6cb42be154a06e20ab08b2328248
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-06 01:17:53 +0000

    COMP: Remove Slicer3 ClipModel module source code
    The corresponding source code is still available in Slicer3 repository.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18837
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18837 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/ClipModel/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-if {[catch {package require Itcl}]} { return }
-package ifneeded ClipModel 3.0 [list 
-  source [file join $dir ClipModel.tcl]
-  source [file join $dir ClipModelNode.tcl]
-  source [file join $dir ClipModelLogic.tcl]
-  source [file join $dir ClipModelGUI.tcl]
-]
