f010d894e3354983369fa6eaf498a2dc2a939a25
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-06 01:17:45 +0000

    COMP: Remove Slicer3 DistanceMapFiducials module source code
    The corresponding source code is still available in Slicer3 repository.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18833
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18833 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/DistanceMapFiducials/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-if {[catch {package require Itcl}]} { return }
-package ifneeded DistanceMapFiducials 3.0 [list 
-  source [file join $dir DistanceMapFiducials.tcl]
-  source [file join $dir DistanceMapFiducialsNode.tcl]
-  source [file join $dir DistanceMapFiducialsLogic.tcl]
-  source [file join $dir DistanceMapFiducialsGUI.tcl]
-]
