31b91a24a5912a77187c89373f2257f5605b61a4
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-06 01:17:59 +0000

    COMP: Remove Slicer3 FastMarchingSegmentation module source code
    The corresponding source code is still available in Slicer3 repository.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18840
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18840 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/FastMarchingSegmentation/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-if {[catch {package require Itcl}]} { return }
-package ifneeded FastMarchingSegmentation 3.0 [list 
-  source [file join $dir FastMarchingSegmentation.tcl]
-  source [file join $dir FastMarchingSegmentationNode.tcl]
-  source [file join $dir FastMarchingSegmentationLogic.tcl]
-  source [file join $dir FastMarchingSegmentationGUI.tcl]
-]
