577a9871bb75e985aea42e714860336dd3273344
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-06 01:18:05 +0000

    COMP: Remove Slicer3 ChangeTracker module source code
    The corresponding source code is still available in Slicer3 repository.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18841
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18841 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/ChangeTracker/Testing/ChangeTrackerTest.cxx
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "itkTestMain.h" 
-void RegisterTests()
-{
-    REGISTER_TEST(ChangeTrackerTest);
-}
-#undef main
-#define main ChangeTrackerTest
-#include "ChangeTrackerCommandLine.cxx"
--- Modules/ChangeTracker/vtkChangeTracker.h
+++
@@ -1,1 +0,0 @@
-#include "vtkChangeTrackerWin32Header.h"
