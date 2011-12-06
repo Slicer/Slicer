99fd30d9d5feed54dd80617186d9c754e029f6dc
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-06 01:17:49 +0000

    COMP: Remove Slicer3 CropVolume module source code
    The corresponding source code is still available in Slicer3 repository.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18835
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18835 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/CropVolume/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-if {[catch {package require Itcl}]} { return }
-package ifneeded CropVolume 3.0 [list 
-  source [file join $dir CropVolume.tcl]
-  source [file join $dir CropVolumeNode.tcl]
-  source [file join $dir CropVolumeLogic.tcl]
-  source [file join $dir CropVolumeGUI.tcl]
-]
