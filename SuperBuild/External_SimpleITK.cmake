d9f21c99db66c98a4c811e2a6b28b9499392e52a
Hans Johnson <hans-johnson@uiowa.edu>
2011-12-10 16:50:23 +0000

    COMP: Syncronized tag of SimpleITK with ITKv4
    SimpleITK and ITKv4 must use compatible versions.
    The recent update of ITKv4 to RC4 required updating
    SimpleITK to a newer tag as well.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18900
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18900 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_SimpleITK.cmake
+++ SuperBuild/External_SimpleITK.cmake
@@ -46,8 +46,10 @@ set(SimpleITK_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/Si
 ExternalProject_add(SimpleITK
   SOURCE_DIR SimpleITK
   BINARY_DIR SimpleITK-build
-  GIT_REPOSITORY http://itk.org/SimpleITK.git
-  GIT_TAG v0.3.0b
+  ## Tag of master branch on Dec. 10th 2011
+  #GIT_REPOSITORY http://itk.org/SimpleITK.git
+  GIT_REPOSITORY https://github.com/dblezek/SimpleITK
+  GIT_TAG "SIMPLEITK-204_UpdateITKTowardsRC4"
   UPDATE_COMMAND ""
   CMAKE_ARGS
     ${ep_common_args}
