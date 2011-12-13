46dd9991ba116b8d126d380412865e833fa00076
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-13 23:51:22 +0000

    ENH: Add test checking integration of EMSegmentInterface with qSlicerApplication
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18917
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18917 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/SlicerQT/Testing/Cpp/qSlicerApplicationTpyclEMSegmentIntegrationTest.cxx
@@ -0,0 +1,58 @@
+
+// CTK includes
+#include <ctkScopedCurrentDir.h>
+
+// Slicer includes
+#include "qSlicerApplication.h"
+#include "qSlicerCorePythonManager.h"
+
+// EMSegment includes
+#include "vtkSlicerCommonInterface.h"
+
+// STD includes
+#include <cstdio>
+
+// test the tcl adapter provided by the Slicer Common Interface
+int qSlicerApplicationTpyclEMSegmentIntegrationTest(int argc, char** argv)
+{
+  if (argc < 2)
+    {
+    std::cerr << "Usage:\n"
+              << "  " << argv[0] << " </path/to/EMSegmentCommandLine>" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  vtkSlicerCommonInterface* common = vtkSlicerCommonInterface::New();
+
+  // Slicer4
+  qSlicerApplication* app = new qSlicerApplication(argc, argv);
+
+#ifdef Slicer_USE_PYTHONQT_WITH_TCL
+  bool disablePython = qSlicerApplication::testAttribute(qSlicerApplication::AA_DisablePython);
+  if(!disablePython)
+    {
+    // Load EMSegment Python module(s)
+    QString emsegmentModulePath = QString::fromStdString(
+          vtkSlicerApplicationLogic::GetModuleSlicerXYLibDirectory(argv[1]));
+    emsegmentModulePath.append("/" Slicer_QTLOADABLEMODULES_SUBDIR);
+    app->pythonManager()->appendPythonPath(emsegmentModulePath);
+    // Update current application directory, so that *PythonD modules can be loaded
+    ctkScopedCurrentDir scopedCurrentDir(emsegmentModulePath);
+
+    QString emsegmentModulePythonPath = emsegmentModulePath + "/Python";
+    std::cout << "emsegmentModulePythonPath:" << qPrintable(emsegmentModulePythonPath) << std::endl;
+    app->pythonManager()->executeString(QString(
+      "from slicer.util import importVTKClassesFromDirectory;"
+      "importVTKClassesFromDirectory('%1', 'slicer.modulelogic', filematch='vtkSlicer%2ModuleLogic.py');"
+      "importVTKClassesFromDirectory('%1', 'slicer.modulemrml', filematch='vtkSlicer%2ModuleMRML.py');"
+      ).arg(emsegmentModulePythonPath).arg("EMSegment"));
+
+    }
+#endif
+
+  delete app;
+
+  common->Delete();
+
+  return EXIT_SUCCESS;
+}
