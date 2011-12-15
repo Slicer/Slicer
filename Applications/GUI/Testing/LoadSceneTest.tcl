8dfe483942c1cc0c08233255aa7c22801d733c3b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-15 07:45:21 +0000

    COMP: Remove unused directory - Applications/GUI
    The corresponding source code is still available in Slicer3 repository.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18930
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18930 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/GUI/Testing/LoadSceneTest.tcl
+++
@@ -1,9 +0,0 @@
-update
-set scenes {volScene.mrml cube.mrml cube.mrml volScene.mrml}
-foreach scene $scenes {
-  $::slicer3::MRMLScene SetURL $::SlicerHome/share/MRML/Testing/$scene
-  $::slicer3::MRMLScene Connect
-  update
-}
-
-after idle "$::slicer3::Application Exit"
--- Applications/GUI/Testing/TestScript.tcl
+++
@@ -1,6 +0,0 @@
-# this message goes to the tcl console and isn't seen on the output
-puts "hoot!"
-
-# just exit the program directly - this is the overridden version of
-# exit that waits for the application to shut down.
-exit 0
