7c3a3700c7b905190755d3ad0ced3e0a63ecd1a3
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-12-15 08:04:36 +0000

    BUG: Do no install LibArchive if Slicer_BUILD_EXTENSIONMANAGER_SUPPORT is OFF
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=18936
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@18936 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallPythonQt.cmake
+++ CMake/SlicerBlockInstallPythonQt.cmake
@@ -1,20 +1,17 @@
 # -------------------------------------------------------------------------
 # Find and install PythonQt
 # -------------------------------------------------------------------------
-if(Slicer_USE_PYTHONQT)
+set(PYTHONQT_INSTALL_LIB_DIR "${Slicer_INSTALL_LIB_DIR}")
 
-  set(PYTHONQT_INSTALL_LIB_DIR "${Slicer_INSTALL_LIB_DIR}")
-
-  if(WIN32)
-    install(FILES ${PYTHONQT_INSTALL_DIR}/bin/PythonQt.dll
-      DESTINATION bin COMPONENT Runtime)
-  elseif(APPLE)
-    # needs to install symlink version named libraries as well.
-    install(FILES ${PYTHONQT_INSTALL_DIR}/lib/libPythonQt.dylib
-      DESTINATION ${PYTHONQT_INSTALL_LIB_DIR} COMPONENT Runtime)
-  elseif(UNIX)
-    install(FILES ${PYTHONQT_INSTALL_DIR}/lib/libPythonQt.so
-      DESTINATION ${PYTHONQT_INSTALL_LIB_DIR} COMPONENT Runtime)
-  endif()
+if(WIN32)
+  install(FILES ${PYTHONQT_INSTALL_DIR}/bin/PythonQt.dll
+    DESTINATION bin COMPONENT Runtime)
+elseif(APPLE)
+  # needs to install symlink version named libraries as well.
+  install(FILES ${PYTHONQT_INSTALL_DIR}/lib/libPythonQt.dylib
+    DESTINATION ${PYTHONQT_INSTALL_LIB_DIR} COMPONENT Runtime)
+elseif(UNIX)
+  install(FILES ${PYTHONQT_INSTALL_DIR}/lib/libPythonQt.so
+    DESTINATION ${PYTHONQT_INSTALL_LIB_DIR} COMPONENT Runtime)
 endif()
 
