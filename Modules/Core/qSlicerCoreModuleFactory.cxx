c0b6462fc64fff47d033664b6a3edee61ebd2415
Nicole Aucoin <nicole@bwh.harvard.edu>
2012-01-11 05:36:05 +0000

    ENH: moving the fiducials IO to Annotations module
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19055
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19055 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Core/qSlicerCoreModuleFactory.cxx
+++ Modules/Core/qSlicerCoreModuleFactory.cxx
@@ -22,7 +22,6 @@
 #include "qSlicerCoreModuleFactory.h"
 #include "qSlicerCamerasModule.h"
 #include "qSlicerEventBrokerModule.h"
-#include "qSlicerFiducialsIO.h"
 //#include "qSlicerROIModule.h"
 // FIXME:Move the following to the Models module (when it will be ready in Qt.)
 #include "qSlicerCoreApplication.h"
@@ -94,16 +93,6 @@ void qSlicerCoreModuleFactory::registerItems()
   // vtkMRMLROINode are obsolete, one should use the Annotations module to
   // interact with nodes.
   //d->registerCoreModule<qSlicerROIModule>();
-
-  Q_ASSERT(qSlicerCoreApplication::application());
-  if(!qSlicerCoreApplication::application())
-    {
-    return;
-    }
-  qSlicerCoreIOManager * ioManager =
-      qSlicerCoreApplication::application()->coreIOManager();
-
-  ioManager->registerIO(new qSlicerFiducialsIO());
 }
 
 //-----------------------------------------------------------------------------
