db19c3ea9b7b09fdf2d20cfa6e37b0531502058a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-02-02 06:50:24 +0000

    ENH: Remove extra parameter passed to run_MergeModelsTest.cmake
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19176
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19176 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/CLI/MergeModels/Testing/run_MergeModelsTest.cmake
+++ Modules/CLI/MergeModels/Testing/run_MergeModelsTest.cmake
@@ -2,12 +2,11 @@
 # test_cmd .........: command to run without args
 # test_name ........: name of the test found in the testing wrapper <test_cmd>
 # model{1,2} .......:
-# output_model .....:
+# output_model .....: name of the output file the <test_cmd> will produce
 # output_baseline ..: name of the "baseline" output file
-# output_test ......: name of the output file the <test_cmd> will produce
 
 # Sanity checks
-set(expected_defined_vars test_cmd test_name model1 model2 output_model output_baseline output_test)
+set(expected_defined_vars test_cmd test_name model1 model2 output_model output_baseline)
 foreach(var ${expected_defined_vars})
   if(NOT ${var})
     message(FATAL_ERROR "Variable ${var} not defined !")
@@ -20,17 +19,17 @@ execute_process(
   RESULT_VARIABLE exec_not_successful
   )
 
-if (exec_not_successful)
- message( SEND_ERROR "${test_cmd} failed with args ${test_name} ${model1} ${model2} ${output_model}" )
+if(exec_not_successful)
+  message( SEND_ERROR "${test_cmd} failed with args ${test_name} ${model1} ${model2} ${output_model}" )
 endif()
 
 execute_process(
-  COMMAND ${CMAKE_COMMAND} -E compare_files ${output_baseline} ${output_test}
+  COMMAND ${CMAKE_COMMAND} -E compare_files ${output_baseline} ${output_model}
   RESULT_VARIABLE test_not_successful
   OUTPUT_QUIET
   ERROR_QUIET
   )
 
 if(test_not_successful)
-  message(SEND_ERROR "${output_test} does not match ${output_baseline}!")
+  message(SEND_ERROR "${output_model} does not match ${output_baseline}!")
 endif()
