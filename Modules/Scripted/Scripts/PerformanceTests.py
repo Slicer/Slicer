a4c1e22cbb53294cfb95aaad5ba9b81b73b22891
Steve Pieper <pieper@bwh.harvard.edu>
2012-02-02 14:46:50 +0000

    ENH: add Jim's new features to demo, including click signal
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19204
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19204 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Scripted/Scripts/PerformanceTests.py
+++ Modules/Scripted/Scripts/PerformanceTests.py
@@ -165,12 +165,27 @@ class PerformanceTestsWidget:
     self.imageViewer2.Render()
 
 
+  def chartCallback(self, mrmlID, pointIndex, x, y):
+    node = slicer.util.getNode(mrmlID)
+    name = node.GetName()
+    qt.QMessageBox.information(
+        slicer.util.mainWindow(), "Chart Callback", 
+        """Clicked at point {x}, {y}
+on node {name} (id {mrmlID})
+with point index of {pointIndex}
+        """.format(x=x,y=y,name=name,mrmlID=mrmlID,pointIndex=pointIndex))
+
+
+
   def chartTest(self):
     import math,random
     lns = slicer.mrmlScene.GetNodesByClass('vtkMRMLLayoutNode')
     lns.InitTraversal()
     ln = lns.GetNextItemAsObject()
     ln.SetViewArrangement(24)
+
+    chartView = findChildren(className='qMRMLChartView')[0]
+    chartView.connect("dataPointClicked(const char *,int,double,double)", self.chartCallback)
 
     cvns = slicer.mrmlScene.GetNodesByClass('vtkMRMLChartViewNode')
     cvns.InitTraversal()
@@ -204,6 +219,53 @@ class PerformanceTestsWidget:
     cn.AddArray('Another double array', dn2.GetID())
 
     cvn.SetChartNodeID(cn.GetID())
+
+    cn.SetProperty('default', 'title', 'A simple chart with 2 curves')
+    cn.SetProperty('default', 'xAxisLabel', 'Something in x')
+    cn.SetProperty('default', 'yAxisLabel', 'Something in y')
+
+    cvn.SetChartNodeID(cn.GetID())
+
+    cn = slicer.mrmlScene.CreateNodeByClass('vtkMRMLChartNode')
+    cn = slicer.mrmlScene.AddNode(cn)
+    print cn.GetID()
+    cn.AddArray('Just one array', dn.GetID())
+    cn.SetProperty('default', 'title', 'A simple chart with 1 curve')
+    cn.SetProperty('default', 'xAxisLabel', 'Just x')
+    cn.SetProperty('default', 'yAxisLabel', 'Just y')
+
+
+    cn = slicer.mrmlScene.CreateNodeByClass('vtkMRMLChartNode')
+    cn = slicer.mrmlScene.AddNode(cn)
+    print cn.GetID()
+    cn.AddArray('The other array', dn2.GetID())
+    cn.SetProperty('default', 'title', 'A simple chart with another curve')
+    cn.SetProperty('default', 'xAxisLabel', 'time')
+    cn.SetProperty('default', 'yAxisLabel', 'velocity')
+    cn.SetProperty('The other array', 'showLines', 'on')
+    cn.SetProperty('The other array', 'showMarkers', 'off')
+    cn.SetProperty('The other array', 'color', '#FF0000')
+
+
+    dn3 = slicer.mrmlScene.CreateNodeByClass('vtkMRMLDoubleArrayNode')
+    dn3 = slicer.mrmlScene.AddNode(dn3)
+    print dn3.GetID()
+    a = dn3.GetArray()
+    a.SetNumberOfTuples(12)
+    x = range(0, 12)
+    for i in range(len(x)):
+        a.SetComponent(i, 0, x[i]/4.0)
+        a.SetComponent(i, 1, math.sin(x[i]/4.0))
+        a.SetComponent(i, 2, 0)
+
+    cn = slicer.mrmlScene.CreateNodeByClass('vtkMRMLChartNode')
+    cn = slicer.mrmlScene.AddNode(cn)
+    print cn.GetID()
+    cn.AddArray('Periodic', dn3.GetID())
+    cn.SetProperty('default', 'title', 'A bar chart')
+    cn.SetProperty('default', 'xAxisLabel', 'time')
+    cn.SetProperty('default', 'yAxisLabel', 'velocity')
+    cn.SetProperty('default', 'type', 'Bar');
 
   def webViewCallback(self,qurl):
     url = qurl.toString()
