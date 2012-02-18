044a61a80b303d0eadc833dacb3ac98017c7af94
Hans Johnson <hans-johnson@uiowa.edu>
2012-02-18 21:04:14 +0000

    ENH: Increment SWIG to version 2.0.4, download binary
    Upgraded the version of SWIG used to match the version used in SimpleITK.
    Copied the code from SimpleITK to just download the binary of SWIG on
    windows platforms.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19309
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19309 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_Swig.cmake
+++ SuperBuild/External_Swig.cmake
@@ -11,25 +11,41 @@ if(DEFINED Swig_DIR AND NOT EXISTS ${Swig_DIR})
   message(FATAL_ERROR "Swig_DIR variable is defined but corresponds to non-existing directory")
 endif()
 
-if(WIN32 AND NOT EXISTS ${Swig_DIR})
-  message(FATAL_ERROR "Swig_DIR variable is expected to be set on Windows.")
-endif()
+set( TARGET_SWIG_VERSION 2.0.4 )
+if(NOT SWIG_DIR)
+  if(WIN32)
+    # swig.exe available as pre-built binary on Windows:
+    ExternalProject_Add(Swig
+      URL http://prdownloads.sourceforge.net/swig/swigwin-${TARGET_SWIG_VERSION}.zip
+      URL_MD5 4ab8064b1a8894c8577ef9d0fb2523c8
+      SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/swigwin-${TARGET_SWIG_VERSION}
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ""
+      INSTALL_COMMAND ""
+      )
 
-# Set dependency list
-set(Swig_DEPENDENCIES "PCRE")
+    set(SWIG_DIR ${CMAKE_CURRENT_BINARY_DIR}/swigwin-${TARGET_SWIG_VERSION}) # ??
+    set(SWIG_EXECUTABLE ${CMAKE_CURRENT_BINARY_DIR}/swigwin-${TARGET_SWIG_VERSION}/swig.exe)
+    set(Swig_DEPEND Swig)
+  else()
+    # Set dependency list
+    set(Swig_DEPENDENCIES "PCRE")
 
-# Include dependent projects if any
-SlicerMacroCheckExternalProjectDependency(Swig)
+    # Include dependent projects if any
+    SlicerMacroCheckExternalProjectDependency(Swig)
 
-ExternalProject_add(Swig
-  URL http://prdownloads.sourceforge.net/swig/swig-2.0.1.tar.gz
-  URL_MD5 df4465a62ccc5f0120fee0890ea1a31f
-  CONFIGURE_COMMAND ../Swig/configure --prefix=${CMAKE_CURRENT_BINARY_DIR}
-#  --without-pcre
-  --with-pcre-prefix=${CMAKE_CURRENT_BINARY_DIR}
-  DEPENDS ${Swig_DEPENDENCIES}
-  )
-set(SWIG_DIR ${CMAKE_CURRENT_BINARY_DIR}/share/swig/2.0.1)
-set(SWIG_EXECUTABLE ${CMAKE_CURRENT_BINARY_DIR}/bin/swig)
-set(Swig_DEPEND Swig)
+    ExternalProject_add(Swig
+      URL http://prdownloads.sourceforge.net/swig/swig-${TARGET_SWIG_VERSION}.tar.gz
+      URL_MD5  4319c503ee3a13d2a53be9d828c3adc0
+      CONFIGURE_COMMAND ../Swig/configure --prefix=${CMAKE_INSTALL_PREFIX}
+      --with-pcre-prefix=${CMAKE_INSTALL_PREFIX}
+      DEPENDS PCRE
+      )
+
+    set(SWIG_DIR ${CMAKE_INSTALL_PREFIX}/share/swig/${TARGET_SWIG_VERSION})
+    set(SWIG_EXECUTABLE ${CMAKE_INSTALL_PREFIX}/bin/swig)
+    set(Swig_DEPEND Swig)
+  endif()
+endif(NOT SWIG_DIR)
+
 
