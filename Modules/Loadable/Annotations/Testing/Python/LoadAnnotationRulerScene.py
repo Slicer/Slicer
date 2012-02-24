1c866699cbb31aded4b15cce8d2f97fad8125cc9
Nicole Aucoin <nicole@bwh.harvard.edu>
2012-02-24 17:53:24 +0000

    ENH: add a test to load a scene containing a ruler, trying to track down a crash on mac
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19370
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19370 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/Loadable/Annotations/Testing/Python/LoadAnnotationRulerScene.py
@@ -0,0 +1,17 @@
+import slicer.testing
+import os
+
+# set the url
+scenePath =  os.path.join(os.environ['SLICER_HOME'], "../../Slicer4/Modules/Loadable/Annotations/Testing/Data/Input/ruler.mrml")
+scenePath = os.path.normpath(scenePath)
+print "Trying to load ruler mrml file", scenePath
+slicer.mrmlScene.SetURL(scenePath)
+
+
+# and load it
+slicer.mrmlScene.Connect()
+
+
+slicer.testing.setEnabled()
+
+slicer.testing.exitSuccess()
