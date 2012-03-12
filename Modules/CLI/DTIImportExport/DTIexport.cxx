3058963490ce27867d046caf0860465e123149c8
Demian Wassermann <demian@bwh.harvard.edu>
2012-03-12 16:21:31 +0000

    ENH: Added DTI import/export tools
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19568
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19568 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/CLI/DTIImportExport/DTIexport.cxx
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "DTIexportCLP.h"
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkDiffusionTensor3D.h"
+
+int main(int argc, char * argv [])
+{
+
+  PARSE_ARGS;
+ 
+
+  typedef itk::DiffusionTensor3D<float> PixelType;
+  typedef itk::Image< PixelType, 3 >   ImageType;
+  typedef itk::ImageFileReader< ImageType  >  ReaderType;
+  typedef itk::ImageFileWriter< ImageType >  WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+  
+  ReaderType::Pointer readerImport = ReaderType::New();
+  WriterType::Pointer writerImport = WriterType::New();
+
+  ReaderType::Pointer readerExport = ReaderType::New();
+  WriterType::Pointer writerExport = WriterType::New();
+  
+  readerExport->SetFileName(inputTensor);
+  writerExport->SetFileName(outputFile);
+  
+  try
+    {
+      writerExport->SetInput(readerExport->GetOutput());
+      // writerExport->UseCompressionOn();
+      writerExport->Update();
+      }
+  catch (itk::ExceptionObject &excep)
+    {
+      std::cerr << excep << std::endl;
+      return EXIT_FAILURE;
+    }
+
+  std::cout << "DTI Volume successfully written on disk" << std::endl;
+  return EXIT_SUCCESS;
+}
---
+++ Modules/CLI/DTIImportExport/DTIimport.cxx
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "DTIimportCLP.h"
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkDiffusionTensor3D.h"
+
+int main(int argc, char * argv [])
+{
+
+  PARSE_ARGS;
+  std::cout << "DTI Import" << std::endl;
+
+  typedef itk::DiffusionTensor3D<float> PixelType;
+  typedef itk::Image< PixelType, 3 >   ImageType;
+  typedef itk::ImageFileReader< ImageType  >  ReaderType;
+  typedef itk::ImageFileWriter< ImageType >  WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+  
+  ReaderType::Pointer readerImport = ReaderType::New();
+  WriterType::Pointer writerImport = WriterType::New();
+
+  ReaderType::Pointer readerExport = ReaderType::New();
+  WriterType::Pointer writerExport = WriterType::New();
+  
+  readerImport->SetFileName(inputFile);
+  writerImport->SetFileName(outputTensor);
+
+  try
+    {
+      writerImport->SetInput(readerImport->GetOutput());
+      writerImport->UseCompressionOn();
+      writerImport->Update();
+    }
+  catch (itk::ExceptionObject &excep)
+    {
+      std::cerr << excep << std::endl;
+      return EXIT_FAILURE;
+    }
+  return EXIT_SUCCESS;
+}
