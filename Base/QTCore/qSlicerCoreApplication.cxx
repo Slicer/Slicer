3135447f517f0f62e4f15d56f2d7935b44e425a1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-03-13 14:18:57 +0000

    ENH: Make sure python script expecting 'sys.executable' remains happy
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19580
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19580 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTCore/qSlicerCoreApplication.cxx
+++ Base/QTCore/qSlicerCoreApplication.cxx
@@ -784,6 +784,10 @@ void qSlicerCoreApplication::handleCommandLineArguments()
     // See http://docs.python.org/c-api/init.html
     PySys_SetArgvEx(pythonArgc, pythonArgv, /*updatepath=*/false);
 
+    // Set 'sys.executable' so that Slicer can be used as a "regular" python interpreter
+    this->corePythonManager()->executeString(
+          QString("import sys; sys.executable = '%1'; del sys").arg(this->applicationFilePath()));
+
     // Clean memory
     for(int i = 0; i < pythonArgc; ++i){ delete[] pythonArgv[i];}
     delete[] pythonArgv;
