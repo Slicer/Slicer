cf73681cd770cc7afa6df79bcd7a2eec6e385989
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-03-19 09:04:26 +0000

    COMP: Add missing macro
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19656
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19656 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ CMake/SlicerMacroSimpleTest.cmake
@@ -0,0 +1,43 @@
+
+
+
+#! Usage:
+#! \code
+#! SIMPLE_TEST(<testname> [argument1 ...])
+#! \endcode
+#!
+#! This macro add a test using the complete add_test signature specifying target using 
+#! $<TARGET_FILE:...> generator expression. Optionnal test argument(s) can be passed
+#! after specifying the <testname>.
+#!
+#! Variable named KIT (or CLP or EXTENSION_NAME) is expected to be defined in the current scope. 
+#! KIT (or CLP or EXTENSION_NAME) variable usually matches the value of PROJECT_NAME.
+#!
+#! The macro also associates a label to the test based on the current value of KIT (or CLP or EXTENSION_NAME).
+#!
+#! \sa http://www.cmake.org/cmake/help/cmake-2-8-docs.html#command:add_test
+#! \sa http://www.cmake.org/cmake/help/cmake-2-8-docs.html#variable:PROJECT_NAME
+#!
+#! \ingroup CMakeUtilities
+macro(SIMPLE_TEST testname)
+
+  if("${KIT}" STREQUAL "")
+    set(KIT ${CLP})
+  endif()
+  
+  if("${KIT}" STREQUAL "")
+    set(KIT ${EXTENSION_NAME})
+  endif()
+  
+  if("${KIT}" STREQUAL "")
+    message(FATAL_ERROR "error: KIT or CLP or EXTENSION_NAME variable is not set !")
+  endif()
+  
+  if(NOT TARGET ${KIT}CxxTests)
+    message(FATAL_ERROR "error: ${KIT}CxxTests target does NOT exist !")
+  endif()
+  
+  add_test(NAME ${testname} COMMAND $<TARGET_FILE:${KIT}CxxTests> ${testname} ${ARGN})
+  set_property(TEST ${testname} PROPERTY LABELS ${KIT})
+endmacro()
+
