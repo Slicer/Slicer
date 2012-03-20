4bcb4391809aab89ed9bcd574b3578842e15f195
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-03-20 06:18:51 +0000

    COMP: Ensure environment is properly set by using launcher within SIMPLE_TEST macro.
    * This fix a regression introduced by r16655 and r16656
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19660
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19660 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerMacroSimpleTest.cmake
+++ CMake/SlicerMacroSimpleTest.cmake
@@ -6,11 +6,11 @@
 #! SIMPLE_TEST(<testname> [argument1 ...])
 #! \endcode
 #!
-#! This macro add a test using the complete add_test signature specifying target using 
+#! This macro add a test using the complete add_test signature specifying target using
 #! $<TARGET_FILE:...> generator expression. Optionnal test argument(s) can be passed
 #! after specifying the <testname>.
 #!
-#! Variable named KIT (or CLP or EXTENSION_NAME) is expected to be defined in the current scope. 
+#! Variable named KIT (or CLP or EXTENSION_NAME) is expected to be defined in the current scope.
 #! KIT (or CLP or EXTENSION_NAME) variable usually matches the value of PROJECT_NAME.
 #!
 #! The macro also associates a label to the test based on the current value of KIT (or CLP or EXTENSION_NAME).
@@ -24,20 +24,25 @@ macro(SIMPLE_TEST testname)
   if("${KIT}" STREQUAL "")
     set(KIT ${CLP})
   endif()
-  
+
   if("${KIT}" STREQUAL "")
     set(KIT ${EXTENSION_NAME})
   endif()
-  
+
   if("${KIT}" STREQUAL "")
     message(FATAL_ERROR "error: KIT or CLP or EXTENSION_NAME variable is not set !")
   endif()
-  
-  if(NOT TARGET ${KIT}CxxTests)
-    message(FATAL_ERROR "error: ${KIT}CxxTests target does NOT exist !")
+
+  if("${Slicer_LAUNCH_COMMAND}" STREQUAL "")
+    message(FATAL_ERROR "error: KIT or CLP or EXTENSION_NAME variable is not set !")
   endif()
-  
-  add_test(NAME ${testname} COMMAND $<TARGET_FILE:${KIT}CxxTests> ${testname} ${ARGN})
+
+  if(NOT TARGET ${KIT}CxxTests)
+    message(FATAL_ERROR "error: Slicer_LAUNCH_COMMAND variable is not set !")
+  endif()
+
+  add_test(NAME ${testname}
+           COMMAND ${Slicer_LAUNCH_COMMAND} $<TARGET_FILE:${KIT}CxxTests> ${testname} ${ARGN})
   set_property(TEST ${testname} PROPERTY LABELS ${KIT})
 endmacro()
 
