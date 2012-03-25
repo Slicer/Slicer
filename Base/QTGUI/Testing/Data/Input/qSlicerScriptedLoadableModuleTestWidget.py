edf370b75058b601032ddc2765efe2d863de495f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-03-25 23:08:19 +0000

    COMP: Add test data associated with qSlicerScriptedLoadableModuleWidget unit test
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19703
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19703 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/QTGUI/Testing/Data/Input/qSlicerScriptedLoadableModuleTestWidget.py
@@ -0,0 +1,14 @@
+
+
+class qSlicerScriptedLoadableModuleTestWidget:
+  def __init__(self, parent=None):
+    self.parent = parent
+
+  def setup(self):
+    self.parent.setProperty('setup_called_within_Python', True)
+
+  def enter(self):
+    self.parent.setProperty('enter_called_within_Python', True)
+
+  def exit(self):
+    self.parent.setProperty('exit_called_within_Python', True)
