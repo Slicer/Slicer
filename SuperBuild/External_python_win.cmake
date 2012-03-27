106feae9c207f9c4d72a3761512e641e73c6499b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-03-27 22:08:46 +0000

    COMP: Ease future maintenance by re-organizing content of python_patch_step_win.cmake.in
    * Rename variable "python_CONFIGURE_COMMAND" into "python_PATCH_COMMAND_ONE"
    
    * Rename variable "python_PATCH_COMMAND" into "python_PATCH_COMMAND_TWO"
    
    * Add comments
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19720
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19720 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_python_win.cmake
+++ SuperBuild/External_python_win.cmake
@@ -3,16 +3,33 @@ if(NOT External_python_win_PROJECT_INCLUDED)
   message(FATAL_ERROR "This file shouldn't be included directly !")
 endif()
 
+#-----------------------------------------------------------------------------
+# General purpose variables
+#-----------------------------------------------------------------------------
+
+# Path to solution file
 set(python_sln ${CMAKE_BINARY_DIR}/${proj}-build/PCbuild/pcbuild.sln)
 string(REPLACE "/" "\\" python_sln ${python_sln})
 
-#message("python_sln:${python_sln}")
-
+# Compute python home
 get_filename_component(python_base ${python_sln} PATH)
 get_filename_component(python_home ${python_base} PATH)
 
-# point the tkinter build file to the slicer tcl-build
-set(python_PATCH_COMMAND)
+#-----------------------------------------------------------------------------
+# Prepare patch command 1
+#-----------------------------------------------------------------------------
+
+set(python_SOURCE_DIR ${python_build})
+configure_file(SuperBuild/python_patch_step_win.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/python_patch_step.cmake
+  @ONLY)
+set(python_PATCH_COMMAND_ONE ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/python_patch_step.cmake)
+
+#-----------------------------------------------------------------------------
+# Prepare patch command 2
+#-----------------------------------------------------------------------------
+
+# Point the tkinter build file to the slicer tcl-build
 if(Slicer_USE_PYTHONQT_WITH_TCL)
   set(python_tkinter ${python_base}/pyproject.vsprops)
   # The following if statement is specific to VS2010
@@ -26,33 +43,23 @@ if(Slicer_USE_PYTHONQT_WITH_TCL)
   set(in ${python_tkinter})
 endif()
 
-
-#-----------------------------------------------------------------------------
 # 32-bit or 64-bit
-#-----------------------------------------------------------------------------
-
+set(python_PATCH_COMMAND_TWO)
 if("${CMAKE_SIZEOF_VOID_P}" EQUAL 8)
   set(python_build_type "Release")
   set(python_platform "x64")
   set(python_configuration "${python_build_type}|${python_platform}")
   set(PythonPCBuildDir ${CMAKE_BINARY_DIR}/python-build/PCbuild/amd64)
-  set(python_PATCH_COMMAND
+  set(python_PATCH_COMMAND_TWO
     ${CMAKE_COMMAND} -Din=${in} -Dout=${out} -Dfind=\\tcltk64 -Dreplace=\\tcl-build -P ${script})
 else()
   set(python_build_type "Release")
   set(python_platform "Win32")
   set(python_configuration "${python_build_type}|${python_platform}")
   set(PythonPCBuildDir ${CMAKE_BINARY_DIR}/python-build/PCbuild)
-  set(python_PATCH_COMMAND
+  set(python_PATCH_COMMAND_TWO
     ${CMAKE_COMMAND} -Din=${in} -Dout=${out} -Dfind=\\tcltk -Dreplace=\\tcl-build -P ${script})
 endif()
-
-set(python_SOURCE_DIR ${python_build})
-configure_file(SuperBuild/python_patch_step_win.cmake.in
-  ${CMAKE_CURRENT_BINARY_DIR}/python_patch_step.cmake
-  @ONLY)
-set(python_CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/python_patch_step.cmake)
-
 
 #-----------------------------------------------------------------------------
 # Convenient helper macro
@@ -81,8 +88,8 @@ ExternalProject_Add(${proj}
   URL_MD5 ${python_MD5}
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR python-build
-  UPDATE_COMMAND ${python_CONFIGURE_COMMAND}
-  PATCH_COMMAND ${python_PATCH_COMMAND}
+  UPDATE_COMMAND ${python_PATCH_COMMAND_ONE}
+  PATCH_COMMAND ${python_PATCH_COMMAND_TWO}
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ${CMAKE_BUILD_TOOL} ${python_sln} ${ep_build_command_args}
   BUILD_IN_SOURCE 1
