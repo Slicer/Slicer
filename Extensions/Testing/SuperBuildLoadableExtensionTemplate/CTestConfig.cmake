80cc9bf32655bb1f95d97bf46554696cdf78a1c7
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-04-09 14:57:31 +0000

    COMP: Associate a valid project in SuperbuildLoadableExtensionTemplate/CTestConfig .cmake
    * The "WARNING non-zero return value in ctest from" was occurring on
    windows and was the expression of one of the problem preventing the
    build from being successfully driven and completed.
    
    * By configuring properly the variable CTEST_PROJECT_NAME in CTestConfig,
    the proper build command is now generated by CTest.
    
    * the problem wasn't occurring on unix-like system because knowing the
    project name is not required to either configure, build or package.
    
    See r19814
    
    Note also that a mechanism to centralize all CTest relate settings
    could be established, it would avoid to redefined the drop site
    and the drop location.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19834
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19834 3bd1e089-480b-0410-8dfb-8563597acbee

--- Extensions/Testing/SuperBuildLoadableExtensionTemplate/CTestConfig.cmake
+++ Extensions/Testing/SuperBuildLoadableExtensionTemplate/CTestConfig.cmake
@@ -1,7 +1,7 @@
-set(CTEST_PROJECT_NAME "Slicer")
+set(CTEST_PROJECT_NAME "SuperBuildLoadableExtensionTemplate")
 set(CTEST_NIGHTLY_START_TIME "3:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/slicer4/submit.php?project=Slicer4")
+set(CTEST_DROP_SITE "slicer.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=Slicer4")
 set(CTEST_DROP_SITE_CDASH TRUE)
