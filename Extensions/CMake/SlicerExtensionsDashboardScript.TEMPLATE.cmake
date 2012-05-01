9b3abf0c632fe39d74d627d69edc3b571a694e6c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-05-01 00:11:12 +0000

    ENH: Extensions build system now support ExtensionsIndex git repository
    The "ExtensionsIndex" repository is a dedicated git repository used to
    host extension description files. The associated integration branches
    are "master", "4.1", ...
    
    When Slicer 4.2 will be released, the integration branch 4.2 will be created.
    This later one will host the extensions developed for this specific release.
    
    The "master" branch will host the bleeding edge extensions build against
    the bleeding edge Slicer.
    
    In a nutshell, the extensions build system can be used in two ways:
      "Inside"  a slicer build tree
      or
      "Outside" a slicer build tree
    
    "Inside": This is done by enabling option Slicer_BUILD_EXTENSIONS (and optionally Slicer_UPLOAD_EXTENSIONS)
    when building Slicer.
    
    "Outside": This is done by building the project located in "Slicer/Extensions/CMake"
    
    The advantage of doing an "Outside" build is that the continuous integration associated
    with extensions will be fast (systematic configure/build of Slicer isn't required)
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19970
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19970 3bd1e089-480b-0410-8dfb-8563597acbee

--- Extensions/CMake/SlicerExtensionsDashboardScript.TEMPLATE.cmake
+++ Extensions/CMake/SlicerExtensionsDashboardScript.TEMPLATE.cmake
@@ -1,51 +1,52 @@
+####################################################################################
+# OS      :
+# Hardware:
+# GPU     :
+####################################################################################
+# WARNING - The specific version and processor type of this machine should be reported
+# in the header above. Indeed, this file will be send to the dashboard as a NOTE file.
 #
-# OS: Ubuntu 9.04  2.6.28-19-generic
-# Hardware: x86_64 GNU/Linux
-# GPU: NA
-
-# Note: The specific version and processor type of this machine should be reported in the
-# header above. Indeed, this file will be sent to the dashboard as a NOTE file.
+# On linux, you could run:
+#     'uname -o' and 'cat /etc/*-release' to obtain the OS name.
+#     'uname -mpi' to obtain hardware details.
+#     'glxinfo | grep OpenGL' to obtain GPU details.
+####################################################################################
 
 cmake_minimum_required(VERSION 2.8.7)
 
 #
-# For additional information, see http://www.slicer.org/slicerWiki/index.php/Documentation/4.1/Developers/Tutorials/DashboardSetup
+# For additional information, see http://http://www.slicer.org/slicerWiki/index.php/Documentation/Snapshot/Developers/Tutorials/DashboardSetup
 #
 
-#
+#-----------------------------------------------------------------------------
 # Dashboard properties
-#
+#-----------------------------------------------------------------------------
 set(MY_OPERATING_SYSTEM   "Linux") # Windows, Linux, Darwin...
 set(MY_COMPILER           "g++4.4.3")
-set(MY_QT_VERSION         "4.7")
-set(Slicer_DIR            "$ENV{HOME}/Projects/Slicer4-Superbuild-Debug/Slicer-build")
+set(MY_QT_VERSION         "4.7.4")
 set(CTEST_SITE            "karakoram.kitware") # for example: mymachine.kitware, mymachine.bwh.harvard.edu, ...
 set(CTEST_DASHBOARD_ROOT  "$ENV{HOME}/Dashboards/")
-set(CTEST_CMAKE_COMMAND   "$ENV{HOME}/Dashboards/Support/cmake-2.8.7-Linux-i386/bin/cmake")
 # Open a shell and type in "cmake --help" to obtain the proper spelling of the generator
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(MY_BITNESS            "64")
-set(MIDAS_PACKAGE_URL "slicer.kitware.com/midas3")
-#set(MIDAS_PACKAGE_EMAIL "EMAIL")
-#set(MIDAS_PACKAGE_API_KEY "API_KEY")
+set(MIDAS_PACKAGE_URL "http://slicer.kitware.com/midas3")
+set(MIDAS_PACKAGE_EMAIL "EMAIL")
+set(MIDAS_PACKAGE_API_KEY "API_KEY")
 
-#
+#-----------------------------------------------------------------------------
 # Dashboard options
-#
+#-----------------------------------------------------------------------------
+set(EXTENSIONS_BUILDSYSTEM_TESTING FALSE)
 set(WITH_KWSTYLE FALSE)
 set(WITH_MEMCHECK FALSE)
 set(WITH_COVERAGE FALSE)
 set(WITH_DOCUMENTATION FALSE)
 #set(DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY ) # for example: $ENV{HOME}/Projects/Doxygen
-set(CTEST_BUILD_CONFIGURATION "Release")
+set(CTEST_BUILD_CONFIGURATION "Release") # Release or Debug
 set(CTEST_TEST_TIMEOUT 500)
 set(CTEST_BUILD_FLAGS "") # Use multiple CPU cores to build. For example "-j4" on unix
 set(CTEST_PARALLEL_LEVEL 8) # Number of tests running in parallel
 
-find_program(CTEST_SVN_COMMAND NAMES svn)
-find_program(CTEST_GIT_COMMAND NAMES git)
-find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
-find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
 # experimental:
 #     - run_ctest() macro will be called *ONE* time
 #     - binary directory will *NOT* be cleaned
@@ -59,33 +60,64 @@ find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
 set(SCRIPT_MODE "experimental") # "experimental", "continuous", "nightly"
 
 # You could invoke the script with the following syntax:
-#  ctest -S karakoram_Slicer4_nightly.cmake -V
-
-
+#  ctest -S karakoram_Slicer4_nightly.cmake -C <CTEST_BUILD_CONFIGURATION> -V
 #
+# Note that '-C <CTEST_BUILD_CONFIGURATION>' is mandatory on windows
+
+#-----------------------------------------------------------------------------
 # Additional CMakeCache options
-# For example:
-#   ADDITIONAL_{C/CXX}_FLAGS: /MP -> Multi process build with MSVC
-#
+#-----------------------------------------------------------------------------
 set(ADDITIONAL_CMAKECACHE_OPTION "
   ADDITIONAL_C_FLAGS:STRING=
   ADDITIONAL_CXX_FLAGS:STRING=
 ")
 
-# Update the following variable to match the chosen build options. This variable is used to
-# generate both the build directory and the build name.
-set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}")
-
-#
-# Project specific properties
-#
-set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions")
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}")
-
-# set any extra environment variables here
+#-----------------------------------------------------------------------------
+# Set any extra environment variables here
+#-----------------------------------------------------------------------------
 if(UNIX)
   set(ENV{DISPLAY} ":0")
 endif()
+
+#-----------------------------------------------------------------------------
+# Required executables
+#-----------------------------------------------------------------------------
+find_program(CTEST_SVN_COMMAND NAMES svn)
+find_program(CTEST_GIT_COMMAND NAMES git)
+find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
+find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+
+#-----------------------------------------------------------------------------
+# Git tag
+#-----------------------------------------------------------------------------
+set(EXTENSIONS_TRACK_QUALIFIER "master") # "master", 4.1, ...
+set(EXTENSIONS_INDEX_GIT_TAG "origin/${EXTENSIONS_TRACK_QUALIFIER}") # origin/master, origin/4.1, ...
+
+#-----------------------------------------------------------------------------
+# Git repository
+#-----------------------------------------------------------------------------
+set(EXTENSIONS_INDEX_GIT_REPOSITORY git://github.com/jcfr/ExtensionsIndex.git)
+
+#-----------------------------------------------------------------------------
+# Build Name
+#-----------------------------------------------------------------------------
+# Update the following variable to match the chosen build options. This variable is used to
+# generate both the build directory and the build name.
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-PythonQt-With-Tcl-CLI")
+
+#-----------------------------------------------------------------------------
+# Build directories
+#-----------------------------------------------------------------------------
+set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
+
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-build-${dir_suffix}/Slicer-build")
+
+#-----------------------------------------------------------------------------
+# Source directories
+#-----------------------------------------------------------------------------
+set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer4/Extensions/CMake")
 
 
 ##########################################
@@ -98,32 +130,25 @@ set(CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
 # Project specific properties
 #
 set(CTEST_PROJECT_NAME "Slicer4")
-set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-${BUILD_OPTIONS_STRING}-${EXTENSIONS_INDEX_GIT_TAG}-${CTEST_BUILD_CONFIGURATION}")
 
 #
-# Display build info
+# Convenient function allowing to download a file
 #
-message("site name: ${CTEST_SITE}")
-message("build name: ${CTEST_BUILD_NAME}")
-message("script mode: ${SCRIPT_MODE}")
-message("coverage: ${WITH_COVERAGE}, memcheck: ${WITH_MEMCHECK}")
-
-#
-# Convenient macro allowing to download a file
-#
-macro(downloadFile url dest)
+function(download_file url dest)
   file(DOWNLOAD ${url} ${dest} STATUS status)
   list(GET status 0 error_code)
   list(GET status 1 error_msg)
   if(error_code)
     message(FATAL_ERROR "error: Failed to download ${url} - ${error_msg}")
   endif()
-endmacro()
+endfunction()
 
 #
 # Download and include dashboard driver script
 #
 set(url http://svn.slicer.org/Slicer4/trunk/Extensions/CMake/SlicerExtensionsDashboardDriverScript.cmake)
 set(dest ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}.driver)
-downloadFile(${url} ${dest})
+#download_file(${url} ${dest})
 include(${dest})
+
