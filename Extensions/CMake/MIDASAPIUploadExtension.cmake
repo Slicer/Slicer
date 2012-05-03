3d15c8116cd53d3a9ae668a5c37380497b5266fd
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-05-03 21:48:47 +0000

    BUG: Do not use "SlicerFunctionToday" in MIDASAPI macro tests. See #1973
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=19991
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@19991 3bd1e089-480b-0410-8dfb-8563597acbee

--- Extensions/CMake/MIDASAPIUploadExtension.cmake
+++ Extensions/CMake/MIDASAPIUploadExtension.cmake
@@ -139,8 +139,7 @@ if(TEST_midas_api_upload_extension_test)
 
     set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../../CMake ${CMAKE_MODULE_PATH})
 
-    include(SlicerFunctionToday)
-    TODAY(Test_TESTDATE)
+    set(Test_TESTDATE "2011-12-26")
 
     # Sanity check
     set(expected_nonempty_vars Test_TESTDATE)
