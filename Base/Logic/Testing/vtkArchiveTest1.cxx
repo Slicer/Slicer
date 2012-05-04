aab9529d29596ca443ecbc629c2690816902a890
Steve Pieper <pieper@bwh.harvard.edu>
2012-05-04 00:10:30 +0000

    ENH: added zip/unzip tests (1962, 1963)
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20010
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20010 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Logic/Testing/vtkArchiveTest1.cxx
+++ Base/Logic/Testing/vtkArchiveTest1.cxx
@@ -15,9 +15,23 @@
 
 // VTK includes
 
+// VTKSYS includes
+#include <vtksys/SystemTools.hxx>
+#include <vtksys/Directory.hxx>
+
+
 // STD includes
 
 #include "vtkMRMLCoreTestingMacros.h"
+
+bool validFile(const char *fileName)
+{
+  if (!strcmp(fileName, ".")) return true;
+  if (!strcmp(fileName, "..")) return true;
+  if (!strcmp(fileName, "vol.mrml")) return true;
+  if (!strcmp(fileName, "vol_and_cube.mrml")) return true;
+  return false;
+}
 
 int vtkArchiveTest1(int argc, char * argv[] )
 {
@@ -26,12 +40,114 @@ int vtkArchiveTest1(int argc, char * argv[] )
     std::cerr << "Usage: vtkArchiveTest1 archive.zip" << std::endl;
     return EXIT_FAILURE;
     }
+
+  //
+  // unzip test file to local directory
+  //
+  if ( vtksys::SystemTools::FileExists("archiveTest") )
+    {
+    if ( !vtksys::SystemTools::RemoveADirectory("archiveTest") )
+      {
+      std::cerr << "Error: could not remove archiveTest directory" << std::endl;
+      return EXIT_FAILURE;
+      }
+    }
+
+  vtksys::SystemTools::MakeDirectory("archiveTest");
+  vtksys::SystemTools::ChangeDirectory("archiveTest");
   bool res = extract_tar(argv[1], true, true);
   if (!res)
     {
     std::cerr << "failed to extract archive :" << argc << std::endl;
     return EXIT_FAILURE;
     }
+
+  //
+  // check that correct files were extracted
+  //
+  vtksys::Directory cwd;
+  cwd.Load(".");
+  unsigned int numberOfFiles = cwd.GetNumberOfFiles();
+  unsigned int validFiles = 0;
+  for (unsigned int i = 0; i < numberOfFiles; i++)
+    {
+    std::cout << "Extracted file: " << cwd.GetFile(i) << std::endl;
+    if ( validFile(cwd.GetFile(i)) ) validFiles++;
+    }
+
+  if (validFiles != 4)
+    {
+    std::cerr << "failed to extract archive :" << argv[1] << std::endl;
+    return EXIT_FAILURE;
+    }
+  vtksys::SystemTools::ChangeDirectory("..");
+
+
+  //
+  // Create a new zip file of the archive
+  //
+
+  if ( vtksys::SystemTools::FileExists("archiveTest.zip") )
+    {
+    if ( !vtksys::SystemTools::RemoveFile("archiveTest.zip") )
+      {
+      std::cerr << "Error: could not remove archiveTest.zip file" << std::endl;
+      return EXIT_FAILURE;
+      }
+    }
+
+  std::cout << "creating archiveTest.zip" << std::endl;
+  std::string zipFilePath = vtksys::SystemTools::GetCurrentWorkingDirectory() +
+                                                    std::string("/archiveTest.zip");
+  std::string zipDirPath = vtksys::SystemTools::GetCurrentWorkingDirectory() +
+                                                    std::string("/archiveTest");
+  res = zip(zipFilePath.c_str(), zipDirPath.c_str());
+  if (!res)
+    {
+    std::cerr << "failed to create new archive" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // make fresh output directory
+  std::cout << "creating extractedArchiveTest" << std::endl;
+  if ( vtksys::SystemTools::FileExists("extractedArchiveTest") )
+    {
+    if ( !vtksys::SystemTools::RemoveADirectory("extractedArchiveTest") )
+      {
+      std::cerr << "Error: could not remove extractedArchiveTest directory" << std::endl;
+      return EXIT_FAILURE;
+      }
+    }
+  vtksys::SystemTools::MakeDirectory("extractedArchiveTest");
+
+  std::cout << "unzipping archiveTest.zip" << std::endl;
+  res = unzip(zipFilePath.c_str(), "extractedArchiveTest");
+  if (!res)
+    {
+    std::cerr << "failed to extract new archive" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  //
+  // check that correct files were extracted
+  //
+  std::cout << "checking files in extractedArchiveTest" << std::endl;
+  vtksys::SystemTools::ChangeDirectory("extractedArchiveTest");
+  cwd.Load("archiveTest");
+  numberOfFiles = cwd.GetNumberOfFiles();
+  validFiles = 0;
+  for (unsigned int i = 0; i < numberOfFiles; i++)
+    {
+    std::cerr << "Extracted file: " << cwd.GetFile(i) << std::endl;
+    if ( validFile(cwd.GetFile(i)) ) validFiles++;
+    }
+
+  if (validFiles != 4)
+    {
+    std::cerr << "failed to extract archive : " << "extractedArchiveTest" << std::endl;
+    return EXIT_FAILURE;
+    }
+
   return EXIT_SUCCESS;
 }
 
