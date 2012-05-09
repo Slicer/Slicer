66577e88987215eaefea6acc094dab681e07a19e
Steve Pieper <pieper@bwh.harvard.edu>
2012-05-09 22:32:28 +0000

    ENH: add a run-time test to replicate bug 1822
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20054
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20054 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Scripted/EditorLib/Testing/ThresholdThreadingTest.py
+++ Modules/Scripted/EditorLib/Testing/ThresholdThreadingTest.py
@@ -1,14 +1,86 @@
 
 
 comment = """
-import os
-
-src = os.environ['HOME'] + '/slicer4/latest/Slicer/Modules/Scripted/EditorLib/Testing/ThresholdThreadingTest.py'
-execfile(src)
-thresholdThreadingTest()
+import os; src = os.environ['HOME'] + '/slicer4/latest/Slicer/Modules/Scripted/EditorLib/Testing/ThresholdThreadingTest.py'; execfile(src); thresholdThreadingTest()
 
 """
 
 def thresholdThreadingTest():
-  print('running test!')
+  """
+  Replicate the issue reported in bug 1822 where spliting
+  a grow-cut produced volume causes a multi-threading related
+  issue on mac release builds
+  """
+
+  #
+  # first, get some sample data
+  #
+  import SampleData
+  sampleDataLogic = SampleData.SampleDataLogic()
+  head = sampleDataLogic.downloadMRHead()
+
+  #
+  # now, define an ROI in it
+  #
+  roi = slicer.vtkMRMLAnnotationROINode()
+  slicer.mrmlScene.AddNode(roi) 
+  roi.SetXYZ(-2, 104, -80)
+  roi.SetRadiusXYZ(30, 30, 30)
+
+  #
+  # apply the cropping to the head
+  #
+  cropLogic = slicer.modules.cropvolume.logic()
+  cvpn = slicer.vtkMRMLCropVolumeParametersNode()
+  cvpn.SetROINodeID( roi.GetID() )
+  cvpn.SetInputVolumeNodeID( head.GetID() )
+  cropLogic.Apply( cvpn )
+  croppedHead = slicer.mrmlScene.GetNodeByID( cvpn.GetOutputVolumeNodeID() )
+
+  #
+  # create a label map and set it for editing
+  #
+  volumesLogic = slicer.modules.volumes.logic()
+  croppedHeadLabel = volumesLogic.CreateLabelVolume( slicer.mrmlScene, croppedHead, croppedHead.GetName() + '-label' )
+  selectionNode = slicer.app.applicationLogic().GetSelectionNode()
+  selectionNode.SetReferenceActiveVolumeID( croppedHead.GetID() )
+  selectionNode.SetReferenceActiveLabelVolumeID( croppedHeadLabel.GetID() )
+  slicer.app.applicationLogic().PropagateVolumeSelection(0)
+
+  #
+  # got to the editor and do some drawing
+  #
+  editUtil = EditorLib.EditUtil.EditUtil()
+  parameterNode = editUtil.getParameterNode()
+  lm = slicer.app.layoutManager()
+  paintEffect = EditorLib.PaintEffectOptions()
+  paintEffect.setMRMLDefaults()
+  paintEffect.__del__()
+  sliceWidget = lm.sliceWidget('Red')
+  paintTool = EditorLib.PaintEffectTool(sliceWidget)
+  editUtil.setLabel(1)
+  paintTool.paintAddPoint(100,100)
+  paintTool.paintApply()
+  editUtil.setLabel(2)
+  paintTool.paintAddPoint(200,200)
+  paintTool.paintApply()
+  paintTool = None
+
+  #
+  # now do GrowCut
+  #
+  growCutLogic = EditorLib.GrowCutEffectLogic(sliceWidget.sliceLogic())
+  growCutLogic.growCut()
+
+  #
+  # now split the volume, merge it back, and see if it looks right
+  #
+  slicer.util.selectModule('Editor')
+  slicer.util.findChildren(text='Split Merge Volume')[0].clicked()
+  slicer.util.findChildren(text='Merge All')[0].clicked()
+
+
+
+
+
 
