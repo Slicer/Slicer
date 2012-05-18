100ad4afd9782eaf241a975a86d5eb8e60423213
Michael Jeulinl <michael.jeulinl@kitware.com>
2012-05-18 14:36:47 +0000

    ENH: Alias application image resources for SlicerApp.
    Make resources access more generic.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20113
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20113 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/SlicerApp/Main.cxx
+++ Applications/SlicerApp/Main.cxx
@@ -249,7 +249,7 @@ int SlicerAppMain(int argc, char* argv[])
   QScopedPointer<QSplashScreen> splashScreen;
   if (showSplashScreen)
     {
-    QPixmap pixmap(":/3DSlicerSplashScreen.png");
+    QPixmap pixmap(":/SplashScreen.png");
     splashScreen.reset(new QSplashScreen(pixmap));
     splashMessage(splashScreen, "Initializing...");
     splashScreen->show();
