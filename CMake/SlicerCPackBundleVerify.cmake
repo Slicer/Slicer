45fb4aba13d134bd43273cf419e3396d3608ec87
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-05-28 22:17:10 +0000

    BUG: Fix extension bundling and simplify mechanism used to specify the main application
    1) In Applications/SlicerApp/CMakeLists.txt:
    
      Improve comment explaining what should be considered when adding an other
    application into Applications folder.
    
      Given the fact Slicer_APPLICATION_NAME is used locally, minimize confusion
    by renaming variable renaming it into SlicerApp_APPLICATION_NAME.
    
      In case the application being configured is the *Main* application,
    explicitly compare "PROJECT_NAME" with "Slicer_MAIN_PROJECT".
    
    2) Rename variable Slicer_APPLICATION_NAME used to identified the main
    application into Slicer_MAIN_PROJECT_APPLICATION_NAME. This will
    minimize confusion and help maintaining the code.
    
    3) Let's note that the mechanism used to fixup application should probably
    be extended to account for other application build in the Applications folder.
    
    4) In top-level CMakeLists, move code specific to main application description
    into its own section.
    
    5) Configure variable "Slicer_MAIN_PROJECT" and "Slicer_MAIN_PROJECT_APPLICATION_NAME"
    into SlicerConfig so that "SlicerExtensionCPackBundleFixup" could successfully
    refer to Slicer_MAIN_PROJECT_APPLICATION_NAME. This closes #2130
    
    See https://issues.slicer.org/view.php?id=2130
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20275
    git-svn-id: http://svn.slicer.org/Slicer4/branches/Slicer-4-1-1@20275 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerCPackBundleVerify.cmake
+++ CMake/SlicerCPackBundleVerify.cmake
@@ -19,11 +19,11 @@ endif()
 
 
 # Sanity checks
-set(expected_existing_vars Slicer_INSTALL_DIR)
+set(expected_existing_vars Slicer_INSTALL_DIR Slicer_MAIN_PROJECT_APPLICATION_NAME)
 foreach(var ${expected_existing_vars})
   if(NOT EXISTS "${MY_${var}}")
     message(FATAL_ERROR "Variable ${var} is set to an inexistent directory or file ! [${${var}}]")
   endif()
 endforeach()
 
-verify_app("${Slicer_INSTALL_DIR}/${${Slicer_MAIN_PROJECT}_APPLICATION_NAME}.app")
+verify_app("${Slicer_INSTALL_DIR}/${Slicer_MAIN_PROJECT_APPLICATION_NAME}.app")
