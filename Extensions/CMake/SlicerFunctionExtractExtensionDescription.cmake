427cf495766a1ab63c4f051353019d82c45cf6ed
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-06-01 17:48:58 +0000

    ENH: Extension build system now supports svnusername/svnpassword
    See https://issues.slicer.org/view.php?id=2152
    
    Closes #2152
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20307
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20307 3bd1e089-480b-0410-8dfb-8563597acbee

--- Extensions/CMake/SlicerFunctionExtractExtensionDescription.cmake
+++ Extensions/CMake/SlicerFunctionExtractExtensionDescription.cmake
@@ -26,6 +26,8 @@
 # The function defines the following variables in the caller scope:
 #  <var-prefix>_SEXT_SCM - type of source repository (i.e. 'svn', 'git', 'local')
 #  <var-prefix>_SEXT_SCMURL - URL of the associated source repository
+#  <var-prefix>_SEXT_SVNUSERNAME - Username for Subversion checkout and update
+#  <var-prefix>_SEXT_SVNPASSWORD - Password for Subversion checkout and update
 #  <var-prefix>_SEXT_SCMREVISION - REVISION associated with the source repository
 #  <var-prefix>_SEXT_BUILD_SUBDIRECTORY - Corresponds to the extension inner build directory (default is ".")
 #  <var-prefix>_SEXT_DEPENDS - list of dependencies
@@ -66,6 +68,8 @@ function(slicerFunctionExtractExtensionDescription)
   set(extension_description_tokens
     scm:
     scmurl:
+    svnusername:
+    svnpassword:""
     scmrevision:
     depends:
     build_subdirectory:.
