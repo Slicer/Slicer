83a64dc1924a989207119707f9d90f220d695ee4
Steve Pieper <pieper@bwh.harvard.edu>
2012-06-19 12:00:20 +0000

    ENH: add reload button to the default scripted extension
    This allows developers to quickly iterate on new code.  The button
    can be removed when the module is ready for users.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20400
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20400 3bd1e089-480b-0410-8dfb-8563597acbee

--- Extensions/Testing/ScriptedLoadableExtensionTemplate/ScriptedLoadableExtensionTemplate.py
+++ Extensions/Testing/ScriptedLoadableExtensionTemplate/ScriptedLoadableExtensionTemplate.py
@@ -9,13 +9,12 @@ class ScriptedLoadableExtensionTemplate:
     parent.title = "Scripted Loadable Extension Template"
     parent.categories = ["Examples"]
     parent.dependencies = []
-    parent.contributors = ["Jean-Christophe Fillion-Robin (Kitware)"] # replace with "Firstname Lastname (Org)"
+    parent.contributors = ["Jean-Christophe Fillion-Robin (Kitware), Steve Pieper (Isomics)"] # replace with "Firstname Lastname (Org)"
     parent.helpText = """
     Example of scripted loadable extension.
     """
     parent.acknowledgementText = """
-    This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
-and was partially funded by NIH grant 3P41RR013218-12S1.
+    This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc. and Steve Pieper, Isomics, Inc.  and was partially funded by NIH grant 3P41RR013218-12S1.
 """ # replace with organization, grant and thanks.
     self.parent = parent
 
@@ -38,6 +37,16 @@ class ScriptedLoadableExtensionTemplateWidget:
 
   def setup(self):
     # Instantiate and connect widgets ...
+
+    # reload button
+    # (use this during development, but remove it when delivering
+    #  your module to users)
+    self.reloadButton = qt.QPushButton("Reload")
+    self.reloadButton.toolTip = "Reload this module."
+    self.reloadButton.name = "ScriptedLoadableExtensionTemplate Reload"
+    self.layout.addWidget(self.reloadButton)
+    self.reloadButton.connect('clicked()', self.onReload)
+    
 
     # Collapsible button
     dummyCollapsibleButton = ctk.ctkCollapsibleButton()
@@ -62,3 +71,35 @@ class ScriptedLoadableExtensionTemplateWidget:
   def onHelloWorldButtonClicked(self):
     print "Hello World !"
 
+  def onReload(self,moduleName="ScriptedLoadableExtensionTemplate"):
+    """Generic reload method for any scripted module.
+    ModuleWizard will subsitute correct default moduleName.
+    """
+    import imp, sys, os, slicer
+
+    widgetName = moduleName + "Widget"
+
+    # reload the source code
+    # - set source file path
+    # - load the module to the global space
+    filePath = eval('slicer.modules.%s.path' % moduleName.lower())
+    p = os.path.dirname(filePath)
+    if not sys.path.__contains__(p):
+      sys.path.insert(0,p)
+    fp = open(filePath, "r")
+    globals()[moduleName] = imp.load_module(
+        moduleName, fp, filePath, ('.py', 'r', imp.PY_SOURCE))
+    fp.close()
+
+    # rebuild the widget
+    # - find and hide the existing widget
+    # - create a new widget in the existing parent
+    parent = slicer.util.findChildren(name='%s Reload' % moduleName)[0].parent()
+    for child in parent.children():
+      try:
+        child.hide()
+      except AttributeError:
+        pass
+    globals()[widgetName.lower()] = eval(
+        'globals()["%s"].%s(parent)' % (moduleName, widgetName))
+    globals()[widgetName.lower()].setup()
