Slicer/Slicer

commit e3c7c4ebe5aee625f038e9f3838825654c63616f

ENH: add window/level interaction mode to vtkSliceViewInteractorStyle
This replaces the current VolumeDisplaySWidget Tcl code.
New mode AdjustWindowLevel is added to enum and corresponding
Start/End-AdjustWindowLevel methods are implemented.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20460
git-svn-id: http://svn.slicer.org/Slicer4/trunk@20460 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/DisplayableManager/vtkSliceViewInteractorStyle.cxx
@@ -228,6 +228,10 @@ void vtkSliceViewInteractorStyle::OnLeftButtonDown()
     {
     this->StartBlend();
     }
+  else
+    {
+    this->StartAdjustWindowLevel();
+    }
   this->SetActionStartWindow(this->GetInteractor()->GetEventPosition());
   this->SetLastActionWindow(this->GetInteractor()->GetEventPosition());
   this->Superclass::OnLeftButtonDown();
@@ -242,6 +246,10 @@ void vtkSliceViewInteractorStyle::OnLeftButtonUp()
   else if (this->ActionState == this->Blend)
     {
     this->EndBlend();
+    }
+  else
+    {
+    this->EndAdjustWindowLevel();
     }
   this->Superclass::OnLeftButtonUp();
 }
@@ -302,6 +310,28 @@ void vtkSliceViewInteractorStyle::OnMouseMove()
         {
         sliceCompositeNode->SetLabelOpacity(newLabelOpacity);
         }
+      }
+      break;
+    case vtkSliceViewInteractorStyle::AdjustWindowLevel:
+      {
+      int startX = this->GetActionStartWindow()[0];
+      int startY = this->GetActionStartWindow()[0];
+      int deltaX = windowX - startX;
+      int deltaY = windowY - startY;
+      int deltaSX = (windowX - startX)*(windowX - startX);
+      int deltaSY = (windowY - startY)*(windowY - startY);
+
+      double rangeLow = this->GetActionStartVolumeRangeLow();
+      double rangeHigh = this->GetActionStartVolumeRangeHigh();
+      double gain = (rangeHigh - rangeLow) / 10.0;
+      double newWindow = this->GetActionStartVolumeWindow() + (gain * deltaX);
+      double newLevel = this->GetActionStartVolumeLevel() + (gain * deltaY);
+
+      // Ignore the first point because it is not initialized correctly.
+      //if ( (lastX == 0) || (lastY == 0) ) window = 0;
+      // Filter out small changes from incidental clicks
+      if ( !(deltaSX < 2 || deltaSY < 2) )
+        this->SliceLogic->SetBackgroundWindowLevel(newWindow, newLevel);
       }
       break;
     default:
@@ -435,6 +465,31 @@ void vtkSliceViewInteractorStyle::EndBlend()
   this->SetActionStartForegroundOpacity(sliceCompositeNode->GetForegroundOpacity());
   this->SetActionStartLabelOpacity(sliceCompositeNode->GetLabelOpacity());
 }
+
+//----------------------------------------------------------------------------
+void vtkSliceViewInteractorStyle::StartAdjustWindowLevel()
+{
+  this->SetActionState(this->AdjustWindowLevel);
+  double window = 0.0;
+  double level = 0.0;
+  double rangeLow = 0.0;
+  double rangeHigh = 0.0;
+  this->SliceLogic->GetBackgroundWindowLevelAndRange(window, level,
+                                                     rangeLow, rangeHigh);
+  this->SetActionStartVolumeWindow(window);
+  this->SetActionStartVolumeLevel(level);
+  this->SetActionStartVolumeRangeLow(rangeLow);
+  this->SetActionStartVolumeRangeHigh(rangeHigh);
+}
+
+//----------------------------------------------------------------------------
+void vtkSliceViewInteractorStyle::EndAdjustWindowLevel()
+{
+  this->SetActionState(this->None);
+  this->SetActionStartVolumeWindow(0);
+  this->SetActionStartVolumeLevel(0);
+}
+
 
 //----------------------------------------------------------------------------
 void vtkSliceViewInteractorStyle::GetEventRAS(double ras[4])

Libs/MRML/DisplayableManager/vtkSliceViewInteractorStyle.h
@@ -84,7 +84,8 @@ public:
     Translate,
     Zoom,
     Rotate, /* Rotate not currently used */
-    Blend /* fg to bg, labelmap to bg */
+    Blend, /* fg to bg, labelmap to bg */
+    AdjustWindowLevel
     };
   vtkGetMacro(ActionState, int);
   vtkSetMacro(ActionState, int);
@@ -109,6 +110,18 @@ public:
   vtkSetMacro(ActionStartForegroundOpacity, double);
   vtkGetMacro(ActionStartLabelOpacity, double);
   vtkSetMacro(ActionStartLabelOpacity, double);
+
+  /// what was the state of the Window/Level when the action started
+  vtkGetMacro(ActionStartVolumeWindow, double);
+  vtkSetMacro(ActionStartVolumeWindow, double);
+  vtkGetMacro(ActionStartVolumeLevel, double);
+  vtkSetMacro(ActionStartVolumeLevel, double);
+
+  /// what was the state of the Scalar Range when the action started
+  vtkGetMacro(ActionStartVolumeRangeLow, double);
+  vtkSetMacro(ActionStartVolumeRangeLow, double);
+  vtkGetMacro(ActionStartVolumeRangeHigh, double);
+  vtkSetMacro(ActionStartVolumeRangeHigh, double);
 
   /// what was the state of the slice node when the action started
   vtkGetObjectMacro(ActionStartSliceToRAS, vtkMatrix4x4);
@@ -144,6 +157,11 @@ public:
   void StartBlend();
   void EndBlend();
 
+  /// Enter a mode where the mouse moves are used to change the window/level
+  /// setting.
+  void StartAdjustWindowLevel();
+  void EndAdjustWindowLevel();
+
   /// Get the RAS coordinates of the interactor's EventPosition
   /// with respect to the current poked renderer (taking into
   /// account the lightbox)
@@ -176,6 +194,10 @@ protected:
 
   double ActionStartForegroundOpacity;
   double ActionStartLabelOpacity;
+  double ActionStartVolumeWindow;
+  double ActionStartVolumeLevel;
+  double ActionStartVolumeRangeLow;
+  double ActionStartVolumeRangeHigh;
 
   vtkMatrix4x4 *ActionStartSliceToRAS;
   vtkMatrix4x4 *ActionStartXYToRAS;

commit 2a0d6308a30b6b06dc61a23be82c006acb839a18

BUG: Crash when interacting with views
Crashes introduced by r20460 and r20455

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20477
git-svn-id: http://svn.slicer.org/Slicer4/trunk@20477 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/DisplayableManager/vtkMRMLModelSliceDisplayableManager.cxx
@@ -454,7 +454,9 @@ void vtkMRMLModelSliceDisplayableManager::vtkInternal
     return;
     
   PipelinesCacheType::iterator it;
-  for (it = this->DisplayPipelines.begin(); it != this->DisplayPipelines.end(); ++it)
+  for (it = this->DisplayPipelines.begin();
+       it != this->DisplayPipelines.end();
+       it = this->DisplayPipelines.begin())
     {
     this->RemoveDisplayNode(it->first);
     }

Libs/MRML/DisplayableManager/vtkSliceViewInteractorStyle.cxx
@@ -331,7 +331,9 @@ void vtkSliceViewInteractorStyle::OnMouseMove()
       //if ( (lastX == 0) || (lastY == 0) ) window = 0;
       // Filter out small changes from incidental clicks
       if ( !(deltaSX < 2 || deltaSY < 2) )
+        {
         this->SliceLogic->SetBackgroundWindowLevel(newWindow, newLevel);
+        }
       }
       break;
     default:

Libs/MRML/Logic/vtkMRMLSliceLogic.cxx
@@ -753,11 +753,11 @@ void vtkMRMLSliceLogic
   vtkMRMLScalarVolumeNode* volumeNode = 
     vtkMRMLScalarVolumeNode::SafeDownCast( this->GetLayerVolumeNode (0) ); 
     // 0 is background layer, defined in this::GetLayerVolumeNode
-  vtkMRMLScalarVolumeDisplayNode* volumeDisplayNode = NULL;
-  if (volumeNode)
+  vtkMRMLScalarVolumeDisplayNode* volumeDisplayNode =
+    volumeNode ? volumeNode->GetScalarVolumeDisplayNode() : 0;
+  if (!volumeDisplayNode)
     {
-     volumeDisplayNode = 
-      vtkMRMLScalarVolumeDisplayNode::SafeDownCast( volumeNode->GetVolumeDisplayNode() );
+    return;
     }
   int disabledModify = volumeDisplayNode->StartModify();
   volumeDisplayNode->SetAutoWindowLevel(0);