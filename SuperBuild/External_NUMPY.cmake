5a0d229025f0309f257cb2db0328888e6a9c332f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-07-02 02:28:12 +0000

    BUG: Prevent NUMPY runtime issue by using its built-in "clapack"
    Summary:
    
    * CLAPACK is not needed anymore.
    
    * Also refactored and simplified the different NUMPY steps. There are now
    three distinct steps: configure, make and install
    
    Details:
    
    * All the steps include a common file named "NUMPY_environement.cmake" that
    both set the environment within each step and provide a convenient
    macro "numpy_setup_command" allowing to invoke "setup.by".
    
    * After investigating, it resumes it is pointless to compile CLAPACK and
    build numpy against it. Indeed, the modules "blas_lite.c" and
    "dlapack_lite.c" already bundle part of CLAPACK that are required
    by "linalg" numpy module.
    
    While looking for the root cause of the problem, I also realized
    that CLAPACK can't be linked against numpy. The "f2c.h" bundled
    within clapack-3.2.1-CMAKE.tgz is different from the "f2c.h"
    available in "numpy/linalg" folder. For example, the typedefs used
    to specify "integer" or "uinteger" are different.
    
    In clapack/INCLUDE/f2c.h => "integer" is defined as "long int"
    In numpy/linalg/f2c.h => "integer" is defined as "int"
    
    Within the python module "lapack_litemodule.c", we can read that
    the function signature used to specify the function to link against
    is of the form:
    
     extern int foo_(char *arg1, char *arg2, int *n)
    
    whereas in CLapack, the same function is defined as:
    
     int foo_(char *arg1, char *arg2, integer *n)    // integer being "long int"
    
    This difference in type was causing the error reported in the bug.
    
    While working on that issue, I also created a small project
    illustrating the problem. See https://github.com/jcfr/CFunclash#readme
    
    On linux 64bit (Ubuntu 10.04), the following numpy test suite run without
    any errors:
    
    => nose version 1.1.2
    => gcc 4.4.3
    
    >>> numpy.test()
    [...]
    Ran 2483 tests in 10.194s
    
    OK (KNOWNFAIL=4, SKIP=12)
    
    
    Details (package by package)
    
    >>> np.linalg.test()
    Running unit tests for numpy.linalg
    NumPy version 1.4.1
    [...]
    Ran 116 tests in 0.203s
    
    OK
    <nose.result.TextTestResult run=116 errors=0 failures=0>
    
    
    >>> np.ma.test()
    Running unit tests for numpy.ma
    NumPy version 1.4.1
    [...]
    Ran 282 tests in 1.011s
    
    OK
    <nose.result.TextTestResult run=282 errors=0 failures=0>
    
    
    >>> np.polynomial.test()
    Running unit tests for numpy.polynomial
    NumPy version 1.4.1
    [...]
    Ran 86 tests in 0.324s
    
    OK
    <nose.result.TextTestResult run=86 errors=0 failures=0>
    
    
    >>> np.random.test()
    Running unit tests for numpy.random
    NumPy version 1.4.1
    [...]
    Ran 11 tests in 0.020s
    
    OK
    <nose.result.TextTestResult run=11 errors=0 failures=0>
    
    
    >>> np.fft.test()
    Running unit tests for numpy.fft
    NumPy version 1.4.1
    [...]
    Ran 5 tests in 0.023s
    
    OK
    <nose.result.TextTestResult run=5 errors=0 failures=0>
    
    
    >>> np.core.test()
    Running unit tests for numpy.core
    NumPy version 1.4.1
    [...]
    Ran 861 tests in 2.181s
    
    OK (KNOWNFAIL=4, SKIP=11)
    <nose.result.TextTestResult run=861 errors=0 failures=0>
    
    
    >>> np.linalg.test()
    Running unit tests for numpy.linalg
    NumPy version 1.4.1
    [...]
    Ran 116 tests in 0.184s
    
    OK
    <nose.result.TextTestResult run=116 errors=0 failures=0>
    
    >>> np.matrixlib.test()
    Running unit tests for numpy.matrixlib
    NumPy version 1.4.1
    [...]
    Ran 38 tests in 0.077s
    
    OK
    <nose.result.TextTestResult run=38 errors=0 failures=0>
    
    
    >>> np.lib.test()
    Running unit tests for numpy.lib
    NumPy version 1.4.1
    [...]
    Ran 994 tests in 6.313s
    
    OK (SKIP=1)
    
    >>> numpy.oldnumeric.test()
    [...]
    Ran 2 tests in 0.020s
    
    OK
    
    
    >>> numpy.testing.test()
    [...]
    Ran 69 tests in 0.128s
    
    OK
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20539
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20539 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_NUMPY.cmake
+++ SuperBuild/External_NUMPY.cmake
@@ -1,15 +1,3 @@
-## NUMPY requires fortran, so set up the fortran compiler properly
-## http://www.cmake.org/Wiki/CMakeForFortranExample
-
-set(CMAKE_Fortran_COMPILER_WORKS FALSE)
-if(NOT WIN32)
-  # Work-around for CMake issue 0009220
-  if(DEFINED CMAKE_Fortran_COMPILER AND CMAKE_Fortran_COMPILER MATCHES "^$")
-    set(CMAKE_Fortran_COMPILER CMAKE_Fortran_COMPILER-NOTFOUND)
-  endif()
-
-  enable_language(Fortran OPTIONAL)
-endif()
 
 # Make sure this file is included only once
 get_filename_component(CMAKE_CURRENT_LIST_FILENAME ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
@@ -19,63 +7,39 @@ endif()
 set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
 
 # Set dependency list
-set(NUMPY_DEPENDENCIES CLAPACK python)
+set(NUMPY_DEPENDENCIES python)
 
 # Include dependent projects if any
 SlicerMacroCheckExternalProjectDependency(NUMPY)
 set(proj NUMPY)
 
-if(CMAKE_Fortran_COMPILER_WORKS)
-  # FFLAGS depend on the compiler
-  get_filename_component(Fortran_COMPILER_NAME ${CMAKE_Fortran_COMPILER} NAME)
-
-  if(Fortran_COMPILER_NAME STREQUAL "gfortran")
-    # gfortran
-    set(CMAKE_Fortran_FLAGS_RELEASE "-funroll-all-loops -fno-f2c -O3")
-    set(CMAKE_Fortran_FLAGS_DEBUG   "-fno-f2c -O0 -g")
-  elseif(Fortran_COMPILER_NAME STREQUAL "ifort")
-    # ifort (untested)
-    set(CMAKE_Fortran_FLAGS_RELEASE "-f77rtl -O3")
-    set(CMAKE_Fortran_FLAGS_DEBUG   "-f77rtl -O0 -g")
-  elseif(Fortran_COMPILER_NAME STREQUAL "g77")
-    # g77
-    message(FATAL_ERROR "The g77 compiler should not be used as specified on NUMPY page http://www.scipy.org/Installing_SciPy/Linux")
-  else()
-    message(STATUS "No optimized Fortran compiler flags are known, defaulting to-O2...")
-    set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
-    set(CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g")
-  endif()
-  message(STATUS "${__${proj}_superbuild_message} - CMAKE_Fortran_COMPILER: " ${CMAKE_Fortran_COMPILER})
-  message(STATUS "${__${proj}_superbuild_message} - Fortran_COMPILER_NAME: " ${Fortran_COMPILER_NAME})
-  message(STATUS "${__${proj}_superbuild_message} - CMAKE_Fortran_FLAGS_RELEASE: " ${CMAKE_Fortran_FLAGS_RELEASE})
-  message(STATUS "${__${proj}_superbuild_message} - CMAKE_Fortran_FLAGS_DEBUG: " ${CMAKE_Fortran_FLAGS_DEBUG})
-else()
-  message(STATUS "${__${proj}_superbuild_message} - No Fortran compiler found - Non-optimized code will be built !")
-endif()
+message(STATUS "${__${proj}_superbuild_message} - Building without Fortran compiler - Non-optimized code will be built !")
 
 #message(STATUS "${__indent}Adding project ${proj}")
 
 set(numpy_URL http://svn.slicer.org/Slicer3-lib-mirrors/trunk/numpy-1.4.1.tar.gz)
 set(numpy_MD5 5c7b5349dc3161763f7f366ceb96516b)
 
-# Note: Both NUMPY_configure_step.cmake and NUMPY_make_step.cmake expects
-#       this variable to be defined.
-set(NUMPY_DIR "${CMAKE_BINARY_DIR}/NUMPY")
+#------------------------------------------------------------------------------
+set(NUMPY_DIR "${CMAKE_BINARY_DIR}/${proj}")
 
-# To configure numpy we run a cmake -P script
-# the script will create a site.cfg file
-# then run python setup.py config to verify setup
 configure_file(
-  SuperBuild/NUMPY_configure_step.cmake.in
-  ${CMAKE_CURRENT_BINARY_DIR}/NUMPY_configure_step.cmake @ONLY)
+  SuperBuild/${proj}_environment.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/${proj}_environment.cmake @ONLY)
 
-# To build numpy we also run a cmake -P script.
-# the script will set LD_LIBRARY_PATH so that
-# python can run after it is built on linux
+configure_file(
+  SuperBuild/${proj}_configure_step.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/${proj}_configure_step.cmake @ONLY)
+
 configure_file(
   SuperBuild/NUMPY_make_step.cmake.in
-  ${CMAKE_CURRENT_BINARY_DIR}/NUMPY_make_step.cmake @ONLY)
+  ${CMAKE_CURRENT_BINARY_DIR}/${proj}_make_step.cmake @ONLY)
 
+configure_file(
+  SuperBuild/${proj}_install_step.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/${proj}_install_step.cmake @ONLY)
+
+#------------------------------------------------------------------------------
 ExternalProject_Add(${proj}
   URL ${numpy_URL}
   URL_MD5 ${numpy_MD5}
@@ -83,11 +47,12 @@ ExternalProject_Add(${proj}
   SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/NUMPY
   BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/NUMPY
   CONFIGURE_COMMAND ${CMAKE_COMMAND}
-    -DCONFIG_TYPE=${CMAKE_CFG_INTDIR} -P ${CMAKE_CURRENT_BINARY_DIR}/NUMPY_configure_step.cmake
+    -P ${CMAKE_CURRENT_BINARY_DIR}/${proj}_configure_step.cmake
   BUILD_COMMAND ${CMAKE_COMMAND}
-    -P ${CMAKE_CURRENT_BINARY_DIR}/NUMPY_make_step.cmake
+    -P ${CMAKE_CURRENT_BINARY_DIR}/${proj}_make_step.cmake
   UPDATE_COMMAND ""
-  INSTALL_COMMAND ""
+  INSTALL_COMMAND ${CMAKE_COMMAND}
+    -P ${CMAKE_CURRENT_BINARY_DIR}/${proj}_install_step.cmake
   DEPENDS
     ${NUMPY_DEPENDENCIES}
   )
