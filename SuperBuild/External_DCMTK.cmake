2ada818666f77bc9cc7b740a1095437ca88cb9a1
Steve Pieper <pieper@bwh.harvard.edu>
2012-07-19 16:15:45 +0000

    COMP: Integrate slicer-built dcmtk with FindSlicer.cmake conventions
    Addresses suggestions made by jc:
    https://github.com/pieper/Slicer/commit/716dc9496642b6b08e9308227111707d6a2e6901#commitcomment-1517211
    
    Except that for now we have not implemented a
     Slicer_BUILD_DICOM_SUPPORT
    until we have a definite use case that requires it.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20634
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20634 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_DCMTK.cmake
+++ SuperBuild/External_DCMTK.cmake
@@ -13,47 +13,52 @@ set(DCMTK_DEPENDENCIES "")
 SlicerMacroCheckExternalProjectDependency(DCMTK)
 set(proj DCMTK)
 
-# Set CMake OSX variable to pass down the external project
-set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)
-if(APPLE)
-  list(APPEND CMAKE_OSX_EXTERNAL_PROJECT_ARGS
-    -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
-    -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
+if(NOT DEFINED DCMTK_DIR)
+  # Set CMake OSX variable to pass down the external project
+  set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)
+  if(APPLE)
+    list(APPEND CMAKE_OSX_EXTERNAL_PROJECT_ARGS
+      -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+      -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
+  endif()
+
+  set(DCMTK_REPOSITORY ${git_protocol}://git.dcmtk.org/dcmtk.git.rt)
+  set(DCMTK_GIT_TAG "master")
+
+  ExternalProject_Add(${proj}
+    GIT_REPOSITORY ${DCMTK_REPOSITORY}
+    GIT_TAG ${DCMTK_GIT_TAG}
+    SOURCE_DIR ${proj}
+    BINARY_DIR ${proj}-build
+    INSTALL_DIR ${proj}-install
+    UPDATE_COMMAND ""
+    CMAKE_GENERATOR ${gen}
+    CMAKE_ARGS
+      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+      -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+      -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+      -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+      -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
+      ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
+      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+      -DBUILD_SHARED_LIBS:BOOL=OFF
+      -DDCMTK_WITH_DOXYGEN:BOOL=OFF
+      -DDCMTK_WITH_ZLIB:BOOL=OFF # see CTK github issue #25
+      -DDCMTK_WITH_OPENSSL:BOOL=OFF # see CTK github issue #25
+      -DDCMTK_WITH_PNG:BOOL=OFF # see CTK github issue #25
+      -DDCMTK_WITH_TIFF:BOOL=OFF  # see CTK github issue #25
+      -DDCMTK_WITH_XML:BOOL=OFF  # see CTK github issue #25
+      -DDCMTK_WITH_ICONV:BOOL=OFF  # see CTK github issue #178
+      -DDCMTK_FORCE_FPIC_ON_UNIX:BOOL=ON
+      -DDCMTK_OVERWRITE_WIN32_COMPILER_FLAGS:BOOL=OFF
+    DEPENDS
+      ${DCMTK_DEPENDENCIES}
+  )
+
+  set(DCMTK_DIR ${CMAKE_BINARY_DIR}/${proj}-install)
+else()
+  # The project is provided with DCMTK_DIR, nevertheless since other project may depend on DCMTK_DIR,
+  # let's add an 'empty' one
+  SlicerMacroEmptyExternalProject(${proj} "${DCMTK_DEPENDENCIES}")
 endif()
-
-set(DCMTK_REPOSITORY ${git_protocol}://git.dcmtk.org/dcmtk.git.rt)
-set(DCMTK_GIT_TAG "master")
-
-ExternalProject_Add(${proj}
-  GIT_REPOSITORY ${DCMTK_REPOSITORY}
-  GIT_TAG ${DCMTK_GIT_TAG}
-  SOURCE_DIR ${proj}
-  BINARY_DIR ${proj}-build
-  INSTALL_DIR ${proj}-install
-  UPDATE_COMMAND ""
-  CMAKE_GENERATOR ${gen}
-  CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-    -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
-    -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
-    -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
-    -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
-    ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-    -DBUILD_SHARED_LIBS:BOOL=OFF
-    -DDCMTK_WITH_DOXYGEN:BOOL=OFF
-    -DDCMTK_WITH_ZLIB:BOOL=OFF # see CTK github issue #25
-    -DDCMTK_WITH_OPENSSL:BOOL=OFF # see CTK github issue #25
-    -DDCMTK_WITH_PNG:BOOL=OFF # see CTK github issue #25
-    -DDCMTK_WITH_TIFF:BOOL=OFF  # see CTK github issue #25
-    -DDCMTK_WITH_XML:BOOL=OFF  # see CTK github issue #25
-    -DDCMTK_WITH_ICONV:BOOL=OFF  # see CTK github issue #178
-    -DDCMTK_FORCE_FPIC_ON_UNIX:BOOL=ON
-    -DDCMTK_OVERWRITE_WIN32_COMPILER_FLAGS:BOOL=OFF
-  DEPENDS
-    ${DCMTK_DEPENDENCIES}
-)
-
-set(DCMTK_DIR ${CMAKE_BINARY_DIR}/${proj}-install)
-
