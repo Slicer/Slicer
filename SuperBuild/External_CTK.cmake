06140c19f1c9b44cda3def2ecb4359359c6038bb
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-07-24 09:40:30 +0000

    COMP: Update CTK to fix compilation warnings discovered using gcc-47
    See https://github.com/commontk/CTK/issues/207,
    https://github.com/commontk/CTK/issues/212,
    https://github.com/commontk/CTK/issues/213,
    and https://github.com/commontk/CTK/issues/214
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20667
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20667 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -66,7 +66,7 @@ if(NOT DEFINED CTK_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "5fab487c193f060b7155b3db29d332cc4c402873"
+    GIT_TAG "fe1afe5d48fbf2b29055edff135ce78b4af4971a"
     "${slicer_external_update}"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
