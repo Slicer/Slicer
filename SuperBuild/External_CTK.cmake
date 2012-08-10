05a250df7f8248f7364573bcd90dc1e5b5601f62
Steve Pieper <pieper@bwh.harvard.edu>
2012-08-10 15:58:57 +0000

    COMP: update to latest CTK for DKFZ database improvements
    Improved importer and install rules.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20746
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20746 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -66,7 +66,7 @@ if(NOT DEFINED CTK_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "52eb334d493190fe07a577e19e762453864b9c7b"
+    GIT_TAG "aed2c6d6c95a5d0ae6ca3a72e14657e3a4fe6ae8"
     "${slicer_external_update}"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
