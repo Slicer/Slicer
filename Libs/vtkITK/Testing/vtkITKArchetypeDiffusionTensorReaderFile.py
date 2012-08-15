3adeab851b2ef14bf7197cc87583e51a780302d3
Demian Wassermann <demian@bwh.harvard.edu>
2012-08-15 19:31:19 +0000

    ENH: Added tests to compare the vtkITK scalar and tensor readers to the NRRD reader
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20769
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20769 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/vtkITK/Testing/vtkITKArchetypeDiffusionTensorReaderFile.py
@@ -0,0 +1,60 @@
+#Testing against the NRRD reader
+import unittest
+import slicer
+import vtkITK
+from vtk.util import numpy_support as ns
+import numpy
+
+class vtkITKReaderAgainstNRRDReader(unittest.TestCase):
+    def setUp(self):
+        import SampleData
+        sampleDataLogic = SampleData.SampleDataLogic()
+        dti = sampleDataLogic.downloadDTIBrain()
+        self.file_name = dti.GetStorageNode().GetFileName()
+
+        self.ritk = vtkITK.vtkITKArchetypeDiffusionTensorImageReaderFile()
+        self.ritk.SetArchetype(self.file_name)
+        self.ritk.Update()
+
+        self.rnrrd = slicer.vtkNRRDReader()
+        self.rnrrd.SetFileName(self.file_name)
+        self.rnrrd.Update()
+
+    def test_measurement_frame(self):
+        self.assertTrue(
+            compare_vtk_matrix(
+                self.ritk.GetMeasurementFrameMatrix(),
+                self.rnrrd.GetMeasurementFrameMatrix()
+            )
+        )
+
+    def test_ras_to_ijk(self):
+        print "ITK Matrix"
+        print self.ritk.GetRasToIjkMatrix(),
+        print "NRRD Reader Matrix"
+        print self.rnrrd.GetRasToIjkMatrix()
+
+        self.assertTrue(
+            compare_vtk_matrix(
+                self.ritk.GetRasToIjkMatrix(),
+                self.rnrrd.GetRasToIjkMatrix()
+            )
+        )
+
+    def test_pointdata(self):
+        self.assertTrue(numpy.allclose(
+            ns.vtk_to_numpy(self.rnrrd.GetOutput().GetPointData().GetTensors()),
+            ns.vtk_to_numpy(self.ritk.GetOutput().GetPointData().GetTensors())
+        ))
+
+
+
+
+def compare_vtk_matrix(m1, m2, n=4):
+    for i in xrange(0,n):
+        for j in xrange(0,n):
+            assert(m1.GetElement(i,j) == m2.GetElement(i,j))
+
+
+
+
