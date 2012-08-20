3140e1b2be4ecc45262c0ff3ec4433db47691842
Julien Finet <julien.finet@kitware.com>
2012-08-20 23:11:45 +0000

    STYLE: Cleanup vtkMRMLDoubleArrayStorageNode
    Indentation was wrong.
    vtkMRMLDoubleArrayStorageNode::ReadDataInternal now returns 0 if the file
    is empty (GetFullNameFromFileName() returns the current directory with
    no file concatenated).
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20803
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20803 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/Core/vtkMRMLDoubleArrayStorageNode.cxx
+++ Libs/MRML/Core/vtkMRMLDoubleArrayStorageNode.cxx
@@ -12,11 +12,12 @@ Version:   $Revision: 1.6 $
 
 =========================================================================auto=*/
 
-
+// MRML includes
 #include "vtkMRMLDoubleArrayStorageNode.h"
 #include "vtkMRMLDoubleArrayNode.h"
 #include "vtkMRMLScene.h"
 
+// VTK includes
 #include <vtkObjectFactory.h>
 #include <vtkStringArray.h>
 
@@ -35,8 +36,8 @@ vtkMRMLDoubleArrayStorageNode::~vtkMRMLDoubleArrayStorageNode()
 
 //----------------------------------------------------------------------------
 void vtkMRMLDoubleArrayStorageNode::PrintSelf(ostream& os, vtkIndent indent)
-{  
-    vtkMRMLStorageNode::PrintSelf(os,indent);
+{
+  vtkMRMLStorageNode::PrintSelf(os,indent);
 }
 
 //----------------------------------------------------------------------------
@@ -48,165 +49,155 @@ bool vtkMRMLDoubleArrayStorageNode::CanReadInReferenceNode(vtkMRMLNode *refNode)
 //----------------------------------------------------------------------------
 int vtkMRMLDoubleArrayStorageNode::ReadDataInternal(vtkMRMLNode *refNode)
 {
-    std::string fullName = this->GetFullNameFromFileName(); 
+  std::string fullName = this->GetFullNameFromFileName();
 
-    if (fullName == std::string("")) 
+  if (fullName == std::string(""))
     {
-        vtkErrorMacro("vtkMRMLDoubleArrayStorageNode: File name not specified");
-        return 0;
+    vtkErrorMacro("vtkMRMLDoubleArrayStorageNode: File name not specified");
+    return 0;
     }
 
-    // cast the input node
-    vtkMRMLDoubleArrayNode *doubleArrayNode =
-      vtkMRMLDoubleArrayNode::SafeDownCast(refNode);
+  // cast the input node
+  vtkMRMLDoubleArrayNode *doubleArrayNode =
+    vtkMRMLDoubleArrayNode::SafeDownCast(refNode);
 
-    if (doubleArrayNode == NULL)
+  if (doubleArrayNode == NULL)
+    {
+    vtkErrorMacro("ReadData: unable to cast input node " << refNode->GetID()
+                  << " to a double array (measurement) node");
+    return 0;
+    }
+
+  // open the file for reading input
+  fstream fstr;
+  fstr.open(fullName.c_str(), fstream::in);
+
+  if (!fstr.is_open() || !fstr.good() || fstr.eof())
+    {
+    vtkErrorMacro("ERROR opening measurement file \"" << this->FileName << "\"");
+    return 0;
+    }
+
+  char line[1024];
+  // default column ordering for measurement info
+  // independent_variable dependent_veariable err
+  int xColumn = 0;
+  int yColumn = 1;
+  int zColumn = 2;
+
+  int numColumns = 3;
+  std::vector<std::string> labels;
+  // save the valid lines in a vector, parse them once know the max id
+  std::vector<std::string>lines;
+  bool firstLine = true;
+
+  while (fstr.good())
+    {
+    fstr.getline(line, 1024);
+
+
+    // does it start with a #?
+    if (line[0] == '#')
       {
-      vtkErrorMacro("ReadData: unable to cast input node " << refNode->GetID() << " to a double array (measurement) node");
-      return 0;
+      vtkDebugMacro("Comment line, checking:\n\"" << line << "\"");
       }
 
-    // open the file for reading input
-    fstream fstr;
-
-    fstr.open(fullName.c_str(), fstream::in);
-
-    if (fstr.is_open())
-    {
-        //turn off modified events
-        int modFlag = doubleArrayNode->GetDisableModifiedEvent();   //
-        doubleArrayNode->DisableModifiedEventOn();
-        char line[1024];
-        // default column ordering for measurement info
-        // independent_variable dependent_veariable err
-        int xColumn = 0;
-        int yColumn = 1;
-        int zColumn = 2;
-
-        int numColumns = 3;
-        std::vector<std::string> labels;
-        // save the valid lines in a vector, parse them once know the max id
-        std::vector<std::string>lines;
-        bool firstLine = 1;
-
-        while (fstr.good())
-        {
-            fstr.getline(line, 1024);
-
-
-            // does it start with a #?
-            if (line[0] == '#')
-            {
-                vtkDebugMacro("Comment line, checking:\n\"" << line << "\"");
-                
-            }
-
-            else
-            {
-                // is it empty?
-                if (line[0] == '\0')
-                {
-                    vtkDebugMacro("Empty line, skipping:\n\"" << line << "\"");
-                }
-                else
-                {
-                    vtkDebugMacro("got a line: \n\"" << line << "\"");
-                    char *ptr;
-                    bool reTokenise = false;
-                    if (strncmp(line, ",", 1) == 0)
-                    {
-                        ptr = NULL;
-                        reTokenise = true;
-                    }
-                    else
-                    {
-                        ptr = strtok(line, ",");
-                    }
-                    
-                    double x = 0.0, y = 0.0, yerr = 0.0;
-                    int columnNumber = 0;
-
-                    while (columnNumber < numColumns)
-                    {
-                        if (ptr != NULL)
-                        {
-                            if (columnNumber == xColumn)
-                            {
-                                if (firstLine)
-                                {
-                                    labels.push_back(ptr);
-                                }
-                                else
-                                {
-                                    x = atof(ptr);
-                                }
-                            }
-                            else if (columnNumber == yColumn)
-                            {
-                                if (firstLine)
-                                {
-                                    labels.push_back(ptr);
-                                }
-                                else
-                                {
-                                    y = atof(ptr);
-                                }
-                            }
-                            else if (columnNumber == zColumn)
-                            {
-                                if (firstLine)
-                                {
-                                    labels.push_back(ptr);
-                                }
-                                else
-                                {
-                                    yerr = atof(ptr);
-                                }
-                            }
-                        }
-                        if (reTokenise == false)
-                        {
-                            ptr = strtok(NULL, ",");
-                        }
-                        else
-                        {
-                            ptr = strtok(line, ",");
-                            // turn it off
-                            reTokenise = false;
-                        }
-                        columnNumber++;
-                    } // end while over columns
-                    
-                    int   fidIndex;
-                    if (firstLine)
-                    {
-                        doubleArrayNode->vtkMRMLDoubleArrayNode::SetLabels(labels);
-                    }
-                    else
-                    {
-                        fidIndex = doubleArrayNode->vtkMRMLDoubleArrayNode::AddXYValue(x, y, yerr);
-                        if (fidIndex == 0)
-                        {
-                            vtkErrorMacro("Error adding a measurement to the list");
-                        }
-                    }
-                    firstLine = 0;
-
-                } // point line
-            }
-        }
-        doubleArrayNode->SetDisableModifiedEvent(modFlag);
-        doubleArrayNode->InvokeEvent(vtkMRMLScene::NodeAddedEvent, doubleArrayNode);//vtkMRMLFiducialListNode::DisplayModifiedEvent);
-        fstr.close();
-    }
-
     else
-    {
-        vtkErrorMacro("ERROR opening measurement file " << this->FileName << endl);
-        return 0;
-    }
+      {
+      // is it empty?
+      if (line[0] == '\0')
+        {
+        vtkDebugMacro("Empty line, skipping:\n\"" << line << "\"");
+        }
+      else
+        {
+        vtkDebugMacro("got a line: \n\"" << line << "\"");
+        char *ptr;
+        bool reTokenise = false;
+        if (strncmp(line, ",", 1) == 0)
+          {
+          ptr = NULL;
+          reTokenise = true;
+          }
+        else
+          {
+          ptr = strtok(line, ",");
+          }
+        double x = 0.0, y = 0.0, yerr = 0.0;
+        int columnNumber = 0;
 
-  return 1;
+        while (columnNumber < numColumns)
+          {
+          if (ptr != NULL)
+            {
+            if (columnNumber == xColumn)
+              {
+              if (firstLine)
+                {
+                labels.push_back(ptr);
+                }
+              else
+                {
+                x = atof(ptr);
+                }
+              }
+            else if (columnNumber == yColumn)
+              {
+              if (firstLine)
+                {
+                labels.push_back(ptr);
+                }
+              else
+                {
+                y = atof(ptr);
+                }
+              }
+            else if (columnNumber == zColumn)
+              {
+              if (firstLine)
+                {
+                labels.push_back(ptr);
+                }
+              else
+                {
+                yerr = atof(ptr);
+                }
+              }
+            }
+          if (reTokenise == false)
+            {
+            ptr = strtok(NULL, ",");
+            }
+          else
+            {
+            ptr = strtok(line, ",");
+            // turn it off
+            reTokenise = false;
+            }
+          columnNumber++;
+          } // end while over columns
+        int   fidIndex;
+        if (firstLine)
+          {
+          doubleArrayNode->vtkMRMLDoubleArrayNode::SetLabels(labels);
+          }
+        else
+          {
+          fidIndex = doubleArrayNode->vtkMRMLDoubleArrayNode::AddXYValue(x, y, yerr);
+          if (fidIndex == 0)
+            {
+            vtkErrorMacro("Error adding a measurement to the list");
+            }
+          }
+        firstLine = false;
+
+        } // point line
+      }
+    }
+  fstr.close();
+
+  // If it's the first line, that means there was no point in the file.
+  return firstLine ? 0 : 1;
 }
 
 //----------------------------------------------------------------------------
