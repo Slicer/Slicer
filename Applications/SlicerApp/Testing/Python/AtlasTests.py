fd7948107ee80106d2c2acefe0e300c406d7074b
Steve Pieper <pieper@bwh.harvard.edu>
2012-09-07 22:09:03 +0000

    ENH: Add additional self tests related to atlases
    Currently these test for download and load of the atlases, but
    do not confirm that they function or display correctly.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=20957
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@20957 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Applications/SlicerApp/Testing/Python/AtlasTests.py
@@ -0,0 +1,295 @@
+import os
+import unittest
+from __main__ import vtk, qt, ctk, slicer
+
+#
+# AtlasTests
+#
+
+class AtlasTests:
+  def __init__(self, parent):
+    parent.title = "AtlasTests" # TODO make this more human readable by adding spaces
+    parent.categories = ["Testing.TestCases"]
+    parent.dependencies = []
+    parent.contributors = ["Steve Pieper (Isomics)"] # replace with "Firstname Lastname (Org)"
+    parent.helpText = """
+    This is a self test that downloads and displays volumetric atlases from the NA-MIC publication database.
+
+    For more information:
+    
+    Abdominal Atlas: <a>http://www.slicer.org/publications/item/view/1918</a>
+    Brain Atlas: <a>http://www.slicer.org/publications/item/view/1918</a>
+    Knee Atlas: <a>http://www.slicer.org/publications/item/view/195</a>
+
+    """
+    parent.acknowledgementText = """
+    This file was originally developed by Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
+""" # replace with organization, grant and thanks.
+    self.parent = parent
+
+    # Add this test to the SelfTest module's list for discovery when the module
+    # is created.  Since this module may be discovered before SelfTests itself,
+    # create the list if it doesn't already exist.
+    try:
+      slicer.selfTests
+    except AttributeError:
+      slicer.selfTests = {}
+    slicer.selfTests['AtlasTests'] = self.runTest
+
+  def runTest(self):
+    tester = AtlasTestsTest()
+    tester.runTest()
+
+#
+# qAtlasTestsWidget
+#
+
+class AtlasTestsWidget:
+  def __init__(self, parent = None):
+    if not parent:
+      self.parent = slicer.qMRMLWidget()
+      self.parent.setLayout(qt.QVBoxLayout())
+      self.parent.setMRMLScene(slicer.mrmlScene)
+    else:
+      self.parent = parent
+    self.layout = self.parent.layout()
+    if not parent:
+      self.setup()
+      self.parent.show()
+
+  def setup(self):
+    # Instantiate and connect widgets ...
+
+    # reload button
+    # (use this during development, but remove it when delivering
+    #  your module to users)
+    self.reloadButton = qt.QPushButton("Reload")
+    self.reloadButton.toolTip = "Reload this module."
+    self.reloadButton.name = "AtlasTests Reload"
+    self.layout.addWidget(self.reloadButton)
+    self.reloadButton.connect('clicked()', self.onReload)
+
+    # reload and test button
+    # (use this during development, but remove it when delivering
+    #  your module to users)
+    self.reloadAndTestButton = qt.QPushButton("Reload and Test")
+    self.reloadAndTestButton.toolTip = "Reload this module and then run the self tests."
+    self.layout.addWidget(self.reloadAndTestButton)
+    self.reloadAndTestButton.connect('clicked()', self.onReloadAndTest)
+
+    # Collapsible button
+    atlasTests = ctk.ctkCollapsibleButton()
+    atlasTests.text = "Atlas Tests"
+    self.layout.addWidget(atlasTests)
+
+    # Layout within the dummy collapsible button
+    dummyFormLayout = qt.QFormLayout(atlasTests)
+
+    # run Abdominal Test
+    self.abdominalAtlas = qt.QPushButton("Run Abdominal Test")
+    self.abdominalAtlas.toolTip = "Downloads abdominal atlas and loads it."
+    dummyFormLayout.addWidget(self.abdominalAtlas)
+    self.abdominalAtlas.connect('clicked(bool)', self.onAbdominalAtlas)
+
+    # run brain Test
+    self.brainAtlas = qt.QPushButton("Run Brain Test")
+    self.brainAtlas.toolTip = "Downloads brain atlas and loads it."
+    dummyFormLayout.addWidget(self.brainAtlas)
+    self.brainAtlas.connect('clicked(bool)', self.onBrainAtlas)
+
+    # run knee Test
+    self.kneeAtlas = qt.QPushButton("Run Knee Test")
+    self.kneeAtlas.toolTip = "Downloads knee atlas and loads it."
+    dummyFormLayout.addWidget(self.kneeAtlas)
+    self.kneeAtlas.connect('clicked(bool)', self.onKneeAtlas)
+
+    # Add vertical spacer
+    self.layout.addStretch(1)
+
+  def onAbdominalAtlas(self):
+    tester = AtlasTestsTest()
+    tester.runAbdominalTest()
+
+  def onBrainAtlas(self):
+    tester = AtlasTestsTest()
+    tester.runBrainTest()
+
+  def onKneeAtlas(self):
+    tester = AtlasTestsTest()
+    tester.runKneeTest()
+
+  def onReload(self,moduleName="AtlasTests"):
+    """Generic reload method for any scripted module.
+    ModuleWizard will subsitute correct default moduleName.
+    """
+    import imp, sys, os, slicer
+
+    widgetName = moduleName + "Widget"
+
+    # reload the source code
+    # - set source file path
+    # - load the module to the global space
+    filePath = eval('slicer.modules.%s.path' % moduleName.lower())
+    p = os.path.dirname(filePath)
+    if not sys.path.__contains__(p):
+      sys.path.insert(0,p)
+    fp = open(filePath, "r")
+    globals()[moduleName] = imp.load_module(
+        moduleName, fp, filePath, ('.py', 'r', imp.PY_SOURCE))
+    fp.close()
+
+    # rebuild the widget
+    # - find and hide the existing widget
+    # - create a new widget in the existing parent
+    parent = slicer.util.findChildren(name='%s Reload' % moduleName)[0].parent()
+    for child in parent.children():
+      try:
+        child.hide()
+      except AttributeError:
+        pass
+    # Remove spacer items
+    item = parent.layout().itemAt(0)
+    while item:
+      parent.layout().removeItem(item)
+      item = parent.layout().itemAt(0)
+    # create new widget inside existing parent
+    globals()[widgetName.lower()] = eval(
+        'globals()["%s"].%s(parent)' % (moduleName, widgetName))
+    globals()[widgetName.lower()].setup()
+
+  def onReloadAndTest(self,moduleName="AtlasTests"):
+    self.onReload()
+    evalString = 'globals()["%s"].%sTest()' % (moduleName, moduleName)
+    tester = eval(evalString)
+    tester.runTest()
+
+#
+# AtlasTestsLogic
+#
+
+class AtlasTestsLogic:
+  """This class should implement all the actual 
+  computation done by your module.  The interface 
+  should be such that other python code can import
+  this class and make use of the functionality without
+  requiring an instance of the Widget
+  """
+  def __init__(self):
+    pass
+
+  def hasImageData(self,volumeNode):
+    """This is a dummy logic method that 
+    returns true if the passed in volume
+    node has valid image data
+    """
+    if not volumeNode:
+      print('no volume node')
+      return False
+    if volumeNode.GetImageData() == None:
+      print('no image data')
+      return False
+    return True
+
+
+class AtlasTestsTest(unittest.TestCase):
+  """
+  This is the test case for your scripted module.
+  """
+
+  def delayDisplay(self,message,msec=1000):
+    """This utility method displays a small dialog and waits.
+    This does two things: 1) it lets the event loop catch up
+    to the state of the test so that rendering and widget updates
+    have all taken place before the test continues and 2) it
+    shows the user/developer/tester the state of the test
+    so that we'll know when it breaks.
+    """
+    print(message)
+    self.info = qt.QDialog()
+    self.infoLayout = qt.QVBoxLayout()
+    self.info.setLayout(self.infoLayout)
+    self.label = qt.QLabel(message,self.info)
+    self.infoLayout.addWidget(self.label)
+    qt.QTimer.singleShot(msec, self.info.close)
+    self.info.exec_()
+
+  def setUp(self):
+    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+    slicer.mrmlScene.Clear(0)
+
+  def runTest(self):
+    """Run as few or as many tests as needed here.
+    """
+    self.setUp()
+    self.test_AbdominalAtlasTest()
+    self.setUp()
+    self.test_BrainAtlasTest()
+    self.setUp()
+    self.test_KneeAtlasTest()
+
+  def runAbdominalTest(self):
+    self.setUp()
+    self.test_AbdominalAtlasTest()
+
+  def runBrainTest(self):
+    self.setUp()
+    self.test_BrainAtlasTest()
+
+  def runKneeTest(self):
+    self.setUp()
+    self.test_KneeAtlasTest()
+
+  def test_AbdominalAtlasTest(self):
+    self.delayDisplay('Running Abdominal Atlas Test')
+    downloads = (
+        ('http://slicer.kitware.com/midas3/download?items=7926', 'AbdominalAtlas.mrb', slicer.util.loadScene),
+        )
+    self.test_AtlasTest(downloads,'I')
+
+  def test_BrainAtlasTest(self):
+    self.delayDisplay('Running Brain Atlas Test')
+    downloads = (
+        ('http://slicer.kitware.com/midas3/download?items=7927', 'BrainAtlas.mrb', slicer.util.loadScene),
+        )
+    self.test_AtlasTest(downloads,'grayscale')
+
+  def test_KneeAtlasTest(self):
+    self.delayDisplay('Running Knee Atlas Test')
+    downloads = (
+        ('http://slicer.kitware.com/midas3/download?items=7928', 'KneeAtlas.mrb', slicer.util.loadScene),
+        )
+    self.test_AtlasTest(downloads,'I')
+
+  def test_AtlasTest(self, downloads,testVolumePattern):
+    """ Ideally you should have several levels of tests.  At the lowest level
+    tests sould exercise the functionality of the logic with different inputs
+    (both valid and invalid).  At higher levels your tests should emulate the
+    way the user would interact with your code and confirm that it still works
+    the way you intended.
+    One of the most important features of the tests is that it should alert other
+    developers when their changes will have an impact on the behavior of your
+    module.  For example, if a developer removes a feature that you depend on,
+    your test should break so they know that the feature is needed.
+    """
+
+    self.delayDisplay("Starting the test")
+    #
+    # first, get some data
+    #
+    import urllib
+
+    for url,name,loader in downloads:
+      filePath = slicer.app.temporaryPath + '/' + name
+      if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
+        print('Requesting download %s from %s...\n' % (name, url))
+        urllib.urlretrieve(url, filePath)
+      if loader:
+        print('Loading %s...\n' % (name,))
+        loader(filePath)
+    self.delayDisplay('Finished with download and loading\n')
+
+    volumeNode = slicer.util.getNode(pattern=testVolumePattern)
+    logic = AtlasTestsLogic()
+    self.assertTrue( logic.hasImageData(volumeNode) )
+    self.delayDisplay('Test passed!')
