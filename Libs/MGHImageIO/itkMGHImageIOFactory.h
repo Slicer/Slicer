26d0733fc6dba895e81cd39203dce86993645d96
Matt McCormick <matt.mccormick@kitware.com>
2012-09-19 00:49:03 +0000

    STYLE: Fix style in Libs/MGHImageIO/itkMGHImageIO.h.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21008
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21008 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MGHImageIO/itkMGHImageIOFactory.h
+++ Libs/MGHImageIO/itkMGHImageIOFactory.h
@@ -11,42 +11,41 @@
 namespace itk
 {
 
-  class MGHImageIO_EXPORT MGHImageIOFactory : public ObjectFactoryBase
-  {
-  public:
-    /** Standard class typedefs **/
-    typedef MGHImageIOFactory         Self;
-    typedef ObjectFactoryBase         Superclass;
-    typedef SmartPointer<Self>        Pointer;
-    typedef SmartPointer<const Self>  ConstPointer;
+class MGHImageIO_EXPORT MGHImageIOFactory : public ObjectFactoryBase
+{
+public:
+  /** Standard class typedefs **/
+  typedef MGHImageIOFactory         Self;
+  typedef ObjectFactoryBase         Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
 
-    /** Class methods used to interface with the registered factories **/
-    virtual const char* GetITKSourceVersion(void) const;
-    virtual const char* GetDescription(void)  const;
-    
-    /** Method for class instantiation **/
-    itkFactorylessNewMacro(Self);
-    
-    /** RTTI (and related methods) **/
-    itkTypeMacro(MGHImageIOFactory, ObjectFactoryBase);
+  /** Class methods used to interface with the registered factories **/
+  virtual const char* GetITKSourceVersion(void) const;
+  virtual const char* GetDescription(void)  const;
+  
+  /** Method for class instantiation **/
+  itkFactorylessNewMacro(Self);
+  
+  /** RTTI (and related methods) **/
+  itkTypeMacro(MGHImageIOFactory, ObjectFactoryBase);
 
-    /** Register one factory of this type **/
-    static void RegisterOneFactory(void)
-      {
-  MGHImageIOFactory::Pointer MGHFactory = MGHImageIOFactory::New();
-  ObjectFactoryBase::RegisterFactory(MGHFactory);
-      }
+  /** Register one factory of this type **/
+  static void RegisterOneFactory(void)
+    {
+MGHImageIOFactory::Pointer MGHFactory = MGHImageIOFactory::New();
+ObjectFactoryBase::RegisterFactory(MGHFactory);
+    }
 
-  protected:
-    MGHImageIOFactory();
-    ~MGHImageIOFactory();
+protected:
+  MGHImageIOFactory();
+  ~MGHImageIOFactory();
 
-  private:
-    MGHImageIOFactory(const Self&); /// purposely not implemented
-    void operator=(const Self&); /// purposely not implemented
-    
-  }; /// end class MGHImageIOFactory
+private:
+  MGHImageIOFactory(const Self&); /// purposely not implemented
+  void operator=(const Self&); /// purposely not implemented
 
+}; /// end class MGHImageIOFactory
 
 } /// end namespace itk
 
