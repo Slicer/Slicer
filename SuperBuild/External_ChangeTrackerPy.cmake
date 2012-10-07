bc111f3d756744ae939911d674d6c03f0bcacb45
Fedorov <fedorov@bwh.harvard.edu>
2012-10-07 00:31:42 +0000

    ENH: add references to all wizard steps from self to facilitate automated testing
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21127
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21127 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_ChangeTrackerPy.cmake
+++ SuperBuild/External_ChangeTrackerPy.cmake
@@ -27,7 +27,7 @@ if(NOT DEFINED ChangeTrackerPy_SOURCE_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/fedorov/ChangeTrackerPy.git"
-    GIT_TAG "9353be3a3adc5df34ce23a073480964d00d01639"
+    GIT_TAG "1fd4aaa79e9a4859563598df1e8cb3ae2046d218"
     "${slicer_external_update}"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
