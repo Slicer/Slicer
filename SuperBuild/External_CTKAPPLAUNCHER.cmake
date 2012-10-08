152cf91a931cf64a99544077ffd95b755326b733
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-10-08 18:13:47 +0000

    COMP: Verify CTK AppLauncher package by specifying md5
    This will fix the following warning:
    
    // ---------------------
    -- verifying file...
         file='/Users/kitware/Dashboards/Nightly/Slicer-build-64bits-QT4.7.4-PythonQt-With-Tcl-CLI-Release-nightly/CTKAPPLAUNCHER-prefix/src/CTKAppLauncher-0.1.8-macosx-amd64.tar.gz'
    -- verifying file... warning: did not verify file - no URL_MD5 checksum argument? corrupt file?
    // ---------------------
    
    See http://slicer.cdash.org/viewBuildError.php?type=1&buildid=42212
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21133
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21133 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTKAPPLAUNCHER.cmake
+++ SuperBuild/External_CTKAPPLAUNCHER.cmake
@@ -25,10 +25,16 @@ if(Slicer_USE_CTKAPPLAUNCHER)
     # On windows, use i386 launcher unconditionally
     if("${CTKAPPLAUNCHER_OS}" STREQUAL "win")
       set(CTKAPPLAUNCHER_ARCHITECTURE "i386")
+      set(md5 "44c2bdb75d8c0bed0403dee90f71a175")
+    elseif("${CTKAPPLAUNCHER_OS}" STREQUAL "linux")
+      set(md5 "1cdf84b59aa50c032a4f6e8d0d57984c")
+    elseif("${CTKAPPLAUNCHER_OS}" STREQUAL "macosx")
+      set(md5 "36248b23933d267a4d903acd1017194f")
     endif()
     #message(STATUS "${__indent}Adding project ${proj}")
     ExternalProject_Add(${proj}
       URL http://cloud.github.com/downloads/commontk/AppLauncher/CTKAppLauncher-${launcher_version}-${CTKAPPLAUNCHER_OS}-${CTKAPPLAUNCHER_ARCHITECTURE}.tar.gz
+      URL_MD5 ${md5}
       SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
       "${slicer_external_update}"
       CONFIGURE_COMMAND ""
