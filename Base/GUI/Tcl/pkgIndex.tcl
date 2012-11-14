d52247e38877e093976b96fbc682c9845b6ee141
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-11-14 16:31:04 +0000

    COMP: Remove SWidget tcl related code
    Keep the Slicer3Adapter.tcl around since it's still required by EMSegment
    
    Later on should also remove the slicer.sliceWidet* object since these
    ones can be accessed using the slicer.app.layoutManager() object.
    
    See https://issues.slicer.org/view.php?id=2735
    Fixes #2735
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21409
    git-svn-id: http://svn.slicer.org/Slicer4/branches/Slicer-4-2@21409 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/GUI/Tcl/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-
-package ifneeded SlicerBaseGUITcl 3.0 [list 
-  source [file join $dir SWidget.tcl]
-  source [file join $dir SliceSWidget.tcl]
-  source [file join $dir GridSWidget.tcl]
-  source [file join $dir ModelSWidget.tcl]
-  source [file join $dir RulerSWidget.tcl]
-]
