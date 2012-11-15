bbb182c4f41c4811a7b2b9770a234cd4fb6f31eb
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2012-11-15 18:26:08 +0000

    COMP: Fix build error occurring when disabling QtTesting
    See https://issues.slicer.org/view.php?id=2760
    
    Fixes #2760
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21419
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21419 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/QTGUI/qSlicerBaseQTGUIPythonQtDecorators.h
+++ Base/QTGUI/qSlicerBaseQTGUIPythonQtDecorators.h
@@ -16,10 +16,14 @@
 // PythonQt includes
 #include <PythonQt.h>
 
-// CTK QtTesting includes
-#include <ctkQtTestingUtility.h>
+#include "vtkSlicerConfigure.h" // For Slicer_USE_QtTesting
 
-// SlicerQt includes
+// CTK includes
+#ifdef Slicer_USE_QtTesting
+# include <ctkQtTestingUtility.h>
+#endif
+
+// Slicer includes
 #include "qSlicerAbstractModuleRepresentation.h"
 #include "qSlicerAbstractModuleWidget.h"
 #include "qSlicerAbstractModule.h"
@@ -47,7 +51,9 @@ public:
     PythonQt::self()->registerClass(&qSlicerAbstractModuleWidget::staticMetaObject);
     PythonQt::self()->registerClass(&qSlicerPythonManager::staticMetaObject);
     PythonQt::self()->registerClass(&qSlicerCommandOptions::staticMetaObject);
+#ifdef Slicer_USE_QtTesting
     PythonQt::self()->registerClass(&ctkQtTestingUtility::staticMetaObject);
+#endif
     // Note: Use registerCPPClassForPythonQt to register pure Cpp classes
     }
 
