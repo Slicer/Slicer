bac140d47182a524d8320b60b5e4f1ca9e9694b2
Hans Johnson <hans-johnson@uiowa.edu>
2012-11-19 00:01:03 +0000

    ENH: Updated BRAINSTools.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21444
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21444 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_BRAINSTools.cmake
+++ SuperBuild/External_BRAINSTools.cmake
@@ -23,7 +23,7 @@ if(NOT DEFINED BRAINSTools_SOURCE_DIR)
 if(${ITK_VERSION_MAJOR} STREQUAL "3")
   set(GIT_TAG "fa2159607df370451e7a26d09e4e2a61b6e6c5c8" CACHE STRING "" FORCE)
 else()
-  set(GIT_TAG "4dfc44016ae3eff3ea878b68ba59b4caeec2fe4c" CACHE STRING "" FORCE) # 2012-11-12 tag with Slicer work arounds
+  set(GIT_TAG "1a467459a259d2d8b150b4d3184a73d041a1de3f" CACHE STRING "" FORCE) # 2012-11-12 tag with Slicer work arounds
 endif()
 
   if(NOT DEFINED git_protocol)
