98240c8d9b6afa7ea48253ef52f032fce240aafe
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-01-09 15:59:04 +0000

    COMP: Download AppLauncher packages from packages.kitware.com
    Since github.com deprecated its download api [1], the launcher packages
    have been uploaded on packages.kitware.com.
    
    [1] https://github.com/blog/1302-goodbye-uploads
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21571
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21571 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTKAPPLAUNCHER.cmake
+++ SuperBuild/External_CTKAPPLAUNCHER.cmake
@@ -22,18 +22,22 @@ if(Slicer_USE_CTKAPPLAUNCHER)
   if(NOT DEFINED CTKAPPLAUNCHER_DIR)
     SlicerMacroGetOperatingSystemArchitectureBitness(VAR_PREFIX CTKAPPLAUNCHER)
     set(launcher_version "0.1.10")
+    set(item_id "")
     # On windows, use i386 launcher unconditionally
     if("${CTKAPPLAUNCHER_OS}" STREQUAL "win")
       set(CTKAPPLAUNCHER_ARCHITECTURE "i386")
       set(md5 "fee82ee869cfedb54f48c58d0bad3710")
+      set(item_id "53")
     elseif("${CTKAPPLAUNCHER_OS}" STREQUAL "linux")
       set(md5 "7da4bcc68ab09833aebc156a7d0a5f06")
+      set(item_id "65")
     elseif("${CTKAPPLAUNCHER_OS}" STREQUAL "macosx")
       set(md5 "de87ffad9d5909c7525c61dac50b0c9e")
+      set(item_id "63")
     endif()
     #message(STATUS "${__indent}Adding project ${proj}")
     ExternalProject_Add(${proj}
-      URL http://cloud.github.com/downloads/commontk/AppLauncher/CTKAppLauncher-${launcher_version}-${CTKAPPLAUNCHER_OS}-${CTKAPPLAUNCHER_ARCHITECTURE}.tar.gz
+      URL http://packages.kitware.com/api/rest?method=midas.item.download&id=${item_id}&dummy=CTKAppLauncher-${launcher_version}-${CTKAPPLAUNCHER_OS}-${CTKAPPLAUNCHER_ARCHITECTURE}.tar.gz
       URL_MD5 ${md5}
       SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
       "${slicer_external_update}"
