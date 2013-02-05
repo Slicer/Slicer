28480f610b3c22a858ecd2d240561ca00e2cd043
Steve Pieper <pieper@bwh.harvard.edu>
2013-02-05 16:57:39 +0000

    COMP: restore waitForImportFinished API (no-op) for dicom indexer
    See:
    https://github.com/commontk/CTK/commit/ac7cf1665a8c8581998476a11778f78dfcedc4d9
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21664
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21664 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -66,7 +66,7 @@ if(NOT DEFINED CTK_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "b183487871c43408890b2785e58643ba8d6b1b13"
+    GIT_TAG "1e823001cb41c92667299635643f1007876d09f6"
     "${slicer_external_update}"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
