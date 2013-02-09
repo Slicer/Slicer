de736f6a38af9c12ef99aaf2963572ebbfdd223a
Fedorov <fedorov@bwh.harvard.edu>
2013-02-09 00:48:46 +0000

    ENH: disable switching to quantitative layout on entering the module; resolves #2845
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21676
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21676 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_MultiVolumeExplorer.cmake
+++ SuperBuild/External_MultiVolumeExplorer.cmake
@@ -27,7 +27,7 @@ if(NOT DEFINED MultiVolumeExplorer_SOURCE_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/fedorov/MultiVolumeExplorer.git"
-    GIT_TAG "72d55d2ab12e7301ca8ced49ad05ce28346e625d"
+    GIT_TAG "ece91b888f4b235e1d68ab20275274e753667ffb"
     "${${PROJECT_NAME}_EP_UPDATE_IF_GREATER_288}"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
