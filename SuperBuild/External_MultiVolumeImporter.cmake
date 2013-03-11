79cb983cf5a4cffffb60d2ceeb04858c88c599b2
Fedorov <fedorov@bwh.harvard.edu>
2013-03-11 23:15:18 +0000

    BUG: load() now returns the loaded node
    This behavior is consistent with the other plugins.
    
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21789
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21789 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_MultiVolumeImporter.cmake
+++ SuperBuild/External_MultiVolumeImporter.cmake
@@ -27,7 +27,7 @@ if(NOT DEFINED MultiVolumeImporter_SOURCE_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/fedorov/MultiVolumeImporter.git"
-    GIT_TAG "63883c9176c2ee1fe24803cbc2cc5268ab086aa8"
+    GIT_TAG "0f536522797203c7134b9dd577cf93d2d5f73c36"
     "${${PROJECT_NAME}_EP_UPDATE_IF_GREATER_288}"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
