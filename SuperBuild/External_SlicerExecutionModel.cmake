899cb90c90625f8c8e66d35a1b2f537a7cd7dcaf
Matt McCormick <matt.mccormick@kitware.com>
2013-03-12 20:30:02 +0000

    ENH: Bump SlicerExecutionModel ExternalProject to be11bda829.
    Include improvements to GenerateCLP.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21801
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21801 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_SlicerExecutionModel.cmake
+++ SuperBuild/External_SlicerExecutionModel.cmake
@@ -36,7 +36,7 @@ if(NOT DEFINED SlicerExecutionModel_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/Slicer/SlicerExecutionModel.git"
-    GIT_TAG "2986286df7819b4bd2c0b7e2d25efe7fd23ff51f"
+    GIT_TAG "be11bda82946770af05e14eedf3793d5d7ff4642"
     "${${PROJECT_NAME}_EP_UPDATE_IF_GREATER_288}"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
