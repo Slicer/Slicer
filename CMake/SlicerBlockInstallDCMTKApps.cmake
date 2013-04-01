047bf14f44168a401b6ae4a8aa0ffb354a7ffd00
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-04-01 22:59:33 +0000

    COMP: Fix installation of DCMTK apps on windows
    Following commit r21849, DCMTK is used from the build tree and it is required
    to specify the intermediate directory. Since package are made only in Release,
    the value is forced to Release.
    
    Ideally, dcmtk apps and library should be installed using DCMTK install
    system by setting CPACK_INSTALL_CMAKE_PROJECTS.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21855
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21855 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallDCMTKApps.cmake
+++ CMake/SlicerBlockInstallDCMTKApps.cmake
@@ -7,8 +7,12 @@ if(WIN32)
 endif()
 
 set(DCMTK_Apps storescu storescp dcmdump dump2dcm img2dcm dcmdjpeg dcmqrscp)
+set(int_dir "")
+if(WIN32)
+  set(int_dir "Release/")
+endif()
 foreach(dcmtk_App ${DCMTK_Apps})
-  install(FILES ${CTK_DCMTK_DIR}/bin/${dcmtk_App}${EXE}
+  install(FILES ${CTK_DCMTK_DIR}/bin/${int_dir}${dcmtk_App}${EXE}
     DESTINATION ${Slicer_INSTALL_BIN_DIR}
     PERMISSIONS OWNER_WRITE OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ
     COMPONENT Runtime
