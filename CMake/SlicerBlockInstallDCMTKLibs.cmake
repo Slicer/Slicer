bd4dfad5568c5fa604bd905e8c2a802aebfa5462
Steve Pieper <pieper@bwh.harvard.edu>
2013-04-26 23:54:24 +0000

    COMP: fix packaging for linux DCMTK shared libraries
    Be sure to include the symbolic links for the versioning.
    
    Also remove APPLE branch since this is not called/needed for apple.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21949
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21949 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallDCMTKLibs.cmake
+++ CMake/SlicerBlockInstallDCMTKLibs.cmake
@@ -6,12 +6,9 @@ foreach(dcmtk_Lib ${DCMTK_LIBRARIES})
   if(WIN32)
     install(FILES ${DCMTK_DIR}/bin/Release/${dcmtk_Lib}.dll
       DESTINATION ${Slicer_INSTALL_LIB_DIR} COMPONENT Runtime)
-  elseif(APPLE)
-    # needs to install symlink version named libraries as well.
-    install(FILES ${DCMTK_DIR}/lib/lib${dcmtk_Lib}.dylib
-      DESTINATION ${Slicer_INSTALL_LIB_DIR} COMPONENT Runtime)
   elseif(UNIX)
-    install(FILES ${DCMTK_DIR}/lib/lib${dcmtk_Lib}.so
-      DESTINATION ${Slicer_INSTALL_LIB_DIR} COMPONENT Runtime)
+    install(DIRECTORY ${DCMTK_DIR}/lib/
+      DESTINATION ${Slicer_INSTALL_LIB_DIR} COMPONENT Runtime
+      FILES_MATCHING PATTERN lib${dcmtk_Lib}.so*)
   endif()
 endforeach()
