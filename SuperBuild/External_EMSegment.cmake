1d20626971c880328b4186e44c69d8dc26579045
Kilian Pohl <pohl.kilian@gmail.com>
2013-04-29 23:28:39 +0000

    BUG: Skull stripping in test mode did not work
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21955
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21955 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_EMSegment.cmake
+++ SuperBuild/External_EMSegment.cmake
@@ -22,7 +22,7 @@ if(NOT DEFINED EMSegment_SOURCE_DIR)
   #message(STATUS "${__indent}Adding project ${proj}")
   ExternalProject_Add(${proj}
     SVN_REPOSITORY "http://svn.slicer.org/Slicer3/trunk/Modules/EMSegment"
-    SVN_REVISION -r "17025"
+    SVN_REVISION -r "17026"
     "${${PROJECT_NAME}_EP_UPDATE_IF_GREATER_288}"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
