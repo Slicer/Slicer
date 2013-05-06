e8e1254849bfbafde097b0b1033b86b6d24d0efa
Andrey Fedorov <fedorov@bwh.harvard.edu>
2013-05-06 09:16:30 -0400

    ENH: Helper script to explore dicom tags

---
+++ Modules/Scripted/MultiVolumeImporter/Util/list_tags.py
@@ -0,0 +1,47 @@
+import dicom, string, os, sys
+
+dirIn = sys.argv[1]
+
+files = os.listdir(dirIn)
+acqTimes = {}
+trigTimes = {}
+contTimes = {}
+serTimes = {}
+tempPositions = {}
+for f in files:
+  if f.find('.dcm')<=0:
+    continue
+  dcm = dicom.read_file(dirIn+'/'+f)
+  trigTime = dcm.TriggerTime
+  acqTime = dcm.AcquisitionTime
+  contTime = dcm.ContentTime
+  serTime = dcm.SeriesTime
+  tempPosition = dcm.TemporalPositionIdentifier
+
+  try:
+    trigTimes[trigTime]=trigTimes[trigTime]+1
+  except:
+    trigTimes[trigTime]=1
+  try:
+    acqTimes[acqTime]=acqTimes[acqTime]+1
+  except:
+    acqTimes[acqTime]=1
+
+  try:
+    contTimes[contTime]=contTimes[contTime]+1
+  except:
+    contTimes[contTime]=1
+  try:
+    serTimes[serTime]=serTimes[serTime]+1
+  except:
+    serTimes[serTime]=1
+
+  try:
+    tempPositions[tempPosition]=tempPositions[tempPosition]+1
+  except:
+    tempPositions[tempPosition]=1
+print 'Trigger times: ',trigTimes
+print 'Acquisition times: ',acqTimes
+print 'Conent times: ',contTimes
+print 'Series times: ',serTimes
+print 'Temporal positions: ', tempPositions
