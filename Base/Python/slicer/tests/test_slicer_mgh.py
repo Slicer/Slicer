1b652dd3b859acdddf07d272a6d676a910026e23
Hans Johnson <hans-johnson@uiowa.edu>
2013-05-11 17:09:13 +0000

    ENH: Add MGH test suite for reading/writing.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=21980
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@21980 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/Python/slicer/tests/test_slicer_mgh.py
@@ -0,0 +1,35 @@
+import unittest
+import slicer
+import os
+import shutil
+
+class SlicerUtilLoadSaveMGHTests(unittest.TestCase):
+
+  def setUp(self):
+    for MGHFileNames in ['MGH_T1.mgz','MGH_T1_longname.mgh.gz','MGH_T1_uncompressed.mgz']:
+      try:
+        os.remove(os.path.join(slicer.app.temporaryPath, MGHFileNames))
+      except OSError:
+        pass
+    shutil.rmtree(os.path.join(slicer.app.temporaryPath, 'SlicerUtilLoadSaveMGHTests' ), True)
+
+  def test_saveShortCompressedNode(self):
+    node = slicer.util.getNode('T1')
+    filename = os.path.join(slicer.app.temporaryPath, 'MGH_T1.mgz')
+    self.assertTrue(slicer.util.saveNode(node, filename))
+    self.assertTrue(os.path.exists(filename))
+
+  def test_saveUnCompressedNode(self):
+    node = slicer.util.getNode('T1_uncompressed')
+    filename = os.path.join(slicer.app.temporaryPath, 'MGH_T1_uncompressed.mgh')
+    self.assertTrue(slicer.util.saveNode(node, filename))
+    self.assertTrue(os.path.exists(filename))
+
+""" NOT YET SUPPORTED
+  def test_saveLongCompressedNode(self):
+    node = slicer.util.getNode('T1_longname')
+    filename = os.path.join(slicer.app.temporaryPath, 'MGH_T1_longname.mgh.gz')
+    self.assertTrue(slicer.util.saveNode(node, filename))
+    self.assertTrue(os.path.exists(filename))
+"""
+
