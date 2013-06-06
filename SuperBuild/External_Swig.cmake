2eea3787893bdcb8f5690e4ad4dde50634e16a76
Steve Pieper <pieper@bwh.harvard.edu>
2013-06-06 17:12:48 +0000

    COMP: fix swig build to match SimpleITK
    Use the same version and url that are known to work in SimpleITK's
    superbuild.
    
    Note: also remove "${${PROJECT_NAME}_EP_DISABLED_UPDATE}" and replace
    it with UPDATE_COMMAND "" as I believe this gives the same effect and
    avoids confusion about the intent.  More importantly, on windows
    the empty cmake argument led to the addition of a semicolon to the
    directory name (so it became "swigwin-2.0.9;") which was ugly and
    confusing.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22084
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22084 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_Swig.cmake
+++ SuperBuild/External_Swig.cmake
@@ -11,29 +11,39 @@ if(DEFINED Swig_DIR AND NOT EXISTS ${Swig_DIR})
   message(FATAL_ERROR "Swig_DIR variable is defined but corresponds to non-existing directory")
 endif()
 
-set( TARGET_SWIG_VERSION 2.0.9 )
 if(NOT SWIG_DIR)
+
+  set(SWIG_TARGET_VERSION 2.0.9)
+  set(SWIG_DOWNLOAD_SOURCE_HASH "54d534b14a70badc226129159412ea85")
+  set(SWIG_DOWNLOAD_SOURCE_ID "147969")
+  set(SWIG_DOWNLOAD_WIN_HASH "a1dc34766cf599f49e2092f7973c85f4" )
+  set(SWIG_DOWNLOAD_WIN_ID "147968a")
+
   if(WIN32)
     # swig.exe available as pre-built binary on Windows:
     ExternalProject_Add(Swig
-      URL http://prdownloads.sourceforge.net/swig/swigwin-${TARGET_SWIG_VERSION}.zip
-      URL_MD5 a1dc34766cf599f49e2092f7973c85f4
-      SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/swigwin-${TARGET_SWIG_VERSION}
-      "${${PROJECT_NAME}_EP_DISABLED_UPDATE}"
+      URL  http://midas3.kitware.com/midas/api/rest?method=midas.bitstream.download&id=${SWIG_DOWNLOAD_WIN_ID}&name=swigwin-${SWIG_TARGET_VERSION}.tar.gz
+      URL_MD5 ${SWIG_DOWNLOAD_WIN_HASH}
+      SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/swigwin-${SWIG_TARGET_VERSION}"
       CONFIGURE_COMMAND ""
       BUILD_COMMAND ""
       INSTALL_COMMAND ""
+      UPDATE_COMMAND ""
       )
 
-    set(SWIG_DIR ${CMAKE_CURRENT_BINARY_DIR}/swigwin-${TARGET_SWIG_VERSION}) # ??
-    set(SWIG_EXECUTABLE ${CMAKE_CURRENT_BINARY_DIR}/swigwin-${TARGET_SWIG_VERSION}/swig.exe)
+    set(SWIG_DIR "${CMAKE_CURRENT_BINARY_DIR}/swigwin-${SWIG_TARGET_VERSION}") # ??
+    set(SWIG_EXECUTABLE "${CMAKE_CURRENT_BINARY_DIR}/swigwin-${SWIG_TARGET_VERSION}"/swig.exe)
     set(Swig_DEPEND Swig)
+
   else()
+    # not windows
+
     # Set dependency list
     set(Swig_DEPENDENCIES "PCRE")
 
     # Include dependent projects if any
     SlicerMacroCheckExternalProjectDependency(Swig)
+
     #
     # SWIG
     #
@@ -55,13 +65,13 @@ if(NOT SWIG_DIR)
     set ( swig_CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/swig_configure_step.cmake )
 
     ExternalProject_add(Swig
-      URL http://prdownloads.sourceforge.net/swig/swig-${TARGET_SWIG_VERSION}.tar.gz
-      URL_MD5  54d534b14a70badc226129159412ea85
+      URL  http://midas3.kitware.com/midas/api/rest?method=midas.bitstream.download&id=${SWIG_DOWNLOAD_SOURCE_ID}&name=swig-${SWIG_TARGET_VERSION}.tar.gz
+      URL_MD5 ${SWIG_DOWNLOAD_SOURCE_HASH}
       CONFIGURE_COMMAND ${swig_CONFIGURE_COMMAND}
-      DEPENDS PCRE
+      DEPENDS "${Swig_DEPENDENCIES}"
       )
 
-    set(SWIG_DIR ${swig_install_dir}/share/swig/${TARGET_SWIG_VERSION})
+    set(SWIG_DIR "${swig_install_dir}/share/swig/${SWIG_TARGET_VERSION}")
     set(SWIG_EXECUTABLE ${swig_install_dir}/bin/swig)
     set(Swig_DEPEND Swig)
   endif()
