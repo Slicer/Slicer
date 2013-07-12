0d3067e9af1796df8ee77ebcba4a9b4fa37de0f0
Junichi Tokuda <tokuda@bwh.harvard.edu>
2013-07-12 20:06:13 +0000

    BUG: Update OpenIGTLinkIF GIT_TAG to fix issues #3224 and #3225
    Issue 3224: std::list increment assertion failure on Windows
    Issue 3225: vtkMRMLIGTLConnectorNode: bug in unregistering incoming MRML node
    
    See https://github.com/openigtlink/OpenIGTLinkIF/compare/f9b65b1ffd2992862025f3c6ec5fe30e6f5dd395...533af932a1c899a0872c35b41113075f7da1ac1b
    
    Co-authored-by: Isaiah Norton <inorton@bwh.harvard.edu>
    Co-authored-by: Adam Rankin <rankin@cs.queensu.ca>
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22172
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22172 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_OpenIGTLinkIF.cmake
+++ SuperBuild/External_OpenIGTLinkIF.cmake
@@ -22,7 +22,7 @@ if(NOT DEFINED OpenIGTLinkIF_SOURCE_DIR)
   #message(STATUS "${__indent}Adding project ${proj}")
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/openigtlink/OpenIGTLinkIF.git"
-    GIT_TAG "f9b65b1ffd2992862025f3c6ec5fe30e6f5dd395"
+    GIT_TAG "533af932a1c899a0872c35b41113075f7da1ac1b"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
