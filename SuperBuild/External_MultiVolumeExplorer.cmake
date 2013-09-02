bc71b252973c582a62b2a02116c92a87f86e5374
Fedorov <fedorov@bwh.harvard.edu>
2013-09-02 00:17:21 +0000

    ENH: update MultiVolumeExplorer module
    * BUG: fix 3058 - support relative change plotting
    * BUG: fix static plotting
    * STYLE: clean up the code a bit
    
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22380
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22380 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_MultiVolumeExplorer.cmake
+++ SuperBuild/External_MultiVolumeExplorer.cmake
@@ -27,7 +27,7 @@ if(NOT DEFINED MultiVolumeExplorer_SOURCE_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/fedorov/MultiVolumeExplorer.git"
-    GIT_TAG "b22dadfc1db0c5f5a9c927cab45f691c1da8efe1"
+    GIT_TAG "7db418aa139722b919416875aecd4c967a2442dd"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
