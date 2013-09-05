668044843a32afb4fe330a5d82eef6114eb0e040
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-09-05 02:47:40 +0000

    COMP: Ensure teem is properly link against png if CMake > 2.8.11.2 is used
    See inlined comment for more details.
    
    Associated discussion: http://massmail.spl.harvard.edu/public-archives/slicer-devel/2013/thread.html#13536
    
    Fixes #3369
    
    Co-authored-by: Steve Pieper <pieper@bwh.harvard.edu>
    Co-authored-by: James Cook <james.j.cook@duke.edu>
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22411
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22411 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_teem.cmake
+++ SuperBuild/External_teem.cmake
@@ -48,6 +48,20 @@ else()
   set(teem_PNG_LIBRARY ${teem_PNG_LIBRARY_DIR}/libvtkpng.so)
 endif()
 
+if(${CMAKE_VERSION} VERSION_GREATER "2.8.11.2")
+  # Following CMake commit 2a7975398, the FindPNG.cmake module
+  # supports detection of release and debug libraries. Specifying only
+  # the release variable is enough to ensure the variable PNG_LIBRARY
+  # is internally set if the project is built either in Debug or Release.
+  list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
+    -DPNG_LIBRARY_RELEASE:FILEPATH=${teem_PNG_LIBRARY}
+    )
+else()
+  list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
+    -DPNG_LIBRARY:FILEPATH=${teem_PNG_LIBRARY}
+    )
+endif()
+
 set(teem_URL http://svn.slicer.org/Slicer3-lib-mirrors/trunk/teem-1.10.0-src.tar.gz)
 set(teem_MD5 efe219575adc89f6470994154d86c05b)
 
@@ -78,7 +92,6 @@ ExternalProject_Add(${proj}
     -DTeem_VTK_MANGLE:BOOL=OFF ## NOT NEEDED FOR EXTERNAL ZLIB outside of vtk
     -DPNG_PNG_INCLUDE_DIR:PATH=${VTK_SOURCE_DIR}/Utilities/vtkpng
     -DTeem_PNG_DLLCONF_IPATH:PATH=${VTK_DIR}/Utilities
-    -DPNG_LIBRARY:FILEPATH=${teem_PNG_LIBRARY}
     ${EXTERNAL_PROJECT_OPTIONAL_ARGS}
   INSTALL_COMMAND ""
   DEPENDS
