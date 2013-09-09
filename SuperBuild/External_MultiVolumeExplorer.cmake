8c5179bbf97128fdb94332a1908fc3b07a692310
Fedorov <fedorov@bwh.harvard.edu>
2013-09-09 00:58:28 +0000

    ENH: adding the capability for interactive plotting of the multivolume selected in Fg; this feature is needed for debugging PkModeling and similar extensions that implement multivolume fitting
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22415
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22415 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_MultiVolumeExplorer.cmake
+++ SuperBuild/External_MultiVolumeExplorer.cmake
@@ -27,7 +27,7 @@ if(NOT DEFINED MultiVolumeExplorer_SOURCE_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/fedorov/MultiVolumeExplorer.git"
-    GIT_TAG "a379403580ecbae68f8b82e792fd0fbef605889d"
+    GIT_TAG "48f17d79b2de0f19af45b6a42cc28819b0e685e8"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
