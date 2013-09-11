042ed29efbecebfdda33c2d216142c173981b517
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-09-11 03:20:18 +0000

    BUG: Fix memory mgt in PythonQt, ENH: Config of dicom log level from python
    // ---------------------------------------
    
    CTK Commits:
    
    Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    Date:   Tue Sep 10 04:27:56 2013 -0400
    
        Re-factor ctkDICOMUtil to be consistent with ctkUtil
    
        Add the methods dicomLogLevel and setDICOMLogLevel to "ctk" namespace
    
        Update PythonQt external project to include fix allowing to wrap
        QFlags.
    
        The DICOM log level can be set from python doing:
    
         >>> import ctk
         >>> ctk.ctk.dicomLogLevel()
         16
         >>> ctk.ctk.dicomLogLevelAsString()
         u'Info'
         >>> ctk.ctk.setDICOMLogLevel(ctk.ctkErrorLogLevel.Critical)
         >>> ctk.ctk.dicomLogLevel()
         256
         >>> ctk.ctkErrorLogLevel.logLevelAsString(ctk.ctk.dicomLogLevel())
         u'Critical'
         >>> ctk.ctkErrorLogLevel.logLevelAsString(32)
         u'Warning'
         >>> ctk.ctk.setDICOMLogLevel(64)
         >>> ctk.ctk.dicomLogLevelAsString()
         u'Error'
    
    commit 637da4b68ed93741a4c8cf73b406d66986dbe864
    Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    Date:   Tue Sep 10 04:12:10 2013 -0400
    
        Define ctkErrorLogLevel::logLevelAsString as static for easier re-use
    
        Explicitly add associated decorator. See code comment for explanation.
    
    commit 8542108ef7d15a39d4d302aa64f4ecc2d15bf3e5
    Author: Isaiah Norton <inorton@bwh.harvard.edu>
    Date:   Wed Aug 28 11:20:31 2013 -0400
    
        Add ctkDICOMUtil and ::setDICOMLogLevel helper
    
        This function allows to set the DICOM log level for
        diagnostic purposes.
    
    // ---------------------------------------
    
    PythonQt commits:
    
    
    commit e1f1c77d9675c3c5fb1cba19d2a32ace483eda2c
    Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    Date:   Tue Sep 10 02:49:23 2013 -0400
    
        Revert part of r91 preventing QFlags from being wrapped
    
        By checking if an enum member has already been cached it is not required
        anymore to skip it if is a QFlags.
    
        Additionally, the wrapping of QFlags can now be done by using
        only Q_FLAGS without having a corresponding Q_ENUMS.
    
    commit 5d5600c4d3134e69b51442a58b58e14c7fd47e54
    Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    Date:   Tue Sep 10 02:36:26 2013 -0400
    
        Add CMake testing support
    
        Disabled by default to keep behavior consistent with previous
        CMake'ified version.
    
    
    [...]
    
    commit 7af5c8a4d2194db388ac67f340e3f555e02fea78
    Author: florianlink <florianlink@ea8d5007-eb21-0410-b261-ccb3ea6e24a9>
    Date:   Wed Jul 3 14:54:20 2013 +0000
    
        improved wrapper check to avoid crashes on virtual methods while python object gets deleted
    
        git-svn-id: http://svn.code.sf.net/p/pythonqt/code/trunk@245 ea8d5007-eb21-0410-b261-ccb3ea6e24a9
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22425
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22425 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -71,7 +71,7 @@ if(NOT DEFINED CTK_DIR)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "76219ba324ce1a976256fb7c7da5705d7e5f214b"
+    GIT_TAG "71c234bb8febd32f3b00b068061e93105303ebc0"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
