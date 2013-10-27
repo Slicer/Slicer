Slicer/Slicer

commit c70b036d4ffa295bd59c1056e9142665d8de09d2

ENH: Improve readability using vtkNew where possible
In addition to be shorter, it simply conveys the idea of locally
instantiating an object.

Indeed, vtkNew allow to instantiate an object on the heap and deleting it
when it goes out of scope if its reference count has been incremented by
either assigning the object to a smart pointer or calling Register.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22539
git-svn-id: http://svn.slicer.org/Slicer4/trunk@22539 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Annotations/MRMLDM/vtkMRMLAnnotationROIDisplayableManager.cxx
@@ -36,7 +36,6 @@
 #include <vtkPolyDataMapper2D.h>
 #include <vtkProperty.h>
 #include <vtkRenderer.h>
-#include <vtkSmartPointer.h>
 #include <vtkTransform.h>
 #include <vtkTransformPolyDataFilter.h>
 
@@ -340,18 +339,18 @@ void vtkMRMLAnnotationROIDisplayableManager::PropagateMRMLToWidget(vtkMRMLAnnota
 
 
   // handle ROI transform to world space
-  vtkSmartPointer<vtkMatrix4x4> transformToWorld = vtkSmartPointer<vtkMatrix4x4>::New();
+  vtkNew<vtkMatrix4x4> transformToWorld;
   transformToWorld->Identity();
 
   vtkMRMLTransformNode* tnode = roiNode->GetParentTransformNode();
   if (tnode != NULL && tnode->IsLinear())
     {
     vtkMRMLLinearTransformNode *lnode = vtkMRMLLinearTransformNode::SafeDownCast(tnode);
-    lnode->GetMatrixTransformToWorld(transformToWorld);
+    lnode->GetMatrixTransformToWorld(transformToWorld.GetPointer());
     }
   transformToWorld->Invert();
 
-  rep->SetWorldToLocalMatrix(transformToWorld);
+  rep->SetWorldToLocalMatrix(transformToWorld.GetPointer());
 
   // update widget from mrml
   double xyz[3];
@@ -475,29 +474,29 @@ void vtkMRMLAnnotationROIDisplayableManager::PropagateMRMLToWidget2D(vtkMRMLAnno
   //this->SetParentTransformToWidget(roiNode, roiWidget);
 
   // handle ROI transform to world space
-  vtkSmartPointer<vtkMatrix4x4> transformToWorld = vtkSmartPointer<vtkMatrix4x4>::New();
+  vtkNew<vtkMatrix4x4> transformToWorld;
   transformToWorld->Identity();
 
   vtkMRMLTransformNode* tnode = roiNode->GetParentTransformNode();
   if (tnode != NULL && tnode->IsLinear())
     {
     vtkMRMLLinearTransformNode *lnode = vtkMRMLLinearTransformNode::SafeDownCast(tnode);
-    lnode->GetMatrixTransformToWorld(transformToWorld);
+    lnode->GetMatrixTransformToWorld(transformToWorld.GetPointer());
     }
 
   // update the transform from world to screen space
   // for the extracted cut plane
-  vtkSmartPointer<vtkMatrix4x4> rasToXY = vtkSmartPointer<vtkMatrix4x4>::New();
+  vtkNew<vtkMatrix4x4> rasToXY;
   rasToXY->DeepCopy(sliceNode->GetXYToRAS());
   rasToXY->Invert();
 
-  vtkSmartPointer<vtkMatrix4x4> XYToWorld = vtkSmartPointer<vtkMatrix4x4>::New();
+  vtkNew<vtkMatrix4x4> XYToWorld;
   XYToWorld->Identity();
-  XYToWorld->Multiply4x4(rasToXY, transformToWorld, XYToWorld);
+  XYToWorld->Multiply4x4(rasToXY.GetPointer(), transformToWorld.GetPointer(), XYToWorld.GetPointer());
 
-  vtkSmartPointer<vtkTransform> transform = rep->GetIntersectionPlaneTransform();
+  vtkNew<vtkTransform> transform;
 
-  transform->SetMatrix(XYToWorld);
+  transform->SetMatrix(XYToWorld.GetPointer());
 
   //
   // update the plane equation for the current slice cutting plane
@@ -506,10 +505,10 @@ void vtkMRMLAnnotationROIDisplayableManager::PropagateMRMLToWidget2D(vtkMRMLAnno
   transformToWorld->Invert();
   rasToXY->DeepCopy(sliceNode->GetXYToRAS());
 
-  vtkSmartPointer<vtkMatrix4x4> mat = vtkSmartPointer<vtkMatrix4x4>::New();
+  vtkNew<vtkMatrix4x4> mat;
   mat->Identity();
-  mat->Multiply4x4(transformToWorld, rasToXY, mat);
-  rasToXY->DeepCopy(mat);
+  mat->Multiply4x4(transformToWorld.GetPointer(), rasToXY.GetPointer(), mat.GetPointer());
+  rasToXY->DeepCopy(mat.GetPointer());
 
   double normal[4]={0,0,0,1};
   double origin[4]={0,0,0,1};
@@ -732,7 +731,7 @@ void vtkMRMLAnnotationROIDisplayableManager::SetParentTransformToWidget(vtkMRMLA
 
   vtkAnnotationROIRepresentation *rep = vtkAnnotationROIRepresentation::SafeDownCast(widget->GetRepresentation());
 
-  vtkSmartPointer<vtkMatrix4x4> transformToWorld = vtkSmartPointer<vtkMatrix4x4>::New();
+  vtkNew<vtkMatrix4x4> transformToWorld;
   transformToWorld->Identity();
 
   // get the nodes's transform node
@@ -740,7 +739,7 @@ void vtkMRMLAnnotationROIDisplayableManager::SetParentTransformToWidget(vtkMRMLA
   if (rep != NULL && tnode != NULL && tnode->IsLinear())
     {
     vtkMRMLLinearTransformNode *lnode = vtkMRMLLinearTransformNode::SafeDownCast(tnode);
-    lnode->GetMatrixTransformToWorld(transformToWorld);
+    lnode->GetMatrixTransformToWorld(transformToWorld.GetPointer());
     }
 
   vtkNew<vtkPropCollection> actors;
@@ -749,7 +748,7 @@ void vtkMRMLAnnotationROIDisplayableManager::SetParentTransformToWidget(vtkMRMLA
   for (int i=0; i<actors->GetNumberOfItems(); i++)
     {
     vtkActor *actor = vtkActor::SafeDownCast(actors->GetItemAsObject(i));
-    actor->SetUserMatrix(transformToWorld);
+    actor->SetUserMatrix(transformToWorld.GetPointer());
     }
   //rep->SetTransform(xform);
 

commit 20dd1ed6a95bf1ee4f587fedd12081817d88bc41

BUG: Ensure ROI worked as expected. Fixes #3459
Fix bug introduced by r22539 when updating code to use vtkNew where
possible.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22642
git-svn-id: http://svn.slicer.org/Slicer4/trunk@22642 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Annotations/MRMLDM/vtkMRMLAnnotationROIDisplayableManager.cxx
@@ -494,7 +494,7 @@ void vtkMRMLAnnotationROIDisplayableManager::PropagateMRMLToWidget2D(vtkMRMLAnno
   XYToWorld->Identity();
   XYToWorld->Multiply4x4(rasToXY.GetPointer(), transformToWorld.GetPointer(), XYToWorld.GetPointer());
 
-  vtkNew<vtkTransform> transform;
+  vtkSmartPointer<vtkTransform> transform = rep->GetIntersectionPlaneTransform();
 
   transform->SetMatrix(XYToWorld.GetPointer());
 