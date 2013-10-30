Slicer/Slicer

commit cc09c38c087425dab8a402662fcf78576b528921

BUG: Attempt to fix font issue related to webkit performance on MacOSX 10.9
Associated message in the system log:
CoreText performance note: Client called CTFontCreateWithName() using name "Lucida Grande" and got font with PostScript name "LucidaGrande". For best performance, only use PostScript names when calling this API.

See https://bugreports.qt-project.org/browse/QTBUG-32789 and
http://successfulsoftware.net/2013/10/23/fixing-qt-4-for-mac-os-x-10-9-mavericks/

Fixes #3460

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22641
git-svn-id: http://svn.slicer.org/Slicer4/trunk@22641 3bd1e089-480b-0410-8dfb-8563597acbee

Applications/SlicerApp/Main.cxx
@@ -24,6 +24,7 @@
 #include <QSplashScreen>
 #include <QString>
 #include <QStyleFactory>
+#include <QSysInfo>
 #include <QTimer>
 
 // Slicer includes
@@ -96,6 +97,15 @@ int SlicerAppMain(int argc, char* argv[])
 {
 #if ITK_VERSION_MAJOR > 3
   itk::itkFactoryRegistration(); 
+#endif
+
+#ifdef Q_OS_MACX
+  if (QSysInfo::MacintoshVersion > QSysInfo::MV_10_8)
+    {
+    // Fix Mac OS X 10.9 (mavericks) font issue
+    // https://bugreports.qt-project.org/browse/QTBUG-32789
+    QFont::insertSubstitution(".Lucida Grande UI", "Lucida Grande");
+    }
 #endif
   
   QCoreApplication::setApplicationName("Slicer");

commit 249a4e8613a40e6b606ddfebd83defcecd6cb35a

COMP: Fix MacOSX build issue when building against Qt 4.7.4. Fixes #3471
Since "QSysInfo::MV_10_8" has been first introduced in Qt 4.8.3,
exclude the fix for issue #3460 introduced in commit r22641.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22657
git-svn-id: http://svn.slicer.org/Slicer4/trunk@22657 3bd1e089-480b-0410-8dfb-8563597acbee

Applications/SlicerApp/Main.cxx
@@ -99,6 +99,7 @@ int SlicerAppMain(int argc, char* argv[])
   itk::itkFactoryRegistration(); 
 #endif
 
+#if QT_VERSION >= 0x040803
 #ifdef Q_OS_MACX
   if (QSysInfo::MacintoshVersion > QSysInfo::MV_10_8)
     {
@@ -106,6 +107,7 @@ int SlicerAppMain(int argc, char* argv[])
     // https://bugreports.qt-project.org/browse/QTBUG-32789
     QFont::insertSubstitution(".Lucida Grande UI", "Lucida Grande");
     }
+#endif
 #endif
   
   QCoreApplication::setApplicationName("Slicer");