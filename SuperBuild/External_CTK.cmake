4fd50a38b167af3c1ce2cb394f48deae3c483e73
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-12-02 21:52:50 +0000

    COMP: Update CTK external project
    $ git shortlog 46a1d6605..36fb628b1
    Andreas Fetzer (11):
          Adding possibility to dynamically change the layout of the dicomTableManager
          ctkDicomBrowser uses dynamic table layout now
          Children of the QSplitter of the tablemanager are no longer collapsible
          Fixed missing update if new data is added
          Fixed bug where filter textfield's background remained black
          m_dynamic layout is now in pimpl and has read accessor. Some code cleaning
          Improved behaviour of dynamic layout: between 800 and 1024px window size use     vertical layout. If the window is bigger than 1024 switch to horizontal layout
          Renaming method for setting dicom database
          Not using const references when declaring a connection
          Setting dynamic table layout in ui file
          Proper naming of property with const read method
    
    Jean-Christophe Fillion-Robin (3):
          Rename macro "ctk_include_once" into "superbuild_include_once"
          Ensure 'superbuild_include_once' works independently of the caller scope.
          Merge branch '418-support-use-system-option'
    
    Julien Finet (1):
          Merge pull request #422 from AndreasFetzer/allow-dynamic-table-layout-for-dicombrowser-final
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22752
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22752 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -72,7 +72,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "46a1d66055f09ac894fbdacf52e36432deda997c"
+    GIT_TAG "36fb628b14e9eba019ac877c14903dbe09ec903c"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
