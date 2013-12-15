43b1956b677a3aae3c45f957d69d2f062ebc76fe
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-12-15 23:09:45 +0000

    COMP: Update CTK - Unconditionally rebuild "CTK-build" inner project.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22775
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22775 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "a91b2b21a97c0fab6c1dbd3f36aa428027649ffe"
+    GIT_TAG "00be8fae583173f338893659b9d8296ce9d5765b"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
