5b0f1a04a7bfa2be4ad86c4db9c78e348350735f
Steve Pieper <pieper@isomics.com>
2014-01-12 20:24:40 +0000

    COMP: expose the displable manager and other MRML libs in python
    The MRMLDisplayableManager code is needed to access the picker
    from a python module.  This feature was added to support
    DTI/Tractography processing tools being written by Lauren
    O'Donnel.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22816
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22816 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Python/mrml.py
+++ Base/Python/mrml.py
@@ -1,4 +1,7 @@
 """ This module loads all the classes from the MRML library into its
 namespace."""
 
+from MRMLCLIPython import *
 from MRMLCorePython import *
+from MRMLDisplayableManagerPython import *
+from MRMLLogicPython import *
