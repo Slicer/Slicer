Slicer/Slicer

commit 5b52b9e77daa7b129cb282beb7450fd4e2671665

ENH: slicer.util - Add convenience method "modulePath(moduleName)"
svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22851
git-svn-id: http://svn.slicer.org/Slicer4/trunk@22851 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Python/slicer/util.py
@@ -320,6 +320,9 @@ def getNewModuleGui(module):
     print("Could not find module widget representation with name '%s" % module.name, file=sys.stderr)
   return widgetRepr
 
+def modulePath(moduleName):
+  return eval('slicer.modules.%s.path' % moduleName.lower())
+
 def reloadScriptedModule(moduleName):
   """Generic reload method for any scripted module.
   """
@@ -329,7 +332,7 @@ def reloadScriptedModule(moduleName):
   widgetName = moduleName + "Widget"
 
   # reload the source code
-  filePath = eval('slicer.modules.%s.path' % moduleName.lower())
+  filePath = modulePath(moduleName)
   p = os.path.dirname(filePath)
   if not p in sys.path:
     sys.path.insert(0,p)

Applications/SlicerApp/Testing/Python/ScenePerformance.py
@@ -46,9 +46,8 @@ class ScenePerformanceWidget:
   def setup(self):
 
     loader = qt.QUiLoader()
-    moduleName = 'ScenePerformance' #scriptedModulesPath = '/Users/exxos/Work/Slicer/Slicer4/Slicer-Debug/Slicer-build/lib/Slicer-4.3/qt-scripted-modules'
-    scriptedModulesPath = eval('slicer.modules.%s.path' % moduleName.lower())
-    scriptedModulesPath = os.path.dirname(scriptedModulesPath)
+    moduleName = 'ScenePerformance'
+    scriptedModulesPath = os.path.dirname(slicer.util.modulePath(moduleName))
     path = os.path.join(scriptedModulesPath, 'Resources', 'UI', 'ScenePerformance.ui')
 
     qfile = qt.QFile(path)

commit 2f230d8825b5a0a4719478af87f2b01f7b9ba9de

BUG: Fix convenience method slicer.util.modulePath introduced in r22851
Since "eval" is executed in the context where it is called, the 'slicer'
module should be imported.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22853
git-svn-id: http://svn.slicer.org/Slicer4/trunk@22853 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Python/slicer/util.py
@@ -321,6 +321,7 @@ def getNewModuleGui(module):
   return widgetRepr
 
 def modulePath(moduleName):
+  import slicer
   return eval('slicer.modules.%s.path' % moduleName.lower())
 
 def reloadScriptedModule(moduleName):