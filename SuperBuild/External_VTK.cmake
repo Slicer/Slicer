e4ca294159527245294c64634c69eac5d3fdb80b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-02-12 23:04:30 +0000

    ENH: Update VTK - Add support for additional observer CallDataType
    //--------------
    vtkPythonCommand - Add support for additional call data types
    
    This commit extends the mechanism added in 9366ca67 to support additional
    data types.
    
    Supported types now includes 'string0', 'vtkobject', 'int', 'long',
    'double' and 'float'.
    
    Also added convenience decorator 'set_call_data_type'
    
    See inlined comments for more details.
    
    The integration in VTK proper has been proposed here:
    http://review.source.kitware.com/#/t/3966/
    //--------------
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22871
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22871 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -102,7 +102,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT ${CMAKE_PROJECT_N
   endif()
 
   set(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY "github.com/Slicer/VTK.git" CACHE STRING "Repository from which to get VTK" FORCE)
-  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG "f387593148a8bc49cf46c1e27537674d5b779f49" CACHE STRING "VTK git tag to use" FORCE)
+  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG "e687deb642ca3097c5b086edbf04eb662767f0c1" CACHE STRING "VTK git tag to use" FORCE)
 
   mark_as_advanced(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG)
 
