48e9172dd4d91cca2ab7708915a52eb23ec92ca7
Matthew Woehlke <matthew.woehlke@kitware.com>
2014-03-12 18:51:00 +0000

    STYLE: Document SlicerWizard/ExtensionProject.py
    Add documentation for the public API of SlicerWizard.ExtensionProject.
    Also, fix some code issues revealed in the course of writing the same:
    
    - Merge superfluous private helper method into public method
    - Change getValue() to return the value from the last 'set()'
    - Allow use in 'with'
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22983
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22983 3bd1e089-480b-0410-8dfb-8563597acbee

--- Utilities/Scripts/SlicerWizard/ExtensionProject.py
+++ Utilities/Scripts/SlicerWizard/ExtensionProject.py
@@ -15,10 +15,29 @@ def _trimIndent(indent):
 
 #=============================================================================
 class ExtensionProject(object):
+  """Convenience class for manipulating an extension project.
+
+  This class provides an additional layer of convenience for users that wish to
+  manipulate the CMakeLists.txt of an extension project. The term "build
+  script" is used throughout to refer to the CMakeLists.txt so encapsulated.
+
+  Modifications to the script are made to the in-memory, parsed representation.
+  Use :meth:`.save` to write changes back to disk.
+
+  This class may be used as a context manager. When used in this manner, any
+  changes made are automatically written back to the project's CMakeLists.txt
+  when the context goes out of scope.
+  """
+
   _moduleInsertPlaceholder = "# NEXT_MODULE"
 
   #---------------------------------------------------------------------------
   def __init__(self, path):
+    """
+    :param path: Top level directory of the extension project.
+    :type path: :class:`basestring`
+    """
+
     cmakeFile = os.path.join(path, "CMakeLists.txt")
     if not os.path.exists(cmakeFile):
       raise IOError("CMakeLists.txt not found")
@@ -29,8 +48,24 @@ class ExtensionProject(object):
       self._scriptContents = CMakeParser.CMakeScript(fp.read())
 
   #---------------------------------------------------------------------------
+  def __enter__(self):
+    return self
+
+  #---------------------------------------------------------------------------
+  def __exit__(self, exc_type, exc_value, traceback):
+    self.save()
+
+  #---------------------------------------------------------------------------
   @property
   def project(self):
+    """Name of extension project.
+
+    :type: :class:`str`
+
+    This provides the name of the extension project, i.e. the identifier passed
+    to ``project()`` in the extension's build script.
+    """
+
     for t in self._scriptContents.tokens:
       if _isCommand(t, "project") and len(t.arguments):
         return t.arguments[0].text
@@ -39,7 +74,31 @@ class ExtensionProject(object):
 
   #---------------------------------------------------------------------------
   def getValue(self, name, default=None):
-    for t in self._scriptContents.tokens:
+    """Get value of CMake variable set in project.
+
+    :param name: Name of the variable.
+    :type name: :class:`basestring`
+    :param default: Value to return if no such variable exists.
+
+    :returns: Value of the variable, or ``default`` if not set.
+    :rtype: :class:`str` or ``type(default)``
+
+    :raises:
+      :exc:`~exceptions.KeyError` if no such ``name`` is set and ``default`` is
+      ``None``.
+
+    This returns the raw value of the variable ``name`` which is set in the
+    build script. No substitution is performed (the result may contain
+    substitution placeholders like '``${var}``'). If more than one ``set()``
+    command sets the same ``name``, the result is the raw argument to the last
+    such command. If the value consists of more than one argument, only the
+    first is returned.
+
+    If no ``set()`` command sets ``name``, and ``default`` is not ``None``,
+    ``default`` is returned. Otherwise a :exc:`~exceptions.KeyError` is raised.
+    """
+
+    for t in reversed(self._scriptContents.tokens):
       if _isCommand(t, "set") and len(t.arguments) and \
          t.arguments[0].text == name:
         if len(t.arguments) < 2:
@@ -54,6 +113,24 @@ class ExtensionProject(object):
 
   #---------------------------------------------------------------------------
   def setValue(self, name, value):
+    """Change value of CMake variable set in project.
+
+    :param name: Name of the variable.
+    :type name: :class:`basestring`
+    :param value: Value to assign to the variable.
+    :type value: :class:`basestring`
+
+    :raises: :exc:`~exceptions.KeyError` if no such ``name`` is set.
+
+    This modifies the build script to set the variable ``name`` to ``value``.
+    If more than one ``set()`` command sets the same ``name``, only the first
+    is modified. If the value of the modified ``set()`` command has more than
+    one argument, only the first is modified.
+
+    The build script must already contain a ``set()`` command which sets
+    ``name``. If it does not, a :exc:`~exceptions.KeyError` is raised.
+    """
+
     for t in self._scriptContents.tokens:
       if _isCommand(t, "set") and len(t.arguments) and \
          t.arguments[0].text == name:
@@ -72,7 +149,20 @@ class ExtensionProject(object):
     raise KeyError("script does not set %r" % name)
 
   #---------------------------------------------------------------------------
-  def _addModuleToScript(self, name):
+  def addModule(self, name):
+    """Add a module to the build rules of the project.
+
+    :param name: Name of the module to be added.
+    :type name: :class:`basestring`
+
+    :raises: :exc:`~exceptions.EOFError` if no insertion point can be found.
+
+    This adds an ``add_subdirectory()`` call for ``name`` to the build script.
+    If possible, the new call is inserted immediately before a placeholder
+    comment which is designated for this purpose. Otherwise, the new call is
+    inserted after the last existing call to ``add_subdirectory()``.
+    """
+
     indent = ""
     after = -1
 
@@ -99,11 +189,29 @@ class ExtensionProject(object):
     self._scriptContents.tokens.insert(after, t)
 
   #---------------------------------------------------------------------------
-  def addModule(self, name):
-    self._addModuleToScript(name)
-
-  #---------------------------------------------------------------------------
   def save(self, destination=None):
+    """Save the project.
+
+    :param destination: Location to which to write the build script.
+    :type destination: :class:`basestring` or ``None``
+
+    This saves the extension project CMake script to the specified file:
+
+    .. code-block:: python
+
+      # Open a project
+      p = ExtensionProject('.')
+
+      # Set a value in the project
+      p.setValue('EXTENSION_DESCRIPTION', 'This is an awesome extension!')
+
+      # Save the changes
+      p.save()
+
+    If ``destination`` is ``None``, the CMakeLists.txt file from which the
+    project instance was created is overwritten.
+    """
+
     if destination is None:
       destination = self._scriptPath
 
