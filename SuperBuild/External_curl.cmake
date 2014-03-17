Slicer/Slicer

commit 93d72a5f05d0e4dfb2b56e24b09f837d48e57abc

COMP: Simplify external project includes & passing of CMake vars to project
A new module named "ExternalProjectDependency" has been introduced, it is
equivalent to the Artichoke module at revision commontk/Artichoke@70ed903.

This new module is now used within Slicer to support its build system. To
maintain backward compatibility with existing extensions, the modules
"SlicerMacroEmptyExternalProject" and "SlicerMacroCheckExternalProjectDependency"
are kept around.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22773
git-svn-id: http://svn.slicer.org/Slicer4/trunk@22773 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_curl.cmake
@@ -1,19 +1,18 @@
 
-superbuild_include_once()
+set(proj curl)
 
 # Set dependency list
-set(curl_DEPENDENCIES zlib)
+set(${proj}_DEPENDENCIES zlib)
 if(CURL_ENABLE_SSL)
-  if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_curl)
-    list(APPEND curl_DEPENDENCIES OpenSSL)
+  if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
+    list(APPEND ${proj}_DEPENDENCIES OpenSSL)
   else()
     # XXX - Add a test checking if system curl support OpenSSL
   endif()
 endif()
 
 # Include dependent projects if any
-SlicerMacroCheckExternalProjectDependency(curl)
-set(proj curl)
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   unset(CURL_INCLUDE_DIR CACHE)
@@ -24,43 +23,25 @@ endif()
 if((NOT DEFINED CURL_INCLUDE_DIRS
    OR NOT DEFINED CURL_LIBRARIES) AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
-  set(EXTERNAL_PROJECT_OPTIONAL_ARGS)
-
-  # Set CMake OSX variable to pass down the external project
-  if(APPLE)
-    list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
-      -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
-      -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
-      -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
-  endif()
-
-  if(NOT CMAKE_CONFIGURATION_TYPES)
-    list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
-      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE})
-  endif()
+  set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_ARGS CMAKE_ARGS)
 
   if(CURL_ENABLE_SSL)
-    list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
-      -DCMAKE_USE_OPENSSL:BOOL=ON
+    list(APPEND EXTERNAL_PROJECT_OPTIONAL_CMAKE_ARGS
       -DOPENSSL_INCLUDE_DIR:PATH=${OPENSSL_INCLUDE_DIR}
       )
     if(UNIX)
-      list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
+      list(APPEND EXTERNAL_PROJECT_OPTIONAL_CMAKE_ARGS
         -DOPENSSL_SSL_LIBRARY:STRING=${OPENSSL_SSL_LIBRARY}
         -DOPENSSL_CRYPTO_LIBRARY:STRING=${OPENSSL_CRYPTO_LIBRARY}
         )
     elseif(WIN32)
-      list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
+      list(APPEND EXTERNAL_PROJECT_OPTIONAL_CMAKE_ARGS
         -DLIB_EAY_DEBUG:FILEPATH=${LIB_EAY_DEBUG}
         -DLIB_EAY_RELEASE:FILEPATH=${LIB_EAY_RELEASE}
         -DSSL_EAY_DEBUG:FILEPATH=${SSL_EAY_DEBUG}
         -DSSL_EAY_RELEASE:FILEPATH=${SSL_EAY_RELEASE}
         )
     endif()
-  else()
-    list(APPEND EXTERNAL_PROJECT_OPTIONAL_ARGS
-      -DCMAKE_USE_OPENSSL:BOOL=OFF
-      )
   endif()
 
   set(${proj}_CMAKE_C_FLAGS ${ep_common_c_flags})
@@ -72,12 +53,12 @@ if((NOT DEFINED CURL_INCLUDE_DIRS
   set(EP_INSTALL_DIR ${CMAKE_BINARY_DIR}/${proj}-install)
 
   ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/Slicer/curl.git"
     GIT_TAG "c2bc1187192ea9565f16db6382abc574114af193"
     SOURCE_DIR curl
     BINARY_DIR ${EP_BUILD_DIR}
-    CMAKE_GENERATOR ${gen}
-    CMAKE_ARGS
+    CMAKE_CACHE_ARGS
     #Not needed -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
     #Not needed -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
       -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
@@ -99,9 +80,10 @@ if((NOT DEFINED CURL_INCLUDE_DIRS
       -DCURL_DISABLE_FILE:BOOL=ON
       -DCURL_DISABLE_TFTP:BOOL=ON
       -DHAVE_LIBIDN:BOOL=FALSE
-      ${EXTERNAL_PROJECT_OPTIONAL_ARGS}
+      -DCMAKE_USE_OPENSSL:BOOL=${CURL_ENABLE_SSL}
+    ${EXTERNAL_PROJECT_OPTIONAL_ARGS}
     DEPENDS
-      ${curl_DEPENDENCIES}
+      ${${proj}_DEPENDENCIES}
     )
 
   if(UNIX)
@@ -119,8 +101,15 @@ if((NOT DEFINED CURL_INCLUDE_DIRS
   set(CURL_LIBRARY "${EP_INSTALL_DIR}/lib/libcurl${curl_IMPORT_SUFFIX}")
 
 else()
-  SlicerMacroEmptyExternalProject(${proj} "${curl_DEPENDENCIES}")
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()
 
-message(STATUS "${__${proj}_superbuild_message} - CURL_INCLUDE_DIR:${CURL_INCLUDE_DIR}")
-message(STATUS "${__${proj}_superbuild_message} - CURL_LIBRARY:${CURL_LIBRARY}")
+mark_as_superbuild(
+  VARS
+    CURL_INCLUDE_DIR:PATH
+    CURL_LIBRARY:FILEPATH
+  LABELS "FIND_PACKAGE"
+  )
+
+ExternalProject_Message(${proj} "CURL_INCLUDE_DIR:${CURL_INCLUDE_DIR}")
+ExternalProject_Message(${proj} "CURL_LIBRARY:${CURL_LIBRARY}")

CMake/SlicerConfig.cmake.in
@@ -330,103 +330,13 @@ macro(slicer_config_set_ep var value)
   endif()
 endmacro()
 
-# For sake of simplification, the macro 'slicer_config_set_ep' is not invoked conditionally, if
-# the configured 'value' parameter is an empty string, the macro 'slicer_config_set_ep' is a no-op.
-
-slicer_config_set_ep(
-  QT_QMAKE_EXECUTABLE
-  "@QT_QMAKE_EXECUTABLE_CONFIG@"
-  )
-
-slicer_config_set_ep(
-  BatchMake_DIR
-  "@BatchMake_DIR_CONFIG@"
-  CACHE PATH "Path to BatchMake build or install directory" FORCE)
-
-slicer_config_set_ep(
-  DCMTK_DIR
-  "@DCMTK_DIR_CONFIG@"
-  CACHE PATH "Path to DCMTK build or install directory" FORCE)
-
-slicer_config_set_ep(
-  CTK_DIR
-  "@CTK_DIR_CONFIG@"
-  CACHE PATH "Path to CTK build or install directory" FORCE)
-
-slicer_config_set_ep(
-  QtTesting_DIR
-  "@QtTesting_DIR_CONFIG@"
-  CACHE PATH "Path to QtTesting build or install directory" FORCE)
-
-slicer_config_set_ep(
-  ITK_DIR
-  "@ITK_DIR_CONFIG@"
-  CACHE PATH "Path to ITK build or install directory" FORCE)
-
-slicer_config_set_ep(
-  SimpleITK_DIR
-  "@SimpleITK_DIR_CONFIG@"
-  CACHE PATH "Path to SimpleITK build or install directory" FORCE)
-
-slicer_config_set_ep(
-  OpenIGTLink_DIR
-  "@OpenIGTLink_DIR_CONFIG@"
-  CACHE PATH "Path to OpenIGTLink build or install directory" FORCE)
-
-slicer_config_set_ep(
-  PYTHON_EXECUTABLE
-  "@PYTHON_EXECUTABLE_CONFIG@"
-  CACHE PATH "Path to Python executable" FORCE)
-
-slicer_config_set_ep(
-  PYTHON_INCLUDE_DIR
-  "@PYTHON_INCLUDE_DIR_CONFIG@"
-  CACHE PATH "Path to Python include directory" FORCE)
-
-slicer_config_set_ep(
-  PYTHON_LIBRARY
-  "@PYTHON_LIBRARY_CONFIG@"
-  CACHE FILEPATH "Path to Python library" FORCE)
-
-slicer_config_set_ep(
-  PYTHON_DEBUG_LIBRARY
-  "@PYTHON_DEBUG_LIBRARY_CONFIG@"
-  CACHE FILEPATH "Path to Python debug library" FORCE)
-
-slicer_config_set_ep(
-  qRestAPI_DIR
-  "@qRestAPI_DIR_CONFIG@"
-  CACHE PATH "Path to qRestAPI build or install directory" FORCE)
-
-slicer_config_set_ep(
-  CURL_INCLUDE_DIR
-  "@CURL_INCLUDE_DIR_CONFIG@"
-  CACHE PATH "Path to CURL include directory" FORCE)
-
-slicer_config_set_ep(
-  CURL_LIBRARY
-  "@CURL_LIBRARY_CONFIG@"
-  CACHE PATH "Path to CURL library" FORCE)
-
 # SlicerExecutionModel settings
 set(SlicerExecutionModel_CLI_LIBRARY_WRAPPER_CXX "@SlicerExecutionModel_CLI_LIBRARY_WRAPPER_CXX_CONFIG@")
 set(SlicerExecutionModel_EXTRA_INCLUDE_DIRECTORIES "@SlicerExecutionModel_EXTRA_INCLUDE_DIRECTORIES_CONFIG@")
 set(SlicerExecutionModel_EXTRA_EXECUTABLE_TARGET_LIBRARIES "@SlicerExecutionModel_EXTRA_EXECUTABLE_TARGET_LIBRARIES_CONFIG@")
 
-slicer_config_set_ep(
-  SlicerExecutionModel_DIR
-  "@SlicerExecutionModel_DIR_CONFIG@"
-  CACHE PATH "Path to SlicerExecutionModel build or install directory" FORCE)
-
-slicer_config_set_ep(
-  Teem_DIR
-  "@Teem_DIR_CONFIG@"
-  CACHE PATH "Path to Teem build or install directory")
-
-slicer_config_set_ep(
-  VTK_DIR
-  "@VTK_DIR_CONFIG@"
-  CACHE PATH "Path to VTK build or install directory")
+# Slicer external projects variables
+@Slicer_SUPERBUILD_EP_VARS_CONFIG@
 
 # --------------------------------------------------------------------------
 # Consistent Compiler Selections

CMake/SlicerGenerateSlicerConfig.cmake
@@ -58,45 +58,20 @@ set(RemoteIO_INCLUDE_DIRS_CONFIG ${RemoteIO_INCLUDE_DIRS})
 set(vtkTeem_INCLUDE_DIRS_CONFIG ${vtkTeem_INCLUDE_DIRS})
 set(vtkITK_INCLUDE_DIRS_CONFIG ${vtkITK_INCLUDE_DIRS})
 
-# Qt
-set(QT_QMAKE_EXECUTABLE_CONFIG ${QT_QMAKE_EXECUTABLE})
+# Note: For sake of simplification, the macro 'slicer_config_set_ep' is not invoked conditionally, if
+# the configured 'value' parameter is an empty string, the macro 'slicer_config_set_ep' is a no-op.
 
-# External project
-if(Slicer_USE_BatchMake)
-  set(BatchMake_DIR_CONFIG ${BatchMake_DIR})
-endif()
-set(CTK_DIR_CONFIG ${CTK_DIR})
-if(Slicer_BUILD_DICOM_SUPPORT)
-  set(DCMTK_DIR_CONFIG ${DCMTK_DIR})
-endif()
-if(Slicer_USE_QtTesting)
-  set(QtTesting_DIR_CONFIG ${QtTesting_DIR})
-endif()
-set(ITK_DIR_CONFIG ${ITK_DIR})
-if(Slicer_USE_SimpleITK)
-  set(SimpleITK_DIR_CONFIG ${SimpleITK_DIR})
-endif()
-if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
-  set(qRestAPI_DIR_CONFIG ${qRestAPI_DIR})
-endif()
-if(Slicer_USE_OpenIGTLink)
-  set(OpenIGTLink_DIR_CONFIG ${OpenIGTLink_DIR})
-endif()
-if(Slicer_USE_PYTHONQT)
-  set(PYTHON_EXECUTABLE_CONFIG ${PYTHON_EXECUTABLE})
-  set(PYTHON_INCLUDE_DIR_CONFIG ${PYTHON_INCLUDE_DIR})
-  set(PYTHON_LIBRARY_CONFIG ${PYTHON_LIBRARY})
-  if(WIN32)
-    set(PYTHON_DEBUG_LIBRARY_CONFIG ${PYTHON_DEBUG_LIBRARY})
-  endif()
-endif()
-if(Slicer_BUILD_CLI_SUPPORT)
-  set(SlicerExecutionModel_DIR_CONFIG ${SlicerExecutionModel_DIR})
-endif()
-set(CURL_INCLUDE_DIR_CONFIG ${CURL_INCLUDE_DIR})
-set(CURL_LIBRARY_CONFIG ${CURL_LIBRARY})
-set(Teem_DIR_CONFIG ${Teem_DIR})
-set(VTK_DIR_CONFIG ${VTK_DIR})
+# Slicer external projects variables
+set(Slicer_SUPERBUILD_EP_VARS_CONFIG)
+foreach(varname ${Slicer_EP_LABEL_FIND_PACKAGE} QtTesting_DIR)
+  set(Slicer_SUPERBUILD_EP_VARS_CONFIG
+   "${Slicer_SUPERBUILD_EP_VARS_CONFIG}
+slicer_config_set_ep(
+  ${varname}
+  \"${${varname}}\"
+  CACHE STRING \"Path to project build directory or file\" FORCE)
+")
+endforeach()
 
 # List all required external project
 set(Slicer_EXTERNAL_PROJECTS_CONFIG CTK ITK CURL Teem VTK)
@@ -139,12 +114,3 @@ set(Slicer_TARGETS_FILE "${Slicer_BINARY_DIR}/SlicerTargets.cmake")
 configure_file(
   ${Slicer_SOURCE_DIR}/CMake/SlicerConfig.cmake.in
   ${Slicer_BINARY_DIR}/SlicerConfig.cmake @ONLY)
-
-# Settings specific for installation trees
-#
-# (Note we configure from a different file than use for the build tree)
-
-# Configure SlicerConfig.cmake for the install tree.
-configure_file(
-  ${Slicer_SOURCE_DIR}/CMake/SlicerInstallConfig.cmake.in
-  ${Slicer_BINARY_DIR}/Utilities/SlicerConfig.cmake @ONLY)

CMake/UseSlicer.cmake.in
@@ -232,6 +232,7 @@ endif()
 include(CMakeParseArguments)
 include(ctkMacroCompilePythonScript)
 include(ctkMacroWrapPythonQt)
+include(ExternalProjectDependency)
 include(SlicerMacroParseArguments)
 include(SlicerMacroCheckExternalProjectDependency)
 include(SlicerMacroEmptyExternalProject)

CMakeLists.txt
@@ -1,10 +1,20 @@
-#-----------------------------------------------------------------------------
+
 cmake_minimum_required(VERSION 2.8.9)
+
+#-----------------------------------------------------------------------------
+set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/CMake" ${CMAKE_MODULE_PATH})
+include(ExternalProject)
+include(ExternalProjectDependency)
+
 #-----------------------------------------------------------------------------
 if(APPLE)
   # Note: By setting CMAKE_OSX_* variables before any enable_language() or project() calls,
   #       we ensure that the bitness will be properly detected.
-  include(${CMAKE_SOURCE_DIR}/CMake/SlicerBlockSetCMakeOSXVariables.cmake)
+  include(SlicerBlockSetCMakeOSXVariables)
+  mark_as_superbuild(
+    VARS CMAKE_OSX_ARCHITECTURES:STRING CMAKE_OSX_SYSROOT:PATH CMAKE_OSX_DEPLOYMENT_TARGET:STRING
+    ALL_PROJECTS
+    )
 endif()
 
 project(Slicer)
@@ -20,7 +30,6 @@ endif()
 # Update CMake module path
 #------------------------------------------------------------------------------
 set(CMAKE_MODULE_PATH
-  ${Slicer_SOURCE_DIR}/CMake
   ${Slicer_SOURCE_DIR}/Extensions/CMake
   ${CMAKE_MODULE_PATH}
   )
@@ -29,7 +38,7 @@ set(Slicer_CMAKE_DIR ${Slicer_SOURCE_DIR}/CMake)
 set(Slicer_EXTENSIONS_CMAKE_DIR ${Slicer_SOURCE_DIR}/Extensions/CMake)
 
 #-----------------------------------------------------------------------------
-# http://www.slicer.org/slicerWiki/index.php/Documentation/4.3/Developers/DevelopmentWithGit
+# http://www.slicer.org/slicerWiki/index.php/Documentation/Nightly/Developers/DevelopmentWithGit
 #-----------------------------------------------------------------------------
 include(SlicerCheckSourceTree)
 
@@ -101,6 +110,8 @@ endif()
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   message(STATUS "Setting build type to 'Debug' as none was specified.")
   set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
+  mark_as_advanced(CMAKE_BUILD_TYPE)
+  mark_as_superbuild(VARS CMAKE_BUILD_TYPE ALL_PROJECTS)
   # Set the possible values of build type for cmake-gui
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release"
     "MinSizeRel" "RelWithDebInfo")
@@ -117,6 +128,7 @@ set(Slicer_LOGOS_RESOURCE "${Slicer_SOURCE_DIR}/Resources/qSlicer.qrc")
 # Platform checks
 #-----------------------------------------------------------------------------
 include(SlicerBlockPlatformCheck)
+mark_as_superbuild(Slicer_PLATFORM_CHECK:BOOL)
 
 #-----------------------------------------------------------------------------
 # Determine endian type
@@ -131,11 +143,13 @@ find_package(Subversion)
 if(NOT Subversion_FOUND)
   message(FATAL_ERROR "error: Install SVN and try to re-configure")
 endif()
+mark_as_superbuild(Subversion_SVN_EXECUTABLE)
 
 find_package(Git)
 if(NOT GIT_FOUND)
   message(FATAL_ERROR "error: Install Git and try to re-configure")
 endif()
+mark_as_superbuild(GIT_EXECUTABLE)
 
 #-----------------------------------------------------------------------------
 # Qt requirements
@@ -143,11 +157,12 @@ endif()
 if(NOT DEFINED Slicer_REQUIRED_QT_VERSION)
   set(Slicer_REQUIRED_QT_VERSION "4.7.4" CACHE STRING "Minimum required Qt version" FORCE)
 endif()
+mark_as_superbuild(Slicer_REQUIRED_QT_VERSION)
 
 #-----------------------------------------------------------------------------
 # Superbuild Option - Enabled by default
 #-----------------------------------------------------------------------------
-option(Slicer_SUPERBUILD "Build Slicer and the projects it depends on via SuperBuild.cmake." ON)
+option(Slicer_SUPERBUILD "Build ${PROJECT_NAME} and the projects it depends on." ON)
 mark_as_advanced(Slicer_SUPERBUILD)
 
 set(Slicer_BINARY_INNER_SUBDIR Slicer-build)
@@ -157,12 +172,14 @@ set(Slicer_BINARY_INNER_SUBDIR Slicer-build)
 #------------------------------------------------------------------------------
 # option(BUILD_SHARED_LIBS "Build Slicer with shared libraries." ON)
 set(BUILD_SHARED_LIBS ON)
+mark_as_superbuild(BUILD_SHARED_LIBS:BOOL)
 
 #-----------------------------------------------------------------------------
 # Append the library version information to the library target properties.
 #------------------------------------------------------------------------------
 option(Slicer_WITH_LIBRARY_VERSION "Build with library version information" OFF)
 mark_as_advanced(Slicer_WITH_LIBRARY_VERSION)
+mark_as_superbuild(Slicer_WITH_LIBRARY_VERSION)
 if(Slicer_WITH_LIBRARY_VERSION)
   set(Slicer_LIBRARY_PROPERTIES ${Slicer_LIBRARY_PROPERTIES}
     VERSION ${Slicer_VERSION_FULL}
@@ -176,31 +193,58 @@ endif()
 include(CMakeDependentOption)
 
 option(BUILD_TESTING "Test the project" ON)
+mark_as_superbuild(BUILD_TESTING)
+
 #option(WITH_MEMCHECK "Run tests through valgrind." OFF)
+#mark_as_superbuild(WITH_MEMCHECK)
+
 option(WITH_COVERAGE "Enable/Disable coverage" OFF)
+mark_as_superbuild(WITH_COVERAGE)
+
 option(Slicer_USE_VTK_DEBUG_LEAKS "Enable VTKs Debug Leaks functionality in both VTK and Slicer." ON)
+set(VTK_DEBUG_LEAKS ${Slicer_USE_VTK_DEBUG_LEAKS})
+mark_as_superbuild(VTK_DEBUG_LEAKS:BOOL)
+
 option(Slicer_BUILD_DICOM_SUPPORT "Build Slicer with DICOM support" ON)
+mark_as_superbuild(Slicer_BUILD_DICOM_SUPPORT)
+
 option(Slicer_BUILD_DIFFUSION_SUPPORT "Build Slicer with diffusion (DWI, DTI) support" ON)
+mark_as_superbuild(Slicer_BUILD_DIFFUSION_SUPPORT)
+
 option(Slicer_BUILD_I18N_SUPPORT "Build Slicer with Internationalization support" OFF)
+
 option(Slicer_BUILD_QTLOADABLEMODULES "Build Slicer QT Loadable Modules" ON)
 mark_as_advanced(Slicer_BUILD_QTLOADABLEMODULES)
+mark_as_superbuild(Slicer_BUILD_QTLOADABLEMODULES)
+
 option(Slicer_USE_PYTHONQT "Integrate a python-QT interpreter into Slicer." ON)
+mark_as_superbuild(Slicer_USE_PYTHONQT)
+
 CMAKE_DEPENDENT_OPTION(
   Slicer_BUILD_QTSCRIPTEDMODULES "Build Slicer Python QT Modules" ON
   "Slicer_USE_PYTHONQT" OFF)
 mark_as_advanced(Slicer_BUILD_QTSCRIPTEDMODULES)
+mark_as_superbuild(Slicer_BUILD_QTSCRIPTEDMODULES)
+
 option(Slicer_BUILD_CLI_SUPPORT "Build Slicer with CLI support" ON)
+mark_as_superbuild(Slicer_BUILD_CLI_SUPPORT)
 CMAKE_DEPENDENT_OPTION(
   Slicer_BUILD_CLI "Build Slicer CLI Plugins" ON
   "Slicer_BUILD_CLI_SUPPORT" OFF
   )
+
+mark_as_superbuild(Slicer_BUILD_CLI)
 CMAKE_DEPENDENT_OPTION(
   Slicer_BUILD_LEGACY_CLI "Build Slicer LEGACY_CLI Plugins" ON
   "Slicer_BUILD_CLI" OFF
   )
+
 option(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT "Build Slicer extension manager" ON)
+mark_as_superbuild(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
+
 CMAKE_DEPENDENT_OPTION(Slicer_UPDATE_TRANSLATION "update translation" OFF "Slicer_BUILD_I18N_SUPPORT" OFF)
 mark_as_advanced(Slicer_UPDATE_TRANSLATION)
+mark_as_superbuild(Slicer_UPDATE_TRANSLATION)
 
 set(Slicer_FORCED_WC_REVISION "" CACHE STRING "Overwrite value of auto-discovered Slicer_WC_REVISION")
 mark_as_advanced(Slicer_FORCED_WC_REVISION)
@@ -211,20 +255,34 @@ mark_as_advanced(Slicer_FORCED_WC_REVISION)
 CMAKE_DEPENDENT_OPTION(
   Slicer_USE_PYTHONQT_WITH_OPENSSL "Enable PythonQt SSL support" OFF
   "Slicer_USE_PYTHONQT" OFF)
+mark_as_superbuild(Slicer_USE_PYTHONQT_WITH_OPENSSL)
+
 CMAKE_DEPENDENT_OPTION(
   Slicer_USE_PYTHONQT_WITH_TCL "Enable PythonQt Tcl adapter layer" ON
   "Slicer_USE_PYTHONQT" OFF)
+mark_as_superbuild(Slicer_USE_PYTHONQT_WITH_TCL)
+
 CMAKE_DEPENDENT_OPTION(
   Slicer_USE_NUMPY "Build Slicer with NumPy" ON
   "Slicer_USE_PYTHONQT" OFF)
-# option(Slicer_USE_BatchMake   "Build Slicer with BatchMake support." ON)
+mark_as_superbuild(Slicer_USE_NUMPY)
+
 set(Slicer_USE_BatchMake OFF CACHE INTERNAL "Build Slicer with BatchMake support." FORCE)
+mark_as_superbuild(Slicer_USE_BatchMake:BOOL)
+
 option(Slicer_USE_MIDAS       "Build Slicer with MIDAS support." OFF)
+mark_as_superbuild(Slicer_USE_MIDAS)
+
 option(Slicer_USE_OpenIGTLink "Build Slicer with OpenIGTLink support." ON)
+mark_as_superbuild(Slicer_USE_OpenIGTLink)
+
 option(Slicer_USE_CTKAPPLAUNCHER "Configure ctkAppLauncher." ON)
 mark_as_advanced(Slicer_USE_CTKAPPLAUNCHER)
+mark_as_superbuild(Slicer_USE_CTKAPPLAUNCHER)
+
 option(Slicer_USE_QtTesting    "Integrate QtTesting framework into Slicer." ON)
 mark_as_advanced(Slicer_USE_QtTesting)
+mark_as_superbuild(Slicer_USE_QtTesting)
 
 set(ITK_VERSION_MAJOR 4 CACHE STRING "Choose the expected ITK major version to build Slicer (3 or 4).")
 # Set the possible values of ITK major version for cmake-gui
@@ -233,6 +291,7 @@ if(NOT "${ITK_VERSION_MAJOR}" STREQUAL "3" AND NOT "${ITK_VERSION_MAJOR}" STREQU
   set(ITK_VERSION_MAJOR 3 CACHE STRING "Choose the expected ITK major version to build Slicer (3 or 4)." FORCE)
   message(WARNING "Setting ITK_VERSION_MAJOR to '3' as an valid value was specified.")
 endif()
+mark_as_superbuild(ITK_VERSION_MAJOR)
 
 set(USE_ITKv3 OFF)
 set(USE_ITKv4 ON)
@@ -242,6 +301,7 @@ if(${ITK_VERSION_MAJOR} STREQUAL "3")
 endif()
 
 CMAKE_DEPENDENT_OPTION(Slicer_USE_SimpleITK "Build Slicer with SimpleITK support" OFF "USE_ITKv4" OFF)
+mark_as_superbuild(Slicer_USE_SimpleITK)
 
 #
 # SimpleITK has large internal libraries, which take an extremely long
@@ -258,6 +318,7 @@ if(MSVC OR _CMAKE_BUILD_TYPE MATCHES "DEBUG")
   set(Slicer_USE_SimpleITK_SHARED_DEFAULT ON)
 endif()
 CMAKE_DEPENDENT_OPTION(Slicer_USE_SimpleITK_SHARED "Build SimpleITK with shared libraries. Reduces linking time, increases run-time load time." ${Slicer_USE_SimpleITK_SHARED_DEFAULT} "Slicer_USE_SimpleITK" OFF )
+mark_as_superbuild(Slicer_USE_SimpleITK_SHARED)
 
 #-----------------------------------------------------------------------------
 # The following option has been added for conveniency so that "extension-like"
@@ -266,17 +327,22 @@ CMAKE_DEPENDENT_OPTION(
   Slicer_BUILD_OpenIGTLinkIF "Build OpenIGTLinkIF." ON
   "Slicer_BUILD_QTLOADABLEMODULES AND Slicer_USE_OpenIGTLink" OFF)
 mark_as_advanced(CLEAR Slicer_BUILD_OpenIGTLinkIF)
+mark_as_superbuild(Slicer_BUILD_OpenIGTLinkIF)
 
 option(Slicer_BUILD_MULTIVOLUME_SUPPORT "Build MultiVolume support." ON)
 mark_as_advanced(Slicer_BUILD_MULTIVOLUME_SUPPORT)
+
 CMAKE_DEPENDENT_OPTION(
   Slicer_BUILD_MultiVolumeImporter "Build MultiVolumeImporter." ON
   "Slicer_BUILD_QTSCRIPTEDMODULES AND Slicer_BUILD_MULTIVOLUME_SUPPORT AND Slicer_USE_NUMPY" OFF)
 mark_as_advanced(Slicer_BUILD_MultiVolumeImporter)
+mark_as_superbuild(Slicer_BUILD_MultiVolumeImporter)
+
 CMAKE_DEPENDENT_OPTION(
   Slicer_BUILD_MultiVolumeExplorer "Build MultiVolumeExplorer." ON
   "Slicer_BUILD_QTLOADABLEMODULES AND Slicer_BUILD_MULTIVOLUME_SUPPORT AND Slicer_USE_PYTHONQT" OFF)
 mark_as_advanced(Slicer_BUILD_MultiVolumeExplorer)
+mark_as_superbuild(Slicer_BUILD_MultiVolumeExplorer)
 
 CMAKE_DEPENDENT_OPTION(
   Slicer_BUILD_SimpleFilters "Build SimpleFilters." ON
@@ -287,11 +353,13 @@ CMAKE_DEPENDENT_OPTION(
   Slicer_BUILD_BRAINSTOOLS "Build the BRAINS subset of registration and segmentation tools." ON
   "Slicer_BUILD_CLI_SUPPORT AND Slicer_BUILD_CLI" OFF)
 mark_as_advanced(Slicer_BUILD_BRAINSTOOLS)
+mark_as_superbuild(Slicer_BUILD_BRAINSTOOLS)
 
 CMAKE_DEPENDENT_OPTION(
   Slicer_BUILD_EMSegment "Build EMSegment." ON
   "Slicer_BUILD_BRAINSTOOLS AND Slicer_BUILD_QTLOADABLEMODULES AND Slicer_USE_PYTHONQT" OFF)
 mark_as_advanced(Slicer_BUILD_EMSegment)
+mark_as_superbuild(Slicer_BUILD_EMSegment)
 
 #-----------------------------------------------------------------------------
 # Install no development files by default, but allow the user to get
@@ -312,12 +380,16 @@ endif()
 find_package(Doxygen QUIET)
 
 if(DOXYGEN_DOT_FOUND)
+  mark_as_superbuild(DOXYGEN_EXECUTABLE)
+
   set(DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
    CACHE PATH "Where documentation archives should be stored")
   mark_as_advanced(DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY)
+  mark_as_superbuild(DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY)
 
   option(DOCUMENTATION_TARGET_IN_ALL "Include the custom target for building documentation in 'all'" OFF)
   mark_as_advanced(DOCUMENTATION_TARGET_IN_ALL)
+  mark_as_superbuild(DOCUMENTATION_TARGET_IN_ALL)
 endif()
 
 #-----------------------------------------------------------------------------
@@ -345,7 +417,6 @@ include(CMakeParseArguments)
 include(Slicer3PluginsMacros)
 include(SlicerMacroExtractRepositoryInfo)
 include(SlicerMacroGetOperatingSystemArchitectureBitness)
-include(SlicerMacroEmptyExternalProject)
 if(Slicer_BUILD_I18N_SUPPORT)
   include(SlicerMacroTranslation)
 endif()
@@ -381,6 +452,7 @@ set(SLICER_REVISION_SPECIFIC_USER_SETTINGS_FILEBASENAME "")
 if(NOT DEFINED Slicer_MAIN_PROJECT)
   set(Slicer_MAIN_PROJECT SlicerApp CACHE INTERNAL "Main project name")
 endif()
+mark_as_superbuild(Slicer_MAIN_PROJECT:STRING)
 if(NOT DEFINED ${Slicer_MAIN_PROJECT}_APPLICATION_NAME)
   set(${Slicer_MAIN_PROJECT}_APPLICATION_NAME Slicer CACHE INTERNAL "Main application name")
 else()
@@ -574,11 +646,23 @@ if(BUILD_TESTING)
 endif()
 list(APPEND Slicer_REQUIRED_QT_MODULES ${Slicer_ADDITIONAL_REQUIRED_QT_MODULES})
 
+if(DEFINED Slicer_ADDITIONAL_REQUIRED_QT_MODULES)
+  mark_as_superbuild(Slicer_ADDITIONAL_REQUIRED_QT_MODULES:STRING)
+endif()
+
+mark_as_superbuild(Slicer_USE_SYSTEM_QT:BOOL)
+
 #-----------------------------------------------------------------------------
 # Qt plugins (designer, imageformats, ...) relative directories
 #-----------------------------------------------------------------------------
 set(Slicer_QtPlugins_DIR "lib/QtPlugins")
 set(Slicer_INSTALL_QtPlugins_DIR "${Slicer_INSTALL_ROOT}${Slicer_QtPlugins_DIR}")
+
+#-----------------------------------------------------------------------------
+# Qt
+#-----------------------------------------------------------------------------
+include(SlicerBlockFindQtAndCheckVersion)
+mark_as_superbuild(VARS QT_QMAKE_EXECUTABLE LABELS "FIND_PACKAGE")
 
 #-----------------------------------------------------------------------------
 # Testing
@@ -768,11 +852,6 @@ if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
   find_package(qRestAPI REQUIRED)
   include(${qRestAPI_USE_FILE})
 endif()
-
-#-----------------------------------------------------------------------------
-# Qt
-#-----------------------------------------------------------------------------
-include(SlicerBlockFindQtAndCheckVersion)
 
 #-----------------------------------------------------------------------------
 # Update CMake module path

Extensions/Testing/SuperBuildExtensionTemplate/CMakeLists.txt
@@ -1,7 +1,6 @@
 cmake_minimum_required(VERSION 2.8.9)
 
-#-----------------------------------------------------------------------------
-set(EXTENSION_NAME SuperBuildExtensionTemplate)
+project(SuperBuildExtensionTemplate)
 
 #-----------------------------------------------------------------------------
 set(EXTENSION_HOMEPAGE "http://www.slicer.org/slicerWiki/index.php/Documentation/4.3/Extensions/SuperBuildExtensionTemplate")
@@ -13,14 +12,18 @@ set(EXTENSION_SCREENSHOTURLS "http://wiki.slicer.org/slicerWiki/images/b/b3/Slic
 set(EXTENSION_DEPENDS "NA") # Specified as a space separated list or 'NA' if any
 set(EXTENSION_BUILD_SUBDIRECTORY inner-build)
 
+set(SUPERBUILD_TOPLEVEL_PROJECT inner)
+
 #-----------------------------------------------------------------------------
 find_package(Slicer REQUIRED)
 include(${Slicer_USE_FILE})
+mark_as_superbuild(Slicer_DIR)
 
 find_package(Git REQUIRED)
+mark_as_superbuild(GIT_EXECUTABLE)
 
 #-----------------------------------------------------------------------------
-option(${EXTENSION_NAME}_SUPERBUILD "Build ${EXTENSION_NAME} and the projects it depends on via SuperBuild.cmake." ON)
+option(${EXTENSION_NAME}_SUPERBUILD "Build ${EXTENSION_NAME} and the projects it depends on." ON)
 mark_as_advanced(${EXTENSION_NAME}_SUPERBUILD)
 if(${EXTENSION_NAME}_SUPERBUILD)
   include("${CMAKE_CURRENT_SOURCE_DIR}/SuperBuild.cmake")
@@ -34,4 +37,4 @@ add_subdirectory(SuperLoadableModuleTemplate)
 #-----------------------------------------------------------------------------
 set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${CMAKE_BINARY_DIR};${EXTENSION_NAME};ALL;/")
 #set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Foo_DIR};Foo;RuntimeLibraries;/")
-INCLUDE(${Slicer_EXTENSION_CPACK})
+include(${Slicer_EXTENSION_CPACK})

commit 40747437540aa9317e33394b4c97b4a8fa249dfc

COMP: Ensure "curl" is built against Slicer OpenSSL library
This commit ensure the correct CMake variable is used. The issue
was originally introduced by commit r22773.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23007
git-svn-id: http://svn.slicer.org/Slicer4/trunk@23007 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_curl.cmake
@@ -23,10 +23,10 @@ endif()
 if((NOT DEFINED CURL_INCLUDE_DIR
    OR NOT DEFINED CURL_LIBRARY) AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
-  set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_ARGS CMAKE_ARGS)
+  set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_ARGS)
 
   if(CURL_ENABLE_SSL)
-    list(APPEND EXTERNAL_PROJECT_OPTIONAL_CMAKE_ARGS
+    list(APPEND EXTERNAL_PROJECT_OPTIONAL_CMAKE_ARGS CMAKE_ARGS
       -DOPENSSL_INCLUDE_DIR:PATH=${OPENSSL_INCLUDE_DIR}
       )
     if(UNIX)
@@ -81,7 +81,7 @@ if((NOT DEFINED CURL_INCLUDE_DIR
       -DCURL_DISABLE_TFTP:BOOL=ON
       -DHAVE_LIBIDN:BOOL=FALSE
       -DCMAKE_USE_OPENSSL:BOOL=${CURL_ENABLE_SSL}
-    ${EXTERNAL_PROJECT_OPTIONAL_ARGS}
+    ${EXTERNAL_PROJECT_OPTIONAL_CMAKE_ARGS}
     DEPENDS
       ${${proj}_DEPENDENCIES}
     )