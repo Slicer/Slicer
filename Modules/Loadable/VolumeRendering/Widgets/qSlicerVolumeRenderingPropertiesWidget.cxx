0e9ed7f0b4b1474693d1230f95a8c66ccc46a5fc
Julien Finet <julien.finet@kitware.com>
2014-03-24 22:11:52 +0000

    STYLE: Fix header copyright.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23014
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23014 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Loadable/VolumeRendering/Widgets/qSlicerVolumeRenderingPropertiesWidget.cxx
+++ Modules/Loadable/VolumeRendering/Widgets/qSlicerVolumeRenderingPropertiesWidget.cxx
@@ -13,7 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 
-  This file was originally developed by Julien Finet, Isomics Inc.
+  This file was originally developed by Julien Finet, Kitware Inc.
   and was partially funded by NIH grant 3P41RR013218-12S1
 
 ==============================================================================*/
