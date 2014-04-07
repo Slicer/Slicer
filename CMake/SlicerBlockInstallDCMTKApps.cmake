832bbfbbbc95df3b5145bff334bb39f2ff368bfd
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-04-07 23:41:19 +0000

    STYLE: Simplify installation rules associated with DCMTK applications
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23048
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23048 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallDCMTKApps.cmake
+++ CMake/SlicerBlockInstallDCMTKApps.cmake
@@ -2,19 +2,14 @@
 # Find and install DCMTK Apps
 # -------------------------------------------------------------------------
 
-if(WIN32)
-  set(EXE ".exe")
-endif()
-
 set(DCMTK_Apps storescu storescp dcmdump dump2dcm img2dcm dcmdjpeg dcmqrscp)
 set(int_dir "")
 if(WIN32)
   set(int_dir "Release/")
 endif()
 foreach(dcmtk_App ${DCMTK_Apps})
-  install(FILES ${CTK_DCMTK_DIR}/bin/${int_dir}${dcmtk_App}${EXE}
+  install(PROGRAMS ${CTK_DCMTK_DIR}/bin/${int_dir}${dcmtk_App}${CMAKE_EXECUTABLE_SUFFIX}
     DESTINATION ${Slicer_INSTALL_BIN_DIR}
-    PERMISSIONS OWNER_WRITE OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ
     COMPONENT Runtime
     )
 endforeach()
