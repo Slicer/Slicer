8c1afab01d9cca0739dde64a07fa8fbe0ffad620
Junichi Tokuda <tokuda@bwh.harvard.edu>
2014-04-17 20:35:11 +0000

    ENH: Update OpenIGTLinkIF SHA1 to add color image support. See #3649
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23107
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23107 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -147,7 +147,7 @@ list(APPEND Slicer_REMOTE_DEPENDENCIES jqPlot)
 
 Slicer_Remote_Add(OpenIGTLinkIF
   GIT_REPOSITORY ${git_protocol}://github.com/openigtlink/OpenIGTLinkIF.git
-  GIT_TAG 38491441f112df5a6fb6062f2cdf0a3cfcb1450b
+  GIT_TAG 24ae5443ecb633a54ab7b7b8178da53d1c5e2f74
   OPTION_NAME Slicer_BUILD_OpenIGTLinkIF
   OPTION_DEPENDS "Slicer_BUILD_QTLOADABLEMODULES;Slicer_USE_OpenIGTLink"
   LABELS REMOTE_MODULE
