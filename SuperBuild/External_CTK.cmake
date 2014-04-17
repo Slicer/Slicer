9935b4e241890f6381ca203ac8a01b8bd8b09f5c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-04-17 19:52:32 +0000

    ENH: Update CTK do not display ctkWorkflow debug message
    $ git shortlog 2f1ff4..880bdd --no-merges
    Jean-Christophe Fillion-Robin (1):
          Introduce ctkWorkflow verbose property to print debug msg when needed
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23106
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23106 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "2f1ff4481104b6e9047a07ac418b25a3b1a233da"
+    GIT_TAG "880bdd632c19af8c8118d62ecea329fbeb720711"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
