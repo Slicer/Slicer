Slicer/Slicer

commit 41725aee6bde27518f6ba037370e9c84e0bde8a1

BUG: fix fiber bundle color display
Check for fiber bundle display nodes and use the old method,
igoring the new scalar range options for models. Fixes bug
introduced with issue #3573 in svn revision 23083

Issue #3672


svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23118
git-svn-id: http://svn.slicer.org/Slicer4/trunk@23118 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/DisplayableManager/vtkMRMLModelDisplayableManager.cxx
@@ -1478,6 +1478,7 @@ void vtkMRMLModelDisplayableManager::SetModelDisplayProperty(vtkMRMLDisplayableN
     vtkMRMLDisplayNode *mrmlDisplayNode = thisDisplayNode;
     vtkMRMLModelDisplayNode *modelDisplayNode =
       vtkMRMLModelDisplayNode::SafeDownCast(mrmlDisplayNode);
+    bool isFiberDisplayNode = mrmlDisplayNode->IsA("vtkMRMLFiberBundleDisplayNode");
     if (thisDisplayNode != 0)
       {
       vtkProp3D *prop = this->GetActorByID(thisDisplayNode->GetID());
@@ -1686,20 +1687,29 @@ void vtkMRMLModelDisplayableManager::SetModelDisplayProperty(vtkMRMLDisplayableN
               actor->GetMapper()->SetScalarModeToUseCellFieldData();
               actor->GetMapper()->SetColorModeToDefault();
               }
-            actor->GetMapper()->UseLookupTableScalarRangeOn();
-            if  (newMapperRange[0] != mapperTableRange[0] ||
-                 newMapperRange[1] != mapperTableRange[1])
+            if (isFiberDisplayNode)
               {
-              actor->GetMapper()->SetScalarRange(newMapperRange);
-              if (actor->GetMapper()->GetLookupTable()->IsA("vtkColorTransferFunction"))
+              // for fiber bundle display nodes, don't use the new scalar range options
+              actor->GetMapper()->UseLookupTableScalarRangeOff();
+              actor->GetMapper()->SetScalarRange(modelDisplayNode->GetScalarRange());
+              }
+            else
+              {
+              actor->GetMapper()->UseLookupTableScalarRangeOn();
+              if  (newMapperRange[0] != mapperTableRange[0] ||
+                   newMapperRange[1] != mapperTableRange[1])
                 {
-                vtkWarningMacro("Setting the range on a color transfer function will not work!");
+                actor->GetMapper()->SetScalarRange(newMapperRange);
+                if (actor->GetMapper()->GetLookupTable()->IsA("vtkColorTransferFunction"))
+                  {
+                  vtkWarningMacro("Setting the range on a color transfer function will not work!");
+                  }
+                actor->GetMapper()->GetLookupTable()->SetRange(newMapperRange);
+                vtkDebugMacro("Mapper use lookup table scalar range = "
+                              << actor->GetMapper()->GetUseLookupTableScalarRange()
+                              << ", newMapperRange = "
+                              << newMapperRange[0] << ", " << newMapperRange[1]);
                 }
-              actor->GetMapper()->GetLookupTable()->SetRange(newMapperRange);
-              vtkDebugMacro("Mapper use lookup table scalar range = "
-                            << actor->GetMapper()->GetUseLookupTableScalarRange()
-                            << ", newMapperRange = "
-                            << newMapperRange[0] << ", " << newMapperRange[1]);
               }
             } // Color node
           } // scalar visibility

commit 43e4d6ced0fcd762bc4bc232c701f0d7564be4ba

BUG: Fix crash in vtkMRMLModelDisplayableManager
Regression from r23118

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23125
git-svn-id: http://svn.slicer.org/Slicer4/trunk@23125 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/DisplayableManager/vtkMRMLModelDisplayableManager.cxx
@@ -1492,7 +1492,6 @@ void vtkMRMLModelDisplayableManager::SetModelDisplayProperty(vtkMRMLDisplayableN
   vtkMRMLTransformNode* tnode = model->GetParentTransformNode();
 
   vtkNew<vtkMatrix4x4> matrixTransformToWorld;
-  matrixTransformToWorld->Identity();
   if (tnode != 0 && tnode->IsLinear())
     {
     vtkMRMLLinearTransformNode *lnode = vtkMRMLLinearTransformNode::SafeDownCast(tnode);
@@ -1508,7 +1507,6 @@ void vtkMRMLModelDisplayableManager::SetModelDisplayProperty(vtkMRMLDisplayableN
     vtkMRMLDisplayNode *mrmlDisplayNode = thisDisplayNode;
     vtkMRMLModelDisplayNode *modelDisplayNode =
       vtkMRMLModelDisplayNode::SafeDownCast(mrmlDisplayNode);
-    bool isFiberDisplayNode = mrmlDisplayNode->IsA("vtkMRMLFiberBundleDisplayNode");
     if (thisDisplayNode != 0)
       {
       vtkProp3D *prop = this->GetActorByID(thisDisplayNode->GetID());
@@ -1717,6 +1715,7 @@ void vtkMRMLModelDisplayableManager::SetModelDisplayProperty(vtkMRMLDisplayableN
               actor->GetMapper()->SetScalarModeToUseCellFieldData();
               actor->GetMapper()->SetColorModeToDefault();
               }
+            bool isFiberDisplayNode = mrmlDisplayNode->IsA("vtkMRMLFiberBundleDisplayNode");
             if (isFiberDisplayNode)
               {
               // for fiber bundle display nodes, don't use the new scalar range options
@@ -1809,14 +1808,7 @@ void vtkMRMLModelDisplayableManager::SetModelDisplayProperty(vtkMRMLDisplayableN
           imageActor->SetInput(0);
           }
 #else
-        if (modelDisplayNode->GetTextureImageDataConnection() != 0)
-          {
-          imageActor->GetMapper()->SetInputConnection(modelDisplayNode->GetTextureImageDataConnection());
-          }
-        else
-          {
-          imageActor->GetMapper()->SetInputConnection(0);
-          }
+        imageActor->GetMapper()->SetInputConnection(modelDisplayNode->GetTextureImageDataConnection());
 #endif
         imageActor->SetDisplayExtent(-1, 0, 0, 0, 0, 0);
         }