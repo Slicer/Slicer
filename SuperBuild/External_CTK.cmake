31ddebdf463de6e6ae8381d51535ad78052e0060
Julien Finet <julien.finet@kitware.com>
2014-05-15 23:27:38 +0000

    COMP: Fix VTK linking inside CTK
    git shortlog 880bdd632c19af8c8118d62ecea329fbeb720711..8a17c21630f28a7dd22ff98514aaac1059d60433
    Alireza Mehrtash (3):
          Fix ctkDICOMobjectListWidget memory leak and other enahancements
          Added Option for changing tables text density in dicom browser
          added repair tool for database and also message box for importin dicom browser
    
    Jean-Christophe Fillion-Robin (16):
          COMP: Pass CMAKE_BUILD_TYPE to all projects
          Allow FindDCMTK.cmake to be re-used directly adding missing module.
          Merge pull request #453 from mwoehlke-kitware/ctksettingspanel-python
          Fixes CTK configuration renaming ctkDICOM2 application test. Fixes #454
          Merge branch '454-fix-configure-with-ctkdicom2'
          Fix pluginframework build error happening when wrapping is enabled.
          Merge branch '455-fix-pluginframework-python-wrap-error'
          Update dashboard driver script to submit a regular project layout.
          Remove configuration of unneeded Project.xml and CTestConfigSubProject.cmake
          Merge branch '457-revert-to-regular-cdash-submission'
          Do not include now nonexistent module ctkFunctionGenerateProjectXml
          Merge branch '457-revert-to-regular-cdash-submission'
          Fix condition used to unset DICOMApplicationHosting top level option
          Ensure ctk option are considered as superbuild candidate variable.
          Merge branch 'fix-multiple-configure-and-passing-of-options'
          Merge pull request #458 from mwoehlke-kitware/fix-superbuild-ctk-force-build
    
    Julien Finet (12):
          Fix VTK 6 support
          Merge branch 'add-vtk-6-support'
          Fix ctkVTKSliceView for VTK v5
          Merge branch 'add-vtk-7-support'
          Propagate new ctkBackTracePrivate::trace signature
          Merge branch 'fix-ctkbacktrace-out-of-line-definition'
          Remove duplicates from the include dirs
          Merge branch 'remove-include-duplicates'
          Do not link with VTK targets that don't exist
          Merge branch 'fix-vtk6-linking'
          Don't use set_directory_properties in UseCTK.cmake
          Merge branch 'remove-include-duplicates'
    
    Marco Nolden (5):
          Some size_t related type fixes for VS2013
          Fixed some unused variable warnings
          Removed non-functional copy constructor
          Make VTK6 linking more robust
          Fix DCMTK dictionary crashes in plugins
    
    Matthew Woehlke (3):
          Improve Python access to ctkSettingsPanel
          Improve Python access to ctkSettingsDialog
          Fix force build for CTK
    
    Sascha Zelzer (2):
          Added missing virtual destructor.
          Add header files to fix moc generation due to changed qt4_generate_mocs macro.
    
    Steve Pieper (5):
          Clean up cmake code for test
          Merge branch '387-dicom-test-cmake'
          Merge pull request #448 from mehrtash/ctk-dicom-object-list-widget-enhancements
          Merge pull request #438 from mehrtash/3579-very-few-patients-visible-in-DICOM-Browser
          Merge pull request #461 from mehrtash/added-repair-tool-and-copy-on-import-message-box
    
    Yuzheng Zhou (1):
          ENH: Support VTK6 in CTK
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23184
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23184 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/CommonTK/CTK.git"
-    GIT_TAG "eda759b8f6a4661ffd22a586f2a81a21c3f4bd5a"
+    GIT_TAG "8a17c21630f28a7dd22ff98514aaac1059d60433"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
