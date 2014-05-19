66f755e6cc9dafe1c4d4fdaef200a54918e52a6a
Julien Finet <julien.finet@kitware.com>
2014-05-19 19:10:03 +0000

    BUG: Fix unit scaling change
    A scale of 0 could be generated when changing from one scale to another
    (e.g. 0.1 to 1.)
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23206
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23206 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/CommonTK/CTK.git"
-    GIT_TAG "cd344ef5e4b23bc83e0adb60466746b96548fd5c"
+    GIT_TAG "1b2973d580b59eaea7db0f200892236eb050f0fa"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
