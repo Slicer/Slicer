9e5db9f8cca0d3851156c41fb1ee5a52393dc98b
Matthew Woehlke <matthew.woehlke@kitware.com>
2014-05-19 21:31:58 +0000

    ENH: Generate tarball of SlicerWizard doc
    Create a new build target to generate a tarball of the SlicerWizard
    documentation. This is meant to be used similar to the existing Doxygen
    tarball, to be scp'd or otherwise transferred from a build machine to
    another machine which will serve the pages publicly.
    
    Issue #3601
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23209
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23209 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Utilities/Scripts/SlicerWizard/doc/create_doc_tarball.cmake
@@ -0,0 +1,16 @@
+file(GLOB files RELATIVE ${INPUT_PATH} "${INPUT_PATH}/*")
+file(GLOB excluded_files RELATIVE ${INPUT_PATH} "${INPUT_PATH}/.*")
+
+foreach(excluded_file ${excluded_files})
+  list(REMOVE_ITEM files ${excluded_file})
+endforeach()
+
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E tar cfz ${OUTPUT_FILE} ${files}
+  WORKING_DIRECTORY ${INPUT_PATH}
+  RESULT_VARIABLE result
+  ERROR_VARIABLE err
+  )
+if(result)
+  message(FATAL_ERROR "Failed to create tarball: ${result}\n${err}")
+endif()
