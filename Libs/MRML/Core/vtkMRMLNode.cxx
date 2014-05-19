813382b8268139dc371e1b1711579197caa5e468
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-05-19 03:28:31 +0000

    COMP: Fix unused variable in vtkMRMLNode::PrintSelf
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23187
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23187 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/Core/vtkMRMLNode.cxx
+++ Libs/MRML/Core/vtkMRMLNode.cxx
@@ -301,7 +301,6 @@ void vtkMRMLNode::PrintSelf(ostream& os, vtkIndent indent)
 
   //print node references
   NodeReferencesType::iterator it;
-  std::map< std::string, std::string>::iterator itName;
   for (it = this->NodeReferences.begin(); it != this->NodeReferences.end(); it++)
     {
     const std::string& referenceRole = it->first;
