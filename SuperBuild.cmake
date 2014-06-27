a7ed6a9d02752d7d02fd8f19dc7f103aa5eb4c63
Bradley Lowekamp <blowekamp@mail.nih.gov>
2014-06-27 14:03:29 +0000

    ENH: Update SimpleFilters
    This commit updates both SimpleFilters documentation strings
    presented in the help section, and json description used to described the
    filters.
    
    b27cdf6 Remove statistics only filters
    148d863 Update help and acknowledgment text
    bcdabde Fix json syntax issue StatisticsImageFilter
    c62f9a8 Removing Filters which produce labelmaps as outputs
    77fb3ac SimpleITK cd897256 Filter descriptions (reduced)
    85d1ab2 Add utility script to generate Wiki table of filter
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23388
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23388 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -204,7 +204,7 @@ list_conditional_append(Slicer_BUILD_MultiVolumeImporter Slicer_REMOTE_DEPENDENC
 
 Slicer_Remote_Add(SimpleFilters
   GIT_REPOSITORY ${git_protocol}://github.com/SimpleITK/SlicerSimpleFilters.git
-  GIT_TAG c939ce0b8e50d6903b65fdca59e369c0c05d1763
+  GIT_TAG b27cdf629257b1919a4e7d61daba86fa137f9f4d
   OPTION_NAME Slicer_BUILD_SimpleFilters
   OPTION_DEPENDS "Slicer_BUILD_QTSCRIPTEDMODULES;Slicer_USE_SimpleITK"
   LABELS REMOTE_MODULE
