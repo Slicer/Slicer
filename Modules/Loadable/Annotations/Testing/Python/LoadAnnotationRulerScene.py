dd8940a6a845683f74c4345834dca01c1acf6805
Nicole Aucoin <nicole@bwh.harvard.edu>
2014-07-03 18:18:27 +0000

    BUG: fix the path to the ruler scene file for test
    Pass the Input data directory via the CMakeLists file on the command line.
    Avoids the hard coded path that assumes a specific directory name for
    the Slicer source code check out.
    
    Issue #3758
    
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23410
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23410 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Loadable/Annotations/Testing/Python/LoadAnnotationRulerScene.py
+++ Modules/Loadable/Annotations/Testing/Python/LoadAnnotationRulerScene.py
@@ -1,7 +1,13 @@
 import os
 
-# set the url
-scenePath =  os.path.join(os.environ['SLICER_HOME'], "../../Slicer4/Modules/Loadable/Annotations/Testing/Data/Input/ruler.mrml")
+# try get the path of the ruler scene file from the arguments
+numArgs = len(sys.argv)
+if numArgs > 1:
+  scenePath = sys.argv[1]
+else:
+  # set the url as best guess from SLICER_HOME
+  scenePath =  os.path.join(os.environ['SLICER_HOME'], "../../Slicer4/Modules/Loadable/Annotations/Testing/Data/Input/ruler.mrml")
+
 scenePath = os.path.normpath(scenePath)
 print "Trying to load ruler mrml file", scenePath
 slicer.mrmlScene.SetURL(scenePath)
