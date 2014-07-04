7c20e18d174794ef052983989df3e5b95118c0d1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-07-04 06:36:09 +0000

    COMP: Fix BRAINSTools configure error related to missing ITKVtkGlue
    Commit Slicer/BRAINSTools@94d53a64:
    
    //---------------
    COMP: Conditionally specify ITK component "ITKVtkGlue" to fix Slicer build.
    
    This commit conditionally append "ITKVtkGlue" to the list of components
    introduced in commit 2c751a5. Considering that (1) Slicer does NOT build
    the module BRAINSConstellationDetector and ConvertBetweenFileFormats
    depending on "ITKVtkGlue" and (2) Slicer does NOT build ITK with
    module ITKVtkGlue enabled, this commit will ensure BRAINSTools can
    be built within Slicer.
    
    Ideally, every module should specify the list of ITK components it expects.
    
    To find out which module depended on "ITKVtkGlue", assuming the source of
    ITKv4 are in /path/to/src/ITKv5, the following one-liner has been used:
    
    cd /path/to/src/BRAINSTools
    
    for i in `ls -d */`; do \
      echo Directory [$i]; \
      ../ITKv4/Utilities/Maintenance/WhatModulesITK.py \
        ../ITKv4 $(find $i -type f) | ack -i ITKVtkGlue; \
    done
    //---------------
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23416
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23416 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -247,7 +247,7 @@ set(BRAINSTools_options
   )
 Slicer_Remote_Add(BRAINSTools
   GIT_REPOSITORY "${git_protocol}://github.com/Slicer/BRAINSTools.git"
-  GIT_TAG "ce609cb13d25191a7995e71a56d56c11c439ac53" # Version f82a01b with Slicer patches
+  GIT_TAG "94d53a640b398c6b4e50335082bfa34eb8adf35b" # Version f82a01b with Slicer patches
   OPTION_NAME Slicer_BUILD_BRAINSTOOLS
   OPTION_DEPENDS "Slicer_BUILD_CLI_SUPPORT;Slicer_BUILD_CLI"
   LABELS REMOTE_MODULE
