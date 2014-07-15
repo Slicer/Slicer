9591e524f61bcaa6586350a9676d6535cae98764
Steve Pieper <pieper@bwh.harvard.edu>
2014-07-15 15:21:33 +0000

    BUG: fix type of argument to SetGridExtent for LandmarkRegistration
    Exposed by vtk v5 vs v6 testing.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23434
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23434 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -283,7 +283,7 @@ list_conditional_append(Slicer_BUILD_CompareVolumes Slicer_REMOTE_DEPENDENCIES C
 
 Slicer_Remote_Add(LandmarkRegistration
   GIT_REPOSITORY "${git_protocol}://github.com/pieper/LandmarkRegistration"
-  GIT_TAG "002ba088d828bf2c0b0fb59c45aeafc5dbefd50c"
+  GIT_TAG "e3e1783f00182b0d3d872f375a56c82c320cecd5"
   OPTION_NAME Slicer_BUILD_LandmarkRegistration
   OPTION_DEPENDS "Slicer_BUILD_CompareVolumes;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
