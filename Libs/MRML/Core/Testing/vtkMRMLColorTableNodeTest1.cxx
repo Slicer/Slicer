d98bc94693f1095b95487af7178675060e3098ed
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-07-17 22:54:10 +0000

    STYLE: Re-organize and simplify vtkMRMLColorTableNodeTest1
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23445
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23445 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/Core/Testing/vtkMRMLColorTableNodeTest1.cxx
+++ Libs/MRML/Core/Testing/vtkMRMLColorTableNodeTest1.cxx
@@ -51,69 +51,89 @@ int vtkMRMLColorTableNodeTest1(int argc, char * argv[] )
 
   EXERCISE_BASIC_TRANSFORMABLE_MRML_METHODS(vtkMRMLColorTableNodeTestHelper1, node1.GetPointer());
 
+  // set up the scene
+  const char *sceneFileName = "/tmp/colorTickTest.mrml";
+  if (argc > 1)
+    {
+    sceneFileName = argv[1];
+    }
+
   // check that extra single quotes don't appear in color names via round trip
   // to xml
-  std::cout << "\nTesting for tick marks around color names saved to mrml and then loaded (argc = " << argc << ")" << std::endl;
+
   vtkNew<vtkMRMLColorTableNode> colorNode;
   colorNode->SetTypeToUser();
   colorNode->SetNumberOfColors(3);
   colorNode->SetColor(0, "zero", 0.0, 0.0, 0.0, 1.0);
   colorNode->SetColor(1, "one", 1.0, 0.0, 0.0, 1.0);
   colorNode->SetColor(2, "two", 0.0, 1.0, 0.0, 1.0);
-  // set up the scene
-  vtkMRMLScene *scene = vtkMRMLScene::New();
-  scene->AddNode(colorNode.GetPointer());
-  const char *id = colorNode->GetID();
-  std::cout << "Added color node to scene, id = " << id << std::endl;
-  // write it out
-  const char *filename = "/tmp/colorTickTest.mrml";
-  if (argc > 1)
-    {
-    filename = argv[1];
-    }
-  std::cout << "Using mrml file name of " << filename << std::endl;
-  scene->SetURL(filename);
-  scene->Commit();
-  scene->Delete();
-  // read and parse
-  vtkMRMLParser* parser = vtkMRMLParser::New();
-  vtkMRMLScene *loadedScene = vtkMRMLScene::New();
-  parser->SetMRMLScene(loadedScene);
-  parser->SetFileName(filename);
-  int result = parser->Parse();
-  parser->Delete();
-  if (result != 1)
-    {
-    std::cerr << "Failed to parse file " << filename << std::endl;
-    loadedScene->Delete();
-    return EXIT_FAILURE;
-    }
-  // test the color node
-  vtkMRMLNode *mrmlNode = loadedScene->GetNodeByID(id);
-  if (mrmlNode)
-    {
-    vtkMRMLColorTableNode *readNode = vtkMRMLColorTableNode::SafeDownCast(mrmlNode);
-    if (readNode)
+
+  std::string colorNodeId;
+
+  {
+    // add node to the scene
+    vtkNew<vtkMRMLScene> scene;
+    if (!scene->AddNode(colorNode.GetPointer()))
       {
-      const char *zero = readNode->GetColorName(0);
-      const char *one = readNode->GetColorName(1);
-      const char *two = readNode->GetColorName(2);
-      std::cout << "\nColor names:\n\tzero = " << zero << "\n\tone = " << one << "\n\ttwo = " << two << std::endl;
-      const char *tickPtr0 = strstr(zero, "'");
-      const char *tickPtr1 = strstr(one, "'");
-      const char *tickPtr2 = strstr(two, "'");
-      if (tickPtr0 != NULL ||
-          tickPtr1 != NULL ||
-          tickPtr2 != NULL)
-        {
-        std::cerr << "ERROR: Tick marks are left in color names read from MRML." << std::endl;
-        loadedScene->Delete();
-        return EXIT_FAILURE;
-        }
+      std::cerr << "Problem adding colorNode to the scene !" << std::endl;
+      return EXIT_FAILURE;
+      }
+
+    // keep track of the id
+    colorNodeId = std::string(colorNode->GetID());
+
+    // write MRML file
+    scene->SetURL(sceneFileName);
+    if (!scene->Commit())
+      {
+      std::cerr << "Failed to save color node [id:" << colorNode->GetID() << "]"
+                << " into scene " << sceneFileName
+                << std::endl;
+      return EXIT_FAILURE;
+      }
+  }
+
+  {
+    vtkNew<vtkMRMLScene> scene;
+    vtkNew<vtkMRMLParser> parser;
+    parser->SetMRMLScene(scene.GetPointer());
+    parser->SetFileName(sceneFileName);
+    int result = parser->Parse();
+    if (result != 1)
+      {
+      std::cerr << "Failed to parse scene file " << sceneFileName << std::endl;
+      return EXIT_FAILURE;
+      }
+
+    // test the color node
+    vtkMRMLColorTableNode *colorNode =
+        vtkMRMLColorTableNode::SafeDownCast(scene->GetNodeByID(colorNodeId.c_str()));
+    if (!colorNode)
+      {
+      std::cerr << "Failed to get colorNode [id: " << colorNodeId << "]"
+                << " from scene file " << sceneFileName
+                << std::endl;
+      return EXIT_FAILURE;
+      }
+
+    const char *zero = colorNode->GetColorName(0);
+    const char *one = colorNode->GetColorName(1);
+    const char *two = colorNode->GetColorName(2);
+    std::cout << "\nColor names:\n"
+                 "\tzero = " << zero << "\n"
+                 "\tone = " << one << "\n"
+                 "\ttwo = " << two
+              << std::endl;
+    const char *tickPtr0 = strstr(zero, "'");
+    const char *tickPtr1 = strstr(one, "'");
+    const char *tickPtr2 = strstr(two, "'");
+    if (tickPtr0 != NULL ||
+        tickPtr1 != NULL ||
+        tickPtr2 != NULL)
+      {
+      std::cerr << "ERROR: Tick marks are left in color names read from MRML." << std::endl;
+      return EXIT_FAILURE;
       }
     }
-  // clean up
-  loadedScene->Delete();
-
   return EXIT_SUCCESS;
 }
