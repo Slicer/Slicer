11fdca0ce434a3d8d8c9ef4f2ea129df344bd974
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-07-17 22:54:17 +0000

    ENH: Update vtkMRMLColorTableNodeTest1 to test against expected output
    This commit updates the test to check that color table entry
    can be written and read from a color table file.
    
    Some background into ...
    
    Commit r20729 attempted to implement a test checking that the color table
    entry could be written to a scene and read from the scene. The issue was
    that the test was only displaying value and was not testing against
    expected value.
    Then, in commit r22344, the color mrml nodes have been updated to
    to not write anymore into the mrml scene.
    
    The test wasn't reporting that no color could be read but was still
    passing, this commit updates the test and check for expected values instead
    of just displaying them.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23448
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23448 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/Core/Testing/vtkMRMLColorTableNodeTest1.cxx
+++ Libs/MRML/Core/Testing/vtkMRMLColorTableNodeTest1.cxx
@@ -12,78 +12,104 @@
 
 // MRML includes
 #include "vtkMRMLColorTableNode.h"
+#include "vtkMRMLColorTableStorageNode.h"
 #include "vtkMRMLCoreTestingMacros.h"
 #include "vtkMRMLParser.h"
 #include "vtkMRMLScene.h"
 
 // VTK includes
-#include <vtkObjectFactory.h>
+#include <vtkNew.h>
+#include <vtkSmartPointer.h>
+
+namespace
+{
+
+//----------------------------------------------------------------------------
+bool CheckString(int line, const std::string& function, const char* current, const char* expected)
+{
+  bool different = true;
+  if (current == 0 || expected == 0)
+    {
+    different = !(current == 0 && expected == 0);
+    }
+  else if(strcmp(current, expected) == 0)
+    {
+    different = false;
+    }
+  if(different)
+    {
+    std::cerr << "Line " << line << " - " << function << " : CheckString failed"
+              << "\n\tcurrent:" << (current ? current : "<null>")
+              << "\n\texpected:" << (expected ? expected : "<null>")
+              << std::endl;
+    return false;
+    }
+  return true;
+}
+
+}
 
 //---------------------------------------------------------------------------
-class vtkMRMLColorTableNodeTestHelper1 : public vtkMRMLColorTableNode
+int vtkMRMLColorTableNodeTest1(int argc, char * argv[])
 {
-public:
-  // Provide a concrete New.
-  static vtkMRMLColorTableNodeTestHelper1 *New();
-
-  vtkTypeMacro(vtkMRMLColorTableNodeTestHelper1,vtkMRMLColorTableNode);
-
-  virtual vtkMRMLNode* CreateNodeInstance()
-    {
-    return vtkMRMLColorTableNodeTestHelper1::New();
-    }
-  virtual const char* GetNodeTagName()
-    {
-    return "vtkMRMLColorTableNodeTestHelper1";
-    }
-
-  virtual int ReadData(vtkMRMLNode *vtkNotUsed(refNode)) { return 0; }
-  virtual int WriteData(vtkMRMLNode *vtkNotUsed(refNode)) { return 0; }
-};
-vtkStandardNewMacro(vtkMRMLColorTableNodeTestHelper1);
-
-//---------------------------------------------------------------------------
-int vtkMRMLColorTableNodeTest1(int argc, char * argv[] )
-{
-  vtkNew<vtkMRMLColorTableNodeTestHelper1> node1;
+  vtkNew<vtkMRMLColorTableNode> node1;
 
   EXERCISE_BASIC_OBJECT_METHODS(node1.GetPointer());
 
-  EXERCISE_BASIC_TRANSFORMABLE_MRML_METHODS(vtkMRMLColorTableNodeTestHelper1, node1.GetPointer());
+  EXERCISE_BASIC_TRANSFORMABLE_MRML_METHODS(vtkMRMLColorTableNode, node1.GetPointer());
 
-  // set up the scene
-  const char *sceneFileName = "/tmp/colorTickTest.mrml";
-  if (argc > 1)
+  if (argc != 2)
     {
-    sceneFileName = argv[1];
+    std::cerr << "Missing parameters !\n"
+              << "Usage: " << argv[0] << " /path/to/temp"
+              << std::endl;
+    return EXIT_FAILURE;
     }
+
+  const char* tempDir = argv[1];
+
+  std::string sceneFileName = std::string(tempDir) + "/vtkMRMLColorTableNodeTest1.mrml";
+  std::string colorTableFileName = std::string(tempDir) + "/vtkMRMLColorTableNodeTest1.ctbl";
 
   // check that extra single quotes don't appear in color names via round trip
   // to xml
 
-  vtkNew<vtkMRMLColorTableNode> colorNode;
-  colorNode->SetTypeToUser();
-  colorNode->SetNumberOfColors(3);
-  colorNode->SetColor(0, "zero", 0.0, 0.0, 0.0, 1.0);
-  colorNode->SetColor(1, "one", 1.0, 0.0, 0.0, 1.0);
-  colorNode->SetColor(2, "two", 0.0, 1.0, 0.0, 1.0);
-
-  std::string colorNodeId;
+  std::string expectedColorNodeId;
 
   {
+    vtkNew<vtkMRMLColorTableNode> colorNode;
+    colorNode->SetTypeToUser();
+    colorNode->SetNumberOfColors(3);
+    colorNode->SetColor(0, "zero", 0.0, 0.0, 0.0, 1.0);
+    colorNode->SetColor(1, "one", 1.0, 0.0, 0.0, 1.0);
+    colorNode->SetColor(2, "two", 0.0, 1.0, 0.0, 1.0);
+
+    vtkSmartPointer<vtkMRMLStorageNode> colorStorageNode =
+        vtkSmartPointer<vtkMRMLStorageNode>::Take(colorNode->CreateDefaultStorageNode());
+
     // add node to the scene
     vtkNew<vtkMRMLScene> scene;
+    scene->SetRootDirectory(tempDir);
     if (!scene->AddNode(colorNode.GetPointer()))
       {
       std::cerr << "Problem adding colorNode to the scene !" << std::endl;
       return EXIT_FAILURE;
       }
 
+    // add storage node to the scene
+    scene->AddNode(colorStorageNode);
+    colorNode->SetAndObserveStorageNodeID(colorStorageNode->GetID());
+
     // keep track of the id
-    colorNodeId = std::string(colorNode->GetID());
+    expectedColorNodeId = std::string(colorNode->GetID());
+
+    // write color table file
+    colorStorageNode->SetFileName(colorTableFileName.c_str());
+    colorStorageNode->WriteData(colorNode.GetPointer());
+    colorNode->SetName("CustomColorTable");
 
     // write MRML file
-    scene->SetURL(sceneFileName);
+    scene->SetURL(sceneFileName.c_str());
     if (!scene->Commit())
       {
       std::cerr << "Failed to save color node [id:" << colorNode->GetID() << "]"
@@ -95,9 +121,10 @@ int vtkMRMLColorTableNodeTest1(int argc, char * argv[] )
 
   {
     vtkNew<vtkMRMLScene> scene;
+    scene->SetRootDirectory(tempDir);
     vtkNew<vtkMRMLParser> parser;
     parser->SetMRMLScene(scene.GetPointer());
-    parser->SetFileName(sceneFileName);
+    parser->SetFileName(sceneFileName.c_str());
     int result = parser->Parse();
     if (result != 1)
       {
@@ -107,33 +134,34 @@ int vtkMRMLColorTableNodeTest1(int argc, char * argv[] )
 
     // test the color node
     vtkMRMLColorTableNode *colorNode =
-        vtkMRMLColorTableNode::SafeDownCast(scene->GetNodeByID(colorNodeId.c_str()));
+        vtkMRMLColorTableNode::SafeDownCast(scene->GetNodeByID(expectedColorNodeId.c_str()));
     if (!colorNode)
       {
-      std::cerr << "Failed to get colorNode [id: " << colorNodeId << "]"
+      std::cerr << "Failed to get colorNode [id: " << expectedColorNodeId << "]"
                 << " from scene file " << sceneFileName
                 << std::endl;
       return EXIT_FAILURE;
       }
 
-    const char *zero = colorNode->GetColorName(0);
-    const char *one = colorNode->GetColorName(1);
-    const char *two = colorNode->GetColorName(2);
-    std::cout << "\nColor names:\n"
-                 "\tzero = " << zero << "\n"
-                 "\tone = " << one << "\n"
-                 "\ttwo = " << two
-              << std::endl;
-    const char *tickPtr0 = strstr(zero, "'");
-    const char *tickPtr1 = strstr(one, "'");
-    const char *tickPtr2 = strstr(two, "'");
-    if (tickPtr0 != NULL ||
-        tickPtr1 != NULL ||
-        tickPtr2 != NULL)
+    if (!colorNode->GetStorageNode()->ReadData(colorNode))
       {
-      std::cerr << "ERROR: Tick marks are left in color names read from MRML." << std::endl;
+      std::cerr << "Failed to read " << colorTableFileName << std::endl;
       return EXIT_FAILURE;
       }
-    }
+
+    if (!CheckString(__LINE__, "GetColorName", colorNode->GetColorName(0), "zero"))
+      {
+      return EXIT_FAILURE;
+      }
+    if (!CheckString(__LINE__, "GetColorName", colorNode->GetColorName(1), "one"))
+      {
+      return EXIT_FAILURE;
+      }
+    if (!CheckString(__LINE__, "GetColorName", colorNode->GetColorName(2), "two"))
+      {
+      return EXIT_FAILURE;
+      }
+  }
+
   return EXIT_SUCCESS;
 }
