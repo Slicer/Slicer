149ec64068c4399578603f4546a6ecea75f1e8fa
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-07-18 07:41:22 +0000

    BUG: Fix memory initialization in vtkMRMLScalarVolumeNodeTest2
    This commits fixes a problem discovered while running the test
     vtkMRMLScalarVolumeNodeTest2 through valgrind. It ensure the image
    data is initialized with zeros.
    
    [...]
    
    ==32668== Conditional jump or move depends on uninitialised value(s)
    ==32668==    at 0x1BC3DE03: void vtkImageAccumulateExecute<unsigned short>(vtkImageAccumulate*, vtkImageData*, unsigned short*, vtkImageData*, int*, double*, double*, double*, double*, long long*, int*) (vtkImageAccumulate.cxx:197)
    ==32668==    by 0x1BC34531: vtkImageAccumulate::RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*) (vtkImageAccumulate.cxx:313)
    ==32668==    by 0x16C94CB7: vtkImageAlgorithm::ProcessRequest(vtkInformation*, vtkInformationVector**, vtkInformationVector*) (vtkImageAlgorithm.cxx:103)
    ==32668==    by 0x16C8A6AD: vtkExecutive::CallAlgorithm(vtkInformation*, int, vtkInformationVector**, vtkInformationVector*) (vtkExecutive.cxx:745)
    ==32668==    by 0x16C85002: vtkDemandDrivenPipeline::ExecuteData(vtkInformation*, vtkInformationVector**, vtkInformationVector*) (vtkDemandDrivenPipeline.cxx:497)
    ==32668==    by 0x16C7E07E: vtkCompositeDataPipeline::ExecuteData(vtkInformation*, vtkInformationVector**, vtkInformationVector*) (vtkCompositeDataPipeline.cxx:175)
    ==32668==    by 0x16C8476E: vtkDemandDrivenPipeline::ProcessRequest(vtkInformation*, vtkInformationVector**, vtkInformationVector*) (vtkDemandDrivenPipeline.cxx:279)
    ==32668==    by 0x16CA9B25: vtkStreamingDemandDrivenPipeline::ProcessRequest(vtkInformation*, vtkInformationVector**, vtkInformationVector*) (vtkStreamingDemandDrivenPipeline.cxx:346)
    ==32668==    by 0x16C801BA: vtkCompositeDataPipeline::ForwardUpstream(vtkInformation*) (vtkCompositeDataPipeline.cxx:712)
    ==32668==    by 0x16C846DD: vtkDemandDrivenPipeline::ProcessRequest(vtkInformation*, vtkInformationVector**, vtkInformationVector*) (vtkDemandDrivenPipeline.cxx:265)
    ==32668==    by 0x16CA9B25: vtkStreamingDemandDrivenPipeline::ProcessRequest(vtkInformation*, vtkInformationVector**, vtkInformationVector*) (vtkStreamingDemandDrivenPipeline.cxx:346)
    ==32668==    by 0x16C84E5B: vtkDemandDrivenPipeline::UpdateData(int) (vtkDemandDrivenPipeline.cxx:448)
    
    [...]
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23459
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23459 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/Core/Testing/vtkMRMLScalarVolumeNodeTest2.cxx
+++ Libs/MRML/Core/Testing/vtkMRMLScalarVolumeNodeTest2.cxx
@@ -41,6 +41,20 @@ int vtkMRMLScalarVolumeNodeTest2(int , char * [] )
   imageData->AllocateScalars(VTK_UNSIGNED_SHORT, 1); // allocate storage for image data
 #endif
 
+  // Fill every entry of the image data with "0"
+  int* dims = imageData->GetDimensions();
+  for (int z = 0; z < dims[2]; z++)
+    {
+    for (int y = 0; y < dims[1]; y++)
+      {
+      for (int x = 0; x < dims[0]; x++)
+        {
+        vtkTypeUInt16* pixel = static_cast<vtkTypeUInt16*>(imageData->GetScalarPointer(x,y,z));
+        pixel[0] = 0;
+        }
+      }
+    }
+
   vtkNew<vtkMRMLScene> scene;
 
   vtkNew<vtkMRMLScalarVolumeNode> volumeNode;
