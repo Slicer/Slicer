7ede1032fe7b6a172127919d5c331a4c6951dcb5
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-07-21 03:36:54 +0000

    COMP: Update SlicerExecutionModel to fix issue with find_package(ITK)
    Associated issues:
      https://issues.itk.org/jira/browse/ITK-3294
      https://github.com/Slicer/SlicerExecutionModel/issues/35
    
    Thanks to "Hans Johnson <hans-johnson@uiowa.edu>" for tracking down
    and identifying the problem and thanks to "Matt McCormick <matt.mccormick@kitware.com>"
    for submitting a fix.
    
    //---------------
    $ git shortlog 4e61f6..feef59e --no-merges
    Matt McCormick (1):
          BUG: Allow projects to specify their own list of ITK COMPONENTS. #35
    //---------------
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23471
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23471 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_SlicerExecutionModel.cmake
+++ SuperBuild/External_SlicerExecutionModel.cmake
@@ -33,7 +33,7 @@ if(NOT DEFINED SlicerExecutionModel_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/Slicer/SlicerExecutionModel.git"
-    GIT_TAG "4e61f6c9a8712c150bb4c63ed9842d92a3880bc6"
+    GIT_TAG "feef59eae446e4881d7992b6a229b03731ccb8d1"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
