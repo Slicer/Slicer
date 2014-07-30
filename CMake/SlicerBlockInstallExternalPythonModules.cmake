080920a2216b88497c73b34046b379bc56799ab6
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-07-30 20:47:57 +0000

    COMP: Fix install of VTK6 python modules on MacOSX
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23520
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23520 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallExternalPythonModules.cmake
+++ CMake/SlicerBlockInstallExternalPythonModules.cmake
@@ -1,13 +1,26 @@
 
 # Install VTK python module
-list(GET VTK_PYTHONPATH_DIRS 0 VTK_PYTHON_MODULE)
+if(${VTK_VERSION_MAJOR} GREATER 5)
+  set(VTK_PYTHON_MODULE "${VTK_DIR}/Wrapping/Python")
+else()
+  list(GET VTK_PYTHONPATH_DIRS 0 VTK_PYTHON_MODULE)
+endif()
 install(DIRECTORY ${VTK_PYTHON_MODULE}/vtk
   DESTINATION ${Slicer_INSTALL_BIN_DIR}/Python
   USE_SOURCE_PERMISSIONS
   COMPONENT Runtime)
 
 # Install external python runtime libraries that we don't link to (fixupbundle won't copy them)
-file(GLOB vtk_python_modules "${VTK_DIR}/bin/*Python.so")
+if(${VTK_VERSION_MAJOR} GREATER 5)
+  if(UNIX)
+    set(vtk_python_library_subdir "lib")
+  else()
+    set(vtk_python_library_subdir "bin")
+  endif()
+else()
+  set(vtk_python_library_subdir "bin")
+endif()
+file(GLOB vtk_python_modules "${VTK_DIR}/${vtk_python_library_subdir}/*Python.so")
 install(FILES ${vtk_python_modules}
         DESTINATION ${Slicer_INSTALL_LIB_DIR}
         COMPONENT Runtime)
