198ed254d375aaec8267b324978a6d9097cdd751
Fedorov <fedorov@bwh.harvard.edu>
2014-09-02 03:09:44 +0000

    BUG: seriesNumber tag was not initialized, leading to series parsing failure
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23642
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23642 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -191,7 +191,7 @@ list_conditional_append(Slicer_BUILD_MultiVolumeExplorer Slicer_REMOTE_DEPENDENC
 
 Slicer_Remote_Add(MultiVolumeImporter
   GIT_REPOSITORY ${git_protocol}://github.com/fedorov/MultiVolumeImporter.git
-  GIT_TAG fb38cbbd000926c1849fa4e3c81abb6d3065f4a4
+  GIT_TAG 92553ae51442190e3f456d72726220f34b6f8c57
   OPTION_NAME Slicer_BUILD_MultiVolumeImporter
   OPTION_DEPENDS "Slicer_BUILD_QTLOADABLEMODULES;Slicer_BUILD_MULTIVOLUME_SUPPORT;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
