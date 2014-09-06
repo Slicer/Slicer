06c3ad4dffb8b83f5f3e0f964305ddb2c9f89adc
Dirk Padfield <padfield@ge.com>
2014-09-06 20:28:02 +0000

    ENH: Added a test for the ExtractSkeleton CLI. Fixes #3333
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23657
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23657 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/CLI/ExtractSkeleton/Testing/ExtractSkeletonTest.cxx
@@ -0,0 +1,14 @@
+#include "itkTestMain.h"
+
+#ifdef WIN32
+#define MODULE_IMPORT __declspec(dllimport)
+#else
+#define MODULE_IMPORT
+#endif
+
+extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char * []);
+
+void RegisterTests()
+{
+  StringToTestFunctionMap["ModuleEntryPoint"] = ModuleEntryPoint;
+}
