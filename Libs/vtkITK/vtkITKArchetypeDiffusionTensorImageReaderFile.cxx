Slicer/Slicer

commit b9b3300b1b263a962c78b5337b083f45c3142c9d

ENH: Add VTK 6 support 2/2
Issue #3514

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23122
git-svn-id: http://svn.slicer.org/Slicer4/trunk@23122 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/vtkITK/vtkITKArchetypeDiffusionTensorImageReaderFile.cxx
@@ -100,11 +100,25 @@ void vtkITKExecuteDataFromFileDiffusionTensor3D(
     it( reader->GetOutput(), reader->GetOutput()->GetLargestPossibleRegion() );
   tensors->SetNumberOfComponents(9);
   tensors->SetNumberOfTuples(data->GetNumberOfPoints());
-  tensors->Modified();
+  //tensors->Modified();
   for ( it.GoToBegin(); !it.IsAtEnd() ; ++it )
     {
     const itk::Index<3u> index = it.GetIndex();
+#if VTK_MAJOR_VERSION <= 5
     vtkIdType position = data->FindPoint(index[0], index[1], index[2]);
+#else
+    vtkIdType position = index[0] +
+      index[1] * data->GetDimensions()[0] +
+      index[2] * data->GetDimensions()[0] * data->GetDimensions()[1];
+#endif
+    if (position == static_cast<vtkIdType>(-1) ||
+        position >= tensors->GetNumberOfTuples())
+      {
+      std::cout << "bad position" << position << " for index " << index <<std::endl;
+      itkGenericExceptionMacro(<< "Can't find index " << index
+                               << " in output image data.");
+      continue;
+      }
     float value[9];
     const DiffusionTensor3DPixelType tensor = it.Get();
     for (int i=0; i<3; i++)
@@ -124,26 +138,36 @@ void vtkITKExecuteDataFromFileDiffusionTensor3D(
 #if (VTK_MAJOR_VERSION <= 5)
 void vtkITKArchetypeDiffusionTensorImageReaderFile::ExecuteData(vtkDataObject *output)
 #else
-void vtkITKArchetypeDiffusionTensorImageReaderFile::ExecuteDataWithInformation(
-  vtkDataObject *output, vtkInformation* outInfo)
+int vtkITKArchetypeDiffusionTensorImageReaderFile::RequestData(
+  vtkInformation *vtkNotUsed(request),
+  vtkInformationVector **vtkNotUsed(inputVector),
+  vtkInformationVector *outputVector)
 #endif
 {
   if (!this->Superclass::Archetype)
     {
     vtkErrorMacro("An Archetype must be specified.");
+#if (VTK_MAJOR_VERSION <= 5)
     return;
+#else
+    return 1;
+#endif
     }
 
+  int extent[6] = {0,-1,0,-1,0,-1};
+#if (VTK_MAJOR_VERSION <= 5)
   vtkImageData *data = vtkImageData::SafeDownCast(output);
   //data->UpdateInformation();
-  data->SetExtent(0,0,0,0,0,0);
-#if (VTK_MAJOR_VERSION <= 5)
-  data->SetExtent(data->GetWholeExtent());
-#else
-  data->SetExtent(outInfo->Get(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT()));
-#endif
+  data->GetWholeExtent(extent);
   data->SetOrigin(0, 0, 0);
   data->SetSpacing(1, 1, 1);
+#else
+  vtkImageData* data = vtkImageData::GetData(outputVector);
+  vtkInformation* outInfo = outputVector->GetInformationObject(0);
+  outInfo->Get
+    (vtkStreamingDemandDrivenPipeline::UPDATE_EXTENT(), extent);
+#endif
+  data->SetExtent(extent);
   vtkNew<vtkFloatArray> tensors;
   tensors->SetName("ArchetypeReader");
 
@@ -165,11 +189,18 @@ void vtkITKArchetypeDiffusionTensorImageReaderFile::ExecuteDataWithInformation(
     // ERROR - should have used the series reader
     vtkErrorMacro("There is more than one file, use the vtkITKArchetypeImageSeriesReader instead");
     }
+#if (VTK_MAJOR_VERSION > 5)
+  return 1;
+#endif
 }
 
-void vtkITKArchetypeDiffusionTensorImageReaderFile::ReadProgressCallback(itk::ProcessObject* obj,const itk::ProgressEvent&,void* data)
+//----------------------------------------------------------------------------
+void vtkITKArchetypeDiffusionTensorImageReaderFile
+::ReadProgressCallback(itk::ProcessObject* obj,
+                       const itk::ProgressEvent&,void* data)
 {
-  vtkITKArchetypeDiffusionTensorImageReaderFile* me=reinterpret_cast<vtkITKArchetypeDiffusionTensorImageReaderFile*>(data);
+  vtkITKArchetypeDiffusionTensorImageReaderFile* me =
+    reinterpret_cast<vtkITKArchetypeDiffusionTensorImageReaderFile*>(data);
   me->Progress=obj->GetProgress();
   me->InvokeEvent(vtkCommand::ProgressEvent,&me->Progress);
 }

commit 16a084fdffcbe2a477e37d85b27931713b1ee2be

BUG: Update diffusion tensor reader to force origin/spacing. Fixes #3845
This commit fixes a regression introduced in r23122 and forces the
origin/spacing for VTK 6 similarly to what was done for VTK 5.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23695
git-svn-id: http://svn.slicer.org/Slicer4/trunk@23695 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/vtkITK/vtkITKArchetypeDiffusionTensorImageReaderFile.cxx
@@ -152,14 +152,14 @@ int vtkITKArchetypeDiffusionTensorImageReaderFile::RequestData(
   vtkImageData *data = vtkImageData::SafeDownCast(output);
   //data->UpdateInformation();
   data->GetWholeExtent(extent);
-  data->SetOrigin(0, 0, 0);
-  data->SetSpacing(1, 1, 1);
 #else
   vtkImageData* data = vtkImageData::GetData(outputVector);
   vtkInformation* outInfo = outputVector->GetInformationObject(0);
   outInfo->Get
     (vtkStreamingDemandDrivenPipeline::UPDATE_EXTENT(), extent);
 #endif
+  data->SetOrigin(0, 0, 0);
+  data->SetSpacing(1, 1, 1);
   data->SetExtent(extent);
   vtkNew<vtkFloatArray> tensors;
   tensors->SetName("ArchetypeReader");