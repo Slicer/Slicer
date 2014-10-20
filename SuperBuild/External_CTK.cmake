0980bee8be00750608daa366cb7271a628302066
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-10-20 17:49:56 +0000

    BUG: Update CTK to fix issue with ctkMenuComboBox
    $ git lg 19c9668..68a1ce2 --no-merges
    * a2cce54 - ctkXnatResource download function implemented (11 days ago) <Tom Doel>
    * 6ab9b11 - BUG: Fixed missing list elements problem (25 hours ago) <Andras Lasso>
    * d3af228 - Replaced Qt5::Gui dependency with Qt5::Widgets. (4 days ago) <Stefan Kislinskiy>
    * b9393da - Set QT_INSTALLED_LIBRARY_DIR in case of Qt5. (7 days ago) <Stefan Kislinskiy>
    * 055c319 - Change sleep duration from 200ms to 250ms. (11 days ago) <Stefan Kislinskiy>
    * 30d745a - Add NOMINMAX definition to global compile flags. (11 days ago) <Stefan Kislinskiy>
    * 5002282 - Fixed incompatibilities between Windows.h and Qt 5 (NOMINMAX). (12 days ago) <Stefan Kislinskiy>
    * a1f5a9e - Fixed Qt4/5 MOC issues regarding QIconEngineV2. (13 days ago) <Stefan Kislinskiy>
    * 8f8208d - Add missing error log context parameter. (13 days ago) <Stefan Kislinskiy>
    * 4f9073e - ENH: Clear box icon replaced with a less harsh one, a simple 'x' that does not indicate error (3 weeks ago) <Csaba Pitner>
    * c2b9c56 - Add ctkCollapsibleButton::flat property (3 weeks ago) <Julien Finet>
    * 553ecd7 - Add documentation for ctk*PopupWidget (8 weeks ago) <Julien Finet>
    * 8e9b0a0 - Added a Signal which is used to do something before the session is closed. (8 weeks ago) <Daniel Knorr>
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23750
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23750 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "19c9668aed25dfd397d2bcfd00656502622d82c5"
+    GIT_TAG "68a1ce2b42bc9312c48bc21bd1dc5e130252dbe9"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
