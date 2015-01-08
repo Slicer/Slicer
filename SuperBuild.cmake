53e1b7366925891fb3ca85152a9ae00c70b4d62a
Fedorov <fedorov@bwh.harvard.edu>
2015-01-08 05:20:22 +0000

    ENH: update DICOMMultiVolumePlugin
    Include TR and TE attributes for FA-separated multivolume nodes, as these are necessare for T1 calculation
    
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23856
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23856 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -191,7 +191,7 @@ list_conditional_append(Slicer_BUILD_MultiVolumeExplorer Slicer_REMOTE_DEPENDENC
 
 Slicer_Remote_Add(MultiVolumeImporter
   GIT_REPOSITORY ${git_protocol}://github.com/fedorov/MultiVolumeImporter.git
-  GIT_TAG ee07a89bd48ab9ea97772de2609bda8f3ab8ee35
+  GIT_TAG 37a30b3dd2b632340b7d77fcd7af76765df959b0
   OPTION_NAME Slicer_BUILD_MultiVolumeImporter
   OPTION_DEPENDS "Slicer_BUILD_QTLOADABLEMODULES;Slicer_BUILD_MULTIVOLUME_SUPPORT;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
