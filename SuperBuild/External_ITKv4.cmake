ac1c9c2a93def9edafb1fb9a6590db321c6fd8fc
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-01-10 01:05:54 +0000

    BUG: Update ITKv4 to fix Resample DTIVolume/ScalarVectorDWIVolume tests
    This commit fixes a regression introduced by the update of ITK in r23858
    
    // ----------------
    BUG: Clear TransformIO's list of transforms in TransformFileReader.
    
    Since TransformFileReaderTemplate and TransformIO do not have very good const
    correctness and encapsulation, it is possible to manipulate their members and
    change their state in a way they might not expect.
    
    In InsightSoftwareConsortium/ITK@81faafe29, a m_TransformIO member
    was added. This is used in
    TransformFileReaderTemplate's Update() step instead of creating a new
    TransformIOBase instance on every Update.  To keep completely consistent
    with the previous behavior, we must clear the TransformIOBase's
    internal transform list at the time the class was previously instantiated.
    
    Thanks to Jean-Christophe Fillion-Robin and the Slicer community for
    identifying this regression.
    
    Change-Id: If5dfd03b32b31ba765c6781d689e91edb944be3f
    // ----------------
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23875
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23875 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -27,7 +27,7 @@ if(NOT DEFINED ITK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   endif()
 
   set(ITKv4_REPOSITORY ${git_protocol}://github.com/Slicer/ITK.git)
-  set(ITKv4_GIT_TAG 9a02d0eaa894637be7b684e6161f115d0d5b94d9) # slicer-v4.7.0
+  set(ITKv4_GIT_TAG 0f6c2b3e00bdb4fdd590c428c19c2ed379e9a71d) # slicer-v4.7.0
 
   set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
 
