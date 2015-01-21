3aae8031f195958973b2d08bbb60f3830ac03cdb
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-01-21 15:56:15 +0000

    COMP: Fix typo in SLICER_ADD_PYTHON_TEST introduced in r23882
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23901
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23901 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerMacroPythonTesting.cmake
+++ CMake/SlicerMacroPythonTesting.cmake
@@ -26,7 +26,7 @@ macro(SLICER_ADD_PYTHON_TEST)
   cmake_parse_arguments(MY "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
   get_filename_component(test_name ${MY_SCRIPT} NAME_WE)
   if(NOT IS_ABSOLUTE ${MY_SCRIPT})
-    set(${MY_SCRIPT} "${CMAKE_CURRENT_SOURCE_DIR}/${MY_SCRIPT}")
+    set(MY_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/${MY_SCRIPT}")
   endif()
   ExternalData_add_test(${Slicer_ExternalData_DATA_MANAGEMENT_TARGET}
     NAME py_${MY_TESTNAME_PREFIX}${test_name}
