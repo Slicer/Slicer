e8ff8a9bb799071f3ce6c05c5140d112c9072757
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-01-21 07:56:30 +0000

    COMP: Update DCMTK to fix Syntax warning
    This commit fixes a warning occurring when using CMake >= 2.8.12
    
    //--------------
    Author: Joerg Riesmeier <dicom@jriesmeier.com>
    Date:   Wed Jan 21 02:49:54 2015 -0500
    
        Fixed small issue reported by CMake 2.8.12.
    
        A trailing "/" had to be removed in order to keep CMake 2.8.12 quiet.
    //--------------
    
    
    This commit fixes the following warning:
    
    CMake Warning (dev) at CMake/dcmtkPrepare.cmake:317 (INCLUDE):
      Syntax Warning in cmake code at
    
        /path/to/Slicer-0-build/DCMTK/CMake/3rdparty.cmake:9:70
    
      Argument not separated from preceding token by whitespace.
    Call Stack (most recent call first):
      CMakeLists.txt:8 (INCLUDE)
    This warning is for project developers.  Use -Wno-dev to suppress it.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23897
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23897 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_DCMTK.cmake
+++ SuperBuild/External_DCMTK.cmake
@@ -34,7 +34,7 @@ if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   endif()
 
   set(${proj}_REPOSITORY ${git_protocol}://github.com/commontk/DCMTK.git)
-  set(${proj}_GIT_TAG "b68bdd8e329dd41fc17ab60539793aa31ec69162")
+  set(${proj}_GIT_TAG "de4f9bae3150d4a1209d235bb5b1306309343043")
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
