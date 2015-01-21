3cef5dba8fc640b09ed1312987153e825f856b85
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-01-21 08:12:35 +0000

    COMP: Update CTK to fix CMP0054 warning
    This commit fixes warnings occurring when using CMake >= 3.1.0
    
    //------------
    Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    Date:   Wed Jan 21 03:09:53 2015 -0500
    
        Set CMake policy CMP0054 to shut-up associated warning
    
        This commit will avoid the following warning:
    
        CMake Warning (dev) at /path/to/cmake-3.1/Modules/ExternalProject.cmake:919 (if):
          Policy CMP0054 is not set: Only interpret if() arguments as variables or
          keywords when unquoted.  Run "cmake --help-policy CMP0054" for policy
          details.  Use the cmake_policy command to set the policy and suppress this
          warning.
    
          Quoted variables like "PYTHONQT_INSTALL_DIR" will no longer be dereferenced
          when the policy is set to NEW.  Since the policy is not set the OLD
          behavior will be used.
        Call Stack (most recent call first):
          /path/to/cmake-3.1/Modules/ExternalProject.cmake:1764 (_ep_write_initial_cache)
          /path/to/cmake-3.1/Modules/ExternalProject.cmake:1997 (_ep_add_configure_command)
          SuperBuild.cmake:37 (ExternalProject_Add)
          CMakeLists.txt:977 (include)
        This warning is for project developers.  Use -Wno-dev to suppress it.
    //------------
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23898
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23898 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "c37e26a1b54a3272bf28a0354b7dec290d5135ac"
+    GIT_TAG "cdcd74621b4ba028ed4bbc841b6ad361130f750b"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
