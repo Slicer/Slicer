Slicer/Slicer

commit b55525344bcd255940d9da4b623555284e891bdc

ENH: Use Python logging framework for scripted module logging
Use Python's built-in logging module to manage logging. A direct advantage is that line number, file name, method name can be logged, too. Also, it follows the convention of using qt-style logging in qt classes, vtk-style logging in vtk classes, etc.

Default behavior of log levels:
 DEBUG: log to application log only, includes file name, line number, thread
 INFO: log to stdout (appears in application log, too)
 WARNING: log to stderr (appears in application log, too)
 ERROR: log to stderr (appears in application log, too)

Usage:
 import logging

 [...]

 logging.debug("this is a debug message")
 logging.info("this is an info message")
 logging.warning("this is a warning message")
 logging.error("this is a error message")

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23828
git-svn-id: http://svn.slicer.org/Slicer4/trunk@23828 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Python/slicer/__init__.py.in
@@ -1,6 +1,92 @@
-""" This module loads the Slicer library modules into its namespace."""
+""" This module sets up root logging and loads the Slicer library modules into its namespace."""
 
 import imp
+import logging
+import sys
+from __main__ import ctk, qt
+
+#-----------------------------------------------------------------------------
+class _LogReverseLevelFilter(logging.Filter):
+  """
+  Rejects log records that are at or above the specified level
+  """
+  def __init__(self, levelLimit):
+    self._levelLimit = levelLimit
+  def filter(self, record):
+    return record.levelno < self._levelLimit
+
+#-----------------------------------------------------------------------------
+class SlicerApplicationLogHandler(logging.Handler):
+  """
+  Writes logging records to Slicer application log.
+  """
+  def __init__(self):
+    logging.Handler.__init__(self)
+    self.pythonToCtkLevelConverter = {
+      logging.DEBUG : ctk.ctkErrorLogLevel.Debug,
+      logging.INFO : ctk.ctkErrorLogLevel.Info,
+      logging.WARNING : ctk.ctkErrorLogLevel.Warning,
+      logging.ERROR : ctk.ctkErrorLogLevel.Error }
+    self.origin = "Python"
+    self.category = "Python"
+  def emit(self, record):
+    try:
+      context = ctk.ctkErrorLogContext()
+      context.setCategory(self.category)
+      context.setLine(record.lineno)
+      context.setFile(record.pathname)
+      context.setFunction(record.funcName)
+      context.setMessage(record.msg)
+      threadId = "{0}({1})".format(record.threadName, record.thread)
+      app.errorLogModel().addEntry(qt.QDateTime.currentDateTime(), threadId,
+        self.pythonToCtkLevelConverter[record.levelno], self.origin, context, record.msg)
+    except:
+      self.handleError(record)
+
+#-----------------------------------------------------------------------------
+def initLogging(logger):
+  """
+  Initialize logging by creating log handlers and setting default log level.
+  """
+
+  # Prints debug messages to Slicer application log.
+  # Only debug level messages are logged this way, as higher level messages are printed on console
+  # and all console outputs are sent automatically to the application log anyway.
+  applicationLogHandler = SlicerApplicationLogHandler()
+  applicationLogHandler.setLevel(logging.DEBUG)
+  # Filter messages at INFO level or above (they will be printed on the console)
+  applicationLogHandler.addFilter(_LogReverseLevelFilter(logging.INFO))
+  applicationLogHandler.setFormatter(logging.Formatter('%(levelname)s: %(message)s (%(pathname)s:%(lineno)d)'))
+  logger.addHandler(applicationLogHandler)
+
+  # Prints info message to stdout (anything on stdout will also show up in the application log)
+  consoleInfoHandler = logging.StreamHandler(sys.stdout)
+  consoleInfoHandler.setLevel(logging.INFO)
+  # Filter messages at WARNING level or above (they will be printed on stderr)
+  consoleInfoHandler.addFilter(_LogReverseLevelFilter(logging.WARNING))
+  logger.addHandler(consoleInfoHandler)
+
+  # Prints error and warning messages to stderr (anything on stderr will also show it in the application log)
+  consoleErrorHandler = logging.StreamHandler(sys.stderr)
+  consoleErrorHandler.setLevel(logging.WARNING)
+  logger.addHandler(consoleErrorHandler)
+
+  # Log debug messages from scripts by default, as they are useful for troubleshooting with users
+  logger.setLevel(logging.DEBUG)
+
+#-----------------------------------------------------------------------------
+# Set up the root logger
+#
+# We initialize the root logger because if somebody just called logging.debug(),
+# logging.info(), etc. then it would create a default root logger with defaut settings
+# that do not work nicely in Slicer (prints everything in console, messages are
+# not sent to the application log, etc).
+#
+initLogging(logging.getLogger())
+
+
+#-----------------------------------------------------------------------------
+# Load modules
 
 # Add module 'slicer.modules'
 __modules = imp.new_module( 'modules' )

Base/Python/CMakeLists.txt
@@ -1,6 +1,5 @@
 
 set(Slicer_PYTHON_SCRIPTS
-  slicer/log
   slicer/logic
   slicer/ScriptedLoadableModule
   slicer/slicerqt

Base/Python/slicer/ScriptedLoadableModule.py
@@ -1,7 +1,7 @@
 import os, string
 import unittest
 from __main__ import qt, ctk, slicer
-import slicer.log
+import logging
 
 class ScriptedLoadableModule:
   def __init__(self, parent):
@@ -11,17 +11,15 @@ class ScriptedLoadableModule:
     parent.title = ""
     parent.categories = []
     parent.dependencies = []
-    parent.contributors = []
+    parent.contributors = ["Andras Lasso (PerkLab, Queen's University), Steve Pieper (Isomics)"]
 
     parent.helpText = string.Template("""
 This module was created from a template and the help section has not yet been updated.
-
 Please refer to <a href=\"$a/Documentation/$b.$c/Modules/ScriptedLoadableModule\">the documentation</a>.
-
     """).substitute({ 'a':parent.slicerWikiUrl, 'b':slicer.app.majorVersion, 'c':slicer.app.minorVersion })
 
     parent.acknowledgementText = """
-This work is supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community. See <a>http://www.slicer.org</a> for details.  Module implemented by Steve Pieper.
+This work is supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community. See <a>http://www.slicer.org</a> for details.
 This work is partially supported by PAR-07-249: R01CA131718 NA-MIC Virtual Colonoscopy (See <a href=http://www.slicer.org>http://www.na-mic.org/Wiki/index.php/NA-MIC_NCBC_Collaboration:NA-MIC_virtual_colonoscopy</a>).
     """
 
@@ -102,8 +100,9 @@ class ScriptedLoadableModuleWidget:
     pass
 
   def onReload(self):
-    """Generic reload method for any scripted module.
-    ModuleWizard will subsitute correct default moduleName.
+    """
+    ModuleWizard will substitute correct default moduleName.
+    Generic reload method for any scripted module.
     """
     globals()[self.moduleName] = slicer.util.reloadScriptedModule(self.moduleName)
 
@@ -117,7 +116,7 @@ class ScriptedLoadableModuleWidget:
       import traceback
       traceback.print_exc()
       errorMessage = "Reload and Test: Exception!\n\n" + str(e) + "\n\nSee Python Console for Stack Trace"
-      slicer.log.error(errorMessage, showMessageBox=True)
+      slicer.util.errorDisplay(errorMessage)
 
 class ScriptedLoadableModuleLogic():
 
@@ -134,7 +133,8 @@ class ScriptedLoadableModuleLogic():
     self.isSingletonParameterNode = True
 
   def getParameterNode(self):
-    """return the first available parameter node for this module
+    """
+    Return the first available parameter node for this module
     If no parameter nodes are available for this module then a new one is created.
     """
     numberOfScriptedModuleNodes =  slicer.mrmlScene.GetNumberOfNodesByClass("vtkMRMLScriptedModuleNode")
@@ -148,7 +148,8 @@ class ScriptedLoadableModuleLogic():
     return parameterNode
 
   def getAllParameterNodes(self):
-    """return a list of all parameter nodes for this module
+    """
+    Return a list of all parameter nodes for this module
     Multiple parameter nodes are useful for storing multiple parameter sets in a single scene.
     """
     foundParameterNodes = []
@@ -160,7 +161,8 @@ class ScriptedLoadableModuleLogic():
     return foundParameterNodes
 
   def createParameterNode(self):
-    """create a new parameter node
+    """
+    Create a new parameter node
     The node is of vtkMRMLScriptedModuleNode class. Module name is added as an attribute to allow filtering
     in node selector widgets (attribute name: ModuleName, attribute value: the module's name).
     This method can be overridden in derived classes to create a default parameter node with all
@@ -176,12 +178,16 @@ class ScriptedLoadableModuleLogic():
     return node
 
   def delayDisplay(self,message,msec=1000):
-    """Display a message in a popup window for a short time
     """
-    slicer.log.info(message, True, msec)
+    Display a message in a popup window for a short time.
+    It is recommended to directly use slicer.util.delayDisplay function.
+    This method is only kept for backward compatibility and may be removed in the future.
+    """
+    slicer.util.delayDisplay(message, msec)
 
   def clickAndDrag(self,widget,button='Left',start=(10,10),end=(10,40),steps=20,modifiers=[]):
-    """Send synthetic mouse events to the specified widget (qMRMLSliceWidget or qMRMLThreeDView)
+    """
+    Send synthetic mouse events to the specified widget (qMRMLSliceWidget or qMRMLThreeDView)
     button : "Left", "Middle", "Right", or "None"
     start, end : window coordinates for action
     steps : number of steps to move in
@@ -225,7 +231,8 @@ class ScriptedLoadableModuleTest(unittest.TestCase):
   """
 
   def delayDisplay(self,message,msec=1000):
-    """Display messages to the user/tester during testing.
+    """
+    Display messages to the user/tester during testing.
     This method can be temporarily overridden to allow tests running
     with longer or shorter message display time.
     Displaying a dialog and waiting does two things:
@@ -236,13 +243,16 @@ class ScriptedLoadableModuleTest(unittest.TestCase):
     so that we'll know when it breaks.
     Note:
     Information that might be useful (but not important enough to show
-    to the user) can be logged using slicer.log.info function.
-    Error messages should be displayed using slicer.log.error function.
+    to the user) can be logged using logging.info() function
+    (printed to console and application log) or logging.debug()
+    function (printed to application log only).
+    Error messages should be logged by logging.error() function
+    and displayed to user by slicer.util.errorDisplay function.
     """
-    # By default simply use slicer.log.info with message popup enabled
-    slicer.log.info(message, True, msec)
+    slicer.util.delayDisplay(message, msec)
 
   def runTest(self):
-    """Run as few or as many tests as needed here.
     """
-    slicer.log.error('No test is defined in '+self.__class__.__name__)
+    Run a default selection of tests here.
+    """
+    logging.warning('No test is defined in '+self.__class__.__name__)

Base/Python/slicer/log.py
@@ -1,43 +1,0 @@
-""" This module contains functions for writing info or error messages to the application log."""
-
-from __future__ import print_function
-import sys
-from __main__ import qt, slicer
-
-__all__ = ['info', 'error']
-
-# Define an object (__m) to store all module global variables
-class ModuleGlobals(object): pass
-__m = ModuleGlobals()
-__m.errorPopup = None
-
-def info(logMessage, showMessageBox=False, autoCloseMsec=1000):
-  """Write info message to the application log
-  If showMessageBox is True then this method displays a small dialog and
-  waits until the user clicks on OK button or after autoCloseMsec millisecond
-  elapses.
-  This does two things: 1) it lets the event loop catch up
-  to the state of the test so that rendering and widget updates
-  have all taken place before the test continues and 2) it
-  shows the user/developer/tester the state of the test
-  so that we'll know when it breaks.
-  """
-  print(logMessage)
-  if showMessageBox:
-    __m.errorPopup = qt.QDialog()
-    layout = qt.QVBoxLayout()
-    __m.errorPopup.setLayout(layout)
-    label = qt.QLabel(logMessage,__m.errorPopup)
-    layout.addWidget(label)
-    if autoCloseMsec>0:
-      qt.QTimer.singleShot(autoCloseMsec, __m.errorPopup.close)
-    __m.errorPopup.exec_()
-
-def error(logMessage, showMessageBox=False):
-  """Write error message to the application log
-  If showMessageBox is True then this method displays a small dialog box and waits
-  until the user clicks on OK button.
-  """
-  print(logMessage, file=sys.stderr)
-  if showMessageBox:
-    qt.QMessageBox.warning(slicer.util.mainWindow(), "Slicer module error", logMessage)

Base/Python/slicer/util.py
@@ -572,3 +572,42 @@ def unicodeify(s):
   http://stackoverflow.com/questions/9942594/unicodeencodeerror-ascii-codec-cant-encode-character-u-xa0-in-position-20
   """
   return u' '.join(s).encode('utf-8').strip()
+
+def delayDisplay(message,autoCloseMsec=1000):
+  """Display an information message in a popup window for a short time.
+  If autoCloseMsec>0 then the window is closed after waiting for autoCloseMsec milliseconds
+  If autoCloseMsec=0 then the window is not closed until the user clicks on it.
+  """
+  from __main__ import qt, slicer
+  import logging
+  logging.info(message)
+  messagePopup = qt.QDialog()
+  layout = qt.QVBoxLayout()
+  messagePopup.setLayout(layout)
+  label = qt.QLabel(message,messagePopup)
+  layout.addWidget(label)
+  if autoCloseMsec>0:
+    qt.QTimer.singleShot(autoCloseMsec, messagePopup.close)
+  else:
+    okButton = qt.QPushButton("OK")
+    layout.addWidget(okButton)
+    okButton.connect('clicked()', messagePopup.close)
+  messagePopup.exec_()
+
+def warningDisplay(message,autoCloseMsec=1000,windowTitle="Slicer warning"):
+  """Display popup with a warning message.
+  """
+  from __main__ import qt, slicer
+  import logging
+  logging.warning(message)
+  if mainWindow(verbose=False):
+    qt.QMessageBox.warning(slicer.util.mainWindow(), windowTitle, message)
+
+def errorDisplay(message,autoCloseMsec=1000,windowTitle="Slicer error"):
+  """Display an error popup.
+  """
+  from __main__ import qt, slicer
+  import logging
+  logging.error(message)
+  if mainWindow(verbose=False):
+    qt.QMessageBox.critical(slicer.util.mainWindow(), windowTitle, message)

Extensions/Testing/ScriptedLoadableExtensionTemplate/ScriptedLoadableModuleTemplate/ScriptedLoadableModuleTemplate.py
@@ -2,7 +2,7 @@ import os
 import unittest
 from __main__ import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
-import slicer.log
+import logging
 
 #
 # ScriptedLoadableModuleTemplate
@@ -153,10 +153,10 @@ class ScriptedLoadableModuleTemplateLogic(ScriptedLoadableModuleLogic):
     node has valid image data
     """
     if not volumeNode:
-      slicer.log.error('hasImageData failed: no volume node')
+      logging.debug('hasImageData failed: no volume node')
       return False
     if volumeNode.GetImageData() == None:
-      slicer.log.error('hasImageData failed: no image data in volume node')
+      logging.debug('hasImageData failed: no image data in volume node')
       return False
     return True
 
@@ -164,19 +164,19 @@ class ScriptedLoadableModuleTemplateLogic(ScriptedLoadableModuleLogic):
     """Validates if the output is not the same as input
     """
     if not inputVolumeNode:
-      slicer.log.error('isValidInputOutputData failed: no input volume node defined')
+      logging.debug('isValidInputOutputData failed: no input volume node defined')
       return False
     if not outputVolumeNode:
-      slicer.log.error('isValidInputOutputData failed: no output volume node defined')
+      logging.debug('isValidInputOutputData failed: no output volume node defined')
       return False
     if inputVolumeNode.GetID()==outputVolumeNode.GetID():
-      slicer.log.error('isValidInputOutputData failed: input and output volume is the same. Create a new volume for output to avoid this error.')
+      logging.debug('isValidInputOutputData failed: input and output volume is the same. Create a new volume for output to avoid this error.')
       return False
     return True
 
   def takeScreenshot(self,name,description,type=-1):
     # show the message even if not taking a screen shot
-    slicer.log.info('Take screenshot: '+description+'.\nResult is available in the Annotations module.', True, 3000)
+    slicer.util.delayDisplay('Take screenshot: '+description+'.\nResult is available in the Annotations module.', 3000)
 
     lm = slicer.app.layoutManager()
     # switch on the type to get the requested window
@@ -217,10 +217,10 @@ class ScriptedLoadableModuleTemplateLogic(ScriptedLoadableModuleLogic):
     """
 
     if not self.isValidInputOutputData(inputVolume, outputVolume):
-      slicer.log.error('Input volume is the same as output volume. Choose a different output volume.', True)
+      slicer.util.errorDisplay('Input volume is the same as output volume. Choose a different output volume.')
       return False
 
-    slicer.log.info('Processing started')
+    logging.info('Processing started')
 
     # Compute the thresholded output volume using the Threshold Scalar Volume CLI module
     cliParams = {'InputVolume': inputVolume.GetID(), 'OutputVolume': outputVolume.GetID(), 'ThresholdValue' : imageThreshold, 'ThresholdType' : 'Above'}
@@ -230,7 +230,7 @@ class ScriptedLoadableModuleTemplateLogic(ScriptedLoadableModuleLogic):
     if enableScreenshots:
       self.takeScreenshot('ScriptedLoadableModuleTemplate-Start','MyScreenshot',-1)
 
-    slicer.log.info('Processing completed')
+    logging.info('Processing completed')
 
     return True
 
@@ -277,10 +277,10 @@ class ScriptedLoadableModuleTemplateTest(ScriptedLoadableModuleTest):
     for url,name,loader in downloads:
       filePath = slicer.app.temporaryPath + '/' + name
       if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
-        slicer.log.info('Requesting download %s from %s...\n' % (name, url))
+        logging.info('Requesting download %s from %s...\n' % (name, url))
         urllib.urlretrieve(url, filePath)
       if loader:
-        slicer.log.info('Loading %s...' % (name,))
+        logging.info('Loading %s...' % (name,))
         loader(filePath)
     self.delayDisplay('Finished with download and loading')
 

Utilities/Scripts/SlicerWizard/Utilities.py
@@ -198,10 +198,10 @@ def initLogging(logger, args):
   lhe.setLevel(logging.WARNING)
   lhe.setFormatter(f)
 
-  # Set root logging level and add handlers
-  logging.getLogger().addHandler(lho)
-  logging.getLogger().addHandler(lhe)
-  logging.getLogger().setLevel(_logLevel)
+  # Set logging level and add handlers
+  logger.addHandler(lho)
+  logger.addHandler(lhe)
+  logger.setLevel(logLevel)
 
   # Turn of github debugging
   ghLogger = logging.getLogger("github")

Utilities/Templates/Modules/Scripted/TemplateKey.py
@@ -2,7 +2,7 @@ import os
 import unittest
 from __main__ import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
-import slicer.log
+import logging
 
 #
 # TemplateKey
@@ -154,10 +154,10 @@ class TemplateKeyLogic(ScriptedLoadableModuleLogic):
     node has valid image data
     """
     if not volumeNode:
-      slicer.log.error('hasImageData failed: no volume node')
+      logging.debug('hasImageData failed: no volume node')
       return False
     if volumeNode.GetImageData() == None:
-      slicer.log.error('hasImageData failed: no image data in volume node')
+      logging.debug('hasImageData failed: no image data in volume node')
       return False
     return True
 
@@ -165,19 +165,19 @@ class TemplateKeyLogic(ScriptedLoadableModuleLogic):
     """Validates if the output is not the same as input
     """
     if not inputVolumeNode:
-      slicer.log.error('isValidInputOutputData failed: no input volume node defined')
+      logging.debug('isValidInputOutputData failed: no input volume node defined')
       return False
     if not outputVolumeNode:
-      slicer.log.error('isValidInputOutputData failed: no output volume node defined')
+      logging.debug('isValidInputOutputData failed: no output volume node defined')
       return False
     if inputVolumeNode.GetID()==outputVolumeNode.GetID():
-      slicer.log.error('isValidInputOutputData failed: input and output volume is the same. Create a new volume for output to avoid this error.')
+      logging.debug('isValidInputOutputData failed: input and output volume is the same. Create a new volume for output to avoid this error.')
       return False
     return True
 
   def takeScreenshot(self,name,description,type=-1):
     # show the message even if not taking a screen shot
-    slicer.log.info('Take screenshot: '+description+'.\nResult is available in the Annotations module.', True, 3000)
+    slicer.util.delayDisplay('Take screenshot: '+description+'.\nResult is available in the Annotations module.', 3000)
 
     lm = slicer.app.layoutManager()
     # switch on the type to get the requested window
@@ -218,10 +218,10 @@ class TemplateKeyLogic(ScriptedLoadableModuleLogic):
     """
 
     if not self.isValidInputOutputData(inputVolume, outputVolume):
-      slicer.log.error('Input volume is the same as output volume. Choose a different output volume.', True)
+      slicer.util.errorDisplay('Input volume is the same as output volume. Choose a different output volume.')
       return False
 
-    slicer.log.info('Processing started')
+    logging.info('Processing started')
 
     # Compute the thresholded output volume using the Threshold Scalar Volume CLI module
     cliParams = {'InputVolume': inputVolume.GetID(), 'OutputVolume': outputVolume.GetID(), 'ThresholdValue' : imageThreshold, 'ThresholdType' : 'Above'}
@@ -231,7 +231,7 @@ class TemplateKeyLogic(ScriptedLoadableModuleLogic):
     if enableScreenshots:
       self.takeScreenshot('TemplateKeyTest-Start','MyScreenshot',-1)
 
-    slicer.log.info('Processing completed')
+    logging.info('Processing completed')
 
     return True
 
@@ -278,10 +278,10 @@ class TemplateKeyTest(ScriptedLoadableModuleTest):
     for url,name,loader in downloads:
       filePath = slicer.app.temporaryPath + '/' + name
       if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
-        slicer.log.info('Requesting download %s from %s...\n' % (name, url))
+        logging.info('Requesting download %s from %s...\n' % (name, url))
         urllib.urlretrieve(url, filePath)
       if loader:
-        slicer.log.info('Loading %s...' % (name,))
+        logging.info('Loading %s...' % (name,))
         loader(filePath)
     self.delayDisplay('Finished with download and loading')
 

commit 3b0418f5a828ca3dd93a8e2f1b42fd5dfe6e30bb

BUG: Fix qSlicerLoadableModuleWithPythonTest
To fix the failing test due to a regression introduced by r23828,
this commit
  (1) updates CTK to include update to ctk/__init__.py
  (2) update slicer/__init__.py
to ensure "slicer" module can be imported without an instantiated
  (3) update launcher settings associated with CTK external project to
allow ctk module to be imported without having a qSlicerCoreApplication
instantiated. (See function "pythonPaths" at line 65 of
qSlicerCorePythonManager.cpp)
QApplication.

CTK changes:

$ git shortlog a3d8aa9..e23537d --no-merges
Andreas Fetzer (32):
      Use same way to display data like in the ctkXnatTreeModel
      Make root object protected to allow access to the root object from     within subclasses.
      Use lower case property keys consistently.
      Use label instead of abstractresourceid since a newly generated resource may not have an id yet
      Fixed resource uri for ctkXnatFile
      Added new file which contains all the xnat object fields that are accessible via the xnat rest api
      Use properties for fields that are accessible via the rest api instead of members     of the private implementation.     Use the object field constants for accessing the properties.
      Use constants for accessing the properties of an object
      Use constants for accessing the properties of an subject
      Use constants for accessing the properties of an xnat object
      Added convenience methods for setting file properties
      Added missing includes
      Use constant for accessing properties
      Added functions for getting/setting the description of a project
      The description which is displayed as a tooltip by the Tree- and TableModel     must not be synchronized with the XNAT server.     Hence it should be a member of the private implementation

Base/Python/slicer/__init__.py.in
@@ -1,9 +1,10 @@
 """ This module sets up root logging and loads the Slicer library modules into its namespace."""
 
+import ctk
 import imp
 import logging
+import qt
 import sys
-from __main__ import ctk, qt
 
 #-----------------------------------------------------------------------------
 class _LogReverseLevelFilter(logging.Filter):
@@ -22,11 +23,12 @@ class SlicerApplicationLogHandler(logging.Handler):
   """
   def __init__(self):
     logging.Handler.__init__(self)
-    self.pythonToCtkLevelConverter = {
-      logging.DEBUG : ctk.ctkErrorLogLevel.Debug,
-      logging.INFO : ctk.ctkErrorLogLevel.Info,
-      logging.WARNING : ctk.ctkErrorLogLevel.Warning,
-      logging.ERROR : ctk.ctkErrorLogLevel.Error }
+    if hasattr(ctk, 'ctkErrorLogLevel'):
+      self.pythonToCtkLevelConverter = {
+        logging.DEBUG : ctk.ctkErrorLogLevel.Debug,
+        logging.INFO : ctk.ctkErrorLogLevel.Info,
+        logging.WARNING : ctk.ctkErrorLogLevel.Warning,
+        logging.ERROR : ctk.ctkErrorLogLevel.Error }
     self.origin = "Python"
     self.category = "Python"
   def emit(self, record):

SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "a3d8aa94cad5a7704414365505ee05365c3a1f1e"
+    GIT_TAG "e23537d146048845b092ad2320f0f42f659aa838"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
@@ -99,6 +99,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   #-----------------------------------------------------------------------------
   # Launcher setting specific to build tree
 
+  # library paths
   set(${proj}_LIBRARY_PATHS_LAUNCHER_BUILD ${CTK_DIR}/CTK-build/bin/<CMAKE_CFG_INTDIR>)
   if(Slicer_USE_QtTesting)
     list(APPEND ${proj}_LIBRARY_PATHS_LAUNCHER_BUILD
@@ -113,6 +114,16 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   mark_as_superbuild(
     VARS ${proj}_LIBRARY_PATHS_LAUNCHER_BUILD
     LABELS "LIBRARY_PATHS_LAUNCHER_BUILD"
+    )
+
+  # pythonpath
+  set(${proj}_PYTHONPATH_LAUNCHER_BUILD
+    ${CTK_DIR}/CTK-build/bin/Python
+    ${CTK_DIR}/CTK-build/bin/<CMAKE_CFG_INTDIR>
+    )
+  mark_as_superbuild(
+    VARS ${proj}_PYTHONPATH_LAUNCHER_BUILD
+    LABELS "PYTHONPATH_LAUNCHER_BUILD"
     )
 
   #-----------------------------------------------------------------------------