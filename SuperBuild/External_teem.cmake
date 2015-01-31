db321dc54c1bf4a7db36dc7c4304ad3f23fbcd4e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-01-31 03:14:08 +0000

    COMP: Update teem to allow configuration with CMake < 2.8.12.1
    Thanks to the following community members for their investigation and
    issue reports:
      Anderle, Kristjan <K.Anderle@gsi.de>
      Andras Lasso <lasso@queensu.ca>
      Sharp, Gregory C. <GCSHARP@mgh.harvard.edu>
      Steve Pieper <pieper@isomics.com>
      Csaba Pinter <csaba.pinter@queensu.ca>
      Matthew Woehlke <matthew.woehlke@kitware.com>
      Christopher Mullins <christopher.mullins@kitware.com>
    
    // ------------------
    Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    Date:   Fri Jan 30 22:00:50 2015 -0500
    
        COMP: Prevent target double inclusion with older version of CMake
    
        This commit fixes a regression introduced by Slicer/teem@e590e60c
        (COMP: Build/Install modernization) where "export(TARGETS tee, ...)"
        call has been introduced.
    
        With older version of CMake, the generated target file didn't have the
        multiple inclusion safe guard. Since this feature has been introduced
        in CMake < 2.8.12.1 [1], this commit removes the existing
        test and add a valid multiple inclusion guard.
    
        [1] http://slicer-devel.65872.n3.nabble.com/Problems-with-building-Slicer-tp4033327p4033330.html
    // ------------------
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23936
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23936 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_teem.cmake
+++ SuperBuild/External_teem.cmake
@@ -47,7 +47,7 @@ if(NOT DEFINED Teem_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/Slicer/teem"
-    GIT_TAG 71e15b1aaca10c705f46518e94396f4fa5d08e3c
+    GIT_TAG a0ae38afb8913119ca7d584816d81de091fb49d7
     SOURCE_DIR teem
     BINARY_DIR teem-build
     CMAKE_CACHE_ARGS
