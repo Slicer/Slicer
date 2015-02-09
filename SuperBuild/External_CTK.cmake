43acf59edf408314141da8d7b383ca836cece4c4
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-02-09 22:19:11 +0000

    COMP: Update CTK to fix Slicer packaging issue on windows and linux
    This commit fixes a regression introduced in r23958 and discussed in [1]
    
    [1] http://slicer-devel.65872.n3.nabble.com/Windows-and-Linux-nightly-packages-tp4033451p4033452.html
    
    // -------------
    Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    Date:   Mon Feb 9 17:02:42 2015 -0500
    
        Ensure PYTHONQT_INSTALL_DIR is not reset in project including CTK
    
        This commit is an attempt to fix a regression introduced by commontk/CTK@fc92239
        that was causing the Slicer packaging to fail (mainly because the
        variable PYTHONQT_INSTALL_DIR was reset to an empty value after
        "find_path" was called in FindPythonQt.cmake)
    
        Note that this a workaround, the issue is being discussed with CMake
        developer.
    // -------------
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23968
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@23968 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "0592a4ec6d3a217dab09d40787dc9cd58a959c62"
+    GIT_TAG "59dfc7f24b0355be907a030a351fbd9f065794bf"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
