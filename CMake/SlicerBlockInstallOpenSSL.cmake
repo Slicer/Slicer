e81072e07fb2d94a47e56ea0dae37fb2a40bd0ae
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-03-03 22:34:48 +0000

    COMP: Fix windows packaging error introduced by r24006
    This commit updates the OpenSSL install rules to consider the library
    keyword "debug" or "optimized" introduced in r24006 by the use of
    select_library_configurations macro.
    
    Error fixed by this commit is the following:
    
    5>CPack: - Install project: Slicer
    5>CMake Error at C:/D/N/Slicer-1-build/Slicer-build/CMake/LastConfigureStep/cmake_install.cmake:255 (file):
    5>  file INSTALL cannot find
    5>  "C:/D/N/Slicer-1-build/OpenSSL-install/Release/bin/optimized.dll".
    5>Call Stack (most recent call first):
    5>  C:/D/N/Slicer-1-build/Slicer-build/cmake_install.cmake:47 (include)
    5>CMake Error at C:/D/N/Slicer-1-build/Slicer-build/CMake/LastConfigureStep/cmake_install.cmake:263 (file):
    5>  file INSTALL cannot find
    5>  "C:/D/N/Slicer-1-build/OpenSSL-install/Release/bin/debug.dll".
    5>Call Stack (most recent call first):
    5>  C:/D/N/Slicer-1-build/Slicer-build/cmake_install.cmake:47 (include)
    5>CMake Error at C:/D/N/Slicer-1-build/Slicer-build/CMake/LastConfigureStep/cmake_install.cmake:271 (file):
    5>  file INSTALL cannot find
    5>  "C:/D/N/Slicer-1-build/OpenSSL-install/Release/bin/optimized.dll".
    5>Call Stack (most recent call first):
    5>  C:/D/N/Slicer-1-build/Slicer-build/cmake_install.cmake:47 (include)
    5>CMake Error at C:/D/N/Slicer-1-build/Slicer-build/CMake/LastConfigureStep/cmake_install.cmake:279 (file):
    5>  file INSTALL cannot find
    5>  "C:/D/N/Slicer-1-build/OpenSSL-install/Release/bin/debug.dll".
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24012
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24012 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallOpenSSL.cmake
+++ CMake/SlicerBlockInstallOpenSSL.cmake
@@ -2,16 +2,23 @@
 # Find and install OpenSSL Libs
 # -------------------------------------------------------------------------
 
+set(library_config "general")
 foreach(library ${OPENSSL_LIBRARIES})
-  get_filename_component(library_without_extension ${library} NAME_WE)
-  if(WIN32)
-    install(FILES ${OPENSSL_EXPORT_LIBRARY_DIR}/${library_without_extension}.dll
-      DESTINATION ${Slicer_INSTALL_LIB_DIR} COMPONENT Runtime)
-  elseif(UNIX)
-    slicerInstallLibrary(
-      FILE ${library}
-      DESTINATION ${Slicer_INSTALL_LIB_DIR}
-      COMPONENT Runtime
-      )
+  if(library MATCHES "debug|general|optimized")
+    set(library_config ${library})
+  else()
+    if(library_config MATCHES "general|optimized")
+      if(WIN32)
+        get_filename_component(library_without_extension ${library} NAME_WE)
+        install(FILES ${OPENSSL_EXPORT_LIBRARY_DIR}/${library_without_extension}.dll
+          DESTINATION ${Slicer_INSTALL_LIB_DIR} COMPONENT Runtime)
+      elseif(UNIX)
+        slicerInstallLibrary(
+          FILE ${library}
+          DESTINATION ${Slicer_INSTALL_LIB_DIR}
+          COMPONENT Runtime
+          )
+      endif()
+    endif()
   endif()
 endforeach()
