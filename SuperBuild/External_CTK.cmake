fa2fb38ca594863a846309b5287f5c0648959677
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-03-10 03:52:04 +0000

    BUG: Update CTK to include fix for VTK log message parsing regexp
    $ git shortlog 6983ecc..a358caf --no-merges
    Andras Lasso (1):
          Fixed VTK log message parsing regexp
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24043
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24043 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "6983eccff0bd161df0329b3a1a33d9f235b6baa0"
+    GIT_TAG "a358caf79f861d081d7e5f3bf01fda59f5800219"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
