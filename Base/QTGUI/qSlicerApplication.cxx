Slicer/Slicer

commit 22d9c4c81c76fd9dd566c99a372b0c9abfb9b2ea

ENH: Add convenience function to append text to current log file.
svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24051
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24051 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerApplication.cxx
@@ -643,6 +643,24 @@ QStringList qSlicerApplication::recentLogFiles()
 }
 
 // --------------------------------------------------------------------------
+QString qSlicerApplication::currentLogFile()const
+{
+  Q_D(const qSlicerApplication);
+  return d->ErrorLogModel->filePath();
+}
+
+// --------------------------------------------------------------------------
+void qSlicerApplication::appendToLogFile(const QString& txt)
+{
+  QFile f(this->currentLogFile());
+  if (f.open(QFile::Append))
+    {
+    f.write(qPrintable(txt));
+    }
+  f.close();
+}
+
+// --------------------------------------------------------------------------
 void qSlicerApplication::setupFileLogging()
 {
   Q_D(qSlicerApplication);
@@ -690,11 +708,10 @@ void qSlicerApplication::setupFileLogging()
 
   // Log essential information about the application version and the host computer.
   // This helps in reproducing reported problems.
-
-  QFile f(currentLogFilePath);
-  if (f.open(QFile::Append))
+  if (QFile(currentLogFilePath).exists())
     {
-    QTextStream s(&f);
+    QString logTxt;
+    QTextStream s(&logTxt);
     s << QString("Session start time: %1\n").arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"));
     s << QString("Slicer version: %1 (revision %2) %3 - %4\n").arg(Slicer_VERSION_FULL).arg(this->repositoryRevision()).arg(this->platform()).arg((this->isInstalled() ? "installed" : "not installed"));
 
@@ -756,6 +773,6 @@ void qSlicerApplication::setupFileLogging()
     QStringList additionalModulePaths = this->revisionUserSettings()->value("Modules/AdditionalPaths").toStringList();
     s << QString("Additional module paths: %1\n").arg(additionalModulePaths.isEmpty() ? QString("(none)") : additionalModulePaths.join(", "));
 
-    f.close();
+    this->appendToLogFile(logTxt);
     }
 }

Base/QTGUI/qSlicerApplication.h
@@ -142,6 +142,14 @@ public slots:
   /// Paths of recent log files
   QStringList recentLogFiles();
 
+  /// Path of the current log file
+  /// \sa recentLogFiles(), setupFileLogging()
+  QString currentLogFile()const;
+
+  /// Append text to current log file.
+  /// \sa currentLogFile()
+  void appendToLogFile(const QString& txt);
+
 protected:
   /// Reimplemented from qSlicerCoreApplication
   virtual void handleCommandLineArguments();

commit 7f76441dc3d8f68ced1c421cea9c104e13088a7c

BUG: Do not expect log file to exist to try to append text for the first time
This commit fixes a regression introduced in r24051 (ENH: Add convenience
function to append text to current log file.)

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24054
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24054 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerApplication.cxx
@@ -722,7 +722,6 @@ void qSlicerApplication::setupFileLogging()
 
   // Log essential information about the application version and the host computer.
   // This helps in reproducing reported problems.
-  if (QFile(currentLogFilePath).exists())
     {
     QString logTxt;
     QTextStream s(&logTxt);