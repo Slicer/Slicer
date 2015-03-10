18fe93d892e62565e2ea5df8feb11fc0b0bd0ddf
Andras Lasso <lasso@queensu.ca>
2015-03-10 03:27:02 +0000

    COMP: Added missing file to source control
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24041
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24041 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Libs/vtkAddon/vtkAddon.h
@@ -0,0 +1,12 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+=========================================================================auto=*/
+
+#include <vtkVersion.h>
+
+#include "vtkAddonWin32Header.h"
