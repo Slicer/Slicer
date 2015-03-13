e63f12e735734c722ee63954b864f5c2922f6730
Fedorov <fedorov@bwh.harvard.edu>
2015-03-13 15:05:14 +0000

    ENH: Updated revision number for MultiVolumeImporter
    Explicit import of re is apparently needed on some systems. See
    https://github.com/fedorov/MultiVolumeImporter/issues/10.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24063
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24063 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -194,7 +194,7 @@ list_conditional_append(Slicer_BUILD_MultiVolumeExplorer Slicer_REMOTE_DEPENDENC
 
 Slicer_Remote_Add(MultiVolumeImporter
   GIT_REPOSITORY ${git_protocol}://github.com/fedorov/MultiVolumeImporter.git
-  GIT_TAG 37a30b3dd2b632340b7d77fcd7af76765df959b0
+  GIT_TAG 7741251bfc8c9229a0fc02aaf7b12d1326c13091
   OPTION_NAME Slicer_BUILD_MultiVolumeImporter
   OPTION_DEPENDS "Slicer_BUILD_QTLOADABLEMODULES;Slicer_BUILD_MULTIVOLUME_SUPPORT;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
