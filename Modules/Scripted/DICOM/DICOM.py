Slicer/Slicer

commit bb12238fb033e4a0ec07aaed0f78b85779e03435

ENH: Consolidate qSlicerAppMainWindow
qSlicerAppMainWindowCore brings confusion to the code. It is now merged
with qSlicerAppMainWindow.
The custom macro is replaced by the automatic signal/slot connection of Qt
i.e. on_<SENDER_NAME>_<SENDER_SIGNAL>())
The objects had to be renamed to support the feature.

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=22763
git-svn-id: http://svn.slicer.org/Slicer4/trunk@22763 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DICOM/DICOM.py
@@ -90,11 +90,11 @@ This work is supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community. Se
     connection is made that will also cause the instance-created
     DICOM browser to be raised by this menu action"""
     a = self.parent.action()
-    menuFile = slicer.util.lookupTopLevelWidget('menuFile')
-    if menuFile:
-      for action in menuFile.actions():
+    fileMenu = slicer.util.lookupTopLevelWidget('FileMenu')
+    if fileMenu:
+      for action in fileMenu.actions():
         if action.text == 'Save':
-          menuFile.insertAction(action,a)
+          fileMenu.insertAction(action,a)
 
   def __del__(self):
     if hasattr(slicer, 'dicomListener'):
@@ -281,9 +281,9 @@ class DICOMWidget:
       print('Could not connect to the main window DICOM button')
 
     # connect to our menu file entry so it raises the browser
-    menuFile = slicer.util.lookupTopLevelWidget('menuFile')
-    if menuFile:
-      for action in menuFile.actions():
+    fileMenu = slicer.util.lookupTopLevelWidget('FileMenu')
+    if fileMenu:
+      for action in fileMenu.actions():
         if action.text == 'DICOM':
           action.connect('triggered()',self.detailsPopup.open)
 

Applications/SlicerApp/CMakeLists.txt
@@ -38,16 +38,12 @@ set(SlicerApp_SRCS
   qSlicerAppAboutDialog.h
   qSlicerAppMainWindow.cxx
   qSlicerAppMainWindow.h
-  qSlicerAppMainWindowCore.cxx
-  qSlicerAppMainWindowCore.h
-  qSlicerAppMainWindowCore_p.h
+  qSlicerAppMainWindow_p.h
   )
 
 set(SlicerApp_MOC_SRCS
   qSlicerAppAboutDialog.h
   qSlicerAppMainWindow.h
-  qSlicerAppMainWindowCore.h
-  qSlicerAppMainWindowCore_p.h
   )
 
 set(SlicerApp_UI_SRCS

Applications/SlicerApp/Data/Input/NeurosurgicalPlanningTutorial.xml
@@ -36,7 +36,7 @@
         <event widget="qSlicerAppMainWindow/PanelDockWidget/dockWidgetContents/ModulePanel/ScrollArea/qt_scrollarea_viewport/scrollAreaWidgetContents/qSlicerVolumesModuleWidget/DisplayCollapsibleButton/VolumeDisplayWidget/qSlicerScalarVolumeDisplayWidget/MRMLWindowLevelWidget/WindowSpinBox/1ctkQDoubleSpinBox0" command="set_double" arguments="3200"/>
         <event widget="qSlicerAppMainWindow/PanelDockWidget/dockWidgetContents/ModulePanel/ScrollArea/qt_scrollarea_viewport/scrollAreaWidgetContents/qSlicerVolumesModuleWidget/DisplayCollapsibleButton/VolumeDisplayWidget/qSlicerScalarVolumeDisplayWidget/MRMLWindowLevelWidget/LevelSpinBox/1ctkQDoubleSpinBox0" command="set_double" arguments="1857"/>
         <event widget="qSlicerAppMainWindow/PanelDockWidget/dockWidgetContents/ModulePanel/ScrollArea/qt_scrollarea_viewport/scrollAreaWidgetContents/qSlicerVolumesModuleWidget/DisplayCollapsibleButton/VolumeDisplayWidget/qSlicerScalarVolumeDisplayWidget/MRMLWindowLevelWidget/RangeWidgetPopup" command="popupOpen" arguments="false"/>
-        <event widget="qSlicerAppMainWindow/menubar/menuView/MenuLayout" command="activate" arguments="actionViewLayoutOneUpRedSlice"/>
+        <event widget="qSlicerAppMainWindow/menubar/ViewMenu/LayoutMenu" command="activate" arguments="ViewLayoutOneUpRedSliceAction"/>
         <event widget="qSlicerAppMainWindow/ModuleSelectorToolBar/1ctkMenuComboBox0" command="show_popup" arguments="true"/>
         <event widget="qSlicerAppMainWindow/ModuleSelectorToolBar/1ctkMenuComboBox0" command="show_popup" arguments="false"/>
         <event widget="qSlicerAppMainWindow/ModuleSelectorToolBar/1ctkMenuComboBox0" command="triggered" arguments="Editor"/>
@@ -437,7 +437,7 @@
         <event widget="qSlicerAppMainWindow/PanelDockWidget/dockWidgetContents/ModulePanel/ScrollArea/qt_scrollarea_vcontainer/1QScrollBar0" command="mouseMove" arguments="1,0,0,-9,57"/>
         <event widget="qSlicerAppMainWindow/PanelDockWidget/dockWidgetContents/ModulePanel/ScrollArea/qt_scrollarea_vcontainer/1QScrollBar0" command="mouseRelease" arguments="1,0,0,-9,57"/>
         <event widget="qSlicerAppMainWindow/PanelDockWidget/dockWidgetContents/ModulePanel/ScrollArea/qt_scrollarea_viewport/scrollAreaWidgetContents/EditorModuleWidget/VolumeCollapsibleButton/1QFrame0/PerStructureVolumesFrame/AllButtonsFrameButton/MergeStructuresAndBuildModelsButton" command="activate" arguments=""/>
-        <event widget="qSlicerAppMainWindow/menubar/menuView/MenuLayout" command="activate" arguments="actionViewLayoutConventional"/>
+        <event widget="qSlicerAppMainWindow/menubar/ViewMenu/LayoutMenu" command="activate" arguments="ViewLayoutConventionalAction"/>
         <event widget="qSlicerAppMainWindow/CentralWidget/CentralWidgetLayoutFrame/1QSplitter0/ThreeDWidget1/1qMRMLThreeDView0/1QVTKWidget0" command="mouseMove" arguments="(-0.0674374,0.507353,1,1,0)"/>
         <event widget="qSlicerAppMainWindow/CentralWidget/CentralWidgetLayoutFrame/1QSplitter0/ThreeDWidget1/1qMRMLThreeDView0/1QVTKWidget0" command="mousePress" arguments="(-0.0674374,0.507353,1,1,0)"/>
         <event widget="qSlicerAppMainWindow/CentralWidget/CentralWidgetLayoutFrame/1QSplitter0/ThreeDWidget1/1qMRMLThreeDView0/1QVTKWidget0" command="mouseMove" arguments="(-0.0674374,0.492647,0,1,0)"/>

Applications/SlicerApp/Testing/Cpp/CMakeLists.txt
@@ -11,7 +11,6 @@ endif()
 set(CMAKE_TESTDRIVER_BEFORE_TESTMAIN "DEBUG_LEAKS_ENABLE_EXIT_ERROR();" )
 set(KIT_TEST_SRCS
   qSlicerAppAboutDialogTest1.cxx
-  qSlicerAppMainWindowCoreTest1.cxx
   qSlicerAppMainWindowTest1.cxx
   qSlicerModuleFactoryManagerTest1.cxx
   )
@@ -54,7 +53,6 @@ if(Slicer_USE_PYTHONQT_WITH_TCL)
     endif()
   endif()
 endif()
-simple_test( qSlicerAppMainWindowCoreTest1 )
 simple_test( qSlicerAppMainWindowTest1 )
 simple_test( qSlicerModuleFactoryManagerTest1 )
 

Applications/SlicerApp/Testing/Cpp/qSlicerAppMainWindowCoreTest1.cxx
@@ -1,43 +1,0 @@
-/*==============================================================================
-
-  Program: 3D Slicer
-
-  Copyright (c) Kitware Inc.
-
-  See COPYRIGHT.txt
-  or http://www.slicer.org/copyright/copyright.txt for details.
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-  This file was originally developed by Julien Finet, Kitware Inc.
-  and was partially funded by NIH grant 3P41RR013218-12S1
-
-==============================================================================*/
-
-// Qt includes
-#include <QTimer>
-
-// SlicerApp includes
-#include "qSlicerCoreApplication.h"
-#include "qSlicerAppMainWindowCore.h"
-
-// STD includes
-
-int qSlicerAppMainWindowCoreTest1(int argc, char * argv[] )
-{
-  qSlicerCoreApplication app(argc, argv);
-
-  qSlicerAppMainWindowCore mainWindow;
-
-  if (argc < 2 || QString(argv[1]) != "-I")
-    {
-    QTimer::singleShot(100, qApp, SLOT(quit()));
-    }
-
-  return app.exec();
-}
-

Applications/SlicerApp/qSlicerAppMainWindow.h
@@ -27,12 +27,13 @@
 
 // CTK includes
 #include <ctkErrorLogModel.h>
-#include <ctkPimpl.h>
-#include <ctkVTKObject.h>
+class ctkErrorLogWidget;
+class ctkPythonConsole;
 
 // Slicer includes
 #include "qSlicerAppExport.h"
 #include "qSlicerIO.h"
+#include "vtkSlicerConfigure.h" // For Slicer_BUILD_DICOM_SUPPORT, Slicer_USE_PYTHONQT, Slicer_USE_QtTesting
 
 class qSlicerAbstractCoreModule;
 class qSlicerModulePanel;
@@ -40,62 +41,108 @@ class qSlicerModuleSelectorToolBar;
 class qSlicerAppMainWindowCore;
 class qSlicerAppMainWindowPrivate;
 
+// VTK includes
+class vtkObject;
+
 class Q_SLICER_APP_EXPORT qSlicerAppMainWindow : public QMainWindow
 {
   Q_OBJECT
-  QVTK_OBJECT
 public:
-
   typedef QMainWindow Superclass;
+
   qSlicerAppMainWindow(QWidget *parent=0);
   virtual ~qSlicerAppMainWindow();
 
-  /// Return the main window core.
-  qSlicerAppMainWindowCore* core()const;
-
-  /// Return the module selector
+  /// Return a pointer to the module selector toolbar that can change the
+  /// current module.
+  /// \sa pythonConsole(), errorLogWidget()
   Q_INVOKABLE qSlicerModuleSelectorToolBar* moduleSelector()const;
 
+#ifdef Slicer_USE_PYTHONQT
+  /// Return a pointer to the python console.
+  /// \sa moduleSelector(), errorLogWidget()
+  Q_INVOKABLE ctkPythonConsole* pythonConsole()const;
+#endif
+  /// Return a pointer to the error log widget.
+  /// \sa moduleSelector(), pythonConsole()
+  Q_INVOKABLE ctkErrorLogWidget* errorLogWidget()const;
+
 public slots:
-  void setHomeModuleCurrent();
-  void restoreToolbars();
+  virtual void setHomeModuleCurrent();
+  virtual void restoreToolbars();
+
+  virtual void on_FileAddDataAction_triggered();
+  virtual void on_FileLoadDataAction_triggered();
+  virtual void on_FileImportSceneAction_triggered();
+  virtual void on_FileLoadSceneAction_triggered();
+  virtual void on_FileAddVolumeAction_triggered();
+  virtual void on_FileAddTransformAction_triggered();
+  virtual void on_FileSaveSceneAction_triggered();
+  virtual void on_FileExitAction_triggered();
+  virtual void onFileRecentLoadedActionTriggered();
+  virtual void on_SDBSaveToDirectoryAction_triggered();
+  virtual void on_SDBSaveToMRBAction_triggered();
+  virtual void on_SDBSaveToDCMAction_triggered();
+  virtual void on_FileCloseSceneAction_triggered();
+  virtual void on_LoadDICOMAction_triggered();
+
+  virtual void on_EditRecordMacroAction_triggered();
+  virtual void on_EditPlayMacroAction_triggered();
+  virtual void on_EditUndoAction_triggered();
+  virtual void on_EditRedoAction_triggered();
+
+  virtual void onLayoutActionTriggered(QAction* action);
+  virtual void onLayoutCompareActionTriggered(QAction* action);
+  virtual void onLayoutCompareWidescreenActionTriggered(QAction* action);
+  virtual void onLayoutCompareGridActionTriggered(QAction* action);
+
+  virtual void setLayout(int);
+  virtual void setLayoutNumberOfCompareViewRows(int);
+  virtual void setLayoutNumberOfCompareViewColumns(int);
+
+  virtual void on_WindowErrorLogAction_triggered();
+  virtual void on_WindowPythonInteractorAction_triggered();
+  virtual void on_WindowToolbarsResetToDefaultAction_triggered();
+
+  virtual void on_HelpKeyboardShortcutsAction_triggered();
+  virtual void on_HelpBrowseTutorialsAction_triggered();
+  virtual void on_HelpInterfaceDocumentationAction_triggered();
+  virtual void on_HelpSlicerPublicationsAction_triggered();
+  virtual void on_HelpVisualBlogAction_triggered();
+
+  virtual void on_HelpReportBugOrFeatureRequestAction_triggered();
+  virtual void on_HelpAboutSlicerAppAction_triggered();
+
+  virtual void on_EditApplicationSettingsAction_triggered();
+  virtual void on_CutAction_triggered();
+  virtual void on_CopyAction_triggered();
+  virtual void on_PasteAction_triggered();
+  virtual void on_ViewExtensionsManagerAction_triggered();
 
 protected slots:
-  void onModuleLoaded(const QString& moduleName);
-  void onModuleAboutToBeUnloaded(const QString& moduleName);
-  void onEditApplicationSettingsActionTriggered();
-  void onCutActionTriggered();
-  void onCopyActionTriggered();
-  void onPasteActionTriggered();
-  void onViewExtensionsManagerActionTriggered();
+  virtual void onModuleLoaded(const QString& moduleName);
+  virtual void onModuleAboutToBeUnloaded(const QString& moduleName);
+  virtual void onNewFileLoaded(const qSlicerIO::IOProperties &fileProperties);
 
-  void onFileRecentLoadedActionTriggered();
-  void onNewFileLoaded(const qSlicerIO::IOProperties &fileProperties);
-
-  void onMRMLSceneModified(vtkObject*);
-  void onLayoutActionTriggered(QAction* action);
-  void onLayoutCompareActionTriggered(QAction* action);
-  void onLayoutCompareWidescreenActionTriggered(QAction* action);
-  void onLayoutCompareGridActionTriggered(QAction* action);
-  void onLayoutChanged(int);
-  void loadDICOMActionTriggered();
-
-  void onWarningsOrErrorsOccurred(ctkErrorLogLevel::LogLevel logLevel);
+  virtual void onMRMLSceneModified(vtkObject*);
+  virtual void onLayoutChanged(int);
+  virtual void onWarningsOrErrorsOccurred(ctkErrorLogLevel::LogLevel logLevel);
 
 protected:
-
   /// Connect MainWindow action with slots defined in MainWindowCore
-  void setupMenuActions();
+  virtual void setupMenuActions();
 
   /// Open a popup to warn the user Slicer is not for clinical use.
-  void disclaimer();
+  virtual void disclaimer();
 
   /// Forward the dragEnterEvent to the IOManager which will
   /// decide if it could accept a drag/drop or not.
-  void dragEnterEvent(QDragEnterEvent *event);
+  /// \sa dropEvent()
+  virtual void dragEnterEvent(QDragEnterEvent *event);
 
   /// Forward the dropEvent to the IOManager.
-  void dropEvent(QDropEvent *event);
+  /// \sa dragEnterEvent()
+  virtual void dropEvent(QDropEvent *event);
 
   /// Reimplemented to catch activationChange/show/hide events.
   /// More specifically it allows to:
@@ -110,6 +157,8 @@ protected:
 
 protected:
   QScopedPointer<qSlicerAppMainWindowPrivate> d_ptr;
+
+  qSlicerAppMainWindow(qSlicerAppMainWindowPrivate* pimpl, QWidget* parent);
 
 private:
   Q_DECLARE_PRIVATE(qSlicerAppMainWindow);

commit 823aa4ca00841d977ee7fb7241cb5f58accea015

STYLE: Remove redundant code from DICOM module
This commit removes duplicated code that was connecting the FileMenu
action in addition of the action already directly retrieved by its name.

The duplication became unneeded after commit r23628 (ENH: Fixed issues
3579, 3798 and other dicom browser improvements) that changed
the string used to lookup the action from "actionLoadDICOM" to
"LoadDICOMAction".

Note that the action lookup has been first broken in r22763 (ENH: Consolidate
qSlicerAppMainWindow)

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24091
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24091 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DICOM/DICOM.py
@@ -290,18 +290,12 @@ class DICOMWidget:
 
     # connect to the main window's dicom button
     mw = slicer.util.mainWindow()
-    try:
-      action = slicer.util.findChildren(mw,name='LoadDICOMAction')[0]
-      action.connect('triggered()',self.detailsPopup.open)
-    except IndexError:
-      print('Could not connect to the main window DICOM button')
-
-    # connect to our menu file entry so it raises the browser
-    fileMenu = slicer.util.lookupTopLevelWidget('FileMenu')
-    if fileMenu:
-      for action in fileMenu.actions():
-        if action.text == 'DICOM':
-          action.connect('triggered()',self.detailsPopup.open)
+    if mw:
+      try:
+        action = slicer.util.findChildren(mw,name='LoadDICOMAction')[0]
+        action.connect('triggered()',self.detailsPopup.open)
+      except IndexError:
+        print('Could not connect to the main window DICOM button')
 
     # make the tables view a bit bigger
     self.tables.setMinimumHeight(250)