652bfaceb82ac320490b76fae8466f0ed8877e14
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-04-01 07:01:43 +0000

    STYLE: Update CTK to fix warning related to wrapping of CTKDICOM module
    $ git shortlog 8011739..d72ab0c --no-merges
    Jean-Christophe Fillion-Robin (1):
          Workaround to fix HAVE_STAT redefined warning related to DICOM module wrapping
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24136
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24136 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "801173949bf57a332aaa2f7bc86b725e233f593d"
+    GIT_TAG "d72ab0c0e30b390d7349c808a72b8d11a5dac659"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
