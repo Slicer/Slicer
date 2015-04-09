f399862f1cf8ab4d2684a115bca3505292eb1a82
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-04-09 07:14:24 +0000

    ENH: Update LandmarkRegistration to show warning if plugin failed to be loaded
    See pieper/LandmarkRegistration@13d7b32
    
    Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    Date:   Wed Apr 8 00:20:27 2015 -0400
    
        Display a warning when a built-in registration plugin failed to be loaded.
    
        For example, if Slicer is build without SimpleITK support, the following
        is displayed:
    
        8<----8<----8<----8<----8<----8<----8<----8<----
        [...]
        Number of registered modules: 139
        Registration: Failed to import 'LocalSimpleITK' plugin: No module named SimpleITK
        Number of instantiated modules: 139
        [...]
        8<----8<----8<----8<----8<----8<----8<----8<----
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24164
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24164 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -296,7 +296,7 @@ list_conditional_append(Slicer_BUILD_CompareVolumes Slicer_REMOTE_DEPENDENCIES C
 
 Slicer_Remote_Add(LandmarkRegistration
   GIT_REPOSITORY "${git_protocol}://github.com/pieper/LandmarkRegistration"
-  GIT_TAG "0bcbcce0de4e66c17dc7e14d940d4bc9e3a11f6f"
+  GIT_TAG "13d7b32ffc0be030256fd3d1b010a0efc18e3bff"
   OPTION_NAME Slicer_BUILD_LandmarkRegistration
   OPTION_DEPENDS "Slicer_BUILD_CompareVolumes;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
