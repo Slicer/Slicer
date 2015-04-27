1d6fbd4083f496e4aa10ca0f37305c3d458c80be
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-04-27 04:47:54 +0000

    ENH: Update CTK
    $ git shortlog  d6171b8..b643fd8 --no-merges
    Andras Lasso (1):
          BUG: Enable Ctrl-C shortcut in Python console in the history area
    
    Andreas Fetzer (2):
          Fixed again duplicate signal connection
          Removed redundant call of hideUIDColumns
    
    Csaba Pitner (1):
          BUG: Clear box icon renamed to be considered
    
    Julien Finet (1):
          Add white border to clear icon
    
    Laurenn Lam (1):
          Add setChecked method to ctkButtonGroup
    
    Sascha Zelzer (2):
          QAtomicInt does not have a int conversion operator any more.
          Fixed deprecated warning with Qt5.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24193
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24193 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "d6171b88d206985a32597ac5dada8d2958b3e655"
+    GIT_TAG "b643fd82ca4c8030daca1bdbe22e38c15459bdc5"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
