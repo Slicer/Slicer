496bab830037083102e5ca1d3af94120550b8186
Fedorov <fedorov@bwh.harvard.edu>
2015-04-29 16:12:57 +0000

    ENH: add more DCMTK applications
    Applications added:
     - dcm2xml: convert DICOM into XML representation
       (http://support.dcmtk.org/docs/dcm2xml.html)
     - dsr2xml: convert SR DICOM into XML representation
       (http://support.dcmtk.org/docs/dsr2xml.html
     - dsrdump: SR-specific version of dcmdump already included
       (http://support.dcmtk.org/docs/dsrdump.html)
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24197
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24197 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallDCMTKApps.cmake
+++ CMake/SlicerBlockInstallDCMTKApps.cmake
@@ -2,7 +2,7 @@
 # Find and install DCMTK Apps
 # -------------------------------------------------------------------------
 
-set(DCMTK_Apps storescu storescp dcmdump dump2dcm img2dcm dcmdjpeg dcmqrscp)
+set(DCMTK_Apps storescu storescp dcmdump dump2dcm img2dcm dcmdjpeg dcmqrscp dcm2xml dsr2xml dsrdump)
 set(int_dir "")
 if(CMAKE_CONFIGURATION_TYPES)
   set(int_dir "Release/")
