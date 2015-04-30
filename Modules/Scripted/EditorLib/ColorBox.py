67f766d4aff145ea91da7477b43640d26ca2a088
Robin Weiss <robinweiss@uchicago.edu>
2015-04-30 02:32:37 +0000

    ENH: Make color selector search box case-insensitive
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24199
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24199 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Scripted/EditorLib/ColorBox.py
+++ Modules/Scripted/EditorLib/ColorBox.py
@@ -116,7 +116,7 @@ class ColorBox(object):
       self.row+=1
     for c in xrange(self.colorNode.GetNumberOfColors()):
       name = self.colorNode.GetColorName(c)
-      if name != "(none)" and name.find(pattern) >= 0:
+      if name != "(none)" and name.lower().find(pattern.lower()) >= 0:
         self.addRow(c)
     self.view.setColumnWidth(0,75)
     self.view.setColumnWidth(1,50)
