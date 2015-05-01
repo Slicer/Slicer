ba77aaa7b8cee3da6a01ff670d5196d618749abf
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-05-01 02:31:56 +0000

    ENH: Update CTK
    $ git shortlog b643fd8..e771b4a --no-merges
    Jean-Christophe Fillion-Robin (2):
          Do not override widget visibility if already set in a layout view factory
          Do not require QWidget in decorator generated by ctkWrapPythonQt
    
    Julien Finet (4):
          Fix compilation error with Qt < 5.2.1
          Add Qt5 support for VTK.
          Add Qt5 support to QtTesting module
          Fix ctkFileDialogEventTranslatorPlayerTests
    
    Sascha Zelzer (6):
          Added application support and improved debugging mechanism.
          Fixed installing plug-ins via symbolic names.
          Fixed class / struct forward decls and incomplete type.
          Improved test output.
          Visual Studio seems to need the full class definition.
          During shutdown of the plugin framework, also stop all plug-ins.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24205
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24205 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "b643fd82ca4c8030daca1bdbe22e38c15459bdc5"
+    GIT_TAG "e771b4a92fe3d3302c4159115510682e3f23f10f"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
