c3259e9e9eafe83b2e4d362258b41e217c9b6f68
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-05-12 14:44:12 +0000

    BUG: Update SimpleFilters to fix failing py_SimpleFiltersModuleTest
    $ git shortlog 625d941..73add6b --no-merges
    Jean-Christophe Fillion-Robin (2):
          Prevent already destroyed widget exception occurring on Slicer exit
          Remove broken 'STAPLEImageFilter'
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24242
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24242 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -203,7 +203,7 @@ list_conditional_append(Slicer_BUILD_MultiVolumeImporter Slicer_REMOTE_DEPENDENC
 
 Slicer_Remote_Add(SimpleFilters
   GIT_REPOSITORY ${git_protocol}://github.com/SimpleITK/SlicerSimpleFilters.git
-  GIT_TAG 625d9414d4d5e69cae9d29424693f170d81d5f4f
+  GIT_TAG 73add6b2046af693a063196237c105f1b6632251
   OPTION_NAME Slicer_BUILD_SimpleFilters
   OPTION_DEPENDS "Slicer_BUILD_QTSCRIPTEDMODULES;Slicer_USE_SimpleITK"
   LABELS REMOTE_MODULE
