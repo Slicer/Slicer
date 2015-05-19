2d5d5b7ad4891c75a7ddcefa0b0c5555f038ee2f
Csaba Pinter <csaba.pinter@queensu.ca>
2015-05-19 21:17:58 +0000

    STYLE: Added doxygen comments to simple markups widget header
    Also fixed compilation warning by removing Z order tag from ui file.
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24265
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24265 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Loadable/Markups/Widgets/qSlicerSimpleMarkupsWidget.h
+++ Modules/Loadable/Markups/Widgets/qSlicerSimpleMarkupsWidget.h
@@ -44,39 +44,62 @@ public:
   qSlicerSimpleMarkupsWidget(QWidget *parent=0);
   virtual ~qSlicerSimpleMarkupsWidget();
 
+  /// Get the currently selected markups node.
   Q_INVOKABLE vtkMRMLNode* getCurrentNode();
   
 public slots:
+  /// Set the currently selected markups node.
   void setCurrentNode(vtkMRMLNode* currentNode);
+  /// Set the default name of the markups node created in the combo box.
   void setNodeBaseName(QString newNodeBaseName);
 
+  /// Get the selected color of the currently selected markups node.
   void getNodeColor(QColor color);
+  /// Set the selected color of the currently selected markups node.
   void setNodeColor(QColor color);
+  /// Set the default color that is assigned to newly created markups nodes in the combo box.
   void setDefaultNodeColor(QColor color);
 
+  /// Highlight the Nth fiducial in the table of fiducials.
   void highlightNthFiducial(int n);
+  /// Set the currently selected markups node to be the active markups node in the Slicer scene.
   void activate();
 
 protected slots:
+  /// Update the currently selected markups node to have its selected color changed.
   void onColorButtonChanged(QColor);
+  /// Toggle the visibility of the markups in the viewers.
   void onVisibilityButtonClicked();
+  /// Toggle whether the current markups node is locked.
   void onLockButtonClicked();
+  /// Delete a fiducial from the list.
   void onDeleteButtonClicked();
+  /// Set the current node to be the active node in the scene and start place mode.
   void onPlaceButtonClicked();
+  /// Make the currently selected markups node the active markups node in the scene.
   void onActiveButtonClicked();
 
+  /// Update the widget when a different markups node is selected by the combo box.
   void onMarkupsFiducialNodeChanged();
+  /// Setup a newly created markups node - add display node, set color.
   void onMarkupsFiducialNodeAdded(vtkMRMLNode*);
+  /// Create context menu for the table displaying the currently selected markups node.
   void onMarkupsFiducialTableContextMenu(const QPoint& position);
 
+  /// Edit the name or position of the currently selected markups node.
   void onMarkupsFiducialEdited(int row, int column);
 
+  /// Update the GUI to reflect the currently selected markups node.
   void updateWidget();
 
 signals:
+  /// The currently selected markups node has changed.
   void markupsFiducialNodeChanged();
+  /// The currently selected markups node has been activated.
   void markupsFiducialActivated();
+  /// The place mode has changed. This is independent of whether the currently selected markups node is active.
   void markupsFiducialPlaceModeChanged();
+  /// The updates to the GUI have are finished.
   void updateFinished();
 
 protected:
@@ -85,6 +108,7 @@ protected:
   virtual void setup();
   virtual void enter();
 
+  /// Monitor for changes in the interaction and selection nodes.
   void connectInteractionAndSelectionNodes();
 
 private:
