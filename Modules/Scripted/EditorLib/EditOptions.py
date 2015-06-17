597611bbf90f8fe8590ff113f8e549293e2e06b3
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-06-17 15:44:52 +0000

    STYLE: Editor-EditOptions: Use VTKObservationMixin for scene observation
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24334
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24334 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Scripted/EditorLib/EditOptions.py
+++ Modules/Scripted/EditorLib/EditOptions.py
@@ -4,6 +4,7 @@ from __main__ import ctk
 from __main__ import vtk
 from __main__ import getNodes
 from EditUtil import EditUtil
+from slicer.util import VTKObservationMixin
 
 #########################################################
 #
@@ -52,7 +53,7 @@ class HelpButton(object):
 #########################################################
 # Options
 #########################################################
-class EditOptions(object):
+class EditOptions(VTKObservationMixin):
   """ This EditOptions is a parent class for all the GUI options
   for editor effects.  These are small custom interfaces
   that it in the toolOptionsFrame of the Editor interface.
@@ -60,9 +61,9 @@ class EditOptions(object):
   """
 
   def __init__(self, parent=None):
+    VTKObservationMixin.__init__(self)
     self.parent = parent
     self.updatingGUI = False
-    self.observerTags = []
     self.widgets = []
     self.parameterNode = None
     self.parameterNodeTag = None
@@ -86,15 +87,13 @@ class EditOptions(object):
 
     # TODO: change this to look for specfic events (added, removed...)
     # but this requires being able to access events by number from wrapped code
-    tag = slicer.mrmlScene.AddObserver(vtk.vtkCommand.ModifiedEvent, self.updateParameterNode)
-    self.observerTags.append( (slicer.mrmlScene, tag) )
+    self.addObserver(slicer.mrmlScene, vtk.vtkCommand.ModifiedEvent, self.updateParameterNode)
 
   def __del__(self):
     self.destroy()
     if self.parameterNode:
       self.parameterNode.RemoveObserver(self.parameterNodeTag)
-    for tagpair in self.observerTags:
-      tagpair[0].RemoveObserver(tagpair[1])
+    self.removeObservers()
 
   def connectWidgets(self):
     if self.connectionsConnected: return
