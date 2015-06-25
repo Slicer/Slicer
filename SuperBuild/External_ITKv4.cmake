4186c505bba573b0cab948fceefdbbf3bbabf171
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-06-25 23:32:58 +0000

    COMP: Update ITKv4 to fix MacOSX compilation issue.
    //------------------
    commit 5f95aa6712c508a637a12faaea119d35e574208c
    Author: Bradley Lowekamp <blowekamp@mail.nih.gov>
    Date:   Thu Jun 25 10:25:11 2015 -0400
    
        COMP: Explicitly export SingleValuedNonLinearVnlOptimizerv4
    
        Address the following linking issue:
    
        Undefined symbols for architecture x86_64:
          "typeinfo for itk::SingleValuedNonLinearVnlOptimizerv4", referenced
          from:
              typeinfo for itk::LBFGSOptimizerBasev4<vnl_lbfgsb> in
              libSimpleITKRegistration-0.9.a(sitkImageRegistrationMethod_CreateOptimizer.cxx.o)
        ld: symbol(s) not found for architecture x86_64
        clang: error: linker command failed with exit code 1 (use -v to see
        invocation)
    //------------------
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24361
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24361 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -27,7 +27,7 @@ if(NOT DEFINED ITK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   endif()
 
   set(ITKv4_REPOSITORY ${git_protocol}://github.com/Slicer/ITK.git)
-  set(ITKv4_GIT_TAG 650d1694c5fe7991abb9c79b361080ff2fb16db6) # slicer-v4.8rc02
+  set(ITKv4_GIT_TAG 5f95aa6712c508a637a12faaea119d35e574208c) # slicer-v4.8rc02
 
   set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
 
