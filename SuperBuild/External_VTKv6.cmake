Slicer/Slicer

commit 1d70ec23b89122155145c6c72c9949be5d7a6a14

ENH: Update VTK to master (soon to be v6.3.0) and also update CTK
CTK updates:

$ git shortlog 7bb708c..7081550 --no-merges
Jean-Christophe Fillion-Robin (2):
      Fix link error when linking against VTK build with KITS enabled.
      Tweak logic specifying libraries to link against VisualizationVTKCore

Rostislav Khlebnikov (1):
      DEBUG variable renamed to DEBUG_FLAG

VTK Updates:

$git shortlog 4b46455..d645b35 --no-merges
Aashish Chaudhary (41):
      Maximize or minimize each component for independent components
      Fixed additive blending for independent components
      Added initial support for 2 and 3 components
      Fixed style
      Fixed bad switch case
      Compute the range correctly when dealing with 2-3 components
      Keep the comments in the final shader string
      Added support for dependent components
      Match tokens with method names
      Fixing shader for 2 & 3 component
      Fixed dependent component test failing
      Fix GL extensions not reported on Mac OpenGL2 context
      Reset active texture to texture 0
      Saving and restoring GL state
      Fixed comment
      Clamp texture unit to 0 and GL_MAX_TEXTURE_IMAGE_UNITS
      Fixed accessing invalid texture unit manager
      Using a consistent style within a method
      Compute light position only once
      Optimize lighting code some more
      Reset number of lights
      Some more optimizations for better performance
      Respect interpolation type set on volume property
      Fixed invalid variable names used for gradient computation
      Fixed check on interpolation type
      Updated default interpolation type for test
      Fixed style
      Added check on when to update volume parameters
      Do not update light parameters if shading is OFF
      Fixed compiler warning
      Some more optimizations
      Do not compute color if opacity is zero
      Updated code to new computeColor signature
      Avoid computation of fragment world position repeatedly

SuperBuild/External_VTKv6.cmake
@@ -94,7 +94,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT ${CMAKE_PROJECT_N
   endif()
 
   set(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY "github.com/Slicer/VTK.git" CACHE STRING "Repository from which to get VTK" FORCE)
-  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG "4b464557bcdcc0a73ad7201bce090d5f61ae9c3d" CACHE STRING "VTK git tag to use" FORCE)
+  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG "d645b35fdbbf36c3b4c2b4e60cb66294af8a61fd" CACHE STRING "VTK git tag to use" FORCE)
 
   mark_as_advanced(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG)
 

CMakeLists.txt
@@ -589,8 +589,16 @@ if(${VTK_VERSION_MAJOR} GREATER 5)
     if(FONTCONFIG_FOUND)
       list(APPEND Slicer_VTK_COMPONENTS
         vtkRenderingFreeTypeFontConfig
-        vtkRenderingFreeType${Slicer_VTK_RENDERING_BACKEND}
+        # vtkRenderingFreeTypeOpenGL # Since commit 4f7460a5 (> 6.2.0), vtkRenderingFreeTypeOpenGL
+                                     # has been removed. This mean Slicer will not compile
+                                     # against version of VTK folder than 6.3.0 having FontConfig
+                                     # capabilities.
+                                     # If this is needed, a configure test would have to
+                                     # be added to check if the module is available or not.
         )
+      if(TARGET vtkRenderingFreeTypeOpenGL)
+        list(APPEND VTK_LIBRARIES vtkRenderingFreeTypeOpenGL)
+      endif()
     endif()
   endif()
 endif()

Modules/CLI/MergeModels/Data/Baseline/sphereCube.vtp
@@ -1,5 +1,5 @@
 <?xml version="1.0"?>
-<VTKFile type="PolyData" version="0.1" byte_order="LittleEndian" compressor="vtkZLibDataCompressor">
+<VTKFile type="PolyData" version="0.1" byte_order="LittleEndian" header_type="UInt32" compressor="vtkZLibDataCompressor">
   <PolyData>
     <Piece NumberOfPoints="74"                   NumberOfVerts="0"                    NumberOfLines="0"                    NumberOfStrips="0"                    NumberOfPolys="102"                 >
       <PointData Normals="Normals">

SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "7bb708c8b857a9e47175abdff8dd3e33a28faa76"
+    GIT_TAG "708155066f082aa5127c4c0bcb0aa58d857c327b"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS

commit 47535d7e12589b5cb755d821c9927c2f5c99b36e

BUG: Update VTK - Re-enable GPU VR on Mac fixing a Slicer/VTK rebase mistake
This commit

   (1) reverts Slicer specific commit disabling the failing color transfer
       function improvements and include the fix done by Cory.

   (2) re-enable GPU volume rendering on MacOSX fixing a rebase mistake
       introduced in r24461 (ENH: Update VTK to master (soon to be v6.3.0) and also update CTK)


// --------------
$ git log --no-merges d645b35..716b0f5
commit 716b0f56fc4bd4b9fcca05f386ec375bcd674122
Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
Date:   Tue Aug 4 17:49:39 2015 -0400

    Fix error preventing GPU volume rendering from working on MacOSX

    This commit fixes error introduced when rebasing commit Slicer/VTK@add058e
    (Enable GPU Raycast volume mapper for recent NVIDIA drivers on Mac OS X)

commit 74733a0e7d5dd55f5eb52ca411c06ecbb3431ed2
Author: Cory Quammen <cory.quammen@kitware.com>
Date:   Thu Jul 23 22:45:26 2015 -0400

    Fix crash where vtkColorTransferFunction has 0 nodes

    Thanks to Jean-Christophe Fillion-Robin for pointing this out.

    See

    https://github.com/Slicer/Slicer/commit/f59a689a0f6d2912538ed72dad16efc70601f0ed#commitcomment-9407867

    for reference.

    A basic test for vtkColorTransferFunction is added that crashes
    prior to this patch and succeeds with this patch.

    Tested-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

commit 7b51a097aead0c872f199a2039d010f28c96e2f5
Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
Date:   Tue Aug 4 15:08:41 2015 -0400

    Re-enable "6657: Add out-of-range colors to color maps"

    This reverts commit Slicer/VTK@d645b35.

    Thanks: Cory Quammen <cory.quammen@kitware.com>
// --------------

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24484
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24484 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_VTKv6.cmake
@@ -94,7 +94,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT ${CMAKE_PROJECT_N
   endif()
 
   set(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY "github.com/Slicer/VTK.git" CACHE STRING "Repository from which to get VTK" FORCE)
-  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG "d645b35fdbbf36c3b4c2b4e60cb66294af8a61fd" CACHE STRING "VTK git tag to use" FORCE)
+  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG "716b0f56fc4bd4b9fcca05f386ec375bcd674122" CACHE STRING "VTK git tag to use" FORCE)
 
   mark_as_advanced(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG)
 