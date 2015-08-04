Slicer/Slicer

commit 3507473a4687432b601b5879565b16588a710973

BUG: Propagate ModifiedEvent to volume/modelNode
When setting a raw (vs pipeline filter) input data to a mrml node,
the node was not notified (ProcessMRMLEvents) whenever the data
was changed because the trivial producer internally used
is not forwarding events.

Issue #3780

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23524
git-svn-id: http://svn.slicer.org/Slicer4/trunk@23524 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkMRMLVolumeNode.cxx
@@ -22,6 +22,7 @@ Version:   $Revision: 1.14 $
 #include <vtkAlgorithmOutput.h>
 #include <vtkAppendPolyData.h>
 #include <vtkCallbackCommand.h>
+#include <vtkEventForwarderCommand.h>
 #include <vtkGeneralTransform.h>
 #include <vtkHomogeneousTransform.h>
 #include <vtkImageData.h>
@@ -67,6 +68,7 @@ vtkMRMLVolumeNode::vtkMRMLVolumeNode()
   this->ImageData = NULL;
 #else
   this->ImageDataConnection = NULL;
+  this->DataEventForwarder = NULL;
 #endif
 }
 
@@ -74,6 +76,12 @@ vtkMRMLVolumeNode::vtkMRMLVolumeNode()
 vtkMRMLVolumeNode::~vtkMRMLVolumeNode()
 {
   this->SetAndObserveImageData(NULL);
+#if (VTK_MAJOR_VERSION > 5)
+  if (this->DataEventForwarder)
+    {
+    this->DataEventForwarder->Delete();
+    }
+#endif
 }
 
 //----------------------------------------------------------------------------
@@ -740,8 +748,27 @@ void vtkMRMLVolumeNode::SetAndObserveImageData(vtkImageData *imageData)
     }
   else
     {
+    vtkTrivialProducer* oldProducer = vtkTrivialProducer::SafeDownCast(
+      this->GetImageDataConnection() ? this->GetImageDataConnection()->GetProducer() : 0);
+    if (oldProducer && oldProducer->GetOutputDataObject(0) == imageData)
+      {
+      return;
+      }
+    else if (oldProducer && oldProducer->GetOutputDataObject(0))
+      {
+      oldProducer->GetOutputDataObject(0)->RemoveObservers(
+        vtkCommand::ModifiedEvent, this->DataEventForwarder);
+      }
     vtkNew<vtkTrivialProducer> tp;
     tp->SetOutput(imageData);
+    // Propagate ModifiedEvent onto the trivial producer to make sure
+    // PolyDataModifiedEvent is triggered.
+    if (!this->DataEventForwarder)
+      {
+      this->DataEventForwarder = vtkEventForwarderCommand::New();
+      }
+    this->DataEventForwarder->SetTarget(tp.GetPointer());
+    imageData->AddObserver(vtkCommand::ModifiedEvent, this->DataEventForwarder);
     this->SetImageDataConnection(tp->GetOutputPort());
     }
 }

Libs/MRML/Core/vtkMRMLModelNode.cxx
@@ -27,6 +27,7 @@ Version:   $Revision: 1.3 $
 #include <vtkCallbackCommand.h>
 #include <vtkCellData.h>
 #include <vtkColorTransferFunction.h>
+#include <vtkEventForwarderCommand.h>
 #include <vtkFloatArray.h>
 #include <vtkGeneralTransform.h>
 #include <vtkMatrix4x4.h>
@@ -52,6 +53,7 @@ vtkMRMLModelNode::vtkMRMLModelNode()
   this->PolyData = NULL;
 #else
   this->PolyDataConnection = NULL;
+  this->DataEventForwarder = NULL;
 #endif
 }
 
@@ -59,6 +61,12 @@ vtkMRMLModelNode::vtkMRMLModelNode()
 vtkMRMLModelNode::~vtkMRMLModelNode()
 {
   this->SetAndObservePolyData(NULL);
+#if (VTK_MAJOR_VERSION > 5)
+  if (this->DataEventForwarder)
+    {
+    this->DataEventForwarder->Delete();
+    }
+#endif
 }
 
 //----------------------------------------------------------------------------
@@ -164,8 +172,28 @@ void vtkMRMLModelNode::SetAndObservePolyData(vtkPolyData *polyData)
     }
   else
     {
+    vtkTrivialProducer* oldProducer = vtkTrivialProducer::SafeDownCast(
+      this->GetPolyDataConnection() ? this->GetPolyDataConnection()->GetProducer() : 0);
+    if (oldProducer && oldProducer->GetOutputDataObject(0) == polyData)
+      {
+      return;
+      }
+    else if (oldProducer && oldProducer->GetOutputDataObject(0))
+      {
+      oldProducer->GetOutputDataObject(0)->RemoveObservers(
+        vtkCommand::ModifiedEvent, this->DataEventForwarder);
+      }
+
     vtkNew<vtkTrivialProducer> tp;
     tp->SetOutput(polyData);
+    // Propagate ModifiedEvent onto the trivial producer to make sure
+    // PolyDataModifiedEvent is triggered.
+    if (!this->DataEventForwarder)
+      {
+      this->DataEventForwarder = vtkEventForwarderCommand::New();
+      }
+    this->DataEventForwarder->SetTarget(tp.GetPointer());
+    polyData->AddObserver(vtkCommand::ModifiedEvent, this->DataEventForwarder);
     this->SetPolyDataConnection(tp->GetOutputPort());
     }
 }

Libs/MRML/Core/vtkMRMLModelNode.h
@@ -23,6 +23,7 @@ class vtkMRMLStorageNode;
 // VTK includes
 class vtkAlgorithmOutput;
 class vtkAssignAttributes;
+class vtkEventForwarderCommand;
 class vtkDataArray;
 class vtkPolyData;
 class vtkMRMLDisplayNode;
@@ -225,6 +226,7 @@ protected:
   vtkPolyData *PolyData;
 #else
   vtkAlgorithmOutput* PolyDataConnection;
+  vtkEventForwarderCommand* DataEventForwarder;
 #endif
 };
 

Libs/MRML/Core/vtkMRMLVolumeNode.h
@@ -21,6 +21,7 @@ class vtkMRMLVolumeDisplayNode;
 
 // VTK includes
 class vtkAlgorithmOutput;
+class vtkEventForwarderCommand;
 class vtkImageData;
 class vtkMatrix4x4;
 
@@ -227,6 +228,7 @@ protected:
   vtkImageData* ImageData;
 #else
   vtkAlgorithmOutput* ImageDataConnection;
+  vtkEventForwarderCommand* DataEventForwarder;
 #endif
 
   itk::MetaDataDictionary Dictionary;

commit 14231e1443486f43819629e45a1e93efbe35db2d

STYLE: Remove erroneous comments in vtkMRMLVolumeNode.cxx
This commit removes incorrect comment originally introduced
in r23524 (BUG: Propagate ModifiedEvent to volume/modelNode)

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24481
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24481 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkMRMLVolumeNode.cxx
@@ -771,7 +771,7 @@ void vtkMRMLVolumeNode::SetAndObserveImageData(vtkImageData *imageData)
     vtkNew<vtkTrivialProducer> tp;
     tp->SetOutput(imageData);
     // Propagate ModifiedEvent onto the trivial producer to make sure
-    // PolyDataModifiedEvent is triggered.
+    // ImageDataModifiedEvent is triggered.
     if (!this->DataEventForwarder)
       {
       this->DataEventForwarder = vtkEventForwarderCommand::New();