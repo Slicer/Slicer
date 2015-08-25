bbed9d7d1a368fffc8c83df1bf32d09b4ad0ac84
Fedorov <fedorov@bwh.harvard.edu>
2015-08-25 16:56:41 +0000

    ENH: update MultiVolumeExplorer git hash
    MultiVolumeExplorer has been refactored to facilitate reuse in other modules and
    to simplify user interface. Refactoring done by Christian Herz!
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24504
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24504 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -185,7 +185,7 @@ mark_as_advanced(Slicer_BUILD_MULTIVOLUME_SUPPORT)
 
 Slicer_Remote_Add(MultiVolumeExplorer
   GIT_REPOSITORY ${git_protocol}://github.com/fedorov/MultiVolumeExplorer.git
-  GIT_TAG 82d10b495d6cec19b6dd2fb015da958a974e4eae
+  GIT_TAG c0d56225f6dd48bbe381cb3d04b80dc502c85fe4
   OPTION_NAME Slicer_BUILD_MultiVolumeExplorer
   OPTION_DEPENDS "Slicer_BUILD_QTLOADABLEMODULES;Slicer_BUILD_MULTIVOLUME_SUPPORT;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
