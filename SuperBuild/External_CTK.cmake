30a1d44cc428398ed902e75094445618771d0b8b
Max Smolens <max.smolens@kitware.com>
2015-09-09 14:20:07 +0000

    ENH: Update CTK to include PythonQt cleanup fixes
    $ git shortlog 35a187ff5ae15f1c590a2ac317cd102c5746a81b..3884436e0e089360c900e786513f821281a00805 --no-merges
    Julien Finet (1):
          Fix compilation on Visual Studio
    
    Max Smolens (3):
          Update PythonQt to patched-6
          ctkAbstractPythonManager: Clean up PythonQt before finalizing the Python interpreter
          Update PythonQt to patched-5, which now includes selected updates from patched-6
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24524
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24524 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "35a187ff5ae15f1c590a2ac317cd102c5746a81b"
+    GIT_TAG "3884436e0e089360c900e786513f821281a00805"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
