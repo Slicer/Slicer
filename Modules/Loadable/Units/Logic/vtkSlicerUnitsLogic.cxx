Slicer/Slicer

commit d8b8dee0cbc7ade5131deec8a89d086e11319112

ENH: Adding extra units support (frequency, velocity and intensity)
Fixes #4017

Reviewed-by: Julien Finet <julien.finet@kitware.com>

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24413
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24413 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Units/Logic/vtkSlicerUnitsLogic.cxx
@@ -132,6 +132,18 @@ void vtkSlicerUnitsLogic::AddDefaultsUnits()
   node = this->AddUnitNode("ApplicationTime", "time", "", "s", 3);
   node->SetSaveWithScene(false);
   this->SetDefaultUnit(node->GetQuantity(), node->GetID());
+
+  node = this->AddUnitNode("ApplicationFrequency", "frequency", "", "Hz", 3);
+  node->SetSaveWithScene(false);
+  this->SetDefaultUnit(node->GetQuantity(), node->GetID());
+
+  node = this->AddUnitNode("ApplicationVelocity", "velocity", "", "m/s", 3);
+  node->SetSaveWithScene(false);
+  this->SetDefaultUnit(node->GetQuantity(), node->GetID());
+
+  node = this->AddUnitNode("ApplicationIntensity", "intensity", "", "W/m\xB2", 3);
+  node->SetSaveWithScene(false);
+  this->SetDefaultUnit(node->GetQuantity(), node->GetID());
 }
 
 //---------------------------------------------------------------------------
@@ -174,6 +186,28 @@ void vtkSlicerUnitsLogic::AddBuiltInUnits(vtkMRMLScene* scene)
   this->AddUnitNodeToScene(scene,
     "Microsecond", "time", "", "\xB5s", 3, -10000., 10000., 1000., 0.);
 
+  this->AddUnitNodeToScene(scene,
+    "Herz", "frequency", "", "Hz", 3, -10000., 10000., 1., 0.);
+  this->AddUnitNodeToScene(scene,
+    "decahertz", "frequency", "", "daHz", 3, -10000., 10000., 0.1, 0.);
+  this->AddUnitNodeToScene(scene,
+    "HectoHerz", "frequency", "", "hHz", 3, -10000., 10000., 0.01, 0.);
+  this->AddUnitNodeToScene(scene,
+    "KiloHerz", "frequency", "", "kHz", 3, -10000., 10000., 0.001, 0.);
+  this->AddUnitNodeToScene(scene,
+    "MegaHerz", "frequency", "", "MHz", 3, -10000., 10000., 0.000001, 0.);
+  this->AddUnitNodeToScene(scene,
+    "GigaHerz", "frequency", "", "GHz", 3, -10000., 10000., 0.000000001, 0.);
+  this->AddUnitNodeToScene(scene,
+    "TeraHerz", "frequency", "", "THz", 3, -10000., 10000., 0.000000000001, 0.);
+
+  this->AddUnitNodeToScene(scene,
+    "Metre per second", "velocity", "", "m/s", 3, -10000., 10000., 1., 0.);
+  this->AddUnitNodeToScene(scene,
+    "Kilometre per second", "velocity", "", "km/s", 3, -10000., 10000., 0.01, 0.);
+
+  this->AddUnitNodeToScene(scene,
+    "Intensity", "intensity", "", "W/m\xB2", 3, -10000., 10000., 1., 0.);
 }
 
 //-----------------------------------------------------------------------------

Modules/Loadable/Units/Widgets/Resources/UI/qMRMLUnitWidget.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>296</width>
-    <height>263</height>
+    <height>283</height>
    </rect>
   </property>
   <property name="windowTitle">

Modules/Loadable/Units/qSlicerUnitsSettingsPanel.cxx
@@ -145,6 +145,8 @@ void qSlicerUnitsSettingsPanelPrivate::addQuantity(const QString& quantity)
   groupboxTitle[0] = groupboxTitle[0].toUpper();
   groupbox->setTitle(groupboxTitle);
   QVBoxLayout* layout = new QVBoxLayout;
+  layout->setSizeConstraint(QLayout::SetMaximumSize);
+
   groupbox->setLayout(layout);
 
   // Add unit widget
@@ -191,7 +193,7 @@ void qSlicerUnitsSettingsPanelPrivate::setMRMLScene(vtkMRMLScene* scene)
   //q->registerProperty("Units", q, "quantities",
   //  SIGNAL(quantitiesChanged(QStringList)));
   QStringList quantities; // delete this when "un-hardcoding" quantities
-  quantities << "length" << "time";
+  quantities << "length" << "time" << "frequency" << "velocity" << "intensity";
   q->setQuantities(quantities);
 
   foreach (qMRMLSettingsUnitWidget* widget, this->Quantities.values())

commit 0ea3fbbe96a5cdc549ab3e08c33766cdc8938c6d

BUG: UnitLogic: Fix display coefficient associated with "velocity" unit.
This commit fixes a regression introduced in r24413 (ENH: Adding extra
units support (frequency, velocity and intensity) )

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24536
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24536 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Units/Logic/vtkSlicerUnitsLogic.cxx
@@ -204,7 +204,7 @@ void vtkSlicerUnitsLogic::AddBuiltInUnits(vtkMRMLScene* scene)
   this->AddUnitNodeToScene(scene,
     "Metre per second", "velocity", "", "m/s", 3, -10000., 10000., 1., 0.);
   this->AddUnitNodeToScene(scene,
-    "Kilometre per second", "velocity", "", "km/s", 3, -10000., 10000., 0.01, 0.);
+    "Kilometre per second", "velocity", "", "km/s", 3, -10000., 10000., 0.001, 0.);
 
   this->AddUnitNodeToScene(scene,
     "Intensity", "intensity", "", "W/m\xB2", 3, -10000., 10000., 1., 0.);