cb87bb38aa8669e726b91725d7a9fd0fcba2dbd2
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-10-06 05:14:48 +0000

    STYLE: Update CTK to fix "QSqlQuery::exec: database not open" message
    This commit update CTK to fix message reported when opening
    DICOM module.
    
    $ git shortlog 6453c27..7ea654e --no-merges
    Jean-Christophe Fillion-Robin (1):
          Fix runtime warning in ctkDICOMTableView
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24603
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24603 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "6453c27f5ba25c06a2b914bb71d1d3376b45afec"
+    GIT_TAG "7ea654e193b7d03d4186817b68a26a151fa129f0"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
