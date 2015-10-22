bfad91537181dd8d2284e3302412c9c620679e00
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-10-22 02:26:55 +0000

    ENH: Update ITK to release branch (v4.8.1 with VS2015 and c++11 fixes)
    $ git shortlog slicer-v4.8..2eb422b --no-merges
    Abhishek Tiwari (1):
          BUG: "tmpImageIndex" not initialized to 0.
    
    Davis Vigneault (2):
          DOC: Expanded TreeContainer Example
          COMP: Warnings in itkVTKImageExport when compiling with c++11
    
    Hyun Jae Kang (1):
          BUG: Fix HDF5 CMake configuration with VS2015
    
    Jean-Christophe Fillion-Robin (2):
          COMP: Fix Slicer MacOSX extension packaging setting CMP0042 policy to OLD
          COMP: Set policy CMP0042 only if CMake version support it.
    
    Matthew McCormick (12):
          BUG: Implement UpdateLargestPossibleRegion for ImageToVTKImageFilter.
          BUG: Initialize mutex for 32 bit AtomicInt.
          COMP: Disable the gold linker for static builds on Ubuntu 14.04.
          BUG: Build BridgeNumPy with Visual Studio.
          BUG: Support Python 3 wrapping module initialization.
          BUG: Update itkTemplate.py for Python 3.
          BUG: Update itkExtras.py for Python 3.
          BUG: Prevent Python callback addition recursion.
          BUG: Fix test scripts for Python 3.
          BUG: Replace __GCCXML__ with CABLE_CONFIGURATION.
          COMP: Do not use C++11 alignas in itkAlignedTypedef.
          ENH: Bump CMakeLists.txt version to 4.8.1.
    
    Michka Popoff (1):
          ENH: Do not link against specific Python library on OS X
    
    Shawn Waldon (1):
          COMP: Set target OS X version in environment for tools using compiler
    
    Tobias Wood (2):
          BUG: LBFGSB was printing messages even with debug switched off.
          BUG: Removed an unguarded print statement.
    
    Vladimir S. FONOV (2):
          BUG: Missing ITKIOMINC_EXPORT on __Private()
          COMP: Changing the order of HDF5 library components
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24653
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24653 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -30,7 +30,7 @@ if(NOT DEFINED ITK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   endif()
 
   set(ITKv4_REPOSITORY ${git_protocol}://github.com/Slicer/ITK.git)
-  set(ITKv4_GIT_TAG 06a56e6399905033cb12dab45104384a8461cdf7) # slicer-v4.8
+  set(ITKv4_GIT_TAG 2eb422bb91c688980a16a5821adf1e3af60f8aa5) # slicer-v4.8.1 + few patches
 
   set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
 
