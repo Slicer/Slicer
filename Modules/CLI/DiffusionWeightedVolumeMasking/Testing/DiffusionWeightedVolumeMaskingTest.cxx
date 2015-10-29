2421498515790ee4bcad220cf6a8393ff332d1ef
Max Smolens <max.smolens@kitware.com>
2015-10-29 22:23:28 +0000

    COMP: Fix DiffusionWeightedVolumeMaskingTest linker error
    Changes in r24673 [1] cause a linker error when building
    DiffusionWeightedVolumeMaskingTest in Visual Studio 2013:
    
        1>------ Build started: Project: DiffusionWeightedVolumeMaskingTest, Configuration: Debug x64 ------
        1>DiffusionWeightedVolumeMaskingTest.obj : error LNK2019: unresolved external symbol __imp_ModuleEntryPoint referenced in function "void __cdecl RegisterTests(void)" (?RegisterTests@@YAXXZ)
        1>C:\dev\SD\Slicer-build\bin\Debug\DiffusionWeightedVolumeMaskingTest.exe : fatal error LNK1120: 1 unresolved externals
    
    This commit reverts some of the changes to make the test build and run.
    
    [1] http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24673
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24677
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24677 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/CLI/DiffusionWeightedVolumeMasking/Testing/DiffusionWeightedVolumeMaskingTest.cxx
+++ Modules/CLI/DiffusionWeightedVolumeMasking/Testing/DiffusionWeightedVolumeMaskingTest.cxx
@@ -1,14 +1,10 @@
 #include "itkTestMain.h"
 
-#ifdef WIN32
-#define MODULE_IMPORT __declspec(dllimport)
-#else
-#define MODULE_IMPORT
-#endif
-
-extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char * []);
-
 void RegisterTests()
 {
-  StringToTestFunctionMap["ModuleEntryPoint"] = ModuleEntryPoint;
+  REGISTER_TEST(DiffusionWeightedVolumeMaskingTest);
 }
+
+#undef main
+#define main DiffusionWeightedVolumeMaskingTest
+#include "../DiffusionWeightedVolumeMasking.cxx"
