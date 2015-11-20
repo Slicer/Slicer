Slicer/Slicer

commit a33deda65e353d40c243cc7c5ea2165115db30f8

STYLE: Update python tests to use more specific asserts.
Using the more specific assert functions usually gives more useful
error messages.

Suggested-by: Csaba Pinter <csaba.pinter@queensu.ca>
Suggested-by: Max Smolens <max.smolens@kitware.com>

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24759
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24759 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/LabelStatistics/LabelStatistics.py
@@ -476,13 +476,13 @@ class LabelStatisticsTest(unittest.TestCase):
 
     self.delayDisplay("Warnings for mismatch:\n%s" % warnings)
 
-    self.assertTrue( warnings != "" )
+    self.assertNotEqual( warnings, "" )
 
     warnings = volumesLogic.CheckForLabelVolumeValidity(mrHead, mrHeadLabel)
 
     self.delayDisplay("Warnings for match:\n%s" % warnings)
 
-    self.assertTrue( warnings == "" )
+    self.assertNotEqual( warnings, "" )
 
     self.delayDisplay('test_LabelStatisticsBasic passed!')
 

Applications/SlicerApp/Testing/Python/AtlasTests.py
@@ -254,7 +254,7 @@ class AtlasTestsTest(unittest.TestCase):
 
     volumeNode = slicer.util.getNode(pattern=testVolumePattern)
     logic = AtlasTestsLogic()
-    self.assertTrue( logic.hasImageData(volumeNode) )
+    self.assertIsNotNone( logic.hasImageData(volumeNode) )
 
     m = slicer.util.mainWindow()
 

Applications/SlicerApp/Testing/Python/Charting.py
@@ -195,7 +195,7 @@ class ChartingTest(unittest.TestCase):
 
     #volumeNode = slicer.util.getNode(pattern="FA")
     #logic = ChartingLogic()
-    #self.assertTrue( logic.hasImageData(volumeNode) )
+    #self.assertIsNotNone( logic.hasImageData(volumeNode) )
 
     # Change the layout to one that has a chart.  This created the ChartView
     ln = slicer.util.getNode(pattern='vtkMRMLLayoutNode*')

Applications/SlicerApp/Testing/Python/DTINotReproducibleIssue3977.py
@@ -155,7 +155,7 @@ class DTINotReproducibleIssue3977Test(ScriptedLoadableModuleTest):
 
     volumeNode = slicer.util.getNode(pattern="dwi")
     logic = DTINotReproducibleIssue3977Logic()
-    self.assertTrue( logic.hasImageData(volumeNode) )
+    self.assertIsNotNone( logic.hasImageData(volumeNode) )
 
     # Run 1
     (outputBaseline, outputFA) = logic.computeFA(volumeNode)

Applications/SlicerApp/Testing/Python/FiducialLayoutSwitchBug1914.py
@@ -369,6 +369,6 @@ class FiducialLayoutSwitchBug1914Test(unittest.TestCase):
       slicer.util.delayDisplay('Test passed!')
     else:
       slicer.util.delayDisplay('Test failed!')
-    self.assertTrue(returnFlag == True)
+    self.assertTrue(returnFlag)
 
 

Applications/SlicerApp/Testing/Python/SliceLinkLogic.py
@@ -200,7 +200,7 @@ class SliceLinkLogicTest(unittest.TestCase):
 
     volumeNode = slicer.util.getNode(pattern="FA")
     logic = SliceLinkLogicLogic()
-    self.assertTrue( logic.hasImageData(volumeNode) )
+    self.assertIsNotNone( logic.hasImageData(volumeNode) )
 
     eps = 1e-2
     print('eps = ' + str(eps) + '\n')
@@ -227,17 +227,17 @@ class SliceLinkLogicTest(unittest.TestCase):
     logic.StartSliceCompositeNodeInteraction(1)  #ForegroundVolumeFlag
     compareCNode.SetForegroundVolumeID(volumeNode.GetID())
     logic.EndSliceCompositeNodeInteraction()
-    self.assertTrue( compareCNode.GetForegroundVolumeID() == volumeNode.GetID())
+    self.assertEqual( compareCNode.GetForegroundVolumeID(), volumeNode.GetID())
     print('')
 
     # Check that whether the volume was propagated
     self.delayDisplay('Broadcasted volume selection to all Compare Views')
     compareNode2 = slicer.util.getNode('vtkMRMLSliceNodeCompare2')
     compareCNode2 = slicer.util.getNode('vtkMRMLSliceCompositeNodeCompare2')
-    self.assertTrue(compareCNode2.GetForegroundVolumeID() == volumeNode.GetID())
+    self.assertEqual(compareCNode2.GetForegroundVolumeID(), volumeNode.GetID())
     compareNode3 = slicer.util.getNode('vtkMRMLSliceNodeCompare3')
     compareCNode3 = slicer.util.getNode('vtkMRMLSliceCompositeNodeCompare3')
-    self.assertTrue(compareCNode3.GetForegroundVolumeID() == volumeNode.GetID())
+    self.assertEqual(compareCNode3.GetForegroundVolumeID(), volumeNode.GetID())
     print('')
 
     # Set the orientation to axial
@@ -257,11 +257,11 @@ class SliceLinkLogicTest(unittest.TestCase):
     self.delayDisplay('Broadcasted a reset of the field of view to all Compare Views')
     diff = abs(compareNode2.GetFieldOfView()[1]-compareNode.GetFieldOfView()[1]) / compareNode.GetFieldOfView()[1]
     print "Field of view of comparison (y) between compare viewers #1 and #2: " + str(diff)
-    self.assertTrue(diff < eps)
+    self.assertLess(diff, eps)
 
     diff = abs(compareNode3.GetFieldOfView()[1]-compareNode.GetFieldOfView()[1]) / compareNode.GetFieldOfView()[1]
     print "Field of view of comparison (y) between compare viewers #1 and #3: " + str(diff)
-    self.assertTrue(diff < eps)
+    self.assertLess(diff, eps)
     print('')
 
     # Changed the number of lightboxes
@@ -276,11 +276,11 @@ class SliceLinkLogicTest(unittest.TestCase):
     self.delayDisplay('Changed the number of lightboxes')
     diff = abs(compareNode2.GetFieldOfView()[1]-compareNode.GetFieldOfView()[1]) / compareNode.GetFieldOfView()[1]
     print "Field of view of comparison (y) between compare viewers #1 and #2: " + str(diff)
-    self.assertTrue(diff < eps)
+    self.assertLess(diff, eps)
 
     diff = abs(compareNode3.GetFieldOfView()[1]-compareNode.GetFieldOfView()[1]) / compareNode.GetFieldOfView()[1]
     print "Field of view of comparison between compare viewers #1 and #3: " + str(diff)
-    self.assertTrue(diff < eps)
+    self.assertLess(diff, eps)
     print('')
 
     # Pan
@@ -292,11 +292,11 @@ class SliceLinkLogicTest(unittest.TestCase):
     self.delayDisplay('Broadcasted a pan to all Compare Views')
     diff = abs(compareNode2.GetXYZOrigin()[0]-compareNode.GetXYZOrigin()[0])
     print "Origin comparison (x) between compare viewers #1 and #2: " + str(diff)
-    self.assertTrue(diff < eps)
+    self.assertLess(diff, eps)
 
     diff = abs(compareNode3.GetXYZOrigin()[0]-compareNode.GetXYZOrigin()[0])
     print "Origin comparison (x) between compare viewers #1 and #3: " + str(diff)
-    self.assertTrue(diff < eps)
+    self.assertLess(diff, eps)
     print('')
 
     # Zoom
@@ -309,11 +309,11 @@ class SliceLinkLogicTest(unittest.TestCase):
     self.delayDisplay('Broadcasted a zoom to all Compare Views')
     diff = abs(compareNode2.GetFieldOfView()[0]-compareNode.GetFieldOfView()[0]) / compareNode.GetFieldOfView()[0]
     print "Field of view of comparison (x) between compare viewers #1 and #2: " + str(diff)
-    self.assertTrue(diff < eps)
+    self.assertLess(diff, eps)
 
     diff = abs(compareNode3.GetFieldOfView()[0]-compareNode.GetFieldOfView()[0]) / compareNode.GetFieldOfView()[0]
     print "Field of view of comparison (x) between compare viewers #1 and #3: " + str(diff)
-    self.assertTrue(diff < eps)
+    self.assertLess(diff, eps)
     print('')
 
     # Change the slice
@@ -323,11 +323,11 @@ class SliceLinkLogicTest(unittest.TestCase):
     self.delayDisplay('Broadcasted a change in slice offset to all Compare Views')
     diff = abs(compareNode2.GetSliceOffset()-compareNode.GetSliceOffset())
     print "Slice offset comparison between compare viewers #1 and #2: " + str(diff)
-    self.assertTrue(diff < eps)
+    self.assertLess(diff, eps)
 
     diff = abs(compareNode3.GetSliceOffset()-compareNode.GetSliceOffset())
     print "Slice offset comparison between compare viewers #1 and #3: " + str(diff)
-    self.assertTrue(diff < eps)
+    self.assertLess(diff, eps)
     print('')
 
     # Change the orientation
@@ -337,8 +337,8 @@ class SliceLinkLogicTest(unittest.TestCase):
     compareNode.UpdateMatrices()
     logic.EndSliceNodeInteraction()
     self.delayDisplay('Broadcasted a change in slice orientation to all Compare Views')
-    self.assertTrue(compareNode2.GetOrientationString() == compareNode.GetOrientationString())
-    self.assertTrue(compareNode3.GetOrientationString() == compareNode.GetOrientationString())
+    self.assertEqual(compareNode2.GetOrientationString(), compareNode.GetOrientationString())
+    self.assertEqual(compareNode3.GetOrientationString(), compareNode.GetOrientationString())
     print('')
 
     self.delayDisplay('Test passed!')

Applications/SlicerApp/Testing/Python/Slicer4Minute.py
@@ -199,7 +199,7 @@ class Slicer4MinuteTest(unittest.TestCase):
     # check volume is loaded out of scene
     volumeNode = slicer.util.getNode(pattern="grayscale")
     logic = Slicer4MinuteLogic()
-    self.assertTrue( logic.hasImageData(volumeNode) )
+    self.assertIsNotNone( logic.hasImageData(volumeNode) )
 
     # check the slice planes
     red = slicer.util.getNode(pattern="vtkMRMLSliceNode1")

Applications/SlicerApp/Testing/Python/SlicerMRBMultipleSaveRestoreLoopTest.py
@@ -115,12 +115,12 @@ execfile('/Users/pieper/slicer4/latest/Slicer/Applications/SlicerApp/Testing/Pyt
       # confirm that MRHead is in the background of the Red slice
       redComposite = slicer.util.getNode('vtkMRMLSliceCompositeNodeRed')
       mrHead = slicer.util.getNode('MRHead')
-      self.assertTrue( redComposite.GetBackgroundVolumeID() == mrHead.GetID() )
+      self.assertEqual(redComposite.GetBackgroundVolumeID(), mrHead.GetID())
       self.delayDisplay('The MRHead volume is AGAIN in the background of the Red viewer')
 
       # confirm that the fiducial list exists with 1 points
       fidNode = slicer.util.getNode('F')
-      self.assertTrue(fidNode.GetNumberOfFiducials() == 1)
+      self.assertEqual(fidNode.GetNumberOfFiducials(), 1)
       self.delayDisplay('The fiducial list has 1 point in it')
 
       # adjust the fid list location
@@ -134,7 +134,7 @@ execfile('/Users/pieper/slicer4/latest/Slicer/Applications/SlicerApp/Testing/Pyt
     finalFiducialPosition = [ 0,0,0 ]
     fidNode.GetNthFiducialPosition(0, finalFiducialPosition)
     print 'Final fiducial scene pos = ',finalFiducialPosition
-    self.assertTrue(self.fiducialPosition == finalFiducialPosition)
+    self.assertEqual(self.fiducialPosition, finalFiducialPosition)
 
     self.delayDisplay("Test Finished")
 

commit 4c5d695e4f50911178a61c18730962de8ecf7de8

BUG: Fix regression in LabelStatisticsTest
This commit fixes a regression introduced in r24759 (STYLE: Update python
tests to use more specific asserts.) where

  self.assertTrue( warnings == "" )

has improperly changed into

  self.assertNotEqual( warnings, "" )

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24761
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24761 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/LabelStatistics/LabelStatistics.py
@@ -482,7 +482,7 @@ class LabelStatisticsTest(unittest.TestCase):
 
     self.delayDisplay("Warnings for match:\n%s" % warnings)
 
-    self.assertNotEqual( warnings, "" )
+    self.assertEqual( warnings, "" )
 
     self.delayDisplay('test_LabelStatisticsBasic passed!')
 