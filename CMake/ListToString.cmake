bd71eb602c89ea83e36a8caf4ce122f23acabe67
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-11-28 04:23:48 +0000

    STYLE: ListToString: Update function to not set CMP0007 to OLD
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24778
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24778 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/ListToString.cmake
+++ CMake/ListToString.cmake
@@ -18,10 +18,10 @@
 #
 ################################################################################
 
+cmake_minimum_required(VERSION 2.8.9)
+
 function(list_to_string separator input_list output_string_var)
   set(_string "")
-  cmake_policy(PUSH)
-  cmake_policy(SET CMP0007 OLD)
   # Get list length
   list(LENGTH input_list list_length)
   # If the list has 0 or 1 element, there is no need to loop over.
@@ -32,6 +32,10 @@ function(list_to_string separator input_list output_string_var)
     foreach(index RANGE ${last_element_index})
       # Get current item_value
       list(GET input_list ${index} item_value)
+      # but ignore empty value
+      if(item_value STREQUAL "")
+        continue()
+      endif()
       # .. and append to output string
       set(_string  "${_string}${item_value}")
       # Append separator if current element is NOT the last one.
@@ -41,7 +45,6 @@ function(list_to_string separator input_list output_string_var)
     endforeach()
   endif()
   set(${output_string_var} ${_string} PARENT_SCOPE)
-  cmake_policy(POP)
 endfunction()
 
 
