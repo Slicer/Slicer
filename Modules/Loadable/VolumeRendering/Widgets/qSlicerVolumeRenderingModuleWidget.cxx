Slicer/Slicer

commit cc0a5ca1d649b47f2ea012245629b9a98601dd7e

ENH: Support VR ROI visibility update through MRML node
This commit ensures the setting the ROI node DisplayVisibility property
show/hide the ROI and update the ROI visibility checkbox state.

Reviewed-by: Andras Lasso <lasso@queensu.ca>
Reviewed-by: Nicole Aucoin <nicole@bwh.harvard.edu>
Reviewed-by: Steve Pieper <pieper@bwh.harvard.edu>
Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24803
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24803 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/VolumeRendering/Widgets/qSlicerVolumeRenderingModuleWidget.cxx
@@ -487,6 +487,21 @@ void qSlicerVolumeRenderingModuleWidget::updateFromMRMLDisplayNode()
     }
 }
 
+
+// --------------------------------------------------------------------------
+void qSlicerVolumeRenderingModuleWidget::updateFromMRMLDisplayROINode()
+{
+  Q_D(qSlicerVolumeRenderingModuleWidget);
+  if (!d->ROIWidget->mrmlROINode())
+    {
+    return;
+    }
+  //ROI visibility
+  d->ROICropDisplayCheckBox->setChecked(
+        d->ROIWidget->mrmlROINode()->GetDisplayVisibility());
+}
+
+
 // --------------------------------------------------------------------------
 void qSlicerVolumeRenderingModuleWidget::addVolumeIntoView(vtkMRMLNode* viewNode)
 {
@@ -596,7 +611,12 @@ void qSlicerVolumeRenderingModuleWidget::onCurrentMRMLROINodeChanged(vtkMRMLNode
     return;
     }
   vtkMRMLAnnotationROINode *roiNode = vtkMRMLAnnotationROINode::SafeDownCast(node);
+  this->qvtkReconnect(d->DisplayNode->GetROINode(), roiNode,
+                        vtkMRMLDisplayableNode::DisplayModifiedEvent,
+                        this, SLOT(updateFromMRMLDisplayROINode()));
+
   d->DisplayNode->SetAndObserveROINodeID(roiNode ? roiNode->GetID() : 0);
+  this->updateFromMRMLDisplayROINode();
 }
 
 // --------------------------------------------------------------------------
@@ -805,6 +825,24 @@ void qSlicerVolumeRenderingModuleWidget
   // cropping (to follow the "what you see is what you get" pattern).
   if (toggle)
     {
-    d->ROICropCheckBox->setChecked(true);
+    d->DisplayNode->SetCroppingEnabled(toggle);
+    }
+
+  int numberOfDisplayNodes =
+      d->ROIWidget->mrmlROINode()->GetNumberOfDisplayNodes();
+  int wasModifying[numberOfDisplayNodes];
+
+  for(int index = 0; index < numberOfDisplayNodes; index++)
+    {
+    wasModifying[index] =
+        d->ROIWidget->mrmlROINode()->GetNthDisplayNode(index)->StartModify();
+    }
+
+  d->ROIWidget->mrmlROINode()->SetDisplayVisibility(toggle);
+
+  for(int index = 0; index < numberOfDisplayNodes; index++)
+    {
+    d->ROIWidget->mrmlROINode()->GetNthDisplayNode(index)->EndModify(
+          wasModifying[index]);
     }
 }

Modules/Loadable/Annotations/Widgets/qMRMLAnnotationROIWidget.cxx
@@ -26,6 +26,7 @@
 
 // MRML includes
 #include <vtkMRMLAnnotationROINode.h>
+#include <vtkMRMLDisplayNode.h>
 
 // 0.001 because the sliders only handle 2 decimals
 #define SLIDERS_EPSILON 0.001
@@ -100,11 +101,17 @@ vtkMRMLAnnotationROINode* qMRMLAnnotationROIWidget::mrmlROINode()const
 void qMRMLAnnotationROIWidget::setMRMLAnnotationROINode(vtkMRMLAnnotationROINode* roiNode)
 {
   Q_D(qMRMLAnnotationROIWidget);
-  qvtkReconnect(d->ROINode, roiNode, vtkCommand::ModifiedEvent,
+
+  this->qvtkReconnect(d->ROINode, roiNode, vtkCommand::ModifiedEvent,
                 this, SLOT(onMRMLNodeModified()));
 
+  this->qvtkReconnect(d->ROINode, roiNode, vtkMRMLDisplayableNode::DisplayModifiedEvent,
+                      this, SLOT(onMRMLDisplayNodeModified()));
+
   d->ROINode = roiNode;
+
   this->onMRMLNodeModified();
+  this->onMRMLDisplayNodeModified();
   this->setEnabled(roiNode != 0);
 }
 
@@ -125,9 +132,6 @@ void qMRMLAnnotationROIWidget::onMRMLNodeModified()
     }
 
   d->IsProcessingOnMRMLNodeModified = true;
-
-  // Visibility
-  d->DisplayClippingBoxButton->setChecked(d->ROINode->GetDisplayVisibility());
 
   // Interactive Mode
   bool interactive = d->ROINode->GetInteractiveMode();
@@ -191,8 +195,21 @@ void qMRMLAnnotationROIWidget::setExtent(double minLR, double maxLR,
 void qMRMLAnnotationROIWidget::setDisplayClippingBox(bool visible)
 {
   Q_D(qMRMLAnnotationROIWidget);
+
+  int numberOfDisplayNodes = d->ROINode->GetNumberOfDisplayNodes();
+  int wasModifying[numberOfDisplayNodes];
+
+  for(int index = 0; index < numberOfDisplayNodes; index++)
+    {
+    wasModifying[index] = d->ROINode->GetNthDisplayNode(index)->StartModify();
+    }
+
   d->ROINode->SetDisplayVisibility(visible);
-  emit displayClippingBoxChanged(visible);
+
+  for(int index = 0; index < numberOfDisplayNodes; index++)
+    {
+    d->ROINode->GetNthDisplayNode(index)->EndModify(wasModifying[index]);
+    }
 }
 
 // --------------------------------------------------------------------------
@@ -228,4 +245,18 @@ void qMRMLAnnotationROIWidget::updateROI()
                            0.5*(bounds[3]-bounds[2]),
                            0.5*(bounds[5]-bounds[4]));
   d->ROINode->EndModify(wasModifying);
+}
+
+// --------------------------------------------------------------------------
+void qMRMLAnnotationROIWidget::onMRMLDisplayNodeModified()
+{
+  Q_D(qMRMLAnnotationROIWidget);
+
+  if (!d->ROINode)
+    {
+    return;
+    }
+
+  // Visibility
+  d->DisplayClippingBoxButton->setChecked(d->ROINode->GetDisplayVisibility());
 }

Modules/Loadable/Annotations/Widgets/qMRMLAnnotationROIWidget.h
@@ -75,6 +75,8 @@ protected slots:
   void onMRMLNodeModified();
   /// Internal function to update the ROI node based on the sliders
   void updateROI();
+  /// Internal function to update the ROIDisplay node
+  void onMRMLDisplayNodeModified();
 
 protected:
   QScopedPointer<qMRMLAnnotationROIWidgetPrivate> d_ptr;

Modules/Loadable/VolumeRendering/Resources/UI/qSlicerVolumeRenderingModuleWidget.ui
@@ -555,7 +555,7 @@
   </customwidget>
   <customwidget>
    <class>ctkCheckablePushButton</class>
-   <extends>QPushButton</extends>
+   <extends>ctkPushButton</extends>
    <header>ctkCheckablePushButton.h</header>
   </customwidget>
   <customwidget>
@@ -584,6 +584,11 @@
    <class>ctkExpandButton</class>
    <extends>QToolButton</extends>
    <header>ctkExpandButton.h</header>
+  </customwidget>
+  <customwidget>
+   <class>ctkPushButton</class>
+   <extends>QPushButton</extends>
+   <header>ctkPushButton.h</header>
   </customwidget>
   <customwidget>
    <class>ctkSliderWidget</class>
@@ -817,38 +822,6 @@
     <hint type="destinationlabel">
      <x>230</x>
      <y>732</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>ROICropDisplayCheckBox</sender>
-   <signal>toggled(bool)</signal>
-   <receiver>ROIWidget</receiver>
-   <slot>setDisplayClippingBox(bool)</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>238</x>
-     <y>193</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>114</x>
-     <y>541</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>ROIWidget</sender>
-   <signal>displayClippingBoxChanged(bool)</signal>
-   <receiver>ROICropDisplayCheckBox</receiver>
-   <slot>setChecked(bool)</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>114</x>
-     <y>541</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>238</x>
-     <y>193</y>
     </hint>
    </hints>
   </connection>

Modules/Loadable/VolumeRendering/Widgets/qSlicerVolumeRenderingModuleWidget.h
@@ -96,6 +96,7 @@ protected slots:
   void resetOffset();
   void updatePresetSliderRange();
   void updateFromMRMLDisplayNode();
+  void updateFromMRMLDisplayROINode();
 
   void synchronizeScalarDisplayNode();
   void setFollowVolumeDisplayNode(bool);

commit 29ee7d1d0e291a545e255fa695da3e8b93e04168

COMP: qSlicerVolumeRenderingModuleWidget: Fix Visual Studio build
This commit fixes a regression introduced in r24803 (ENH: Support VR
ROI visibility update through MRML node) removing the use of Variable
Length Array allocated (VLA) on the stack that are allowed in gcc but not
supported in Visual Studio.

For more details, see [1][2]

[1] http://stackoverflow.com/questions/7812566/why-no-variable-size-array-in-stack/7812587#7812587
[2] https://msdn.microsoft.com/library/zb1574zs(v=vs.100).aspx

Error reported by Visual Studio (VS) was the following:

    Modules\Loadable\VolumeRendering\Widgets\qSlicerVolumeRenderingModuleWidget.cxx(833): error C2057: expected constant expression
    Modules\Loadable\VolumeRendering\Widgets\qSlicerVolumeRenderingModuleWidget.cxx(833): error C2466: cannot allocate an array of constant size 0
    Modules\Loadable\VolumeRendering\Widgets\qSlicerVolumeRenderingModuleWidget.cxx(833): error C2133: 'wasModifying' : unknown size

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24806
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24806 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/VolumeRendering/Widgets/qSlicerVolumeRenderingModuleWidget.cxx
@@ -52,6 +52,7 @@
 
 // STD includes
 #include <cassert>
+#include <vector>
 
 //-----------------------------------------------------------------------------
 /// \ingroup Slicer_QtModules_VolumeRendering
@@ -830,7 +831,9 @@ void qSlicerVolumeRenderingModuleWidget
 
   int numberOfDisplayNodes =
       d->ROIWidget->mrmlROINode()->GetNumberOfDisplayNodes();
-  int wasModifying[numberOfDisplayNodes];
+
+  std::vector<int> wasModifying;
+  wasModifying.resize(numberOfDisplayNodes);
 
   for(int index = 0; index < numberOfDisplayNodes; index++)
     {