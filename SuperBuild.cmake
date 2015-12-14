cbc3fa74466b742fbb4343d577928baa0504c3e7
Fedorov <fedorov@bwh.harvard.edu>
2015-12-14 21:46:47 +0000

    ENH: update git hash of MultiVolumeImporter
    this fixes incorrect parsing of Philips trace DWI datasets that used standard
    DICOM attribute for storing b-values. The issue was due to a typo in the attribute
    number...
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24817
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24817 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -194,7 +194,7 @@ list_conditional_append(Slicer_BUILD_MultiVolumeExplorer Slicer_REMOTE_DEPENDENC
 
 Slicer_Remote_Add(MultiVolumeImporter
   GIT_REPOSITORY ${git_protocol}://github.com/fedorov/MultiVolumeImporter.git
-  GIT_TAG 95b399d8df992b1fbb696fafc302be4e4bcf1631
+  GIT_TAG 4cdbbe301554a46d4bfae87625ab415bb4bbee01
   OPTION_NAME Slicer_BUILD_MultiVolumeImporter
   OPTION_DEPENDS "Slicer_BUILD_QTLOADABLEMODULES;Slicer_BUILD_MULTIVOLUME_SUPPORT;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
