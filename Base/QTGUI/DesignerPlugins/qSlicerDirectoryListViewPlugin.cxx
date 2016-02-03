dc1572099afbd10ff71635ea00693a5193182722
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-02-03 04:58:06 +0000

    COMP: Fix compilation adding missing qSlicerDirectoryListViewPlugin sources.
    This commit fixes a regression introduced in r24914 (ENH: Allow adding
    module directories by drag-and-drop).
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24916
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24916 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Base/QTGUI/DesignerPlugins/qSlicerDirectoryListViewPlugin.cxx
@@ -0,0 +1,55 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) Kitware Inc.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+  and was partially funded by NIH grant 1U24CA194354-01
+
+==============================================================================*/
+
+#include "qSlicerDirectoryListViewPlugin.h"
+#include "qSlicerDirectoryListView.h"
+
+qSlicerDirectoryListViewPlugin::qSlicerDirectoryListViewPlugin(QObject* parent)
+  : qSlicerQTGUIAbstractPlugin(parent)
+{
+}
+
+QWidget *qSlicerDirectoryListViewPlugin::createWidget(QWidget* parentWidget)
+{
+  qSlicerDirectoryListView* widget = new qSlicerDirectoryListView(parentWidget);
+  return widget;
+}
+
+QString qSlicerDirectoryListViewPlugin::domXml() const
+{
+  return "<widget class=\"qSlicerDirectoryListView\" \
+          name=\"SlicerDirectoryListView\">\n"
+          "</widget>\n";
+}
+
+QString qSlicerDirectoryListViewPlugin::includeFile() const
+{
+  return "qSlicerDirectoryListView.h";
+}
+
+bool qSlicerDirectoryListViewPlugin::isContainer() const
+{
+  return false;
+}
+
+QString qSlicerDirectoryListViewPlugin::name() const
+{
+  return "qSlicerDirectoryListView";
+}
---
+++ Base/QTGUI/DesignerPlugins/qSlicerDirectoryListViewPlugin.h
@@ -0,0 +1,40 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) Kitware Inc.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+  and was partially funded by NIH grant 1U24CA194354-01
+
+==============================================================================*/
+
+#ifndef __qSlicerDirectoryListViewPlugin_h
+#define __qSlicerDirectoryListViewPlugin_h
+
+#include "qSlicerQTGUIAbstractPlugin.h"
+
+class Q_SLICER_DESIGNER_PLUGINS_EXPORT qSlicerDirectoryListViewPlugin
+  : public qSlicerQTGUIAbstractPlugin
+{
+  Q_OBJECT
+public:
+  qSlicerDirectoryListViewPlugin(QObject* parent = 0);
+
+  QWidget *createWidget(QWidget *_parent);
+  QString domXml() const;
+  QString includeFile() const;
+  bool isContainer() const;
+  QString name() const;
+};
+
+#endif
