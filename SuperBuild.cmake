bf55c71e24ed321a754504f9ad38aaf79099872a
Fedorov <fedorov@bwh.harvard.edu>
2016-02-10 21:50:45 +0000

    BUG: update MultiVolumeImporter hash
    This fixes a bug where plugin would fail with certain strategy when a certain
    DICOM attribute is absent or empty
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24927
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24927 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -194,7 +194,7 @@ list_conditional_append(Slicer_BUILD_MultiVolumeExplorer Slicer_REMOTE_DEPENDENC
 
 Slicer_Remote_Add(MultiVolumeImporter
   GIT_REPOSITORY ${git_protocol}://github.com/fedorov/MultiVolumeImporter.git
-  GIT_TAG 227f8d58b440ccec21f434ebc3b75941ae48ab5c
+  GIT_TAG f1dfd21d1804bae22a308a97772f7f83eef363df
   OPTION_NAME Slicer_BUILD_MultiVolumeImporter
   OPTION_DEPENDS "Slicer_BUILD_QTLOADABLEMODULES;Slicer_BUILD_MULTIVOLUME_SUPPORT;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
