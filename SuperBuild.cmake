17dbee89a6f29a7031b38fedc2df47e7cbf5a033
Andras Lasso <lasso@queensu.ca>
2016-02-25 21:19:22 +0000

    ENH: update SlicerDMRI remote module to a82fde48
    $ git shortlog --no-merges d712d9522a..a82fde4870
    
    Isaiah Norton (3):
          DWMasking: set b-value select slider max to 20k
          DWMasking: COMP: fix test that was using old parameter
          COMP: fix windows build error due to outdated CLI entrypoint
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24949
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24949 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -308,7 +308,7 @@ Slicer_Remote_Add(SlicerDMRI
   GIT_REPOSITORY "${git_protocol}://github.com/SlicerDMRI/SlicerDMRI"
   # SlicerDMRI Maintainer: If new revision of the extension add or remove modules, consider updating
   #                        the module lists below. Thanks.
-  GIT_TAG "d712d9522a61fc855aa21ac899dc0656d6fb42a7"
+  GIT_TAG "a82fde4870cbf03d8e418b01464cc23d5891d98f"
   OPTION_NAME Slicer_BUILD_SlicerDMRI
   OPTION_DEPENDS "Slicer_BUILD_DIFFUSION_SUPPORT"
   LABELS REMOTE_EXTENSION
