b1cd4200d7676bd9f1234ab7d1a7b0022a4e0f76
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-03-30 23:31:24 +0000

    ENH: Update DCMTK to include patch avoiding unneeded recompilation.
    $ git shortlog --no-merges 0b5b174..023b8de
    Jean-Christophe Fillion-Robin (1):
          CMake: Avoid unneeded recompilation improving generation of 'arith.h'
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24993
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@24993 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_DCMTK.cmake
+++ SuperBuild/External_DCMTK.cmake
@@ -33,9 +33,9 @@ if(NOT DEFINED DCMTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       )
   endif()
 
-  # DCMTK-3.6.1_20160216
-  set(${proj}_REPOSITORY ${git_protocol}://git.dcmtk.org/dcmtk)
-  set(${proj}_GIT_TAG "DCMTK-3.6.1_20160216")
+  # DCMTK-3.6.1_20160216 + patch to avoid unneeded recompilation
+  set(${proj}_REPOSITORY ${git_protocol}://github.com/commontk/DCMTK)
+  set(${proj}_GIT_TAG "023b8deab3b64bdcf4e40544b40bdbdbbd05e7a3")
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
