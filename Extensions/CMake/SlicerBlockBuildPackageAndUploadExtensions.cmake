0bbee4633ca4c63b8c9f8661a134e00484387e01
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-05-02 06:41:14 +0000

    STYLE: BuildPackageAndUploadExtensions: Simplify setting of extension name
    Simplify the code removing the extraction of the extension name from
    the description file. Description file path was already set using the
    extension name. Reduce code complexity by reusing the variable.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25036
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25036 3bd1e089-480b-0410-8dfb-8563597acbee

--- Extensions/CMake/SlicerBlockBuildPackageAndUploadExtensions.cmake
+++ Extensions/CMake/SlicerBlockBuildPackageAndUploadExtensions.cmake
@@ -75,9 +75,9 @@ endforeach()
 include(TopologicalSort)
 topological_sort(EXTENSION_LIST "EXTENSION_" "_DEPENDS")
 
-foreach(extension_name ${EXTENSION_LIST})
-  # Set extension description filename using extension_name
-  set(file ${Slicer_EXTENSION_DESCRIPTION_DIR}/${extension_name}.s4ext)
+foreach(EXTENSION_NAME ${EXTENSION_LIST})
+  # Set extension description filename using EXTENSION_NAME
+  set(file ${Slicer_EXTENSION_DESCRIPTION_DIR}/${EXTENSION_NAME}.s4ext)
 
   # Extract extension description info
   slicerFunctionExtractExtensionDescription(EXTENSION_FILE ${file} VAR_PREFIX EXTENSION)
@@ -96,154 +96,169 @@ foreach(extension_name ${EXTENSION_LIST})
   #  message(${v}:${EXTENSION_EXT_${v}})
   #endforeach()
 
-  # Extract file basename
-  get_filename_component(EXTENSION_NAME ${file} NAME_WE)
-  if("${EXTENSION_NAME}" STREQUAL "")
-    message(WARNING "Failed to extract extension name associated with file: ${file}")
+  message(STATUS "Configuring extension: ${EXTENSION_NAME}")
+  if("${EXTENSION_EXT_SCM}" STREQUAL "" AND "${EXTENSION_EXT_SCMURL}" STREQUAL "")
+    message(WARNING "Failed to extract extension information associated to file: ${file}")
   else()
-    message(STATUS "Configuring extension: ${EXTENSION_NAME}")
-    if("${EXTENSION_EXT_SCM}" STREQUAL "" AND "${EXTENSION_EXT_SCMURL}" STREQUAL "")
-      message(WARNING "Failed to extract extension information associated to file: ${file}")
-    else()
-      set(ext_add_project True)
-      set(ext_ep_options_repository)
-      set(ext_ep_cmake_args)
-      set(ext_revision ${EXTENSION_EXT_SCMREVISION})
-      if("${EXTENSION_EXT_SCM}" STREQUAL "git")
-        set(EXTENSION_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/${EXTENSION_NAME})
-        if("${ext_revision}" STREQUAL "")
-          set(ext_revision "origin/master")
-        endif()
-        set(ext_ep_options_repository
-          GIT_REPOSITORY ${EXTENSION_EXT_SCMURL} GIT_TAG ${ext_revision})
-        list(APPEND ext_ep_cmake_args
-           -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE})
-      elseif("${EXTENSION_EXT_SCM}" STREQUAL "svn")
-        set(EXTENSION_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/${EXTENSION_NAME})
-        if("${ext_revision}" STREQUAL "")
-          set(ext_revision "HEAD")
-        endif()
-        set(ext_ep_options_repository
-          SVN_REPOSITORY ${EXTENSION_EXT_SCMURL} SVN_REVISION -r ${ext_revision})
-        list(APPEND ext_ep_cmake_args
-           -DSubversion_SVN_EXECUTABLE:FILEPATH=${Subversion_SVN_EXECUTABLE})
-        if(NOT ${EXTENSION_EXT_SVNUSERNAME} STREQUAL "")
-           list(APPEND ext_ep_options_repository
-             SVN_USERNAME "${EXTENSION_EXT_SVNUSERNAME}"
-             SVN_PASSWORD "${EXTENSION_EXT_SVNPASSWORD}"
-             SVN_TRUST_CERT 1
-             )
-        endif()
-      elseif("${EXTENSION_EXT_SCM}" STREQUAL "local")
-        set(ext_ep_options_repository DOWNLOAD_COMMAND "")
-        set(EXTENSION_SOURCE_DIR ${EXTENSION_EXT_SCMURL})
-        if(NOT EXISTS ${EXTENSION_SOURCE_DIR})
-          set(EXTENSION_SOURCE_DIR ${Slicer_LOCAL_EXTENSIONS_DIR}/${EXTENSION_SOURCE_DIR})
-        endif()
-      else()
-        set(ext_add_project False)
-        message(WARNING "Unknown type of SCM [${EXTENSION_EXT_SCM}] associated with extension named ${EXTENSION_NAME} - See file ${file}")
+    set(ext_add_project True)
+    set(ext_ep_options_repository)
+    set(ext_ep_cmake_args)
+    set(ext_revision ${EXTENSION_EXT_SCMREVISION})
+    if("${EXTENSION_EXT_SCM}" STREQUAL "git")
+      set(EXTENSION_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/${EXTENSION_NAME})
+      if("${ext_revision}" STREQUAL "")
+        set(ext_revision "origin/master")
       endif()
-      if(ext_add_project)
-        # Set external project DEPENDS parameter
-        set(EP_ARG_EXTENSION_DEPENDS)
-        if(Slicer_SOURCE_DIR)
-          set(EXTENSIONEP_ARG_EXTENSION_DEPENDS DEPENDS Slicer ${EXTENSION_DEPENDS})
-        else()
-          if(NOT "${EXTENSION_DEPENDS}" STREQUAL "")
-            set(EP_ARG_EXTENSION_DEPENDS DEPENDS ${EXTENSION_DEPENDS})
-          endif()
-        endif()
-        set(EXTENSION_REBUILD_DEPENDS)
+      set(ext_ep_options_repository
+        GIT_REPOSITORY ${EXTENSION_EXT_SCMURL} GIT_TAG ${ext_revision})
+      list(APPEND ext_ep_cmake_args
+         -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE})
+    elseif("${EXTENSION_EXT_SCM}" STREQUAL "svn")
+      set(EXTENSION_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/${EXTENSION_NAME})
+      if("${ext_revision}" STREQUAL "")
+        set(ext_revision "HEAD")
+      endif()
+      set(ext_ep_options_repository
+        SVN_REPOSITORY ${EXTENSION_EXT_SCMURL} SVN_REVISION -r ${ext_revision})
+      list(APPEND ext_ep_cmake_args
+         -DSubversion_SVN_EXECUTABLE:FILEPATH=${Subversion_SVN_EXECUTABLE})
+      if(NOT ${EXTENSION_EXT_SVNUSERNAME} STREQUAL "")
+         list(APPEND ext_ep_options_repository
+           SVN_USERNAME "${EXTENSION_EXT_SVNUSERNAME}"
+           SVN_PASSWORD "${EXTENSION_EXT_SVNPASSWORD}"
+           SVN_TRUST_CERT 1
+           )
+      endif()
+    elseif("${EXTENSION_EXT_SCM}" STREQUAL "local")
+      set(ext_ep_options_repository DOWNLOAD_COMMAND "")
+      set(EXTENSION_SOURCE_DIR ${EXTENSION_EXT_SCMURL})
+      if(NOT EXISTS ${EXTENSION_SOURCE_DIR})
+        set(EXTENSION_SOURCE_DIR ${Slicer_LOCAL_EXTENSIONS_DIR}/${EXTENSION_SOURCE_DIR})
+      endif()
+    else()
+      set(ext_add_project False)
+      message(WARNING "Unknown type of SCM [${EXTENSION_EXT_SCM}] associated with extension named ${EXTENSION_NAME} - See file ${file}")
+    endif()
+    if(ext_add_project)
+      # Set external project DEPENDS parameter
+      set(EP_ARG_EXTENSION_DEPENDS)
+      if(Slicer_SOURCE_DIR)
+        set(EXTENSIONEP_ARG_EXTENSION_DEPENDS DEPENDS Slicer ${EXTENSION_DEPENDS})
+      else()
         if(NOT "${EXTENSION_DEPENDS}" STREQUAL "")
-          set(EP_ARG_EXTENSION_REBUILD_DEPENDS DEPENDS)
-          foreach(dep ${EXTENSION_DEPENDS})
-            list(APPEND EP_ARG_EXTENSION_REBUILD_DEPENDS ${dep}-rebuild)
-          endforeach()
+          set(EP_ARG_EXTENSION_DEPENDS DEPENDS ${EXTENSION_DEPENDS})
         endif()
-        if(Slicer_UPLOAD_EXTENSIONS)
+      endif()
+      set(EXTENSION_REBUILD_DEPENDS)
+      if(NOT "${EXTENSION_DEPENDS}" STREQUAL "")
+        set(EP_ARG_EXTENSION_REBUILD_DEPENDS DEPENDS)
+        foreach(dep ${EXTENSION_DEPENDS})
+          list(APPEND EP_ARG_EXTENSION_REBUILD_DEPENDS ${dep}-rebuild)
+        endforeach()
+      endif()
+      if(Slicer_UPLOAD_EXTENSIONS)
 
-          #-----------------------------------------------------------------------------
-          # Slicer_UPLOAD_EXTENSIONS: TRUE
-          #-----------------------------------------------------------------------------
-          set(EXTENSION_SUPERBUILD_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${EXTENSION_NAME}-build)
-          set(EXTENSION_BUILD_SUBDIRECTORY ${EXTENSION_EXT_BUILD_SUBDIRECTORY})
-          if(NOT DEFINED CTEST_MODEL)
-            set(CTEST_MODEL "Experimental")
-          endif()
-          include(SlicerBlockUploadExtension)
-          # Add extension external project
-          set(proj ${EXTENSION_NAME})
-          ExternalProject_Add(${proj}
+        #-----------------------------------------------------------------------------
+        # Slicer_UPLOAD_EXTENSIONS: TRUE
+        #-----------------------------------------------------------------------------
+        set(EXTENSION_SUPERBUILD_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/${EXTENSION_NAME}-build)
+        set(EXTENSION_BUILD_SUBDIRECTORY ${EXTENSION_EXT_BUILD_SUBDIRECTORY})
+        if(NOT DEFINED CTEST_MODEL)
+          set(CTEST_MODEL "Experimental")
+        endif()
+        include(SlicerBlockUploadExtension)
+        # Add extension external project
+        set(proj ${EXTENSION_NAME})
+        ExternalProject_Add(${proj}
+          ${ext_ep_options_repository}
+          SOURCE_DIR ${EXTENSION_SOURCE_DIR}
+          BINARY_DIR ${EXTENSION_NAME}-build
+          CONFIGURE_COMMAND ""
+          BUILD_COMMAND ${EXTENSION_UPLOAD_COMMAND}
+          INSTALL_COMMAND ""
+          ${EP_ARG_EXTENSION_DEPENDS}
+          )
+        # This custom external project step forces the build and later
+        # steps to run whenever a top level build is done...
+        ExternalProject_Add_Step(${proj} forcebuild
+          DEPENDEES configure
+          DEPENDERS build
+          ALWAYS 1
+          )
+        if(Slicer_SOURCE_DIR)
+          # Add convenient external project allowing to build the extension
+          # independently of Slicer
+          ExternalProject_Add(${proj}-rebuild
             ${ext_ep_options_repository}
             SOURCE_DIR ${EXTENSION_SOURCE_DIR}
             BINARY_DIR ${EXTENSION_NAME}-build
             CONFIGURE_COMMAND ""
             BUILD_COMMAND ${EXTENSION_UPLOAD_COMMAND}
             INSTALL_COMMAND ""
-            ${EP_ARG_EXTENSION_DEPENDS}
+            ${EP_ARG_EXTENSION_REBUILD_DEPENDS}
             )
           # This custom external project step forces the build and later
           # steps to run whenever a top level build is done...
-          ExternalProject_Add_Step(${proj} forcebuild
+          ExternalProject_Add_Step(${proj}-rebuild forcebuild
             DEPENDEES configure
             DEPENDERS build
             ALWAYS 1
             )
-          if(Slicer_SOURCE_DIR)
-            # Add convenient external project allowing to build the extension
-            # independently of Slicer
-            ExternalProject_Add(${proj}-rebuild
-              ${ext_ep_options_repository}
-              SOURCE_DIR ${EXTENSION_SOURCE_DIR}
-              BINARY_DIR ${EXTENSION_NAME}-build
-              CONFIGURE_COMMAND ""
-              BUILD_COMMAND ${EXTENSION_UPLOAD_COMMAND}
-              INSTALL_COMMAND ""
-              ${EP_ARG_EXTENSION_REBUILD_DEPENDS}
-              )
-            # This custom external project step forces the build and later
-            # steps to run whenever a top level build is done...
-            ExternalProject_Add_Step(${proj}-rebuild forcebuild
-              DEPENDEES configure
-              DEPENDERS build
-              ALWAYS 1
-              )
-            set_property(TARGET ${proj}-rebuild PROPERTY EXCLUDE_FROM_ALL TRUE)
-          endif()
+          set_property(TARGET ${proj}-rebuild PROPERTY EXCLUDE_FROM_ALL TRUE)
+        endif()
 
-        else()
-          #-----------------------------------------------------------------------------
-          # Slicer_UPLOAD_EXTENSIONS: FALSE
-          #-----------------------------------------------------------------------------
+      else()
+        #-----------------------------------------------------------------------------
+        # Slicer_UPLOAD_EXTENSIONS: FALSE
+        #-----------------------------------------------------------------------------
+        list(APPEND ext_ep_cmake_args
+          -D${EXTENSION_NAME}_BUILD_SLICER_EXTENSION:BOOL=ON
+          -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+          -DBUILD_TESTING:BOOL=${BUILD_TESTING}
+          -DSlicer_DIR:PATH=${Slicer_DIR}
+          -DSlicer_EXTENSIONS_TRACK_QUALIFIER:STRING=${Slicer_EXTENSIONS_TRACK_QUALIFIER}
+          -DEXTENSION_BUILD_SUBDIRECTORY:STRING=${EXTENSION_EXT_BUILD_SUBDIRECTORY}
+          -DEXTENSION_ENABLED:BOOL=${EXTENSION_ENABLED}
+          -DEXTENSION_DEPENDS:STRING=${EXTENSION_DEPENDS}
+          -DMIDAS_PACKAGE_URL:STRING=${MIDAS_PACKAGE_URL}
+          -DMIDAS_PACKAGE_EMAIL:STRING=${MIDAS_PACKAGE_EMAIL}
+          -DMIDAS_PACKAGE_API_KEY:STRING=${MIDAS_PACKAGE_API_KEY}
+          )
+        if(APPLE)
           list(APPEND ext_ep_cmake_args
-            -D${EXTENSION_NAME}_BUILD_SLICER_EXTENSION:BOOL=ON
-            -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-            -DBUILD_TESTING:BOOL=${BUILD_TESTING}
-            -DSlicer_DIR:PATH=${Slicer_DIR}
-            -DSlicer_EXTENSIONS_TRACK_QUALIFIER:STRING=${Slicer_EXTENSIONS_TRACK_QUALIFIER}
-            -DEXTENSION_BUILD_SUBDIRECTORY:STRING=${EXTENSION_EXT_BUILD_SUBDIRECTORY}
-            -DEXTENSION_ENABLED:BOOL=${EXTENSION_ENABLED}
-            -DEXTENSION_DEPENDS:STRING=${EXTENSION_DEPENDS}
-            -DMIDAS_PACKAGE_URL:STRING=${MIDAS_PACKAGE_URL}
-            -DMIDAS_PACKAGE_EMAIL:STRING=${MIDAS_PACKAGE_EMAIL}
-            -DMIDAS_PACKAGE_API_KEY:STRING=${MIDAS_PACKAGE_API_KEY}
+            -DCMAKE_OSX_ARCHITECTURES:STRING=${CMAKE_OSX_ARCHITECTURES}
+            -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=${CMAKE_OSX_DEPLOYMENT_TARGET}
+            -DCMAKE_OSX_SYSROOT:PATH=${CMAKE_OSX_SYSROOT}
             )
-          if(APPLE)
-            list(APPEND ext_ep_cmake_args
-              -DCMAKE_OSX_ARCHITECTURES:STRING=${CMAKE_OSX_ARCHITECTURES}
-              -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=${CMAKE_OSX_DEPLOYMENT_TARGET}
-              -DCMAKE_OSX_SYSROOT:PATH=${CMAKE_OSX_SYSROOT}
-              )
-          endif()
+        endif()
 
-          foreach(dep ${EXTENSION_DEPENDS})
-            list(APPEND ext_ep_cmake_args -D${dep}_DIR:PATH=${CMAKE_CURRENT_BINARY_DIR}/${dep}-build/${${dep}_BUILD_SUBDIRECTORY})
-          endforeach()
+        foreach(dep ${EXTENSION_DEPENDS})
+          list(APPEND ext_ep_cmake_args -D${dep}_DIR:PATH=${CMAKE_CURRENT_BINARY_DIR}/${dep}-build/${${dep}_BUILD_SUBDIRECTORY})
+        endforeach()
 
-          # Add extension external project
-          set(proj ${EXTENSION_NAME})
-          ExternalProject_Add(${proj}
+        # Add extension external project
+        set(proj ${EXTENSION_NAME})
+        ExternalProject_Add(${proj}
+          ${ext_ep_options_repository}
+          INSTALL_COMMAND ""
+          SOURCE_DIR ${EXTENSION_SOURCE_DIR}
+          BINARY_DIR ${EXTENSION_NAME}-build
+          CMAKE_GENERATOR ${Slicer_EXTENSION_CMAKE_GENERATOR}
+          CMAKE_ARGS
+            ${ext_ep_cmake_args}
+          ${EP_ARG_EXTENSION_DEPENDS}
+          )
+        # This custom external project step forces the build and later
+        # steps to run whenever a top level build is done...
+        ExternalProject_Add_Step(${proj} forcebuild
+          DEPENDEES configure
+          DEPENDERS build
+          ALWAYS 1
+          )
+        if(Slicer_SOURCE_DIR)
+          # Add convenient external project allowing to build the extension
+          # independently of Slicer
+          ExternalProject_Add(${proj}-rebuild
             ${ext_ep_options_repository}
             INSTALL_COMMAND ""
             SOURCE_DIR ${EXTENSION_SOURCE_DIR}
@@ -251,37 +266,16 @@ foreach(extension_name ${EXTENSION_LIST})
             CMAKE_GENERATOR ${Slicer_EXTENSION_CMAKE_GENERATOR}
             CMAKE_ARGS
               ${ext_ep_cmake_args}
-            ${EP_ARG_EXTENSION_DEPENDS}
+            ${EP_ARG_EXTENSION_REBUILD_DEPENDS}
             )
           # This custom external project step forces the build and later
           # steps to run whenever a top level build is done...
-          ExternalProject_Add_Step(${proj} forcebuild
+          ExternalProject_Add_Step(${proj}-rebuild forcebuild
             DEPENDEES configure
             DEPENDERS build
             ALWAYS 1
             )
-          if(Slicer_SOURCE_DIR)
-            # Add convenient external project allowing to build the extension
-            # independently of Slicer
-            ExternalProject_Add(${proj}-rebuild
-              ${ext_ep_options_repository}
-              INSTALL_COMMAND ""
-              SOURCE_DIR ${EXTENSION_SOURCE_DIR}
-              BINARY_DIR ${EXTENSION_NAME}-build
-              CMAKE_GENERATOR ${Slicer_EXTENSION_CMAKE_GENERATOR}
-              CMAKE_ARGS
-                ${ext_ep_cmake_args}
-              ${EP_ARG_EXTENSION_REBUILD_DEPENDS}
-              )
-            # This custom external project step forces the build and later
-            # steps to run whenever a top level build is done...
-            ExternalProject_Add_Step(${proj}-rebuild forcebuild
-              DEPENDEES configure
-              DEPENDERS build
-              ALWAYS 1
-              )
-            set_property(TARGET ${proj}-rebuild PROPERTY EXCLUDE_FROM_ALL TRUE)
-          endif()
+          set_property(TARGET ${proj}-rebuild PROPERTY EXCLUDE_FROM_ALL TRUE)
         endif()
       endif()
     endif()
