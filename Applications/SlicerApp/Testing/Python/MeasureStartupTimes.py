e579216b6c404f61dd490f55f9db53a0d917ff20
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-05-21 06:06:01 +0000

    ENH: Improve MeasureStartupTimes script
    collect_startup_times_overall:
    
     * Move existing code into "collect_startup_times_overall" function and
       save stats to a json file "StartupTimes.json" to facilitate analysis.
    
    collect_startup_times_excluding_one_module:
    
     * Added "collect_startup_times_excluding_one_module" starting Slicer N
       times excluding each one of the N modules one by one. Stats are
       then saved to "StartupTimesExcludingOneModule.json".
    
     * List of module names (and their type) is also saved to "Modules.json"
    
     * This allows to check if there are modules impacting the startup
       time more than the average.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25101
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25101 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/SlicerApp/Testing/Python/MeasureStartupTimes.py
+++ Applications/SlicerApp/Testing/Python/MeasureStartupTimes.py
@@ -18,8 +18,11 @@
 #  and was partially funded by NIH grant 1U24CA194354-01
 #
 
+import argparse
+import json
 import os
 import sys
+import tempfile
 
 from SlicerAppTesting import *
 
@@ -28,13 +31,18 @@ Usage:
     MeasureStartupTimes.py /path/to/Slicer
 """
 
-if __name__ == '__main__':
+def TemporaryPythonScript(code, *args, **kwargs):
+  if 'suffix' not in kwargs:
+    kwargs['suffix'] = '.py'
+  if 'mode' not in kwargs:
+    kwargs['mode'] = 'w'
+  script = tempfile.NamedTemporaryFile(*args, **kwargs)
+  script.write(code)
+  script.flush()
+  print("Written script %s [%s]" % (script.name, code))
+  return script
 
-  if len(sys.argv) != 2:
-    print(os.path.basename(sys.argv[0]) +" /path/to/Slicer")
-    exit(EXIT_FAILURE)
-
-  slicer_executable = os.path.expanduser(sys.argv[1])
+def collect_startup_times_overall(outut_file):
 
   tests = [
     [],
@@ -45,11 +53,98 @@ if __name__ == '__main__':
     ['--disable-modules']
   ]
 
-  runSlicerAndExit = timecall(runSlicerAndExit)
+  results= {}
 
   for test in tests:
-    runSlicerAndExit(slicer_executable, test)
+    (duration, result) = runSlicerAndExitWithTime(slicer_executable, test)
+    results[" ".join(test)] = duration
+    print("{:.3f} seconds".format(duration))
+    print("")
 
   for test in tests:
     test.insert(0, '--disable-python')
-    runSlicerAndExit(slicer_executable, test)
+    (duration, result) = runSlicerAndExitWithTime(slicer_executable, test)
+    results[" ".join(test)] = duration
+    print("{:.3f} seconds".format(duration))
+    print("")
+
+  with open(outut_file, 'w') as file:
+    file.write(json.dumps(results, indent=4))
+
+
+def collect_startup_times_excluding_one_module(output_file):
+
+  # Collect list of all modules and their associated types
+  python_script = TemporaryPythonScript("""
+import json
+
+modules = {}
+moduleManager = slicer.app.moduleManager()
+for name in dir(slicer.moduleNames):
+  module = moduleManager.module(name)
+  if module is None:
+    continue
+  moduleType = None
+  if module.inherits('qSlicerLoadableModule'):
+    module_type = "Loadable"
+  elif module.inherits('qSlicerCLIModule'):
+    module_type = "CLI"
+  elif module.inherits('qSlicerScriptedLoadableModule'):
+    module_type = "Scripted"
+  elif module.inherits('qSlicerCoreModule'):
+    module_type = "Core"
+  else:
+    raise Exception("Failed to module type of %s" % name)
+  print("Found module %s of type %s" % (name, module_type))
+  modules[name] = module_type
+with open("Modules.json", 'w') as output:
+  output.write(json.dumps(modules, indent=4))
+""")
+
+  (returnCode, stdout, stderr) = runSlicerAndExit(slicer_executable, ['--python-script', python_script.name])
+  assert returnCode == EXIT_SUCCESS
+  print("=> ok\n")
+
+  # Read list of modules
+  with open('Modules.json') as input:
+    modules = json.load(input)
+  print("Found %d  modules\n" % len(modules))
+
+  # Collect startup times disabling each module one by one
+  moduleTimes = {}
+  for (idx, (moduleName, moduleType)) in enumerate(modules.iteritems(), start=1):
+    #if moduleType == "CLI":
+    #  print("=> Skipping CLI [%s]\n" % moduleName)
+    #  continue
+    print("[%d/%d]" % (idx, len(modules)))
+    (duration, result) = runSlicerAndExitWithTime(slicer_executable, ['--testing', '--modules-to-ignore', moduleName])
+    (returnCode, stdout, stderr) = result
+    if returnCode != EXIT_SUCCESS:
+      # XXX Ignore module with dependencies
+      duration = None
+      print("=> failed\n")
+    else:
+      moduleTimes[moduleName] = duration
+      print("=> {:.3f} seconds\n".format(duration))
+
+  with open(output_file, 'w') as file:
+    file.write(json.dumps(moduleTimes, indent=4))
+
+if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser(description='Measure startup times.')
+  parser.add_argument("--overall", action="store_true")
+  parser.add_argument("--excluding-one-module", action="store_true")
+  parser.add_argument("/path/to/Slicer")
+  args = parser.parse_args()
+
+  slicer_executable = os.path.expanduser(getattr(args, "/path/to/Slicer"))
+  all = not args.overall and not args.excluding_one_module
+
+  runSlicerAndExitWithTime = timecall(runSlicerAndExit)
+
+  if all or args.overall:
+    collect_startup_times_overall("StartupTimes.json")
+
+  if all or args.excluding_one_module:
+    collect_startup_times_excluding_one_module("StartupTimesExcludingOneModule.json")
--- Applications/SlicerApp/Testing/Python/SlicerAppTesting.py
+++ Applications/SlicerApp/Testing/Python/SlicerAppTesting.py
@@ -27,9 +27,12 @@
 # We mean it.
 #
 
+from __future__ import print_function
+
 import os
 import pipes
 import subprocess
+import sys
 import time
 
 __all__ = ['EXIT_FAILURE', 'EXIT_SUCCESS', 'run', 'runSlicer', 'runSlicerAndExit', 'timecall']
@@ -38,10 +41,11 @@ EXIT_FAILURE=1
 EXIT_SUCCESS=0
 
 
-def run(executable, arguments=[], verbose=False, shell=False):
+def run(executable, arguments=[], verbose=True, shell=False):
   """Run ``executable`` with provided ``arguments``.
   """
-  print("%s %s" % (os.path.basename(executable), " ".join([pipes.quote(arg) for arg in arguments])))
+  if verbose:
+    print("%s %s" % (os.path.basename(executable), " ".join([pipes.quote(arg) for arg in arguments])))
   arguments.insert(0, executable)
   if shell:
     arguments = " ".join([pipes.quote(arg) for arg in arguments])
@@ -49,38 +53,30 @@ def run(executable, arguments=[], verbose=False, shell=False):
                        stderr=subprocess.PIPE, shell=shell)
   stdout, stderr = p.communicate()
 
-  if verbose:
-    print('STDOUT: ' + stdout)
-
   if p.returncode != EXIT_SUCCESS:
-    if not verbose:
-      print('STDOUT: ' + stdout)
-    print('STDERR: ' + stderr)
+    print('STDERR: ' + stderr, file=sys.stderr)
 
   return (p.returncode, stdout, stderr)
 
-def runSlicer(slicer_executable, arguments=[], verbose=False):
+def runSlicer(slicer_executable, arguments=[], verbose=True):
   """Run ``slicer_executable`` with provided ``arguments``.
   """
   args = ['--no-splash']
   args.extend(arguments)
   return run(slicer_executable, args, verbose)
 
-def runSlicerAndExit(slicer_executable, arguments=[], verbose=False):
+def runSlicerAndExit(slicer_executable, arguments=[], verbose=True):
   """Run ``slicer_executable`` with provided ``arguments`` and exit.
   """
   arguments.insert(0, '--exit-after-startup')
   return runSlicer(slicer_executable, arguments, verbose)
 
 def timecall(method):
-  """Wrap ``method`` and print its execution time.
+  """Wrap ``method`` and return its execution time.
   """
   def wrapper(*args, **kwargs):
-    try:
-      start = time.time()
-      result = method(*args, **kwargs)
-      return result
-    finally:
-      print("{:.3f} seconds".format(time.time() - start))
-      print("")
+    start = time.time()
+    result = method(*args, **kwargs)
+    duration = time.time() - start
+    return (duration, result)
   return wrapper
