3f15c493374b30645b1201456d14882dc0ffa3f0
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-05-26 22:28:50 +0000

    STYLE: Remove redundant modules 'mrmlLogic' and 'mrmlcli'
    These modules are already imported in mrml.py
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25120
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25120 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Python/mrmlLogic.py
+++
@@ -1,4 +0,0 @@
-""" This module loads all the classes from the MRML Logic library into its
-namespace."""
-
-from MRMLLogicPython import *
--- Base/Python/mrmlcli.py
+++
@@ -1,4 +0,0 @@
-""" This module loads all the classes from the MRMLCLI library into its
-namespace."""
-
-from MRMLCLIPython import *
