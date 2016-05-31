46ca757f9a112359007fb73bf937a0f3692d999c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-05-31 06:11:26 +0000

    STYLE: Relocate logging initialization from slicer/__init__ to slicerqt.py
    Relocate configuration of the application loggers into the application
    initialization script. This is done anticipating the possible introduction
    of lazy loading of slicer module attributes.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25124
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25124 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Python/slicer/__init__.py
+++ Base/Python/slicer/__init__.py
@@ -1,95 +1,9 @@
 """ This module sets up root logging and loads the Slicer library modules into its namespace."""
 
-import ctk
-import logging
-import qt
-import sys
-
-#-----------------------------------------------------------------------------
-class _LogReverseLevelFilter(logging.Filter):
-  """
-  Rejects log records that are at or above the specified level
-  """
-  def __init__(self, levelLimit):
-    self._levelLimit = levelLimit
-  def filter(self, record):
-    return record.levelno < self._levelLimit
-
-#-----------------------------------------------------------------------------
-class SlicerApplicationLogHandler(logging.Handler):
-  """
-  Writes logging records to Slicer application log.
-  """
-  def __init__(self):
-    logging.Handler.__init__(self)
-    if hasattr(ctk, 'ctkErrorLogLevel'):
-      self.pythonToCtkLevelConverter = {
-        logging.DEBUG : ctk.ctkErrorLogLevel.Debug,
-        logging.INFO : ctk.ctkErrorLogLevel.Info,
-        logging.WARNING : ctk.ctkErrorLogLevel.Warning,
-        logging.ERROR : ctk.ctkErrorLogLevel.Error }
-    self.origin = "Python"
-    self.category = "Python"
-  def emit(self, record):
-    try:
-      context = ctk.ctkErrorLogContext()
-      context.setCategory(self.category)
-      context.setLine(record.lineno)
-      context.setFile(record.pathname)
-      context.setFunction(record.funcName)
-      context.setMessage(record.msg)
-      threadId = "{0}({1})".format(record.threadName, record.thread)
-      app.errorLogModel().addEntry(qt.QDateTime.currentDateTime(), threadId,
-        self.pythonToCtkLevelConverter[record.levelno], self.origin, context, record.msg)
-    except:
-      self.handleError(record)
-
-#-----------------------------------------------------------------------------
-def initLogging(logger):
-  """
-  Initialize logging by creating log handlers and setting default log level.
-  """
-
-  # Prints debug messages to Slicer application log.
-  # Only debug level messages are logged this way, as higher level messages are printed on console
-  # and all console outputs are sent automatically to the application log anyway.
-  applicationLogHandler = SlicerApplicationLogHandler()
-  applicationLogHandler.setLevel(logging.DEBUG)
-  # We could filter out messages at INFO level or above (as they will be printed on the console anyway) by adding
-  # applicationLogHandler.addFilter(_LogReverseLevelFilter(logging.INFO))
-  # but then we would not log file name and line number of info, warning, and error level messages.
-  applicationLogHandler.setFormatter(logging.Formatter('%(levelname)s: %(message)s (%(pathname)s:%(lineno)d)'))
-  logger.addHandler(applicationLogHandler)
-
-  # Prints info message to stdout (anything on stdout will also show up in the application log)
-  consoleInfoHandler = logging.StreamHandler(sys.stdout)
-  consoleInfoHandler.setLevel(logging.INFO)
-  # Filter messages at WARNING level or above (they will be printed on stderr)
-  consoleInfoHandler.addFilter(_LogReverseLevelFilter(logging.WARNING))
-  logger.addHandler(consoleInfoHandler)
-
-  # Prints error and warning messages to stderr (anything on stderr will also show it in the application log)
-  consoleErrorHandler = logging.StreamHandler(sys.stderr)
-  consoleErrorHandler.setLevel(logging.WARNING)
-  logger.addHandler(consoleErrorHandler)
-
-  # Log debug messages from scripts by default, as they are useful for troubleshooting with users
-  logger.setLevel(logging.DEBUG)
-
-#-----------------------------------------------------------------------------
-# Set up the root logger
-#
-# We initialize the root logger because if somebody just called logging.debug(),
-# logging.info(), etc. then it would create a default root logger with defaut settings
-# that do not work nicely in Slicer (prints everything in console, messages are
-# not sent to the application log, etc).
-#
-initLogging(logging.getLogger())
-
-
 #-----------------------------------------------------------------------------
 def _createModule(name, globals, docstring):
   import imp
+  import sys
   moduleName = name.split('.')[-1]
   module = imp.new_module( moduleName )
   module.__file__ = __file__
