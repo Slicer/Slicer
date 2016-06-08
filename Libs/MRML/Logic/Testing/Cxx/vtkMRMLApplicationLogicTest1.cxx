2db3dd8c73310a62e2cbb4e3b84186987730967c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-06-08 07:43:41 +0000

    STYLE: vtkMRMLApplicationLogicTest1: Refactor code to allow adding new tests
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25167
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25167 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/Logic/Testing/Cxx/vtkMRMLApplicationLogicTest1.cxx
+++ Libs/MRML/Logic/Testing/Cxx/vtkMRMLApplicationLogicTest1.cxx
@@ -18,8 +18,9 @@
 
 ==============================================================================*/
 
-// MRMLLogic includes
+// MRML includes
 #include "vtkMRMLApplicationLogic.h"
+#include "vtkMRMLCoreTestingMacros.h"
 
 // VTK includes
 #include <vtkNew.h>
@@ -27,46 +28,55 @@
 // STD includes
 #include <string>
 
+
+//-----------------------------------------------------------------------------
+int TemporaryPathTest();
+
 //-----------------------------------------------------------------------------
 int vtkMRMLApplicationLogicTest1(int , char * [])
 {
-  //-----------------------------------------------------------------------------
-  // Test void vtkMRMLApplicationLogic::SetTemporaryPath(const char* path);
-  // Test const char* vtkMRMLApplicationLogic::GetTemporaryPath();
-  //-----------------------------------------------------------------------------
+  CHECK_INT(TemporaryPathTest(), EXIT_SUCCESS);
+  return EXIT_SUCCESS;
+}
 
-    vtkNew<vtkMRMLApplicationLogic> MRMLAppLogic;
+//-----------------------------------------------------------------------------
+// Test void vtkMRMLApplicationLogic::SetTemporaryPath(const char* path);
+// Test const char* vtkMRMLApplicationLogic::GetTemporaryPath();
+//-----------------------------------------------------------------------------
+int TemporaryPathTest()
+{
+  vtkNew<vtkMRMLApplicationLogic> MRMLAppLogic;
 
-    MRMLAppLogic->SetTemporaryPath(NULL); // Test NULL
-    if (strlen(MRMLAppLogic->GetTemporaryPath()) != 0)
+  MRMLAppLogic->SetTemporaryPath(NULL); // Test NULL
+  if (strlen(MRMLAppLogic->GetTemporaryPath()) != 0)
+    {
+    std::string temporaryEmptyString;
+    std::cerr << "Line " << __LINE__ << " - Problem with SetTemporaryPath using NULL" << "\n"
+              << "\ttemporaryPath:" << MRMLAppLogic->GetTemporaryPath() << "\n"
+              << "\texpected:" << temporaryEmptyString << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  typedef std::vector<std::string> TestDataType;
+  TestDataType data;
+  data.push_back("/tmp/slicer");
+  data.push_back("/tmp/slicer/test");
+  data.push_back("/tmp/slicer/test"); // Test same path as before
+  data.push_back("c:\\path\\to\\temp\\slicer");
+  data.push_back(""); // Test empty string
+
+  for(TestDataType::size_type rowIdx = 0; rowIdx < data.size(); ++rowIdx)
+    {
+    std::string temporaryPath(data.at(rowIdx));
+    MRMLAppLogic->SetTemporaryPath(temporaryPath.c_str());
+    if (MRMLAppLogic->GetTemporaryPath() != temporaryPath)
       {
-      std::string temporaryEmptyString;
-      std::cerr << "Line " << __LINE__ << " - Problem with SetTemporaryPath using NULL" << "\n"
+      std::cerr << "Line " << __LINE__ << " - Problem with SetTemporaryPath using " << temporaryPath << " :\n"
                 << "\ttemporaryPath:" << MRMLAppLogic->GetTemporaryPath() << "\n"
-                << "\texpected:" << temporaryEmptyString << std::endl;
+                << "\texpected:" << temporaryPath << std::endl;
       return EXIT_FAILURE;
       }
+    }
 
-    typedef std::vector<std::string> TestDataType;
-    TestDataType data;
-    data.push_back("/tmp/slicer");
-    data.push_back("/tmp/slicer/test");
-    data.push_back("/tmp/slicer/test"); // Test same path as before
-    data.push_back("c:\\path\\to\\temp\\slicer");
-    data.push_back(""); // Test empty string
-
-    for(TestDataType::size_type rowIdx = 0; rowIdx < data.size(); ++rowIdx)
-      {
-      std::string temporaryPath(data.at(rowIdx));
-      MRMLAppLogic->SetTemporaryPath(temporaryPath.c_str());
-      if (MRMLAppLogic->GetTemporaryPath() != temporaryPath)
-        {
-        std::cerr << "Line " << __LINE__ << " - Problem with SetTemporaryPath using " << temporaryPath << " :\n"
-                  << "\ttemporaryPath:" << MRMLAppLogic->GetTemporaryPath() << "\n"
-                  << "\texpected:" << temporaryPath << std::endl;
-        return EXIT_FAILURE;
-        }
-      }
-
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
