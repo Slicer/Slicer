Slicer/Slicer

commit 1f9045d0389edcdba85f9ec439863cdd3dec16ae

ENH: qMRMLThreeDViewControllerWidget: Set ctkAxesWidget labels from view node
This commit updates the code using feature integrated in
r24961 (ENH: Made view axis labels configurable) and
 r25157 (ENH: Update CTK to include ctkAxesWidget support for
setting AxesLabels)

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25161
git-svn-id: http://svn.slicer.org/Slicer4/trunk@25161 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Widgets/qMRMLThreeDViewControllerWidget.cxx
@@ -333,6 +333,15 @@ void qMRMLThreeDViewControllerWidget::updateWidgetFromMRML()
     return;
     }
 
+  QStringList axesLabels;
+  axesLabels <<  d->ViewNode->GetAxisLabel(0);
+  axesLabels <<  d->ViewNode->GetAxisLabel(1);
+  axesLabels <<  d->ViewNode->GetAxisLabel(4);
+  axesLabels <<  d->ViewNode->GetAxisLabel(5);
+  axesLabels <<  d->ViewNode->GetAxisLabel(2);
+  axesLabels <<  d->ViewNode->GetAxisLabel(3);
+  d->AxesWidget->setAxesLabels(axesLabels);
+
   d->actionSet3DAxisVisible->setChecked(d->ViewNode->GetBoxVisible());
   d->actionSet3DAxisLabelVisible->setChecked(
     d->ViewNode->GetAxisLabelsVisible());

commit 3dfed13b1e1f544294562c3ee2c4e257c4e637a1

BUG: qMRMLThreeDViewControllerWidget: Fix axes labels
This fixes a regression introduced in r25161 (Set ctkAxesWidget labels
from view node) that caused the axis labels to be swapped.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25208
git-svn-id: http://svn.slicer.org/Slicer4/trunk@25208 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Widgets/qMRMLThreeDViewControllerWidget.cxx
@@ -333,13 +333,15 @@ void qMRMLThreeDViewControllerWidget::updateWidgetFromMRML()
     return;
     }
 
+  // In the axes widget the order of labels is: +X, -X, +Z, -Z, +Y, -Y
+  // and in the view node axis labels order is: -X, +X, -Y, +Y, -Z, +Z.
   QStringList axesLabels;
-  axesLabels <<  d->ViewNode->GetAxisLabel(0);
-  axesLabels <<  d->ViewNode->GetAxisLabel(1);
-  axesLabels <<  d->ViewNode->GetAxisLabel(4);
-  axesLabels <<  d->ViewNode->GetAxisLabel(5);
-  axesLabels <<  d->ViewNode->GetAxisLabel(2);
-  axesLabels <<  d->ViewNode->GetAxisLabel(3);
+  axesLabels <<  d->ViewNode->GetAxisLabel(1); // +X
+  axesLabels <<  d->ViewNode->GetAxisLabel(0); // -X
+  axesLabels <<  d->ViewNode->GetAxisLabel(5); // +Z
+  axesLabels <<  d->ViewNode->GetAxisLabel(4); // -Z
+  axesLabels <<  d->ViewNode->GetAxisLabel(3); // +Y
+  axesLabels <<  d->ViewNode->GetAxisLabel(2); // -Y
   d->AxesWidget->setAxesLabels(axesLabels);
 
   d->actionSet3DAxisVisible->setChecked(d->ViewNode->GetBoxVisible());