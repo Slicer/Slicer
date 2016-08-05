0d4896a4d6a07ef016f8802e3da248fd6db4714f
Fedorov <fedorov@bwh.harvard.edu>
2016-08-05 18:49:09 +0000

    ENH: update MultiVolumeImporter hash
    This fixes the off-by-one problem that prevented making static plots for single-
    slice multivolumes
    
    (issue reported in this thread: http://slicer-users.65878.n3.nabble.com/MultiVolume-Explorer-td4030844.html)
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25265
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25265 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -200,7 +200,7 @@ list_conditional_append(Slicer_BUILD_MultiVolumeExplorer Slicer_REMOTE_DEPENDENC
 
 Slicer_Remote_Add(MultiVolumeImporter
   GIT_REPOSITORY ${git_protocol}://github.com/fedorov/MultiVolumeImporter.git
-  GIT_TAG 669594fb993b34a104795cd0b1e1a3340c4311c2
+  GIT_TAG 7e4425257c8e8e9ad637ccca99249b1b36b12296
   OPTION_NAME Slicer_BUILD_MultiVolumeImporter
   OPTION_DEPENDS "Slicer_BUILD_QTLOADABLEMODULES;Slicer_BUILD_MULTIVOLUME_SUPPORT;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
