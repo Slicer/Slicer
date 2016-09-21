1689decf3ed558a6e0de9292b89403dc0637e24b
Isaiah Norton <inorton@bwh.harvard.edu>
2016-09-21 14:22:24 +0000

    ENH: Update SlicerDMRI
    $ git shortlog 248df4b..a780e4b --no-merges
    Isaiah Norton (12):
          ENH: update FBStorageNode for upstream changes.
          ENH: add initial DICOM tractio features
          ENH: tractio xml updates
          COMP: tractio feature gate
          ENH: DTEstim, DTScalarMaps, Seeding GUI updates
          STYLE: fix dos line endings
          ENH: ROISeeding: move seed spacing/grid parameters to advanced
          ENH: ROISeeding: remove archaic WriteToFile option
          ENH: Intx seeding unit label consistency with ROI seeding
          COMP: remove old CLI options, ref 03d39612b
          COMP: add MRML_DATA and TEMP defs for local/extension testing
          COMP: fix and clean-up FBLabelSelect tests
    
    zhangfanmark (2):
          EHN: FBLabelSelect: fiber resampling before selection
          ENH: Sampling faster only using points in lines
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25383
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25383 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -314,7 +314,7 @@ Slicer_Remote_Add(SlicerDMRI
   GIT_REPOSITORY "${git_protocol}://github.com/SlicerDMRI/SlicerDMRI"
   # SlicerDMRI Maintainer: If new revision of the extension add or remove modules, consider updating
   #                        the module lists below. Thanks.
-  GIT_TAG "248df4bce1ab63333a8ea6402b4bae0d841d6ff6"
+  GIT_TAG "a780e4b939ddfcbbf7da3567733a79387805ecc9"
   OPTION_NAME Slicer_BUILD_SlicerDMRI
   OPTION_DEPENDS "Slicer_BUILD_DIFFUSION_SUPPORT"
   LABELS REMOTE_EXTENSION
