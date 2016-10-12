59b2c790bd9c1dd90d551605f708ae6474109909
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-10-12 04:57:58 +0000

    ENH: DMRIInstall module: Tweak UI
    * Change help text into HTML and use a ctkTextFittedBrowser to display it
    
    * Associated icon with the "Open Extension Manager" button
    
    * Other minor code layout tweaks
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25427
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25427 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Scripted/DMRIInstall/DMRIInstall.py
+++ Modules/Scripted/DMRIInstall/DMRIInstall.py
@@ -1,5 +1,6 @@
 import os
 import string
+import textwrap
 import unittest
 import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
@@ -13,14 +14,18 @@ class DMRIInstall(ScriptedLoadableModule):
   """
   """
 
-  helpText = """
-Please use the Extension Manager to install the "SlicerDMRI" extension for diffusion-related tools including:
+  helpText = textwrap.dedent(
+  """
+  Please use the Extension Manager to install the "SlicerDMRI" extension for
+  diffusion-related tools including:
 
-    Diffusion Tensor Estimation
-    Tractography Display
-    Tractography Seeding
-    Fiber Tract Measurement
-"""
+  <ul>
+    <li> Diffusion Tensor Estimation</li>
+    <li>Tractography Display</li>
+    <li>Tractography Seeding</li>
+    <li>Fiber Tract Measurement</li>
+  </ul>
+  """)
 
   def __init__(self, parent):
 
@@ -37,9 +42,12 @@ Please use the Extension Manager to install the "SlicerDMRI" extension for diffu
     self.parent.contributors = ["Isaiah Norton"]
     self.parent.helpText = DMRIInstall.helpText
     self.parent.helpText += self.getDefaultModuleDocumentationLink()
-    self.parent.acknowledgementText = """
-SlicerDMRI supported by NIH NCI ITCR U01CA199459 (Open Source Diffusion MRI Technology For Brain Cancer Research), and made possible by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community.
-"""
+    self.parent.acknowledgementText = textwrap.dedent(
+    """
+    SlicerDMRI supported by NIH NCI ITCR U01CA199459 (Open Source Diffusion MRI
+    Technology For Brain Cancer Research), and made possible by NA-MIC, NAC,
+    BIRN, NCIGT, and the Slicer Community.
+    """)
 
 class DMRIInstallWidget(ScriptedLoadableModuleWidget):
   """Uses ScriptedLoadableModuleWidget base class, available at:
@@ -49,16 +57,16 @@ class DMRIInstallWidget(ScriptedLoadableModuleWidget):
   def setup(self):
     ScriptedLoadableModuleWidget.setup(self)
 
-    self.textBox = qt.QLabel()
-    self.textBox.setWordWrap(1)
-    self.textBox.setText(DMRIInstall.helpText)
+    self.textBox = ctk.ctkFittedTextBrowser()
+    self.textBox.setHtml(DMRIInstall.helpText)
     self.parent.layout().addWidget(self.textBox)
 
     #
     # Apply Button
     #
     self.applyButton = qt.QPushButton("Open Extension Manager")
-    self.applyButton.toolTip = """Install the "SlicerDMRI" extension from the Diffusion category."""
+    self.applyButton.toolTip = 'Install the "SlicerDMRI" extension from the Diffusion category.'
+    self.applyButton.icon = qt.QIcon(":/Icons/ExtensionDefaultIcon.png")
     self.applyButton.enabled = True
     self.applyButton.connect('clicked()', self.onApply)
     self.parent.layout().addWidget(self.applyButton)
