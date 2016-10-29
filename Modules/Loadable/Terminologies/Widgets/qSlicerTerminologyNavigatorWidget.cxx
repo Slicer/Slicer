Slicer/Slicer

commit 6d5baa3edff777cd7d623da872ff11af088fa18d

BUG: Added missing null-pointer checks to terminology widget
Caused qt designer to crash on startup.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25487
git-svn-id: http://svn.slicer.org/Slicer4/trunk@25487 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Terminologies/Widgets/qSlicerTerminologyNavigatorWidget.cxx
@@ -202,17 +202,25 @@ void qSlicerTerminologyNavigatorWidgetPrivate::init()
 //-----------------------------------------------------------------------------
 vtkSlicerTerminologiesModuleLogic* qSlicerTerminologyNavigatorWidgetPrivate::terminologyLogic()
 {
-  qSlicerAbstractCoreModule* terminologiesModule =
-    qSlicerCoreApplication::application()->moduleManager()->module("Terminologies");
-  if (terminologiesModule)
-  {
-    vtkSlicerTerminologiesModuleLogic* terminologyLogic =
-      vtkSlicerTerminologiesModuleLogic::SafeDownCast(terminologiesModule->logic());
-    return terminologyLogic;
-  }
-
-  qCritical() << Q_FUNC_INFO << ": Terminologies module is not found";
-  return NULL;
+  if (!qSlicerCoreApplication::application()
+    || !qSlicerCoreApplication::application()->moduleManager())
+    {
+    qCritical() << Q_FUNC_INFO << ": Module manager is not found";
+    return NULL;
+    }
+  qSlicerAbstractCoreModule* terminologiesModule = qSlicerCoreApplication::application()->moduleManager()->module("Terminologies");
+  if (!terminologiesModule)
+    {
+    qCritical() << Q_FUNC_INFO << ": Terminologies module is not found";
+    return NULL;
+    }
+  vtkSlicerTerminologiesModuleLogic* terminologyLogic =
+    vtkSlicerTerminologiesModuleLogic::SafeDownCast(terminologiesModule->logic());
+  if (!terminologyLogic)
+    {
+    qWarning() << Q_FUNC_INFO << ": Terminologies module logic is invalid";
+    }
+  return terminologyLogic;
 }
 
 //-----------------------------------------------------------------------------
@@ -588,13 +596,7 @@ bool qSlicerTerminologyNavigatorWidget::terminologyEntryFromCodeMeanings(QString
     }
 
   // Get terminology logic
-  vtkSlicerTerminologiesModuleLogic* terminologyLogic = NULL;
-  qSlicerAbstractCoreModule* terminologiesModule =
-    qSlicerCoreApplication::application()->moduleManager()->module("Terminologies");
-  if (terminologiesModule)
-    {
-    terminologyLogic = vtkSlicerTerminologiesModuleLogic::SafeDownCast(terminologiesModule->logic());
-    }
+  vtkSlicerTerminologiesModuleLogic* terminologyLogic = d->terminologyLogic();
   if (!terminologyLogic)
     {
     qCritical() << Q_FUNC_INFO << ": Unable to access terminology logic";

commit 4aef43b33cbe293e7c3fa6ca299439a7eb568621

COMP: TerminologyNavigatorWidget: Fix Gcc compilation error
This commit fixes a compilation error introduced in r25487 (BUG: Added
missing null-pointer checks to terminology widget)

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25488
git-svn-id: http://svn.slicer.org/Slicer4/trunk@25488 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Terminologies/Widgets/qSlicerTerminologyNavigatorWidget.cxx
@@ -55,7 +55,7 @@ public:
   void init();
 
   /// Get terminology module logic
-  vtkSlicerTerminologiesModuleLogic* terminologyLogic();
+  static vtkSlicerTerminologiesModuleLogic* terminologyLogic();
 
   /// Reset current category name and container object
   void resetCurrentCategory();
@@ -596,7 +596,8 @@ bool qSlicerTerminologyNavigatorWidget::terminologyEntryFromCodeMeanings(QString
     }
 
   // Get terminology logic
-  vtkSlicerTerminologiesModuleLogic* terminologyLogic = d->terminologyLogic();
+  vtkSlicerTerminologiesModuleLogic* terminologyLogic =
+      qSlicerTerminologyNavigatorWidgetPrivate::terminologyLogic();
   if (!terminologyLogic)
     {
     qCritical() << Q_FUNC_INFO << ": Unable to access terminology logic";