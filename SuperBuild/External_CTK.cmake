fc8917ba80def8a7a6e71b18695ee46ce60f49c5
Andras Lasso <lasso@queensu.ca>
2017-03-01 18:59:46 +0000

    ENH: Updated to latest CTK
    * ENH: Implemented justified style for flow layout
    
    It allows variable sized items (so that they use the space optimally) and nice visual alignment at the same time
    (all rows have the same width).
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25739
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25739 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "443dc276ce122a047970a67ea580cd5bcf645808"
+    GIT_TAG "3b68bff002d6e67e65743ff53f6701b479d10290"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
