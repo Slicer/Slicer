befe3b414f3ddca93acf15fc8171afd43916ac0a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-03-12 19:44:22 +0000

    COMP: Update CTK to fix unused variable warning
    $ git shortlog 3b68bff..a6738bd --no-merges
    Jean-Christophe Fillion-Robin (1):
          COMP: ctkVTKDataSetArrayComboBox: Fix unused variable warnings
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25819
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25819 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "3b68bff002d6e67e65743ff53f6701b479d10290"
+    GIT_TAG "a6738bd330f35fa445aba22650b9b882fdbcdfa9"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS
