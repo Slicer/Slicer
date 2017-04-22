b2e121f5a2c091579d9c68df3c46b7cc4dd7886e
Andras Lasso <lasso@queensu.ca>
2017-04-22 02:01:39 +0000

    ENH: Install SSL dlls into bin folder
    SSL libraries (libeay32.dll, ssleay32.dll) sometimes fail to load on Windows if they are not in the same folder as SlicerApp-real.exe.
    
    This commit makes the SSL libraries installed in SlicerApp-real.exe's folder (c:\Program Files\Slicer 4.7.0-2017-04-20\bin) instead of lib folder (c:\Program Files\Slicer 4.7.0-2017-04-20\lib\Slicer-4.7\ssleay32.dll).
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25942
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@25942 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockInstallOpenSSL.cmake
+++ CMake/SlicerBlockInstallOpenSSL.cmake
@@ -11,7 +11,7 @@ foreach(library ${OPENSSL_LIBRARIES})
       if(WIN32)
         get_filename_component(library_without_extension ${library} NAME_WE)
         install(FILES ${OPENSSL_EXPORT_LIBRARY_DIR}/${library_without_extension}.dll
-          DESTINATION ${Slicer_INSTALL_LIB_DIR} COMPONENT Runtime)
+          DESTINATION ${Slicer_INSTALL_BIN_DIR} COMPONENT Runtime)
       elseif(UNIX)
         slicerInstallLibrary(
           FILE ${library}
