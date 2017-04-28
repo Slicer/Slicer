Slicer/Slicer

commit 81837973e13738e6901da7758600d2377d05bd34

ENH: Update python-cmake-buildsystem version
$ git shortlog 39ccab0..30b0d56 --no-merges
David Brooks (2):
      Correctly name the installed include and library configuration directories.
      Set PLATFORM_TRIPLET as per Python's `configure` script.

Jean-Christophe Fillion-Robin (9):
      ConfigureChecks: Add support for system not expecting "libutil" (e.g VxWorks)
      ConfigureChecks: Ensure LIBUTIL_LIBRARIES is set after first configure
      circleci: simplify load/pull/save of docker images using scikit-ci-addons
      extension_ctypes: Fix compilation on linux-x86
      circleci: Use dockcross scripts
      cmake: Change version built by default from 2.7.12 to 3.5.2
      circleci: Enable parallel build using circleci-matrix
      circleci: Speed-up build using system python
      Add support for Python 2.7.13

Jérémy Viau-Trudel (1):
      change default python version on readme.md

Marcel Metz (1):
      Replace remaining instances of BUILD_{SHARED,STATIC} control variable

Martin Oberhuber (2):
      ConfigureChecks: libutil is now optional. See #163
      ConfigureChecks: VxWorks:  Disable _XOPEN_SOURCE due to "sys/select.h" failing

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25955
git-svn-id: http://svn.slicer.org/Slicer4/trunk@25955 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_python.cmake
@@ -118,7 +118,7 @@ if((NOT DEFINED PYTHON_INCLUDE_DIR
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "39ccab04fc48d957f93d83f056e65a1dfb09d784"
+    "30b0d561e84900a4b400d2ce967ce8d5dcee8f25"
     QUIET
     )
 
@@ -135,8 +135,7 @@ if((NOT DEFINED PYTHON_INCLUDE_DIR
       -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
       -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/${proj}-install
       #-DBUILD_TESTING:BOOL=OFF
-      -DBUILD_SHARED:BOOL=ON
-      -DBUILD_STATIC:BOOL=OFF
+      -DBUILD_LIBPYTHON_SHARED:BOOL=ON
       -DUSE_SYSTEM_LIBRARIES:BOOL=OFF
       -DSRC_DIR:PATH=${python_SOURCE_DIR}
       -DDOWNLOAD_SOURCES:BOOL=OFF

commit 23661a0b4d91bda6c4ab7761f266c0616285551a

COMP: Update python-cmake-buildsystem to address NUMPY build issue
This commit fixes a regression introduced in r25955 (ENH: Update
python-cmake-buildsystem version) and python-cmake-buildsystem/python-cmake-buildsystem@9cce62e

See #4366

Update details:

$ git shortlog 30b0d56..d0f4719 --no-merges
Isaiah Norton (1):
      Fix broken python2.7 config location


Thanks: Isaiah Norton <inorton@bwh.harvard.edu>

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=25986
git-svn-id: http://svn.slicer.org/Slicer4/trunk@25986 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_python.cmake
@@ -118,7 +118,7 @@ if((NOT DEFINED PYTHON_INCLUDE_DIR
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "30b0d561e84900a4b400d2ce967ce8d5dcee8f25"
+    "d0f47193837db6ad9e08bc99617276d4a24687c3"
     QUIET
     )
 