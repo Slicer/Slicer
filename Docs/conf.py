77c252504ad7ad0120471412153d904a29144f98
Andras Lasso <lasso@queensu.ca>
2017-05-17 16:30:06 -0400

    DOC: Add documentation prolog (global.rst.in)
    The global.rst.in file contains reStructuredText will be included at
    the beginning of every source file that is read while generating the
    documentation.
    
    See https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-rst-prolog

--- Docs/conf.py
+++ Docs/conf.py
@@ -85,6 +85,8 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+# A string of reStructuredText that will be included at the beginning of every source file that is read.
+rst_prolog = open('global.rst.in', 'r').read()
 
 # -- Options for HTML output ----------------------------------------------
 
