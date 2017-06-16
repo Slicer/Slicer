19135bc0cbdfe0a32cc4a2fb5c11aafc7e33738f
Andras Lasso <lasso@queensu.ca>
2017-06-16 19:55:25 +0000

    ENH: Avoid recheck of CMake SSL capability
    Reconfiguring Slicer required internet connection for CMake SSL test to pass.
    It caused inconvenience when Slicer build was attempted from an unstable network connection.
    
    Fixes #4326 (https://issues.slicer.org/view.php?id=4326)
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26101
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@26101 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerCheckCMakeHTTPS.cmake
+++ CMake/SlicerCheckCMakeHTTPS.cmake
@@ -16,11 +16,18 @@ function(slicer_check_cmake_https)
 
   set(msg "Checking if CMake supports https")
   message(STATUS "${msg}")
-  file(DOWNLOAD ${url} ${dest} STATUS status)
-  list(GET status 0 error_code)
-  file(REMOVE ${dest})
-  if(error_code)
-    message(FATAL_ERROR "error: This CMake does not support the HTTPS protocol. Ensure that CMake is compiled with CMAKE_USE_OPENSSL enabled.")
+
+  if(NOT Slicer_CMake_HTTPS_Supported)
+    file(DOWNLOAD ${url} ${dest} STATUS status)
+    list(GET status 0 error_code)
+    file(REMOVE ${dest})
+    if(error_code)
+      message(FATAL_ERROR "error: This CMake does not support the HTTPS protocol. Ensure that CMake is compiled with CMAKE_USE_OPENSSL enabled.")
+    else()
+      # Save result of check, this way there will be no need for a working network connection when reconfiguring
+      set(Slicer_CMake_HTTPS_Supported TRUE CACHE INTERNAL "CMake supports HTTPS protocol")
+    endif()
   endif()
+
   message(STATUS "${msg} - ok")
 endfunction()
