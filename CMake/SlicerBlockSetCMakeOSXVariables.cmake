0136d8f16dcd1ea5004972b9c80fa90ed74431ed
Max Smolens <max.smolens@kitware.com>
2017-08-14 02:16:26 +0000

    COMP: Require minimum OSX deployment target of 10.9 if using Qt5 or VTK8
    Require minimum OSX deployment target of 10.9. In 10.9 libc++ replaces
    libstdc++ as the default runtime. Requiring this minimum ensures that
    all libraries use libc++.
    
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26267
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@26267 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerBlockSetCMakeOSXVariables.cmake
+++ CMake/SlicerBlockSetCMakeOSXVariables.cmake
@@ -116,6 +116,18 @@ if(APPLE)
     endif()
   endif()
 
+  if(DEFINED Slicer_SOURCE_DIR AND ("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8" OR DEFINED Qt5_DIR))
+    # Require minimum deployment target of 10.9. In 10.9 libc++ replaces libstdc++
+    # as the default runtime. Requiring this minimum ensures that all libraries
+    # use libc++.
+    set(required_deployment_target "10.9")
+  else()
+    set(required_deployment_target "10.6")
+  endif()
+  if(CMAKE_OSX_DEPLOYMENT_TARGET VERSION_LESS ${required_deployment_target})
+    message(FATAL_ERROR "CMAKE_OSX_DEPLOYMENT_TARGET must be ${required_deployment_target} or greater.")
+  endif()
+
   if(NOT "${CMAKE_OSX_SYSROOT}" STREQUAL "")
     if(NOT EXISTS "${CMAKE_OSX_SYSROOT}")
       message(FATAL_ERROR "error: CMAKE_OSX_SYSROOT='${CMAKE_OSX_SYSROOT}' does not exist")
