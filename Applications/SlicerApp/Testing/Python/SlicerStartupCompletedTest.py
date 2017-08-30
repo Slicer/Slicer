31c88b1c5033f4b71c9c84e0278c2b9fcda16053
Andras Lasso <lasso@queensu.ca>
2017-08-30 16:26:32 +0000

    BUG: Made SlicerStartupCompletedTest not require console
    These tests relied on having console (standard output) available:
    py_startupcompleted_signal_emitted_without_mainwindow_test
    py_startupcompleted_signal_emitted_with_mainwindow_test
    
    When console was not enabled (it is optional on Windows) then these tests failed.
    
    The solution was to use the same method as for testing slicerrc startup options: communicate with the test using an output file.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26320
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@26320 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/SlicerApp/Testing/Python/SlicerStartupCompletedTest.py
+++ Applications/SlicerApp/Testing/Python/SlicerStartupCompletedTest.py
@@ -1,19 +1,90 @@
-import slicer
+#!/usr/bin/env python
 
-from slicer.ScriptedLoadableModule import *
+#
+#  Program: 3D Slicer
+#
+#  Copyright (c) Kitware Inc.
+#
+#  See COPYRIGHT.txt
+#  or http://www.slicer.org/copyright/copyright.txt for details.
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+#  and was partially funded by NIH grant 1U24CA194354-01
+#
 
+import os
+import sys
+import tempfile
 
-class SlicerStartupCompletedTest(ScriptedLoadableModule):
+from SlicerAppTesting import *
 
-  def __init__(self, parent):
-    ScriptedLoadableModule.__init__(self, parent)
-    self.parent.title = "SlicerStartupCompletedTest"
-    self.parent.categories = ["Testing.TestCases"]
-    self.parent.contributors = ["Jean-Christophe Fillion-Robin (Kitware)"]
-    self.parent.widgetRepresentationCreationEnabled = False
+"""
+This test verifies that startupCompleted() signal is emitted.
 
-    slicer.app.connect("startupCompleted()", self.onStartupCompleted)
+It uses an output file for communication because on Windows,
+standard output is not always enabled.
 
-  def onStartupCompleted(self):
-    print("StartupCompleted emitted")
+Usage:
+    SlicerOptionIgnoreSlicerRCTest.py /path/to/Slicer
+"""
 
+if __name__ == '__main__':
+
+  debug = False # Set to True to:
+                #  * display the path of the expected test output file
+                #  * avoid deleting the created temporary directory
+
+  if len(sys.argv) != 2:
+    print(os.path.basename(sys.argv[0]) +" /path/to/Slicer")
+    exit(EXIT_FAILURE)
+
+  temporaryModuleDirPath = tempfile.mkdtemp().replace('\\','/')
+  try:
+
+    # Copy helper module that creates a file when startup completed event is received
+    currentDirPath = os.path.dirname(os.path.abspath(__file__)).replace('\\','/')
+    from shutil import copyfile
+    copyfile(currentDirPath+'/SlicerStartupCompletedTestHelperModule.py',
+      temporaryModuleDirPath+'/SlicerStartupCompletedTestHelperModule.py')
+  
+    slicer_executable = os.path.expanduser(sys.argv[1])
+    common_args = [
+      '--testing',
+      '--no-splash',
+      '--disable-builtin-cli-modules',
+      '--disable-builtin-loadable-modules',
+      '--disable-builtin-scripted-loadable-modules',
+      '--additional-module-path', temporaryModuleDirPath,
+      ]
+
+    test_output_file = temporaryModuleDirPath + "/StartupCompletedTest.out"
+    os.environ['SLICER_STARTUP_COMPLETED_TEST_OUTPUT'] = test_output_file
+    if debug:
+      print("SLICER_STARTUP_COMPLETED_TEST_OUTPUT=%s" % test_output_file)
+
+    # Test startupCompleted with main window
+    args = list(common_args)
+    (returnCode, stdout, stderr) = runSlicerAndExit(slicer_executable, args)
+    assert(os.path.isfile(test_output_file))
+    os.remove(test_output_file)
+    assert(returnCode == EXIT_SUCCESS)
+    print("Test startupCompleted with main window - passed\n")
+
+    # Test startupCompleted without main window
+    args = list(common_args)
+    args.extend(['--no-main-window'])
+    (returnCode, stdout, stderr) = runSlicerAndExit(slicer_executable, args)
+    assert(os.path.isfile(test_output_file))
+    assert(returnCode == EXIT_SUCCESS)
+    print("Test startupCompleted without main window - passed\n")
+
+  finally:
+    if not debug:
+      import shutil
+      shutil.rmtree(temporaryModuleDirPath)
---
+++ Applications/SlicerApp/Testing/Python/SlicerStartupCompletedTestHelperModule.py
@@ -0,0 +1,29 @@
+import slicer
+import os
+
+from slicer.ScriptedLoadableModule import *
+
+class SlicerStartupCompletedTestHelperModule(ScriptedLoadableModule):
+
+  def __init__(self, parent):
+    ScriptedLoadableModule.__init__(self, parent)
+    self.parent.title = "SlicerStartupCompletedTest"
+    self.parent.categories = ["Testing.TestCases"]
+    self.parent.contributors = ["Jean-Christophe Fillion-Robin (Kitware), Andras Lasso (PerkLab)"]
+    self.parent.widgetRepresentationCreationEnabled = False
+
+    self.testOutputFileName = os.environ['SLICER_STARTUP_COMPLETED_TEST_OUTPUT']
+    if os.path.isfile(self.testOutputFileName):
+     os.remove(self.testOutputFileName)
+
+    slicer.app.connect("startupCompleted()", self.onStartupCompleted)
+
+    print("SlicerStartupCompletedTestHelperModule initialized")
+
+  def onStartupCompleted(self):
+    print("StartupCompleted emitted")
+    import os
+    fd = os.open(self.testOutputFileName, os.O_RDWR|os.O_CREAT)
+    os.write(fd, 'SlicerStartupCompletedTestHelperModule.py generated this file')
+    os.write(fd, 'when slicer.app emitted startupCompleted() signal\n')
+    os.close(fd)
