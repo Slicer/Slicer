627782201075e030a25c650b504fff1ee07c4441
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-09-08 17:20:26 +0000

    COMP: Update EMSegment to fix remaining CMP0020 warnings
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26360
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@26360 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -289,7 +289,7 @@ endif()
 
 Slicer_Remote_Add(EMSegment
   SVN_REPOSITORY "http://svn.slicer.org/Slicer3/branches/Slicer4-EMSegment"
-  SVN_REVISION -r "17139"
+  SVN_REVISION -r "17140"
   OPTION_NAME Slicer_BUILD_EMSegment
   OPTION_DEPENDS "Slicer_BUILD_BRAINSTOOLS;Slicer_BUILD_QTLOADABLEMODULES;Slicer_USE_PYTHONQT_WITH_TCL"
   LABELS REMOTE_MODULE
