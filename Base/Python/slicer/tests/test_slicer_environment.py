4840b1a47bffb50d4c12ae4838cfed6020bd9dfe
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-09-22 03:59:45 +0000

    BUG: Fix test_slicer_environment on windows
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26392
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@26392 3bd1e089-480b-0410-8dfb-8563597acbee

--- Base/Python/slicer/tests/test_slicer_environment.py
+++ Base/Python/slicer/tests/test_slicer_environment.py
@@ -12,12 +12,12 @@ class SlicerEnvironmentTests(unittest.TestCase):
   def test_slicer_util_startupEnvironment(self):
     startupEnv = slicer.util.startupEnvironment()
     assert type(startupEnv) is dict
-    assert "Slicer-build" not in startupEnv["PATH"]
+    assert "PATH" not in startupEnv or "Slicer-build" not in startupEnv["PATH"]
 
   def test_slicer_app_startupEnvironment(self):
     startupEnv = slicer.app.startupEnvironment()
     assert type(startupEnv) is qt.QProcessEnvironment
-    assert "Slicer-build" not in startupEnv.value("PATH")
+    assert "Slicer-build" not in startupEnv.value("PATH", "")
 
   def test_slicer_app_environment(self):
     env = slicer.app.environment()
