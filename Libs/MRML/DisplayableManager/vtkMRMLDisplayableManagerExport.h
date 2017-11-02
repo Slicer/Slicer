Slicer/Slicer

commit dca7d7837f3f9017bc72acdc5f90a2ff700f4f6e

BUG: Fix initialization of module displayable managers
This commit removes the auto-initialization of displayable managers when
the displayable manager library was loaded. This approach was failing
when Slicer was built without Python support. Indeed, in that case the
displayable manager libraries were not loaded as a side effect of importing
the wrapper libraries.

Instead, this commit requires module to explicitly initialize its
associated displayable managers (similarly to what is already done in
tests.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26540
git-svn-id: http://svn.slicer.org/Slicer4/trunk@26540 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/vtkSlicerObjectFactory.cxx.in
@@ -42,7 +42,7 @@ void @vtk-module@ObjectFactory::PrintSelf(ostream &os, vtkIndent indent)
 // Registration of object factories.
 static unsigned int @vtk-module@Count;
 
-/* @VTK-MODULE@_EXPORT */ void @vtk-module@_AutoInit_Construct()
+@VTK-MODULE@_EXPORT void @vtk-module@_AutoInit_Construct()
 {
   if(++@vtk-module@Count == 1)
     {
@@ -57,7 +57,7 @@ static unsigned int @vtk-module@Count;
     }
 }
 
-/* @VTK-MODULE@_EXPORT */ void @vtk-module@_AutoInit_Destruct()
+@VTK-MODULE@_EXPORT void @vtk-module@_AutoInit_Destruct()
 {
   if(--@vtk-module@Count == 0)
     {

Libs/MRML/DisplayableManager/vtkMRMLDisplayableManagerExport.h
@@ -26,9 +26,4 @@
 #define VTK_MRML_DISPLAYABLEMANAGER_EXPORT
 #endif
 
-#if defined(MRMLDisplayableManager_AUTOINIT)
-#include "vtkAutoInit.h"
-VTK_AUTOINIT(MRMLDisplayableManager)
-#endif
-
 #endif

CMake/SlicerMacroBuildModuleLogic.cmake
@@ -86,19 +86,6 @@ macro(SlicerMacroBuildModuleLogic)
     set(MODULELOGIC_NO_INSTALL_OPTION "NO_INSTALL")
   endif()
 
-  set(Slicer_EXPORT_HEADER_CUSTOM_CONTENT)
-  if(${MODULELOGIC_NAME}_AUTOINIT)
-    set(Slicer_EXPORT_HEADER_CUSTOM_CONTENT "
-      // ####### Expanded from \@Slicer_EXPORT_HEADER_CUSTOM_CONTENT\@ #######
-      #if defined(${MODULELOGIC_NAME}_AUTOINIT)
-      # include \"vtkAutoInit.h\"
-      VTK_AUTOINIT(${MODULELOGIC_NAME})
-      #endif
-      // ##################################################
-      "
-      )
-  endif()
-
   SlicerMacroBuildModuleVTKLibrary(
     NAME ${MODULELOGIC_NAME}
     EXPORT_DIRECTIVE ${MODULELOGIC_EXPORT_DIRECTIVE}
@@ -108,13 +95,6 @@ macro(SlicerMacroBuildModuleLogic)
     TARGET_LIBRARIES ${MODULELOGIC_TARGET_LIBRARIES}
     ${MODULELOGIC_NO_INSTALL_OPTION}
     )
-
-  if(${MODULELOGIC_NAME}_AUTOINIT)
-    set_property(TARGET ${MODULELOGIC_NAME}
-      APPEND PROPERTY COMPILE_DEFINITIONS
-      "${PROJECT_NAME}_AUTOINIT=1(${MODULELOGIC_NAME})"
-      )
-  endif()
 
   set_property(GLOBAL APPEND PROPERTY SLICER_MODULE_LOGIC_TARGETS ${MODULELOGIC_NAME})
 

Modules/Loadable/Annotations/qSlicerAnnotationsModule.cxx
@@ -21,6 +21,10 @@
 #include "PythonQt.h"
 #endif
 
+// DisplayableManager initialization
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkSlicerAnnotationsModuleMRMLDisplayableManager)
+
 //-----------------------------------------------------------------------------
 #if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
 #include <QtPlugin>

Modules/Loadable/Markups/qSlicerMarkupsModule.cxx
@@ -42,6 +42,10 @@
 //#include "qSlicerMarkupsSettingsPanel.h"
 //#include "vtkSlicerMarkupsLogic.h"
 
+// DisplayableManager initialization
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkSlicerMarkupsModuleMRMLDisplayableManager)
+
 //-----------------------------------------------------------------------------
 #if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
 #include <QtPlugin>

Modules/Loadable/Segmentations/qSlicerSegmentationsModule.cxx
@@ -62,6 +62,10 @@
 // Qt includes
 #include <QDebug>
 
+// DisplayableManager initialization
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkSlicerSegmentationsModuleMRMLDisplayableManager)
+
 //-----------------------------------------------------------------------------
 #if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
 #include <QtPlugin>

Modules/Loadable/Transforms/qSlicerTransformsModule.cxx
@@ -31,9 +31,6 @@
 #include "qSlicerTransformsModule.h"
 #include "qSlicerTransformsModuleWidget.h"
 #include "qSlicerTransformsReader.h"
-#include "vtkMRMLLinearTransformsDisplayableManager3D.h"
-#include "vtkMRMLTransformsDisplayableManager2D.h"
-#include "vtkMRMLTransformsDisplayableManager3D.h"
 
 // VTK includes
 #include "vtkSmartPointer.h"
@@ -41,6 +38,10 @@
 // SubjectHierarchy Plugins includes
 #include "qSlicerSubjectHierarchyPluginHandler.h"
 #include "qSlicerSubjectHierarchyTransformsPlugin.h"
+
+// DisplayableManager initialization
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkSlicerTransformsModuleMRMLDisplayableManager)
 
 //-----------------------------------------------------------------------------
 #if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
@@ -148,14 +149,6 @@ void qSlicerTransformsModule::setup()
   app->coreIOManager()->registerIO(new qSlicerNodeWriter(
     "Transforms", QString("TransformFile"),
     QStringList() << "vtkMRMLTransformNode", true, this));
-
-  // Use the displayable manager class to make sure the the containing library is loaded
-  vtkSmartPointer<vtkMRMLTransformsDisplayableManager2D> dm2d=
-    vtkSmartPointer<vtkMRMLTransformsDisplayableManager2D>::New();
-  vtkSmartPointer<vtkMRMLTransformsDisplayableManager3D> dm3d=
-    vtkSmartPointer<vtkMRMLTransformsDisplayableManager3D>::New();
-  vtkSmartPointer<vtkMRMLLinearTransformsDisplayableManager3D> ldm3d=
-    vtkSmartPointer<vtkMRMLLinearTransformsDisplayableManager3D>::New();
 
   // Register displayable managers
   vtkMRMLSliceViewDisplayableManagerFactory::GetInstance()->RegisterDisplayableManager("vtkMRMLTransformsDisplayableManager2D");

Modules/Loadable/VolumeRendering/qSlicerVolumeRenderingModule.cxx
@@ -37,6 +37,10 @@
 #include "qSlicerSubjectHierarchyPluginHandler.h"
 #include "qSlicerSubjectHierarchyVolumeRenderingPlugin.h"
 
+// DisplayableManager initialization
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkSlicerVolumeRenderingModuleMRMLDisplayableManager)
+
 //-----------------------------------------------------------------------------
 #if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
 #include <QtPlugin>

commit 2c404f155fdcb46a65c0b9dfa57a52082694b681

BUG: Ensure core displayable managers are registered and available
Since the core displayable manager library is linked to other libraries,
it is reasonable to rely on its auto initialization.

This commit fixes a regression introduced in r26540 (BUG: Fix initialization
of module displayable managers) and addresses warnings like the following:

  Warning: In /path/to/Slicer/Libs/MRML/DisplayableManager/vtkMRMLDisplayableManagerFactory.cxx, line 139
  vtkMRMLSliceViewDisplayableManagerFactory (0xd85cb0): RegisterDisplayableManager - vtkMRMLVolumeGlyphSliceDisplayableManager is not a displayable manager. Failed to register

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26542
git-svn-id: http://svn.slicer.org/Slicer4/trunk@26542 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/vtkSlicerObjectFactory.cxx.in
@@ -42,7 +42,13 @@ void @vtk-module@ObjectFactory::PrintSelf(ostream &os, vtkIndent indent)
 // Registration of object factories.
 static unsigned int @vtk-module@Count;
 
-@VTK-MODULE@_EXPORT void @vtk-module@_AutoInit_Construct()
+#ifdef @vtk-module@_AUTOINIT
+# define @VTK-MODULE@_MODULEINIT_EXPORT
+#else
+# define @VTK-MODULE@_MODULEINIT_EXPORT @VTK-MODULE@_EXPORT
+#endif
+
+@VTK-MODULE@_MODULEINIT_EXPORT void @vtk-module@_AutoInit_Construct()
 {
   if(++@vtk-module@Count == 1)
     {
@@ -57,7 +63,7 @@ static unsigned int @vtk-module@Count;
     }
 }
 
-@VTK-MODULE@_EXPORT void @vtk-module@_AutoInit_Destruct()
+@VTK-MODULE@_MODULEINIT_EXPORT void @vtk-module@_AutoInit_Destruct()
 {
   if(--@vtk-module@Count == 0)
     {

Libs/MRML/DisplayableManager/vtkMRMLDisplayableManagerExport.h
@@ -26,4 +26,9 @@
 #define VTK_MRML_DISPLAYABLEMANAGER_EXPORT
 #endif
 
+#if defined(MRMLDisplayableManager_AUTOINIT)
+#include <vtkAutoInit.h>
+VTK_AUTOINIT(MRMLDisplayableManager)
+#endif
+
 #endif