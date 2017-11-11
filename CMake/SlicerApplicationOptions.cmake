2ec1ea538a22afc6e514bb5a3c3f9b78b56ea78b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-11-11 17:57:55 +0000

    COMP: SlicerApplicationOptions: Only mark as advanced cache variables
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26606
    git-svn-id: http://svn.slicer.org/Slicer4/branches/Slicer-4-8@26606 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerApplicationOptions.cmake
+++ CMake/SlicerApplicationOptions.cmake
@@ -30,15 +30,15 @@ mark_as_superbuild(Slicer_BUILD_WIN32_CONSOLE:BOOL)
 #-----------------------------------------------------------------------------
 if(NOT DEFINED Slicer_ORGANIZATION_DOMAIN)
   set(Slicer_ORGANIZATION_DOMAIN "www.na-mic.org" CACHE STRING "Organization domain")
+  mark_as_advanced(Slicer_ORGANIZATION_DOMAIN)
 endif()
-mark_as_advanced(Slicer_ORGANIZATION_DOMAIN)
 mark_as_superbuild(Slicer_ORGANIZATION_DOMAIN)
 message(STATUS "Configuring ${Slicer_MAIN_PROJECT_APPLICATION_NAME} organization domain [${Slicer_ORGANIZATION_DOMAIN}]")
 
 if(NOT DEFINED Slicer_ORGANIZATION_NAME)
   set(Slicer_ORGANIZATION_NAME "NA-MIC" CACHE STRING "Organization name")
+  mark_as_advanced(Slicer_ORGANIZATION_NAME)
 endif()
-mark_as_advanced(Slicer_ORGANIZATION_NAME)
 mark_as_superbuild(Slicer_ORGANIZATION_NAME)
 message(STATUS "Configuring ${Slicer_MAIN_PROJECT_APPLICATION_NAME} organization name [${Slicer_ORGANIZATION_NAME}]")
 
@@ -53,16 +53,16 @@ mark_as_superbuild(SLICER_REVISION_SPECIFIC_USER_SETTINGS_FILEBASENAME:STRING)
 #-----------------------------------------------------------------------------
 if(NOT DEFINED Slicer_DEFAULT_HOME_MODULE)
   set(Slicer_DEFAULT_HOME_MODULE "Welcome" CACHE STRING "Name of the module that Slicer activates on start-up by default")
+  mark_as_advanced(Slicer_DEFAULT_HOME_MODULE)
 endif()
-mark_as_advanced(Slicer_DEFAULT_HOME_MODULE)
 mark_as_superbuild(Slicer_DEFAULT_HOME_MODULE)
 message(STATUS "Configuring ${Slicer_MAIN_PROJECT_APPLICATION_NAME} default home module [${Slicer_DEFAULT_HOME_MODULE}]")
 
 if(NOT DEFINED Slicer_DEFAULT_FAVORITE_MODULES)
   set(Slicer_DEFAULT_FAVORITE_MODULES "Data, Volumes, Models, Transforms, Markups, Editor, SegmentEditor"
       CACHE STRING "Name of the modules shown on the toolbar by default (comma-separated list)")
+  mark_as_advanced(Slicer_DEFAULT_FAVORITE_MODULES)
 endif()
-mark_as_advanced(Slicer_DEFAULT_FAVORITE_MODULES)
 mark_as_superbuild(Slicer_DEFAULT_FAVORITE_MODULES)
 message(STATUS "Configuring ${Slicer_MAIN_PROJECT_APPLICATION_NAME} default favorite modules [${Slicer_DEFAULT_FAVORITE_MODULES}]")
 
@@ -73,8 +73,8 @@ if(NOT DEFINED Slicer_DISCLAIMER_AT_STARTUP)
   set(Slicer_DISCLAIMER_AT_STARTUP "Thank you for using %1!<br><br>This software is not intended for clinical use." CACHE STRING "Text displayed at the first startup of Slicer in a popup window")
   # Note: %1 is replaced by the application name and version. Add <br> for newline.
   # If the disclaimer text is set to empty then no disclaimer popup is displayed.
+  mark_as_advanced(Slicer_DISCLAIMER_AT_STARTUP)
 endif()
-mark_as_advanced(Slicer_DISCLAIMER_AT_STARTUP)
 mark_as_superbuild(Slicer_DISCLAIMER_AT_STARTUP)
 message(STATUS "Configuring ${Slicer_MAIN_PROJECT_APPLICATION_NAME} text of disclaimer at startup [${Slicer_DISCLAIMER_AT_STARTUP}]")
 
