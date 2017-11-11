3129c083a1011d070f92b9422a3be99c510a4027
Steve Pieper <pieper@isomics.com>
2017-11-11 17:57:23 +0000

    ENH: model DICOM acquisition geometry (addresses #4409)
    Add a step to the DICOMScalarVolumePlugin to check that the loaded
    volume node's slice geometry matches what is expected from
    the position and orientation information of the individual
    slices in the DICOM headers.
    
    If needed, a grid transform is created to reposition each slice
    to the appropriate position in patient space.
    
    Use an empirically defined epsilon threshold to decide if
    an acquisition transform is needed.  The selected value of
    1e-3 millimeters (one micrometer) is still very small by
    medical imaging standards but is large enough that rounding
    errors and other small variations do not result in extra
    correction transforms when they won't add real value.
    The error is defined as the max absolute difference
    in any coordinate of the corner of any slice so it
    is designed to handle gantry tilt, missing slices,
    and irregularly spaced slices.
    
    The DICOMReaders self test has been extended to test this in the
    case where slices are missing from the middle of the volume.  The
    grid transform results in an interpolated band of pixels using
    information from adjacent slices to 'fill in' for the missing
    data.  The test confirms that the last slice of the reconstructed
    volume is in the expected location in patient space.  This correction
    means that calculations like ruler or segment statistics can be
    robust even with missing slices.
    
    Currently the only available testing data is for the case of missing
    slices.  However this code should allow proper loading and display of
    several not-uncommon imaging scenarios:
    
    1) Missing slices due to network failure or accidental file deletion.
    
    2) Gantry tilted image acquisitons (see #4409) which is
    used, for example, to minimize radiation exposure to the retinas
    especially in pediatric imaging.
    
    3) Variable table speed during CT acquisition, which is sometimes
    used in abdominal or musculoskeletal imaging to provide high resolution
    in selected body parts and lower resolution between (e.g. high res
    at the ankle, knee and hip, but low res in the shin and thigh).
    
    Consderations:
    
    By creating a transform we preserve the original data and leave it
    to the user to resample if desired.  The option of resampling
    during load was rejected since it is unclear what sampling grid
    would be best in all cases.
    
    Although the transform should correct the geometry, a warning dialog is
    still generated during loading since the issue could be correctable
    (such as missing slices) or may be triggered by other data issues
    not forseen by this code.
    
    Most DICOM data has identical row and column spacing
    but when they do not it's critical to have the right
    spacing geomtry.  This code uses the correct definitions
    from the standard.  Where PixelSpacing is row/column
    but ImageOrientationPatient if column/row
    
    We check and avoid ValueError warning when dicom files do not have
    geometry information.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26569
    git-svn-id: http://svn.slicer.org/Slicer4/branches/Slicer-4-8@26569 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/SlicerApp/Testing/Python/DICOMReaders.py
+++ Applications/SlicerApp/Testing/Python/DICOMReaders.py
@@ -83,9 +83,11 @@ class DICOMReadersTest(ScriptedLoadableModuleTest):
     """Run as few or as many tests as needed here.
     """
     self.setUp()
-    self.test_Part1DICOM()
+    self.test_AlternateReaders()
+    self.setUp()
+    self.test_MissingSlices()
 
-  def test_Part1DICOM(self):
+  def test_AlternateReaders(self):
     """ Test the DICOM loading of sample testing data
     """
     testPass = True
@@ -153,7 +155,7 @@ class DICOMReadersTest(ScriptedLoadableModuleTest):
         indexer.waitForImportFinished()
 
         #
-        # selct the series
+        # select the series
         #
         detailsPopup = slicer.modules.DICOMWidget.detailsPopup
         detailsPopup.open()
@@ -222,5 +224,111 @@ class DICOMReadersTest(ScriptedLoadableModuleTest):
     mainWindow.moduleSelector().selectModule('DICOMReaders')
 
     logging.info(loadingResult)
+
+    return testPass
+
+  def test_MissingSlices(self):
+    """ Test behavior of the readers when slices are missing
+
+    To edit and run this test from the python console, paste this below:
+
+reloadScriptedModule('DICOMReaders'); import DICOMReaders; tester = DICOMReaders.DICOMReadersTest(); tester.setUp(); tester.test_MissingSlices()
+
+    """
+    testPass = True
+    import os, json
+    self.delayDisplay("Starting the DICOM test", 100)
+
+    datasetURL = "http://slicer.kitware.com/midas3/download/item/294857/deidentifiedMRHead-dcm-one-series.zip"
+    fileName = "deidentifiedMRHead-dcm-one-series.zip"
+    filePath = os.path.join(slicer.app.temporaryPath,fileName)
+    seriesUID = "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.270.0"
+    seriesRASBounds = [-87.29489517211913, 81.70450973510744,
+                       -121.57139587402344, 134.42860412597656,
+                       -138.71430206298828, 117.28569793701172]
+    seriesDirectory = "Series 004 [MR - SAG RF FAST VOL FLIP 20]"
+    lastSliceCorners = [[[81.05451202, 133.92860413, 116.78569794], [81.05451202, -122.07139587, 116.78569794]],
+                        [[81.05451202, 133.92860413, -139.21429443], [81.05451202, -122.07139587, -139.21429443]]]
+    filesToRemove = [
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.361.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.362.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.363.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.364.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.365.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.366.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.367.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.368.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.369.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.370.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.371.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.372.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.373.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.374.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.375.0.dcm",
+      "1.3.6.1.4.1.5962.99.1.3814087073.479799962.1489872804257.376.0.dcm",
+    ]
+
+    try:
+      if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
+        self.delayDisplay('Requesting download %s from %s...\n' % (fileName, datasetURL), 100)
+        urllib.urlretrieve(datasetURL, filePath)
+      self.delayDisplay('Finished with download\n', 100)
+
+      self.delayDisplay("Unzipping", 100)
+      dicomFilesDirectory = slicer.app.temporaryPath + 'MRhead'
+      qt.QDir().mkpath(dicomFilesDirectory)
+      slicer.app.applicationLogic().Unzip(filePath, dicomFilesDirectory)
+
+      print('Removing %d files from the middle of the series' % len(filesToRemove))
+      for file in filesToRemove:
+        filePath = os.path.join(dicomFilesDirectory, seriesDirectory, file)
+        os.remove(filePath)
+
+      #
+      # insert the data into the database
+      #
+      self.delayDisplay("Switching to temp database directory", 100)
+      originalDatabaseDirectory = DICOMUtils.openTemporaryDatabase('tempDICOMDatabase')
+
+      self.delayDisplay('Importing DICOM', 100)
+      mainWindow = slicer.util.mainWindow()
+      mainWindow.moduleSelector().selectModule('DICOM')
+
+      indexer = ctk.ctkDICOMIndexer()
+      indexer.addDirectory(slicer.dicomDatabase, dicomFilesDirectory, None)
+      indexer.waitForImportFinished()
+
+      #
+      # select the series
+      #
+      detailsPopup = slicer.modules.DICOMWidget.detailsPopup
+      detailsPopup.open()
+      # load the data by series UID
+      detailsPopup.offerLoadables(seriesUID,'Series')
+      detailsPopup.examineForLoading()
+      loadable = detailsPopup.getAllSelectedLoadables().keys()[0]
+
+      if len(loadable.warning) == 0:
+        raise Exception("Expected warning about geometry issues due to missing slices!")
+
+      #
+      # load and correct for acquisition then check the geometry
+      #
+      scalarVolumePlugin = slicer.modules.dicomPlugins['DICOMScalarVolumePlugin']()
+      volumeNode = scalarVolumePlugin.load(loadable)
+
+      if not numpy.allclose(scalarVolumePlugin.acquisitionModeling.fixedCorners[-1], lastSliceCorners):
+        raise Exception("Acquisition transform didn't fix slice corners!")
+
+      self.delayDisplay('test_MissingSlices passed!', 200)
+
+    except Exception, e:
+      import traceback
+      traceback.print_exc()
+      self.delayDisplay('Missing Slices Test caused exception!\n' + str(e), 2000)
+      testPass = False
+
+    self.delayDisplay("Restoring original database directory", 200)
+    mainWindow.moduleSelector().selectModule('DICOMReaders')
 
     return testPass
