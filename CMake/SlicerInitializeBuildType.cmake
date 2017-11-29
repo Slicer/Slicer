5f72b0fe094dbe737e99adb290258bd12efeee77
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-11-29 14:15:36 +0000

    STYLE: Update Extension Build System to use SlicerInitializeBuildType
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26657
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@26657 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerInitializeBuildType.cmake
+++ CMake/SlicerInitializeBuildType.cmake
@@ -22,9 +22,11 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
 endif()
 
 # Pass variables to dependent projects
-if(NOT CMAKE_CONFIGURATION_TYPES)
-  mark_as_superbuild(VARS CMAKE_BUILD_TYPE ALL_PROJECTS)
-else()
-  mark_as_superbuild(VARS CMAKE_CONFIGURATION_TYPES ALL_PROJECTS)
+if(COMMAND mark_as_superbuild)
+  if(NOT CMAKE_CONFIGURATION_TYPES)
+    mark_as_superbuild(VARS CMAKE_BUILD_TYPE ALL_PROJECTS)
+  else()
+    mark_as_superbuild(VARS CMAKE_CONFIGURATION_TYPES ALL_PROJECTS)
+  endif()
 endif()
 
