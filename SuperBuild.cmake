b5f72d3bb4299a957a45a738334754425ca167fa
Andras Lasso <lasso@queensu.ca>
2018-01-22 14:13:55 +0000

    ENH: Updated LandmarkRegistration extension
    Updated LandmarkRegistration extension to reduca usage of slicer.util.getNode.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26845
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@26845 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -350,7 +350,7 @@ list_conditional_append(Slicer_BUILD_CompareVolumes Slicer_REMOTE_DEPENDENCIES C
 
 Slicer_Remote_Add(LandmarkRegistration
   GIT_REPOSITORY "${git_protocol}://github.com/pieper/LandmarkRegistration"
-  GIT_TAG "8ff85fc303e84ad4dbf612973532f9982596c236"
+  GIT_TAG "48e5be6c83079dec85113e2c0310b623b2161066"
   OPTION_NAME Slicer_BUILD_LandmarkRegistration
   OPTION_DEPENDS "Slicer_BUILD_CompareVolumes;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
