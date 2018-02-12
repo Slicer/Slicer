Slicer/Slicer

commit 8d258e591c61ee1f6242e442368b52731aaaa969

BUG: update CTK for Qt5 PythonQt wrapping
Fixes an issue with QSocketNotify.

There should be a follow on fix in the upstream repository
but this commit is workable until that is ready.

See https://sourceforge.net/p/pythonqt/discussion/631392/thread/c989429c/

$ git shortlog 9604eda05cbe1016395b7e197b8fde6b2d1181e9..a6da1ca8f9ff890c4587b4f16059f4cc54e32ecb --no-merges
Steve Pieper (1):
      Update PythonQt for QSocketNotifier for Qt5

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26898
git-svn-id: http://svn.slicer.org/Slicer4/trunk@26898 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTK.cmake
@@ -61,7 +61,6 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       -DCTK_QT_VERSION:STRING=5
       )
   endif()
-
   if(NOT DEFINED git_protocol)
     set(git_protocol "git")
   endif()
@@ -74,7 +73,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "9604eda05cbe1016395b7e197b8fde6b2d1181e9"
+    "a6da1ca8f9ff890c4587b4f16059f4cc54e32ecb"
     QUIET
     )
 

commit 0c44edbb524b32ba21462cc51db7bf1d4cc0cb13

COMP: Fix macOS package ensuring PythonQt library "id" is the full path
This commit addresses a regression introduced by r26898 (BUG: update CTK
for Qt5 PythonQt wrapping) where PythonQt build system was updated to
no set the INSTALL_NAME_DIR property.

Since the macOS fixup process done during the packaging requires all the
library to have a full path, this commit adds a custom step ensuring it
is the case. (Similarly to what is already done for LibArchive)

See https://discourse.slicer.org/t/python-qt-library-is-missing-from-todays-mac-nightly/2072/6

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26911
git-svn-id: http://svn.slicer.org/Slicer4/trunk@26911 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTK.cmake
@@ -135,6 +135,17 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
+  if(Slicer_USE_PYTHONQT)
+    if(APPLE)
+      ExternalProject_Add_Step(${proj} fix_pythonqt_rpath
+        COMMAND install_name_tool
+          -id ${EP_BINARY_DIR}/CMakeExternals/Install/lib/libPythonQt.dylib
+          ${EP_BINARY_DIR}/CMakeExternals/Install/lib/libPythonQt.dylib
+        DEPENDEES build
+        )
+    endif()
+  endif()
+
   ExternalProject_GenerateProjectDescription_Step(${proj})
 
   set(CTK_DIR ${EP_BINARY_DIR})