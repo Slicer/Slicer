Slicer/Slicer

commit 8b16b1e86aef84813774235d45f8cb7072fb8ced

COMP: Update Swig external project to avoid race condition
This commit forces Swig install step to be sequential to avoid race
condition like the following:

mkdir: /Volumes/Dashboards/Nightly/Slicer-0-build/Swig-install: File exists
install: mkdir /Volumes/Dashboards/Nightly/Slicer-0-build/Swig-install/bin: File exists
make[4]: *** [install] Error 71
make[3]: *** [install-ccache] Error 2
make[3]: *** Waiting for unfinished jobs....

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26877
git-svn-id: http://svn.slicer.org/Slicer4/trunk@26877 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_Swig.cmake
@@ -88,6 +88,7 @@ ExternalProject_Execute(${proj} \"configure\" sh ${EP_SOURCE_DIR}/configure
       SOURCE_DIR ${EP_SOURCE_DIR}
       BINARY_DIR ${EP_BINARY_DIR}
       CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${_configure_script}
+      INSTALL_COMMAND $(MAKE) install -j1  # Avoid race condition disabling parallel build
       DEPENDS ${${proj}_DEPENDENCIES}
       )
 

commit 42c98d6a084bf50610afe6a7f2c2e11fc0a54a7d

COMP: BuildSystem: Update External_Swig.cmake to fix ninja build error
Update the install command to use variable CMAKE_MAKE_PROGRAM instead of
the hard-coded name of the build tool.

This commit fixes a regression introduced in r26877 (COMP: Update Swig
external project to avoid race condition)

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26917
git-svn-id: http://svn.slicer.org/Slicer4/trunk@26917 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_Swig.cmake
@@ -88,7 +88,7 @@ ExternalProject_Execute(${proj} \"configure\" sh ${EP_SOURCE_DIR}/configure
       SOURCE_DIR ${EP_SOURCE_DIR}
       BINARY_DIR ${EP_BINARY_DIR}
       CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${_configure_script}
-      INSTALL_COMMAND $(MAKE) install -j1  # Avoid race condition disabling parallel build
+      INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install -j1  # Avoid race condition disabling parallel build
       DEPENDS ${${proj}_DEPENDENCIES}
       )
 