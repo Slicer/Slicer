7313ee621311f8228e0418daa36f409a09bc4f6a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-02 07:50:32 +0000

    STYLE: Simplify and cleanup SuperBuild extension template
    * Add comments to organize the different parameters passed as CMAKE_CACHE_ARGS
    
    * Remove explicit passing of GIT_EXECUTABLE and Subversion_SVN_EXECUTABLE. These
      variables are already "marked as superbuild" after including "${Slicer_USE_FILE}".
      See r26271
    
    * Remove explicit passing of CMAKE_BUILD_TYPE, the variable is also marked
      as superbuild in "${Slicer_USE_FILE}" by including "SlicerInitializeBuildType"
      module.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26985
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@26985 3bd1e089-480b-0410-8dfb-8563597acbee

--- Extensions/Testing/SuperBuildExtensionTemplate/SuperBuild/External_Foo.cmake
+++ Extensions/Testing/SuperBuildExtensionTemplate/SuperBuild/External_Foo.cmake
@@ -18,29 +18,37 @@ endif()
 
 if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
+  set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
+  set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
+
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     #GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/Foo/Foo.git"
     #GIT_TAG "1e823001cb41c92667299635643f1007876d09f6"
     DOWNLOAD_COMMAND ${CMAKE_COMMAND} -E echo "Remove this line and uncomment GIT_REPOSITORY and GIT_TAG"
-    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
-    BINARY_DIR ${proj}-build
+    SOURCE_DIR ${EP_SOURCE_DIR}
+    BINARY_DIR ${EP_BINARY_DIR}
     CMAKE_CACHE_ARGS
-      -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
-      -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+      # Compiler settings
       -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
       -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
+      -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+      -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
       -DCMAKE_CXX_STANDARD:STRING=${CMAKE_CXX_STANDARD}
       -DCMAKE_CXX_STANDARD_REQUIRED:BOOL=${CMAKE_CXX_STANDARD_REQUIRED}
       -DCMAKE_CXX_EXTENSIONS:BOOL=${CMAKE_CXX_EXTENSIONS}
-      -DBUILD_TESTING:BOOL=OFF
+      # Output directories
       -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH=${CMAKE_BINARY_DIR}/${Slicer_THIRDPARTY_BIN_DIR}
       -DCMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH=${CMAKE_BINARY_DIR}/${Slicer_THIRDPARTY_LIB_DIR}
       -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH=${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}
-       # The following two variables should be udpated to match the
-       # requirements of a real CMake based external project.
+      # Install directories
+      # XXX The following two variables should be udpated to match the
+      #     requirements of a real CMake based external project
+      # XXX Then, this comment and the one above should be removed. Really.
       -DFOO_INSTALL_RUNTIME_DIR:STRING=${Slicer_INSTALL_THIRDPARTY_LIB_DIR}
       -DFOO_INSTALL_LIBRARY_DIR:STRING=${Slicer_INSTALL_THIRDPARTY_LIB_DIR}
+      # Options
+      -DBUILD_TESTING:BOOL=OFF
     CONFIGURE_COMMAND ${CMAKE_COMMAND} -E echo
       "This CONFIGURE_COMMAND is just here as a placeholder."
       "Remove this line to enable configuring of a real CMake based external project"
