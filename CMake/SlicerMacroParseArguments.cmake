31080f1e0e4c49dd320397fb16d35d44338f6821
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-03 06:16:12 +0000

    STYLE: Remove obsolete CMake module SlicerMacroParseArguments
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=26990
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@26990 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerMacroParseArguments.cmake
+++
@@ -1,9 +0,0 @@
-
-#
-# See http://www.cmake.org/Wiki/CMakeMacroParseArguments
-#
-macro(slicer_parse_arguments prefix arg_names option_names)
-  message(FATAL_ERROR "Macro 'slicer_parse_arguments' has been removed. Consider using the built-in"
-                      " macro 'cmake_parse_arguments' instead."
-                      "See http://www.cmake.org/cmake/help/v2.8.10/cmake.html#module:CMakeParseArguments")
-endmacro()
