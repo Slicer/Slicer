70ab588849efa4c602b373041f011af3486df592
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-09 10:41:48 +0000

    COMP: Fix extension build by setting CMAKE_DEFAULT_BUILD_TYPE in SlicerConfig
    This commit ensures that CMAKE_BUILD_TYPE is properly initialized when
    configuring extension without explicitly specifying the build type. It
    fixes a regression introduced in r26978 (ENH: Update UseSlicer to use
    SlicerInitializeBuildType) and ensure that SlicerConfig is always configured
    with a non-empty value for "CMAKE_DEFAULT_BUILD_TYPE".
    
    Reported-by: Csaba Pinter <csaba.pinter@queensu.ca>
    Reported-by: Kristjan Anderle <gsi-kanderle@users.noreply.github.com>
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27032
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@27032 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/SlicerInitializeBuildType.cmake
+++ CMake/SlicerInitializeBuildType.cmake
@@ -1,11 +1,11 @@
 
+# Default build type to use if none was specified
+if(NOT DEFINED CMAKE_DEFAULT_BUILD_TYPE)
+  set(CMAKE_DEFAULT_BUILD_TYPE "Debug")
+endif()
+
 # Set a default build type if none was specified
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-
-  # Default build type to use if none was specified
-  if(NOT DEFINED CMAKE_DEFAULT_BUILD_TYPE)
-    set(CMAKE_DEFAULT_BUILD_TYPE "Debug")
-  endif()
 
   message(STATUS "Setting build type to '${CMAKE_DEFAULT_BUILD_TYPE}' as none was specified.")
 
