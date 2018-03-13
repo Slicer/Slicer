1f6727d1a0ba66420cfd97aad0e5987de493cf46
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-13 09:45:10 +0000

    BUG: Update ExternalProjectDependency based on commontk/Artichoke@75af1d0
    This will ensure dependent options are properly passed to external projects
    even after their value is updated due to a change in a dependent option.
    
    For example, Slicer_BUILD_EMSegment is ON by default and depends on
    Slicer_USE_PYTHONQT_WITH_TCL also ON by default.
    
    After setting Slicer_USE_PYTHONQT_WITH_TCL to OFF, Slicer_BUILD_EMSegment
    is expected to be OFF but the value ON was still passed to Slicer.
    
    It turns out that using "cmake_dependent_option" sets an INTERNAL cache
    value used to keep track of the previous value set by the user.
    Prior to this commit, "mark_as_superbuild" was incorrectly passing the
    INTERNAL value instead of the non cached value.
    
    List of changes:
    
    $ git shortlog 75af1d0..67624ee --no-merges
    Jean-Christophe Fillion-Robin (1):
          ExternalProjectDependency: Ignore CACHE value of type INTERNAL
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27059
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@27059 3bd1e089-480b-0410-8dfb-8563597acbee

--- CMake/ExternalProjectDependency.cmake
+++ CMake/ExternalProjectDependency.cmake
@@ -271,9 +271,15 @@ function(_sb_cmakevar_to_cmakearg cmake_varname_and_type cmake_arg_var has_cfg_i
   _sb_extract_varname_and_vartype(${cmake_varname_and_type} _varname _vartype)
 
   set(_var_value "${${_varname}}")
-  get_property(_value_set_in_cache CACHE ${_varname} PROPERTY VALUE SET)
-  if(_value_set_in_cache)
-    get_property(_var_value CACHE ${_varname} PROPERTY VALUE)
+
+  # Use cache value unless it is INTERNAL
+  if(_vartype STREQUAL "INTERNAL")
+    set(_vartype "STRING")
+  else()
+    get_property(_value_set_in_cache CACHE ${_varname} PROPERTY VALUE SET)
+    if(_value_set_in_cache)
+      get_property(_var_value CACHE ${_varname} PROPERTY VALUE)
+    endif()
   endif()
 
   set(_has_cfg_intdir FALSE)
