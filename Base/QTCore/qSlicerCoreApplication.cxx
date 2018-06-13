Slicer/Slicer

commit c99f36429624aab956a8af02b24fe346c9f1b2eb

ENH: Support import of Slicer python packages using SlicerPython
This commit updates the version of CTKAppLauncher to 0.1.23 to support
import of package like vtk using SlicerPython.

More specifically, it configures the SlicerPython launcher ensuring
the Slicer settings can be loaded (if available) from either the build or
installed tree.

To prevent unwanted Slicer settings from overwriting the SlicerPython
settings, the setting groups "General,Application,ExtraApplicationToLaunch"
are explicitly ignored if found in additional settings files.

List of CTKAppLauncher/CTKAppLauncherLib changes:

$ git shortlog 48b4ae6..37c0e80 --no-merges
Jean-Christophe Fillion-Robin (21):
      ci: Switch to CircleCi v2
      Fix travis failures
      Add support for <APPLAUNCHER_SETTINGS_DIR>
      Update AppLauncher-TestPlaceHolderStrings to include additional settings file
      Rename "additionalSettingsDir()" to "userAdditionalSettingsDir()"
      Consistently display and define "UserAdditionalSettings" variables
      doc: Document "SettingsType" enum found in ctkAppLauncherSettingsPrivate
      Fix "AppLauncher-TestLauncherAdditionalSettings-CheckForSettingsOverwrite"
      style: Improve ivar prefix used in AppLauncher-TestLauncherAdditionalSettings tests
      Add support for additionalSettingsFilePath
      CTKAppLauncher 0.1.22
      Begin post-0.1.22 development
      Report launcher info only after reading the settings
      test: Update CheckForSettingsOverwrite to include LauncherNoSplashScreen and AdditionalSettingsFilePath
      Do not return an error if "AdditionalSettingsFile" is set to an non-existing file
      test: Update extract_application_settings_value to check return code
      style: Update tests to consistently use "check_(expected|unexpected)_string" function
      style: Consistently name ivars in TestLauncherAdditionalSettings test
      Add support for "AdditionalSettingsExcludeGroups"

Base/QTCore/qSlicerCoreApplication.cxx
@@ -237,6 +237,7 @@ void qSlicerCoreApplicationPrivate::init()
   ctkAppLauncherSettings appLauncherSettings;
   appLauncherSettings.setLauncherName(q->applicationName());
   appLauncherSettings.setLauncherDir(this->SlicerHome);
+  appLauncherSettings.setLauncherSettingsDir(QFileInfo(q->launcherSettingsFilePath()).absolutePath());
   if (!appLauncherSettings.readSettings(q->launcherSettingsFilePath()))
     {
     qCritical() << "Failed to read launcher settings" << q->launcherSettingsFilePath();

SuperBuild/External_CTKAPPLAUNCHER.cmake
@@ -24,15 +24,15 @@ if(Slicer_USE_CTKAPPLAUNCHER)
   if(NOT DEFINED CTKAppLauncher_DIR)
 
     SlicerMacroGetOperatingSystemArchitectureBitness(VAR_PREFIX CTKAPPLAUNCHER)
-    set(launcher_version "0.1.21")
+    set(launcher_version "0.1.23")
     # On windows, use i386 launcher unconditionally
     if("${CTKAPPLAUNCHER_OS}" STREQUAL "win")
       set(CTKAPPLAUNCHER_ARCHITECTURE "i386")
-      set(md5 "9fa2acb7d934a43720a0bee8679cbeff")
+      set(md5 "bd3b78c36365e7938c72aed4febdac1e")
     elseif("${CTKAPPLAUNCHER_OS}" STREQUAL "linux")
-      set(md5 "57d9e6fc1fdfe42d78fc4e277b9559ba")
+      set(md5 "0c2d385eff14692defa674e1c6426234")
     elseif("${CTKAPPLAUNCHER_OS}" STREQUAL "macosx")
-      set(md5 "1f0d86b1eeb386d6892a76db7b111280")
+      set(md5 "ee6bc0e339c5378019d8b3de788b09f8")
     endif()
 
     set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj})

SuperBuild/External_CTKAppLauncherLib.cmake
@@ -26,7 +26,7 @@ if(NOT DEFINED CTKAppLauncherLib_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "48b4ae63168e3930e7a4d48da705c27c557e5564"
+    "37c0e80b95581e510d1b942324cbf98cf3e550c3"
     QUIET
     )
 

CMake/SlicerBlockCTKAppLauncherSettings.cmake
@@ -67,7 +67,7 @@
 # LIBRARY_PATHS
 #-----------------------------------------------------------------------------
 set(SLICER_LIBRARY_PATHS_BUILD
-  <APPLAUNCHER_DIR>/bin/<CMAKE_CFG_INTDIR>
+  <APPLAUNCHER_SETTINGS_DIR>/bin/<CMAKE_CFG_INTDIR>
   )
 
 if(NOT Slicer_USE_SYSTEM_QT)
@@ -83,13 +83,13 @@ endif()
 
 if(Slicer_BUILD_CLI_SUPPORT AND Slicer_BUILD_CLI)
   list(APPEND SLICER_LIBRARY_PATHS_BUILD
-    <APPLAUNCHER_DIR>/${Slicer_CLIMODULES_LIB_DIR}/<CMAKE_CFG_INTDIR>
+    <APPLAUNCHER_SETTINGS_DIR>/${Slicer_CLIMODULES_LIB_DIR}/<CMAKE_CFG_INTDIR>
     )
 endif()
 
 if(Slicer_BUILD_QTLOADABLEMODULES)
   list(APPEND SLICER_LIBRARY_PATHS_BUILD
-    <APPLAUNCHER_DIR>/${Slicer_QTLOADABLEMODULES_LIB_DIR}/<CMAKE_CFG_INTDIR>
+    <APPLAUNCHER_SETTINGS_DIR>/${Slicer_QTLOADABLEMODULES_LIB_DIR}/<CMAKE_CFG_INTDIR>
     )
 endif()
 
@@ -102,13 +102,13 @@ endforeach()
 # PATHS
 #-----------------------------------------------------------------------------
 set(SLICER_PATHS_BUILD
-  <APPLAUNCHER_DIR>/bin/<CMAKE_CFG_INTDIR>
+  <APPLAUNCHER_SETTINGS_DIR>/bin/<CMAKE_CFG_INTDIR>
   ${QT_BINARY_DIR}
   )
 
 if(Slicer_BUILD_CLI_SUPPORT AND Slicer_BUILD_CLI)
   list(APPEND SLICER_PATHS_BUILD
-    <APPLAUNCHER_DIR>/${Slicer_CLIMODULES_BIN_DIR}/<CMAKE_CFG_INTDIR>
+    <APPLAUNCHER_SETTINGS_DIR>/${Slicer_CLIMODULES_BIN_DIR}/<CMAKE_CFG_INTDIR>
     )
 endif()
 
@@ -122,11 +122,11 @@ endforeach()
 #-----------------------------------------------------------------------------
 set(SLICER_ENVVARS_BUILD
   "SLICER_HOME=${Slicer_BINARY_DIR}" # See note below
-  "ITK_AUTOLOAD_PATH=<APPLAUNCHER_DIR>/${Slicer_ITKFACTORIES_DIR}/<CMAKE_CFG_INTDIR>"
+  "ITK_AUTOLOAD_PATH=<APPLAUNCHER_SETTINGS_DIR>/${Slicer_ITKFACTORIES_DIR}/<CMAKE_CFG_INTDIR>"
   )
 if(Slicer_USE_PYTHONQT_WITH_OPENSSL)
   list(APPEND SLICER_ENVVARS_BUILD
-    "SSL_CERT_FILE=<APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/Slicer.crt"
+    "SSL_CERT_FILE=<APPLAUNCHER_SETTINGS_DIR>/${Slicer_SHARE_DIR}/Slicer.crt"
     )
 endif()
 
@@ -141,7 +141,7 @@ endforeach()
 
 # QT_PLUGIN_PATH
 set(SLICER_QT_PLUGIN_PATH_BUILD
-  "<APPLAUNCHER_DIR>/bin"
+  "<APPLAUNCHER_SETTINGS_DIR>/bin"
   "${CTK_DIR}/CTK-build/bin"
   "${QT_PLUGINS_DIR}"
   )
@@ -160,19 +160,19 @@ set(SLICER_ADDITIONAL_PATH_ENVVARS_BUILD
 if(Slicer_USE_PYTHONQT)
 
   set(SLICER_PYTHONPATH_BUILD
-    "<APPLAUNCHER_DIR>/bin/<CMAKE_CFG_INTDIR>"
-    "<APPLAUNCHER_DIR>/bin/Python"
+    "<APPLAUNCHER_SETTINGS_DIR>/bin/<CMAKE_CFG_INTDIR>"
+    "<APPLAUNCHER_SETTINGS_DIR>/bin/Python"
     )
   if(Slicer_BUILD_QTLOADABLEMODULES)
     list(APPEND SLICER_PYTHONPATH_BUILD
-      "<APPLAUNCHER_DIR>/${Slicer_QTLOADABLEMODULES_LIB_DIR}/<CMAKE_CFG_INTDIR>"
-      "<APPLAUNCHER_DIR>/${Slicer_QTLOADABLEMODULES_PYTHON_LIB_DIR}"
+      "<APPLAUNCHER_SETTINGS_DIR>/${Slicer_QTLOADABLEMODULES_LIB_DIR}/<CMAKE_CFG_INTDIR>"
+      "<APPLAUNCHER_SETTINGS_DIR>/${Slicer_QTLOADABLEMODULES_PYTHON_LIB_DIR}"
       )
   endif()
 
   if(Slicer_USE_PYTHONQT)
     list(APPEND SLICER_PYTHONPATH_BUILD
-      "<APPLAUNCHER_DIR>/${Slicer_QTSCRIPTEDMODULES_LIB_DIR}"
+      "<APPLAUNCHER_SETTINGS_DIR>/${Slicer_QTSCRIPTEDMODULES_LIB_DIR}"
       )
   endif()
 
@@ -202,18 +202,18 @@ endif()
 #
 #  Indeed, on MacOSX, since Slicer_INSTALL_* variables already includes
 #  Slicer_BUNDLE_LOCATION (<appname>.app/Contents) they can *NOT*
-#  be used to reference paths from <APPLAUNCHER_DIR> which is itself
-#  set to /path/to/<appname>.app/Contents.
+#  be used to reference paths from <APPLAUNCHER_SETTINGS_DIR> which is itself
+#  set to /path/to/<appname>.app/Contents/bin.
 #
 
 #-----------------------------------------------------------------------------
 # LIBRARY_PATHS
 #-----------------------------------------------------------------------------
 set(SLICER_LIBRARY_PATHS_INSTALLED
-  <APPLAUNCHER_DIR>/${Slicer_BIN_DIR}
-  <APPLAUNCHER_DIR>/${Slicer_LIB_DIR}
-  <APPLAUNCHER_DIR>/${Slicer_CLIMODULES_LIB_DIR}
-  <APPLAUNCHER_DIR>/${Slicer_QTLOADABLEMODULES_LIB_DIR}
+  <APPLAUNCHER_SETTINGS_DIR>/../${Slicer_BIN_DIR}
+  <APPLAUNCHER_SETTINGS_DIR>/../${Slicer_LIB_DIR}
+  <APPLAUNCHER_SETTINGS_DIR>/../${Slicer_CLIMODULES_LIB_DIR}
+  <APPLAUNCHER_SETTINGS_DIR>/../${Slicer_QTLOADABLEMODULES_LIB_DIR}
   )
 
 # The following lines allow Slicer to load a CLI module extension that depends
@@ -232,9 +232,9 @@ endforeach()
 # PATHS
 #-----------------------------------------------------------------------------
 set(SLICER_PATHS_INSTALLED
-  <APPLAUNCHER_DIR>/${Slicer_BIN_DIR}
-  <APPLAUNCHER_DIR>/${Slicer_CLIMODULES_BIN_DIR}
-  <APPLAUNCHER_DIR>/${Slicer_QTLOADABLEMODULES_BIN_DIR}
+  <APPLAUNCHER_SETTINGS_DIR>/../${Slicer_BIN_DIR}
+  <APPLAUNCHER_SETTINGS_DIR>/../${Slicer_CLIMODULES_BIN_DIR}
+  <APPLAUNCHER_SETTINGS_DIR>/../${Slicer_QTLOADABLEMODULES_BIN_DIR}
   )
 
 # External projects - paths
@@ -248,12 +248,12 @@ endforeach()
 set(SLICER_ENVVARS_INSTALLED
   # SLICER_HOME might already be set on the machine, overwrite it because it
   # could have unwanted side effects
-  "SLICER_HOME=<APPLAUNCHER_DIR>"
-  "ITK_AUTOLOAD_PATH=<APPLAUNCHER_DIR>/${Slicer_ITKFACTORIES_DIR}"
+  "SLICER_HOME=<APPLAUNCHER_SETTINGS_DIR>/.."
+  "ITK_AUTOLOAD_PATH=<APPLAUNCHER_SETTINGS_DIR>/../${Slicer_ITKFACTORIES_DIR}"
   )
 if(Slicer_USE_PYTHONQT_WITH_OPENSSL)
   list(APPEND SLICER_ENVVARS_INSTALLED
-    "SSL_CERT_FILE=<APPLAUNCHER_DIR>/${Slicer_SHARE_DIR}/Slicer.crt"
+    "SSL_CERT_FILE=<APPLAUNCHER_SETTINGS_DIR>/../${Slicer_SHARE_DIR}/Slicer.crt"
     )
 endif()
 
@@ -268,7 +268,7 @@ endforeach()
 
 # QT_PLUGIN_PATH
 set(SLICER_QT_PLUGIN_PATH_INSTALLED
-  "<APPLAUNCHER_DIR>/${Slicer_QtPlugins_DIR}"
+  "<APPLAUNCHER_SETTINGS_DIR>/../${Slicer_QtPlugins_DIR}"
   )
 set(SLICER_ADDITIONAL_PATH_ENVVARS_INSTALLED
   "QT_PLUGIN_PATH"
@@ -277,12 +277,12 @@ set(SLICER_ADDITIONAL_PATH_ENVVARS_INSTALLED
 # PYTHONPATH
 if(Slicer_USE_PYTHONQT)
   set(SLICER_PYTHONPATH_INSTALLED
-    "<APPLAUNCHER_DIR>/${Slicer_LIB_DIR}"
-    "<APPLAUNCHER_DIR>/${Slicer_QTSCRIPTEDMODULES_LIB_DIR}"
-    "<APPLAUNCHER_DIR>/${Slicer_QTLOADABLEMODULES_LIB_DIR}"
-    "<APPLAUNCHER_DIR>/lib/vtkTeem"
-    "<APPLAUNCHER_DIR>/bin/Python"
-    "<APPLAUNCHER_DIR>/${Slicer_QTLOADABLEMODULES_PYTHON_LIB_DIR}"
+    "<APPLAUNCHER_SETTINGS_DIR>/../${Slicer_LIB_DIR}"
+    "<APPLAUNCHER_SETTINGS_DIR>/../${Slicer_QTSCRIPTEDMODULES_LIB_DIR}"
+    "<APPLAUNCHER_SETTINGS_DIR>/../${Slicer_QTLOADABLEMODULES_LIB_DIR}"
+    "<APPLAUNCHER_SETTINGS_DIR>/../lib/vtkTeem"
+    "<APPLAUNCHER_SETTINGS_DIR>/../bin/Python"
+    "<APPLAUNCHER_SETTINGS_DIR>/../${Slicer_QTLOADABLEMODULES_PYTHON_LIB_DIR}"
     )
 
   # External projects - pythonpath

SuperBuild/External_CTK.cmake
@@ -185,7 +185,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   if(UNIX AND NOT APPLE)
     # On windows, pythonQt libraries are installed along with the executable
-    set(${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED <APPLAUNCHER_DIR>/lib/PythonQt)
+    set(${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED <APPLAUNCHER_SETTINGS_DIR>/../lib/PythonQt)
     mark_as_superbuild(
       VARS ${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED
       LABELS "LIBRARY_PATHS_LAUNCHER_INSTALLED"

SuperBuild/External_NUMPY.cmake
@@ -151,8 +151,8 @@ ExternalProject_Execute(${proj} \"install\" \"${PYTHON_EXECUTABLE}\" setup.py in
   # Launcher setting specific to install tree
 
   set(${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED
-    <APPLAUNCHER_DIR>/lib/Python/${PYTHON_SITE_PACKAGES_SUBDIR}/numpy/core
-    <APPLAUNCHER_DIR>/lib/Python/${PYTHON_SITE_PACKAGES_SUBDIR}/numpy/lib
+    <APPLAUNCHER_SETTINGS_DIR>/../lib/Python/${PYTHON_SITE_PACKAGES_SUBDIR}/numpy/core
+    <APPLAUNCHER_SETTINGS_DIR>/../lib/Python/${PYTHON_SITE_PACKAGES_SUBDIR}/numpy/lib
     )
   mark_as_superbuild(
     VARS ${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED

SuperBuild/External_OpenIGTLink.cmake
@@ -85,7 +85,7 @@ if(NOT DEFINED OpenIGTLink_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   # Launcher setting specific to install tree
 
   if(UNIX AND NOT APPLE)
-    set(${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED <APPLAUNCHER_DIR>/lib/igtl)
+    set(${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED <APPLAUNCHER_SETTINGS_DIR>/../lib/igtl)
     mark_as_superbuild(
       VARS ${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED
       LABELS "LIBRARY_PATHS_LAUNCHER_INSTALLED"

SuperBuild/External_VTKv9.cmake
@@ -234,16 +234,16 @@ endif()
     if(Slicer_VTK_VERSION_MAJOR VERSION_GREATER 8)
       if(UNIX)
         set(${proj}_PYTHONPATH_LAUNCHER_INSTALLED
-          <APPLAUNCHER_DIR>/${Slicer_INSTALL_LIB_DIR}/python2.7/site-packages
+          <APPLAUNCHER_SETTINGS_DIR>/../${Slicer_INSTALL_LIB_DIR}/python2.7/site-packages
           )
       else()
         set(${proj}_PYTHONPATH_LAUNCHER_INSTALLED
-          <APPLAUNCHER_DIR>/${Slicer_INSTALL_BIN_DIR}/Lib/site-packages
+          <APPLAUNCHER_SETTINGS_DIR>/../${Slicer_INSTALL_BIN_DIR}/Lib/site-packages
           )
       endif()
     else()
       set(${proj}_PYTHONPATH_LAUNCHER_INSTALLED
-        <APPLAUNCHER_DIR>/${Slicer_INSTALL_LIB_DIR}/python2.7/site-packages
+        <APPLAUNCHER_SETTINGS_DIR>/../${Slicer_INSTALL_LIB_DIR}/python2.7/site-packages
         )
     endif()
     mark_as_superbuild(

commit 10ca74a025d0512cd6c1cc7552a129f302347528

BUG: Fix settings associated with SlicerPython launcher
This commit updates the version of CTKAppLauncher to 0.1.24 to
(1) fix support for "<APPLAUNCHER_SETTINGS_DIR>" special string
(2) ignore directory and non executable files when searching PATH for
    application to launch.

It addresses issue originally introduced in r27198 (ENH: Support import
of Slicer python packages using SlicerPython)

List of CTKAppLauncher/CTKAppLauncherLib changes:

$ git shortlog 37c0e80..b0dd46a --no-merges
Jean-Christophe Fillion-Robin (13):
      test: Rename TestAdditonalSettings script to TestUserAdditonalSettings
      ctkAppLauncherSettings::readSettings: Use better name for ivar
      ctkAppLauncherSettings::readSettings: Improve logic flow
      ctkAppLauncher: AdditionalSettings(FilePath|ExcludeGroups) -> Settings
      Testing/../AppwithLauncherLib/main: Use better name for ivar
      style: Refactor code introducing CTKAppLauncherTestingHelper static library
      AppLauncher: Fix reporting of "AdditionalSettingsFilePath" value
      style: Document ctkAppLauncherPrivate::buildEnvironment() function
      applauncher: Fix support for "<APPLAUNCHER_SETTINGS_DIR>" special string
      AppLauncherSettings: Add tests for AdditionalSettings(ExcludeGroups|FilePath) setter/getter
      style: Rename "settings.ini" to "settings-for-read-array-values-test.ini"
      ctkAppLauncher: Only consider executable files when searching PATH. Fixes #57
      CTKAppLauncher 0.1.24

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27247
git-svn-id: http://svn.slicer.org/Slicer4/trunk@27247 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTCore/qSlicerCoreApplication.cxx
@@ -237,7 +237,6 @@ void qSlicerCoreApplicationPrivate::init()
   ctkAppLauncherSettings appLauncherSettings;
   appLauncherSettings.setLauncherName(q->applicationName());
   appLauncherSettings.setLauncherDir(this->SlicerHome);
-  appLauncherSettings.setLauncherSettingsDir(QFileInfo(q->launcherSettingsFilePath()).absolutePath());
   if (!appLauncherSettings.readSettings(q->launcherSettingsFilePath()))
     {
     qCritical() << "Failed to read launcher settings" << q->launcherSettingsFilePath();

SuperBuild/External_CTKAPPLAUNCHER.cmake
@@ -24,15 +24,15 @@ if(Slicer_USE_CTKAPPLAUNCHER)
   if(NOT DEFINED CTKAppLauncher_DIR)
 
     SlicerMacroGetOperatingSystemArchitectureBitness(VAR_PREFIX CTKAPPLAUNCHER)
-    set(launcher_version "0.1.23")
+    set(launcher_version "0.1.24")
     # On windows, use i386 launcher unconditionally
     if("${CTKAPPLAUNCHER_OS}" STREQUAL "win")
       set(CTKAPPLAUNCHER_ARCHITECTURE "i386")
-      set(md5 "bd3b78c36365e7938c72aed4febdac1e")
+      set(md5 "fcc4ae69ceaebf2453f894e769585224")
     elseif("${CTKAPPLAUNCHER_OS}" STREQUAL "linux")
-      set(md5 "0c2d385eff14692defa674e1c6426234")
+      set(md5 "0d32c7d92eac2b87f6ec353ca67ea542")
     elseif("${CTKAPPLAUNCHER_OS}" STREQUAL "macosx")
-      set(md5 "ee6bc0e339c5378019d8b3de788b09f8")
+      set(md5 "985fce5454b0d2528d519dd17007bef4")
     endif()
 
     set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj})

SuperBuild/External_CTKAppLauncherLib.cmake
@@ -26,7 +26,7 @@ if(NOT DEFINED CTKAppLauncherLib_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "37c0e80b95581e510d1b942324cbf98cf3e550c3"
+    "b0dd46a876a6620f900845c357b326725ed11db8"
     QUIET
     )
 