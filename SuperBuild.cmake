7ef881808f6a2ba14fad163d5837ad367aec55eb
Andras Lasso <lasso@queensu.ca>
2018-07-09 17:17:54 +0000

    ENH: update MultiVolumeImporter hash
    Confidence for the loadables was revisited to prefer loadables that have temporal resolution as needed for quantitative analysis applications. Issue identified using a GE Signa HDxt DSC MRI dataset provided by a user.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27271
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@27271 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -213,7 +213,7 @@ list_conditional_append(Slicer_BUILD_MultiVolumeExplorer Slicer_REMOTE_DEPENDENC
 
 Slicer_Remote_Add(MultiVolumeImporter
   GIT_REPOSITORY ${EP_GIT_PROTOCOL}://github.com/fedorov/MultiVolumeImporter.git
-  GIT_TAG 146a5738e1a97cf2b2c133e4c6e5ff752f3aeddd
+  GIT_TAG 1f7b395566cdfa09aecf68a429afff431657d57c
   OPTION_NAME Slicer_BUILD_MultiVolumeImporter
   OPTION_DEPENDS "Slicer_BUILD_QTLOADABLEMODULES;Slicer_BUILD_MULTIVOLUME_SUPPORT;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
