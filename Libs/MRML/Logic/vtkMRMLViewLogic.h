c5cb0583ce9f89e412aff49fff3755afbe0a4b98
Andras Lasso <lasso@queensu.ca>
2018-07-28 14:12:03 +0000

    STYLE: edited Copyright mrml view logic
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27300
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@27300 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/MRML/Logic/vtkMRMLViewLogic.h
+++ Libs/MRML/Logic/vtkMRMLViewLogic.h
@@ -1,16 +1,21 @@
-/*=auto=========================================================================
+/*==============================================================================
 
-  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+  Copyright (c) Kapteyn Astronomical Institute
+  University of Groningen, Groningen, Netherlands. All Rights Reserved.
 
   See COPYRIGHT.txt
   or http://www.slicer.org/copyright/copyright.txt for details.
 
-  Program:   3D Slicer
-  Module:    $RCSfile: vtkMRMLSliceLogic.h,v $
-  Date:      $Date$
-  Version:   $Revision: 18866
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 
-=========================================================================auto=*/
+  This file was originally developed by Davide Punzo, Kapteyn Astronomical Institute,
+  and was supported through the European Research Council grant nr. 291531.
+
+==============================================================================*/
 
 #ifndef __vtkMRMLViewLogic_h
 #define __vtkMRMLViewLogic_h
