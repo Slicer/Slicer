51d01cdcbfb5c40bbf62182f754758cfc9185216
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-10-15 02:53:52 +0000

    STYLE: Update DataStore module
    List of changes:
    
    $ git shortlog 908efda..fb67917 --no-merges
    Jean-Christophe Fillion-Robin (1):
          STYLE: Coherently re-organize UI and Widgets sources
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27483
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@27483 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -301,7 +301,7 @@ list_conditional_append(Slicer_BUILD_OtsuThresholdImageFilter Slicer_REMOTE_DEPE
 
 Slicer_Remote_Add(DataStore
   GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/Slicer/Slicer-DataStore"
-  GIT_TAG "908efdacf4450701871a238542bff56756d041ca"
+  GIT_TAG "fb679171ca9722e8f716902a582e45495331f801"
   OPTION_NAME Slicer_BUILD_DataStore
   LABELS REMOTE_MODULE
   )
