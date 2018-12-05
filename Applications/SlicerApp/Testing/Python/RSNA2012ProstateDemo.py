239da46321e335e1ca194b459a37841521b9b5b6
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-12-05 22:39:06 +0000

    STYLE: Re-factor RSNA2012ProstateDemo to use ScriptedLoadableModule base classes
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27600
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@27600 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/SlicerApp/Testing/Python/RSNA2012ProstateDemo.py
+++ Applications/SlicerApp/Testing/Python/RSNA2012ProstateDemo.py
@@ -1,13 +1,18 @@
 import os
 import unittest
 import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
 
 #
 # RSNA2012ProstateDemo
 #
 
-class RSNA2012ProstateDemo:
+class RSNA2012ProstateDemo(ScriptedLoadableModule):
+  """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
   def __init__(self, parent):
+    ScriptedLoadableModule.__init__(self, parent)
     parent.title = "RSNA2012ProstateDemo" # TODO make this more human readable by adding spaces
     parent.categories = ["Testing.TestCases"]
     parent.dependencies = []
@@ -18,122 +23,32 @@ class RSNA2012ProstateDemo:
     parent.acknowledgementText = """
     This file was originally developed by Steve Pieper, Isomics, Inc.  and was partially funded by NIH grant 3P41RR013218-12S1.
 """ # replace with organization, grant and thanks.
-    self.parent = parent
-
-    # Add this test to the SelfTest module's list for discovery when the module
-    # is created.  Since this module may be discovered before SelfTests itself,
-    # create the list if it doesn't already exist.
-    try:
-      slicer.selfTests
-    except AttributeError:
-      slicer.selfTests = {}
-    slicer.selfTests['RSNA2012ProstateDemo'] = self.runTest
-
-  def runTest(self):
-    tester = RSNA2012ProstateDemoTest()
-    tester.runTest()
 
 #
 # qRSNA2012ProstateDemoWidget
 #
 
-class RSNA2012ProstateDemoWidget:
-  def __init__(self, parent = None):
-    if not parent:
-      self.parent = slicer.qMRMLWidget()
-      self.parent.setLayout(qt.QVBoxLayout())
-      self.parent.setMRMLScene(slicer.mrmlScene)
-    else:
-      self.parent = parent
-    self.layout = self.parent.layout()
-    if not parent:
-      self.setup()
-      self.parent.show()
+class RSNA2012ProstateDemoWidget(ScriptedLoadableModuleWidget):
+  """Uses ScriptedLoadableModuleWidget base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
 
   def setup(self):
+    ScriptedLoadableModuleWidget.setup(self)
     # Instantiate and connect widgets ...
-
-    # reload button
-    # (use this during development, but remove it when delivering
-    #  your module to users)
-    self.reloadButton = qt.QPushButton("Reload")
-    self.reloadButton.toolTip = "Reload this module."
-    self.reloadButton.name = "RSNA2012ProstateDemo Reload"
-    self.layout.addWidget(self.reloadButton)
-    self.reloadButton.connect('clicked()', self.onReload)
-
-    # reload and test button
-    # (use this during development, but remove it when delivering
-    #  your module to users)
-    self.reloadAndTestButton = qt.QPushButton("Reload and Test")
-    self.reloadAndTestButton.toolTip = "Reload this module and then run the self tests."
-    self.layout.addWidget(self.reloadAndTestButton)
-    self.reloadAndTestButton.connect('clicked()', self.onReloadAndTest)
 
     # Add vertical spacer
     self.layout.addStretch(1)
 
-  def onReload(self,moduleName="RSNA2012ProstateDemo"):
-    """Generic reload method for any scripted module.
-    ModuleWizard will substitute correct default moduleName.
-    """
-    globals()[moduleName] = slicer.util.reloadScriptedModule(moduleName)
-
-  def onReloadAndTest(self,moduleName="RSNA2012ProstateDemo"):
-    self.onReload()
-    evalString = 'globals()["%s"].%sTest()' % (moduleName, moduleName)
-    tester = eval(evalString)
-    tester.runTest()
-
-#
-# RSNA2012ProstateDemoLogic
-#
-
-class RSNA2012ProstateDemoLogic:
-  """This class should implement all the actual
-  computation done by your module.  The interface
-  should be such that other python code can import
-  this class and make use of the functionality without
-  requiring an instance of the Widget
-  """
-  def __init__(self):
+  def cleanup(self):
     pass
 
-  def hasImageData(self,volumeNode):
-    """This is a dummy logic method that
-    returns true if the passed in volume
-    node has valid image data
-    """
-    if not volumeNode:
-      print('no volume node')
-      return False
-    if volumeNode.GetImageData() is None:
-      print('no image data')
-      return False
-    return True
-
-
-class RSNA2012ProstateDemoTest(unittest.TestCase):
+class RSNA2012ProstateDemoTest(ScriptedLoadableModuleTest):
   """
   This is the test case for your scripted module.
+  Uses ScriptedLoadableModuleTest base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
-
-  def delayDisplay(self,message,msec=100):
-    """This utility method displays a small dialog and waits.
-    This does two things: 1) it lets the event loop catch up
-    to the state of the test so that rendering and widget updates
-    have all taken place before the test continues and 2) it
-    shows the user/developer/tester the state of the test
-    so that we'll know when it breaks.
-    """
-    print(message)
-    self.info = qt.QDialog()
-    self.infoLayout = qt.QVBoxLayout()
-    self.info.setLayout(self.infoLayout)
-    self.label = qt.QLabel(message,self.info)
-    self.infoLayout.addWidget(self.label)
-    qt.QTimer.singleShot(msec, self.info.close)
-    self.info.exec_()
 
   def setUp(self):
     slicer.mrmlScene.Clear(0)
@@ -141,7 +56,6 @@ class RSNA2012ProstateDemoTest(unittest.TestCase):
   def runTest(self):
     self.setUp()
     self.test_RSNA2012ProstateDemo()
-
 
   def test_RSNA2012ProstateDemo(self):
     """
