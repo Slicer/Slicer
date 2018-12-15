Slicer/Slicer

commit 1fcb85238012850cdb55122806611283856dcd20

STYLE: Update self-test to use SampleData downloadFromURL API
svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27639
git-svn-id: http://svn.slicer.org/Slicer4/trunk@27639 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DataProbe/DataProbe.py
@@ -583,19 +583,10 @@ class DataProbeTest(ScriptedLoadableModuleTest):
     #
     # first, get some data
     #
-    import urllib
-    downloads = (
-        ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
-        )
-
-    for url,name,loader in downloads:
-      filePath = slicer.app.temporaryPath + '/' + name
-      if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
-        logging.info('Requesting download %s from %s...\n' % (name, url))
-        urllib.urlretrieve(url, filePath)
-      if loader:
-        logging.info('Loading %s...' % (name,))
-        loader(filePath)
+    import SampleData
+    SampleData.downloadFromURL(
+      fileNames='FA.nrrd',
+      uris='http://slicer.kitware.com/midas3/download?items=5767')
     self.delayDisplay('Finished with download and loading')
 
     self.widget = DataProbeInfoWidget()

Modules/Scripted/SampleData/SampleData.py
@@ -5,6 +5,39 @@ from slicer.ScriptedLoadableModule import *
 import logging
 
 #
+# SampleData methods
+#
+
+def downloadFromURL(uris, fileNames, nodeNames=None,
+  customDownloader=None, loadFileTypes=None, loadFileProperties={}):
+  """Download and optionally load data into the application.
+
+  :param uris: Download URL(s).
+  :param fileNames: File name(s) that will be loaded.
+  :param nodeNames: Node name(s) in the scene.
+  :param customDownloader: Custom function for downloading.
+  :param loadFileTypes: file format name(s) ('VolumeFile' by default).
+  :param loadFileProperties: custom properties passed to the IO plugin.
+
+  If the given ``fileNames`` are not found in the application cache directory, they
+  are downloaded using the associated URIs.
+  See ``slicer.mrmlScene.GetCacheManager().GetRemoteCacheDirectory()``
+
+  If not explicitly provided or if set to ``None``, the ``loadFileTypes`` are
+  guessed based on the corresponding filename extensions.
+
+  If not explicitly provided or if set to ``None``, the ``nodeNames`` are
+  set based on the corresponding filename basename.
+
+  The ``loadFileProperties`` are common for all files. If different properties
+  need to be associated with files of different types, downloadFromURL must
+  be called for each.
+  """
+  return SampleDataLogic().downloadFromURL(
+    uris, fileNames, nodeNames, customDownloader, loadFileTypes, loadFileProperties)
+
+
+#
 # SampleData
 #
 
@@ -74,7 +107,6 @@ class SampleDataSource:
 
     import SampleData
     dataSource = SampleData.SampleDataSource(
-      nodeNames='fixed',
       fileNames='fixed.nrrd',
       uris='http://slicer.kitware.com/midas3/download/item/157188/small-mr-eye-fixed.nrrd')
     loadedNode = SampleData.SampleDataLogic().downloadFromSource(dataSource)[0]
@@ -82,7 +114,7 @@ class SampleDataSource:
 
   def __init__(self, sampleName=None, uris=None, fileNames=None, nodeNames=None,
     customDownloader=None, thumbnailFileName=None,
-    loadFileType='VolumeFile', loadFileProperties={}):
+    loadFileType=None, loadFileProperties={}):
     """
     :param sampleName: Displayed name of data set in SampleData module GUI.
     :param thumbnailFileName: Displayed thumbnail of data set in SampleData module GUI,
@@ -95,23 +127,41 @@ class SampleDataSource:
     """
     self.sampleName = sampleName
     if (isinstance(uris, list) or isinstance(uris, tuple)):
-      if isinstance(loadFileType, basestring):
+      if isinstance(loadFileType, basestring) or loadFileType is None:
         loadFileType = [loadFileType] * len(uris)
-      if nodeNames is None:
-        nodeNames = [os.path.splitext(fileName) for fileName in fileNames]
     elif isinstance(uris, basestring):
       uris = [uris,]
       fileNames = [fileNames,]
       nodeNames = [nodeNames,]
       loadFileType = [loadFileType,]
+
+    updatedFileType = []
+    updatedNodeNames = []
+    for fileName, nodeName, fileType in zip(fileNames, nodeNames, loadFileType):
+      # If not explicitly specified, attempt to guess fileType
+      if fileType is None:
+        # TODO: Use method from Slicer IO logic ?
+        ext = os.path.splitext(fileName.lower())[1]
+        if ext in [".mrml", ".mrb"]:
+          fileType = "SceneFile"
+        elif ext in [".zip"]:
+          fileType = "ZipFile"
+        else:
+          fileType = "VolumeFile"
+      updatedFileType.append(fileType)
+      # If not explicitly specified, attempt to guess nodeName
+      if nodeName is None and fileType not in ["SceneFile", "ZipFile"]:
+        nodeName = fileName.split(".")[0]
+      updatedNodeNames.append(nodeName)
+
     self.uris = uris
     self.fileNames = fileNames
-    self.nodeNames = nodeNames
+    self.nodeNames = updatedNodeNames
     self.customDownloader = customDownloader
     self.thumbnailFileName = thumbnailFileName
-    self.loadFileType = loadFileType
+    self.loadFileType = updatedFileType
     self.loadFileProperties = loadFileProperties
-    if len(uris) != len(fileNames) or len(uris) != len(nodeNames) or len(uris) != len(loadFileType):
+    if len(uris) != len(fileNames) or len(uris) != len(updatedNodeNames) or len(uris) != len(updatedFileType):
       raise Exception("All fields of sample data source must have the same length")
 
 
@@ -349,11 +399,6 @@ class SampleDataLogic:
       filePaths.append(self.downloadFileIntoCache(uri, fileName))
     return filePaths
 
-  def downloadFromSources(self,sources,attemptCount=0):
-    """Given one or multiple instances of SampleDataSource, downloads the associated data and
-    load them into Slicer if it applies."""
-    return [self.downloadFromSource(source, attemptCount=attemptCount) for source in sources]
-
   def downloadFromSource(self,source,attemptCount=0):
     """Given an instance of SampleDataSource, downloads the associated data and
     load them into Slicer if it applies.
@@ -425,6 +470,36 @@ class SampleDataLogic:
         if sampleName == source.sampleName:
           return source
     return None
+
+  def downloadFromURL(self, uris, fileNames, nodeNames=None,
+    customDownloader=None, loadFileTypes=None, loadFileProperties={}):
+    """Download and optionally load data into the application.
+
+    :param uris: Download URL(s).
+    :param fileNames: File name(s) that will be loaded.
+    :param nodeNames: Node name(s) in the scene.
+    :param customDownloader: Custom function for downloading.
+    :param loadFileTypes: file format name(s) ('VolumeFile' by default).
+    :param loadFileProperties: custom properties passed to the IO plugin.
+
+    If the given ``fileNames`` are not found in the application cache directory, they
+    are downloaded using the associated URIs.
+    See ``slicer.mrmlScene.GetCacheManager().GetRemoteCacheDirectory()``
+
+    If not explicitly provided or if set to ``None``, the ``loadFileTypes`` are
+    guessed based on the corresponding filename extensions.
+
+    If not explicitly provided or if set to ``None``, the ``nodeNames`` are
+    set based on the corresponding filename basename.
+
+    The ``loadFileProperties`` are common for all files. If different properties
+    need to be associated with files of different types, downloadFromURL must
+    be called for each.
+    """
+    return self.downloadFromSource(SampleDataSource(
+      uris=uris, fileNames=fileNames, nodeNames=nodeNames,
+      loadFileType=loadFileTypes, loadFileProperties=loadFileProperties
+    ))
 
   def downloadSample(self,sampleName):
     """For a given sample name this will search the available sources

Modules/Scripted/SurfaceToolbox/SurfaceToolbox.py
@@ -542,20 +542,11 @@ class SurfaceToolboxTest(ScriptedLoadableModuleTest):
     #
     # first, get some data
     #
-    import urllib
-    downloads = (
-        ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
-        )
-
-    for url,name,loader in downloads:
-      filePath = slicer.app.temporaryPath + '/' + name
-      if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
-        print('Requesting download %s from %s...\n' % (name, url))
-        urllib.urlretrieve(url, filePath)
-      if loader:
-        print('Loading %s...\n' % (name,))
-        loader(filePath)
-    self.delayDisplay('Finished with download and loading\n')
+    import SampleData
+    SampleData.downloadFromURL(
+      fileNames='FA.nrrd',
+      uris='http://slicer.kitware.com/midas3/download?items=5767')
+    self.delayDisplay('Finished with download and loading')
 
     volumeNode = slicer.util.getNode(pattern="FA")
     logic = SurfaceToolboxLogic()

Utilities/Templates/Modules/Scripted/TemplateKey.py
@@ -232,19 +232,10 @@ class TemplateKeyTest(ScriptedLoadableModuleTest):
     #
     # first, get some data
     #
-    import urllib
-    downloads = (
-        ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
-        )
-
-    for url,name,loader in downloads:
-      filePath = slicer.app.temporaryPath + '/' + name
-      if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
-        logging.info('Requesting download %s from %s...\n' % (name, url))
-        urllib.urlretrieve(url, filePath)
-      if loader:
-        logging.info('Loading %s...' % (name,))
-        loader(filePath)
+    import SampleData
+    SampleData.downloadFromURL(
+      fileNames='FA.nrrd',
+      uris='http://slicer.kitware.com/midas3/download?items=5767')
     self.delayDisplay('Finished with download and loading')
 
     volumeNode = slicer.util.getNode(pattern="FA")

Applications/SlicerApp/Testing/Python/SliceLinkLogic.py
@@ -122,19 +122,10 @@ class SliceLinkLogicTest(ScriptedLoadableModuleTest):
     #
     # first, get some data
     #
-    import urllib
-    downloads = (
-        ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
-        )
-
-    for url,name,loader in downloads:
-      filePath = slicer.app.temporaryPath + '/' + name
-      if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
-        print('Requesting download %s from %s...\n' % (name, url))
-        urllib.urlretrieve(url, filePath)
-      if loader:
-        print('Loading %s...\n' % (name,))
-        loader(filePath)
+    import SampleData
+    SampleData.downloadFromURL(
+      fileNames='FA.nrrd',
+      uris='http://slicer.kitware.com/midas3/download?items=5767')
     self.delayDisplay('Finished with download and loading')
     print('')
 

Extensions/Testing/ScriptedLoadableExtensionTemplate/ScriptedLoadableModuleTemplate/ScriptedLoadableModuleTemplate.py
@@ -232,19 +232,10 @@ class ScriptedLoadableModuleTemplateTest(ScriptedLoadableModuleTest):
     #
     # first, get some data
     #
-    import urllib
-    downloads = (
-        ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
-        )
-
-    for url,name,loader in downloads:
-      filePath = slicer.app.temporaryPath + '/' + name
-      if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
-        logging.info('Requesting download %s from %s...\n' % (name, url))
-        urllib.urlretrieve(url, filePath)
-      if loader:
-        logging.info('Loading %s...' % (name,))
-        loader(filePath)
+    import SampleData
+    SampleData.downloadFromURL(
+      fileNames='FA.nrrd',
+      uris='http://slicer.kitware.com/midas3/download?items=5767')
     self.delayDisplay('Finished with download and loading')
 
     volumeNode = slicer.util.getNode(pattern="FA")

commit 74e11a11f4c0975cb1f018f834d44ba4f3dcd7d9

BUG: Update SampleData.downloadFromSource to fix loading of multi-files node
This commit fixes regression introduced in r27639 (STYLE: Update self-test
to use SampleData downloadFromURL API)

It restores the use of "nodeNames" when specifying volume data sources
expected to be loaded in Slicer. Systematically setting loadFileType to
VolumeFile if a fileName was neither a ZipFile or a SceneFile was
incorrect. Indeed, it broke the support for loading volume composed of
multiple files (e.g somefile.raw.gz and somefile.nhdr)

This commit also adds SampleDataTest with:
  test_downloadFromSource_downloadFiles
  test_downloadFromSource_downloadZipFile
  test_downloadFromSource_loadSceneFile
  test_downloadFromSource_loadNode
  test_downloadFromSource_loadNodeFromMultipleFiles
  test_downloadFromSource_loadNodes

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27642
git-svn-id: http://svn.slicer.org/Slicer4/trunk@27642 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DataProbe/DataProbe.py
@@ -585,6 +585,7 @@ class DataProbeTest(ScriptedLoadableModuleTest):
     #
     import SampleData
     SampleData.downloadFromURL(
+      nodeNames='FA',
       fileNames='FA.nrrd',
       uris='http://slicer.kitware.com/midas3/download?items=5767')
     self.delayDisplay('Finished with download and loading')

Modules/Scripted/SampleData/CMakeLists.txt
@@ -31,3 +31,12 @@ slicerMacroBuildScriptedModule(
   RESOURCES ${MODULE_PYTHON_RESOURCES}
   WITH_GENERIC_TESTS
   )
+
+#-----------------------------------------------------------------------------
+if(BUILD_TESTING)
+
+  # Register the unittest subclass in the main script as a ctest.
+  # Note that the test will also be available at runtime.
+  slicer_add_python_unittest(SCRIPT ${MODULE_NAME}.py)
+
+endif()

Modules/Scripted/SampleData/SampleData.py
@@ -8,7 +8,7 @@ import logging
 # SampleData methods
 #
 
-def downloadFromURL(uris, fileNames, nodeNames=None,
+def downloadFromURL(uris=None, fileNames=None, nodeNames=None,
   customDownloader=None, loadFileTypes=None, loadFileProperties={}):
   """Download and optionally load data into the application.
 
@@ -25,9 +25,6 @@ def downloadFromURL(uris, fileNames, nodeNames=None,
 
   If not explicitly provided or if set to ``None``, the ``loadFileTypes`` are
   guessed based on the corresponding filename extensions.
-
-  If not explicitly provided or if set to ``None``, the ``nodeNames`` are
-  set based on the corresponding filename basename.
 
   The ``loadFileProperties`` are common for all files. If different properties
   need to be associated with files of different types, downloadFromURL must
@@ -100,13 +97,14 @@ use it for commercial purposes.</p>
 #
 # SampleDataSource
 #
-class SampleDataSource:
+class SampleDataSource(object):
   """Describe a set of sample data associated with one or multiple URIs and filenames.
 
   Example::
 
     import SampleData
     dataSource = SampleData.SampleDataSource(
+      nodeNames='fixed',
       fileNames='fixed.nrrd',
       uris='http://slicer.kitware.com/midas3/download/item/157188/small-mr-eye-fixed.nrrd')
     loadedNode = SampleData.SampleDataLogic().downloadFromSource(dataSource)[0]
@@ -122,13 +120,15 @@ class SampleDataSource:
     :param fileNames: File name(s) that will be loaded.
     :param nodeNames: Node name(s) in the scene.
     :param customDownloader: Custom function for downloading.
-    :param loadFileType: file format name(s) ('VolumeFile' by default).
+    :param loadFileType: file format name(s) ('VolumeFile' by default if node name is specified).
     :param loadFileProperties: custom properties passed to the IO plugin.
     """
     self.sampleName = sampleName
     if (isinstance(uris, list) or isinstance(uris, tuple)):
       if isinstance(loadFileType, basestring) or loadFileType is None:
         loadFileType = [loadFileType] * len(uris)
+      if nodeNames is None:
+        nodeNames = [None] * len(uris)
     elif isinstance(uris, basestring):
       uris = [uris,]
       fileNames = [fileNames,]
@@ -136,32 +136,28 @@ class SampleDataSource:
       loadFileType = [loadFileType,]
 
     updatedFileType = []
-    updatedNodeNames = []
     for fileName, nodeName, fileType in zip(fileNames, nodeNames, loadFileType):
       # If not explicitly specified, attempt to guess fileType
       if fileType is None:
-        # TODO: Use method from Slicer IO logic ?
-        ext = os.path.splitext(fileName.lower())[1]
-        if ext in [".mrml", ".mrb"]:
-          fileType = "SceneFile"
-        elif ext in [".zip"]:
-          fileType = "ZipFile"
-        else:
+        if nodeName is not None:
+          # TODO: Use method from Slicer IO logic ?
           fileType = "VolumeFile"
+        else:
+          ext = os.path.splitext(fileName.lower())[1]
+          if ext in [".mrml", ".mrb"]:
+            fileType = "SceneFile"
+          elif ext in [".zip"]:
+            fileType = "ZipFile"
       updatedFileType.append(fileType)
-      # If not explicitly specified, attempt to guess nodeName
-      if nodeName is None and fileType not in ["SceneFile", "ZipFile"]:
-        nodeName = fileName.split(".")[0]
-      updatedNodeNames.append(nodeName)
 
     self.uris = uris
     self.fileNames = fileNames
-    self.nodeNames = updatedNodeNames
+    self.nodeNames = nodeNames
     self.customDownloader = customDownloader
     self.thumbnailFileName = thumbnailFileName
     self.loadFileType = updatedFileType
     self.loadFileProperties = loadFileProperties
-    if len(uris) != len(fileNames) or len(uris) != len(updatedNodeNames) or len(uris) != len(updatedFileType):
+    if len(uris) != len(fileNames) or len(uris) != len(nodeNames) or len(uris) != len(updatedFileType):
       raise Exception("All fields of sample data source must have the same length")
 
 
@@ -278,7 +274,7 @@ class SampleDataWidget(ScriptedLoadableModuleWidget):
 # SampleData logic
 #
 
-class SampleDataLogic:
+class SampleDataLogic(object):
   """Manage the slicer.modules.sampleDataSources dictionary.
   The dictionary keys are categories of sample data sources.
   The BuiltIn category is managed here.  Modules or extensions can
@@ -405,19 +401,24 @@ class SampleDataLogic:
 
     The function always returns a list.
 
-    Based on the file type associated with the source, different values may
-    be returned.
+    Based on the filetype(s) and nodename(s) associated with the source, different
+    values may be returned.
 
-      - for ``SceneFile``, returns path of downloaded file
-      - for ``VolumeFile`` or any other type supported by Slicer, returns associated node
-      - for ``ZipFile``, returns directory of extracted archive
+      - if nodename is specified, returns loaded nodes
+      - if filetype is ``SceneFile``, returns path of downloaded file
+      - if filetype is ``ZipFile``, returns directory of extracted archive
+
+    If no nodenames and no filetypes are specified, returns the list of all
+    downloaded filepaths.
     """
     nodes = []
+    filePaths = []
 
     for uri,fileName,nodeName,loadFileType in zip(source.uris,source.fileNames,source.nodeNames,source.loadFileType):
 
       current_source = SampleDataSource(uris=uri, fileNames=fileName, nodeNames=nodeName, loadFileType=loadFileType, loadFileProperties=source.loadFileProperties)
       filePath = self.downloadFileIntoCache(uri, fileName)
+      filePaths.append(filePath)
 
       if loadFileType == 'ZipFile':
         outputDir = slicer.mrmlScene.GetCacheManager().GetRemoteCacheDirectory() + "/" + os.path.splitext(os.path.basename(filePath))[0]
@@ -460,7 +461,10 @@ class SampleDataLogic:
           loadedNode = self.downloadFromSource(current_source,attemptCount)[0]
         nodes.append(loadedNode)
 
-    return nodes
+    if nodes:
+      return nodes
+    else:
+      return filePaths
 
   def sourceForSampleName(self,sampleName):
     """For a given sample name this will search the available sources.
@@ -471,7 +475,7 @@ class SampleDataLogic:
           return source
     return None
 
-  def downloadFromURL(self, uris, fileNames, nodeNames=None,
+  def downloadFromURL(self, uris=None, fileNames=None, nodeNames=None,
     customDownloader=None, loadFileTypes=None, loadFileProperties={}):
     """Download and optionally load data into the application.
 
@@ -488,9 +492,6 @@ class SampleDataLogic:
 
     If not explicitly provided or if set to ``None``, the ``loadFileTypes`` are
     guessed based on the corresponding filename extensions.
-
-    If not explicitly provided or if set to ``None``, the ``nodeNames`` are
-    set based on the corresponding filename basename.
 
     The ``loadFileProperties`` are common for all files. If different properties
     need to be associated with files of different types, downloadFromURL must
@@ -623,3 +624,103 @@ class SampleDataLogic:
       loadedNode.SetAndObserveStorageNodeID(None)
 
     return loadedNodes.GetItemAsObject(0)
+
+
+class SampleDataTest(ScriptedLoadableModuleTest):
+  """
+  This is the test case for your scripted module.
+  Uses ScriptedLoadableModuleTest base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setUp(self):
+    slicer.mrmlScene.Clear(0)
+
+  def runTest(self):
+    self.setUp()
+    self.test_downloadFromSource_downloadFiles()
+
+    self.setUp()
+    self.test_downloadFromSource_downloadZipFile()
+
+    self.setUp()
+    self.test_downloadFromSource_loadSceneFile()
+
+    self.setUp()
+    self.test_downloadFromSource_loadNode()
+
+    self.setUp()
+    self.test_downloadFromSource_loadNodeFromMultipleFiles()
+
+    self.setUp()
+    self.test_downloadFromSource_loadNodes()
+
+
+  def test_downloadFromSource_downloadFiles(self):
+    """Specifying URIs and fileNames without nodeNames is expected to download the files
+    without loading into Slicer.
+    """
+    logic = SampleDataLogic()
+
+    sceneMTime = slicer.mrmlScene.GetMTime()
+    filePaths = logic.downloadFromSource(SampleDataSource(
+      uris='http://slicer.kitware.com/midas3/download/item/292308/MR-head.nrrd', fileNames='MR-head.nrrd'))
+    self.assertEqual(len(filePaths), 1)
+    self.assertTrue(os.path.exists(filePaths[0]))
+    self.assertTrue(os.path.isfile(filePaths[0]))
+    self.assertEqual(sceneMTime, slicer.mrmlScene.GetMTime())
+
+    sceneMTime = slicer.mrmlScene.GetMTime()
+    filePaths = logic.downloadFromSource(SampleDataSource(
+      uris=['http://slicer.kitware.com/midas3/download/item/292308/MR-head.nrrd', 'http://slicer.kitware.com/midas3/download/item/292307/CT-chest.nrrd'],
+      fileNames=['MR-head.nrrd', 'CT-chest.nrrd']))
+    self.assertEqual(len(filePaths), 2)
+    self.assertTrue(os.path.exists(filePaths[0]))
+    self.assertTrue(os.path.isfile(filePaths[0]))
+    self.assertTrue(os.path.exists(filePaths[1]))
+    self.assertTrue(os.path.isfile(filePaths[1]))
+    self.assertEqual(sceneMTime, slicer.mrmlScene.GetMTime())
+
+  def test_downloadFromSource_downloadZipFile(self):
+    logic = SampleDataLogic()
+    sceneMTime = slicer.mrmlScene.GetMTime()
+    filePaths = logic.downloadFromSource(SampleDataSource(
+      uris='http://slicer.kitware.com/midas3/download/folder/3763/TinyPatient_Seg.zip', fileNames='TinyPatient_Seg.zip'))
+    self.assertEqual(len(filePaths), 1)
+    self.assertTrue(os.path.exists(filePaths[0]))
+    self.assertTrue(os.path.isdir(filePaths[0]))
+    self.assertEqual(sceneMTime, slicer.mrmlScene.GetMTime())
+
+  def test_downloadFromSource_loadSceneFile(self):
+    logic = SampleDataLogic()
+    filePaths = logic.downloadFromSource(SampleDataSource(
+      uris='http://slicer.kitware.com/midas3/download?items=8466', fileNames='slicer4minute.mrb'))
+    self.assertEqual(len(filePaths), 1)
+    self.assertTrue(os.path.exists(filePaths[0]))
+    self.assertTrue(os.path.isfile(filePaths[0]))
+
+  def test_downloadFromSource_loadNode(self):
+    logic = SampleDataLogic()
+    nodes = logic.downloadFromSource(SampleDataSource(
+      uris='http://slicer.kitware.com/midas3/download/item/292308/MR-head.nrrd', fileNames='MR-head.nrrd', nodeNames='MRHead'))
+    self.assertEqual(len(nodes), 1)
+    self.assertEqual(nodes[0], slicer.mrmlScene.GetFirstNodeByName("MRHead"))
+
+  def test_downloadFromSource_loadNodeFromMultipleFiles(self):
+    logic = SampleDataLogic()
+    nodes = logic.downloadFromSource(SampleDataSource(
+      uris=['http://slicer.kitware.com/midas3/download/?items=2011,1', 'http://slicer.kitware.com/midas3/download/?items=2010,1'],
+      fileNames=['DTIVolume.raw.gz', 'DTIVolume.nhdr'],
+      nodeNames=[None, 'DTIVolume']))
+    self.assertEqual(len(nodes), 1)
+    self.assertEqual(nodes[0], slicer.mrmlScene.GetFirstNodeByName("DTIVolume"))
+
+  def test_downloadFromSource_loadNodes(self):
+    logic = SampleDataLogic()
+    nodes = logic.downloadFromSource(SampleDataSource(
+      uris=['http://slicer.kitware.com/midas3/download/item/292308/MR-head.nrrd', 'http://slicer.kitware.com/midas3/download/item/292307/CT-chest.nrrd'],
+      fileNames=['MR-head.nrrd', 'CT-chest.nrrd'],
+      nodeNames=['MRHead', 'CTChest']))
+    self.assertEqual(len(nodes), 2)
+    self.assertEqual(nodes[0], slicer.mrmlScene.GetFirstNodeByName("MRHead"))
+    self.assertEqual(nodes[1], slicer.mrmlScene.GetFirstNodeByName("CTChest"))

Modules/Scripted/SurfaceToolbox/SurfaceToolbox.py
@@ -544,6 +544,7 @@ class SurfaceToolboxTest(ScriptedLoadableModuleTest):
     #
     import SampleData
     SampleData.downloadFromURL(
+      nodeNames='FA',
       fileNames='FA.nrrd',
       uris='http://slicer.kitware.com/midas3/download?items=5767')
     self.delayDisplay('Finished with download and loading')

Utilities/Templates/Modules/Scripted/TemplateKey.py
@@ -234,6 +234,7 @@ class TemplateKeyTest(ScriptedLoadableModuleTest):
     #
     import SampleData
     SampleData.downloadFromURL(
+      nodeNames='FA',
       fileNames='FA.nrrd',
       uris='http://slicer.kitware.com/midas3/download?items=5767')
     self.delayDisplay('Finished with download and loading')

Applications/SlicerApp/Testing/Python/SliceLinkLogic.py
@@ -124,6 +124,7 @@ class SliceLinkLogicTest(ScriptedLoadableModuleTest):
     #
     import SampleData
     SampleData.downloadFromURL(
+      nodeNames='FA',
       fileNames='FA.nrrd',
       uris='http://slicer.kitware.com/midas3/download?items=5767')
     self.delayDisplay('Finished with download and loading')

Extensions/Testing/ScriptedLoadableExtensionTemplate/ScriptedLoadableModuleTemplate/ScriptedLoadableModuleTemplate.py
@@ -234,6 +234,7 @@ class ScriptedLoadableModuleTemplateTest(ScriptedLoadableModuleTest):
     #
     import SampleData
     SampleData.downloadFromURL(
+      nodeNames='FA',
       fileNames='FA.nrrd',
       uris='http://slicer.kitware.com/midas3/download?items=5767')
     self.delayDisplay('Finished with download and loading')

Modules/Scripted/SampleData/Testing/CMakeLists.txt
@@ -1,1 +1,0 @@
-add_subdirectory(Python)

Modules/Scripted/SampleData/Testing/Python/CMakeLists.txt
@@ -1,2 +1,0 @@
-
-slicer_add_python_unittest(SCRIPT qSlicerSampleDataModuleDownloadVolumeTest.py)

Modules/Scripted/SampleData/Testing/Python/qSlicerSampleDataModuleDownloadVolumeTest.py
@@ -1,20 +1,0 @@
-import unittest
-import slicer
-
-class qSlicerSampleDataModuleTest(unittest.TestCase):
-  def setUp(self):
-    mainWindow = slicer.util.mainWindow()
-    mainWindow.moduleSelector().selectModule('SampleData')
-    module = slicer.modules.sampledata
-    self.moduleWidget = module.widgetRepresentation()
-
-  def test_download_volume(self):
-    self.moduleWidget.findChild('QPushButton').click()
-    scene = slicer.mrmlScene
-    self.assertEqual(scene.GetNumberOfNodesByClass('vtkMRMLScalarVolumeNode'), 1)
-
-  def test_download_volume2(self):
-    self.moduleWidget.findChild('QPushButton').click()
-    scene = slicer.mrmlScene
-    self.assertEqual(scene.GetNumberOfNodesByClass('vtkMRMLScalarVolumeNode'), 2)
-