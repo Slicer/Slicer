e59a57c7acf0c8de6e6f649d0d624f33e3427143
Andras Lasso <lasso@queensu.ca>
2018-12-18 15:54:16 +0000

    BUG: Revert changes in MRML annotation node, use array->GetSize() again
    Using GetSize() may not be correct but it has not been causing issues and changing it to use GetNumberOfValues() makes many test to fail. Instead of investigating now, just revert the changes, because soon markups will replace annotations.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27664
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@27664 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Loadable/Annotations/MRML/vtkMRMLAnnotationControlPointsNode.cxx
+++ Modules/Loadable/Annotations/MRML/vtkMRMLAnnotationControlPointsNode.cxx
@@ -410,10 +410,10 @@ void vtkMRMLAnnotationControlPointsNode::DeleteControlPoint(int id)
   for (int j = NUM_TEXT_ATTRIBUTE_TYPES ; j < NUM_CP_ATTRIBUTE_TYPES; j ++)
     {
       vtkBitArray *dataArray = dynamic_cast <vtkBitArray *> (this->GetAnnotationAttributes(j));
-      if (!dataArray || dataArray->GetNumberOfValues() != n)
+      if (!dataArray || dataArray->GetSize() != n)
         {
           vtkErrorMacro("Annotation " << this->GetName() << " Attribute " << this->GetAttributeTypesEnumAsString(j)
-                        << " is out of sync with PolyData->GetPoints()! Size of Attribute:" << dataArray->GetNumberOfValues()
+                        << " is out of sync with PolyData->GetPoints()! Size of Attribute:" << dataArray->GetSize()
                         << " Number of Points: " << n);
         }
       else
