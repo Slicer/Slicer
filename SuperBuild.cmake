b2d3655ec4f25f205f1403a0fd38651f9bf0c77d
Steve Pieper <pieper@isomics.com>
2019-01-16 02:58:09 +0000

    BUG: fix LandmarkRegistration SimpleITK
    Fixes issues performing landmark refinement
    near image borders.
    
    https://github.com/pieper/LandmarkRegistration/pull/24
    
    Thanks Ziv Yaniv!
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27779
    git-svn-id: http://svn.slicer.org/Slicer4/branches/Slicer-4-10@27779 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -318,7 +318,7 @@ list_conditional_append(Slicer_BUILD_CompareVolumes Slicer_REMOTE_DEPENDENCIES C
 
 Slicer_Remote_Add(LandmarkRegistration
   GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/pieper/LandmarkRegistration"
-  GIT_TAG "3bfa3c1b27930d7c9eee0fecc22e2bcee1d7fe95"
+  GIT_TAG "beb4e47a5455bf2bfa8cde8aee6b03c76844c4c2"
   OPTION_NAME Slicer_BUILD_LandmarkRegistration
   OPTION_DEPENDS "Slicer_BUILD_CompareVolumes;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
