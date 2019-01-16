320be564275db2838c55c1c59b1511e8442cd36c
Steve Pieper <pieper@isomics.com>
2019-01-16 03:00:36 +0000

    ENH: add test code for calling python from javascript
    This captures the methodology for calling python
    from within a page running in a qSlicerWebView,
    which works if invoked via the web concole of a
    page, but does not work within a selftest.
    
    The code is stubbed out but left as examples for
    use by web page developers.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27909
    git-svn-id: http://svn.slicer.org/Slicer4/branches/Slicer-4-10@27909 3bd1e089-480b-0410-8dfb-8563597acbee

--- Applications/SlicerApp/Testing/Python/WebEngine.py
+++ Applications/SlicerApp/Testing/Python/WebEngine.py
@@ -144,11 +144,14 @@ class WebEngineTest(ScriptedLoadableModuleTest):
 
     self.delayDisplay("Starting the test")
 
+
     webWidget = slicer.qSlicerWebWidget()
     webWidget.size = qt.QSize(1024,512)
     webView = webWidget.webView()
     webWidget.show()
     self.delayDisplay('Showing widget')
+
+
 
     webWidget.connect("evalResult(QString,QString)", self.onEvalResult)
 
@@ -165,8 +168,63 @@ class WebEngineTest(ScriptedLoadableModuleTest):
     webWidget.evalJS("const valueFromSlicer = 42;")
     webWidget.evalJS("valueFromSlicer;");
 
+    iteration = 0
     while not self.gotResponse:
       self.delayDisplay('Waiting for response...')
     webWidget.disconnect("evalResult(QString,QString)", self.onEvalResult)
+
+    if iteration >= 10:
+      raise Exception("Never got response from evalJS")
+
+
+    #
+    # Note - calling slicerPython code from JavaScript code works when
+    # executed from the web page, but oddly not from within the
+    # context of a self test, so the following tests are disabled for now.
+    #
+
+    if False:
+
+        self.delayDisplay('Call a python method')
+
+        webWidget.evalJS("""
+            new QWebChannel(qt.webChannelTransport, channel => {
+                const slicerPython = channel.objects.slicerPython;
+
+                let pythonCode = "dialog = qt.QInputDialog(slicer.util.mainWindow())\n";
+                pythonCode += "dialog.setLabelText('hello')\n";
+                pythonCode += "dialog.open()\n";
+                pythonCode += "qt.QTimer.singleShot(1000, dialog.close)\n";
+
+                slicerPython.evalPython(pythonCode);
+            });
+        """)
+
+
+
+        self.delayDisplay('Test access to python via js')
+
+        if hasattr(slicer.modules, 'slicerPythonValueFromJS'):
+          del slicer.modules.slicerPythonValueFromJS
+
+        webWidget.evalJS("""
+            new QWebChannel(qt.webChannelTransport, channel => {
+                channel.objects.slicerPython.evalPython("slicer.modules.slicerPythonValueFromJS = 42");
+            });
+        """)
+
+        iteration = 0
+        while not hasattr(slicer.modules, 'slicerPythonValueFromJS') and iteration < 5:
+          self.delayDisplay('Waiting for python value from JS...')
+          iteration += 1
+
+        if iteration >= 5:
+          raise Exception("Couldn't get python value back from JS")
+
+        del slicer.modules.slicerPythonValueFromJS
+
+
+        self.delayDisplay('Value of %d receieved via javascipt' % slicerPythonValueFromJS)
+
 
     self.delayDisplay('Test passed!')
