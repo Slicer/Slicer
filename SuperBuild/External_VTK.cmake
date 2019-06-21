babd799733f81dcfb72ae3a95bbdb7e4e843899e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-06-21 00:10:14 +0000

    BUG: Update VTK to backport fixes for disappearing labels in tall views.
    See https://issues.slicer.org/view.php?id=4689
    
    Original VTK Merge requests:
    * https://gitlab.kitware.com/vtk/vtk/merge_requests/4836
    * https://gitlab.kitware.com/vtk/vtk/merge_requests/5567
    * https://gitlab.kitware.com/vtk/vtk/merge_requests/5621
    
    List of VTK changes:
    
    $ git shortlog 4961b3efb0..e6094a46e1 --no-merges
    Andras Lasso (2):
          [backport] Fix disappearing labels when anchor points are not in world
          [backport] Fix loss of precision in vtkPlanes::SetFrustumPlanes
    
    Ben Boeckel (1):
          [backport] vtkLabelPlacementMapper: fix in-frame detection
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=28321
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@28321 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -122,7 +122,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
 set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "4961b3efb0317470370ed0d8ea12ced76d7eb241")
+  set(_git_tag "e6094a46e12045b06ca54b5ca116add69b44e41d")
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()
