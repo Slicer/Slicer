a5a3b51611e3de2acf28262d63a5c5a5edf15025
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-07-01 17:41:15 +0000

    BUG: Update CTK to fix auto-completion including unicode characters
    See https://discourse.slicer.org/t/python-interactor-disconnecting/7166/8
    
    List of changes:
    
    $ git shortlog a18ece7d..b93e8dca --no-merges
    Jean-Christophe Fillion-Robin (1):
          BUG: Update ctkPythonConsole to support auto-completion including unicode characters
    
    
    Co-authored-by: Andras Lasso <lasso@queensu.ca>
    Co-authored-by: James Butler <jbutler@sonovol.com>
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=28353
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@28353 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -57,7 +57,7 @@ if(NOT DEFINED CTK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "a18ece7db314a6f638db747ca8d05cc6f5c41bb3"
+    "b93e8dca6b932bdb1d25fe70155c7715b1f386af"
     QUIET
     )
 
