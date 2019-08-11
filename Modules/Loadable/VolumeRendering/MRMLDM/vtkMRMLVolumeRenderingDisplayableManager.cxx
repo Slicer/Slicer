993804c12bf59a56910c12ac4a78d4e48b1e8e50
Steve Pieper <pieper@isomics.com>
2019-08-11 17:01:16 +0000

    BUG: stub out MultiVolume call that prints vtkError
    See inline comment - this change prevents printing
    of an error every render and doesn't appear
    to change behavior.
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=28433
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@28433 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Loadable/VolumeRendering/MRMLDM/vtkMRMLVolumeRenderingDisplayableManager.cxx
+++ Modules/Loadable/VolumeRendering/MRMLDM/vtkMRMLVolumeRenderingDisplayableManager.cxx
@@ -806,10 +806,17 @@ void vtkMRMLVolumeRenderingDisplayableManager::vtkInternal::UpdateDisplayNodePip
   pipeline->VolumeActor->SetProperty(volumeProperty);
   // vtkMultiVolume's GetProperty returns the volume property from the first volume actor, and that is used when assembling the
   // shader, so need to set the volume property to the the first volume actor (in this case dummy actor, see above TODO)
+  /*
+   * This call causes VTK to trigger an error message if the MultiVolumeActor has not yet
+   * been initialized (see line line 76 in vtkMultiVolume::GetVolume)
+   * and I don't see any behavior difference with this commented out.
+   * Probably it can be added back when the rest of the multivolume
+   * issues have been resolved.
   if (this->MultiVolumeActor && this->MultiVolumeActor->GetVolume(0))
     {
     this->MultiVolumeActor->GetVolume(0)->SetProperty(volumeProperty);
     }
+  */
 
   // Set shader property
   vtkShaderProperty* shaderProperty = displayNode->GetShaderPropertyNode() ? displayNode->GetShaderPropertyNode()->GetShaderProperty() : nullptr;
