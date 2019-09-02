ff49db55b99bf19a58dd2ecac547dbd052e2dc41
Steve Pieper <pieper@isomics.com>
2019-09-02 22:09:42 +0000

    BUG: fix single quote escape in CTK SQL DICOM
    Addresses an issue when there's an apostrophe
    in a field name (e.g. Briham and Women's)
    
    See: https://github.com/commontk/CTK/pull/881
    
    $ git shortlog 6a8d584d3eb0ed750f6a850ab4f9c1089820f701..9e89c22d8599763bc20960d1b22175e465fac65c --no-merges
    Csaba Pinter (1):
          BUG: Escape single quote in DICOM database SQL statement
    
    Steve Pieper (1):
          STYLE: remove ^M characters
    
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=28474
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@28474 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -57,7 +57,7 @@ if(NOT DEFINED CTK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "6a8d584d3eb0ed750f6a850ab4f9c1089820f701"
+    "9e89c22d8599763bc20960d1b22175e465fac65c"
     QUIET
     )
 
