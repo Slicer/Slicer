Slicer/Slicer

commit 2ed5886033cd11a5e323cbbbe84b3e9f5962df43

BUG: Fix tests timeout ensuring dicom database directory is auto created
This commit is a follow up of r28452 (BUG: Set defaults specific to
testing related to DICOM loading), it sets schemaUpdateAutoCreateDirectory
to True to prevent "Choose existing database / Select empty folder for new DICOM database"
dialog from being displayed when running tests.

Co-authored-by: Sam Horvath <sam.horvath@kitware.com>
svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=28464
git-svn-id: http://svn.slicer.org/Slicer4/trunk@28464 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DICOMLib/DICOMWidgets.py
@@ -523,6 +523,7 @@ class DICOMDetailsBase(VTKObservationMixin, SizePositionSettingsMixin):
         But, if the application is in testing mode, just pick a temp directory.
     """
     if slicer.app.commandOptions().testingEnabled:
+      self.dicomBrowser.schemaUpdateAutoCreateDirectory = True
       databaseDirectory = os.path.join(slicer.app.temporaryPath, 'tempDICOMDatabase')
     else:
       databaseDirectory = self.settings.value('DatabaseDirectory')

commit 686c76ed1794085415ba8e308895288d052a37df

BUG: Fix python self-tests ensuring dicomBrowser is properly initialized
This commit fixes a regression introduced in r28464 (BUG: Fix tests
timeout ensuring dicom database directory is auto created)

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=28481
git-svn-id: http://svn.slicer.org/Slicer4/trunk@28481 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DICOMLib/DICOMWidgets.py
@@ -97,6 +97,9 @@ class DICOMDetailsBase(VTKObservationMixin, SizePositionSettingsMixin):
     # is specified in the settings, therefore promptForDatabaseDirectory must be called before this.
     self.dicomBrowser = dicomBrowser if dicomBrowser is not None else ctkDICOMBrowser()
 
+    if slicer.app.commandOptions().testingEnabled:
+      self.dicomBrowser.schemaUpdateAutoCreateDirectory = True
+
     self.browserPersistent = settingsValue('DICOM/BrowserPersistent', False, converter=toBool)
     self.tableDensity = settingsValue('DICOM/tableDensity', 'Compact')
     self.advancedView = settingsValue('DICOM/advancedView', 0, converter=int)
@@ -523,7 +526,6 @@ class DICOMDetailsBase(VTKObservationMixin, SizePositionSettingsMixin):
         But, if the application is in testing mode, just pick a temp directory.
     """
     if slicer.app.commandOptions().testingEnabled:
-      self.dicomBrowser.schemaUpdateAutoCreateDirectory = True
       databaseDirectory = os.path.join(slicer.app.temporaryPath, 'tempDICOMDatabase')
     else:
       databaseDirectory = self.settings.value('DatabaseDirectory')