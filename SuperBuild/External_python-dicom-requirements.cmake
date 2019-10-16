eb3e603340dc26ab51ba2039c9d35b302f1bb3a9
Andras Lasso <lasso@queensu.ca>
2019-10-16 01:56:56 +0000

    COMP: Fix python-dicom-requirements not installing
    svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=28549
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@28549 3bd1e089-480b-0410-8dfb-8563597acbee

--- SuperBuild/External_python-dicom-requirements.cmake
+++ SuperBuild/External_python-dicom-requirements.cmake
@@ -1,13 +1,33 @@
 set(proj python-dicom-requirements)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python python-setuptools python-requests-requirements python-pillow python-numpy python-pip)
+set(${proj}_DEPENDENCIES python python-setuptools python-pip)
 
 set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
 file(WRITE ${requirements_file} [===[
 dicom==0.9.9.post1 --hash=sha256:9caac735341cfb7ef4c718186ab4339088b083cf12ae8847464a924ec5c6a185
 pydicom==1.2.2 --hash=sha256:44a11323e9ee5c189da20958c28ca426656778ee7c7942a2126bf0a9f6aa4e95
+# NOTE: The requirements specified below are needed for dicomweb_client to install correctly and not in a project dependency
+# Hashes correspond to the following packages:
+# - numpy-1.16.2-cp36-cp36m-win_amd64.whl
+# - numpy-1.16.2-cp36-cp36m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl
+# - numpy-1.16.2-cp36-cp36m-manylinux1_x86_64.whl
+numpy==1.16.2 --hash=sha256:d20c0360940f30003a23c0adae2fe50a0a04f3e48dc05c298493b51fd6280197 \
+              --hash=sha256:bd2834d496ba9b1bdda3a6cf3de4dc0d4a0e7be306335940402ec95132ad063d \
+              --hash=sha256:23cc40313036cffd5d1873ef3ce2e949bdee0646c5d6f375bf7ee4f368db2511
+# Hashes correspond to the following packages:
+#  - Pillow-6.2.0-cp36-cp36m-win_amd64.whl
+#  - Pillow-6.2.0-cp36-cp36m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl
+#  - Pillow-6.2.0-cp36-cp36m-manylinux1_x86_64.whl
+pillow==6.2.0 --hash=sha256:338581b30b908e111be578f0297255f6b57a51358cd16fa0e6f664c9a1f88bff \
+              --hash=sha256:ec883b8e44d877bda6f94a36313a1c6063f8b1997aa091628ae2f34c7f97c8d5 \
+              --hash=sha256:ceb76935ac4ebdf6d7bc845482a4450b284c6ccfb281e34da51d510658ab34d8
 six==1.12.0 --hash=sha256:3350809f0555b11f552448330d0b52d5f24c91a322ea4a15ef22629740f3761c
+certifi==2018.11.29 --hash=sha256:993f830721089fef441cdfeb4b2c8c9df86f0c63239f06bd025a76a7daddb033
+idna==2.8 --hash=sha256:ea8b7f6188e6fa117537c3df7da9fc686d485087abf6ac197f9c46432f7e4a3c
+chardet==3.0.4 --hash=sha256:fc323ffcaeaed0e0a02bf4d117757b98aed530d9ed4531e3e15460124c106691
+urllib3==1.24.1 --hash=sha256:61bf29cada3fc2fbefad4fdf059ea4bd1b4a86d2b6d15e1c7c0b582b9752fe39
+requests==2.21.0 --hash=sha256:7bf2a778576d825600030a110f3c0e3e8edc51dfaafe1c146e39a2027784957b
 dicomweb_client==0.11.0 --hash=sha256:7e92bfdcb8aac7d1ffe059bb77df66dc7276df32aa7e6bc56ab74635ea918739
 ]===])
 
@@ -19,7 +39,7 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(Slicer_USE_SYSTEM_${proj})
-  foreach(module_name IN ITEMS dicom pydicom dicomweb_client)
+  foreach(module_name IN ITEMS dicom pydicom numpy pillow six certifi idna chardet urllib3 requests dicomweb_client)
     ExternalProject_FindPythonPackage(
       MODULE_NAME "${module_name}"
       REQUIRED
