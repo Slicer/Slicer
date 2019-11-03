Slicer/SlicerGitSVNArchive

commit 26ecf484d51e4c298a76b80b3a0dec7e9c157a1d

COMP: Install pip, setuptools, wheel using whl files
From: James Butler <jbutler@sonovol.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28580 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild.cmake
@@ -124,7 +124,6 @@ endif()
 if(Slicer_USE_PYTHONQT)
   list(APPEND Slicer_DEPENDENCIES
     python-pythonqt-requirements  # This provides the "packaging.version.parse()" function
-    python-pip
     )
 endif()
 

SuperBuild/External_python-ensurepip.cmake
@@ -1,14 +1,7 @@
-set(proj python-pythonqt-requirements)
+set(proj python-ensurepip)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python python-ensurepip python-pip python-setuptools python-wheel)
-
-set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
-file(WRITE ${requirements_file} [===[
-packaging==19.0 --hash=sha256:9e1cbf8c12b1f1ce0bb5344b8d7ecf66a6f8a6e91bcb0c84593ed6d3ab5c4ab3
-pyparsing==2.3.1 --hash=sha256:f6c5ef0d7480ad048c054c37632c67fca55299990fff127850181659eea33fc3
-six==1.12.0 --hash=sha256:3350809f0555b11f552448330d0b52d5f24c91a322ea4a15ef22629740f3761c
-]===])
+set(${proj}_DEPENDENCIES python)
 
 if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
   set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
@@ -18,7 +11,7 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(Slicer_USE_SYSTEM_${proj})
-  foreach(module_name IN ITEMS pyparsing packaging)
+  foreach(module_name IN ITEMS ensurepip pip setuptools)
     ExternalProject_FindPythonPackage(
       MODULE_NAME "${module_name}"
       REQUIRED
@@ -35,7 +28,7 @@ if(NOT Slicer_USE_SYSTEM_${proj})
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m ensurepip --default-pip
     LOG_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}

SuperBuild/External_python-pip.cmake
@@ -1,7 +1,12 @@
 set(proj python-pip)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python python-wheel)
+set(${proj}_DEPENDENCIES python python-ensurepip python-setuptools)
+
+set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
+file(WRITE ${requirements_file} [===[
+pip==19.0.3 --hash=sha256:bd812612bbd8ba84159d9ddc0266b7fbce712fc9bc98c82dee5750546ec8ec64
+]===])
 
 if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
   set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
@@ -11,26 +16,24 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(Slicer_USE_SYSTEM_${proj})
-  ExternalProject_FindPythonPackage(
-    MODULE_NAME "pip"
-    REQUIRED
-    )
+  foreach(module_name IN ITEMS pip)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
 endif()
 
 if(NOT Slicer_USE_SYSTEM_${proj})
 
-  set(_version "19.0.3")
-
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
-    URL "https://files.pythonhosted.org/packages/36/fa/51ca4d57392e2f69397cd6e5af23da2a8d37884a605f9e3f2d3bfdc48397/pip-${_version}.tar.gz"
-    URL_HASH "SHA256=6e6f197a1abfb45118dbb878b5c859a0edbdd33fd250100bc015b67fded4b9f2"
-    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
+    DOWNLOAD_COMMAND ""
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
     LOG_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}

SuperBuild/External_python-setuptools.cmake
@@ -1,7 +1,12 @@
 set(proj python-setuptools)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python)
+set(${proj}_DEPENDENCIES python python-ensurepip)
+
+set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
+file(WRITE ${requirements_file} [===[
+setuptools==40.8.0 --hash=sha256:e8496c0079f3ac30052ffe69b679bd876c5265686127a3159cfa415669b7f9ab
+]===])
 
 if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
   set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
@@ -11,51 +16,31 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(Slicer_USE_SYSTEM_${proj})
-  ExternalProject_FindPythonPackage(
-    MODULE_NAME "setuptools"
-    REQUIRED
-    )
+  foreach(module_name IN ITEMS setuptools)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
 endif()
 
 if(NOT Slicer_USE_SYSTEM_${proj})
 
-  include(ExternalProjectForNonCMakeProject)
-
-  # environment
-  set(_env_script ${CMAKE_BINARY_DIR}/${proj}_Env.cmake)
-  ExternalProject_Write_SetBuildEnv_Commands(${_env_script})
-  ExternalProject_Write_SetPythonSetupEnv_Commands(${_env_script} APPEND)
-
-  set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
-
-  # install step
-  # - we use "easy_install" only to allow installing with "--always-unzip"
-  set(_install_script ${CMAKE_BINARY_DIR}/${proj}_install_step.cmake)
-  file(WRITE ${_install_script}
-"include(\"${_env_script}\")
-set(${proj}_WORKING_DIR \"${CMAKE_BINARY_DIR}/${proj}\")
-ExternalProject_Execute(${proj} \"bootstrap\" \"${PYTHON_EXECUTABLE}\" bootstrap.py)
-ExternalProject_Execute(${proj} \"easy_install\" \"${PYTHON_EXECUTABLE}\" setup.py easy_install --always-unzip .)
-")
-
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
-    # slicer-v40.8.0
-    # - include patch to support parallel build
-    GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/Slicer/setuptools.git"
-    GIT_TAG "cb19e30abe623914ad21297ea64838c38c82a2fe"
-    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
-    SOURCE_DIR ${EP_SOURCE_DIR}
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${CMAKE_COMMAND} -P ${_install_script}
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    LOG_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}
     )
 
   ExternalProject_GenerateProjectDescription_Step(${proj}
-    SOURCE_DIR ${EP_SOURCE_DIR}
+    VERSION ${_version}
     )
 
 else()

SuperBuild/External_python-wheel.cmake
@@ -1,7 +1,12 @@
 set(proj python-wheel)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python python-setuptools)
+set(${proj}_DEPENDENCIES python python-setuptools python-pip)
+
+set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
+file(WRITE ${requirements_file} [===[
+wheel==0.33.1 --hash=sha256:8eb4a788b3aec8abf5ff68d4165441bc57420c9f64ca5f471f58c3969fe08668
+]===])
 
 if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
   set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
@@ -11,26 +16,24 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(Slicer_USE_SYSTEM_${proj})
-  ExternalProject_FindPythonPackage(
-    MODULE_NAME "wheel"
-    REQUIRED
-    )
+  foreach(module_name IN ITEMS wheel)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
 endif()
 
 if(NOT Slicer_USE_SYSTEM_${proj})
 
-  set(_version "0.33.1")
-
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
-    URL "https://files.pythonhosted.org/packages/b7/cf/1ea0f5b3ce55cacde1e84cdde6cee1ebaff51bd9a3e6c7ba4082199af6f6/wheel-${_version}.tar.gz"
-    URL_HASH "SHA256=66a8fd76f28977bb664b098372daef2b27f60dc4d1688cfab7b37a09448f0e9d"
-    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
+    DOWNLOAD_COMMAND ""
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
     LOG_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}

Slicer/Slicer

commit 47a8e5b90fe7ca2ce2e3834a8f2b1019413907e5

BUG: Revert "COMP: Install pip, setuptools, wheel using whl files"
This reverts commit 26ecf484d51e4c298a76b80b3a0dec7e9c157a1d, which broke import of packages such as dicom and pydicom:

>>> import dicom
Traceback (most recent call last):
  File "<console>", line 1, in <module>
ModuleNotFoundError: No module named 'dicom'
>>> import pydicom
Traceback (most recent call last):
  File "<console>", line 1, in <module>
ModuleNotFoundError: No module named 'pydicom'

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=28587
git-svn-id: http://svn.slicer.org/Slicer4/trunk@28587 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild.cmake
@@ -124,6 +124,7 @@ endif()
 if(Slicer_USE_PYTHONQT)
   list(APPEND Slicer_DEPENDENCIES
     python-pythonqt-requirements  # This provides the "packaging.version.parse()" function
+    python-pip
     )
 endif()
 

SuperBuild/External_python-ensurepip.cmake
@@ -1,43 +1,0 @@
-set(proj python-ensurepip)
-
-# Set dependency list
-set(${proj}_DEPENDENCIES python)
-
-if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
-  set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
-endif()
-
-# Include dependent projects if any
-ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
-
-if(Slicer_USE_SYSTEM_${proj})
-  foreach(module_name IN ITEMS ensurepip pip setuptools)
-    ExternalProject_FindPythonPackage(
-      MODULE_NAME "${module_name}"
-      REQUIRED
-      )
-  endforeach()
-endif()
-
-if(NOT Slicer_USE_SYSTEM_${proj})
-
-  ExternalProject_Add(${proj}
-    ${${proj}_EP_ARGS}
-    DOWNLOAD_COMMAND ""
-    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
-    BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m ensurepip --default-pip
-    LOG_INSTALL 1
-    DEPENDS
-      ${${proj}_DEPENDENCIES}
-    )
-
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
-else()
-  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
-endif()

SuperBuild/External_python-pip.cmake
@@ -1,12 +1,7 @@
 set(proj python-pip)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python python-ensurepip python-setuptools)
-
-set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
-file(WRITE ${requirements_file} [===[
-pip==19.0.3 --hash=sha256:bd812612bbd8ba84159d9ddc0266b7fbce712fc9bc98c82dee5750546ec8ec64
-]===])
+set(${proj}_DEPENDENCIES python python-wheel)
 
 if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
   set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
@@ -16,24 +11,26 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(Slicer_USE_SYSTEM_${proj})
-  foreach(module_name IN ITEMS pip)
-    ExternalProject_FindPythonPackage(
-      MODULE_NAME "${module_name}"
-      REQUIRED
-      )
-  endforeach()
+  ExternalProject_FindPythonPackage(
+    MODULE_NAME "pip"
+    REQUIRED
+    )
 endif()
 
 if(NOT Slicer_USE_SYSTEM_${proj})
 
+  set(_version "19.0.3")
+
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
-    DOWNLOAD_COMMAND ""
+    URL "https://files.pythonhosted.org/packages/36/fa/51ca4d57392e2f69397cd6e5af23da2a8d37884a605f9e3f2d3bfdc48397/pip-${_version}.tar.gz"
+    URL_HASH "SHA256=6e6f197a1abfb45118dbb878b5c859a0edbdd33fd250100bc015b67fded4b9f2"
+    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
     LOG_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}

SuperBuild/External_python-pythonqt-requirements.cmake
@@ -1,7 +1,7 @@
 set(proj python-pythonqt-requirements)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python python-ensurepip python-pip python-setuptools python-wheel)
+set(${proj}_DEPENDENCIES python python-setuptools python-wheel python-pip)
 
 set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
 file(WRITE ${requirements_file} [===[

SuperBuild/External_python-setuptools.cmake
@@ -1,12 +1,7 @@
 set(proj python-setuptools)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python python-ensurepip)
-
-set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
-file(WRITE ${requirements_file} [===[
-setuptools==40.8.0 --hash=sha256:e8496c0079f3ac30052ffe69b679bd876c5265686127a3159cfa415669b7f9ab
-]===])
+set(${proj}_DEPENDENCIES python)
 
 if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
   set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
@@ -16,31 +11,51 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(Slicer_USE_SYSTEM_${proj})
-  foreach(module_name IN ITEMS setuptools)
-    ExternalProject_FindPythonPackage(
-      MODULE_NAME "${module_name}"
-      REQUIRED
-      )
-  endforeach()
+  ExternalProject_FindPythonPackage(
+    MODULE_NAME "setuptools"
+    REQUIRED
+    )
 endif()
 
 if(NOT Slicer_USE_SYSTEM_${proj})
 
+  include(ExternalProjectForNonCMakeProject)
+
+  # environment
+  set(_env_script ${CMAKE_BINARY_DIR}/${proj}_Env.cmake)
+  ExternalProject_Write_SetBuildEnv_Commands(${_env_script})
+  ExternalProject_Write_SetPythonSetupEnv_Commands(${_env_script} APPEND)
+
+  set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
+
+  # install step
+  # - we use "easy_install" only to allow installing with "--always-unzip"
+  set(_install_script ${CMAKE_BINARY_DIR}/${proj}_install_step.cmake)
+  file(WRITE ${_install_script}
+"include(\"${_env_script}\")
+set(${proj}_WORKING_DIR \"${CMAKE_BINARY_DIR}/${proj}\")
+ExternalProject_Execute(${proj} \"bootstrap\" \"${PYTHON_EXECUTABLE}\" bootstrap.py)
+ExternalProject_Execute(${proj} \"easy_install\" \"${PYTHON_EXECUTABLE}\" setup.py easy_install --always-unzip .)
+")
+
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
-    DOWNLOAD_COMMAND ""
-    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    # slicer-v40.8.0
+    # - include patch to support parallel build
+    GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/Slicer/setuptools.git"
+    GIT_TAG "cb19e30abe623914ad21297ea64838c38c82a2fe"
+    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
+    SOURCE_DIR ${EP_SOURCE_DIR}
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
-    LOG_INSTALL 1
+    INSTALL_COMMAND ${CMAKE_COMMAND} -P ${_install_script}
     DEPENDS
       ${${proj}_DEPENDENCIES}
     )
 
   ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
+    SOURCE_DIR ${EP_SOURCE_DIR}
     )
 
 else()

SuperBuild/External_python-wheel.cmake
@@ -1,12 +1,7 @@
 set(proj python-wheel)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python python-setuptools python-pip)
-
-set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
-file(WRITE ${requirements_file} [===[
-wheel==0.33.1 --hash=sha256:8eb4a788b3aec8abf5ff68d4165441bc57420c9f64ca5f471f58c3969fe08668
-]===])
+set(${proj}_DEPENDENCIES python python-setuptools)
 
 if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
   set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
@@ -16,24 +11,26 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(Slicer_USE_SYSTEM_${proj})
-  foreach(module_name IN ITEMS wheel)
-    ExternalProject_FindPythonPackage(
-      MODULE_NAME "${module_name}"
-      REQUIRED
-      )
-  endforeach()
+  ExternalProject_FindPythonPackage(
+    MODULE_NAME "wheel"
+    REQUIRED
+    )
 endif()
 
 if(NOT Slicer_USE_SYSTEM_${proj})
 
+  set(_version "0.33.1")
+
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
-    DOWNLOAD_COMMAND ""
+    URL "https://files.pythonhosted.org/packages/b7/cf/1ea0f5b3ce55cacde1e84cdde6cee1ebaff51bd9a3e6c7ba4082199af6f6/wheel-${_version}.tar.gz"
+    URL_HASH "SHA256=66a8fd76f28977bb664b098372daef2b27f60dc4d1688cfab7b37a09448f0e9d"
+    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
     LOG_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}