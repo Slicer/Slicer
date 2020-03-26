Slicer/Slicer

commit 3814f1af649d63a1737005304b0924a03393446a

ENH: Use commit count as working copy revision
When using git, revision is a sha1 hash, which is not as user-friendly as a revision counter as in SVN.

Added Slicer_REVISION_TYPE variable that controls how Slicer_REVISION variable is set: commit count on the current branch or git hash.
Same variables are added for Slicer_MAIN_PROJECT, too.

Updated scripts to use Slicer_REVISION instead of Slicer_WC_REVISION.

CMake/SlicerExtensionCPack.cmake
@@ -29,7 +29,7 @@ endif()
 # -------------------------------------------------------------------------
 # Sanity checks
 # -------------------------------------------------------------------------
-set(expected_nonempty_vars EXTENSION_NAME Slicer_WC_REVISION Slicer_OS Slicer_ARCHITECTURE)
+set(expected_nonempty_vars EXTENSION_NAME Slicer_REVISION Slicer_OS Slicer_ARCHITECTURE)
 foreach(var ${expected_nonempty_vars})
   if("${${var}}" STREQUAL "")
     message(FATAL_ERROR "error: ${var} is either NOT defined or empty.")
@@ -88,7 +88,7 @@ if(NOT "${Slicer_CPACK_SKIP_GENERATE_EXTENSION_DESCRIPTION}")
     EXTENSION_WC_ROOT ${${EXTENSION_NAME}_WC_READONLY_ROOT}
     EXTENSION_WC_URL ${${EXTENSION_NAME}_WC_READONLY_URL}
     DESTINATION_DIR ${CMAKE_BINARY_DIR}
-    SLICER_WC_REVISION ${Slicer_WC_REVISION}
+    SLICER_REVISION ${Slicer_REVISION}
     SLICER_WC_ROOT ${Slicer_WC_ROOT}
     )
   set(description_file "${CMAKE_BINARY_DIR}/${EXTENSION_NAME}.s4ext")
@@ -121,7 +121,9 @@ set(CMAKE_PROJECT_NAME ${EXTENSION_NAME})
 set(CPACK_PACKAGE_VENDOR "NA-MIC")
 set(CPACK_PACKAGE_DESCRIPTION_FILE "${EXTENSION_README_FILE}")
 set(CPACK_RESOURCE_FILE_LICENSE "${EXTENSION_LICENSE_FILE}")
-set(CPACK_PACKAGE_FILE_NAME "${Slicer_WC_REVISION}-${Slicer_OS}-${Slicer_ARCHITECTURE}-${EXTENSION_NAME}-${${EXTENSION_NAME}_WC_TYPE}${${EXTENSION_NAME}_WC_REVISION}-${${EXTENSION_NAME}_BUILDDATE}")
+string(CONCAT CPACK_PACKAGE_FILE_NAME
+  "${Slicer_REVISION}-${Slicer_OS}-${Slicer_ARCHITECTURE}-${EXTENSION_NAME}"
+  "-${${EXTENSION_NAME}_WC_TYPE}${${EXTENSION_NAME}_REVISION}-${${EXTENSION_NAME}_BUILDDATE}")
 #set(CPACK_PACKAGE_VERSION_MAJOR "${Slicer_VERSION_MAJOR}")
 #set(CPACK_PACKAGE_VERSION_MINOR "${Slicer_VERSION_MINOR}")
 #set(CPACK_PACKAGE_VERSION_PATCH "${Slicer_VERSION_PATCH}")

Base/Logic/Testing/vtkSlicerVersionConfigureTest1.cxx
@@ -32,6 +32,7 @@ int vtkSlicerVersionConfigureTest1(int /*argc*/, char * /*argv*/ [])
   CHECK_STRING_DIFFERENT(Slicer_BUILDDATE, "");
   CHECK_STRING_DIFFERENT(Slicer_WC_URL, "");
   CHECK_STRING_DIFFERENT(Slicer_WC_REVISION, "");
+  CHECK_STRING_DIFFERENT(Slicer_REVISION, "");
 
   // From vtkSlicerVersionConfigureInternal
   CHECK_STRING(Slicer_OS_LINUX_NAME, "linux");

Base/QTApp/qSlicerAboutDialog.cxx
@@ -51,7 +51,8 @@ qSlicerAboutDialog::qSlicerAboutDialog(QWidget* parentWidget)
   d->CreditsTextBrowser->append("");
   if (!slicer->isCustomMainApplication())
     {
-    d->CreditsTextBrowser->append(slicer->applicationVersion() + " " + "r" + slicer->repositoryRevision());
+    d->CreditsTextBrowser->append(slicer->applicationVersion() + " " + "r" + slicer->revision()
+      + " / " + slicer->repositoryRevision());
     d->CreditsTextBrowser->append("");
     d->CreditsTextBrowser->append("");
     d->CreditsTextBrowser->insertHtml("<a href=\"http://download.slicer.org/\">Download</a> a newer version<br />");

Base/QTCore/qSlicerCoreApplication.cxx
@@ -1420,6 +1420,12 @@ QString qSlicerCoreApplication::mainApplicationRepositoryRevision()const
 }
 
 //-----------------------------------------------------------------------------
+QString qSlicerCoreApplication::mainApplicationRevision()const
+{
+  return QString(Slicer_MAIN_PROJECT_REVISION);
+}
+
+//-----------------------------------------------------------------------------
 int qSlicerCoreApplication::mainApplicationMajorVersion()const
 {
   return Slicer_MAIN_PROJECT_VERSION_MAJOR;
@@ -1495,6 +1501,12 @@ QString qSlicerCoreApplication::repositoryBranch()const
 QString qSlicerCoreApplication::repositoryRevision()const
 {
   return Slicer_WC_REVISION;
+}
+
+//-----------------------------------------------------------------------------
+QString qSlicerCoreApplication::revision()const
+{
+  return Slicer_REVISION;
 }
 
 //-----------------------------------------------------------------------------

Base/QTCore/qSlicerCoreApplication.h
@@ -83,6 +83,7 @@ class Q_SLICER_BASE_QTCORE_EXPORT qSlicerCoreApplication : public QApplication
   Q_PROPERTY(QString repositoryUrl READ repositoryUrl CONSTANT)
   Q_PROPERTY(QString repositoryBranch READ repositoryBranch CONSTANT)
   Q_PROPERTY(QString repositoryRevision READ repositoryRevision CONSTANT)
+  Q_PROPERTY(QString revision READ revision CONSTANT)
   Q_PROPERTY(int majorVersion READ majorVersion CONSTANT)
   Q_PROPERTY(int minorVersion READ minorVersion CONSTANT)
   Q_PROPERTY(QString platform READ platform CONSTANT)
@@ -92,6 +93,7 @@ class Q_SLICER_BASE_QTCORE_EXPORT qSlicerCoreApplication : public QApplication
   Q_PROPERTY(QString mainApplicationName READ mainApplicationName CONSTANT)
   Q_PROPERTY(QString mainApplicationRepositoryUrl READ mainApplicationRepositoryUrl CONSTANT)
   Q_PROPERTY(QString mainApplicationRepositoryRevision READ mainApplicationRepositoryRevision CONSTANT)
+  Q_PROPERTY(QString mainApplicationRevision READ mainApplicationRevision CONSTANT)
   Q_PROPERTY(int mainApplicationMajorVersion READ mainApplicationMajorVersion CONSTANT)
   Q_PROPERTY(int mainApplicationMinorVersion READ mainApplicationMinorVersion CONSTANT)
   Q_PROPERTY(int mainApplicationPatchVersion READ mainApplicationPatchVersion CONSTANT)
@@ -386,6 +388,10 @@ public:
   /// \sa qSlicerCoreApplicationPrivate::discoverRepository
   QString mainApplicationRepositoryRevision()const;
 
+  /// Return the main application's user-friendly revision identifier.
+  /// Useful for custom applications.
+  QString mainApplicationRevision()const;
+
   /// Return the main application's major version number.
   /// Useful for custom applications.
   int mainApplicationMajorVersion() const;
@@ -419,6 +425,8 @@ public:
   /// \sa qSlicerCoreApplicationPrivate::discoverRepository
   QString repositoryRevision()const;
 
+  /// Return Slicer's user-friendly revision identifier.
+  QString revision()const;
 
   /// Return the Slicer major version number
   int majorVersion() const;

Base/QTGUI/qSlicerApplication.cxx
@@ -874,9 +874,11 @@ void qSlicerApplication::logApplicationInformation() const
          qPrintable(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss")));
 
   // Slicer version
-  qDebug("%s: %s (revision %s) %s - %s %s",
+  qDebug("%s: %s (revision %s / %s) %s - %s %s",
          qPrintable(titles.at(titleIndex++).leftJustified(titleWidth, '.')),
-         Slicer_VERSION_FULL, qPrintable(this->repositoryRevision()),
+         Slicer_VERSION_FULL,
+         qPrintable(this->revision()),
+         qPrintable(this->repositoryRevision()),
          qPrintable(this->platform()),
          this->isInstalled() ? "installed" : "not installed",
 #ifdef _DEBUG
@@ -889,9 +891,12 @@ void qSlicerApplication::logApplicationInformation() const
   // Custom application version
   if (this->isCustomMainApplication())
     {
-    qDebug("%s: %s (revision %s)",
+    qDebug("%s: %s (revision %s / %s)",
       qPrintable(titles.at(titleIndex++).leftJustified(titleWidth, '.')),
-      Slicer_MAIN_PROJECT_VERSION_FULL, qPrintable(Slicer_MAIN_PROJECT_WC_REVISION));
+      Slicer_MAIN_PROJECT_VERSION_FULL,
+      qPrintable(Slicer_MAIN_PROJECT_REVISION),
+      qPrintable(Slicer_MAIN_PROJECT_REVISION),
+      qPrintable(Slicer_MAIN_PROJECT_WC_REVISION));
     }
 
   // Operating system

CMake/FindGit.cmake
@@ -48,6 +48,7 @@
 #  <var-prefix>_WC_LAST_CHANGED_DATE - date of last commit
 #  <var-prefix>_WC_LAST_CHANGED_REV - revision of last commit
 #  <var-prefix>_WC_LAST_CHANGED_LOG - last log of base revision
+#  <var-prefix>_WC_COMMIT_COUNT - number of commits in current branch
 #
 # Example usage:
 #   find_package(Git)
@@ -120,6 +121,11 @@ if(GIT_EXECUTABLE)
        OUTPUT_STRIP_TRAILING_WHITESPACE)
     string(REGEX REPLACE "^([0-9][0-9][0-9][0-9]\\-[0-9][0-9]\\-[0-9][0-9]).*"
       "\\1" ${prefix}_WC_LAST_CHANGED_DATE "${${prefix}_show_output}")
+
+    execute_process(COMMAND ${GIT_EXECUTABLE} rev-list HEAD --count
+       WORKING_DIRECTORY ${dir}
+       OUTPUT_VARIABLE ${prefix}_WC_COMMIT_COUNT
+       OUTPUT_STRIP_TRAILING_WHITESPACE)
 
     set(${prefix}_WC_GITSVN False)
 

CMake/SlicerConfig.cmake.in
@@ -142,18 +142,20 @@ set(Subversion_SVN_EXECUTABLE "@Subversion_SVN_EXECUTABLE@" CACHE FILEPATH "subv
 set(GIT_EXECUTABLE "@GIT_EXECUTABLE@"  CACHE FILEPATH "Git command line client")
 
 # Slicer working copy Revision, URL and Root
+set(Slicer_REVISION "@Slicer_REVISION@")
 set(Slicer_WC_REVISION "@Slicer_WC_REVISION@")
+set(Slicer_WC_REVISION_HASH "@Slicer_WC_REVISION_HASH@")
 set(Slicer_WC_URL "@Slicer_WC_URL@")
 set(Slicer_WC_ROOT "@Slicer_WC_ROOT@")
 
-# Note that the variable "Slicer_WC_REVISION" can be forced to
+# Note that the variable "Slicer_REVISION" can be forced to
 # a particular value by setting environment variable of the same name.
 # This is particularly useful when the revision of Slicer changes because of
 # updates to the extension build system and the extensions still need to be
 # built against the revision prior to the update.
-if(NOT "$ENV{Slicer_WC_REVISION}" STREQUAL "")
-  set(Slicer_WC_REVISION "$ENV{Slicer_WC_REVISION}")
-  message(STATUS "SlicerConfig: Forcing Slicer_WC_REVISION to '${Slicer_WC_REVISION}'")
+if(NOT "$ENV{Slicer_REVISION}" STREQUAL "")
+  set(Slicer_REVISION "$ENV{Slicer_REVISION}")
+  message(STATUS "SlicerConfig: Forcing Slicer_REVISION to '${Slicer_REVISION}'")
 endif()
 
 # Slicer os and architecture

commit b88531612e28dc860a790a0835d19523f925ca77

BUG: Fix missing extension revision in extension package
This commit fixes a regression introduced in 3814f1af6 (ENH: Use commit count as
working copy revision )

Prior to the regression, extension packages used to have names like
28257-win-amd64-SegmentEditorExtraEffects-git5085c07-2020-03-23.zip

, but because of the typo they were missing the extension revision and were like
28879-win-amd64-SegmentEditorExtraEffects-git-2020-03-23.zip

CMake/SlicerExtensionCPack.cmake
@@ -123,7 +123,7 @@ set(CPACK_PACKAGE_DESCRIPTION_FILE "${EXTENSION_README_FILE}")
 set(CPACK_RESOURCE_FILE_LICENSE "${EXTENSION_LICENSE_FILE}")
 string(CONCAT CPACK_PACKAGE_FILE_NAME
   "${Slicer_REVISION}-${Slicer_OS}-${Slicer_ARCHITECTURE}-${EXTENSION_NAME}"
-  "-${${EXTENSION_NAME}_WC_TYPE}${${EXTENSION_NAME}_REVISION}-${${EXTENSION_NAME}_BUILDDATE}")
+  "-${${EXTENSION_NAME}_WC_TYPE}${${EXTENSION_NAME}_WC_REVISION}-${${EXTENSION_NAME}_BUILDDATE}")
 #set(CPACK_PACKAGE_VERSION_MAJOR "${Slicer_VERSION_MAJOR}")
 #set(CPACK_PACKAGE_VERSION_MINOR "${Slicer_VERSION_MINOR}")
 #set(CPACK_PACKAGE_VERSION_PATCH "${Slicer_VERSION_PATCH}")