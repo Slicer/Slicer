37ab24085623bb2155d2e49e4d4c07414eea5fb9
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2020-04-01 11:59:10 -0400

    COMP: Fix configuration with CMake >= 3.17
    This commit renames the variable conditionally from CMAKE_DEFAULT_BUILD_TYPE
    to Slicer_DEFAULT_BUILD_TYPE.
    
    This change is required because Slicer build-system introduced the
    variable CMAKE_DEFAULT_BUILD_TYPE in Nov 2017 (see c808da1) and it now
    triggers a configuration error following the introduction of the same
    variable in CMake 3.17.
    See https://cmake.org/cmake/help/latest/variable/CMAKE_DEFAULT_BUILD_TYPE.html
    
    The variable CMAKE_DEFAULT_BUILD_TYPE may only be set with the "Ninja Multi-Config"
    generator.
    
    Error fixed by this commit:
    
    ```
      CMake Error:
         Generator
    
           <GeneratorName>
    
         does not support variable
    
           CMAKE_DEFAULT_BUILD_TYPE
    
         but it has been specified.
    ```
    
    where <GeneratorName> is any generator different from "Ninja Multi-Config".
    
    Custom Slicer application may have to be updated to set
    Slicer_DEFAULT_BUILD_TYPE instead of CMAKE_DEFAULT_BUILD_TYPE.
    
    Fixes #4798

--- CMake/SlicerInitializeBuildType.cmake
+++ CMake/SlicerInitializeBuildType.cmake
@@ -1,15 +1,15 @@
 
 # Default build type to use if none was specified
-if(NOT DEFINED CMAKE_DEFAULT_BUILD_TYPE)
-  set(CMAKE_DEFAULT_BUILD_TYPE "Debug")
+if(NOT DEFINED Slicer_DEFAULT_BUILD_TYPE)
+  set(Slicer_DEFAULT_BUILD_TYPE "Debug")
 endif()
 
 # Set a default build type if none was specified
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
 
-  message(STATUS "Setting build type to '${CMAKE_DEFAULT_BUILD_TYPE}' as none was specified.")
+  message(STATUS "Setting build type to '${Slicer_DEFAULT_BUILD_TYPE}' as none was specified.")
 
-  set(CMAKE_BUILD_TYPE ${CMAKE_DEFAULT_BUILD_TYPE} CACHE STRING "Choose the type of build." FORCE)
+  set(CMAKE_BUILD_TYPE ${Slicer_DEFAULT_BUILD_TYPE} CACHE STRING "Choose the type of build." FORCE)
   mark_as_advanced(CMAKE_BUILD_TYPE)
 
   # Set the possible values of build type for cmake-gui
@@ -29,4 +29,3 @@ if(COMMAND mark_as_superbuild)
     mark_as_superbuild(VARS CMAKE_CONFIGURATION_TYPES ALL_PROJECTS)
   endif()
 endif()
-
