7bcb6a16f6183dee4992d92098cc7c9330d89984
Andras Lasso <lasso@queensu.ca>
2020-04-06 21:54:39 -0400

    STYLE: Remove unnecessary semicolons from util.py

--- Base/Python/slicer/util.py
+++ Base/Python/slicer/util.py
@@ -653,7 +653,7 @@ def saveNode(node, filename, properties={}):
   by specifying a 'fileType'attribute to the optional 'properties' dictionary.
   """
   from slicer import app
-  properties["nodeID"] = node.GetID();
+  properties["nodeID"] = node.GetID()
   properties["fileName"] = filename
   if hasattr(properties, "fileType"):
     filetype = properties["fileType"]
@@ -720,7 +720,7 @@ def moduleNames():
 
 def getModule(moduleName):
   from slicer import app
-  module = app.moduleManager().module(moduleName);
+  module = app.moduleManager().module(moduleName)
   if not module:
     import sys
     print("Could not find module with name '%s" % moduleName, file=sys.stderr)
