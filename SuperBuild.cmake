Slicer/Slicer

commit 39054218c3a6bffb0e0e9adffd4c7a515f2f08a2

ENH: Update BRAINSTools from v5.0.0 to v5.2.0
Summary:
* BRAINSTools v5.2.0 now uses C++11, and ITKv5.
* This version compiles and passes all Slicer regression tests.
* Several outdated programs have been removed from the build tree.

List of changes:

$ git shortlog d87ea8f35..da5d1e9da --no-merges
Ali Ghayoor (1):
      BUG: three landmarks are enough for rigid transformation in BRAINSLandmarkInitializer

Andras Lasso (1):
      COMP: Fix build errors with current ITK master

Hans J. Johnson (132):
      ENH: ARCHIVE BRAINSDemonWarp as not needed.
      COMP: Updated `enum` to `enum class` for c++11
      STYLE: Enforce ITK style defined by .clang-format
      COMP: ANTs git has was invalid pre-merge value
      ENH: ITKv5 changes for ResampleImage.
      ENH: Make verbosity of BRAINSFit much less by default
      COMP: BRAINSDemonWarp is in the ARCHIVE build
      STYLE: Use range-based loops from C++11
      PERF: Improve build performance on dashboards
      ENH: Diagnostics for cache.
      ENH: Improve script robustness by quoting variables
      ENH: Fix verbose messages identified incorrectly as errors by cdash.
      ENH: Update external project dependancies.
      ENH: Update to ITKv5.1rc0
      STYLE: Remove redundant code.
      DOC: Fix diagnostic error message.
      STYLE: Remove redundant void argument lists
      STYLE: Make prototype match definition names
      ENH: remove redundant control fllow
      STYLE: Use auto for variable type matches the type of the initializer expression
      PERF: readability container size empty
      STYLE: Use range-based loops from C++11
      STYLE: Assign initial values to variables
      STYLE: Pefer = default to explicitly trivial implementations
      STYLE: One declaration per line for readability
      PERF: Replace explicit return calls of constructor
      STYLE: Replace integer literals which are cast to bool.
      PERF: emplace_back method results in potentially more efficient code

SuperBuild.cmake
@@ -277,7 +277,6 @@ set(BRAINSTools_options
   USE_BRAINSROIAuto:BOOL=ON
   USE_BRAINSResample:BOOL=ON
   # BRAINSTools comes with some extra tool that should not be compiled by default
-  USE_BRAINSDemonWarp:BOOL=OFF
   USE_AutoWorkup:BOOL=OFF
   USE_ReferenceAtlas:BOOL=OFF
   USE_ANTS:BOOL=OFF
@@ -288,16 +287,13 @@ set(BRAINSTools_options
   USE_BRAINSMush:BOOL=OFF
   USE_BRAINSInitializedControlPoints:BOOL=OFF
   USE_BRAINSMultiModeSegment:BOOL=OFF
-  USE_BRAINSCut:BOOL=OFF
   USE_BRAINSLandmarkInitializer:BOOL=OFF
   USE_ImageCalculator:BOOL=OFF
-  USE_BRAINSSurfaceTools:BOOL=OFF
   USE_BRAINSSnapShotWriter:BOOL=OFF
   USE_ConvertBetweenFileFormats:BOOL=OFF
   USE_BRAINSMultiSTAPLE:BOOL=OFF
   USE_BRAINSCreateLabelMapFromProbabilityMaps:BOOL=OFF
   USE_BRAINSContinuousClass:BOOL=OFF
-  USE_ICCDEF:BOOL=OFF
   USE_BRAINSPosteriorToContinuousClass:BOOL=OFF
   USE_DebugImageViewer:BOOL=OFF
   BRAINS_DEBUG_IMAGE_WRITE:BOOL=OFF
@@ -308,7 +304,7 @@ set(BRAINSTools_options
 
 Slicer_Remote_Add(BRAINSTools
   GIT_REPOSITORY ${EP_GIT_PROTOCOL}://github.com/Slicer/BRAINSTools.git
-  GIT_TAG d87ea8f357622c27abc6a2162b4623abe782b995 # slicer-2020-01-08-d0e6c01
+  GIT_TAG da5d1e9dab95d786f503379bd1278f52c2fef39d # slicer-2020-04-24-v5.2.0
   LICENSE_FILES "http://www.apache.org/licenses/LICENSE-2.0.txt"
   OPTION_NAME Slicer_BUILD_BRAINSTOOLS
   OPTION_DEPENDS "Slicer_BUILD_CLI_SUPPORT;Slicer_BUILD_CLI"

commit 7d3442240b9583be32d44d9285d1ada55a5891bd

COMP: Update BRAINSTools and VTK to fix windows CMake configuration error
This commit fixes a regression introduced in 39054218c3 (ENH: Update
BRAINSTools from v5.0.0 to v5.2.0)


List of BRAINSTools changes:

$ git shortlog da5d1e9d..3fe08e96 --no-merges
Jean-Christophe Fillion-Robin (1):
      [backport BRAINSia/BRAINSTools#473] COMP: Fix windows compilation adding missing check_avx_flags function


List of VTK changes:

$ git shortlog 9853cac9f9..8b29c955d8 --no-merges
Jean-Christophe Fillion-Robin (2):
      [backport MR-6754] COMP: Rename CheckCXXSourceRuns CMake module bundled in VTK
      [backport MR-6754] COMP: Rename CHECK_CXX_SOURCE_RUNS to avoid conflict with CMake implementation

SuperBuild.cmake
@@ -304,7 +304,7 @@ set(BRAINSTools_options
 
 Slicer_Remote_Add(BRAINSTools
   GIT_REPOSITORY ${EP_GIT_PROTOCOL}://github.com/Slicer/BRAINSTools.git
-  GIT_TAG da5d1e9dab95d786f503379bd1278f52c2fef39d # slicer-2020-04-24-v5.2.0
+  GIT_TAG 3fe08e96be8967945cab79451ffb21c0c5a83bf8 # slicer-2020-04-24-v5.2.0
   LICENSE_FILES "http://www.apache.org/licenses/LICENSE-2.0.txt"
   OPTION_NAME Slicer_BUILD_BRAINSTOOLS
   OPTION_DEPENDS "Slicer_BUILD_CLI_SUPPORT;Slicer_BUILD_CLI"

SuperBuild/External_VTK.cmake
@@ -122,7 +122,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
 set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "9853cac9f934a6e68635c23b37d1c8cc74d51670") # slicer-v8.2.0-2020-03-16-16f7bbd
+  set(_git_tag "8b29c955d8b73cc26f6616ee734d8c501ff79dcd") # slicer-v8.2.0-2018-10-02-74d9488523
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()