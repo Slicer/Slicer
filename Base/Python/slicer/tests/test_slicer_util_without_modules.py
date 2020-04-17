b531f867e350e46ad06952d20dd0b7d37df62f86
James Butler <jamesobutler@users.noreply.github.com>
2020-04-17 11:31:38 -0400

    BUG: Fix failing slicer_util_without_modules test (#4815)
    The test was failing due to compute-checksum.txt line endings having
    changed from LF to CRLF during transition from SVN to Git
    
    Co-authored-by: James Butler <jbutler@sonovol.com>
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

--- Base/Python/slicer/tests/test_slicer_util_without_modules.py
+++ Base/Python/slicer/tests/test_slicer_util_without_modules.py
@@ -1,4 +1,6 @@
+import io
 import os
+import tempfile
 import unittest
 import slicer.util
 
@@ -12,12 +14,19 @@ class SlicerUtilWithoutModulesTest(unittest.TestCase):
     with self.assertRaises(IOError):
       slicer.util.computeChecksum('SHA256', 'compute-checksum-nonexistent.txt')
 
-    input_file = os.path.join(os.path.dirname(__file__), 'compute-checksum.txt')
-    self.assertEqual(slicer.util.computeChecksum('SHA256', input_file), '4a57f3207b97f26a6061f86948483c00b03893ddfef9e82b639ebe66e3aba338')
-    self.assertEqual(slicer.util.computeChecksum('SHA512', input_file), '5080ee92e951c5f8336053f11d278c23f5d26b5eb78805c952960eac0194f357f98b0e350611ce081d4a1e28dd8ea182d3a276c99b1752e0def2de0f47b8b27b')
+    with tempfile.TemporaryDirectory() as tmpdirname:
 
-    with self.assertRaises(ValueError):
-      slicer.util.computeChecksum('SHAINVALID', input_file)
+      input_file = os.path.join(tmpdirname, 'compute-checksum.txt')
+      with io.open(input_file, 'w', newline='\n') as content:
+        content.write('This is a text file!\n')
+
+      self.assertTrue(os.path.exists(input_file))
+
+      self.assertEqual(slicer.util.computeChecksum('SHA256', input_file), '4a57f3207b97f26a6061f86948483c00b03893ddfef9e82b639ebe66e3aba338')
+      self.assertEqual(slicer.util.computeChecksum('SHA512', input_file), '5080ee92e951c5f8336053f11d278c23f5d26b5eb78805c952960eac0194f357f98b0e350611ce081d4a1e28dd8ea182d3a276c99b1752e0def2de0f47b8b27b')
+
+      with self.assertRaises(ValueError):
+        slicer.util.computeChecksum('SHAINVALID', input_file)
 
   def test_extractAlgoAndDigest(self):
     with self.assertRaises(ValueError):
