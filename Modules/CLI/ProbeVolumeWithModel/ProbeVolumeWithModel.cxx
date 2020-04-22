0110c72e1ed13a438d897fc3d5ee7462c13c7f1d
Andras Lasso <lasso@queensu.ca>
2020-04-21 21:49:37 -0400

    BUG: Fix ProbeVolumeWithModel to work with all input mesh coordinate systems
    Use storage node for reading/writing mesh files to take into account coordinate system stored in the file.

--- Modules/CLI/ProbeVolumeWithModel/ProbeVolumeWithModel.cxx
+++ Modules/CLI/ProbeVolumeWithModel/ProbeVolumeWithModel.cxx
@@ -1,9 +1,6 @@
 
 #include "ProbeVolumeWithModelCLP.h"
 
-// vtkTeem includes
-#include <vtkTeemNRRDReader.h>
-
 // VTK includes
 #include <vtkImageData.h>
 #include <vtkNew.h>
@@ -13,137 +10,75 @@
 #include <vtkProbeFilter.h>
 #include <vtkTransform.h>
 #include <vtkTransformFilter.h>
-#include <vtkUnstructuredGridReader.h>
-#include <vtkUnstructuredGridWriter.h>
-#include <vtkXMLPolyDataReader.h>
-#include <vtkXMLPolyDataWriter.h>
-#include <vtkXMLUnstructuredGridReader.h>
-#include <vtkXMLUnstructuredGridWriter.h>
+#include <vtkImageChangeInformation.h>
+
+#include "vtkMRMLModelNode.h"
+#include "vtkMRMLModelStorageNode.h"
+
+#include "vtkITKArchetypeImageSeriesScalarReader.h"
 
 #include <vtksys/SystemTools.hxx>
 
-int main( int argc, char * argv[] )
+int main(int argc, char* argv[])
 {
-
   PARSE_ARGS;
 
-  vtkNew<vtkTeemNRRDReader> readerVol;
-  readerVol->SetFileName(InputVolume.c_str() );
+  // Read the file
+  vtkNew<vtkITKArchetypeImageSeriesScalarReader> readerVol;
+  readerVol->SetArchetype(InputVolume.c_str());
+  readerVol->SetOutputScalarTypeToNative();
+  readerVol->SetDesiredCoordinateOrientationToNative();
+  readerVol->SetUseNativeOriginOn();
   readerVol->Update();
-
-  vtkNew<vtkTransformFilter> transformer;
-
-  vtkNew<vtkXMLPolyDataReader> readerXMLPD;
-  readerXMLPD->SetFileName(InputModel.c_str());
-  vtkNew<vtkPolyDataReader> readerVTKPD;
-  readerVTKPD->SetFileName(InputModel.c_str());
-  vtkNew<vtkXMLUnstructuredGridReader> readerXMLUG;
-  readerXMLUG->SetFileName(InputModel.c_str());
-  vtkNew<vtkUnstructuredGridReader> readerVTKUG;
-  readerVTKUG->SetFileName(InputModel.c_str());
-
-  bool isPolyData = true; // polydata or unstructured grid
-
-  if (readerXMLPD->CanReadFile(InputModel.c_str()))
+  vtkImageData* volume = readerVol->GetOutput();
+  int* extent = volume->GetExtent();
+  if (extent[0]>extent[1] || extent[2] > extent[3] || extent[4] > extent[5])
     {
-    readerXMLPD->Update();
-    transformer->SetInputConnection(readerXMLPD->GetOutputPort());
+    std::cerr << "Input image file is empty: " << InputModel << std::endl;
+    return EXIT_FAILURE;
     }
-  else if (readerVTKPD->IsFileValid("polydata"))
+  std::cout << "Done reading the file " << InputVolume << endl;
+
+  vtkNew<vtkImageChangeInformation> ici;
+  ici->SetInputConnection(readerVol->GetOutputPort());
+  ici->SetOutputSpacing(1, 1, 1);
+  ici->SetOutputOrigin(0, 0, 0);
+  ici->Update();
+  vtkImageData* volume_Ijk = ici->GetOutput();
+
+  vtkNew<vtkMRMLModelStorageNode> modelStorageNode;
+  vtkNew<vtkMRMLModelNode> modelNode;
+  modelStorageNode->SetFileName(InputModel.c_str());
+  if (!modelStorageNode->ReadData(modelNode))
     {
-    readerVTKPD->Update();
-    transformer->SetInputConnection(readerVTKPD->GetOutputPort());
-    }
-  else if (readerXMLUG->CanReadFile(InputModel.c_str()))
-    {
-    readerXMLUG->Update();
-    transformer->SetInputConnection(readerXMLUG->GetOutputPort());
-    isPolyData = false;
-    }
-  else if (readerVTKUG->IsFileValid("unstructured_grid"))
-    {
-    readerVTKUG->Update();
-    transformer->SetInputConnection(readerVTKUG->GetOutputPort());
-    isPolyData = false;
-    }
-  else
-    {
-    std::cerr << "Failed to find suitable reader for " << InputModel.c_str() << std::endl;
-    return -1;
+    std::cerr << "Failed to read input model file " << InputModel << std::endl;
+    return EXIT_FAILURE;
     }
 
-  // Set up matrices to put model points into ijk space of volume
-  // This assumes points are in RAS space of volume (i.e. RAS==world)
-  vtkNew<vtkMatrix4x4> ROI_A_RASToIJK;
-  ROI_A_RASToIJK->DeepCopy(readerVol->GetRasToIjkMatrix());
-
-  // the volume we're probing knows its spacing so take this out of the matrix
-  double sp[3] = { 1.0, 1.0, 1.0 };
-  readerVol->GetOutput()->GetSpacing(sp);
-  vtkNew<vtkTransform> trans;
-  trans->Identity();
-  trans->PreMultiply();
-  trans->SetMatrix(ROI_A_RASToIJK.GetPointer());
-  // Trans from IJK to RAS
-  trans->Inverse();
-  // Take into account spacing to compute Scaled IJK
-  trans->Scale(1 / sp[0], 1 / sp[1], 1 / sp[2]);
-  // now it's RAS to scaled IJK
-  trans->Inverse();
-
-  transformer->SetTransform(trans.GetPointer());
-  transformer->Update();
+  // Transform the model into the volume's IJK space
+  vtkNew<vtkTransformFilter> modelTransformerRasToIjk;
+  vtkNew<vtkTransform> transformRasToIjk;
+  transformRasToIjk->SetMatrix(readerVol->GetRasToIjkMatrix());
+  modelTransformerRasToIjk->SetTransform(transformRasToIjk);
+  modelTransformerRasToIjk->SetInputConnection(modelNode->GetMeshConnection());
 
   vtkNew<vtkProbeFilter> probe;
-  probe->SetSourceConnection(readerVol->GetOutputPort());
-  probe->SetInputConnection( transformer->GetOutputPort() );
-  probe->Update();
+  probe->SetSourceData(volume_Ijk);
+  probe->SetInputConnection(modelTransformerRasToIjk->GetOutputPort());
 
-  // Transform the model back into original space
-  vtkNew<vtkTransform> trans2;
-  trans2->DeepCopy(trans.GetPointer());
-  trans2->Inverse();
-  vtkNew<vtkTransformFilter> transformer2;
-  transformer2->SetTransform(trans2.GetPointer());
-  transformer2->SetInputConnection(probe->GetOutputPort());
-  transformer2->Update();
+  // Transform the model back into RAS space
+  vtkNew<vtkTransformFilter> modelTransformerIjkToRas;
+  modelTransformerIjkToRas->SetTransform(transformRasToIjk->GetInverse());
+  modelTransformerIjkToRas->SetInputConnection(probe->GetOutputPort());
+  modelTransformerIjkToRas->Update();
 
   // Save the output
-  std::string ext = vtksys::SystemTools::LowerCase(vtksys::SystemTools::GetFilenameLastExtension(OutputModel));
-  bool isLegacyFileFormat = (ext.compare(".vtk") == 0);
-  if (isPolyData)
+  modelNode->SetAndObserveMesh(modelTransformerIjkToRas->GetOutput());
+  modelStorageNode->SetFileName(OutputModel.c_str());
+  if (!modelStorageNode->WriteData(modelNode))
     {
-    if (isLegacyFileFormat)
-      {
-      vtkNew<vtkPolyDataWriter> dataWriter;
-      dataWriter->SetFileName(OutputModel.c_str());
-      dataWriter->SetInputConnection(transformer2->GetOutputPort());
-      dataWriter->Write();
-      }
-    else
-      {
-      vtkNew<vtkXMLPolyDataWriter> xmlWriter;
-      xmlWriter->SetFileName(OutputModel.c_str());
-      xmlWriter->SetInputConnection(transformer2->GetOutputPort());
-      xmlWriter->Write();
-      }
-    }
-  else
-    {
-    if (isLegacyFileFormat)
-      {
-      vtkNew<vtkUnstructuredGridWriter> dataWriter;
-      dataWriter->SetFileName(OutputModel.c_str());
-      dataWriter->SetInputConnection(transformer2->GetOutputPort());
-      dataWriter->Write();
-      }
-    else
-      {
-      vtkNew<vtkXMLUnstructuredGridWriter> xmlWriter;
-      xmlWriter->SetFileName(OutputModel.c_str());
-      xmlWriter->SetInputConnection(transformer2->GetOutputPort());
-      xmlWriter->Write();
-      }
+    std::cerr << "Failed to write output model file " << OutputModel << std::endl;
+    return EXIT_FAILURE;
     }
 
   return EXIT_SUCCESS;
