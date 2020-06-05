Slicer/Slicer

commit 29b7e29742acc8fc227a3789c82d6ee764fdeb4f

COMP: Update VTK to remove vtk-m dependency
List of changes:

$ git shortlog a81d1af082..a44e00db0d --no-merges
Jean-Christophe Fillion-Robin (1):
      COMP: Remove vtk-m submodule

SuperBuild/External_VTK.cmake
@@ -126,7 +126,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
 set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "a81d1af0825b26946f05f0af9451e993489e801b") # slicer-v9.0.0-2020-05-05-987ae267
+  set(_git_tag "a44e00db0dd000d41268fa77c1d86d4d5977aeaa") # slicer-v9.0.0-2020-05-05-987ae267
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()

commit b0f6c2cd1582fa0685a4c26c471c0a113e839e8e

COMP: Fix clean build updating VTK and disabling vtkAcceleratorsVTKm module
This commit fixes a regression introduced in 29b7e29742 (COMP: Update VTK
to remove vtk-m dependency)

List of changes:

$ git shortlog a44e00db0d..a843afe3ac --no-merges
Jean-Christophe Fillion-Robin (1):
      COMP: Update vtkAcceleratorsVTKm to remove vtkVTKm dependency

Fixes #4964

SuperBuild/External_VTK.cmake
@@ -126,7 +126,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
 set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "a44e00db0dd000d41268fa77c1d86d4d5977aeaa") # slicer-v9.0.0-2020-05-05-987ae267
+  set(_git_tag "a843afe3ac8987d76444e0b4aa6632301ee1b303") # slicer-v8.2.0-2018-10-02-74d9488523
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()
@@ -173,6 +173,7 @@ endif()
       -DVTK_ENABLE_KITS:BOOL=ON
       -DVTK_RENDERING_BACKEND:STRING=${Slicer_VTK_RENDERING_BACKEND}
       -DVTK_SMP_IMPLEMENTATION_TYPE:STRING=${Slicer_VTK_SMP_IMPLEMENTATION_TYPE}
+      -DModule_vtkAcceleratorsVTKm:BOOL=OFF
       ${EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS}
     INSTALL_COMMAND ""
     DEPENDS