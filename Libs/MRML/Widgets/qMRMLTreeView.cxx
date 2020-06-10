Slicer/Slicer

commit 2f932d8d55abaf6282dda215313768e5097cd53b

ENH: Remove model hierarchy nodes
- Remove ModelHierarchy mode from qMRMLTreeView
- Remove ModelHierarchyDisplayNodeClassName concept from selection node
- Update segmentation import/export from/to model hierarchies to use folders instead where appropriate
  - Use subject hierarchy combobox instead of MRML node combobox in Import/export section in Segmentations module
  - Add option to filter for multiple levels in subject hierarchy
- Update qMRMLModelDisplayNodeWidget to support folders
  - Set SH item instead of "ModelOrHierarchyNode"
  - Use generic display node and only show model display node features if item of model node is set

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=28510
git-svn-id: http://svn.slicer.org/Slicer4/trunk@28510 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Widgets/qMRMLTreeView.cxx
@@ -33,7 +33,6 @@
 // qMRML includes
 #include "qMRMLItemDelegate.h"
 #include "qMRMLSceneDisplayableModel.h"
-#include "qMRMLSceneModelHierarchyModel.h"
 #include "qMRMLSceneTransformModel.h"
 #include "qMRMLSortFilterHierarchyProxyModel.h"
 #include "qMRMLTreeView_p.h"
@@ -42,8 +41,6 @@
 #include <vtkMRMLDisplayableHierarchyNode.h>
 #include <vtkMRMLDisplayableNode.h>
 #include <vtkMRMLDisplayNode.h>
-#include <vtkMRMLModelHierarchyLogic.h>
-#include <vtkMRMLModelHierarchyNode.h>
 #include <vtkMRMLModelNode.h>
 #include <vtkMRMLSelectionNode.h>
 #include <vtkMRMLScene.h>
@@ -353,11 +350,6 @@ void qMRMLTreeView::setSceneModelType(const QString& modelName)
     {
     newModel = new qMRMLSceneDisplayableModel(this);
     }
-  else if (modelName == QString("ModelHierarchy"))
-    {
-    newModel = new qMRMLSceneModelHierarchyModel(this);
-    newFilterModel = new qMRMLSortFilterHierarchyProxyModel(this);
-    }
   else if (modelName == QString(""))
     {
     newModel = new qMRMLSceneModel(this);
@@ -373,7 +365,6 @@ void qMRMLTreeView::setSceneModelType(const QString& modelName)
     newFilterModel->setShowHidden(this->showHidden());
     }
   d->setSceneModel(newModel);
-  // typically a no op except for ModelHierarchy
   d->setSortFilterProxyModel(newFilterModel);
 }
 
@@ -870,36 +861,10 @@ void qMRMLTreeView::toggleVisibility(const QModelIndex& index)
       {
       visibility = (hierDisplayNode->GetVisibility() ? 0 : 1);
       }
-    std::map<std::string, std::string> nodeTypes =  selectionNode->GetModelHierarchyDisplayNodeClassNames();
-    if (nodeTypes.empty())
-      {
-      vtkMRMLModelHierarchyLogic::SetChildrenVisibility(displayableHierarchyNode,
-                                                        nullptr, nullptr, visibility);
-      }
-    for (std::map<std::string, std::string>::iterator it = nodeTypes.begin();
-         it != nodeTypes.end(); it++)
-      {
-      std::string displayableType = it->first;
-      std::string displayType = it->second;
-      vtkMRMLModelHierarchyLogic::SetChildrenVisibility(displayableHierarchyNode,
-                                              displayableType.c_str(), displayType.c_str(), visibility);
-      }
     }
   else if (selectionNode && displayNode)
     {
     displayNode->SetVisibility(displayNode->GetVisibility() ? 0 : 1);
-    }
-  else if (selectionNode && displayableNode)
-    {
-    char *displayableType = (char *)node->GetClassName();
-    char *displayType = nullptr;
-    std::string ds = selectionNode->GetModelHierarchyDisplayNodeClassName(displayableType);
-    if (!ds.empty())
-      {
-      displayType = (char *)ds.c_str();
-      }
-    displayableNode->SetDisplayClassVisibility(displayType,
-            displayableNode->GetDisplayClassVisibility(displayType) ? 0 : 1);
     }
 }
 

Base/Logic/vtkSlicerApplicationLogic.cxx
@@ -18,16 +18,9 @@
 #ifdef Slicer_BUILD_CLI_SUPPORT
 # include <vtkMRMLCommandLineModuleNode.h>
 #endif
-#include <vtkMRMLDisplayNode.h>
-#include <vtkMRMLFreeSurferModelOverlayStorageNode.h>
-#include <vtkMRMLFreeSurferModelStorageNode.h>
-#include <vtkMRMLLabelMapVolumeNode.h>
-#include <vtkMRMLModelNode.h>
-#include <vtkMRMLModelHierarchyNode.h>
+#include <vtkMRMLRemoteIOLogic.h>
 #include <vtkMRMLScene.h>
 #include <vtkMRMLSelectionNode.h>
-#include <vtkMRMLTableNode.h>
-#include <vtkMRMLRemoteIOLogic.h>
 
 // VTKAddon includes
 #include <vtkPersonInformation.h>
@@ -35,8 +28,6 @@
 // VTK includes
 #include <vtkNew.h>
 #include <vtkObjectFactory.h>
-#include <vtkPointData.h>
-#include <vtkPolyData.h>
 
 // ITKSYS includes
 #include <itksys/SystemTools.hxx>
@@ -579,7 +570,6 @@ vtkMTimeType vtkSlicerApplicationLogic::RequestWriteData(const char *refNode, co
   this->WriteDataQueueLock.unlock();
   return uid;
 }
-
 
 //----------------------------------------------------------------------------
 vtkMTimeType vtkSlicerApplicationLogic::RequestReadScene(

Base/Logic/vtkSlicerApplicationLogicRequests.h
@@ -11,7 +11,13 @@
 #ifndef __vtkSlicerApplicationLogicRequests_h
 #define __vtkSlicerApplicationLogicRequests_h
 
+#include <vtkMRMLDisplayNode.h>
+#include <vtkMRMLLabelMapVolumeNode.h>
+#include <vtkMRMLModelHierarchyNode.h>
+#include <vtkMRMLModelNode.h>
+#include <vtkMRMLStorageNode.h>
 #include <vtkMRMLSubjectHierarchyNode.h>
+#include <vtkMRMLTableNode.h>
 
 //----------------------------------------------------------------------------
 class DataRequest

Base/QTCore/qSlicerSlicer2SceneReader.cxx
@@ -23,29 +23,24 @@
 #include <QDir>
 
 /// SlicerQt includes
-//#include "qSlicerAbstractModule.h"
 #include "qSlicerCoreApplication.h"
-//#include "qSlicerModuleManager.h"
 #include "qSlicerCoreIOManager.h"
 #include "qSlicerSlicer2SceneReader.h"
 #include "vtkSlicerApplicationLogic.h"
 
-/// Logic includes
-//#include "vtkSlicerVolumesLogic.h"
-//#include "vtkSlicerModelsLogic.h"
-
 /// MRML includes
 #include <vtkMRMLColorTableNode.h>
 #include <vtkMRMLFiducialListNode.h>
+#include <vtkMRMLFolderDisplayNode.h>
 #include <vtkMRMLLabelMapVolumeDisplayNode.h>
 #include <vtkMRMLLinearTransformNode.h>
 #include <vtkMRMLModelDisplayNode.h>
-#include <vtkMRMLModelHierarchyNode.h>
 #include <vtkMRMLModelNode.h>
 #include <vtkMRMLScene.h>
 #include <vtkMRMLScalarVolumeDisplayNode.h>
 #include <vtkMRMLScalarVolumeNode.h>
 #include <vtkMRMLSelectionNode.h>
+#include <vtkMRMLSubjectHierarchyNode.h>
 
 /// VTK includes
 #include <vtkImageReader.h>
@@ -126,24 +121,7 @@ int dataType(QString type)
 }
 
 }
-//-----------------------------------------------------------------------------
-/*
-vtkSlicerVolumesLogic* volumesLogic()
-{
-  return vtkSlicerVolumesLogic::SafeDownCast(
-    qSlicerCoreApplication::application()->moduleManager()
-    ->module("Volumes")->logic());
-}
 
-//-----------------------------------------------------------------------------
-vtkSlicerModelsLogic* modelsLogic()
-{
-  Q_ASSERT(qSlicerCoreApplication::application()->moduleManager()->module("Models"));
-  return vtkSlicerModelsLogic::SafeDownCast(
-    qSlicerCoreApplication::application()->moduleManager()
-    ->module("Models")->logic());
-}
-*/
 //-----------------------------------------------------------------------------
 class qSlicerSlicer2SceneReaderPrivate
 {
@@ -971,12 +949,11 @@ void qSlicerSlicer2SceneReaderPrivate::importModelGroupNode(NodeType& node)
   //set dnode [vtkMRMLModelDisplayNode New]
   //$hnode SetScene $::slicer3::MRMLScene
   //$dnode SetScene $::slicer3::MRMLScene
-  vtkSmartPointer<vtkMRMLModelHierarchyNode> hnode =
-    vtkSmartPointer<vtkMRMLModelHierarchyNode>::New();
-  vtkSmartPointer<vtkMRMLModelDisplayNode> dnode =
-    vtkSmartPointer<vtkMRMLModelDisplayNode>::New();
-  hnode->SetScene(q->mrmlScene());
+  // Folder display node belongs to the folder item, which replaced the model hierarchy
+  vtkSmartPointer<vtkMRMLFolderDisplayNode> dnode =
+    vtkSmartPointer<vtkMRMLFolderDisplayNode>::New();
   dnode->SetScene(q->mrmlScene());
+  dnode->SetHideFromEditors(0); // Need to set this so that the folder shows up in SH as folder
 
   // if { [info exists n(visibility)] } {
   //   if {$n(visibility) == "false"} {
@@ -996,7 +973,7 @@ void qSlicerSlicer2SceneReaderPrivate::importModelGroupNode(NodeType& node)
   //}
   if (node.contains("name"))
     {
-    hnode->SetName(node["name"].toLatin1());
+    dnode->SetName(node["name"].toLatin1());
     }
 
   // if { [info exists n(color)] } {
@@ -1023,31 +1000,37 @@ void qSlicerSlicer2SceneReaderPrivate::importModelGroupNode(NodeType& node)
     }
   //set dnode [$::slicer3::MRMLScene AddNode $dnode]
   //set hnode [$::slicer3::MRMLScene AddNode $hnode]
-  dnode = vtkMRMLModelDisplayNode::SafeDownCast(
+  dnode = vtkMRMLFolderDisplayNode::SafeDownCast(
     q->mrmlScene()->AddNode(dnode));
   Q_ASSERT(dnode);
-  hnode = vtkMRMLModelHierarchyNode::SafeDownCast(
-    q->mrmlScene()->AddNode(hnode));
-  Q_ASSERT(hnode);
-  this->LoadedNodes << dnode->GetID() << hnode->GetID();
+  this->LoadedNodes << dnode->GetID();
 
   //if {$::S2_HParent_ID != ""} {
   //  $hnode SetParentNodeIDReference $::S2_HParent_ID
   //}
   if (!this->ParentID.isEmpty())
     {
-    hnode->SetParentNodeID(this->ParentID.toLatin1());
+    vtkMRMLSubjectHierarchyNode* shNode = vtkMRMLSubjectHierarchyNode::GetSubjectHierarchyNode(q->mrmlScene());
+    if (!shNode)
+      {
+      qCritical() << Q_FUNC_INFO << "Unable to access subject hierarchy node";
+      return;
+      }
+
+    vtkMRMLNode* parentNode = q->mrmlScene()->GetNodeByID(this->ParentID.toLatin1());
+    if (parentNode)
+      {
+      vtkIdType parentItemId = shNode->GetItemByDataNode(parentNode);
+      vtkIdType folderItemId = shNode->GetItemByDataNode(dnode);
+      if (parentItemId && folderItemId)
+        {
+        shNode->SetItemParent(folderItemId, parentItemId);
+        }
+      }
     }
 
-  //$hnode SetAndObserveDisplayNodeID [$dnode GetID]
-  //$hnode SetHideFromEditors 0
-  //$hnode SetSelectable 1
-  hnode->SetAndObserveDisplayNodeID(dnode->GetID());
-  hnode->SetHideFromEditors(0);
-  hnode->SetSelectable(1);
-
   //set ::S2_HParent_ID [$hnode GetID]
-  this->ParentID = hnode->GetID();
+  this->ParentID = dnode->GetID();
 }
 
 //-----------------------------------------------------------------------------
@@ -1058,12 +1041,9 @@ void qSlicerSlicer2SceneReaderPrivate::importModelRefNode(NodeType& node)
   //upvar $node n
   //set hnode [vtkMRMLModelHierarchyNode New]
   //$hnode SetScene $::slicer3::MRMLScene
-  vtkSmartPointer<vtkMRMLModelHierarchyNode> hnode =
-    vtkSmartPointer<vtkMRMLModelHierarchyNode>::New();
-  hnode->SetScene(q->mrmlScene());
 
-  //$hnode SetExpanded 1
-  hnode->SetExpanded(1);
+  // Important! Leaf hierarchy nodes are not needed any more.
+  //   Simply set parent folder to the model node in subject hierarchy
 
   //set id2 $n(ModelRefID)
   //set id3 $::S2_Model_ID($id2)
@@ -1071,28 +1051,36 @@ void qSlicerSlicer2SceneReaderPrivate::importModelRefNode(NodeType& node)
   QString id3 = this->ModelIDs[id2];
 
   //$hnode SetName [[$::slicer3::MRMLScene GetNodeByID $id3] GetName]
-  Q_ASSERT(q->mrmlScene()->GetNodeByID(id3.toLatin1()));
-  hnode->SetName(q->mrmlScene()->GetNodeByID(id3.toLatin1())->GetName());
-
-  //set hnode [$::slicer3::MRMLScene AddNode $hnode]
-  hnode = vtkMRMLModelHierarchyNode::SafeDownCast(
-    q->mrmlScene()->AddNode(hnode));
-  this->LoadedNodes << hnode->GetID();
+  vtkMRMLNode* modelNode = q->mrmlScene()->GetNodeByID(id3.toLatin1());
+  if (!modelNode)
+    {
+    qCritical() << Q_FUNC_INFO << "Unable to access referenced model node";
+    return;
+    }
 
   //if {$::S2_HParent_ID != ""} {
   //  $hnode SetParentNodeIDReference $::S2_HParent_ID
   //}
   if (!this->ParentID.isEmpty())
     {
-    hnode->SetParentNodeID(this->ParentID.toLatin1());
-    }
-  //$hnode SetModelNodeIDReference $id3
-  hnode->SetModelNodeIDReference(id3.toLatin1());
+    vtkMRMLSubjectHierarchyNode* shNode = vtkMRMLSubjectHierarchyNode::GetSubjectHierarchyNode(q->mrmlScene());
+    if (!shNode)
+      {
+      qCritical() << Q_FUNC_INFO << "Unable to access subject hierarchy node";
+      return;
+      }
 
-  //$hnode SetHideFromEditors 1
-  //$hnode SetSelectable 0
-  hnode->SetHideFromEditors(1);
-  hnode->SetSelectable(0);
+    vtkMRMLNode* parentNode = q->mrmlScene()->GetNodeByID(this->ParentID.toLatin1());
+    if (parentNode)
+      {
+      vtkIdType parentItemId = shNode->GetItemByDataNode(parentNode);
+      vtkIdType modelItemId = shNode->GetItemByDataNode(modelNode);
+      if (parentItemId && modelItemId)
+        {
+        shNode->SetItemParent(modelItemId, parentItemId);
+        }
+      }
+    }
 }
 
 //-----------------------------------------------------------------------------

Libs/MRML/Core/vtkMRMLFolderDisplayNode.cxx
@@ -103,10 +103,7 @@ vtkMRMLDisplayNode* vtkMRMLFolderDisplayNode::GetOverridingHierarchyDisplayNode(
   vtkIdType nodeShId = shNode->GetItemByDataNode(node);
   if (!nodeShId)
     {
-    if (!node->GetHideFromEditors())
-      {
-      vtkErrorWithObjectMacro(node, "GetOverridingHierarchyDisplayNode: Failed to get subject hierarchy item for node " << node->GetName());
-      }
+    // May happen if an AddNode event is caught before SH had the chance to add the item
     return nullptr;
     }
 

Libs/MRML/Core/vtkMRMLSelectionNode.cxx
@@ -157,27 +157,6 @@ void vtkMRMLSelectionNode::WriteXML(ostream& of, int nIndent)
   of << " activeViewID=\"" << (this->ActiveViewID ? this->ActiveViewID : "NULL") << "\"";
   of << " activeLayoutID=\"" << (this->ActiveLayoutID ? this->ActiveLayoutID : "NULL") << "\"";
   of << " activePlotChartID=\"" << (this->ActivePlotChartID ? this->ActivePlotChartID : "NULL") << "\"";
-
-  if (this->ModelHierarchyDisplayNodeClassName.size() > 0)
-    {
-    of << " modelHierarchyDisplayableNodeClassName=\"";
-
-    for (std::map<std::string, std::string>::const_iterator it = this->ModelHierarchyDisplayNodeClassName.begin();
-         it != this->ModelHierarchyDisplayNodeClassName.end(); it++)
-      {
-      of << " " << it->first;
-      }
-    of << "\"";
-
-    of << " modelHierarchyDisplayNodeClassName=\"";
-
-    for (std::map<std::string, std::string>::const_iterator it = this->ModelHierarchyDisplayNodeClassName.begin();
-         it != this->ModelHierarchyDisplayNodeClassName.end(); it++)
-      {
-      of << " " << it->second;
-      }
-    of << "\"";
-    }
 }
 
 //----------------------------------------------------------------------------
@@ -294,9 +273,6 @@ void vtkMRMLSelectionNode::ReadXMLAttributes(const char** atts)
 
   this->Superclass::ReadXMLAttributes(atts);
 
-  std::vector<std::string> modelHierarchyDisplayableNodeClassName;
-  std::vector<std::string> modelHierarchyDisplayNodeClassName;
-
   const char* attName;
   const char* attValue;
   while (*atts != nullptr)
@@ -357,47 +333,6 @@ void vtkMRMLSelectionNode::ReadXMLAttributes(const char** atts)
       this->SetActivePlotChartID (attValue);
       //this->Scene->AddReferencedNodeID ( this->ActivePlotChartID, this);
       }
-    if (!strcmp (attName, "modelHierarchyDisplayableNodeClassName"))
-      {
-      std::stringstream ss(attValue);
-      while (!ss.eof())
-        {
-        std::string name;
-        ss >> name;
-        if (!name.empty())
-          {
-          modelHierarchyDisplayableNodeClassName.push_back(name);
-          }
-        }
-      }
-    if (!strcmp (attName, "modelHierarchyDisplayNodeClassName"))
-      {
-      std::stringstream ss(attValue);
-      while (!ss.eof())
-        {
-        std::string name;
-        ss >> name;
-        if (!name.empty())
-          {
-          modelHierarchyDisplayNodeClassName.push_back(name);
-          }
-        }
-      }
-    }
-
-  if (modelHierarchyDisplayableNodeClassName.size() != modelHierarchyDisplayNodeClassName.size() )
-    {
-    vtkErrorMacro(<< "ReadXMLAttributes: Number of modelHierarchyDisplayableNodeClassName is not equal to modelHierarchyDisplayNodeClassName.");
-    }
-  else
-    {
-    std::vector<std::string>::const_iterator it1 = modelHierarchyDisplayableNodeClassName.begin();
-    for (std::vector<std::string>::const_iterator it = modelHierarchyDisplayNodeClassName.begin();
-        it != modelHierarchyDisplayNodeClassName.end(); it++)
-      {
-      this->AddModelHierarchyDisplayNodeClassName(*it1, *it);
-      it1++;
-      }
     }
 
   this->EndModify(disabledModify);
@@ -424,7 +359,6 @@ void vtkMRMLSelectionNode::Copy(vtkMRMLNode *anode)
   this->SetActiveViewID (node->GetActiveViewID() );
   this->SetActiveLayoutID (node->GetActiveLayoutID() );
   this->SetActivePlotChartID (node->GetActivePlotChartID());
-  this->ModelHierarchyDisplayNodeClassName = node->ModelHierarchyDisplayNodeClassName;
   this->EndModify(disabledModify);
 }
 
@@ -468,16 +402,6 @@ void vtkMRMLSelectionNode::PrintSelf(ostream& os, vtkIndent indent)
   os << "ActiveViewID: " << ( (this->ActiveViewID) ? this->ActiveViewID : "None" ) << "\n";
   os << "ActiveLayoutID: " << ( (this->ActiveLayoutID) ? this->ActiveLayoutID : "None" ) << "\n";
   os << "ActivePlotChartID: " << ( (this->ActivePlotChartID) ? this->ActivePlotChartID : "None" ) << "\n";
-
-  if (this->ModelHierarchyDisplayNodeClassName.size() > 0)
-    {
-    os << "Mode lHierarchy Display Node Class Name: \n";
-    for (std::map<std::string, std::string>::const_iterator it = this->ModelHierarchyDisplayNodeClassName.begin();
-         it != this->ModelHierarchyDisplayNodeClassName.end(); it++)
-      {
-      os << indent.GetNextIndent() << it->first << " " << it->second << "\n";
-      }
-    }
 }
 
 //----------------------------------------------------------------------------
@@ -731,32 +655,4 @@ void vtkMRMLSelectionNode::SetReferenceActivePlaceNodeClassName (const char *cla
 {
   this->SetActivePlaceNodeClassName(className);
   this->InvokeEvent(vtkMRMLSelectionNode::ActivePlaceNodeClassNameChangedEvent);
-}
-
-//----------------------------------------------------------------------------
-std::string
-vtkMRMLSelectionNode::GetModelHierarchyDisplayNodeClassName(const std::string& dispayableNodeClass)const
-{
-  std::map<std::string, std::string>::const_iterator it = this->ModelHierarchyDisplayNodeClassName.find(dispayableNodeClass);
-  return (it == this->ModelHierarchyDisplayNodeClassName.end() ? std::string() : it->second);
-}
-
-//----------------------------------------------------------------------------
-std::map<std::string, std::string>
-vtkMRMLSelectionNode::GetModelHierarchyDisplayNodeClassNames()const
-{
-  return this->ModelHierarchyDisplayNodeClassName;
-}
-
-//----------------------------------------------------------------------------
-void vtkMRMLSelectionNode::AddModelHierarchyDisplayNodeClassName(const std::string& dispayableNodeClass,
-                                                                 const std::string& displayNodeClass)
-{
-  this->ModelHierarchyDisplayNodeClassName[dispayableNodeClass] = displayNodeClass;
-}
-
-//----------------------------------------------------------------------------
-void vtkMRMLSelectionNode::ClearModelHierarchyDisplayNodeClassNames()
-{
-  this->ModelHierarchyDisplayNodeClassName.clear();
 }

Libs/MRML/Core/vtkMRMLSelectionNode.h
@@ -238,27 +238,6 @@ class VTK_MRML_EXPORT vtkMRMLSelectionNode : public vtkMRMLNode
   /// Get the number of class names in the list
   int GetNumberOfPlaceNodeClassNamesInList() { return static_cast<int>(this->PlaceNodeClassNameList.size()); };
 
-  /// Get node display class name for a displayable class name
-  /// to which display node to apply properties in hierarchy display widgets
-  std::string GetModelHierarchyDisplayNodeClassName(const std::string& dispayableNodeClass)const;
-
-  /// Get node display class names
-  /// to which display node to apply properties in hierarchy display widgets
-  std::map<std::string, std::string>  GetModelHierarchyDisplayNodeClassNames()const;
-
-  /// Add display node class for a displayable class.
-  /// Controls which display node to apply properties in hierarchy display widgets
-  /// affected by setting visibility and other properties.
-  /// This mostly applies to displayable nodes that have multiple display nodes
-  /// For example, parameters of "vtkMRMLFiberbundleNode" , "vtkMRMLFiberbundleTubeDisplayNode"
-  /// will change tube visibility
-  void AddModelHierarchyDisplayNodeClassName(const std::string& dispayableNodeClass,
-                                             const std::string& displayNodeClass);
-
-  /// Clear node display class names
-  /// to which display node to apply properties in hierarchy display widgets
-   void ClearModelHierarchyDisplayNodeClassNames();
-
 protected:
   vtkMRMLSelectionNode();
   ~vtkMRMLSelectionNode() override;
@@ -283,10 +262,6 @@ protected:
   char *ActiveViewID;
   char *ActiveLayoutID;
   char *ActivePlotChartID;
-
-  /// displayable/Display node class pairs for contolling
-  /// to which display node to apply properties in hierarchy display widgets
-  std::map<std::string, std::string> ModelHierarchyDisplayNodeClassName;
 
   std::vector<std::string> PlaceNodeClassNameList;
   std::vector<std::string> PlaceNodeResourceList;

Libs/MRML/Core/vtkMRMLSubjectHierarchyNode.cxx
@@ -2043,6 +2043,9 @@ void vtkMRMLSubjectHierarchyNode::SetItemDataNode(vtkIdType itemID, vtkMRMLNode*
     }
 
   item->DataNode = dataNode;
+
+  // Add observers for data node
+  this->Internal->AddItemObservers(item);
 }
 
 //----------------------------------------------------------------------------

commit cc99543e3df21bb7af7bf223d9e97c597a55687d

BUG: Update qMRMLTreeView to fix visibility toggle
This commit fixes a regression introduced in 2f932d8 / r28510 (ENH: Remove
model hierarchy nodes) and ensures visibility of both display and displayable
nodes can be toggled using a qMRMLTreeView associated with a "Displayable"
scene model (qMRMLSceneDisplayableModel)

Libs/MRML/Widgets/qMRMLTreeView.cxx
@@ -827,8 +827,13 @@ bool qMRMLTreeView::clickDecoration(const QModelIndex& index)
 void qMRMLTreeView::toggleVisibility(const QModelIndex& index)
 {
   vtkMRMLNode* node = this->sortFilterProxyModel()->mrmlNodeFromIndex(index);
-  vtkMRMLDisplayNode* displayNode =
-    vtkMRMLDisplayNode::SafeDownCast(node);
+  vtkMRMLDisplayNode* displayNode = vtkMRMLDisplayNode::SafeDownCast(node);
+  vtkMRMLDisplayableNode* displayableNode = vtkMRMLDisplayableNode::SafeDownCast(node);
+
+  if (displayableNode && displayableNode->GetDisplayNode())
+    {
+    displayNode = displayableNode->GetDisplayNode();
+    }
 
   vtkMRMLSelectionNode* selectionNode = vtkMRMLSelectionNode::SafeDownCast(
     this->mrmlScene()->GetNodeByID("vtkMRMLSelectionNodeSingleton"));