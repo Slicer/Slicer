Slicer/Slicer

commit 128fb86dd18040d9f7871d2276346e093d3248ae

ENH: Update curl from 7.34 to 7.70
This change is needed to support building against OpenSSL 1.1.1g

* Remove obsolete options BUILD_CURL_TESTS and BUILD_DASHBOARD_REPORTS
  and use BUILD_TESTING introduced in curl/curl@12e21fa (cmake: now
  using BUILD_TESTING=ON/OFF)

* Use ENABLE_ARES instead of CURL_USE_ARES. Option has been
  renamed in curl/curl@17d2780 (cmake: add ENABLE_THREADED_RESOLVER,
  rename ARES)

* Set BUILD_SHARED_LIBS instead of CURL_STATICLIB removed
  in curl/curl@c892795 (CMake: Respect BUILD_SHARED_LIBS)

* Set HAVE_LIBIDN2 instead of HAVE_LIBIDN that was removed
  in curl/curl@9c91ec7 (idn: switch to libidn2 use and
  IDNA2008 support)

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

SuperBuild/External_curl.cmake
@@ -44,6 +44,7 @@ if((NOT DEFINED CURL_INCLUDE_DIR
     endif()
   endif()
 
+
   set(${proj}_CMAKE_C_FLAGS ${ep_common_c_flags})
   if(CMAKE_SIZEOF_VOID_P EQUAL 8) # 64-bit
     set(${proj}_CMAKE_C_FLAGS "${ep_common_c_flags} -fPIC")
@@ -57,7 +58,7 @@ if((NOT DEFINED CURL_INCLUDE_DIR
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "c5e0ea129b9d187214156f0f704e43ef45ff1aec" # curl-7_34_0-maint
+    "ca5fe8e63df7faea0bfb988ef3fe58f538e6950b" # slicer-7.70.0-2020-04-29-53cdc2c
     QUIET
     )
 
@@ -84,11 +85,10 @@ if((NOT DEFINED CURL_INCLUDE_DIR
       -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
       -DCMAKE_C_FLAGS:STRING=${${proj}_CMAKE_C_FLAGS}
       -DCMAKE_INSTALL_PREFIX:PATH=${EP_INSTALL_DIR}
-      -DBUILD_CURL_TESTS:BOOL=OFF # BUILD_TESTING is not used
+      -DBUILD_TESTING:BOOL=OFF
       -DBUILD_CURL_EXE:BOOL=OFF
-      -DBUILD_DASHBOARD_REPORTS:BOOL=OFF
-      -DCURL_STATICLIB:BOOL=ON
-      -DCURL_USE_ARES:BOOL=OFF
+      -DBUILD_SHARED_LIBS:BOOL=OFF  # Before enabling this option, see https://github.com/Slicer/curl/commit/ca5fe8e63df7faea0bfb988ef3fe58f538e6950b
+      -DENABLE_ARES:BOOL=OFF
       -DCURL_ZLIB:BOOL=ON
       -DZLIB_INCLUDE_DIR:PATH=${ZLIB_INCLUDE_DIR}
       -DZLIB_LIBRARY:FILEPATH=${ZLIB_LIBRARY}
@@ -99,7 +99,7 @@ if((NOT DEFINED CURL_INCLUDE_DIR
       -DCURL_DISABLE_DICT:BOOL=ON
       -DCURL_DISABLE_FILE:BOOL=ON
       -DCURL_DISABLE_TFTP:BOOL=ON
-      -DHAVE_LIBIDN:BOOL=FALSE
+      -DHAVE_LIBIDN2:BOOL=FALSE
       -DCMAKE_USE_OPENSSL:BOOL=${CURL_ENABLE_SSL}
     ${EXTERNAL_PROJECT_OPTIONAL_CMAKE_ARGS}
     DEPENDS

commit e693f4f69fe12c55cd0dd56c93d7fc444c9cd8bb

COMP: Fix linux build ensuring curl library is install in "lib" directory
This commit fixes the following error:

  error: '/work/Preview/Slicer-0-build/curl-install/lib/libcurl.a', needed by 'bin/libRemoteIO.so', missing and no known rule to make it

It fixes a regression originally introduced in 128fb86dd1 (ENH: Update
curl from 7.34 to 7.70)

See #5000 and #4992

SuperBuild/External_curl.cmake
@@ -86,6 +86,7 @@ if((NOT DEFINED CURL_INCLUDE_DIR
       -DCMAKE_C_FLAGS:STRING=${${proj}_CMAKE_C_FLAGS}
       -DCMAKE_DEBUG_POSTFIX:STRING=
       -DCMAKE_INSTALL_PREFIX:PATH=${EP_INSTALL_DIR}
+      -DCMAKE_INSTALL_LIBDIR:STRING=lib  # Override value set in GNUInstallDirs CMake module
       -DBUILD_TESTING:BOOL=OFF
       -DBUILD_CURL_EXE:BOOL=OFF
       -DBUILD_SHARED_LIBS:BOOL=OFF  # Before enabling this option, see https://github.com/Slicer/curl/commit/ca5fe8e63df7faea0bfb988ef3fe58f538e6950b