263be18f0bdf9044ea3519269c6806841177f3fa
Andras Lasso <lasso@queensu.ca>
2020-06-29 21:35:26 -0400

    BUG: Remove .vti file extension from volume reader
    .vti (VTK polydata image file) is actually not supported by the reader, so it should not be listed.

--- Modules/Loadable/Volumes/qSlicerVolumesReader.cxx
+++ Modules/Loadable/Volumes/qSlicerVolumesReader.cxx
@@ -103,7 +103,7 @@ QStringList qSlicerVolumesReader::extensions()const
 {
   // pic files are bio-rad images (see itkBioRadImageIO)
   return QStringList()
-    << "Volume (*.hdr *.nhdr *.nrrd *.mhd *.mha *.mnc *.vti *.nii *.nii.gz *.mgh *.mgz *.mgh.gz *.img *.img.gz *.pic)"
+    << "Volume (*.hdr *.nhdr *.nrrd *.mhd *.mha *.mnc *.nii *.nii.gz *.mgh *.mgz *.mgh.gz *.img *.img.gz *.pic)"
     << "Dicom (*.dcm *.ima)"
     << "Image (*.png *.tif *.tiff *.jpg *.jpeg)"
     << "All Files (*)";
