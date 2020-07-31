Slicer/Slicer

commit b30151532b1a5c3f51065bff436b81042d2a2351

COMP: fix signed/unsigned comparison introduced by pull request 5045.

Libs/MRML/Core/vtkMRMLMessageCollection.cxx
@@ -63,9 +63,12 @@ vtkMRMLMessageCollection
 ::GetNumberOfMessagesOfType(unsigned long messageType) const
 {
   int response = 0;
-  for (int i = 0; i < this->Messages.size(); ++i)
+  for (int i = 0; i < static_cast<int>(this->Messages.size()); ++i)
     {
-    response += static_cast<int>(GetNthMessageType(i) == messageType);
+    if (this->GetNthMessageType(i) == messageType)
+      {
+      ++response;
+      }
     }
   return response;
 }

commit 9e24e9cc1da588a378205362b14fdb596d541c3c

COMP: Fix -Wsign-comp warning in vtkMRMLMessageCollection
This commit fixes a regression introduced in b30151532 (ENH: Display warnings
and errors in the Save Data dialog.)

Libs/MRML/Core/vtkMRMLMessageCollection.cxx
@@ -63,9 +63,12 @@ vtkMRMLMessageCollection
 ::GetNumberOfMessagesOfType(unsigned long messageType) const
 {
   int response = 0;
-  for (int i = 0; i < this->Messages.size(); ++i)
+  for (int i = 0; i < static_cast<int>(this->Messages.size()); ++i)
     {
-    response += static_cast<int>(GetNthMessageType(i) == messageType);
+    if (this->GetNthMessageType(i) == messageType)
+      {
+      ++response;
+      }
     }
   return response;
 }