8fd4205aca7332a660a81117bf94669b5b293988
James Butler <jbutler@sonovol.com>
2020-08-15 18:00:51 -0400

    COMP: Update TBB to 2019_U9

--- SuperBuild/External_tbb.cmake
+++ SuperBuild/External_tbb.cmake
@@ -5,16 +5,16 @@ if(Slicer_USE_SYSTEM_${proj})
   message(FATAL_ERROR "Enabling Slicer_USE_SYSTEM_${proj} is not supported!")
 endif()
 
-set(tbb_ver "2018_20171205oss")
+set(tbb_ver "2019_20191006oss")
 if (WIN32)
   set(tbb_file "tbb${tbb_ver}_win.zip")
-  set(tbb_md5 "e37abf02d74a638f7a6629c992f23918")
+  set(tbb_md5 "a061a7c9821a374023201e8592860730")
 elseif (APPLE)
   set(tbb_file "tbb${tbb_ver}_mac.tgz")
-  set(tbb_md5 "ff7a02f58fee4e2e637db6da19a21806")
+  set(tbb_md5 "43a0d6409317ee94f047622fd489a6c8")
 else ()
   set(tbb_file "tbb${tbb_ver}_lin.tgz")
-  set(tbb_md5 "d637d29f59ee31fe5830a0366e2e973a")
+  set(tbb_md5 "b5025847fa47040b4d2da8d6bdba0224")
 endif ()
 
 #------------------------------------------------------------------------------
@@ -23,7 +23,7 @@ ExternalProject_Message(${proj} "TBB_INSTALL_DIR:${TBB_INSTALL_DIR}")
 
 ExternalProject_Add(${proj}
   ${${proj}_EP_ARGS}
-  URL https://github.com/Slicer/SlicerBinaryDependencies/releases/download/tbb/${tbb_file}
+  URL https://github.com/oneapi-src/oneTBB/releases/download/2019_U9/${tbb_file}
   URL_MD5 ${tbb_md5}
   DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
   SOURCE_DIR ${TBB_INSTALL_DIR}
@@ -40,14 +40,8 @@ else()
 endif()
 
 if (WIN32)
-  if (NOT MSVC_VERSION VERSION_GREATER 1500)
-    message(FATAL_ERROR "At least Visual Studio 10.0 is required")
-  elseif (NOT MSVC_VERSION VERSION_GREATER 1600)
-    set(tbb_vsdir vc10)
-  elseif (NOT MSVC_VERSION VERSION_GREATER 1700)
-    set(tbb_vsdir vc11)
-  elseif (NOT MSVC_VERSION VERSION_GREATER 1800)
-    set(tbb_vsdir vc12)
+  if (NOT MSVC_VERSION VERSION_GREATER_EQUAL 1900)
+    message(FATAL_ERROR "At least Visual Studio 2015 is required")
   elseif (NOT MSVC_VERSION VERSION_GREATER 1900)
     set(tbb_vsdir vc14)
   elseif (NOT MSVC_VERSION VERSION_GREATER 1919)
@@ -91,20 +85,20 @@ endif ()
 
 #------------------------------------------------------------------------------
 
-set(TBB_INCLUDE_DIR "${TBB_INSTALL_DIR}/include")
-set(TBB_LIBRARY_DEBUG "${TBB_INSTALL_DIR}/${tbb_libdir}/${tbb_lib_prefix}tbb_debug${tbb_libsuffix}")
-set(TBB_LIBRARY_RELEASE "${TBB_INSTALL_DIR}/${tbb_libdir}/${tbb_lib_prefix}tbb${tbb_libsuffix}")
+set(TBB_INCLUDE_DIR "${TBB_INSTALL_DIR}/tbb${tbb_ver}/include")
+set(TBB_LIBRARY_DEBUG "${TBB_INSTALL_DIR}/tbb${tbb_ver}/${tbb_libdir}/${tbb_lib_prefix}tbb_debug${tbb_libsuffix}")
+set(TBB_LIBRARY_RELEASE "${TBB_INSTALL_DIR}/tbb${tbb_ver}/${tbb_libdir}/${tbb_lib_prefix}tbb${tbb_libsuffix}")
 
-set(TBB_MALLOC_INCLUDE_DIR "${TBB_INSTALL_DIR}/include/tbb")
-set(TBB_MALLOC_LIBRARY_DEBUG "${TBB_INSTALL_DIR}/${tbb_libdir}/${tbb_lib_prefix}tbbmalloc_debug${tbb_libsuffix}")
-set(TBB_MALLOC_LIBRARY_RELEASE "${TBB_INSTALL_DIR}/${tbb_libdir}/${tbb_lib_prefix}tbbmalloc${tbb_libsuffix}")
+set(TBB_MALLOC_INCLUDE_DIR "${TBB_INSTALL_DIR}/tbb${tbb_ver}/include/tbb")
+set(TBB_MALLOC_LIBRARY_DEBUG "${TBB_INSTALL_DIR}/tbb${tbb_ver}/${tbb_libdir}/${tbb_lib_prefix}tbbmalloc_debug${tbb_libsuffix}")
+set(TBB_MALLOC_LIBRARY_RELEASE "${TBB_INSTALL_DIR}/tbb${tbb_ver}/${tbb_libdir}/${tbb_lib_prefix}tbbmalloc${tbb_libsuffix}")
 
-set(TBB_MALLOC_PROXY_INCLUDE_DIR "${TBB_INSTALL_DIR}/include/tbb")
-set(TBB_MALLOC_PROXY_LIBRARY_DEBUG "${TBB_INSTALL_DIR}/${tbb_libdir}/${tbb_lib_prefix}tbbmalloc_proxy_debug${tbb_libsuffix}")
-set(TBB_MALLOC_PROXY_LIBRARY_RELEASE "${TBB_INSTALL_DIR}/${tbb_libdir}/${tbb_lib_prefix}tbbmalloc_proxy${tbb_libsuffix}")
+set(TBB_MALLOC_PROXY_INCLUDE_DIR "${TBB_INSTALL_DIR}/tbb${tbb_ver}/include/tbb")
+set(TBB_MALLOC_PROXY_LIBRARY_DEBUG "${TBB_INSTALL_DIR}/tbb${tbb_ver}/${tbb_libdir}/${tbb_lib_prefix}tbbmalloc_proxy_debug${tbb_libsuffix}")
+set(TBB_MALLOC_PROXY_LIBRARY_RELEASE "${TBB_INSTALL_DIR}/tbb${tbb_ver}/${tbb_libdir}/${tbb_lib_prefix}tbbmalloc_proxy${tbb_libsuffix}")
 
-set(TBB_BIN_DIR "${TBB_INSTALL_DIR}/${tbb_bindir}")
-set(TBB_LIB_DIR "${TBB_INSTALL_DIR}/${tbb_libdir}")
+set(TBB_BIN_DIR "${TBB_INSTALL_DIR}/tbb${tbb_ver}/${tbb_bindir}")
+set(TBB_LIB_DIR "${TBB_INSTALL_DIR}/tbb${tbb_ver}/${tbb_libdir}")
 mark_as_superbuild(
   VARS
     TBB_BIN_DIR:PATH
@@ -115,13 +109,13 @@ mark_as_superbuild(
 
 ExternalProject_GenerateProjectDescription_Step(${proj}
   VERSION ${tbb_ver}
-  LICENSE_FILES "https://raw.githubusercontent.com/01org/tbb/tbb_2018/LICENSE"
+  LICENSE_FILES "https://raw.githubusercontent.com/oneapi-src/oneTBB/2019_U9/LICENSE"
   )
 
 #-----------------------------------------------------------------------------
 # Launcher setting specific to build tree
 
-set(${proj}_LIBRARY_PATHS_LAUNCHER_BUILD "${TBB_INSTALL_DIR}/${tbb_bindir}")
+set(${proj}_LIBRARY_PATHS_LAUNCHER_BUILD "${TBB_INSTALL_DIR}/tbb${tbb_ver}/${tbb_bindir}")
 mark_as_superbuild(
   VARS ${proj}_LIBRARY_PATHS_LAUNCHER_BUILD
   LABELS "LIBRARY_PATHS_LAUNCHER_BUILD"
