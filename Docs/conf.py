e3d43184d8edfb14749fb850ee9d4ac4679e6122
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2020-08-19 09:36:26 -0400

    DOC: Update ReadTheDocs style
    Co-authored-by: Faiza Ahmed <faiza.ahmed@kitware.com>

--- Docs/conf.py
+++ Docs/conf.py
@@ -112,8 +112,13 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
 
 # -- Options for HTMLHelp output ------------------------------------------
 
