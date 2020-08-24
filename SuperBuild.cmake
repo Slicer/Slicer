7c59786f9a3a546e7b67156477ab983283ebb737
Andras Lasso <lasso@queensu.ca>
2020-08-24 10:20:39 -0400

    BUG: Update MultiVolumeImporter to fix multivolume/volumesequence import into subject hierarchy
    Content:
    
    Revision: 528fd7bef53a464853db860605f371b3255f36dd
    Author: Andras Lasso <lasso@queensu.ca>
    Date: 2020-08-24 9:53:44 AM
    Message:
    BUG: Add imported multivolume/volumesequence node to subject hierarchy
    
    When a multi-volume or volume sequence was loaded then it was not placed under the patient/study in the subject hierarchy.

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -243,7 +243,7 @@ list_conditional_append(Slicer_BUILD_MultiVolumeExplorer Slicer_REMOTE_DEPENDENC
 
 Slicer_Remote_Add(MultiVolumeImporter
   GIT_REPOSITORY ${EP_GIT_PROTOCOL}://github.com/fedorov/MultiVolumeImporter.git
-  GIT_TAG 8cd1631ff496fef930ec2bb4f4e0a640d2ae37ea
+  GIT_TAG 8a81b4395e9093875b0f90a3a1b479b0832f8c36
   OPTION_NAME Slicer_BUILD_MultiVolumeImporter
   OPTION_DEPENDS "Slicer_BUILD_QTLOADABLEMODULES;Slicer_BUILD_MULTIVOLUME_SUPPORT;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
