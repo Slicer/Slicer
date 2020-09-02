016ebf1b0192d5ede02235eecb3da3098650f2c5
Steve Pieper <pieper@bwh.harvard.edu>
2020-09-02 10:22:29 -0400

    COMP: Update CompareVolumes
    This set of changes makes the module more generally usable by
    streamlining the UI options and fixing some bugs introduced
    by the python3 transition.
    
    ```
    git shortlog 34f1b3da7.. 94224926f --no-merges
    Steve Pieper (5):
          BUG: incorrect row/column, revert division change
          ENH: add option to scale reveal cursor
          BUG: use float division
          STYLE: remove print, fix test & message
          ENH: major overhaul to support MR-US use case
    ```

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -333,15 +333,13 @@ list_conditional_append(Slicer_BUILD_DataStore Slicer_REMOTE_DEPENDENCIES DataSt
 
 Slicer_Remote_Add(CompareVolumes
   GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/pieper/CompareVolumes"
-  GIT_TAG a78f04592488e45eb7700679acd418ba0240651a
+  GIT_TAG 94224926fa0b0900597343e4db991fd028136bd2
   OPTION_NAME Slicer_BUILD_CompareVolumes
   OPTION_DEPENDS "Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
   )
 list_conditional_append(Slicer_BUILD_CompareVolumes Slicer_REMOTE_DEPENDENCIES CompareVolumes)
 
-# Disable this module until it can be ported to the new markups code
-# Tracked at https://github.com/pieper/LandmarkRegistration/issues/29
 Slicer_Remote_Add(LandmarkRegistration
   GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/pieper/LandmarkRegistration"
   GIT_TAG c1385ba2cd5069a018cd97c19085788006276918
