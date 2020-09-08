5fcf1cf74f7c0e3938c5de230cfea17f4fa26571
Andras Lasso <lasso@queensu.ca>
2020-09-08 14:20:57 -0400

    BUG: Update VTK to fix crash in multi-volume volume renderer
    When some volumes were added then removed from the volume rendering in multi-volume mode then VTK crashed (load 3 volumes, switch to multi-volume rendering mode, show them all 3 using volume rendering, hide the first volume, apply Gaussian smoothing on the second volume -> crash).
    
    A fix was pushed to VTK master and it was cherry-picked to Slicer-VTK.

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -122,7 +122,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
 set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "8b86723b9d9eafd7c0b87b47e3937a512876b063") # slicer-v8.2.0-2018-10-02-74d9488523
+  set(_git_tag "97904fdcc7e73446b3131f32eac9fc9781b23c2d") # slicer-v8.2.0-2018-10-02-74d9488523
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()
