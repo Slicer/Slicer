Slicer/Slicer

commit 26f1f47a8aeb5905459376f9a4948716541d21b1

BUG: Fix application startup when built against Qt 5.15.1 (#5207)
This commit ensures QmlModels introduced in 5.14 is packaged.
See https://github.com/qt/qtdeclarative/commit/325e6305b418ffe1dfb9a36c2516c6a8a3de5733

CMake/SlicerBlockInstallQt.cmake
@@ -23,6 +23,10 @@ set(QT_INSTALL_LIB_DIR ${Slicer_INSTALL_LIB_DIR})
       "Qt5::QuickWidgets"
       "Qt5::WebEngineCore"
       )
+    # QmlModels moved into its own library in Qt >= 5.14
+    if(TARGET Qt5::QmlModels)
+      list(APPEND QT_LIBRARIES "Qt5::QmlModels")
+    endif()
     if(TARGET Qt5::Positioning)
       list(APPEND QT_LIBRARIES "Qt5::Positioning")
     endif()

commit 92b35263300ea6ddedb18e66cdb979ea538ed939

BUG: Fix installed Slicer app startup on windows when built against Qt 5.15.1
This commit fixes a regression introduced in 26f1f47a8a (BUG: Fix
application startup when built against Qt 5.15.1 (#5207)) that was intended
to fix Linux packaging.

It ensures the installed Slicer application can successfully start.

On windows the "windeployqt" tool implicitly takes care of copying the
"Qt5QmlModels.dll" by only specifying the "-qml" option.

Fixes #5208

CMake/SlicerBlockInstallQt.cmake
@@ -24,7 +24,9 @@ set(QT_INSTALL_LIB_DIR ${Slicer_INSTALL_LIB_DIR})
       "Qt5::WebEngineCore"
       )
     # QmlModels moved into its own library in Qt >= 5.14
-    if(TARGET Qt5::QmlModels)
+    # Since windeployqt implicitly copies "Qt5QmlModels.dll" when
+    # "-qml" is specified, exclude it on Windows.
+    if(TARGET Qt5::QmlModels AND NOT WIN32)
       list(APPEND QT_LIBRARIES "Qt5::QmlModels")
     endif()
     if(TARGET Qt5::Positioning)