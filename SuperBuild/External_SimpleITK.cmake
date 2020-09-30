Slicer/Slicer

commit 127dfbad28cfb778f5661e923da028da28831d48

COMP: Update SimpleITK from v2.0rc3 to v2.0.0 (#5212)
$ git shortlog --no-merges v2.0rc3..v2.0.0
Bradley Lowekamp (19):
      Add __version__ to Python module for PEP 376 compatibility.
      Remove outdated, and disabled download_url field from setup.py
      Add project_urls to setup.py for link on  PyPi
      Use Readme for long description in setup.py and PyPi
      Update ITK version to official 5.1.1 tag
      Fix PasteImageFilter doxygen class name
      Update IO docs with Python keyword argument usage.
      Various improvements to the Sphinx build document.
      Update Sphinx for SimpleITK 2.0
      Minor tweaks to the Sphinx Getting Started documentation.
      Fix unsigned to signed integer comparison warning in generated tests
      Fix Sphinx warnings.
      Add C++ example for executing filters in place
      Add example for processing high dimension images.
      Fix Sphinx building docs label syntax
      Only add the path from the source external data if it exists
      Fix typo in CMake file
      Extend and improve the 2.0 migration guide
      Address signed to unsigned comparison warning

Ziv Yaniv (2):
      Updating the documentation for transformation IO.
      Migration guide for SimpleITK 2.0.

SuperBuild/External_SimpleITK.cmake
@@ -51,7 +51,7 @@ ExternalProject_Execute(${proj} \"install\" \"${PYTHON_EXECUTABLE}\" setup.py in
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "v2.0rc3"  # v2.0rc3 is paired with a specific SimpleITKFilters remote module git tag in the ITK project
+    "v2.0.0"  # Paired with a specific SimpleITKFilters remote module git tag in the ITK project
     QUIET
     )
 

commit 366aab0adc3e6e1539234a6d98303ff0b61d86d9

COMP: Fix installation of SimpleITK on windows
This commit fixes a regression introduced in 127dfbad28 (COMP: Update
SimpleITK from v2.0rc3 to v2.0.0 (#5212)) and ensures that SimpleITK
libraries are also in the PATH when running the command:

  cd /path/to/SimpleITK-build/SimpleITK-build/Wrapping/Python
  python setup.py install

Adding the SimpleITK libraries is required on windows where
Slicer_USE_SimpleITK_SHARED is enabled.

SuperBuild/External_SimpleITK.cmake
@@ -20,6 +20,10 @@ if(NOT Slicer_USE_SYSTEM_${proj})
 
   include(ExternalProjectForNonCMakeProject)
 
+  set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
+  set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
+  set(EP_INSTALL_DIR ${CMAKE_BINARY_DIR}/${proj}-install)
+
   # Variables used to update PATH, LD_LIBRARY_PATH or DYLD_LIBRARY_PATH in env. script below
   if(WIN32)
     set(_varname "PATH")
@@ -53,6 +57,31 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       # ITK
       ${ITK_DIR}/${_lib_subdir}
       )
+  endif()
+
+  if(Slicer_USE_SimpleITK_SHARED)
+    if(CMAKE_CONFIGURATION_TYPES)
+      foreach(config ${CMAKE_CONFIGURATION_TYPES})
+        list(APPEND _paths
+          # SimpleITK
+          ${EP_BINARY_DIR}/SimpleITK-build/${_lib_subdir}/${config}
+          # DCMTK
+          ${DCMTK_DIR}/${_lib_subdir}/${config}
+          )
+      endforeach()
+    else()
+      list(APPEND _paths
+        # SimpleITK
+        ${EP_BINARY_DIR}/SimpleITK-build/${_lib_subdir}
+        # DCMTK
+        ${DCMTK_DIR}/${_lib_subdir}
+        )
+    endif()
+
+    # TBB
+    if(Slicer_USE_TBB)
+      list(APPEND _paths ${TBB_BIN_DIR})
+    endif()
   endif()
 
   list(JOIN _paths "${_path_sep}" _paths)
@@ -96,11 +125,6 @@ ExternalProject_Execute(${proj} \"install\" \"${PYTHON_EXECUTABLE}\" setup.py in
     "v2.0.0"  # Paired with a specific SimpleITKFilters remote module git tag in the ITK project
     QUIET
     )
-
-  set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
-  set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
-  set(EP_INSTALL_DIR ${CMAKE_BINARY_DIR}/${proj}-install)
-
 
   # A separate project is used to download, so that the SuperBuild
   # subdirectory can be use for SimpleITK's SuperBuild to build