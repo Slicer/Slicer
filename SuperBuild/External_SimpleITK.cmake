Slicer/Slicer

commit 127dfbad28cfb778f5661e923da028da28831d48

COMP: Update SimpleITK from v2.0rc3 to v2.0.0 (#5212)
$ git shortlog --no-merges v2.0rc3..v2.0.0
Bradley Lowekamp (19):
      Add __version__ to Python module for PEP 376 compatibility.
      Remove outdated, and disabled download_url field from setup.py
      Add project_urls to setup.py for link on  PyPi
      Use Readme for long description in setup.py and PyPi
      Update ITK version to official 5.1.1 tag
      Fix PasteImageFilter doxygen class name
      Update IO docs with Python keyword argument usage.
      Various improvements to the Sphinx build document.
      Update Sphinx for SimpleITK 2.0
      Minor tweaks to the Sphinx Getting Started documentation.
      Fix unsigned to signed integer comparison warning in generated tests
      Fix Sphinx warnings.
      Add C++ example for executing filters in place
      Add example for processing high dimension images.
      Fix Sphinx building docs label syntax
      Only add the path from the source external data if it exists
      Fix typo in CMake file
      Extend and improve the 2.0 migration guide
      Address signed to unsigned comparison warning

Ziv Yaniv (2):
      Updating the documentation for transformation IO.
      Migration guide for SimpleITK 2.0.

SuperBuild/External_SimpleITK.cmake
@@ -51,7 +51,7 @@ ExternalProject_Execute(${proj} \"install\" \"${PYTHON_EXECUTABLE}\" setup.py in
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "v2.0rc3"  # v2.0rc3 is paired with a specific SimpleITKFilters remote module git tag in the ITK project
+    "v2.0.0"  # Paired with a specific SimpleITKFilters remote module git tag in the ITK project
     QUIET
     )
 

commit 5c6e7e317cf55daba7e3de2767aa6122f52eca85

COMP: Fix installation of SimpleITK
This commit fixes a regression introduced in 127dfbad28 (COMP: Update
SimpleITK from v2.0rc3 to v2.0.0 (#5212)) and ensures that ITK libraries
are in the PATH when running the command:

  cd /path/to/SimpleITK-build/SimpleITK-build/Wrapping/Python
  python setup.py install

It addresses the following error:

  Traceback (most recent call last):
    File "<stdin>", line 1, in <module>
    File "/work/Preview/Slicer-0-build/SimpleITK-build/SimpleITK-build/Wrapping/Python/SimpleITK/__init__.py", line 18, in <module>
      from SimpleITK.SimpleITK import *
    File "/work/Preview/Slicer-0-build/SimpleITK-build/SimpleITK-build/Wrapping/Python/SimpleITK/SimpleITK.py", line 13, in <module>
      from . import _SimpleITK
  ImportError: libITKIOBruker-5.1.so.1: cannot open shared object file: No such file or directory

SuperBuild/External_SimpleITK.cmake
@@ -20,9 +20,32 @@ if(NOT Slicer_USE_SYSTEM_${proj})
 
   include(ExternalProjectForNonCMakeProject)
 
+  # Variables used to update PATH, LD_LIBRARY_PATH or DYLD_LIBRARY_PATH in env. script below
+  if(WIN32)
+    set(_varname "PATH")
+    set(_path_sep ";")
+  elseif(UNIX)
+    set(_path_sep ":")
+    if(APPLE)
+      set(_varname "DYLD_LIBRARY_PATH")
+    else()
+      set(_varname "LD_LIBRARY_PATH")
+    endif()
+  endif()
+
   # environment
   set(_env_script ${CMAKE_BINARY_DIR}/${proj}_Env.cmake)
   ExternalProject_Write_SetPythonSetupEnv_Commands(${_env_script})
+  file(APPEND ${_env_script}
+"#------------------------------------------------------------------------------
+# Added by '${CMAKE_CURRENT_LIST_FILE}'
+set(ENV{${_varname}} \"${ITK_DIR}/lib${_path_sep}\$ENV{${_varname}}\")
+set(ENV{${_varname}} \"${ITK_DIR}/bin${_path_sep}\$ENV{${_varname}}\")
+set(ENV{${_varname}} \"${ITK_DIR}/bin/Release${_path_sep}\$ENV{${_varname}}\")
+set(ENV{${_varname}} \"${ITK_DIR}/bin/MinSizeRel${_path_sep}\$ENV{${_varname}}\")
+set(ENV{${_varname}} \"${ITK_DIR}/bin/RelWithDebInfo${_path_sep}\$ENV{${_varname}}\")
+set(ENV{${_varname}} \"${ITK_DIR}/bin/Debug${_path_sep}\$ENV{${_varname}}\")
+")
 
   set(EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS)
 