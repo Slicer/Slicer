a3275775e3338614aaaa37d2c3336603e5cf6ff9
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2020-11-19 18:34:27 -0500

    BUG: Update CTK to fix installation of extension archive with hidden files
    List of changes:
    
    $ git shortlog 7210c5bc..18715f18 --no-merges
    Jean-Christophe Fillion-Robin (3):
          ENH: Improve ctkUtilsCopyDirRecursivelyTest1 to check file existence
          BUG: Ensure copyDirRecursively considers hidden directories and files
          ENH: Update copyDirRecursively to support skipping copy of hidden files

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -70,7 +70,7 @@ if(NOT DEFINED CTK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "7210c5bcc37bf1140a04c97bfe1e27142735c6ab"
+    "18715f18b44fd557106a180a01deeca7f8a89946"
     QUIET
     )
 
