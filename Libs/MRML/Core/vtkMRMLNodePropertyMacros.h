Slicer/Slicer

commit 1e0b70925d57f199e20eefdf5938365bc081c657

BUG: Fixed MRML string vector attribute writing
Fixes regression introduced in commit 2bdb50ac0c65cc10e7f20019f35b35fd94633ccb on 2020-11-30 that broke writing of string vector attributes of MRML nodes.

Libs/MRML/Core/vtkMRMLNodePropertyMacros.h
@@ -11,6 +11,7 @@
 #define __vtkMRMLNodePropertyMacros_h
 
 #include <sstream> // needed for std::stringstream
+#include <vtksys/SystemTools.hxx> // needed for vtksys::SystemTools functions
 
 /// @file
 
@@ -121,7 +122,7 @@
     vectorType<std::string> vector = Get##propertyName(); \
     for (vectorType<std::string>::iterator it=vector.begin(); it!=vector.end(); it++) \
       { \
-      if (it==vector.begin()) \
+      if (it!=vector.begin()) \
         { \
         xmlWriteOutputStream << ";"; \
         } \
@@ -322,29 +323,22 @@
       separatorPosition = valueString.find(" "); \
       } \
     this->Set##propertyName(vector); \
-  }
+    }
 
 /// Macro for reading an iterable container (of std::string) node property from XML.
 #define vtkMRMLReadXMLStdStringVectorMacro(xmlAttributeName, propertyName, vectorType) \
   if (!strcmp(xmlReadAttName, #xmlAttributeName)) \
     { \
-    vectorType<std::string> vector; \
+    vectorType<std::string> attributeValues; \
     std::string valueString(xmlReadAttValue); \
-    size_t separatorPosition = valueString.find(";"); \
-    while(separatorPosition != std::string::npos) \
+    std::vector<vtksys::String> splitXmlReadAttValue = vtksys::SystemTools::SplitString(valueString, ';'); \
+    for (std::string attributeValue : splitXmlReadAttValue) \
       { \
-      std::string attributeValue = valueString.substr(0,separatorPosition); \
       vtksys::SystemTools::ReplaceString(attributeValue, "%3B", ";"); \
       vtksys::SystemTools::ReplaceString(attributeValue, "%25", "%"); \
-      vector.insert(vector.end(), attributeValue); \
-      valueString = valueString.substr(separatorPosition+1); \
-      separatorPosition = valueString.find(";"); \
+      attributeValues.emplace_back(attributeValue); \
       } \
-    if (!valueString.empty()) \
-      { \
-      vector.push_back(valueString); \
-      } \
-    this->Set##propertyName(vector); \
+    this->Set##propertyName(attributeValues); \
     }
 
 /// Macro for reading a vtkMatrix4x4* node property from XML.

Libs/MRML/Core/Testing/vtkMRMLNodeTest1.cxx
@@ -2861,18 +2861,20 @@ int TestReadWriteXMLProperties()
   node1->SetTestingFloatVector(testingFloatVector);
 
   std::vector<int> testingIntVector;
-  testingFloatVector.push_back(0);
-  testingFloatVector.push_back(1);
-  testingFloatVector.push_back(2);
-  testingFloatVector.push_back(3);
-  testingFloatVector.push_back(4);
-  testingFloatVector.push_back(5);
+  testingIntVector.push_back(0);
+  testingIntVector.push_back(1);
+  testingIntVector.push_back(2);
+  testingIntVector.push_back(3);
+  testingIntVector.push_back(4);
+  testingIntVector.push_back(5);
   node1->SetTestingIntVector(testingIntVector);
 
   std::stringstream ss;
   ss << "<MRMLNode ";
   node1->WriteXML(ss, 0);
   ss << " />";
+  std::cout << "Scene XML: " << ss.str() << std::endl;
+
   vtkSmartPointer<vtkXMLDataElement> element = vtkSmartPointer<vtkXMLDataElement>::Take(vtkXMLUtilities::ReadElementFromStream(ss));
 
   const char *atts[] = {
@@ -2886,7 +2888,16 @@ int TestReadWriteXMLProperties()
 
   if (node1->GetTestingStringVector() != node2->GetTestingStringVector())
     {
-    return EXIT_FAILURE;
+    std::cout << "Node 1 string vector:" << std::endl;
+    for (auto s : node1->GetTestingStringVector())
+      {
+      std::cout << "  " << s << std::endl;
+      }
+    std::cout << "Node 2 string vector:" << std::endl;
+    for (auto s : node2->GetTestingStringVector())
+      {
+      std::cout << "  " << s << std::endl;
+      }
     }
 
   if (node1->GetTestingFloatVector() != node2->GetTestingFloatVector())

commit 2522baef9910d91d1b9ceb54fbc4e24b2bb25443

COMP: Update vtkMRMLReadXMLStdStringVectorMacro to fix VTK9 build error
This commit updates the macro to use "std::string" because "vtksys::String"
is not available when building against VTK 9.

It fixes a regression introduced in commit 1e0b70925 (BUG: Fixed MRML
string vector attribute writing)

Libs/MRML/Core/vtkMRMLNodePropertyMacros.h
@@ -326,6 +326,24 @@
     }
 
 /// Macro for reading an iterable container (of std::string) node property from XML.
+#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 90)
+// Use std::string
+#define vtkMRMLReadXMLStdStringVectorMacro(xmlAttributeName, propertyName, vectorType) \
+  if (!strcmp(xmlReadAttName, #xmlAttributeName)) \
+    { \
+    vectorType<std::string> attributeValues; \
+    std::string valueString(xmlReadAttValue); \
+    std::vector<std::string> splitXmlReadAttValue = vtksys::SystemTools::SplitString(valueString, ';'); \
+    for (std::string attributeValue : splitXmlReadAttValue) \
+      { \
+      vtksys::SystemTools::ReplaceString(attributeValue, "%3B", ";"); \
+      vtksys::SystemTools::ReplaceString(attributeValue, "%25", "%"); \
+      attributeValues.emplace_back(attributeValue); \
+      } \
+    this->Set##propertyName(attributeValues); \
+    }
+#else
+// Use vtksys::String
 #define vtkMRMLReadXMLStdStringVectorMacro(xmlAttributeName, propertyName, vectorType) \
   if (!strcmp(xmlReadAttName, #xmlAttributeName)) \
     { \
@@ -340,6 +358,7 @@
       } \
     this->Set##propertyName(attributeValues); \
     }
+#endif
 
 /// Macro for reading a vtkMatrix4x4* node property from XML.
 /// "Owned" means that the node owns the matrix, the object is always valid and cannot be replaced from outside