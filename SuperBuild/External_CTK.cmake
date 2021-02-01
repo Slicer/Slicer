499dc7b150f6189e32d20e13ba9eac2555963943
xriobe <66792993+xriobe@users.noreply.github.com>
2021-02-01 22:26:40 +0100

    BUG: Update CTK to fix a crash in ctkRangeWidget::SetRange in Debug mode (#5422)
    List of CTK changes:
    
    $ git shortlog 3b195778..24e2f7f4 --no-merges 
    xriobe (1):
          BUG: Fix crash in Debug replacing Q_ASSERT by qWarning in ctkRangeWidget::setRange (PR-948)
    
    See discussion here: https://discourse.slicer.org/t/crash-with-ctkrangewidget/15299/4
    
    Co-authored-by: x_riobe <riobe@certis-therapeutics.com>

--- SuperBuild/External_CTK.cmake
+++ SuperBuild/External_CTK.cmake
@@ -70,7 +70,7 @@ if(NOT DEFINED CTK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "3b195778f004684c715eff7450a3260cc68daf2d"
+    "24e2f7f49cdfac3502a82719f53213b6eea19cf4"
     QUIET
     )
 
