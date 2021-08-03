Slicer/Slicer

commit 4c8b2803fa9fb34c2281989e273a3bdeba2ac8a5

ENH: Report more scene loading errors
If a node fails to be instantiated then a message is displayed to the user.

List of installed extensions is saved in the scene and in case a scene loading error occurs then the list of missing extensions (that were installed at the time of saving the scene but not now) are displayed to the user. Since most extensions do not affect scene loading, if some extensions are not installed but scene loading is successful then no warning is displayed.

Modules/Loadable/Data/qSlicerSceneReader.cxx
@@ -88,12 +88,12 @@ bool qSlicerSceneReader::load(const qSlicerIO::IOProperties& properties)
   QString file = properties["fileName"].toString();
   this->mrmlScene()->SetURL(file.toUtf8());
   bool clear = properties.value("clear", false).toBool();
-  int res = 0;
+  bool success = false;
   if (clear)
     {
     qDebug("Clear and import into main MRML scene");
-    res = this->mrmlScene()->Connect();
-    if (res)
+    success = this->mrmlScene()->Connect(this->userMessages());
+    if (success)
       {
       // Set default scene file format to .mrml
       qSlicerCoreIOManager* coreIOManager = qSlicerCoreApplication::application()->coreIOManager();
@@ -106,11 +106,7 @@ bool qSlicerSceneReader::load(const qSlicerIO::IOProperties& properties)
       {
       qWarning() << Q_FUNC_INFO << ": copyCameras=false property is ignored, cameras are now always replaced in the scene";
       }
-    res = this->mrmlScene()->Import();
-    }
-  if (!this->mrmlScene()->GetErrorMessage().empty())
-    {
-    this->userMessages()->AddMessage(vtkCommand::ErrorEvent, "\n\n" + this->mrmlScene()->GetErrorMessage());
+    success = this->mrmlScene()->Import(this->userMessages());
     }
 
   // Display warning message if scene file was created with a different application or with a future application version
@@ -151,5 +147,30 @@ bool qSlicerSceneReader::load(const qSlicerIO::IOProperties& properties)
       }
     }
 
-  return res;
+  // If there were scene loading errors then log the list of extensions that were installed when the scene was saved.
+  // It may provide useful hints for why the extension load failed.
+  if (!success
+    || this->userMessages()->GetNumberOfMessagesOfType(vtkCommand::ErrorEvent) > 0
+    || this->userMessages()->GetNumberOfMessagesOfType(vtkCommand::WarningEvent) > 0)
+    {
+    std::string extensions = this->mrmlScene()->GetExtensions() ? this->mrmlScene()->GetExtensions() : "";
+    std::string lastLoadedExtensions = this->mrmlScene()->GetLastLoadedExtensions() ? this->mrmlScene()->GetLastLoadedExtensions() : "";
+    if (extensions != lastLoadedExtensions)
+      {
+      QStringList extensionsList = QString::fromStdString(extensions).split(";");
+      QStringList lastLoadedExtensionsList = QString::fromStdString(lastLoadedExtensions).split(";");
+      QSet<QString> notInstalledExtensions = lastLoadedExtensionsList.toSet().subtract(extensionsList.toSet());
+      if (!notInstalledExtensions.isEmpty())
+        {
+        QStringList notInstalledExtensionsList(notInstalledExtensions.begin(), notInstalledExtensions.end());
+        QString extensionsInformation =
+          tr("These extensions were installed when the scene was saved but not installed now: %1."
+             " These extensions may be required for successful loading of the scene.")
+          .arg(notInstalledExtensionsList.join(", "));
+        this->userMessages()->AddMessage(vtkCommand::MessageEvent, extensionsInformation.toStdString());
+        }
+      }
+    }
+
+  return success;
 }

Base/QTCore/qSlicerCoreApplication.cxx
@@ -447,6 +447,11 @@ void qSlicerCoreApplicationPrivate::init()
     qDebug() << "Successfully uninstalled extension" << extensionName;
     }
 
+  // Set the list of installed extensions in the scene so that we can warn the user
+  // if the scene is loaded with some of the extensions not present.
+  QString extensionList = model->installedExtensions().join(";");
+  scene->SetExtensions(extensionList.toStdString().c_str());
+
 #endif
 
   if (q->userSettings()->value("Internationalization/Enabled").toBool())

Base/QTCore/qSlicerSceneBundleReader.cxx
@@ -90,12 +90,7 @@ bool qSlicerSceneBundleReader::load(const qSlicerIO::IOProperties& properties)
     clear = properties["clear"].toBool();
     }
 
-  this->mrmlScene()->SetErrorMessage("");
-  bool success = this->mrmlScene()->ReadFromMRB(file.toUtf8(), clear);
-  if (!this->mrmlScene()->GetErrorMessage().empty())
-    {
-    this->userMessages()->AddMessage(vtkCommand::ErrorEvent, this->mrmlScene()->GetErrorMessage());
-    }
+  bool success = this->mrmlScene()->ReadFromMRB(file.toUtf8(), clear, this->userMessages());
   if (success)
     {
     // Set default scene file format to mrb

Base/QTCore/qSlicerSlicer2SceneReader.cxx
@@ -1219,8 +1219,8 @@ void qSlicerSlicer2SceneReaderPrivate::importOptionsNode(NodeType& node)
   Q_Q(qSlicerSlicer2SceneReader);
   //$::slicer3::MRMLScene SetErrorMessage "warning: option nodes cannot be imported"
   //$::slicer3::MRMLScene SetErrorCode 1
-  q->mrmlScene()->SetErrorMessage("Warning: option nodes cannot be imported");
-  q->mrmlScene()->SetErrorCode(1);
+  //q->mrmlScene()->SetErrorMessage("Warning: option nodes cannot be imported");
+  //q->mrmlScene()->SetErrorCode(1);
 }
 
 //-----------------------------------------------------------------------------

Libs/MRML/Core/vtkMRMLFiducialListNode.cxx
@@ -185,11 +185,15 @@ void vtkMRMLFiducialListNode::ReadXMLAttributes(const char** atts)
           {
           if (this->GetScene()->GetLastLoadedVersion())
             {
-            const char *lastLoadedVersion = this->GetScene()->GetLastLoadedVersion();
-            int versionNumber = atoi(lastLoadedVersion);
-            if (versionNumber < 12553)
+            std::string loadedApplication;
+            int loadedMajor = 0;
+            int loadedMinor = 0;
+            int loadedPatch = 0;
+            int loadedRevision = 0;
+            if (vtkMRMLScene::ParseVersion(this->GetScene()->GetLastLoadedVersion(), loadedApplication, loadedMajor, loadedMinor, loadedPatch, loadedRevision)
+              && loadedRevision < 12553)
               {
-              vtkDebugMacro("Older mrml file version " << versionNumber << ", increasing the glyph type by 1");
+              vtkDebugMacro("Older mrml file version " << loadedRevision << ", increasing the glyph type by 1");
               adjustment = 1;
               }
             else

Libs/MRML/Core/vtkMRMLFiducialListStorageNode.cxx
@@ -381,9 +381,14 @@ int vtkMRMLFiducialListStorageNode::ReadDataInternal(vtkMRMLNode *refNode)
     int glyphType = fiducialListNode->GetGlyphType();
     if (this->GetScene() && this->GetScene()->GetLastLoadedVersion())
       {
-      if (this->GetScene()->GetLastLoadedVersion() != std::string(""))
+      std::string loadedApplication;
+      int loadedMajor = 0;
+      int loadedMinor = 0;
+      int loadedPatch = 0;
+      int loadedRevision = 0;
+      if (vtkMRMLScene::ParseVersion(this->GetScene()->GetLastLoadedVersion(), loadedApplication, loadedMajor, loadedMinor, loadedPatch, loadedRevision))
         {
-        if (atoi(this->GetScene()->GetLastLoadedVersion()) < 12553)
+        if (loadedRevision < 12553)
           {
           vtkDebugMacro("ReadData: last loaded version '" << this->GetScene()->GetLastLoadedVersion() << "' is less than 12553, incrementing glyph type from " << glyphType);
           glyphType++;

Libs/MRML/Core/vtkMRMLParser.cxx
@@ -50,6 +50,10 @@ void vtkMRMLParser::StartElement(const char* tagName, const char** atts)
         {
         this->GetMRMLScene()->SetLastLoadedVersion(attValue);
         }
+      if (!strcmp(attName, "extensions"))
+        {
+        this->GetMRMLScene()->SetLastLoadedExtensions(attValue);
+        }
       else if (!strcmp(attName, "userTags"))
         {
         if ( this->MRMLScene->GetUserTagTable() == nullptr )
@@ -132,7 +136,7 @@ void vtkMRMLParser::StartElement(const char* tagName, const char** atts)
   vtkMRMLNode* node = this->MRMLScene->CreateNodeByClass( className.c_str() );
   if (!node)
     {
-    vtkWarningMacro(<< "Failed to CreateNodeByClass: " << className);
+    vtkErrorMacro("Failed to CreateNodeByClass: " << className);
     return;
     }
 

commit aacd5a3cad4d9d329abe8790c2b5c015a4efd5bb

COMP: Update qSlicerSceneReader to fix compilation using Qt < 5.14
This commit fixes a regression introduced in 4c8b2803f (ENH: Report more
scene loading errors)

Issue was discussed in this thread: https://discourse.slicer.org/t/build-error-on-ubuntu-20-4-qt-5-12-8-cmake-3-16-3/19041

Co-authored-by: Rafael Palomar <rafael.palomar@rr-research.no>

Modules/Loadable/Data/qSlicerSceneReader.cxx
@@ -162,11 +162,10 @@ bool qSlicerSceneReader::load(const qSlicerIO::IOProperties& properties)
       QSet<QString> notInstalledExtensions = lastLoadedExtensionsList.toSet().subtract(extensionsList.toSet());
       if (!notInstalledExtensions.isEmpty())
         {
-        QStringList notInstalledExtensionsList(notInstalledExtensions.begin(), notInstalledExtensions.end());
         QString extensionsInformation =
           tr("These extensions were installed when the scene was saved but not installed now: %1."
              " These extensions may be required for successful loading of the scene.")
-          .arg(notInstalledExtensionsList.join(", "));
+          .arg(QStringList::fromSet(notInstalledExtensions).join(", "));
         this->userMessages()->AddMessage(vtkCommand::MessageEvent, extensionsInformation.toStdString());
         }
       }