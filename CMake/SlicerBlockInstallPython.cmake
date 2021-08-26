df917721d2d73c08482acae99314cbab903f8c5a
Andras Lasso <lasso@queensu.ca>
2021-08-26 00:59:56 -0400

    BUG: Add missing Python3.dll to the install package
    fixes #5816

--- CMake/SlicerBlockInstallPython.cmake
+++ CMake/SlicerBlockInstallPython.cmake
@@ -15,7 +15,7 @@ if(Slicer_USE_PYTHONQT)
     message(WARNING "###############################################################################
 Skipping generation of python install rules
 
-Unexistant directory PYTHON_DIR:${PYTHON_DIR}/${PYTHON_STDLIB_SUBDIR}
+Non-existing directory PYTHON_DIR:${PYTHON_DIR}/${PYTHON_STDLIB_SUBDIR}
 
 This probably means that you are building Slicer against your own installation of Python.
 
@@ -73,8 +73,17 @@ To create a Slicer package including python libraries, you can *NOT* provide you
         COMPONENT Runtime)
     endif()
   elseif(WIN32)
+    # Copy Python36.dll
     get_filename_component(PYTHON_LIB_BASE ${PYTHON_LIBRARY} NAME_WE)
     install(FILES "${PYTHON_LIBRARY_PATH}/${PYTHON_LIB_BASE}.dll"
+      DESTINATION bin
+      COMPONENT Runtime)
+    # Copy Python3.dll
+    # The file name is always exactly this, as this file is guaranteed
+    # to be ABI compatible across all Python3 versions
+    # (see https://www.python.org/dev/peps/pep-0384/#linkage
+    # and https://github.com/Slicer/Slicer/issues/5816 for details).
+    install(FILES "${PYTHON_LIBRARY_PATH}/Python3.dll"
       DESTINATION bin
       COMPONENT Runtime)
   endif()
