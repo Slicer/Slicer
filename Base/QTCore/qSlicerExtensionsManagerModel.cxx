Slicer/Slicer

commit e2ea8afa61a4d4b0372f3650c7843c56b757a20c

BUG: Ensure extensions installed using "Girder_v1" serverAPI are enabled
See #5788 and #5786

Base/QTCore/qSlicerExtensionsManagerModel.cxx
@@ -1720,11 +1720,16 @@ bool qSlicerExtensionsManagerModel::installExtension(
   const ExtensionMetadataType& extensionIndexMetadata =
     Self::parseExtensionDescriptionFile(extensionIndexDescriptionFile);
 
+  // Enable or disable the extension if state was not already set
+  if (!extensionMetadata.contains("enabled"))
+    {
+    extensionMetadata.insert("enabled", d->NewExtensionEnabledByDefault);
+    }
+
   // Copy metadata if not provided from server (e.g. installing from file)
   if (extensionMetadata.isEmpty())
     {
     extensionMetadata.insert("archivename", QFileInfo(archiveFile).fileName());
-    extensionMetadata.insert("enabled", d->NewExtensionEnabledByDefault);
 
     // Copy expected keys from archive description
     QStringList expectedKeys;

commit 530a43777da3438b1dafcb5aa84a81348e4a23b6

BUG: Fix support for installing extensions from file
This commit fixes a regression introduced in e2ea8afa6 (BUG: Ensure
extensions installed using "Girder_v1" serverAPI are enabled)

See https://discourse.slicer.org/t/unable-to-manually-install-extension-to-manual-slicer-build/19359/6

Fixes #5840
Fixes #5848

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

Base/QTCore/qSlicerExtensionsManagerModel.cxx
@@ -1741,12 +1741,6 @@ bool qSlicerExtensionsManagerModel::installExtension(
   const ExtensionMetadataType& extensionIndexMetadata =
     Self::parseExtensionDescriptionFile(extensionIndexDescriptionFile);
 
-  // Enable or disable the extension if state was not already set
-  if (!extensionMetadata.contains("enabled"))
-    {
-    extensionMetadata.insert("enabled", d->NewExtensionEnabledByDefault);
-    }
-
   // Copy metadata if not provided from server (e.g. installing from file)
   if (extensionMetadata.isEmpty())
     {
@@ -1777,6 +1771,12 @@ bool qSlicerExtensionsManagerModel::installExtension(
     extensionMetadata.insert("os", this->slicerOs());
     extensionMetadata.insert("arch", this->slicerArch());
     extensionMetadata.insert("slicer_revision", this->slicerRevision());
+    }
+
+  // Enable or disable the extension if state was not already set
+  if (!extensionMetadata.contains("enabled"))
+    {
+    extensionMetadata.insert("enabled", d->NewExtensionEnabledByDefault);
     }
 
   // Gather information on dependency extensions