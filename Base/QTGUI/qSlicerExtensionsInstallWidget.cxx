Slicer/Slicer

commit 98af6f222d163e8dd20f5dec3edc6b89ab6c7fd3

ENH: ExtensionsManager: Ensure theme of extension catalog webpage is updated
See https://vuetifyjs.com/en/features/theme/#light-and-dark

Fixes #4935

Base/QTGUI/qSlicerExtensionsInstallWidget.cxx
@@ -155,6 +155,36 @@ void qSlicerExtensionsInstallWidgetPrivate::setFailurePage(const QStringList& er
 }
 
 // --------------------------------------------------------------------------
+void qSlicerExtensionsInstallWidgetPrivate::updateTheme()
+{
+  Q_Q(qSlicerExtensionsInstallWidget);
+  this->setDarkThemeEnabled(q->style()->objectName().compare("Dark Slicer", Qt::CaseInsensitive) == 0);
+}
+
+// --------------------------------------------------------------------------
+void qSlicerExtensionsInstallWidgetPrivate::setDarkThemeEnabled(bool enabled)
+{
+  Q_Q(qSlicerExtensionsInstallWidget);
+  if(!this->BrowsingEnabled)
+    {
+    return;
+    }
+  int serverAPI = this->ExtensionsManagerModel->serverAPI();
+  if (serverAPI == qSlicerExtensionsManagerModel::Midas_v1)
+    {
+    // Not supported
+    }
+  else if (serverAPI == qSlicerExtensionsManagerModel::Girder_v1)
+    {
+    q->evalJS(QString("app.$vuetify.theme.dark = %1;").arg(enabled ? "true" : "false"));
+    }
+  else
+    {
+    qWarning() << Q_FUNC_INFO << " failed: missing implementation for serverAPI" << serverAPI;
+    }
+}
+
+// --------------------------------------------------------------------------
 void qSlicerExtensionsInstallWidgetPrivate::initializeWebChannelTransport(QByteArray& webChannelScript)
 {
   this->Superclass::initializeWebChannelTransport(webChannelScript);
@@ -415,6 +445,10 @@ void qSlicerExtensionsInstallWidget::onLoadFinished(bool ok)
     d->setFailurePage(QStringList() << QString("Failed to load extension page using this URL: <strong>%1</strong>")
                       .arg(d->extensionsListUrl().toString()));
     }
+  if (ok)
+    {
+    d->updateTheme();
+    }
 }
 
 // --------------------------------------------------------------------------
@@ -423,4 +457,19 @@ bool qSlicerExtensionsInstallWidget::acceptNavigationRequest(const QUrl & url, Q
   Q_D(qSlicerExtensionsInstallWidget);
   d->InternalHosts = QStringList() << this->extensionsManagerModel()->frontendServerUrl().host();
   return Superclass::acceptNavigationRequest(url, type, isMainFrame);
+}
+
+// --------------------------------------------------------------------------
+void qSlicerExtensionsInstallWidget::changeEvent(QEvent *e)
+{
+  Q_D(qSlicerExtensionsInstallWidget);
+  switch (e->type())
+    {
+    case QEvent::StyleChange:
+      d->updateTheme();
+    break;
+    default:
+    break;
+    }
+  this->Superclass::changeEvent(e);
 }

Base/QTGUI/qSlicerExtensionsInstallWidget.h
@@ -80,6 +80,7 @@ public slots:
 
 protected:
   bool acceptNavigationRequest(const QUrl & url, QWebEnginePage::NavigationType type, bool isMainFrame) override;
+  void changeEvent(QEvent *e) override;
 
 protected slots:
   void initJavascript() override;

Base/QTGUI/qSlicerExtensionsInstallWidget_p.h
@@ -62,6 +62,9 @@ public:
 
   void setFailurePage(const QStringList &errors);
 
+  void updateTheme();
+  void setDarkThemeEnabled(bool enabled);
+
   void initializeWebChannel(QWebChannel* webChannel) override;
   void initializeWebChannelTransport(QByteArray& webChannelScript) override;
   void registerExtensionsManagerModel(qSlicerExtensionsManagerModel* oldModel, qSlicerExtensionsManagerModel* newModel);

commit f57f7c79f1ed1c1aaa1a9db704c10adf3d103901

COMP: Fix qSlicerExtensionsInstallWidget build error adding  missing include
This commit fixes the following error reported when building against Qt > 5.10
and introduced by 98af6f222d (ENH: ExtensionsManager: Ensure theme of
extension catalog webpage is updated):

  Base/QTGUI/qSlicerExtensionsInstallWidget.cxx:161:39: error: invalid use of incomplete type ‘class QStyle’

Base/QTGUI/qSlicerExtensionsInstallWidget.cxx
@@ -21,6 +21,7 @@
 // Qt includes
 #include <QDebug>
 #include <QDesktopServices>
+#include <QStyle>
 #include <QUrlQuery>
 #include <QWebEngineView>
 