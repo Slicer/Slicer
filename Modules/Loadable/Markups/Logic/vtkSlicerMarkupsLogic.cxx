Slicer/Slicer

commit 74d3ec4bb1996e73d82470195847a81703979488

ENH: Add export/import of markups to/from table node
New "Export/import table" section is added to Markups module.

Modules/Loadable/Markups/Logic/vtkSlicerMarkupsLogic.cxx
@@ -51,13 +51,16 @@
 #include "vtkMRMLSliceCompositeNode.h"
 #include "vtkMRMLSliceNode.h"
 #include "vtkMRMLSceneViewNode.h"
+#include "vtkMRMLTableNode.h"
 
 // vtkAddon includes
 #include "vtkAddonMathUtilities.h"
 
 // VTK includes
+#include <vtkBitArray.h>
 #include <vtkNew.h>
 #include <vtkObjectFactory.h>
+#include <vtkTable.h>
 
 // STD includes
 #include <cassert>
@@ -1734,4 +1737,247 @@ bool vtkSlicerMarkupsLogic::GetCreateMarkupsPushButton(const char* markupName) c
 const std::list<std::string>& vtkSlicerMarkupsLogic::GetRegisteredMarkupsTypes() const
 {
   return this->RegisteredMarkupsOrder;
+}
+
+//----------------------------------------------------------------------------
+bool vtkSlicerMarkupsLogic::ImportControlPointsFromTable(vtkMRMLMarkupsNode* markupsNode, vtkMRMLTableNode* tableNode,
+  int startRow/*=0*/, int numberOfRows/*=-1*/)
+{
+  if (!markupsNode || !tableNode || !tableNode->GetTable())
+    {
+    vtkGenericWarningMacro("vtkSlicerMarkupsLogic::ImportControlPointsFromTable failed: Invalid markupsNode or tableNode.");
+    return false;
+    }
+  if (numberOfRows < 0)
+    {
+    numberOfRows = tableNode->GetNumberOfRows();
+    }
+
+  MRMLNodeModifyBlocker blocker(markupsNode);
+
+  vtkTable* table = tableNode->GetTable();
+
+  vtkAbstractArray* arrayX = nullptr;
+  vtkAbstractArray* arrayY = nullptr;
+  vtkAbstractArray* arrayZ = nullptr;
+  bool rasCoordinates = true;
+  if (table->GetColumnByName("r") && table->GetColumnByName("a") && table->GetColumnByName("s"))
+    {
+    arrayX = table->GetColumnByName("r");
+    arrayY = table->GetColumnByName("a");
+    arrayZ = table->GetColumnByName("s");
+    }
+  else if (table->GetColumnByName("l") && table->GetColumnByName("p") && table->GetColumnByName("s"))
+    {
+    rasCoordinates = false;
+    arrayX = table->GetColumnByName("l");
+    arrayY = table->GetColumnByName("p");
+    arrayZ = table->GetColumnByName("s");
+    }
+
+  vtkAbstractArray* arrayLabel = table->GetColumnByName("label");
+  vtkAbstractArray* arrayDescription = table->GetColumnByName("description");
+  vtkAbstractArray* arraySelected = table->GetColumnByName("selected");
+  vtkAbstractArray* arrayVisible = table->GetColumnByName("visible");
+  vtkAbstractArray* arrayLocked = table->GetColumnByName("locked");
+  vtkAbstractArray* arrayDefined= table->GetColumnByName("defined");
+
+  for (int row = 0; row < numberOfRows; row++)
+    {
+    // vtkVariant cannot convert values from VTK_BIT type, therefore we need to handle it
+    // as a special case here.
+    int positionStatus = vtkMRMLMarkupsNode::PositionDefined;
+    if (vtkBitArray::SafeDownCast(arrayDefined))
+      {
+      if (vtkBitArray::SafeDownCast(arrayDefined)->GetValue(row) == 0)
+        {
+        positionStatus = vtkMRMLMarkupsNode::PositionUndefined;
+        }
+      }
+    else if (arrayDefined)
+      {
+      if (arrayDefined->GetVariantValue(row).ToInt() == 0)
+        {
+        positionStatus = vtkMRMLMarkupsNode::PositionUndefined;
+        }
+      }
+
+    vtkMRMLMarkupsNode::ControlPoint* controlPoint = new vtkMRMLMarkupsNode::ControlPoint;
+
+    bool validX = false;
+    bool validY = false;
+    bool validZ = false;
+    if (arrayX && arrayY && arrayZ)
+      {
+      controlPoint->Position[0] = arrayX->GetVariantValue(row).ToDouble(&validX);
+      controlPoint->Position[1] = arrayY->GetVariantValue(row).ToDouble(&validY);
+      controlPoint->Position[2] = arrayZ->GetVariantValue(row).ToDouble(&validZ);
+      if (!rasCoordinates)
+        {
+        controlPoint->Position[0] = -controlPoint->Position[0];
+        controlPoint->Position[1] = -controlPoint->Position[1];
+        }
+      }
+    if (!validX || !validY || !validZ)
+      {
+      controlPoint->PositionStatus = vtkMRMLMarkupsNode::PositionUndefined;
+      }
+
+    if (arrayLabel)
+      {
+      controlPoint->Label = arrayLabel->GetVariantValue(row).ToString();
+      }
+    if (arrayDescription)
+      {
+      controlPoint->Description = arrayDescription->GetVariantValue(row).ToString();
+      }
+
+    if (vtkBitArray::SafeDownCast(arraySelected))
+      {
+      controlPoint->Selected = (vtkBitArray::SafeDownCast(arraySelected)->GetValue(row) != 0);
+      }
+    else if (arraySelected)
+      {
+      controlPoint->Selected = (arraySelected->GetVariantValue(row).ToInt() != 0);
+      }
+
+    if (vtkBitArray::SafeDownCast(arrayVisible))
+      {
+      controlPoint->Visibility = (vtkBitArray::SafeDownCast(arrayVisible)->GetValue(row) != 0);
+      }
+    else if (arrayVisible)
+      {
+      controlPoint->Visibility = (arrayVisible->GetVariantValue(row).ToInt() != 0);
+      }
+
+    if (vtkBitArray::SafeDownCast(arrayLocked))
+      {
+      controlPoint->Locked = (vtkBitArray::SafeDownCast(arrayLocked)->GetValue(row) != 0);
+      }
+    else if (arrayLocked)
+      {
+      controlPoint->Locked = (arrayLocked->GetVariantValue(row).ToInt() != 0);
+      }
+
+    controlPoint->PositionStatus = positionStatus;
+
+    markupsNode->AddControlPoint(controlPoint);
+    }
+  return true;
+}
+
+//----------------------------------------------------------------------------
+bool vtkSlicerMarkupsLogic::ExportControlPointsToTable(vtkMRMLMarkupsNode* markupsNode, vtkMRMLTableNode* tableNode,
+  int coordinateSystem/*=vtkMRMLStorageNode::CoordinateSystemRAS*/)
+{
+  if (!markupsNode || !tableNode || !tableNode->GetTable())
+    {
+    vtkGenericWarningMacro("vtkSlicerMarkupsLogic::ExportControlPointsToTable failed: Invalid markupsNode or tableNode.");
+    return false;
+    }
+
+  bool rasCoordinates = (coordinateSystem != vtkMRMLStorageNode::CoordinateSystemLPS);
+
+  vtkTable* table = tableNode->GetTable();
+
+  vtkAbstractArray* arrayLabel = table->GetColumnByName("label");
+  if (!arrayLabel)
+    {
+    arrayLabel = vtkStringArray::New();
+    arrayLabel->SetName("label");
+    table->AddColumn(arrayLabel);
+    arrayLabel->UnRegister(nullptr);
+    }
+
+  // Get/create coordinate arrays
+  vtkAbstractArray* arrayCoordinates[3] = { nullptr, nullptr, nullptr };
+  std::string columnNames[3] =
+    {
+    rasCoordinates ? "r" : "l",
+    rasCoordinates ? "a" : "p",
+    "s"
+    };
+  for (int coordIndex = 0; coordIndex < 3; coordIndex++)
+    {
+    arrayCoordinates[coordIndex] = table->GetColumnByName(columnNames[coordIndex].c_str());
+    if (arrayCoordinates[coordIndex])
+      {
+      continue;
+      }
+    arrayCoordinates[coordIndex] = vtkDoubleArray::New();
+    arrayCoordinates[coordIndex]->SetName(columnNames[coordIndex].c_str());
+    tableNode->AddColumn(arrayCoordinates[coordIndex]);
+    arrayCoordinates[coordIndex]->UnRegister(nullptr);
+    }
+
+  vtkAbstractArray* arrayDefined = table->GetColumnByName("defined");
+  if (!arrayDefined)
+    {
+    arrayDefined = vtkBitArray::New();
+    arrayDefined->SetName("defined");
+    table->AddColumn(arrayDefined);
+    arrayDefined->UnRegister(nullptr);
+    }
+
+  vtkAbstractArray* arraySelected = table->GetColumnByName("selected");
+  if (!arraySelected)
+    {
+    arraySelected = vtkBitArray::New();
+    arraySelected->SetName("selected");
+    table->AddColumn(arraySelected);
+    arraySelected->UnRegister(nullptr);
+    }
+
+  vtkAbstractArray* arrayVisible = table->GetColumnByName("visible");
+  if (!arrayVisible)
+    {
+    arrayVisible = vtkBitArray::New();
+    arrayVisible->SetName("visible");
+    table->AddColumn(arrayVisible);
+    arrayVisible->UnRegister(nullptr);
+    }
+
+  vtkAbstractArray* arrayLocked = table->GetColumnByName("locked");
+  if (!arrayLocked)
+    {
+    arrayLocked = vtkBitArray::New();
+    arrayLocked->SetName("locked");
+    table->AddColumn(arrayLocked);
+    arrayLocked->UnRegister(nullptr);
+    }
+
+  vtkAbstractArray* arrayDescription = table->GetColumnByName("description");
+  if (!arrayDescription)
+    {
+    arrayDescription = vtkStringArray::New();
+    arrayDescription->SetName("description");
+    table->AddColumn(arrayDescription);
+    arrayDescription->UnRegister(nullptr);
+    }
+
+  for (int controlPointIndex = 0; controlPointIndex < markupsNode->GetNumberOfControlPoints(); controlPointIndex++)
+    {
+    int row = tableNode->AddEmptyRow();
+    vtkMRMLMarkupsNode::ControlPoint* controlPoint = markupsNode->GetNthControlPoint(controlPointIndex);
+    if (rasCoordinates)
+      {
+      arrayCoordinates[0]->SetVariantValue(row, vtkVariant(controlPoint->Position[0]));
+      arrayCoordinates[1]->SetVariantValue(row, vtkVariant(controlPoint->Position[1]));
+      }
+    else
+      {
+      arrayCoordinates[0]->SetVariantValue(row, vtkVariant(-controlPoint->Position[0]));
+      arrayCoordinates[1]->SetVariantValue(row, vtkVariant(-controlPoint->Position[1]));
+      }
+    arrayCoordinates[2]->SetVariantValue(row, vtkVariant(controlPoint->Position[2]));
+
+    arrayLabel->SetVariantValue(row, controlPoint->Label.c_str());
+    arrayDescription->SetVariantValue(row, controlPoint->Description.c_str());
+    arraySelected->SetVariantValue(row, controlPoint->Selected);
+    arrayVisible->SetVariantValue(row, controlPoint->Visibility);
+    arrayLocked->SetVariantValue(row, controlPoint->Locked);
+    arrayDefined->SetVariantValue(row, controlPoint->PositionStatus==vtkMRMLMarkupsNode::PositionDefined);
+    }
+
+  return true;
 }

Modules/Loadable/Markups/Logic/vtkSlicerMarkupsLogic.h
@@ -27,6 +27,9 @@
 // Slicer includes
 #include "vtkSlicerModuleLogic.h"
 
+// MRML includes
+#include "vtkMRMLStorageNode.h"
+
 // Widgets includes
 #include "vtkSlicerMarkupsWidget.h"
 
@@ -44,6 +47,7 @@ class vtkMRMLMarkupsDisplayNode;
 class vtkMRMLMarkupsJsonStorageNode;
 class vtkMRMLMessageCollection;
 class vtkMRMLSelectionNode;
+class vtkMRMLTableNode;
 class vtkPlane;
 class vtkPoints;
 class vtkPolyData;
@@ -291,6 +295,14 @@ public:
 
   /// This returns the flags that indicates whether the GUI push button should be created.
   bool GetCreateMarkupsPushButton(const char* markupName) const;
+
+  /// Import markups control points from table node
+  /// Column names: label, r, a, s, (or l, p, s), defined, selected, visible, locked, description.
+  static bool ImportControlPointsFromTable(vtkMRMLMarkupsNode* markupsNode, vtkMRMLTableNode* tableNode,
+    int startRow = 0, int numberOfRows = -1);
+
+  static bool ExportControlPointsToTable(vtkMRMLMarkupsNode* markupsNode, vtkMRMLTableNode* tableNode,
+    int coordinateSystem = vtkMRMLStorageNode::CoordinateSystemRAS);
 
 protected:
 

Modules/Loadable/Markups/Resources/UI/qSlicerMarkupsModule.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>599</width>
-    <height>1133</height>
+    <width>338</width>
+    <height>637</height>
    </rect>
   </property>
   <property name="sizePolicy">
@@ -31,7 +31,7 @@
      </item>
      <item>
       <widget class="ctkExpandableWidget" name="ResizableFrame">
-       <property name="sizeGripInside" stdset="0">
+       <property name="sizeGripInside">
         <bool>false</bool>
        </property>
        <layout class="QVBoxLayout" name="verticalLayout_3">
@@ -64,22 +64,22 @@
           <property name="indentation">
            <number>8</number>
           </property>
-          <property name="editMenuActionVisible" stdset="0">
+          <property name="editMenuActionVisible">
            <bool>false</bool>
           </property>
          </widget>
         </item>
         <item>
-         <widget class="qMRMLCollapsibleButton" name="displayCollapsibleButton" native="true">
-          <property name="text" stdset="0">
+         <widget class="qMRMLCollapsibleButton" name="displayCollapsibleButton">
+          <property name="text">
            <string>Display</string>
           </property>
-          <property name="collapsed" stdset="0">
+          <property name="collapsed">
            <bool>true</bool>
           </property>
           <layout class="QVBoxLayout" name="verticalLayout_4">
            <item>
-            <widget class="qMRMLMarkupsDisplayNodeWidget" name="markupsDisplayWidget" native="true"/>
+            <widget class="qMRMLMarkupsDisplayNodeWidget" name="markupsDisplayWidget"/>
            </item>
            <item>
             <layout class="QHBoxLayout" name="horizontalLayout_4">
@@ -112,11 +112,11 @@
       </widget>
      </item>
      <item>
-      <widget class="ctkCollapsibleButton" name="controlPointsCollapsibleButton" native="true">
-       <property name="text" stdset="0">
+      <widget class="ctkCollapsibleButton" name="controlPointsCollapsibleButton">
+       <property name="text">
         <string>Control Points</string>
        </property>
-       <property name="collapsed" stdset="0">
+       <property name="collapsed">
         <bool>true</bool>
        </property>
        <layout class="QFormLayout" name="formLayout">
@@ -485,7 +485,7 @@
           <property name="title">
            <string>Advanced</string>
           </property>
-          <property name="collapsed" stdset="0">
+          <property name="collapsed">
            <bool>true</bool>
           </property>
           <layout class="QVBoxLayout" name="verticalLayout">
@@ -649,11 +649,11 @@
       </widget>
      </item>
      <item>
-      <widget class="ctkCollapsibleButton" name="measurementsCollapsibleButton" native="true">
-       <property name="text" stdset="0">
+      <widget class="ctkCollapsibleButton" name="measurementsCollapsibleButton">
+       <property name="text">
         <string>Measurements</string>
        </property>
-       <property name="collapsed" stdset="0">
+       <property name="collapsed">
         <bool>true</bool>
        </property>
        <layout class="QVBoxLayout" name="verticalLayout_7">
@@ -724,10 +724,131 @@
        </layout>
       </widget>
      </item>
+     <item>
+      <widget class="qMRMLCollapsibleButton" name="exportImportCollapsibleButton">
+       <property name="text">
+        <string>Export/import Table</string>
+       </property>
+       <property name="collapsed">
+        <bool>true</bool>
+       </property>
+       <layout class="QFormLayout" name="formLayout_2">
+        <item row="0" column="0">
+         <widget class="QLabel" name="label_4">
+          <property name="text">
+           <string>Operation:</string>
+          </property>
+         </widget>
+        </item>
+        <item row="0" column="1">
+         <layout class="QHBoxLayout" name="horizontalLayout">
+          <item>
+           <widget class="QRadioButton" name="tableExportRadioButton">
+            <property name="text">
+             <string>Export</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QRadioButton" name="tableImportRadioButton">
+            <property name="text">
+             <string>Import</string>
+            </property>
+           </widget>
+          </item>
+         </layout>
+        </item>
+        <item row="1" column="0">
+         <widget class="QLabel" name="exportImportTableLabel">
+          <property name="text">
+           <string>Table:</string>
+          </property>
+         </widget>
+        </item>
+        <item row="1" column="1">
+         <widget class="qMRMLNodeComboBox" name="exportedImportedNodeComboBox">
+          <property name="nodeTypes">
+           <stringlist>
+            <string>vtkMRMLTableNode</string>
+           </stringlist>
+          </property>
+          <property name="editEnabled">
+           <bool>true</bool>
+          </property>
+          <property name="renameEnabled">
+           <bool>true</bool>
+          </property>
+         </widget>
+        </item>
+        <item row="3" column="0" colspan="2">
+         <widget class="QPushButton" name="exportImportPushButton">
+          <property name="text">
+           <string>Apply</string>
+          </property>
+         </widget>
+        </item>
+        <item row="2" column="0" colspan="2">
+         <widget class="ctkCollapsibleGroupBox" name="CollapsibleGroupBox">
+          <property name="title">
+           <string>Advanced</string>
+          </property>
+          <property name="collapsed">
+           <bool>true</bool>
+          </property>
+          <layout class="QFormLayout" name="formLayout_3">
+           <property name="horizontalSpacing">
+            <number>4</number>
+           </property>
+           <property name="verticalSpacing">
+            <number>4</number>
+           </property>
+           <property name="leftMargin">
+            <number>4</number>
+           </property>
+           <property name="topMargin">
+            <number>4</number>
+           </property>
+           <property name="rightMargin">
+            <number>4</number>
+           </property>
+           <property name="bottomMargin">
+            <number>4</number>
+           </property>
+           <item row="0" column="0">
+            <widget class="QLabel" name="label_5">
+             <property name="text">
+              <string>Export coordinate system:</string>
+             </property>
+            </widget>
+           </item>
+           <item row="0" column="1">
+            <layout class="QHBoxLayout" name="horizontalLayout_3">
+             <item>
+              <widget class="QRadioButton" name="rasExportRadioButton">
+               <property name="text">
+                <string>RAS</string>
+               </property>
+              </widget>
+             </item>
+             <item>
+              <widget class="QRadioButton" name="lpsExportRadioButton">
+               <property name="text">
+                <string>LPS</string>
+               </property>
+              </widget>
+             </item>
+            </layout>
+           </item>
+          </layout>
+         </widget>
+        </item>
+       </layout>
+      </widget>
+     </item>
     </layout>
    </item>
    <item>
-    <widget class="ctkDynamicSpacer" name="DynamicSpacer" native="true">
+    <widget class="ctkDynamicSpacer" name="DynamicSpacer">
      <property name="sizePolicy">
       <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
        <horstretch>0</horstretch>
@@ -740,15 +861,15 @@
  </widget>
  <customwidgets>
   <customwidget>
-   <class>qSlicerWidget</class>
-   <extends>QWidget</extends>
-   <header>qSlicerWidget.h</header>
+   <class>qMRMLCollapsibleButton</class>
+   <extends>ctkCollapsibleButton</extends>
+   <header>qMRMLCollapsibleButton.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
-   <class>ctkCollapsibleButton</class>
+   <class>qMRMLNodeComboBox</class>
    <extends>QWidget</extends>
-   <header>ctkCollapsibleButton.h</header>
+   <header>qMRMLNodeComboBox.h</header>
    <container>1</container>
   </customwidget>
   <customwidget>
@@ -807,12 +928,6 @@
    <extends>QPushButton</extends>
    <header>ctkMenuButton.h</header>
   </customwidget>
-  <customwidget>
-   <class>qMRMLCollapsibleButton</class>
-   <extends>ctkCollapsibleButton</extends>
-   <header>qMRMLCollapsibleButton.h</header>
-   <container>1</container>
-  </customwidget>
  </customwidgets>
  <resources>
   <include location="../qSlicerMarkupsModule.qrc"/>
@@ -845,8 +960,24 @@
      <y>2</y>
     </hint>
     <hint type="destinationlabel">
-     <x>109</x>
-     <y>203</y>
+     <x>128</x>
+     <y>259</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>qSlicerMarkupsModule</sender>
+   <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
+   <receiver>exportedImportedNodeComboBox</receiver>
+   <slot>setMRMLScene(vtkMRMLScene*)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>168</x>
+     <y>262</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>197</x>
+     <y>387</y>
     </hint>
    </hints>
   </connection>

Modules/Loadable/Markups/qSlicerMarkupsModuleWidget.cxx
@@ -17,6 +17,7 @@
 
 // Qt includes
 #include <QApplication>
+#include <QButtonGroup>
 #include <QClipboard>
 #include <QDebug>
 #include <QInputDialog>
@@ -50,6 +51,7 @@
 #include "vtkMRMLSliceLogic.h"
 #include "vtkMRMLSliceNode.h"
 #include "vtkMRMLSubjectHierarchyNode.h"
+#include "vtkMRMLTableNode.h"
 
 // MRMLDM includes
 #include "vtkMRMLMarkupsDisplayableManager.h"
@@ -144,6 +146,10 @@ private:
   // Dynamic list of create markups push buttons
   QList<QPushButton*> ceateMarkupsPushButtons;
   unsigned int createMarkupsButtonsColumns;
+
+  // Export/import setion
+  QButtonGroup* ImportExportOperationButtonGroup;
+  QButtonGroup* ImportExportCoordinateSystemButtonGroup;
 };
 
 //-----------------------------------------------------------------------------
@@ -172,13 +178,15 @@ qSlicerMarkupsModuleWidgetPrivate::qSlicerMarkupsModuleWidgetPrivate(qSlicerMark
   this->copyAction = nullptr;
   this->pasteAction = nullptr;
 
-
   this->SlicerLockIcon = QPixmap(":/Icons/Small/SlicerLock.png");
   this->SlicerUnlockIcon = QPixmap(":/Icons/Small/SlicerUnlock.png");
   this->SlicerVisibleIcon = QPixmap(":/Icons/Small/SlicerVisible.png");
   this->SlicerInvisibleIcon = QPixmap(":/Icons/Small/SlicerInvisible.png");
 
   this->createMarkupsButtonsColumns = 0;
+
+  this->ImportExportOperationButtonGroup = nullptr;
+  this->ImportExportCoordinateSystemButtonGroup = nullptr;
 }
 
 //-----------------------------------------------------------------------------
@@ -470,6 +478,29 @@ void qSlicerMarkupsModuleWidgetPrivate::setupUi(qSlicerWidget* widget)
 
   // hide measurement settings table until markups node containing measurement is set
   this->measurementSettingsTableWidget->setVisible(false);
+
+  // Export/import
+  this->ImportExportOperationButtonGroup = new QButtonGroup(this->exportImportCollapsibleButton);
+  this->ImportExportOperationButtonGroup->addButton(this->tableExportRadioButton);
+  this->ImportExportOperationButtonGroup->addButton(this->tableImportRadioButton);
+
+  this->ImportExportCoordinateSystemButtonGroup = new QButtonGroup(this->exportImportCollapsibleButton);
+  this->ImportExportCoordinateSystemButtonGroup->addButton(this->lpsExportRadioButton);
+  this->ImportExportCoordinateSystemButtonGroup->addButton(this->rasExportRadioButton);
+
+  this->tableExportRadioButton->setChecked(true);
+  this->rasExportRadioButton->setChecked(true);
+
+  QObject::connect(this->exportedImportedNodeComboBox, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
+    q, SLOT(updateImportExportWidgets()));
+
+  QObject::connect(this->ImportExportOperationButtonGroup, SIGNAL(buttonClicked(QAbstractButton*)),
+    q, SLOT(updateImportExportWidgets()));
+
+  QObject::connect(this->exportImportPushButton, SIGNAL(clicked()),
+    q, SLOT(onImportExportApply()));
+
+  q->updateImportExportWidgets();
 }
 
 //-----------------------------------------------------------------------------
@@ -2704,6 +2735,56 @@ for (int i=0; i<d->MarkupsNode->Measurements->GetNumberOfItems(); ++i)
 //-----------------------------------------------------------------------------
 void qSlicerMarkupsModuleWidget::setCreateMarkupsButtonsColumns(unsigned int columns)
 {
-Q_D(qSlicerMarkupsModuleWidget);
-d->createMarkupsButtonsColumns = columns;
+  Q_D(qSlicerMarkupsModuleWidget);
+  d->createMarkupsButtonsColumns = columns;
+}
+
+//-----------------------------------------------------------------------------
+void qSlicerMarkupsModuleWidget::updateImportExportWidgets()
+{
+  Q_D(qSlicerMarkupsModuleWidget);
+  bool isExport = d->tableExportRadioButton->isChecked();
+  if (isExport)
+    {
+    // Export
+    d->exportImportTableLabel->setText("Output table:");
+    d->exportImportPushButton->setText("Export");
+    d->exportImportPushButton->setToolTip(tr("Export control points coordinates and properties to table."));
+    }
+  else
+    {
+    // Import
+    d->exportImportTableLabel->setText("Input table:");
+    d->exportImportPushButton->setText("Import");
+    d->exportImportPushButton->setToolTip(
+      tr("Import control points coordinates and properties from table node.\n"
+      "Table column names : label, r, a, s, (or l, p, s), defined, selected, visible, locked, description."));
+    }
+  d->lpsExportRadioButton->setEnabled(isExport);
+  d->rasExportRadioButton->setEnabled(isExport);
+  d->exportImportPushButton->setEnabled(d->exportedImportedNodeComboBox->currentNode() != nullptr);
+}
+
+//-----------------------------------------------------------------------------
+void qSlicerMarkupsModuleWidget::onImportExportApply()
+{
+  Q_D(qSlicerMarkupsModuleWidget);
+  if (!this->markupsLogic())
+    {
+    return;
+    }
+  QApplication::setOverrideCursor(QCursor(Qt::WaitCursor));
+  if (d->tableExportRadioButton->isChecked())
+    {
+    // export
+    int coordinateSystem = d->rasExportRadioButton->isChecked() ? vtkMRMLStorageNode::CoordinateSystemRAS : vtkMRMLStorageNode::CoordinateSystemLPS;
+    this->markupsLogic()->ExportControlPointsToTable(d->MarkupsNode,
+      vtkMRMLTableNode::SafeDownCast(d->exportedImportedNodeComboBox->currentNode()), coordinateSystem);
+    }
+  else
+    {
+    this->markupsLogic()->ImportControlPointsFromTable(d->MarkupsNode,
+      vtkMRMLTableNode::SafeDownCast(d->exportedImportedNodeComboBox->currentNode()));
+    }
+  QApplication::restoreOverrideCursor();
 }

Modules/Loadable/Markups/qSlicerMarkupsModuleWidget.h
@@ -249,6 +249,10 @@ public slots:
   /// Update set of create markups push buttons
   void onPlaceMarkupsAdditionalOptionsWidgets();
 
+  void updateImportExportWidgets();
+
+  void onImportExportApply();
+
 protected:
   QScopedPointer<qSlicerMarkupsModuleWidgetPrivate> d_ptr;
 

commit f4d61fe236aae7a4a28065c3329b1c42ca790805

BUG: Update MarkupsLogic::ImportControlPointsFromTable to use startRow
This regression was introduced in 74d3ec4bb (ENH: Add export/import
of markups to/from table node) and was identified thanks to
-Wunused-parameter warning.

Co-authored-by: Andras Lasso <lasso@queensu.ca>

Modules/Loadable/Markups/Logic/vtkSlicerMarkupsLogic.cxx
@@ -1772,16 +1772,15 @@ const std::list<std::string>& vtkSlicerMarkupsLogic::GetRegisteredMarkupsTypes()
 bool vtkSlicerMarkupsLogic::ImportControlPointsFromTable(vtkMRMLMarkupsNode* markupsNode, vtkMRMLTableNode* tableNode,
   int startRow/*=0*/, int numberOfRows/*=-1*/)
 {
-  if (!markupsNode || !tableNode || !tableNode->GetTable())
+  if (!markupsNode || !tableNode || !tableNode->GetTable() || startRow < 0)
     {
-    vtkGenericWarningMacro("vtkSlicerMarkupsLogic::ImportControlPointsFromTable failed: Invalid markupsNode or tableNode.");
+    vtkGenericWarningMacro("vtkSlicerMarkupsLogic::ImportControlPointsFromTable failed: Invalid markupsNode or tableNode or startRow.");
     return false;
     }
-  if (numberOfRows < 0)
+  if (numberOfRows < 0 || numberOfRows > tableNode->GetNumberOfRows() - startRow)
     {
-    numberOfRows = tableNode->GetNumberOfRows();
+    numberOfRows = tableNode->GetNumberOfRows() - startRow;
     }
-
   MRMLNodeModifyBlocker blocker(markupsNode);
 
   vtkTable* table = tableNode->GetTable();
@@ -1811,7 +1810,7 @@ bool vtkSlicerMarkupsLogic::ImportControlPointsFromTable(vtkMRMLMarkupsNode* mar
   vtkAbstractArray* arrayLocked = table->GetColumnByName("locked");
   vtkAbstractArray* arrayDefined= table->GetColumnByName("defined");
 
-  for (int row = 0; row < numberOfRows; row++)
+  for (int row = startRow; row < startRow + numberOfRows; row++)
     {
     // vtkVariant cannot convert values from VTK_BIT type, therefore we need to handle it
     // as a special case here.