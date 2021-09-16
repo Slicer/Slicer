e58a85022630186ad8ad580c6cbff754b130f514
Andras Lasso <lasso@queensu.ca>
2021-09-16 07:33:00 -0400

    BUG: Update SurfaceToolbox to fix parameter name for boundary deletion
    List of changes:
    
    Revision: 7e88cda5f28a5a6bfff1c437db56469f0d5afc9f
    Author: Sara Rolfe <43060230+smrolfe@users.noreply.github.com>
    Date: 2021-09-14 9:29:24 PM
    Message:
    BUG: Correct parameter name for boundary deletion

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -362,7 +362,7 @@ list_conditional_append(Slicer_BUILD_LandmarkRegistration Slicer_REMOTE_DEPENDEN
 Slicer_Remote_Add(SurfaceToolbox
   GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/Slicer/SlicerSurfaceToolbox"
   # When updating the GIT_TAG, consider updating documentation in Docs/user_guide (see https://github.com/Slicer/Slicer/issues/5669)
-  GIT_TAG 362684dced4d3202f3947ee47ac662110a4405b9
+  GIT_TAG 7e88cda5f28a5a6bfff1c437db56469f0d5afc9f
   OPTION_NAME Slicer_BUILD_SurfaceToolbox
   OPTION_DEPENDS "Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
