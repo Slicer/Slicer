Slicer/Slicer

commit 85e6328aa00e9f0611bf80e29b2737a800f3c0e5

ENH: Add a property to hide Record and Snapshot buttons in qMRMLSequenceBrowserPlayWidget
Co-authored-by: Andras Lasso <lasso@queensu.ca>

Modules/Loadable/Sequences/Widgets/qMRMLSequenceBrowserPlayWidget.cxx
@@ -37,6 +37,7 @@ protected:
   qMRMLSequenceBrowserPlayWidget* const q_ptr;
 public:
   qMRMLSequenceBrowserPlayWidgetPrivate(qMRMLSequenceBrowserPlayWidget& object);
+  bool RecordingControlsVisible;
   void init();
 
   vtkWeakPointer<vtkMRMLSequenceBrowserNode> SequenceBrowserNode;
@@ -147,9 +148,10 @@ void qMRMLSequenceBrowserPlayWidget::updateWidgetFromMRML()
   bool recordingAllowed = d->SequenceBrowserNode->IsAnySequenceNodeRecording();
   bool playbackActive = d->SequenceBrowserNode->GetPlaybackActive();
   bool recordingActive = d->SequenceBrowserNode->GetRecordingActive();
-  d->pushButton_VcrRecord->setVisible(recordingAllowed);
+
+  d->pushButton_VcrRecord->setVisible(recordingAllowed && this->RecordingControlsVisible);
   d->pushButton_VcrRecord->setEnabled(!playbackActive);
-  d->pushButton_Snapshot->setVisible(recordingAllowed);
+  d->pushButton_Snapshot->setVisible(recordingAllowed && this->RecordingControlsVisible);
   d->pushButton_Snapshot->setEnabled(!playbackActive && !recordingActive);
 
   foreach( QObject*w, vcrPlaybackControls ) { w->setProperty( "enabled", vcrControlsEnabled ); }
@@ -324,3 +326,19 @@ void qMRMLSequenceBrowserPlayWidget::setNextFrameShortcut(QString keySequence)
   QObject::connect(new QShortcut(QKeySequence(keySequence), this), SIGNAL(activated()), SLOT(onVcrNext()));
   d->pushButton_VcrNext->setToolTip(d->pushButton_VcrNext->toolTip() + " (" + keySequence + ")");
 }
+
+//---------------------------------------------------------------------------
+void qMRMLSequenceBrowserPlayWidget::setRecordingControlsVisible(bool show)
+{
+  Q_D(qMRMLSequenceBrowserPlayWidget);
+  d->RecordingControlsVisible = show;
+  this->updateWidgetFromMRML();
+}
+
+//---------------------------------------------------------------------------
+bool qMRMLSequenceBrowserPlayWidget::recordingControlsVisible() const
+{
+  Q_D(const qMRMLSequenceBrowserPlayWidget);
+  return d->RecordingControlsVisible;
+}
+

Modules/Loadable/Sequences/Widgets/qMRMLSequenceBrowserPlayWidget.h
@@ -44,6 +44,12 @@ class Q_SLICER_MODULE_SEQUENCES_WIDGETS_EXPORT qMRMLSequenceBrowserPlayWidget
   Q_OBJECT
   QVTK_OBJECT
 
+  /// Enable displaying recording control buttons (record and snapshot).
+  /// The buttons are only visible if this flag is enabled and there is at least one sequence that
+  /// has recording enabled.
+  Q_PROPERTY(bool RecordingControlsVisible READ recordingControlsVisible WRITE setRecordingControlsVisible)
+
+
 public:
   typedef qMRMLWidget Superclass;
   qMRMLSequenceBrowserPlayWidget(QWidget *newParent = 0);
@@ -58,6 +64,9 @@ public:
   /// Add a keyboard shortcut for next frame button
   void setNextFrameShortcut(QString keySequence);
 
+  /// Get RecordingControlsVisible value
+  bool recordingControlsVisible() const;
+
 public slots:
   void setMRMLSequenceBrowserNode(vtkMRMLSequenceBrowserNode* browserNode);
   void setMRMLSequenceBrowserNode(vtkMRMLNode* browserNode);
@@ -65,6 +74,7 @@ public slots:
   void setRecordingEnabled(bool play);
   void setPlaybackRateFps(double playbackRateFps);
   void setPlaybackLoopEnabled(bool loopEnabled);
+  void setRecordingControlsVisible(bool show);
   void onVcrFirst();
   void onVcrPrevious();
   void onVcrNext();

commit b73160d1d755dc037da94abac6b095714aa7166f

COMP: Fix qMRMLSequenceBrowserPlayWidget build error
This commit fixes a regression introduced in 85e6328aa0 (ENH: Add a property
to hide Record and Snapshot buttons in qMRMLSequenceBrowserPlayWidget)

See #5878

Modules/Loadable/Sequences/Widgets/qMRMLSequenceBrowserPlayWidget.cxx
@@ -149,9 +149,9 @@ void qMRMLSequenceBrowserPlayWidget::updateWidgetFromMRML()
   bool playbackActive = d->SequenceBrowserNode->GetPlaybackActive();
   bool recordingActive = d->SequenceBrowserNode->GetRecordingActive();
 
-  d->pushButton_VcrRecord->setVisible(recordingAllowed && this->RecordingControlsVisible);
+  d->pushButton_VcrRecord->setVisible(recordingAllowed && d->RecordingControlsVisible);
   d->pushButton_VcrRecord->setEnabled(!playbackActive);
-  d->pushButton_Snapshot->setVisible(recordingAllowed && this->RecordingControlsVisible);
+  d->pushButton_Snapshot->setVisible(recordingAllowed && d->RecordingControlsVisible);
   d->pushButton_Snapshot->setEnabled(!playbackActive && !recordingActive);
 
   foreach( QObject*w, vcrPlaybackControls ) { w->setProperty( "enabled", vcrControlsEnabled ); }