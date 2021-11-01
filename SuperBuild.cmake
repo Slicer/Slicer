4a7f8c0fe1f95c65c9503437d22021226aed94ef
Andras Lasso <lasso@queensu.ca>
2021-11-01 13:02:39 -0400

    COMP: Fix linux and mac build error

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -362,7 +362,7 @@ list_conditional_append(Slicer_BUILD_LandmarkRegistration Slicer_REMOTE_DEPENDEN
 Slicer_Remote_Add(SurfaceToolbox
   GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/Slicer/SlicerSurfaceToolbox"
   # When updating the GIT_TAG, consider updating documentation in Docs/user_guide (see https://github.com/Slicer/Slicer/issues/5669)
-  GIT_TAG 3e4245303e1d1599d4ef514373711598d6bd62c6
+  GIT_TAG 8dd9ce0532dcb5e5c5f50d095bd15954192d02cb
   OPTION_NAME Slicer_BUILD_SurfaceToolbox
   OPTION_DEPENDS "Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE
