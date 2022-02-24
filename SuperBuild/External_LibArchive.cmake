Slicer/Slicer

commit e5c5ad7801b81c99a0e37b44ce570e9acbfdb7f0

COMP: Update LibArchive from 3.4.3 to 3.5.2 to support GCC v11
This commit fixes the following error reported when LibArchive
external project is compiled gcc-11.2.0 in Gentoo Linux:

  FAILED: libarchive/CMakeFiles/archive_static.dir/archive_read_support_format_warc.c.o
  [...]
  /home/rafael/src/Slicer/Slicer-master/build/LibArchive/libarchive/archive_read_support_format_warc.c:610:25: error: argument 1 of type ‘const char *’ declared as a pointer [-Werror=array-parameter=]
    610 | _warc_rdver(const char *buf, size_t bsz)
        |             ~~~~~~~~~~~~^~~
  /home/rafael/src/Slicer/Slicer-master/build/LibArchive/libarchive/archive_read_support_format_warc.c:130:44: note: previously declared as an array ‘const char[10]’
    130 | static unsigned int _warc_rdver(const char buf[10], size_t bsz);
        |                                 ~~~~~~~~~~~^~~~~~~

The specific commit addressing the issue is libarchive/libarchive@22568630f (Repair
gcc11 build error in archive_read_support_format_warc.c)

Fixes #6144

List of changes:

$ git shortlog 34940ef6e..1b2c437b9 --no-merges
Adrian Ebeling (2):
      7zip: Use compression settings for file header
      Use tabs for indentation

Alex Richardson (3):
      Avoid mismatch between library and test crypto configuration
      warc: Fix undefined behaviour in deconst() function
      Silence stderr in test_read_append_filter_program

Alexandre Janniaux (2):
      configure.ac: add AC_PROG_CPP
      configure.ac: remove trailing characters

Andy Brown (1):
      Windows: Use full path including prefix when looking up file handle

Anton Kochkov (1):
      archive_entry.c is not 3-clause UC Regents License

Brad King (1):
      Use uint8_t instead of u_char

Christos Zoulas (11):
      Extract common transfer setting code for statfs and statvfs. 1. Makes detection of f_iosize constent. 2. Avoid infinite loops by detecting 0 sizes and converting to -1.    This happens with FUSE. NetBSD PR/56083.
      Fix typo

SuperBuild/External_LibArchive.cmake
@@ -44,7 +44,7 @@ if((NOT DEFINED LibArchive_INCLUDE_DIR
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "34940ef6ea0b21d77cb501d235164ad88f19d40c"  # master (v3.4.3) with fix-enum-cases
+    "1b2c437b99b361c7692538fa373e99955e9b93ae" # master v3.5.2
     QUIET
     )
 

commit 2ce004f1664cf3424bbe70f5ed9d08516efb8ffd

COMP: Fix LibArchive external project install step on macOS
This commit fixes a regression introduced in e5c5ad780 (COMP: Update
LibArchive from 3.4.3 to 3.5.2 to support GCC v11), it addresses the
following error by updating the interface number hard-coded in the library
path used in the "fix_rpath" external project step specific to macOS.

Error:

  error: /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/install_name_tool:
  can't open file: /path/to/S-0-build/LibArchive-install/lib/libarchive.17.dylib (No such file or directory)

See #6219

SuperBuild/External_LibArchive.cmake
@@ -97,7 +97,7 @@ if((NOT DEFINED LibArchive_INCLUDE_DIR
     )
   if(APPLE)
     ExternalProject_Add_Step(${proj} fix_rpath
-      COMMAND install_name_tool -id ${EP_INSTALL_DIR}/lib/libarchive.17.dylib ${EP_INSTALL_DIR}/lib/libarchive.17.dylib
+      COMMAND install_name_tool -id ${EP_INSTALL_DIR}/lib/libarchive.18.dylib ${EP_INSTALL_DIR}/lib/libarchive.18.dylib
       DEPENDEES install
       )
   endif()