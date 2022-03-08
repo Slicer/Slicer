Slicer/Slicer

commit 716ed4a1df486241b8e13e757a470d77ca43eccc

BUG: Fix character encoding of command-line arguments
Use QString's built-in toWCharArray converter because that is guaranteed to convert correctly to wchar.
Python's Py_DecodeLocale converter only works well for the QString's UTF-8 output if Python determines
that locale is UTF-8 (which should be the case most of the time, but there might be special environments
where it is not).

Base/QTCore/qSlicerCoreApplication.cxx
@@ -127,6 +127,20 @@
 #endif
 
 //-----------------------------------------------------------------------------
+// Helper function
+
+namespace
+{
+wchar_t* QStringToPythonWCharPointer(QString str)
+  {
+  wchar_t* res = (wchar_t*)PyMem_RawMalloc(str.size()+1);
+  int len = str.toWCharArray(res);
+  res[len] = 0; // ensure zero termination
+  return res;
+  }
+}
+
+//-----------------------------------------------------------------------------
 // qSlicerCoreApplicationPrivate methods
 
 //-----------------------------------------------------------------------------
@@ -954,14 +968,10 @@ void qSlicerCoreApplication::handleCommandLineArguments()
 
     int pythonArgc = 1 /*scriptname*/ + scriptArgs.count();
     wchar_t** pythonArgv = new wchar_t*[pythonArgc];
-    //pythonArgv[0] = new wchar_t[pythonScript.size() + 1];
-    //pythonScript.toWCharArray(pythonArgv[0]);
-    pythonArgv[0] = Py_DecodeLocale(pythonScript.toUtf8(), nullptr);
+    pythonArgv[0] = QStringToPythonWCharPointer(pythonScript);
     for(int i = 0; i < scriptArgs.count(); ++i)
       {
-      //pythonArgv[i + 1] = new wchar_t[scriptArgs.at(i).size() + 1];
-      //scriptArgs.at(i).toWCharArray(pythonArgv[i + 1]);
-      pythonArgv[i + 1] = Py_DecodeLocale(scriptArgs.at(i).toUtf8(), nullptr);
+      pythonArgv[i + 1] = QStringToPythonWCharPointer(scriptArgs.at(i));
       }
 
     // See http://docs.python.org/c-api/init.html

commit 0c4bc036ad84a91b4bb0e25f98d576197382b836

COMP: Fix build with Slicer_USE_PYTHONQT set to OFF
This commit fixes a regression introduced in 716ed4a1d (BUG: Fix
character encoding of command-line arguments)

Base/QTCore/qSlicerCoreApplication.cxx
@@ -132,6 +132,7 @@
 //-----------------------------------------------------------------------------
 // Helper function
 
+#ifdef Slicer_USE_PYTHONQT
 namespace
 {
 wchar_t* QStringToPythonWCharPointer(QString str)
@@ -142,6 +143,7 @@ wchar_t* QStringToPythonWCharPointer(QString str)
   return res;
   }
 }
+#endif
 
 //-----------------------------------------------------------------------------
 // qSlicerCoreApplicationPrivate methods