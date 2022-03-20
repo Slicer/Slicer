84065636881e576490a2af503d5aa7ee5b14eb3c
James Butler <jbutler@sonovol.com>
2022-03-20 12:47:35 -0400

    STYLE: Update ITK website links to https versions

--- Libs/vtkITK/itkConstrainedValueMultiplicationImageFilter.h
+++ Libs/vtkITK/itkConstrainedValueMultiplicationImageFilter.h
@@ -7,7 +7,7 @@
   Version:   $Revision: 1.7 $
 
   Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+  See ITKCopyright.txt or https://www.itk.org/HTML/Copyright.htm for details.
 
      This software is distributed WITHOUT ANY WARRANTY; without even
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
--- Modules/CLI/ResampleDTIVolume/Testing/itkDifferenceDiffusionTensor3DImageFilter.h
+++ Modules/CLI/ResampleDTIVolume/Testing/itkDifferenceDiffusionTensor3DImageFilter.h
@@ -5,7 +5,7 @@
   Language:  C++
 
   Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+  See ITKCopyright.txt or https://www.itk.org/HTML/Copyright.htm for details.
 
      This software is distributed WITHOUT ANY WARRANTY; without even
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
