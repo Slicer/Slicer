Slicer/Slicer

commit abd2544d4f9bc4c790b3a465f7fd3f6cd954cf62

BUG: Fixed application window not clickable when popup is displayed
On Windows, some popup windows (Add data, Settings, Extensions manager, etc.) prevented switching to the application. When the user clicked on the application window, the application window or the popup was not raised to the top.

How to reproduce:
- Open "Add data" window
- Switch to another application that covers part of the Slicer application window
- Click on the application window (outside the "Add data" window) => ERROR: the application window is not brought to the top.

The problem was that the parent of "Add data" window (and a number of other popups) did not set the application main window as parent. On Windows, modal popups prevent processing events in all other windows, therefore clicking on the application main window has no effect, unless it is parent of the popup window.

Fixed the issue by setting the main window as parent for all modal popups.

Base/QTGUI/qSlicerApplication.cxx
@@ -177,13 +177,17 @@ qSlicerApplicationPrivate::qSlicerApplicationPrivate(
 //-----------------------------------------------------------------------------
 qSlicerApplicationPrivate::~qSlicerApplicationPrivate()
 {
+  // Delete settings dialog. deleteLater would cause mamory leaks on exit.
+  this->SettingsDialog->setParent(nullptr);
   delete this->SettingsDialog;
   this->SettingsDialog = nullptr;
+
 #ifdef Slicer_BUILD_EXTENSIONMANAGER_SUPPORT
   if(this->ExtensionsManagerDialog)
     {
+    this->ExtensionsManagerDialog->setParent(nullptr);
     delete this->ExtensionsManagerDialog;
-    this->ExtensionsManagerDialog =nullptr;
+    this->ExtensionsManagerDialog = nullptr;
     }
 #endif
 #ifdef Slicer_USE_QtTesting
@@ -266,6 +270,7 @@ void qSlicerApplicationPrivate::init()
   //----------------------------------------------------------------------------
   // Settings Dialog
   //----------------------------------------------------------------------------
+  // mainWindow is still nullptr, so the parent will be set later.
   this->SettingsDialog = new ctkSettingsDialog(nullptr);
   this->SettingsDialog->setResetButton(true);
   // Some settings panels are quite large, show maximize button to allow resizing with a single click
@@ -623,12 +628,13 @@ void qSlicerApplication::confirmRestart(QString reason)
     reason = tr("Are you sure you want to restart?");
     }
 
-  ctkMessageBox confirmDialog;
-  confirmDialog.setText(reason);
-  confirmDialog.setIcon(QMessageBox::Question);
-  confirmDialog.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
-  confirmDialog.setDontShowAgainSettingsKey( "MainWindow/DontConfirmRestart" );
-  bool restartConfirmed = (confirmDialog.exec() == QMessageBox::Ok);
+  ctkMessageBox* confirmDialog = new ctkMessageBox(this->mainWindow());
+  confirmDialog->setText(reason);
+  confirmDialog->setIcon(QMessageBox::Question);
+  confirmDialog->setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+  confirmDialog->setDontShowAgainSettingsKey( "MainWindow/DontConfirmRestart" );
+  bool restartConfirmed = (confirmDialog->exec() == QMessageBox::Ok);
+  confirmDialog->deleteLater();
 
   if (restartConfirmed)
     {
@@ -728,15 +734,27 @@ void qSlicerApplication::openSettingsDialog(const QString& settingsPanel/*=QStri
 {
   Q_D(qSlicerApplication);
 
+  if (!d->SettingsDialog->parent() && this->mainWindow())
+    {
+    // Set the parent before displaying the dialog to make sure that
+    // when the user clicks on the application screen (even if outside the dialog)
+    // then the main window is brought to the front.
+
+    // setParent resets window flags, so save them and then restore
+    Qt::WindowFlags windowFlags = d->SettingsDialog->windowFlags();
+    d->SettingsDialog->setParent(this->mainWindow());
+    d->SettingsDialog->setWindowFlags(windowFlags);
+    }
+
   // Reload settings to apply any changes that have been made outside of the
   // dialog (e.g. changes to module paths due to installing extensions). See
   // https://github.com/Slicer/Slicer/issues/3658.
   d->SettingsDialog->reloadSettings();
 
   if (!settingsPanel.isNull())
-  {
+    {
     d->SettingsDialog->setCurrentPanel(settingsPanel);
-  }
+    }
 
   // Now show the dialog
   d->SettingsDialog->exec();
@@ -786,7 +804,7 @@ void qSlicerApplication::openExtensionsManagerDialog()
 
   if(!d->ExtensionsManagerDialog)
     {
-    d->ExtensionsManagerDialog = new qSlicerExtensionsManagerDialog(nullptr);
+    d->ExtensionsManagerDialog = new qSlicerExtensionsManagerDialog(this->mainWindow());
     }
   if (!d->ExtensionsManagerDialog->extensionsManagerModel() &&
       this->mainWindow())

Base/QTGUI/qSlicerDataDialog.cxx
@@ -23,6 +23,7 @@
 #include <QComboBox>
 #include <QDropEvent>
 #include <QFileDialog>
+#include <QMainWindow>
 #include <QMimeData>
 #include <QMessageBox>
 #include <QTemporaryDir>
@@ -46,8 +47,9 @@
 #include <vtkNew.h>
 
 //-----------------------------------------------------------------------------
-qSlicerDataDialogPrivate::qSlicerDataDialogPrivate(QWidget* _parent)
+qSlicerDataDialogPrivate::qSlicerDataDialogPrivate(qSlicerDataDialog* object, QWidget* _parent/*=nullptr*/)
   :QDialog(_parent)
+  ,q_ptr(object)
 {
   this->setupUi(this);
 
@@ -503,9 +505,8 @@ void qSlicerDataDialogPrivate::updateCheckBoxHeader(int itemRow, int itemColumn)
 //-----------------------------------------------------------------------------
 qSlicerDataDialog::qSlicerDataDialog(QObject* _parent)
   : qSlicerFileDialog(_parent)
-  , d_ptr(new qSlicerDataDialogPrivate(nullptr))
+  , d_ptr(new qSlicerDataDialogPrivate(this, nullptr))
 {
-  // FIXME give qSlicerDataDialog as a parent of qSlicerDataDialogPrivate;
 }
 
 //-----------------------------------------------------------------------------
@@ -584,7 +585,25 @@ bool qSlicerDataDialog::exec(const qSlicerIO::IOProperties& readerProperties)
     }
 
   bool success = false;
-  if (d->exec() != QDialog::Accepted)
+
+  Qt::WindowFlags windowFlags = d->windowFlags();
+  qSlicerApplication* app = qSlicerApplication::application();
+  QWidget* mainWindow = app ? app->mainWindow() : nullptr;
+  if (mainWindow)
+    {
+    // setParent resets window flags, so save them and then restore
+    Qt::WindowFlags windowFlags = d->windowFlags();
+    d->setParent(mainWindow);
+    d->setWindowFlags(windowFlags);
+    }
+  int result = d->exec();
+  if (mainWindow)
+    {
+    d->setParent(nullptr);
+    d->setWindowFlags(windowFlags);
+    }
+
+  if (result != QDialog::Accepted)
     {
     d->reset();
     return success;

Base/QTGUI/qSlicerDataDialog_p.h
@@ -34,8 +34,9 @@ class qSlicerDataDialogPrivate
   , public Ui_qSlicerDataDialog
 {
   Q_OBJECT
+  Q_DECLARE_PUBLIC(qSlicerDataDialog);
 public:
-  explicit qSlicerDataDialogPrivate(QWidget* _parent=nullptr);
+  explicit qSlicerDataDialogPrivate(qSlicerDataDialog* object, QWidget* _parent=nullptr);
   ~qSlicerDataDialogPrivate() override;
 
   QList<qSlicerIO::IOProperties> selectedFiles()const;
@@ -53,6 +54,8 @@ protected slots:
 //  void updateCheckBoxHeader(int row, int column);
 
 protected:
+  qSlicerDataDialog* const q_ptr;
+
   enum ColumnType
   {
     FileColumn = 0,

Base/QTGUI/qSlicerFileDialog.cxx
@@ -20,6 +20,7 @@
 
 // Qt includes
 #include <QDebug>
+#include <QMainWindow>
 
 // CTK includes
 #include <ctkFileDialog.h>
@@ -176,7 +177,7 @@ QStringList qSlicerStandardFileDialog::loadedNodes()const
 
 //-----------------------------------------------------------------------------
 ctkFileDialog* qSlicerStandardFileDialog::createFileDialog(
-    const qSlicerIO::IOProperties& ioProperties)
+    const qSlicerIO::IOProperties& ioProperties, QWidget* parent/*=nullptr*/)
 {
   if(ioProperties["objectName"].toString().isEmpty())
     {
@@ -185,7 +186,7 @@ ctkFileDialog* qSlicerStandardFileDialog::createFileDialog(
     }
 
   qSlicerIOManager* ioManager = qSlicerApplication::application()->ioManager();
-  ctkFileDialog* fileDialog = new ctkFileDialog();
+  ctkFileDialog* fileDialog = new ctkFileDialog(parent);
 
   if(ioProperties["fileType"].toBool())
     {
@@ -268,8 +269,10 @@ bool qSlicerStandardFileDialog::exec(const qSlicerIO::IOProperties& ioProperties
 
   qSlicerIO::IOProperties properties = ioProperties;
   properties["fileType"] = d->FileType;
+  qSlicerApplication* app = qSlicerApplication::application();
+  QWidget* mainWindow = app ? app->mainWindow() : nullptr;
   ctkFileDialog* fileDialog = qSlicerStandardFileDialog::createFileDialog(
-                                properties);
+                                properties, mainWindow);
   QFileDialog::AcceptMode  acceptMode = (d->Action == qSlicerFileDialog::Read) ?
     QFileDialog::AcceptOpen : QFileDialog::AcceptSave;
   fileDialog->setAcceptMode(acceptMode);
@@ -353,7 +356,7 @@ bool qSlicerStandardFileDialog::exec(const qSlicerIO::IOProperties& ioProperties
     options = nullptr;
     }
 
-  delete fileDialog;
+  fileDialog->deleteLater();
   return res;
 }
 
@@ -364,8 +367,10 @@ QStringList qSlicerStandardFileDialog::getOpenFileName(
   QStringList files;
   ioProperties["multipleFiles"] = QFileDialog::ExistingFiles;
   ioProperties["objectName"] = "getOpenFileName";
+  qSlicerApplication* app = qSlicerApplication::application();
+  QWidget* mainWindow = app ? app->mainWindow() : nullptr;
   ctkFileDialog* fileDialog = qSlicerStandardFileDialog::createFileDialog(
-                                ioProperties);
+                                ioProperties, mainWindow);
   qSlicerIOManager* ioManager = qSlicerApplication::application()->ioManager();
 
   if(fileDialog->exec() == QDialog::Accepted)
@@ -373,7 +378,7 @@ QStringList qSlicerStandardFileDialog::getOpenFileName(
     files = fileDialog->selectedFiles();
     }
   ioManager->setFavorites(fileDialog->sidebarUrls());
-  delete fileDialog;
+  fileDialog->deleteLater();
   return files;
 }
 
@@ -384,8 +389,10 @@ QString qSlicerStandardFileDialog::getExistingDirectory(
   QString directory;
   ioProperties["fileMode"] = QFileDialog::Directory;
   ioProperties["objectName"] = "getExistingDirectory";
+  qSlicerApplication* app = qSlicerApplication::application();
+  QWidget* mainWindow = app ? app->mainWindow() : nullptr;
   ctkFileDialog* fileDialog = qSlicerStandardFileDialog::createFileDialog(
-                                ioProperties);
+                                ioProperties, mainWindow);
   qSlicerIOManager* ioManager = qSlicerApplication::application()->ioManager();
 
   if (fileDialog->exec() == QDialog::Accepted)
@@ -393,6 +400,6 @@ QString qSlicerStandardFileDialog::getExistingDirectory(
     directory = fileDialog->selectedFiles().value(0);
     }
   ioManager->setFavorites(fileDialog->sidebarUrls());
-  delete fileDialog;
+  fileDialog->deleteLater();
   return directory;
 }

Base/QTGUI/qSlicerFileDialog.h
@@ -147,7 +147,7 @@ public:
 
 protected:
   static ctkFileDialog* createFileDialog(const qSlicerIO::IOProperties& ioProperties =
-                                         qSlicerIO::IOProperties());
+                                         qSlicerIO::IOProperties(), QWidget* parent = nullptr);
 
   qSlicerIOOptions* options(const qSlicerIO::IOProperties& ioProperties)const;
 

Base/QTGUI/qSlicerIOManager.cxx
@@ -4,6 +4,7 @@
 #include <QDropEvent>
 #include <QFileDialog>
 #include <QInputDialog>
+#include <QMainWindow>
 #include <QMetaProperty>
 #include <QProgressDialog>
 #include <QSettings>
@@ -570,30 +571,33 @@ void qSlicerIOManager::showLoadNodesResultDialog(bool overallSuccess, vtkMRMLMes
     // Everything is OK, no need to show error popup.
     return;
     }
-  ctkMessageBox messageBox;
+  qSlicerApplication* app = qSlicerApplication::application();
+  QWidget* mainWindow = app ? app->mainWindow() : nullptr;
+  ctkMessageBox* messageBox = new ctkMessageBox(mainWindow);
   QString text;
   if (overallSuccess)
     {
-    messageBox.setWindowTitle(tr("Adding data succeeded"));
-    messageBox.setIcon(QMessageBox::Information);
+    messageBox->setWindowTitle(tr("Adding data succeeded"));
+    messageBox->setIcon(QMessageBox::Information);
     text = tr("The selected files were loaded successfully but errors or warnings were reported.");
     }
   else
     {
-    messageBox.setWindowTitle(tr("Adding data failed"));
-    messageBox.setIcon(QMessageBox::Critical);
+    messageBox->setWindowTitle(tr("Adding data failed"));
+    messageBox->setIcon(QMessageBox::Critical);
     text = tr("Error occurred while loading the selected files.");
     }
   text += "\n";
   if (userMessages)
     {
     text += tr("Click 'Show details' button and check the application log for more information.");
-    messageBox.setDetailedText(QString::fromStdString(userMessages->GetAllMessagesAsString()));
+    messageBox->setDetailedText(QString::fromStdString(userMessages->GetAllMessagesAsString()));
     }
   else
     {
     text += tr("Check the application log for more information.");
     }
-  messageBox.setText(text);
-  messageBox.exec();
+  messageBox->setText(text);
+  messageBox->exec();
+  messageBox->deleteLater();
 }

Modules/Loadable/Data/qSlicerSceneWriter.cxx
@@ -23,6 +23,7 @@
 #include <QDebug>
 #include <QDir>
 #include <QFileInfo>
+#include <QMainWindow>
 #include <QPixmap>
 
 // CTK includes
@@ -32,6 +33,7 @@
 // QtCore includes
 #include "qMRMLUtils.h"
 #include "qSlicerCoreApplication.h"
+#include "qSlicerApplication.h"
 #include "qSlicerSceneWriter.h"
 #include "vtkSlicerApplicationLogic.h"
 
@@ -218,7 +220,9 @@ bool qSlicerSceneWriter::writeToDirectory(const qSlicerIO::IOProperties& propert
   int numFiles = saveDir.count() - 2;
   if (numFiles != 0)
     {
-    ctkMessageBox *emptyMessageBox = new ctkMessageBox(nullptr);
+    qSlicerApplication* app = qSlicerApplication::application();
+    QWidget* mainWindow = app ? app->mainWindow() : nullptr;
+    ctkMessageBox *emptyMessageBox = new ctkMessageBox(mainWindow);
     QString error;
     switch(numFiles)
       {
@@ -238,10 +242,10 @@ bool qSlicerSceneWriter::writeToDirectory(const qSlicerIO::IOProperties& propert
                          "Please choose an empty directory.")
                          .arg(saveDirName)
                          .arg(error);
-    emptyMessageBox->setAttribute( Qt::WA_DeleteOnClose, true );
     emptyMessageBox->setIcon(QMessageBox::Warning);
     emptyMessageBox->setText(message);
     emptyMessageBox->exec();
+    emptyMessageBox->deleteLater();
     return false;
     }
 

Modules/Loadable/SceneViews/GUI/qSlicerSceneViewsModuleDialog.cxx
@@ -22,7 +22,8 @@
 #include <vector>
 
 //-----------------------------------------------------------------------------
-qSlicerSceneViewsModuleDialog::qSlicerSceneViewsModuleDialog()
+qSlicerSceneViewsModuleDialog::qSlicerSceneViewsModuleDialog(QWidget* parent/*=nullptr*/)
+  : qMRMLScreenShotDialog(parent)
 {
   this->m_Logic = nullptr;
   this->setLayoutManager(qSlicerApplication::application()->layoutManager());

commit a6c56dd5e4bff8262a606271c93a59e77ca63555

BUG: Fix on exit Segfault ensuring ExtensionsManagerDialog is deleted once
This commit applies a fix similar to acc618074 (BUG: Segfault at application
exit due to SettingsDialog deletion) and fixes a regression likely introduced
in abd2544d4 (BUG: Fixed application window not clickable when popup is
displayed) where the extension manager instance is associated with the
parent window.

See https://discourse.slicer.org/t/help-for-package-testing-on-macos-monterrey-or-bigsur-system/23249

Co-authored-by: Hollister Herhold <hherhold@amnh.org>
Co-authored-by: Rafael Palomar <rafael.palomar@ous-research.no>
Co-authored-by: Steve Pieper <pieper@isomics.com>
Co-authored-by: Andras Lasso <lasso@queensu.ca>
Co-authored-by: Mauro I. Dominguez <mau_igna_06@hotmail.com>

Base/QTGUI/qSlicerApplication.cxx
@@ -148,7 +148,7 @@ public:
   // to keep track if the object is deleted already by the MainWindow.
   QPointer<ctkSettingsDialog> SettingsDialog;
 #ifdef Slicer_BUILD_EXTENSIONMANAGER_SUPPORT
-  qSlicerExtensionsManagerDialog* ExtensionsManagerDialog;
+  QPointer<qSlicerExtensionsManagerDialog> ExtensionsManagerDialog;
   bool IsExtensionsManagerDialogOpen;
 #endif
 #ifdef Slicer_USE_QtTesting