Slicer/Slicer

commit a258951f473d74f24a93cff95673ea6163a7b5e1

BUG: Consolidate vtk.util.numpy_support imports to fix UnboundLocalError
This commit ensures attributes associated with vtk modules can be referenced
from functions where "vtk" is imported at the top-level
and "vtk.util.numpy_support" import is delayed.

It fixes a regression introduced in 3a9490ed1 (STYLE: Remove unused
python imports) where imported have also been re-organized.

See https://bugs.python.org/issue35069 (Unexecuted import in function
causes UnboundLocalError)

Note that imports associated with Python/slicer/utils.py have not been
updated. They are all consistently done at the function level.

Co-authored-by: Andras Lasso <lasso@queensu.ca>
Co-authored-by: Connor Bowley <connor.bowley@kitware.com>

Modules/Scripted/Endoscopy/Endoscopy.py
@@ -1,6 +1,7 @@
 import ctk
 import qt
 import vtk
+import vtk.util.numpy_support
 
 import slicer
 from slicer.ScriptedLoadableModule import *
@@ -364,8 +365,7 @@ class EndoscopyComputePath:
       if originalPointsPerSegment<pointsPerSegment:
         self.fids.SetNumberOfPointsPerInterpolatingSegment(originalPointsPerSegment)
       # Get it as a numpy array as an independent copy
-      import vtk.util.numpy_support as VN
-      self.path = VN.vtk_to_numpy(resampledPoints.GetData())
+      self.path = vtk.util.numpy_support(resampledPoints.GetData())
       return
 
     # hermite interpolation functions
@@ -534,8 +534,7 @@ class EndoscopyPathModel:
       linesIDArray.SetTuple1( 0, linesIDArray.GetNumberOfTuples() - 1 )
       lines.SetNumberOfCells(1)
 
-    import vtk.util.numpy_support as VN
-    pointsArray = VN.vtk_to_numpy(points.GetData())
+    pointsArray = vtk.util.numpy_support(points.GetData())
     self.planePosition, self.planeNormal = self.planeFit(pointsArray.T)
 
     # Create model node

Modules/Scripted/DICOMPlugins/DICOMGeAbusPlugin.py
@@ -3,6 +3,7 @@ import logging
 import numpy
 import pydicom as dicom
 import vtk
+import vtk.util.numpy_support
 
 import slicer
 
@@ -272,7 +273,6 @@ class DICOMGeAbusPluginClass(DICOMPlugin):
     # is mapped from the source corner to the target corner
 
     nshape = tuple(reversed(gridImage.GetDimensions()))
-    import vtk.util.numpy_support
     nshape = nshape + (3,)
     displacements = vtk.util.numpy_support.vtk_to_numpy(gridImage.GetPointData().GetScalars()).reshape(nshape)
 

commit cff242ebc2cbb4b4a5b35ee8c8ceebaa178bd311

BUG: vtk_to_numpy funciton missing in Endoscopy
the bug was introduced in a258951f473d74f24a93cff95673ea6163a7b5e1, and it's fixed here

Modules/Scripted/Endoscopy/Endoscopy.py
@@ -365,7 +365,7 @@ class EndoscopyComputePath:
       if originalPointsPerSegment<pointsPerSegment:
         self.fids.SetNumberOfPointsPerInterpolatingSegment(originalPointsPerSegment)
       # Get it as a numpy array as an independent copy
-      self.path = vtk.util.numpy_support(resampledPoints.GetData())
+      self.path = vtk.util.numpy_support.vtk_to_numpy(resampledPoints.GetData())
       return
 
     # hermite interpolation functions
@@ -534,7 +534,7 @@ class EndoscopyPathModel:
       linesIDArray.SetTuple1( 0, linesIDArray.GetNumberOfTuples() - 1 )
       lines.SetNumberOfCells(1)
 
-    pointsArray = vtk.util.numpy_support(points.GetData())
+    pointsArray = vtk.util.numpy_support.vtk_to_numpy(points.GetData())
     self.planePosition, self.planeNormal = self.planeFit(pointsArray.T)
 
     # Create model node