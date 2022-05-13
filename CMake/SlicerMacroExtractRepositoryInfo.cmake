Slicer/Slicer

commit 9b15994e58e2f120681526f46dc3717b2b46666e

COMP: Do not require building extension from git repository
Developers often complained that their extension build failed for some unknown reason and frequently the issue was that they did not store the source code in a git repository.

It is good to encourage developers to use best practices, such as version control, but making the build fail if they build from a non-version-controlled repository was unnecessarily strong measure.

This commit changes the behavior so that if an extension is built from a non-git folder then the build succeeds, just a warning is displayed during project configuration step and `NA` is used as the extension's WC revision.

CMake/SlicerMacroExtractRepositoryInfo.cmake
@@ -72,7 +72,13 @@ macro(SlicerMacroExtractRepositoryInfo)
 
   if(NOT EXISTS ${MY_SOURCE_DIR}/.git)
 
-    message(AUTHOR_WARNING "Skipping repository info extraction: directory [${MY_SOURCE_DIR}] is not a GIT or SVN checkout")
+    if(DEFINED ${wc_info_prefix}_WC_REVISION)
+      message(STATUS "Using manually defined WC revision: ${wc_info_prefix}_WC_REVISION=${${wc_info_prefix}_WC_REVISION}")
+    else()
+      message(AUTHOR_WARNING "Skipping repository info extraction: directory [${MY_SOURCE_DIR}] is not a git repository."
+        "To resolve this warning store the extension source code in a git repository or set ${wc_info_prefix}_WC_REVISION CMake variable.")
+      set(${wc_info_prefix}_WC_REVISION "NA")
+    endif()
 
   else()
 

commit 5bc8256ab936266aaa9fb7857aa57b27ff630891

COMP: Revert changes inadvertently integrated
This commit reverts 9b15994e5 (Do not require building
extension from git repository) inadvertently integrated
as part of the unrelated pull request #6371.

CMake/SlicerMacroExtractRepositoryInfo.cmake
@@ -72,13 +72,7 @@ macro(SlicerMacroExtractRepositoryInfo)
 
   if(NOT EXISTS ${MY_SOURCE_DIR}/.git)
 
-    if(DEFINED ${wc_info_prefix}_WC_REVISION)
-      message(STATUS "Using manually defined WC revision: ${wc_info_prefix}_WC_REVISION=${${wc_info_prefix}_WC_REVISION}")
-    else()
-      message(AUTHOR_WARNING "Skipping repository info extraction: directory [${MY_SOURCE_DIR}] is not a git repository."
-        "To resolve this warning store the extension source code in a git repository or set ${wc_info_prefix}_WC_REVISION CMake variable.")
-      set(${wc_info_prefix}_WC_REVISION "NA")
-    endif()
+    message(AUTHOR_WARNING "Skipping repository info extraction: directory [${MY_SOURCE_DIR}] is not a GIT or SVN checkout")
 
   else()
 