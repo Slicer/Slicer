640879004a66e69ccaaf6342bf9323ce48e83da6
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-05-18 12:33:00 -0400

    BUG: Backport slicer.util.(delayDisplay|messageBox) updates
    Cherry picked from:
    * c4ac067f0d - ENH: Improve slicer.util.delayDisplay to show dialog relative to mainwindow
    * a28a2c2d59 - BUG: Update slicer.util.delayDisplay to remove from taskbar thumbnail preview
    * 7ddbe774f3 - ENH: Update slicer.util.messageBox to display a delayed pop in testing mode

--- Base/Python/slicer/util.py
+++ Base/Python/slicer/util.py
@@ -2480,7 +2480,7 @@ def tempDirectory(key='__SlicerTemp__',tempDir=None,includeDateTime=True):
   return dirPath
 
 
-def delayDisplay(message, autoCloseMsec=1000):
+def delayDisplay(message, autoCloseMsec=1000, parent=None, **kwargs):
   """Display an information message in a popup window for a short time.
 
   If ``autoCloseMsec < 0`` then the window is not closed until the user clicks on it
@@ -2495,7 +2495,10 @@ def delayDisplay(message, autoCloseMsec=1000):
   if 0 <= autoCloseMsec < 400:
     slicer.app.processEvents()
     return
-  messagePopup = qt.QDialog()
+  messagePopup = qt.QDialog(parent if parent else mainWindow())
+  for key, value in kwargs.items():
+    if hasattr(messagePopup, key):
+      setattr(messagePopup, key, value)
   layout = qt.QVBoxLayout()
   messagePopup.setLayout(layout)
   label = qt.QLabel(message,messagePopup)
@@ -2506,6 +2509,13 @@ def delayDisplay(message, autoCloseMsec=1000):
     okButton = qt.QPushButton("OK")
     layout.addWidget(okButton)
     okButton.connect('clicked()', messagePopup.close)
+  # Windows 10 peek feature in taskbar shows all hidden but not destroyed windows
+  # (after creating and closing a messagebox, hovering over the mouse on Slicer icon, moving up the
+  # mouse to the peek thumbnail would show it again).
+  # Popup windows in other Qt applications often show closed popups (such as
+  # Paraview's Edit / Find data dialog, MeshMixer's File/Preferences dialog).
+  # By calling deleteLater, the messagebox is permanently deleted when the current call is completed.
+  messagePopup.deleteLater()
   messagePopup.exec_()
 
 
@@ -2629,12 +2639,14 @@ def messageBox(text, parent=None, **kwargs):
     slicer.util.messageBox("Some message", dontShowAgainSettingsKey = "MainWindow/DontShowSomeMessage")
 
   When the application is running in testing mode (``slicer.app.testingEnabled() == True``),
-  the popup is skipped and ``qt.QMessageBox.Ok`` is returned, with the text being logged to indicate this.
+  an auto-closing popup with a delay of 3s is shown using :func:`delayDisplay()` and ``qt.QMessageBox.Ok``
+  is returned, with the text being logged to indicate this.
   """
   import logging, qt, slicer
   if slicer.app.testingEnabled():
     testingReturnValue = qt.QMessageBox.Ok
-    logging.info("Testing mode is enabled: Returning %s (qt.QMessageBox.Ok) and skipping message box [%s]." % (testingReturnValue, text))
+    logging.info("Testing mode is enabled: Returning %s (qt.QMessageBox.Ok) and displaying an auto-closing message box [%s]." % (testingReturnValue, text))
+    slicer.util.delayDisplay(text, autoCloseMsec=3000, parent=parent, **kwargs)
     return testingReturnValue
 
   import ctk
