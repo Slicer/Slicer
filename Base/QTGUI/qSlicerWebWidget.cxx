Slicer/Slicer

commit 9c0ae2ad896a6c0292f51581b3f999e6a5f79b75

BUG: Allow multiple qSlicerWebWidget instance injecting scripts into page (#6396)
Since the script injection associated with qSlicerWebWidget was done globally
and only once at the profile level, instantiating first qSlicerWebWidget
and then qSlicerExtensionsInstallWidget was preventing the customization
done in qSlicerExtensionsInstallWidget::initializeWebChannelTransport()
from being effective.

This commit addresses this by injecting the "qwebchannel_appended.js" script
on per page basis. Doing so ensures each qSlicerWebWidget (and derived
object like qSlicerExtensionsInstallWidget) will inject the expected script.

It follows up on da201359b (ENH: WIP - WebEngine support for
extension manager).

Base/QTGUI/qSlicerWebWidget.cxx
@@ -114,12 +114,13 @@ void qSlicerWebWidgetPrivate::init()
 
   this->PythonProxy = new qSlicerWebPythonProxy(q);
   QWebEngineProfile* profile = QWebEngineProfile::defaultProfile();
-  this->initializeWebEngineProfile(profile);
 
   this->WebEnginePage = new qSlicerWebEnginePage(profile, this->WebView);
   this->WebEnginePage->JavaScriptConsoleMessageLoggingEnabled = developerModeEnabled;
   this->WebEnginePage->WebWidget = q;
   this->WebView->setPage(this->WebEnginePage);
+
+  this->initializeWebEngineProfile(profile);
 
   this->WebChannel = new QWebChannel(this->WebView->page());
   this->initializeWebChannel(this->WebChannel);
@@ -196,7 +197,7 @@ void qSlicerWebWidgetPrivate::initializeWebEngineProfile(QWebEngineProfile* prof
     return;
     }
 
-  if (!profile->scripts()->findScript("qwebchannel_appended.js").isNull())
+  if (!this->WebEnginePage->scripts().findScript("qwebchannel_appended.js").isNull())
     {
     // profile is already initialized
     return;
@@ -218,7 +219,7 @@ void qSlicerWebWidgetPrivate::initializeWebEngineProfile(QWebEngineProfile* prof
     script.setWorldId(QWebEngineScript::MainWorld);
     script.setInjectionPoint(QWebEngineScript::DocumentCreation);
     script.setRunsOnSubFrames(false);
-    profile->scripts()->insert(script);
+    this->WebEnginePage->scripts().insert(script);
     }
 
   // setup default download handler shared across all widgets

commit 7a424292fca7beef644f4be68b2bf3571bef09f8

BUG: Ensure download request are handled by web widget triggering the request
This commit fixes a regression introduced in 9c0ae2ad8 (BUG: Allow multiple
qSlicerWebWidget instance injecting scripts into page) where the observation
of the profile "downloadRequested()" signal was done multiple time.

Since the download request is emitted by the default profile observed by
all web widget instances, this change ignores the request if it does not
originate from the page associated with this web widget instance.

Base/QTGUI/qSlicerWebWidget.cxx
@@ -240,6 +240,14 @@ void qSlicerWebWidgetPrivate::handleDownload(QWebEngineDownloadItem* download)
 {
   Q_Q(qSlicerWebWidget);
 
+  if (this->WebEnginePage != download->page())
+    {
+    // Since the download request is emitted by the default profile observed by
+    // all web widget instances, we ignore the request if it does not originate
+    // from the page associated with this web widget instance.
+    return;
+    }
+
   qSlicerWebDownloadWidget *downloader = new qSlicerWebDownloadWidget(q);
   downloader->setAttribute(Qt::WA_DeleteOnClose);
   downloader->show();