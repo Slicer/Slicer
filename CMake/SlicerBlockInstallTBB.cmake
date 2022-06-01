3ea8ba4b4e0981ea05677df65bc3e7c9819c1d5b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-06-01 16:03:58 -0400

    COMP: Update TBB from 2019_U9 to 2021.5.0
    Remove TBB support for VTK 8. Since older VTK versions do not support
    the updated TBB api, this commit removes support for TBB backend when
    building on windows with VTK < 9.
    
    Prior to kitware/VTK@3e5313e71 (Replace task_scheduler_init with task_arena),
    the header "tbb/task_scheduler_init.h" was required but is not available
    anymore in newer TBB version.
    
    SlicerBlockInstallTBB:
    * Update rules based on new library names.
    * On linux, switch to use slicerInstallLibrary to ensure
      the symlink chain is followed.
    * On macOS, explicitly report an error if the CMake module
      because the libraries are copied through the fix-up process.

--- CMake/SlicerBlockInstallTBB.cmake
+++ CMake/SlicerBlockInstallTBB.cmake
@@ -14,28 +14,18 @@ endif()
 if(WIN32)
   install(
     FILES
-      ${TBB_BIN_DIR}/tbb.dll
+      ${TBB_BIN_DIR}/tbb12.dll
       ${TBB_BIN_DIR}/tbbmalloc.dll
       ${TBB_BIN_DIR}/tbbmalloc_proxy.dll
     DESTINATION bin COMPONENT Runtime)
 elseif(APPLE)
-  install(
-    FILES
-      ${TBB_LIB_DIR}/libtbb.dylib
-      ${TBB_LIB_DIR}/libtbbmalloc.dylib
-      ${TBB_LIB_DIR}/libtbbmalloc_proxy.dylib
-    DESTINATION ${TBB_INSTALL_LIB_DIR} COMPONENT Runtime)
+  message(FATAL_ERROR "This CMake module does NOT suppot macOS")
 elseif(UNIX)
-  install(
-    FILES
-      ${TBB_LIB_DIR}/libtbb.so.2
-      ${TBB_LIB_DIR}/libtbbmalloc.so.2
-      ${TBB_LIB_DIR}/libtbbmalloc_proxy.so.2
-    DESTINATION ${TBB_INSTALL_LIB_DIR} COMPONENT Runtime)
-  slicerStripInstalledLibrary(
-    FILES
-      "${TBB_INSTALL_LIB_DIR}/libtbb.so.2"
-      "${TBB_INSTALL_LIB_DIR}/libtbbmalloc.so.2"
-      "${TBB_INSTALL_LIB_DIR}/libtbbmalloc_proxy.so.2"
-    COMPONENT Runtime)
+  foreach(file IN ITEMS
+    ${TBB_LIB_DIR}/libtbb.so.12
+    ${TBB_LIB_DIR}/libtbbmalloc.so.2.5
+    ${TBB_LIB_DIR}/libtbbmalloc_proxy.so.2.5
+    )
+    slicerInstallLibrary(FILE ${file} DESTINATION ${TBB_INSTALL_LIB_DIR} COMPONENT Runtime STRIP)
+  endforeach()
 endif()
