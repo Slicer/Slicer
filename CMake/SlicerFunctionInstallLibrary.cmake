4d97ce70ef2e9f32c93dfc78325d67c19fdc68ab
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-06-01 18:59:59 -0400

    COMP: Fix slicerStripInstalledLibrary to effectively support list of libraries
    This is a follow-up of 52f2d6c04 (ENH: Reduce linux package size stripping
    regular symbols) where the CMake function slicerStripInstalledLibrary
    was first introduced.

--- CMake/SlicerFunctionInstallLibrary.cmake
+++ CMake/SlicerFunctionInstallLibrary.cmake
@@ -182,16 +182,17 @@ function(slicerStripInstalledLibrary)
   if(UNIX AND NOT APPLE)
     if("${CMAKE_BUILD_TYPE}" IN_LIST configs)
       if(NOT EXISTS "${CMAKE_STRIP}")
-        message(FATAL_ERROR "failed to add install rule for stripping symbols of '${file}'. CMAKE_STRIP CMake variable is either not set or pointing to an nonexistent file.")
+        message(FATAL_ERROR "failed to add install rule for stripping symbols. CMAKE_STRIP CMake variable is either not set or pointing to an nonexistent file.")
       endif()
-      set(file "${_slicerStripInstalledLibrary_FILES}")
       set(dollar "$")
       if(_slicerStripInstalledLibrary_FILES)
-        install(
-          CODE "message(STATUS \"Stripping: ${dollar}ENV{DESTDIR}${dollar}{CMAKE_INSTALL_PREFIX}/${file}\")
-execute_process(COMMAND \"${CMAKE_STRIP}\" \"${dollar}ENV{DESTDIR}${dollar}{CMAKE_INSTALL_PREFIX}/${file}\")"
-          COMPONENT ${_slicerStripInstalledLibrary_COMPONENT}
-          )
+        foreach(file IN LISTS _slicerStripInstalledLibrary_FILES)
+          install(
+            CODE "message(STATUS \"Stripping: ${dollar}ENV{DESTDIR}${dollar}{CMAKE_INSTALL_PREFIX}/${file}\")
+  execute_process(COMMAND \"${CMAKE_STRIP}\" \"${dollar}ENV{DESTDIR}${dollar}{CMAKE_INSTALL_PREFIX}/${file}\")"
+            COMPONENT ${_slicerStripInstalledLibrary_COMPONENT}
+            )
+        endforeach()
       endif()
       if(_slicerStripInstalledLibrary_PATTERN)
         install(
