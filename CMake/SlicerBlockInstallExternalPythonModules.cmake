Slicer/Slicer

commit b9ac6ed61cae9de90986eaf8766c0e8b1b4461c1

COMP: Remove support for VTK8
Build support for VTK8 or VTK9 was introduced in October 2020 in https://github.com/Slicer/Slicer/commit/c658b7b428a37d6f4e925664099ee30fd4ca408c when VTK9 support was added.

To streamline maintenance, VTK8 support is now being removed.

CMake/SlicerBlockInstallExternalPythonModules.cmake
@@ -3,40 +3,6 @@ if(NOT APPLE)
   message(FATAL_ERROR "This module was designed for macOS")
 endif()
 
-if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-
-# Install VTK python module
-set(VTK_PYTHON_MODULE "${VTK_DIR}/lib/python${Slicer_PYTHON_VERSION_DOT}/site-packages")
-set(_vtk_package "vtk")
-if(EXISTS ${VTK_PYTHON_MODULE}/vtkmodules)
-  set(_vtk_package "vtkmodules") # Introduced in VTK9 kitware/vtk@2404228 on 2017.12.15
-endif()
-install(DIRECTORY ${VTK_PYTHON_MODULE}/${_vtk_package}
-  DESTINATION ${Slicer_INSTALL_BIN_DIR}/Python
-  USE_SOURCE_PERMISSIONS
-  COMPONENT Runtime
-  # VTK9: Add exclusions to avoid installing VTK's C++ Python modules
-  # These have their own install rules (see below).
-  PATTERN "vtk*Python.so" EXCLUDE
-  PATTERN "*.pyo" EXCLUDE)
-
-if(EXISTS ${VTK_PYTHON_MODULE}/vtk.py)
-  # Introduced in VTK9 kitware/vtk@2404228 on 2017.12.15
-  install(FILES ${VTK_PYTHON_MODULE}/vtk.py
-    DESTINATION ${Slicer_INSTALL_BIN_DIR}/Python
-    COMPONENT Runtime)
-endif()
-
-# Install external python runtime libraries that we don't link to (fixupbundle won't copy them)
-set(vtk_python_library_subdir "lib/python${Slicer_PYTHON_VERSION_DOT}/site-packages/${_vtk_package}")
-file(GLOB vtk_python_modules "${VTK_DIR}/${vtk_python_library_subdir}/*Python.so")
-install(FILES ${vtk_python_modules}
-        DESTINATION ${Slicer_INSTALL_LIB_DIR}
-        COMPONENT Runtime)
-
-
-else()
-
 # Install VTK python module
 set(VTK_PYTHON_MODULE "${VTK_DIR}/lib/python${Slicer_PYTHON_VERSION_DOT}/site-packages")
 install(DIRECTORY ${VTK_PYTHON_MODULE}/vtkmodules

Base/Logic/CMakeLists.txt
@@ -202,12 +202,6 @@ if(VTK_WRAP_PYTHON)
   set(_python_wrapped_libraries
     )
 
-  if(${VTK_VERSION} VERSION_LESS "8.90")
-    list(APPEND _python_wrapped_libraries
-      MRMLLogicPythonD
-      )
-  endif()
-
   vtkMacroKitPythonWrap(
     KIT_NAME ${lib_name}
     KIT_SRCS ${SlicerBaseLogic_SRCS}
@@ -217,14 +211,7 @@ if(VTK_WRAP_PYTHON)
     )
   # Export target
   set_property(GLOBAL APPEND PROPERTY Slicer_TARGETS ${lib_name}Python)
-  if(${VTK_VERSION} VERSION_LESS "8.90")
-    set_property(GLOBAL APPEND PROPERTY Slicer_TARGETS ${lib_name}PythonD)
-  endif()
-
   set_target_properties(${lib_name}Python PROPERTIES FOLDER "Core-Base")
-  if(${VTK_VERSION} VERSION_LESS "8.90")
-    set_target_properties(${lib_name}PythonD PROPERTIES FOLDER "Core-Base")
-  endif()
   if(TARGET ${lib_name}Hierarchy)
     set_target_properties(${lib_name}Hierarchy PROPERTIES FOLDER "Core-Base")
   endif()

Base/QTApp/qSlicerApplicationHelper.cxx
@@ -64,10 +64,7 @@
 // VTK includes
 #include <vtksys/SystemTools.hxx>
 #include <vtkNew.h>
-#include <vtkVersionMacros.h>
-#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 90)
 #include <vtkLogger.h>
-#endif
 
 // PythonQt includes
 #ifdef Slicer_USE_PYTHONQT
@@ -91,9 +88,7 @@ qSlicerApplicationHelper::~qSlicerApplicationHelper() = default;
 void qSlicerApplicationHelper::preInitializeApplication(
     const char* argv0, ctkProxyStyle* style)
 {
-#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 90)
   vtkLogger::SetStderrVerbosity(vtkLogger::VERBOSITY_OFF);
-#endif
   itk::itkFactoryRegistration();
   qMRMLWidget::preInitializeApplication();
 

Base/QTCLI/CMakeLists.txt
@@ -138,12 +138,6 @@ if(VTK_WRAP_PYTHON)
   set(_python_wrapped_libraries
     )
 
-  if(${VTK_VERSION} VERSION_LESS "8.90")
-    list(APPEND _python_wrapped_libraries
-      SlicerBaseLogicPythonD
-      )
-  endif()
-
   vtkMacroKitPythonWrap(
     KIT_NAME ${PROJECT_NAME}
     KIT_SRCS ${KIT_VTK_SRCS}
@@ -152,14 +146,8 @@ if(VTK_WRAP_PYTHON)
     KIT_PYTHON_LIBRARIES ${_python_wrapped_libraries}
     )
   set_property(GLOBAL APPEND PROPERTY Slicer_TARGETS ${PROJECT_NAME}Python)
-  if(${VTK_VERSION} VERSION_LESS "8.90")
-    set_property(GLOBAL APPEND PROPERTY Slicer_TARGETS ${PROJECT_NAME}PythonD)
-  endif()
   # Folder
   set_target_properties(${PROJECT_NAME}Python PROPERTIES FOLDER "Core-Base")
-  if(${VTK_VERSION} VERSION_LESS "8.90")
-    set_target_properties(${PROJECT_NAME}PythonD PROPERTIES FOLDER "Core-Base")
-  endif()
   if(TARGET ${PROJECT_NAME}Hierarchy)
     set_target_properties(${PROJECT_NAME}Hierarchy PROPERTIES FOLDER "Core-Base")
   endif()

Base/QTGUI/Testing/Cxx/qSlicerWidgetTest2.cxx
@@ -174,11 +174,7 @@ int qSlicerWidgetTest2(int argc, char * argv[] )
 
   vtkWidget->setParent(&parentWidget);
   vbox.addWidget(vtkWidget);
-#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 90)
   vtkWidget->renderWindow()->Render();
-#else
-  vtkWidget->GetRenderWindow()->Render();
-#endif
 
 #ifdef Slicer_BUILD_WEBENGINE_SUPPORT
   QWebEngineView webView;
@@ -192,13 +188,8 @@ int qSlicerWidgetTest2(int argc, char * argv[] )
   parentWidget.show();
   parentWidget.raise();
 
-#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 90)
   vtkMRMLSliceLogic* sliceLogic = setupSliceDisplay(
     scene, vtkWidget->renderWindow(), argv[1] );
-#else
-  vtkMRMLSliceLogic* sliceLogic = setupSliceDisplay(
-    scene, vtkWidget->GetRenderWindow(), argv[1]);
-#endif
 
   if (argc < 3 || QString(argv[2]) != "-I")
   {

Base/QTGUI/qSlicerApplication.cxx
@@ -113,10 +113,7 @@
 
 // VTK includes
 #include <vtkNew.h>
-#include <vtkVersionMacros.h> // must precede reference to VTK_*_VERSION
-#if VTK_MAJOR_VERSION >= 9
 #include <vtkSMP.h> // For VTK_SMP_BACKEND
-#endif
 
 //-----------------------------------------------------------------------------
 class qSlicerApplicationPrivate : public qSlicerCoreApplicationPrivate

CMake/SlicerCPackBundleFixup.cmake.in
@@ -172,8 +172,7 @@ function(fixup_bundle_with_plugins app)
       )
   endif()
 
-  set(Slicer_VTK_VERSION_MAJOR "@Slicer_VTK_VERSION_MAJOR@")
-  if(Slicer_USE_PYTHONQT AND ${Slicer_VTK_VERSION_MAJOR} VERSION_GREATER_EQUAL 9)
+  if(Slicer_USE_PYTHONQT)
     list(APPEND candidates_pattern
       "${app_dir}/Contents/bin/Python/vtkmodules/*.so"
       )

CMake/SlicerExtensionCPackBundleFixup.cmake.in
@@ -312,9 +312,8 @@ function(fixup_slicer_extension)
     )
 
   set(Slicer_USE_PYTHONQT "@Slicer_USE_PYTHONQT@")
-  set(Slicer_VTK_VERSION_MAJOR "@Slicer_VTK_VERSION_MAJOR@")
 
-  if(Slicer_USE_PYTHONQT AND ${Slicer_VTK_VERSION_MAJOR} VERSION_GREATER_EQUAL 9)
+  if(Slicer_USE_PYTHONQT)
     list(APPEND candidates_pattern
       "${ext_dir}/Contents/@Slicer_BUNDLE_EXTENSIONS_LOCATION@bin/Python/vtkmodules/*.so"
       )

commit 8ec576ec39e65723065f8b6c53edbab15ffde87b

COMP: Fix inner project config removing invalid CMake statement
This commit updates "SlicerBlockInstallExternalPythonModules"
CMake module fixing the following regression introduced
in b9ac6ed61 (COMP: Remove support for VTK8)

  CMake Error at CMake/SlicerBlockInstallExternalPythonModules.cmake:16 (endif):
    Flow control statements are not properly nested.
  Call Stack (most recent call first):
    CMake/SlicerCPack.cmake:172 (include)
    CMake/LastConfigureStep/CMakeLists.txt:44 (include)

CMake/SlicerBlockInstallExternalPythonModules.cmake
@@ -13,8 +13,6 @@ install(FILES ${VTK_PYTHON_MODULE}/vtk.py
   DESTINATION ${Slicer_INSTALL_BIN_DIR}/Python
   COMPONENT Runtime)
 
-endif()
-
 # Install CTK python modules
 install(DIRECTORY ${CTK_DIR}/CTK-build/bin/Python/ctk ${CTK_DIR}/CTK-build/bin/Python/qt
   DESTINATION ${Slicer_INSTALL_BIN_DIR}/Python