Slicer/Slicer

commit 4fe811f63fd7d0aa10d0d8037e0c4485a967724d

ENH: Refactor ExtensionsManagerModel introducing convertExtensionMetadata

Base/QTCore/qSlicerExtensionsManagerModel.cxx
@@ -1182,15 +1182,7 @@ qSlicerExtensionsManagerModel::ExtensionMetadataType qSlicerExtensionsManagerMod
     this->ServerResponseCache[serverResponseCacheKey] = result;
     }
 
-  ExtensionMetadataType updatedExtensionMetadata = result;
-  QHash<QString, QString> serverToExtensionDescriptionKey = q->serverToExtensionDescriptionKey(q->serverAPI());
-  foreach(const QString& key, result.keys())
-    {
-    updatedExtensionMetadata.insert(
-      serverToExtensionDescriptionKey.value(key, key), result.value(key));
-    }
-
-  return updatedExtensionMetadata;
+  return qSlicerExtensionsManagerModel::convertExtensionMetadata(result, q->serverAPI());
 }
 
 // --------------------------------------------------------------------------
@@ -2885,6 +2877,20 @@ QHash<QString, QString> qSlicerExtensionsManagerModel::serverToExtensionDescript
     qWarning() << Q_FUNC_INFO << " failed: missing implementation for serverAPI" << serverAPI;
     }
   return serverToExtensionDescriptionKey;
+}
+
+// --------------------------------------------------------------------------
+qSlicerExtensionsManagerModel::ExtensionMetadataType
+qSlicerExtensionsManagerModel::convertExtensionMetadata(const ExtensionMetadataType &extensionMetadata, int serverAPI)
+{
+  ExtensionMetadataType updatedExtensionMetadata;
+  QHash<QString, QString> serverToExtensionDescriptionKey = Self::serverToExtensionDescriptionKey(serverAPI);
+  foreach(const QString& key, extensionMetadata.keys())
+    {
+    updatedExtensionMetadata.insert(
+      serverToExtensionDescriptionKey.value(key, key), extensionMetadata.value(key));
+    }
+  return updatedExtensionMetadata;
 }
 
 // --------------------------------------------------------------------------

Base/QTCore/qSlicerExtensionsManagerModel.h
@@ -278,10 +278,18 @@ public:
   /// Return the item model used internally
   Q_INVOKABLE const QStandardItemModel * model()const;
 
-  static QHash<QString, QString> serverToExtensionDescriptionKey(int serverAPI = Self::Midas_v1);
+  /// \sa convertExtensionMetadata()
+  static QHash<QString, QString> serverToExtensionDescriptionKey(int serverAPI);
 
+  /// Convert server keys to description keys.
+  /// \sa serverToExtensionDescriptionKey()
+  static ExtensionMetadataType convertExtensionMetadata(const ExtensionMetadataType &extensionMetadata, int serverAPI);
+
+  /// \sa filterExtensionMetadata()
   static QStringList serverKeysToIgnore(int serverAPI = Self::Midas_v1);
 
+  /// Exclude irrelevant server metadata.
+  /// \sa serverKeysToIgnore()
   static ExtensionMetadataType filterExtensionMetadata(const ExtensionMetadataType &extensionMetadata, int serverAPI = Self::Midas_v1);
 
   static QStringList readArrayValues(QSettings& settings,

commit 1cefeec649c6938f4bb1d14d661550f7361a36d8

BUG: Fix installation of extension
This commit fixes a regression introduced in 4fe811f63 (ENH: Refactor
ExtensionsManagerModel introducing convertExtensionMetadata) by making
sure the converted metadata "extension_id" is used instead of the server
key "_id". Conversion rules are encoded in function "serverToExtensionDescriptionKey"

Co-authored-by: James Butler <jbutler@sonovol.com>

Base/QTCore/qSlicerExtensionsManagerModel.cxx
@@ -1522,7 +1522,7 @@ qSlicerExtensionsManagerModelPrivate::downloadExtension(
 
   if (q->serverAPI() == qSlicerExtensionsManagerModel::Girder_v1)
     {
-    QString item_id = extensionMetadata["_id"].toString();
+    QString item_id = extensionMetadata["extension_id"].toString();
 
     // Retrieve file_id associated with the item
     QString file_id;