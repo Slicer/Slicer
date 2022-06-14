ea70cee3ac90e835751d46f4082f0bae5517634d
Csaba Pinter <pinter.csaba@gmail.com>
2022-06-14 17:14:12 +0100

    BUG: Fix RemoveAllSegments function to actually remove the segments
    The segment ID list was called early in vtkSegmentation::RemoveAllSegments, and the segments were thus not properly removed. Besides the memory leak, this caused bugs related to expecting the SegmentRemoved event, such as that the segments were not removed from Subject Hierarchy when using said function.
    
    Fixing segmentation automated test by setting selected segment explicitly, which selection was lost due to now properly removing the segments.

--- Libs/vtkSegmentationCore/vtkSegmentation.cxx
+++ Libs/vtkSegmentationCore/vtkSegmentation.cxx
@@ -569,8 +569,6 @@ void vtkSegmentation::RemoveSegment(SegmentMap::iterator segmentIt)
 //---------------------------------------------------------------------------
 void vtkSegmentation::RemoveAllSegments()
 {
-  this->SegmentIds.clear();
-
   std::vector<std::string> segmentIds;
   this->GetSegmentIDs(segmentIds);
   for (std::vector<std::string>::iterator segmentIt = segmentIds.begin(); segmentIt != segmentIds.end(); ++segmentIt)
@@ -578,6 +576,7 @@ void vtkSegmentation::RemoveAllSegments()
     this->RemoveSegment(*segmentIt);
     }
   this->Segments.clear();
+  this->SegmentIds.clear();
 
   this->SegmentIdAutogeneratorIndex = 0;
 }
