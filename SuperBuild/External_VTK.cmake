83331664db4f89b45a397460bfd0a0cb16a06dd2
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-06-17 00:51:05 -0400

    BUG: Update VTK with selected vtkRendering[VR|OpenVR|OpenXR] fixes and features
    (cherry picked from commit f00f401c3)
    
    Rendering changes associated with these merge requests have not
    been backported:
    - MR-9174: volume-light-uniforms
    - MR-9231: advanced-shadows
    - MR-9219: fix-specular-twosided
    - MR-9207: volumetric-shadows-feature
    - MR-9126: volumetric-rendering-correction
    
    List of VTK changes:
    
    $ git shortlog efbbf9c02..1abf75af7 --no-merges
    Andrew Wilson (3):
          [Backport] Add OpenXR haptics json reading & vibration function
          [Backport] Remove doxygen grouping symbol
          [Backport] Comments and better defaults for haptic duration
    
    David Berger (1):
          [Backport] Add SetConstrainedFontSize to vtkLeaderActor2D to enable fixed font sizes
    
    Gaspard Thevenon (3):
          [Backport] Fix shader crash in Multi Volume Rendering without GradientTF
          [Backport] Fix 2D TF with MultiVolume
          [Backport] Add checks for number of lights and warning in vtkOpenGLGPUVolumeRayCastMapper
    
    Ken Martin (2):
          [Backport] feat(Rendering): add coordinate systems to Prop3D
          [Backport] fix(Rendering): fix prop3d DEVICE coordinates on OpenGL
    
    Lucas Gandel (4):
          [Backport] Fix vtkOpenXRManager singleton destructor not being called
          [Backport] Remove useless modifications to the OpenXR render window state
          [Backport] Fix OpenXR view pose
          [Backport] Fix undefined vtkOpenXRManager::GetRecommendedImageRectSize
    
    Lucas Givord (2):
          [Backport] Fix missing check dimension in the vtkImageItem
          [Backport] Fix crash when we use renderLineAsTube without light in scene
    
    LucasGandel (1):
          [Backport] Fix use of vtkDebugMacro in vtkOpenXRManager that is not a vtkObject
    
    Mathieu Westphal (1):
          [Backport] Ensure XCloseDisplay is called, and at the right time
    
    Nicolas Vuaille (1):
          [Backport] Update vtkCamera ParallelProjectionScale documentation
    
    Sankhesh Jhaveri (4):
          [Backport] Ensure that dangling references are cleaned up
          [Backport] Remove use of deprecated cell types API for ospray
          [Backport] Avoid using variable names that shadow Windows keywords
          [Backport] Avoid using variable names that shadow Windows keywords
    
    Scott Wittenburg (2):
          [Backport] RenderingOpenXR: Fixes to support build/run on Linux
          [Backport] OpenXR: Create graphics binding with non-null objects
    
    Sjors Peterse (1):
          [Backport] Change vtkProp to vtkProp3D in documentation
    
    Spiros Tsalikis (3):
          [Backport] vtkOpenXRUtilities: Remove constexpr
          [Backport] vtkOpenXRManager: Fix usage of strcmp
          [Backport] vtkOpenXRModel: Fix atomic instantiation
    
    Stephen Hogarth (1):
          [Backport] Add Faster AddRGBPoints Routine
    
    Thomas Galland (3):
          [Backport] Add new movement style for VR
          [Backport] Desunite ground movement and elevation in VR
          [Backport] VRInteractorStyle code refactoring
    
    Tiffany Chhim (1):
          [Backport] Move getter for window initialization from OpenXR to VR module
    
    Timothee Chabat (1):
          [Backport] Make wide lines have a minimal screen space size
    
    luz paz (1):
          [Backport] Fix typos in Rendering/

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -139,7 +139,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
   set(_git_tag)
   if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "9")
-    set(_git_tag "efbbf9c02e989a8f2f12395634044779653f6f0e") # slicer-v9.1.20220125-efbe2afc2
+    set(_git_tag "1abf75af73840b478ad5705eba72d455dd67a09a") # slicer-v9.1.20220125-efbe2afc2
     set(vtk_egg_info_version "9.1.20220125")
   else()
     message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
