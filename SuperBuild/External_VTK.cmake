4c0f4a531082af25eff7d3392462aca458eab4a6
Andras Lasso <lasso@queensu.ca>
2022-09-01 09:33:08 -0400

    BUG: Update VTK to fix empty volume crash and joint smoothing
    VTK bugs caused Slicer issues, this pull request updates VTK to include the backported fixes:
    - Crash when creating empty volume from module (https://github.com/Slicer/Slicer/issues/6381)
    - Joint smoothing has no effect (in Segment Editor's Smoothing effect; and when generating closed surface labelmap representation)
    
    fixes #6381

--- SuperBuild/External_VTK.cmake
+++ SuperBuild/External_VTK.cmake
@@ -139,7 +139,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
   set(_git_tag)
   if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "9")
-    set(_git_tag "bccd2b8f7eff92ed08ef60b54ef60303861b5221") # slicer-v9.1.20220125-efbe2afc2
+    set(_git_tag "fb5f0740500a5bc251da4e19f3c162700c8b560a") # slicer-v9.1.20220125-efbe2afc2
     set(vtk_egg_info_version "9.1.20220125")
   else()
     message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
