Slicer/Slicer

commit 4d1c98ef8f9623cc4ef175b9dc734e9540bd9f02

COMP: Update ITK to post-v5.3rc04 version and SimpleITK to post-v2.2.0
ITK list of changes:
$ git shortlog --no-merges 46a71c7..05adf2d
Alex (1):
      BUG: GitHub Workflows security hardening

Brad King (2):
      ENH: Update double-conversion import script to record prior import
      ENH: Update double-conversion import script to get latest version

Bradley Lowekamp (12):
      BUG: Fix numeric overflow in SLIC initialization
      BUG: Add DOXYGEN_(FORMAT) options to DoxygenConfig.cmake
      BUG: Fix Doxygen configuration for output formats
      BUG: Remove doxygen syntax markup for cmake lists
      ENH: Add option to disable the server side searching
      ENH: Adding GTest for LabelOverlapMeasuresImageFilter
      BUG: Use named inputs for LabelOverlapMeasuresImageFilter
      ENH: Update LabelOverlapMeasures to use dynamic threading
      ENH: Use ImageSink for LabelOverlapMeasures base class
      BUG: Update LabelOverlapMeasures internal class
      PERF: improve map usage and increment prefix
      BUG: initialize image buffer in Nifti direction test

Bryn Lloyd (1):
      BUG: ReconstructBiasField is setting bias field regions incorrectly

Butui Hu (1):
      COMP: use sysconfig.get_path as PEP 632 deprecate distutils module

Dženan Zukić (25):
      ENH: Refactor SpatialOrientation enumeration classes
      ENH: Add a Python test which demonstrates use of spatial orientation
      DOC: VLV CanBeDivided struct divides, not multiplies
      COMP: Update use of SpatialOrientation in non-default module PhilipsREC
      ENH: Follow-up to SpatialOrientationEnums refactoring
      ENH: Update remote modules by using the script
      ENH: Updating remote modules using the script
      ENH: Update Remote modules using the script
      ENH: Git hooks were updated
      ENH: Add Python wrapping for SpatialOrientationAdapter
      ENH: Make OrientationAdapterBase deprecated
      ENH: Mark CMake variables as advanced

SuperBuild/External_SimpleITK.cmake
@@ -123,7 +123,7 @@ set(ENV{LibraryPaths} \"${_paths}${_path_sep}\$ENV{${_varname}}\")
   file(WRITE ${_install_script}
 "include(\"${_env_script}\")
 set(${proj}_WORKING_DIR \"${EP_BINARY_DIR}/SimpleITK-build/Wrapping/Python\")
-ExternalProject_Execute(${proj} \"install\" \"${PYTHON_EXECUTABLE}\" setup.py install)
+ExternalProject_Execute(${proj} \"install\" \"${PYTHON_EXECUTABLE}\" \"-m\" \"pip\" \"install\" \".\")
 ")
 
   ExternalProject_SetIfNotDefined(
@@ -134,7 +134,7 @@ ExternalProject_Execute(${proj} \"install\" \"${PYTHON_EXECUTABLE}\" setup.py in
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "ffd48e274e2112928e95e5dfc802d8ca3121c840"
+    "1c0cf5de03fd8b8279a6522e910bbce67a4d8bf8"  # post-v2.2.0 2022-08-30
     QUIET
     )
 

SuperBuild/External_ITK.cmake
@@ -30,13 +30,13 @@ if(NOT DEFINED ITK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_REPOSITORY
-    "${EP_GIT_PROTOCOL}://github.com/Slicer/ITK"
+    "${EP_GIT_PROTOCOL}://github.com/InsightSoftwareConsortium/ITK"
     QUIET
     )
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "46a71c744d4856a5166df227e13c76e8c3ef55cc" # slicer-v5.3rc03-2022-02-10-be81e62
+    "05adf2d1f617c2c4b206cb119f72cbb0904158e4" # post-v5.3rc04 2022-08-30
     QUIET
     )
 

commit 68a056adf490d2efceb284cf26d3cac05cc8f2d5

COMP: Fix SimpleITK installation ensuring its uses newer version of pip
This commit fixes a regression introduced in 4d1c98ef8 (COMP: Update ITK
to post-v5.3rc04 version and SimpleITK to post-v2.2.0) where SimpleITK
installation method was changed to use "pip install" instead of "setup.py"

Since by default the project "python-ensurepip" leads to the installation
of pip 21.2.4, SimpleITK was installed using a version of pip older than
21.3 and didn't have the "in-tree-build" feature enabled by default.

To address the problem, this commit adds the dependency to the
python-pip project and ensures that pip >= 21.3 is used.

Co-authored-by: James Butler <jamesobutler@users.noreply.github.com>

SuperBuild/External_SimpleITK.cmake
@@ -2,7 +2,13 @@
 set(proj SimpleITK)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES ITK Swig python python-setuptools)
+set(${proj}_DEPENDENCIES
+  ITK
+  python
+  python-pip
+  python-setuptools
+  Swig
+  )
 
 # Include dependent projects if any
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)