Slicer/Slicer

commit 3ce46788cde6d74167cd5bb7639855dc1e59401e

ENH: Add check for available application updates
Now that a new Slicer Stable Releases is planned to be created in every quarter, but extensions are only updated for the latest stable, it is important to keep moving users to the latest Slicer version.

This commit adds an application update check to make users aware that a more recent Slicer Stable Release is available that the currently running Slicer.

Automatic update check is enabled by default. It can be enabled/disabled in the Welcome module and in Application Settings.
Update check can be initiated manually in the Welcome module.

Application updates can be completely disabled using Slicer_BUILD_APPLICATIONUPDATE_SUPPORT CMake variable, or can be temporarily disabled in Slicer.ini (ApplicationUpdates/Enabled=false).

fixes #5502

Base/QTGUI/qSlicerSettingsGeneralPanel.cxx
@@ -32,6 +32,7 @@
 
 // QtGUI includes
 #include "qSlicerApplication.h"
+#include "qSlicerApplicationUpdateManager.h"
 #include "qSlicerRelativePathMapper.h"
 #include "qSlicerSettingsGeneralPanel.h"
 #include "ui_qSlicerSettingsGeneralPanel.h"
@@ -92,6 +93,37 @@ void qSlicerSettingsGeneralPanelPrivate::init()
   this->LanguageLabel->setVisible(false);
   this->LanguageComboBox->setVisible(false);
 #endif
+
+  bool applicationUpdateEnabled = false;
+#ifdef Slicer_BUILD_I18N_SUPPORT
+  applicationUpdateEnabled = qSlicerApplicationUpdateManager::isApplicationUpdateEnabled();
+  if (applicationUpdateEnabled)
+    {
+    this->ApplicationUpdateServerURLLineEdit->setText("https://download.slicer.org");
+
+    q->registerProperty("ApplicationUpdate/AutoUpdateCheck", this->ApplicationAutoUpdateCheckCheckBox, "checked",
+      SIGNAL(toggled(bool)));
+
+    q->registerProperty("ApplicationUpdate/ServerUrl", this->ApplicationUpdateServerURLLineEdit, "text",
+      SIGNAL(textChanged(QString)),
+      "Application update server URL");
+
+    qSlicerApplication* app = qSlicerApplication::application();
+    if (app && app->applicationUpdateManager())
+      {
+      QObject::connect(app->applicationUpdateManager(), SIGNAL(autoUpdateCheckChanged()),
+        q, SLOT(updateAutoUpdateApplicationFromManager()));
+
+      QObject::connect(this->ApplicationAutoUpdateCheckCheckBox, SIGNAL(toggled(bool)),
+        app->applicationUpdateManager(), SLOT(setAutoUpdateCheck(bool)));
+      }
+
+    }
+#endif
+  this->ApplicationAutoUpdateCheckLabel->setVisible(applicationUpdateEnabled);
+  this->ApplicationAutoUpdateCheckCheckBox->setVisible(applicationUpdateEnabled);
+  this->ApplicationUpdateServerURLLabel->setVisible(applicationUpdateEnabled);
+  this->ApplicationUpdateServerURLLineEdit->setVisible(applicationUpdateEnabled);
 
 #ifdef Slicer_USE_PYTHONQT
   if (!qSlicerCoreApplication::testAttribute(qSlicerCoreApplication::AA_DisablePython))
@@ -217,4 +249,17 @@ void qSlicerSettingsGeneralPanel::openSlicerRCFile()
       }
     }
   QDesktopServices::openUrl(QUrl("file:///" + slicerRcFileName, QUrl::TolerantMode));
+}
+
+// --------------------------------------------------------------------------
+void qSlicerSettingsGeneralPanel::updateAutoUpdateApplicationFromManager()
+{
+  Q_D(qSlicerSettingsGeneralPanel);
+  qSlicerApplication* app = qSlicerApplication::application();
+  if (!app->applicationUpdateManager())
+    {
+    return;
+    }
+  QSignalBlocker blocker1(d->ApplicationAutoUpdateCheckCheckBox);
+  d->ApplicationAutoUpdateCheckCheckBox->setChecked(app->applicationUpdateManager()->autoUpdateCheck());
 }

Base/QTCore/CMakeLists.txt
@@ -40,7 +40,7 @@ endif()
 #
 # qRestAPI
 #
-if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
+if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT OR Slicer_BUILD_APPLICATIONUPDATE_SUPPORT)
   find_package(qRestAPI REQUIRED)
   include(${qRestAPI_USE_FILE})
 endif()
@@ -120,6 +120,13 @@ if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
     )
 endif()
 
+if(Slicer_BUILD_APPLICATIONUPDATE_SUPPORT)
+  list(APPEND KIT_SRCS
+    qSlicerApplicationUpdateManager.cxx
+    qSlicerApplicationUpdateManager.h
+    )
+endif()
+
 if(Slicer_USE_PYTHONQT)
   list(APPEND KIT_SRCS
     qSlicerScriptedFileReader.cxx
@@ -162,6 +169,12 @@ if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
     )
 endif()
 
+if(Slicer_BUILD_APPLICATIONUPDATE_SUPPORT)
+  list(APPEND KIT_MOC_SRCS
+    qSlicerApplicationUpdateManager.h
+    )
+endif()
+
 if(Slicer_USE_PYTHONQT)
   list(APPEND KIT_MOC_SRCS
     qSlicerScriptedFileReader.h
@@ -188,7 +201,7 @@ if(Slicer_BUILD_DICOM_SUPPORT)
     )
 endif()
 
-if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
+if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT OR Slicer_BUILD_APPLICATIONUPDATE_SUPPORT)
   list(APPEND KIT_target_libraries
     qRestAPI
     )

Base/QTCore/Testing/Cxx/CMakeLists.txt
@@ -34,6 +34,9 @@ if(BUILD_TESTING)
       "30987_markupstomodel_macosx_amd64_835453f.json"
       "30987_curvemaker_macosx_amd64_5619cf5.json"
       "30338_slicerheart_win_amd64_1db3627.json"
+      "application_30893_slicer_linux_amd64.json"
+      "application_30893_slicer_macosx_amd64.json"
+      "application_30893_slicer_win_amd64.json"
       )
     configure_file(
       ../Data/Input/${filename}
@@ -64,6 +67,11 @@ if(BUILD_TESTING)
       qSlicerPersistentCookieJarTest.cxx
       )
   endif()
+  if(Slicer_BUILD_APPLICATIONUPDATE_SUPPORT)
+    list(APPEND KIT_TEST_SRCS
+      qSlicerApplicationUpdateManagerTest.cxx
+      )
+  endif()
   if(Slicer_USE_PYTHONQT)
     list(APPEND KIT_TEST_SRCS
       qSlicerCorePythonManagerWithoutApplicationTest.cxx
@@ -89,6 +97,12 @@ if(BUILD_TESTING)
       qSlicerExtensionsManagerModelTest.cxx
       qSlicerPersistentCookieJarTest.cxx
       )
+  endif()
+
+  if(Slicer_BUILD_APPLICATIONUPDATE_SUPPORT)
+  list(APPEND KIT_TEST_GENERATE_MOC_SRCS
+    qSlicerApplicationUpdateManagerTest.cxx
+    )
   endif()
 
   if(Slicer_USE_PYTHONQT)
@@ -130,6 +144,10 @@ if(BUILD_TESTING)
   if(Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
     simple_test( qSlicerExtensionsManagerModelTest )
     simple_test( qSlicerPersistentCookieJarTest )
+  endif()
+
+  if(Slicer_BUILD_APPLICATIONUPDATE_SUPPORT)
+    simple_test( qSlicerApplicationUpdateManagerTest )
   endif()
 
   if(Slicer_USE_PYTHONQT)

Base/QTCore/Testing/Cxx/Resources/qSlicerBaseQTCoreTesting.qrc
@@ -15,5 +15,9 @@
     <file alias="extension-macosx-2.tar.gz">30987-macosx-amd64-CurveMaker-git5619cf5-2019-09-09.tar.gz</file>
 
     <file alias="extension-win-0.json">30338_slicerheart_win_amd64_1db3627.json</file>
+
+    <file>application_30893_slicer_linux_amd64.json</file>
+    <file>application_30893_slicer_macosx_amd64.json</file>
+    <file>application_30893_slicer_win_amd64.json</file>
   </qresource>
 </RCC>

Base/QTCore/Testing/Cxx/qSlicerApplicationUpdateManagerTest.cxx
@@ -1,0 +1,222 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) Kitware Inc.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+==============================================================================*/
+
+// Qt includes
+#include <qSlicerCoreApplication.h>
+#include <QScriptEngine>
+
+// CTK includes
+#include <ctkTest.h>
+#include <ctkUtils.h>
+
+// qRestAPI includes
+#include <qRestAPI.h>
+
+// QtCore includes
+#include "qSlicerApplicationUpdateManager.h"
+#include "vtkSlicerConfigure.h"
+#include "vtkSlicerVersionConfigure.h"
+
+// STD includes
+#include <iostream>
+
+// ----------------------------------------------------------------------------
+class qSlicerApplicationUpdateManagerTester: public QObject
+{
+  Q_OBJECT
+  typedef qSlicerApplicationUpdateManagerTester Self;
+
+private:
+
+  bool prepareJson(const QString& jsonFile);
+
+  bool resetTmp();
+  QDir Tmp;
+  QString TemporaryDirName;
+
+private slots:
+  void initTestCase();
+  void init();
+  void cleanup();
+  void cleanupTestCase();
+
+  void testServerUrl();
+  void testServerUrl_data();
+
+  void testUpdateCheck();
+  void testUpdateCheck_data();
+};
+
+// ----------------------------------------------------------------------------
+bool qSlicerApplicationUpdateManagerTester::prepareJson(const QString& jsonFile)
+{
+  bool success = true;
+  QDir tmp = QDir::temp();
+  if (!QFile::exists(tmp.filePath(this->TemporaryDirName)))
+    {
+    success = tmp.mkdir(this->TemporaryDirName);
+    }
+  success = tmp.cd(this->TemporaryDirName);
+  QString filename = QLatin1String("find");
+  success = success && QFile::copy(jsonFile, tmp.filePath(filename));
+  success = success && QFile::setPermissions(tmp.filePath(filename), QFile::ReadOwner | QFile::WriteOwner);
+  return success;
+}
+
+// ----------------------------------------------------------------------------
+bool qSlicerApplicationUpdateManagerTester::resetTmp()
+{
+  if (this->TemporaryDirName.isEmpty())
+    {
+    return false;
+    }
+  QDir tmp = QDir::temp();
+  QFile(tmp.filePath(this->TemporaryDirName)).setPermissions(QFile::ReadUser | QFile::WriteUser | QFile::ExeUser);
+  ctk::removeDirRecursively(tmp.filePath(this->TemporaryDirName));
+  tmp.mkdir(this->TemporaryDirName);
+  tmp.cd(this->TemporaryDirName);
+  this->Tmp = tmp;
+  return this->Tmp.exists();
+}
+
+// ----------------------------------------------------------------------------
+void qSlicerApplicationUpdateManagerTester::initTestCase()
+{
+  QSettings::setDefaultFormat(QSettings::IniFormat);
+
+  QVERIFY(QDir::temp().exists());
+
+  this->TemporaryDirName =
+      QString("qSlicerApplicationUpdateManagerTester.%1").arg(QTime::currentTime().toString("hhmmsszzz"));
+
+  QSettings().clear();
+}
+
+// ----------------------------------------------------------------------------
+void qSlicerApplicationUpdateManagerTester::init()
+{
+}
+
+// ----------------------------------------------------------------------------
+void qSlicerApplicationUpdateManagerTester::cleanup()
+{
+  QSettings().clear();
+}
+
+// ----------------------------------------------------------------------------
+void qSlicerApplicationUpdateManagerTester::cleanupTestCase()
+{
+  if (this->Tmp != QDir::current() && this->Tmp.exists())
+    {
+    ctk::removeDirRecursively(this->Tmp.absolutePath());
+    this->Tmp = QDir();
+    }
+  QFile::remove(QSettings().fileName());
+}
+
+// ----------------------------------------------------------------------------
+void qSlicerApplicationUpdateManagerTester::testServerUrl()
+{
+  QFETCH(QString, serverUrl);
+
+  QSettings().setValue("ApplicationUpdate/ServerUrl", serverUrl);
+  qSlicerApplicationUpdateManager model;
+
+  QUrl currentServerUrl = model.serverUrl();
+  QVERIFY(currentServerUrl.isValid());
+  QCOMPARE(currentServerUrl.toString(), serverUrl);
+}
+
+// ----------------------------------------------------------------------------
+void qSlicerApplicationUpdateManagerTester::testServerUrl_data()
+{
+  QTest::addColumn<QString>("serverUrl");
+
+  QTest::newRow("localhost") << "http://localhost";
+
+  QTest::newRow("windows-file") << QUrl::fromLocalFile("C:/path/to/foo").toString();
+}
+
+// ----------------------------------------------------------------------------
+void qSlicerApplicationUpdateManagerTester::testUpdateCheck()
+{
+  QVERIFY(this->resetTmp());
+
+  QFETCH(QString, jsonFile);
+  QFETCH(QString, operatingSystem);
+  QFETCH(QString, architecture);
+  QFETCH(QString, slicerRevision);
+  QFETCH(bool, expectedIsUpdateAvailable);
+  QFETCH(QString, expectedLatestReleaseVersion);
+  QFETCH(QString, expectedLatestReleaseRevision);
+
+  QVERIFY2(this->prepareJson(jsonFile),
+           QString("Failed to prepare json file: %1").arg(jsonFile).toUtf8());
+
+  QSettings().setValue("ApplicationUpdate/ServerUrl", QUrl::fromLocalFile(this->Tmp.absolutePath()));
+
+  qSlicerApplicationUpdateManager updateManager;
+  updateManager.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
+
+  QSignalSpy spyUpdateCheckCompleted(&updateManager, SIGNAL(updateCheckCompleted(bool)));
+  QSignalSpy spyUpdateAvailable(&updateManager, SIGNAL(updateAvailable(bool)));
+
+  updateManager.checkForUpdate(/* force= */ true, /* waitForCompletion= */ true);
+
+  QCOMPARE(spyUpdateCheckCompleted.count(), 1);
+  if (expectedIsUpdateAvailable)
+    {
+    QCOMPARE(spyUpdateAvailable.count(), 1);
+    }
+  else
+    {
+    QCOMPARE(spyUpdateAvailable.count(), 0);
+    }
+  QCOMPARE(updateManager.isUpdateAvailable(), expectedIsUpdateAvailable);
+  QCOMPARE(updateManager.latestReleaseVersion(), expectedLatestReleaseVersion);
+  QCOMPARE(updateManager.latestReleaseRevision(), expectedLatestReleaseRevision);
+}
+
+// ----------------------------------------------------------------------------
+void qSlicerApplicationUpdateManagerTester::testUpdateCheck_data()
+{
+  QTest::addColumn<QString>("jsonFile");
+  QTest::addColumn<QString>("operatingSystem");
+  QTest::addColumn<QString>("architecture");
+  QTest::addColumn<QString>("slicerRevision");
+  QTest::addColumn<bool>("expectedIsUpdateAvailable");
+  QTest::addColumn<QString>("expectedLatestReleaseVersion");
+  QTest::addColumn<QString>("expectedLatestReleaseRevision");
+
+  QTest::newRow("1") << ":/application_30893_slicer_win_amd64.json" << Slicer_OS_WIN_NAME << "amd64" << "29000" << true << "5.0.3" << "30893";
+  QTest::newRow("2") << ":/application_30893_slicer_linux_amd64.json" << Slicer_OS_LINUX_NAME << "amd64" << "29000" << true << "5.0.3" << "30893";
+  QTest::newRow("3") << ":/application_30893_slicer_macosx_amd64.json" << Slicer_OS_MAC_NAME << "amd64" << "29000" << true << "5.0.3" << "30893";
+
+  QTest::newRow("4") << ":/application_30893_slicer_win_amd64.json" << Slicer_OS_WIN_NAME << "amd64" << "31000" << false << "5.0.3" << "30893";
+  QTest::newRow("5") << ":/application_30893_slicer_linux_amd64.json" << Slicer_OS_LINUX_NAME << "amd64" << "31000" << false << "5.0.3" << "30893";
+  QTest::newRow("6") << ":/application_30893_slicer_macosx_amd64.json" << Slicer_OS_MAC_NAME << "amd64" << "31000" << false << "5.0.3" << "30893";
+}
+
+// ----------------------------------------------------------------------------
+int qSlicerApplicationUpdateManagerTest(int argc, char* argv[])
+{
+  qSlicerCoreApplication app(argc, argv);
+  QTEST_DISABLE_KEYPAD_NAVIGATION
+  qSlicerApplicationUpdateManagerTester tc;
+  return QTest::qExec(&tc, argc, argv);
+}
+#include "moc_qSlicerApplicationUpdateManagerTest.cxx"

commit 97f24af025d5c89491cfe149067ae4ab21c91815

COMP: Fix build error for Application Update off and I18N on
This appears to have been a typo introduced in https://github.com/Slicer/Slicer/commit/3ce46788cde6d74167cd5bb7639855dc1e59401e

Base/QTGUI/qSlicerSettingsGeneralPanel.cxx
@@ -95,7 +95,7 @@ void qSlicerSettingsGeneralPanelPrivate::init()
 #endif
 
   bool applicationUpdateEnabled = false;
-#ifdef Slicer_BUILD_I18N_SUPPORT
+#ifdef Slicer_BUILD_APPLICATIONUPDATE_SUPPORT
   applicationUpdateEnabled = qSlicerApplicationUpdateManager::isApplicationUpdateEnabled();
   if (applicationUpdateEnabled)
     {
@@ -254,6 +254,7 @@ void qSlicerSettingsGeneralPanel::openSlicerRCFile()
 // --------------------------------------------------------------------------
 void qSlicerSettingsGeneralPanel::updateAutoUpdateApplicationFromManager()
 {
+#ifdef Slicer_BUILD_APPLICATIONUPDATE_SUPPORT
   Q_D(qSlicerSettingsGeneralPanel);
   qSlicerApplication* app = qSlicerApplication::application();
   if (!app->applicationUpdateManager())
@@ -262,4 +263,5 @@ void qSlicerSettingsGeneralPanel::updateAutoUpdateApplicationFromManager()
     }
   QSignalBlocker blocker1(d->ApplicationAutoUpdateCheckCheckBox);
   d->ApplicationAutoUpdateCheckCheckBox->setChecked(app->applicationUpdateManager()->autoUpdateCheck());
+#endif
 }