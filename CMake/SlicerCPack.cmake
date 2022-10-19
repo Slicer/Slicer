Slicer/Slicer

commit c1537605c8c60c2772b29402e524e2116a27437d

BUG: Fix macOS packaging of Qt designer
This commit ensures designer executable is fixed-up and packaged on macOS.
It also configures a launcher named "SlicerDesigner" allowing to start
Qt designer consistently on all platform.

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27709
git-svn-id: http://svn.slicer.org/Slicer4/trunk@27709 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerCPack.cmake
@@ -66,29 +66,52 @@ if(Slicer_BUILD_DICOM_SUPPORT AND NOT Slicer_USE_SYSTEM_DCMTK)
   include(${Slicer_CMAKE_DIR}/SlicerBlockInstallDCMTKApps.cmake)
 endif()
 
-# Get Qt root directory
-set(qt_root_dir "")
-if(Slicer_REQUIRED_QT_VERSION VERSION_GREATER "5")
-  get_property(_filepath TARGET "Qt5::Core" PROPERTY LOCATION_RELEASE)
-  get_filename_component(_dir ${_filepath} PATH)
-  set(qt_root_dir "${_dir}/..")
-endif()
-
-# Qt designer
-if(Slicer_BUILD_QT_DESIGNER_PLUGINS AND Slicer_REQUIRED_QT_VERSION VERSION_GREATER "5")
-  install(PROGRAMS ${qt_root_dir}/bin/designer${CMAKE_EXECUTABLE_SUFFIX}
-    DESTINATION ${Slicer_INSTALL_ROOT}/bin COMPONENT Runtime
-    )
+# Install Qt designer launcher
+if(Slicer_BUILD_QT_DESIGNER_PLUGINS AND ${Slicer_REQUIRED_QT_VERSION} VERSION_GREATER_EQUAL 5)
+  set(executablename "SlicerDesigner")
+  set(build_designer_executable "${QT_BINARY_DIR}/designer${CMAKE_EXECUTABLE_SUFFIX}")
   if(APPLE)
-    set(dollar "$")
-    install(CODE
-      "set(app ${Slicer_INSTALL_BIN_DIR}/designer)
-       set(appfilepath \"${dollar}ENV{DESTDIR}${dollar}{CMAKE_INSTALL_PREFIX}/${dollar}{app}\")
-       message(\"CPack: - Adding rpath to ${dollar}{app}\")
-       execute_process(COMMAND install_name_tool -add_rpath @loader_path/..  ${dollar}{appfilepath})"
-      COMPONENT Runtime
-      )
+    set(build_designer_executable "${QT_BINARY_DIR}/Designer.app/Contents/MacOS/designer")
   endif()
+  set(installed_designer_executable "designer-real${CMAKE_EXECUTABLE_SUFFIX}")
+  set(installed_designer_subdir ".")
+  if(APPLE)
+    set(installed_designer_executable "Designer")
+    set(installed_designer_subdir "Designer.app/Contents/MacOS/designer")
+  endif()
+  find_package(CTKAppLauncher REQUIRED)
+  ctkAppLauncherConfigureForExecutable(
+    APPLICATION_NAME ${executablename}
+    SPLASHSCREEN_DISABLED
+    # Additional settings exclude groups
+    ADDITIONAL_SETTINGS_EXCLUDE_GROUPS "General,Application,ExtraApplicationToLaunch"
+    # Launcher settings specific to build tree
+    APPLICATION_EXECUTABLE ${build_designer_executable}
+    DESTINATION_DIR ${CMAKE_BINARY_DIR}/${Slicer_BIN_DIR}
+    ADDITIONAL_SETTINGS_FILEPATH_BUILD "${Slicer_BINARY_DIR}/${Slicer_BINARY_INNER_SUBDIR}/SlicerLauncherSettings.ini"
+    # Launcher settings specific to install tree
+    APPLICATION_INSTALL_EXECUTABLE_NAME designer-real${CMAKE_EXECUTABLE_SUFFIX}
+    APPLICATION_INSTALL_SUBDIR "${installed_designer_subdir}"
+    ADDITIONAL_SETTINGS_FILEPATH_INSTALLED "<APPLAUNCHER_SETTINGS_DIR>/SlicerLauncherSettings.ini"
+    )
+  # Install designer launcher settings
+  install(
+    FILES ${CMAKE_BINARY_DIR}/${Slicer_BIN_DIR}/${executablename}LauncherSettingsToInstall.ini
+    DESTINATION ${Slicer_INSTALL_BIN_DIR}
+    RENAME ${executablename}LauncherSettings.ini
+    COMPONENT Runtime
+    )
+  # Install designer launcher
+  set(_launcher CTKAppLauncher)
+  if(Slicer_BUILD_WIN32_CONSOLE)
+    set(_launcher CTKAppLauncherW)
+  endif()
+  install(
+    PROGRAMS ${CTKAppLauncher_DIR}/bin/${_launcher}${CMAKE_EXECUTABLE_SUFFIX}
+    DESTINATION ${Slicer_INSTALL_BIN_DIR}
+    RENAME ${executablename}${CMAKE_EXECUTABLE_SUFFIX}
+    COMPONENT Runtime
+    )
 endif()
 
 # -------------------------------------------------------------------------

CMake/SlicerBlockInstallQt.cmake
@@ -30,10 +30,6 @@ if(Slicer_REQUIRED_QT_VERSION VERSION_LESS "5")
 
 else()
 
-  if(NOT EXISTS ${qt_root_dir})
-    message(FATAL_ERROR "qt_root_dir set to nonexistent directory [${qt_root_dir}]")
-  endif()
-
   list(APPEND QT_LIBRARIES
     "Qt5::Gui"
     )
@@ -56,6 +52,11 @@ else()
   endif()
 
   if(UNIX)
+
+    # Get root directory
+    get_property(_filepath TARGET "Qt5::Core" PROPERTY LOCATION_RELEASE)
+    get_filename_component(_dir ${_filepath} PATH)
+    set(qt_root_dir "${_dir}/..")
 
     find_package(Qt5 REQUIRED COMPONENTS
       DBus
@@ -183,6 +184,14 @@ else()
         execute_process(COMMAND \"${windeployqt}\" ${_args} \"\${CMAKE_INSTALL_PREFIX}/bin/${executable}\")
       "
       COMPONENT Runtime
+      )
+  endif()
+
+  # Qt designer
+  if(Slicer_BUILD_QT_DESIGNER_PLUGINS)
+    install(PROGRAMS ${qt_root_dir}/bin/designer${CMAKE_EXECUTABLE_SUFFIX}
+      DESTINATION ${Slicer_INSTALL_ROOT}/bin COMPONENT Runtime
+      RENAME designer-real${CMAKE_EXECUTABLE_SUFFIX}
       )
   endif()
 endif()

CMake/SlicerCPackBundleFixup.cmake.in
@@ -322,12 +322,13 @@ function(fixup_bundle_with_plugins app)
     "${libs_path}"
     )
 
-  # Since BundleUtilities function do not copy the "Helpers" directory found
-  # in frameworks. The following will explicitly copy and fixup
-  # QtWebEngineCore.framework/Versions/5/Helpers/QtWebEngineProcess.app
   set(Slicer_USE_SYSTEM_QT "@Slicer_USE_SYSTEM_QT@")
   if(NOT Slicer_USE_SYSTEM_QT)
     set(qt_root_dir "@qt_root_dir@")
+
+    # Since BundleUtilities function do not copy the "Helpers" directory found
+    # in frameworks. The following will explicitly copy and fixup
+    # QtWebEngineCore.framework/Versions/5/Helpers/QtWebEngineProcess.app
     set(qtwebenginecore_app_subdir "Helpers/QtWebEngineProcess.app")
     set(qtwebenginecore_src_dir "${qt_root_dir}/QtWebEngineCore.framework/${qtwebenginecore_app_subdir}")
     set(qtwebenginecore_dest_dir "${app_dir}/Contents/Frameworks/QtWebEngineCore.framework/Versions/Current/${qtwebenginecore_app_subdir}")
@@ -364,6 +365,38 @@ function(fixup_bundle_with_plugins app)
         WORKING_DIRECTORY "${app_dir}/Contents/Frameworks/QtWebEngineCore.framework"
         )
     endif()
+
+   # Since BundleUtilities module does not know how to install .app directory withing
+   # an existing app, the following explicitly install the designer application.
+   set(Slicer_BUILD_QT_DESIGNER_PLUGINS "@Slicer_BUILD_QT_DESIGNER_PLUGINS@")
+   set(designer_app_subdir "Designer.app")
+   set(designer_src_dir "${qt_root_dir}/../bin/${designer_app_subdir}")
+   set(designer_dest_dir "${app_dir}/Contents/bin/${designer_app_subdir}")
+   if(Slicer_BUILD_QT_DESIGNER_PLUGINS AND EXISTS ${designer_src_dir})
+     file(COPY "${designer_src_dir}/Contents" DESTINATION "${designer_dest_dir}/" USE_SOURCE_PERMISSIONS)
+     set(designer_executable "${designer_dest_dir}/Contents/MacOS/Designer")
+     # Collect list of Qt framework dependencies
+     set(prereqs )
+     get_prerequisites(${designer_executable} "prereqs" 1 0 "" "")
+     # Generate list of "-change" arguments
+     set(changes "")
+     foreach(item ${prereqs})
+       gp_item_default_embedded_path_override("${item}" path)
+       get_filename_component(item_name "${item}" NAME)
+       # Examples of values:
+       #  item          : /Volumes/Dashboards/Support/qt-everywhere-build-5.10.0/lib/QtNetwork.framework/Versions/5/QtNetwork
+       #  item_name     : QtNetwork
+       #  path          : @rpath/Frameworks
+       #  embedded_item : @rpath/Frameworks/QtNetwork.framework/Versions/5/QtNetwork
+       string(REGEX REPLACE "^.*(${item_name}.framework/.*/?${item_name}).*$" "${path}/\\1" embedded_item "${item}")
+       set(changes ${changes} "-change" "${item}" "${embedded_item}")
+     endforeach()
+     # Update executable
+     execute_process(COMMAND install_name_tool
+       -rpath "@executable_path/Frameworks" "@loader_path/../../../../"
+       ${changes} "${designer_executable}"
+       )
+   endif()
   endif()
 
 endfunction()

CMake/SlicerMacroBuildApplication.cmake
@@ -604,11 +604,11 @@ macro(slicerMacroBuildApplication)
         endif()
       endforeach()
 
-      if(${Slicer_REQUIRED_QT_VERSION} VERSION_GREATER_EQUAL 5 AND EXISTS ${QT_DESIGNER_EXECUTABLE})
+      if(${Slicer_REQUIRED_QT_VERSION} VERSION_GREATER_EQUAL 5 AND EXISTS ${QT_DESIGNER_EXECUTABLE} AND NOT APPLE)
         ctkAppLauncherAppendExtraAppToLaunchToList(
           LONG_ARG designer
           HELP "Start Qt designer using Slicer plugins"
-          PATH "<APPLAUNCHER_SETTINGS_DIR>/../bin/designer${CMAKE_EXECUTABLE_SUFFIX}"
+          PATH "<APPLAUNCHER_SETTINGS_DIR>/../bin/designer-real${CMAKE_EXECUTABLE_SUFFIX}"
           OUTPUTVAR extraApplicationToLaunchListForInstallTree
           )
       endif()

commit 8f7ae9aa1a4e01a868aa08ec9f4b0472995d42b8

BUG: Fix SlicerDesigner build-tree launcher settings
This commit fixes a regression introduced in c1537605 (BUG: Fix macOS
packaging of Qt designer) ensuring the `additionalSettingsFilePath` key
is associated with a valid path.

Before:

  additionalSettingsFilePath=/path/to/Slicer-Release/Slicer-build/Slicer-build/SlicerLauncherSettings.ini

After:

  additionalSettingsFilePath=/path/to/Slicer-Release/Slicer-build/SlicerLauncherSettings.ini

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

CMake/SlicerCPack.cmake
@@ -85,7 +85,7 @@ if(Slicer_BUILD_QT_DESIGNER_PLUGINS)
     # Launcher settings specific to build tree
     APPLICATION_EXECUTABLE ${build_designer_executable}
     DESTINATION_DIR ${CMAKE_BINARY_DIR}/${Slicer_BIN_DIR}
-    ADDITIONAL_SETTINGS_FILEPATH_BUILD "${Slicer_BINARY_DIR}/${Slicer_BINARY_INNER_SUBDIR}/SlicerLauncherSettings.ini"
+    ADDITIONAL_SETTINGS_FILEPATH_BUILD "${Slicer_BINARY_DIR}/SlicerLauncherSettings.ini"
     # Launcher settings specific to install tree
     APPLICATION_INSTALL_EXECUTABLE_NAME "${installed_designer_executable}"
     APPLICATION_INSTALL_SUBDIR "${installed_designer_subdir}"