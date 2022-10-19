77ba7bb616ac132ce59ae3fa18e2a4b6954740be
kerim371 <kerim.khemraev@mail.ru>
2022-10-19 04:00:25 -0400

    BUG: Fix SlicerDesigner settings for custom app by removing hard-coded name
    Remove hard-coded reference to the Slicer application name from
    "additionalSettingsFilePath" key found in the SlicerDesign launcher
    settings by using "Slicer_MAIN_PROJECT_APPLICATION_NAME".
    
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

--- CMake/SlicerCPack.cmake
+++ CMake/SlicerCPack.cmake
@@ -85,11 +85,11 @@ if(Slicer_BUILD_QT_DESIGNER_PLUGINS)
     # Launcher settings specific to build tree
     APPLICATION_EXECUTABLE ${build_designer_executable}
     DESTINATION_DIR ${CMAKE_BINARY_DIR}/${Slicer_BIN_DIR}
-    ADDITIONAL_SETTINGS_FILEPATH_BUILD "${Slicer_BINARY_DIR}/SlicerLauncherSettings.ini"
+    ADDITIONAL_SETTINGS_FILEPATH_BUILD "${Slicer_BINARY_DIR}/${Slicer_MAIN_PROJECT_APPLICATION_NAME}LauncherSettings.ini"
     # Launcher settings specific to install tree
     APPLICATION_INSTALL_EXECUTABLE_NAME "${installed_designer_executable}"
     APPLICATION_INSTALL_SUBDIR "${installed_designer_subdir}"
-    ADDITIONAL_SETTINGS_FILEPATH_INSTALLED "<APPLAUNCHER_SETTINGS_DIR>/SlicerLauncherSettings.ini"
+    ADDITIONAL_SETTINGS_FILEPATH_INSTALLED "<APPLAUNCHER_SETTINGS_DIR>/${Slicer_MAIN_PROJECT_APPLICATION_NAME}LauncherSettings.ini"
     )
   # Install designer launcher settings
   install(
