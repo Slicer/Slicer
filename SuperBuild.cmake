Slicer/Slicer

commit c0b92b4359c3d4a3b449307741e1d7a2be99aedb

ENH: Update Surface Toolbox to add uniform remeshing (ACVD) option
Adds a single commit:

Revision: 94957ef2c87438e6d21f6071a036232eb8119717
Author: Andras Lasso <lasso@queensu.ca>
Date: 2022-11-10 1:13:54 PM
Message:
ENH: Add uniform remeshing (ACVD) option to Surface Toolbox

This resampling typically provides higher quality meshes than decimation, with similar computation time.

SuperBuild.cmake
@@ -337,7 +337,7 @@ list_conditional_append(Slicer_BUILD_LandmarkRegistration Slicer_REMOTE_DEPENDEN
 
 Slicer_Remote_Add(SurfaceToolbox
   GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/Slicer/SlicerSurfaceToolbox"
-  GIT_TAG 74af7b5abb4af28c23c0ce3fe495a5c99bbb0be1
+  GIT_TAG 94957ef2c87438e6d21f6071a036232eb8119717
   OPTION_NAME Slicer_BUILD_SurfaceToolbox
   OPTION_DEPENDS "Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE

commit 98945ad80cd381ba4c30649eddf5491cf38f6f18

BUG: Update SurfaceToolbox to fix "Uniform remesh" parameter name
This commit fixes a regression introduced in c0b92b4359 (ENH: Update Surface Toolbox
to add uniform remeshing (ACVD) option.

List of changes:

$ git shortlog 94957ef2c..0de80503e --no-merges
Andras Lasso (1):
      BUG: Fix description of Uniform remesh cluster count parameter name

Co-authored-by: Andras Lasso <lasso@queensu.ca>

SuperBuild.cmake
@@ -337,7 +337,7 @@ list_conditional_append(Slicer_BUILD_LandmarkRegistration Slicer_REMOTE_DEPENDEN
 
 Slicer_Remote_Add(SurfaceToolbox
   GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/Slicer/SlicerSurfaceToolbox"
-  GIT_TAG 94957ef2c87438e6d21f6071a036232eb8119717
+  GIT_TAG 0de80503e4bd47b5d46c11766e65186c470746cb
   OPTION_NAME Slicer_BUILD_SurfaceToolbox
   OPTION_DEPENDS "Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE