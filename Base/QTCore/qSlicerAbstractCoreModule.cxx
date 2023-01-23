Slicer/Slicer

commit 1031050a0a677381ff40030a218702cd45fda668

BUG: Mark translatable strings in Base/QTCore
See https://github.com/Slicer/SlicerLanguagePacks/issues/12

Base/QTCore/qSlicerAbstractCoreModule.cxx
@@ -342,6 +342,8 @@ QString qSlicerAbstractCoreModule::defaultDocumentationLink()const
     return "";
     }
   QString url = app->moduleDocumentationUrl(this->name());
-  QString linkText = QString("<p>For more information see the <a href=\"%1\">online documentation</a>.</p>").arg(url);
-  return linkText;
+  QString linkText = QString("<p>")
+    + tr("For more information see the %1.").arg(
+        QString("<a href=\"%1\">%2</a>").arg(url).arg(tr("online documentation")))
+    + QString("</p>");
 }

Base/QTCore/qSlicerCoreApplication.cxx
@@ -222,9 +222,10 @@ void qSlicerCoreApplicationPrivate::init()
   // allow a debugger to be attached during startup
   if(qApp->arguments().contains("--attach-process"))
     {
-    QString msg("This message box is here to give you time to attach "
+    // Message for developers - do not translate.
+    QString msg(/*no tr*/"This message box is here to give you time to attach "
                 "your debugger to process [PID %1]");
-    QMessageBox::information(nullptr, "Attach process", msg.arg(QCoreApplication::applicationPid()));
+    QMessageBox::information(nullptr, /*no tr*/"Attach process", msg.arg(QCoreApplication::applicationPid()));
     }
 
   QCoreApplication::setOrganizationDomain(Slicer_ORGANIZATION_DOMAIN);
@@ -1299,7 +1300,7 @@ void qSlicerCoreApplication::setMRMLScene(vtkMRMLScene* newMRMLScene)
 
     // First scene needs a crosshair to be added manually
     vtkNew<vtkMRMLCrosshairNode> crosshair;
-    crosshair->SetCrosshairName("default");
+    crosshair->SetCrosshairName(/*no tr*/"default");
     newMRMLScene->AddNode(crosshair.GetPointer());
     }
 
@@ -1732,8 +1733,8 @@ int qSlicerCoreApplication::mainApplicationPatchVersion()const
 QString qSlicerCoreApplication::libraries()const
 {
   QString librariesText(
-    "Built on top of: "
-    "<a href=\"https://www.vtk.org/\">VTK</a>, "
+    tr("Built on top of:") +
+    " <a href=\"https://www.vtk.org/\">VTK</a>, "
     "<a href=\"https://www.itk.org/\">ITK</a>, "
     "<a href=\"https://www.commontk.org/index.php/Main_Page\">CTK</a>, "
     "<a href=\"https://www.qt.io/\">Qt</a>, "
@@ -1746,26 +1747,29 @@ QString qSlicerCoreApplication::libraries()const
 //-----------------------------------------------------------------------------
 QString qSlicerCoreApplication::copyrights()const
 {
-  QString copyrightsText(
+  QString copyrightsText(QString(
     "<table align=\"center\" border=\"0\" width=\"80%\"><tr>"
-    "<td align=\"center\"><a href=\"https://slicer.readthedocs.io/en/latest/user_guide/about.html#license\">Licensing Information</a></td>"
-    "<td align=\"center\"><a href=\"https://slicer.org/\">Website</a></td>"
-    "<td align=\"center\"><a href=\"https://slicer.readthedocs.io/en/latest/user_guide/about.html#acknowledgments\">Acknowledgments</a></td>"
-    "</tr></table>");
+    "<td align=\"center\"><a href=\"https://slicer.readthedocs.io/en/latest/user_guide/about.html#license\">%1</a></td>"
+    "<td align=\"center\"><a href=\"https://slicer.org/\">%2</a></td>"
+    "<td align=\"center\"><a href=\"https://slicer.readthedocs.io/en/latest/user_guide/about.html#acknowledgments\">%3</a></td>"
+    "</tr></table>")
+    .arg(tr("Licensing Information"))
+    .arg(tr("Website"))
+    .arg(tr("Acknowledgments")));
   return copyrightsText;
 }
 //-----------------------------------------------------------------------------
 QString qSlicerCoreApplication::acknowledgment()const
 {
   QString acknowledgmentText(
-    "Slicer is NOT an FDA approved medical device.<br /><br />"
-    "Supported by: NA-MIC, NAC, BIRN, NCIGT and the Slicer Community.<br /><br />"
-    "Special thanks to the NIH and our other supporters.<br /><br />"
+    tr("Slicer is NOT an FDA approved medical device.<br><br>"
+    "Supported by: NA-MIC, NAC, BIRN, NCIGT and the Slicer Community.<br><br>"
+    "Special thanks to the NIH and our other supporters.<br><br>"
     "This work is part of the National Alliance for Medical Image Computing "
     "(NA-MIC), funded by the National Institutes of Health through the NIH "
     "Roadmap for Medical Research, Grant U54 EB005149. Information on the "
-    "National Centers for Biomedical Computing can be obtained from "
-    "<a href=\"https://commonfund.nih.gov/bioinformatics\">https://commonfund.nih.gov/bioinformatics</a>.<br /><br />");
+    "National Centers for Biomedical Computing can be obtained from"
+    "<a href=\"https://commonfund.nih.gov/bioinformatics\">https://commonfund.nih.gov/bioinformatics</a>.<br><br>"));
   return acknowledgmentText;
 }
 
@@ -2246,8 +2250,8 @@ QString qSlicerCoreApplication::documentationBaseUrl() const
 // --------------------------------------------------------------------------
 QString qSlicerCoreApplication::documentationVersion() const
 {
-  QString version = "latest";
-  if (this->releaseType() == "Stable")
+  QString version = /*no tr*/"latest";
+  if (this->releaseType() == /*no tr*/"Stable")
     {
     version = QString("v%1.%2").arg(this->mainApplicationMajorVersion()).arg(this->mainApplicationMinorVersion());
     }

Base/QTCore/qSlicerCoreCommandOptions.cxx
@@ -114,7 +114,7 @@ bool qSlicerCoreCommandOptions::parse(const QStringList& arguments)
 bool qSlicerCoreCommandOptions::displayHelpAndExit()const
 {
   Q_D(const qSlicerCoreCommandOptions);
-  return d->ParsedArgs.value("help").toBool();
+  return d->ParsedArgs.value(/*no tr*/"help").toBool();
 }
 
 //-----------------------------------------------------------------------------
@@ -251,7 +251,7 @@ CTK_SET_CPP(qSlicerCoreCommandOptions, bool, setRunPythonAndExit, RunPythonAndEx
 bool qSlicerCoreCommandOptions::displayVersionAndExit() const
 {
   Q_D(const qSlicerCoreCommandOptions);
-  return d->ParsedArgs.value("version").toBool();
+  return d->ParsedArgs.value(/*no tr*/"version").toBool();
 }
 
 //-----------------------------------------------------------------------------
@@ -265,7 +265,7 @@ bool qSlicerCoreCommandOptions::displayProgramPathAndExit() const
 bool qSlicerCoreCommandOptions::displayHomePathAndExit() const
 {
   Q_D(const qSlicerCoreCommandOptions);
-  return d->ParsedArgs.value("home").toBool();
+  return d->ParsedArgs.value(/*no tr*/"home").toBool();
 }
 
 //-----------------------------------------------------------------------------
@@ -351,7 +351,7 @@ bool qSlicerCoreCommandOptions::keepTemporarySettings() const
 bool qSlicerCoreCommandOptions::isTestingEnabled() const
 {
   Q_D(const qSlicerCoreCommandOptions);
-  return d->ParsedArgs.value("testing").toBool();
+  return d->ParsedArgs.value(/*no tr*/"testing").toBool();
 }
 
 #ifdef Slicer_USE_PYTHONQT
@@ -367,109 +367,109 @@ bool qSlicerCoreCommandOptions::isPythonDisabled() const
 void qSlicerCoreCommandOptions::addArguments()
 {
   this->addArgument("ignore-rest", "-", QVariant::Bool,
-                    "Ignores the rest of the labeled arguments following this flag.",
+                    /*no tr*/"Ignores the rest of the labeled arguments following this flag.",
                     QVariant(false), true);
 
-  this->addArgument("help", "h", QVariant::Bool,
-                    "Display available command line arguments.");
+  this->addArgument(/*no tr*/"help", "h", QVariant::Bool,
+                    /*no tr*/"Display available command line arguments.");
 
 #ifdef Slicer_USE_PYTHONQT
-  QString testingDescription = "Activate testing mode. It implies --disable-settings and --ignore-slicerrc.";
+  QString testingDescription = /*no tr*/"Activate testing mode. It implies --disable-settings and --ignore-slicerrc.";
 #else
-  QString testingDescription = "Activate testing mode. It implies --disable-settings.";
+  QString testingDescription = /*no tr*/"Activate testing mode. It implies --disable-settings.";
 #endif
-  this->addArgument("testing", "", QVariant::Bool,
+  this->addArgument(/*no tr*/"testing", "", QVariant::Bool,
                     testingDescription,
                     QVariant(false));
 
 #ifdef Slicer_USE_PYTHONQT
   this->addArgument("disable-python", "", QVariant::Bool,
-                    "Disable python support. This is equivalent to build the application with Slicer_USE_PYTHONQT=OFF.");
+                    /*no tr*/"Disable python support. This is equivalent to build the application with Slicer_USE_PYTHONQT=OFF.");
 
   this->addArgument("python-script", "", QVariant::String,
-                    "Python script to execute after slicer loads.");
+                    /*no tr*/"Python script to execute after slicer loads.");
 
   this->addArgument("python-code", "", QVariant::String,
-                    "Python code to execute after slicer loads.");
+                    /*no tr*/"Python code to execute after slicer loads.");
 
   this->addArgument("", "c", QVariant::String,
-                    "Python code to execute after slicer loads. By default, no modules are loaded and Slicer exits afterward.");
+                    /*no tr*/"Python code to execute after slicer loads. By default, no modules are loaded and Slicer exits afterward.");
 
   this->addArgument("ignore-slicerrc", "", QVariant::Bool,
-                    "Do not load the Slicer resource file (~/.slicerrc.py).");
+                    /*no tr*/"Do not load the Slicer resource file (~/.slicerrc.py).");
 #endif
 
   this->addArgument("additional-module-path", "", QVariant::String,
-                    "Additional module path to consider when searching for modules to load.");
+                    /*no tr*/"Additional module path to consider when searching for modules to load.");
 
   this->addArgument("additional-module-paths", "", QVariant::StringList,
-                    "List of additional module path to consider when searching for modules to load.");
+                    /*no tr*/"List of additional module path to consider when searching for modules to load.");
 
   this->addArgument("modules-to-ignore", "", QVariant::String,
-                    "Comma separated list of modules that should *NOT* be loaded.");
+                    /*no tr*/"Comma separated list of modules that should *NOT* be loaded.");
 
   this->addArgument("disable-modules", "", QVariant::Bool,
-                    "Disable the loading of any Modules.");
+                    /*no tr*/"Disable the loading of any Modules.");
 
   this->addArgument("disable-builtin-modules", "", QVariant::Bool,
-                    "Disable the loading of builtin Modules.");
+                    /*no tr*/"Disable the loading of builtin Modules.");
 
 #ifdef Slicer_BUILD_CLI_SUPPORT
   this->addArgument("disable-cli-modules", "", QVariant::Bool,
-                    "Disable the loading of any Command Line Modules.");
+                    /*no tr*/"Disable the loading of any Command Line Modules.");
 
   this->addArgument("disable-builtin-cli-modules", "", QVariant::Bool,
-                    "Disable the loading of builtin Command Line Modules.");
+                    /*no tr*/"Disable the loading of builtin Command Line Modules.");
 #endif
 
   this->addArgument("disable-loadable-modules", "", QVariant::Bool,
-                    "Disable the loading of any Loadable Modules.");
+                    /*no tr*/"Disable the loading of any Loadable Modules.");
 
   this->addArgument("disable-builtin-loadable-modules", "", QVariant::Bool,
-                    "Disable the loading of builtin Loadable Modules.");
+                    /*no tr*/"Disable the loading of builtin Loadable Modules.");
 
 #ifdef Slicer_USE_PYTHONQT
   this->addArgument("disable-scripted-loadable-modules", "", QVariant::Bool,
-                    "Disable the loading of any Scripted Loadable Modules.");
+                    /*no tr*/"Disable the loading of any Scripted Loadable Modules.");
 
   this->addArgument("disable-builtin-scripted-loadable-modules", "", QVariant::Bool,
-                    "Disable the loading of builtinScripted Loadable Modules.");
+                    /*no tr*/"Disable the loading of builtinScripted Loadable Modules.");
 #endif
 
-  this->addArgument("version", "", QVariant::Bool,
-                    "Display version information and exits.");
+  this->addArgument(/*no tr*/"version", "", QVariant::Bool,
+                    /*no tr*/"Display version information and exits.");
 
   this->addArgument("program-path", "", QVariant::Bool,
-                    "Display application program path and exits.");
+                    /*no tr*/"Display application program path and exits.");
 
-  this->addArgument("home", "", QVariant::Bool,
-                    "Display home path and exits.");
+  this->addArgument(/*no tr*/"home", "", QVariant::Bool,
+                    /*no tr*/"Display home path and exits.");
 
   this->addArgument("settings-path", "", QVariant::Bool,
-                    "Display settings path and exits.");
+                    /*no tr*/"Display settings path and exits.");
 
   this->addArgument("temporary-path", "", QVariant::Bool,
-                    "Display temporary path and exits.");
+                    /*no tr*/"Display temporary path and exits.");
 
   this->addArgument("application-information", "", QVariant::Bool,
-                    "Display application information in the terminal.");
+                    /*no tr*/"Display application information in the terminal.");
 
   this->addArgument("verbose-module-discovery", "", QVariant::Bool,
-                    "Enable verbose output during module discovery process.");
+                    /*no tr*/"Enable verbose output during module discovery process.");
 
   this->addArgument("disable-settings", "", QVariant::Bool,
-                    "Start application ignoring user settings and using new temporary settings.");
+                    /*no tr*/"Start application ignoring user settings and using new temporary settings.");
 
   this->addArgument("keep-temporary-settings", "", QVariant::Bool,
-                    "Indicate whether temporary settings should be maintained.");
+                    /*no tr*/"Indicate whether temporary settings should be maintained.");
 
   this->addArgument("disable-message-handlers", "", QVariant::Bool,
-                    "Start application disabling the 'terminal' message handlers.");
+                    /*no tr*/"Start application disabling the 'terminal' message handlers.");
 
 #if defined (Q_OS_WIN32) && !defined (Slicer_BUILD_WIN32_CONSOLE)
 #else
   this->addArgument("disable-terminal-outputs", "", QVariant::Bool,
-                    "Start application disabling stdout/stderr outputs and capturing outputs only using the error log.");
+                    /*no tr*/"Start application disabling stdout/stderr outputs and capturing outputs only using the error log.");
 #endif
 }
 

Base/QTCore/qSlicerCoreIOManager.cxx
@@ -577,7 +577,8 @@ bool qSlicerCoreIOManager::loadNodes(const qSlicerIO::IOFileType& fileType,
   // If no readers were able to read and load the file(s), success will remain false
   bool success = false;
   int numberOfUserMessagesBefore = userMessages ? userMessages->GetNumberOfMessages() : 0;
-  QString userMessagePrefix = QString("Loading %1 - ").arg(parameters["fileName"].toString());
+  //: %1 is the filename
+  QString userMessagePrefix = tr("Loading %1").arg(parameters["fileName"].toString()) + " - ";
 
   QStringList nodes;
   foreach (qSlicerFileReader* reader, readers)
@@ -612,7 +613,7 @@ bool qSlicerCoreIOManager::loadNodes(const qSlicerIO::IOFileType& fileType,
   if (!success && userMessages != nullptr && userMessages->GetNumberOfMessages() == numberOfUserMessagesBefore)
     {
     // Make sure that at least one message is logged if reading failed.
-    userMessages->AddMessage(vtkCommand::ErrorEvent, (QString(tr("%1 load failed.")).arg(userMessagePrefix)).toStdString());
+    userMessages->AddMessage(vtkCommand::ErrorEvent, (tr("%1 load failed.").arg(userMessagePrefix)).toStdString());
     }
 
   loadedFileParameters.insert("nodeIDs", nodes);

Base/QTCore/qSlicerExtensionsManagerModel.cxx
@@ -1026,21 +1026,22 @@ QStringList qSlicerExtensionsManagerModelPrivate::isExtensionCompatible(
 {
   if (slicerRevision.isEmpty())
     {
-    return QStringList() << qSlicerExtensionsManagerModel::tr("slicerRevision is not specified");
+    return QStringList() << qSlicerExtensionsManagerModel::tr("'slicerRevision' parameter is not specified");
     }
   if (slicerOs.isEmpty())
     {
-    return QStringList() << qSlicerExtensionsManagerModel::tr("slicerOs is not specified");
+    return QStringList() << qSlicerExtensionsManagerModel::tr("'slicerOs' parameter is not specified");
     }
   if (slicerArch.isEmpty())
     {
-    return QStringList() << qSlicerExtensionsManagerModel::tr("slicerArch is not specified");
+    return QStringList() << qSlicerExtensionsManagerModel::tr("'slicerArch' parameter is not specified");
     }
   QStringList reasons;
   QString extensionSlicerRevision = metadata.value("slicer_revision").toString();
   if (!extensionSlicerRevision.isEmpty() && slicerRevision != extensionSlicerRevision)
     {
-    reasons << qSlicerExtensionsManagerModel::tr("extensionSlicerRevision [%1] is different from slicerRevision [%2]").arg(extensionSlicerRevision).arg(slicerRevision);
+    reasons << qSlicerExtensionsManagerModel::tr("extensionSlicerRevision [%1] is different from slicerRevision [%2]")
+      .arg(extensionSlicerRevision).arg(slicerRevision);
     }
   QString extensionArch = metadata.value("arch").toString();
   if (!extensionArch.isEmpty() && slicerArch != extensionArch)
@@ -1851,8 +1852,8 @@ bool qSlicerExtensionsManagerModel::installExtension(
 
     // Copy expected keys from archive description
     QStringList expectedKeys;
-    expectedKeys << "category" << "contributors" << "description" << "homepage"
-      << "iconurl" << "screenshots" << "status" << "updated";
+    expectedKeys << /*no tr*/"category" << /*no tr*/"contributors" << /*no tr*/"description" << /*no tr*/"homepage"
+      << /*no tr*/"iconurl" << /*no tr*/"screenshots" << /*no tr*/"status" << /*no tr*/"updated";
 
     const ExtensionMetadataType::const_iterator notFound =
       extensionIndexMetadata.constEnd();
@@ -1943,18 +1944,19 @@ bool qSlicerExtensionsManagerModel::installExtension(
     if (!unresolvedDependencies.isEmpty())
       {
       success = false;
-      qWarning() << tr("%1 extension depends on the following extensions, which could not be found: %2")
+      qWarning() << QString(/*no tr*/"%1 extension depends on the following extensions, which could not be found: %2")
         .arg(extensionName)
         .arg(unresolvedDependencies.join(", "));
       if (d->Interactive)
         {
-        QString msg = QString("<p>%1 depends on the following extensions, which could not be found:</p><ul>").arg(extensionName);
+        //: %1 is the extension name
+        QString msg = QString("<p>%1</p><ul>").arg(tr("%1 depends on the following extensions, which could not be found:")).arg(extensionName);
         foreach(const QString & dependencyName, unresolvedDependencies)
           {
           msg += QString("<li>%1</li>").arg(dependencyName);
           }
-        msg += "</ul><p>The extension may not function properly.</p>";
-        QMessageBox::warning(nullptr, "Unresolved dependencies", msg);
+        msg += QString("</ul><p>%1</p>").arg(tr("The extension may not function properly."));
+        QMessageBox::warning(nullptr, tr("Unresolved dependencies"), msg);
         }
       }
     }
@@ -2287,6 +2289,7 @@ void qSlicerExtensionsManagerModel::onUpdateDownloadFinished(
     QFile file(archivePath);
     if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate))
       {
+      //: %1 is the path name, %2 is the error message
       d->critical(tr("Could not write file: '%1' (%2)").arg(archivePath).arg(file.errorString()));
       d->ActiveTasks.remove(task);
       emit activeTasksChanged();
@@ -2350,6 +2353,7 @@ bool qSlicerExtensionsManagerModel::scheduleExtensionForUpdate(
     {
     if (updateInfo.ExtensionId.isEmpty())
       {
+      //: %1 is extension name
       d->critical(tr("Missing download information for %1 extension").arg(extensionName));
       return false;
       }
@@ -2363,6 +2367,7 @@ bool qSlicerExtensionsManagerModel::scheduleExtensionForUpdate(
     qSlicerExtensionDownloadTask* const task = d->downloadExtensionByName(extensionName);
     if (!task)
       {
+      //: %1 is extension name
       d->critical(tr("Failed to retrieve metadata for %1 extension").arg(extensionName));
       return false;
       }
@@ -2380,6 +2385,7 @@ bool qSlicerExtensionsManagerModel::scheduleExtensionForUpdate(
   scheduled[extensionName] = updateInfo.ArchiveName;
   settings.setValue("Extensions/ScheduledForUpdate", scheduled);
 
+  //: %1 is extension name
   d->info(tr("%1 extension scheduled for update").arg(extensionName));
   emit this->extensionScheduledForUpdate(extensionName);
 
@@ -2425,12 +2431,14 @@ bool qSlicerExtensionsManagerModelPrivate::updateExtension(
   QStandardItem * item = this->extensionItem(extensionName);
   if (!item)
     {
+    //: %1 is extension name
     critical(qSlicerExtensionsManagerModel::tr("Failed to update %1 extension").arg(extensionName));
     return false;
     }
 
   if (!q->isExtensionScheduledForUpdate(extensionName))
     {
+    //: %1 is extension name
     critical(qSlicerExtensionsManagerModel::tr("Failed to update %1 extension: it is not scheduled for update").arg(extensionName));
     return false;
     }
@@ -2554,12 +2562,14 @@ bool qSlicerExtensionsManagerModel::uninstallExtension(const QString& extensionN
   QStandardItem* item = d->extensionItem(extensionName);
   if (!item)
     {
+    //: %1 is extension name
     d->critical(tr("Failed to uninstall %1 extension").arg(extensionName));
     return false;
     }
 
   if (this->isExtensionLoaded(extensionName))
     {
+    //: %1 is extension name
     d->critical(tr("Failed to uninstall %1 extension: extension is already loaded").arg(extensionName));
     return false;
     }
@@ -2834,6 +2844,7 @@ void qSlicerExtensionsManagerModel::identifyIncompatibleExtensions()
     QStringList reasons = this->isExtensionCompatible(extensionName, d->SlicerRevision, d->SlicerOs, d->SlicerArch);
     if (!reasons.isEmpty())
       {
+      //: %1 is extension name
       reasons.prepend(tr("Extension %1 is incompatible").arg(extensionName));
       qCritical() << reasons.join("\n  ");
       this->setExtensionEnabled(extensionName, false);
@@ -2864,7 +2875,7 @@ QStringList qSlicerExtensionsManagerModel::isExtensionCompatible(
 {
   if (extensionName.isEmpty())
     {
-    return QStringList() << tr("extensionName is not specified");
+    return QStringList() << tr("'extensionName' parameter is not specified");
     }
   ExtensionMetadataType metadata = this->extensionMetadata(extensionName);
   return qSlicerExtensionsManagerModelPrivate::isExtensionCompatible(
@@ -3057,6 +3068,7 @@ bool qSlicerExtensionsManagerModel::extractExtensionArchive(
 
   if (extensionName.isEmpty())
     {
+    //: %1 is extension name
     d->critical(tr("Corrupted %1 extension package").arg(extensionName));
     return false;
     }
@@ -3064,6 +3076,7 @@ bool qSlicerExtensionsManagerModel::extractExtensionArchive(
   QString error;
   if (!d->checkExtensionsInstallDestinationPath(destinationPath, error))
     {
+    //: %1 is extension name, %2 is extension package file path, %3 is destination path
     d->critical(tr("Failed to extract %1 extension archive %2 into directory %3")
       .arg(extensionName).arg(archiveFile).arg(destinationPath));
     d->critical(error);

Base/QTCore/qSlicerSceneBundleReader.cxx
@@ -56,7 +56,7 @@ qSlicerSceneBundleReader::qSlicerSceneBundleReader(QObject* _parent)
 //-----------------------------------------------------------------------------
 QString qSlicerSceneBundleReader::description()const
 {
-  return "MRB Slicer Data Bundle";
+  return tr("MRB Slicer Data Bundle");
 }
 
 //-----------------------------------------------------------------------------
@@ -95,7 +95,7 @@ bool qSlicerSceneBundleReader::load(const qSlicerIO::IOProperties& properties)
     {
     // Set default scene file format to mrb
     qSlicerCoreIOManager* coreIOManager = qSlicerCoreApplication::application()->coreIOManager();
-    coreIOManager->setDefaultSceneFileType("Medical Reality Bundle (.mrb)");
+    coreIOManager->setDefaultSceneFileType(tr("Medical Reality Bundle") + " (.mrb)");
     }
 
   return success;

Base/QTGUI/qSlicerExtensionsManagerWidget.cxx
@@ -208,7 +208,7 @@ void qSlicerExtensionsManagerWidgetPrivate::init()
   this->ExtensionsManageBrowser->setObjectName("ExtensionsManageBrowser");
   this->ManageExtensionsPager->addWidget(this->ExtensionsManageBrowser);
   this->ExtensionsManageBrowser->setBrowsingEnabled(false);
-  this->ExtensionsManageBrowser->webView()->load(QUrl("about:"));
+  this->ExtensionsManageBrowser->webView()->load(QUrl(/*no tr*/"about:"));
 #endif
 
   qSlicerExtensionsActionsWidget * actionsWidget = new qSlicerExtensionsActionsWidget;
@@ -502,7 +502,7 @@ void qSlicerExtensionsManagerWidget::onEditBookmarksTriggered()
 
   // Update bookmarks
   QSettings settings;
-  settings.setValue("Extensions/Bookmarked", newList);
+  settings.setValue(/*no tr*/"Extensions/Bookmarked", newList);
   this->extensionsManagerModel()->updateModel();
 }
 
@@ -574,7 +574,7 @@ void qSlicerExtensionsManagerWidget::onManageLinkActivated(const QUrl& link)
 void qSlicerExtensionsManagerWidget::onManageUrlChanged(const QUrl& newUrl)
 {
   Q_D(qSlicerExtensionsManagerWidget);
-  d->ManageExtensionsPager->setCurrentIndex(newUrl.scheme() == "about" ? 0 : 1);
+  d->ManageExtensionsPager->setCurrentIndex(newUrl.scheme() == /*no tr*/"about" ? 0 : 1);
 }
 
 // --------------------------------------------------------------------------

commit 210b88bc44650fad13921a1fcf666d1bed98c4c3

BUG: Fix crash ensuring defaultDocumentationLink() returns a value
This commit fixes a regression introduced in 1031050a0a (BUG: Mark
translatable strings in Base/QTCore).

It ensures the application starts successfully and addresses the
following warning:

  /path/to/Slicer/Base/QTCore/qSlicerAbstractCoreModule.cxx: In member function ‘QString qSlicerAbstractCoreModule::defaultDocumentationLink() const’:
  /path/to/Slicer/Base/QTCore/qSlicerAbstractCoreModule.cxx:344:57: warning: control reaches end of non-void function [-Wreturn-type]
    344 |   QString url = app->moduleDocumentationUrl(this->name());
        |                                                         ^

Co-authored-by: Connor Bowley <connor.bowley@kitware.com>
Co-authored-by: Sam Horvath <sam.horvath@kitware.com>

Base/QTCore/qSlicerAbstractCoreModule.cxx
@@ -346,4 +346,5 @@ QString qSlicerAbstractCoreModule::defaultDocumentationLink()const
     + tr("For more information see the %1.").arg(
         QString("<a href=\"%1\">%2</a>").arg(url).arg(tr("online documentation")))
     + QString("</p>");
+  return linkText;
 }