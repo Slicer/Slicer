Slicer/Slicer

commit 0ad10f0cb406d25c8d2ea62058b31dcc33173bdf

ENH: Support disconnecting markups toolbar from mouse mode toolbar
This change allows for calling to removeAction(ToolBarAction) to remove the markups toolbar connection and have it remain disconnected. Previously it would always add the action back, overwriting the customization. Now the action is always in the set of mouse mode actions, but selectively shown/hidden

Base/QTGUI/qSlicerMouseModeToolBar.cxx
@@ -169,6 +169,8 @@ void qSlicerMouseModeToolBarPrivate::init()
 
   connect(this->PlaceWidgetAction, SIGNAL(triggered()), q, SLOT(switchPlaceMode()));
   this->InteractionModesActionGroup->addAction(this->PlaceWidgetAction);
+  this->PlaceWidgetAction->setVisible(false);
+  q->addAction(this->PlaceWidgetAction);
 
 }
 
@@ -327,16 +329,16 @@ void qSlicerMouseModeToolBarPrivate::updatePlaceWidget()
   bool validNodeForPlacement = selectionNode->GetActivePlaceNodePlacementValid();
   if (!validNodeForPlacement || activePlaceNodeClassName.isEmpty())
     {
-    q->removeAction(this->PlaceWidgetAction);
-    q->addAction(this->ToolBarAction);
+    this->PlaceWidgetAction->setVisible(false);
+    this->ToolBarAction->setVisible(true);
     return;
     }
 
   QString activePlaceNodeID = selectionNode->GetActivePlaceNodeID();
   if (activePlaceNodeID.isEmpty())
     {
-    q->removeAction(this->PlaceWidgetAction);
-    q->addAction(this->ToolBarAction);
+    this->PlaceWidgetAction->setVisible(false);
+    this->ToolBarAction->setVisible(true);
     return;
     }
 
@@ -352,7 +354,7 @@ void qSlicerMouseModeToolBarPrivate::updatePlaceWidget()
       break;
       }
     }
-  q->removeAction(this->ToolBarAction);
+  this->ToolBarAction->setVisible(false);
 
   QIcon icon(placeNodeResource);
   if (icon.availableSizes().empty())
@@ -367,7 +369,7 @@ void qSlicerMouseModeToolBarPrivate::updatePlaceWidget()
   this->PlaceWidgetAction->setCheckable(true);
 
   connect(this->PlaceWidgetAction, SIGNAL(triggered()), q, SLOT(switchPlaceMode()));
-  q->addAction(this->PlaceWidgetAction);
+  this->PlaceWidgetAction->setVisible(true);
 }
 
 //---------------------------------------------------------------------------

commit a3dd32fc1160ffc0ff15fa7e62718b08a6b820bb

BUG: Fix failing qSlicerMouseModeToolBarTest1
The change in order of the actions made in https://github.com/Slicer/Slicer/commit/0ad10f0cb406d25c8d2ea62058b31dcc33173bdf caused the hardcoded index used to find the place widget action in the test to fail.

Base/QTGUI/qSlicerMouseModeToolBar.cxx
@@ -152,7 +152,6 @@ void qSlicerMouseModeToolBarPrivate::init()
 
   QObject::connect(this->ToolBarAction, SIGNAL(triggered()),
     q, SLOT(toggleMarkupsToolBar()));
-  q->addAction(this->ToolBarAction);
 
   this->PlaceWidgetMenu = new QMenu(qSlicerMouseModeToolBar::tr("Place Menu"), q);
   this->PlaceWidgetMenu->setObjectName("PlaceWidgetMenu");
@@ -172,6 +171,7 @@ void qSlicerMouseModeToolBarPrivate::init()
   this->PlaceWidgetAction->setVisible(false);
   q->addAction(this->PlaceWidgetAction);
 
+  q->addAction(this->ToolBarAction);  // add Toggle Markups ToolBar action last
 }
 
 //---------------------------------------------------------------------------

Base/QTGUI/Testing/Cxx/qSlicerMouseModeToolBarTest1.cxx
@@ -36,24 +36,15 @@
 
 QString activePlaceActionText(qSlicerMouseModeToolBar& mouseModeToolBar)
 {
-  QAction* placeAction = mouseModeToolBar.actions()[2];
-  if (!placeAction->isEnabled())
-    {
-    return QString();
-    }
-  return placeAction->text();
-  /*
-  return mouseModeToolBar.actions()[2]->text();
   foreach(QAction* action, mouseModeToolBar.actions())
     {
-    if (action->objectName() == QString("ToolBarAction"))
+    if (action->objectName() == QString("PlaceWidgetAction"))
       {
       return action->text();
       break;
       }
     }
   return QString();
-  */
 }
 
 QString getActiveActionText(qSlicerMouseModeToolBar& mouseModeToolBar)
@@ -100,7 +91,7 @@ int qSlicerMouseModeToolBarTest1(int argc, char * argv[] )
   // without a qSlicerApplication, setting the cursor is a noop
   mouseToolBar.changeCursorTo(QCursor(Qt::BusyCursor));
 
-  CHECK_PLACE_ACTION_TEXT("Toggle Markups Toolbar", mouseToolBar);
+  CHECK_PLACE_ACTION_TEXT("Place", mouseToolBar);
 
   // get the selection and interaction nodes that the mouse mode tool bar
   // listens to