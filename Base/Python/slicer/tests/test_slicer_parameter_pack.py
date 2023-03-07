fa3286508223bff6614e519114660350bce07b7c
Connor Bowley <connor.bowley@kitware.com>
2023-03-07 17:39:16 -0500

    ENH: Parameter pack invariants
    Allow invariants on parameterPacks.

--- Base/Python/slicer/tests/test_slicer_parameter_pack.py
+++ Base/Python/slicer/tests/test_slicer_parameter_pack.py
@@ -24,6 +24,70 @@ class BoundingBox:
     bottomRight: Point
 
 
+class BadDateException(ValueError):
+    pass
+
+
+@parameterPack
+class Date:
+    _month: Annotated[int, Default(1)]
+    _day: Annotated[int, Default(1)]
+    _year: Annotated[int, Default(1970)]
+
+    @staticmethod
+    def checkDate(month, day, year):
+        if month < 1 or month > 12 or day < 1 or day > 31:
+            raise BadDateException(f"Bad date: {month}/{day}/{year}")
+        if month == 2 and day > 28:
+            raise BadDateException(f"Bad date: {month}/{day}/{year}")
+        if month in (4, 6, 9, 11) and day > 30:
+            raise BadDateException(f"Bad date: {month}/{day}/{year}")
+
+    def __init__(self, month=None, day=None, year=None) -> None:
+        if month is not None:
+            self._month = month
+        if day is not None:
+            self._day = day
+        if year is not None:
+            self._year = year
+        self.checkDate(self._month, self._day, self._year)
+
+    def __str__(self) -> str:
+        return f"Date(month={self.month}, day={self.day}, year={self.year})"
+
+    @property
+    def month(self):
+        return self._month
+
+    @month.setter
+    def month(self, value):
+        self.checkDate(value, self.day, self.year)
+        self._month = value
+
+    @property
+    def day(self):
+        return self._day
+
+    @day.setter
+    def day(self, value):
+        self.checkDate(self.month, value, self.year)
+        self._day = value
+
+    @property
+    def year(self):
+        return self._year
+
+    @year.setter
+    def year(self, value):
+        self._year = value
+
+    def setDate(self, month: int, day: int, year: int) -> None:
+        self.checkDate(month, day, year)
+        self._month = month
+        self._day = day
+        self._year = year
+
+
 class TypedParameterNodeTest(unittest.TestCase):
     def setUp(self):
         slicer.mrmlScene.Clear(0)
@@ -419,3 +483,71 @@ class TypedParameterNodeTest(unittest.TestCase):
         # add weird recursive use
         param.any = AnyPack(Point(3, 4))
         self.assertEqual(param.any.any, Point(3, 4))
+
+    def test_parameter_pack_invariants(self):
+        self.assertEqual(Date(), Date(1, 1, 1970))
+        with self.assertRaises(BadDateException):
+            Date(0, 1, 1970)
+        with self.assertRaises(BadDateException):
+            Date(13, 1, 1970)
+        with self.assertRaises(BadDateException):
+            Date(1, 0, 1970)
+        with self.assertRaises(BadDateException):
+            Date(1, 32, 1970)
+        with self.assertRaises(BadDateException):
+            Date(2, 29, 1970)
+
+        date = Date()
+        with self.assertRaises(BadDateException):
+            date.month = 0
+        self.assertEqual(date.month, 1)
+        date = Date()
+        with self.assertRaises(BadDateException):
+            date.month = 55
+        self.assertEqual(date.month, 1)
+        date.month = 2
+        with self.assertRaises(BadDateException):
+            date.day = 30
+
+        date.setValue("month", 4)
+        with self.assertRaises(BadDateException):
+            date.setValue("day", 31)
+
+        date.setDate(1, 31, 1980)
+        self.assertEqual(date, Date(1, 31, 1980))
+
+        with self.assertRaises(BadDateException):
+            date.month = 44
+
+    def test_parameter_pack_invariants_in_wrapper(self):
+        @parameterNodeWrapper
+        class ParameterNodeType:
+            date: Date
+            date2: Annotated[Date, Default(Date(2, 28, 2028))]
+
+        param = ParameterNodeType(newParameterNode())
+        self.assertEqual(param.date, Date())
+        self.assertEqual(param.date2, Date(2, 28, 2028))
+
+        with self.assertRaises(BadDateException):
+            param.date.month = 0
+        self.assertEqual(param.date.month, 1)
+        with self.assertRaises(BadDateException):
+            param.date.month = 55
+        self.assertEqual(param.date.month, 1)
+        param.date.month = 2
+        with self.assertRaises(BadDateException):
+            param.date.day = 30
+
+        param.date.setDate(1, 31, 1980)
+        self.assertEqual(param.date, Date(1, 31, 1980))
+
+        with self.assertRaises(BadDateException):
+            param.date.month = 44
+
+        self.assertEqual(param.date, Date(1, 31, 1980))
+        self.assertEqual(param.date2, Date(2, 28, 2028))
+
+        param2 = ParameterNodeType(param.parameterNode)
+        self.assertEqual(param2.date, Date(1, 31, 1980))
+        self.assertEqual(param2.date2, Date(2, 28, 2028))
