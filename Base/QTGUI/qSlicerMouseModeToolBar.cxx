Slicer/Slicer

commit 32675b18cb60123a7a873889f8c7383549e3ad27

BUG: Fix failing qSlicerMouseModeToolBarTest1
The change in order of the actions made in https://github.com/Slicer/Slicer/commit/0ad10f0cb406d25c8d2ea62058b31dcc33173bdf caused the hardcoded index used to find the place widget action in the test to fail.

Base/QTGUI/qSlicerMouseModeToolBar.cxx
@@ -152,7 +152,6 @@ void qSlicerMouseModeToolBarPrivate::init()
 
   QObject::connect(this->ToolBarAction, SIGNAL(triggered()),
     q, SLOT(toggleMarkupsToolBar()));
-  q->addAction(this->ToolBarAction);
 
   this->PlaceWidgetMenu = new QMenu(qSlicerMouseModeToolBar::tr("Place Menu"), q);
   this->PlaceWidgetMenu->setObjectName("PlaceWidgetMenu");
@@ -172,6 +171,7 @@ void qSlicerMouseModeToolBarPrivate::init()
   this->PlaceWidgetAction->setVisible(false);
   q->addAction(this->PlaceWidgetAction);
 
+  q->addAction(this->ToolBarAction);  // add Toggle Markups ToolBar action last
 }
 
 //---------------------------------------------------------------------------

Base/QTGUI/Testing/Cxx/qSlicerMouseModeToolBarTest1.cxx
@@ -36,24 +36,15 @@
 
 QString activePlaceActionText(qSlicerMouseModeToolBar& mouseModeToolBar)
 {
-  QAction* placeAction = mouseModeToolBar.actions()[2];
-  if (!placeAction->isEnabled())
-    {
-    return QString();
-    }
-  return placeAction->text();
-  /*
-  return mouseModeToolBar.actions()[2]->text();
   foreach(QAction* action, mouseModeToolBar.actions())
     {
-    if (action->objectName() == QString("ToolBarAction"))
+    if (action->objectName() == QString("PlaceWidgetAction"))
       {
       return action->text();
       break;
       }
     }
   return QString();
-  */
 }
 
 QString getActiveActionText(qSlicerMouseModeToolBar& mouseModeToolBar)
@@ -100,7 +91,7 @@ int qSlicerMouseModeToolBarTest1(int argc, char * argv[] )
   // without a qSlicerApplication, setting the cursor is a noop
   mouseToolBar.changeCursorTo(QCursor(Qt::BusyCursor));
 
-  CHECK_PLACE_ACTION_TEXT("Toggle Markups Toolbar", mouseToolBar);
+  CHECK_PLACE_ACTION_TEXT("Place", mouseToolBar);
 
   // get the selection and interaction nodes that the mouse mode tool bar
   // listens to

commit cc9781e5f269233e7ef8646a04c6c7e171c5263b

BUG: Fix missing toggle markups action in Markups Place button menu
This fixes the following error which was introduced by https://github.com/Slicer/Slicer/commit/32675b18cb60123a7a873889f8c7383549e3ad27:
[Qt] QWindowsWindow::setMouseGrabEnabled: Not setting mouse grab for invisible window QWidgetWindow/'PlaceWidgetMenuWindow'

Base/QTGUI/qSlicerMouseModeToolBar.cxx
@@ -172,6 +172,17 @@ void qSlicerMouseModeToolBarPrivate::init()
   q->addAction(this->PlaceWidgetAction);
 
   q->addAction(this->ToolBarAction);  // add Toggle Markups ToolBar action last
+
+  this->PlaceWidgetToolBarAction = new QAction(this);
+  this->PlaceWidgetToolBarAction->setObjectName("PlaceWidgetToolBarAction");
+  this->PlaceWidgetToolBarAction->setToolTip(qSlicerMouseModeToolBar::tr("Toggle Markups Toolbar"));
+  this->PlaceWidgetToolBarAction->setText(qSlicerMouseModeToolBar::tr("Toggle Markups Toolbar"));
+  this->PlaceWidgetToolBarAction->setEnabled(true);
+  this->PlaceWidgetToolBarAction->setIcon(QIcon(":/Icons/MarkupsDisplayToolBar.png"));
+
+  QObject::connect(this->PlaceWidgetToolBarAction, SIGNAL(triggered()),
+    q, SLOT(toggleMarkupsToolBar()));
+  this->PlaceWidgetMenu->addAction(this->PlaceWidgetToolBarAction);
 }
 
 //---------------------------------------------------------------------------

Base/QTGUI/qSlicerMouseModeToolBar_p.h
@@ -100,7 +100,12 @@ public:
   QAction* AdjustViewAction;
   QAction* AdjustWindowLevelAction;
   QAction* PlaceWidgetAction;
+
+  /// Action represented by a QToolButton in the Markups Toolbar for toggling visibility of the advanced Markups toolbar.
   QAction* ToolBarAction;
+
+  /// Action added to the place widget menu for toggling visibility of the advanced Markups toolbar.
+  QAction* PlaceWidgetToolBarAction;
 
   QMenu* PlaceWidgetMenu;
 