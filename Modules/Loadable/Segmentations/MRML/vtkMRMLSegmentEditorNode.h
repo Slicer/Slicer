Slicer/Slicer

commit 5dfb6fb6945d3fb56320c2920571f3ffcc5b48fb

BUG: Use "source" instead of "reference" volume node in Segmentations
"Reference" word is used for many different concepts and specifically in segmentation is used for reference image geometry.
Therefore, "master volume" is better renamed to "source volume" instead of "reference volume".
This commit also restores compatibility with previous public API (segmentation method names that contain "master" remain available as deprecated methods for at least a year).

MRML node attribute and reference names will still use master for a while for ensuring backward compatibility, but new names are also recognized for compatibility with
scenes created by future Slicer versions.

Modules/Loadable/Segmentations/MRML/vtkMRMLSegmentEditorNode.h
@@ -38,7 +38,7 @@ class vtkMRMLScalarVolumeNode;
 /// \ingroup Segmentations
 /// \brief Parameter set node for the segment editor widget
 ///
-/// Stores parameters for a segment editor widget (selected segmentation, segment, reference volume),
+/// Stores parameters for a segment editor widget (selected segmentation, segment, source volume),
 /// and all the editor effects. The effect parameters are stored as attributes with names
 /// EffectName.ParameterName. If a parameter is changed, the node Modified event is not emitted,
 /// but the custom EffectParameterModified event that triggers update of the effect options widget only.
@@ -95,10 +95,16 @@ public:
 public:
 
   //@{
-  /// Get/set reference volume node.
-  /// Reference volume node is used when editing requires an underlying image.
-  vtkMRMLScalarVolumeNode* GetReferenceVolumeNode();
-  void SetAndObserveReferenceVolumeNode(vtkMRMLScalarVolumeNode* node);
+  /// Get/set source volume node.
+  /// Source volume node is used when editing requires an underlying image.
+  vtkMRMLScalarVolumeNode* GetSourceVolumeNode();
+  void SetAndObserveSourceVolumeNode(vtkMRMLScalarVolumeNode* node);
+  //@}
+
+  //@{
+  /// Deprecated. Use GetSourceVolumeNode/SetAndObserveSourceVolumeNode methods instead.
+  vtkMRMLScalarVolumeNode* GetMasterVolumeNode();
+  void SetAndObserveMasterVolumeNode(vtkMRMLScalarVolumeNode* node);
   //@}
 
   //@{
@@ -139,19 +145,60 @@ public:
   //@}
 
   //@{
-  /// Restrict editable area to regions where reference volume intensity is in the specified range.
-  vtkBooleanMacro(ReferenceVolumeIntensityMask, bool);
-  vtkGetMacro(ReferenceVolumeIntensityMask, bool);
-  vtkSetMacro(ReferenceVolumeIntensityMask, bool);
+  /// Restrict editable area to regions where source volume intensity is in the specified range.
+  vtkBooleanMacro(SourceVolumeIntensityMask, bool);
+  vtkGetMacro(SourceVolumeIntensityMask, bool);
+  vtkSetMacro(SourceVolumeIntensityMask, bool);
   //@}
 
   //@{
-  /// Get/set reference volume intensity range for masking.
-  /// If ReferenceVolumeIntensityMask is enabled then only those areas are editable where
-  /// reference volume voxels are in this intensity range.
-  /// \sa SetReferenceVolumeIntensityMask()
-  vtkSetVector2Macro(ReferenceVolumeIntensityMaskRange, double);
-  vtkGetVector2Macro(ReferenceVolumeIntensityMaskRange, double);
+  /// Deprecated. Use Get/SetSourceVolumeIntensityMask method instead.
+  virtual void MasterVolumeIntensityMaskOn() { this->SourceVolumeIntensityMaskOn(); }
+  virtual void MasterVolumeIntensityMaskOff() { this->SourceVolumeIntensityMaskOff(); }
+  virtual bool GetMasterVolumeIntensityMask() { return this->GetSourceVolumeIntensityMask(); }
+  //@}
+
+  //@{
+  /// Get/set source volume intensity range for masking.
+  /// If SourceVolumeIntensityMask is enabled then only those areas are editable where
+  /// source volume voxels are in this intensity range.
+  /// \sa SetSourceVolumeIntensityMask()
+  vtkSetVector2Macro(SourceVolumeIntensityMaskRange, double);
+  vtkGetVector2Macro(SourceVolumeIntensityMaskRange, double);
+  //@}
+
+  //@{
+  /// Deprecated. Use Get/SetSourceVolumeIntensityMaskRange method instead.
+  virtual void SetMasterVolumeIntensityMaskRange(double _arg1, double _arg2)
+    {
+    vtkWarningMacro("vtkMRMLSegmentEditorNode::SetMasterVolumeIntensityMaskRange() method is deprecated, use SetSourceVolumeIntensityMaskRange method instead");
+    this->SetSourceVolumeIntensityMaskRange(_arg1, _arg2);
+    }
+  void SetMasterVolumeIntensityMaskRange(const double _arg[2])
+    {
+    vtkWarningMacro("vtkMRMLSegmentEditorNode::SetMasterVolumeIntensityMaskRange() method is deprecated, use SetSourceVolumeIntensityMaskRange method instead");
+    this->SetSourceVolumeIntensityMaskRange(_arg);
+    }
+
+  virtual double* GetMasterVolumeIntensityMaskRange() VTK_SIZEHINT(2)
+    {
+    vtkWarningMacro("vtkMRMLSegmentEditorNode::GetMasterVolumeIntensityMaskRange() method is deprecated, use GetSourceVolumeIntensityMaskRange method instead");
+    return this->GetSourceVolumeIntensityMaskRange();
+    }
+
+  VTK_WRAPEXCLUDE
+  virtual void GetMasterVolumeIntensityMaskRange(double& _arg1, double& _arg2)
+    {
+    vtkWarningMacro("vtkMRMLSegmentEditorNode::GetMasterVolumeIntensityMaskRange() method is deprecated, use GetSourceVolumeIntensityMaskRange method instead");
+    this->GetSourceVolumeIntensityMaskRange(_arg2, _arg2);
+    }
+
+  VTK_WRAPEXCLUDE
+  virtual void GetMasterVolumeIntensityMaskRange(double _arg[2])
+    {
+    vtkWarningMacro("vtkMRMLSegmentEditorNode::GetMasterVolumeIntensityMaskRange() method is deprecated, use GetSourceVolumeIntensityMaskRange method instead");
+    this->GetSourceVolumeIntensityMaskRange(_arg);
+    }
   //@}
 
   //@{
@@ -178,8 +225,8 @@ protected:
 
   int OverwriteMode{OverwriteAllSegments};
 
-  bool ReferenceVolumeIntensityMask{false};
-  double ReferenceVolumeIntensityMaskRange[2];
+  bool SourceVolumeIntensityMask{false};
+  double SourceVolumeIntensityMaskRange[2];
 };
 
 #endif // __vtkMRMLSegmentEditorNode_h

Extensions/Testing/ScriptedSegmentEditorEffectExtensionTemplate/ScriptedSegmentEditorEffectModuleTemplate/SegmentEditorScriptedSegmentEditorEffectModuleTemplate.py
@@ -64,16 +64,16 @@ class SegmentEditorScriptedSegmentEditorEffectModuleTemplateTest(ScriptedLoadabl
         from SegmentStatistics import SegmentStatisticsLogic
 
         ##################################
-        self.delayDisplay("Load reference volume")
+        self.delayDisplay("Load source volume")
 
-        referenceVolumeNode = SampleData.downloadSample('MRBrainTumor1')
+        sourceVolumeNode = SampleData.downloadSample('MRBrainTumor1')
 
         ##################################
         self.delayDisplay("Create segmentation containing a few spheres")
 
         segmentationNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLSegmentationNode')
         segmentationNode.CreateDefaultDisplayNodes()
-        segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(referenceVolumeNode)
+        segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(sourceVolumeNode)
 
         # Segments are defined by a list of: name and a list of sphere [radius, posX, posY, posZ]
         segmentGeometries = [
@@ -104,7 +104,7 @@ class SegmentEditorScriptedSegmentEditorEffectModuleTemplateTest(ScriptedLoadabl
         slicer.mrmlScene.AddNode(segmentEditorNode)
         segmentEditorWidget.setMRMLSegmentEditorNode(segmentEditorNode)
         segmentEditorWidget.setSegmentationNode(segmentationNode)
-        segmentEditorWidget.setMasterVolumeNode(referenceVolumeNode)
+        segmentEditorWidget.setSourceVolumeNode(sourceVolumeNode)
 
         ##################################
         self.delayDisplay("Run segmentation")
@@ -123,7 +123,7 @@ class SegmentEditorScriptedSegmentEditorEffectModuleTemplateTest(ScriptedLoadabl
         self.delayDisplay("Compute statistics")
 
         segStatLogic = SegmentStatisticsLogic()
-        segStatLogic.computeStatistics(segmentationNode, referenceVolumeNode)
+        segStatLogic.computeStatistics(segmentationNode, sourceVolumeNode)
 
         # Export results to table (just to see all results)
         resultsTableNode = slicer.vtkMRMLTableNode()

Extensions/Testing/ScriptedSegmentEditorEffectExtensionTemplate/ScriptedSegmentEditorEffectModuleTemplate/SegmentEditorScriptedSegmentEditorEffectModuleTemplateLib/SegmentEditorEffect.py
@@ -90,24 +90,24 @@ To segment a single object, create a segment and paint inside and create another
         # Allow users revert to this state by clicking Undo
         self.scriptedEffect.saveStateForUndo()
 
-        # Export reference image data to temporary new volume node.
-        # Note: Although the original reference volume node is already in the scene, we do not use it here,
-        # because the reference volume may have been resampled to match segmentation geometry.
-        referenceVolumeNode = slicer.vtkMRMLScalarVolumeNode()
-        slicer.mrmlScene.AddNode(referenceVolumeNode)
-        referenceVolumeNode.SetAndObserveTransformNodeID(segmentationNode.GetTransformNodeID())
-        slicer.vtkSlicerSegmentationsModuleLogic.CopyOrientedImageDataToVolumeNode(self.scriptedEffect.referenceVolumeImageData(), referenceVolumeNode)
+        # Export source image data to temporary new volume node.
+        # Note: Although the original source volume node is already in the scene, we do not use it here,
+        # because the source volume may have been resampled to match segmentation geometry.
+        sourceVolumeNode = slicer.vtkMRMLScalarVolumeNode()
+        slicer.mrmlScene.AddNode(sourceVolumeNode)
+        sourceVolumeNode.SetAndObserveTransformNodeID(segmentationNode.GetTransformNodeID())
+        slicer.vtkSlicerSegmentationsModuleLogic.CopyOrientedImageDataToVolumeNode(self.scriptedEffect.sourceVolumeImageData(), sourceVolumeNode)
         # Generate merged labelmap of all visible segments, as the filter expects a single labelmap with all the labels.
         mergedLabelmapNode = slicer.vtkMRMLLabelMapVolumeNode()
         slicer.mrmlScene.AddNode(mergedLabelmapNode)
-        slicer.vtkSlicerSegmentationsModuleLogic.ExportSegmentsToLabelmapNode(segmentationNode, visibleSegmentIds, mergedLabelmapNode, referenceVolumeNode)
+        slicer.vtkSlicerSegmentationsModuleLogic.ExportSegmentsToLabelmapNode(segmentationNode, visibleSegmentIds, mergedLabelmapNode, sourceVolumeNode)
 
         # Run segmentation algorithm
         import SimpleITK as sitk
         import sitkUtils
         # Read input data from Slicer into SimpleITK
         labelImage = sitk.ReadImage(sitkUtils.GetSlicerITKReadWriteAddress(mergedLabelmapNode.GetName()))
-        backgroundImage = sitk.ReadImage(sitkUtils.GetSlicerITKReadWriteAddress(referenceVolumeNode.GetName()))
+        backgroundImage = sitk.ReadImage(sitkUtils.GetSlicerITKReadWriteAddress(sourceVolumeNode.GetName()))
         # Run watershed filter
         featureImage = sitk.GradientMagnitudeRecursiveGaussian(backgroundImage, float(self.scriptedEffect.doubleParameter("ObjectScaleMm")))
         del backgroundImage
@@ -126,7 +126,7 @@ To segment a single object, create a segment and paint inside and create another
 
         # Update segmentation from labelmap node and remove temporary nodes
         slicer.vtkSlicerSegmentationsModuleLogic.ImportLabelmapToSegmentationNode(mergedLabelmapNode, segmentationNode, visibleSegmentIds)
-        slicer.mrmlScene.RemoveNode(referenceVolumeNode)
+        slicer.mrmlScene.RemoveNode(sourceVolumeNode)
         slicer.mrmlScene.RemoveNode(mergedLabelmapNode)
 
         qt.QApplication.restoreOverrideCursor()

Libs/MRML/Core/vtkMRMLSegmentationNode.cxx
@@ -570,7 +570,7 @@ bool vtkMRMLSegmentationNode::GenerateMergedLabelmapForAllSegments(vtkOrientedIm
 bool vtkMRMLSegmentationNode::GenerateEditMask(vtkOrientedImageData* maskImage, int editMode,
   vtkOrientedImageData* referenceGeometry,
   std::string editedSegmentID/*=""*/, std::string maskSegmentID/*=""*/,
-  vtkOrientedImageData* referenceVolume/*=nullptr*/, double editableIntensityRange[2]/*=nullptr*/,
+  vtkOrientedImageData* sourceVolume/*=nullptr*/, double editableIntensityRange[2]/*=nullptr*/,
   vtkMRMLSegmentationDisplayNode* displayNode/*=nullptr*/)
 {
   if (!maskImage)
@@ -698,11 +698,11 @@ bool vtkMRMLSegmentationNode::GenerateEditMask(vtkOrientedImageData* maskImage, 
     }
 
   // Apply threshold mask if paint threshold is turned on
-  if (referenceVolume != nullptr && editableIntensityRange != nullptr)
+  if (sourceVolume != nullptr && editableIntensityRange != nullptr)
     {
     // Create threshold image
     vtkNew<vtkImageThreshold> threshold;
-    threshold->SetInputData(referenceVolume);
+    threshold->SetInputData(sourceVolume);
     threshold->ThresholdBetween(editableIntensityRange[0], editableIntensityRange[1]);
     threshold->SetInValue(1);
     threshold->SetOutValue(0);
@@ -710,9 +710,9 @@ bool vtkMRMLSegmentationNode::GenerateEditMask(vtkOrientedImageData* maskImage, 
     threshold->Update();
     vtkNew<vtkOrientedImageData> thresholdMask; //  == 0 in editable region
     thresholdMask->ShallowCopy(threshold->GetOutput());
-    vtkNew<vtkMatrix4x4> referenceVolumeToWorldMatrix;
-    referenceVolume->GetImageToWorldMatrix(referenceVolumeToWorldMatrix.GetPointer());
-    thresholdMask->SetImageToWorldMatrix(referenceVolumeToWorldMatrix.GetPointer());
+    vtkNew<vtkMatrix4x4> sourceVolumeToWorldMatrix;
+    sourceVolume->GetImageToWorldMatrix(sourceVolumeToWorldMatrix.GetPointer());
+    thresholdMask->SetImageToWorldMatrix(sourceVolumeToWorldMatrix.GetPointer());
 
     if (!vtkOrientedImageDataResample::ApplyImageMask(maskImage, thresholdMask.GetPointer(), 1))
       {

Libs/MRML/Core/vtkMRMLSegmentationNode.h
@@ -186,7 +186,7 @@ public:
   /// \param editMode defines editable regions based on existing segments
   /// \param referenceGeometry defines image geometry (extent and IJK to world matrix) of the output
   /// \param editedSegmentID this segment will be always editable (regardless of editMode), optional
-  /// \param referenceVolume used for intensity-based masking
+  /// \param sourceVolume used for intensity-based masking
   /// \param editableIntensityRange used for intensity-based masking
   /// \param displayNode used when edit mode refers to visible segments.
   ///   If not specified then the first display node is used.
@@ -194,7 +194,7 @@ public:
   virtual bool GenerateEditMask(vtkOrientedImageData* maskImage, int editMode,
     vtkOrientedImageData* referenceGeometry,
     std::string editedSegmentID="", std::string maskSegmentID="",
-    vtkOrientedImageData* referenceVolume = nullptr, double editableIntensityRange[2] = nullptr,
+    vtkOrientedImageData* sourceVolume = nullptr, double editableIntensityRange[2] = nullptr,
     vtkMRMLSegmentationDisplayNode* displayNode = nullptr);
 
   /// Expose reference identifier to get the volume node defining the reference image geometry if any

Modules/Loadable/Markups/Testing/Python/NeurosurgicalPlanningTutorialMarkupsSelfTest.py
@@ -241,7 +241,7 @@ class NeurosurgicalPlanningTutorialMarkupsSelfTestTest(ScriptedLoadableModuleTes
         segmentEditorWidget = slicer.modules.segmenteditor.widgetRepresentation().self().editor
         segmentEditorNode = segmentEditorWidget.mrmlSegmentEditorNode()
         segmentEditorNode.SetAndObserveSegmentationNode(segmentationNode)
-        segmentEditorNode.SetAndObserveReferenceVolumeNode(baselineVolume)
+        segmentEditorNode.SetAndObserveSourceVolumeNode(baselineVolume)
 
         # create segments
         region1SegmentId = segmentationNode.GetSegmentation().AddEmptySegment("Tumor-cystic")

Modules/Loadable/Segmentations/EditorEffects/Python/AbstractScriptedSegmentEditorAutoCompleteEffect.py
@@ -396,7 +396,7 @@ class AbstractScriptedSegmentEditorAutoCompleteEffect(AbstractScriptedSegmentEdi
             return True
         vtkSegmentationCore.vtkSegmentationConverter.DeserializeImageGeometry(effectiveGeometryString, effectiveGeometryImage)
 
-        masterImageData = self.scriptedEffect.referenceVolumeImageData()
+        masterImageData = self.scriptedEffect.sourceVolumeImageData()
         masterImageExtent = masterImageData.GetExtent()
 
         # The effective extent of the selected segments
@@ -413,7 +413,7 @@ class AbstractScriptedSegmentEditorAutoCompleteEffect(AbstractScriptedSegmentEdi
                 (masterImageExtent[5] != currentLabelExtent[5] and currentLabelExtent[5] < effectiveLabelExtent[5] + self.minimumExtentMargin))
 
     def preview(self):
-        # Get reference volume image data
+        # Get source volume image data
         import vtkSegmentationCorePython as vtkSegmentationCore
 
         # Get segmentation
@@ -449,7 +449,7 @@ class AbstractScriptedSegmentEditorAutoCompleteEffect(AbstractScriptedSegmentEdi
                 return
             vtkSegmentationCore.vtkSegmentationConverter.DeserializeImageGeometry(commonGeometryString, self.mergedLabelmapGeometryImage)
 
-            masterImageData = self.scriptedEffect.referenceVolumeImageData()
+            masterImageData = self.scriptedEffect.sourceVolumeImageData()
             masterImageExtent = masterImageData.GetExtent()
             labelsEffectiveExtent = self.mergedLabelmapGeometryImage.GetExtent()
             # Margin size is relative to combined seed region size, but minimum of 3 voxels
@@ -502,14 +502,16 @@ class AbstractScriptedSegmentEditorAutoCompleteEffect(AbstractScriptedSegmentEdi
             self.clippedMaskImageData = None
             if self.clippedMaskImageDataRequired:
                 self.clippedMaskImageData = slicer.vtkOrientedImageData()
-                intensityBasedMasking = self.scriptedEffect.parameterSetNode().GetReferenceVolumeIntensityMask()
+                intensityBasedMasking = self.scriptedEffect.parameterSetNode().GetSourceVolumeIntensityMask()
+                maskSegmentID = self.scriptedEffect.parameterSetNode().GetMaskSegmentID() if self.scriptedEffect.parameterSetNode().GetMaskSegmentID() else ""
+                intensityRange = self.scriptedEffect.parameterSetNode().GetSourceVolumeIntensityMaskRange() if intensityBasedMasking else None
                 success = segmentationNode.GenerateEditMask(self.clippedMaskImageData,
                                                             self.scriptedEffect.parameterSetNode().GetMaskMode(),
                                                             self.clippedMasterImageData,  # reference geometry
                                                             "",  # edited segment ID
-                                                            self.scriptedEffect.parameterSetNode().GetMaskSegmentID() if self.scriptedEffect.parameterSetNode().GetMaskSegmentID() else "",
+                                                            maskSegmentID,
                                                             self.clippedMasterImageData if intensityBasedMasking else None,
-                                                            self.scriptedEffect.parameterSetNode().GetReferenceVolumeIntensityMaskRange() if intensityBasedMasking else None)
+                                                            intensityRange)
                 if not success:
                     logging.error("Failed to create edit mask")
                     self.clippedMaskImageData = None

Modules/Loadable/Segmentations/EditorEffects/Python/SegmentEditorGrowFromSeedsEffect.py
@@ -20,7 +20,7 @@ class SegmentEditorGrowFromSeedsEffect(AbstractScriptedSegmentEditorAutoComplete
         AbstractScriptedSegmentEditorAutoCompleteEffect.__init__(self, scriptedEffect)
         scriptedEffect.name = 'Grow from seeds'
         self.minimumNumberOfSegments = 2
-        self.clippedMasterImageDataRequired = True  # reference volume intensities are used by this effect
+        self.clippedMasterImageDataRequired = True  # source volume intensities are used by this effect
         self.clippedMaskImageDataRequired = True  # masking is used
         self.growCutFilter = None
 
@@ -38,8 +38,8 @@ class SegmentEditorGrowFromSeedsEffect(AbstractScriptedSegmentEditorAutoComplete
 
     def helpText(self):
         return """<html>Growing segments to create complete segmentation<br>.
-Location, size, and shape of initial segments and content of reference volume are taken into account.
-Final segment boundaries will be placed where reference volume brightness changes abruptly. Instructions:<p>
+Location, size, and shape of initial segments and content of source volume are taken into account.
+Final segment boundaries will be placed where source volume brightness changes abruptly. Instructions:<p>
 <ul style="margin: 0">
 <li>Use Paint or other offects to draw seeds in each region that should belong to a separate segment.
 Paint each seed with a different segment. Minimum two segments are required.</li>

commit 16eb6a42b66a034a8086241bd15692f06fc06863

BUG: Fix deprecated vtkMRMLSegmentEditorNode::GetMasterVolumeIntensityMaskRange
This commit fixes regression introduced in 5dfb6fb69 (BUG: Use "source" instead
of "reference" volume node in Segmentations) by addressing the following
-Wunused-parameter warning:

/path/to/Slicer/Modules/Loadable/Segmentations/MRML/vtkMRMLSegmentEditorNode.h: In member function ‘virtual void vtkMRMLSegmentEditorNode::GetMasterVolumeIntensityMaskRange(double&, double&)’:
/path/to/Slicer/Modules/Loadable/Segmentations/MRML/vtkMRMLSegmentEditorNode.h:191:58: warning: unused parameter ‘_arg1’ [-Wunused-parameter]
  191 |   virtual void GetMasterVolumeIntensityMaskRange(double& _arg1, double& _arg2)
      |                                                  ~~~~~~~~^~~~~

It ensures that the deprecated method behaves as expected.

Modules/Loadable/Segmentations/MRML/vtkMRMLSegmentEditorNode.h
@@ -191,7 +191,7 @@ public:
   virtual void GetMasterVolumeIntensityMaskRange(double& _arg1, double& _arg2)
     {
     vtkWarningMacro("vtkMRMLSegmentEditorNode::GetMasterVolumeIntensityMaskRange() method is deprecated, use GetSourceVolumeIntensityMaskRange method instead");
-    this->GetSourceVolumeIntensityMaskRange(_arg2, _arg2);
+    this->GetSourceVolumeIntensityMaskRange(_arg1, _arg2);
     }
 
   VTK_WRAPEXCLUDE