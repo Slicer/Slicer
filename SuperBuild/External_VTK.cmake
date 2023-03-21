Slicer/Slicer

commit 396c8e5d0ac6bcacf413103c3cb358211a9d5d8c

BUG: Fix crash in vtkPolyData::BuildCells() and add .pyi generator
Backport fix to solve Slicer crash during VMTK centerline extraction.
Backport .pyi interface file generation.

Git log:

Revision: 9b178707481f7539eb84a84e652dc48b85f60eff
Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
Date: 2022-07-06 11:27:29 AM
Message:
[Backport MR-9352] Fix "unreferenced formal parameter" warning in vtkPolyData::BuildCells()

Cherry-picked from https://gitlab.kitware.com/vtk/vtk/-/merge_requests/9352/diffs?commit_id=2ce51d65c44e68198cfe6248b7e50aa33acc425e

This fixes the following warning introduced in
aac909deb9 (Fix vtkPolyData::GetCell() crash)

  warning C4100: 'size': unreferenced formal parameter

(cherry picked from commit 2ce51d65c44e68198cfe6248b7e50aa33acc425e)

Revision: a4d78327358bbc23f6bea46111eaa99d9bc2475f
Author: Andras Lasso <lasso@queensu.ca>
Date: 2022-06-30 10:23:29 PM
Message:
[Backport MR-9352] Fix vtkPolyData::GetCell() crash

Cherry-picked from https://gitlab.kitware.com/vtk/vtk/-/merge_requests/9352/diffs?commit_id=1f04cc68e795ba3a3bd76975be72cec71c9f9759

vtkPolyData::GetCell() crashed the application if any of a cell in a polydata contained less points than expected.

Details: There was a very strict check in vtkPolyData::BuildCells() that cleared out vtkPolyData::Cells when
the number of points in any of the cells in a polydata was less than expected.
Since vtkPolyData::GetCell() assumes that vtkPolyData::BuildCells() creates a valid vtkPolyData::Cells object,
when that strict check failed, vtkPolyData::GetCell() crashed.

The crash could be fixed by an additional null-pointer checks at every use of vtkPolyData::Cells.
However, it has been valid to store less than 2 points in a VTK_POLY_LINE cell for decades.
Very complex algorithms have been built, relying on this feature.
This feature cannot be just suddenly taken away, because it is extremely difficult to rework large

SuperBuild/External_VTK.cmake
@@ -139,7 +139,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
   set(_git_tag)
   if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "9")
-    set(_git_tag "6efd97c519d0cb813a31e52c232161e73b64a7e8") # slicer-v9.1.20220125-efbe2afc2
+    set(_git_tag "9b178707481f7539eb84a84e652dc48b85f60eff") # slicer-v9.1.20220125-efbe2afc2
     set(vtk_egg_info_version "9.1.20220125")
   else()
     message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")

commit 5500809c10afb8a2c1af0b7c02004638b9037ba7

COMP: Update VTK to fix Windows build error related to .pyi files generation
This commit fixes a regression introduced in 396c8e5d0a (BUG: Fix crash in
vtkPolyData::BuildCells() and add .pyi generator) where the feature for
generation pyi files was inadvertently integrated.

The Windows build is fixed by removing vtkpythonmodules_pyi from the
default target (the "ALL" parameter was commented out) in the associated
custom target.

Until this is addressed, this should ensure the build succeeds.

Creating .pyi files for vtkpythonmodules
Traceback (most recent call last):
  File "C:\path\to\S-0-build\python-install\Lib\runpy.py", line 197, in _run_module_as_main
    return _run_code(code, main_globals, None,
  File "C:\path\to\S-0-build\python-install\Lib\runpy.py", line 87, in _run_code
    exec(code, run_globals)
  File "C:\path\to\S-0-build\VTK-build\bin\Lib\site-packages\vtkmodules\generate_pyi.py", line 37, in <module>
    from vtkmodules.vtkCommonCore import vtkObject, vtkSOADataArrayTemplate
ImportError: DLL load failed while importing vtkCommonCore: The specified module could not be found.

List of VTK changes:

$ git shortlog 9b17870748..3ecff39979 --no-merges
Jean-Christophe Fillion-Robin (1):
      COMP: Fix Windows build removing vtkpythonmodules_pyi from the default target

SuperBuild/External_VTK.cmake
@@ -139,7 +139,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
   set(_git_tag)
   if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "9")
-    set(_git_tag "9b178707481f7539eb84a84e652dc48b85f60eff") # slicer-v9.1.20220125-efbe2afc2
+    set(_git_tag "3ecff39979df7b7f4a288ba64ecccb5108fc2a8a") # slicer-v9.1.20220125-efbe2afc2
     set(vtk_egg_info_version "9.1.20220125")
   else()
     message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")