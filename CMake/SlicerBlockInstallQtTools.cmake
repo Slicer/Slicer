Slicer/Slicer

commit 4c91fd0524892571d2fcad40fa3ab4460cf3b3af

COMP: Ensure Qt language tools are bundled on macOS
This commit is a follow-up of 0a00473ea (ENH: Bundle Qt language tools).

It factors out the logic for installing the tools into a CMake module
included on all platforms.

CMake/SlicerBlockInstallQtTools.cmake
@@ -1,0 +1,32 @@
+set(Slicer_INSTALLED_QT_TOOLS)
+
+if(Slicer_BUILD_I18N_SUPPORT)
+  # Bundle Qt language tools with the application if internationalization is enabled.
+  # These tools allow Slicer modules to update and process Qt translation (.ts) files
+  # without requiring installation of Qt.
+  list(APPEND Slicer_INSTALLED_QT_TOOLS
+    lconvert
+    lrelease
+    lupdate
+  )
+endif()
+
+foreach(tool IN LISTS Slicer_INSTALLED_QT_TOOLS)
+  install(PROGRAMS ${qt_root_dir}/bin/${tool}${CMAKE_EXECUTABLE_SUFFIX}
+    DESTINATION ${Slicer_INSTALL_ROOT}/bin COMPONENT Runtime
+    )
+  slicerStripInstalledLibrary(
+    FILES "${Slicer_INSTALL_ROOT}/bin/${tool}"
+    COMPONENT Runtime
+    )
+  if(APPLE)
+    set(dollar "$")
+    install(CODE
+      "set(app ${Slicer_INSTALL_BIN_DIR}/${tool})
+      set(appfilepath \"${dollar}ENV{DESTDIR}${dollar}{CMAKE_INSTALL_PREFIX}/${dollar}{app}\")
+      message(\"CPack: - Adding rpath to ${dollar}{app}\")
+      execute_process(COMMAND install_name_tool -add_rpath @loader_path/..  ${dollar}{appfilepath})"
+      COMPONENT Runtime
+      )
+  endif()
+endforeach()

CMake/SlicerBlockInstallQt.cmake
@@ -191,28 +191,3 @@ set(QT_INSTALL_LIB_DIR ${Slicer_INSTALL_LIB_DIR})
       FILES "${Slicer_INSTALL_ROOT}/bin/designer-real"
       COMPONENT Runtime)
   endif()
-
-  # Bundle Qt language tools with the application if internationalization is enabled.
-  # These tools allow Slicer modules to update and process Qt translation (.ts) files
-  # without requiring installation of Qt.
-  if(Slicer_BUILD_I18N_SUPPORT)
-    foreach(tool lconvert lrelease lupdate)
-      install(PROGRAMS ${qt_root_dir}/bin/${tool}${CMAKE_EXECUTABLE_SUFFIX}
-        DESTINATION ${Slicer_INSTALL_ROOT}/bin COMPONENT Runtime
-        )
-      slicerStripInstalledLibrary(
-        FILES "${Slicer_INSTALL_ROOT}/bin/${tool}"
-        COMPONENT Runtime
-        )
-      if(APPLE)
-        set(dollar "$")
-        install(CODE
-          "set(app ${Slicer_INSTALL_BIN_DIR}/${tool})
-          set(appfilepath \"${dollar}ENV{DESTDIR}${dollar}{CMAKE_INSTALL_PREFIX}/${dollar}{app}\")
-          message(\"CPack: - Adding rpath to ${dollar}{app}\")
-          execute_process(COMMAND install_name_tool -add_rpath @loader_path/..  ${dollar}{appfilepath})"
-          COMPONENT Runtime
-          )
-      endif()
-    endforeach()
-  endif()

CMake/SlicerCPack.cmake
@@ -28,6 +28,8 @@ if(Slicer_USE_PYTHONQT AND NOT Slicer_USE_SYSTEM_python)
 endif()
 
 if(NOT Slicer_USE_SYSTEM_QT)
+  include(${Slicer_CMAKE_DIR}/SlicerBlockInstallQtTools.cmake)
+
   set(SlicerBlockInstallQtPlugins_subdirectories
     audio
     imageformats

commit 214e781813be8b75c9f71a1862b693c69dac9a45

COMP: Fix packaging of Qt tools
This commit fixes a regression introduced in 4c91fd052 (COMP: Ensure Qt
language tools are bundled on macOS).

It sets the variable "qt_root_dir" and adds the relevant checks to report
an error at configuration time.

CMake/SlicerBlockInstallQtTools.cmake
@@ -1,3 +1,35 @@
+
+# Get root directory
+get_property(_filepath TARGET "Qt5::Core" PROPERTY LOCATION_RELEASE)
+get_filename_component(_dir ${_filepath} PATH)
+if(APPLE)
+  # "_dir" of the form "<qt_root_dir>/lib/QtCore.framework"
+  set(qt_root_dir "${_dir}/../..")
+else()
+  # "_dir" of the form "<qt_root_dir>/lib"
+  set(qt_root_dir "${_dir}/..")
+endif()
+
+# Sanity checks
+set(expected_defined_vars
+  Slicer_BUILD_I18N_SUPPORT
+  Slicer_INSTALL_BIN_DIR
+  )
+foreach(var ${expected_defined_vars})
+  if(NOT DEFINED ${var})
+    message(FATAL_ERROR "Variable ${var} is not defined !")
+  endif()
+endforeach()
+
+set(expected_existing_vars
+  qt_root_dir
+  )
+foreach(var ${expected_existing_vars})
+  if(NOT EXISTS "${${var}}")
+    message(FATAL_ERROR "Variable ${var} is set to an inexistent directory or file ! [${${var}}]")
+  endif()
+endforeach()
+
 set(Slicer_INSTALLED_QT_TOOLS)
 
 if(Slicer_BUILD_I18N_SUPPORT)
@@ -12,11 +44,16 @@ if(Slicer_BUILD_I18N_SUPPORT)
 endif()
 
 foreach(tool IN LISTS Slicer_INSTALLED_QT_TOOLS)
-  install(PROGRAMS ${qt_root_dir}/bin/${tool}${CMAKE_EXECUTABLE_SUFFIX}
-    DESTINATION ${Slicer_INSTALL_ROOT}/bin COMPONENT Runtime
+  set(tool_executable ${qt_root_dir}/bin/${tool}${CMAKE_EXECUTABLE_SUFFIX})
+  if(NOT EXISTS "${tool_executable}")
+    message(FATAL_ERROR "Qt tool ${tool} not found: ${tool_executable}")
+  endif()
+  install(PROGRAMS ${tool_executable}
+    DESTINATION ${Slicer_INSTALL_BIN_DIR}
+    COMPONENT Runtime
     )
   slicerStripInstalledLibrary(
-    FILES "${Slicer_INSTALL_ROOT}/bin/${tool}"
+    FILES "${Slicer_INSTALL_BIN_DIR}/${tool}"
     COMPONENT Runtime
     )
   if(APPLE)