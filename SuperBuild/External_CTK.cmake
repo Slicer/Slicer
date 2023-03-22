Slicer/Slicer

commit 2e4cfbd9a2f51f66a7b65ef4d2277c5f00f340ca

BUG: Update CTK to make DICOM browser translatable
Updated CTK to make more strings in DICOM browser translatable.

Change log:

Revision: ca5e014451e693b1f9e42250f5ce2ecfd0e7bd0f
Author: Mouhamed DIOP <mouhamed.diop@esp.sn>
Date: 2023-03-01 6:47:47 PM
Message:
BUG: Mark translatable strings in DICOM/Core

see Slicer/SlicerLanguagePacks#17
see Slicer/SlicerLanguagePacks#12

Revision: 631acef7ccb35f3b965686700f9a74e234269057
Author: Mouhamed DIOP <mouhamed.diop@esp.sn>
Date: 2023-03-01 4:05:20 PM
Message:
BUG: Mark translatable strings in DICOM/Widgets

see Slicer/SlicerLanguagePacks#17
see Slicer/SlicerLanguagePacks#12

Revision: dd9c6754935b7635e1fb528a73646b463f093a01
Author: Steve Pieper <pieper@isomics.com>
Date: 2023-03-15 9:10:21 AM
Message:
BUG: fix spelling error

Thanks @Mohamed Alalli BILAL for finding this 👍

SuperBuild/External_CTK.cmake
@@ -73,7 +73,7 @@ if(NOT DEFINED CTK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "a203172b634253cc3717346de30305ffe721d91c"
+    "c819ae7376b2c5677430ac18e430a16abfc9fa4b"
     QUIET
     )
 

commit c17cfbb7eb3527f75e42c380d8625e712cc67eb1

COMP: Update CTK to fix build of CTK DICOM Core library
This commit fixes a regression introduced in 2e4cfbd9a2 (BUG: Update CTK
to make DICOM browser translatable)

It ensures the build works on case-sensitive file-system by consistently
including Qt headers.

List of CTK changes:

$ git shortlog c819ae737..04618a43f --no-merges
Jean-Christophe Fillion-Robin (1):
      COMP: Fix DICOM/Core build ensuring Qt headers can always be included

SuperBuild/External_CTK.cmake
@@ -73,7 +73,7 @@ if(NOT DEFINED CTK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "c819ae7376b2c5677430ac18e430a16abfc9fa4b"
+    "04618a43f83448838b2b89dbab8b4a215308552e"
     QUIET
     )
 