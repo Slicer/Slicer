a97ed7d1a492e0967cf3878ae77af9578aea40c3
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-04-19 02:49:56 -0400

    COMP: Fix setting of extension module paths when slicer_add_python testing macros
    This commit is a follow-up of both f07a6c3d7 (BUG: Teach 'slicer_add_python_unittest'
    to add module search paths) and the previous commit.
    
    It ensures that arguments like "--disable-modules" and "--disable-*-modules"
    are respected when adding tests using the slicer_add_python testing macros.

--- CMake/SlicerMacroPythonTesting.cmake
+++ CMake/SlicerMacroPythonTesting.cmake
@@ -18,12 +18,37 @@
 #
 ################################################################################
 
-set(UNITTEST_LIB_PATHS
-  "--additional-module-paths"
-  ${CMAKE_BINARY_DIR}/${Slicer_QTSCRIPTEDMODULES_LIB_DIR}
-  ${CMAKE_BINARY_DIR}/${Slicer_CLIMODULES_LIB_DIR}
-  ${CMAKE_BINARY_DIR}/${Slicer_QTLOADABLEMODULES_LIB_DIR}
-  )
+# Setting this policy to NEW avoids error due to use of IN_LIST in build-system
+# of Slicer extension specifying a minimum required version older than 3.3.
+cmake_policy(SET CMP0057 NEW)
+
+# When called from an extension build-system, this function updates the given
+# list of arguments by appending appending "--additional-module-paths" along
+# with the loadable, scripted loadable and CLI module paths if not explicitly
+# disabled passing options like "--disable-modules" or "--disable-<module_type>-modules"
+function(_slicer_python_test_append_module_paths arguments_var)
+  if(DEFINED Slicer_SOURCE_DIR)
+    return()
+  endif()
+  if("--disable-modules" IN_LIST "${arguments_var}")
+    return()
+  endif()
+  set(module_paths)
+  if(NOT "--disable-scripted-loadable-modules" IN_LIST "${arguments_var}" )
+    list(APPEND module_paths ${CMAKE_BINARY_DIR}/${Slicer_QTSCRIPTEDMODULES_LIB_DIR})
+  endif()
+  if(NOT "--disable-cli-modules" IN_LIST "${arguments_var}" )
+    list(APPEND module_paths ${CMAKE_BINARY_DIR}/${Slicer_CLIMODULES_LIB_DIR})
+  endif()
+  if(NOT "--disable-loadable-modules" IN_LIST "${arguments_var}" )
+    list(APPEND module_paths ${CMAKE_BINARY_DIR}/${Slicer_QTLOADABLEMODULES_LIB_DIR})
+  endif()
+  if(module_paths)
+    list(INSERT module_paths 0 "--additional-module-paths")
+  endif()
+  list(APPEND ${arguments_var} ${module_paths})
+  set(${arguments_var} ${${arguments_var}} PARENT_SCOPE)
+endfunction()
 
 macro(slicer_add_python_test)
   set(options)
@@ -34,9 +59,7 @@ macro(slicer_add_python_test)
   if(NOT IS_ABSOLUTE ${MY_SCRIPT})
     set(MY_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/${MY_SCRIPT}")
   endif()
-  if(NOT Slicer_SOURCE_DIR)
-    list(APPEND MY_SLICER_ARGS ${UNITTEST_LIB_PATHS})
-  endif()
+  _slicer_python_test_append_module_paths(MY_SLICER_ARGS)
   ExternalData_add_test(${Slicer_ExternalData_DATA_MANAGEMENT_TARGET}
     NAME py_${MY_TESTNAME_PREFIX}${test_name}
     COMMAND ${Slicer_LAUNCHER_EXECUTABLE}
@@ -59,9 +82,7 @@ macro(slicer_add_python_unittest)
   if("${_script_source_dir}" STREQUAL "")
     set(_script_source_dir ${CMAKE_CURRENT_SOURCE_DIR})
   endif()
-  if(NOT Slicer_SOURCE_DIR)
-    list(APPEND MY_SLICER_ARGS ${UNITTEST_LIB_PATHS})
-  endif()
+  _slicer_python_test_append_module_paths(MY_SLICER_ARGS)
   ExternalData_add_test(${Slicer_ExternalData_DATA_MANAGEMENT_TARGET}
     NAME py_${MY_TESTNAME_PREFIX}${test_name}
     COMMAND ${Slicer_LAUNCHER_EXECUTABLE}
