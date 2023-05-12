d87a9fe8d427417edd7783615795f549b66b194b
Andras Lasso <lasso@queensu.ca>
2023-05-12 13:27:56 -0400

    ENH: Improve Python scripted module detection when drag-and-dropping
    When the user drag-and-dropped a Python scripted module folder that contained Python libraries (subfolders that contained __init__.py file) then the module detection logic could detect .py files in them as Slicer Python scripted modules. These commit improves the scripted module detection logic so that .py files in the embedded Python libraries can no longer be mistaken for Slicer Python scripted modules.
    
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

--- Modules/Scripted/ExtensionWizard/ExtensionWizardLib/ModuleInfo.py
+++ Modules/Scripted/ExtensionWizard/ExtensionWizardLib/ModuleInfo.py
@@ -39,34 +39,37 @@ class ModuleInfo:
             entries = [path]
         else:
             entries = [os.path.join(path, entry) for entry in os.listdir(path)]
+            # If the folder contains __init__.py, it means that this folder
+            # is not a Slicer module but an embedded Python library that a module will load.
+            if any(entry.endswith('__init__.py') for entry in entries):
+                entries = []
 
         if depth > 0:
             for entry in filter(os.path.isdir, entries):
                 result += ModuleInfo.findModules(entry, depth - 1)
 
         for entry in filter(os.path.isfile, entries):
-            # __init__.py is not a module but an embedded Python library
-            # that a module will load.
-            if entry.endswith(".py") and not entry.endswith("__init__.py"):
+            if not entry.endswith(".py"):
+                continue
 
-                # Criteria for a Slicer module to have a module class
-                # that has the same name as the filename.
+            # Criteria for a Slicer module to have a module class
+            # that has the same name as the filename.
 
-                try:
-                    # Find all class definitions
-                    with open(entry) as entry_file:
-                        tree = ast.parse(entry_file.read())
-                    classes = [node for node in tree.body if isinstance(node, ast.ClassDef)]
+            try:
+                # Find all class definitions
+                with open(entry) as entry_file:
+                    tree = ast.parse(entry_file.read())
+                classes = [node for node in tree.body if isinstance(node, ast.ClassDef)]
 
-                    # Add file if module class is found
-                    filename = os.path.basename(entry)
-                    expectedClassName = os.path.splitext(filename)[0]
-                    for cls in classes:
-                        if cls.name == expectedClassName:
-                            result.append(ModuleInfo(entry))
-                except:
-                    # Error while processing the file (e.g., syntax error),
-                    # it cannot be a Slicer module.
-                    pass
+                # Add file if module class is found
+                filename = os.path.basename(entry)
+                expectedClassName = os.path.splitext(filename)[0]
+                for cls in classes:
+                    if cls.name == expectedClassName:
+                        result.append(ModuleInfo(entry))
+            except:
+                # Error while processing the file (e.g., syntax error),
+                # it cannot be a Slicer module.
+                pass
 
         return result
