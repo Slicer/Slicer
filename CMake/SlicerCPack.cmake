Slicer/Slicer

commit 43dd44e85c210fc09375997bd435da61adbe136b

ENH: SlicerCPack: Introduce CPACK_NSIS_INSTALL_SUBDIRECTORY
If Slicer is configured using -DSlicer_CPACK_NSIS_INSTALL_SUBDIRECTORY:STRING=Foo
the application will be installed in

   "C:\Program Files\Foo\Slicer X.Y.Z"

instead of

   "C:\Program Files\Slicer X.Y.Z"

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24593
git-svn-id: http://svn.slicer.org/Slicer4/trunk@24593 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerCPack.cmake
@@ -219,19 +219,27 @@ endif()
 # -------------------------------------------------------------------------
 if(CPACK_GENERATOR STREQUAL "NSIS")
 
+  set(Slicer_CPACK_NSIS_INSTALL_SUBDIRECTORY "")
+  slicer_cpack_set("CPACK_NSIS_INSTALL_SUBDIRECTORY")
+
   # Installers for 32- vs. 64-bit CMake:
   #  - Root install directory (displayed to end user at installer-run time)
   #  - "NSIS package/display name" (text used in the installer GUI)
   #  - Registry key used to store info about the installation
   if(CMAKE_CL_64)
-    slicer_verbose_set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
+    set(_nsis_install_root "$PROGRAMFILES64")
     slicer_verbose_set(CPACK_NSIS_PACKAGE_NAME "${CPACK_PACKAGE_INSTALL_DIRECTORY}")
     slicer_verbose_set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "${CPACK_PACKAGE_INSTALL_DIRECTORY} (Win64)")
   else()
-    slicer_verbose_set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES")
+    set(_nsis_install_root "$PROGRAMFILES")
     slicer_verbose_set(CPACK_NSIS_PACKAGE_NAME "${CPACK_PACKAGE_INSTALL_DIRECTORY} (Win32)")
     slicer_verbose_set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "${CPACK_PACKAGE_INSTALL_DIRECTORY}")
   endif()
+
+  if(NOT CPACK_NSIS_INSTALL_SUBDIRECTORY STREQUAL "")
+    set(_nsis_install_root "${_nsis_install_root}\\\\${CPACK_NSIS_INSTALL_SUBDIRECTORY}")
+  endif()
+  slicer_verbose_set(CPACK_NSIS_INSTALL_ROOT ${_nsis_install_root})
 
   # Slicer does *NOT* require setting the windows path
   set(CPACK_NSIS_MODIFY_PATH OFF)

commit 132bdec730c259f8e9e30ba0aecf9af6c35c69d8

BUG: Fix CPACK_NSIS_INSTALL_SUBDIRECTORY being overwritten
Introduced by https://github.com/Slicer/Slicer/commit/43dd44e85c210fc09375997bd435da61adbe136b

CMake/SlicerCPack.cmake
@@ -354,7 +354,9 @@ endif()
 # -------------------------------------------------------------------------
 if(CPACK_GENERATOR STREQUAL "NSIS")
 
-  set(Slicer_CPACK_NSIS_INSTALL_SUBDIRECTORY "")
+  if(NOT DEFINED Slicer_CPACK_NSIS_INSTALL_SUBDIRECTORY)
+    set(Slicer_CPACK_NSIS_INSTALL_SUBDIRECTORY "")
+  endif()
   slicer_cpack_set("CPACK_NSIS_INSTALL_SUBDIRECTORY")
 
   set(_nsis_install_root "${Slicer_CPACK_NSIS_INSTALL_ROOT}")
132bdec730c259f8e9e30ba0aecf9af6c35c69d8
James Butler <james.butler@perkinelmer.com>
2023-07-13 15:48:23 -0400

    BUG: Fix CPACK_NSIS_INSTALL_SUBDIRECTORY being overwritten
    Introduced by https://github.com/Slicer/Slicer/commit/43dd44e85c210fc09375997bd435da61adbe136b

--- CMake/SlicerCPack.cmake
+++ CMake/SlicerCPack.cmake
@@ -354,7 +354,9 @@ endif()
 # -------------------------------------------------------------------------
 if(CPACK_GENERATOR STREQUAL "NSIS")
 
-  set(Slicer_CPACK_NSIS_INSTALL_SUBDIRECTORY "")
+  if(NOT DEFINED Slicer_CPACK_NSIS_INSTALL_SUBDIRECTORY)
+    set(Slicer_CPACK_NSIS_INSTALL_SUBDIRECTORY "")
+  endif()
   slicer_cpack_set("CPACK_NSIS_INSTALL_SUBDIRECTORY")
 
   set(_nsis_install_root "${Slicer_CPACK_NSIS_INSTALL_ROOT}")
