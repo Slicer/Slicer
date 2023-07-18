Slicer/Slicer

commit e3aadb552458c17fe1af6b8d7c5dd3ba43f246de

BUG: Update VTK to fix regression preventing from loading older vtp files
This ensures that reading an empty "vtkPolyData" reports a warning
instead of an error message.

List of VTK changes:

$ git shortlog 6c02c9b376..8e0f1f9af7 --no-merges
Jean-Christophe Fillion-Robin (1):
      [Backport MR-10295] Allow const std::vector<vtkSmartPointer<T>> in Python (part 2)

Steve Pieper (1):
      [Backport MR-9648] BUG: error for non-fatal xml issue

Co-authored-by: Steve Pieper <pieper@isomics.com>

SuperBuild/External_VTK.cmake
@@ -139,7 +139,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
   set(_git_tag)
   if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "9")
-    set(_git_tag "6c02c9b3769bbb433b1451128993945198fa6bc3") # slicer-v9.2.20230607-1ff325c54
+    set(_git_tag "8e0f1f9af77a8a018d2a609cd6779c95f2bbda18") # slicer-v9.2.20230607-1ff325c54
     set(vtk_egg_info_version "9.2.20230607")
   else()
     message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")

commit 818829110591e48f8e9750e2948a20d33c20c044

COMP: Update VTK to workaround external project output flagged as error
This commit updates VTK to include an empty commit working around
the fact CTest incorrectly flag the external project update output
as an error.

It fixes a regression introduced in e3aadb552 (BUG: Update VTK to fix
regression preventing from loading older vtp files)

To illustrate, it flags the following output as an error
on Linux and Windows:

Cloning into 'VTK'...
HEAD is now at 8e0f1f9af7 [Backport MR-9648] BUG: error for non-fatal xml issue
Submodule 'VTK-m' (https://github.com/KitwareMedical/vtk-m.git) registered for path 'ThirdParty/vtkm/vtkvtkm/vtk-m'
Cloning into '/.../Slicer-0-build/VTK/ThirdParty/vtkm/vtkvtkm/vtk-m'...

This happens despite of having "HEAD is now" being listed in CTEST_CUSTOM_WARNING_EXCEPTION
in CMake/CTestCustom.cmake.in.

List of VTK changes:

$ git shortlog 8e0f1f9af..a3ab617b0 --no-merges
Jean-Christophe Fillion-Robin (1):
      [Slicer] COMP: Empty commit to workaround CTest false positive

Suggested-by: Andras Lasso <lasso@queensu.ca>

SuperBuild/External_VTK.cmake
@@ -139,7 +139,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
   set(_git_tag)
   if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "9")
-    set(_git_tag "8e0f1f9af77a8a018d2a609cd6779c95f2bbda18") # slicer-v9.2.20230607-1ff325c54
+    set(_git_tag "a3ab617b0f356f566b5749ffdb9a164568dd348f") # slicer-v9.2.20230607-1ff325c54
     set(vtk_egg_info_version "9.2.20230607")
   else()
     message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")