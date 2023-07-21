Slicer/Slicer

commit 530b8fcdca4987533a03cfc9361585007883808b

ENH: Update Slicer Controller adding Thick Slab Reconstruction (#7021)
This commit updates the Slice Controller widget adding controls for
enabling the Thick Slab Reconstruction (TSR), selecting the reconstruction
type (max, min, mean, sum) as well as adjusting the slab thickness.

It also updates the DataProbe/SlicerViewAnnotations to allow displaying
the slab type and thickness.

It updates the Slice node adding the following properties:
* SlabReconstructionEnabled
* SlabReconstructionType
* SlabReconstructionThickness
* SlabReconstructionOversamplingFactor

Finally, it also updates vtkMRMLSliceNodeTest adding the following tests:
* SlabReconstructionEnabledTest
* SlabReconstructionTypeTest
* SlabReconstructionThicknessTest

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

Libs/MRML/Logic/vtkMRMLSliceLogic.cxx
@@ -466,6 +466,10 @@ void vtkMRMLSliceLogic::OnMRMLNodeModified(vtkMRMLNode* node)
       sliceDisplayNode->SetVisibility( this->SliceNode->GetSliceVisible() );
       sliceDisplayNode->SetViewNodeIDs( this->SliceNode->GetThreeDViewIDs());
       }
+
+    vtkMRMLSliceLogic::UpdateReconstructionSlab(this, this->GetBackgroundLayer());
+    vtkMRMLSliceLogic::UpdateReconstructionSlab(this, this->GetForegroundLayer());
+
     }
   else if (node == this->SliceCompositeNode)
     {
@@ -968,6 +972,42 @@ bool vtkMRMLSliceLogic::UpdateFractions(vtkImageMathematics* fraction, double op
   return modified;
 }
 
+//----------------------------------------------------------------------------
+void vtkMRMLSliceLogic::UpdateReconstructionSlab(vtkMRMLSliceLogic* sliceLogic, vtkMRMLSliceLayerLogic* sliceLayerLogic)
+{
+  if (!sliceLogic || !sliceLayerLogic || !sliceLogic->GetSliceNode())
+    {
+    return;
+    }
+
+  vtkImageReslice* reslice = sliceLayerLogic->GetReslice();
+  vtkMRMLSliceNode* sliceNode = sliceLayerLogic->GetSliceNode();
+
+  double sliceSpacing;
+  if (sliceNode->GetSliceSpacingMode() == vtkMRMLSliceNode::PrescribedSliceSpacingMode)
+    {
+    sliceSpacing = sliceNode->GetPrescribedSliceSpacing()[2];
+    }
+  else
+    {
+    sliceSpacing = sliceLogic->GetLowestVolumeSliceSpacing()[2];
+    }
+
+  int slabNumberOfSlices = 1;
+  if (sliceNode->GetSlabReconstructionEnabled()
+      && sliceSpacing > 0
+      && sliceNode->GetSlabReconstructionThickness() > sliceSpacing
+      )
+    {
+    slabNumberOfSlices = static_cast<int>(sliceNode->GetSlabReconstructionThickness() / sliceSpacing);
+    }
+  reslice->SetSlabNumberOfSlices(slabNumberOfSlices);
+
+  reslice->SetSlabMode(sliceNode->GetSlabReconstructionType());
+
+  double slabSliceSpacingFraction = sliceSpacing / sliceNode->GetSlabReconstructionOversamplingFactor();
+  reslice->SetSlabSliceSpacingFraction(slabSliceSpacingFraction);
+}
 
 //----------------------------------------------------------------------------
 void vtkMRMLSliceLogic::UpdatePipeline()

Libs/MRML/Core/Testing/vtkMRMLSliceNodeTest1.cxx
@@ -32,6 +32,9 @@ int GetSliceOrientationPresetTest();
 int GetSliceOrientationPresetNameTest();
 int SetOrientationTest();
 int InitializeDefaultMatrixTest();
+int SlabReconstructionEnabledTest();
+int SlabReconstructionTypeTest();
+int SlabReconstructionThicknessTest();
 
 //----------------------------------------------------------------------------
 int vtkMRMLSliceNodeTest1(int , char * [] )
@@ -48,6 +51,9 @@ int vtkMRMLSliceNodeTest1(int , char * [] )
   CHECK_EXIT_SUCCESS(GetSliceOrientationPresetNameTest());
   CHECK_EXIT_SUCCESS(SetOrientationTest());
   CHECK_EXIT_SUCCESS(InitializeDefaultMatrixTest());
+  CHECK_EXIT_SUCCESS(SlabReconstructionEnabledTest());
+  CHECK_EXIT_SUCCESS(SlabReconstructionTypeTest());
+  CHECK_EXIT_SUCCESS(SlabReconstructionThicknessTest());
 
   return EXIT_SUCCESS;
 }
@@ -423,6 +429,90 @@ int InitializeDefaultMatrixTest()
   vtkNew<vtkMatrix3x3> sagittal;
   vtkMRMLSliceNode::GetSagittalSliceToRASMatrix(sagittal.GetPointer());
   CHECK_NOT_NULL(sagittal.GetPointer());
+
+  return EXIT_SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+int SlabReconstructionEnabledTest()
+{
+  vtkNew<vtkMRMLSliceNode> sliceNode;
+
+  CHECK_BOOL(sliceNode->GetSlabReconstructionEnabled(), false);
+
+  // Set using set macro
+  {
+    sliceNode->SetSlabReconstructionEnabled(true);
+    CHECK_BOOL(sliceNode->GetSlabReconstructionEnabled(), true);
+  }
+
+  // Set using on/off macro
+  {
+    sliceNode->SlabReconstructionEnabledOn();
+    CHECK_BOOL(sliceNode->GetSlabReconstructionEnabled(), true);
+    sliceNode->SlabReconstructionEnabledOff();
+    CHECK_BOOL(sliceNode->GetSlabReconstructionEnabled(), false);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+int SlabReconstructionTypeTest()
+{
+  vtkNew<vtkMRMLSliceNode> sliceNode;
+
+  CHECK_INT(sliceNode->GetSlabReconstructionType(), VTK_IMAGE_SLAB_MAX);
+
+  // Set to min
+  {
+    sliceNode->SetSlabReconstructionType(VTK_IMAGE_SLAB_MIN);
+    CHECK_INT(sliceNode->GetSlabReconstructionType(), VTK_IMAGE_SLAB_MIN);
+  }
+
+  // Set to mean
+  {
+    sliceNode->SetSlabReconstructionType(VTK_IMAGE_SLAB_MEAN);
+    CHECK_INT(sliceNode->GetSlabReconstructionType(), VTK_IMAGE_SLAB_MEAN);
+  }
+
+  // Set to sum
+  {
+    sliceNode->SetSlabReconstructionType(VTK_IMAGE_SLAB_SUM);
+    CHECK_INT(sliceNode->GetSlabReconstructionType(), VTK_IMAGE_SLAB_SUM);
+  }
+
+  // Check GetSlabReconstructionTypeAsString
+  {
+    CHECK_STRING(sliceNode->GetSlabReconstructionTypeAsString(VTK_IMAGE_SLAB_MAX), "Max");
+    CHECK_STRING(sliceNode->GetSlabReconstructionTypeAsString(VTK_IMAGE_SLAB_MIN), "Min");
+    CHECK_STRING(sliceNode->GetSlabReconstructionTypeAsString(VTK_IMAGE_SLAB_MEAN), "Mean");
+    CHECK_STRING(sliceNode->GetSlabReconstructionTypeAsString(VTK_IMAGE_SLAB_SUM), "Sum");
+  }
+
+  // Check GetSlabReconstructionTypeFromString
+  {
+    CHECK_INT(sliceNode->GetSlabReconstructionTypeFromString("Max"), VTK_IMAGE_SLAB_MAX);
+    CHECK_INT(sliceNode->GetSlabReconstructionTypeFromString("Min"), VTK_IMAGE_SLAB_MIN);
+    CHECK_INT(sliceNode->GetSlabReconstructionTypeFromString("Mean"), VTK_IMAGE_SLAB_MEAN);
+    CHECK_INT(sliceNode->GetSlabReconstructionTypeFromString("Sum"), VTK_IMAGE_SLAB_SUM);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+int SlabReconstructionThicknessTest()
+{
+  vtkNew<vtkMRMLSliceNode> sliceNode;
+
+  CHECK_DOUBLE(sliceNode->GetSlabReconstructionThickness(), 1.);
+
+  // Set using set macro
+  {
+    sliceNode->SetSlabReconstructionThickness(99.5);
+    CHECK_DOUBLE(sliceNode->GetSlabReconstructionThickness(), 99.5);
+  }
 
   return EXIT_SUCCESS;
 }

Libs/MRML/Core/vtkMRMLSliceNode.cxx
@@ -84,6 +84,11 @@ vtkMRMLSliceNode::vtkMRMLSliceNode()
 
   this->SliceResolutionMode = vtkMRMLSliceNode::SliceResolutionMatch2DView;
 
+  this->SlabReconstructionEnabled = false;
+  this->SlabReconstructionType = VTK_IMAGE_SLAB_MAX;
+  this->SlabReconstructionThickness = 1.;
+  this->SlabReconstructionOversamplingFactor = 2.0;
+
   this->XYZOrigin[0] = 0;
   this->XYZOrigin[1] = 0;
   this->XYZOrigin[2] = 0;
@@ -905,6 +910,11 @@ void vtkMRMLSliceNode::WriteXML(ostream& of, int nIndent)
     vtkMRMLWriteXMLStdStringVectorMacro(threeDViewNodeRef, ThreeDViewIDs, std::vector);
     }
 
+  vtkMRMLWriteXMLBooleanMacro(slabReconstructionEnabled, SlabReconstructionEnabled);
+  vtkMRMLWriteXMLEnumMacro(slabReconstructionType, SlabReconstructionType);
+  vtkMRMLWriteXMLFloatMacro(slabReconstructionThickness, SlabReconstructionThickness);
+  vtkMRMLWriteXMLFloatMacro(slabReconstructionOversamplingFactor, SlabReconstructionOversamplingFactor);
+
   vtkMRMLWriteXMLEndMacro();
 }
 
@@ -991,6 +1001,11 @@ void vtkMRMLSliceNode::ReadXMLAttributes(const char** atts)
       this->AddThreeDViewID(id.c_str());
       }
     }
+
+  vtkMRMLReadXMLBooleanMacro(slabReconstructionEnabled, SlabReconstructionEnabled);
+  vtkMRMLReadXMLEnumMacro(slabReconstructionType, SlabReconstructionType);
+  vtkMRMLReadXMLFloatMacro(slabReconstructionThickness, SlabReconstructionThickness);
+  vtkMRMLReadXMLFloatMacro(slabReconstructionOversamplingFactor, SlabReconstructionOversamplingFactor);
 
   vtkMRMLReadXMLEndMacro();
 
@@ -1105,6 +1120,11 @@ void vtkMRMLSliceNode::CopyContent(vtkMRMLNode* anode, bool deepCopy/*=true*/)
   vtkMRMLCopyVectorMacro(UVWMaximumDimensions, int, 3);
   vtkMRMLCopyVectorMacro(PrescribedSliceSpacing, double, 3);
 
+  vtkMRMLCopyBooleanMacro(SlabReconstructionEnabled);
+  vtkMRMLCopyEnumMacro(SlabReconstructionType);
+  vtkMRMLCopyFloatMacro(SlabReconstructionThickness);
+  vtkMRMLCopyFloatMacro(SlabReconstructionOversamplingFactor);
+
   vtkMRMLCopyEndMacro();
 
   this->UpdateMatrices();
@@ -1198,6 +1218,11 @@ void vtkMRMLSliceNode::PrintSelf(ostream& os, vtkIndent indent)
     }
 
   vtkMRMLPrintStringMacro(DefaultOrientation);
+
+  vtkMRMLPrintBooleanMacro(SlabReconstructionEnabled);
+  vtkMRMLPrintEnumMacro(SlabReconstructionType);
+  vtkMRMLPrintFloatMacro(SlabReconstructionThickness);
+  vtkMRMLPrintFloatMacro(SlabReconstructionOversamplingFactor);
 
   vtkMRMLPrintEndMacro();
 }
@@ -2023,4 +2048,40 @@ bool vtkMRMLSliceNode::SetOrientationToDefault()
     return false;
     }
   return this->SetOrientation(this->GetDefaultOrientation());
+}
+
+//---------------------------------------------------------------------------
+const char* vtkMRMLSliceNode::GetSlabReconstructionTypeAsString(int slabReconstructionType)
+{
+  switch (slabReconstructionType)
+    {
+    case VTK_IMAGE_SLAB_MAX: return "Max";
+    case VTK_IMAGE_SLAB_MIN: return "Min";
+    case VTK_IMAGE_SLAB_MEAN: return "Mean";
+    case VTK_IMAGE_SLAB_SUM: return "Sum";
+    default:
+      vtkGenericWarningMacro("Unknown reconstruction type: " << slabReconstructionType);
+      return "";
+    }
+}
+
+//-----------------------------------------------------------
+int vtkMRMLSliceNode::GetSlabReconstructionTypeFromString(const char* name)
+{
+  if (name == nullptr)
+    {
+    // invalid name
+    return -1;
+    }
+  // VTK_IMAGE_SLAB enum doesn't use last
+  for (int ii = 0; ii < 4; ii++)
+    {
+    if (strcmp(name, GetSlabReconstructionTypeAsString(ii)) == 0)
+      {
+      // found a matching name
+      return ii;
+      }
+    }
+  // unknown name
+  return -1;
 }

Libs/MRML/Core/vtkMRMLSliceNode.h
@@ -523,6 +523,33 @@ public:
   virtual void SetSliceResolutionMode(int mode);
   vtkGetMacro(SliceResolutionMode, int);
 
+  /// @{
+  /// Get/set the slab reconstruction visibility.
+  vtkGetMacro(SlabReconstructionEnabled, bool);
+  vtkSetMacro(SlabReconstructionEnabled, bool);
+  vtkBooleanMacro(SlabReconstructionEnabled, bool);
+  /// @}
+
+  /// @{
+  /// Get/set the slab reconstruction type.
+  vtkGetMacro(SlabReconstructionType, int);
+  vtkSetMacro(SlabReconstructionType, int);
+  static const char* GetSlabReconstructionTypeAsString(int slabReconstructionType);
+  static int GetSlabReconstructionTypeFromString(const char* name);
+  /// @}
+
+  /// @{
+  /// Get/set the slab reconstruction thickness in physical unit.
+  vtkGetMacro(SlabReconstructionThickness, double);
+  vtkSetMacro(SlabReconstructionThickness, double);
+  /// @}
+
+  /// @{
+  /// Get/set the slab reconstruction oversampling factor.
+  vtkGetMacro(SlabReconstructionOversamplingFactor, double);
+  vtkSetMacro(SlabReconstructionOversamplingFactor, double);
+  /// @}
+
 protected:
   vtkMRMLSliceNode();
   ~vtkMRMLSliceNode() override;
@@ -557,6 +584,11 @@ protected:
   double UVWExtents[3];
   int UVWDimensions[3];
   int UVWMaximumDimensions[3];
+
+  bool SlabReconstructionEnabled;
+  int SlabReconstructionType;
+  double SlabReconstructionThickness;
+  double SlabReconstructionOversamplingFactor;
 
   // Hold the string returned by GetOrientationString
   std::string OrientationString;

Libs/MRML/Logic/vtkMRMLSliceLogic.h
@@ -430,6 +430,9 @@ protected:
   /// Helper to update foreground opacity when adding/subtracting the background layer
   bool UpdateFractions(vtkImageMathematics* fraction, double opacity);
 
+  /// Helper to update reconstruction slab settings for a given layer.
+  static void UpdateReconstructionSlab(vtkMRMLSliceLogic* sliceLogic, vtkMRMLSliceLayerLogic* sliceLayerLogic);
+
   /// Returns true if position is inside the selected layer volume.
   /// Use background flag to choose between foreground/background layer.
   bool IsEventInsideVolume(bool background, double worldPos[3]);

Libs/MRML/Widgets/Resources/UI/qMRMLSliceControllerWidget.ui
@@ -627,6 +627,23 @@
          </widget>
         </item>
         <item>
+         <widget class="QToolButton" name="ShowSlabReconstructionButton">
+          <property name="toolTip">
+           <string>Show slab reconstruction</string>
+          </property>
+          <property name="popupMode">
+           <enum>QToolButton::InstantPopup</enum>
+          </property>
+          <property name="icon">
+           <iconset resource="../qMRMLWidgets.qrc">
+            <normaloff>:/Icons/SlabReconstruction.png</normaloff>:/Icons/SlabReconstruction.png</iconset>
+          </property>
+          <property name="autoRaise">
+           <bool>true</bool>
+          </property>
+         </widget>
+        </item>
+        <item>
          <widget class="QToolButton" name="SliceMoreOptionButton">
           <property name="toolTip">
            <string>Advanced options</string>
@@ -752,6 +769,14 @@
    </property>
    <property name="text">
     <string>Show reformat widget</string>
+   </property>
+  </action>
+  <action name="actionShow_slab_reconstruction_widget">
+   <property name="checkable">
+    <bool>true</bool>
+   </property>
+   <property name="text">
+    <string>Show Slab Reconstruction</string>
    </property>
   </action>
   <action name="actionOrientationMarkerTypeCube">
@@ -1166,6 +1191,50 @@
    </property>
    <property name="text">
     <string>Yellow ruler</string>
+   </property>
+  </action>
+  <action name="actionSlabReconstructionMax">
+   <property name="checkable">
+    <bool>true</bool>
+   </property>
+   <property name="text">
+    <string>Max</string>
+   </property>
+   <property name="toolTip">
+    <string>Set slab reconstruction type to Max</string>
+   </property>
+  </action>
+  <action name="actionSlabReconstructionMin">
+   <property name="checkable">
+    <bool>true</bool>
+   </property>
+   <property name="text">
+    <string>Min</string>
+   </property>
+   <property name="toolTip">
+    <string>Set slab reconstruction type to Min</string>
+   </property>
+  </action>
+  <action name="actionSlabReconstructionMean">
+   <property name="checkable">
+    <bool>true</bool>
+   </property>
+   <property name="text">
+    <string>Mean</string>
+   </property>
+   <property name="toolTip">
+    <string>Set slab reconstruction type to Mean</string>
+   </property>
+  </action>
+  <action name="actionSlabReconstructionSum">
+   <property name="checkable">
+    <bool>true</bool>
+   </property>
+   <property name="text">
+    <string>Sum</string>
+   </property>
+   <property name="toolTip">
+    <string>Set slab reconstruction type to Sum</string>
    </property>
   </action>
   <action name="actionSegmentationOutlineFill">
@@ -1711,6 +1780,12 @@
      <y>1</y>
     </hint>
    </hints>
+  </connection>
+  <connection>
+   <sender>MoreButton</sender>
+   <signal>toggled(bool)</signal>
+   <receiver>ShowSlabReconstructionButton</receiver>
+   <slot>setVisible(bool)</slot>
   </connection>
  </connections>
 </ui>

Libs/MRML/Widgets/Resources/qMRMLWidgets.qrc
@@ -161,5 +161,6 @@
         <file>Icons/LayoutFourUpQuantitativeTableView.png</file>
         <file>Icons/ViewMaximize.png</file>
         <file>Icons/ViewRestore.png</file>
+        <file>Icons/SlabReconstruction.png</file>
     </qresource>
 </RCC>

commit f159a97e41774bb80ba4e3b42946c4b473cdfb93

BUG: Fix potential segfault during initialization of qMRMLSliceWidget
This commit fixes a regression introduced in 530b8fcdc (ENH: Update Slicer
Controller adding Thick Slab Reconstruction (#7021)) and fixes these
tests:
- qMRMLSliceWidgetTest1_vol_and_cube.mrml
- qMRMLVolumeThresholdWidgetTest2_vol_and_cube.mrml

The problem was that in the context of these tests the call to
vtkMRMLSliceLayerLogic::SetSliceNode happens after "UpdateSliceCompositeNode"

For reference the sequence of calls lead to the segfault for test
"qMRMLSliceWidgetTest1_vol_and_cube" was the following:

#20 main(int, char**) (ac=<optimized out>, av=<optimized out>)
#19 qMRMLSliceWidgetTest1(int, char**)
#18 qMRMLSliceWidget::setMRMLSliceNode(vtkMRMLSliceNode*)
#17 qMRMLSliceControllerWidget::setMRMLSliceNode(vtkMRMLSliceNode*) (this=
#16 vtkMRMLSliceLogic::SetSliceNode(vtkMRMLSliceNode*)
#15 vtkMRMLSliceLogic::UpdateSliceCompositeNode() (this=0x56366358b080)
#14 vtkMRMLSliceLogic::UpdatePipeline() (this=0x56366358b080)
#13 vtkMRMLSliceLogic::SetSliceExtentsToSliceNode() (this=0x56366358b080)
#12 vtkMRMLSliceNode::UpdateMatrices() (this=0x563662f41e00)
#11 vtkMRMLNode::EndModify(int) (previousDisableModifiedEventState=0, this=0x563662f41e00)
#10 vtkMRMLNode::InvokePendingModifiedEvent() (this=0x563662f41e00)
#9  vtkSubjectHelper::InvokeEvent(unsigned long, void*, vtkObject*)
#8  vtkCallbackCommand::Execute(vtkObject*, unsigned long, void*)
#7  vtkEventBroker::ProcessEvent(vtkObservation*, vtkObject*, unsigned long, void*) (this=0x563662ddf450, observation=0x5636638d4170, caller=<optimized out>, eid=33, callData=0x0)
#6  vtkEventBroker::InvokeObservation(vtkObservation*, unsigned long, void*)
#5  vtkCallbackCommand::Execute(vtkObject*, unsigned long, void*)
#4  vtkMRMLAbstractLogic::MRMLNodesCallback(vtkObject*, unsigned long, void*, void*)
#3  vtkMRMLAbstractLogic::MRMLNodesCallback(vtkObject*, unsigned long, void*, void*) (callData=0x0, clientData=0x56366358b080, eid=33, caller=0x563662f41e00)

Libs/MRML/Logic/vtkMRMLSliceLogic.cxx
@@ -975,7 +975,7 @@ bool vtkMRMLSliceLogic::UpdateFractions(vtkImageMathematics* fraction, double op
 //----------------------------------------------------------------------------
 void vtkMRMLSliceLogic::UpdateReconstructionSlab(vtkMRMLSliceLogic* sliceLogic, vtkMRMLSliceLayerLogic* sliceLayerLogic)
 {
-  if (!sliceLogic || !sliceLayerLogic || !sliceLogic->GetSliceNode())
+  if (!sliceLogic || !sliceLayerLogic || !sliceLogic->GetSliceNode() || !sliceLayerLogic->GetSliceNode())
     {
     return;
     }