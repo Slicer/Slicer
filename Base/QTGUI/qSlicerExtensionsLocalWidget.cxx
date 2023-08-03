Slicer/Slicer

commit c904ea00b0eeed3bfa3eb632df35535b51657c66

STYLE: Mark translatable strings in Base/QTGUI
See https://github.com/Slicer/SlicerLanguagePacks/issues/12

Base/QTGUI/qSlicerExtensionsLocalWidget.cxx
@@ -417,17 +417,17 @@ protected:
     QString statusText;
     if (!available && missing) // "missing" is checked so that we don't display this message when there have been no update checks before
       {
-      statusText += QString("<p style=\"font-weight: bold; font-size: 80%; color: %1;\">"
-        "<img style=\"float: left\" src=\":/Icons/ExtensionIncompatible.svg\"/> "
-        "Not found for this version of the application (r%2)</p>")
+      statusText += QLatin1Literal("<p style=\"font-weight: bold; font-size: 80%; color: %1;\">"
+        "<img style=\"float: left\" src=\":/Icons/ExtensionIncompatible.svg\"/> ") +
+        qSlicerExtensionsLocalWidget::tr("Not found for this version of the application (r%2)") + QLatin1Literal("</p>")
         .arg(this->WarningColor)
         .arg(this->SlicerRevision);
       }
     if (!compatible)
       {
-      statusText += QString("<p style=\"font-weight: bold; font-size: 80%; color: %1;\">"
-        "<img style=\"float: left\" src=\":/Icons/ExtensionIncompatible.svg\"/> "
-        "Incompatible with Slicer r%2 [built for r%3]</p>")
+      statusText += QLatin1Literal("<p style=\"font-weight: bold; font-size: 80%; color: %1;\">"
+        "<img style=\"float: left\" src=\":/Icons/ExtensionIncompatible.svg\"/> ") +
+        qSlicerExtensionsLocalWidget::tr("Incompatible with Slicer r%2 [built for r%3]") + QLatin1Literal("</p>")
         .arg(this->WarningColor)
         .arg(this->SlicerRevision)
         .arg(this->WidgetItem->data(qSlicerExtensionsLocalWidgetPrivate::InstalledExtensionSlicerVersionRole).toString());

Base/QTGUI/qSlicerExportNodeDialog.cxx
@@ -133,7 +133,7 @@ NodeTypeWidgetSet::NodeTypeWidgetSet(QWidget* parent, vtkMRMLStorableNode* stora
     this->exportFormatComboBox->setCurrentIndex(-1);
     this->exportFormatComboBox->setEditable(true); // so we can set custom text below
     this->exportFormatComboBox->lineEdit()->setReadOnly(true);
-    this->exportFormatComboBox->setEditText("Select a format");
+    this->exportFormatComboBox->setEditText(qSlicerExportNodeDialog::tr("Select a format"));
     }
 
   // We make this connection now, not earlier, so that we don't trigger the signal with the above initialization.

Base/QTGUI/qSlicerExtensionsManagerDialog.cxx
@@ -66,7 +66,7 @@ void qSlicerExtensionsManagerDialogPrivate::init()
     q, SLOT(onBatchProcessingChanged()));
 
   QPushButton * restartButton = this->ButtonBox->button(QDialogButtonBox::Ok);
-  restartButton->setText("Restart");
+  restartButton->setText(qSlicerExtensionsManagerDialog::tr("Restart"));
 
   q->setRestartRequested(false);
 

Base/QTGUI/qSlicerExtensionsManagerWidget.cxx
@@ -453,7 +453,8 @@ void qSlicerExtensionsManagerWidget::onModelUpdated()
     }
 
   d->tabWidget->setTabText(manageExtensionsTabIndex,
-                           QString("Manage Extensions (%1)").arg(managedExtensionsCount));
+                           //: %1 represents the number of managed extensions
+                           tr("Manage Extensions (%1)").arg(managedExtensionsCount));
 
   if (managedExtensionsCount == 0)
     {
@@ -714,9 +715,9 @@ void qSlicerExtensionsManagerWidget::onInstallFromFileTriggered()
   Q_D(qSlicerExtensionsManagerWidget);
   const QStringList& archiveNames =
     QFileDialog::getOpenFileNames(
-      this, "Select extension archive file(s)...", QString(),
-      "Archives (*.zip *.7z *.tar *.tar.gz *.tgz *.tar.bz2 *.tar.xz);;"
-      "All files (*)");
+      this, tr("Select extension archive file(s)..."), QString(),
+      tr("Archives") + " (*.zip *.7z *.tar *.tar.gz *.tgz *.tar.bz2 *.tar.xz);;" +
+      tr("All files") + " (*)");
   if (archiveNames.empty())
     {
     return;

Base/QTGUI/qSlicerExtensionsServerWidget.cxx
@@ -92,27 +92,29 @@ void qSlicerExtensionsServerWidgetPrivate::setFailurePage(const QStringList& err
       "</style>"
       "<div class='viewWrapperSlicer'>"
       "  <div class='extensionsHeader'>"
-      "    <div class='extensionsTitle'>Slicer Extensions</div>"
+      "    <div class='extensionsTitle'>" + qSlicerExtensionsServerWidget::tr("Slicer Extensions") + "</div>"
       "  </div>"
       "  <div class='extensionsBody'>"
       "    <!-- Following layout and associated CSS style are inspired from Mozilla error message. -->"
       "    <!-- It is originally covered by https://mozilla.org/MPL/2.0/ license -->"
       "    <!-- MPL 2.0 license is compatible with Slicer (BSD-like) license -->"
       "    <div class='error'>"
-      "      <div id='errorTitle'><h1>Extensions can not be installed.</h1></div>"
+      "      <div id='errorTitle'><h1>" + qSlicerExtensionsServerWidget::tr("Extensions can not be installed.") + "</h1></div>"
       "      <div id='errorDescription'>"
       "        <ul>"
       "%1"
 #ifdef Q_OS_MAC
-    "          <li><b>Extensions manager requires <em>3D Slicer</em> to be installed. "
+    "          <li><b>" + qSlicerExtensionsServerWidget::tr("Extensions manager requires <em>3D Slicer</em> to be installed. "
     "Open the disk image (.dmg) file, drag <em>Slicer.app</em> into the the <em>Applications</em> folder, "
-    "and launch <em>3D Slicer</em> from the <em>Applications</em> folder.</b> "
+    "and launch <em>3D Slicer</em> from the <em>Applications</em> folder.") + "</b> "
 #else
-    "          <li>Check that <b>3D Slicer</b> is properly installed. "
+    "          <li>" + qSlicerExtensionsServerWidget::tr("Check that <b>3D Slicer</b> is properly installed.") + " "
 #endif
-      "<a href='https://slicer.readthedocs.io/en/latest/user_guide/getting_started.html#installing-3d-slicer'>Read more...</a></li>"
+      "<a href='https://slicer.readthedocs.io/en/latest/user_guide/getting_started.html#installing-3d-slicer'>" +
+        qSlicerExtensionsServerWidget::tr("Read more...") + "</a></li>"
       "        </ul>"
-      "        <button id='errorTryAgain' onclick='window.extensions_install_widget.refresh();' autofocus='true'>Try Again</button>"
+      "        <button id='errorTryAgain' onclick='window.extensions_install_widget.refresh();' autofocus='true'>" +
+                qSlicerExtensionsServerWidget::tr("Try Again") + "</button>"
       "      </div>"
       "    </div>"
       "  </div>"

Base/QTGUI/qSlicerFileDialog.cxx
@@ -75,7 +75,7 @@ QStringList qSlicerFileDialog::nameFilters(qSlicerIO::IOFileType fileType)
       extensions << ctk::nameFilterToExtensions(nameFilter);
       }
     }
-  filters.insert(0, QString("All (") + extensions.join(" ") + QString(")"));
+  filters.insert(0, tr("All") + " (" + extensions.join(" ") + ")");
   return filters;
 }
 

Base/QTGUI/qSlicerIOManager.cxx
@@ -94,8 +94,8 @@ bool qSlicerIOManagerPrivate::startProgressDialog(int steps)
     return false;
     }
   int max = (steps != 1 ? steps : 100);
-  this->ProgressDialog = new QProgressDialog("Loading file... ", "Cancel", 0, max);
-  this->ProgressDialog->setWindowTitle(QString("Loading ..."));
+  this->ProgressDialog = new QProgressDialog(qSlicerIOManager::tr("Loading file... "), qSlicerIOManager::tr("Cancel"), 0, max);
+  this->ProgressDialog->setWindowTitle(qSlicerIOManager::tr("Loading ..."));
   if (steps == 1)
     {
     // We only support cancelling a load action if we can have control over it
@@ -177,10 +177,10 @@ createUniqueDialogName(qSlicerIO::IOFileType fileType,
 {
   QString objectName;
 
-  objectName += action == qSlicerFileDialog::Read ? "Add" : "Save";
+  objectName += action == qSlicerFileDialog::Read ?/*no tr*/"Add" : /*no tr*/"Save";
   objectName += fileType;
   objectName += ioProperties["multipleFiles"].toBool() ? "s" : "";
-  objectName += "Dialog";
+  objectName += /*no tr*/"Dialog";
 
   return objectName;
 }
@@ -437,7 +437,7 @@ bool qSlicerIOManager::loadNodes(const qSlicerIO::IOFileType& fileType,
   SlicerRenderBlocker renderBlocker;
   bool needStop = d->startProgressDialog(1);
   d->ProgressDialog->setLabelText(
-    "Loading file " + parameters.value("fileName").toString() + " ...");
+    qSlicerIOManager::tr("Loading file ") + parameters.value("fileName").toString() + " ...");
   if (needStop)
     {
     d->ProgressDialog->setValue(25);

Base/QTGUI/qSlicerModelsDialog.cxx
@@ -61,7 +61,7 @@ void qSlicerModelsDialogPrivate::openAddModelFileDialog()
   QStringList filters = qSlicerFileDialog::nameFilters(q->fileType());
   // TODO add last open directory
   this->SelectedFiles = QFileDialog::getOpenFileNames(
-    this, "Select Model file(s)", "", filters.join(", "));
+    this, qSlicerModelsDialog::tr("Select Model file(s)"), "", filters.join(", "));
   if (this->SelectedFiles.count() < 1)
     {
     return;
@@ -75,7 +75,7 @@ void qSlicerModelsDialogPrivate::openAddModelDirectoryDialog()
   Q_Q(qSlicerModelsDialog);
   // TODO add last open directory.
   QString modelDirectory = QFileDialog::getExistingDirectory(
-    this, "Select a Model directory", "", QFileDialog::ReadOnly);
+    this, qSlicerModelsDialog::tr("Select a Model directory"), "", QFileDialog::ReadOnly);
   if (modelDirectory.isEmpty())
     {
     return;

commit 0b7774f71b32db925b084470d9ad680bb562f64d

BUG: Fix setting of color in qSlicerExtensionsLocalWidget status messages
This commit fixes a regression introduced in c904ea00b (STYLE: Mark translatable
strings in Base/QTGUI)

It addresses warnings like the following:

QString::arg: Argument missing: Incompatible with Slicer r#bd8530 [built for r31917], 31916

Base/QTGUI/qSlicerExtensionsLocalWidget.cxx
@@ -420,18 +420,16 @@ protected:
     QString statusText;
     if (!available && missing) // "missing" is checked so that we don't display this message when there have been no update checks before
       {
-      statusText += QLatin1String("<p style=\"font-weight: bold; font-size: 80%; color: %1;\">"
-        "<img style=\"float: left\" src=\":/Icons/ExtensionIncompatible.svg\"/> ") +
-        qSlicerExtensionsLocalWidget::tr("Not found for this version of the application (r%2)")
-        .arg(this->WarningColor)
+      statusText += QString("<p style=\"font-weight: bold; font-size: 80%; color: %1;\">"
+        "<img style=\"float: left\" src=\":/Icons/ExtensionIncompatible.svg\"/> ").arg(this->WarningColor) +
+        qSlicerExtensionsLocalWidget::tr("Not found for this version of the application (r%1)")
         .arg(this->SlicerRevision) + QLatin1String("</p>");
       }
     if (!compatible)
       {
       statusText += QLatin1String("<p style=\"font-weight: bold; font-size: 80%; color: %1;\">"
-        "<img style=\"float: left\" src=\":/Icons/ExtensionIncompatible.svg\"/> ") +
-        qSlicerExtensionsLocalWidget::tr("Incompatible with Slicer r%2 [built for r%3]")
-        .arg(this->WarningColor)
+        "<img style=\"float: left\" src=\":/Icons/ExtensionIncompatible.svg\"/> ").arg(this->WarningColor) +
+        qSlicerExtensionsLocalWidget::tr("Incompatible with Slicer r%1 [built for r%2]")
         .arg(this->SlicerRevision)
         .arg(this->WidgetItem->data(qSlicerExtensionsLocalWidgetPrivate::InstalledExtensionSlicerVersionRole).toString()) + QLatin1String("</p>");
       }