Slicer/Slicer

commit 8e70f763b0b351db00d8851871e68f11469d1a2e

ENH: Remove different color background for slice view label and offset

Libs/MRML/Widgets/qMRMLSliceControllerWidget.cxx
@@ -117,11 +117,8 @@ qMRMLSliceControllerWidgetPrivate::~qMRMLSliceControllerWidgetPrivate() = defaul
 //---------------------------------------------------------------------------
 void qMRMLSliceControllerWidgetPrivate::setColor(QColor barColor)
 {
-  //this->SliceOffsetSlider->spinBox()->setAutoFillBackground(true);
   this->Superclass::setColor(barColor);
-  QPalette spinBoxPalette( this->SliceOffsetSlider->spinBox()->palette());
-  spinBoxPalette.setColor(QPalette::Base, barColor.lighter(130));
-  this->SliceOffsetSlider->spinBox()->setPalette(spinBoxPalette);
+  this->SliceOffsetSlider->spinBox()->setStyleSheet("background-color: transparent");
 }
 
 //---------------------------------------------------------------------------

Libs/MRML/Widgets/qMRMLViewControllerBar.cxx
@@ -121,7 +121,6 @@ void qMRMLViewControllerBarPrivate::init()
 #else
   this->ViewLabel->setMinimumWidth(this->ViewLabel->fontMetrics().width("XX"));
 #endif
-  this->ViewLabel->setAutoFillBackground(true);
   this->BarLayout->addWidget(this->ViewLabel);
 
   this->ViewMaximizeIcon = QIcon(":Icons/ViewMaximize.png");

commit 88b33fa43d751dc63dd2eb29ba78dcc73efaa6e4

BUG: Fix difficult to see slice offset text in Slicer dark mode
https://github.com/Slicer/Slicer/commit/8e70f763b0b351db00d8851871e68f11469d1a2e introduced this issue of light text being used for the slice offset text when in Slicer dark mode. Dark text was correctly being used in Slicer light mode. This change uses dark text regardless of current Slicer style.

Libs/MRML/Widgets/qMRMLSliceControllerWidget.cxx
@@ -118,7 +118,6 @@ qMRMLSliceControllerWidgetPrivate::~qMRMLSliceControllerWidgetPrivate() = defaul
 void qMRMLSliceControllerWidgetPrivate::setColor(QColor barColor)
 {
   this->Superclass::setColor(barColor);
-  this->SliceOffsetSlider->spinBox()->setStyleSheet("background-color: transparent");
 }
 
 //---------------------------------------------------------------------------
@@ -409,6 +408,8 @@ void qMRMLSliceControllerWidgetPrivate::init()
     ctkDoubleSpinBox::DecimalsByShortcuts |
     ctkDoubleSpinBox::DecimalsByKey |
     ctkDoubleSpinBox::DecimalsAsMin );
+  // Slice controller background color is independent from the color palette, therefore the color of text and controls are hardcoded to black
+  this->SliceOffsetSlider->spinBox()->setStyleSheet("color: black; background-color: transparent;");
 
   //this->SliceOffsetSlider->spinBox()->setParent(this->PopupWidget);
   ctkDoubleSpinBox* spinBox = this->SliceOffsetSlider->spinBox();