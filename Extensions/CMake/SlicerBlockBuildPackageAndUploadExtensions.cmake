Slicer/Slicer

commit 7ed6f2fbd8ee6952ef2b650e7720716d77bca7ba

COMP: Remove obsolete EXTENSION_COMMAND_BUILD_CONF_ARG_LIST CMake var
Following 49828902f (ENH: Extension buildsystem: Remove custom
(Experimental|Nightly)Upload targets), setting the variable
EXTENSION_COMMAND_BUILD_CONF_ARG_LIST is not needed.

Since the CTEST_BUILD_CONFIGURATION variable associated with the
upload and test commands is set in the extension dashboard script, remove
the explicit setting of CTEST_BUILD_CONFIGURATION variable.

Extensions/CMake/SlicerBlockUploadExtension.cmake
@@ -129,18 +129,8 @@ if(CTEST_EXTRA_VERBOSE)
 endif()
 
 #-----------------------------------------------------------------------------
-# Set CTEST_BUILD_CONFIGURATION here
-# See https://www.cmake.org/cmake/help/cmake-2-8-docs.html#variable:CMAKE_CFG_INTDIR
-if(CMAKE_CONFIGURATION_TYPES)
-  set(CTEST_BUILD_CONFIGURATION ${CMAKE_CFG_INTDIR})
-else()
-  set(CTEST_BUILD_CONFIGURATION ${CMAKE_BUILD_TYPE})
-endif()
-set(EXTENSION_COMMAND_BUILD_CONF_ARG_LIST
-  -C ${CTEST_BUILD_CONFIGURATION}
-  -DCTEST_BUILD_CONFIGURATION:STRING=${CTEST_BUILD_CONFIGURATION}
-  )
-set(dollar "$")
+# The CTEST_BUILD_CONFIGURATION variable associated with the upload and test commands
+# defined below is set in the extension dashboard script.
 set(EXTENSION_COMMAND_BUILD_CONF_WRAPPER_ARG_LIST
   -C ${dollar}{CTEST_BUILD_CONFIGURATION}
   -DCTEST_BUILD_CONFIGURATION:STRING=${dollar}{CTEST_BUILD_CONFIGURATION}

commit 4af8253c3dc8c0ba94fa29b3c37d6bbe53b81f09

COMP: Fix extensions build ensuring build-type is set
Fixes a regression introduced in 7ed6f2fbd (COMP: Remove obsolete
EXTENSION_COMMAND_BUILD_CONF_ARG_LIST CMake var).

Extensions/CMake/SlicerBlockBuildPackageAndUploadExtensions.cmake
@@ -236,7 +236,7 @@ foreach(EXTENSION_NAME ${EXTENSION_LIST})
     SOURCE_DIR ${EXTENSION_SOURCE_DIR}
     BINARY_DIR ${EXTENSION_SUPERBUILD_BINARY_DIR}
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${CMAKE_COMMAND} -DCTEST_BUILD_CONFIGURATION=${CTEST_BUILD_CONFIGURATION} -P ${build_extension_wrapper_script}
+    BUILD_COMMAND ${CMAKE_COMMAND} -DCTEST_BUILD_CONFIGURATION=$<CONFIG> -P ${build_extension_wrapper_script}
     INSTALL_COMMAND ""
     ${EP_ARG_EXTENSION_DEPENDS}
     )

Extensions/CMake/SlicerBlockUploadExtension.cmake
@@ -129,8 +129,7 @@ if(CTEST_EXTRA_VERBOSE)
 endif()
 
 #-----------------------------------------------------------------------------
-# The CTEST_BUILD_CONFIGURATION variable associated with the upload and test commands
-# defined below is set in the extension dashboard script.
+set(dollar "$")
 set(EXTENSION_COMMAND_BUILD_CONF_WRAPPER_ARG_LIST
   -C ${dollar}{CTEST_BUILD_CONFIGURATION}
   -DCTEST_BUILD_CONFIGURATION:STRING=${dollar}{CTEST_BUILD_CONFIGURATION}