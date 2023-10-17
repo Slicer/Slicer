Slicer/Slicer

commit d3f49ed3540b8f9374d2e34227be5c0c1a060132

BUG: Make more strings translatable
Marked strings that were found to be non-translatable in https://github.com/Slicer/SlicerLanguagePacks/issues/41

Modules/Scripted/SampleData/SampleData.py
@@ -7,6 +7,7 @@ import qt
 import vtk
 
 import slicer
+from slicer.i18n import tr as _
 from slicer.ScriptedLoadableModule import *
 from slicer.util import computeChecksum, extractAlgoAndDigest, TESTING_DATA_URL
 
@@ -72,11 +73,11 @@ class SampleData(ScriptedLoadableModule):
         self.parent.categories = ["Informatics"]
         self.parent.dependencies = []
         self.parent.contributors = ["Steve Pieper (Isomics), Benjamin Long (Kitware), Jean-Christophe Fillion-Robin (Kitware)"]
-        self.parent.helpText = """
+        self.parent.helpText = _("""
 This module provides data sets that can be used for testing 3D Slicer.
-"""
+""")
         self.parent.helpText += self.getDefaultModuleDocumentationLink()
-        self.parent.acknowledgementText = """
+        self.parent.acknowledgementText = _("""
 <p>This work was was funded in part by Cancer Care Ontario
 and the Ontario Consortium for Adaptive Interventions in Radiation Oncology (OCAIRO)</p>
 
@@ -88,7 +89,7 @@ with a permissive copyright-license (<a href="https://creativecommons.org/licens
 <p>CTA abdomen (Panoramix) dataset comes from <a href="https://www.osirix-viewer.com/resources/dicom-image-library/">Osirix DICOM image library</a>
 and is exclusively available for research and teaching. You are not authorized to redistribute or sell it, or
 use it for commercial purposes.</p>
-"""
+""")
 
         if slicer.mrmlScene.GetTagByClassName("vtkMRMLScriptedModuleNode") != 'ScriptedModule':
             slicer.mrmlScene.RegisterNodeClass(vtkMRMLScriptedModuleNode())
@@ -105,8 +106,8 @@ use it for commercial purposes.</p>
             slicer.modules.sampleDataSources = {}
 
     def addMenu(self):
-        a = qt.QAction('Download Sample Data', slicer.util.mainWindow())
-        a.setToolTip('Go to the SampleData module to download data from the network')
+        a = qt.QAction(_('Download Sample Data'), slicer.util.mainWindow())
+        a.setToolTip(_('Go to the SampleData module to download data from the network'))
         a.connect('triggered()', self.select)
 
         fileMenu = slicer.util.lookupTopLevelWidget('FileMenu')
@@ -270,7 +271,6 @@ class SampleDataWidget(ScriptedLoadableModuleWidget):
         self.log = qt.QTextEdit()
         self.log.readOnly = True
         self.layout.addWidget(self.log)
-        self.logMessage('<p>Status: <i>Idle</i></p>')
 
         # Add spacer to layout
         self.layout.addStretch(1)
@@ -368,21 +368,25 @@ class SampleDataWidget(ScriptedLoadableModuleWidget):
                     b.connect('clicked()', lambda s=source: logic.downloadFromSource(s))
 
     def logMessage(self, message, logLevel=logging.DEBUG):
-        # Set text color based on log level
+
+        # Format based on log level
         if logLevel >= logging.ERROR:
-            message = '<font color="red">' + message + '</font>'
+            message = '<b><font color="red">' + message + '</font></b>'
         elif logLevel >= logging.WARNING:
-            message = '<font color="orange">' + message + '</font>'
+            message = '<b><font color="orange">' + message + '</font></b>'
+
         # Show message in status bar
         doc = qt.QTextDocument()
         doc.setHtml(message)
         slicer.util.showStatusMessage(doc.toPlainText(), 3000)
+        logging.log(logLevel, doc.toPlainText())
+
         # Show message in log window at the bottom of the module widget
         self.log.insertHtml(message)
         self.log.insertPlainText('\n')
         self.log.ensureCursorVisible()
         self.log.repaint()
-        logging.log(logLevel, message)
+
         slicer.app.processEvents(qt.QEventLoop.ExcludeUserInputEvents)
 
     def isCategoryVisible(self, category):
@@ -501,8 +505,8 @@ class SampleDataLogic:
     def __init__(self, logMessage=None):
         if logMessage:
             self.logMessage = logMessage
-        self.builtInCategoryName = 'BuiltIn'
-        self.developmentCategoryName = 'Development'
+        self.builtInCategoryName = _('General')
+        self.developmentCategoryName = _('Development')
         self.registerBuiltInSampleDataSources()
         self.registerDevelopmentSampleDataSources()
         if slicer.app.testingEnabled():
@@ -624,9 +628,9 @@ class SampleDataLogic:
             try:
                 os.makedirs(destFolderPath, exist_ok=True)
             except:
-                self.logMessage('<b>Failed to create cache folder %s</b>' % destFolderPath, logging.ERROR)
+                self.logMessage(_('Failed to create cache folder {path}').format(path=destFolderPath), logging.ERROR)
             if not os.access(destFolderPath, os.W_OK):
-                self.logMessage('<b>Cache folder %s is not writable</b>' % destFolderPath, logging.ERROR)
+                self.logMessage(_('Cache folder {path} is not writable').format(path=destFolderPath), logging.ERROR)
         return self.downloadFile(uri, destFolderPath, name, checksum)
 
     def downloadSourceIntoCache(self, source):
@@ -670,7 +674,8 @@ class SampleDataLogic:
                 try:
                     filePath = self.downloadFileIntoCache(uri, fileName, checksum)
                 except ValueError:
-                    self.logMessage('<b>Download failed (attempt %d of %d)...</b>' % (attemptsCount + 1, maximumAttemptsCount), logging.ERROR)
+                    self.logMessage(_('Download failed (attempt {current} of {total})...').format(
+                        current=attemptsCount + 1, total=maximumAttemptsCount), logging.ERROR)
                     continue
                 resultFilePaths.append(filePath)
 
@@ -708,11 +713,12 @@ class SampleDataLogic:
                 # Failed. Clean up downloaded file (it might have been a partial download)
                 file = qt.QFile(filePath)
                 if file.exists() and not file.remove():
-                    self.logMessage('<b>Load failed (attempt %d of %d). Unable to delete and try again loading %s</b>'
-                                    % (attemptsCount + 1, maximumAttemptsCount, filePath), logging.ERROR)
+                    self.logMessage(_('Load failed (attempt {current} of {total}). Unable to delete and try again loading {path}').format(
+                        current=attemptsCount + 1, total=maximumAttemptsCount, path=filePath), logging.ERROR)
                     resultNodes.append(loadedNode)
                     break
-                self.logMessage('<b>Load failed (attempt %d of %d)...</b>' % (attemptsCount + 1, maximumAttemptsCount), logging.ERROR)
+                self.logMessage(_('Load failed (attempt {current} of {total})...').format(
+                    current=attemptsCount + 1, total=maximumAttemptsCount), logging.ERROR)
 
         if resultNodes:
             return resultNodes
@@ -841,7 +847,8 @@ class SampleDataLogic:
             # we clamp to totalSize when blockSize is larger than totalSize
             humanSizeSoFar = self.humanFormatSize(min(blocksSoFar * blockSize, totalSize))
             humanSizeTotal = self.humanFormatSize(totalSize)
-            self.logMessage('<i>Downloaded %s (%d%% of %s)...</i>' % (humanSizeSoFar, percent, humanSizeTotal))
+            self.logMessage('<i>' + _('Downloaded {sizeCompleted} ({percentCompleted}% of {sizeTotal})...').format(
+                            sizeCompleted=humanSizeSoFar, percentCompleted=percent, sizeTotal=humanSizeTotal) + '</i>')
             self.downloadPercent = percent
 
     def downloadFile(self, uri, destFolderPath, name, checksum=None):
@@ -856,51 +863,53 @@ class SampleDataLogic:
         (algo, digest) = extractAlgoAndDigest(checksum)
         if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
             import urllib.request, urllib.parse, urllib.error
-            self.logMessage(f'<b>Requesting download</b> <i>{name}</i> from {uri} ...')
+            self.logMessage(_('Requesting download {name} from {uri} ...').format(name=name, uri=uri))
             try:
                 urllib.request.urlretrieve(uri, filePath, self.reportHook)
-                self.logMessage('<b>Download finished</b>')
+                self.logMessage(_('Download finished'))
             except OSError as e:
-                self.logMessage('<b>\tDownload failed: %s</b>' % e, logging.ERROR)
-                raise ValueError(f"Failed to download {uri} to {filePath}")
+                self.logMessage('\t' + _('Download failed: {errorMessage}').format(errorMessage=e), logging.ERROR)
+                raise ValueError(_('Failed to download {uri} to {filePath}').format(uri=uri, filePath=filePath))
 
             if algo is not None:
-                self.logMessage('<b>Verifying checksum</b>')
+                self.logMessage(_('Verifying checksum'))
                 current_digest = computeChecksum(algo, filePath)
                 if current_digest != digest:
-                    self.logMessage(f'<b>Checksum verification failed. Computed checksum {current_digest} different from expected checksum {digest}</b>')
+                    self.logMessage(
+                        _('Checksum verification failed. Computed checksum {currentChecksum} different from expected checksum {expectedChecksum}').format(
+                            currentChecksum=current_digest, expectedChecksum=digest))
                     qt.QFile(filePath).remove()
                 else:
                     self.downloadPercent = 100
-                    self.logMessage('<b>Checksum OK</b>')
+                    self.logMessage(_('Checksum OK'))
         else:
             if algo is not None:
-                self.logMessage('<b>Verifying checksum</b>')
+                self.logMessage(_('Verifying checksum'))
                 current_digest = computeChecksum(algo, filePath)
                 if current_digest != digest:
-                    self.logMessage('<b>File already exists in cache but checksum is different - re-downloading it.</b>')
+                    self.logMessage(_('File already exists in cache but checksum is different - re-downloading it.'))
                     qt.QFile(filePath).remove()
                     return self.downloadFile(uri, destFolderPath, name, checksum)
                 else:
                     self.downloadPercent = 100
-                    self.logMessage('<b>File already exists and checksum is OK - reusing it.</b>')
+                    self.logMessage(_('File already exists and checksum is OK - reusing it.'))
             else:
                 self.downloadPercent = 100
-                self.logMessage('<b>File already exists in cache - reusing it.</b>')
+                self.logMessage(_('File already exists in cache - reusing it.'))
         return filePath
 
     def loadScene(self, uri, fileProperties={}):
-        self.logMessage('<b>Requesting load</b> %s ...' % uri)
+        self.logMessage('<b>' + _('Requesting load {uri}').format(uri=uri) + '</b>')
         fileProperties['fileName'] = uri
         success = slicer.app.coreIOManager().loadNodes('SceneFile', fileProperties)
         if not success:
-            self.logMessage('<b>\tLoad failed!</b>', logging.ERROR)
+            self.logMessage('\t' + _('Load failed!'), logging.ERROR)
             return False
-        self.logMessage('<b>Load finished</b>')
+        self.logMessage('<b>' + _('Load finished'), '</b><p></p>')
         return True
 
     def loadNode(self, uri, name, fileType='VolumeFile', fileProperties={}):
-        self.logMessage(f'<b>Requesting load</b> <i>{name}</i> from {uri} ...')
+        self.logMessage('<b>' + _('Requesting load {name} from {uri} ...').format(name=name, uri=uri) + '</b>')
 
         fileProperties['fileName'] = uri
         fileProperties['name'] = name
@@ -909,10 +918,10 @@ class SampleDataLogic:
         success = slicer.app.coreIOManager().loadNodes(fileType, fileProperties, loadedNodes)
 
         if not success or loadedNodes.GetNumberOfItems() < 1:
-            self.logMessage('<b>\tLoad failed!</b>', logging.ERROR)
+            self.logMessage('\t' + _('Load failed!'), logging.ERROR)
             return None
 
-        self.logMessage('<b>Load finished</b>')
+        self.logMessage('<b>' + _('Load finished') + '</b><p></p>')
 
         # since nodes were read from a temp directory remove the storage nodes
         for i in range(loadedNodes.GetNumberOfItems()):

Extensions/Testing/ScriptedLoadableExtensionTemplate/ScriptedLoadableModuleTemplate/ScriptedLoadableModuleTemplate.py
@@ -4,6 +4,8 @@ import ctk
 import qt
 
 import slicer
+from slicer.i18n import tr as _
+from slicer.i18n import translate
 from slicer.ScriptedLoadableModule import *
 from slicer.util import TESTING_DATA_URL
 
@@ -19,19 +21,20 @@ class ScriptedLoadableModuleTemplate(ScriptedLoadableModule):
 
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
-        self.parent.title = "ScriptedLoadableModuleTemplate"  # TODO make this more human readable by adding spaces
-        self.parent.categories = ["Examples"]
+        self.parent.title = _("ScriptedLoadableModuleTemplate")  # TODO make this more human readable by adding spaces
+        self.parent.categories = [translate("qSlicerAbstractCoreModule", "Examples")]
         self.parent.dependencies = []
         self.parent.contributors = ["John Doe (AnyWare Corp.)"]  # replace with "Firstname Lastname (Organization)"
-        self.parent.helpText = """
+        # _() function marks text as translatable to other languages
+        self.parent.helpText = _("""
 This is an example of scripted loadable module bundled in an extension.
 It performs a simple thresholding on the input volume and optionally captures a screenshot.
-"""
+""")
         self.parent.helpText += self.getDefaultModuleDocumentationLink()
-        self.parent.acknowledgementText = """
+        self.parent.acknowledgementText = _("""
 This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
 and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
-"""  # replace with organization, grant and thanks.
+""")  # replace with organization, grant and thanks.
 
 #
 # ScriptedLoadableModuleTemplateWidget
@@ -70,8 +73,8 @@ class ScriptedLoadableModuleTemplateWidget(ScriptedLoadableModuleWidget):
         self.inputSelector.showHidden = False
         self.inputSelector.showChildNodeTypes = False
         self.inputSelector.setMRMLScene(slicer.mrmlScene)
-        self.inputSelector.setToolTip("Pick the input to the algorithm.")
-        parametersFormLayout.addRow("Input Volume: ", self.inputSelector)
+        self.inputSelector.setToolTip(_("Pick the input to the algorithm."))
+        parametersFormLayout.addRow(_("Input Volume: "), self.inputSelector)
 
         #
         # output volume selector
@@ -85,8 +88,8 @@ class ScriptedLoadableModuleTemplateWidget(ScriptedLoadableModuleWidget):
         self.outputSelector.showHidden = False
         self.outputSelector.showChildNodeTypes = False
         self.outputSelector.setMRMLScene(slicer.mrmlScene)
-        self.outputSelector.setToolTip("Pick the output to the algorithm.")
-        parametersFormLayout.addRow("Output Volume: ", self.outputSelector)
+        self.outputSelector.setToolTip(_("Pick the output to the algorithm."))
+        parametersFormLayout.addRow(_("Output Volume: "), self.outputSelector)
 
         #
         # threshold value
@@ -96,22 +99,23 @@ class ScriptedLoadableModuleTemplateWidget(ScriptedLoadableModuleWidget):
         self.imageThresholdSliderWidget.minimum = -100
         self.imageThresholdSliderWidget.maximum = 100
         self.imageThresholdSliderWidget.value = 0.5
-        self.imageThresholdSliderWidget.setToolTip("Set threshold value for computing the output image. Voxels that have intensities lower than this value will set to zero.")
-        parametersFormLayout.addRow("Image threshold", self.imageThresholdSliderWidget)
+        self.imageThresholdSliderWidget.setToolTip(
+            _("Set threshold value for computing the output image. Voxels that have intensities lower than this value will set to zero."))
+        parametersFormLayout.addRow(_("Image threshold"), self.imageThresholdSliderWidget)
 
         #
         # check box to trigger taking screen shots for later use in tutorials
         #
         self.enableScreenshotsFlagCheckBox = qt.QCheckBox()
         self.enableScreenshotsFlagCheckBox.checked = 0
-        self.enableScreenshotsFlagCheckBox.setToolTip("If checked, take screen shots for tutorials. Use Save Data to write them to disk.")
-        parametersFormLayout.addRow("Enable Screenshots", self.enableScreenshotsFlagCheckBox)
+        self.enableScreenshotsFlagCheckBox.setToolTip(_("If checked, take screen shots for tutorials. Use Save Data to write them to disk."))
+        parametersFormLayout.addRow(_("Enable Screenshots"), self.enableScreenshotsFlagCheckBox)
 
         #
         # Apply Button
         #
-        self.applyButton = qt.QPushButton("Apply")
-        self.applyButton.toolTip = "Run the algorithm."
+        self.applyButton = qt.QPushButton(_("Apply"))
+        self.applyButton.toolTip = _("Run the algorithm.")
         self.applyButton.enabled = False
         parametersFormLayout.addRow(self.applyButton)
 
@@ -186,7 +190,7 @@ class ScriptedLoadableModuleTemplateLogic(ScriptedLoadableModuleLogic):
         """
 
         if not self.isValidInputOutputData(inputVolume, outputVolume):
-            slicer.util.errorDisplay('Input volume is the same as output volume. Choose a different output volume.')
+            slicer.util.errorDisplay(_('Input volume is the same as output volume. Choose a different output volume.'))
             return False
 
         logging.info('Processing started')

Extensions/Testing/ScriptedSegmentEditorEffectExtensionTemplate/ScriptedSegmentEditorEffectModuleTemplate/SegmentEditorScriptedSegmentEditorEffectModuleTemplate.py
@@ -2,6 +2,8 @@ import os
 import vtk
 
 import slicer
+from slicer.i18n import tr as _
+from slicer.i18n import translate
 from slicer.ScriptedLoadableModule import *
 
 
@@ -12,14 +14,14 @@ class SegmentEditorScriptedSegmentEditorEffectModuleTemplate(ScriptedLoadableMod
 
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
-        self.parent.title = "SegmentEditorScriptedSegmentEditorEffectModuleTemplate"
-        self.parent.categories = ["Segmentation"]
+        self.parent.title = _("SegmentEditorScriptedSegmentEditorEffectModuleTemplate")
+        self.parent.categories = [translate("qSlicerAbstractCoreModule", "Segmentation")]
         self.parent.dependencies = ["Segmentations"]
         self.parent.contributors = ["Andras Lasso (PerkLab)"]
         self.parent.hidden = True
-        self.parent.helpText = "This hidden module registers the segment editor effect"
+        self.parent.helpText = _("This hidden module registers the segment editor effect")
         self.parent.helpText += self.getDefaultModuleDocumentationLink()
-        self.parent.acknowledgementText = "Supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community. See https://www.slicer.org for details."
+        self.parent.acknowledgementText = _("Supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community. See https://www.slicer.org for details.")
         slicer.app.connect("startupCompleted()", self.registerEditorEffect)
 
     def registerEditorEffect(self):

Modules/Loadable/Models/qSlicerModelsModule.cxx
@@ -71,7 +71,7 @@ qSlicerModelsModule::~qSlicerModelsModule() = default;
 //-----------------------------------------------------------------------------
 QString qSlicerModelsModule::helpText()const
 {
-  QString help =
+  QString help = tr(
     "The Models Module loads and adjusts display parameters of models such as Color, Transparency, and Clipping.<br>"
     "Save models via the File menu, Save button.<br>"
     "The Add 3D model or a model directory button will allow you to load any "
@@ -86,7 +86,7 @@ QString qSlicerModelsModule::helpText()const
     "Clipping is turned on for a model in the Display pane, and the slice "
     "planes that will clip the model are selected in the Clipping pane.<br>"
     "The Model Hierarchy pane allows you to group models together and set the "
-    "group's properties.";
+    "group's properties.");
   help += this->defaultDocumentationLink();
   return help;
 }

Modules/Loadable/VolumeRendering/qSlicerVolumeRenderingModule.cxx
@@ -93,15 +93,15 @@ QString qSlicerVolumeRenderingModule::helpText()const
 QString qSlicerVolumeRenderingModule::acknowledgementText()const
 {
   QString acknowledgement =
-    tr("<center><table border=\"0\"><tr>"
+    "<center><table border=\"0\"><tr>"
     "<td><img src=\":Logos/NAMIC.png\" alt\"NA-MIC\"></td>"
     "<td><img src=\":Logos/NAC.png\" alt\"NAC\"></td>"
     "</tr><tr>"
     "<td><img src=\":Logos/BIRN-NoText.png\" alt\"BIRN\"></td>"
     "<td><img src=\":Logos/NCIGT.png\" alt\"NCIGT\"></td>"
     "</tr></table></center>"
-    "This work is supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community."
-    "Some of the transfer functions were contributed by Kitware Inc. (VolView)");
+    + tr("This work is supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community."
+      " Some of the transfer functions were contributed by Kitware Inc. (VolView)");
   return acknowledgement;
 }
 

Modules/Scripted/CropVolumeSequence/CropVolumeSequence.py
@@ -5,6 +5,8 @@ import qt
 import vtk
 
 import slicer
+from slicer.i18n import tr as _
+from slicer.i18n import translate
 from slicer.ScriptedLoadableModule import *
 
 
@@ -20,14 +22,14 @@ class CropVolumeSequence(ScriptedLoadableModule):
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
         self.parent.title = "Crop volume sequence"
-        self.parent.categories = ["Sequences"]
+        self.parent.categories = [translate("qSlicerAbstractCoreModule", "Sequences")]
         self.parent.dependencies = []
         self.parent.contributors = ["Andras Lasso (PerkLab, Queen's University)"]
-        self.parent.helpText = """This module can crop and resample a volume sequence to reduce its size for faster rendering and processing."""
+        self.parent.helpText = _("""This module can crop and resample a volume sequence to reduce its size for faster rendering and processing.""")
         self.parent.helpText += self.getDefaultModuleDocumentationLink()
-        self.parent.acknowledgementText = """
+        self.parent.acknowledgementText = _("""
 This file was originally developed by Andras Lasso
-"""
+""")
 
 
 #
@@ -48,7 +50,7 @@ class CropVolumeSequenceWidget(ScriptedLoadableModuleWidget):
         # Parameters Area
         #
         parametersCollapsibleButton = ctk.ctkCollapsibleButton()
-        parametersCollapsibleButton.text = "Parameters"
+        parametersCollapsibleButton.text = _("Parameters")
         self.layout.addWidget(parametersCollapsibleButton)
 
         # Layout within the dummy collapsible button
@@ -65,8 +67,8 @@ class CropVolumeSequenceWidget(ScriptedLoadableModuleWidget):
         self.inputSelector.showHidden = False
         self.inputSelector.showChildNodeTypes = False
         self.inputSelector.setMRMLScene(slicer.mrmlScene)
-        self.inputSelector.setToolTip("Pick a sequence node of volumes that will be cropped and resampled.")
-        parametersFormLayout.addRow("Input volume sequence: ", self.inputSelector)
+        self.inputSelector.setToolTip(_("Pick a sequence node of volumes that will be cropped and resampled."))
+        parametersFormLayout.addRow(_("Input volume sequence: "), self.inputSelector)
 
         #
         # output volume selector
@@ -77,12 +79,12 @@ class CropVolumeSequenceWidget(ScriptedLoadableModuleWidget):
         self.outputSelector.addEnabled = True
         self.outputSelector.removeEnabled = True
         self.outputSelector.noneEnabled = True
-        self.outputSelector.noneDisplay = "(Overwrite input)"
+        self.outputSelector.noneDisplay = _("(Overwrite input)")
         self.outputSelector.showHidden = False
         self.outputSelector.showChildNodeTypes = False
         self.outputSelector.setMRMLScene(slicer.mrmlScene)
-        self.outputSelector.setToolTip("Pick a sequence node where the cropped and resampled volumes will be stored.")
-        parametersFormLayout.addRow("Output volume sequence: ", self.outputSelector)
+        self.outputSelector.setToolTip(_("Pick a sequence node where the cropped and resampled volumes will be stored."))
+        parametersFormLayout.addRow(_("Output volume sequence: "), self.outputSelector)
 
         #
         # Crop parameters selector
@@ -97,23 +99,23 @@ class CropVolumeSequenceWidget(ScriptedLoadableModuleWidget):
         self.cropParametersSelector.showHidden = True
         self.cropParametersSelector.showChildNodeTypes = False
         self.cropParametersSelector.setMRMLScene(slicer.mrmlScene)
-        self.cropParametersSelector.setToolTip("Select a crop volumes parameters.")
+        self.cropParametersSelector.setToolTip(_("Select a crop volumes parameters."))
 
         self.editCropParametersButton = qt.QPushButton()
         self.editCropParametersButton.setIcon(qt.QIcon(':Icons/Go.png'))
         # self.editCropParametersButton.setMaximumWidth(60)
         self.editCropParametersButton.enabled = True
-        self.editCropParametersButton.toolTip = "Go to Crop Volume module to edit cropping parameters."
+        self.editCropParametersButton.toolTip = _("Go to Crop Volume module to edit cropping parameters.")
         hbox = qt.QHBoxLayout()
         hbox.addWidget(self.cropParametersSelector)
         hbox.addWidget(self.editCropParametersButton)
-        parametersFormLayout.addRow("Crop volume settings: ", hbox)
+        parametersFormLayout.addRow(_("Crop volume settings: "), hbox)
 
         #
         # Apply Button
         #
-        self.applyButton = qt.QPushButton("Apply")
-        self.applyButton.toolTip = "Run the algorithm."
+        self.applyButton = qt.QPushButton(_("Apply"))
+        self.applyButton.toolTip = _("Run the algorithm.")
         self.applyButton.enabled = False
         parametersFormLayout.addRow(self.applyButton)
 

commit 257f0fc950d5bb49f449324eb6dc0c78c6de838a

BUG: Fix logging in SampleDataLogic.loadScene
Fix regression introduced in d3f49ed35 (BUG: Make more strings translatable)
and restore the passing of the following tests:

* py_Slicer4Minute
* py_ShaderProperties
* py_RSNA2012ProstateDemo
* py_VolumeRenderingSceneClose
* py_SubjectHierarchyGenericSelfTest
* py_SlicerRestoreSceneViewCrashIssue3445
* py_RSNAVisTutorial
* py_JRC2013Vis
* py_AtlasTests

Modules/Scripted/SampleData/SampleData.py
@@ -905,7 +905,7 @@ class SampleDataLogic:
         if not success:
             self.logMessage('\t' + _('Load failed!'), logging.ERROR)
             return False
-        self.logMessage('<b>' + _('Load finished'), '</b><p></p>')
+        self.logMessage('<b>' + _('Load finished') + '</b><p></p>')
         return True
 
     def loadNode(self, uri, name, fileType='VolumeFile', fileProperties={}):