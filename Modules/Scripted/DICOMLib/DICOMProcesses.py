Slicer/Slicer

commit adb997269bacf3dfe15b597480391ce8e880d12e

COMP: Refactor `DICOMProcess` and `DICOMCommand`
Changes:
- Adds documentation to `DICOMProcess`
- Refactors `DICOMCommand` to use underlying process instead of masking
  with a new process, and adds documentation

Modules/Scripted/DICOMLib/DICOMProcesses.py
@@ -65,19 +65,23 @@ class DICOMProcess:
     def __del__(self):
         self.stop()
 
-    def start(self, cmd, args):
+    def start(self, cmd: str, args: list[str]) -> qt.QProcess:
+        """Run the given command as a standalone process."""
         if self.process is not None:
             self.stop()
         self.cmd = cmd
         self.args = args
 
-        # start the server!
+        # start the process!
         self.process = qt.QProcess()
         self.process.connect('stateChanged(QProcess::ProcessState)', self.onStateChanged)
         logging.debug(("Starting %s with " % cmd, args))
         self.process.start(cmd, args)
+        return self.process
 
     def onStateChanged(self, newState):
+        """Callback to indicate that the process state has changed
+        and is now either starting, running, or not running."""
         logging.debug(f"Process {self.cmd} now in state {self.QProcessState[newState]}")
         if newState == 0 and self.process:
             stdout = self.process.readAllStandardOutput()
@@ -88,14 +92,14 @@ class DICOMProcess:
             return stdout, stderr
         return None, None
 
-    def stop(self):
-        if hasattr(self, 'process'):
-            if self.process:
-                logging.debug("stopping DICOM process")
-                self.process.kill()
-                # Wait up to 3 seconds for the process to stop
-                self.process.waitForFinished(3000)
-                self.process = None
+    def stop(self) -> None:
+        """Stop the running standalone process."""
+        if hasattr(self, "process") and self.process:
+            logging.debug("stopping DICOM process")
+            self.process.kill()
+            # Wait up to 3 seconds for the process to stop
+            self.process.waitForFinished(3000)
+            self.process = None
 
 
 class DICOMCommand(DICOMProcess):
@@ -108,14 +112,16 @@ class DICOMCommand(DICOMProcess):
         self.executable = self.exeDir + '/' + cmd + self.exeExtension
         self.args = args
 
-    def __del__(self):
-        super().__del__()
+    def start(self) -> qt.QByteArray:
+        """
+        Run the DICOM process and block until return.
 
-    def start(self):
-        # run the process!
-        self.process = qt.QProcess()
-        logging.debug(('DICOM process running: ', self.executable, self.args))
-        self.process.start(self.executable, self.args)
+        :return: A qt.QByteArray representing standard output (stdout) \
+            from the DICOM process. \
+            https://doc.qt.io/qt-5/qprocess.html#readAllStandardOutput
+        """
+        super().start(self.executable, self.args)
+        logging.debug(("DICOM process running: ", self.executable, self.args))
         self.process.waitForFinished()
         if self.process.exitStatus() == qt.QProcess.CrashExit or self.process.exitCode() != 0:
             stdout = self.process.readAllStandardOutput()

commit 27c96531c5ab0cd8885d783ccc90d09b533d95b6

BUG: Ensure DICOMCommand.start() returns process output
Resolve py_nomainwindow_DCMTKPrivateDictTest failure caused by a regression
introduced in commit adb997269 (COMP: Refactor DICOMProcess and DICOMCommand).

The issue stemmed from the refactoring commit, which guaranteed that the
"start()" function of the base class was consistently invoked. This resulted in
the connection of the "onStateChanged()" signal and the immediate reading of standard output and error.
Subsequently, any further calls to "self.process.readAllStandardOutput()" were
returning an empty string. This fix restores the expected behavior.

Modules/Scripted/DICOMLib/DICOMProcesses.py
@@ -75,6 +75,8 @@ class DICOMProcess:
         self.connections = {}
         self.exeDir = self.getDCMTKToolsPath()
         self.exeExtension = ".exe" if os.name == "nt" else ""
+        self.stdout = None
+        self.stderr = None
 
     def __del__(self):
         self.stop()
@@ -97,14 +99,20 @@ class DICOMProcess:
         """Callback to indicate that the process state has changed
         and is now either starting, running, or not running."""
         logging.debug(f"Process {self.cmd} now in state {self.PROCESS_STATE_NAMES[newState]}")
+        stdout = None
+        stderr = None
         if newState == 0 and self.process:
             stdout = self.process.readAllStandardOutput()
             stderr = self.process.readAllStandardError()
-            logging.debug('DICOM process error code is: %d' % self.process.error())
-            logging.debug('DICOM process standard out is: %s' % stdout)
-            logging.debug('DICOM process standard error is: %s' % stderr)
-            return stdout, stderr
-        return None, None
+
+            logging.debug(f"DICOM process exit status is: {self.process.exitStatus()}")
+            logging.debug(f"DICOM process exit code is: {self.process.exitCode()}")
+            logging.debug(f"DICOM process error is: {self.process.error()}")
+            logging.debug(f"DICOM process standard out is: {stdout}")
+            logging.debug(f"DICOM process standard error is: {stderr}")
+        self.stdout = stdout
+        self.stderr = stderr
+        return self.stdout, self.stderr
 
     def stop(self) -> None:
         """Stop the running standalone process."""
@@ -138,16 +146,8 @@ class DICOMCommand(DICOMProcess):
         logging.debug(("DICOM process running: ", self.executable, self.args))
         self.process.waitForFinished()
         if self.process.exitStatus() == qt.QProcess.CrashExit or self.process.exitCode() != 0:
-            stdout = self.process.readAllStandardOutput()
-            stderr = self.process.readAllStandardError()
-            logging.debug('DICOM process exit status is: %d' % self.process.exitStatus())
-            logging.debug('DICOM process exit code is: %d' % self.process.exitCode())
-            logging.debug('DICOM process error is: %d' % self.process.error())
-            logging.debug('DICOM process standard out is: %s' % stdout)
-            logging.debug('DICOM process standard error is: %s' % stderr)
             raise UserWarning(f"Could not run {self.executable} with {self.args}")
-        stdout = self.process.readAllStandardOutput()
-        return stdout
+        return self.stdout
 
 
 class DICOMStoreSCPProcess(DICOMProcess):
@@ -565,12 +565,6 @@ class DICOMSender:
             # success
             return True
 
-        stdout = self.process.readAllStandardOutput()
-        stderr = self.process.readAllStandardError()
-        logging.debug('DICOM send using standard configuration failed: process error code is %d' % self.process.error())
-        logging.debug('DICOM send process standard out is: %s' % stdout)
-        logging.debug('DICOM send process standard error is: %s' % stderr)
-
         # Retry transfer with alternative configuration with presentation contexts which support SEG/SR.
         # A common cause of failure is an incomplete set of dcmtk/DCMSCU presentation context UIDS.
         # Refer to https://book.orthanc-server.com/faq/dcmtk-tricks.html#id2 for additional detail.
@@ -582,12 +576,6 @@ class DICOMSender:
         ):
             # success
             return True
-
-        stdout = self.process.readAllStandardOutput()
-        stderr = self.process.readAllStandardError()
-        logging.debug('DICOM send using extended configuration failed: process error code is %d' % self.process.error())
-        logging.debug('DICOM send process standard out is: %s' % stdout)
-        logging.debug('DICOM send process standard error is: %s' % stderr)
 
         userMsg = f"Could not send {file} to {self.destinationUrl.host()}:{self.destinationUrl.port()}"
         raise UserWarning(userMsg)