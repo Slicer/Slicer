Slicer/Slicer

commit f348d6f5bf977204e726146ec5b3f46143ff9109

COMP: Install python packages using PyPI wheels
svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=28540
git-svn-id: http://svn.slicer.org/Slicer4/trunk@28540 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_python-dicom-requirements.cmake
@@ -1,0 +1,52 @@
+set(proj python-dicom-requirements)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES python python-setuptools python-requests-requirements python-pillow python-numpy python-pip)
+
+set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
+file(WRITE ${requirements_file} [===[
+dicom==0.9.9.post1 --hash=sha256:9caac735341cfb7ef4c718186ab4339088b083cf12ae8847464a924ec5c6a185
+pydicom==1.2.2 --hash=sha256:44a11323e9ee5c189da20958c28ca426656778ee7c7942a2126bf0a9f6aa4e95
+six==1.12.0 --hash=sha256:3350809f0555b11f552448330d0b52d5f24c91a322ea4a15ef22629740f3761c
+dicomweb_client==0.11.0 --hash=sha256:7e92bfdcb8aac7d1ffe059bb77df66dc7276df32aa7e6bc56ab74635ea918739
+]===])
+
+if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
+  set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
+endif()
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(Slicer_USE_SYSTEM_${proj})
+  foreach(module_name IN ITEMS dicom pydicom dicomweb_client)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
+endif()
+
+if(NOT Slicer_USE_SYSTEM_${proj})
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    LOG_INSTALL 1
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+
+  ExternalProject_GenerateProjectDescription_Step(${proj}
+    VERSION ${_version}
+    )
+
+else()
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()
+

SuperBuild/External_python-extension-manager-requirements.cmake
@@ -1,0 +1,54 @@
+set(proj python-extension-manager-requirements)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES python python-setuptools python-pip)
+
+set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
+file(WRITE ${requirements_file} [===[
+chardet==3.0.4 --hash=sha256:fc323ffcaeaed0e0a02bf4d117757b98aed530d9ed4531e3e15460124c106691
+couchdb==1.2 --hash=sha256:13a28a1159c49f8346732e8724b9a4d65cba54bec017c4a7eeb1499fe88151d1
+gitdb2==2.0.5 --hash=sha256:e3a0141c5f2a3f635c7209d56c496ebe1ad35da82fe4d3ec4aaa36278d70648a
+smmap2==2.0.5 --hash=sha256:0555a7bf4df71d1ef4218e4807bbf9b201f910174e6e08af2e138d4e517b4dde
+GitPython==2.1.11 --hash=sha256:563221e5a44369c6b79172f455584c9ebbb122a13368cc82cb4b5addff788f82
+six==1.12.0 --hash=sha256:3350809f0555b11f552448330d0b52d5f24c91a322ea4a15ef22629740f3761c
+]===])
+
+if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
+  set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
+endif()
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(Slicer_USE_SYSTEM_${proj})
+  foreach(module_name IN ITEMS chardet couchdb gitdb2 smmap2 GitPython six)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
+endif()
+
+if(NOT Slicer_USE_SYSTEM_${proj})
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    LOG_INSTALL 1
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+
+  ExternalProject_GenerateProjectDescription_Step(${proj}
+    VERSION ${_version}
+    )
+
+else()
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()
+

SuperBuild/External_python-extension-manager-ssl-requirements.cmake
@@ -1,7 +1,13 @@
-set(proj python-couchdb)
+set(proj python-extension-manager-ssl-requirements)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python python-setuptools)
+set(${proj}_DEPENDENCIES python python-setuptools python-requests-requirements python-pip)
+
+set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
+file(WRITE ${requirements_file} [===[
+PyJWT==1.7.1 --hash=sha256:5c6eca3c2940464d106b99ba83b00c6add741c9becaec087fb7ccdefea71350e
+PyGithub==1.40 --hash=sha256:b9f5cf80197ba0e3848e084de289ffa19ce1c8c279070a58754ad694d364d481
+]===])
 
 if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
   set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
@@ -11,26 +17,24 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(Slicer_USE_SYSTEM_${proj})
-  ExternalProject_FindPythonPackage(
-    MODULE_NAME "couchdb"
-    REQUIRED
-    )
+  foreach(module_name IN ITEMS PyJWT PyGithub)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
 endif()
 
 if(NOT Slicer_USE_SYSTEM_${proj})
 
-  set(_version "1.2")
-
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
-    URL "https://pypi.python.org/packages/7c/c8/f94a107eca0c178e5d74c705dad1a5205c0f580840bd1b155cd8a258cb7c/CouchDB-${_version}.tar.gz"
-    URL_HASH "SHA256=1386a1a43f25bed3667e3b805222054940d674fa1967fa48e9d2012a18630ab7"
-    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
+    DOWNLOAD_COMMAND ""
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
     LOG_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}
@@ -43,3 +47,4 @@ if(NOT Slicer_USE_SYSTEM_${proj})
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()
+

SuperBuild/External_python-numpy.cmake
@@ -1,0 +1,86 @@
+set(proj python-numpy)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES python python-setuptools python-pip)
+
+set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
+file(WRITE ${requirements_file} [===[
+nose==1.3.7 --hash=sha256:9ff7c6cc443f8c51994b34a667bbcf45afd6d945be7477b52e97516fd17c53ac  # needed for NumPy unit tests
+numpy==1.16.2 --hash=sha256:23cc40313036cffd5d1873ef3ce2e949bdee0646c5d6f375bf7ee4f368db2511 # auto pick platform
+]===])
+
+if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
+  set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
+endif()
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(Slicer_USE_SYSTEM_${proj})
+  foreach(module_name IN ITEMS nose numpy)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
+endif()
+
+if(NOT Slicer_USE_SYSTEM_${proj})
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    LOG_INSTALL 1
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+
+  ExternalProject_GenerateProjectDescription_Step(${proj}
+    VERSION ${_version}
+    )
+
+  #-----------------------------------------------------------------------------
+  # Sanity checks
+
+  foreach(varname IN ITEMS
+      python_DIR
+      PYTHON_SITE_PACKAGES_SUBDIR
+      )
+    if("${${varname}}" STREQUAL "")
+      message(FATAL_ERROR "${varname} CMake variable is expected to be set")
+    endif()
+  endforeach()
+
+  #-----------------------------------------------------------------------------
+  # Launcher setting specific to build tree
+
+  set(${proj}_LIBRARY_PATHS_LAUNCHER_BUILD
+    ${python_DIR}/${PYTHON_SITE_PACKAGES_SUBDIR}/numpy/core
+    ${python_DIR}/${PYTHON_SITE_PACKAGES_SUBDIR}/numpy/lib
+    )
+  mark_as_superbuild(
+    VARS ${proj}_LIBRARY_PATHS_LAUNCHER_BUILD
+    LABELS "LIBRARY_PATHS_LAUNCHER_BUILD"
+    )
+
+  #-----------------------------------------------------------------------------
+  # Launcher setting specific to install tree
+
+  set(${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED
+    <APPLAUNCHER_SETTINGS_DIR>/../lib/Python/${PYTHON_SITE_PACKAGES_SUBDIR}/numpy/core
+    <APPLAUNCHER_SETTINGS_DIR>/../lib/Python/${PYTHON_SITE_PACKAGES_SUBDIR}/numpy/lib
+    )
+  mark_as_superbuild(
+    VARS ${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED
+    LABELS "LIBRARY_PATHS_LAUNCHER_INSTALLED"
+    )
+
+else()
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()
+

SuperBuild/External_python-pythonqt-requirements.cmake
@@ -1,0 +1,50 @@
+set(proj python-pythonqt-requirements)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES python python-setuptools python-wheel python-pip)
+
+set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
+file(WRITE ${requirements_file} [===[
+packaging==19.0 --hash=sha256:9e1cbf8c12b1f1ce0bb5344b8d7ecf66a6f8a6e91bcb0c84593ed6d3ab5c4ab3
+pyparsing==2.3.1 --hash=sha256:f6c5ef0d7480ad048c054c37632c67fca55299990fff127850181659eea33fc3
+six==1.12.0 --hash=sha256:d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73
+]===])
+
+if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
+  set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
+endif()
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(Slicer_USE_SYSTEM_${proj})
+  foreach(module_name IN ITEMS pyparsing packaging)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
+endif()
+
+if(NOT Slicer_USE_SYSTEM_${proj})
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    LOG_INSTALL 1
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+
+  ExternalProject_GenerateProjectDescription_Step(${proj}
+    VERSION ${_version}
+    )
+
+else()
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()

SuperBuild/External_python-requests-requirements.cmake
@@ -1,0 +1,53 @@
+set(proj python-requests-requirements)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES python python-setuptools python-pip)
+
+set(requirements_file ${CMAKE_BINARY_DIR}/${proj}-requirements.txt)
+file(WRITE ${requirements_file} [===[
+certifi==2018.11.29 --hash=sha256:993f830721089fef441cdfeb4b2c8c9df86f0c63239f06bd025a76a7daddb033
+idna==2.8 --hash=sha256:ea8b7f6188e6fa117537c3df7da9fc686d485087abf6ac197f9c46432f7e4a3c
+chardet==3.0.4 --hash=sha256:fc323ffcaeaed0e0a02bf4d117757b98aed530d9ed4531e3e15460124c106691
+urllib3==1.24.1 --hash=sha256:61bf29cada3fc2fbefad4fdf059ea4bd1b4a86d2b6d15e1c7c0b582b9752fe39
+requests==2.21.0 --hash=sha256:7bf2a778576d825600030a110f3c0e3e8edc51dfaafe1c146e39a2027784957b
+]===])
+
+if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
+  set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
+endif()
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(Slicer_USE_SYSTEM_${proj})
+  foreach(module_name IN ITEMS certifi idna chardet urllib3 requests)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
+endif()
+
+if(NOT Slicer_USE_SYSTEM_${proj})
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install --require-hashes -r ${requirements_file}
+    LOG_INSTALL 1
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+
+  ExternalProject_GenerateProjectDescription_Step(${proj}
+    VERSION ${_version}
+    )
+
+else()
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()
+

SuperBuild/External_python-scipy.cmake
@@ -1,7 +1,7 @@
-set(proj python-chardet)
+set(proj python-scipy)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES python python-setuptools)
+set(${proj}_DEPENDENCIES python python-setuptools python-numpy python-pip)
 
 if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
   set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
@@ -11,26 +11,24 @@ endif()
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
 if(Slicer_USE_SYSTEM_${proj})
-  ExternalProject_FindPythonPackage(
-    MODULE_NAME "chardet"
-    REQUIRED
-    )
+  foreach(module_name IN ITEMS scipy)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
 endif()
 
 if(NOT Slicer_USE_SYSTEM_${proj})
 
-  set(_version "3.0.4")
-
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
-    URL "https://files.pythonhosted.org/packages/fc/bb/a5768c230f9ddb03acc9ef3f0d4a3cf93462473795d18e9535498c8f929d/chardet-${_version}.tar.gz"
-    URL_HASH "SHA256=84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
-    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
+    DOWNLOAD_COMMAND ""
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install scipy==1.3.1 # auto pick platform
     LOG_INSTALL 1
     DEPENDS
       ${${proj}_DEPENDENCIES}
@@ -43,3 +41,4 @@ if(NOT Slicer_USE_SYSTEM_${proj})
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()
+

SuperBuild.cmake
@@ -118,28 +118,25 @@ if(Slicer_BUILD_DICOM_SUPPORT)
 endif()
 
 if(Slicer_BUILD_DICOM_SUPPORT AND Slicer_USE_PYTHONQT_WITH_OPENSSL)
-  list(APPEND Slicer_DEPENDENCIES python-pydicom1)
-  list(APPEND Slicer_DEPENDENCIES python-pydicom)
-  list(APPEND Slicer_DEPENDENCIES python-dicomweb-client)
+  list(APPEND Slicer_DEPENDENCIES python-dicom-requirements)
 endif()
 
 if(Slicer_USE_PYTHONQT)
   list(APPEND Slicer_DEPENDENCIES
-    python-packaging # This package provides the "packaging.version.parse()" function
+    python-pythonqt-requirements  # This provides the "packaging.version.parse()" function
     python-pip
     )
 endif()
 
 if(Slicer_USE_PYTHONQT AND Slicer_BUILD_EXTENSIONMANAGER_SUPPORT)
   list(APPEND Slicer_DEPENDENCIES
-    python-chardet
-    python-couchdb
-    python-GitPython
-    python-six
+    python-extension-manager-requirements
     )
   if(Slicer_USE_PYTHONQT_WITH_OPENSSL OR Slicer_USE_SYSTEM_python)
     # python-PyGithub requires SSL support in Python
-    list(APPEND Slicer_DEPENDENCIES python-PyGithub)
+    list(APPEND Slicer_DEPENDENCIES
+      python-extension-manager-ssl-requirements
+      )
   else()
     message(STATUS "--------------------------------------------------")
     message(STATUS "Python was built without SSL support; "
@@ -159,7 +156,7 @@ if(Slicer_USE_PYTHONQT)
 endif()
 
 if(Slicer_USE_NUMPY)
-  list(APPEND Slicer_DEPENDENCIES NUMPY)
+  list(APPEND Slicer_DEPENDENCIES python-numpy)
 endif()
 
 if(Slicer_USE_PYTHONQT_WITH_TCL AND UNIX)

commit 1e4ddea014240dc2e469f9b55659ff93daeb3631

COMP: Fix description generation warnings for Python external projects
This commit addresses warnings related to description generation for
Python external projects and fixes a regression introduced in commit
f348d6f5b (COMP: Install python packages using PyPI wheels), which
updated external projects to install wheels instead of installing
packages from source.


It avoids the following warnings that occur when the function
"ExternalProject_GenerateProjectDescription_Step" attempts to extract
version and license information from an external project's source tree.

```
Generate version-python-pip.txt and license-python-pip.txt
No configure step for 'python-pip'
fatal: not a git repository (or any of the parent directories): .git
CMake Warning (dev) at /path/to/Slicer-Release/CMakeFiles/python-pip-generate-project-description.cmake:69 (message):
  python-pip: Could not find a license file
This warning is for project developers.  Use -Wno-dev to suppress it.
```

SuperBuild/External_python-dicom-requirements.cmake
@@ -88,10 +88,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()

SuperBuild/External_python-ensurepip.cmake
@@ -42,10 +42,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()

SuperBuild/External_python-extension-manager-requirements.cmake
@@ -66,10 +66,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()

SuperBuild/External_python-extension-manager-ssl-requirements.cmake
@@ -144,10 +144,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()

SuperBuild/External_python-numpy.cmake
@@ -59,10 +59,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
   #-----------------------------------------------------------------------------
   # Sanity checks
 

SuperBuild/External_python-pip.cmake
@@ -44,10 +44,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()

SuperBuild/External_python-pythonqt-requirements.cmake
@@ -55,10 +55,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()

SuperBuild/External_python-requests-requirements.cmake
@@ -74,10 +74,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()

SuperBuild/External_python-scipy.cmake
@@ -59,10 +59,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()

SuperBuild/External_python-setuptools.cmake
@@ -43,10 +43,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()

SuperBuild/External_python-wheel.cmake
@@ -44,10 +44,6 @@ if(NOT Slicer_USE_SYSTEM_${proj})
       ${${proj}_DEPENDENCIES}
     )
 
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${_version}
-    )
-
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
 endif()