Slicer/Slicer

commit 39c5a0877bbdbb449ae6468e7aa12204489431e1

ENH: Set MRML interactor style camera node observing displayable manager

Libs/MRML/DisplayableManager/vtkMRMLViewInteractorStyle.cxx
@@ -38,12 +38,29 @@ vtkMRMLViewInteractorStyle::vtkMRMLViewInteractorStyle()
 {
   this->EventCallbackCommand->SetCallback(vtkMRMLViewInteractorStyle::CustomProcessEvents);
 
+  this->DisplayableManagerCallbackCommand = vtkCallbackCommand::New();
+  this->DisplayableManagerCallbackCommand->SetClientData(this);
+  this->DisplayableManagerCallbackCommand->SetCallback(vtkMRMLViewInteractorStyle::DisplayableManagerCallback);
+
   this->FocusedDisplayableManager = nullptr;
   this->MouseMovedSinceButtonDown = false;
 }
 
 //----------------------------------------------------------------------------
-vtkMRMLViewInteractorStyle::~vtkMRMLViewInteractorStyle() = default;
+vtkMRMLViewInteractorStyle::~vtkMRMLViewInteractorStyle()
+{
+  if (this->DisplayableManagers)
+    {
+    int numberOfDisplayableManagers = this->DisplayableManagers->GetDisplayableManagerCount();
+    for (int displayableManagerIndex = 0; displayableManagerIndex < numberOfDisplayableManagers; ++displayableManagerIndex)
+      {
+      vtkMRMLAbstractDisplayableManager* displayableManager =
+          this->DisplayableManagers->GetNthDisplayableManager(displayableManagerIndex);
+      displayableManager->RemoveObserver(this->DisplayableManagerCallbackCommand);
+      }
+    }
+  this->DisplayableManagerCallbackCommand->Delete();
+}
 
 //----------------------------------------------------------------------------
 void vtkMRMLViewInteractorStyle::PrintSelf(ostream& os, vtkIndent indent)
@@ -459,4 +476,18 @@ void vtkMRMLViewInteractorStyle::CustomProcessEvents(vtkObject* object,
 void vtkMRMLViewInteractorStyle::SetInteractor(vtkRenderWindowInteractor *interactor)
 {
   this->Superclass::SetInteractor(interactor);
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLViewInteractorStyle::DisplayableManagerCallback(vtkObject *object, unsigned long event, void *clientData, void *callData)
+{
+  vtkMRMLViewInteractorStyle* self = reinterpret_cast<vtkMRMLViewInteractorStyle *>(clientData);
+  assert(!object->IsA("vtkMRMLAbstractDisplayableManager"));
+  self->ProcessDisplayableManagerEvents(vtkMRMLAbstractDisplayableManager::SafeDownCast(object), event, callData);
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLViewInteractorStyle::ProcessDisplayableManagerEvents(
+    vtkMRMLAbstractDisplayableManager * vtkNotUsed(displayableManager), unsigned long vtkNotUsed(event), void *vtkNotUsed(callData))
+{
 }

Libs/MRML/DisplayableManager/vtkMRMLCameraDisplayableManager.cxx
@@ -253,7 +253,6 @@ void vtkMRMLCameraDisplayableManager::SetAndObserveCameraNode(vtkMRMLCameraNode 
     this->Internal->CameraNode, newCameraNode, cameraNodeEvents.GetPointer());
 
   this->SetCameraToRenderer();
-  this->SetCameraToInteractor();
   this->InvokeEvent(vtkMRMLCameraDisplayableManager::ActiveCameraChangedEvent, newCameraNode);
   vtkMRMLViewNode *viewNode = this->GetMRMLViewNode();
   if (viewNode)
@@ -342,22 +341,6 @@ void vtkMRMLCameraDisplayableManager::SetCameraToRenderer()
     this->GetRenderer()->ResetCameraClippingRange();
     // Default to ParallelProjection Off
     //camera->ParallelProjectionOff();
-    }
-}
-
-//---------------------------------------------------------------------------
-void vtkMRMLCameraDisplayableManager::SetCameraToInteractor()
-{
-  if (!this->GetInteractor())
-    {
-    return;
-    }
-  vtkInteractorObserver *iobs = this->GetInteractor()->GetInteractorStyle();
-  vtkMRMLThreeDViewInteractorStyle *istyle =
-    vtkMRMLThreeDViewInteractorStyle::SafeDownCast(iobs);
-  if (istyle)
-    {
-    istyle->SetCameraNode(this->Internal->CameraNode);
     }
 }
 

Libs/MRML/DisplayableManager/vtkMRMLCameraDisplayableManager.h
@@ -78,7 +78,6 @@ protected:
   void SetAndObserveCameraNode(vtkMRMLCameraNode * newCameraNode);
   void AdditionalInitializeStep() override;
   void SetCameraToRenderer();
-  void SetCameraToInteractor();
 
 private:
 

Libs/MRML/DisplayableManager/vtkMRMLThreeDViewInteractorStyle.cxx
@@ -15,12 +15,15 @@
 #include "vtkMRMLThreeDViewInteractorStyle.h"
 
 // MRML includes
+#include "vtkMRMLCameraDisplayableManager.h"
 #include "vtkMRMLCrosshairDisplayableManager.h"
 #include "vtkMRMLCrosshairNode.h"
+#include "vtkMRMLDisplayableManagerGroup.h"
 #include "vtkMRMLInteractionEventData.h"
 #include "vtkMRMLScene.h"
 
 // VTK includes
+#include <vtkCallbackCommand.h>
 #include <vtkCamera.h>
 #include <vtkCellPicker.h>
 #include <vtkEvent.h>
@@ -56,6 +59,33 @@ vtkMRMLThreeDViewInteractorStyle::~vtkMRMLThreeDViewInteractorStyle()
 void vtkMRMLThreeDViewInteractorStyle::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLThreeDViewInteractorStyle::SetDisplayableManagers(vtkMRMLDisplayableManagerGroup* displayableManagers)
+{
+  if (displayableManagers == this->DisplayableManagers)
+    {
+    return;
+    }
+
+  this->Superclass::SetDisplayableManagers(displayableManagers);
+
+  // Observe ActiveCameraChangedEvent
+  vtkMRMLAbstractDisplayableManager* cameraDisplayableManager =
+      this->DisplayableManagers->GetDisplayableManagerByClassName("vtkMRMLCameraDisplayableManager");
+  if (cameraDisplayableManager)
+    {
+    // Listen for ActiveCameraChangedEvent to detect when the camera displayable manager
+    // sets its camera node.
+    // See vtkMRMLThreeDViewInteractorStyle::ProcessDisplayableManagerEvents for details on how the event
+    // is handled.
+    // A simpler approach would be to directly store a reference to the camera in the view node.
+    // See https://github.com/Slicer/Slicer/issues/7333
+    cameraDisplayableManager->AddObserver(
+          vtkMRMLCameraDisplayableManager::ActiveCameraChangedEvent,
+          this->DisplayableManagerCallbackCommand);
+    }
 }
 
 //----------------------------------------------------------------------------
@@ -183,4 +213,21 @@ bool vtkMRMLThreeDViewInteractorStyle::QuickPick(int x, int y, double pickPoint[
     }
 
   return true;
+}
+
+//---------------------------------------------------------------------------
+void vtkMRMLThreeDViewInteractorStyle::ProcessDisplayableManagerEvents(vtkMRMLAbstractDisplayableManager * displayableManager,
+                                     unsigned long event, void *callData)
+{
+  this->Superclass::ProcessDisplayableManagerEvents(displayableManager, event, callData);
+
+  if (vtkMRMLCameraDisplayableManager::SafeDownCast(displayableManager))
+    {
+    vtkMRMLCameraDisplayableManager* cameraDisplayableManager =
+      vtkMRMLCameraDisplayableManager::SafeDownCast(displayableManager);
+    if (event == vtkMRMLCameraDisplayableManager::ActiveCameraChangedEvent)
+      {
+      this->SetCameraNode(cameraDisplayableManager->GetCameraNode());
+      }
+    }
 }

Libs/MRML/DisplayableManager/vtkMRMLThreeDViewInteractorStyle.h
@@ -56,6 +56,8 @@ public:
   vtkTypeMacro(vtkMRMLThreeDViewInteractorStyle, vtkMRMLViewInteractorStyle);
   void PrintSelf(ostream& os, vtkIndent indent) override;
 
+  void SetDisplayableManagers(vtkMRMLDisplayableManagerGroup* displayableManagers) override;
+
   ///
   /// Event bindings controlling the effects of pressing mouse buttons
   /// or moving the mouse.
@@ -80,6 +82,9 @@ protected:
   ~vtkMRMLThreeDViewInteractorStyle() override;
 
   bool QuickPick(int x, int y, double pickPoint[3]);
+
+  void ProcessDisplayableManagerEvents(vtkMRMLAbstractDisplayableManager * displayableManager,
+                                       unsigned long event, void *callData) override;
 
   vtkMRMLCameraNode *CameraNode;
 

Libs/MRML/DisplayableManager/vtkMRMLViewDisplayableManager.cxx
@@ -649,9 +649,12 @@ void vtkMRMLViewDisplayableManager::Create()
               "vtkMRMLCameraDisplayableManager"));
   assert(cameraDisplayableManager);
 
-  // Listen for ActiveCameraChangedEvent
-  // \tbd active camera should be set to view node instead and only observing
-  //  view node should be necessary.
+  // Listen for ActiveCameraChangedEvent to detect when the camera displayable manager
+  // sets its camera node.
+  // See vtkMRMLViewDisplayableManager::ProcessWidgetsEvents for details on how the event
+  // is handled.
+  // A simpler approach would be to directly store a reference to the camera in the view node.
+  // See https://github.com/Slicer/Slicer/issues/7333
   cameraDisplayableManager->AddObserver(vtkMRMLCameraDisplayableManager::ActiveCameraChangedEvent,
                                         this->GetWidgetsCallbackCommand());
 

Libs/MRML/DisplayableManager/vtkMRMLViewInteractorStyle.h
@@ -75,7 +75,7 @@ public:
   void OnExpose() override;
   void OnConfigure() override;
 
-  void SetDisplayableManagers(vtkMRMLDisplayableManagerGroup* displayableManagers);
+  virtual void SetDisplayableManagers(vtkMRMLDisplayableManagerGroup* displayableManagers);
 
   /// Give a chance to displayable managers to process the event.
   /// It just creates vtkMRMLInteractionEventData and calls
@@ -102,6 +102,11 @@ protected:
   ~vtkMRMLViewInteractorStyle() override;
 
   static void CustomProcessEvents(vtkObject* object, unsigned long event, void* clientdata, void* calldata);
+
+  vtkCallbackCommand* DisplayableManagerCallbackCommand;
+  static void DisplayableManagerCallback(vtkObject *object, unsigned long event, void *clientData, void *callData);
+  virtual void ProcessDisplayableManagerEvents(vtkMRMLAbstractDisplayableManager * displayableManager,
+                                               unsigned long event, void *callData);
 
   void SetMouseCursor(int cursor);
 

commit 0203cff2af9b11323523bc639800e3a597bcc3e9

BUG: Fix crash for debug build due to invalid assert in MRMLViewInteractorStyle
Fix regression introduced in 39c5a0877 (ENH: Set MRML interactor style camera node observing
displayable manager )

Libs/MRML/DisplayableManager/vtkMRMLViewInteractorStyle.cxx
@@ -482,7 +482,7 @@ void vtkMRMLViewInteractorStyle::SetInteractor(vtkRenderWindowInteractor *intera
 void vtkMRMLViewInteractorStyle::DisplayableManagerCallback(vtkObject *object, unsigned long event, void *clientData, void *callData)
 {
   vtkMRMLViewInteractorStyle* self = reinterpret_cast<vtkMRMLViewInteractorStyle *>(clientData);
-  assert(!object->IsA("vtkMRMLAbstractDisplayableManager"));
+  assert(object->IsA("vtkMRMLAbstractDisplayableManager"));
   self->ProcessDisplayableManagerEvents(vtkMRMLAbstractDisplayableManager::SafeDownCast(object), event, callData);
 }
 