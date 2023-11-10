83cd71e84eab0b4707bb042973d8f33e141f5a63
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2023-11-10 10:39:14 -0500

    ENH: Add test for the `slicer.util.loadFiberBundle` method
    Add test for the `slicer.util.loadFiberBundle` method.

---
+++ Base/Python/slicer/tests/test_slicer_util_load_data.py
@@ -0,0 +1,42 @@
+import tempfile
+import unittest
+from pathlib import Path
+from urllib.parse import urljoin
+
+import slicer
+
+
+class SlicerUtilLoadDataTests(unittest.TestCase):
+
+    def setUp(self):
+
+        pass
+
+
+    def test_loadFiberBundle(self):
+
+        def _load(fiberBundleFilePath):
+
+            success, node = slicer.util.loadFiberBundle(fiberBundleFilePath, returnNode=True)
+            self.assertTrue(success)
+            self.assertIsInstance(node, slicer.vtkMRMLFiberBundleNode)
+            node = slicer.util.loadFiberBundle(fiberBundleFilePath)
+            self.assertIsInstance(node, slicer.vtkMRMLFiberBundleNode)
+
+        hash_algorithm = "SHA256"
+        checksum = "e1848e4d0b90d6c7442b2474c513d0464c6f141253c25aee6f5bcda4c828deac"
+        url = urljoin(slicer.util.TESTING_DATA_URL, f"{hash_algorithm}/{checksum}")
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+
+            file_basename = "tractography.vtk"
+            fiberBundleFilePath = Path(tmpdirname).joinpath(file_basename)
+            downloadSuccess = slicer.util.downloadFile(
+                url, fiberBundleFilePath, checksum=f"{hash_algorithm}:{checksum}", reDownloadIfChecksumInvalid=True)
+            self.assertTrue(downloadSuccess)
+
+            if slicer.app.ioManager().registeredFileReaderCount("FiberBundleFile") == 0:
+                with self.assertRaisesRegex(RuntimeError, "FiberBundleFile reader not registered") as cm:
+                    _load(fiberBundleFilePath)
+            else:
+                _load(fiberBundleFilePath)
