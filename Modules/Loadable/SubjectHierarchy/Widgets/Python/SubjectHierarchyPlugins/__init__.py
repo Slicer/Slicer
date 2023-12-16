Slicer/Slicer

commit 1d77e473abebc3b554d886614e2958adff34e38e

BUG: Fix language translation of subject hierarchy plugins
Translation context is the Python package name (parent folder name that contains __init__.py).
Subject hierarchy abstract plugin was moved to a different folder during installation, therefore the context that was determined in the source tree did not work in the install tree. It is fixed by using the same folder structure in the source and install tree.
Where modules specified their plugins, __init__.py was missing and therefore the context name was not correct (it was the parent folder of SubjectHierarchyPlugins). It is fixed by adding the missing __init__.py file.

see https://github.com/Slicer/SlicerLanguagePacks/issues/21

fixes https://github.com/Slicer/SlicerLanguagePacks/issues/52

Co-authored-by: Andras Lasso <lasso@queensu.ca>

Modules/Loadable/SubjectHierarchy/Widgets/Python/CMakeLists.txt
@@ -2,9 +2,10 @@
 # imports all deployed scripted plugins deployed by modules
 set(SubjectHierarchyWidgets_PYTHON_SCRIPTS
   __init__
-  AbstractScriptedSubjectHierarchyPlugin
-  parameterNodeWrapper/__init__.py
-  parameterNodeWrapper/guiConnectors.py
+  SubjectHierarchyPlugins/__init__
+  SubjectHierarchyPlugins/AbstractScriptedSubjectHierarchyPlugin
+  parameterNodeWrapper/__init__
+  parameterNodeWrapper/guiConnectors
   )
 
 set(SubjectHierarchyWidgets_PYTHON_RESOURCES

Modules/Loadable/SubjectHierarchy/Widgets/Python/SubjectHierarchyPlugins/__init__.py
@@ -1,0 +1,1 @@
+"""This package contains plugins for Subject Hierarchy implemented in Python."""

Modules/Scripted/SegmentEditor/SubjectHierarchyPlugins/__init__.py
@@ -1,0 +1,1 @@
+"""This package contains plugins for Subject Hierarchy implemented in Python."""

Modules/Scripted/SegmentStatistics/SubjectHierarchyPlugins/__init__.py
@@ -1,0 +1,1 @@
+"""This package contains plugins for Subject Hierarchy implemented in Python."""

Modules/Loadable/SubjectHierarchy/Widgets/Python/SubjectHierarchyPlugins/AbstractScriptedSubjectHierarchyPlugin.py
@@ -24,7 +24,8 @@ class AbstractScriptedSubjectHierarchyPlugin:
     scriptedPlugin.setPythonSource(VolumeClipSubjectHierarchyPlugin.filePath)
     ...
 
-    Example can be found here: https://slicer.readthedocs.io/en/latest/developer_guide/script_repository.html#subject-hierarchy-plugin-offering-view-context-menu-action
+    Example can be found here:
+    https://slicer.readthedocs.io/en/latest/developer_guide/script_repository.html#subject-hierarchy-plugin-offering-view-context-menu-action
     """
 
     def __init__(self, scriptedPlugin):

commit 8d0e12c631b9b1bfce17538874f7c8c2f437faba

BUG: Fix loading of SubjectHierarchyPlugins at startup
This commit addresses the incorrect loading of SubjectHierarchyPlugins during
startup, as well as the regression identified in the py_SubjectHierarchyCorePluginsSelfTest.
Both issues were introduced in in 1d77e473a (BUG: Fix language translation of
subject hierarchy plugins).

To resolve these issues, it updates the folder structure to align with the
changes introduced in fc85b2f05 (BUG: Make Segment Editor effect names translatable).

This structural adjustment ensures proper loading of SubjectHierarchyPlugins.

Modules/Loadable/SubjectHierarchy/Widgets/Python/CMakeLists.txt
@@ -1,21 +1,6 @@
-# Deploy abstract scripted plugin and init file that
-# imports all deployed scripted plugins deployed by modules
-set(SubjectHierarchyWidgets_PYTHON_SCRIPTS
-  __init__
-  SubjectHierarchyPlugins/__init__
-  SubjectHierarchyPlugins/AbstractScriptedSubjectHierarchyPlugin
-  parameterNodeWrapper/__init__
-  parameterNodeWrapper/guiConnectors
-  )
+# Note: The SubjectHierarchyPlugins subdirectory is needed to have a Python package
+# (https://docs.python.org/3/tutorial/modules.html#packages) in the source tree
+# the same way as it is in the install tree. The Python package name is used
+# for determining the "context" string (packageName.filename) for language translation (i18n).
 
-set(SubjectHierarchyWidgets_PYTHON_RESOURCES
-  )
-
-ctkMacroCompilePythonScript(
-  TARGET_NAME SubjectHierarchyWidgets
-  SCRIPTS "${SubjectHierarchyWidgets_PYTHON_SCRIPTS}"
-  RESOURCES "${SubjectHierarchyWidgets_PYTHON_RESOURCES}"
-  DESTINATION_DIR ${Slicer_BINARY_DIR}/${Slicer_QTSCRIPTEDMODULES_LIB_DIR}/SubjectHierarchyPlugins
-  INSTALL_DIR ${Slicer_INSTALL_QTSCRIPTEDMODULES_LIB_DIR}/SubjectHierarchyPlugins
-  NO_INSTALL_SUBDIR
-  )
+add_subdirectory(SubjectHierarchyPlugins)

Modules/Loadable/SubjectHierarchy/Widgets/Python/SubjectHierarchyPlugins/CMakeLists.txt
@@ -1,0 +1,21 @@
+
+# Deploy abstract scripted plugin and init file that
+# imports all deployed scripted plugins deployed by modules
+set(SubjectHierarchyPlugins_PYTHON_SCRIPTS
+  __init__
+  AbstractScriptedSubjectHierarchyPlugin
+  parameterNodeWrapper/__init__
+  parameterNodeWrapper/guiConnectors
+  )
+
+set(SubjectHierarchyPlugins_PYTHON_RESOURCES
+  )
+
+ctkMacroCompilePythonScript(
+  TARGET_NAME ${MODULE_NAME}SubjectHierarchyPlugins
+  SCRIPTS "${SubjectHierarchyPlugins_PYTHON_SCRIPTS}"
+  RESOURCES "${SubjectHierarchyPlugins_PYTHON_RESOURCES}"
+  DESTINATION_DIR ${Slicer_BINARY_DIR}/${Slicer_QTSCRIPTEDMODULES_LIB_DIR}/SubjectHierarchyPlugins
+  INSTALL_DIR ${Slicer_INSTALL_QTSCRIPTEDMODULES_LIB_DIR}/SubjectHierarchyPlugins
+  NO_INSTALL_SUBDIR
+  )

Modules/Loadable/SubjectHierarchy/Widgets/Python/SubjectHierarchyPlugins/__init__.py
@@ -1,1 +1,20 @@
 """This package contains plugins for Subject Hierarchy implemented in Python."""
+
+import os
+import traceback
+import logging
+import sys
+
+from . import parameterNodeWrapper  # Required to ensure parameterNodeWrapper plugins are registered
+
+currentDir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(currentDir)
+for fileName in os.listdir(currentDir):
+    fileNameNoExtension = os.path.splitext(fileName)[0]
+    fileExtension = os.path.splitext(fileName)[1]
+    if fileExtension == ".py" and fileNameNoExtension != "__init__":
+        importStr = f"from {fileNameNoExtension} import *"
+        try:
+            exec(importStr)
+        except Exception as e:
+            logging.error("Failed to import " + fileNameNoExtension + ": " + traceback.format_exc())

Modules/Loadable/SubjectHierarchy/Widgets/Python/__init__.py
@@ -1,18 +1,0 @@
-import os
-import traceback
-import logging
-import sys
-
-from . import parameterNodeWrapper  # Required to ensure parameterNodeWrapper plugins are registered
-
-currentDir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(currentDir)
-for fileName in os.listdir(currentDir):
-    fileNameNoExtension = os.path.splitext(fileName)[0]
-    fileExtension = os.path.splitext(fileName)[1]
-    if fileExtension == ".py" and fileNameNoExtension != "__init__":
-        importStr = "from " + fileNameNoExtension + " import *"
-        try:
-            exec(importStr)
-        except Exception as e:
-            logging.error("Failed to import " + fileNameNoExtension + ": " + traceback.format_exc())
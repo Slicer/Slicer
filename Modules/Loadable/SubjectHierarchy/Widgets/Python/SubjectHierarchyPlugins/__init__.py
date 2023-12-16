8d0e12c631b9b1bfce17538874f7c8c2f437faba
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-12-15 22:58:43 -0500

    BUG: Fix loading of SubjectHierarchyPlugins at startup
    This commit addresses the incorrect loading of SubjectHierarchyPlugins during
    startup, as well as the regression identified in the py_SubjectHierarchyCorePluginsSelfTest.
    Both issues were introduced in in 1d77e473a (BUG: Fix language translation of
    subject hierarchy plugins).
    
    To resolve these issues, it updates the folder structure to align with the
    changes introduced in fc85b2f05 (BUG: Make Segment Editor effect names translatable).
    
    This structural adjustment ensures proper loading of SubjectHierarchyPlugins.

--- Modules/Loadable/SubjectHierarchy/Widgets/Python/SubjectHierarchyPlugins/__init__.py
+++ Modules/Loadable/SubjectHierarchy/Widgets/Python/SubjectHierarchyPlugins/__init__.py
@@ -1,1 +1,20 @@
 """This package contains plugins for Subject Hierarchy implemented in Python."""
+
+import os
+import traceback
+import logging
+import sys
+
+from . import parameterNodeWrapper  # Required to ensure parameterNodeWrapper plugins are registered
+
+currentDir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(currentDir)
+for fileName in os.listdir(currentDir):
+    fileNameNoExtension = os.path.splitext(fileName)[0]
+    fileExtension = os.path.splitext(fileName)[1]
+    if fileExtension == ".py" and fileNameNoExtension != "__init__":
+        importStr = f"from {fileNameNoExtension} import *"
+        try:
+            exec(importStr)
+        except Exception as e:
+            logging.error("Failed to import " + fileNameNoExtension + ": " + traceback.format_exc())
