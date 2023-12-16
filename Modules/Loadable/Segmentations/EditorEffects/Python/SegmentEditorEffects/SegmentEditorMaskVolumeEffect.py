Slicer/Slicer

commit 238eeb1f669ac038733c7c7786245ea7841644da

ENH: Add node reference convenience functions to segment editor effects
Segment Editor effects had convenience functions for setting string and numeric parameters, but not for node references.

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

Modules/Loadable/Segmentations/EditorEffects/Python/SegmentEditorEffects/SegmentEditorMaskVolumeEffect.py
@@ -210,9 +210,9 @@ Fill inside and outside operation creates a binary labelmap volume as output, wi
         self.softEdgeMmSpinBox.setValue(float(self.scriptedEffect.parameter("SoftEdgeMm"))
                                         if self.scriptedEffect.parameter("SoftEdgeMm") else 0)
 
-        inputVolume = self.scriptedEffect.parameterSetNode().GetNodeReference("Mask volume.InputVolume")
+        inputVolume = self.scriptedEffect.nodeReference("InputVolume")
         self.inputVolumeSelector.setCurrentNode(inputVolume)
-        outputVolume = self.scriptedEffect.parameterSetNode().GetNodeReference("Mask volume.OutputVolume")
+        outputVolume = self.scriptedEffect.nodeReference("OutputVolume")
         self.outputVolumeSelector.setCurrentNode(outputVolume)
 
         sourceVolume = self.scriptedEffect.parameterSetNode().GetSourceVolumeNode()
@@ -245,8 +245,8 @@ Fill inside and outside operation creates a binary labelmap volume as output, wi
         self.scriptedEffect.setParameter("FillValue", self.fillValueEdit.value)
         self.scriptedEffect.setParameter("BinaryMaskFillValueInside", self.binaryMaskFillInsideEdit.value)
         self.scriptedEffect.setParameter("BinaryMaskFillValueOutside", self.binaryMaskFillOutsideEdit.value)
-        self.scriptedEffect.parameterSetNode().SetNodeReferenceID("Mask volume.InputVolume", self.inputVolumeSelector.currentNodeID)
-        self.scriptedEffect.parameterSetNode().SetNodeReferenceID("Mask volume.OutputVolume", self.outputVolumeSelector.currentNodeID)
+        self.scriptedEffect.setNodeReference("InputVolume", self.inputVolumeSelector.currentNode)
+        self.scriptedEffect.setNodeReference("OutputVolume", self.outputVolumeSelector.currentNode)
         self.scriptedEffect.setParameter("SoftEdgeMm", self.softEdgeMmSpinBox.value)
 
     def activate(self):
@@ -272,7 +272,7 @@ Fill inside and outside operation creates a binary labelmap volume as output, wi
         return inputVolume
 
     def onInputVisibilityButtonClicked(self):
-        inputVolume = self.scriptedEffect.parameterSetNode().GetNodeReference("Mask volume.InputVolume")
+        inputVolume = self.scriptedEffect.nodeReference("InputVolume")
         sourceVolume = self.scriptedEffect.parameterSetNode().GetSourceVolumeNode()
         if inputVolume is None:
             inputVolume = sourceVolume
@@ -281,17 +281,17 @@ Fill inside and outside operation creates a binary labelmap volume as output, wi
             self.updateGUIFromMRML()
 
     def onOutputVisibilityButtonClicked(self):
-        outputVolume = self.scriptedEffect.parameterSetNode().GetNodeReference("Mask volume.OutputVolume")
+        outputVolume = self.scriptedEffect.nodeReference("OutputVolume")
         if outputVolume:
             slicer.util.setSliceViewerLayers(background=outputVolume)
             self.updateGUIFromMRML()
 
     def onInputVolumeChanged(self):
-        self.scriptedEffect.parameterSetNode().SetNodeReferenceID("Mask volume.InputVolume", self.inputVolumeSelector.currentNodeID)
+        self.scriptedEffect.setNodeReference("InputVolume", self.inputVolumeSelector.currentNode)
         self.updateGUIFromMRML()  # node reference changes are not observed, update GUI manually
 
     def onOutputVolumeChanged(self):
-        self.scriptedEffect.parameterSetNode().SetNodeReferenceID("Mask volume.OutputVolume", self.outputVolumeSelector.currentNodeID)
+        self.scriptedEffect.setNodeReference("OutputVolume", self.outputVolumeSelector.currentNode)
         self.updateGUIFromMRML()  # node reference changes are not observed, update GUI manually
 
     def fillValueChanged(self):

Modules/Loadable/Segmentations/EditorEffects/Python/SegmentEditorEffects/AbstractScriptedSegmentEditorAutoCompleteEffect.py
@@ -232,7 +232,7 @@ class AbstractScriptedSegmentEditorAutoCompleteEffect(AbstractScriptedSegmentEdi
                 self.segmentationNodeObserverTags.append(self.observedSegmentation.AddObserver(eventId, self.onSegmentationModified))
 
     def getPreviewNode(self):
-        previewNode = self.scriptedEffect.parameterSetNode().GetNodeReference(ResultPreviewNodeReferenceRole)
+        previewNode = self.scriptedEffect.nodeReference(ResultPreviewNodeReferenceRole)
         if (previewNode
                 and self.scriptedEffect.parameterDefined("SegmentationResultPreviewOwnerEffect")
                 and self.scriptedEffect.parameter("SegmentationResultPreviewOwnerEffect") != self.scriptedEffect.name):
@@ -293,9 +293,9 @@ class AbstractScriptedSegmentEditorAutoCompleteEffect(AbstractScriptedSegmentEdi
     def reset(self):
         self.delayedAutoUpdateTimer.stop()
         self.observeSegmentation(False)
-        previewNode = self.scriptedEffect.parameterSetNode().GetNodeReference(ResultPreviewNodeReferenceRole)
+        previewNode = self.scriptedEffect.nodeReference(ResultPreviewNodeReferenceRole)
         if previewNode:
-            self.scriptedEffect.parameterSetNode().SetNodeReferenceID(ResultPreviewNodeReferenceRole, None)
+            self.scriptedEffect.setCommonNodeReference(ResultPreviewNodeReferenceRole, None)
             slicer.mrmlScene.RemoveNode(previewNode)
             self.scriptedEffect.setCommonParameter("SegmentationResultPreviewOwnerEffect", "")
         segmentationNode = self.scriptedEffect.parameterSetNode().GetSegmentationNode()
@@ -499,7 +499,7 @@ class AbstractScriptedSegmentEditorAutoCompleteEffect(AbstractScriptedSegmentEdi
             previewNode.GetDisplayNode().SetVisibility2DOutline(False)
             if segmentationNode.GetParentTransformNode():
                 previewNode.SetAndObserveTransformNodeID(segmentationNode.GetParentTransformNode().GetID())
-            self.scriptedEffect.parameterSetNode().SetNodeReferenceID(ResultPreviewNodeReferenceRole, previewNode.GetID())
+            self.scriptedEffect.setCommonNodeReference(ResultPreviewNodeReferenceRole, previewNode)
             self.scriptedEffect.setCommonParameter("SegmentationResultPreviewOwnerEffect", self.scriptedEffect.name)
             self.setPreviewOpacity(0.6)
 

Modules/Loadable/Segmentations/EditorEffects/qSlicerSegmentEditorAbstractEffect.cxx
@@ -969,6 +969,26 @@ double qSlicerSegmentEditorAbstractEffect::doubleParameter(QString name)
 }
 
 //-----------------------------------------------------------------------------
+vtkMRMLNode* qSlicerSegmentEditorAbstractEffect::nodeReference(QString name)
+{
+  Q_D(qSlicerSegmentEditorAbstractEffect);
+  if (!d->ParameterSetNode)
+    {
+    return nullptr;
+    }
+
+  // Get effect-specific prefixed parameter first
+  QString attributeName = QString("%1.%2").arg(this->name()).arg(name);
+  vtkMRMLNode* node = d->ParameterSetNode->GetNodeReference(attributeName.toUtf8().constData());
+  // Look for common parameter if effect-specific one is not found
+  if (!node)
+    {
+    node = d->ParameterSetNode->GetNodeReference(name.toUtf8().constData());
+    }
+  return node;
+}
+
+//-----------------------------------------------------------------------------
 void qSlicerSegmentEditorAbstractEffect::setParameter(QString name, QString value)
 {
   Q_D(qSlicerSegmentEditorAbstractEffect);
@@ -1224,6 +1244,47 @@ void qSlicerSegmentEditorAbstractEffect::setCommonParameterDefault(QString name,
     return;
     }
   this->setCommonParameter(name, value);
+}
+
+//-----------------------------------------------------------------------------
+void qSlicerSegmentEditorAbstractEffect::setNodeReference(QString name, vtkMRMLNode* node)
+{
+  Q_D(qSlicerSegmentEditorAbstractEffect);
+  if (!d->ParameterSetNode)
+    {
+    qCritical() << Q_FUNC_INFO << ": Invalid segment editor parameter set node set to effect " << this->name();
+    return;
+    }
+
+  // Set parameter as attribute
+  QString attributeName = QString("%1.%2").arg(this->name()).arg(name);
+  this->setCommonNodeReference(attributeName, node);
+}
+
+//-----------------------------------------------------------------------------
+void qSlicerSegmentEditorAbstractEffect::setCommonNodeReference(QString name, vtkMRMLNode* node)
+{
+  Q_D(qSlicerSegmentEditorAbstractEffect);
+  if (!d->ParameterSetNode)
+    {
+    qCritical() << Q_FUNC_INFO << ": Invalid segment editor parameter set node set to effect " << this->name();
+    return;
+    }
+
+  vtkMRMLNode* oldNode = d->ParameterSetNode->GetNodeReference(name.toUtf8().constData());
+  if (node == oldNode)
+    {
+    // no change
+    return;
+    }
+
+  // Set parameter as attribute
+  d->ParameterSetNode->SetNodeReferenceID(name.toUtf8().constData(), node ? node->GetID() : nullptr);
+
+  // Emit parameter modified event
+  // Don't pass parameter name as char pointer, as custom modified events may be compressed and invoked after EndModify()
+  // and by that time the pointer may not be valid anymore.
+  d->ParameterSetNode->InvokeCustomModifiedEvent(vtkMRMLSegmentEditorNode::EffectParameterModified);
 }
 
 //-----------------------------------------------------------------------------

Modules/Loadable/Segmentations/EditorEffects/qSlicerSegmentEditorAbstractEffect.h
@@ -38,6 +38,7 @@ class qSlicerSegmentEditorAbstractEffectPrivate;
 
 class vtkActor2D;
 class vtkMRMLInteractionNode;
+class vtkMRMLNode;
 class vtkMRMLScene;
 class vtkMRMLSegmentEditorNode;
 class vtkMRMLAbstractViewNode;
@@ -288,14 +289,17 @@ public:
 
 // Effect parameter functions
 public:
-  /// Get effect or common parameter from effect parameter set node
+  /// Get effect-specific or common string type parameter from effect parameter set node.
   Q_INVOKABLE QString parameter(QString name);
 
-  /// Convenience function to get integer parameter
+  /// Get effect-specific or common integer type parameter from effect parameter set node.
   Q_INVOKABLE int integerParameter(QString name);
 
-  /// Convenience function to get double parameter
+  /// Get effect-specific or common double type parameter from effect parameter set node.
   Q_INVOKABLE double doubleParameter(QString name);
+
+  /// Get effect-specific or common node reference type parameter from effect parameter set node.
+  Q_INVOKABLE vtkMRMLNode* nodeReference(QString name);
 
   /// Set effect parameter in effect parameter set node. This function is called by both convenience functions.
   /// \param name Parameter name string
@@ -340,6 +344,13 @@ public:
   /// Set parameter only if it is not defined already.
   /// \sa setCommonParameter
   Q_INVOKABLE void setCommonParameterDefault(QString name, double value);
+
+  /// Convenience function to set node reference parameter
+  /// \param name Parameter name string
+  /// \param value Parameter node reference
+  Q_INVOKABLE void setNodeReference(QString name, vtkMRMLNode* node);
+  /// Convenience function to set node reference common parameter \sa setCommonParameter
+  Q_INVOKABLE void setCommonNodeReference(QString name, vtkMRMLNode* node);
 
 // Utility functions
 public:

Modules/Loadable/Segmentations/MRML/vtkMRMLSegmentEditorNode.cxx
@@ -131,7 +131,7 @@ void vtkMRMLSegmentEditorNode::ReadXMLAttributes(const char** atts)
   // For future compatibility (when masterVolumeRef will be replaced by sourceVolumeRef)
   if (this->GetNodeReferenceID("sourceVolumeRef"))
     {
-		this->SetNodeReferenceID(SOURCE_VOLUME_REFERENCE_ROLE, this->GetNodeReferenceID("sourceVolumeRef"));
+    this->SetNodeReferenceID(SOURCE_VOLUME_REFERENCE_ROLE, this->GetNodeReferenceID("sourceVolumeRef"));
     }
 
   this->EndModify(disabledModify);

commit 8ec42bf526a9c90587ddb29a685c45573f730528

BUG: Fix Mask volume effect node selector (#7478)
Fix regression introduced in 238eeb1f6 (`ENH: Add node reference convenience
functions to segment editor effects`)

Modules/Loadable/Segmentations/EditorEffects/Python/SegmentEditorEffects/SegmentEditorMaskVolumeEffect.py
@@ -245,8 +245,8 @@ Fill inside and outside operation creates a binary labelmap volume as output, wi
         self.scriptedEffect.setParameter("FillValue", self.fillValueEdit.value)
         self.scriptedEffect.setParameter("BinaryMaskFillValueInside", self.binaryMaskFillInsideEdit.value)
         self.scriptedEffect.setParameter("BinaryMaskFillValueOutside", self.binaryMaskFillOutsideEdit.value)
-        self.scriptedEffect.setNodeReference("InputVolume", self.inputVolumeSelector.currentNode)
-        self.scriptedEffect.setNodeReference("OutputVolume", self.outputVolumeSelector.currentNode)
+        self.scriptedEffect.setNodeReference("InputVolume", self.inputVolumeSelector.currentNode())
+        self.scriptedEffect.setNodeReference("OutputVolume", self.outputVolumeSelector.currentNode())
         self.scriptedEffect.setParameter("SoftEdgeMm", self.softEdgeMmSpinBox.value)
 
     def activate(self):
@@ -287,11 +287,11 @@ Fill inside and outside operation creates a binary labelmap volume as output, wi
             self.updateGUIFromMRML()
 
     def onInputVolumeChanged(self):
-        self.scriptedEffect.setNodeReference("InputVolume", self.inputVolumeSelector.currentNode)
+        self.scriptedEffect.setNodeReference("InputVolume", self.inputVolumeSelector.currentNode())
         self.updateGUIFromMRML()  # node reference changes are not observed, update GUI manually
 
     def onOutputVolumeChanged(self):
-        self.scriptedEffect.setNodeReference("OutputVolume", self.outputVolumeSelector.currentNode)
+        self.scriptedEffect.setNodeReference("OutputVolume", self.outputVolumeSelector.currentNode())
         self.updateGUIFromMRML()  # node reference changes are not observed, update GUI manually
 
     def fillValueChanged(self):