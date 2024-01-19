246380370d139fd023f3761fa6bf00af25c69cf4
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-01-19 14:49:43 -0500

    BUG: Fix support for registering parameterNodeWrapper plugins (#7480)
    Revisit the support for module-specific parameterNodeWrapper introduced
    in 19f787127 (BUG: Register parameterNodeWrapper plugins in corresponding
    module). The goal is to prevent the installation of existing plugins from
    being overwritten by those provided by other modules.
    
    To address this, the commit organizes each module-specific parameterNodeWrapper
    in a package following the convention "<moduleName>Lib/parameterNodeWrapper"
    consistent with how existing module organizes their python libraries (e.g DICOMLib,
    WebServerLib, ...)
    
    This approach updates the `qSlicerSubjectHierarchyModule::setup` function
    to explicitly import the `SubjectHierarchyLib` package and indirectly ensures
    that the plugins are registered, whether they originate from the Slicer core or
    a Slicer extension.
    
    Co-authored-by: Andras Lasso <lasso@queensu.ca>

---
+++ Modules/Loadable/SubjectHierarchy/SubjectHierarchyLib/__init__.py
@@ -0,0 +1,3 @@
+"""This package contains SubjectHierarchy libraries and plugins."""
+
+from . import parameterNodeWrapper  # Required to ensure parameterNodeWrapper plugins are registered
---
+++ Modules/Loadable/SubjectHierarchy/SubjectHierarchyLib/parameterNodeWrapper/__init__.py
@@ -0,0 +1,3 @@
+"""This package contains parameterNodeWrapper plugins."""
+
+from .guiConnectors import *
