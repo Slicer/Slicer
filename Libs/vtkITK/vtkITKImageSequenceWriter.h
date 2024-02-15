68b51f799bf75a7e4e3f87daf659298bb473fa31
Csaba Pinter <pinter.csaba@gmail.com>
2024-02-15 15:43:59 +0000

    DOC: Update copyright in the image sequence classes

--- Libs/vtkITK/vtkITKImageSequenceWriter.h
+++ Libs/vtkITK/vtkITKImageSequenceWriter.h
@@ -1,14 +1,18 @@
-/*=========================================================================
+/*==========================================================================
 
-  Copyright Brigham and Women's Hospital (BWH) All Rights Reserved.
+  Copyright (c) Ebatinca S.L., Las Palmas de Gran Canaria, Spain
 
   See COPYRIGHT.txt
   or http://www.slicer.org/copyright/copyright.txt for details.
 
-  Program:   vtkITK
-  Module:    $HeadURL$
-  Date:      $Date$
-  Version:   $Revision$
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Csaba Pinter, Ebatinca, funded
+  by the grant GRT-00000485 of Children's Hospital of Philadeplhia, USA.
 
 ==========================================================================*/
 
