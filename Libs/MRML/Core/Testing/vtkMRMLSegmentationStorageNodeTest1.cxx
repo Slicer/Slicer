37d06b584b9b4040de873a0ffda3ed1b591c9368
Andras Lasso <lasso@queensu.ca>
2024-02-29 08:31:18 -0500

    BUG: Fix loading of empty segmentation (#7609)
    NRRD file cannot be created without voxel data, so an 1x1x1 array was used as image data.
    However, when the empty segmentation was loaded then this single-voxel geometry was treated as a valid geometry.
    Fixed by detecting this special case of a single voxel in the segmentation, and considering the geometry invalid in this case.
    
    fixes #7576
    
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

--- Libs/MRML/Core/Testing/vtkMRMLSegmentationStorageNodeTest1.cxx
+++ Libs/MRML/Core/Testing/vtkMRMLSegmentationStorageNodeTest1.cxx
@@ -23,6 +23,7 @@ Care Ontario.
 #include "vtkMRMLScene.h"
 #include "vtkMRMLSegmentationNode.h"
 #include "vtkMRMLSegmentationStorageNode.h"
+#include "vtkOrientedImageData.h"
 #include "vtkSegmentationConverterFactory.h"
 
 // Converter rules
@@ -39,10 +40,10 @@ int vtkMRMLSegmentationStorageNodeTest1(int argc, char * argv[] )
   scene->AddNode(node1.GetPointer());
   EXERCISE_ALL_BASIC_MRML_METHODS(node1.GetPointer());
 
-  if (argc != 4)
+  if (argc != 5)
   {
     std::cerr << "Line " << __LINE__
-              << " - Missing parameters !\n"
+              << " - Missing or extra parameters!\n"
               << "Usage: " << argv[0] << " /path/to/ITKSnapSegmentation.nii.gz /path/to/OldSlicerSegmentation.seg.nrrd /path/to/SlicerSegmentation.seg.nrrd"
               << std::endl;
     return EXIT_FAILURE;
@@ -57,6 +58,7 @@ int vtkMRMLSegmentationStorageNodeTest1(int argc, char * argv[] )
   const char* itkSnapSegmentationFilename = argv[1]; // ITKSnapSegmentation.nii.gz
   const char* oldSlicerSegmentationFilename = argv[2]; // OldSlicerSegmentation.seg.nrrd: Segmentation before shared labelmaps implemented.
   const char* slicerSegmentationFilename = argv[3]; // SlicerSegmentation.seg.nrrd: Segmentation with shared labelmaps.
+  const char* tempDir = argv[4]; // Temporary folder where test segmentation files will be created
 
   // Test segmentation exported from ITK-SNAP
   std::cout << "Testing ITK-SNAP segmentation" << std::endl;
@@ -112,6 +114,45 @@ int vtkMRMLSegmentationStorageNodeTest1(int argc, char * argv[] )
 
     int numberOfLayers = segmentation->GetNumberOfLayers(vtkSegmentationConverter::GetBinaryLabelmapRepresentationName());
     CHECK_INT(numberOfLayers, 2);
+  }
+
+  std::cout << "Testing empty segmentation" << std::endl;
+  {
+    // Create empty segmentation
+    vtkNew<vtkMRMLSegmentationNode> segmentationNode;
+    scene->AddNode(segmentationNode);
+    segmentationNode->GetSegmentation()->AddEmptySegment();
+    segmentationNode->GetSegmentation()->AddEmptySegment();
+    segmentationNode->GetSegmentation()->AddEmptySegment();
+
+    // Write to file
+    vtkNew<vtkMRMLSegmentationStorageNode> segmentationStorageNode;
+    scene->AddNode(segmentationStorageNode);
+    std::string emptySegmentationFilename = std::string(tempDir) + "/EmptySegmentation.seg.nrrd";
+    std::cout << "Write empty segmentation file: " << emptySegmentationFilename;
+    segmentationStorageNode->SetFileName(emptySegmentationFilename.c_str());
+    CHECK_INT(segmentationStorageNode->WriteData(segmentationNode), 1);
+
+    // Read from file
+    vtkNew<vtkMRMLSegmentationNode> segmentationNodeFromFile;
+    scene->AddNode(segmentationNodeFromFile);
+    segmentationStorageNode->ReadData(segmentationNodeFromFile);
+
+    // Check basic content
+    vtkSegmentation* segmentation = segmentationNode->GetSegmentation();
+    CHECK_NOT_NULL(segmentation);
+    int numberOfSegments = segmentation->GetNumberOfSegments();
+    CHECK_INT(numberOfSegments, 3);
+
+    // Check that no valid geometry is found.
+    // The segmentation is stored as a single voxel, which would specify a geometry,
+    // the storage node should ignore that when reading the file (single-voxel volume is a special case).
+    std::string segmentationGeometryString = segmentation->DetermineCommonLabelmapGeometry(
+      vtkSegmentation::EXTENT_UNION_OF_EFFECTIVE_SEGMENTS_AND_REFERENCE_GEOMETRY);
+    CHECK_STD_STRING(segmentationGeometryString, "");
+
+    // Clean up
+    vtksys::SystemTools::RemoveFile(emptySegmentationFilename);
   }
 
   return EXIT_SUCCESS;
