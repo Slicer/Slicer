Slicer/Slicer

commit 524ddb6d6df1de73dd669f601445bb6be27bd5f9

ENH: De-duplicate setting of extension metadata
Anticipating the simplification of the metadata used to describe an extension
Simplifies handling of metadata
catalog entry, this removes the parsing and handling of metadata already defined
in the extension CMakeLists.txt by the extension index build-system.

The following metadata values are now extracted from the `<extensionname>.s4ext`
file locally generated in the extension build tree:
- "category"
- "contributors"
- "enabled"
- "depends" (runtime dependencies)
- "description"
- "iconurl"
- "homepage"
- "screenshots"

The only metadata values propagated from the Slicer/ExtensionsIndex files
are the following:
- "scm", "scmurl" and "scmrevision"
- "depends" (built-time dependencies)
- "build_subdirectory"

The extension name is still derived from the catalog entry filename.

Extensions/CMake/SlicerBlockBuildPackageAndUploadExtension.cmake
@@ -23,7 +23,6 @@ set(expected_defined_vars
   CDASH_PROJECT_NAME
   EXTENSION_BUILD_OPTIONS_STRING
   EXTENSION_BUILD_SUBDIRECTORY
-  EXTENSION_ENABLED
   EXTENSION_NAME
   EXTENSION_SOURCE_DIR
   EXTENSION_SUPERBUILD_BINARY_DIR
@@ -61,20 +60,6 @@ set(CMAKE_MODULE_PATH
 include(CMakeParseArguments)
 include(SlicerCTestUploadURL)
 include(UseSlicerMacros) # for slicer_setting_variable_message
-
-#-----------------------------------------------------------------------------
-set(optional_vars
-  EXTENSION_CATEGORY
-  EXTENSION_CONTRIBUTORS
-  EXTENSION_DESCRIPTION
-  EXTENSION_HOMEPAGE
-  EXTENSION_ICONURL
-  EXTENSION_SCREENSHOTURLS
-  EXTENSION_STATUS
-  )
-foreach(var ${optional_vars})
-  slicer_setting_variable_message(${var})
-endforeach()
 
 #-----------------------------------------------------------------------------
 # Set site name and force to lower case
@@ -152,7 +137,6 @@ CTEST_MODEL:STRING=${CTEST_MODEL}
 GIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
 Slicer_DIR:PATH=${Slicer_DIR}
 Slicer_EXTENSIONS_TRACK_QUALIFIER:STRING=${Slicer_EXTENSIONS_TRACK_QUALIFIER}
-EXTENSION_DEPENDS:STRING=${EXTENSION_DEPENDS}
 ")
 
 if(APPLE)

Extensions/CMake/SlicerBlockBuildPackageAndUploadExtensions.cmake
@@ -82,15 +82,6 @@ foreach(EXTENSION_NAME ${EXTENSION_LIST})
 
   # Extract extension description info
   slicerFunctionExtractExtensionDescription(EXTENSION_FILE ${file} VAR_PREFIX EXTENSION)
-  set(EXTENSION_CATEGORY ${EXTENSION_EXT_CATEGORY})
-  set(EXTENSION_STATUS ${EXTENSION_EXT_STATUS})
-  set(EXTENSION_ICONURL ${EXTENSION_EXT_ICONURL})
-  set(EXTENSION_CONTRIBUTORS ${EXTENSION_EXT_CONTRIBUTORS})
-  set(EXTENSION_DESCRIPTION ${EXTENSION_EXT_DESCRIPTION})
-  set(EXTENSION_HOMEPAGE ${EXTENSION_EXT_HOMEPAGE})
-  set(EXTENSION_SCREENSHOTURLS ${EXTENSION_EXT_SCREENSHOTURLS})
-  set(EXTENSION_ENABLED ${EXTENSION_EXT_ENABLED})
-  set(EXTENSION_DEPENDS ${EXTENSION_EXT_DEPENDS})
 
   # Ensure extensions depending on this extension can lookup the corresponding
   # _DIR and _BUILD_SUBDIRECTORY variables.
@@ -108,8 +99,8 @@ foreach(EXTENSION_NAME ${EXTENSION_LIST})
 
   # Set external project DEPENDS parameter
   set(EP_ARG_EXTENSION_DEPENDS)
-  if(NOT "${EXTENSION_DEPENDS}" STREQUAL "")
-    set(EP_ARG_EXTENSION_DEPENDS DEPENDS ${EXTENSION_DEPENDS})
+  if(NOT "${EXTENSION_EXT_DEPENDS}" STREQUAL "")
+    set(EP_ARG_EXTENSION_DEPENDS DEPENDS ${EXTENSION_EXT_DEPENDS})
   endif()
 
   #-----------------------------------------------------------------------------

Extensions/CMake/SlicerBlockUploadExtension.cmake
@@ -5,7 +5,6 @@ set(expected_defined_vars
   EXTENSION_BITNESS
   EXTENSION_BUILD_SUBDIRECTORY
   EXTENSION_COMPILER
-  EXTENSION_ENABLED
   EXTENSION_NAME
   EXTENSION_SOURCE_DIR
   EXTENSION_SUPERBUILD_BINARY_DIR
@@ -87,17 +86,9 @@ set(CDASH_PROJECT_NAME \"${CDASH_PROJECT_NAME}\")
 set(EXTENSION_BUILD_OPTIONS_STRING \"${EXTENSION_BITNESS}bits-Qt${Slicer_QT_VERSION_MAJOR}.${Slicer_QT_VERSION_MINOR}\")
 set(EXTENSION_COMPILER \"${EXTENSION_COMPILER}\")
 set(EXTENSION_NAME \"${EXTENSION_NAME}\")
-set(EXTENSION_CATEGORY \"${EXTENSION_CATEGORY}\")
-set(EXTENSION_STATUS \"${EXTENSION_STATUS}\")
-set(EXTENSION_ICONURL \"${EXTENSION_ICONURL}\")
-set(EXTENSION_CONTRIBUTORS \"${EXTENSION_CONTRIBUTORS}\")
-set(EXTENSION_DESCRIPTION \"${EXTENSION_DESCRIPTION}\")
-set(EXTENSION_HOMEPAGE \"${EXTENSION_HOMEPAGE}\")
-set(EXTENSION_SCREENSHOTURLS \"${EXTENSION_SCREENSHOTURLS}\")
 set(EXTENSION_SOURCE_DIR \"${EXTENSION_SOURCE_DIR}\")
 set(EXTENSION_SUPERBUILD_BINARY_DIR \"${EXTENSION_SUPERBUILD_BINARY_DIR}\")
 set(EXTENSION_BUILD_SUBDIRECTORY \"${EXTENSION_BUILD_SUBDIRECTORY}\")
-set(EXTENSION_ENABLED \"${EXTENSION_ENABLED}\")
 set(EXTENSION_DEPENDS \"${EXTENSION_DEPENDS}\")
 set(Slicer_CMAKE_DIR \"${Slicer_CMAKE_DIR}\")
 set(Slicer_DIR \"${Slicer_DIR}\")

Extensions/CMake/SlicerExtensionPackageAndUploadTarget.cmake
@@ -6,10 +6,11 @@
 # extension.
 #
 # The new target that will
-#  (1) build the standard 'package' target,
-#  (2) extract the list of generated packages from its standard output,
-#  (3) append the list of generated package filepaths to a file named PACKAGES.txt,
-#  (4) upload the first package.
+#  (1) configure the project and build the standard 'package' target,
+#  (2) extract metadata from the locally generated .s4ext file
+#  (3) extract the list of generated packages from its standard output,
+#  (4) append the list of generated package filepaths to a file named PACKAGES.txt,
+#  (5) upload the first package.
 #
 # The following variables are expected to be defined in the including scope:
 #  CMAKE_SOURCE_DIR
@@ -18,15 +19,19 @@
 #  Slicer_CMAKE_DIR
 #  Slicer_REVISION
 #  EXTENSION_NAME
-#  EXTENSION_CATEGORY
-#  EXTENSION_ICONURL
-#  EXTENSION_CONTRIBUTORS
-#  EXTENSION_DESCRIPTION
-#  EXTENSION_HOMEPAGE
-#  EXTENSION_SCREENSHOTURLS
-#  EXTENSION_ENABLED
 #  EXTENSION_OPERATING_SYSTEM
 #  EXTENSION_ARCHITECTURE
+#
+# The following variables are internally set by extracting corresponding values
+# from the locally generated "<extension_name>.s4ext" file:
+#  EXTENSION_EXT_CATEGORY
+#  EXTENSION_EXT_CONTRIBUTORS
+#  EXTENSION_EXT_DEPENDS
+#  EXTENSION_EXT_DESCRIPTION
+#  EXTENSION_EXT_HOMEPAGE
+#  EXTENSION_EXT_ICONURL
+#  EXTENSION_EXT_SCREENSHOTURLS
+#  EXTENSION_EXT_ENABLED
 #
 # The following variables can either be defined in the including scope or
 # as environment variables:
@@ -98,12 +103,6 @@ if(NOT PACKAGEUPLOAD)
     Slicer_REVISION
     EXTENSION_NAME
     EXTENSION_CATEGORY
-    EXTENSION_ICONURL
-    EXTENSION_CONTRIBUTORS
-    EXTENSION_DEPENDS
-    EXTENSION_DESCRIPTION
-    EXTENSION_HOMEPAGE
-    EXTENSION_SCREENSHOTURLS
     EXTENSION_ENABLED
     EXTENSION_OPERATING_SYSTEM
     EXTENSION_ARCHITECTURE
@@ -265,8 +264,30 @@ foreach(p ${package_list})
     set(package_uploaded 1)
     get_filename_component(package_name "${p}" NAME)
 
+    # Extract extension metadata from ".s4ext" file generated in the build
+    # directory based on the variables set in the extension CMakeLists.txt
+    include(SlicerFunctionExtractExtensionDescription)
+    slicerFunctionExtractExtensionDescription(
+      EXTENSION_FILE "${EXTENSION_BINARY_DIR}/${EXTENSION_NAME}.s4ext"
+      VAR_PREFIX EXTENSION)
+
+    # Sanity checks
+    set(expected_defined_vars
+      EXTENSION_EXT_DEPENDS
+      EXTENSION_EXT_DESCRIPTION
+      EXTENSION_EXT_ICONURL
+      EXTENSION_EXT_HOMEPAGE
+      EXTENSION_EXT_SCREENSHOTURLS
+      EXTENSION_EXT_CONTRIBUTORS
+      )
+    foreach(var ${expected_defined_vars})
+      if(NOT DEFINED ${var})
+        message(FATAL_ERROR "Variable ${var} is not defined !")
+      endif()
+    endforeach()
+
     # Convert to space separated list
-    list(JOIN EXTENSION_DEPENDS " " dependency)
+    list(JOIN EXTENSION_EXT_DEPENDS " " dependency)
 
     message("Uploading [${package_name}] to [${SLICER_EXTENSION_MANAGER_URL}]")
     get_filename_component(package_directory ${p} DIRECTORY)
@@ -287,12 +308,12 @@ foreach(p ${package_list})
             --revision "${EXTENSION_WC_REVISION}"
             --app_revision "${Slicer_REVISION}"
             --category "${EXTENSION_CATEGORY}"
-            --desc "${EXTENSION_DESCRIPTION}"
+            --desc "${EXTENSION_EXT_DESCRIPTION}"
             --dependency "${dependency}"
-            --icon_url "${EXTENSION_ICONURL}"
-            --homepage "${EXTENSION_HOMEPAGE}"
-            --screenshots "${EXTENSION_SCREENSHOTURLS}"
-            --contributors "${EXTENSION_CONTRIBUTORS}"
+            --icon_url "${EXTENSION_EXT_ICONURL}"
+            --homepage "${EXTENSION_EXT_HOMEPAGE}"
+            --screenshots "${EXTENSION_EXT_SCREENSHOTURLS}"
+            --contributors "${EXTENSION_EXT_CONTRIBUTORS}"
       RESULT_VARIABLE slicer_extension_manager_upload_status
       ERROR_FILE ${error_file}
       )

commit 93cb700845ccbdc14d9107d3024e869a8ce8bb95

COMP: Fix extension build ensuring build dependency directories are passed
Fix regression introduced in 524ddb6d6d ("ENH: De-duplicate setting of extension metadata", 2024-03-13)
by ensuring <dep>_BUILD_SUBDIRECTORY and <dep>_BUILD_SUBDIRECTORY
variables are set in `<extensionName>-(test|upload)-command-args.cmake`
script itself used in `SlicerBlockBuildPackageAndUploadExtension.cmake` to
set the extension cache variables `<dep>_DIR`, `<dep>_BINARY_DIR` and `<dep>_BUILD_SUBDIRECTORY`.

Extensions/CMake/SlicerBlockBuildPackageAndUploadExtension.cmake
@@ -21,6 +21,7 @@ set(expected_defined_vars
   CTEST_CMAKE_GENERATOR
   CTEST_DROP_SITE
   CDASH_PROJECT_NAME
+  EXTENSION_BUILD_DEPENDENCIES
   EXTENSION_BUILD_OPTIONS_STRING
   EXTENSION_BUILD_SUBDIRECTORY
   EXTENSION_CATALOG_ENTRY_FILE

Extensions/CMake/SlicerBlockBuildPackageAndUploadExtensions.cmake
@@ -177,6 +177,7 @@ foreach(EXTENSION_NAME ${EXTENSION_LIST})
   #-----------------------------------------------------------------------------
   set(EXTENSION_SUPERBUILD_BINARY_DIR ${${EXTENSION_NAME}_BINARY_DIR})
   set(EXTENSION_BUILD_SUBDIRECTORY ${${EXTENSION_NAME}_BUILD_SUBDIRECTORY})
+  set(EXTENSION_BUILD_DEPENDENCIES "${EXTENSION_EXT_BUILD_DEPENDENCIES}")
   if(NOT DEFINED CTEST_MODEL)
     set(CTEST_MODEL "Experimental")
   endif()

Extensions/CMake/SlicerBlockUploadExtension.cmake
@@ -3,6 +3,7 @@
 set(expected_defined_vars
   CMAKE_CTEST_COMMAND
   EXTENSION_BITNESS
+  EXTENSION_BUILD_DEPENDENCIES
   EXTENSION_BUILD_SUBDIRECTORY
   EXTENSION_COMPILER
   EXTENSION_NAME