9b27fa47480f021c31fdcc0bb88ecbce3bec7245
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-09-20 23:01:41 -0400

    ENH: Add Github workflow to update the "nightly-main" branch daily
    Introduce a new workflow that updates the 'nightly-main' preview branch
    daily at 04:00 UTC. This scheduled workflow ensures it runs after Kitware-hosted nightly
    builds, aligned with the dashboards' 11:00 PM ET trigger time.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,4 +1,8 @@
 set(CTEST_PROJECT_NAME "Slicer")
+
+# If the nightly start time is updated, ensure the corresponding change is reflected
+# in ".github/workflows/update-slicer-preview-branch.yml" and in the crontab or task scheduler
+# of the Kitware-hosted dashboards to maintain synchronization across all systems.
 set(CTEST_NIGHTLY_START_TIME "3:00:00 UTC")
 
 if(NOT DEFINED CDASH_PROJECT_NAME)
