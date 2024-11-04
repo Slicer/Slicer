fe8af8d0b66785d46ce683bfed71c2dbd907c248
Andras Lasso <lasso@queensu.ca>
2024-11-04 12:24:25 -0500

    ENH: Check if a node is present in the scene before adding it
    Previously, if a node was already added to the scene, scene->AddNode caused corruptions in the scene cache.
    A check was only implemented for debug mode, claiming that checking if a node is in the scene is expensive.
    
    While checking if a node is already in the scene is indeed somewhat expensive, but this operation is not performed frequently and the consequences of an inconsistent node cache in the scene are hard to predict.
    
    Therefore the check is now enabled for both debug and release mode, and in case the node is already found in the scene it is not added again.

--- Libs/MRML/Core/vtkMRMLScene.cxx
+++ Libs/MRML/Core/vtkMRMLScene.cxx
@@ -1334,19 +1334,16 @@ vtkMRMLNode* vtkMRMLScene::AddNode(vtkMRMLNode *n)
   {
     return nullptr;
   }
-#ifndef NDEBUG
-  // Since calling IsNodePresent is costly, a "developer hint" is printed only
-  // if build as debug. We can't exit here as the release would then be
-  // different from debug.
-  // The caller should make sure the node has not been added yet.
+
   if (this->IsNodePresent(n) != 0)
   {
     vtkErrorMacro("AddNode: Node " << n->GetClassName() << "/"
       << (n->GetName() ? n->GetName() : "(undefined)") << "/"
       << (n->GetID() ? n->GetID() : "(undefined)")
       << "[" << n << "]" << " already added");
+    return n;
   }
-#endif
+
   // We need to know if the node will be actually added to the scene before
   // it is effectively added to know if NodeAboutToBeAddedEvent needs to be
   // fired.
