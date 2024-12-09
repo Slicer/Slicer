Slicer/Slicer

commit d6f6b7dbc2f797f8189b91b583d201dffd76d825

ENH: Add clipping options to models, segmentation, and volume rendering
Adds the new vtkMRMLClipNode class, which specifies the combination of implicit functions used for clipping other node types.
Nodes such as vtkMRMLMarkupsPlaneNode, vtkMRMLMarkupsROINode, vtkMRMLSliceNode, vtkMRMLModelNodes, and other vtkMRMLClipNode can be added as node references on the vtkMRMLClipNode. These nodes all implement the GetImplicitFunctionWorld() function, which returns a vtkImplicitFunction that can be used to clip other nodes.

Node clipping is currently supported in MRMLDM for model, segmentation and volume rendering node types.

Resolves #7494

Co-authored-by: Andras Lasso <lasso@queensu.ca>

Libs/MRML/DisplayableManager/vtkMRMLModelDisplayableManager.h
@@ -16,7 +16,7 @@
 
 // MRML includes
 #include <vtkMRMLModelNode.h>
-class vtkMRMLClipModelsNode;
+class vtkMRMLClipNode;
 class vtkMRMLDisplayNode;
 class vtkMRMLDisplayableNode;
 class vtkMRMLTransformNode;
@@ -28,10 +28,12 @@ class vtkAlgorithm;
 class vtkCellPicker;
 class vtkLookupTable;
 class vtkMatrix4x4;
+class vtkMapper;
 class vtkPlane;
 class vtkPointPicker;
 class vtkProp3D;
 class vtkPropPicker;
+class vtkTransform;
 class vtkWorldPointPicker;
 
 /// \brief Manage display nodes with polydata in 3D views.
@@ -47,18 +49,14 @@ class VTK_MRML_DISPLAYABLEMANAGER_EXPORT vtkMRMLModelDisplayableManager
 {
 public:
   static vtkMRMLModelDisplayableManager* New();
-  vtkTypeMacro(vtkMRMLModelDisplayableManager,vtkMRMLAbstractThreeDViewDisplayableManager);
+  vtkTypeMacro(vtkMRMLModelDisplayableManager, vtkMRMLAbstractThreeDViewDisplayableManager);
   void PrintSelf(ostream& os, vtkIndent indent) override;
 
-  /// Get/Set the ClipModels Node
-  vtkMRMLClipModelsNode* GetClipModelsNode();
-  void SetClipModelsNode(vtkMRMLClipModelsNode *snode);
-
   /// Return the current model actor corresponding to a give MRML ID
-  vtkProp3D *GetActorByID(const char *id);
+  vtkProp3D* GetActorByID(const char* id);
 
   /// Return the current node ID corresponding to a given vtkProp3D
-  const char *GetIDByActor(vtkProp3D *actor);
+  const char* GetIDByActor(vtkProp3D* actor);
 
   /// Get world point picker
   vtkWorldPointPicker* GetWorldPointPicker();
@@ -103,10 +101,6 @@ public:
   /// Set the picked point id, returns -1 if no pick
   void SetPickedPointID(vtkIdType newPointID);
 
-  void SetClipPlaneFromMatrix(vtkMatrix4x4 *sliceMatrix,
-                             int planeDirection,
-                             vtkPlane *plane);
-
   /// Return true if the node can be represented as a model
   bool IsModelDisplayable(vtkMRMLDisplayableNode* node)const;
   /// Return true if the display node is a model
@@ -130,7 +124,7 @@ protected:
   void OnMRMLSceneNodeRemoved(vtkMRMLNode* node) override;
 
   void OnInteractorStyleEvent(int eventId) override;
-  void ProcessMRMLNodesEvents(vtkObject *caller, unsigned long event, void *callData) override;
+  void ProcessMRMLNodesEvents(vtkObject* caller, unsigned long event, void* callData) override;
 
   /// Returns true if something visible in modelNode has changed and would
   /// require a refresh.
@@ -158,11 +152,20 @@ protected:
                                   vtkMRMLModelNode* model = nullptr);
 
   /// Returns not null if modified
-  int UpdateClipSlicesFromMRML();
-  vtkAlgorithm *CreateTransformedClipper(vtkMRMLTransformNode *tnode,
-                                         vtkMRMLModelNode::MeshTypeHint type);
+  vtkAlgorithm* GetClipper(vtkMRMLDisplayNode* dnode,
+                           vtkMRMLModelNode::MeshTypeHint type,
+                           vtkImplicitFunction* clipFunction,
+                           int clipMethod);
 
-  void RemoveDisplayedID(std::string &id);
+  void RemoveDisplayedID(const std::string& id);
+  void ClearDisplayMaps();
+
+  void UpdateMapperProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* displayNode,
+    vtkMapper* actor);
+  void UpdateActorProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* displayNode,
+    vtkActor* actor, double opacity);
+  void UpdateCapActorProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* displayNode,
+    vtkActor* capActor, double opacity);
 
 protected:
   vtkMRMLModelDisplayableManager();

commit 3006ae7964943e51fdab5fa12aa30ab81d61c63b

BUG: Fix folder display override for model nodes
Folder opacity/color was broken in d6f6b7dbc2f797f8189b91b583d201dffd76d825.

Fixed by using overriding display node rather than model display node for applicable properties.
Fixes failing py_nomainwindow_SubjectHierarchyFoldersTest1 test.

Libs/MRML/DisplayableManager/vtkMRMLModelDisplayableManager.cxx
@@ -1363,14 +1363,14 @@ void vtkMRMLModelDisplayableManager::SetModelDisplayProperty(vtkMRMLDisplayableN
 
     if (visible && actor)
     {
-      this->UpdateMapperProperties(vtkMRMLModelNode::SafeDownCast(model), modelDisplayNode, actor->GetMapper());
-      this->UpdateActorProperties(vtkMRMLModelNode::SafeDownCast(model), modelDisplayNode, actor, opacity);
+      this->UpdateMapperProperties(vtkMRMLModelNode::SafeDownCast(model), displayNode, actor->GetMapper());
+      this->UpdateActorProperties(vtkMRMLModelNode::SafeDownCast(model), modelDisplayNode, displayNode, actor, opacity);
     }
 
     if (capVisible && capActor)
     {
-      this->UpdateMapperProperties(vtkMRMLModelNode::SafeDownCast(model), modelDisplayNode, capActor->GetMapper());
-      this->UpdateCapActorProperties(vtkMRMLModelNode::SafeDownCast(model), modelDisplayNode, capActor, opacity);
+      this->UpdateMapperProperties(vtkMRMLModelNode::SafeDownCast(model), displayNode, capActor->GetMapper());
+      this->UpdateCapActorProperties(vtkMRMLModelNode::SafeDownCast(model), modelDisplayNode, displayNode, capActor, opacity);
     }
 
     if (imageActor)
@@ -1382,8 +1382,7 @@ void vtkMRMLModelDisplayableManager::SetModelDisplayProperty(vtkMRMLDisplayableN
 }
 
 //---------------------------------------------------------------------------
-void vtkMRMLModelDisplayableManager::UpdateMapperProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* displayNode,
-  vtkMapper* mapper)
+void vtkMRMLModelDisplayableManager::UpdateMapperProperties(vtkMRMLModelNode* modelNode, vtkMRMLDisplayNode* displayNode, vtkMapper* mapper)
 {
   if (!mapper)
   {
@@ -1441,55 +1440,60 @@ void vtkMRMLModelDisplayableManager::UpdateMapperProperties(vtkMRMLModelNode* mo
 
 //---------------------------------------------------------------------------
 void vtkMRMLModelDisplayableManager::UpdateActorProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* modelDisplayNode,
-  vtkActor* actor, double opacity)
+  vtkMRMLDisplayNode* displayNode, vtkActor* actor, double opacity)
 {
   if (!modelNode || !modelDisplayNode || !actor)
   {
     return;
   }
 
+  if (!displayNode)
+  {
+    displayNode = modelDisplayNode;
+  }
+
   vtkProperty* actorProperties = actor->GetProperty();
-  actorProperties->SetRepresentation(modelDisplayNode->GetRepresentation());
-  actorProperties->SetPointSize(modelDisplayNode->GetPointSize());
-  actorProperties->SetLineWidth(modelDisplayNode->GetLineWidth());
-  actorProperties->SetLighting(modelDisplayNode->GetLighting());
-  actorProperties->SetInterpolation(modelDisplayNode->GetInterpolation());
-  actorProperties->SetShading(modelDisplayNode->GetShading());
-  actorProperties->SetFrontfaceCulling(modelDisplayNode->GetFrontfaceCulling());
-  actorProperties->SetBackfaceCulling(modelDisplayNode->GetBackfaceCulling());
+  actorProperties->SetRepresentation(displayNode->GetRepresentation());
+  actorProperties->SetPointSize(displayNode->GetPointSize());
+  actorProperties->SetLineWidth(displayNode->GetLineWidth());
+  actorProperties->SetLighting(displayNode->GetLighting());
+  actorProperties->SetInterpolation(displayNode->GetInterpolation());
+  actorProperties->SetShading(displayNode->GetShading());
+  actorProperties->SetFrontfaceCulling(displayNode->GetFrontfaceCulling());
+  actorProperties->SetBackfaceCulling(displayNode->GetBackfaceCulling());
 
   actor->SetPickable(modelNode->GetSelectable());
-  if (modelDisplayNode->GetSelected())
+  if (displayNode->GetSelected())
   {
-    actorProperties->SetColor(modelDisplayNode->GetSelectedColor());
-    actorProperties->SetAmbient(modelDisplayNode->GetSelectedAmbient());
-    actorProperties->SetSpecular(modelDisplayNode->GetSelectedSpecular());
+    actorProperties->SetColor(displayNode->GetSelectedColor());
+    actorProperties->SetAmbient(displayNode->GetSelectedAmbient());
+    actorProperties->SetSpecular(displayNode->GetSelectedSpecular());
   }
   else
   {
-    actorProperties->SetColor(modelDisplayNode->GetColor());
-    actorProperties->SetAmbient(modelDisplayNode->GetAmbient());
-    actorProperties->SetSpecular(modelDisplayNode->GetSpecular());
+    actorProperties->SetColor(displayNode->GetColor());
+    actorProperties->SetAmbient(displayNode->GetAmbient());
+    actorProperties->SetSpecular(displayNode->GetSpecular());
   }
   // Opacity will be the product of the opacities of the model and the overriding
   // hierarchy, in order to keep the relative opacities the same.
   actorProperties->SetOpacity(opacity);
-  actorProperties->SetDiffuse(modelDisplayNode->GetDiffuse());
-  actorProperties->SetSpecularPower(modelDisplayNode->GetPower());
-  actorProperties->SetMetallic(modelDisplayNode->GetMetallic());
-  actorProperties->SetRoughness(modelDisplayNode->GetRoughness());
-  actorProperties->SetEdgeVisibility(modelDisplayNode->GetEdgeVisibility());
-  actorProperties->SetEdgeColor(modelDisplayNode->GetEdgeColor());
+  actorProperties->SetDiffuse(displayNode->GetDiffuse());
+  actorProperties->SetSpecularPower(displayNode->GetPower());
+  actorProperties->SetMetallic(displayNode->GetMetallic());
+  actorProperties->SetRoughness(displayNode->GetRoughness());
+  actorProperties->SetEdgeVisibility(displayNode->GetEdgeVisibility());
+  actorProperties->SetEdgeColor(displayNode->GetEdgeColor());
 
-  if (modelDisplayNode->GetTextureImageDataConnection() != nullptr)
+  if (displayNode->GetTextureImageDataConnection() != nullptr)
   {
     if (actor->GetTexture() == nullptr)
     {
       vtkNew<vtkTexture> texture;
       actor->SetTexture(texture);
     }
-    actor->GetTexture()->SetInputConnection(modelDisplayNode->GetTextureImageDataConnection());
-    actor->GetTexture()->SetInterpolate(modelDisplayNode->GetInterpolateTexture());
+    actor->GetTexture()->SetInputConnection(displayNode->GetTextureImageDataConnection());
+    actor->GetTexture()->SetInterpolate(displayNode->GetInterpolateTexture());
     actorProperties->SetColor(1., 1., 1.);
 
     // Force actors to be treated as opaque. Otherwise, transparent
@@ -1538,9 +1542,14 @@ void vtkMRMLModelDisplayableManager::UpdateActorProperties(vtkMRMLModelNode* mod
 
 //---------------------------------------------------------------------------
 void vtkMRMLModelDisplayableManager::UpdateCapActorProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* modelDisplayNode,
-  vtkActor* capActor, double opacity)
+  vtkMRMLDisplayNode* displayNode, vtkActor* capActor, double opacity)
 {
-  this->UpdateActorProperties(modelNode, modelDisplayNode, capActor, opacity);
+  if (!displayNode)
+  {
+    displayNode = modelDisplayNode;
+  }
+
+  this->UpdateActorProperties(modelNode, modelDisplayNode, displayNode, capActor, opacity);
 
   vtkSmartPointer<vtkProperty> capActorProperties = capActor->GetProperty();
   if (!capActorProperties)

Libs/MRML/DisplayableManager/vtkMRMLModelDisplayableManager.h
@@ -160,11 +160,11 @@ protected:
   void RemoveDisplayedID(const std::string& id);
   void ClearDisplayMaps();
 
-  void UpdateMapperProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* displayNode,
+  void UpdateMapperProperties(vtkMRMLModelNode* modelNode, vtkMRMLDisplayNode* displayNode,
     vtkMapper* actor);
-  void UpdateActorProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* displayNode,
+  void UpdateActorProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* modelDisplayNode, vtkMRMLDisplayNode* displayNode,
     vtkActor* actor, double opacity);
-  void UpdateCapActorProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* displayNode,
+  void UpdateCapActorProperties(vtkMRMLModelNode* modelNode, vtkMRMLModelDisplayNode* modelDisplayNode, vtkMRMLDisplayNode* displayNode,
     vtkActor* capActor, double opacity);
 
 protected: