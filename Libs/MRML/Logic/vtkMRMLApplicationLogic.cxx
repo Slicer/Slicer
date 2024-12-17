492a5b7207159bb647bccfcb491d675ca324771b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-12-16 19:24:15 -0500

    ENH: Add MRML application API for registering volume resampler

--- Libs/MRML/Logic/vtkMRMLApplicationLogic.cxx
+++ Libs/MRML/Logic/vtkMRMLApplicationLogic.cxx
@@ -23,6 +23,7 @@
 
 // MRMLLogic includes
 #include "vtkMRMLApplicationLogic.h"
+#include "vtkMRMLAbstractVolumeResampler.h"
 #include "vtkMRMLColorLogic.h"
 #include "vtkMRMLLogic.h"
 #include "vtkMRMLMessageCollection.h"
@@ -98,6 +99,7 @@ public:
   std::string TemporaryPath;
   std::map<std::string, vtkWeakPointer<vtkMRMLAbstractLogic> > ModuleLogicMap;
   std::map<int, std::string> FontFileNames;
+  std::map<std::string, vtkSmartPointer<vtkMRMLAbstractVolumeResampler>> Resamplers;
 };
 
 //----------------------------------------------------------------------------
@@ -1285,4 +1287,72 @@ std::string vtkMRMLApplicationLogic::GetFontsDirectory()
   filesVector.emplace_back(FONTS_DIR);
   std::string fullPath = vtksys::SystemTools::JoinPath(filesVector);
   return fullPath;
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLApplicationLogic::RegisterVolumeResampler(
+    const std::string& resamplerName, vtkMRMLAbstractVolumeResampler* resampler)
+{
+  if (resamplerName.empty())
+  {
+    vtkErrorMacro("RegisterVolumeResampler: invalid sampler name.");
+    return;
+  }
+  if (this->IsVolumeResamplerRegistered(resamplerName))
+  {
+    return;
+  }
+  this->Internal->Resamplers[resamplerName] = resampler;
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLApplicationLogic::UnregisterVolumeResampler(const std::string& resamplerName)
+{
+  if (!this->IsVolumeResamplerRegistered(resamplerName))
+  {
+    return;
+  }
+  this->Internal->Resamplers.erase(resamplerName);
+}
+
+//----------------------------------------------------------------------------
+bool vtkMRMLApplicationLogic::IsVolumeResamplerRegistered(const std::string& resamplerName)
+{
+  return this->Internal->Resamplers.find(resamplerName) != this->Internal->Resamplers.end();
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLAbstractVolumeResampler* vtkMRMLApplicationLogic::GetVolumeResampler(const std::string& resamplerName)
+{
+  if (!this->IsVolumeResamplerRegistered(resamplerName))
+  {
+    return nullptr;
+  }
+  return this->Internal->Resamplers[resamplerName];
+}
+
+//----------------------------------------------------------------------------
+bool vtkMRMLApplicationLogic::ResampleVolume(std::string& resamplerName,
+                                             vtkMRMLVolumeNode* inputVolume,
+                                             vtkMRMLVolumeNode* outputVolume,
+                                             vtkMRMLTransformNode* resamplingTransform,
+                                             vtkMRMLVolumeNode* referenceVolume,
+                                             int interpolationType,
+                                             int windowedSincFunction,
+                                             const vtkMRMLAbstractVolumeResampler::ResamplingParameters& resamplingParameters)
+{
+  vtkMRMLAbstractVolumeResampler* resampler = this->GetVolumeResampler(resamplerName);
+  if (!resampler)
+  {
+    vtkErrorMacro("ResampleVolume: resampler not registered " << resamplerName);
+    return false;
+  }
+  return resampler->Resample(
+        inputVolume,
+        outputVolume,
+        resamplingTransform,
+        referenceVolume,
+        interpolationType,
+        windowedSincFunction,
+        resamplingParameters);
 }
