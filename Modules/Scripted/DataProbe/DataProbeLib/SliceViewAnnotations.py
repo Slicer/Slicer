Slicer/Slicer

commit 169df528402804da17f646c8cb9c81c5244de920

ENH: Fix 3943 and other enhancements for slice view annotations
* Fixed #3943 by replacing DICOM study date and study time with series date and
  series time.

* Moved slice view annotations settings gear from Data Probe small widget (main
  screen) to the Data probe module in order to preserve more screen real
  estate for the module UIs.

* Fixed problem with color scalarbar range labels.
* Added an option to change format of color scalarbar range label
  and save the user settings in application settings.

* Change ruler text font size and family according to other text
  annotations.

* Removed the settings gear

* Store previous status of ruler and colorscalarbar after enabling
  slice view annotations

svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23981
git-svn-id: http://svn.slicer.org/Slicer4/trunk@23981 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DataProbe/DataProbeLib/SliceViewAnnotations.py
@@ -36,10 +36,10 @@ class SliceAnnotations(object):
       '1-PatientName':{'text':'','category':'B'},
       '2-PatientID':{'text':'','category':'A'},
       '3-PatientInfo':{'text':'','category':'B'},
-      '4-Bg-StudyDate':{'text':'','category':'B'},
-      '5-Fg-StudyDate':{'text':'','category':'B'},
-      '6-Bg-StudyTime':{'text':'','category':'C'},
-      '7-Bg-StudyTime':{'text':'','category':'C'},
+      '4-Bg-SeriesDate':{'text':'','category':'B'},
+      '5-Fg-SeriesDate':{'text':'','category':'B'},
+      '6-Bg-SeriesTime':{'text':'','category':'C'},
+      '7-Bg-SeriesTime':{'text':'','category':'C'},
       '8-Bg-SeriesDescription':{'text':'','category':'C'},
       '9-Fg-SeriesDescription':{'text':'','category':'C'}
       })
@@ -90,12 +90,14 @@ class SliceAnnotations(object):
           'DataProbe/sliceViewAnnotations.showScalingRuler'))
     else:
       self.showScalingRuler = 1
+    self.showScalingRulerLastStatus = self.showScalingRuler
 
     if settings.contains('DataProbe/sliceViewAnnotations.showColorScalarBar'):
       self.showColorScalarBar= int(settings.value(
           'DataProbe/sliceViewAnnotations.showColorScalarBar'))
     else:
       self.showColorScalarBar = 0
+    self.showColorScalarBarLastStatus = self.showColorScalarBar
 
     if settings.contains('DataProbe/sliceViewAnnotations.fontFamily'):
       self.fontFamily = settings.value('DataProbe/sliceViewAnnotations.fontFamily')
@@ -113,6 +115,12 @@ class SliceAnnotations(object):
           'DataProbe/sliceViewAnnotations.bgDicomAnnotationsPersistence'))
     else:
       self.backgroundDicomAnnotationsPersistence = 0
+
+    if settings.contains('DataProbe/sliceViewAnnotations.rangeLabelFormat'):
+      self.rangeLabelFormat= str(settings.value(
+          'DataProbe/sliceViewAnnotations.rangeLabelFormat'))
+    else:
+      self.rangeLabelFormat = '%G'
 
     self.parameter = 'showSliceViewAnnotations'
     self.parameterNode = self.dataProbeUtil.getParameterNode()
@@ -199,9 +207,9 @@ class SliceAnnotations(object):
     self.backgroundRadioButton.checked = True
     self.foregroundRadioButton = slicer.util.findChildren(self.window,
        'foregroundRadioButton')[0]
-
-    self.colorScalarBarMaxWidthSlider = slicer.util.findChildren(self.window,
-       'colorScalarBarMaxWidthSlider')[0]
+    self.rangeLabelFormatLineEdit = slicer.util.findChildren(self.window,
+       'rangeLabelFormatLineEdit')[0]
+    self.rangeLabelFormatLineEdit.text = self.rangeLabelFormat
 
     restorDefaultsButton = slicer.util.findChildren(self.window,
         'restoreDefaultsButton')[0]
@@ -226,18 +234,21 @@ class SliceAnnotations(object):
     self.showColorScalarBarCheckBox.connect('clicked()', self.onShowColorScalarBarCheckbox)
     self.backgroundRadioButton.connect('clicked()',self.onLayerSelectionRadioButton)
     self.foregroundRadioButton.connect('clicked()',self.onLayerSelectionRadioButton)
-    self.colorScalarBarMaxWidthSlider.connect('valueChanged(double)', self.updateSliceViewFromGUI)
+    self.rangeLabelFormatLineEdit.connect('editingFinished()',self.onRangeLabelFormatLineEdit)
+    self.rangeLabelFormatLineEdit.connect('returnPressed()',self.onRangeLabelFormatLineEdit)
 
     restorDefaultsButton.connect('clicked()', self.restoreDefaultValues)
 
   def onSliceViewAnnotationsCheckbox(self):
     if self.sliceViewAnnotationsCheckBox.checked:
-      self.showColorScalarBarCheckBox.checked = True
-      self.showScalingRulerCheckBox.checked = True
       self.showSliceViewAnnotations = 1
-      self.showScalingRuler = 1
-      self.showColorScalarBar = 1
+      self.showScalingRulerCheckBox.checked = self.showScalingRulerLastStatus
+      self.showScalingRuler = self.showScalingRulerLastStatus
+      self.showColorScalarBarCheckBox.checked = self.showColorScalarBarLastStatus
+      self.showColorScalarBar = self.showColorScalarBarLastStatus
     else:
+      self.showScalingRulerLastStatus = self.showScalingRuler
+      self.showColorScalarBarLastStatus = self.showColorScalarBar
       self.showColorScalarBarCheckBox.checked = False
       self.showScalingRulerCheckBox.checked = False
       self.showSliceViewAnnotations = 0
@@ -319,7 +330,6 @@ class SliceAnnotations(object):
       self.fontFamily = 'Times'
     else:
       self.fontFamily = 'Arial'
-    self.updateSliceViewFromGUI()
     settings = qt.QSettings()
     settings.setValue('DataProbe/sliceViewAnnotations.fontFamily',
         self.fontFamily)
@@ -334,6 +344,14 @@ class SliceAnnotations(object):
 
   def onTextLengthSpinBox(self):
     self.maximumTextLength = self.textLengthSpinBox.value
+    self.updateSliceViewFromGUI()
+
+  def onRangeLabelFormatLineEdit(self):
+    # Updating font size and family
+    self.rangeLabelFormat =  self.rangeLabelFormatLineEdit.text
+    settings = qt.QSettings()
+    settings.setValue('DataProbe/sliceViewAnnotations.rangeLabelFormat',
+        self.rangeLabelFormat)
     self.updateSliceViewFromGUI()
 
   def restoreDefaultValues(self):
@@ -352,7 +370,8 @@ class SliceAnnotations(object):
     self.showScalingRuler = 1
     self.showColorScalarBarCheckBox.checked = False
     self.showColorScalarBar = 0
-    self.colorScalarBarMaxWidthSlider.value = 50
+    self.rangeLabelFormat = '%G'
+    self.rangeLabelFormatLineEdit.text = '%G'
 
     settings = qt.QSettings()
     settings.setValue('DataProbe/sliceViewAnnotations.show',
@@ -373,6 +392,8 @@ class SliceAnnotations(object):
         self.showScalingRuler)
     settings.setValue('DataProbe/sliceViewAnnotations.showColorScalarBar',
         self.showColorScalarBar)
+    settings.setValue('DataProbe/sliceViewAnnotations.rangeLabelFormat',
+        self.rangeLabelFormat)
     self.updateSliceViewFromGUI()
 
   def updateMRMLFromGUI(self):
@@ -568,7 +589,6 @@ class SliceAnnotations(object):
     actor = self.scalingRulerActors[sliceViewName]
     actor.SetMapper(mapper)
     # color actor
-    actor.GetProperty().SetColor(1,1,1)
     actor.GetProperty().SetLineWidth(1)
     textActor = self.scalingRulerTextActors[sliceViewName]
     textProperty = textActor.GetTextProperty()
@@ -581,7 +601,7 @@ class SliceAnnotations(object):
     scalarBar.SetTitle(" ")
     # adjust text property
     if vtk.VTK_MAJOR_VERSION > 5:
-      scalarBar.SetRangeLabelFormat('%.0f')
+      scalarBar.SetRangeLabelFormat(self.rangeLabelFormat)
     lookupTable = vtk.vtkLookupTable()
     scalarBar.SetLookupTable(lookupTable)
     scalarBarWidget = vtk.vtkScalarBarWidget()
@@ -692,9 +712,19 @@ class SliceAnnotations(object):
 
         textActor = self.scalingRulerTextActors[sliceViewName]
         textActor.SetInput(scalingFactorString)
+        textProperty = textActor.GetTextProperty()
+        # set font size
+        textProperty.SetFontSize(self.fontSize)
+        # set font family
+        if self.fontFamily == 'Times':
+          textProperty.SetFontFamilyToTimes()
+        else:
+          textProperty.SetFontFamilyToArial()
+        # set ruler text actor position
         textActor.SetDisplayPosition(int((viewWidth+RASRulerSize*scalingFactor)/2)+10,5)
 
         renderer.AddActor2D(self.scalingRulerActors[sliceViewName])
+        renderer.RemoveActor2D(textActor)
         renderer.AddActor2D(textActor)
 
       else:
@@ -723,6 +753,7 @@ class SliceAnnotations(object):
 
       scalarBar = self.colorScalarBars[sliceViewName]
       scalarBar.SetTextPositionToPrecedeScalarBar()
+      scalarBar.SetRangeLabelFormat(self.rangeLabelFormat)
       if vtk.VTK_MAJOR_VERSION > 5:
         scalarBar.SetAddRangeAnnotations(0)
       else:
@@ -863,10 +894,6 @@ class SliceAnnotations(object):
         sliceLogic.GetForegroundWindowLevelAndRange(width,level,rangeLow,rangeHigh)
       lut2.SetRange(level-width/2,level+width/2)
       scalarBar.SetLookupTable(lut2)
-      if width < 1:
-        scalarBar.AddRangeLabelsOff()
-      else:
-        scalarBar.AddRangeLabelsOn()
 
   def makeDicomAnnotation(self,bgUid,fgUid):
     viewHeight = self.sliceViews[self.currentSliceViewName].height
@@ -891,23 +918,23 @@ class SliceAnnotations(object):
           if self.cornerTexts[2].has_key('3-PatientInfo'):
             self.cornerTexts[2]['3-PatientInfo']['text'] = self.makePatientInfo(backgroundDicomDic)
 
-          if (backgroundDicomDic['Study Date'] != foregroundDicomDic['Study Date']):
-            if self.cornerTexts[2].has_key('4-Bg-StudyDate'):
-              self.cornerTexts[2]['4-Bg-StudyDate']['text'] = 'B: ' + self.formatDICOMDate(backgroundDicomDic['Study Date'])
-            if self.cornerTexts[2].has_key('5-Fg-StudyDate'):
-              self.cornerTexts[2]['5-Fg-StudyDate']['text'] = 'F: ' + self.formatDICOMDate(foregroundDicomDic['Study Date'])
+          if (backgroundDicomDic['Series Date'] != foregroundDicomDic['Series Date']):
+            if self.cornerTexts[2].has_key('4-Bg-SeriesDate'):
+              self.cornerTexts[2]['4-Bg-SeriesDate']['text'] = 'B: ' + self.formatDICOMDate(backgroundDicomDic['Series Date'])
+            if self.cornerTexts[2].has_key('5-Fg-SeriesDate'):
+              self.cornerTexts[2]['5-Fg-SeriesDate']['text'] = 'F: ' + self.formatDICOMDate(foregroundDicomDic['Series Date'])
           else:
-            if self.cornerTexts[2].has_key('4-Bg-StudyDate'):
-              self.cornerTexts[2]['4-Bg-StudyDate']['text'] =  self.formatDICOMDate(backgroundDicomDic['Study Date'])
+            if self.cornerTexts[2].has_key('4-Bg-SeriesDate'):
+              self.cornerTexts[2]['4-Bg-SeriesDate']['text'] =  self.formatDICOMDate(backgroundDicomDic['Series Date'])
 
-          if (backgroundDicomDic['Study Time'] != foregroundDicomDic['Study Time']):
-            if self.cornerTexts[2].has_key('6-Bg-StudyTime'):
-              self.cornerTexts[2]['6-Bg-StudyTime']['text'] = 'B: ' + self.formatDICOMTime(backgroundDicomDic['Study Time'])
-            if self.cornerTexts[2].has_key('7-Fg-StudyTime'):
-              self.cornerTexts[2]['7-Fg-StudyTime']['text'] = 'F: ' + self.formatDICOMTime(foregroundDicomDic['Study Time'])
+          if (backgroundDicomDic['Series Time'] != foregroundDicomDic['Series Time']):
+            if self.cornerTexts[2].has_key('6-Bg-SeriesTime'):
+              self.cornerTexts[2]['6-Bg-SeriesTime']['text'] = 'B: ' + self.formatDICOMTime(backgroundDicomDic['Series Time'])
+            if self.cornerTexts[2].has_key('7-Fg-SeriesTime'):
+              self.cornerTexts[2]['7-Fg-SeriesTime']['text'] = 'F: ' + self.formatDICOMTime(foregroundDicomDic['Series Time'])
           else:
-            if self.cornerTexts[2].has_key('6-Bg-StudyTime'):
-              self.cornerTexts[2]['6-Bg-StudyTime']['text'] = self.formatDICOMTime(backgroundDicomDic['Study Time'])
+            if self.cornerTexts[2].has_key('6-Bg-SeriesTime'):
+              self.cornerTexts[2]['6-Bg-SeriesTime']['text'] = self.formatDICOMTime(backgroundDicomDic['Series Time'])
 
           if (backgroundDicomDic['Series Description'] != foregroundDicomDic['Series Description']):
             if self.cornerTexts[2].has_key('8-Bg-SeriesDescription'):
@@ -928,8 +955,8 @@ class SliceAnnotations(object):
         self.cornerTexts[2]['2-PatientID']['text'] = 'ID: ' + dicomDic ['Patient ID']
         dicomDic['Patient Birth Date'] = self.formatDICOMDate(dicomDic['Patient Birth Date'])
         self.cornerTexts[2]['3-PatientInfo']['text'] = self.makePatientInfo(dicomDic)
-        self.cornerTexts[2]['4-Bg-StudyDate']['text'] = self.formatDICOMDate(dicomDic['Study Date'])
-        self.cornerTexts[2]['6-Bg-StudyTime']['text'] = self.formatDICOMTime(dicomDic['Study Time'])
+        self.cornerTexts[2]['4-Bg-SeriesDate']['text'] = self.formatDICOMDate(dicomDic['Series Date'])
+        self.cornerTexts[2]['6-Bg-SeriesTime']['text'] = self.formatDICOMTime(dicomDic['Series Time'])
         self.cornerTexts[2]['8-Bg-SeriesDescription']['text'] = dicomDic['Series Description']
 
       # top right corner annotation would be hidden if colorscalarbar is on and
@@ -1017,6 +1044,9 @@ class SliceAnnotations(object):
       sliceCornerAnnotation.SetText(i, cornerAnnotation)
       textProperty = sliceCornerAnnotation.GetTextProperty()
       textProperty.SetShadow(1)
+      self.renderers[self.currentSliceViewName].RemoveActor(sliceCornerAnnotation)
+      self.renderers[self.currentSliceViewName].AddActor(sliceCornerAnnotation)
+
     self.sliceViews[self.currentSliceViewName].scheduleRender()
 
   def resetTexts(self):
@@ -1027,8 +1057,8 @@ class SliceAnnotations(object):
   def extractDICOMValues(self,uid):
     p ={}
     tags = {
-    "0008,0020": "Study Date",
-    "0008,0030": "Study Time",
+    "0008,0021": "Series Date",
+    "0008,0031": "Series Time",
     "0008,0060": "Modality",
     "0008,0070": "Manufacturer",
     "0008,0080": "Institution Name",

Modules/Scripted/DataProbe/DataProbe.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-import qt, vtk
+import qt, vtk, ctk
 from __main__ import slicer
 import DataProbeLib
 
@@ -24,7 +24,7 @@ See <a>http://www.slicer.org</a> for details.  Module implemented by Steve Piepe
     # TODO: need a DataProbe icon
     #parent.icon = qt.QIcon(':Icons/XLarge/SlicerDownloadMRHead.png')
     self.parent = parent
-    self.infoWidget = 0
+    self.infoWidget = None
 
     if slicer.mrmlScene.GetTagByClassName( "vtkMRMLScriptedModuleNode" ) != 'ScriptedModule':
       slicer.mrmlScene.RegisterNodeClass(vtkMRMLScriptedModuleNode())
@@ -269,21 +269,8 @@ class DataProbeInfoWidget(object):
     Data Probe frame (lower left of slicer main window by default)"""
 
     # this method makes SliceView Annotation
-    self.sliceAnnotationsFrame = qt.QFrame(self.frame)
-    self.sliceAnnotationsFrame.setLayout(qt.QHBoxLayout())
-    self.frame.layout().addWidget(self.sliceAnnotationsFrame)
-
-    sliceAnnotationsLabel = qt.QLabel('Slice Annotations:')
-    self.sliceAnnotationsFrame.layout().addWidget(sliceAnnotationsLabel)
-    # Slice Annotations Settings Button
-    sliceAnnotationsSettings = qt.QPushButton()
-    settingsIcon = qt.QIcon("%s/SlicerAdvancedGear-Small.png" %self.iconsDIR)
-    sliceAnnotationsSettings.setIcon(settingsIcon)
-    self.sliceAnnotationsFrame.layout().addWidget(sliceAnnotationsSettings)
-
     self.sliceAnnotations = DataProbeLib.SliceAnnotations()
-    sliceAnnotationsSettings.connect('clicked()', self.sliceAnnotations.openSettingsPopup)
-    self.sliceAnnotationsFrame.layout().addStretch(1)
+
     # goto module button
     self.goToModule = qt.QPushButton('->', self.frame)
     self.goToModule.setToolTip('Go to the DataProbe module for more information and options')
@@ -360,7 +347,6 @@ class DataProbeWidget:
 
   def __init__(self, parent=None):
     self.observerTags = []
-
     if not parent:
       self.parent = slicer.qMRMLWidget()
       self.parent.setLayout(qt.QVBoxLayout())
@@ -389,7 +375,7 @@ class DataProbeWidget:
     self.reloadButton = qt.QPushButton("Reload")
     self.reloadButton.toolTip = "Reload this module."
     self.reloadButton.name = "DataProbe Reload"
-    self.layout.addWidget(self.reloadButton)
+    #self.layout.addWidget(self.reloadButton)
     self.reloadButton.connect('clicked()', self.onReload)
 
     # reload and test button
@@ -397,8 +383,17 @@ class DataProbeWidget:
     #  your module to users)
     self.reloadAndTestButton = qt.QPushButton("Reload and Test")
     self.reloadAndTestButton.toolTip = "Reload this module and then run the self tests."
-    self.layout.addWidget(self.reloadAndTestButton)
+    #self.layout.addWidget(self.reloadAndTestButton)
     self.reloadAndTestButton.connect('clicked()', self.onReloadAndTest)
+
+    settingsCollapsibleButton = ctk.ctkCollapsibleButton()
+    settingsCollapsibleButton.text = "Slice View Annotations Settings"
+    self.layout.addWidget(settingsCollapsibleButton)
+    settingsVBoxLayout = qt.QVBoxLayout(settingsCollapsibleButton)
+    dataProbeInstance = slicer.modules.DataProbeInstance
+    if dataProbeInstance.infoWidget:
+      sliceAnnotationsFrame = dataProbeInstance.infoWidget.sliceAnnotations.window
+      settingsVBoxLayout.addWidget(sliceAnnotationsFrame)
 
     self.parent.layout().addStretch(1)
 

Modules/Scripted/DataProbe/DataProbeLib/Resources/UI/settings.ui
@@ -34,13 +34,13 @@
      <item>
       <widget class="QCheckBox" name="sliceViewAnnotationsCheckBox">
        <property name="text">
-        <string>Slice View Annotations</string>
+        <string>Enable Slice View Annotations</string>
        </property>
       </widget>
      </item>
      <item>
-      <widget class="ctkCollapsibleButton" name="cornerTextParametersCollapsibleButton">
-       <property name="text">
+      <widget class="ctkCollapsibleButton" name="cornerTextParametersCollapsibleButton" native="true">
+       <property name="text" stdset="0">
         <string>Corner Text Annotation</string>
        </property>
        <layout class="QVBoxLayout" name="verticalLayout_4">
@@ -238,11 +238,11 @@
             <layout class="QVBoxLayout" name="verticalLayout_7">
              <item>
               <widget class="QCheckBox" name="backgroundLayerPersistenceCheckbox">
-               <property name="text">
-                <string>Background DICOM annotations persistence</string>
-               </property>
                <property name="toolTip">
                 <string>Show background volume DICOM annotations if foreground volume is non-DICOM.</string>
+               </property>
+               <property name="text">
+                <string>Background DICOM annotations persistence</string>
                </property>
               </widget>
              </item>
@@ -255,8 +255,8 @@
       </widget>
      </item>
      <item>
-      <widget class="ctkCollapsibleButton" name="scalingRulerCollapsibleButton">
-       <property name="text">
+      <widget class="ctkCollapsibleButton" name="scalingRulerCollapsibleButton" native="true">
+       <property name="text" stdset="0">
         <string>Scaling Ruler</string>
        </property>
        <layout class="QVBoxLayout" name="verticalLayout_5">
@@ -271,8 +271,8 @@
       </widget>
      </item>
      <item>
-      <widget class="ctkCollapsibleButton" name="colorScalarBarCollapsibleButton">
-       <property name="text">
+      <widget class="ctkCollapsibleButton" name="colorScalarBarCollapsibleButton" native="true">
+       <property name="text" stdset="0">
         <string>Color Scalar Bar</string>
        </property>
        <layout class="QVBoxLayout" name="verticalLayout_6">
@@ -330,31 +330,22 @@
          </widget>
         </item>
         <item>
-         <widget class="ctkCollapsibleGroupBox" name="widthSettingsCollapsibleGroupBox">
+         <widget class="ctkCollapsibleGroupBox" name="LabelsPropertiesCollapsibleGroupBox">
           <property name="title">
-           <string>Width Settings</string>
+           <string>Labels Properties</string>
           </property>
           <layout class="QHBoxLayout" name="horizontalLayout_4">
            <item>
             <widget class="QLabel" name="label_5">
              <property name="text">
-              <string>Maximum width (pixels):</string>
+              <string>Range Label Format:</string>
              </property>
             </widget>
            </item>
            <item>
-            <widget class="ctkSliderWidget" name="colorScalarBarMaxWidthSlider">
-             <property name="pageStep">
-              <double>10.000000000000000</double>
-             </property>
-             <property name="minimum">
-              <double>20.000000000000000</double>
-             </property>
-             <property name="maximum">
-              <double>200.000000000000000</double>
-             </property>
-             <property name="value">
-              <double>50.000000000000000</double>
+            <widget class="QLineEdit" name="rangeLabelFormatLineEdit">
+             <property name="text">
+              <string>%G</string>
              </property>
             </widget>
            </item>
@@ -416,11 +407,6 @@
    <extends>QGroupBox</extends>
    <header>ctkCollapsibleGroupBox.h</header>
    <container>1</container>
-  </customwidget>
-  <customwidget>
-   <class>ctkSliderWidget</class>
-   <extends>QWidget</extends>
-   <header>ctkSliderWidget.h</header>
   </customwidget>
  </customwidgets>
  <resources/>

commit 1a07e1e28ac27e6d3a5f2d2084f124449995b660

BUG: Ensure background SeriesTime corner annotation is displayed
Fix an issue originally introduced in 169df52840 ("ENH: Fix 3943 and other enhancements for slice view annotations", 2015-02-24)

Modules/Scripted/DataProbe/DataProbeLib/SliceViewAnnotations.py
@@ -70,7 +70,7 @@ class SliceAnnotations(VTKObservationMixin):
             "4-Bg-SeriesDate": {"text": "", "category": "B"},
             "5-Fg-SeriesDate": {"text": "", "category": "B"},
             "6-Bg-SeriesTime": {"text": "", "category": "C"},
-            "7-Bg-SeriesTime": {"text": "", "category": "C"},
+            "7-Fg-SeriesTime": {"text": "", "category": "C"},
             "8-Bg-SeriesDescription": {"text": "", "category": "C"},
             "9-Fg-SeriesDescription": {"text": "", "category": "C"},
         })