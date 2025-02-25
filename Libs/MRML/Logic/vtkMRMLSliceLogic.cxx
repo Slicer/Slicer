Slicer/Slicer

commit 26fd93d49e4ce957be4bdc3aa3ed38522254a141

BUG: Ensure blend pipeline is updated when setting operation Add or Subtract

Libs/MRML/Logic/vtkMRMLSliceLogic.cxx
@@ -176,14 +176,8 @@ struct BlendPipeline
       this->AddSubBackgroundCast->SetInputConnection(backgroundImagePort);
       this->AddSubExtractForegroundAlpha->SetInputConnection(foregroundImagePort);
       this->AddSubExtractBackgroundAlpha->SetInputConnection(backgroundImagePort);
-      if (sliceCompositing == vtkMRMLSliceCompositeNode::Add)
-      {
-        this->AddSubMath->SetOperationToAdd();
-      }
-      else
-      {
-        this->AddSubMath->SetOperationToSubtract();
-      }
+      // See UpdateAddSubOperation() for update to AddSubMath operation.
+
       // If clip to background is disabled, blending occurs over the entire extent
       // of all layers, not just within the background volume region.
       if (!clipToBackgroundVolume)
@@ -965,6 +959,30 @@ bool vtkMRMLSliceLogic::UpdateBlendLayers(vtkImageBlend* blend, const std::deque
 }
 
 //----------------------------------------------------------------------------
+bool vtkMRMLSliceLogic::UpdateAddSubOperation(vtkImageMathematics* addSubMath, int compositing)
+{
+  if (compositing != vtkMRMLSliceCompositeNode::Add && compositing != vtkMRMLSliceCompositeNode::Subtract)
+  {
+    vtkErrorWithObjectMacro(nullptr, << "UpdateAddSubOperation: Unexpected compositing mode (" << compositing << "). "
+                            << "Supported values are "
+                            << "Add (" << vtkMRMLSliceCompositeNode::Add << ") or "
+                            << "Subtract(" << vtkMRMLSliceCompositeNode::Subtract << ")");
+    return false;
+  }
+  vtkMTimeType oldAddSubMathMTime = addSubMath->GetMTime();
+  if (compositing == vtkMRMLSliceCompositeNode::Add)
+  {
+    addSubMath->SetOperationToAdd();
+  }
+  else
+  {
+    addSubMath->SetOperationToSubtract();
+  }
+  bool modified = (addSubMath->GetMTime() > oldAddSubMathMTime);
+  return modified;
+}
+
+//----------------------------------------------------------------------------
 bool vtkMRMLSliceLogic::UpdateFractions(vtkImageMathematics* fraction, double opacity)
 {
   vtkMTimeType oldMTime = fraction->GetMTime();
@@ -1103,6 +1121,16 @@ void vtkMRMLSliceLogic::UpdatePipeline()
     this->PipelineUVW->AddLayers(layersUVW, this->SliceCompositeNode->GetCompositing(), this->SliceCompositeNode->GetClipToBackgroundVolume(),
       backgroundImagePortUVW, foregroundImagePortUVW, this->SliceCompositeNode->GetForegroundOpacity(),
       labelImagePortUVW, this->SliceCompositeNode->GetLabelOpacity());
+
+    // Update add/subtract operations in the pipeline
+    if (vtkMRMLSliceLogic::UpdateAddSubOperation(this->Pipeline->AddSubMath.GetPointer(), this->SliceCompositeNode->GetCompositing()))
+    {
+      modified = 1;
+    }
+    if (vtkMRMLSliceLogic::UpdateAddSubOperation(this->PipelineUVW->AddSubMath.GetPointer(), this->SliceCompositeNode->GetCompositing()))
+    {
+      modified = 1;
+    }
 
     // Update opacity fractions for additional layers in add/subtract blending mode
     if (vtkMRMLSliceLogic::UpdateFractions(this->Pipeline->ForegroundFractionMath.GetPointer(), this->SliceCompositeNode->GetForegroundOpacity()))

Libs/MRML/Logic/vtkMRMLSliceLogic.h
@@ -408,6 +408,9 @@ protected:
   /// is a relatively expensive operation.
   static bool UpdateBlendLayers(vtkImageBlend* blend, const std::deque<SliceLayerInfo> &layers, bool clipToBackgroundVolume);
 
+  /// Helper to update the operation to perform based on compositing mode.
+  static bool UpdateAddSubOperation(vtkImageMathematics* addSubMath, int compositing);
+
   /// Helper to update foreground opacity when adding/subtracting the background layer
   static bool UpdateFractions(vtkImageMathematics* fraction, double opacity);
 

commit cafb3f4206521fca1f5a1d1de9fc9b61eb08c283

BUG: Avoid unnecessary error message in UpdateAddSubOperation
Fixes an issue introduced in 26fd93d49e ("BUG: Ensure blend pipeline is updated
when setting operation Add or Subtract", 2025-02-17). The function
`vtkMRMLSliceLogic::UpdateAddSubOperation` may be called with any compositing
mode, including unsupported ones. Instead of logging an error, silently return
when encountering an unsupported mode, as it does not require special handling.

Libs/MRML/Logic/vtkMRMLSliceLogic.cxx
@@ -963,10 +963,8 @@ bool vtkMRMLSliceLogic::UpdateAddSubOperation(vtkImageMathematics* addSubMath, i
 {
   if (compositing != vtkMRMLSliceCompositeNode::Add && compositing != vtkMRMLSliceCompositeNode::Subtract)
   {
-    vtkErrorWithObjectMacro(nullptr, << "UpdateAddSubOperation: Unexpected compositing mode (" << compositing << "). "
-                            << "Supported values are "
-                            << "Add (" << vtkMRMLSliceCompositeNode::Add << ") or "
-                            << "Subtract(" << vtkMRMLSliceCompositeNode::Subtract << ")");
+    // Silently return for unsupported compositing modes, as this function
+    // may be called with any mode.
     return false;
   }
   vtkMTimeType oldAddSubMathMTime = addSubMath->GetMTime();