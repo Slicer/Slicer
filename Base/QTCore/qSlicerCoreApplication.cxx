Slicer/Slicer

commit a984ec47b50aacbf2f6c732bc87eeec7245714a4

ENH: Make maximum file length configurable
Make maximum file length configurable, with default set to 50 on Windows and 1000 on other operating systems.
Users who do not need compatibility with Windows systems (or use short folder names) can choose to allow longer filenames to keep filenames matching node names.

fixes #8241

Base/QTCore/qSlicerCoreIOManager.cxx
@@ -23,6 +23,7 @@
 #include <QDir>
 #include <QElapsedTimer>
 #include <QFileInfo>
+#include <QSettings>
 
 // CTK includes
 #include <ctkUtils.h>
@@ -69,13 +70,18 @@ public:
     vtkMRMLScene* scene = nullptr
   ) const;
 
-  QSettings*        ExtensionFileType;
   QList<qSlicerFileReader*> Readers;
   QList<qSlicerFileWriter*> Writers;
   QMap<qSlicerIO::IOFileType, QStringList> FileTypes;
 
   QString DefaultSceneFileType;
+
+  // This is the default maximum length of a file name.
+  int DefaultMaximumFileNameLength{ 1000 };
 };
+
+CTK_GET_CPP(qSlicerCoreIOManager, int, defaultMaximumFileNameLength, DefaultMaximumFileNameLength);
+CTK_SET_CPP(qSlicerCoreIOManager, int, setDefaultMaximumFileNameLength, DefaultMaximumFileNameLength);
 
 //-----------------------------------------------------------------------------
 qSlicerCoreIOManagerPrivate::qSlicerCoreIOManagerPrivate() = default;
@@ -216,6 +222,19 @@ qSlicerCoreIOManager::qSlicerCoreIOManager(QObject* _parent)
   // constructor.
   qRegisterMetaType<qSlicerIO::IOFileType>("qSlicerIO::IOFileType");
   qRegisterMetaType<qSlicerIO::IOProperties>("qSlicerIO::IOProperties");
+
+  qSlicerCoreApplication* app = qSlicerCoreApplication::application();
+  QSettings* userSettings = app ? app->userSettings() : nullptr;
+  if (userSettings)
+  {
+    int maximumFileNameLength = userSettings->value("ioManager/MaximumFileNameLength", this->defaultMaximumFileNameLength()).toInt();
+    this->setDefaultMaximumFileNameLength(maximumFileNameLength);
+  }
+  else
+  {
+  qWarning() << Q_FUNC_INFO << ": failed to access application settings, using default defaultMaximumFileNameLength value";
+  }
+
 }
 
 //-----------------------------------------------------------------------------
@@ -486,16 +505,13 @@ QString qSlicerCoreIOManager::forceFileNameValidCharacters(const QString& filena
 }
 
 //-----------------------------------------------------------------------------
-QString qSlicerCoreIOManager::forceFileNameMaxLength(const QString& filename, int maxLength/*=-1*/)
+QString qSlicerCoreIOManager::forceFileNameMaxLength(const QString& filename, int extensionLength, int maxLength/*=-1*/)
 {
-  QString extension = this->extractKnownExtension(filename, nullptr);
-  return qSlicerCoreIOManager::forceFileNameMaxLengthExtension(filename, maxLength, extension.length());
-}
-
-//-----------------------------------------------------------------------------
-QString qSlicerCoreIOManager::forceFileNameMaxLengthExtension(const QString& filename, int extensionLength, int maxLength/*=-1*/)
-{
-  return QString::fromStdString(vtkMRMLStorageNode::ClampFileNameExtension(filename.toStdString(), maxLength, 4, extensionLength));
+  if (maxLength < 0)
+  {
+    maxLength = this->defaultMaximumFileNameLength();
+  }
+  return QString::fromStdString(vtkMRMLStorageNode::ClampFileName(filename.toStdString(), extensionLength, maxLength));
 }
 
 //-----------------------------------------------------------------------------

Base/QTCore/Testing/Cxx/qSlicerCoreIOManagerTest1.cxx
@@ -51,7 +51,10 @@ int TestLongNodeNameSaving(const char* temporaryDirectory)
   std::string extension = ".txt";
   std::string longFileName = longNodeName + extension;
   std::string safeFileName = qSlicerCoreIOManager::forceFileNameValidCharacters(QString::fromStdString(longFileName)).toStdString();
-  safeFileName = qSlicerCoreIOManager::forceFileNameMaxLengthExtension(QString::fromStdString(longFileName), extension.length()).toStdString();
+
+  qSlicerCoreIOManager ioManager;
+  ioManager.setDefaultMaximumFileNameLength(25);
+  safeFileName = ioManager.forceFileNameMaxLength(QString::fromStdString(longFileName), extension.length()).toStdString();
 
   vtkSmartPointer<vtkMRMLTextNode> textNode = vtkMRMLTextNode::SafeDownCast(
     scene->AddNewNodeByClass("vtkMRMLTextNode", longNodeName));

Base/QTCore/qSlicerCoreIOManager.h
@@ -50,6 +50,7 @@ class Q_SLICER_BASE_QTCORE_EXPORT qSlicerCoreIOManager:public QObject
 {
   Q_OBJECT;
   Q_PROPERTY(QString defaultSceneFileType READ defaultSceneFileType WRITE setDefaultSceneFileType)
+  Q_PROPERTY(int defaultMaximumFileNameLength READ defaultMaximumFileNameLength WRITE setDefaultMaximumFileNameLength)
 
 public:
   qSlicerCoreIOManager(QObject* parent = nullptr);
@@ -110,13 +111,16 @@ public:
   Q_INVOKABLE static QString forceFileNameValidCharacters(const QString& filename);
 
   /// Clamp the length of a filename to a maximum number of characters.
-  /// The file extension will be detected and excluded from the specified maximum length.
-  /// \sa qSlicerCoreIOManager::stripKnownExtension()
-  Q_INVOKABLE QString forceFileNameMaxLength(const QString& filename, int maxLength=-1);
-
-  /// Clamp the length of a filename to a maximum number of characters.
   /// The length of the filename extension must also be specified so that it is not included in the shortened section.
-  Q_INVOKABLE static QString forceFileNameMaxLengthExtension(const QString& filename, int extensionLength, int maxLength=-1);
+  /// If the extension is not known then extractKnownExtension() method can be used to get the extension.
+  /// If maxLength is not specified, the default maximum length (defaultMaximumFileNameLength) is used.
+  /// \sa extractKnownExtension(), defaultMaximumFileNameLength()
+  Q_INVOKABLE QString forceFileNameMaxLength(const QString& filename, int extensionLength, int maxLength=-1);
+
+  /// Default maximum length for a filename. It is used when maximum filename length is enforced by calling
+  /// forceFileNameMaxLength() without providing a specific maximum length value.
+  /// \sa forceFileNameMaxLength(), setDefaultMaximumFileNameLength()
+  int defaultMaximumFileNameLength()const;
 
   /// If \a fileName ends with an extension that is associated with \a object,
   /// then return that extension. Otherwise return an empty string.
@@ -282,6 +286,10 @@ public slots:
   /// Valid options are defined in qSlicerSceneWriter (for example, "MRML Scene (.mrml)"
   /// or "Medical Reality Bundle (.mrb)").
   void setDefaultSceneFileType(QString);
+
+  /// Default maximum length for a filename.
+  /// \sa forceFileNameMaxLength()
+  void setDefaultMaximumFileNameLength(int);
 
 signals:
 

Base/QTGUI/Resources/UI/qSlicerSettingsGeneralPanel.ui
@@ -225,6 +225,29 @@ Default: {documentationbaseurl}/user_guide/modules/{lowercasemodulename}.html</s
      </property>
     </widget>
    </item>
+   <item row="12" column="0">
+    <widget class="QLabel" name="MaxFileNameLengthLabel">
+     <property name="text">
+      <string>Maximum filename length:</string>
+     </property>
+    </widget>
+   </item>
+   <item row="12" column="1">
+    <widget class="QSpinBox" name="MaximumFileNameLengthSpinBox">
+     <property name="toolTip">
+      <string>Limit the maximum length of filenames. For compatibility with Windows systems, a low value such as 50 is recommended. Set a higher value to allow using longer filenames that match long node names.</string>
+     </property>
+     <property name="minimum">
+      <number>8</number>
+     </property>
+     <property name="maximum">
+      <number>4000</number>
+     </property>
+     <property name="value">
+      <number>1000</number>
+     </property>
+    </widget>
+   </item>
   </layout>
  </widget>
  <customwidgets>

Base/QTGUI/qSlicerExportNodeDialog.cxx
@@ -359,7 +359,7 @@ QString qSlicerExportNodeDialogPrivate::defaultFilename(vtkMRMLNode* node, QStri
   }
 
   QString safeNodeName = qSlicerCoreIOManager::forceFileNameValidCharacters(unsafeNodeName);
-  safeNodeName = qSlicerCoreIOManager::forceFileNameMaxLengthExtension(safeNodeName, 0);
+  safeNodeName = coreIOManager->forceFileNameMaxLength(safeNodeName, 0);
   return qSlicerExportNodeDialogPrivate::forceFileNameExtension(safeNodeName, extension, node);
 }
 
@@ -1089,6 +1089,13 @@ void qSlicerExportNodeDialogPrivate::formatChangedSlot()
 //-----------------------------------------------------------------------------
 QString qSlicerExportNodeDialogPrivate::recommendedFilename(vtkMRMLStorableNode* node) const
 {
+  qSlicerCoreIOManager* coreIOManager = qSlicerCoreApplication::application()->coreIOManager();
+  if (!coreIOManager)
+  {
+    qCritical() << Q_FUNC_INFO << "failed: Core IO manager not found.";
+    return QString();
+  }
+
   if (!this->FilenameLineEdit->isEnabled())
   {
     qWarning() << Q_FUNC_INFO << "should not be used when the filename text box is disabled.";
@@ -1104,7 +1111,7 @@ QString qSlicerExportNodeDialogPrivate::recommendedFilename(vtkMRMLStorableNode*
   }
 
   // If the filename is too long, suggest a shorter one.
-  QString shortenedFilename = qSlicerCoreIOManager::forceFileNameMaxLengthExtension(this->FilenameLineEdit->text(), extension.length());
+  QString shortenedFilename = coreIOManager->forceFileNameMaxLength(this->FilenameLineEdit->text(), extension.length());
   return qSlicerExportNodeDialogPrivate::forceFileNameExtension(shortenedFilename, extension, node);
 }
 

Base/QTGUI/qSlicerSaveDataDialog.cxx
@@ -82,7 +82,15 @@ QString qSlicerFileNameItemDelegate::forceFileNameExtension(const QString& fileN
                                                    vtkMRMLScene* mrmlScene, const QString& nodeID)
 {
   QString strippedFileName = qSlicerCoreIOManager::forceFileNameValidCharacters(fileName);
-  strippedFileName = qSlicerCoreIOManager::forceFileNameMaxLengthExtension(strippedFileName, extension.length());
+
+  qSlicerCoreIOManager* coreIOManager = qSlicerCoreApplication::application()->coreIOManager();
+  if (!coreIOManager)
+  {
+    qCritical() << Q_FUNC_INFO << "failed: Core IO manager not found.";
+    return strippedFileName;
+  }
+
+  strippedFileName = coreIOManager->forceFileNameMaxLength(strippedFileName, extension.length());
 
   if(!mrmlScene)
   {
@@ -99,8 +107,6 @@ QString qSlicerFileNameItemDelegate::forceFileNameExtension(const QString& fileN
     qCritical() << Q_FUNC_INFO << " failed: node not found by ID " << qPrintable(nodeID);
     return QString();
   }
-  qSlicerCoreIOManager* coreIOManager =
-    qSlicerCoreApplication::application()->coreIOManager();
   strippedFileName = coreIOManager->stripKnownExtension(strippedFileName, object) + extension;
   return strippedFileName;
 }
@@ -871,21 +877,25 @@ bool qSlicerSaveDataDialogPrivate::saveNodes()
         snode->GetUserMessages()->AddMessage(vtkCommand::ErrorEvent,
           (qSlicerSaveDataDialog::tr("Cannot write data file: %1.").arg(file.absoluteFilePath())).toStdString());
 
-        // Add warning messages if the file name or path is too long
-        if (file.fileName().length() > vtkMRMLStorageNode::GetRecommendedFileNameLength())
+#ifdef Q_OS_WIN
+        // On Windows, writing may have failed due to too long path.
+        // Let the user know via a warning message.
+
+        // If filename is very long then most likely the user should shorten that.
+        if (file.fileName().length() > 50)
         {
           snode->GetUserMessages()->AddMessage(vtkCommand::WarningEvent,
-            (qSlicerSaveDataDialog::tr("File name may be too long: %1.").arg(file.fileName())).toStdString());
+            (qSlicerSaveDataDialog::tr("File writing may have failed because filename is too long: '%1'").arg(file.fileName())).toStdString());
         }
-        // Maximum file path is 260 characters on most Windows systems. Warn the user if the path
-        // is long and therefore may fail to be saved or cause compatibility issues later.
-        // The warning limit is set to somewhat lower than the 260 limit, to add some safety margin
-        // (e.g., to avoid issues when the file is moved into another folder).
+        // Maximum file path is 260 characters on most Windows systems, but during saving extra temporary folders may be used,
+        // therefore warn the user when getting near the maximum.
         if (file.absoluteFilePath().length() > 200)
         {
           snode->GetUserMessages()->AddMessage(vtkCommand::WarningEvent,
-            (qSlicerSaveDataDialog::tr("File path may be too long: %1.").arg(file.absoluteFilePath())).toStdString());
+            (qSlicerSaveDataDialog::tr("File writing may have failed because the output folder name is too long: '%1'")
+              .arg(file.absoluteFilePath())).toStdString());
         }
+#endif
 
         this->updateStatusIconFromStorageNode(row, success);
       }

Base/QTGUI/qSlicerSettingsGeneralPanel.cxx
@@ -33,6 +33,7 @@
 // QtGUI includes
 #include "qSlicerApplication.h"
 #include "qSlicerApplicationUpdateManager.h"
+#include "qSlicerCoreIOManager.h"
 #include "qSlicerRelativePathMapper.h"
 #include "qSlicerSettingsGeneralPanel.h"
 #include "ui_qSlicerSettingsGeneralPanel.h"
@@ -198,6 +199,23 @@ void qSlicerSettingsGeneralPanelPrivate::init()
                       SIGNAL(valueChanged(int)),
                       qSlicerSettingsGeneralPanel::tr("Max. number of 'Recent' menu items"),
                       ctkSettingsPanel::OptionRequireRestart);
+
+#ifdef Q_OS_WIN
+  // Total path length limit on Windows is 260 characters, need to keep the filename
+  // length way below that to avoid file saving issues with long paths.
+  this->MaximumFileNameLengthSpinBox->setValue(50);
+#else
+  // Non-Windows systems should have no problem dealing with files with this long name.
+  this->MaximumFileNameLengthSpinBox->setValue(1000);
+#endif
+
+  q->registerProperty("ioManager/MaximumFileNameLength", this->MaximumFileNameLengthSpinBox, /*no tr*/"value",
+    SIGNAL(valueChanged(int)),
+    qSlicerSettingsGeneralPanel::tr("Max. filename length"));
+
+  // Actions to propagate to the application when settings are changed
+  QObject::connect(this->MaximumFileNameLengthSpinBox, SIGNAL(valueChanged(int)),
+    q, SLOT(setMaximumFileNameLength(int)));
 }
 
 // --------------------------------------------------------------------------
@@ -279,4 +297,14 @@ void qSlicerSettingsGeneralPanel::updateAutoUpdateApplicationFromManager()
   QSignalBlocker blocker1(d->ApplicationAutoUpdateCheckCheckBox);
   d->ApplicationAutoUpdateCheckCheckBox->setChecked(app->applicationUpdateManager()->autoUpdateCheck());
 #endif
+}
+
+// --------------------------------------------------------------------------
+void qSlicerSettingsGeneralPanel::setMaximumFileNameLength(int length)
+{
+  qSlicerCoreIOManager* coreIOManager = qSlicerCoreApplication::application()->coreIOManager();
+  if (coreIOManager)
+  {
+    coreIOManager->setDefaultMaximumFileNameLength(length);
+  }
 }

Base/QTGUI/qSlicerSettingsGeneralPanel.h
@@ -50,6 +50,7 @@ public:
 public slots:
   void setDefaultScenePath(const QString& path);
   void openSlicerRCFile();
+  void setMaximumFileNameLength(int length);
 
 protected slots:
   void updateAutoUpdateApplicationFromManager();

commit 2958822578b96cdb8fbcf39fc86d463272d2fcc5

BUG: Fix core IO manager initialization
Addresses a regression introduced in a984ec47b50 ("ENH: Make maximum file
length configurable", 2025-02-20)

- Move the retrieval of `MaximumFileNameLength` from `qSlicerCoreIOManager`'s
  constructor to `qSlicerCoreApplicationPrivate::initDataIO()`, ensuring proper
  initialization after the application object is fully set up.

- Remove redundant settings initialization from `qSlicerCoreIOManager`'s constructor.

- Ensure `qSlicerApplication` correctly applies the `MaximumFileNameLength` setting
  from user preferences when creating the IO Manager.

- Add fallback warning when application settings are inaccessible.

This change ensures consistent settings initialization and avoids potential issues
due to premature access in `qSlicerCoreIOManager`.

It fixes the following tests:
- `qSlicerApplicationUpdateManagerTest`
- `qSlicerCamerasModuleWidgetTest1`
- `qSlicerCoreApplicationTest1`
- `qSlicerCoreIOManagerTest1`
- `qSlicerExtensionsManagerModelTest`
- `qSlicerModuleFactoryManagerTest1`

See https://github.com/Slicer/Slicer/issues/8284

Base/QTCore/qSlicerCoreApplication.cxx
@@ -652,6 +652,20 @@ void qSlicerCoreApplicationPrivate::initDataIO()
   this->DataIOManagerLogic->SetMRMLApplicationLogic(this->AppLogic);
   this->DataIOManagerLogic->SetAndObserveDataIOManager(
     this->MRMLRemoteIOLogic->GetDataIOManager());
+
+  if (!this->CoreIOManager.isNull())
+  {
+    QSettings* userSettings = q->userSettings();
+    if (userSettings)
+    {
+      int maximumFileNameLength = userSettings->value("ioManager/MaximumFileNameLength", this->CoreIOManager->defaultMaximumFileNameLength()).toInt();
+      this->CoreIOManager->setDefaultMaximumFileNameLength(maximumFileNameLength);
+    }
+    else
+    {
+      qWarning() << Q_FUNC_INFO << ": failed to access application settings, using default defaultMaximumFileNameLength value";
+    }
+  }
 }
 
 //-----------------------------------------------------------------------------

Base/QTCore/qSlicerCoreIOManager.cxx
@@ -222,19 +222,6 @@ qSlicerCoreIOManager::qSlicerCoreIOManager(QObject* _parent)
   // constructor.
   qRegisterMetaType<qSlicerIO::IOFileType>("qSlicerIO::IOFileType");
   qRegisterMetaType<qSlicerIO::IOProperties>("qSlicerIO::IOProperties");
-
-  qSlicerCoreApplication* app = qSlicerCoreApplication::application();
-  QSettings* userSettings = app ? app->userSettings() : nullptr;
-  if (userSettings)
-  {
-    int maximumFileNameLength = userSettings->value("ioManager/MaximumFileNameLength", this->defaultMaximumFileNameLength()).toInt();
-    this->setDefaultMaximumFileNameLength(maximumFileNameLength);
-  }
-  else
-  {
-  qWarning() << Q_FUNC_INFO << ": failed to access application settings, using default defaultMaximumFileNameLength value";
-  }
-
 }
 
 //-----------------------------------------------------------------------------

Base/QTGUI/qSlicerApplication.cxx
@@ -527,6 +527,16 @@ qSlicerApplication::qSlicerApplication(int &_argc, char **_argv)
   //       qSlicerIOManager is not added to the constructor initialization list.
   //       Indeed, internally qSlicerIOManager registers qSlicerDataDialog, ...
   d->CoreIOManager = QSharedPointer<qSlicerIOManager>(new qSlicerIOManager);
+  QSettings* userSettings = this->userSettings();
+  if (userSettings)
+  {
+    int maximumFileNameLength = userSettings->value("ioManager/MaximumFileNameLength", d->CoreIOManager->defaultMaximumFileNameLength()).toInt();
+    d->CoreIOManager->setDefaultMaximumFileNameLength(maximumFileNameLength);
+  }
+  else
+  {
+    qWarning() << Q_FUNC_INFO << ": failed to access application settings, using default defaultMaximumFileNameLength value";
+  }
 }
 
 //-----------------------------------------------------------------------------