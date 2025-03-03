Slicer/Slicer

commit 20bd9d5a4d8adbe36704ed141a2e5153401b239e

ENH: Use subject hierarchy widget as node selector in core modules
There are currently two ways of selecting edited node in Slicer core modules:
- subject hierarchy tree: Models, Markups
- node combobox: Segmentations, Transforms, Volumes, Volume rendering

This commit makes Segmentations, Transforms, Volumes, Volume rendering modules to use a subject hierarchy tree for node selection, as it makes it easier to select a node and allows seeing some more context.

fixes #7533

Libs/MRML/Widgets/qMRMLClipNodeDisplayWidget.cxx
@@ -97,7 +97,7 @@ vtkMRMLDisplayNode* qMRMLClipNodeDisplayWidget::mrmlDisplayNode() const
 //------------------------------------------------------------------------------
 void qMRMLClipNodeDisplayWidget::setMRMLDisplayNode(vtkMRMLNode* node)
 {
-  this->setMRMLDisplayNode(vtkMRMLClipNode::SafeDownCast(node));
+  this->setMRMLDisplayNode(vtkMRMLDisplayNode::SafeDownCast(node));
 }
 
 //------------------------------------------------------------------------------
@@ -109,7 +109,6 @@ void qMRMLClipNodeDisplayWidget::setMRMLDisplayNode(vtkMRMLDisplayNode* clipNode
   d->MRMLDisplayNode = clipNode;
   this->updateWidgetFromMRML();
 }
-
 
 //------------------------------------------------------------------------------
 void qMRMLClipNodeDisplayWidget::updateWidgetFromMRML()

Modules/Loadable/Segmentations/Resources/UI/qSlicerSegmentationsModule.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>360</width>
-    <height>909</height>
+    <width>370</width>
+    <height>946</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -30,43 +30,99 @@
     <number>4</number>
    </property>
    <item>
+    <widget class="ctkExpandableWidget" name="ResizableFrame_2">
+     <property name="orientations">
+      <set>Qt::Vertical</set>
+     </property>
+     <property name="sizeGripInside">
+      <bool>false</bool>
+     </property>
+     <layout class="QVBoxLayout" name="verticalLayout_9">
+      <property name="spacing">
+       <number>4</number>
+      </property>
+      <property name="leftMargin">
+       <number>0</number>
+      </property>
+      <property name="topMargin">
+       <number>0</number>
+      </property>
+      <property name="rightMargin">
+       <number>0</number>
+      </property>
+      <property name="bottomMargin">
+       <number>0</number>
+      </property>
+      <item>
+       <widget class="qMRMLSubjectHierarchyTreeView" name="MRMLNodeSelector_Segmentation">
+        <property name="dragDropMode">
+         <enum>QAbstractItemView::InternalMove</enum>
+        </property>
+        <property name="indentation">
+         <number>12</number>
+        </property>
+        <property name="editMenuActionVisible">
+         <bool>false</bool>
+        </property>
+        <property name="addNodeMenuActionVisible">
+         <bool>true</bool>
+        </property>
+        <property name="multiSelection">
+         <bool>true</bool>
+        </property>
+        <property name="nodeTypes">
+         <stringlist notr="true">
+          <string>vtkMRMLSegmentationNode</string>
+         </stringlist>
+        </property>
+        <property name="hideChildNodeTypes">
+         <stringlist notr="true"/>
+        </property>
+        <property name="idColumnVisible">
+         <bool>false</bool>
+        </property>
+        <property name="colorColumnVisible">
+         <bool>false</bool>
+        </property>
+        <property name="transformColumnVisible">
+         <bool>false</bool>
+        </property>
+        <property name="pluginAllowList">
+         <stringlist notr="true">
+          <string>Segmentations</string>
+          <string>Folder</string>
+          <string>Visibility</string>
+         </stringlist>
+        </property>
+        <property name="excludeItemAttributeNamesFilter">
+         <stringlist notr="true">
+          <string>segmentID</string>
+         </stringlist>
+        </property>
+       </widget>
+      </item>
+     </layout>
+    </widget>
+   </item>
+   <item>
     <layout class="QGridLayout" name="gridLayout_4">
      <property name="spacing">
       <number>4</number>
      </property>
-     <item row="0" column="0">
-      <widget class="QLabel" name="label_ActiveSegmentation">
+     <item row="0" column="1">
+      <widget class="QLabel" name="label_ReferenceVolumeName">
        <property name="text">
-        <string> Segmentation:</string>
+        <string/>
        </property>
       </widget>
      </item>
-     <item row="1" column="0">
+     <item row="0" column="0">
       <widget class="QLabel" name="label_ReferenceVolumeText">
        <property name="toolTip">
         <string>Node that was used for setting the segmentation geometry (origin, spacing, axis directions, and default extent)</string>
        </property>
        <property name="text">
         <string> Source geometry:</string>
-       </property>
-      </widget>
-     </item>
-     <item row="0" column="1">
-      <widget class="qMRMLNodeComboBox" name="MRMLNodeComboBox_Segmentation">
-       <property name="nodeTypes">
-        <stringlist notr="true">
-         <string>vtkMRMLSegmentationNode</string>
-        </stringlist>
-       </property>
-       <property name="renameEnabled">
-        <bool>true</bool>
-       </property>
-      </widget>
-     </item>
-     <item row="1" column="1">
-      <widget class="QLabel" name="label_ReferenceVolumeName">
-       <property name="text">
-        <string/>
        </property>
       </widget>
      </item>
@@ -878,6 +934,11 @@
    <header>qMRMLSegmentationShow3DButton.h</header>
   </customwidget>
   <customwidget>
+   <class>qMRMLSubjectHierarchyTreeView</class>
+   <extends>QTreeView</extends>
+   <header>qMRMLSubjectHierarchyTreeView.h</header>
+  </customwidget>
+  <customwidget>
    <class>qMRMLSubjectHierarchyComboBox</class>
    <extends>ctkComboBox</extends>
    <header>qMRMLSubjectHierarchyComboBox.h</header>
@@ -923,22 +984,6 @@
   <connection>
    <sender>qSlicerSegmentationsModule</sender>
    <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
-   <receiver>MRMLNodeComboBox_Segmentation</receiver>
-   <slot>setMRMLScene(vtkMRMLScene*)</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>230</x>
-     <y>3</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>234</x>
-     <y>12</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>qSlicerSegmentationsModule</sender>
-   <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
    <receiver>MRMLNodeComboBox_OtherSegmentationOrRepresentationNode</receiver>
    <slot>setMRMLScene(vtkMRMLScene*)</slot>
    <hints>
@@ -947,24 +992,8 @@
      <y>3</y>
     </hint>
     <hint type="destinationlabel">
-     <x>329</x>
-     <y>632</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>SegmentsTableView</sender>
-   <signal>selectionChanged(QItemSelection,QItemSelection)</signal>
-   <receiver>SegmentationDisplayNodeWidget</receiver>
-   <slot>onSegmentSelectionChanged(QItemSelection,QItemSelection)</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>157</x>
-     <y>136</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>145</x>
-     <y>456</y>
+     <x>359</x>
+     <y>673</y>
     </hint>
    </hints>
   </connection>
@@ -979,8 +1008,8 @@
      <y>634</y>
     </hint>
     <hint type="destinationlabel">
-     <x>326</x>
-     <y>739</y>
+     <x>355</x>
+     <y>785</y>
     </hint>
    </hints>
   </connection>
@@ -995,8 +1024,56 @@
      <y>415</y>
     </hint>
     <hint type="destinationlabel">
-     <x>178</x>
-     <y>816</y>
+     <x>186</x>
+     <y>865</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>qSlicerSegmentationsModule</sender>
+   <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
+   <receiver>SubjectHierarchyComboBox_ImportExport</receiver>
+   <slot>setMRMLScene(vtkMRMLScene*)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>262</x>
+     <y>3</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>324</x>
+     <y>758</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>qSlicerSegmentationsModule</sender>
+   <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
+   <receiver>ColorTableNodeSelector</receiver>
+   <slot>setMRMLScene(vtkMRMLScene*)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>169</x>
+     <y>431</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>354</x>
+     <y>787</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>qSlicerSegmentationsModule</sender>
+   <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
+   <receiver>SegmentationDisplayNodeWidget</receiver>
+   <slot>setMRMLScene(vtkMRMLScene*)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>179</x>
+     <y>454</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>179</x>
+     <y>401</y>
     </hint>
    </hints>
   </connection>
@@ -1017,50 +1094,34 @@
    </hints>
   </connection>
   <connection>
-   <sender>qSlicerSegmentationsModule</sender>
-   <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
-   <receiver>SubjectHierarchyComboBox_ImportExport</receiver>
-   <slot>setMRMLScene(vtkMRMLScene*)</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>262</x>
-     <y>3</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>275</x>
-     <y>708</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>qSlicerSegmentationsModule</sender>
-   <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
-   <receiver>ColorTableNodeSelector</receiver>
-   <slot>setMRMLScene(vtkMRMLScene*)</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>169</x>
-     <y>431</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>325</x>
-     <y>741</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>qSlicerSegmentationsModule</sender>
-   <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
+   <sender>SegmentsTableView</sender>
+   <signal>selectionChanged(QItemSelection,QItemSelection)</signal>
    <receiver>SegmentationDisplayNodeWidget</receiver>
+   <slot>onSegmentSelectionChanged(QItemSelection,QItemSelection)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>161</x>
+     <y>276</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>145</x>
+     <y>456</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>qSlicerSegmentationsModule</sender>
+   <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
+   <receiver>MRMLNodeSelector_Segmentation</receiver>
    <slot>setMRMLScene(vtkMRMLScene*)</slot>
    <hints>
     <hint type="sourcelabel">
-     <x>179</x>
-     <y>454</y>
+     <x>1</x>
+     <y>822</y>
     </hint>
     <hint type="destinationlabel">
-     <x>179</x>
-     <y>401</y>
+     <x>69</x>
+     <y>35</y>
     </hint>
    </hints>
   </connection>

Modules/Loadable/Segmentations/qSlicerSegmentationsModuleWidget.cxx
@@ -185,7 +185,7 @@ void qSlicerSegmentationsModuleWidget::onEnter()
   this->qvtkConnect(this->mrmlScene(), vtkMRMLScene::EndRestoreEvent,
                     this, SLOT(onMRMLSceneEndRestoreEvent()));
 
-  this->onSegmentationNodeChanged( d->MRMLNodeComboBox_Segmentation->currentNode() );
+  this->onSegmentationNodeChanged(d->MRMLNodeSelector_Segmentation->currentNode());
 
   d->populateTerminologyContextComboBox();
 }
@@ -202,7 +202,7 @@ vtkMRMLSegmentationDisplayNode* qSlicerSegmentationsModuleWidget::segmentationDi
   Q_D(qSlicerSegmentationsModuleWidget);
 
   vtkMRMLSegmentationNode* segmentationNode =  vtkMRMLSegmentationNode::SafeDownCast(
-    d->MRMLNodeComboBox_Segmentation->currentNode() );
+    d->MRMLNodeSelector_Segmentation->currentNode() );
   if (!segmentationNode)
   {
     return nullptr;
@@ -284,7 +284,7 @@ void qSlicerSegmentationsModuleWidget::updateCopyMoveButtonStates()
 
   // Set button states that copy/move from current segmentation
   vtkMRMLSegmentationNode* currentSegmentationNode =  vtkMRMLSegmentationNode::SafeDownCast(
-    d->MRMLNodeComboBox_Segmentation->currentNode() );
+    d->MRMLNodeSelector_Segmentation->currentNode() );
   if (currentSegmentationNode && currentSegmentationNode->GetSegmentation()->GetNumberOfSegments() > 0)
   {
     d->toolButton_MoveFromCurrentSegmentation->setEnabled(true);
@@ -311,13 +311,13 @@ void qSlicerSegmentationsModuleWidget::init()
   d->ImportExportTypeButtonGroup->addButton(d->radioButton_Model);
 
   // Make connections
-  connect(d->MRMLNodeComboBox_Segmentation, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
+  connect(d->MRMLNodeSelector_Segmentation, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
     this, SLOT(onSegmentationNodeChanged(vtkMRMLNode*)) );
-  connect(d->MRMLNodeComboBox_Segmentation, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
+  connect(d->MRMLNodeSelector_Segmentation, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
     d->SegmentsTableView, SLOT(setSegmentationNode(vtkMRMLNode*)) );
-  connect(d->MRMLNodeComboBox_Segmentation, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
+  connect(d->MRMLNodeSelector_Segmentation, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
     d->SegmentsTableView_Current, SLOT(setSegmentationNode(vtkMRMLNode*)) );
-  connect(d->MRMLNodeComboBox_Segmentation, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
+  connect(d->MRMLNodeSelector_Segmentation, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
     d->RepresentationsListView, SLOT(setSegmentationNode(vtkMRMLNode*)) );
 
   connect(d->SegmentsTableView, SIGNAL(selectionChanged(QItemSelection,QItemSelection)),
@@ -349,7 +349,7 @@ void qSlicerSegmentationsModuleWidget::init()
     this, SLOT(onExportColorTableChanged()));
 
   d->ExportToFilesWidget->setSettingsKey("ExportSegmentsToFiles");
-  connect(d->MRMLNodeComboBox_Segmentation, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
+  connect(d->MRMLNodeSelector_Segmentation, SIGNAL(currentNodeChanged(vtkMRMLNode*)),
     d->ExportToFilesWidget, SLOT(setSegmentationNode(vtkMRMLNode*)));
 
   connect(d->toolButton_MoveFromCurrentSegmentation, SIGNAL(clicked()),
@@ -423,7 +423,7 @@ void qSlicerSegmentationsModuleWidget::selectSegmentationNode(vtkMRMLSegmentatio
 {
   Q_D(qSlicerSegmentationsModuleWidget);
 
-  d->MRMLNodeComboBox_Segmentation->setCurrentNode(segmentationNode);
+  d->MRMLNodeSelector_Segmentation->setCurrentNode(segmentationNode);
 }
 
 //-----------------------------------------------------------------------------
@@ -446,7 +446,7 @@ void qSlicerSegmentationsModuleWidget::onAddSegment()
   Q_D(qSlicerSegmentationsModuleWidget);
 
   vtkMRMLSegmentationNode* currentSegmentationNode =  vtkMRMLSegmentationNode::SafeDownCast(
-    d->MRMLNodeComboBox_Segmentation->currentNode() );
+    d->MRMLNodeSelector_Segmentation->currentNode() );
   if (!currentSegmentationNode)
   {
     qWarning() << Q_FUNC_INFO << ": No segmentation selected";
@@ -491,7 +491,7 @@ void qSlicerSegmentationsModuleWidget::onEditSegmentation()
 {
   Q_D(qSlicerSegmentationsModuleWidget);
 
-  if (!d->MRMLNodeComboBox_Segmentation->currentNode())
+  if (!d->MRMLNodeSelector_Segmentation->currentNode())
   {
     qCritical() << Q_FUNC_INFO << ": Invalid segmentation";
     return;
@@ -520,7 +520,7 @@ void qSlicerSegmentationsModuleWidget::onEditSegmentation()
       qCritical() << Q_FUNC_INFO << ": MRMLNodeComboBox_Segmentation is not found in Segment Editor module";
       return;
     }
-    nodeSelector->setCurrentNode(d->MRMLNodeComboBox_Segmentation->currentNode());
+    nodeSelector->setCurrentNode(d->MRMLNodeSelector_Segmentation->currentNode());
 
     // Get segments table and select segment
     qMRMLSegmentsTableView* segmentsTable = moduleWidget->findChild<qMRMLSegmentsTableView*>("SegmentsTableView");
@@ -539,7 +539,7 @@ void qSlicerSegmentationsModuleWidget::onRemoveSelectedSegments()
   Q_D(qSlicerSegmentationsModuleWidget);
 
   vtkMRMLSegmentationNode* currentSegmentationNode =  vtkMRMLSegmentationNode::SafeDownCast(
-    d->MRMLNodeComboBox_Segmentation->currentNode() );
+    d->MRMLNodeSelector_Segmentation->currentNode() );
   if (!currentSegmentationNode)
   {
     qCritical() << Q_FUNC_INFO << ": No segmentation selected";
@@ -815,7 +815,7 @@ bool qSlicerSegmentationsModuleWidget::copySegmentsBetweenSegmentations(bool cop
   Q_D(qSlicerSegmentationsModuleWidget);
 
   vtkMRMLSegmentationNode* currentSegmentationNode =  vtkMRMLSegmentationNode::SafeDownCast(
-    d->MRMLNodeComboBox_Segmentation->currentNode() );
+    d->MRMLNodeSelector_Segmentation->currentNode() );
   if (!currentSegmentationNode)
   {
     qWarning() << Q_FUNC_INFO << ": No current segmentation is selected";
@@ -879,7 +879,7 @@ bool qSlicerSegmentationsModuleWidget::exportFromCurrentSegmentation()
   }
 
   vtkMRMLSegmentationNode* currentSegmentationNode =  vtkMRMLSegmentationNode::SafeDownCast(
-    d->MRMLNodeComboBox_Segmentation->currentNode() );
+    d->MRMLNodeSelector_Segmentation->currentNode() );
   if (!currentSegmentationNode || !currentSegmentationNode->GetSegmentation())
   {
     qWarning() << Q_FUNC_INFO << ": No segmentation selected";
@@ -1031,7 +1031,7 @@ bool qSlicerSegmentationsModuleWidget::importToCurrentSegmentation()
   Q_D(qSlicerSegmentationsModuleWidget);
 
   vtkMRMLSegmentationNode* currentSegmentationNode = vtkMRMLSegmentationNode::SafeDownCast(
-    d->MRMLNodeComboBox_Segmentation->currentNode());
+    d->MRMLNodeSelector_Segmentation->currentNode());
   if (!currentSegmentationNode)
   {
     qWarning() << Q_FUNC_INFO << ": No segmentation selected";
@@ -1175,7 +1175,7 @@ bool qSlicerSegmentationsModuleWidget::setEditedNode(
   Q_UNUSED(context);
   if (vtkMRMLSegmentationNode::SafeDownCast(node))
   {
-    d->MRMLNodeComboBox_Segmentation->setCurrentNode(node);
+    d->MRMLNodeSelector_Segmentation->setCurrentNode(node);
     return true;
   }
   return false;

commit 11f7ef9d4dda6f120f254bd0f91e27f505d6b687

BUG: Fix qSlicerTransformsModuleWidgetTest
Addresses a regression introduced in 20bd9d5a4d8 ("ENH: Use subject hierarchy widget as node selector in core modules", 2024-12-29)

The Transforms module widget now uses a Subject Hierarchy tree widget, which requires initialization of the subject hierarchy module.
This was missing from the test, therefore the Transforms module widget test failed. Fixed by adding subject hierarchy module initialization to the test.

fixes #8285

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

Modules/Loadable/Transforms/Testing/Cxx/qSlicerTransformsModuleWidgetTest.cxx
@@ -25,10 +25,20 @@
 #include "ctkTest.h"
 
 // MRML includes
+#include <qMRMLWidget.h>
 #include "qSlicerTransformsModule.h"
 #include "qSlicerTransformsModuleWidget.h"
 #include <vtkMRMLScene.h>
 #include <vtkMRMLTransformNode.h>
+
+// SubjectHierarchy includes
+#include <qSlicerSubjectHierarchyPluginHandler.h>
+#include <qSlicerSubjectHierarchyPluginLogic.h>
+#include <qSlicerSubjectHierarchyTransformsPlugin.h>
+#include <vtkSlicerSubjectHierarchyModuleLogic.h>
+
+// Slicer includes
+#include <qSlicerApplication.h>
 
 // VTK includes
 #include <vtkMatrix4x4.h>
@@ -49,14 +59,26 @@ private slots:
 void qSlicerTransformsModuleWidgetTester::testIdentity()
 {
   vtkNew<vtkMRMLScene> scene;
-  vtkNew<vtkMRMLTransformNode> transformNode;
-  scene->AddNode(transformNode.GetPointer());
+
+  // Set up Subject Hierarchy logic (needed for the subject hierarchy tree view)
+  vtkNew<vtkSlicerSubjectHierarchyModuleLogic> shModuleLogic;
+  shModuleLogic->SetMRMLScene(scene);
+  QScopedPointer<qSlicerSubjectHierarchyPluginLogic> pluginLogic(new qSlicerSubjectHierarchyPluginLogic());
+  pluginLogic->setMRMLScene(scene);
+  qSlicerSubjectHierarchyPluginHandler::instance()->setPluginLogic(pluginLogic.data());
+  qSlicerSubjectHierarchyPluginHandler::instance()->setMRMLScene(scene);
 
   qSlicerTransformsModule transformsModule;
+  transformsModule.initialize(qSlicerApplication::application()->applicationLogic());  // register qSlicerSubjectHierarchyTransformsPlugin
   transformsModule.setMRMLScene(scene.GetPointer());
   transformsModule.logic();
   qSlicerTransformsModuleWidget* transformsWidget =
     dynamic_cast<qSlicerTransformsModuleWidget*>(transformsModule.widgetRepresentation());
+
+  vtkNew<vtkMRMLTransformNode> transformNode;
+  scene->AddNode(transformNode.GetPointer());
+
+  transformsWidget->setEditedNode(transformNode);
 
   vtkNew<vtkMatrix4x4> matrix;
   transformNode->GetMatrixTransformToParent(matrix.GetPointer());
@@ -75,14 +97,26 @@ void qSlicerTransformsModuleWidgetTester::testIdentity()
 void qSlicerTransformsModuleWidgetTester::testInvert()
 {
   vtkNew<vtkMRMLScene> scene;
-  vtkNew<vtkMRMLTransformNode> transformNode;
-  scene->AddNode(transformNode.GetPointer());
+
+  // Set up Subject Hierarchy logic (needed for the subject hierarchy tree view)
+  vtkNew<vtkSlicerSubjectHierarchyModuleLogic> shModuleLogic;
+  shModuleLogic->SetMRMLScene(scene);
+  QScopedPointer<qSlicerSubjectHierarchyPluginLogic> pluginLogic(new qSlicerSubjectHierarchyPluginLogic());
+  pluginLogic->setMRMLScene(scene);
+  qSlicerSubjectHierarchyPluginHandler::instance()->setPluginLogic(pluginLogic.data());
+  qSlicerSubjectHierarchyPluginHandler::instance()->setMRMLScene(scene);
 
   qSlicerTransformsModule transformsModule;
+  transformsModule.initialize(qSlicerApplication::application()->applicationLogic());  // register qSlicerSubjectHierarchyTransformsPlugin
   transformsModule.setMRMLScene(scene.GetPointer());
   transformsModule.logic();
   qSlicerTransformsModuleWidget* transformsWidget =
     dynamic_cast<qSlicerTransformsModuleWidget*>(transformsModule.widgetRepresentation());
+
+  vtkNew<vtkMRMLTransformNode> transformNode;
+  scene->AddNode(transformNode.GetPointer());
+
+  transformsWidget->setEditedNode(transformNode);
 
   vtkNew<vtkMatrix4x4> matrix;
   transformNode->GetMatrixTransformToParent(matrix.GetPointer());
@@ -98,5 +132,14 @@ void qSlicerTransformsModuleWidgetTester::testInvert()
 }
 
 // ----------------------------------------------------------------------------
-CTK_TEST_MAIN(qSlicerTransformsModuleWidgetTest)
+//CTK_TEST_MAIN(qSlicerTransformsModuleWidgetTest)
+int qSlicerTransformsModuleWidgetTest(int argc, char* argv[])
+{
+  qMRMLWidget::preInitializeApplication();
+  qSlicerApplication app(argc, argv);
+  qMRMLWidget::postInitializeApplication();
+  qSlicerTransformsModuleWidgetTester tc;
+  return QTest::qExec(&tc, argc, argv);
+}
+
 #include "moc_qSlicerTransformsModuleWidgetTest.cxx"