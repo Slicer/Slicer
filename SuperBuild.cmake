7625610668738922b5fc71541b751d0eab5d0234
Hans Johnson <hans-johnson@uiowa.edu>
2025-06-23 13:31:19 -0500

    WIP: external packages needing updates for find_package(Python3).

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -203,7 +203,8 @@ endmacro()
 
 Slicer_Remote_Add(vtkAddon
   GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/Slicer/vtkAddon"
-  GIT_TAG b1fa5034077fc04b10457fe25004c65af6091a37
+  # GIT_TAG b1fa5034077fc04b10457fe25004c65af6091a37
+  GIT_TAG update-python-find-package
   OPTION_NAME Slicer_BUILD_vtkAddon
   )
 list_conditional_append(Slicer_BUILD_vtkAddon Slicer_REMOTE_DEPENDENCIES vtkAddon)
