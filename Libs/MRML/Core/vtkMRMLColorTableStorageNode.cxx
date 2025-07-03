9383fbe327bfc2f04bdd089289179b4aa48da99f
Hans Johnson <hans-johnson@uiowa.edu>
2025-07-03 16:19:33 -0500

    COMP: Call `.c_str()` explicitly for conversion
    Libs/MRML/Core/vtkMRMLColorTableStorageNode.cxx:298:28: warning: operator const char *' is deprecated: Call `.c_str()` explicitly
    Libs/MRML/Core/vtkMRMLColorTableStorageNode.cxx:305:28: warning: operator const char *' is deprecated: Call `.c_str()` explicitly
    Libs/MRML/Core/vtkMRMLColorTableStorageNode.cxx:311:28: warning: operator const char *' is deprecated: Call `.c_str()` explicitly
    Libs/MRML/Core/vtkMRMLColorTableStorageNode.cxx:318:28: warning: operator const char *' is deprecated: Call `.c_str()` explicitly
    Libs/MRML/Core/vtkMRMLColorTableStorageNode.cxx:329:10: warning: operator const char *' is deprecated: Call `.c_str()` explicitly
    Libs/MRML/Core/vtkMRMLColorTableStorageNode.cxx:336:10: warning: operator const char *' is deprecated: Call `.c_str()` explicitly

--- Libs/MRML/Core/vtkMRMLColorTableStorageNode.cxx
+++ Libs/MRML/Core/vtkMRMLColorTableStorageNode.cxx
@@ -32,8 +32,7 @@ Version:   $Revision: 1.6 $
 // STD includes
 #include <sstream>
 
-const std::vector<std::string> TERMINOLOGY_COLUMN_NAMES = {
-  "Category_CodingScheme", "Category_CodeValue", "Category_CodeMeaning",
+const std::vector<std::string> TERMINOLOGY_COLUMN_NAMES = { "Category_CodingScheme",       "Category_CodeValue",       "Category_CodeMeaning",
   "Type_CodingScheme", "Type_CodeValue", "Type_CodeMeaning",
   "TypeModifier_CodingScheme", "TypeModifier_CodeValue", "TypeModifier_CodeMeaning",
   "Region_CodingScheme", "Region_CodeValue", "Region_CodeMeaning",
@@ -107,16 +106,17 @@ int vtkMRMLColorTableStorageNode::ReadDataInternal(vtkMRMLNode* refNode)
   std::string fullName = this->GetFullNameFromFileName();
   if (fullName.empty())
   {
-    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadDataInternal",
-      "Filename is not specified (" << (this->ID ? this->ID : "(unknown)") << ").");
+    vtkErrorToMessageCollectionMacro(
+      this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadDataInternal", "Filename is not specified (" << (this->ID ? this->ID : "(unknown)") << ").");
     return 0;
   }
 
   // check that the file exists
   if (vtksys::SystemTools::FileExists(fullName.c_str()) == false)
   {
-    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadDataInternal",
-      "Color table file '" << fullName.c_str() << "' is not found while trying to read node (" << (this->ID ? this->ID : "(unknown)") << ").");
+    vtkErrorToMessageCollectionMacro(this->GetUserMessages(),
+                                     "vtkMRMLColorTableStorageNode::ReadDataInternal",
+                                     "Color table file '" << fullName.c_str() << "' is not found while trying to read node (" << (this->ID ? this->ID : "(unknown)") << ").");
     return 0;
   }
 
@@ -124,8 +124,10 @@ int vtkMRMLColorTableStorageNode::ReadDataInternal(vtkMRMLNode* refNode)
   std::string extension = vtkMRMLStorageNode::GetLowercaseExtensionFromFileName(fullName);
   if (extension.empty())
   {
-    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadDataInternal",
-      "Color table file '" << fullName.c_str() << "' has no file extension while trying to read node (" << (this->ID ? this->ID : "(unknown)") << ").");
+    vtkErrorToMessageCollectionMacro(this->GetUserMessages(),
+                                     "vtkMRMLColorTableStorageNode::ReadDataInternal",
+                                     "Color table file '" << fullName.c_str() << "' has no file extension while trying to read node (" << (this->ID ? this->ID : "(unknown)")
+                                                          << ").");
     return 0;
   }
 
@@ -141,8 +143,10 @@ int vtkMRMLColorTableStorageNode::ReadDataInternal(vtkMRMLNode* refNode)
   }
   else
   {
-    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadDataInternal",
-      "Color table file '" << fullName.c_str() << "' has unknown file extension while trying to read node (" << (this->ID ? this->ID : "(unknown)") << ").");
+    vtkErrorToMessageCollectionMacro(this->GetUserMessages(),
+                                     "vtkMRMLColorTableStorageNode::ReadDataInternal",
+                                     "Color table file '" << fullName.c_str() << "' has unknown file extension while trying to read node (" << (this->ID ? this->ID : "(unknown)")
+                                                          << ").");
     return 0;
   }
 }
@@ -162,8 +166,9 @@ std::string vtkMRMLColorTableStorageNode::GetFieldDelimiterCharacters(std::strin
   }
   else
   {
-    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::GetFieldDelimiterCharacters",
-      "Cannot determine field delimiter character from file extension: '" << lowercaseFileExt << "'.");
+    vtkErrorToMessageCollectionMacro(this->GetUserMessages(),
+                                     "vtkMRMLColorTableStorageNode::GetFieldDelimiterCharacters",
+                                     "Cannot determine field delimiter character from file extension: '" << lowercaseFileExt << "'.");
   }
   return fieldDelimiterCharacters;
 }
@@ -189,23 +194,21 @@ int vtkMRMLColorTableStorageNode::ReadCsvFile(std::string fullFileName, vtkMRMLC
   }
   catch (...)
   {
-    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile",
-      "Failed to read color table file: '" << fullFileName << "'.");
+    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile", "Failed to read color table file: '" << fullFileName << "'.");
     return 0;
   }
 
   if (!table)
   {
-    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile",
-      "Failed to read color table file: '" << fullFileName << "'.");
+    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile", "Failed to read color table file: '" << fullFileName << "'.");
     return 0;
   }
 
   vtkStringArray* labelValueColumn = vtkStringArray::SafeDownCast(table->GetColumnByName("LabelValue"));
   if (!labelValueColumn)
   {
-    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile",
-      "Failed to read color table file: '" << fullFileName << "' due to missing 'LabelValue' column.");
+    vtkErrorToMessageCollectionMacro(
+      this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile", "Failed to read color table file: '" << fullFileName << "' due to missing 'LabelValue' column.");
     return 0;
   }
 
@@ -219,9 +222,12 @@ int vtkMRMLColorTableStorageNode::ReadCsvFile(std::string fullFileName, vtkMRMLC
     vtkStringArray* colorColumn = vtkStringArray::SafeDownCast(table->GetColumnByName(colorColumnName.c_str()));
     if (!colorColumn && colorColumnName != colorColumnNames[3]) // alpha is optional, do not log warning
     {
-      vtkWarningToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile",
+      vtkWarningToMessageCollectionMacro(
+        this->GetUserMessages(),
+        "vtkMRMLColorTableStorageNode::ReadCsvFile",
         vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode", "Column '%1' was not found in color table file: '%2'. Color component is set to 0."),
-          colorColumnName.c_str(), fullFileName.c_str()));
+                            colorColumnName.c_str(),
+                            fullFileName.c_str()));
     }
     colorColumns.push_back(colorColumn);
   }
@@ -249,29 +255,34 @@ int vtkMRMLColorTableStorageNode::ReadCsvFile(std::string fullFileName, vtkMRMLC
     int fileRow = row + 1; // file starts with a header row
     if (labelValueColumn->GetValue(row).empty())
     {
-      vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile",
-        vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode",
-          "labelValue is not specified in color file in line %1. Skipping this line."),
-          std::to_string(fileRow).c_str()).c_str());
+      vtkErrorToMessageCollectionMacro(
+        this->GetUserMessages(),
+        "vtkMRMLColorTableStorageNode::ReadCsvFile",
+        vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode", "labelValue is not specified in color file in line %1. Skipping this line."), std::to_string(fileRow).c_str())
+          .c_str());
       continue;
     }
     int labelValue = labelValueColumn->GetVariantValue(row).ToInt(&valid);
     if (!valid)
     {
-      vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile",
-        vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode",
-          "labelValue '%1' is invalid in color file in line %2. Skipping this line."),
-          labelValueColumn->GetValue(row),
-          std::to_string(fileRow).c_str()).c_str());
+      vtkErrorToMessageCollectionMacro(this->GetUserMessages(),
+                                       "vtkMRMLColorTableStorageNode::ReadCsvFile",
+                                       vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode", "labelValue '%1' is invalid in color file in line %2. Skipping this line."),
+                                                           labelValueColumn->GetValue(row).c_str(),
+                                                           std::to_string(fileRow).c_str())
+                                         .c_str());
       continue;
     }
     if (labelValue < 0)
     {
-      vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile",
+      vtkErrorToMessageCollectionMacro(this->GetUserMessages(),
+                                       "vtkMRMLColorTableStorageNode::ReadCsvFile",
         vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode",
-          "labelValue '%1' is invalid in color file in line %2: the value must be positive (>0). Skipping this line."),
-          labelValueColumn->GetValue(row),
-          std::to_string(fileRow).c_str()).c_str());
+                                                                     "labelValue '%1' is invalid in color file in line %2: the value must be positive "
+                                                                     "(>0). Skipping this line."),
+                                                           labelValueColumn->GetValue(row).c_str(),
+                                                           std::to_string(fileRow).c_str())
+                                         .c_str());
       continue;
     }
     validLabelValues[row] = labelValue;
@@ -281,10 +292,11 @@ int vtkMRMLColorTableStorageNode::ReadCsvFile(std::string fullFileName, vtkMRMLC
     }
     if (maxLabelValue > this->MaximumColorID)
     {
-      vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile",
-        vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode",
-          "labelValue '%1' is invalid in color file in line %2: the value must not go above be %3."),
-          labelValueColumn->GetValue(row),
+      vtkErrorToMessageCollectionMacro(
+        this->GetUserMessages(),
+        "vtkMRMLColorTableStorageNode::ReadCsvFile",
+        vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode", "labelValue '%1' is invalid in color file in line %2: the value must not go above be %3."),
+                            labelValueColumn->GetValue(row).c_str(),
           std::to_string(fileRow).c_str(),
           std::to_string(this->MaximumColorID).c_str()
           ).c_str());
@@ -306,9 +318,12 @@ int vtkMRMLColorTableStorageNode::ReadCsvFile(std::string fullFileName, vtkMRMLC
   // Define helper function for populating terminology entry IDs
   auto GetIndexInEntryForIdType = [](std::string idType)
   {
-    if (idType == "CodingScheme") return 0;
-    else if (idType == "CodeValue") return 1;
-    else if (idType == "CodeMeaning") return 2;
+    if (idType == "CodingScheme")
+      return 0;
+    else if (idType == "CodeValue")
+      return 1;
+    else if (idType == "CodeMeaning")
+      return 2;
     vtkGenericWarningMacro("vtkMRMLColorTableStorageNode::ReadCsvFile::GetIndexInEntryForIdType: Invalid coded entry ID type" << idType);
     return 0;
   };
@@ -338,9 +353,9 @@ int vtkMRMLColorTableStorageNode::ReadCsvFile(std::string fullFileName, vtkMRMLC
         }
         else
         {
-          vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile",
-            vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode",
-              "Failed to read color from column %1 in line %2, value: '%3'"),
+          vtkErrorToMessageCollectionMacro(this->GetUserMessages(),
+                                           "vtkMRMLColorTableStorageNode::ReadCsvFile",
+                                           vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode", "Failed to read color from column %1 in line %2, value: '%3'"),
               colorColumnNames[i].c_str(),
               std::to_string(lineNumber).c_str(),
               colorColumn->GetValue(row).c_str()));
@@ -356,11 +371,14 @@ int vtkMRMLColorTableStorageNode::ReadCsvFile(std::string fullFileName, vtkMRMLC
     }
     if (colorNode->SetColor(validLabelValues[row], name.c_str(), rgba[0], rgba[1], rgba[2], rgba[3]) == 0)
     {
-      vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::ReadCsvFile",
-        vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode",
-          "Unable to set color '%1' with name '%2', stopped processing after '%3' lines in the file %4."),
-          std::to_string(validLabelValues[row]).c_str(), name.c_str(),
-          std::to_string(numberOfTuples).c_str(), fullFileName.c_str()));
+      vtkErrorToMessageCollectionMacro(
+        this->GetUserMessages(),
+        "vtkMRMLColorTableStorageNode::ReadCsvFile",
+        vtkMRMLI18N::Format(vtkMRMLTr("vtkMRMLColorTableStorageNode", "Unable to set color '%1' with name '%2', stopped processing after '%3' lines in the file %4."),
+                            std::to_string(validLabelValues[row]).c_str(),
+                            name.c_str(),
+                            std::to_string(numberOfTuples).c_str(),
+                            fullFileName.c_str()));
       return 0;
     }
 
@@ -468,9 +486,7 @@ int vtkMRMLColorTableStorageNode::ReadCtblFile(std::string fullFileName, vtkMRML
       // sanity check: does the procedural header match?
       if (line.compare(0, 23, "# Color procedural file") == 0)
       {
-        vtkErrorMacro("ReadDataInternal:\nfound a comment that this file "
-          << " is a procedural color file, returning:\n"
-          << line);
+        vtkErrorMacro("ReadDataInternal:\nfound a comment that this file " << " is a procedural color file, returning:\n" << line);
         return 0;
       }
     }
@@ -501,8 +517,7 @@ int vtkMRMLColorTableStorageNode::ReadCtblFile(std::string fullFileName, vtkMRML
   vtkDebugMacro("The largest id is " << maxID);
   if (maxID > this->MaximumColorID)
   {
-    vtkErrorMacro("ReadData: maximum color id " << maxID << " is > "
-      << this->MaximumColorID << ", invalid color file: " << fullFileName);
+    vtkErrorMacro("ReadData: maximum color id " << maxID << " is > " << this->MaximumColorID << ", invalid color file: " << fullFileName);
     colorNode->SetNumberOfColors(0);
     return 0;
   }
@@ -573,8 +588,7 @@ int vtkMRMLColorTableStorageNode::ReadCtblFile(std::string fullFileName, vtkMRML
       size_t firstnottick = name.find_first_not_of("'");
       size_t lastnottick = name.find_last_not_of("'");
       std::string withoutTicks = name.substr(firstnottick, (lastnottick - firstnottick) + 1);
-      vtkDebugMacro("ReadDataInternal: Single quotation marks around name \"" << name
-        << "\", using name without quotation marks instead:  \"" << withoutTicks << "\"");
+      vtkDebugMacro("ReadDataInternal: Single quotation marks around name \"" << name << "\", using name without quotation marks instead:  \"" << withoutTicks << "\"");
       name = withoutTicks;
     }
     if (i < 10)
@@ -583,15 +597,14 @@ int vtkMRMLColorTableStorageNode::ReadCtblFile(std::string fullFileName, vtkMRML
     }
     if (colorNode->SetColor(id, name.c_str(), r, g, b, a) == 0)
     {
-      vtkWarningMacro("ReadData: unable to set color " << id << " with name " << name.c_str()
-        << ", breaking the loop over " << lines.size() << " lines in the file " << this->FileName);
+      vtkWarningMacro("ReadData: unable to set color " << id << " with name " << name.c_str() << ", breaking the loop over " << lines.size() << " lines in the file "
+                                                       << this->FileName);
       return 0;
     }
   }
   if (lines.size() > 0 && !biggerThanOne)
   {
-    vtkWarningMacro("ReadDataInternal: possibly malformed color table file:\n" << this->FileName
-      << ".\n\tNo RGB values are greater than 1. Valid values are 0-255");
+    vtkWarningMacro("ReadDataInternal: possibly malformed color table file:\n" << this->FileName << ".\n\tNo RGB values are greater than 1. Valid values are 0-255");
   }
 
   return 1;
@@ -626,8 +639,10 @@ int vtkMRMLColorTableStorageNode::WriteDataInternal(vtkMRMLNode* refNode)
   }
   else
   {
-    vtkErrorToMessageCollectionMacro(this->GetUserMessages(), "vtkMRMLColorTableStorageNode::WriteDataInternal",
-      "Color table file '" << fullName.c_str() << "' has unknown file extension while trying to write node (" << (this->ID ? this->ID : "(unknown)") << ").");
+    vtkErrorToMessageCollectionMacro(this->GetUserMessages(),
+                                     "vtkMRMLColorTableStorageNode::WriteDataInternal",
+                                     "Color table file '" << fullName.c_str() << "' has unknown file extension while trying to write node (" << (this->ID ? this->ID : "(unknown)")
+                                                          << ").");
     return 0;
   }
 
@@ -657,17 +672,15 @@ int vtkMRMLColorTableStorageNode::WriteCsvFile(std::string fullFileName, vtkMRML
   colorArray->SetNumberOfComponents(4); // RGBA
   colorArray->SetNumberOfTuples(numberOfColors);
   colorTable->AddColumn(colorArray);
-  std::map<vtkIdType, std::vector<std::string>> componentNamesMap = {
-    {colorTable->GetColumnIndex("Color"), {"R", "G", "B", "A"}} };
+  std::map<vtkIdType, std::vector<std::string>> componentNamesMap = { { colorTable->GetColumnIndex("Color"), { "R", "G", "B", "A" } } };
 
   typedef vtkCodedEntry* (vtkMRMLColorNode::*GetEntryFuncPtr)(int);
   std::vector<GetEntryFuncPtr> terminologyGetEntryFuncVector = {
-    &vtkMRMLColorNode::GetTerminologyCategory, &vtkMRMLColorNode::GetTerminologyCategory, &vtkMRMLColorNode::GetTerminologyCategory,
-    &vtkMRMLColorNode::GetTerminologyType, &vtkMRMLColorNode::GetTerminologyType, &vtkMRMLColorNode::GetTerminologyType,
-    &vtkMRMLColorNode::GetTerminologyTypeModifier, &vtkMRMLColorNode::GetTerminologyTypeModifier, &vtkMRMLColorNode::GetTerminologyTypeModifier,
-    &vtkMRMLColorNode::GetTerminologyRegion, &vtkMRMLColorNode::GetTerminologyRegion, &vtkMRMLColorNode::GetTerminologyRegion,
-    &vtkMRMLColorNode::GetTerminologyRegionModifier, &vtkMRMLColorNode::GetTerminologyRegionModifier,
-    &vtkMRMLColorNode::GetTerminologyRegionModifier
+    &vtkMRMLColorNode::GetTerminologyCategory,       &vtkMRMLColorNode::GetTerminologyCategory,       &vtkMRMLColorNode::GetTerminologyCategory,
+    &vtkMRMLColorNode::GetTerminologyType,           &vtkMRMLColorNode::GetTerminologyType,           &vtkMRMLColorNode::GetTerminologyType,
+    &vtkMRMLColorNode::GetTerminologyTypeModifier,   &vtkMRMLColorNode::GetTerminologyTypeModifier,   &vtkMRMLColorNode::GetTerminologyTypeModifier,
+    &vtkMRMLColorNode::GetTerminologyRegion,         &vtkMRMLColorNode::GetTerminologyRegion,         &vtkMRMLColorNode::GetTerminologyRegion,
+    &vtkMRMLColorNode::GetTerminologyRegionModifier, &vtkMRMLColorNode::GetTerminologyRegionModifier, &vtkMRMLColorNode::GetTerminologyRegionModifier
   };
 
   std::vector<vtkStringArray*> terminologyArrays;
@@ -727,8 +740,7 @@ int vtkMRMLColorTableStorageNode::WriteCsvFile(std::string fullFileName, vtkMRML
 
   colorTable->SetNumberOfRows(rowIndex);
 
-  return vtkMRMLTableStorageNode::WriteTable(fullFileName, colorTable,
-    this->GetFieldDelimiterCharacters(fullFileName), componentNamesMap);
+  return vtkMRMLTableStorageNode::WriteTable(fullFileName, colorTable, this->GetFieldDelimiterCharacters(fullFileName), componentNamesMap);
 }
 
 //----------------------------------------------------------------------------
