Slicer/Slicer

commit 406beac7a2583eed3cc91513f7aaa314d6668cc9

ENH: Update JsonCpp from 0.10.6 to 1.9.6
List of JsonCpp changes:

```
$ git shortlog 0.10.6..1.9.6 --no-merges

Aaron Jacobs (1):
      Remove undefined behavior from a left shift of a negative value.

Abigail Bunyan (1):
      Add missing classes to forwards.h

Adam Boseley (1):
      By default use <prefix> relative paths when installing

Alex Beregszaszi (2):
      Document skipBom in CharReaderBuilder (#1332)
      Introduce CharReaderBuilder::ecma404Mode (#1333)

Alexander Gazarov (1):
      Replaced the template-based solution for avoiding calls to localeconv() with a macro-based one (fixes #527)

Alexander V. Brezgin (4):
      Remove repeated condition
      Optimize value range check
      Optimize value check
      Optimize Value::isIntegral() method

Andrea Pappacoda (1):
      build(meson): use find_program('python3') (#1386)

Andrew Childs (1):
      json_writer: fix inverted sense in isAnyCharRequiredQuoting (#1120)

Andrey Okoshkin (3):
      Fix Value::copyPayload() and Value::copy() (#704)
      Add basic test for Value::copy() (#704)
      Make Value copy constructor simplier

Anton Indrawan (2):
      Compiles jsoncpp with QNX 6.6
      Fix QNX build: QNX defines sprintf under the std namespace. Use snprintf instead

Autofuzz team (1):
      Add a simple fuzz test for jsoncpp.

Baptiste Jonglez (1):
      Fix compilation errors for downstream projects caused by incorrect pkgconfig paths

Bartosz Brachaczek (1):
      Opportunistically take advantage of C++20 move-in/out-of stringstream (#1457)

Ben Boeckel (5):
      reader: export CharReader::Factory
      json_writer: improve isfinite support on *nix
      amalgamate: add version.h and allocator.h to the forwards header
      version.h: fix the version number in the header
      test: ensure the version numbers agree

Ben Wolsieffer (1):
      Fix generation of pkg-config file with absolute includedir/libdir. (#1199)

Benjamin Knecht (3):
      Create format string with sprintf.

SuperBuild/External_JsonCpp.cmake
@@ -25,7 +25,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "73b8e172d6615251ef851d883ef02f163e7075b2" # slicer-v0.10.6-2016-04-22
+    "89e2973c754a9c02a49974d839779b151e95afd6" # slicer-v1.9.6-2024-09-09
     QUIET
     )
 
@@ -54,6 +54,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT Slicer_USE_SYSTEM_${proj})
       -DJSONCPP_WITH_CMAKE_PACKAGE:BOOL=ON
       -DBUILD_SHARED_LIBS:BOOL=ON
       -DBUILD_STATIC_LIBS:BOOL=OFF
+      -DCMAKE_INSTALL_LIBDIR:STRING=lib  # Override value set in GNUInstallDirs CMake module
       -DLIBRARY_INSTALL_DIR:PATH=${Slicer_INSTALL_LIB_DIR}
       -DRUNTIME_INSTALL_DIR:PATH=${Slicer_INSTALL_LIB_DIR}
       -DARCHIVE_INSTALL_DIR:PATH=${Slicer_INSTALL_LIB_DIR}
@@ -80,12 +81,12 @@ if(NOT DEFINED ${proj}_DIR AND NOT Slicer_USE_SYSTEM_${proj})
   else()
     set(lib_cfg_dir ".")
   endif()
-  set(${proj}_LIBRARY ${${proj}_DIR}/src/lib_json/${lib_cfg_dir}/${lib_prefix}jsoncpp.${lib_ext})
+  set(${proj}_LIBRARY ${${proj}_DIR}/lib/${lib_cfg_dir}/${lib_prefix}jsoncpp.${lib_ext})
 
   #-----------------------------------------------------------------------------
   # Launcher setting specific to build tree
 
-  set(${proj}_LIBRARY_PATHS_LAUNCHER_BUILD ${JsonCpp_DIR}/src/lib_json/<CMAKE_CFG_INTDIR>)
+  set(${proj}_LIBRARY_PATHS_LAUNCHER_BUILD ${JsonCpp_DIR}/lib/<CMAKE_CFG_INTDIR>)
   mark_as_superbuild(
     VARS ${proj}_LIBRARY_PATHS_LAUNCHER_BUILD
     LABELS "LIBRARY_PATHS_LAUNCHER_BUILD"

commit c3f26475bc1e20f0a288776072f672bb0079fc39

BUG: Fix runtime loading of JsonCpp library from build tree on Windows
Fixes a regression introduced in commit 406beac7a25 ("ENH: Update JsonCpp
from 0.10.6 to 1.9.6", 2025-06-19), which caused modules depending on JsonCpp
(e.g., VolumeRendering) to fail loading when running Slicer from the build
tree on Windows.

The problem was due to the incorrect assumption that the JsonCpp library would
be located under `lib/`, while on Windows it is actually placed under `bin/`.

Co-authored-by: James Butler <james.butler@revvity.com>

SuperBuild/External_JsonCpp.cmake
@@ -86,7 +86,11 @@ if(NOT DEFINED ${proj}_DIR AND NOT Slicer_USE_SYSTEM_${proj})
   #-----------------------------------------------------------------------------
   # Launcher setting specific to build tree
 
-  set(${proj}_LIBRARY_PATHS_LAUNCHER_BUILD ${JsonCpp_DIR}/lib/<CMAKE_CFG_INTDIR>)
+  set(_library_output_subdir bin)
+  if(UNIX)
+    set(_library_output_subdir lib)
+  endif()
+  set(${proj}_LIBRARY_PATHS_LAUNCHER_BUILD ${JsonCpp_DIR}/${_library_output_subdir}/<CMAKE_CFG_INTDIR>)
   mark_as_superbuild(
     VARS ${proj}_LIBRARY_PATHS_LAUNCHER_BUILD
     LABELS "LIBRARY_PATHS_LAUNCHER_BUILD"