Slicer/Slicer

commit d36781511cf38c2c4483051dbe41085ba4f6c70a

COMP: Refactor QDir assignment to use setPath for consistency
- Updated `QDir` assignments in test files to utilize `setPath()` instead of direct object assignment.
- Ensured improved clarity and compatibility with the latest Qt practices.

Base/QTCore/qSlicerExtensionsManagerModel.cxx:932:23: warning: function setPath() has to be used in Qt6
Base/QTCore/Testing/Cxx/qSlicerApplicationUpdateManagerTest.cxx:122:7: warning: function setPath() has to be used in Qt6
Base/QTCore/Testing/Cxx/qSlicerApplicationUpdateManagerTest.cxx:90:12: warning: function setPath() has to be used in Qt6
Base/QTCore/Testing/Cxx/qSlicerExtensionsManagerModelTest.cxx:329:12: warning: function setPath() has to be used in Qt6
Base/QTCore/Testing/Cxx/qSlicerExtensionsManagerModelTest.cxx:432:14: warning: function setPath() has to be used in Qt6
Base/QTCore/Testing/Cxx/qSlicerExtensionsManagerModelTest.cxx:526:43: warning: Use QMultiHash for maps storing multiple values with the same key.
Base/QTCore/Testing/Cxx/qSlicerExtensionsManagerModelTest.cxx:559:54: warning: Use QMultiHash for maps storing multiple values with the same key.
Base/QTGUI/Testing/Cxx/qSlicerScriptedLoadableModuleTest.cxx:72:12: warning: function setPath() has to be used in Qt6
Base/QTGUI/Testing/Cxx/qSlicerScriptedLoadableModuleTest.cxx:93:14: warning: function setPath() has to be used in Qt6
Base/QTGUI/Testing/Cxx/qSlicerScriptedLoadableModuleWidgetTest.cxx:104:7: warning: function setPath() has to be used in Qt6
Base/QTGUI/Testing/Cxx/qSlicerScriptedLoadableModuleWidgetTest.cxx:83:12: warning: function setPath() has to be used in Qt6

Base/QTCore/qSlicerExtensionsManagerModel.cxx
@@ -870,7 +870,7 @@ QString qSlicerExtensionsManagerModelPrivate::extractArchive(const QDir& extensi
   QDir topLevelArchiveDir;
   while (extractDirOfFirstFile != extensionsDir)
   {
-    topLevelArchiveDir = extractDirOfFirstFile;
+    topLevelArchiveDir.setPath(extractDirOfFirstFile.dirName());
     extractDirOfFirstFile.cdUp();
   }
 

Base/QTCore/Testing/Cxx/qSlicerApplicationUpdateManagerTest.cxx
@@ -87,7 +87,7 @@ bool qSlicerApplicationUpdateManagerTester::resetTmp()
   ctk::removeDirRecursively(tmp.filePath(this->TemporaryDirName));
   tmp.mkdir(this->TemporaryDirName);
   tmp.cd(this->TemporaryDirName);
-  this->Tmp = tmp;
+  this->Tmp.setPath(tmp.dirName());
   return this->Tmp.exists();
 }
 
@@ -118,7 +118,7 @@ void qSlicerApplicationUpdateManagerTester::cleanupTestCase()
   if (this->Tmp != QDir::current() && this->Tmp.exists())
   {
     ctk::removeDirRecursively(this->Tmp.absolutePath());
-    this->Tmp = QDir();
+    this->Tmp.setPath(QString());
   }
   QFile::remove(QSettings().fileName());
 }

Base/QTCore/Testing/Cxx/qSlicerExtensionsManagerModelTest.cxx
@@ -321,7 +321,7 @@ bool qSlicerExtensionsManagerModelTester::resetTmp()
   ctk::removeDirRecursively(tmp.filePath(this->TemporaryDirName));
   tmp.mkdir(this->TemporaryDirName);
   tmp.cd(this->TemporaryDirName);
-  this->Tmp = tmp;
+  this->Tmp.setPath(tmp.dirName());
   return this->Tmp.exists();
 }
 
@@ -417,7 +417,7 @@ void qSlicerExtensionsManagerModelTester::cleanupTestCase()
   if (this->Tmp != QDir::current() && this->Tmp.exists())
   {
     ctk::removeDirRecursively(this->Tmp.absolutePath());
-    this->Tmp = QDir();
+    this->Tmp.setPath(QString());
   }
   QFile::remove(QSettings().fileName());
 }

Base/QTGUI/Testing/Cxx/qSlicerScriptedLoadableModuleTest.cxx
@@ -70,7 +70,7 @@ bool qSlicerScriptedLoadableModuleTester::resetTmp()
   ctk::removeDirRecursively(tmp.filePath(this->TemporaryDirName));
   tmp.mkdir(this->TemporaryDirName);
   tmp.cd(this->TemporaryDirName);
-  this->Tmp = tmp;
+  this->Tmp.setPath(tmp.dirName());
   return this->Tmp.exists();
 }
 
@@ -90,7 +90,7 @@ void qSlicerScriptedLoadableModuleTester::cleanupTestCase()
   if (this->Tmp != QDir::current() && this->Tmp.exists())
   {
     ctk::removeDirRecursively(this->Tmp.absolutePath());
-    this->Tmp = QDir();
+    this->Tmp.setPath(QString());
   }
 }
 

Base/QTGUI/Testing/Cxx/qSlicerScriptedLoadableModuleWidgetTest.cxx
@@ -80,7 +80,7 @@ bool qSlicerScriptedLoadableModuleWidgetTester::resetTmp()
   ctk::removeDirRecursively(tmp.filePath(this->TemporaryDirName));
   tmp.mkdir(this->TemporaryDirName);
   tmp.cd(this->TemporaryDirName);
-  this->Tmp = tmp;
+  this->Tmp.setPath(tmp.dirName());
   return this->Tmp.exists();
 }
 
@@ -100,7 +100,7 @@ void qSlicerScriptedLoadableModuleWidgetTester::cleanupTestCase()
   if (this->Tmp != QDir::current() && this->Tmp.exists())
   {
     ctk::removeDirRecursively(this->Tmp.absolutePath());
-    this->Tmp = QDir();
+    this->Tmp.setPath(QString());
   }
 }
 

commit 38a840a90bf50b2d108276b47df045521e976e11

BUG: Fix incorrect QDir assignment introduced during refactoring
The regression introduced in commit d36781511c ("COMP: Refactor QDir assignment to use setPath for consistency", 2025-07-03)
used `QDir::dirName()` instead of `QDir::path()` in several locations. Since
`dirName()` returns only the last component of the directory path (e.g., the leaf),
it resulted in incorrect paths being assigned to `QDir` instances.

This caused failures in both extension extraction and temporary directory
setup during tests.

This commit restores the correct behavior by replacing `dirName()` with
`path()` in the affected locations.

The following tests now pass:
- `qSlicerExtensionsManagerModelTest`
- `qSlicerApplicationUpdateManagerTest`
- `qSlicerScriptedLoadableModuleTest`
- `qSlicerScriptedLoadableModuleWidgetTest`

Base/QTCore/qSlicerExtensionsManagerModel.cxx
@@ -870,7 +870,7 @@ QString qSlicerExtensionsManagerModelPrivate::extractArchive(const QDir& extensi
   QDir topLevelArchiveDir;
   while (extractDirOfFirstFile != extensionsDir)
   {
-    topLevelArchiveDir.setPath(extractDirOfFirstFile.dirName());
+    topLevelArchiveDir.setPath(extractDirOfFirstFile.path());
     extractDirOfFirstFile.cdUp();
   }
 

Base/QTCore/Testing/Cxx/qSlicerApplicationUpdateManagerTest.cxx
@@ -87,7 +87,7 @@ bool qSlicerApplicationUpdateManagerTester::resetTmp()
   ctk::removeDirRecursively(tmp.filePath(this->TemporaryDirName));
   tmp.mkdir(this->TemporaryDirName);
   tmp.cd(this->TemporaryDirName);
-  this->Tmp.setPath(tmp.dirName());
+  this->Tmp.setPath(tmp.path());
   return this->Tmp.exists();
 }
 

Base/QTCore/Testing/Cxx/qSlicerExtensionsManagerModelTest.cxx
@@ -321,7 +321,7 @@ bool qSlicerExtensionsManagerModelTester::resetTmp()
   ctk::removeDirRecursively(tmp.filePath(this->TemporaryDirName));
   tmp.mkdir(this->TemporaryDirName);
   tmp.cd(this->TemporaryDirName);
-  this->Tmp.setPath(tmp.dirName());
+  this->Tmp.setPath(tmp.path());
   return this->Tmp.exists();
 }
 

Base/QTGUI/Testing/Cxx/qSlicerScriptedLoadableModuleTest.cxx
@@ -70,7 +70,7 @@ bool qSlicerScriptedLoadableModuleTester::resetTmp()
   ctk::removeDirRecursively(tmp.filePath(this->TemporaryDirName));
   tmp.mkdir(this->TemporaryDirName);
   tmp.cd(this->TemporaryDirName);
-  this->Tmp.setPath(tmp.dirName());
+  this->Tmp.setPath(tmp.path());
   return this->Tmp.exists();
 }
 

Base/QTGUI/Testing/Cxx/qSlicerScriptedLoadableModuleWidgetTest.cxx
@@ -80,7 +80,7 @@ bool qSlicerScriptedLoadableModuleWidgetTester::resetTmp()
   ctk::removeDirRecursively(tmp.filePath(this->TemporaryDirName));
   tmp.mkdir(this->TemporaryDirName);
   tmp.cd(this->TemporaryDirName);
-  this->Tmp.setPath(tmp.dirName());
+  this->Tmp.setPath(tmp.path());
   return this->Tmp.exists();
 }
 