Slicer/Slicer

commit f06fab25fc60b650729934c5315836d140f40a92

COMP: Prefer c++11 range for to Qt macros
As of Qt 5.7, `foreach` and `Q_FOREACH` macros are deprecated, and
you should use C++11 range-based for loops.

Base/QTCore/qSlicerCoreApplication.cxx
@@ -302,7 +302,7 @@ void qSlicerCoreApplicationPrivate::init()
   {
     QProcessEnvironment updatedEnv;
     ctkAppLauncherEnvironment::saveEnvironment(this->Environment, this->Environment.keys(), updatedEnv);
-    foreach (const QString& varname, updatedEnv.keys())
+    for (const QString& varname : updatedEnv.keys())
     {
       q->setEnvironmentVariable(varname, updatedEnv.value(varname));
     }
@@ -320,13 +320,13 @@ void qSlicerCoreApplicationPrivate::init()
 
   // Regular environment variables
   QHash<QString, QString> envVars = appLauncherSettings.envVars();
-  foreach (const QString& key, envVars.keys())
+  for (const QString& key : envVars.keys())
   {
     q->setEnvironmentVariable(key, envVars.value(key));
   }
   // Path environment variables (includes PATH, (DY)LD_LIBRARY_PATH and variables like PYTHONPATH)
   QHash<QString, QStringList> pathsEnvVars = appLauncherSettings.pathsEnvVars();
-  foreach (const QString& key, pathsEnvVars.keys())
+  for (const QString& key : pathsEnvVars.keys())
   {
     QString value;
     if (this->Environment.contains(key))
@@ -361,7 +361,7 @@ void qSlicerCoreApplicationPrivate::init()
   // Load default settings if any.
   if (q->defaultSettings())
   {
-    foreach (const QString& key, q->defaultSettings()->allKeys())
+    for (const QString& key : q->defaultSettings()->allKeys())
     {
       if (!q->userSettings()->contains(key))
       {
@@ -523,14 +523,14 @@ void qSlicerCoreApplicationPrivate::init()
 
   QStringList updatedExtensions;
   model->updateScheduledExtensions(updatedExtensions);
-  foreach (const QString& extensionName, updatedExtensions)
+  for (const QString& extensionName : updatedExtensions)
   {
     qDebug() << "Successfully updated extension" << extensionName;
   }
 
   QStringList uninstalledExtensions;
   model->uninstallScheduledExtensions(uninstalledExtensions);
-  foreach (const QString& extensionName, uninstalledExtensions)
+  for (const QString& extensionName : uninstalledExtensions)
   {
     qDebug() << "Successfully uninstalled extension" << extensionName;
   }
@@ -720,7 +720,7 @@ QString qSlicerCoreApplicationPrivate::discoverSlicerHomeDirectory()
   Q_Q(qSlicerCoreApplication);
   if (!this->isInstalled(slicerHome))
   {
-    foreach (const QString& subDir, QStringList() << Slicer_BIN_DIR << Slicer_CLIMODULES_BIN_DIR << "Cxx")
+    for (const QString& subDir : QStringList() << Slicer_BIN_DIR << Slicer_CLIMODULES_BIN_DIR << "Cxx")
     {
       qSlicerUtils::pathWithoutIntDir(q->applicationDirPath(), subDir, this->IntDir);
       if (!this->IntDir.isEmpty())
@@ -1134,7 +1134,7 @@ void qSlicerCoreApplication::handleURIArguments(const QStringList& fileNames)
 {
   QStringList filesToLoad;
 
-  foreach (QString fileName, fileNames)
+  for (const QString& fileName : fileNames)
   {
     QUrl url = QUrl(fileName);
     if (url.scheme().toLower() == this->applicationName().toLower()) // Scheme is case insensitive
@@ -2084,7 +2084,7 @@ void qSlicerCoreApplication::loadTranslations(const QString& dir)
 
   QStringList qmFiles = qSlicerCoreApplicationPrivate::findTranslationFiles(dir, app->applicationLocaleName());
 
-  foreach (QString qmFile, qmFiles)
+  for (const QString& qmFile : qmFiles)
   {
     QTranslator* translator = new QTranslator();
     QString qmFilePath = QString(dir + QString("/") + qmFile);
@@ -2136,7 +2136,7 @@ void qSlicerCoreApplication::loadLanguage()
     return;
   }
   QStringList qmDirs = qSlicerCoreApplication::translationFolders();
-  foreach (QString qmDir, qmDirs)
+  for (const QString& qmDir : qmDirs)
   {
     app->loadTranslations(qmDir);
   }
@@ -2264,7 +2264,7 @@ QStringList qSlicerCoreApplication::toSlicerHomeAbsolutePaths(const QStringList&
 {
   Q_D(const qSlicerCoreApplication);
   QStringList absolutePaths;
-  foreach (QString path, paths)
+  for (const QString& path : paths)
   {
     absolutePaths << this->toSlicerHomeAbsolutePath(path);
   }
@@ -2276,7 +2276,7 @@ QStringList qSlicerCoreApplication::toSlicerHomeRelativePaths(const QStringList&
 {
   Q_D(const qSlicerCoreApplication);
   QStringList relativePaths;
-  foreach (QString path, paths)
+  for (const QString& path : paths)
   {
     relativePaths << this->toSlicerHomeRelativePath(path);
   }
@@ -2368,7 +2368,7 @@ QString qSlicerCoreApplication::moduleDocumentationUrl(const QString& moduleName
 bool qSlicerCoreApplication::loadFiles(const QStringList& filePaths, vtkMRMLMessageCollection* userMessages /*=nullptr*/)
 {
   bool success = true;
-  foreach (QString filePath, filePaths)
+  for (const QString& filePath : filePaths)
   {
     QFileInfo file(filePath);
     qSlicerCoreIOManager* ioManager = this->coreIOManager();

Modules/Loadable/SlicerWelcome/qSlicerWelcomeModuleWidget.cxx
@@ -113,16 +113,15 @@ void qSlicerWelcomeModuleWidgetPrivate::setupUi(qSlicerWidget* widget)
 
   // Add all collabsibleWidgetButton to a button group
   QList<ctkCollapsibleButton*> collapsibles = widget->findChildren<ctkCollapsibleButton*>();
-  foreach (ctkCollapsibleButton* collapsible, collapsibles)
+  for (ctkCollapsibleButton* const collapsible : collapsibles)
   {
     group->addButton(collapsible);
   }
 
   // Update occurrences of documentation URLs
   qSlicerCoreApplication* app = qSlicerCoreApplication::application();
-  foreach (QWidget* widget,
-           QWidgetList() << this->FeedbackCollapsibleWidget << this->WelcomeAndAboutCollapsibleWidget << this->OtherUsefulHintsCollapsibleWidget
-                         << this->AcknowledgmentCollapsibleWidget)
+  for (QWidget* const widget : QWidgetList() << this->FeedbackCollapsibleWidget << this->WelcomeAndAboutCollapsibleWidget << this->OtherUsefulHintsCollapsibleWidget
+                                             << this->AcknowledgmentCollapsibleWidget)
   {
     QTextBrowser* textBrowser = widget->findChild<QTextBrowser*>();
     if (!textBrowser)

Base/QTCore/Testing/Cxx/qSlicerExtensionsManagerModelTest.cxx
@@ -329,7 +329,7 @@ bool qSlicerExtensionsManagerModelTester::resetTmp()
 void qSlicerExtensionsManagerModelTester::dumpExtensionMetatype(const char* varname, const ExtensionMetadataType& extensionMetadata)
 {
   std::cout << "[" << varname << "]" << std::endl;
-  foreach (const QString& key, extensionMetadata.keys())
+  for (const QString& key : extensionMetadata.keys())
   {
     std::cout << varname << ".insert(\"" << qPrintable(key) << "\", "
               << "\"" << qPrintable(extensionMetadata.value(key).toString()) << "\");" << std::endl;
@@ -379,7 +379,7 @@ qSlicerExtensionsManagerModelTester::ExtensionMetadataType qSlicerExtensionsMana
 
   QStringList keysToIgnore(qSlicerExtensionsManagerModel::serverKeysToIgnore(serverAPI));
   QVariantMap metadata;
-  foreach (const QString& key, allMetadata.keys())
+  for (const QString& key : allMetadata.keys())
   {
     if (filtered && keysToIgnore.contains(key))
     {
@@ -665,7 +665,7 @@ void qSlicerExtensionsManagerModelTester::testExtractExtensionArchive()
   }
 
   bool expectedFilesExist = true;
-  foreach (const QString& expectedFile, expectedFiles)
+  for (const QString& expectedFile : expectedFiles)
   {
     expectedFilesExist = expectedFilesExist && QFile::exists(this->Tmp.filePath(expectedFile));
     if (!expectedFilesExist)
@@ -909,9 +909,8 @@ void qSlicerExtensionsManagerModelTester::testUninstallExtension()
       this->installHelper(&model, operatingSystem, extensionId, this->Tmp.absolutePath());
     }
 
-    foreach (const QString& extensionName,
-             QStringList() //
-               << "MarkupsToModel")
+    for (const QString& extensionName : QStringList() //
+                                          << "MarkupsToModel")
     {
       QVERIFY(this->uninstallHelper(&model, extensionName));
       QVERIFY(!model.isExtensionInstalled(extensionName));
@@ -923,29 +922,26 @@ void qSlicerExtensionsManagerModelTester::testUninstallExtension()
     model.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
     model.updateModel();
 
-    foreach (const QString& extensionName,
-             QStringList() //
-               << "ImageMaker"
-               << "CurveMaker")
+    for (const QString& extensionName : QStringList() //
+                                          << "ImageMaker"
+                                          << "CurveMaker")
     {
       QVERIFY(model.isExtensionInstalled(extensionName));
     }
 
-    foreach (const QString& extensionName,
-             QStringList() //
-               << "MarkupsToModel")
+    for (const QString& extensionName : QStringList() //
+                                          << "MarkupsToModel")
     {
       QVERIFY(!model.isExtensionInstalled(extensionName));
     }
 
-    foreach (const QString& extensionName,
-             QStringList() //
-               << "MarkupsToModel")
+    for (const QString& extensionName : QStringList() //
+                                          << "MarkupsToModel")
     {
       this->installHelper(&model, operatingSystem, this->expectedExtensionNames().indexOf(extensionName), this->Tmp.absolutePath());
     }
 
-    foreach (const QString& extensionName, this->expectedExtensionNames())
+    for (const QString& extensionName : this->expectedExtensionNames())
     {
       QVERIFY(model.isExtensionInstalled(extensionName));
     }
@@ -973,7 +969,7 @@ void qSlicerExtensionsManagerModelTester::testScheduleExtensionForUninstall()
     model.setExtensionsSettingsFilePath(QSettings().fileName());
     model.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
     model.setSlicerVersion(slicerVersion);
-    foreach (int extensionIdToInstall, extensionIdsToInstall)
+    for (const int& extensionIdToInstall : extensionIdsToInstall)
     {
       this->installHelper(&model, operatingSystem, extensionIdToInstall, this->Tmp.absolutePath());
     }
@@ -989,7 +985,7 @@ void qSlicerExtensionsManagerModelTester::testScheduleExtensionForUninstall()
     model.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
     model.setSlicerVersion(slicerVersion);
     model.updateModel();
-    foreach (const QString& extensionNameToScheduleForUninstall, extensionNamesToScheduleForUninstall)
+    for (const QString& extensionNameToScheduleForUninstall : extensionNamesToScheduleForUninstall)
     {
       model.scheduleExtensionForUninstall(extensionNameToScheduleForUninstall);
     }
@@ -1060,7 +1056,7 @@ void qSlicerExtensionsManagerModelTester::testCancelExtensionScheduledForUninsta
     model.setExtensionsSettingsFilePath(QSettings().fileName());
     model.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
     model.setSlicerVersion(slicerVersion);
-    foreach (int extensionIdToInstall, extensionIdsToInstall)
+    for (const int& extensionIdToInstall : extensionIdsToInstall)
     {
       this->installHelper(&model, operatingSystem, extensionIdToInstall, this->Tmp.absolutePath());
     }
@@ -1073,7 +1069,7 @@ void qSlicerExtensionsManagerModelTester::testCancelExtensionScheduledForUninsta
     model.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
     model.setSlicerVersion(slicerVersion);
     model.updateModel();
-    foreach (const QString& extensionNameToScheduleForUninstall, extensionNamesToScheduleForUninstall)
+    for (const QString& extensionNameToScheduleForUninstall : extensionNamesToScheduleForUninstall)
     {
       model.scheduleExtensionForUninstall(extensionNameToScheduleForUninstall);
     }
@@ -1088,7 +1084,7 @@ void qSlicerExtensionsManagerModelTester::testCancelExtensionScheduledForUninsta
     model.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
     model.setSlicerVersion(slicerVersion);
     model.updateModel();
-    foreach (const QString& extensionNameToCancelScheduledForUninstall, extensionNamesToCancelScheduledForUninstall)
+    for (const QString& extensionNameToCancelScheduledForUninstall : extensionNamesToCancelScheduledForUninstall)
     {
       model.cancelExtensionScheduledForUninstall(extensionNameToCancelScheduledForUninstall);
     }
@@ -1227,9 +1223,8 @@ void qSlicerExtensionsManagerModelTester::testUpdateModel()
     QCOMPARE(spyExtensionEnabledChanged.count(), 1);
 
     QCOMPARE(model.isExtensionEnabled("ImageMaker"), false);
-    foreach (const QString& extensionName,
-             QStringList() << "MarkupsToModel"
-                           << "CurveMaker")
+    for (const QString& extensionName : QStringList() << "MarkupsToModel"
+                                                      << "CurveMaker")
     {
       QCOMPARE(model.isExtensionEnabled(extensionName), true);
     }
@@ -1237,9 +1232,8 @@ void qSlicerExtensionsManagerModelTester::testUpdateModel()
     model.updateModel();
 
     QCOMPARE(model.isExtensionEnabled("ImageMaker"), false);
-    foreach (const QString& extensionName,
-             QStringList() << "MarkupsToModel"
-                           << "CurveMaker")
+    for (const QString& extensionName : QStringList() << "MarkupsToModel"
+                                                      << "CurveMaker")
     {
       QCOMPARE(model.isExtensionEnabled(extensionName), true);
     }
@@ -1348,7 +1342,7 @@ void qSlicerExtensionsManagerModelTester::testInstalledExtensionsCount()
     qSlicerExtensionsManagerModel model;
     model.setExtensionsSettingsFilePath(QSettings().fileName());
     model.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
-    foreach (int extensionIdToInstall, extensionIdsToInstall)
+    for (const int& extensionIdToInstall : extensionIdsToInstall)
     {
       this->installHelper(&model, operatingSystem, extensionIdToInstall, this->Tmp.absolutePath());
     }
@@ -1413,7 +1407,7 @@ void qSlicerExtensionsManagerModelTester::testInstalledExtensions()
     qSlicerExtensionsManagerModel model;
     model.setExtensionsSettingsFilePath(QSettings().fileName());
     model.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
-    foreach (int extensionIdToInstall, extensionIdsToInstall)
+    for (const int& extensionIdToInstall : extensionIdsToInstall)
     {
       this->installHelper(&model, operatingSystem, extensionIdToInstall, this->Tmp.absolutePath());
     }
@@ -1490,25 +1484,25 @@ void qSlicerExtensionsManagerModelTester::testIsExtensionEnabled()
     qSlicerExtensionsManagerModel model;
     model.setExtensionsSettingsFilePath(QSettings().fileName());
     model.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
-    foreach (int extensionIdToInstall, extensionIdsToInstall)
+    for (const int& extensionIdToInstall : extensionIdsToInstall)
     {
       this->installHelper(&model, operatingSystem, extensionIdToInstall, this->Tmp.absolutePath());
     }
     QCOMPARE(model.enabledExtensions(), expectedEnabledExtensionNames);
 
-    foreach (const QString& extensionName, expectedEnabledExtensionNames)
+    for (const QString& extensionName : expectedEnabledExtensionNames)
     {
       additionalModulePathsPerExtension.insert(extensionName, model.extensionModulePaths(extensionName));
     }
 
-    foreach (const QString& extensionName, extensionNamesToDisable)
+    for (const QString& extensionName : extensionNamesToDisable)
     {
       model.setExtensionEnabled(extensionName, false);
     }
     QCOMPARE(model.enabledExtensions(), expectedEnabledExtensionNamesAfterDisable);
 
     QStringList expectedAdditionalPaths;
-    foreach (const QString& extensionName, expectedEnabledExtensionNamesAfterDisable)
+    for (const QString& extensionName : expectedEnabledExtensionNamesAfterDisable)
     {
       expectedAdditionalPaths << additionalModulePathsPerExtension.value(extensionName);
     }
@@ -1524,7 +1518,7 @@ void qSlicerExtensionsManagerModelTester::testIsExtensionEnabled()
     QCOMPARE(model.enabledExtensions(), expectedEnabledExtensionNamesAfterDisable);
 
     QStringList expectedAdditionalPaths;
-    foreach (const QString& extensionName, expectedEnabledExtensionNamesAfterDisable)
+    for (const QString& extensionName : expectedEnabledExtensionNamesAfterDisable)
     {
       expectedAdditionalPaths << additionalModulePathsPerExtension.value(extensionName);
     }
@@ -1930,7 +1924,7 @@ void testRequirementsHelper(qSlicerExtensionsManagerModel* model, QStringSetter 
 
   QSignalSpy spySlicerRequirementsChanged(model, SIGNAL(slicerRequirementsChanged(QString, QString, QString)));
   QSignalSpy spySlicerPropertyChanged(model, changedPropertySignal);
-  foreach (const QString& valuetoSet, valuesToSet)
+  for (const QString& valuetoSet : valuesToSet)
   {
     (model->*qStringSetterFuncPtr)(valuetoSet);
   }

commit f5f8b893abbd2221c07b5f9ce7cca9276298d6e8

BUG: Fix crashes caused by range-based loops over temporary Qt containers
This commit fixes regressions introduced in f06fab25fc ("COMP: Prefer C++11
range-for to Qt macros", 2025-07-16), which replaced `Q_FOREACH` with C++11
range-based `for` loops.

A segmentation fault was reported in `qSlicerWelcomeModuleWidgetPrivate::setupUi`,
caused by iterating over a temporary `QWidgetList` constructed inline
using the `<<` operator. Since range-based `for` loops do not extend the
lifetime of temporaries, the container was destroyed after the loop header
executed, leading to a **use-after-free** and a crash when dereferencing
the iterator.

Similar crashes were observed in tests on Unix systems involving temporary
`QStringList` instances. Although `QStringList` (a `QList<QString>`) uses
implicit sharing, range-based iteration over a destroyed temporary is still
undefined behavior and may result in a crash.

Additionally, a related use in `qSlicerCoreApplicationPrivate::discoverSlicerHomeDirectory`,
which used `QStringList` in the same way, was only compiled on Windows. While
this instance did not crash, it was also updated for correctness and consistency.

To address the issue this commit updates all affected range-based loops by:

* Assigning the container to a named local variable prior to iteration, ensuring
its lifetime extends through the loop.

For reference, the previously used `Q_FOREACH` macro avoided this issue by explicitly
storing the container in a local `_container_` variable:

```cpp
for (auto _container_ = QtPrivate::qMakeForeachContainer(container);
     _container_.i != _container_.e;
     ++_container_.i)
  if (variable = *_container_.i; false) {} else
```

Base/QTCore/qSlicerCoreApplication.cxx
@@ -18,7 +18,7 @@
 
 ==============================================================================*/
 
-// standard library includes
+// STD includes
 #include <clocale>
 #include <stdexcept>
 
@@ -720,7 +720,12 @@ QString qSlicerCoreApplicationPrivate::discoverSlicerHomeDirectory()
   Q_Q(qSlicerCoreApplication);
   if (!this->isInstalled(slicerHome))
   {
-    for (const QString& subDir : QStringList() << Slicer_BIN_DIR << Slicer_CLIMODULES_BIN_DIR << "Cxx")
+    const QStringList subDirs{
+      Slicer_BIN_DIR,
+      Slicer_CLIMODULES_BIN_DIR,
+      "Cxx",
+    };
+    for (const QString& subDir : subDirs)
     {
       qSlicerUtils::pathWithoutIntDir(q->applicationDirPath(), subDir, this->IntDir);
       if (!this->IntDir.isEmpty())

Modules/Loadable/SlicerWelcome/qSlicerWelcomeModuleWidget.cxx
@@ -120,8 +120,13 @@ void qSlicerWelcomeModuleWidgetPrivate::setupUi(qSlicerWidget* widget)
 
   // Update occurrences of documentation URLs
   qSlicerCoreApplication* app = qSlicerCoreApplication::application();
-  for (QWidget* const widget : QWidgetList() << this->FeedbackCollapsibleWidget << this->WelcomeAndAboutCollapsibleWidget << this->OtherUsefulHintsCollapsibleWidget
-                                             << this->AcknowledgmentCollapsibleWidget)
+
+  QWidgetList widgetList;
+  widgetList << this->FeedbackCollapsibleWidget         //
+             << this->WelcomeAndAboutCollapsibleWidget  //
+             << this->OtherUsefulHintsCollapsibleWidget //
+             << this->AcknowledgmentCollapsibleWidget;
+  for (QWidget* const widget : widgetList)
   {
     QTextBrowser* textBrowser = widget->findChild<QTextBrowser*>();
     if (!textBrowser)

Base/QTCore/Testing/Cxx/qSlicerExtensionsManagerModelTest.cxx
@@ -909,8 +909,12 @@ void qSlicerExtensionsManagerModelTester::testUninstallExtension()
       this->installHelper(&model, operatingSystem, extensionId, this->Tmp.absolutePath());
     }
 
-    for (const QString& extensionName : QStringList() //
-                                          << "MarkupsToModel")
+    QStringList extensionNames;
+
+    extensionNames = QStringList() //
+                     << "MarkupsToModel";
+
+    for (const QString& extensionName : extensionNames)
     {
       QVERIFY(this->uninstallHelper(&model, extensionName));
       QVERIFY(!model.isExtensionInstalled(extensionName));
@@ -922,21 +926,29 @@ void qSlicerExtensionsManagerModelTester::testUninstallExtension()
     model.setSlicerRequirements(slicerRevision, operatingSystem, architecture);
     model.updateModel();
 
-    for (const QString& extensionName : QStringList() //
-                                          << "ImageMaker"
-                                          << "CurveMaker")
+    QStringList extensionNames;
+
+    extensionNames = QStringList() //
+                     << "ImageMaker"
+                     << "CurveMaker";
+
+    for (const QString& extensionName : extensionNames)
     {
       QVERIFY(model.isExtensionInstalled(extensionName));
     }
 
-    for (const QString& extensionName : QStringList() //
-                                          << "MarkupsToModel")
+    extensionNames = QStringList() //
+                     << "MarkupsToModel";
+
+    for (const QString& extensionName : extensionNames)
     {
       QVERIFY(!model.isExtensionInstalled(extensionName));
     }
 
-    for (const QString& extensionName : QStringList() //
-                                          << "MarkupsToModel")
+    extensionNames = QStringList() //
+                     << "MarkupsToModel";
+
+    for (const QString& extensionName : extensionNames)
     {
       this->installHelper(&model, operatingSystem, this->expectedExtensionNames().indexOf(extensionName), this->Tmp.absolutePath());
     }
@@ -1222,9 +1234,13 @@ void qSlicerExtensionsManagerModelTester::testUpdateModel()
     model.setExtensionEnabled("ImageMaker", false);
     QCOMPARE(spyExtensionEnabledChanged.count(), 1);
 
+    const QStringList extensionNames{
+      "MarkupsToModel",
+      "CurveMaker",
+    };
+
     QCOMPARE(model.isExtensionEnabled("ImageMaker"), false);
-    for (const QString& extensionName : QStringList() << "MarkupsToModel"
-                                                      << "CurveMaker")
+    for (const QString& extensionName : extensionNames)
     {
       QCOMPARE(model.isExtensionEnabled(extensionName), true);
     }
@@ -1232,8 +1248,7 @@ void qSlicerExtensionsManagerModelTester::testUpdateModel()
     model.updateModel();
 
     QCOMPARE(model.isExtensionEnabled("ImageMaker"), false);
-    for (const QString& extensionName : QStringList() << "MarkupsToModel"
-                                                      << "CurveMaker")
+    for (const QString& extensionName : extensionNames)
     {
       QCOMPARE(model.isExtensionEnabled(extensionName), true);
     }

Base/QTCore/Testing/Cxx/qSlicerUtilsTest1.cxx
@@ -247,7 +247,9 @@ int isPluginInstalledBuiltinTest()
 
   createFile(__LINE__, tmp1, ".", "CMakeCache.txt");
 
-  for (const QString& relativePath : QStringList() << debug << release << relWithDebInfo << minSizeRel << foo << fooDebug << fooRelease << fooBar << fooBarDebug << fooBarRelease)
+  QStringList relativePaths;
+  relativePaths = QStringList() << debug << release << relWithDebInfo << minSizeRel << foo << fooDebug << fooRelease << fooBar << fooBarDebug << fooBarRelease;
+  for (const QString& relativePath : relativePaths)
   {
     createFile(__LINE__, tmp1, relativePath, "plugin.txt");
   }
@@ -327,7 +329,8 @@ int isPluginInstalledBuiltinTest()
   tmp3.mkdir(temporaryDirName);
   tmp3.cd(temporaryDirName);
 
-  for (const QString& relativePath : QStringList() << foo << fooBar)
+  relativePaths = QStringList() << foo << fooBar;
+  for (const QString& relativePath : relativePaths)
   {
     createFile(__LINE__, tmp1, relativePath, "plugin.txt");
   }
@@ -692,6 +695,8 @@ int setPermissionsRecursivelyTest()
   tmp.mkdir(temporaryDirName);
   tmp.cd(temporaryDirName);
 
+  QStringList relativeFilepaths;
+
   QString path1 = QLatin1String("any/foo/bar");
   QString path2 = QLatin1String("any/foo/bie");
 
@@ -701,8 +706,9 @@ int setPermissionsRecursivelyTest()
   createFile(__LINE__, tmp, path2, "sol.txt");
 
   // Let's confirm that created file are readable
-  for (const QString& relativeFilepath : QStringList() //
-                                           << path1 + "/sol.txt" << path1 + "/la.txt" << path2 + "/si.txt" << path2 + "/sol.txt")
+  relativeFilepaths = QStringList() //
+                      << path1 + "/sol.txt" << path1 + "/la.txt" << path2 + "/si.txt" << path2 + "/sol.txt";
+  for (const QString& relativeFilepath : relativeFilepaths)
   {
     CHECK_BOOL((QFile::permissions(tmp.filePath(relativeFilepath)) & QFile::ReadOwner) != 0, true);
   }
@@ -716,8 +722,9 @@ int setPermissionsRecursivelyTest()
   //  https://qt.gitorious.org/qt/qt/blobs/092cd760d5fddf9640a310214fe01929f0fff3a8/src/corelib/io/qfsfileengine_win.cpp#line1781
 
   // Since directory are *NOT* executable, files should *NOT* be readable
-  for (const QString& relativeFilepath : QStringList() //
-                                           << path1 + "/sol.txt" << path1 + "/la.txt" << path2 + "/si.txt" << path2 + "/sol.txt")
+  relativeFilepaths = QStringList() //
+                      << path1 + "/sol.txt" << path1 + "/la.txt" << path2 + "/si.txt" << path2 + "/sol.txt";
+  for (const QString& relativeFilepath : relativeFilepaths)
   {
     CHECK_BOOL((QFile::permissions(tmp.filePath(relativeFilepath)) & QFile::ReadOwner) != 0, false);
   }
@@ -725,8 +732,9 @@ int setPermissionsRecursivelyTest()
   CHECK_BOOL(qSlicerUtils::setPermissionsRecursively(tmp.path(), QFile::ReadOwner | QFile::ExeOwner, QFile::ReadOwner), true);
 
   // Since directory are executable, files should be readable
-  for (const QString& relativeFilepath : QStringList() //
-                                           << path1 + "/sol.txt" << path1 + "/la.txt" << path2 + "/si.txt" << path2 + "/sol.txt")
+  relativeFilepaths = QStringList() //
+                      << path1 + "/sol.txt" << path1 + "/la.txt" << path2 + "/si.txt" << path2 + "/sol.txt";
+  for (const QString& relativeFilepath : relativeFilepaths)
   {
     CHECK_BOOL((QFile::permissions(tmp.filePath(relativeFilepath)) & QFile::ReadOwner) != 0, true);
   }
@@ -735,8 +743,9 @@ int setPermissionsRecursivelyTest()
   CHECK_BOOL(ctk::removeDirRecursively(tmp.path()), false);
 #endif
 
-  for (const QString& relativeFilepath : QStringList() //
-                                           << path1 + "/sol.txt" << path1 + "/la.txt" << path2 + "/si.txt" << path2 + "/sol.txt")
+  relativeFilepaths = QStringList() //
+                      << path1 + "/sol.txt" << path1 + "/la.txt" << path2 + "/si.txt" << path2 + "/sol.txt";
+  for (const QString& relativeFilepath : relativeFilepaths)
   {
     // Since files are read-only, they should *NOT* be writable
     CHECK_BOOL((QFile::permissions(tmp.filePath(relativeFilepath)) & QFile::WriteOwner) != 0, false);
@@ -745,8 +754,9 @@ int setPermissionsRecursivelyTest()
   CHECK_BOOL(qSlicerUtils::setPermissionsRecursively(tmp.path(), QFile::ReadOwner | QFile::ExeOwner | QFile::WriteOwner, QFile::ReadOwner | QFile::WriteOwner), true);
 
   // Make sure files are readable and writable
-  for (const QString& relativeFilepath : QStringList() //
-                                           << path1 + "/sol.txt" << path1 + "/la.txt" << path2 + "/si.txt" << path2 + "/sol.txt")
+  relativeFilepaths = QStringList() //
+                      << path1 + "/sol.txt" << path1 + "/la.txt" << path2 + "/si.txt" << path2 + "/sol.txt";
+  for (const QString& relativeFilepath : relativeFilepaths)
   {
     CHECK_BOOL((QFile::permissions(tmp.filePath(relativeFilepath)) & QFile::WriteOwner) != 0, true);
   }

Libs/MRML/Widgets/Testing/qMRMLNodeComboBoxTest8.cxx
@@ -224,16 +224,14 @@ int qMRMLNodeComboBoxTest8(int argc, char* argv[])
     std::cout << "After disabling remove action, new number of actions = " << expected << std::endl;
   }
 
-  // add a custom action starting respectively with:
-  //  * "Create new "
-  //  * "Delete current "
-  //  * "Edit current "
-  //  * "Rename current "
-  //  * "Create and rename "
-  for (const QString& actionPrefix : QStringList() << "Create new "
-                                                   << "Delete current "
-                                                   << "Edit current "
-                                                   << "Rename current ")
+  // add a custom actions
+  const QStringList actionPrefixes = {
+    "Create new ",
+    "Delete current ",
+    "Edit current ",
+    "Rename current ",
+  };
+  for (const QString& actionPrefix : actionPrefixes)
   {
     startingActions = sceneModel->postItems(sceneModel->mrmlSceneItem()).size();
 