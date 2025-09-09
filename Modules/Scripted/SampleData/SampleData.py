Slicer/Slicer

commit 89104423af85c44d1953054cab548afe956f0bd7

BUG: Ensure MRB loading correctly reports loaded nodes
Previously, the scene reader did not report loaded nodes, leading to
incorrect detection of scene loading failures. This commit ensures
that newly loaded nodes are properly tracked and reported.

Additionally:
- Improved the `SampleDataLogic.loadNode` API to return the first loaded node or the scene if no specific node is reported.
- Refactored `SampleDataLogic.loadScene` to reuse `loadNode`, ensuring consistent behavior.
- Improved `SampleDataLogic.downloadFromSource` to not require node name and file name (it can generate it automatically from the URL)

Modules/Scripted/SampleData/SampleData.py
@@ -676,6 +676,17 @@ class SampleDataLogic:
         for uri, fileName, nodeName, checksum, loadFile, loadFileType in zip(
             source.uris, source.fileNames, source.nodeNames, source.checksums, source.loadFiles, source.loadFileTypes):
 
+            if nodeName is None or fileName is None:
+                import urllib
+                import uuid
+                p = urllib.parse.urlparse(uri)
+                basename, ext = os.path.splitext(os.path.basename(p.path))
+                if nodeName is None:
+                    nodeName = basename
+                if fileName is None:
+                    # Generate a unique filename to avoid overwriting existing file with the same name
+                    fileName = f"{nodeName}-{uuid.uuid4().hex}{ext}"
+
             current_source = SampleDataSource(
                 uris=uri,
                 fileNames=fileName,
@@ -939,29 +950,33 @@ class SampleDataLogic:
         return filePath
 
     def loadScene(self, uri, fileProperties={}):
-        self.logMessage("<b>" + _("Requesting load {uri}").format(uri=uri) + "</b>")
-        fileProperties["fileName"] = uri
-        success = slicer.app.coreIOManager().loadNodes("SceneFile", fileProperties)
-        if not success:
-            self.logMessage("\t" + _("Load failed!"), logging.ERROR)
-            return False
-        self.logMessage("<b>" + _("Load finished") + "</b><p></p>")
-        return True
+        """Returns True is scene loading was successful, False if failed."""
+        loadedNode = self.loadNode(uri, None, "SceneFile", fileProperties)
+        success = loadedNode is not None
+        return success
 
     def loadNode(self, uri, name, fileType=None, fileProperties={}):
+        """Returns the first loaded node (or the scene if the reader did not provide a specific node) on success.
+        Returns None if failed.
+        """
         self.logMessage("<b>" + _("Requesting load {name} from {uri} ...").format(name=name, uri=uri) + "</b>")
 
         fileProperties["fileName"] = uri
-        fileProperties["name"] = name
+        if name:
+            fileProperties["name"] = name
         if not fileType:
             fileType = slicer.app.coreIOManager().fileType(fileProperties["fileName"])
         firstLoadedNode = None
         loadedNodes = vtk.vtkCollection()
         success = slicer.app.coreIOManager().loadNodes(fileType, fileProperties, loadedNodes)
-
-        if not success or loadedNodes.GetNumberOfItems() < 1:
-            self.logMessage("\t" + _("Load failed!"), logging.ERROR)
-            return None
+        if not success:
+            if loadedNodes.GetNumberOfItems() < 1:
+                self.logMessage("\t" + _("Load failed!"), logging.ERROR)
+                return None
+            else:
+                # Loading did not fail, because some nodes were loaded, proceed with a warning
+                self.logMessage(_("Error was reported while loading {count} nodes from {path}").format(
+                                count=loadedNodes.GetNumberOfItems(), path=uri), logging.WARNING)
 
         self.logMessage("<b>" + _("Load finished") + "</b><p></p>")
 
@@ -976,7 +991,13 @@ class SampleDataLogic:
             slicer.mrmlScene.RemoveNode(storageNode)
             loadedNode.SetAndObserveStorageNodeID(None)
 
-        return loadedNodes.GetItemAsObject(0)
+        firstLoadedNode = loadedNodes.GetItemAsObject(0)
+        if firstLoadedNode:
+            return firstLoadedNode
+        else:
+            # If a reader does not report loading of any specific node (it may happen for example with a scene reader)
+            # then return the scene to distinguish from a load error.
+            return slicer.mrmlScene
 
 
 class SampleDataTest(ScriptedLoadableModuleTest):

Base/QTCore/qSlicerSceneBundleReader.cxx
@@ -38,6 +38,7 @@
 #include <vtkMRMLApplicationLogic.h>
 
 // VTK includes
+#include <vtkCollection.h>
 #include <vtkNew.h>
 
 // VTKSYS includes
@@ -90,7 +91,31 @@ bool qSlicerSceneBundleReader::load(const qSlicerIO::IOProperties& properties)
     clear = properties["clear"].toBool();
   }
 
+  // Get all the nodes that have been around before loading
+  QSet<vtkMRMLNode*> nodesPresentBeforeLoading;
+  vtkCollection* nodes = this->mrmlScene()->GetNodes();
+  for (int index = 0; index < nodes->GetNumberOfItems(); ++index)
+  {
+    vtkMRMLNode* node = vtkMRMLNode::SafeDownCast(nodes->GetItemAsObject(index));
+    nodesPresentBeforeLoading.insert(node);
+  }
+
   bool success = this->mrmlScene()->ReadFromMRB(file.toUtf8(), clear, this->userMessages());
+
+  // Get all the new nodes
+  QStringList loadedNodeIds;
+  nodes = this->mrmlScene()->GetNodes();
+  for (int index = 0; index < nodes->GetNumberOfItems(); ++index)
+  {
+    vtkMRMLNode* node = vtkMRMLNode::SafeDownCast(nodes->GetItemAsObject(index));
+    if (nodesPresentBeforeLoading.contains(node) || !node || !node->GetID())
+    {
+      continue;
+    }
+    loadedNodeIds.append(node->GetID());
+  }
+  this->setLoadedNodes(loadedNodeIds);
+
   if (success)
   {
     // Set default scene file format to mrb

commit 34c5479e056857dbc46d3bcddf2f08d481e9f2a6

BUG: Fix loading of multi-file volumes in SampleData module
When loading sample datasets consisting of multiple files, such as
`DTIVolume.nhdr` and `DTIVolume.raw.gz`, the dataset failed to load.
This was caused by automatically generating a node name for the raw
sidecar file, which was not meant to be loaded into the scene.

Fixed by only generating default node names if none are specified for
any of the files in the sample.

Fixes regression introduced in 89104423af ("BUG: Ensure MRB loading
correctly reports loaded nodes", 2025-01-10).

Modules/Scripted/SampleData/SampleData.py
@@ -700,10 +700,16 @@ class SampleDataLogic:
         resultNodes = []
         resultFilePaths = []
 
+        # If some node names are defined and some are left empty then we assume that it is intentional
+        # (e.g., you only want to load the image.nhdr file and not the image.raw file).
+        # In this case default node names are not generated.
+        generateDefaultNodeNames = all(n is None for n in source.nodeNames)
+
         for uri, fileName, nodeName, checksum, loadFile, loadFileType in zip(
             source.uris, source.fileNames, source.nodeNames, source.checksums, source.loadFiles, source.loadFileTypes, strict=False):
 
             if nodeName is None or fileName is None:
+                # Determine file basename and extension from URL or path
                 import urllib
                 import uuid
                 if fileName is not None:
@@ -711,11 +717,15 @@ class SampleDataLogic:
                 else:
                     p = urllib.parse.urlparse(uri)
                     basename, ext = os.path.splitext(os.path.basename(p.path))
-                if nodeName is None:
+
+                # Generate default node name (we only need this if we want to load the file into the scene and no node name is provided)
+                if (nodeName is None) and (loadFile is not False) and generateDefaultNodeNames:
                     nodeName = basename
+
+                # Generate default file name (we always need this, even for just downloading)
                 if fileName is None:
                     # Generate a unique filename to avoid overwriting existing file with the same name
-                    fileName = f"{nodeName}-{uuid.uuid4().hex}{ext}"
+                    fileName = f"{nodeName if nodeName else basename}-{uuid.uuid4().hex}{ext}"
 
             current_source = SampleDataSource(
                 uris=uri,