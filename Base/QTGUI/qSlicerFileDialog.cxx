Slicer/Slicer

commit 2c3bbb7ddbde3618c6056d419eacfb70c12b4e8f

ENH: Improve directory and file selection by using native dialogs
CTK:
$ git shortlog --no-merges 01ab373..9f12861
Csaba Pinter (1):
      BUG: Remove assert from matrix widget that flooded application log

James Butler (4):
      ENH: Update ctkFileDialog to use native dialog by default
      BUG: Fix crash instantiating ctkDICOMBrowser
      STYLE: Remove extra parenthesis around logic
      STYLE: Fix grammar in comment

Base/QTGUI/qSlicerFileDialog.cxx
@@ -187,10 +187,6 @@ ctkFileDialog* qSlicerStandardFileDialog::createFileDialog(
   qSlicerIOManager* ioManager = qSlicerApplication::application()->ioManager();
   ctkFileDialog* fileDialog = new ctkFileDialog();
 
-  // We don't want the odd dialog in Mac: it shows in gray filenames that
-  // don't match the filter -> we don't want to show files that don't match
-  // the filter.
-  fileDialog->setOption(QFileDialog::DontUseNativeDialog);
   if(ioProperties["fileType"].toBool())
     {
     fileDialog->setNameFilters(

Base/QTGUI/Resources/UI/qSlicerSaveDataDialog.ui
@@ -53,9 +53,6 @@
      <property name="text">
       <string>Change directory for selected files</string>
      </property>
-     <property name="options">
-      <set>ctkDirectoryButton::DontUseNativeDialog</set>
-     </property>
     </widget>
    </item>
    <item row="0" column="6">

Modules/Scripted/LabelStatistics/LabelStatistics.py
@@ -208,7 +208,6 @@ class LabelStatisticsWidget(ScriptedLoadableModuleWidget):
     """
     if not self.fileDialog:
       self.fileDialog = qt.QFileDialog(self.parent)
-      self.fileDialog.options = self.fileDialog.DontUseNativeDialog
       self.fileDialog.acceptMode = self.fileDialog.AcceptSave
       self.fileDialog.defaultSuffix = "csv"
       self.fileDialog.setNameFilter("Comma Separated Values (*.csv)")

SuperBuild/External_CTK.cmake
@@ -70,7 +70,7 @@ if(NOT DEFINED CTK_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "01ab373d01e6e8718af42f856278b61bb18fbcfe"
+    "9f12861724155f9929de9be8500c9eabd38507ac"
     QUIET
     )
 

commit 4f4a6d1c42c3d6f4052f4870c998a0cc39cf10b5

BUG: Delete IO options when using native file dialog to prevent leak
When the file dialog is native, custom widgets are not embedded or
reparented. As a result, qSlicerIOOptions remains orphan and must be
explicitly deleted.

Fix regression introduced in 2c3bbb7ddbd ("ENH: Improve directory and file selection by using native dialogs", 2021-06-05)
updating CTK to instantiate `ctkFileDialog` as native dialog by default.

Base/QTGUI/qSlicerFileDialog.cxx
@@ -267,6 +267,8 @@ bool qSlicerStandardFileDialog::exec(const qSlicerIO::IOProperties& ioProperties
   QFileDialog::AcceptMode acceptMode = (d->Action == qSlicerFileDialog::Read) ? QFileDialog::AcceptOpen : QFileDialog::AcceptSave;
   fileDialog->setAcceptMode(acceptMode);
 
+  const bool usingNativeDialog = !fileDialog->testOption(QFileDialog::DontUseNativeDialog);
+
   qSlicerIOManager* ioManager = qSlicerApplication::application()->ioManager();
 
   qSlicerIOOptions* options = this->options(properties);
@@ -275,7 +277,7 @@ bool qSlicerStandardFileDialog::exec(const qSlicerIO::IOProperties& ioProperties
   // options is not necessary a qSlicerIOOptionsWidget (for the case of
   // readers/modules with no UI. If there is a UI then add it inside the file
   // dialog.
-  if (optionsWidget)
+  if (optionsWidget && !usingNativeDialog)
   {
     // fileDialog will reparent optionsWidget and take care of deleting
     // optionsWidget for us.
@@ -334,7 +336,11 @@ bool qSlicerStandardFileDialog::exec(const qSlicerIO::IOProperties& ioProperties
   // If options is not a qSlicerIOOptionsWidget, we are responsible for
   // deleting options. If it is, then fileDialog would have reparent
   // the options and take care of its destruction
-  if (!optionsWidget)
+  //
+  // However, when a *native* dialog is used, custom widgets are not embedded,
+  // so even qSlicerIOOptionsWidget instances won't be reparented. In that case
+  // we must still delete.
+  if (options && (!optionsWidget || usingNativeDialog))
   {
     delete options;
     options = nullptr;