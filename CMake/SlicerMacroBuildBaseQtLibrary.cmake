Slicer/Slicer

commit 8a44717a61b16fa43c3616722375a0eb30950c3a

COMP: Enable AUTOUIC to simplify UI generation and prep for Qt6
Adopt target-level AUTOUIC and remove legacy UI wiring to reduce boilerplate
and align CTK with Qt6-ready CMake practices.

- Replace all `qt5_wrap_ui()` usage.
- Enable `AUTOUIC` on all Slicer libraries and tests.
- Set `AUTOUIC_SEARCH_PATHS` from declared UI forms (or `Resources/UI`).
- Set global `AUTOGEN_SOURCE_GROUP` to "Generated" for consistent IDE layout.
- Add AUTOGEN include dir so generated `ui_*.h` headers resolve reliably.
- Clean up: remove unused UI wrapping in `Modules/Loadable/Markups/Widgets/Testing/Cxx/CMakeLists.txt`

Co-authored-by: Hans Johnson <hans-johnson@uiowa.edu>

CMake/SlicerMacroBuildBaseQtLibrary.cmake
@@ -34,8 +34,8 @@
 #   MOC_SRCS .............: Optional list of headers to run through the meta object compiler (moc)
 #                           using Qt(4|5)_WRAP_CPP CMake macro
 #
-#   UI_SRCS ..............: Optional list of UI file to run through UI compiler (uic) using
-#                           Qt(4|5)_WRAP_UI CMake macro
+#   UI_SRCS ..............: Optional list of UI file to run through UI compiler (uic) leveraging
+#                           CMake's auto-uic feature.
 #
 #   INCLUDE_DIRECTORIES ..: Optional list of extra folder that should be included. See implementation
 #                           for the list of folder included by default.
@@ -119,6 +119,18 @@ macro(SlicerMacroBuildBaseQtLibrary)
   set(Slicer_Base_INCLUDE_DIRS ${Slicer_Base_INCLUDE_DIRS}
     ${CMAKE_CURRENT_SOURCE_DIR}
     ${CMAKE_CURRENT_BINARY_DIR}
+    # Ensure generated AUTOUIC headers (ui_*.h) are discoverable.
+    #
+    # By default CMake writes them to:
+    #   <AUTOGEN_BUILD_DIR>/include
+    #
+    # where AUTOGEN_BUILD_DIR defaults to:
+    #   <target-binary-dir>/<target-name>_autogen
+    #
+    # References:
+    # - https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html#autouic
+    # - https://cmake.org/cmake/help/latest/prop_tgt/AUTOGEN_BUILD_DIR.html
+    ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include
     CACHE INTERNAL "Slicer Base includes" FORCE)
 
   #-----------------------------------------------------------------------------
@@ -140,7 +152,6 @@ macro(SlicerMacroBuildBaseQtLibrary)
   # --------------------------------------------------------------------------
     set(_moc_options OPTIONS -DSlicer_HAVE_QT5)
     QT5_WRAP_CPP(SLICERQTBASELIB_MOC_OUTPUT ${SLICERQTBASELIB_MOC_SRCS} ${_moc_options})
-    QT5_WRAP_UI(SLICERQTBASELIB_UI_CXX ${SLICERQTBASELIB_UI_SRCS})
     if(DEFINED SLICERQTBASELIB_RESOURCES)
       QT5_ADD_RESOURCES(SLICERQTBASELIB_QRC_SRCS ${SLICERQTBASELIB_RESOURCES})
     endif()
@@ -148,7 +159,6 @@ macro(SlicerMacroBuildBaseQtLibrary)
     QT5_ADD_RESOURCES(SLICERQTBASELIB_QRC_SRCS ${Slicer_SOURCE_DIR}/Resources/qSlicer.qrc)
 
   set_source_files_properties(
-    ${SLICERQTBASELIB_UI_CXX}
     ${SLICERQTBASELIB_MOC_OUTPUT}
     ${SLICERQTBASELIB_QRC_SRCS}
     WRAP_EXCLUDE
@@ -164,7 +174,6 @@ macro(SlicerMacroBuildBaseQtLibrary)
   )
 
   source_group("Generated" FILES
-    ${SLICERQTBASELIB_UI_CXX}
     ${SLICERQTBASELIB_MOC_OUTPUT}
     ${SLICERQTBASELIB_QRC_SRCS}
     ${dynamicHeaders}
@@ -200,10 +209,26 @@ macro(SlicerMacroBuildBaseQtLibrary)
   add_library(${lib_name}
     ${SLICERQTBASELIB_SRCS}
     ${SLICERQTBASELIB_MOC_OUTPUT}
-    ${SLICERQTBASELIB_UI_CXX}
     ${SLICERQTBASELIB_QRC_SRCS}
     ${QM_OUTPUT_FILES}
     )
+
+  # Configure CMake Qt automatic code generation
+  set(uic_search_paths)
+  foreach(ui_src IN LISTS SLICERQTBASELIB_UI_SRCS)
+    if(NOT IS_ABSOLUTE ${ui_src})
+      set(ui_src "${CMAKE_CURRENT_SOURCE_DIR}/${ui_src}")
+    endif()
+    get_filename_component(ui_path ${ui_src} PATH)
+    list(APPEND uic_search_paths ${ui_path})
+  endforeach()
+  list(REMOVE_DUPLICATES uic_search_paths)
+
+  set_target_properties(${lib_name} PROPERTIES
+    AUTOUIC ON
+    AUTOUIC_SEARCH_PATHS "${uic_search_paths}"
+    )
+
   set_target_properties(${lib_name} PROPERTIES LABELS ${lib_name})
 
   # Apply user-defined properties to the library target.

CMake/SlicerMacroBuildLoadableModule.cmake
@@ -92,7 +92,22 @@ macro(slicerMacroBuildLoadableModule)
     set(${lib_name}_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR} CACHE INTERNAL "" FORCE)
   endif()
 
-  set(${lib_name}_INCLUDE_DIRS ${${lib_name}_SOURCE_DIR} ${${lib_name}_BINARY_DIR} CACHE INTERNAL "" FORCE)
+  set(${lib_name}_INCLUDE_DIRS
+    ${${lib_name}_SOURCE_DIR}
+    ${${lib_name}_BINARY_DIR}
+    # Ensure generated AUTOUIC headers (ui_*.h) are discoverable.
+    #
+    # By default CMake writes them to:
+    #   <AUTOGEN_BUILD_DIR>/include
+    #
+    # where AUTOGEN_BUILD_DIR defaults to:
+    #   <target-binary-dir>/<target-name>_autogen
+    #
+    # References:
+    # - https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html#autouic
+    # - https://cmake.org/cmake/help/latest/prop_tgt/AUTOGEN_BUILD_DIR.html
+    ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include
+    CACHE INTERNAL "" FORCE)
 
   include_directories(
     ${${lib_name}_INCLUDE_DIRS}
@@ -125,7 +140,6 @@ macro(slicerMacroBuildLoadableModule)
   # Sources
   #-----------------------------------------------------------------------------
   set(LOADABLEMODULE_MOC_OUTPUT)
-  set(LOADABLEMODULE_UI_CXX)
   set(LOADABLEMODULE_QRC_SRCS)
   if(NOT EXISTS ${Slicer_LOGOS_RESOURCE})
     message("Warning, Slicer_LOGOS_RESOURCE doesn't exist: ${Slicer_LOGOS_RESOURCE}")
@@ -133,7 +147,6 @@ macro(slicerMacroBuildLoadableModule)
 
     set(_moc_options OPTIONS -DSlicer_HAVE_QT5)
     QT5_WRAP_CPP(LOADABLEMODULE_MOC_OUTPUT ${LOADABLEMODULE_MOC_SRCS} ${_moc_options})
-    QT5_WRAP_UI(LOADABLEMODULE_UI_CXX ${LOADABLEMODULE_UI_SRCS})
     if(DEFINED LOADABLEMODULE_RESOURCES)
       QT5_ADD_RESOURCES(LOADABLEMODULE_QRC_SRCS ${LOADABLEMODULE_RESOURCES})
     endif()
@@ -141,7 +154,6 @@ macro(slicerMacroBuildLoadableModule)
 
   set_source_files_properties(
     ${LOADABLEMODULE_SRCS} # For now, let's prevent the module widget from being wrapped
-    ${LOADABLEMODULE_UI_CXX}
     ${LOADABLEMODULE_MOC_OUTPUT}
     ${LOADABLEMODULE_QRC_SRCS}
     WRAP_EXCLUDE
@@ -157,7 +169,6 @@ macro(slicerMacroBuildLoadableModule)
     )
 
   source_group("Generated" FILES
-    ${LOADABLEMODULE_UI_CXX}
     ${LOADABLEMODULE_MOC_OUTPUT}
     ${LOADABLEMODULE_QRC_SRCS}
     ${dynamicHeaders}
@@ -191,9 +202,24 @@ macro(slicerMacroBuildLoadableModule)
   add_library(${lib_name}
     ${LOADABLEMODULE_SRCS}
     ${LOADABLEMODULE_MOC_OUTPUT}
-    ${LOADABLEMODULE_UI_CXX}
     ${LOADABLEMODULE_QRC_SRCS}
     ${QM_OUTPUT_FILES}
+    )
+
+  # Configure CMake Qt automatic code generation
+  set(uic_search_paths)
+  foreach(ui_src IN LISTS LOADABLEMODULE_UI_SRCS)
+    if(NOT IS_ABSOLUTE ${ui_src})
+      set(ui_src "${CMAKE_CURRENT_SOURCE_DIR}/${ui_src}")
+    endif()
+    get_filename_component(ui_path ${ui_src} PATH)
+    list(APPEND uic_search_paths ${ui_path})
+  endforeach()
+  list(REMOVE_DUPLICATES uic_search_paths)
+
+  set_target_properties(${lib_name} PROPERTIES
+    AUTOUIC ON
+    AUTOUIC_SEARCH_PATHS "${uic_search_paths}"
     )
 
   # Set loadable modules output path

CMake/SlicerMacroBuildModuleQtLibrary.cmake
@@ -73,6 +73,18 @@ macro(SlicerMacroBuildModuleQtLibrary)
     ${${MODULEQTLIBRARY_NAME}_INCLUDE_DIRS}
     ${CMAKE_CURRENT_SOURCE_DIR}
     ${CMAKE_CURRENT_BINARY_DIR}
+    # Ensure generated AUTOUIC headers (ui_*.h) are discoverable.
+    #
+    # By default CMake writes them to:
+    #   <AUTOGEN_BUILD_DIR>/include
+    #
+    # where AUTOGEN_BUILD_DIR defaults to:
+    #   <target-binary-dir>/<target-name>_autogen
+    #
+    # References:
+    # - https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html#autouic
+    # - https://cmake.org/cmake/help/latest/prop_tgt/AUTOGEN_BUILD_DIR.html
+    ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include
     )
   # Since module developer may have already set the variable to some
   # specific values in the module CMakeLists.txt, we make sure to
@@ -113,7 +125,6 @@ macro(SlicerMacroBuildModuleQtLibrary)
   # Sources
   #-----------------------------------------------------------------------------
   set(MODULEQTLIBRARY_MOC_OUTPUT)
-  set(MODULEQTLIBRARY_UI_CXX)
   set(MODULEQTLIBRARY_QRC_SRCS)
   if(NOT EXISTS ${Slicer_LOGOS_RESOURCE})
     message("Warning, Slicer_LOGOS_RESOURCE doesn't exist: ${Slicer_LOGOS_RESOURCE}")
@@ -121,14 +132,12 @@ macro(SlicerMacroBuildModuleQtLibrary)
 
     set(_moc_options OPTIONS -DSlicer_HAVE_QT5)
     QT5_WRAP_CPP(MODULEQTLIBRARY_MOC_OUTPUT ${MODULEQTLIBRARY_MOC_SRCS} ${_moc_options})
-    QT5_WRAP_UI(MODULEQTLIBRARY_UI_CXX ${MODULEQTLIBRARY_UI_SRCS})
     if(DEFINED MODULEQTLIBRARY_RESOURCES AND NOT MODULEQTLIBRARY_RESOURCES STREQUAL "")
       QT5_ADD_RESOURCES(MODULEQTLIBRARY_QRC_SRCS ${MODULEQTLIBRARY_RESOURCES})
     endif()
     QT5_ADD_RESOURCES(MODULEQTLIBRARY_QRC_SRCS ${Slicer_LOGOS_RESOURCE})
 
   set_source_files_properties(
-    ${MODULEQTLIBRARY_UI_CXX}
     ${MODULEQTLIBRARY_MOC_OUTPUT}
     ${MODULEQTLIBRARY_QRC_SRCS}
     WRAP_EXCLUDE
@@ -144,7 +153,6 @@ macro(SlicerMacroBuildModuleQtLibrary)
     )
 
   source_group("Generated" FILES
-    ${MODULEQTLIBRARY_UI_CXX}
     ${MODULEQTLIBRARY_MOC_OUTPUT}
     ${MODULEQTLIBRARY_QRC_SRCS}
     ${dynamicHeaders}
@@ -156,8 +164,23 @@ macro(SlicerMacroBuildModuleQtLibrary)
   add_library(${lib_name}
     ${MODULEQTLIBRARY_SRCS}
     ${MODULEQTLIBRARY_MOC_OUTPUT}
-    ${MODULEQTLIBRARY_UI_CXX}
     ${MODULEQTLIBRARY_QRC_SRCS}
+    )
+
+  # Configure CMake Qt automatic code generation
+  set(uic_search_paths)
+  foreach(ui_src IN LISTS MODULEQTLIBRARY_UI_SRCS)
+    if(NOT IS_ABSOLUTE ${ui_src})
+      set(ui_src "${CMAKE_CURRENT_SOURCE_DIR}/${ui_src}")
+    endif()
+    get_filename_component(ui_path ${ui_src} PATH)
+    list(APPEND uic_search_paths ${ui_path})
+  endforeach()
+  list(REMOVE_DUPLICATES uic_search_paths)
+
+  set_target_properties(${lib_name} PROPERTIES
+    AUTOUIC ON
+    AUTOUIC_SEARCH_PATHS "${uic_search_paths}"
     )
 
   # Set qt loadable modules output path

Libs/MRML/Widgets/CMakeLists.txt
@@ -390,7 +390,6 @@ endif()
     list(APPEND _moc_options -DMRML_WIDGETS_HAVE_WEBENGINE_SUPPORT)
   endif()
   QT5_WRAP_CPP(MRMLWidgets_MOC_CXX ${MRMLWidgets_MOC_SRCS} ${_moc_options})
-  QT5_WRAP_UI(MRMLWidgets_UI_CXX ${MRMLWidgets_UI_SRCS})
   QT5_ADD_RESOURCES(MRMLWidgets_QRC_CXX ${MRMLWidgets_QRC_SRCS})
 
 source_group("Resources" FILES
@@ -400,7 +399,6 @@ source_group("Resources" FILES
 
 source_group("Generated" FILES
   ${MRMLWidgets_MOC_CXX}
-  ${MRMLWidgets_UI_CXX}
   ${MRMLWidgets_QRC_CXX}
   )
 
@@ -462,9 +460,15 @@ set(targets_to_export ${lib_name})
 add_library(${lib_name}
   ${MRMLWidgets_SRCS}
   ${MRMLWidgets_MOC_CXX}
-  ${MRMLWidgets_UI_CXX}
   ${MRMLWidgets_QRC_CXX}
   ${QM_OUTPUT_FILES}
+  )
+
+# Configure CMake Qt automatic code generation
+set(uic_search_paths "${CMAKE_CURRENT_SOURCE_DIR}/Resources/UI/")
+set_target_properties(${lib_name} PROPERTIES
+  AUTOUIC ON
+  AUTOUIC_SEARCH_PATHS "${uic_search_paths}"
   )
 
 set(MRMLWidgets_LIBRARIES
@@ -550,5 +554,19 @@ endif()
 # --------------------------------------------------------------------------
 # Set INCLUDE_DIRS variable
 # --------------------------------------------------------------------------
-set(${PROJECT_NAME}_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR}
+set(${PROJECT_NAME}_INCLUDE_DIRS
+  ${CMAKE_CURRENT_SOURCE_DIR}
+  ${CMAKE_CURRENT_BINARY_DIR}
+  # Ensure generated AUTOUIC headers (ui_*.h) are discoverable.
+  #
+  # By default CMake writes them to:
+  #   <AUTOGEN_BUILD_DIR>/include
+  #
+  # where AUTOGEN_BUILD_DIR defaults to:
+  #   <target-binary-dir>/<target-name>_autogen
+  #
+  # References:
+  # - https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html#autouic
+  # - https://cmake.org/cmake/help/latest/prop_tgt/AUTOGEN_BUILD_DIR.html
+  ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include
   CACHE INTERNAL "${PROJECT_NAME} include dirs" FORCE)

CMakeLists.txt
@@ -854,6 +854,11 @@ set(CMAKE_MODULE_PATH
   )
 
 #-----------------------------------------------------------------------------
+# CMake Qt automatic code generation
+#-----------------------------------------------------------------------------
+set_property(GLOBAL PROPERTY AUTOGEN_SOURCE_GROUP "Generated")
+
+#-----------------------------------------------------------------------------
 # Folders
 #-----------------------------------------------------------------------------
 set_property(GLOBAL PROPERTY USE_FOLDERS ${Slicer_USE_FOLDERS})

Modules/Loadable/Markups/Widgets/Testing/Cxx/CMakeLists.txt
@@ -28,13 +28,12 @@ set(KIT_TEST_GENERATE_MOC_SRCS
 
 set(_moc_options OPTIONS -DMRML_WIDGETS_HAVE_QT5)
 QT5_WRAP_CPP(Tests_MOC_CXX ${Tests_MOC_SRCS} ${_moc_options})
-QT5_WRAP_UI(Test_UI_CXX ${Test_UI_SRCS})
 QT5_GENERATE_MOCS(${KIT_TEST_GENERATE_MOC_SRCS} USE_MOC_EXTENSION)
 
 include_directories( ${CMAKE_CURRENT_BINARY_DIR})
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR})
 
-ctk_add_executable_utf8(${KIT}CxxTests ${Test_UI_CXX} ${Tests} ${Tests_MOC_CXX} ${Tests_UtilityFiles})
+ctk_add_executable_utf8(${KIT}CxxTests ${Tests} ${Tests_MOC_CXX} ${Tests_UtilityFiles})
 target_link_libraries(${KIT}CxxTests ${KIT})
 
 set_target_properties(${KIT}CxxTests PROPERTIES FOLDER "Module-${KIT}")

CMake/SlicerMacroBuildApplication.cmake
@@ -144,13 +144,11 @@ macro(slicerMacroBuildAppLibrary)
   # --------------------------------------------------------------------------
     set(_moc_options OPTIONS -DSlicer_HAVE_QT5)
     QT5_WRAP_CPP(SLICERAPPLIB_MOC_OUTPUT ${SLICERAPPLIB_MOC_SRCS} ${_moc_options})
-    QT5_WRAP_UI(SLICERAPPLIB_UI_CXX ${SLICERAPPLIB_UI_SRCS})
     if(DEFINED SLICERAPPLIB_RESOURCES)
       QT5_ADD_RESOURCES(SLICERAPPLIB_QRC_SRCS ${SLICERAPPLIB_RESOURCES})
     endif()
 
   set_source_files_properties(
-    ${SLICERAPPLIB_UI_CXX}
     ${SLICERAPPLIB_MOC_OUTPUT}
     ${SLICERAPPLIB_QRC_SRCS}
     WRAP_EXCLUDE
@@ -166,7 +164,6 @@ macro(slicerMacroBuildAppLibrary)
   )
 
   source_group("Generated" FILES
-    ${SLICERAPPLIB_UI_CXX}
     ${SLICERAPPLIB_MOC_OUTPUT}
     ${SLICERAPPLIB_QRC_SRCS}
     ${dynamicHeaders}
@@ -202,10 +199,26 @@ macro(slicerMacroBuildAppLibrary)
   add_library(${lib_name}
     ${SLICERAPPLIB_SRCS}
     ${SLICERAPPLIB_MOC_OUTPUT}
-    ${SLICERAPPLIB_UI_CXX}
     ${SLICERAPPLIB_QRC_SRCS}
     ${QM_OUTPUT_FILES}
     )
+
+  # Configure CMake Qt automatic code generation
+  set(uic_search_paths)
+  foreach(ui_src IN LISTS SLICERAPPLIB_UI_SRCS)
+    if(NOT IS_ABSOLUTE ${ui_src})
+      set(ui_src "${CMAKE_CURRENT_SOURCE_DIR}/${ui_src}")
+    endif()
+    get_filename_component(ui_path ${ui_src} PATH)
+    list(APPEND uic_search_paths ${ui_path})
+  endforeach()
+  list(REMOVE_DUPLICATES uic_search_paths)
+
+  set_target_properties(${lib_name} PROPERTIES
+    AUTOUIC ON
+    AUTOUIC_SEARCH_PATHS "${uic_search_paths}"
+    )
+
   set_target_properties(${lib_name} PROPERTIES LABELS ${lib_name})
 
   # Apply user-defined properties to the library target.

commit 9b986c7bdef9d90469b7234040ad559a1b3ef36b

COMP: Fix AUTOUIC include dir for multi-config generators
Add the autogen include path with the per-config suffix when
GENERATOR_IS_MULTI_CONFIG is true:

  <binary>/<target>_autogen/include_$<CONFIG>

This ensures ui_*.h headers are found under VS/Xcode/Ninja
Multi-Config, while preserving the single-config layout:

  <binary>/<target>_autogen/include

It fixes regression introduced in 8a44717a61b ("COMP: Enable AUTOUIC to
simplify UI generation and prep for Qt6", 2025-11-02)

CMake/SlicerMacroBuildBaseQtLibrary.cmake
@@ -116,13 +116,20 @@ macro(SlicerMacroBuildBaseQtLibrary)
   #-----------------------------------------------------------------------------
   # Update Slicer_Base_INCLUDE_DIRS
   #-----------------------------------------------------------------------------
+  get_property(_isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+
   set(Slicer_Base_INCLUDE_DIRS ${Slicer_Base_INCLUDE_DIRS}
     ${CMAKE_CURRENT_SOURCE_DIR}
     ${CMAKE_CURRENT_BINARY_DIR}
     # Ensure generated AUTOUIC headers (ui_*.h) are discoverable.
     #
     # By default CMake writes them to:
-    #   <AUTOGEN_BUILD_DIR>/include
+    #
+    #   - Single-config generators (Ninja/Makefiles):
+    #       <AUTOGEN_BUILD_DIR>/include
+    #
+    #   - Multi-config generators (VS, Xcode, Ninja Multi-Config):
+    #       <AUTOGEN_BUILD_DIR>/include_<CONFIG>
     #
     # where AUTOGEN_BUILD_DIR defaults to:
     #   <target-binary-dir>/<target-name>_autogen
@@ -130,7 +137,7 @@ macro(SlicerMacroBuildBaseQtLibrary)
     # References:
     # - https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html#autouic
     # - https://cmake.org/cmake/help/latest/prop_tgt/AUTOGEN_BUILD_DIR.html
-    ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include
+    ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include$<$<BOOL:${_isMultiConfig}>:_$<CONFIG>>
     CACHE INTERNAL "Slicer Base includes" FORCE)
 
   #-----------------------------------------------------------------------------

CMake/SlicerMacroBuildLoadableModule.cmake
@@ -92,13 +92,20 @@ macro(slicerMacroBuildLoadableModule)
     set(${lib_name}_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR} CACHE INTERNAL "" FORCE)
   endif()
 
+  get_property(_isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+
   set(${lib_name}_INCLUDE_DIRS
     ${${lib_name}_SOURCE_DIR}
     ${${lib_name}_BINARY_DIR}
     # Ensure generated AUTOUIC headers (ui_*.h) are discoverable.
     #
     # By default CMake writes them to:
-    #   <AUTOGEN_BUILD_DIR>/include
+    #
+    #   - Single-config generators (Ninja/Makefiles):
+    #       <AUTOGEN_BUILD_DIR>/include
+    #
+    #   - Multi-config generators (VS, Xcode, Ninja Multi-Config):
+    #       <AUTOGEN_BUILD_DIR>/include_<CONFIG>
     #
     # where AUTOGEN_BUILD_DIR defaults to:
     #   <target-binary-dir>/<target-name>_autogen
@@ -106,7 +113,7 @@ macro(slicerMacroBuildLoadableModule)
     # References:
     # - https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html#autouic
     # - https://cmake.org/cmake/help/latest/prop_tgt/AUTOGEN_BUILD_DIR.html
-    ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include
+    ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include$<$<BOOL:${_isMultiConfig}>:_$<CONFIG>>
     CACHE INTERNAL "" FORCE)
 
   include_directories(

CMake/SlicerMacroBuildModuleQtLibrary.cmake
@@ -69,6 +69,8 @@ macro(SlicerMacroBuildModuleQtLibrary)
   # --------------------------------------------------------------------------
   # Set <MODULEQTLIBRARY_NAME>_INCLUDE_DIRS
   # --------------------------------------------------------------------------
+  get_property(_isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+
   set(_include_dirs
     ${${MODULEQTLIBRARY_NAME}_INCLUDE_DIRS}
     ${CMAKE_CURRENT_SOURCE_DIR}
@@ -76,7 +78,12 @@ macro(SlicerMacroBuildModuleQtLibrary)
     # Ensure generated AUTOUIC headers (ui_*.h) are discoverable.
     #
     # By default CMake writes them to:
-    #   <AUTOGEN_BUILD_DIR>/include
+    #
+    #   - Single-config generators (Ninja/Makefiles):
+    #       <AUTOGEN_BUILD_DIR>/include
+    #
+    #   - Multi-config generators (VS, Xcode, Ninja Multi-Config):
+    #       <AUTOGEN_BUILD_DIR>/include_<CONFIG>
     #
     # where AUTOGEN_BUILD_DIR defaults to:
     #   <target-binary-dir>/<target-name>_autogen
@@ -84,7 +91,7 @@ macro(SlicerMacroBuildModuleQtLibrary)
     # References:
     # - https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html#autouic
     # - https://cmake.org/cmake/help/latest/prop_tgt/AUTOGEN_BUILD_DIR.html
-    ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include
+    ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include$<$<BOOL:${_isMultiConfig}>:_$<CONFIG>>
     )
   # Since module developer may have already set the variable to some
   # specific values in the module CMakeLists.txt, we make sure to

Libs/MRML/Widgets/CMakeLists.txt
@@ -443,13 +443,20 @@ endif()
 # --------------------------------------------------------------------------
 # Set INCLUDE_DIRS variable
 # --------------------------------------------------------------------------
+get_property(_isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+
 set(${PROJECT_NAME}_INCLUDE_DIRS
   ${CMAKE_CURRENT_SOURCE_DIR}
   ${CMAKE_CURRENT_BINARY_DIR}
   # Ensure generated AUTOUIC headers (ui_*.h) are discoverable.
   #
   # By default CMake writes them to:
-  #   <AUTOGEN_BUILD_DIR>/include
+  #
+  #   - Single-config generators (Ninja/Makefiles):
+  #       <AUTOGEN_BUILD_DIR>/include
+  #
+  #   - Multi-config generators (VS, Xcode, Ninja Multi-Config):
+  #       <AUTOGEN_BUILD_DIR>/include_<CONFIG>
   #
   # where AUTOGEN_BUILD_DIR defaults to:
   #   <target-binary-dir>/<target-name>_autogen
@@ -457,5 +464,5 @@ set(${PROJECT_NAME}_INCLUDE_DIRS
   # References:
   # - https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html#autouic
   # - https://cmake.org/cmake/help/latest/prop_tgt/AUTOGEN_BUILD_DIR.html
-  ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include
+  ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}_autogen/include$<$<BOOL:${_isMultiConfig}>:_$<CONFIG>>
   CACHE INTERNAL "${PROJECT_NAME} include dirs" FORCE)