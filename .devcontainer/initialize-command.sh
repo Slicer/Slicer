af6826bd43015f4fa92786155e41fa78088b0062
Ruffin White <roxfoxpox@gmail.com>
2025-11-19 06:32:53 +0000

    ENH: Add devcontainer configuration
    with Dockerfile, build scripts, and caching setup

---
+++ .devcontainer/initialize-command.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Immediately catch all errors
+set -eo pipefail
+
+# Uncomment for debugging
+# set -x
+# env
+
+# Use first argument as target name
+target=$1
+
+# Intialize directories for personalized bind mounts
+mkdir -p /var/tmp/home/slicer
+
+################################################################################
+# MARK: Build image - bake image using local Dockerfile for local dev container
+# unset DEV_FROM_STAGE
+################################################################################
+
+# Bake the target and export locally to static tag
+docker buildx bake --load \
+    --file .devcontainer/docker-bake.hcl \
+    --set $target.tags=slicer:devcontainer \
+    $target
