621e6f4725de147d4ae55bbecb31f10922da94a7
Thibault Pelletier <thibault.pelletier@kitware.com>
2026-03-25 07:26:50 +0100

    BUG: Fix header install in wheel
    - Add missing headers to wheel SDK install

--- Modules/Loadable/LayerDM/MRML/wheel.cmake
+++ Modules/Loadable/LayerDM/MRML/wheel.cmake
@@ -7,7 +7,12 @@ set(classes
   vtkMRMLLayerDMPythonUtil
 )
 
+set(headers
+  vtkSlicerLayerDMModuleMRMLExport.h
+)
+
 vtk_module_add_module(SlicerLayerDM::MRML
   EXPORT_MACRO_PREFIX VTK_SLICER_LAYERDM_MODULE_MRML
   CLASSES ${classes}
+  HEADERS ${headers}
 )
