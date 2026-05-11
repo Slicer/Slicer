Slicer/Slicer

commit 74d1fa0b9479c304d6dc18050ed94252e833aedb

ENH: Add API to set string using std::string instead of char*
Following trends in VTK API and to allow safer code with simpler syntax, this commit adds possibility to use std::string in set functions (without adding .c_str()).

Libs/MRML/Core/vtkMRMLStorageNode.h
@@ -82,6 +82,7 @@ public:
   /// \sa ReadData(), WriteData()
   vtkSetStringMacro(FileName);
   vtkGetStringMacro(FileName);
+  void SetFileName(const std::string& fileName) { this->SetFileName(fileName.c_str()); };
 
   /// Return complete file extension for the specified filename.
   /// Longest matched extension will be returned (.seg.nrrd will be returned
@@ -182,7 +183,7 @@ public:
   /// the file content then it may return with much higher confidence values.
   /// Subclasses should implement this method.
   virtual int SupportedFileType(const char* fileName);
-
+  int SupportedFileType(const std::string& fileName) { return this->SupportedFileType(fileName.c_str()); }
   ///
   /// Get all the supported read file types
   /// Subclasses should overwrite InitializeSupportedReadFileTypes().
@@ -210,6 +211,8 @@ public:
   ///
   /// Add in another file name to the list of file names
   unsigned int AddFileName(const char* fileName);
+  unsigned int AddFileName(const std::string& fileName) { return this->AddFileName(fileName.c_str()); };
+
   ///
   /// Clear the array of file names
   void ResetFileNameList();
@@ -317,6 +320,7 @@ public:
   /// Remove supported extension from filename.
   /// If filename is not specified then the current FileName will be used.
   std::string GetFileNameWithoutExtension(const char* fileName = nullptr);
+  std::string GetFileNameWithoutExtension(const std::string& fileName) { return GetFileNameWithoutExtension(fileName.c_str()); }
 
   /// Compression parameter that is used to save the node
   vtkSetMacro(CompressionParameter, std::string);

Libs/MRML/Core/vtkDataTransfer.h
@@ -17,8 +17,10 @@ public:
   void PrintSelf(ostream& os, vtkIndent indent) override;
   vtkGetStringMacro(SourceURI);
   vtkSetStringMacro(SourceURI);
+  void SetSourceURI(const std::string& sourceURI) { this->SetSourceURI(sourceURI.c_str()); }
   vtkGetStringMacro(DestinationURI);
   vtkSetStringMacro(DestinationURI);
+  void SetDestinationURI(const std::string& destinationURI) { this->SetDestinationURI(destinationURI.c_str()); }
   vtkGetObjectMacro(Handler, vtkURIHandler);
   virtual void SetHandler(vtkURIHandler* uriHandler);
   vtkGetMacro(TransferType, int);

Libs/MRML/Core/vtkMRMLScene.h
@@ -67,6 +67,7 @@ public:
 
   /// Set URL (file name) of the scene
   void SetURL(const char* url);
+  void SetURL(const std::string& url) { return this->SetURL(url.c_str()); };
 
   /// Get URL (file name) of the scene
   const char* GetURL();

Libs/MRML/Core/vtkURIHandler.h
@@ -24,7 +24,9 @@ public:
   /// virtual methods to be defined in subclasses.
   /// (Maybe these should be defined to handle default file operations)
   virtual void StageFileRead(const char* source, const char* destination);
+  void StageFileRead(const std::string& source, const std::string& destination) { this->StageFileRead(source.c_str(), destination.c_str()); }
   virtual void StageFileWrite(const char* source, const char* destination);
+  void StageFileWrite(const std::string& source, const std::string& destination) { this->StageFileWrite(source.c_str(), destination.c_str()); }
 
   ///
   /// various Read/Write method footprints useful to redefine in specific handlers.

commit 66ca3b3511380f45635e4e07e7356d1a4cb0ec87

BUG: Fix GetFileNameWithoutExtension() failing when called with no arguments
The std::string overload added in https://github.com/Slicer/Slicer/commit/74d1fa0b9479c304d6dc18050ed94252e833aedb caused the VTK Python wrapper
to see two overloads of GetFileNameWithoutExtension, breaking the default
argument on the const char* version. As a result, calling the method
with no arguments from Python raised:

  TypeError: GetFileNameWithoutExtension() takes exactly 1 argument (0 given)

Guard the std::string overload with #ifndef __VTK_WRAP__ so it is hidden
from the VTK wrapper while remaining available to C++ callers.

Libs/MRML/Core/vtkMRMLStorageNode.h
@@ -320,7 +320,11 @@ public:
   /// Remove supported extension from filename.
   /// If filename is not specified then the current FileName will be used.
   std::string GetFileNameWithoutExtension(const char* fileName = nullptr);
+// Make sure that the Python wrapper uses the char* version
+// (because the std::string& version does not have a default argument)
+#ifndef __VTK_WRAP__
   std::string GetFileNameWithoutExtension(const std::string& fileName) { return GetFileNameWithoutExtension(fileName.c_str()); }
+#endif
 
   /// Compression parameter that is used to save the node
   vtkSetMacro(CompressionParameter, std::string);