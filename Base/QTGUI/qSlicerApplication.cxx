4aa40e162d0b761dc406810bba9a2e6428365f71
Andras Lasso <lasso@queensu.ca>
2026-05-17 17:25:51 -0400

    ENH: Initialize application logger earlier
    Moved application logger initialization earlier in application startup, to capture messages that various application objects (such as the CacheManager) may log when they are created.
    The ctkErrorLogFDMessageHandler cannot be initialized before the pythonManager is initialized (it would make it hang), therefore initialization of that message handler is left at its original location.
    
    see #9155

--- Base/QTGUI/qSlicerApplication.cxx
+++ Base/QTGUI/qSlicerApplication.cxx
@@ -332,6 +332,18 @@ void qSlicerApplicationPrivate::init()
   }
 #endif
 
+  //----------------------------------------------------------------------------
+  // Instantiate ErrorLogModel
+  //----------------------------------------------------------------------------
+  this->ErrorLogModel = QSharedPointer<ctkErrorLogModel>(new ctkErrorLogModel);
+  this->ErrorLogModel->setLogEntryGrouping(true);
+  this->ErrorLogModel->setTerminalOutputs(this->CoreCommandOptions->disableTerminalOutputs() ? ctkErrorLogTerminalOutput::None : ctkErrorLogTerminalOutput::All);
+  this->ErrorLogModel->registerMsgHandler(new ctkErrorLogQtMessageHandler);
+  this->ErrorLogModel->registerMsgHandler(new ctkErrorLogStreamMessageHandler);
+  this->ErrorLogModel->registerMsgHandler(new ctkITKErrorLogMessageHandler);
+  this->ErrorLogModel->registerMsgHandler(new ctkVTKErrorLogMessageHandler);
+  this->ErrorLogModel->setAllMsgHandlerEnabled(true);
+
   this->Superclass::init();
 
 #ifdef Slicer_USE_PYTHONQT
@@ -375,24 +387,16 @@ void qSlicerApplicationPrivate::init()
   this->ToolTipTrapper->setToolTipsTrapped(false);
   this->ToolTipTrapper->setToolTipsWordWrapped(true);
 
-  //----------------------------------------------------------------------------
-  // Instantiate ErrorLogModel
-  //----------------------------------------------------------------------------
-  this->ErrorLogModel = QSharedPointer<ctkErrorLogModel>(new ctkErrorLogModel);
-  this->ErrorLogModel->setLogEntryGrouping(true);
-  this->ErrorLogModel->setTerminalOutputs(this->CoreCommandOptions->disableTerminalOutputs() ? ctkErrorLogTerminalOutput::None : ctkErrorLogTerminalOutput::All);
+  // ctkErrorLogFDMessageHandler redirects stdout and stderr to the application log.
+  // It cannot be enabled earlier, because it would make Python manager initialization hang.
 #if defined(Q_OS_WIN32) && !defined(Slicer_BUILD_WIN32_CONSOLE)
   // Must not register ctkErrorLogFDMessageHandler when building a window-based
   // (non-console) application because this handler would not
   // let the application to quit when the last window is closed.
 #else
   this->ErrorLogModel->registerMsgHandler(new ctkErrorLogFDMessageHandler);
+  this->ErrorLogModel->setMsgHandlerEnabled(ctkErrorLogFDMessageHandler::HandlerName, true);
 #endif
-  this->ErrorLogModel->registerMsgHandler(new ctkErrorLogQtMessageHandler);
-  this->ErrorLogModel->registerMsgHandler(new ctkErrorLogStreamMessageHandler);
-  this->ErrorLogModel->registerMsgHandler(new ctkITKErrorLogMessageHandler);
-  this->ErrorLogModel->registerMsgHandler(new ctkVTKErrorLogMessageHandler);
-  this->ErrorLogModel->setAllMsgHandlerEnabled(true);
 
 #ifdef Slicer_USE_PYTHONQT
   // Make ITK, VTK, Qt error messages show up in the Python console
