a0cd9e9d421ac9b59c2940e69416c206bee353b1
Hans J. Johnson <hans-johnson@uiowa.edu>
2026-06-05 07:46:35 -0500

    COMP: Drop dead CMake-version conditionals guaranteed by the 3.28 floor
    The generator-expression $<CONFIG> branch (CMake >= 3.20) and the
    ExternalProject BUILD_JOB_SERVER_AWARE branch (CMake >= 3.28) are now
    always taken; collapse both conditionals to their NEW-behavior bodies.

--- CMake/SlicerMacroBuildScriptedCLI.cmake
+++ CMake/SlicerMacroBuildScriptedCLI.cmake
@@ -44,11 +44,7 @@ macro(SlicerMacroBuildScriptedCLI)
   set(binary_dir "${CMAKE_BINARY_DIR}/${Slicer_CLIMODULES_BIN_DIR}")
 
   if(CMAKE_CONFIGURATION_TYPES)
-    if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.20")
-      set(binary_dir "${binary_dir}/$<CONFIG>")
-    else()
-      set(binary_dir "${binary_dir}/${CMAKE_CFG_INTDIR}")
-    endif()
+    set(binary_dir "${binary_dir}/$<CONFIG>")
   endif()
 
   add_custom_command(
